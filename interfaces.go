0627b440545a861dc74fb47e62aea845f37b2484
Maxwell Krohn <themax@gmail.com>
2014-09-26 20:38:32 -0400

    some boilerplate

---
+++ interfaces.go
@@ -0,0 +1,34 @@
+
+package libkbgo
+
+type CommandLine interface {
+
+
+}
+
+type Server interface {
+
+}
+
+type LocalCache interface {
+
+
+}
+
+type Config interface {
+
+}
+
+type HttpRequest interface {
+
+
+}
+
+type Keychain interface {
+
+}
+
+type ProofCheckers interface {
+
+
+}
