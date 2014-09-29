571f2f3395a8b9634167ff696182c54f5e08178c
Maxwell Krohn <themax@gmail.com>
2014-09-28 20:28:35 -0400

    awesome tests are working

--- assertion_test.go
+++ assertion_test.go
@@ -17,5 +17,40 @@ func TestSuccess1(t *testing.T) {
 	} else if ! expr.MatchSet(*proofs) {
 		t.Errorf("Should have matched")
 	}
+}
 
+func TestSucess2(t *testing.T) {
+	a := "web://maxk.org && (https://foo.com || http://bar.com) && (twitter://bb || max || fingerprint://aabbcc)"	
+	proofsets := []ProofSet {
+		*NewProofSet([]Proof{
+			{"dns", "maxk.org" },
+			{"http", "bar.com" },
+			{"twitter", "bb" },
+		}),
+		*NewProofSet([]Proof{
+			{"https", "maxk.org" },
+			{"http", "foo.com" },
+			{"keybase", "max" },
+		}),
+		*NewProofSet([]Proof{
+			{"http", "maxk.org" },
+			{"https", "foo.com" },
+			{"fingerprint", "00aabbcc" },
+		}),
+		*NewProofSet([]Proof{
+			{"http", "maxk.org" },
+			{"https", "foo.com" },
+			{"twitter", "bb" },
+		}),
+	}
+	expr, err := AssertionParse(a)
+	if err != nil {
+		t.Errorf("Error parsing %s: %s", a, err.Error())
+	} else {
+		for i, proofset := range(proofsets) {
+			if !expr.MatchSet(proofset) {
+				t.Errorf("proofset %d failed to match", i)
+			}
+		}
+	}
 }
