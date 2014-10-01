3d3050096f6418f05d186d90fed517468c9a9d2b
Maxwell Krohn <themax@gmail.com>
2014-10-01 11:58:46 -0400

    work in progress

--- cmdline.go
+++ cmdline.go
@@ -3,33 +3,28 @@ package libkbgo
 
 import (
 	"github.com/codegangsta/cli"
-	"fmt"
 )
 
 type PosixCmdLine struct {
-
+	app *cli.App
+	ctx *cli.Context
 }
+
+func (p PosixCmdLine) GetHome() string { return p.ctx.String("home"); }
 
 func (p *PosixCmdLine) Parse(args []string) error {
-	ch := make(chan error)
-	go p.parse(ch, args)
-	err := <- ch
-	return err
-}
-
-func (p *PosixCmdLine) parse(ch chan error, args []string) {
 	app := cli.NewApp()
 	app.Name = "keybase"
 	app.Usage = "control keybase either with one-off commands, or enable a background daemon"
 	app.Flags = []cli.Flag {
 		cli.StringFlag {
-			Name : "h, home",
+			Name : "home, H",
 			Usage : "specify an (alternate) home directory",
 		},
 	}
-	app.Action = func(c *cli.Context) {
-		fmt.Println()
-		ch <- nil
+	app.Action = func (c *cli.Context) {
+		p.ctx = c
 	}
-	app.Run(args)
+	p.app = app
+	return app.Run(args)
 }
