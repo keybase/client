73d9115324e543515328392b568db7f0f36030a1
Maxwell Krohn <themax@gmail.com>
2014-10-04 10:37:10 -0400

    hacking

--- main.go
+++ main.go
@@ -30,15 +30,14 @@ func testLogging() {
 	G.Log.Error("hello error")
 }
 
-
 func main() {
 	G.Init()
 	cmd := parseArgs()
 	G.ConfigureLogging()
 	if cmd.UseConfig() { G.ConfigureConfig() }
 	if cmd.UseKeyring() { G.ConfigureKeyring() }
+	if cmd.UseAPI() { G.ConfigureAPI() }
 	G.StartupMessage()
-
 	testLogging()
 	if err := cmd.Run(); err != nil { G.Log.Fatal(err.Error()) }
 }
