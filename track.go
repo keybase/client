9d953f61cc080f2dd813616d07081d4656775171
Maxwell Krohn <themax@gmail.com>
2014-11-01 10:20:39 -0400

    refactor track code out

--- track.go
+++ track.go
@@ -1,5 +1,11 @@
 package libkb
 
+import (
+	"fmt"
+	"github.com/keybase/go-jsonw"
+)
+
+
 // Can either be a RemoteProofChainLink or one of the identities
 // listed in a tracking statement
 type TrackIdComponent interface {
@@ -24,3 +30,83 @@ func (a TrackSet) SubsetOf(b TrackSet) bool {
 func (a TrackSet) Equal(b TrackSet) bool {
 	return ((len(a) == len(b)) && a.SubsetOf(b))
 }
+
+//=====================================================================
+
+type TrackEngine struct {
+	TheirName string
+	Them *User
+	Me *User
+	Interactive bool
+	NoSelf bool
+	StrictProofs bool
+}
+
+func (e *TrackEngine) LoadThem() error {
+
+	if e.Them == nil && len(e.TheirName) == 0 {
+		return fmt.Errorf("No 'them' passed to TrackEngine")
+	}
+	if e.Them == nil { 
+		if u, err := LoadUser(LoadUserArg{
+			Name:             e.TheirName,
+			RequirePublicKey: true,
+			Self:             false,
+			LoadSecrets:      false,
+			ForceReload:      false,
+			SkipVerify:       false,
+		}); err != nil {
+			return err
+		} else {
+			e.Them = u				
+		}
+	}	
+	return nil
+}
+
+func (e *TrackEngine) LoadMe() error {
+	if e.Me == nil {
+		if me, err := LoadMe(); err != nil {
+			return err
+		} else {
+			e.Me = me
+		}
+	}
+	return nil
+}
+
+func (e *TrackEngine) Run() error {
+
+	var err error
+
+	if err = e.LoadThem(); err != nil {
+		return err
+	} else if err = e.LoadMe(); err != nil {
+		return err
+	} else if e.NoSelf && e.Me.Equal(*e.Them) {
+		return fmt.Errorf("Cannot track yourself")
+	}
+
+	err = e.Them.Identify()
+	if err != nil {
+		if e.StrictProofs {
+			return err
+		} else {
+			G.Log.Warning("Some proofs failed")
+		}
+	}
+
+	var jw *jsonw.Wrapper 
+	jw, err = e.Me.TrackingProofFor(e.Them)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("%v\n", jw.MarshalPretty())
+	fmt.Printf("%v\n", e.Them.IdTable.MakeTrackSet())
+
+	return nil
+}
+
+
+//=====================================================================
+
