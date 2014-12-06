ecfdde59f50d046982c31c9c51834fb99165d7ea
Maxwell Krohn <themax@gmail.com>
2014-12-05 19:50:07 -0500

    compile it again

--- cmd_prove.go
+++ cmd_prove.go
@@ -140,7 +140,7 @@ func (v *CmdProve) GenerateProof() (err error) {
 	if key, err = G.Keyrings.GetSecretKey("proof signature"); err != nil {
 		return
 	}
-	if v.sig, v.sigId, err = libkb.SimpleSignJson(v.proof, *key); err != nil {
+	if v.sig, v.sigId, err = libkb.SignJson(v.proof, key); err != nil {
 		return
 	}
 	return
