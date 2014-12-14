462d74b3d8592abd3d0817d7cea7eb614d93675c
Maxwell Krohn <themax@gmail.com>
2014-12-14 16:43:42 -0500

    signup

--- signup.go
+++ signup.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"github.com/keybase/protocol/go"
+	"github.com/keybase/go-libkb"
 	"net"
 )
 
@@ -11,7 +12,9 @@ type SignupHandler struct {
 }	
 
 func (h SignupHandler) CheckUsernameAvailable(arg *keybase_1.CheckUsernameAvailableArg, res *keybase_1.CheckUsernameAvailableRes) error {
-	return nil
+	err := libkb.CheckUsernameAvailable(arg.Username)
+	res.Status = libkb.ExportErrorAsStatus(err)
+	return nil	
 }
 func (h SignupHandler) Signup(arg *keybase_1.SignupArg, res *keybase_1.SignupRes) error {
 	return nil
