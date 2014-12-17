2769b40c249b9dc8b4b4aa7e5d882c6559042475
Maxwell Krohn <themax@gmail.com>
2014-12-17 09:23:37 -0500

    simplified

--- signup.go
+++ signup.go
@@ -11,9 +11,9 @@ type SignupHandler struct {
 	conn net.Conn
 }
 
-func (h SignupHandler) CheckUsernameAvailable(arg *keybase_1.CheckUsernameAvailableArg, res *keybase_1.CheckUsernameAvailableRes) error {
-	err := libkb.CheckUsernameAvailable(arg.Username)
-	res.Status = libkb.ExportErrorAsStatus(err)
+func (h SignupHandler) CheckUsernameAvailable(username *string, res *keybase_1.Status) error {
+	err := libkb.CheckUsernameAvailable(*username)
+	*res = libkb.ExportErrorAsStatus(err)
 	return nil
 }
 
@@ -30,26 +30,16 @@ func (h SignupHandler) Signup(arg *keybase_1.SignupArg, res *keybase_1.SignupRes
 	res.Body.PassphraseOk = seres.PassphraseOk
 	res.Body.PostOk = seres.PostOk
 	res.Body.WriteOk = seres.WriteOk
-
-	if seres.Uid != nil {
-		succ := keybase_1.SignupResSuccess{}
-		copy(succ.Uid[:], (*seres.Uid)[:])
-		res.Body.Success = &succ
-	}
 	return nil
 }
 
-func (h SignupHandler) InviteRequest(arg *keybase_1.InviteRequestArg, res *keybase_1.InviteRequestRes) error {
+func (h SignupHandler) InviteRequest(arg *keybase_1.InviteRequestArg, res *keybase_1.Status) error {
 	err := libkb.PostInviteRequest(libkb.InviteRequestArg{
 		Email : arg.Email,
 		Fullname : arg.Fullname,
 		Notes : arg.Notes,
 
 	})
-	res.Status = libkb.ExportErrorAsStatus(err)
-	res.Body = &keybase_1.InviteRequestResBody{
-		Place : 0,
-		Code : "",
-	}
+	*res = libkb.ExportErrorAsStatus(err)
 	return nil
 }
