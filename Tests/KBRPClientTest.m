324b8106356a649807caab1f7d559c4d1f431a80
Gabriel Handford <gabrielh@gmail.com>
2014-12-24 14:28:27 -0800

    Fixing test (moved to messagepack). Fixing project file for test.

--- Tests/KBRPClientTest.m
+++ Tests/KBRPClientTest.m
@@ -30,45 +30,4 @@
   GRTestLog(@"Data: %@", [uid2.data na_hexString]);
 }
 
-- (void)testClient:(dispatch_block_t)completion {
-  
-  MPMessagePackServer *server = [[MPMessagePackServer alloc] initWithOptions:MPMessagePackOptionsFramed];
-  
-  server.requestHandler = ^(NSString *method, id params, MPRequestCompletion completion) {
-    if ([method isEqualToString:@"signup"]) {
-      KBSignupRes *response = [[KBSignupRes alloc] init];
-      response.body.passphraseOk = YES;
-      NSDictionary *dict = [MTLJSONAdapter JSONDictionaryFromModel:response];
-      completion(nil, dict);
-    } else {
-      KBSignupRes *response = [[KBSignupRes alloc] init];
-      response.status = [[KBStatus alloc] init];
-      NSDictionary *dict = [MTLJSONAdapter JSONDictionaryFromModel:response];
-      completion(nil, dict);
-    }
-  };
-  
-  NSError *error = nil;
-  //@"/tmp/keybase-gabe/keybased.sock"
-  NSString *socketName = [NSString stringWithFormat:@"/tmp/msgpacktest-%@.socket", @(arc4random())];
-  if (![server openWithSocket:socketName error:&error]) {
-    GRFail(@"Unable to start server: %@", error);
-  }
-  
-  KBRPClient *client = [[KBRPClient alloc] init];
-  [client open:^(NSError *error) {
-    if (error) GRErrorHandler(error);
-    GRTestLog(@"Sending request");
-
-    KBRSignup *signUp = [[KBRSignup alloc] initWithClient:client];
-    [signUp signupWithEmail:@"test@email.com" inviteCode:@"1" passphrase:@"toomanysecrets" username:@"gabriel" completion:^(NSError *error, KBSignupRes *res) {
-      GRErrorHandler(error);
-      GRTestLog(@"%@", res);
-      completion();
-    }];
-  }];
-  
-  [self wait:10];
-}
-
 @end
