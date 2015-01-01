4ecf78eb2cbfa0430ad156bac19fba0459313614
Maxwell Krohn <themax@gmail.com>
2015-01-01 18:16:47 -0500

    a beachhead, but more work needed

--- cmd_login.go
+++ cmd_login.go
@@ -13,8 +13,16 @@ type LoginUIServer struct {
 	eng libkb.LoginUI
 }
 
+type IdentifyUIServer struct {
+	eng libkb.IdentifyUI
+}
+
 func NewLoginUIServer() *LoginUIServer {
 	return &LoginUIServer{G_UI.GetLoginUI()}
+}
+
+func NewIdentifyUIServer() *IdentifyUIServer {
+	return &IdentifyUIServer{G_UI.GetIdentifySelfUI(nil)}
 }
 
 func (u *LoginUIServer) GetEmailOrUsername() (string, error) {
@@ -25,10 +33,39 @@ func (u *LoginUIServer) GetKeybasePassphrase(prompt string) (string, error) {
 	return u.eng.GetKeybasePassphrase(prompt)
 }
 
+func (i *IdentifyUIServer) FinishAndPrompt(arg keybase_1.FinishAndPromptArg) (res keybase_1.FinishAndPromptRes, err error) {
+	return
+}
+
+func (i *IdentifyUIServer) FinishWebProofCheck(arg keybase_1.FinishWebProofCheckArg) error {
+	return nil
+}
+
+func (i *IdentifyUIServer) FinishSocialProofCheck(arg keybase_1.FinishSocialProofCheckArg) error {
+	return nil
+}
+
+func (i *IdentifyUIServer) DisplayCryptocurrency(arg keybase_1.DisplayCryptocurrencyArg) error {
+	return nil
+}
+
+func (i *IdentifyUIServer) DisplayKey(arg keybase_1.DisplayKeyArg) error {
+	return nil
+}
+
+func (i *IdentifyUIServer) ReportLastTrack(arg keybase_1.ReportLastTrackArg) error {
+	return nil
+}
+
+func (i *IdentifyUIServer) LaunchNetworkChecks(arg keybase_1.LaunchNetworkChecksArg) error {
+	return nil
+}
+
 func (v *CmdLogin) RunClient() (err error) {
 	var cli keybase_1.LoginClient
 	if cli, err = GetLoginClient(); err != nil {
 	} else if err = RegisterLoginUiServer(NewLoginUIServer()); err != nil {
+	} else if err = RegisterIdentifyUiServer(NewIdentifyUIServer()); err != nil {
 	} else {
 		err = cli.PassphraseLogin()
 	}
