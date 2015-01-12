394972d07c1eaa76191fab36c12f8c14b86950ce
Maxwell Krohn <themax@gmail.com>
2015-01-12 17:08:13 -0500

    Add a locktable for locking Uids while we load a user...
    Progress on #59

---
+++ libkb/locktab.go
@@ -0,0 +1,53 @@
+package libkb
+
+import (
+	"sync"
+)
+
+type NamedLock struct {
+	refs   int
+	name   string
+	parent *LockTable
+	lock   sync.Mutex
+}
+
+func (l *NamedLock) incref() {
+	l.refs++
+}
+
+func (l *NamedLock) decref() {
+	l.refs--
+}
+
+func (l *NamedLock) Unlock() {
+	l.lock.Unlock()
+	l.parent.lock.Lock()
+	l.decref()
+	if l.refs == 0 {
+		delete(l.parent.locks, l.name)
+	}
+	l.parent.lock.Unlock()
+}
+
+type LockTable struct {
+	lock  sync.Mutex
+	locks map[string]*NamedLock
+}
+
+func NewLockTable() *LockTable {
+	return &LockTable{
+		locks: make(map[string]*NamedLock),
+	}
+}
+
+func (t *LockTable) Acquire(s string) (ret *NamedLock) {
+	t.lock.Lock()
+	if ret = t.locks[s]; ret == nil {
+		ret = &NamedLock{refs: 0, name: s, parent: t}
+		t.locks[s] = ret
+	}
+	ret.incref()
+	t.lock.Unlock()
+	ret.lock.Lock()
+	return ret
+}
