513aef4f6a6cbc5de114edb7efdf301fd0b21b17
Patrick Crosby <pcrosby@gmail.com>
2015-01-19 16:07:10 -0600

    code cleanup:  moved IdentifyUIServer code out of cmd_login.

--- client/cmd_login.go
+++ client/cmd_login.go
@@ -14,63 +14,12 @@ type LoginUIServer struct {
 	eng libkb.LoginUI
 }
 
-type IdentifyUIServer struct {
-	eng libkb.IdentifyUI
-}
-
 func NewLoginUIProtocol() rpc2.Protocol {
 	return keybase_1.LoginUiProtocol(&LoginUIServer{G_UI.GetLoginUI()})
 }
 
-func NewIdentifyUIProtocol() rpc2.Protocol {
-	return keybase_1.IdentifyUiProtocol(&IdentifyUIServer{G_UI.GetIdentifyUI(nil)})
-}
-
-func NewIdentifySelfUIProtocol() rpc2.Protocol {
-	return keybase_1.IdentifyUiProtocol(&IdentifyUIServer{G_UI.GetIdentifySelfUI()})
-}
-
 func (u *LoginUIServer) GetEmailOrUsername() (string, error) {
 	return u.eng.GetEmailOrUsername()
-}
-
-func (i *IdentifyUIServer) FinishAndPrompt(arg keybase_1.FinishAndPromptArg) (res keybase_1.FinishAndPromptRes, err error) {
-	res, err = i.eng.FinishAndPrompt(&arg.Outcome)
-	return
-}
-
-func (i *IdentifyUIServer) FinishWebProofCheck(arg keybase_1.FinishWebProofCheckArg) error {
-	i.eng.FinishWebProofCheck(arg.Rp, arg.Lcr)
-	return nil
-}
-
-func (i *IdentifyUIServer) FinishSocialProofCheck(arg keybase_1.FinishSocialProofCheckArg) error {
-	i.eng.FinishSocialProofCheck(arg.Rp, arg.Lcr)
-	return nil
-}
-
-func (i *IdentifyUIServer) DisplayCryptocurrency(arg keybase_1.DisplayCryptocurrencyArg) error {
-	i.eng.DisplayCryptocurrency(arg.C)
-	return nil
-}
-
-func (i *IdentifyUIServer) DisplayKey(arg keybase_1.DisplayKeyArg) error {
-	i.eng.DisplayKey(arg.Fokid, arg.Diff)
-	return nil
-}
-
-func (i *IdentifyUIServer) ReportLastTrack(arg keybase_1.ReportLastTrackArg) error {
-	i.eng.ReportLastTrack(arg.Track)
-	return nil
-}
-
-func (i *IdentifyUIServer) LaunchNetworkChecks(arg keybase_1.LaunchNetworkChecksArg) error {
-	return nil
-}
-
-func (i *IdentifyUIServer) DisplayTrackStatement(arg keybase_1.DisplayTrackStatementArg) error {
-	i.eng.DisplayTrackStatement(arg.Stmt)
-	return nil
 }
 
 func (v *CmdLogin) RunClient() (err error) {
