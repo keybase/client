1e83c43bef2eaa55f5df709c34113c4275e85c12
Maxwell Krohn <themax@gmail.com>
2015-02-02 11:55:05 -0500

    Close #125
    hold onto the file handle, i think that was the issue...
    
    note there's still a race condition.  we give up the lock before we remove,
    so it's possible we're removing someone else's lockfile. i don't think it's
    going to hurt us, so go with it for now

--- libkb/flock.go
+++ libkb/flock.go
@@ -6,23 +6,49 @@ import (
 	"syscall"
 )
 
-// LockPIDFile writes the pid to filename after acquiring a lock on the file.
+type LockPIDFile struct {
+	name string
+	file *os.File
+}
+
+func NewLockPIDFile(s string) *LockPIDFile {
+	return &LockPIDFile{name: s}
+}
+
+// Lock writes the pid to filename after acquiring a lock on the file.
 // When the process exits, the lock will be released.
-func LockPIDFile(filename string) error {
+func (f *LockPIDFile) Lock() (err error) {
 	// os.OpenFile adds syscall.O_CLOEXEC automatically
-	f, err := os.OpenFile(filename, os.O_CREATE|os.O_RDWR, 0600)
-	if err != nil {
+	if f.file, err = os.OpenFile(f.name, os.O_CREATE|os.O_RDWR, 0600); err != nil {
 		return err
 	}
 
 	// LOCK_EX = exclusive
 	// LOCK_NB = nonblocking
-	if err = syscall.Flock(int(f.Fd()), syscall.LOCK_EX|syscall.LOCK_NB); err != nil {
+	if err = syscall.Flock(int(f.file.Fd()), syscall.LOCK_EX|syscall.LOCK_NB); err != nil {
+		f.file.Close()
+		f.file = nil
 		return err
 	}
 
-	fmt.Fprintf(f, "%d", os.Getpid())
-	f.Sync()
+	pid := os.Getpid()
+	fmt.Fprintf(f.file, "%d", pid)
+	f.file.Sync()
+
+	G.Log.Debug("Locked pidfile %s for pid=%d", f.name, pid)
 
 	return nil
+}
+
+func (f *LockPIDFile) Close() (err error) {
+	if f.file != nil {
+		if e1 := f.file.Close(); e1 != nil {
+			G.Log.Warning("Error closing pid file: %s\n", e1.Error())
+		}
+		G.Log.Debug("Cleaning up pidfile %s", f.name)
+		if err = os.Remove(f.name); err != nil {
+			G.Log.Warning("Error removing pidfile: %s\n", err.Error())
+		}
+	}
+	return
 }
