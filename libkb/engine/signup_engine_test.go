7023dfa8945cb3735dc11b88b68e20daeaa1a1b2
Maxwell Krohn <themax@gmail.com>
2015-02-02 14:16:07 -0500

    a test for #123 -- just logout and login

--- libkb/engine/signup_engine_test.go
+++ libkb/engine/signup_engine_test.go
@@ -20,14 +20,40 @@ func fakeUser(t *testing.T, prefix string) (username, email string) {
 	return username, email
 }
 
+func fakePassphrase(t *testing.T) string {
+	buf := make([]byte, 12)
+	if _, err := rand.Read(buf); err != nil {
+		t.Fatal(err)
+	}
+	return hex.EncodeToString(buf)
+}
+
 func TestSignupEngine(t *testing.T) {
 	tc := libkb.SetupTest(t, "signup")
 	defer tc.Cleanup()
 	s := NewSignupEngine(G.UI.GetLogUI(), nil)
 	username, email := fakeUser(t, "se")
-	arg := SignupEngineRunArg{username, email, "202020202020202020202020", "passphrase passphrase", "my device"}
+	passphrase := fakePassphrase(t)
+	arg := SignupEngineRunArg{username, email, "202020202020202020202020", passphrase, "my device"}
 	err := s.Run(arg)
 	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Now try to logout and log back in
+	G.LoginState.Logout()
+
+	larg := LoginAndIdentifyArg{
+		Login: libkb.LoginArg{
+			Force:      true,
+			Prompt:     false,
+			Username:   username,
+			Passphrase: passphrase,
+			NoUi:       true,
+		},
+		LogUI: G.UI.GetLogUI(),
+	}
+	if err = LoginAndIdentify(larg); err != nil {
 		t.Fatal(err)
 	}
 }
