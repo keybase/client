68ab4c7c90af861717cf9ad9fc6ef9b4a20515a7
Maxwell Krohn <themax@gmail.com>
2015-02-19 16:37:42 -0500

    Close #27

---
+++ libkb/proxyca.go
@@ -0,0 +1,34 @@
+package libkb
+
+import (
+	"crypto/x509"
+	"io/ioutil"
+)
+
+func addCert(out *x509.CertPool, fn string) (ret *x509.CertPool, err error) {
+	var data []byte
+	ret = nil
+	if ret == nil {
+		ret = x509.NewCertPool()
+	}
+	if data, err = ioutil.ReadFile(fn); err != nil {
+		err = ConfigError{fn, err.Error()}
+	} else if !ret.AppendCertsFromPEM(data) {
+		err = ConfigError{fn, "Bad CA Cert file; failed to parse"}
+	}
+	return
+}
+
+func GetProxyCAs(out *x509.CertPool, r ConfigReader) (ret *x509.CertPool, err error) {
+	ret = out
+	var v []string
+	if v, err = r.GetProxyCACerts(); err != nil {
+		return
+	}
+	for _, fn := range v {
+		if ret, err = addCert(ret, fn); err != nil {
+			return
+		}
+	}
+	return
+}
