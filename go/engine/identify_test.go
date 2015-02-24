60172a60cc6839d6c9cdb2664b18a3e7a2cb7a11
Jack O'Connor <oconnor663@gmail.com>
2015-02-24 11:45:15 -0800

    verify proofs in the identify tests

--- go/engine/identify_test.go
+++ go/engine/identify_test.go
@@ -1,19 +1,21 @@
 package engine
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/keybase/client/go/libkb"
 	keybase_1 "github.com/keybase/client/protocol/go"
 )
 
-func runIdentify(username string) (res *IdentifyRes, err error) {
+func runIdentify(username string) (idUI FakeIdentifyUI, res *IdentifyRes, err error) {
+	idUI = FakeIdentifyUI{Proofs: make(map[string]string)}
 	arg := IdentifyEngineArg{
 		User: username,
 	}
 	ctx := Context{
 		LogUI:      G.UI.GetLogUI(),
-		IdentifyUI: FakeIdentifyUI{},
+		IdentifyUI: idUI,
 	}
 	eng := NewIdentifyEngine(&arg)
 	err = RunEngine(eng, &ctx, nil, nil)
@@ -24,43 +26,68 @@ func runIdentify(username string) (res *IdentifyRes, err error) {
 func TestIdAlice(t *testing.T) {
 	tc := libkb.SetupTest(t, "id")
 	defer tc.Cleanup()
-	_, err := runIdentify("t_alice")
+	idUI, _, err := runIdentify("t_alice")
 	if err != nil {
 		t.Fatal(err)
+	}
+	expectedProofs := map[string]string{
+		"github":  "kbtester2",
+		"twitter": "tacovontaco",
+	}
+	if !reflect.DeepEqual(expectedProofs, idUI.Proofs) {
+		t.Fatal("Wrong proofs.", expectedProofs, idUI.Proofs)
 	}
 }
 
 func TestIdBob(t *testing.T) {
 	tc := libkb.SetupTest(t, "id")
 	defer tc.Cleanup()
-	_, err := runIdentify("t_bob")
+	idUI, _, err := runIdentify("t_bob")
 	if err != nil {
 		t.Fatal(err)
+	}
+	expectedProofs := map[string]string{
+		"github":  "kbtester1",
+		"twitter": "kbtester1",
+	}
+	if !reflect.DeepEqual(expectedProofs, idUI.Proofs) {
+		t.Fatal("Wrong proofs.", expectedProofs, idUI.Proofs)
 	}
 }
 
 func TestIdCharlie(t *testing.T) {
 	tc := libkb.SetupTest(t, "id")
 	defer tc.Cleanup()
-	_, err := runIdentify("t_charlie")
+	idUI, _, err := runIdentify("t_charlie")
 	if err != nil {
 		t.Fatal(err)
+	}
+	expectedProofs := map[string]string{
+		"github":  "tacoplusplus",
+		"twitter": "tacovontaco",
+	}
+	if !reflect.DeepEqual(expectedProofs, idUI.Proofs) {
+		t.Fatal("Wrong proofs.", expectedProofs, idUI.Proofs)
 	}
 }
 
 func TestIdDoug(t *testing.T) {
 	tc := libkb.SetupTest(t, "id")
 	defer tc.Cleanup()
-	_, err := runIdentify("t_doug")
+	idUI, _, err := runIdentify("t_doug")
 	if err != nil {
 		t.Fatal(err)
+	}
+	expectedProofs := map[string]string{}
+	if !reflect.DeepEqual(expectedProofs, idUI.Proofs) {
+		t.Fatal("Wrong proofs.", expectedProofs, idUI.Proofs)
 	}
 }
 
 func TestIdEllen(t *testing.T) {
 	tc := libkb.SetupTest(t, "id")
 	defer tc.Cleanup()
-	_, err := runIdentify("t_ellen")
+	_, _, err := runIdentify("t_ellen")
 	if err == nil {
 		t.Fatal("Expected no public key found error.")
 	} else if _, ok := err.(libkb.NoKeyError); !ok {
@@ -69,11 +96,14 @@ func TestIdEllen(t *testing.T) {
 }
 
 type FakeIdentifyUI struct {
+	Proofs map[string]string
 }
 
 func (ui FakeIdentifyUI) FinishWebProofCheck(proof keybase_1.RemoteProof, result keybase_1.LinkCheckResult) {
+	ui.Proofs[proof.Key] = proof.Value
 }
 func (ui FakeIdentifyUI) FinishSocialProofCheck(proof keybase_1.RemoteProof, result keybase_1.LinkCheckResult) {
+	ui.Proofs[proof.Key] = proof.Value
 }
 func (ui FakeIdentifyUI) FinishAndPrompt(*keybase_1.IdentifyOutcome) (res keybase_1.FinishAndPromptRes, err error) {
 	return
