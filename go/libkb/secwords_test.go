e02f4f4c71bf57510f1863a471e7513309e0e3ab
Maxwell Krohn <themax@gmail.com>
2015-03-09 22:30:45 -0400

    Close #262

--- go/libkb/secwords_test.go
+++ go/libkb/secwords_test.go
@@ -1,13 +1,16 @@
 package libkb
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSecWordList(t *testing.T) {
 	words, err := SecWordList(65)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if len(words) != 5 {
+	G.Log.Info("words: %v", words)
+	if len(words) != 6 {
 		t.Errorf("# words = %d, expected 5", len(words))
 	}
 }
