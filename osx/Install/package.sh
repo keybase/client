4d619a9eb5e6d356f995635697257c94b5a4671a
Gabriel Handford <gabrielh@gmail.com>
2015-03-19 12:30:46 -0700

    Include sparkle framework.

--- osx/Install/package.sh
+++ osx/Install/package.sh
@@ -3,11 +3,6 @@ set -e # Fail on error
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR
 
-rm -rf Keybase.dmg Applications
-
-# Build in Xcode and Export
-#xcodebuild -workspace ../Keybase.xcworkspace -scheme Keybase DSTROOT=$DIR archive
-
 if [ ! -d "Keybase.app" ]; then
 	echo "You need to Export an archived build from Xcode (Keybase.app)"
 	exit 1
@@ -18,8 +13,18 @@ if [ ! -f "keybased" ]; then
 	exit 1
 fi
 
+# Clean up
+rm -rf Keybase.dmg
+
 echo "Copying keybase binaries into Keybase.app..."
 chmod +x keybased
 cp keybased Keybase.app/Contents/MacOS/
+
+echo "Resigning..."
+# Re-sign since we copied in keybased
+codesign --verbose --force --deep --sign "Developer ID Application: Keybase, Inc." Keybase.app
+
+# Verify
+#codesign --verify --verbose=4 Keybase.app
 
 appdmg appdmg.json Keybase.dmg
