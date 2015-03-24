4ac7e0d11a3670cdf3903a606fb83ff627236548
Jack O'Connor <oconnor663@gmail.com>
2015-03-24 17:21:07 -0400

    test User.Export()
    `client status` doesn't have a corresponding engine to test, but this at
    least tests some of the data marshalling it calls into.

---
+++ go/libkb/user_test.go
@@ -0,0 +1,33 @@
+package libkb
+
+import (
+	"encoding/hex"
+	"testing"
+)
+
+func TestExportUser(t *testing.T) {
+	tc := SetupTest(t, "export_user")
+	defer tc.Cleanup()
+	alice, err := LoadUser(LoadUserArg{Name: "t_alice"})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	exportedAlice := alice.Export()
+
+	if hex.EncodeToString(exportedAlice.Uid[:]) != "295a7eea607af32040647123732bc819" {
+		t.Fatal("wrong UID", exportedAlice.Uid)
+	}
+
+	if exportedAlice.Username != "t_alice" {
+		t.Fatal("wrong username", exportedAlice.Username)
+	}
+
+	if len(exportedAlice.PublicKeys) != 1 {
+		t.Fatal("expected 1 public key", exportedAlice.PublicKeys)
+	}
+
+	if hex.EncodeToString(*exportedAlice.PublicKeys[0].Fokid.PgpFingerprint) != "2373fd089f28f328916b88f99c7927c0bdfdadf9" {
+		t.Fatal("wrong fingerprint", exportedAlice.PublicKeys[0].Fokid.PgpFingerprint)
+	}
+}
