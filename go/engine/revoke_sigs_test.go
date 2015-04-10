2ff2a7c4065483ba0af566f426795bbb63fc8ca1
Jack O'Connor <oconnor663@gmail.com>
2015-04-10 17:22:09 -0400

    test RevokeSigsEngine

---
+++ go/engine/revoke_sigs_test.go
@@ -0,0 +1,79 @@
+package engine
+
+import (
+	"testing"
+
+	"github.com/keybase/client/go/libkb"
+)
+
+func TestRevokeSig(t *testing.T) {
+	tc := SetupEngineTest(t, "rev")
+	defer tc.Cleanup()
+
+	// The PGP key is the 5th signature in the user's chain.
+	u := createFakeUserWithPGPSibkey(t)
+	assertNumDevicesAndKeys(t, u, 2, 5)
+
+	secui := libkb.TestSecretUI{Passphrase: u.Passphrase}
+	ctx := &Context{
+		LogUI:    G.UI.GetLogUI(),
+		SecretUI: secui,
+	}
+
+	// Add another PGP key, so that we have a couple to revoke. That means that
+	// signatures #5 and #6 are the ones that delegate our PGP keys.
+	const FIRST_PGP_SIG_SEQNO = 5
+	const SECOND_PGP_SIG_SEQNO = 6
+
+	arg := PGPKeyImportEngineArg{
+		Gen: &libkb.PGPGenArg{
+			PrimaryBits: 768,
+			SubkeyBits:  768,
+		},
+		AllowMulti: true,
+	}
+	arg.Gen.MakeAllIds()
+	pgpEngine := NewPGPKeyImportEngine(arg)
+	err := RunEngine(pgpEngine, ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assertNumDevicesAndKeys(t, u, 2, 6)
+
+	// First test that a bad seqno fails the revoke.
+	revokeEngine := NewRevokeSigsEngine(nil, []int{9999})
+	err = RunEngine(revokeEngine, ctx)
+	if err == nil {
+		t.Fatal(err)
+	}
+	assertNumDevicesAndKeys(t, u, 2, 6) // no change
+
+	// Now make sure a good seqno works.
+	revokeEngine = NewRevokeSigsEngine(nil, []int{FIRST_PGP_SIG_SEQNO})
+	err = RunEngine(revokeEngine, ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assertNumDevicesAndKeys(t, u, 2, 5) // The first PGP key is gone.
+
+	// Revoking the same key again should fail.
+	revokeEngine = NewRevokeSigsEngine(nil, []int{FIRST_PGP_SIG_SEQNO})
+	err = RunEngine(revokeEngine, ctx)
+	if err == nil {
+		t.Fatal(err)
+	}
+	assertNumDevicesAndKeys(t, u, 2, 5) // no change
+
+	// Check it with a raw sig id too.
+	realUser, err := libkb.LoadUser(libkb.LoadUserArg{Name: u.Username})
+	if err != nil {
+		t.Fatal(err)
+	}
+	sigID := realUser.GetSigIDFromSeqno(SECOND_PGP_SIG_SEQNO)
+	revokeEngine = NewRevokeSigsEngine([]string{*sigID}, nil)
+	err = RunEngine(revokeEngine, ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assertNumDevicesAndKeys(t, u, 2, 4) // The second PGP key is gone.
+}
