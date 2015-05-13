69d902ec79070fd8e96d16113b74ab26982b1512
Frederick Akalin <akalin@gmail.com>
2015-05-12 17:14:31 -0700

    Beef up CryptoSign tests

--- go/engine/crypto_sign_test.go
+++ go/engine/crypto_sign_test.go
@@ -6,6 +6,21 @@ import (
 	"github.com/keybase/client/go/libkb"
 )
 
+func doSign(tc libkb.TestContext, fu *FakeUser, msg []byte) ([]byte, error) {
+	secui := libkb.TestSecretUI{Passphrase: fu.Passphrase}
+	ctx := &Context{
+		SecretUI: secui,
+	}
+
+	cse := NewCryptoSignEngine(tc.G, msg, "test reason")
+	err := RunEngine(cse, ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return cse.GetSignature(), nil
+}
+
 func getDeviceSibkey(tc libkb.TestContext, username string) (libkb.GenericKey, error) {
 	u, err := libkb.LoadUser(libkb.LoadUserArg{Name: username})
 	if err != nil {
@@ -18,20 +33,14 @@ func getDeviceSibkey(tc libkb.TestContext, username string) (libkb.GenericKey, e
 
 // Test that CryptoSignEngine yields a valid signature for its given
 // message.
-func TestCryptoSign(t *testing.T) {
+func TestCryptoSignAccept(t *testing.T) {
 	tc := SetupEngineTest(t, "crypto_sign")
 	defer tc.Cleanup()
 
 	fu := CreateAndSignupFakeUser(tc, "sign")
 
-	secui := libkb.TestSecretUI{Passphrase: fu.Passphrase}
-	ctx := &Context{
-		SecretUI: secui,
-	}
-
-	message := []byte("test message")
-	cse := NewCryptoSignEngine(tc.G, message, "test reason")
-	err := RunEngine(cse, ctx)
+	msg := []byte("test message")
+	sig, err := doSign(tc, fu, msg)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -41,23 +50,58 @@ func TestCryptoSign(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	_, err = sibkey.VerifyBytes(cse.GetSignature(), message)
+	err = sibkey.VerifyBytes(sig, msg)
 	if err != nil {
 		t.Error(err)
 	}
+}
 
-	_, err = sibkey.VerifyBytes(append(cse.GetSignature(), []byte("corruption")...), message)
-	if err == nil {
-		t.Error("Verifying corrupt signature unexpectedly passed")
+// Test that VerifyBytes rejects various types of bad signatures.
+func TestCryptoSignReject(t *testing.T) {
+	tc := SetupEngineTest(t, "crypto_sign")
+	defer tc.Cleanup()
+
+	fu := CreateAndSignupFakeUser(tc, "sign")
+
+	msg := []byte("test message")
+	sig, err := doSign(tc, fu, msg)
+	if err != nil {
+		t.Fatal(err)
 	}
 
-	_, err = sibkey.VerifyBytes(cse.GetSignature(), append(message, []byte("corruption")...))
-	if err == nil {
-		t.Error("Verifying signature for corrupt message unexpectedly passed")
+	sibkey, err := getDeviceSibkey(tc, fu.Username)
+	if err != nil {
+		t.Fatal(err)
 	}
 
-	_, err = sibkey.VerifyString(string(cse.GetSignature()), message)
+	// Corrupt signature.
+
+	err = sibkey.VerifyBytes(append(sig, []byte("corruption")...), msg)
 	if err == nil {
-		t.Error("Verifying byte signature as string unexpectedly passed")
+		t.Error("Verifying corrupt signature unexpectedly passes")
+	}
+
+	// Corrupt msg.
+
+	err = sibkey.VerifyBytes(sig, append(msg, []byte("corruption")...))
+	if err == nil {
+		t.Error("Verifying signature for corrupt msg unexpectedly passes")
+	}
+
+	// Signature with different key.
+
+	keyPair, err := libkb.GenerateNaclSigningKeyPair()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	sig2, err := keyPair.SignToBytes(msg)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = sibkey.VerifyBytes(sig2, msg)
+	if err == nil {
+		t.Error("Signature with different key unexpectedly passes")
 	}
 }
