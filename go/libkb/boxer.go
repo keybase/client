5c223cf3288bca8d9097d7913cdb1d328157fef0
Frederick Akalin <akalin@gmail.com>
2015-05-26 18:53:48 -0700

    Add Boxer interface and type assertions

--- go/libkb/boxer.go
+++ go/libkb/boxer.go
@@ -1,6 +1,10 @@
 package libkb
 
-// TODO: Probably need to add Boxer interface, too.
+type Boxer interface {
+	// Encrypts the given data with the peer's public key and our
+	// private key.
+	Box(data []byte, nonce [24]byte, peersPublicKey [32]byte) []byte
+}
 
 type Unboxer interface {
 	// Decrypts the given data, which is assumed to have been
