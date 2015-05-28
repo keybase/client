03ae09edff3fd2c467fd1e3b895879639c7014d3
Frederick Akalin <akalin@gmail.com>
2015-05-28 15:47:42 -0700

    Inline logic

--- go/service/crypto.go
+++ go/service/crypto.go
@@ -1,7 +1,6 @@
 package service
 
 import (
-	"github.com/keybase/client/go/engine"
 	"github.com/keybase/client/go/libkb"
 	keybase1 "github.com/keybase/client/protocol/go"
 	"github.com/maxtaco/go-framed-msgpack-rpc/rpc2"
@@ -9,12 +8,12 @@ import (
 
 type CryptoHandler struct {
 	*BaseHandler
-	// Defaults to CryptoHandler.getDeviceSigningKey(), but
-	// overrideable for testing.
-	getDeviceSigningKeyFn func(sessionID int, reason string) (libkb.GenericKey, error)
+	// Defaults to CryptoHandler.getSecretKey(), but overrideable
+	// for testing.
+	getSecretKeyFn func(secretKeyType libkb.SecretKeyType, sessionID int, reason string) (libkb.GenericKey, error)
 }
 
-func (c *CryptoHandler) getDeviceSigningKey(sessionID int, reason string) (libkb.GenericKey, error) {
+func (c *CryptoHandler) getSecretKey(secretKeyType libkb.SecretKeyType, sessionID int, reason string) (libkb.GenericKey, error) {
 	me, err := libkb.LoadMe(libkb.LoadUserArg{})
 	if err != nil {
 		return nil, err
@@ -23,7 +22,7 @@ func (c *CryptoHandler) getDeviceSigningKey(sessionID int, reason string) (libkb
 	secretUI := c.getSecretUI(sessionID)
 	signingKey, _, err := G.Keyrings.GetSecretKeyWithPrompt(nil, libkb.SecretKeyArg{
 		Me:      me,
-		KeyType: libkb.DeviceSigningKeyType,
+		KeyType: secretKeyType,
 	}, secretUI, reason)
 	return signingKey, err
 }
@@ -31,15 +30,15 @@ func (c *CryptoHandler) getDeviceSigningKey(sessionID int, reason string) (libkb
 func NewCryptoHandler(xp *rpc2.Transport) *CryptoHandler {
 	c := &CryptoHandler{BaseHandler: NewBaseHandler(xp)}
 
-	c.getDeviceSigningKeyFn = func(sessionID int, reason string) (libkb.GenericKey, error) {
-		return c.getDeviceSigningKey(sessionID, reason)
+	c.getSecretKeyFn = func(secretKeyType libkb.SecretKeyType, sessionID int, reason string) (libkb.GenericKey, error) {
+		return c.getSecretKey(secretKeyType, sessionID, reason)
 	}
 
 	return c
 }
 
 func (c *CryptoHandler) SignED25519(arg keybase1.SignED25519Arg) (ret keybase1.ED25519SignatureInfo, err error) {
-	signingKey, err := c.getDeviceSigningKeyFn(arg.SessionID, arg.Reason)
+	signingKey, err := c.getSecretKeyFn(libkb.DeviceSigningKeyType, arg.SessionID, arg.Reason)
 	if err != nil {
 		return
 	}
@@ -60,13 +59,28 @@ func (c *CryptoHandler) SignED25519(arg keybase1.SignED25519Arg) (ret keybase1.E
 }
 
 func (c *CryptoHandler) UnboxTLFCryptKeyClientHalf(arg keybase1.UnboxTLFCryptKeyClientHalfArg) (tlfCryptKeyClientHalf keybase1.TLFCryptKeyClientHalf, err error) {
-	ctx := &engine.Context{
-		SecretUI: c.getSecretUI(arg.SessionID),
-	}
-	eng := engine.NewCryptoDecryptTLFEngine(G, arg.EncryptedData, arg.Nonce, arg.PeersPublicKey, arg.Reason)
-	if err = engine.RunEngine(eng, ctx); err != nil {
+	encryptionKey, err := c.getSecretKeyFn(libkb.DeviceEncryptionKeyType, arg.SessionID, arg.Reason)
+	if err != nil {
 		return
 	}
-	tlfCryptKeyClientHalf = eng.GetTLFCryptKeyClientHalf()
+
+	kp, ok := encryptionKey.(libkb.NaclDHKeyPair)
+	if !ok || kp.Private == nil {
+		err = libkb.KeyCannotUnboxError{}
+		return
+	}
+
+	decryptedData, ok := kp.Unbox(arg.EncryptedData, [24]byte(arg.Nonce), [32]byte(arg.PeersPublicKey))
+	if !ok {
+		err = libkb.DecryptionError{}
+		return
+	}
+
+	if len(decryptedData) != len(tlfCryptKeyClientHalf) {
+		err = libkb.DecryptionError{}
+		return
+	}
+
+	copy(tlfCryptKeyClientHalf[:], decryptedData)
 	return
 }
