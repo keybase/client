c05be19cbaba87782e46c52925462e5d4de7aa9b
Gabriel Handford <gabrielh@gmail.com>
2015-06-22 12:52:39 -0700

    New beta version

--- go/libkb/version.go
+++ go/libkb/version.go
@@ -1,4 +1,4 @@
-// 0.8.1
+// 1.0.0-beta.1
 package libkb
 
-const Version = "0.8.1" // This should always match the comment in the first line of this file
+const Version = "1.0.0-beta.1" // This should always match the comment in the first line of this file
