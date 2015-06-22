d8e3fbd2cac4e34607d8dbd6719a38bd73e9196f
Gabriel Handford <gabrielh@gmail.com>
2015-06-22 11:07:20 -0700

    New version

--- go/libkb/version.go
+++ go/libkb/version.go
@@ -1,4 +1,4 @@
-// 0.1.41
+// 0.8.1
 package libkb
 
-const Version = "0.1.41" // This should always match the comment in the first line of this file
+const Version = "0.8.1" // This should always match the comment in the first line of this file
