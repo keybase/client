82047d91ce811220447005527478e3c76c64bd5d
Frederick Akalin <akalin@gmail.com>
2015-06-23 16:20:23 -0700

    Fix compile breakage on Linux

--- go/pinentry/pinentry_not_osx.go
+++ go/pinentry/pinentry_not_osx.go
@@ -5,7 +5,7 @@ package pinentry
 type pinentrySecretStoreInfo struct{}
 
 func (pi *pinentryInstance) useSecretStore(useSecretStore bool) (pinentrySecretStoreInfo, error) {
-	return nil
+	return pinentrySecretStoreInfo{}, nil
 }
 
 func (pi *pinentryInstance) shouldStoreSecret(info pinentrySecretStoreInfo) bool {
