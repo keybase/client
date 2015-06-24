d438e08d8b434c65a621f932cd708ba76df9a9c4
Frederick Akalin <akalin@gmail.com>
2015-06-24 11:05:40 -0700

    Clear the device secret for reset command
    This closes #350. This closes #496.

--- go/engine/reset.go
+++ go/engine/reset.go
@@ -31,6 +31,10 @@ func (e *ResetEngine) SubConsumers() []libkb.UIConsumer {
 }
 
 func (e *ResetEngine) Run(ctx *Context) (err error) {
+	username := e.G().Env.GetUsername()
+	if clearSecretErr := libkb.ClearStoredSecret(username); clearSecretErr != nil {
+		e.G().Log.Warning("ClearStoredSecret error: %s", clearSecretErr)
+	}
 	if err = e.G().Logout(); err != nil {
 		return
 	}
--- go/engine/reset_test.go
+++ go/engine/reset_test.go
@@ -3,6 +3,8 @@ package engine
 import (
 	"os"
 	"testing"
+
+	"github.com/keybase/client/go/libkb"
 )
 
 func assertFileExists(t *testing.T, path string) {
@@ -21,7 +23,30 @@ func TestReset(t *testing.T) {
 	tc := SetupEngineTest(t, "reset")
 	defer tc.Cleanup()
 
-	_ = CreateAndSignupFakeUser(tc, "reset")
+	// Sign up a new user and have it store its secret in the
+	// secret store (if possible).
+	fu := NewFakeUserOrBust(tc.T, "reset")
+	arg := MakeTestSignupEngineRunArg(fu)
+	arg.StoreSecret = libkb.HasSecretStore()
+	ctx := &Context{
+		LogUI:    tc.G.UI.GetLogUI(),
+		GPGUI:    &gpgtestui{},
+		SecretUI: fu.NewSecretUI(),
+		LoginUI:  libkb.TestLoginUI{Username: fu.Username},
+	}
+	s := NewSignupEngine(&arg, tc.G)
+	err := RunEngine(s, ctx)
+	if err != nil {
+		tc.T.Fatal(err)
+	}
+
+	if libkb.HasSecretStore() {
+		secretStore := libkb.NewSecretStore(fu.Username)
+		_, err := secretStore.RetrieveSecret()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
 
 	dbPath := tc.G.Env.GetDbFilename()
 	sessionPath := tc.G.Env.GetSessionFilename()
@@ -34,13 +59,21 @@ func TestReset(t *testing.T) {
 	}
 
 	e := NewResetEngine(tc.G)
-	ctx := &Context{}
+	ctx = &Context{}
 	if err := RunEngine(e, ctx); err != nil {
 		t.Fatal(err)
 	}
 
 	if LoggedIn(tc) {
 		t.Error("Unexpectedly still logged in")
+	}
+
+	if libkb.HasSecretStore() {
+		secretStore := libkb.NewSecretStore(fu.Username)
+		secret, err := secretStore.RetrieveSecret()
+		if err == nil {
+			t.Error("Unexpectedly got secret %v", secret)
+		}
 	}
 
 	assertFileDoesNotExist(t, dbPath)
