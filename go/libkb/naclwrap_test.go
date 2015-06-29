643a734f91cae1b2408b795218ac11c0363a3432
Maxwell Krohn <themax@gmail.com>
2015-06-28 23:11:45 -0400

    add tests for encryption/decryption

--- go/libkb/naclwrap_test.go
+++ go/libkb/naclwrap_test.go
@@ -2,6 +2,7 @@ package libkb
 
 import (
 	"encoding/base64"
+	"encoding/hex"
 	"testing"
 )
 
@@ -138,5 +139,78 @@ func TestVerifyBytesReject(t *testing.T) {
 	sig2 := keyPair2.Private.Sign(msg)
 	if keyPair.Public.Verify(msg, sig2) {
 		t.Error("Signature with different key unexpectedly passes")
+	}
+}
+
+func TestNaclEncryptEphemeral(t *testing.T) {
+	keyPair, err := GenerateNaclDHKeyPair()
+	if err != nil {
+		t.Fatal(err)
+	}
+	msg := []byte("Man hands on misery to man. It deepens like a coastal shelf.")
+	ctext, err := keyPair.EncryptToString(msg, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	out, kid, err := keyPair.DecryptFromString(ctext)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !FastByteArrayEq(out, msg) {
+		t.Error("Message mismatch: %s != %s", msg, out)
+	}
+	if kid.Eq(keyPair.GetKid()) {
+		t.Error("KID should be an ephemeral key, not ours")
+	}
+}
+
+func TestNaclEncryptKnown(t *testing.T) {
+	recvr, err := GenerateNaclDHKeyPair()
+	if err != nil {
+		t.Fatal(err)
+	}
+	sender, err := GenerateNaclDHKeyPair()
+	if err != nil {
+		t.Fatal(err)
+	}
+	msg := []byte("Man hands on misery to man. It deepens like a coastal shelf.")
+	ctext, err := recvr.EncryptToString(msg, &sender)
+	if err != nil {
+		t.Fatal(err)
+	}
+	out, kid, err := recvr.DecryptFromString(ctext)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !FastByteArrayEq(out, msg) {
+		t.Error("Message mismatch: %s != %s", msg, out)
+	}
+	if !kid.Eq(sender.GetKid()) {
+		t.Error("KID mismatch for sender")
+	}
+}
+
+func TestNaclDecryptFromIced(t *testing.T) {
+	seed := "b26ba6f6865b28f9332620c73c2984e2d2a8a83ef5eb59ca47d3b70cfa9f222f"
+	ctext := "g6Rib2R5hapjaXBoZXJ0ZXh0xEw2dXZRKyUI5wbSfQGSv61xVIl/cpD8hFN+Gsc5LGEtuXmGG1+1rUFv4QWizfLgqhywaitotmApYJv07zFTUT5sxOU+i2er43XQkkwmqGVuY190eXBlIaVub25jZcQY/rYiRGjPmmxurm5PMlhJuJwP9jk7UJIFrHJlY2VpdmVyX2tlecQjASHyQFIJdlHbnV1oT3MKne5ob7Rmf0emMciNbkD1IyfCKgqqc2VuZGVyX2tlecQjASFKiKO16sYJaloJ4URJM+pL6BSYJcz8M/Za2MSrKCvqawqjdGFnzQIDp3ZlcnNpb24B"
+	plaintext := []byte("Man hands on misery to man. It deepens like a coastal shelf.")
+	seedBytes, err := hex.DecodeString(seed); 
+	if err != nil {
+		t.Fatal(err)
+	}
+	var secret [32]byte
+	copy(secret[:], seedBytes)
+	key, err := MakeNaclDHKeyPairFromSecret(secret)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	out, _, err := key.DecryptFromString(ctext)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !FastByteArrayEq(out, plaintext) {
+		t.Error("failed to match plaintext")
 	}
 }
