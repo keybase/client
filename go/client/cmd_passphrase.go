0f4736719763f566bd631818c798d24ac141cb18
Patrick Crosby <pcrosby@gmail.com>
2015-07-16 14:45:39 -0500

    passphrase parent command

---
+++ go/client/cmd_passphrase.go
@@ -0,0 +1,17 @@
+package client
+
+import (
+	"github.com/keybase/cli"
+	"github.com/keybase/client/go/libcmdline"
+)
+
+func NewCmdPassphrase(cl *libcmdline.CommandLine) cli.Command {
+	return cli.Command{
+		Name:        "passphrase",
+		Usage:       "keybase passphrase [change|recover]",
+		Description: "Change or recover your keybase passphrase",
+		Subcommands: []cli.Command{
+			NewCmdPassphraseChange(cl),
+		},
+	}
+}
