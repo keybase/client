e0838a0bf238762648a97ffb992222b558174044
Jeremy Stribling <strib@alum.mit.edu>
2015-07-22 10:40:00 -0700

    kbfsfuse: fix bserver test compilation

--- kbfsfuse/test_test.go
+++ kbfsfuse/test_test.go
@@ -34,7 +34,7 @@ func TestMain(m *testing.M) {
 		fmt.Printf("Testing Using Remote Backend: %s\n", bserver.Config.BServerAddr)
 		bserver.InitConfig("../bserver/testconfig.json")
 		bserver.Config.TestNoSession = true
-		bserver.StartBServer(nil)
+		bserver.StartBServer()
 	}
 
 	os.Exit(m.Run())
