c1312232ea320716b398631cce7d3488f77bac6c
Patrick Crosby <pcrosby@gmail.com>
2015-07-29 15:44:22 -0500

    favorite handler in daemon using favorite_* engines

--- go/service/favorite.go
+++ go/service/favorite.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"github.com/keybase/client/go/engine"
 	keybase1 "github.com/keybase/client/protocol/go"
 	"github.com/maxtaco/go-framed-msgpack-rpc/rpc2"
 )
@@ -18,15 +19,24 @@ func NewFavoriteHandler(xp *rpc2.Transport) *FavoriteHandler {
 
 // FavoriteAdd handles the favoriteAdd RPC.
 func (h *FavoriteHandler) FavoriteAdd(arg keybase1.FavoriteAddArg) error {
-	return nil
+	eng := engine.NewFavoriteAdd(&arg, G)
+	ctx := &engine.Context{}
+	return engine.RunEngine(eng, ctx)
 }
 
 // FavoriteDelete handles the favoriteDelete RPC.
 func (h *FavoriteHandler) FavoriteDelete(arg keybase1.FavoriteDeleteArg) error {
-	return nil
+	eng := engine.NewFavoriteDelete(&arg, G)
+	ctx := &engine.Context{}
+	return engine.RunEngine(eng, ctx)
 }
 
 // FavoriteList handles the favoriteList RPC.
 func (h *FavoriteHandler) FavoriteList(sessionID int) ([]keybase1.Folder, error) {
-	return nil, nil
+	eng := engine.NewFavoriteList(G)
+	ctx := &engine.Context{}
+	if err := engine.RunEngine(eng, ctx); err != nil {
+		return nil, err
+	}
+	return eng.Favorites(), nil
 }
