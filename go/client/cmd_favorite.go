8a5ab94612e38768881bbae83134a85c3a91db62
Patrick Crosby <pcrosby@gmail.com>
2015-07-30 10:49:01 -0500

    fixed Usage string

--- go/client/cmd_favorite.go
+++ go/client/cmd_favorite.go
@@ -12,7 +12,7 @@ import (
 func NewCmdFavorite(cl *libcmdline.CommandLine) cli.Command {
 	return cli.Command{
 		Name:        "favorite",
-		Usage:       "keybase favorite [add|delete]",
+		Usage:       "keybase favorite [add|delete|list]",
 		Description: "Add or delete favorite kbfs folders",
 		Subcommands: []cli.Command{
 			NewCmdFavoriteAdd(cl),
