5802a431fb3c5442ba10eed0a7b03b83a773198b
Jack O'Connor <oconnor663@gmail.com>
2015-08-05 19:17:22 -0400

    let build_debian.sh take a destination arg

--- packaging/debian/build_debian.sh
+++ packaging/debian/build_debian.sh
@@ -1,22 +1,31 @@
 #! /bin/bash
 
+# Builds the keybase binary and packages it into a ".deb" file. Takes a build
+# directory as an argument, or creates one in /tmp. The package file is written
+# to that directory.
+
 set -e -u -o pipefail
 
-cd "$(dirname "$BASH_SOURCE")"
+here="$(dirname "$BASH_SOURCE")"
 
-# Builds `keybase` in the current directory.
+# Take the first argument, or a tmp dir if there is no first argument.
+dest="${1:-$(mktemp -d)}"
+
+echo Building in: "$dest"
+
+# Build the `keybase` binary inside $GOPATH.
 echo "building Go client"
 go install github.com/keybase/client/go/keybase
 
 # TODO: Make `keybase --version` behave better.
 version="$($GOPATH/bin/keybase --version 2> /dev/null | cut -d " " -f 3 || true)"
 
-mkdir -p build/usr/bin
-cp "$GOPATH/bin/keybase" build/usr/bin/
+mkdir -p "$dest/build/usr/bin"
+cp "$GOPATH/bin/keybase" "$dest/build/usr/bin/"
 
-mkdir -p build/DEBIAN
-cat control.template | sed "s/@@VERSION@@/$version/" > build/DEBIAN/control
-cp postinst build/DEBIAN/
+mkdir -p "$dest/build/DEBIAN"
+cat "$here/control.template" | sed "s/@@VERSION@@/$version/" > "$dest/build/DEBIAN/control"
+cp "$here/postinst" "$dest/build/DEBIAN/"
 
-debname="keybase-${version}.deb"
-dpkg-deb --build build "$debname"
+debfile="$dest/keybase-${version}.deb"
+dpkg-deb --build "$dest/build" "$debfile"
