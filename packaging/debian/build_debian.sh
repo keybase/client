f0016006506f9bd6d5223f29d88183ee9d968643
Jack O'Connor <oconnor663@gmail.com>
2015-08-11 00:45:26 -0400

    have build_debian.sh build both i386 and amd64

--- packaging/debian/build_debian.sh
+++ packaging/debian/build_debian.sh
@@ -1,31 +1,45 @@
 #! /bin/bash
 
-# Builds the keybase binary and packages it into a ".deb" file. Takes a build
-# directory as an argument, or creates one in /tmp. The package file is written
-# to that directory.
+# Builds the keybase binary and packages it into two ".deb" files, one for i386
+# and one for amd64. Takes a build directory as an argument, or creates one in
+# /tmp. The package files are created there, in their respective folders.
 
 set -e -u -o pipefail
 
 here="$(dirname "$BASH_SOURCE")"
 
 # Take the first argument, or a tmp dir if there is no first argument.
-dest="${1:-$(mktemp -d)}"
+build_root="${1:-$(mktemp -d)}"
 
-echo Building in: "$dest"
+echo Building in: "$build_root"
 
-# Build the `keybase` binary inside $GOPATH.
-echo "building Go client"
-go install github.com/keybase/client/go/keybase
+build_one_architecture() {
+  echo "building Go client for $GOARCH"
+  dest="$build_root/$debian_arch"
+  mkdir -p "$dest/build/usr/bin"
+  mkdir -p "$dest/build/DEBIAN"
 
-# TODO: Make `keybase --version` behave better.
-version="$($GOPATH/bin/keybase --version 2> /dev/null | cut -d " " -f 3 || true)"
+  # `go build` reads $GOARCH
+  go build -o "$dest/build/usr/bin/keybase" github.com/keybase/client/go/keybase
 
-mkdir -p "$dest/build/usr/bin"
-cp "$GOPATH/bin/keybase" "$dest/build/usr/bin/"
+  # TODO: Make `keybase --version` behave better.
+  version="$("$dest/build/usr/bin/keybase" --version 2> /dev/null | cut -d " " -f 3 || true)"
 
-mkdir -p "$dest/build/DEBIAN"
-cat "$here/control.template" | sed "s/@@VERSION@@/$version/" > "$dest/build/DEBIAN/control"
-cp "$here/postinst" "$dest/build/DEBIAN/"
+  cat "$here/control.template" \
+    | sed "s/@@VERSION@@/$version/" \
+    | sed "s/@@ARCHITECTURE@@/$debian_arch/" \
+    > "$dest/build/DEBIAN/control"
+  cp "$here/postinst" "$dest/build/DEBIAN/"
 
-debfile="$dest/keybase-${version}.deb"
-dpkg-deb --build "$dest/build" "$debfile"
+  dpkg-deb --build "$dest/build" "$dest/keybase-${version}-${debian_arch}.deb"
+}
+
+# Note that Go names the x86 architecture differently than Debian does, which
+# is why we need these two variables.
+export GOARCH=386
+export debian_arch=i386
+build_one_architecture
+
+export GOARCH=amd64
+export debian_arch=amd64
+build_one_architecture
