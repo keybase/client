709af41a66ac3015b2ebc2ee4ff94bfad214dbef
Gabriel Handford <gabrielh@gmail.com>
2015-08-11 18:33:20 -0700

    Update brew.

--- osx/Install/brew/README.md
+++ osx/Install/brew/README.md
@@ -1,4 +1,8 @@
 
+Tag release: v1.0.0-beta.8
+Update source archive ...client/archive/v1.0.0-beta.8.tar.gz
+Update source archive SHA256 in keybase.rb: `shasum -a 256 client-1.0.0-beta.8.tar.gz`
+
 To bottle:
 
     brew install --build-bottle keybase.rb
--- osx/Install/brew/keybase.rb
+++ osx/Install/brew/keybase.rb
@@ -3,19 +3,19 @@ class Keybase < Formula
   homepage "https://keybase.io/"
 
   # Just for testing since brew can't access private repo
-  url "https://keybase-app.s3.amazonaws.com/client-v1.0.0-beta.1.tar.gz"
-  #url "https://github.com/keybase/client/archive/v1.0.0-beta.1.tar.gz"
+  url "https://keybase-app.s3.amazonaws.com/client-1.0.0-beta.8.tar.gz"
+  #url "https://github.com/keybase/client/archive/v1.0.0-beta.8.tar.gz"
 
-  sha256 "6037e413a51ca4d43baf854723d992edc7f6a0f136729a4083ce395c7463bf08"
+  sha256 "645bf9ea42bd8ea17f1212a1d04ac3aafab4faa70bbe9da4ae66ebc4e18b8e47"
   head "https://github.com/keybase/client.git"
-  version "1.0.0-beta.1"
+  version "1.0.0-beta.8"
 
   bottle do
     cellar :any
 
     # Just for testing since brew can't access private repo
     root_url "https://keybase-app.s3.amazonaws.com"
-    #root_url "https://github.com/keybase/client/releases/download/v1.0.0-beta.1"
+    #root_url "https://github.com/keybase/client/releases/download/v1.0.0-beta.8"
 
     sha256 "3c8eeac9fd3012158713f477d907ddae806c4c910da568ec971c9fb33ed055b2" => :yosemite
   end
@@ -49,14 +49,17 @@ class Keybase < Formula
       <key>ProgramArguments</key>
       <array>
         <string>#{opt_bin}/keybase</string>
-        <string>-d</string>
-        <string>-L</string>
+        <string>--log-format=file</string>
         <string>service</string>
       </array>
       <key>KeepAlive</key>
       <true/>
       <key>RunAtLoad</key>
       <true/>
+      <key>StandardErrorPath</key>
+      <string>#{ENV['HOME']}/Library/Logs/#{plist_name}.log</string>
+      <key>StandardOutPath</key>
+      <string>#{ENV['HOME']}/Library/Logs/#{plist_name}.log</string>
     </dict>
     </plist>
     EOS
