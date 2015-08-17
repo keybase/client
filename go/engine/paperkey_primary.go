25835c15a150b87e3a0c08fb4c1cccffbc9e26d8
Patrick Crosby <pcrosby@gmail.com>
2015-08-17 10:49:59 -0500

    renamed engine/paper -> engine/paperkey_primary

--- go/engine/paperkey_primary.go
+++ go/engine/paperkey_primary.go
@@ -1,4 +1,4 @@
-// Paper creates the initial paper backup key for a user.  It
+// PaperKeyPrimary creates the initial paper backup key for a user.  It
 // differs from the Backup engine in that it already knows the
 // signing key and it doesn't offer to revoke any devices, plus it
 // uses a different UI call to display the phrase.
@@ -11,52 +11,52 @@ import (
 	keybase1 "github.com/keybase/client/protocol/go"
 )
 
-// Paper is an engine.
-type Paper struct {
+// PaperKeyPrimary is an engine.
+type PaperKeyPrimary struct {
 	passphrase string
-	args       *PaperArgs
+	args       *PaperKeyPrimaryArgs
 	libkb.Contextified
 }
 
-type PaperArgs struct {
+type PaperKeyPrimaryArgs struct {
 	SigningKey libkb.GenericKey
 	Me         *libkb.User
 }
 
-// NewPaper creates a Paper engine.
-func NewPaper(g *libkb.GlobalContext, args *PaperArgs) *Paper {
-	return &Paper{
+// NewPaperKeyPrimary creates a PaperKeyPrimary engine.
+func NewPaperKeyPrimary(g *libkb.GlobalContext, args *PaperKeyPrimaryArgs) *PaperKeyPrimary {
+	return &PaperKeyPrimary{
 		args:         args,
 		Contextified: libkb.NewContextified(g),
 	}
 }
 
 // Name is the unique engine name.
-func (e *Paper) Name() string {
-	return "Paper"
+func (e *PaperKeyPrimary) Name() string {
+	return "PaperKeyPrimary"
 }
 
 // GetPrereqs returns the engine prereqs.
-func (e *Paper) Prereqs() Prereqs {
+func (e *PaperKeyPrimary) Prereqs() Prereqs {
 	return Prereqs{
 		Session: true,
 	}
 }
 
 // RequiredUIs returns the required UIs.
-func (e *Paper) RequiredUIs() []libkb.UIKind {
+func (e *PaperKeyPrimary) RequiredUIs() []libkb.UIKind {
 	return []libkb.UIKind{
 		libkb.LoginUIKind,
 	}
 }
 
 // SubConsumers returns the other UI consumers for this engine.
-func (e *Paper) SubConsumers() []libkb.UIConsumer {
+func (e *PaperKeyPrimary) SubConsumers() []libkb.UIConsumer {
 	return nil
 }
 
 // Run starts the engine.
-func (e *Paper) Run(ctx *Context) error {
+func (e *PaperKeyPrimary) Run(ctx *Context) error {
 	words, err := libkb.SecWordList(libkb.BackupKeyPhraseEntropy)
 	if err != nil {
 		return err
