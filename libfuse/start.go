7968045865bf0156a0f5439507a49277cab2ee8d
Gabriel Handford <gabrielh@gmail.com>
2015-08-28 15:35:29 -0700

    KBFS start, mounter

---
+++ libfuse/start.go
@@ -0,0 +1,72 @@
+package libfuse
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/keybase/kbfs/libkbfs"
+	"golang.org/x/net/context"
+)
+
+// StartOptions are options for starting up
+type StartOptions struct {
+	LocalUser     string
+	ServerRootDir string
+	CPUProfile    string
+	MemProfile    string
+	VersionFile   string
+	Debug         bool
+}
+
+// Start the filesystem
+func Start(mounter Mounter, options StartOptions) *Error {
+	c, err := mounter.Mount()
+	if err != nil {
+		return MountError(err.Error())
+	}
+	defer c.Close()
+
+	onInterruptFn := func() {
+		select {
+		case <-c.Ready:
+			// Was mounted, so try to unmount if it was successful.
+			if c.MountError == nil {
+				err = mounter.Unmount()
+				if err != nil {
+					return
+				}
+			}
+
+		default:
+			// Was not mounted successfully yet, so do nothing. Note that the mount
+			// could still happen, but that's a rare enough edge case.
+		}
+	}
+
+	config, err := libkbfs.Init(options.LocalUser, &options.ServerRootDir, options.CPUProfile, options.MemProfile, onInterruptFn, options.Debug)
+	if err != nil {
+		return InitError(err.Error())
+	}
+
+	defer libkbfs.Shutdown(options.MemProfile)
+
+	if options.VersionFile != "" {
+		version := fmt.Sprintf("%s-%s", libkbfs.Version, libkbfs.Build)
+		err = ioutil.WriteFile(options.VersionFile, []byte(version), 0644)
+		if err != nil {
+			return InitError(err.Error())
+		}
+	}
+
+	fs := NewFS(config, c)
+	ctx := context.WithValue(context.Background(), CtxAppIDKey, fs)
+	fs.Serve(ctx)
+
+	<-c.Ready
+	err = c.MountError
+	if err != nil {
+		return MountError(err.Error())
+	}
+
+	return nil
+}
