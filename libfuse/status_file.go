08d9326883e8c75637bcf6c8d29dadf52cfbb128
Jeremy Stribling <strib@alum.mit.edu>
2015-09-01 11:02:33 -0700

    status_file: make data immutable for each instance
    Otherwise on OSX, if the file changes between the Open and the Attr,
    you could have problems.

--- libfuse/status_file.go
+++ libfuse/status_file.go
@@ -2,9 +2,11 @@ package libfuse
 
 import (
 	"encoding/json"
+	"sync"
 
 	"bazil.org/fuse"
 	"bazil.org/fuse/fs"
+	"github.com/keybase/kbfs/libkbfs"
 	"golang.org/x/net/context"
 )
 
@@ -13,34 +15,45 @@ import (
 const StatusFileName = ".kbfs_status"
 
 // StatusFile represents a file containing the current JSON-encoded
-// status of the given top-level folder.
+// status of the given top-level folder. Each instance saves exactly
+// one immutable version of the status.  This is because on OSX, we
+// get an Attr call after the Open call (before the Read, which
+// doesn't come here), and if Attr returns more bytes than Open put
+// into the DataHandle, it will return a bunch of garbage after the
+// status text.
 type StatusFile struct {
 	folder *Folder
+	once   sync.Once
+	status []byte
 }
 
 var _ fs.Node = (*StatusFile)(nil)
 
-func (f *StatusFile) encodedStatus(ctx context.Context) ([]byte, error) {
-	status, _, err := f.folder.fs.config.KBFSOps().
-		Status(ctx, f.folder.folderBranch)
-	if err != nil {
-		return nil, err
-	}
-	mStatus, err := json.MarshalIndent(status, "", "  ")
-	if err != nil {
-		return nil, err
-	}
-	return append(mStatus, '\n'), nil
+func (f *StatusFile) saveEncodedStatus(ctx context.Context) error {
+	var err error
+	f.once.Do(func() {
+		var status libkbfs.FolderBranchStatus
+		status, _, err = f.folder.fs.config.KBFSOps().
+			Status(ctx, f.folder.folderBranch)
+		if err != nil {
+			return
+		}
+		mStatus, err := json.MarshalIndent(status, "", "  ")
+		if err != nil {
+			return
+		}
+		f.status = append(mStatus, '\n')
+	})
+	return err
 }
 
 // Attr implements the fs.Node interface for StatusFile.
 func (f *StatusFile) Attr(ctx context.Context, a *fuse.Attr) error {
 	ctx = NewContextWithOpID(ctx, f.folder.fs.log)
-	status, err := f.encodedStatus(ctx)
-	if err != nil {
+	if err := f.saveEncodedStatus(ctx); err != nil {
 		return err
 	}
-	a.Size = uint64(len(status))
+	a.Size = uint64(len(f.status))
 	a.Mode = 0444
 	return nil
 }
@@ -53,9 +66,8 @@ var _ fs.NodeOpener = (*StatusFile)(nil)
 func (f *StatusFile) Open(ctx context.Context, req *fuse.OpenRequest,
 	resp *fuse.OpenResponse) (fs.Handle, error) {
 	ctx = NewContextWithOpID(ctx, f.folder.fs.log)
-	status, err := f.encodedStatus(ctx)
-	if err != nil {
+	if err := f.saveEncodedStatus(ctx); err != nil {
 		return nil, err
 	}
-	return fs.DataHandle(status), nil
+	return fs.DataHandle(f.status), nil
 }
