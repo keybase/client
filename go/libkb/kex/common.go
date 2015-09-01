97487032c10bd9e9f5f18497827a1b8acc692bd2
Patrick Crosby <pcrosby@gmail.com>
2015-09-01 14:38:20 -0500

    don't copy G into this package

--- go/libkb/kex/common.go
+++ go/libkb/kex/common.go
@@ -1,12 +1,10 @@
 package kex
 
-import (
-	"github.com/keybase/client/go/libkb"
-)
-
 // For convenience, store libkb's G here...
+/*
 var G *libkb.GlobalContext
 
 func init() {
 	G = libkb.G
 }
+*/
