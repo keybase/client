98160ef264cd7b7a8352d8adae068bfd1785c73c
Gabriel Handford <gabrielh@gmail.com>
2015-09-01 15:40:03 -0700

    Version and launchd shouldn't use cmds
    because that triggers a connection to the service.

--- go/client/cmd_launchd_osx.go
+++ go/client/cmd_launchd_osx.go
@@ -3,11 +3,10 @@
 package client
 
 import (
-	"fmt"
+	"os"
 
 	"github.com/keybase/cli"
 	"github.com/keybase/client/go/libcmdline"
-	"github.com/keybase/client/go/libkb"
 )
 
 func NewCmdLaunchd(cl *libcmdline.CommandLine) cli.Command {
@@ -30,18 +29,18 @@ func NewCmdLaunchdInstall(cl *libcmdline.CommandLine) cli.Command {
 		Usage:       "keybase launchd install <label> <path/to/keybase>",
 		Description: "Install a keybase launchd service",
 		Action: func(c *cli.Context) {
-			run := func() error {
-				args := c.Args()
-				if len(args) < 1 {
-					return fmt.Errorf("No label specified")
-				}
-				if len(args) < 2 {
-					return fmt.Errorf("No path to keybase executable specified")
-				}
-				return Install(args[0], args[1])
+			args := c.Args()
+			if len(args) < 1 {
+				G.Log.Fatalf("No label specified")
 			}
-			cl.ChooseCommand(&cmdLaunchd{run}, "install", c)
-			cl.SetForkCmd(libcmdline.NoFork)
+			if len(args) < 2 {
+				G.Log.Fatalf("No path to keybase executable specified")
+			}
+			err := Install(args[0], args[1])
+			if err != nil {
+				G.Log.Fatalf("%v", err)
+			}
+			os.Exit(0)
 		},
 	}
 }
@@ -52,15 +51,15 @@ func NewCmdLaunchdUninstall(cl *libcmdline.CommandLine) cli.Command {
 		Usage:       "keybase launchd uninstall <label>",
 		Description: "Uninstall a keybase launchd service",
 		Action: func(c *cli.Context) {
-			run := func() error {
-				args := c.Args()
-				if len(args) < 1 {
-					return fmt.Errorf("No label specified")
-				}
-				return Uninstall(args[0])
+			args := c.Args()
+			if len(args) < 1 {
+				G.Log.Fatalf("No label specified")
 			}
-			cl.ChooseCommand(&cmdLaunchd{run}, "uninstall", c)
-			cl.SetForkCmd(libcmdline.NoFork)
+			err := Uninstall(args[0])
+			if err != nil {
+				G.Log.Fatalf("%v", err)
+			}
+			os.Exit(0)
 		},
 	}
 }
@@ -71,11 +70,11 @@ func NewCmdLaunchdList(cl *libcmdline.CommandLine) cli.Command {
 		Usage:       "keybase launchd list",
 		Description: "List keybase launchd services",
 		Action: func(c *cli.Context) {
-			run := func() error {
-				return ShowServices()
+			err := ShowServices()
+			if err != nil {
+				G.Log.Fatalf("%v", err)
 			}
-			cl.ChooseCommand(&cmdLaunchd{run}, "list", c)
-			cl.SetForkCmd(libcmdline.NoFork)
+			os.Exit(0)
 		},
 	}
 }
@@ -86,31 +85,15 @@ func NewCmdLaunchdStatus(cl *libcmdline.CommandLine) cli.Command {
 		Usage:       "keybase launchd status <label>",
 		Description: "Status for keybase launchd service",
 		Action: func(c *cli.Context) {
-			run := func() error {
-				args := c.Args()
-				if len(args) < 1 {
-					return fmt.Errorf("No label specified")
-				}
-				return ShowStatus(args[0])
+			args := c.Args()
+			if len(args) < 1 {
+				G.Log.Fatalf("No label specified")
 			}
-			cl.ChooseCommand(&cmdLaunchd{run}, "status", c)
-			cl.SetForkCmd(libcmdline.NoFork)
+			err := ShowStatus(args[0])
+			if err != nil {
+				G.Log.Fatalf("%v", err)
+			}
+			os.Exit(0)
 		},
 	}
-}
-
-type cmdLaunchd struct {
-	run func() error
-}
-
-func (c cmdLaunchd) ParseArgv(*cli.Context) error {
-	return nil
-}
-
-func (c cmdLaunchd) Run() error {
-	return c.run()
-}
-
-func (c *cmdLaunchd) GetUsage() libkb.Usage {
-	return libkb.Usage{}
 }
--- go/client/cmd_version.go
+++ go/client/cmd_version.go
@@ -1,24 +1,12 @@
 package client
 
 import (
+	"os"
+
 	"github.com/keybase/cli"
 	"github.com/keybase/client/go/libcmdline"
 	"github.com/keybase/client/go/libkb"
 )
-
-type CmdVersion struct {
-	devel   bool
-	verbose bool
-}
-
-func (v *CmdVersion) Run() error {
-	if v.verbose {
-		libkb.VersionMessage(v.devel, func(s string) { GlobUI.Println(s) })
-	} else {
-		GlobUI.Println(libkb.VersionString(v.devel))
-	}
-	return nil
-}
 
 func NewCmdVersion(cl *libcmdline.CommandLine) cli.Command {
 	return cli.Command{
@@ -35,19 +23,14 @@ func NewCmdVersion(cl *libcmdline.CommandLine) cli.Command {
 			},
 		},
 		Action: func(c *cli.Context) {
-			cl.ChooseCommand(&CmdVersion{}, "version", c)
+			devel := c.Bool("devel")
+			verbose := c.Bool("verbose")
+			if verbose {
+				libkb.VersionMessage(devel, func(s string) { GlobUI.Println(s) })
+			} else {
+				GlobUI.Println(libkb.VersionString(devel))
+			}
+			os.Exit(0)
 		},
-	}
-}
-
-func (v *CmdVersion) ParseArgv(ctx *cli.Context) error {
-	v.verbose = ctx.Bool("verbose")
-	v.devel = ctx.Bool("devel")
-	return nil
-}
-
-func (v *CmdVersion) GetUsage() libkb.Usage {
-	return libkb.Usage{
-		Config: true,
 	}
 }
