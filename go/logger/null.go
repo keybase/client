36953d066dd3eb1629d5dc3f69a7fd86e836837a
Andy Alness <andy.alness@gmail.com>
2015-09-01 16:04:10 -0700

    logger: add proper stubs for null

--- go/logger/null.go
+++ go/logger/null.go
@@ -1,14 +1,37 @@
 package logger
 
+import (
+	keybase1 "github.com/keybase/client/protocol/go"
+	"golang.org/x/net/context"
+)
+
 type Null struct{}
 
 func NewNull() *Null {
 	return &Null{}
 }
 
-func (l *Null) Debug(format string, args ...interface{})    {}
-func (l *Null) Info(format string, args ...interface{})     {}
-func (l *Null) Warning(format string, args ...interface{})  {}
-func (l *Null) Notice(format string, args ...interface{})   {}
-func (l *Null) Errorf(format string, args ...interface{})   {}
-func (l *Null) Critical(format string, args ...interface{}) {}
+// Verify Null fully implements the Logger interface.
+var _ Logger = (*Null)(nil)
+
+func (l *Null) Debug(format string, args ...interface{})                       {}
+func (l *Null) Info(format string, args ...interface{})                        {}
+func (l *Null) Warning(format string, args ...interface{})                     {}
+func (l *Null) Notice(format string, args ...interface{})                      {}
+func (l *Null) Errorf(format string, args ...interface{})                      {}
+func (l *Null) Critical(format string, args ...interface{})                    {}
+func (l *Null) CCriticalf(ctx context.Context, fmt string, arg ...interface{}) {}
+func (l *Null) Fatalf(fmt string, arg ...interface{})                          {}
+func (l *Null) CFatalf(ctx context.Context, fmt string, arg ...interface{})    {}
+func (l *Null) Profile(fmts string, arg ...interface{})                        {}
+func (l *Null) CDebugf(ctx context.Context, fmt string, arg ...interface{})    {}
+func (l *Null) CInfof(ctx context.Context, fmt string, arg ...interface{})     {}
+func (l *Null) CNoticef(ctx context.Context, fmt string, arg ...interface{})   {}
+func (l *Null) CWarningf(ctx context.Context, fmt string, arg ...interface{})  {}
+func (l *Null) CErrorf(ctx context.Context, fmt string, arg ...interface{})    {}
+func (l *Null) Error(fmt string, arg ...interface{})                           {}
+func (l *Null) Configure(style string, debug bool, filename string)            {}
+func (l *Null) RotateLogFile() error                                           { return nil }
+func (l *Null) AddExternalLogger(ExternalLogger) uint64                        { return 0 }
+func (l *Null) RemoveExternalLogger(uint64)                                    {}
+func (l *Null) SetExternalLogLevel(level keybase1.LogLevel)                    {}
