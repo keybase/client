e251a9b1687c741294893fe6d141a615676513ce
Maxwell Krohn <themax@gmail.com>
2015-09-01 21:46:25 -0400

    change this back to slighlty more style

--- go/logger/standard.go
+++ go/logger/standard.go
@@ -17,7 +17,7 @@ const (
 	fancyFormat   = "%{color}%{time:15:04:05.000000} ▶ [%{level:.4s} %{module} %{shortfile}] %{id:03x}%{color:reset} %{message}"
 	plainFormat   = "[%{level:.4s}] %{id:03x} %{message}"
 	fileFormat    = "%{time:15:04:05.000000} ▶ [%{level:.4s} %{module} %{shortfile}] %{id:03x} %{message}"
-	defaultFormat = "%{color}%{message}%{color:reset}"
+	defaultFormat = "%{color}▶ %{level} %{message}%{color:reset}"
 )
 
 const permDir os.FileMode = 0700
