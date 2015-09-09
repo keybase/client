fff83cc1663a74fff7e6fed05a0be56bd243ad3c
Marco Munizaga <marco@marcopolo.io>
2015-09-09 10:24:00 -0700

    Add boilerplate android code

---
+++ android/TipBase/GoTip/tip.go
@@ -0,0 +1,6 @@
+package tip
+
+func TipMe() (tip string) {
+	tip = "Household tip: Tired of buying so much toilet paper? Try unspooling the paper from the roll before using it. A single roll can last for multiple days that way, and it's much easier on your plumbing."
+	return
+}
