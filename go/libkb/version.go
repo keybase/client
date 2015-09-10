9d8ab3d81f7d14169134046dcd38e7d6cc5f7ccb
Gabriel Handford <gabrielh@gmail.com>
2015-09-10 14:25:50 -0700

    Bumping build version.

--- go/libkb/version.go
+++ go/libkb/version.go
@@ -8,7 +8,7 @@ import (
 const Version = "1.0.0"
 
 // Build number
-const Build = "13"
+const Build = "14"
 
 // VersionString returns semantic version string.
 // If devel, include build in version string (for development releases), for
