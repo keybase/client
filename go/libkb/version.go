1c7481a9e6f5e18bbfdf052fc74dc940167c655d
Gabriel Handford <gabrielh@gmail.com>
2015-09-11 11:48:04 -0700

    New version v1.0.0-16

--- go/libkb/version.go
+++ go/libkb/version.go
@@ -8,7 +8,7 @@ import (
 const Version = "1.0.0"
 
 // Build number
-const Build = "15"
+const Build = "16"
 
 // VersionString returns semantic version string.
 // If devel, include build in version string (for development releases), for
