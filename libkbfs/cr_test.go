84c8b5f58f8c84aeeec562be01351f4d4b061dd5
Jeremy Stribling <strib@alum.mit.edu>
2015-09-14 12:38:56 -0700

    cr: add a test for when there's a hole in the cached MD range

--- libkbfs/cr_test.go
+++ libkbfs/cr_test.go
@@ -66,7 +66,7 @@ func TestCRInput(t *testing.T) {
 	}
 	for i := mergedHead + 1; i <= branchPoint+2*maxMDsAtATime; i++ {
 		config.mockMdcache.EXPECT().Get(cr.fbo.id(), i, Merged).Return(
-			nil, NoSuchMDError{cr.fbo.id(), mergedHead + 1, Merged})
+			nil, NoSuchMDError{cr.fbo.id(), i, Merged})
 	}
 	config.mockMdops.EXPECT().GetRange(gomock.Any(), cr.fbo.id(),
 		mergedHead+1, gomock.Any()).Return(nil, nil)
@@ -84,4 +84,64 @@ func TestCRInput(t *testing.T) {
 	// This should return without doing anything (i.e., without
 	// calling any mock methods)
 	cr.Wait(ctx)
+}
+
+func TestCRInputFracturedRange(t *testing.T) {
+	mockCtrl, config, cr := crTestInit(t)
+	defer crTestShutdown(mockCtrl, config, cr)
+	ctx := context.Background()
+
+	// Next, try resolving a few items
+	branchPoint := MetadataRevision(2)
+	unmergedHead := MetadataRevision(5)
+	mergedHead := MetadataRevision(15)
+
+	cr.fbo.head = &RootMetadata{
+		Revision: unmergedHead,
+		Flags:    MetadataFlagUnmerged,
+	}
+	// serve all the MDs from the cache
+	for i := unmergedHead; i >= branchPoint+1; i-- {
+		config.mockMdcache.EXPECT().Get(cr.fbo.id(), i, Unmerged).Return(
+			&RootMetadata{
+				Revision: i,
+				Flags:    MetadataFlagUnmerged,
+			}, nil)
+	}
+	for i := MetadataRevisionInitial; i <= branchPoint; i++ {
+		config.mockMdcache.EXPECT().Get(cr.fbo.id(), i, Unmerged).Return(
+			nil, NoSuchMDError{cr.fbo.id(), branchPoint, Unmerged})
+	}
+	config.mockMdops.EXPECT().GetUnmergedRange(gomock.Any(), cr.fbo.id(),
+		MetadataRevisionInitial, branchPoint).Return(nil, nil)
+
+	skipCacheRevision := MetadataRevision(10)
+	for i := branchPoint + 1; i <= mergedHead; i++ {
+		// Pretend that revision 10 isn't in the cache, and needs to
+		// be fetched from the server.
+		if i != skipCacheRevision {
+			config.mockMdcache.EXPECT().Get(cr.fbo.id(), i, Merged).Return(
+				&RootMetadata{Revision: i}, nil)
+		} else {
+			config.mockMdcache.EXPECT().Get(cr.fbo.id(), i, Merged).Return(
+				nil, NoSuchMDError{cr.fbo.id(), i, Merged})
+		}
+	}
+	config.mockMdops.EXPECT().GetRange(gomock.Any(), cr.fbo.id(),
+		skipCacheRevision, skipCacheRevision).Return(
+		[]*RootMetadata{&RootMetadata{Revision: skipCacheRevision}}, nil)
+	for i := mergedHead + 1; i <= branchPoint+2*maxMDsAtATime; i++ {
+		config.mockMdcache.EXPECT().Get(cr.fbo.id(), i, Merged).Return(
+			nil, NoSuchMDError{cr.fbo.id(), i, Merged})
+	}
+	config.mockMdops.EXPECT().GetRange(gomock.Any(), cr.fbo.id(),
+		mergedHead+1, gomock.Any()).Return(nil, nil)
+
+	// First try a completely unknown revision
+	cr.Resolve(unmergedHead, MetadataRevisionUninitialized)
+	cr.Wait(ctx)
+	// Make sure sure the input is up-to-date
+	if cr.currInput.merged != mergedHead {
+		t.Fatalf("Unexpected merged input: %d\n", cr.currInput.merged)
+	}
 }
