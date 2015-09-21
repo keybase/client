0043ff2ededfa6d80be1620096de3ac73fdfeb9f
Sidney San Martín <sidney@keyba.se>
2015-09-21 17:30:46 -0400

    32 characters it is
    Updates #912

--- go/client/cmd_list_trackers.go
+++ go/client/cmd_list_trackers.go
@@ -215,7 +215,7 @@ func (c *CmdListTrackers) ParseArgv(ctx *cli.Context) error {
 			var err error
 			c.uid, err = libkb.UIDFromHex(ctx.Args()[0])
 			if err != nil {
-				return fmt.Errorf("UID must be a 16-character hex string")
+				return fmt.Errorf("UID must be a 32-character hex string")
 			}
 		} else {
 			c.username = ctx.Args()[0]
