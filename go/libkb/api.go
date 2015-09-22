5ce842f91ef8973a1cd16a8dd0d0d1bb545c0477
Jack O'Connor <oconnor663@gmail.com>
2015-09-22 15:18:24 -0400

    use the IsBrewBuild and PackageName constants

--- go/libkb/api.go
+++ go/libkb/api.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"net/url"
 	"os/exec"
+	"runtime"
 	"strings"
 	"sync"
 	"time"
@@ -294,6 +295,17 @@ func (a *InternalAPIEngine) consumeHeaders(resp *http.Response) error {
 }
 
 func platformSpecificUpgradeInstructions() {
+	switch runtime.GOOS {
+	case "linux":
+		linuxUpgradeInstructions()
+	case "darwin":
+		darwinUpgradeInstructions()
+	case "windows":
+		// TODO
+	}
+}
+
+func linuxUpgradeInstructions() {
 	hasPackageManager := func(name string) bool {
 		// Not all package managers are in /usr/bin. (openSUSE for example puts
 		// Yast in /usr/sbin.) Better to just do the full check now than to get
@@ -301,21 +313,26 @@ func platformSpecificUpgradeInstructions() {
 		_, err := exec.LookPath(name)
 		return err == nil
 	}
-	printInstructions := func(command string) {
-		G.Log.Warning("To upgrade, run the following command:")
-		G.Log.Warning("    " + command)
-	}
 
 	if hasPackageManager("apt-get") {
-		printInstructions("sudo apt-get update && sudo apt-get install keybase")
+		printUpgradeCommand("sudo apt-get update && sudo apt-get install " + PackageName)
 	} else if hasPackageManager("dnf") {
-		printInstructions("sudo dnf upgrade keybase")
+		printUpgradeCommand("sudo dnf upgrade " + PackageName)
 	} else if hasPackageManager("yum") {
-		printInstructions("sudo yum upgrade keybase")
-	} else if hasPackageManager("brew") {
-		// TODO: Detect whether keybase was installed without brew. (Sparkle?)
-		printInstructions("brew update && brew upgrade keybase")
+		printUpgradeCommand("sudo yum upgrade " + PackageName)
 	}
+}
+
+func darwinUpgradeInstructions() {
+	if IsBrewBuild {
+		printUpgradeCommand("brew update && brew upgrade " + PackageName)
+	}
+	// TODO: non-brew update instructions
+}
+
+func printUpgradeCommand(command string) {
+	G.Log.Warning("To upgrade, run the following command:")
+	G.Log.Warning("    " + command)
 }
 
 func (a *InternalAPIEngine) fixHeaders(arg APIArg, req *http.Request) {
