95538cc1bb7db5a5f8cfb4203c247f9a94367069
Jeremy Stribling <strib@alum.mit.edu>
2015-09-22 10:13:44 -0700

    cr: for each unmerged chain, find the corresponding merged chain
    In order to apply the unmerged operations, we need to figure out the
    full merged paths for the corresponding nodes, possibly re-creating
    directories that have been removed.
    
    Issue: #321

--- libkbfs/conflict_resolver.go
+++ libkbfs/conflict_resolver.go
@@ -277,13 +277,19 @@ func (cr *ConflictResolver) getUnmergedPaths(ctx context.Context,
 	unmergedChains *crChains, mostRecentUnmergedMD *RootMetadata) (
 	[]path, error) {
 	newPtrs := make(map[BlockPointer]bool)
-	ptrs := make([]BlockPointer, 0, len(unmergedChains.byMostRecent))
-	for ptr := range unmergedChains.byMostRecent {
+	var ptrs []BlockPointer
+	for ptr, chain := range unmergedChains.byMostRecent {
 		newPtrs[ptr] = true
-		ptrs = append(ptrs, ptr)
+		// We only care about the paths for ptrs that are directly
+		// affected by operations and were live through the entire
+		// unmerged branch.
+		if len(chain.ops) > 0 && !unmergedChains.isCreated(chain.original) &&
+			!unmergedChains.isDeleted(chain.original) {
+			ptrs = append(ptrs, ptr)
+		}
 	}
 
-	nodeMap, err := cr.fbo.searchForNodes(ctx, ptrs, newPtrs,
+	nodeMap, err := cr.fbo.searchForNodes(ctx, cr.fbo.nodeCache, ptrs, newPtrs,
 		mostRecentUnmergedMD)
 	if err != nil {
 		return nil, err
@@ -304,9 +310,264 @@ func (cr *ConflictResolver) getUnmergedPaths(ctx context.Context,
 	}
 
 	// Order by descending path length.
-	var sp crSortedPaths = paths
-	sort.Sort(sp)
-	return sp, nil
+	sort.Sort(crSortedPaths(paths))
+	return paths, nil
+}
+
+type createMapKey struct {
+	ptr  BlockPointer
+	name string
+}
+
+// resolvedMergedPathTail takes an unmerged path, and returns as much
+// of the tail-end of the corresponding merged path that it can, using
+// only information within the chains.  It may not be able to return a
+// complete chain if, for example, a directory was changed in the
+// unmerged branch but not in the merged branch, and so the merged
+// chain would not have enough information to construct the merged
+// branch completely. This function returns the partial path, as well
+// as the most recent pointer to the first changed node in the merged
+// chains (which can be subsequently used to find the beginning of the
+// merged path).
+//
+// The given unmerged path should be for a node that wasn't created
+// during the unmerged branch.
+//
+// It is also possible for directories used in the unmerged path to
+// have been completely removed from the merged path.  In this case,
+// they need to be recreated.  So this function also returns a slice
+// of create ops that will need to be replayed in the merged branch
+// for the conflicts to be resolved.
+func (cr *ConflictResolver) resolveMergedPathTail(ctx context.Context,
+	unmergedPath path, unmergedChains *crChains,
+	mergedChains *crChains) (path, BlockPointer, []*createOp, error) {
+	unmergedOriginal, err :=
+		unmergedChains.originalFromMostRecent(unmergedPath.tailPointer())
+	if err != nil {
+		cr.log.CDebugf(ctx, "Couldn't find original pointer for %v",
+			unmergedPath.tailPointer())
+		return path{}, BlockPointer{}, nil, err
+	}
+
+	var recreateOps []*createOp // fill in backwards, and reverse at the end
+	currOriginal := unmergedOriginal
+	currPath := unmergedPath
+	mergedPath := path{
+		FolderBranch: unmergedPath.FolderBranch,
+		path:         nil, // fill in backwards, and reverse at the end
+	}
+
+	// First find the earliest merged parent.
+	for mergedChains.isDeleted(currOriginal) {
+		cr.log.CDebugf(ctx, "%v was deleted in the merged branch (%s)",
+			currOriginal, currPath)
+		if !currPath.hasValidParent() {
+			return path{}, BlockPointer{}, nil,
+				fmt.Errorf("Couldn't find valid merged parent path for %v",
+					unmergedOriginal)
+		}
+
+		// If this node has been deleted, we need to search
+		// backwards in the path to find the latest node that
+		// hasn't been deleted and re-recreate nodes upward from
+		// there.
+		name := currPath.tailName()
+		mergedPath.path = append(mergedPath.path, pathNode{
+			BlockPointer: currOriginal,
+			Name:         name,
+		})
+		currPath = *currPath.parentPath()
+		currOriginal, err =
+			unmergedChains.originalFromMostRecent(currPath.tailPointer())
+		if err != nil {
+			cr.log.CDebugf(ctx, "Couldn't find original pointer for %v",
+				currPath.tailPointer())
+			return path{}, BlockPointer{}, nil, err
+		}
+
+		recreateOps = append(recreateOps, newCreateOp(name, currOriginal,
+			File /*placeholder type, will look up actual type later*/))
+	}
+	if len(recreateOps) > 0 {
+		// The final recreateOp (the one closest to the root) can
+		// actually use the most recent merged pointer, since we know
+		// the parent directory exists.
+		lastCreateOriginal := recreateOps[len(recreateOps)-1].Dir.Unref
+		mergedMostRecent, err :=
+			mergedChains.mostRecentFromOriginal(lastCreateOriginal)
+		if err != nil {
+			// This directory wasn't touched on the merged branch
+			mergedMostRecent = lastCreateOriginal
+		}
+		recreateOps[len(recreateOps)-1].Dir.Unref = mergedMostRecent
+	}
+
+	// Now we have the latest pointer along the path that is
+	// shared between the branches.  Our next step is to find the
+	// current merged path to the most recent version of that
+	// original.  We can do that as follows:
+	// * If the pointer has been changed in the merged branch, we
+	//   can search for it later using fbo.searchForNodes
+	// * If it hasn't been changed, check if it has been renamed to
+	//   somewhere else.  If so, use fbo.searchForNodes on that parent later.
+	// * Otherwise, iterate up the path towards the root.
+	var mostRecent BlockPointer
+	for i := len(currPath.path) - 1; i >= 0; i-- {
+		currOriginal, err := unmergedChains.originalFromMostRecent(
+			currPath.path[i].BlockPointer)
+		if err != nil {
+			cr.log.CDebugf(ctx, "Couldn't find original pointer for %v",
+				currPath.path[i])
+			return path{}, BlockPointer{}, nil, err
+		}
+
+		// Has it changed in the merged branch?
+		mostRecent, err = mergedChains.mostRecentFromOriginal(currOriginal)
+		if err == nil {
+			break
+		}
+
+		mergedPath.path = append(mergedPath.path, pathNode{
+			BlockPointer: currOriginal,
+			Name:         currPath.path[i].Name,
+		})
+
+		// Has it been renamed?
+		if originalParent, newName, ok :=
+			mergedChains.renamedParentAndName(currOriginal); ok {
+			cr.log.CDebugf(ctx, "%v has been renamed in the merged branch",
+				currOriginal)
+			mostRecentParent, err :=
+				mergedChains.mostRecentFromOriginal(originalParent)
+			if err != nil {
+				cr.log.CDebugf(ctx, "Couldn't find original pointer for %v",
+					originalParent)
+				return path{}, BlockPointer{}, nil, err
+			}
+			mostRecent = mostRecentParent
+			// update the name for this renamed node
+			mergedPath.path[len(mergedPath.path)-1].Name = newName
+			break
+		}
+	}
+
+	// reverse the merged path
+	for i, j := 0, len(mergedPath.path)-1; i < j; i, j = i+1, j-1 {
+		mergedPath.path[i], mergedPath.path[j] =
+			mergedPath.path[j], mergedPath.path[i]
+	}
+
+	// reverse recreateOps
+	for i, j := 0, len(recreateOps)-1; i < j; i, j = i+1, j-1 {
+		recreateOps[i], recreateOps[j] = recreateOps[j], recreateOps[i]
+	}
+
+	return mergedPath, mostRecent, recreateOps, nil
+}
+
+// resolveMergedPaths maps each tail most recent pointer for all the
+// given unmerged paths to a corresponding path in the merged branch.
+// The merged branch may be missing some nodes that have been deleted;
+// in that case, the merged path will contain placeholder path nodes
+// using the original pointers for those directories.
+//
+// This function also returns a set of createOps that can be used to
+// recreate the missing directories in the merged branch.  If the
+// parent directory needing the create has been deleted, then the
+// unref ptr in the createOp contains the original pointer for the
+// directory rather than the most recent merged pointer.
+func (cr *ConflictResolver) resolveMergedPaths(ctx context.Context,
+	unmergedPaths []path, unmergedChains *crChains, mergedChains *crChains,
+	mostRecentMergedMD *RootMetadata) (
+	map[BlockPointer]path, []*createOp, error) {
+	// maps each most recent unmerged pointer to the corresponding
+	// most recent merged path.
+	mergedPaths := make(map[BlockPointer]path)
+
+	if len(unmergedPaths) == 0 {
+		return mergedPaths, nil, nil
+	}
+
+	// For each unmerged path, find the corresponding most recent
+	// pointer in the merged path.  Track which entries need to be
+	// re-created.
+	var recreateOps []*createOp
+	createsSeen := make(map[createMapKey]bool)
+	// maps a merged most recent pointer to the set of unmerged most
+	// recent pointers that need some of their path filled in.
+	chainsToSearchFor := make(map[BlockPointer][]BlockPointer)
+	for _, p := range unmergedPaths {
+		mergedPath, mostRecent, ops, err := cr.resolveMergedPathTail(
+			ctx, p, unmergedChains, mergedChains)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		// Save any recreateOps we've haven't seen yet.
+		for _, op := range ops {
+			key := createMapKey{op.Dir.Unref, op.NewName}
+			if _, ok := createsSeen[key]; ok {
+				continue
+			}
+			createsSeen[key] = true
+			recreateOps = append(recreateOps, op)
+		}
+
+		// Remember to fill in the corresponding mergedPath once we
+		// get mostRecent's full path.
+		chainsToSearchFor[mostRecent] =
+			append(chainsToSearchFor[mostRecent], p.tailPointer())
+
+		// At the end of this process, we are left with a merged path
+		// that begins just after currOriginal.  We will fill this in
+		// later with the searchFromNodes result.
+		mergedPaths[p.tailPointer()] = mergedPath
+	}
+
+	// Now we can search for all the merged paths that need to be
+	// updated due to unmerged operations.  Start with a clean node
+	// cache for the merged branch.
+	mergedNodeCache := newNodeCacheStandard(cr.fbo.folderBranch)
+	// Initialize the root node.  There will always be at least one
+	// unmerged path.
+	mergedNodeCache.GetOrCreate(mostRecentMergedMD.data.Dir.BlockPointer,
+		unmergedPaths[0].path[0].Name, nil)
+
+	newPtrs := make(map[BlockPointer]bool)
+	for ptr := range mergedChains.byMostRecent {
+		newPtrs[ptr] = true
+	}
+	ptrs := make([]BlockPointer, 0, len(chainsToSearchFor))
+	for ptr := range chainsToSearchFor {
+		ptrs = append(ptrs, ptr)
+	}
+
+	nodeMap, err := cr.fbo.searchForNodes(ctx, mergedNodeCache, ptrs, newPtrs,
+		mostRecentMergedMD)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	for ptr, n := range nodeMap {
+		if n == nil {
+			// All the pointers we're looking for should definitely be
+			// findable in the merged branch somewhere.
+			return nil, nil, NodeNotFoundError{ptr}
+		}
+
+		p := mergedNodeCache.PathFromNode(n)
+		for _, unmergedMostRecent := range chainsToSearchFor[ptr] {
+			// Prepend the found path to the existing path
+			mergedPath := mergedPaths[unmergedMostRecent]
+			newPath := make([]pathNode, len(p.path)+len(mergedPath.path))
+			copy(newPath[:len(p.path)], p.path)
+			copy(newPath[len(p.path):], mergedPath.path)
+			mergedPath.path = newPath
+			mergedPaths[unmergedMostRecent] = mergedPath
+		}
+	}
+
+	return mergedPaths, recreateOps, nil
 }
 
 func (cr *ConflictResolver) doResolve(ctx context.Context, ci conflictInput) {
@@ -349,32 +610,31 @@ func (cr *ConflictResolver) doResolve(ctx context.Context, ci conflictInput) {
 	}
 
 	// Make the chains
-	unmergedChains, _, err := cr.makeChains(ctx, unmerged, merged)
+	unmergedChains, mergedChains, err := cr.makeChains(ctx, unmerged, merged)
 	if err != nil {
 		return
 	}
 
-	// Get the full path for every most recent pointer with a chain of
-	// unmerged operations. If the path doesn't exist, that means the
-	// whole node was deleted, so skip.  The returned paths are
-	// ordered by descending path length.
-	_, err = cr.getUnmergedPaths(ctx, unmergedChains, unmerged[len(unmerged)-1])
+	// TODO: if the root node didn't change in either chain, we can
+	// short circuit the rest of the process with a really easy
+	// merge...
+
+	// Get the full path for every most recent unmerged pointer with a
+	// chain of unmerged operations, and which was not created or
+	// deleted within in the unmerged branch.
+	unmergedPaths, err := cr.getUnmergedPaths(ctx, unmergedChains,
+		unmerged[len(unmerged)-1])
 	if err != nil {
 		return
 	}
 
-	// * Find the corresponding path in the merged/resolved branch
-	//   * Find the original pointer for the most recent pointer.
-	//   * Find the merged most recent pointer of that original pointer.
-	//   * Search for the full path to that most recent pointer in the final
-	//     state of B.
-	//   * If it exists, great!
-	//   * If it doesn't exist, repeat on the next component of the path
-	//     (closer to the root), until you find a path that does exist.
-	//   * Now add createOps as necessary to resolvedChains in order to
-	//     create the full path again, and add entries to the corresponding
-	//     dirty blocks.  Use the most recent pointer unmerged block pointer
-	//     for the directory as the starting pointer for the new directory.
+	// * Find the corresponding path in the merged branch for each of
+	// these unmerged paths, and the set of any createOps needed to
+	// apply these unmerged operations in the merged branch.
+	_, _, err = cr.resolveMergedPaths(ctx, unmergedPaths, unmergedChains,
+		mergedChains, merged[len(merged)-1])
+
+	// TODO:
 	// * For syncOps, add a syncAttr setAttrOp to the parent unmerged dir chain
 	//   * If there's a conflict, add a resolving createOp to the parent.
 	// * Otherwise, for each operation in the unmerged chain, check for
