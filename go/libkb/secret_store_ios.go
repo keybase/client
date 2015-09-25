303e9777014fabbbfde1f74a7bb4125ac7faa999
Gabriel Handford <gabrielh@gmail.com>
2015-09-24 19:55:11 -0700

    iOS keychain support

--- go/libkb/secret_store_ios.go
+++ go/libkb/secret_store_ios.go
@@ -1,22 +1,52 @@
 // +build darwin,ios
 
-// TODO
-
 package libkb
 
+import (
+	keychain "github.com/keybase/go-keychain"
+)
+
+const (
+	keychainServiceName = "keybase"
+	accessGroup         = ""
+)
+
+type KeychainSecretStore struct {
+	accountName string
+}
+
+func (k KeychainSecretStore) StoreSecret(secret []byte) (err error) {
+	// TODO: Access group for iOS
+	item := keychain.NewGenericPassword(keychainServiceName, k.accountName, "", secret, accessGroup)
+	item.SetSynchronizable(keychain.SynchronizableNo)
+	item.SetAccessible(keychain.AccessibleWhenUnlockedThisDeviceOnly)
+
+	keychain.DeleteItem(item)
+	return keychain.AddItem(item)
+}
+
+func (k KeychainSecretStore) RetrieveSecret() ([]byte, error) {
+	return keychain.GetGenericPassword(keychainServiceName, k.accountName, "", "")
+}
+
+func (k KeychainSecretStore) ClearSecret() (err error) {
+	query := keychain.NewGenericPassword(keychainServiceName, k.accountName, "", nil, "")
+	query.SetMatchLimit(keychain.MatchLimitAll)
+	return keychain.DeleteItem(query)
+}
+
 func NewSecretStore(username NormalizedUsername) SecretStore {
-	return nil
+	return KeychainSecretStore{string(username)}
 }
 
 func HasSecretStore() bool {
-	return false
+	return true
 }
 
 func GetUsersWithStoredSecrets() ([]string, error) {
-	return nil, nil
+	return keychain.GetAccountsForService(keychainServiceName)
 }
 
 func GetTerminalPrompt() string {
-	// TODO: Come up with specific prompts for other platforms.
-	return "Store your key in the local secret store?"
+	return "Store your key in Apple's local keychain?"
 }
