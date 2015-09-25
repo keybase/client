db1fc84eb5bb773b8ff3a148524efed7436184b2
Jeremy Stribling <strib@alum.mit.edu>
2015-09-25 13:13:36 -0700

    key_bundle: add a test for filling in devices
    Issue: #385

--- libkbfs/key_bundle_test.go
+++ libkbfs/key_bundle_test.go
@@ -17,6 +17,45 @@ func testKeyBundleGetKeysOrBust(t *testing.T, config Config, uid keybase1.UID,
 	keys[uid] = publicKeys
 }
 
+func testKeyBundleCheckKeys(t *testing.T, config Config, uid keybase1.UID,
+	dkb DirKeyBundle, ePubKey TLFEphemeralPublicKey,
+	tlfCryptKey TLFCryptKey, serverMap serverKeyMap) {
+	ctx := context.Background()
+	// Check that every user can recover the crypt key
+	cryptPublicKey, err := config.KBPKI().GetCurrentCryptPublicKey(ctx)
+	if err != nil {
+		t.Fatalf("Couldn't get current public key for user %s: %v", uid, err)
+	}
+	info, ok, err := dkb.GetTLFCryptKeyInfo(uid, cryptPublicKey)
+	if !ok || err != nil {
+		t.Fatalf("Couldn't get current key info for user %s: %v", uid, err)
+	}
+	userEPubKey, err := dkb.GetTLFEphemeralPublicKey(uid, cryptPublicKey)
+	if err != nil {
+		t.Fatalf("Error getting ephemeral public key for user %s: %v", uid, err)
+	}
+	if g, e := userEPubKey, ePubKey; g != e {
+		t.Fatalf("Unexpected ePubKey for user %s: %s vs %s", uid, g, e)
+	}
+	clientHalf, err := config.Crypto().DecryptTLFCryptKeyClientHalf(
+		ctx, userEPubKey, info.ClientHalf)
+	if err != nil {
+		t.Fatalf("Couldn't decrypt client key half for user %s: %v", uid, err)
+	}
+	serverHalf, ok := serverMap[uid][cryptPublicKey.KID]
+	if !ok {
+		t.Fatalf("No server half for user %s", uid)
+	}
+	userTLFCryptKey, err :=
+		config.Crypto().UnmaskTLFCryptKey(serverHalf, clientHalf)
+	if err != nil {
+		t.Fatalf("Couldn't unmask TLF key for user %s: %v", uid, err)
+	}
+	if g, e := userTLFCryptKey, tlfCryptKey; g != e {
+		t.Fatalf("TLF crypt key didn't match for user %s: %s vs. %s", uid, g, e)
+	}
+}
+
 func TestKeyBundleFillInDevices(t *testing.T) {
 	config1 := MakeTestConfigOrBust(t, "u1", "u2", "u3")
 	defer config1.Shutdown()
@@ -25,15 +64,16 @@ func TestKeyBundleFillInDevices(t *testing.T) {
 	config3 := ConfigAsUser(config1, "u3")
 	defer config3.Shutdown()
 
-	u1, err := config1.KBPKI().GetCurrentUID(context.Background())
+	ctx := context.Background()
+	u1, err := config1.KBPKI().GetCurrentUID(ctx)
 	if err != nil {
 		t.Fatalf("Couldn't get uid for user 1: %v", err)
 	}
-	u2, err := config2.KBPKI().GetCurrentUID(context.Background())
+	u2, err := config2.KBPKI().GetCurrentUID(ctx)
 	if err != nil {
 		t.Fatalf("Couldn't get uid for user 2: %v", err)
 	}
-	u3, err := config3.KBPKI().GetCurrentUID(context.Background())
+	u3, err := config3.KBPKI().GetCurrentUID(ctx)
 	if err != nil {
 		t.Fatalf("Couldn't get uid for user 3: %v", err)
 	}
@@ -59,14 +99,43 @@ func TestKeyBundleFillInDevices(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Couldn't make keys: %v", err)
 	}
-	_, err = dkb.fillInDevices(config1.Crypto(), wKeys, rKeys,
+	serverMap, err := dkb.fillInDevices(config1.Crypto(), wKeys, rKeys,
 		ePubKey, ePrivKey, tlfCryptKey)
 	if err != nil {
 		t.Fatalf("Fill in devices failed: %v", err)
 	}
 
-	// Add a device key
+	testKeyBundleCheckKeys(t, config1, u1, dkb, ePubKey, tlfCryptKey, serverMap)
+	testKeyBundleCheckKeys(t, config2, u2, dkb, ePubKey, tlfCryptKey, serverMap)
+	testKeyBundleCheckKeys(t, config3, u3, dkb, ePubKey, tlfCryptKey, serverMap)
+
+	// Add a device key for user 1
+	devIndex := AddDeviceForLocalUserOrBust(t, config1, u1)
+	config1B := ConfigAsUser(config1, "u1")
+	defer config1B.Shutdown()
+	SwitchDeviceForLocalUserOrBust(t, config1B, devIndex)
+	newCryptPublicKey, err := config1B.KBPKI().GetCurrentCryptPublicKey(ctx)
+	if err != nil {
+		t.Fatalf("COuldn't get new publc device key for user %s: %v", u1, err)
+	}
+	wKeys[u1] = append(wKeys[u1], newCryptPublicKey)
 
 	// Fill in the bundle again, make sure only the new device key
 	// gets a ePubKeyIndex bump
+	_, _, ePubKey2, ePrivKey2, _, err := config1.Crypto().MakeRandomTLFKeys()
+	if err != nil {
+		t.Fatalf("Couldn't make keys: %v", err)
+	}
+	serverMap2, err := dkb.fillInDevices(config1.Crypto(), wKeys, rKeys,
+		ePubKey2, ePrivKey2, tlfCryptKey)
+	if err != nil {
+		t.Fatalf("Fill in devices failed: %v", err)
+	}
+
+	testKeyBundleCheckKeys(t, config1B, u1, dkb, ePubKey2, tlfCryptKey,
+		serverMap2)
+	if len(serverMap2) > 1 {
+		t.Fatalf("Generated more than one key after device add: %d",
+			len(serverMap2))
+	}
 }
