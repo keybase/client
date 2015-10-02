5aca7bf14cb306fb3e239335322a51bf1876752c
Marco Munizaga <marco@marcopolo.io>
2015-10-02 15:50:09 -0700

    Send a touch event when we are scrolling ViewPager

--- react-native/react/tabs/people.js
+++ react-native/react/tabs/people.js
@@ -9,11 +9,21 @@ const {
 } = React
 
 export default class People extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {count: 0}
+  }
+
   render () {
     return (
       <View style={{flex: 1, justifyContent: 'center'}}>
         <Text> People goes here </Text>
-        <Text> I mean, it’s one banana, Michael. What could it cost? Ten dollars? </Text>
+        <Text> Count: {this.state.count} </Text>
+        <Text
+          onPress={() => this.setState({count: this.state.count + 1})}
+          style={{fontSize: 32, marginTop: 20, marginBottom: 20}}>
+          I mean, it’s one banana, Michael. What could it cost? Ten dollars?
+        </Text>
       </View>
     )
   }
