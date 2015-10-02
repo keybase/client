57a14dcfef9651fa459885db468ef4aad8cc1dd8
Marco Munizaga <marco@marcopolo.io>
2015-10-01 17:14:38 -0700

    Fix keybase engine's handling of react native's lifecycle

--- react-native/android/app/src/main/java/io/keybase/android/MainActivity.java
+++ react-native/android/app/src/main/java/io/keybase/android/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends Activity implements DefaultHardwareBackBtnHand
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Init(this.getFilesDir().getPath(), "staging");
+        Init(this.getFilesDir().getPath(), "staging", "");
 
         mReactRootView = new ReactRootView(this);
         mReactInstanceManager = ReactInstanceManager.builder()
