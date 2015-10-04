9b9bab27df235ea968fc9191fc3496e648b44ed4
Steve Sanders <steve@zanderz.com>
2015-10-04 12:27:12 -0700

    Create locked file on Windows

---
+++ go/libkb/util_windows.go
@@ -0,0 +1,20 @@
+// +build windows
+
+package libkb
+
+import (
+	"os/exec"
+	"strings"
+)
+
+// LookPath searches for an executable binary named file
+// in the directories named by the PATH environment variable.
+// If file contains a slash, it is tried directly and the PATH is not consulted.
+
+func canExec(s string) error {
+	if strings.IndexAny(s, `:\/`) == -1 {
+		s = s + "/"
+	}
+	_, err := exec.LookPath(s)
+	return err
+}
