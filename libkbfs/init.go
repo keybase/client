5638e37dcec29ef026e87cb744e27670d870d6ed
Patrick Crosby <pcrosby@gmail.com>
2015-10-06 15:47:55 -0500

    Initialize client.GlobUI instead of libkb.G.UI
    Theory that this is why SecretEntry is nil.
    Also, checking error from UI.Configure.

--- libkbfs/init.go
+++ libkbfs/init.go
@@ -206,7 +206,13 @@ func Init(localUser libkb.NormalizedUsername, serverRootDir *string, cpuProfileP
 	config.SetKeyServer(keyServer)
 
 	client.InitUI()
-	libkb.G.UI.Configure()
+	if libkb.G.UI != client.GlobUI {
+		lg := logger.NewWithCallDepth("", 1)
+		lg.Warning("In libkbfs/init: libkb.G.UI != client.GlobUI, which would be why libkb.G.UI.Configure() would fail to initialize SecretEntry for client.GlobUI...")
+	}
+	if err := client.GlobUI.Configure(); err != nil {
+		return nil, fmt.Errorf("problem configuring UI: %s", err)
+	}
 
 	daemon, err := makeKeybaseDaemon(serverRootDir, localUser, config.Codec(), config.MakeLogger(""))
 	if err != nil {
