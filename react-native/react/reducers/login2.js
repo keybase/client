09d3d28e0de0de4db29859cce74029c0c4e1dae7
chrisnojima <cnojima@keyba.se>
2015-10-06 14:00:01 -0400

    removing some comments

--- react-native/react/reducers/login2.js
+++ react-native/react/reducers/login2.js
@@ -1,22 +1,6 @@
 'use strict'
 
-//import * as screen from '../constants/login2Screens'
-//import * as types from '../constants/login2ActionTypes'
-
-/*
-screen.WELCOME
-screen.RESET_ACCOUNT
-screen.CHOOSE_REG_TYPE
-screen.REG_PAPER_KEY
-screen.REG_USER_PASS
-screen.REG_CHOOSE_EXISTING_DEVICE
-screen.REG_EXISTING_IS_COMPUTER
-screen.REG_EXISTING_IS_PHONE
-screen.NEW_DEVICE_NAME
-*/
-
 const initialState = {
-//   screen: screen.WELCOME
 }
 
 export default function (state = initialState, action) {
