374dc0481208e1b7865879c81bfb1c4d703456e9
Marco Munizaga <marco@marcopolo.io>
2015-10-09 16:12:43 -0700

    Support Android M's KeyStore

--- react-native/android/app/src/main/java/io/keybase/android/keystore/KeyStoreHelper.java
+++ react-native/android/app/src/main/java/io/keybase/android/keystore/KeyStoreHelper.java
@@ -6,19 +6,24 @@ import android.os.Build;
 import android.security.KeyChain;
 import android.security.KeyPairGeneratorSpec;
 import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyInfo;
 import android.security.keystore.KeyProperties;
 
 import java.math.BigInteger;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
-import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
+import java.security.PrivateKey;
 import java.security.spec.AlgorithmParameterSpec;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.RSAKeyGenParameterSpec;
 import java.util.Calendar;
 
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
 import javax.security.auth.x500.X500Principal;
 
 public class KeyStoreHelper {
@@ -28,23 +33,22 @@ public class KeyStoreHelper {
         KeyPairGenerator kpg = KeyPairGenerator.getInstance(KeyProperties.KEY_ALGORITHM_RSA, "AndroidKeyStore");
 
         final AlgorithmParameterSpec spec;
-        final boolean isOnHardware;
+        final Calendar endTime = Calendar.getInstance();
+        endTime.add(Calendar.YEAR, 10);
 
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
-            // TODO: this this on android M
             spec = new KeyGenParameterSpec.Builder(
               keyAlias,
               KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT)
+              .setBlockModes(KeyProperties.BLOCK_MODE_ECB)
+              .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_RSA_PKCS1)
+              .setCertificateSerialNumber(BigInteger.ONE)
+              .setCertificateSubject(new X500Principal("CN=" + keyAlias))
+              .setKeyValidityStart(Calendar.getInstance().getTime())
+              .setKeyValidityEnd(endTime.getTime())
+              .setKeySize(4096)
               .build();
-
-            // TODO look at key info and see if crypto is hardware bounded
         } else {
-            // TODO use this information
-            isOnHardware = KeyChain.isBoundKeyAlgorithm("RSA");
-
-            final Calendar endTime = Calendar.getInstance();
-            endTime.add(Calendar.YEAR, 10);
-
             spec = new KeyPairGeneratorSpec.Builder(ctx)
               .setAlias(keyAlias)
               .setEncryptionRequired()
@@ -58,5 +62,16 @@ public class KeyStoreHelper {
 
         kpg.initialize(spec);
         KeyPair kp = kpg.generateKeyPair();
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
+    public static boolean isOnHardware(PrivateKey privateKey) throws NoSuchProviderException, NoSuchAlgorithmException, InvalidKeySpecException {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            SecretKeyFactory factory = SecretKeyFactory.getInstance(privateKey.getAlgorithm(), "AndroidKeyStore");
+            final KeyInfo keyInfo = (KeyInfo) factory.getKeySpec((SecretKey) privateKey, KeyInfo.class);
+            return keyInfo.isInsideSecureHardware();
+        }
+        return KeyChain.isBoundKeyAlgorithm("RSA");
+
     }
 }
