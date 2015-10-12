a921487b534522819027bff320bb8f8439fa948e
Marco Munizaga <marco@marcopolo.io>
2015-10-12 15:20:39 -0700

    Update react native to v12

--- react-native/android/app/src/main/java/io/keybase/android/MainActivity.java
+++ react-native/android/app/src/main/java/io/keybase/android/MainActivity.java
@@ -40,8 +40,12 @@ public class MainActivity extends Activity implements DefaultHardwareBackBtnHand
 
     @Override
     public boolean onKeyUp(int keyCode, KeyEvent event) {
+        // TODO(mm) check if we are in a debug build
         if (keyCode == KeyEvent.KEYCODE_MENU && mReactInstanceManager != null) {
             mReactInstanceManager.showDevOptionsDialog();
+            return true;
+        } else if (keyCode == KeyEvent.KEYCODE_VOLUME_UP && mReactInstanceManager != null) {
+            mReactInstanceManager.getDevSupportManager().handleReloadJS();
             return true;
         }
         return super.onKeyUp(keyCode, event);
@@ -60,7 +64,6 @@ public class MainActivity extends Activity implements DefaultHardwareBackBtnHand
             super.onBackPressed();
         }
     }
-
 
     @Override
     protected void onPause() {
