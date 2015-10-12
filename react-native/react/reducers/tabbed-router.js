f7eabe9a70ceb50068538f2a6ee5d5d3dc70f16e
chrisnojima <cnojima@keyba.se>
2015-10-12 10:09:09 -0400

    removing jump to paperkey page

--- react-native/react/reducers/tabbed-router.js
+++ react-native/react/reducers/tabbed-router.js
@@ -9,10 +9,10 @@ import routerReducer, { createRouterState } from './router'
 import {FOLDER_TAB, CHAT_TAB, PEOPLE_TAB, DEVICES_TAB, MORE_TAB} from '../constants/tabs'
 import * as actionTypes from '../constants/tabbed-router-action-types'
 
-// const emptyRouterState = createRouterState([], [])
+const emptyRouterState = createRouterState([], [])
 
 // Uncomment this to start at a specific page to help speed up
-const emptyRouterState = createRouterState(['login2', 'register', 'regPaperKey'], [])
+// const emptyRouterState = createRouterState(['login2', 'register', 'regPaperKey'], [])
 
 // TODO(mm) add type annotations
 const initialState = Immutable.fromJS({
