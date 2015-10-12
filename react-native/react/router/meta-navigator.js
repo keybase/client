2f34421cf3888de7d95b2e0349d7568f310f708c
Marco Munizaga <marco@marcopolo.io>
2015-10-12 15:33:05 -0700

    Fix mapStateToProps bug

--- react-native/react/router/meta-navigator.js
+++ react-native/react/router/meta-navigator.js
@@ -117,7 +117,7 @@ class MetaNavigator extends Component {
         renderScene={(route, navigator) => {
           return (
             <View style={{flex: 1, marginTop: route.hideNavBar ? 0 : this.props.navBarHeight}}>
-              {React.createElement(connect(componentAtTop.mapStateToProps || (state => state))(route.component), {...route.props})}
+              {React.createElement(connect(route.mapStateToProps || (state => state))(route.component), {...route.props})}
             </View>
           )
         }}
