d572d681f7b513aed86f354e6b37595335a2b0b9
chrisnojima <cnojima@keyba.se>
2015-10-12 09:36:33 -0400

    small cleanup

--- react-native/react/common-adapters/button.js
+++ react-native/react/common-adapters/button.js
@@ -10,20 +10,19 @@ export default class Button extends Component {
   }
 
   render () {
+    const textStyles = [commonStyles.button, this.props.buttonStyle, enabled ? {} : commonStyles.disabledButton]
+    const containerStyles = [commonStyles.buttonHighlight, this.props.style]
+
     const enabled = this.props.hasOwnProperty('enabled') ? this.props.enabled : true
+    const child = this.props.children ? this.props.children : (<Text style={textStyles}>{this.props.title}</Text>)
 
-    const child = this.props.children ? this.props.children
-    : <Text style={[commonStyles.button, this.props.buttonStyle, enabled ? {} : commonStyles.disabledButton]}
-      >{this.props.title}</Text>
-
-    return !enabled ? (
-      <View style={[commonStyles.buttonHighlight, this.props.style]} children={child}/>
-    ) : (
+    return !enabled ? (<View style={containerStyles}>{child}</View>) : (
       <TouchableHighlight
         underlayColor={enabled ? buttonHighlight : disabledButtonHighlight }
         onPress={enabled ? this.props.onPress : null}
-        style={[commonStyles.buttonHighlight, this.props.style]}
-        children={child}/>
+        style={containerStyles}>
+        {child}
+      </TouchableHighlight>
     )
   }
 }
