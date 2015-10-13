4678e136765c4cf740cf04b69583241145e01648
Patrick Crosby <pcrosby@gmail.com>
2015-10-13 14:10:06 -0500

    Implement kex2.MessageRouter

---
+++ go/libkb/kex2_router.go
@@ -0,0 +1,85 @@
+package libkb
+
+import (
+	"encoding/base64"
+	"time"
+
+	"github.com/keybase/client/go/kex2"
+)
+
+// KexRouter implements the kex2.MessageRouter interface.
+type KexRouter struct {
+	Contextified
+}
+
+// NewKexRouter creates a contextified KexRouter.
+func NewKexRouter(g *GlobalContext) *KexRouter {
+	return &KexRouter{Contextified: NewContextified(g)}
+}
+
+// Post implements Post in the kex2.MessageRouter interface.
+func (k *KexRouter) Post(sessID kex2.SessionID, sender kex2.DeviceID, seqno kex2.Seqno, msg []byte) (err error) {
+	k.G().Log.Debug("+ KexRouter.Post(%x, %x, %d, msg)", sessID, sender, seqno)
+	defer func() {
+		k.G().Log.Debug("- KexRouter.Post(%x, %x, %d) -> %s", sessID, sender, seqno, ErrToOk(err))
+	}()
+
+	_, err = k.G().API.Post(APIArg{
+		Endpoint: "kex2/send",
+		Args: HTTPArgs{
+			"I":      HexArg(sessID[:]),
+			"sender": HexArg(sender[:]),
+			"seqno":  I{Val: int(seqno)},
+			"msg":    B64Arg(msg),
+		},
+		Contextified: NewContextified(k.G()),
+	})
+
+	return err
+}
+
+// Get implements Get in the kex2.MessageRouter interface.
+func (k *KexRouter) Get(sessID kex2.SessionID, receiver kex2.DeviceID, low kex2.Seqno, poll time.Duration) (msgs [][]byte, err error) {
+	k.G().Log.Debug("+ KexRouter.Get(%x, %x, %d, %s)", sessID, receiver, low, poll)
+	defer func() {
+		k.G().Log.Debug("- KexRouter.Get(%x, %x, %d, %s) -> %s (messages: %d)", sessID, receiver, low, poll, ErrToOk(err), len(msgs))
+	}()
+
+	arg := APIArg{
+		Endpoint: "kex2/receive",
+		Args: HTTPArgs{
+			"I":        HexArg(sessID[:]),
+			"receiver": HexArg(receiver[:]),
+			"low":      I{Val: int(low)},
+			"poll":     I{Val: int(poll / time.Millisecond)},
+		},
+		Contextified: NewContextified(k.G()),
+	}
+
+	var j struct {
+		Msgs []struct {
+			Msg string `json:"msg"`
+		} `json:"msgs"`
+		Status struct {
+			Code int    `json:"code"`
+			Name string `json:"name"`
+			Desc string `json:"desc"`
+		}
+	}
+	if err = k.G().API.GetDecode(arg, &j); err != nil {
+		return nil, err
+	}
+	if j.Status.Code != SCOk {
+		return nil, AppStatusError{Code: j.Status.Code, Name: j.Status.Name, Desc: j.Status.Desc}
+	}
+
+	for _, m := range j.Msgs {
+		dec, err := base64.StdEncoding.DecodeString(m.Msg)
+		if err != nil {
+			return nil, err
+		}
+		msgs = append(msgs, dec)
+	}
+
+	return msgs, nil
+}
---
+++ go/libkb/kex2_router_test.go
@@ -0,0 +1,109 @@
+package libkb
+
+import (
+	"sync"
+	"testing"
+	"time"
+
+	"crypto/rand"
+
+	"github.com/keybase/client/go/kex2"
+)
+
+type ktester struct {
+	sender   kex2.DeviceID
+	receiver kex2.DeviceID
+	I        kex2.SessionID
+	seqno    kex2.Seqno
+}
+
+func newKtester() *ktester {
+	kt := &ktester{}
+	if _, err := rand.Read(kt.sender[:]); err != nil {
+		panic(err)
+	}
+	if _, err := rand.Read(kt.receiver[:]); err != nil {
+		panic(err)
+	}
+	if _, err := rand.Read(kt.I[:]); err != nil {
+		panic(err)
+	}
+
+	return kt
+}
+
+func (k *ktester) post(mr kex2.MessageRouter, s string) error {
+	k.seqno++
+	return mr.Post(k.I, k.sender, k.seqno, []byte(s))
+}
+
+func (k *ktester) get(mr kex2.MessageRouter, low kex2.Seqno, poll time.Duration) ([][]byte, error) {
+	return mr.Get(k.I, k.receiver, low, poll)
+}
+
+func TestKex2Router(t *testing.T) {
+	tc := SetupTest(t, "kex2 router")
+	mr := NewKexRouter(tc.G)
+	kt := newKtester()
+
+	m1 := "hello everybody"
+	m2 := "goodbye everybody"
+	m3 := "plaid shirt"
+
+	// test send 2 messages
+	if err := kt.post(mr, m1); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := kt.post(mr, m2); err != nil {
+		t.Fatal(err)
+	}
+
+	// test receive 2 messages
+	msgs, err := kt.get(mr, 0, 100*time.Millisecond)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(msgs) != 2 {
+		t.Fatalf("number of messages: %d, expected 2", len(msgs))
+	}
+	if string(msgs[0]) != m1 {
+		t.Errorf("message 0: %q, expected %q", msgs[0], m1)
+	}
+	if string(msgs[1]) != m2 {
+		t.Errorf("message 1: %q, expected %q", msgs[1], m2)
+	}
+
+	// test calling receive before send
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		var merr error
+		msgs, merr = kt.get(mr, 3, 20*time.Millisecond)
+		if merr != nil {
+			t.Errorf("receive error: %s", merr)
+		}
+		wg.Done()
+	}()
+
+	if err := kt.post(mr, m3); err != nil {
+		t.Fatal(err)
+	}
+
+	wg.Wait()
+	if len(msgs) != 1 {
+		t.Fatalf("number of messages: %d, expected 1", len(msgs))
+	}
+	if string(msgs[0]) != m3 {
+		t.Errorf("message: %q, expected %q", msgs[0], m3)
+	}
+
+	// test no messages ready
+	msgs, err = kt.get(mr, 4, 1*time.Millisecond)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(msgs) != 0 {
+		t.Errorf("number of messages: %d, expected 0", len(msgs))
+	}
+}
