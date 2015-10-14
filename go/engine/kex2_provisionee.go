817ee9574705dc72a649271a558c6717f4decd73
Patrick Crosby <pcrosby@gmail.com>
2015-10-14 10:23:42 -0500

    Implement interface with stubs

--- go/engine/kex2_provisionee.go
+++ go/engine/kex2_provisionee.go
@@ -1,7 +1,10 @@
 package engine
 
 import (
+	"github.com/keybase/client/go/kex2"
 	"github.com/keybase/client/go/libkb"
+	keybase1 "github.com/keybase/client/go/protocol"
+	rpc "github.com/keybase/go-framed-msgpack-rpc"
 )
 
 // Kex2Provisionee is an engine.
@@ -38,5 +41,28 @@ func (e *Kex2Provisionee) SubConsumers() []libkb.UIConsumer {
 
 // Run starts the engine.
 func (e *Kex2Provisionee) Run(ctx *Context) error {
+	e.G().Log.Debug("+ Kex2Provisionee.Run()")
+	karg := kex2.KexBaseArg{
+		Mr: libkb.NewKexRouter(e.G()),
+	}
+	parg := kex2.ProvisioneeArg{
+		KexBaseArg:  karg,
+		Provisionee: e,
+	}
+	err := kex2.RunProvisionee(parg)
+	e.G().Log.Debug("- Kex2Provisionee.Run() -> %s", libkb.ErrToOk(err))
+
+	return err
+}
+
+func (e *Kex2Provisionee) GetLogFactory() rpc.LogFactory {
+	return rpc.NewSimpleLogFactory(e.G().Log, nil)
+}
+
+func (e *Kex2Provisionee) HandleHello(harg keybase1.HelloArg) (keybase1.HelloRes, error) {
+	return "", nil
+}
+
+func (e *Kex2Provisionee) HandleDidCounterSign([]byte) error {
 	return nil
 }
