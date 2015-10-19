56fd5b415b2888202e7c90da5106fa9ea970142b
chrisnojima <cnojima@keyba.se>
2015-10-19 15:01:52 -0400

    WIP

--- react-native/react/reducers/config.js
+++ react-native/react/reducers/config.js
@@ -1,9 +1,9 @@
 'use strict'
 
-import * as types from '../constants/config-action-types'
+import * as Constants from '../constants/config'
 
 const initialState = {
-  loaded: false,
+  navState: Constants.navStartingUp,
   status: null,
   config: null,
   error: null
@@ -11,38 +11,48 @@ const initialState = {
 
 export default function (state = initialState, action) {
   switch (action.type) {
-    case types.STARTUP_LOADING:
+    case Constants.startupLoading:
       return {
         ...state,
-        loaded: false,
+        navState: Constants.navStartingUp,
         config: null,
         status: null,
         error: null
       }
-    case types.STARTUP_LOADED:
+    case Constants.startupLoaded:
+      let navState = Constants.navStartingUp
+
+      if (!action.error) {
+        if (!action.payload.status.registered) {
+          navState = Constants.navNeedsRegistration
+        } else if (!action.payload.status.loggedIn) {
+          navState = Constants.navNeedsLogin
+        }
+      }
+
       return {
         ...state,
         config: action.error ? null : action.payload.config,
         status: action.error ? null : action.payload.status,
-        loaded: !action.error,
-        error: action.error ? action.payload : null
+        error: action.error ? action.payload : null,
+        navState
       }
-    case types.DEV_CONFIG_LOADING:
+    case Constants.devConfigLoading:
       return {
         ...state,
         devConfig: null
       }
-    case types.DEV_CONFIG_LOADED:
+    case Constants.devConfigLoaded:
       return {
         ...state,
         devConfig: action.devConfig
       }
-    case types.DEV_CONFIG_SAVED:
+    case Constants.devConfigSaved:
       return {
         ...state,
         devConfig: null
       }
-    case types.DEV_CONFIG_UPDATE:
+    case Constants.devConfigUpdate:
       return {
         ...state,
         devConfig: {
