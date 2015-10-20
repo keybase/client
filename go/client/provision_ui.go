e93b43f710dbe670b25c5cbcc8911347772de127
Patrick Crosby <pcrosby@gmail.com>
2015-10-20 13:31:26 -0500

    Implement UI for ChooseProvisioningMethod

--- go/client/provision_ui.go
+++ go/client/provision_ui.go
@@ -1,6 +1,9 @@
 package client
 
 import (
+	"fmt"
+	"strings"
+
 	"golang.org/x/net/context"
 
 	"github.com/keybase/client/go/libkb"
@@ -13,7 +16,35 @@ type ProvisionUI struct {
 }
 
 func (p ProvisionUI) ChooseProvisioningMethod(ctx context.Context, arg keybase1.ChooseProvisioningMethodArg) (keybase1.ProvisionMethod, error) {
-	return keybase1.ProvisionMethod_DEVICE, nil
+	p.parent.Output("How would you like to sign this install of Keybase?\n\n")
+	p.parent.Output("(1) Use an existing device\n")
+	p.parent.Output("(2) Use a paper key\n")
+	p.parent.Output("(3) Use my Keybase passphrase\n")
+	max := 3
+	if len(arg.GpgUsers) > 0 {
+		p.parent.Printf("(4) Use GPG (private key found for %s)\n", strings.Join(arg.GpgUsers, ", "))
+		max = 4
+	}
+
+	var res keybase1.ProvisionMethod
+	ret, err := p.parent.PromptSelectionOrCancel("Choose a signing option", 1, max)
+	if err != nil {
+		if err == ErrInputCanceled {
+			return res, libkb.CanceledError{M: "user canceled input"}
+		}
+		return res, err
+	}
+	switch ret {
+	case 1:
+		return keybase1.ProvisionMethod_DEVICE, nil
+	case 2:
+		return keybase1.ProvisionMethod_PAPER_KEY, nil
+	case 3:
+		return keybase1.ProvisionMethod_PASSPHRASE, nil
+	case 4:
+		return keybase1.ProvisionMethod_GPG, nil
+	}
+	return res, fmt.Errorf("invalid provision option: %d", ret)
 }
 
 func NewProvisionUIProtocol() rpc.Protocol {
