de47a554379984ffe7e06095702cd4170f9ffa0c
Chris Ball <chris@printf.net>
2015-10-21 13:11:50 -0400

    Remove debugging

--- react-native/react/login/container-render.desktop.js
+++ react-native/react/login/container-render.desktop.js
@@ -4,8 +4,5 @@
 import React from 'react'
 
 export default function () {
-  console.log('inside login container')
-  console.log(this.props)
-  console.log(this.state)
   return (<div><p>login container {this.props.foo}</p></div>)
 }
