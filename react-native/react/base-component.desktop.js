51c92e3fe63b523f952e65162ca22fcfbbbca7c4
Chris Ball <chris@printf.net>
2015-10-28 11:30:40 -0400

    Remove unneeded stub

--- react-native/react/base-component.desktop.js
+++ react-native/react/base-component.desktop.js
@@ -3,9 +3,3 @@
 
 import { Component } from 'react'
 export default Component
-
-// Stubs for react-native call.
-export class AppRegistry {
-  constructor () {}
-  static registerComponent () {}
-}
