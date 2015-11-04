e7d746909d4b6db00cdc513acd6f90ac89fc490f
Marco Munizaga <marco@marcopolo.io>
2015-11-04 10:33:40 -0500

    Fix More Tab on Desktop

---
+++ react-native/react/tabs/more/account-render.desktop.js
@@ -0,0 +1,16 @@
+'use strict'
+/* @flow */
+
+import React from '../../base-react'
+import BaseComponent from '../../base-component'
+
+export default class AccountComponent extends BaseComponent {
+  render () {
+    return (
+      <div>
+        <h2>TODO</h2>
+        <p>Account Page</p>
+      </div>
+    )
+  }
+}
