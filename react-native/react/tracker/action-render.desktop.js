247d0f8379f4e0a2fefb4575690cfe5ff4f6982d
chrisnojima <cnojima@keyba.se>
2015-11-06 13:53:26 -0500

    Show / hide warning on follow uncheck

--- react-native/react/tracker/action-render.desktop.js
+++ react-native/react/tracker/action-render.desktop.js
@@ -9,17 +9,22 @@ export default class ActionRender extends BaseComponent {
   render () {
     return (
       <div style={{display: 'flex', flex: 1, justifyContent: 'space-between', padding: 10, backgroundColor: '#E0E0E0'}}>
-        <div style={{display: 'flex', alignItems: 'center'}}>
-          <Checkbox
-            style={{marginRight: -15}}
-            value='follow'
-            label={'Follow ' + this.props.username}
-            checked={this.props.shouldFollow}
-            onCheck={() => {
-              this.props.followChecked(!this.props.shouldFollow)
-            }}
-            />
-          <FloatingActionButton mini style={{fontSize: 25}} onTouchTap={() => this.props.onFollowHelp() }>?</FloatingActionButton>
+        <div style={{display: 'flex', flexDirection: 'column'}}>
+          <div style={{display: 'flex', alignItems: 'center', justifyContent: 'flex-start'}}>
+            <div style={{minWidth: 200} /* the checkbox does width 100% which is super annoying, fix this when styling */}>
+              <Checkbox
+                style={{marginRight: -15}}
+                value='follow'
+                label={'Follow ' + this.props.username}
+                checked={this.props.shouldFollow}
+                onCheck={() => {
+                  this.props.followChecked(!this.props.shouldFollow)
+                }}
+                />
+              </div>
+            <FloatingActionButton mini style={{fontSize: 25}} onTouchTap={() => this.props.onFollowHelp() }>?</FloatingActionButton>
+          </div>
+          {!this.props.shouldFollow && <p>You'll see this card every time you access the folder</p>}
         </div>
         <FlatButton style={{alignSelf: 'center'}} label='Close' primary onTouchTap={() => this.props.onClose()} />
       </div>
