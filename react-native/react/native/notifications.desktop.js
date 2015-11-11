a794a353fa1846b7b0364426eaedbcd87af84fbb
Marco Munizaga <marco@marcopolo.io>
2015-11-11 14:25:45 -0800

    Add temporary spot to register delegate identify UI and server for identify UI

--- react-native/react/native/notifications.desktop.js
+++ react-native/react/native/notifications.desktop.js
@@ -2,6 +2,7 @@
 /* @flow */
 
 import engine from '../engine'
+import Server from '../engine/server'
 
 export function enableNotifications () {
   console.log('setting up notification')
@@ -21,6 +22,61 @@ export function enableNotifications () {
         console.log('Enabled Notifications')
       }
     })
+
+    // TODO move this somewhere else
+    engine.rpc('delegateUiCtl.registerIdentifyUI', {}, {}, (error, response) => {
+      if (error != null) {
+        console.error('error in registering identify ui: ', error)
+      } else {
+        console.log('Registered identify ui')
+      }
+    })
+
+    type IdentifyKey = any
+
+    const identifyUi = {
+      start: (params: {sessionID: number, username: string}) => {
+        console.log('starting identify ui server instance')
+      },
+      displayKey: (params: {sessionID: number, key: IdentifyKey}) => {
+        console.log('displaying key', params)
+      },
+      reportLastTrack: (params: {sessionID: number, track: any}) => {
+        console.log('Report last track', params)
+      },
+      launchNetworkChecks: (params: {sessionID: number, identity: any, user: any}) => {
+        console.log('network checks', params)
+      },
+      displayTrackStatement: (params: {sessionID: number, stmt: string}) => {
+        console.log('display track statements', params)
+      },
+
+      finishWebProofCheck: (params: {sessionID: number, rp: any, lcr: any}) => {
+        console.log('finish web proof', params)
+      },
+      finishSocialProofCheck: (params: {sessionID: number, rp: any, lcr: any}) => {
+        console.log('finish social proof', params)
+      },
+      displayCryptocurrency: (params: {sessionID: number, c: any}) => {
+        console.log('finish displayCryptocurrency', params)
+      },
+      confirm: (params: {sessionID: number, outcome: any}): bool => {
+        console.log('confirm', params)
+        return true
+      },
+      finish: (params: {sessionID: number}) => {
+        console.log('finish', params)
+      }
+    }
+
+    const identifyUIServer = new Server(
+      engine,
+      'keybase.1.identifyUi.delegateIdentifyUI',
+      'keybase.1.identifyUi.finish',
+      (params) => { return { keybase: { 1: { identifyUi } } } }
+    )
+
+    identifyUIServer.listen()
   })
 }
 
