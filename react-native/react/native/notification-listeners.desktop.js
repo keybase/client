cdfd071f60ac30d4dbc345faa783f8e055117ab4
chrisnojima <cnojima@keyba.se>
2015-11-20 15:40:02 -0500

    showning kbfs notifications

--- react-native/react/native/notification-listeners.desktop.js
+++ react-native/react/native/notification-listeners.desktop.js
@@ -1,7 +1,30 @@
 import notify from '../../../desktop/app/hidden-window-notifications'
+import enums from '../../react/keybase_v1'
 
 export default {
   'keybase.1.NotifySession.loggedOut': () => {
     notify('Logged Out')
+  },
+  'keybase.1.NotifyFS.FSActivity': ({notification}) => {
+    const action = {
+      [enums.kbfs.FSNotificationType.encrypting]: 'Encrypting',
+      [enums.kbfs.FSNotificationType.decrypting]: 'Decrypting',
+      [enums.kbfs.FSNotificationType.signing]: 'Signing',
+      [enums.kbfs.FSNotificationType.verifying]: 'Verifying',
+      [enums.kbfs.FSNotificationType.rekeying]: 'Rekeying'
+    }[notification.notificationType]
+
+    const state = {
+      [enums.kbfs.FSStatusCode.start]: 'Starting',
+      [enums.kbfs.FSStatusCode.finish]: 'Finished',
+      [enums.kbfs.FSStatusCode.error]: 'Errored'
+    }[notification.statusCode]
+
+    const pubPriv = notification.publicTopLevelFolder ? '[Public]' : '[Private]'
+
+    const title = `KBFS: ${action} ${state}`
+    const body = `File: ${notification.filename} ${pubPriv} ${notification.status}`
+
+    notify(title, {body})
   }
 }
