25d99ee12a07a7ff46ce9a12046982e55ad4fb9b
Jeremy Stribling <strib@alum.mit.edu>
2015-11-20 11:30:37 -0800

    test: add simple tests for renaming over an existing file

---
+++ test/tests/simple.rb
@@ -0,0 +1,26 @@
+# These tests all do one conflict-free operation while a user is unstaged.
+module Test
+  # Check that renaming over a file correctly cleans up state
+  test :rename_file_over_file, writers: ["alice", "bob"] do |alice, bob|
+    as alice do
+      mkfile "a/b", "hello"
+      mkfile "a/c", "world"
+      rename "a/c", "a/b"
+      lsdir "a/", { "b" => "FILE" }
+      read "a/b", "world"
+      check_state
+    end
+  end
+
+  # Check that renaming a directory over a file correctly cleans up state
+  test :rename_dir_over_file, writers: ["alice", "bob"] do |alice, bob|
+    as alice do
+      mkfile "a/b", "hello"
+      mkfile "a/c/d", "world"
+      rename "a/c", "a/b"
+      lsdir "a/", { "b" => "DIR" }
+      read "a/b/d", "world"
+      check_state
+    end
+  end
+end
