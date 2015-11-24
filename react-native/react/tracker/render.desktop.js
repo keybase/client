21f2ebc1a371cd995a3d882b281df10b7a32890f
chrisnojima <cnojima@keyba.se>
2015-11-24 15:44:20 -0500

    updating to react 0.15 and some small dep updates

--- react-native/react/tracker/render.desktop.js
+++ react-native/react/tracker/render.desktop.js
@@ -3,19 +3,19 @@
 // $FlowIssue base-react
 import React, {Component} from '../base-react'
 
-// $FlowIssue platform files
 import Header from './header.render'
-// $FlowIssue platform files
 import Action from './action.render'
-// $FlowIssue platform files
 import Bio from './bio.render'
-// $FlowIssue platform files
 import Proofs from './proofs.render'
 
-import type {BioProps} from './bio.render.desktop'
-import type {ActionProps} from './action.render.desktop'
-import type {HeaderProps} from './header.render.desktop'
-import type {ProofsProps} from './proofs.render.desktop'
+// $FlowIssue platform files
+import type {BioProps} from './bio.render'
+// $FlowIssue platform files
+import type {ActionProps} from './action.render'
+// $FlowIssue platform files
+import type {HeaderProps} from './header.render'
+// $FlowIssue platform files
+import type {ProofsProps} from './proofs.render'
 
 export type RenderProps = {
   bioProps: BioProps,
