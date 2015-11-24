12c81e65ea3fd60797f3331232f29c020ddbddd3
Frederick Akalin <akalin@gmail.com>
2015-11-24 03:32:10 -0600

    Remove unneeded

--- go/engine/pgp_keyfinder_test.go
+++ go/engine/pgp_keyfinder_test.go
@@ -9,7 +9,6 @@ import (
 	"testing"
 
 	"github.com/keybase/client/go/libkb"
-	keybase1 "github.com/keybase/client/go/protocol"
 )
 
 func TestPGPKeyfinder(t *testing.T) {
@@ -63,15 +62,3 @@ func TestPGPKeyfinderLoggedOut(t *testing.T) {
 		t.Errorf("number of users found: %d, expected 3", len(up))
 	}
 }
-
-type idLubaUI struct{}
-
-func (u *idLubaUI) FinishWebProofCheck(keybase1.RemoteProof, keybase1.LinkCheckResult)    {}
-func (u *idLubaUI) FinishSocialProofCheck(keybase1.RemoteProof, keybase1.LinkCheckResult) {}
-func (u *idLubaUI) Confirm(*keybase1.IdentifyOutcome) (confirmed bool, err error)         { return }
-func (u *idLubaUI) DisplayCryptocurrency(keybase1.Cryptocurrency)                         {}
-func (u *idLubaUI) ReportLastTrack(*keybase1.TrackSummary)                                {}
-func (u *idLubaUI) Start(string)                                                          {}
-func (u *idLubaUI) LaunchNetworkChecks(*keybase1.Identity, *keybase1.User)                {}
-func (u *idLubaUI) DisplayTrackStatement(string) error                                    { return nil }
-func (u *idLubaUI) SetStrict(b bool)                                                      {}
