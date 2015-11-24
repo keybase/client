6d21cd280d3043126486a45472a9c2793293056f
Frederick Akalin <akalin@gmail.com>
2015-11-24 03:34:36 -0600

    Add (failing) test

--- go/engine/device_keyfinder_test.go
+++ go/engine/device_keyfinder_test.go
@@ -9,9 +9,40 @@ import (
 	"testing"
 
 	"github.com/keybase/client/go/libkb"
+	"github.com/keybase/client/go/protocol"
 )
 
 func TestDeviceKeyfinder(t *testing.T) {
+	tc := SetupEngineTest(t, "DeviceKeyfinder")
+	defer tc.Cleanup()
+
+	u1 := CreateAndSignupFakeUser(tc, "kbcmf")
+	u2 := CreateAndSignupFakeUser(tc, "kbcmf")
+	u3 := CreateAndSignupFakeUser(tc, "kbcmf")
+
+	trackUI := &FakeIdentifyUI{
+		Proofs: make(map[string]string),
+	}
+
+	ctx := &Context{IdentifyUI: trackUI, SecretUI: u3.NewSecretUI()}
+	arg := &DeviceKeyfinderArg{
+		Users: []string{u1.Username, u2.Username, u3.Username},
+		TrackOptions: keybase1.TrackOptions{
+			BypassConfirm: true,
+		},
+	}
+	eng := NewDeviceKeyfinder(arg, tc.G)
+	if err := RunEngine(eng, ctx); err != nil {
+		t.Fatal(err)
+	}
+
+	up := eng.UsersPlusDeviceKeys()
+	if len(up) != 3 {
+		t.Errorf("number of users found: %d, expected 3", len(up))
+	}
+}
+
+func TestDeviceKeyfinderNoTrack(t *testing.T) {
 	tc := SetupEngineTest(t, "DeviceKeyfinder")
 	defer tc.Cleanup()
 
