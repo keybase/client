4e1bace2e22c0b6546e902e2700275392c04d005
Patrick Crosby <pcrosby@gmail.com>
2015-11-30 22:08:53 -0600

    Add production brew PR helper script

---
+++ packaging/brew/brew.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Note:  this script isn't bulletproof, and you have to kind of know what you
+# are doing (editing the formula by hand, etc.), but it saves some time.
+#
+# Steps taken from here:  
+#
+# https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/How-To-Open-a-Homebrew-Pull-Request-(and-get-it-merged).md
+#
+
+set -e -u -o pipefail
+
+if [ "$#" -lt 1 ] ; then
+	echo Usage: brew.sh VERSION
+	echo VERSION should be something like 1.0.3
+	exit 1
+fi
+
+version="$1"
+version_branch="keybase-$version"
+
+cd $(brew --repository)
+git checkout master
+brew update
+git checkout -b $version_branch
+brew edit keybase
+brew audit keybase
+brew tests
+brew unlink keybase
+brew install keybase
+brew test keybase
+git commit Library/Formula/keybase.rb
+git push --set-upstream keybase $version_branch
+open https://github.com/Homebrew/homebrew 
+
