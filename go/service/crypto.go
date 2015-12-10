078a0d1d003cc0169e605c259de2bbee0970710c
Frederick Akalin <akalin@gmail.com>
2015-12-10 13:01:03 -0800

    Use delegateUI when available

--- go/service/crypto.go
+++ go/service/crypto.go
@@ -12,25 +12,84 @@ import (
 )
 
 type CryptoHandler struct {
-	*BaseHandler
 	libkb.Contextified
 }
 
 func NewCryptoHandler(xp rpc.Transporter, g *libkb.GlobalContext) *CryptoHandler {
 	return &CryptoHandler{
-		BaseHandler:  NewBaseHandler(xp),
 		Contextified: libkb.NewContextified(g),
 	}
 }
 
+func (c *CryptoHandler) getDelegatedSecretUI() libkb.SecretUI {
+	if c.G().UIRouter == nil {
+		c.G().Log.Debug("no UIRouter to get SecretUI from")
+		return nil
+	}
+
+	ui, err := c.G().UIRouter.GetSecretUI()
+	if err != nil {
+		c.G().Log.Debug("UIRouter.GetSecretUI() returned %v", err)
+		return nil
+	}
+
+	if ui == nil {
+		c.G().Log.Debug("UIRouter.GetSecretUI() returned nil")
+	}
+
+	return ui
+}
+
+type secretUIError struct{}
+
+func (secretUIError) Error() string {
+	return "Cannot fulfill SecretUI method"
+}
+
+// A libkb.SecretUI implementation that always returns an error.
+type errorSecretUI struct{}
+
+var _ libkb.SecretUI = errorSecretUI{}
+
+func (errorSecretUI) GetSecret(keybase1.SecretEntryArg, *keybase1.SecretEntryArg) (*keybase1.SecretEntryRes, error) {
+	return nil, secretUIError{}
+}
+
+func (errorSecretUI) GetNewPassphrase(keybase1.GetNewPassphraseArg) (keybase1.GetPassphraseRes, error) {
+	return keybase1.GetPassphraseRes{}, secretUIError{}
+}
+
+func (errorSecretUI) GetKeybasePassphrase(keybase1.GetKeybasePassphraseArg) (keybase1.GetPassphraseRes, error) {
+	return keybase1.GetPassphraseRes{}, secretUIError{}
+}
+
+func (errorSecretUI) GetPaperKeyPassphrase(keybase1.GetPaperKeyPassphraseArg) (string, error) {
+	return "", secretUIError{}
+}
+
+func (errorSecretUI) GetPassphrase(keybase1.GUIEntryArg, *keybase1.SecretEntryArg) (keybase1.GetPassphraseRes, error) {
+	return keybase1.GetPassphraseRes{}, secretUIError{}
+}
+
+func (c *CryptoHandler) getSecretUI() libkb.SecretUI {
+	secretUI := c.getDelegatedSecretUI()
+	if secretUI != nil {
+		return secretUI
+	}
+
+	return errorSecretUI{}
+}
+
+// TODO: Remove sessionID from args.
+
 func (c *CryptoHandler) SignED25519(_ context.Context, arg keybase1.SignED25519Arg) (keybase1.ED25519SignatureInfo, error) {
-	return engine.SignED25519(c.G(), c.getSecretUI(arg.SessionID), arg)
+	return engine.SignED25519(c.G(), c.getSecretUI(), arg)
 }
 
 func (c *CryptoHandler) SignToString(_ context.Context, arg keybase1.SignToStringArg) (string, error) {
-	return engine.SignToString(c.G(), c.getSecretUI(arg.SessionID), arg)
+	return engine.SignToString(c.G(), c.getSecretUI(), arg)
 }
 
 func (c *CryptoHandler) UnboxBytes32(_ context.Context, arg keybase1.UnboxBytes32Arg) (keybase1.Bytes32, error) {
-	return engine.UnboxBytes32(c.G(), c.getSecretUI(arg.SessionID), arg)
+	return engine.UnboxBytes32(c.G(), c.getSecretUI(), arg)
 }
