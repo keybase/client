457b1e07a8809d459ce440f76bf6fe4b252941b6
chrisnojima <cnojima@keyba.se>
2015-12-10 17:19:50 -0500

    remove local debug

--- react-native/react/local-debug.desktop.js
+++ react-native/react/local-debug.desktop.js
@@ -17,7 +17,7 @@ let config = {
   reduxDevToolsSelect: state => state // only watch a subset of the store
 }
 
-if (isDev && true) {
+if (isDev && false) {
   config.overrideRouterState = createRouterState([], [])
   config.overrideActiveTab = Tabs.devicesTab
   config.skipLoginRouteToRoot = true
