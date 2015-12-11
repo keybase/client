5aa8da75aedd292b9d97074e96d067dfef663f50
Gabriel Handford <gabrielh@gmail.com>
2015-12-11 15:34:05 -0800

    Prerelease build using custom build var to S3

--- packaging/github/keybase.sh
+++ packaging/github/keybase.sh
@@ -7,18 +7,20 @@ set -e -u -o pipefail # Fail on error
 dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd "$dir"
 
-build_dir="/tmp/build_kbfs"
+build_dir="/tmp/build_keybase"
+
+clientdir="$GOPATH/src/github.com/keybase/client"
 
 echo "Loading release tool"
 go install github.com/keybase/client/go/tools/release
 release_bin="$GOPATH/bin/release"
 
-version=$VERSION
+version="${VERSION:-}"
 if [ "$version" = "" ]; then
-  version=`$release_bin --repo=kbfs-beta latest-version`
+  version=`$release_bin --repo=client latest-version`
 fi
 tag="v$version"
-tgz="kbfs-$version.tgz"
+tgz="keybase-$version.tgz"
 token="${GITHUB_TOKEN:-}"
 
 if [ "$token" = "" ]; then
@@ -28,7 +30,7 @@ fi
 
 check_release() {
   echo "Checking for existing release: $version"
-  api_url=`$release_bin --repo=kbfs-beta --version=$version url`
+  api_url=`$release_bin --repo=client --version=$version url`
   if [ ! "$api_url" = "" ]; then
     echo "Release already exists"
     exit 0
@@ -41,35 +43,34 @@ build() {
   cd "$build_dir"
 
   echo "Downloading source archive"
-  src_url="https://github.com/keybase/kbfs-beta/archive/v$version.tar.gz"
+  src_url="https://github.com/keybase/client/archive/v$version.tar.gz"
   curl -O -J -L $src_url
 
-  src_tgz="kbfs-beta-$version.tar.gz"
+  src_tgz="client-$version.tar.gz"
   echo "Unpacking $src_tgz"
   tar zxpf "$src_tgz"
   rm "$src_tgz"
 
   go_dir=/tmp/go
   rm -rf "$go_dir"
-  mkdir -p "$go_dir/src/github.com"
-  mv "kbfs-beta-$version" "$go_dir/src/github.com/keybase"
+  mkdir -p "$go_dir/src/github.com/keybase"
+  mv "client-$version" "$go_dir/src/github.com/keybase/client"
 
-  echo "Building kbfs"
-  GO15VENDOREXPERIMENT=0 GOPATH=$go_dir go get github.com/keybase/kbfs/kbfsfuse
-  GO15VENDOREXPERIMENT=0 GOPATH=$go_dir go build -a -tags "production" -o kbfs github.com/keybase/kbfs/kbfsfuse
+  echo "Building keybase"
+  GO15VENDOREXPERIMENT=1 GOPATH=$go_dir go build -a -tags "production" -o keybase github.com/keybase/client/go/keybase
 
   echo "Packaging"
   rm -rf "$tgz"
-  tar zcpf "$tgz" kbfs
+  tar zcpf "$tgz" keybase
 }
 
 create_release() {
   cd "$build_dir"
   osname=`$release_bin os-name`
   echo "Creating release"
-  $release_bin -version "$version" -repo "kbfs-beta" create
+  $release_bin -version "$version" -repo "client" create
   echo "Uploading release"
-  $release_bin -src "$tgz" -dest "kbfs-$version-$osname.tgz" -version "$version" -repo "kbfs-beta" upload
+  $release_bin --src="$tgz" --dest="keybase-$version-$osname.tgz" -version "$version" -repo "client" upload
 }
 
 check_release
---
+++ packaging/prerelease/build_keybase.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e -u -o pipefail # Fail on error
+
+dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd "$dir"
+
+build_dir=${BUILD_DIR:-/tmp/build_keybase}
+mkdir -p $build_dir
+
+# This might be different for linux, e.g. date -d @$(git log -n1 --format="%at") +%Y%m%d%H%M%S`
+date_last_commit=`date -r $(git log -n1 --format="%at") +%Y%m%d%H%M%S`
+commit_short=`git log -1 --pretty=format:%h`
+build="$date_last_commit+$commit_short"
+
+echo "Building keybase"
+GO15VENDOREXPERIMENT=1 go build -a -tags "production" -ldflags "-X github.com/keybase/client/go/libkb.CustomBuild=$build" -o $build_dir/keybase github.com/keybase/client/go/keybase
+
+version=`$build_dir/keybase version -S`
+echo "Keybase version: $version"
+
+echo "Building kbfs"
+GO15VENDOREXPERIMENT=0 go get github.com/keybase/kbfs/kbfsfuse
+GO15VENDOREXPERIMENT=0 go build -a -tags "production" -ldflags "-X github.com/keybase/kbfs/libkbfs.CustomBuild=$build" -o $build_dir/kbfs github.com/keybase/kbfs/kbfsfuse
+
+kbfs_version=`$build_dir/kbfs -version`
+echo "KBFS version: $kbfs_version"
