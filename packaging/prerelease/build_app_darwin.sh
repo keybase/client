1d74e6ceda1ccecfe23f3b47d5543a934ef778f1
Gabriel Handford <gabrielh@gmail.com>
2015-12-14 10:54:43 -0800

    Working prerelease script w/ nightly launchd example

--- packaging/prerelease/build_app_darwin.sh
+++ packaging/prerelease/build_app_darwin.sh
@@ -5,10 +5,21 @@ set -e -u -o pipefail # Fail on error
 dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd "$dir"
 
+GOPATH=${GOPATH:-}
+
+if [ "$GOPATH" = "" ]; then
+  echo "No GOPATH"
+  exit 1
+fi
+
 build_dir_keybase="/tmp/build_keybase"
 build_dir_kbfs="/tmp/build_kbfs"
+clientdir="$GOPATH/src/github.com/keybase/client"
 
+"$clientdir/packaging/check_status_and_pull.sh" "$clientdir"
 BUILD_DIR=$build_dir_keybase ./build_keybase.sh
+
+"$clientdir/packaging/check_status_and_pull.sh" "$GOPATH/src/github.com/keybase/kbfs"
 BUILD_DIR=$build_dir_kbfs ./build_kbfs.sh
 
 cd $dir/../desktop
