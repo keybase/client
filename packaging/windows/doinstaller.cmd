c1699ce1b99a61f3b07e7a979ca9b6b0cdac3eeb
Steve Sanders <steve@keyba.se>
2015-12-15 09:52:31 -0800

    Semantic version doesn't work with installer tool after all

--- packaging/windows/doinstaller.cmd
+++ packaging/windows/doinstaller.cmd
@@ -5,10 +5,13 @@
 :: get the target build folder. Assume winresource.exe has been built.
 :: If not, go there and do "go generate"
 For %%A in ("%1") do Set Folder=%%~dpA
-:: Capture the version - this is the only way to store it in a .cmd variable
-for /f %%i in ('%Folder%winresource.exe -v') do set BUILDVER=%%i
+:: Capture the windows style version - this is the only way to store it in a .cmd variable
+for /f %%i in ('%Folder%winresource.exe -w') do set BUILDVER=%%i
 echo %BUILDVER%
 
+:: Capture the semantic version - this is the only way to store it in a .cmd variable
+for /f %%i in ('%Folder%winresource.exe -v') do set SEMVER=%%i
+echo %SEMVER%
 
 :: Other alternate time servers:
 ::   http://timestamp.verisign.com/scripts/timstamp.dll
@@ -20,4 +23,4 @@ SignTool.exe sign /a /tr http://timestamp.digicert.com %1
 IF %ERRORLEVEL% NEQ 0 (
   EXIT /B 1
 )
-"%ProgramFiles(x86)%\Inno Setup 5\iscc.exe" /DMyExePathName=%1 /DMyAppVersion=%BUILDVER% "/sSignCommand=signtool.exe sign /tr http://timestamp.digicert.com $f" setup_windows.iss
+"%ProgramFiles(x86)%\Inno Setup 5\iscc.exe" /DMyExePathName=%1 /DMyAppVersion=%BUILDVER% /DMySemVersion=%SEMVER% "/sSignCommand=signtool.exe sign /tr http://timestamp.digicert.com $f" setup_windows.iss
