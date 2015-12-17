e9fd9339e05d4dc95aee5d949ef7716057a8fb5e
Maxwell Krohn <themax@gmail.com>
2015-12-17 09:08:53 -0500

    fix travis so it fails on errors

---
+++ go/test/run_tests.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -f -u -e
+DIRS=$(go list ./... | grep -v /vendor/ | sed -e 's/^github.com\/keybase\/client\/go\///')
+
+for i in $DIRS
+do
+	echo -n "$i......."
+	(cd $i && go test -timeout 30m)
+done
