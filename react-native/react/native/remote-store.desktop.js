04d20978abd29325295ad977b1a89fd699cd0361
Marco Munizaga <marco@marcopolo.io>
2015-12-17 11:02:59 -0800

    Make sure remote store's dispatch context is correct

--- react-native/react/native/remote-store.desktop.js
+++ react-native/react/native/remote-store.desktop.js
@@ -1,8 +1,6 @@
 /* @flow */
 import {ipcRenderer} from 'electron'
 
-import type {Action, Dispatch} from '../constants/types/flux'
-
 export default class RemoteStore {
   listeners: Array<Function>;
   internalState: any;
@@ -21,6 +19,7 @@ export default class RemoteStore {
 
     this.listeners = []
     this.internalState = {}
+    this.dispatch = this.dispatch.bind(this)
   }
 
   getState (): any {
