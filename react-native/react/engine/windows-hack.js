9604b74b7b6ccdd9d46c05eedcaad2a8cf5f337e
Chris Ball <chris@printf.net>
2015-12-21 17:15:52 -0500

    fake

--- react-native/react/engine/windows-hack.js
+++ react-native/react/engine/windows-hack.js
@@ -1,12 +1,16 @@
-// This net.connect() is a heinous hack.
-//
-// On Windows, but *only* in the renderer thread, our RPC connection
-// hangs until other random net module operations, at which point it
-// unblocks.  Could be Electron, could be a node-framed-msgpack-rpc
-// bug, who knows.
-
 export default function windowsHack () {
+  // This net.connect() is a heinous hack.
+  //
+  // On Windows, but *only* in the renderer thread, our RPC connection
+  // hangs until other random net module operations, at which point it
+  // unblocks.  Could be Electron, could be a node-framed-msgpack-rpc
+  // bug, who knows.
+  console.log('in windowshack')
   if (process.platform === 'win32') {
-    net.connect({})
+    console.log('b')
+    var fake = net.connect({})
+    fake.on('error', function (err) {
+      console.debug(err)
+    })
   }
 }
