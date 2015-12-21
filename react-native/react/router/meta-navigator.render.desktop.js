18fc0f30cff6a7d1b873bf982ec34511bd3b61c3
chrisnojima <cnojima@keyba.se>
2015-12-21 16:28:51 -0500

    back button

--- react-native/react/router/meta-navigator.render.desktop.js
+++ react-native/react/router/meta-navigator.render.desktop.js
@@ -1,13 +1,24 @@
 import React, {Component} from '../base-react'
+import {FlatButton} from 'material-ui'
+import {navigateUp} from '../actions/router'
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
 
-export default class MetaNavigatorRender extends Component {
+class MetaNavigatorRender extends Component {
+  onBack () {
+    this.props.navigateUp()
+  }
+
   render () {
     const {rootComponent, uri, getComponentAtTop} = this.props
     const {componentAtTop} = getComponentAtTop(rootComponent, uri)
     const Module = componentAtTop.component
 
     return (
-      <Module {...componentAtTop.props} />
+      <div>
+        <Module {...componentAtTop.props} />
+        {uri && uri.count() > 1 && <FlatButton onClick={ () => this.onBack() } style={styles.backButton} label='Back'/>}
+      </div>
     )
   }
 }
@@ -15,5 +26,17 @@ export default class MetaNavigatorRender extends Component {
 MetaNavigatorRender.propTypes = {
   uri: React.PropTypes.object.isRequired,
   getComponentAtTop: React.PropTypes.func.isRequired,
-  rootComponent: React.PropTypes.func.isRequired
+  rootComponent: React.PropTypes.func.isRequired,
+  navigateUp: React.PropTypes.func.isRequired
 }
+
+const styles = {
+  backButton: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    backgroundColor: '#cccccc'
+  }
+}
+
+export default connect(state => state, dispatch => bindActionCreators({navigateUp}, dispatch))(MetaNavigatorRender)
