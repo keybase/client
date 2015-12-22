2feec131a859baf81639a40a89e101864cbf89a9
Patrick Crosby <pcrosby@gmail.com>
2015-12-22 12:47:58 -0600

    Add message size, truncation tests

--- go/saltpack/sign_test.go
+++ go/saltpack/sign_test.go
@@ -4,8 +4,10 @@
 package saltpack
 
 import (
+	"bytes"
 	"crypto/rand"
 	"fmt"
+	"io"
 	"testing"
 
 	"github.com/agl/ed25519"
@@ -70,4 +72,56 @@ func TestSign(t *testing.T) {
 	if len(out) == 0 {
 		t.Fatal("Sign returned no error and no output")
 	}
+}
+
+func testSignAndVerify(t *testing.T, message []byte) {
+	key := newSigPrivKey(t)
+	smsg, err := Sign(message, key, MessageTypeAttachedSignature)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(smsg) == 0 {
+		t.Fatal("Sign returned no error and no output")
+	}
+	skey, vmsg, err := Verify(smsg, kr)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !bytes.Equal(skey.ToKID(), key.PublicKey().ToKID()) {
+		t.Errorf("signer key %x, expected %x", skey.ToKID(), key.PublicKey().ToKID())
+	}
+	if !bytes.Equal(vmsg, message) {
+		t.Errorf("verified msg '%x', expected '%x'", vmsg, message)
+	}
+}
+
+func TestSignMessageSizes(t *testing.T) {
+	sizes := []int{10, 128, 1024, 1100, 1024 * 10, 1024*10 + 64, 1024 * 100, 1024*100 + 99, 1024 * 1024 * 3}
+	for _, size := range sizes {
+		t.Logf("testing sign and verify message size = %d", size)
+		testSignAndVerify(t, randomMsg(t, size))
+	}
+}
+
+func TestSignTruncation(t *testing.T) {
+	key := newSigPrivKey(t)
+	smsg, err := Sign(randomMsg(t, 128), key, MessageTypeAttachedSignature)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(smsg) == 0 {
+		t.Fatal("Sign returned no error and no output")
+	}
+	trunced := smsg[:len(smsg)-51]
+	skey, vmsg, err := Verify(trunced, kr)
+	if skey != nil {
+		t.Errorf("Verify returned a key for a truncated message")
+	}
+	if vmsg != nil {
+		t.Errorf("Verify returned a message for a truncated message")
+	}
+	if err != io.ErrUnexpectedEOF {
+		t.Errorf("error: %v, expected %v", err, io.ErrUnexpectedEOF)
+	}
+
 }
