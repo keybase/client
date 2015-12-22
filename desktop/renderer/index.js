c4640ac2dd42c1b46fcd80347966bf05f1ace13c
Chris Ball <chris@printf.net>
2015-12-22 11:06:59 -0500

    Remove the net.connect hack from renderer

--- desktop/renderer/index.js
+++ desktop/renderer/index.js
@@ -46,16 +46,6 @@ class Keybase extends Component {
   constructor () {
     super()
 
-    // This net.connect() is a heinous hack.
-    //
-    // On Windows, but *only* in the renderer thread, our RPC connection
-    // hangs until other random net module operations, at which point it
-    // unblocks.  Could be Electron, could be a node-framed-msgpack-rpc
-    // bug, who knows.
-    if (process.platform === 'win32') {
-      net.connect({})
-    }
-
     this.state = {
       panelShowing: false
     }
