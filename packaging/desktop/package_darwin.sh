577b6fb347519ee1ac6657c04dde575ad6836818
chrisnojima <cnojima@keyba.se>
2015-12-21 20:01:53 -0500

    plumb version through

--- packaging/desktop/package_darwin.sh
+++ packaging/desktop/package_darwin.sh
@@ -126,7 +126,7 @@ package_electron() {
   cd desktop
 
   npm install
-  npm run package appVersion=$app_version comment=$comment
+  npm run package -- --appVersion="$app_version" --comment="$comment"
   rsync -av release/darwin-x64/Keybase-darwin-x64 $build_dir
 }
 
