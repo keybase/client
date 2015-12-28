bf1248e0fbf98fe32f4e57ed2ad09b5d1f8bbb31
Gabriel Handford <gabrielh@gmail.com>
2015-12-28 13:46:57 -0800

    Update secret store to use new keychain API (OSX)
    This will invalidate current keychain items but this is ok and desired behavior since we want to ensure we are using the correct security attributes, especially "this device only" to prevent keychain item from being shared across devices.

--- go/libkb/secret_store_osx.go
+++ go/libkb/secret_store_osx.go
@@ -5,15 +5,11 @@
 
 package libkb
 
-import (
-	"encoding/base64"
-
-	kc "github.com/keybase/go-osxkeychain"
-)
+import keychain "github.com/keybase/go-keychain"
 
 type KeychainSecretStore struct {
 	Contextified
-	accountName NormalizedUsername
+	accountName string
 }
 
 var _ SecretStore = KeychainSecretStore{}
@@ -23,69 +19,31 @@ func (k KeychainSecretStore) getServiceName() string {
 }
 
 func (k KeychainSecretStore) StoreSecret(secret []byte) (err error) {
-	k.G().Log.Debug("+ StoreSecret(%s, %d)", k.accountName, len(secret))
-	defer func() {
-		k.G().Log.Debug("- StoreSecret -> %s", ErrToOk(err))
-	}()
-
-	// base64-encode to make it easy to work with Keychain Access.
-	encodedSecret := base64.StdEncoding.EncodeToString(secret)
-	attributes := kc.GenericPasswordAttributes{
-		ServiceName: k.getServiceName(),
-		AccountName: k.accountName.String(),
-		Password:    []byte(encodedSecret),
-	}
-	err = kc.RemoveAndAddGenericPassword(&attributes)
-	return
+	item := keychain.NewGenericPassword(k.getServiceName(), k.accountName, "", secret, "")
+	item.SetSynchronizable(keychain.SynchronizableNo)
+	item.SetAccessible(keychain.AccessibleAfterFirstUnlockThisDeviceOnly)
+	// TODO: Since we access keychain item in launchd service (background), it's
+	// appropriate to use after first unlock, though we should consider using
+	// "when unlocked" to be more secure.
+	//item.SetAccessible(keychain.AccessibleWhenUnlockedThisDeviceOnly)
+	keychain.DeleteItem(item)
+	return keychain.AddItem(item)
 }
 
-func (k KeychainSecretStore) RetrieveSecret() (secret []byte, err error) {
-	k.G().Log.Debug("+ RetrieveSecret(%s)", k.accountName)
-	defer func() {
-		k.G().Log.Debug("- RetrieveSecret -> %s", ErrToOk(err))
-	}()
-
-	attributes := kc.GenericPasswordAttributes{
-		ServiceName: k.getServiceName(),
-		AccountName: k.accountName.String(),
-	}
-
-	encodedSecret, err := kc.FindGenericPassword(&attributes)
-	if err != nil {
-		return
-	}
-
-	secret, err = base64.StdEncoding.DecodeString(string(encodedSecret))
-	if err != nil {
-		secret = nil
-	}
-
-	return
+func (k KeychainSecretStore) RetrieveSecret() ([]byte, error) {
+	return keychain.GetGenericPassword(k.getServiceName(), k.accountName, "", "")
 }
 
 func (k KeychainSecretStore) ClearSecret() (err error) {
-	k.G().Log.Debug("+ ClearSecret(%s)", k.accountName)
-	defer func() {
-		k.G().Log.Debug("- ClearSecret -> %s", ErrToOk(err))
-	}()
-
-	attributes := kc.GenericPasswordAttributes{
-		ServiceName: k.getServiceName(),
-		AccountName: k.accountName.String(),
-	}
-
-	err = kc.FindAndRemoveGenericPassword(&attributes)
-	// Don't count the item not being found as an error.
-	if err == kc.ErrItemNotFound {
-		err = nil
-	}
-	return
+	query := keychain.NewGenericPassword(k.getServiceName(), k.accountName, "", nil, "")
+	query.SetMatchLimit(keychain.MatchLimitAll)
+	return keychain.DeleteItem(query)
 }
 
 func NewSecretStore(g *GlobalContext, username NormalizedUsername) SecretStore {
 	return KeychainSecretStore{
 		Contextified: NewContextified(g),
-		accountName:  username,
+		accountName:  username.String(),
 	}
 }
 
@@ -94,7 +52,7 @@ func HasSecretStore() bool {
 }
 
 func GetUsersWithStoredSecrets(g *GlobalContext) ([]string, error) {
-	return kc.GetAllAccountNames(g.Env.GetStoredSecretServiceName())
+	return keychain.GetAccountsForService(g.Env.GetStoredSecretServiceName())
 }
 
 func GetTerminalPrompt() string {
