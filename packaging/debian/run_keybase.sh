816bf6ea5620919380b46c8ad2a56b5d01cc7d24
Jack O'Connor <oconnor663@gmail.com>
2016-01-08 13:12:02 -0500

    stop using a local path for kbfsfuse

--- packaging/debian/run_keybase.sh
+++ packaging/debian/run_keybase.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 KEYBASE_RUN_MODE=prod keybase service &>/dev/null &
-KEYBASE_RUN_MODE=prod ./kbfsfuse -mdserver mdserver.kbfs.keybase.io:443 -bserver bserver.kbfs.keybase.io:443 /keybase &>/dev/null &
+KEYBASE_RUN_MODE=prod kbfsfuse -mdserver mdserver.kbfs.keybase.io:443 -bserver bserver.kbfs.keybase.io:443 /keybase &>/dev/null &
 KEYBASE_RUN_MODE=prod /opt/keybase/Keybase &>/dev/null &
