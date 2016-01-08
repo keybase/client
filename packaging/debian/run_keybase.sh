f1fcaba98edd0714f0273084111e053436bea74a
Jack O'Connor <oconnor663@gmail.com>
2016-01-08 15:32:19 -0500

    cleanup commands

--- packaging/debian/run_keybase.sh
+++ packaging/debian/run_keybase.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# Stop any existing services.
+fusermount -uz /keybase
+killall kbfsfuse
+killall Keybase
+killall keybase
+
 KEYBASE_RUN_MODE=prod keybase service &>/dev/null &
 # TODO: Have the installer create /keybase for you.
-# TODO: Do a proactive `fusermount -uz` in case kbfs doens't shut down
-# gracefully.
 KEYBASE_RUN_MODE=prod kbfsfuse -mdserver mdserver.kbfs.keybase.io:443 -bserver bserver.kbfs.keybase.io:443 /keybase &>/dev/null &
 KEYBASE_RUN_MODE=prod /opt/keybase/Keybase &>/dev/null &
