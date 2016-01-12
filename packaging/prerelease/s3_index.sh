69a3e61f5613c804bd342e8b548ce181a87b4723
Gabriel Handford <gabrielh@gmail.com>
2016-01-12 14:02:28 -0800

    Update some stale scripts

--- packaging/prerelease/s3_index.sh
+++ packaging/prerelease/s3_index.sh
@@ -6,14 +6,21 @@ set -e -u -o pipefail # Fail on error
 dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $dir
 
-bucket_name=${BUCKET_NAME:-prerelease.keybase.io}
+client_dir="$dir/../.."
+bucket_name=${BUCKET_NAME:-}
 save_dir="/tmp/s3index"
 
+if [ "$bucket_name" = "" ]; then
+  echo "No BUCKET_NAME"
+  exit 1
+fi
+
 echo "Loading release tool"
-go get github.com/keybase/release
-go install github.com/keybase/release
+"$client_dir/packaging/goinstall.sh" "github.com/keybase/release"
 release_bin="$GOPATH/bin/release"
 
 mkdir -p $save_dir
+echo "Creating index.html"
 $release_bin index-html --bucket-name="$bucket_name" --prefixes="darwin/,linux/,electron-sourcemaps/" --dest="$save_dir/index.html"
+echo "Syncing"
 s3cmd sync --acl-public --disable-multipart $save_dir/* s3://$bucket_name/
