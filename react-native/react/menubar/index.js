5c327234716a036dc15a0b24746493236cfa6578
Marco Munizaga <marco@marcopolo.io>
2016-01-11 17:55:45 -0800

    Use loggedIn to show message for menubar

--- react-native/react/menubar/index.js
+++ react-native/react/menubar/index.js
@@ -109,7 +109,7 @@ class Menubar extends Component {
   }
 
   render () {
-    const openingMessage = this.props.username ? 'Keybase Alpha' : 'Looks like you aren\'t logged in. Try running `keybase login`'
+    const openingMessage = this.props.loggedIn ? 'Keybase Alpha' : 'Looks like you aren\'t logged in. Try running `keybase login`'
 
     const openingButtonInfo = this.props.username && {text: 'WTF?', onClick: this.showHelp}
     const folders = (this.props.folders || []).map(function (f: Folder): FolderInfo {
