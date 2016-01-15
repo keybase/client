954fbbaadff55eb5ed6777fc28a92e5bddbf0915
Jeremy Stribling <strib@alum.mit.edu>
2016-01-14 17:07:59 -0800

    vendor: update MD protocol with MD versions
    Issue: KBFS-15

--- vendor/github.com/keybase/client/go/protocol/keybase_v1.go
+++ vendor/github.com/keybase/client/go/protocol/keybase_v1.go
@@ -676,9 +676,10 @@ type Bytes32 [32]byte
 type EncryptedBytes32 [48]byte
 type BoxNonce [24]byte
 type BoxPublicKey [32]byte
-type CiphertextKIDPair struct {
+type CiphertextBundle struct {
 	Kid        KID              `codec:"kid" json:"kid"`
 	Ciphertext EncryptedBytes32 `codec:"ciphertext" json:"ciphertext"`
+	Nonce      BoxNonce         `codec:"nonce" json:"nonce"`
 }
 
 type UnboxAnyRes struct {
@@ -705,10 +706,9 @@ type UnboxBytes32Arg struct {
 }
 
 type UnboxBytes32AnyArg struct {
-	Pairs          []CiphertextKIDPair `codec:"pairs" json:"pairs"`
-	Nonce          BoxNonce            `codec:"nonce" json:"nonce"`
-	PeersPublicKey BoxPublicKey        `codec:"peersPublicKey" json:"peersPublicKey"`
-	Reason         string              `codec:"reason" json:"reason"`
+	Bundles        []CiphertextBundle `codec:"bundles" json:"bundles"`
+	PeersPublicKey BoxPublicKey       `codec:"peersPublicKey" json:"peersPublicKey"`
+	Reason         string             `codec:"reason" json:"reason"`
 }
 
 type CryptoInterface interface {
@@ -2877,9 +2877,14 @@ type KeyHalf struct {
 	Key       []byte `codec:"key" json:"key"`
 }
 
+type MDBlock struct {
+	Version int    `codec:"version" json:"version"`
+	Block   []byte `codec:"block" json:"block"`
+}
+
 type MetadataResponse struct {
-	FolderID string   `codec:"folderID" json:"folderID"`
-	MdBlocks [][]byte `codec:"mdBlocks" json:"mdBlocks"`
+	FolderID string    `codec:"folderID" json:"folderID"`
+	MdBlocks []MDBlock `codec:"mdBlocks" json:"mdBlocks"`
 }
 
 type AuthenticateArg struct {
@@ -2887,7 +2892,7 @@ type AuthenticateArg struct {
 }
 
 type PutMetadataArg struct {
-	MdBlock []byte            `codec:"mdBlock" json:"mdBlock"`
+	MdBlock MDBlock           `codec:"mdBlock" json:"mdBlock"`
 	LogTags map[string]string `codec:"logTags" json:"logTags"`
 }
 
@@ -4698,6 +4703,7 @@ type SaltpackEncryptOptions struct {
 	Recipients    []string `codec:"recipients" json:"recipients"`
 	HideSelf      bool     `codec:"hideSelf" json:"hideSelf"`
 	NoSelfEncrypt bool     `codec:"noSelfEncrypt" json:"noSelfEncrypt"`
+	Binary        bool     `codec:"binary" json:"binary"`
 }
 
 type SaltpackDecryptOptions struct {
@@ -4707,6 +4713,7 @@ type SaltpackDecryptOptions struct {
 
 type SaltpackSignOptions struct {
 	Detached bool `codec:"detached" json:"detached"`
+	Binary   bool `codec:"binary" json:"binary"`
 }
 
 type SaltpackVerifyOptions struct {
