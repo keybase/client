88faaefc0ead4b2207da45516cf0c52359a5dee2
Jack O'Connor <oconnor663@gmail.com>
2016-01-19 14:31:02 -0500

    debug logs by default

--- packaging/linux/run_keybase.sh
+++ packaging/linux/run_keybase.sh
@@ -12,7 +12,10 @@ killall keybase
 logdir="${XDG_CACHE_HOME:-$HOME/.cache}/keybase"
 mkdir -p "$logdir"
 
-KEYBASE_RUN_MODE=prod keybase service &>> "$logdir/keybase.service.log" &
-KEYBASE_RUN_MODE=prod kbfsfuse -mdserver mdserver.kbfs.keybase.io:443 \
+export KEYBASE_RUN_MODE=prod
+export KEYBASE_DEBUG=1
+
+keybase service &>> "$logdir/keybase.service.log" &
+kbfsfuse -debug -mdserver mdserver.kbfs.keybase.io:443 \
   -bserver bserver.kbfs.keybase.io:443 /keybase &>> "$logdir/keybase.kbfs.log" &
-KEYBASE_RUN_MODE=prod /opt/keybase/Keybase &>> "$logdir/keybase.gui.log" &
+/opt/keybase/Keybase &>> "$logdir/keybase.gui.log" &
