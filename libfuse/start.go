0d7395fdc87f615850e85353d10c8a3ee84d25fa
Jeremy Stribling <strib@alum.mit.edu>
2016-01-22 14:31:31 -0800

    libfuse: don't use double-pointer ctx app id
    At some point NewFS was changed to return a pointer.
    
    Issue: KBFS-661

--- libfuse/start.go
+++ libfuse/start.go
@@ -61,7 +61,7 @@ func Start(mounter Mounter, options StartOptions) *Error {
 	}
 
 	fs := NewFS(config, c, options.KbfsParams.Debug)
-	ctx := context.WithValue(context.Background(), CtxAppIDKey, &fs)
+	ctx := context.WithValue(context.Background(), CtxAppIDKey, fs)
 	logTags := make(logger.CtxLogTags)
 	logTags[CtxIDKey] = CtxOpID
 	ctx = logger.NewContextWithLogTags(ctx, logTags)
