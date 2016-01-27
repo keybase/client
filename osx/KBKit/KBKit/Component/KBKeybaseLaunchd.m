5cb7b29af7bc8fc3b8f1ffe55eda898be91abd1f
Gabriel Handford <gabrielh@gmail.com>
2016-01-27 13:01:27 -0800

    Installer should log with timestamps

--- osx/KBKit/KBKit/Component/KBKeybaseLaunchd.m
+++ osx/KBKit/KBKit/Component/KBKeybaseLaunchd.m
@@ -21,7 +21,7 @@
 
 + (void)install:(NSString *)binPath label:(NSString *)label serviceBinPath:(NSString *)serviceBinPath args:(NSArray *)args completion:(KBCompletion)completion {
   NSMutableArray *pargs = [NSMutableArray array];
-  [pargs addObjectsFromArray:@[@"--log-format=plain", @"launchd", @"install", label, serviceBinPath]];
+  [pargs addObjectsFromArray:@[@"launchd", @"install", label, serviceBinPath]];
   [pargs addObjectsFromArray:args];
   [KBTask execute:binPath args:pargs completion:^(NSError *error, NSData *outData, NSData *errData) {
     completion(error);
@@ -35,7 +35,7 @@
 }
 
 + (void)list:(NSString *)binPath name:(NSString *)name completion:(KBOnServiceStatuses)completion {
-  [KBTask executeForJSONWithCommand:binPath args:@[@"--log-format=plain", @"launchd", @"list", @"--format=json", name] completion:^(NSError *error, id value) {
+  [KBTask executeForJSONWithCommand:binPath args:@[@"launchd", @"list", @"--format=json", name] completion:^(NSError *error, id value) {
     NSArray *statuses = [MTLJSONAdapter modelsOfClass:KBRServiceStatus.class fromJSONArray:value[name] error:&error];
     completion(nil, statuses);
   }];
@@ -43,7 +43,7 @@
 
 + (void)status:(NSString *)binPath name:(NSString *)name completion:(KBOnServiceStatus)completion {
   DDLogDebug(@"Checking launchd status for %@", name);
-  [KBTask executeForJSONWithCommand:binPath args:@[@"--log-format=plain", @"launchd", @"status", @"--format=json", name] completion:^(NSError *error, id value) {
+  [KBTask executeForJSONWithCommand:binPath args:@[@"launchd", @"status", @"--format=json", name] completion:^(NSError *error, id value) {
     if (error) {
       completion(error, nil);
       return;
--- osx/KBKit/KBKit/Log/KBMemLogger.m
+++ osx/KBKit/KBKit/Log/KBMemLogger.m
@@ -23,7 +23,7 @@
     _buffer = [NSMutableArray array];
     _group = dispatch_group_create();
     _bufferQueue = dispatch_queue_create("KBMemLogger", NULL);
-    self.logFormatter = [[KBLogPlainFormatter alloc] init];
+    self.logFormatter = [[KBLogConsoleFormatter alloc] init];
   }
   return self;
 }
