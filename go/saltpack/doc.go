78d71387f195a588eca62bd2a11969dab055008d
Maxwell Krohn <themax@gmail.com>
2016-01-29 14:54:46 -0500

    tes from review [skip ci]

--- go/saltpack/doc.go
+++ go/saltpack/doc.go
@@ -1,14 +1,15 @@
 /*
-Package saltpack is an implementation of the Saltpack message format. Saltpack
-is a very light wrapper around Dan Berstein's famous NaCl library. It adds
-support for longer messages, streaming input and output of data, multiple
-recipients for encrypted messages, and a reasonable armoring format. We intend
-Saltpack as a replacement for the PGP messaging format, as it can be used in
-many of the same circumstances.  However, it is designed to be: (1) simpler;
-(2) easier to implement; (3) judicious (perhaps judgmental) in its crypto
-usage; (4) fully modern (no CBC option here); (5) high performance; (6) less
-bug-prone; (7) generally unwilling to output unauthenticated data; and (8)
-easier to compose with other software in any manner of languages or platforms.
+
+Package saltpack is an implementation of the saltpack message format. Saltpack
+is a light wrapper around Dan Berstein's famous NaCl library. It adds support
+for longer messages, streaming input and output of data, multiple recipients
+for encrypted messages, and a reasonable armoring format. We intend Saltpack
+as a replacement for the PGP messaging format, as it can be used in many of
+the same circumstances.  However, it is designed to be: (1) simpler; (2)
+easier to implement; (3) judicious (perhaps judgmental) in its crypto usage;
+(4) fully modern (no CBC option here); (5) high performance; (6) less bug-
+prone; (7) generally unwilling to output unauthenticated data; and (8) easier
+to compose with other software in any manner of languages or platforms.
 
 Key Management
 
@@ -18,10 +19,12 @@ has a story for key management.
 Modes of Operation
 
 Saltpack supports three modes of operation: encrypted messages, attached
-signatures, and detached signatures. An attached signature contains a message
-and a signature that authenticates it. A detached signature contains just the
-signature, and assumes an independent delievery mechanism for the file
-(this might come up when distributing an ISO an separate signature of it).
+signatures, and detached signatures. Encrypted messages use NaCl's
+authenticated public-key encryption; we add repudiable authentication. An
+attached signature contains a message and a signature that authenticates it. A
+detached signature contains just the signature, and assumes an independent
+delievery mechanism for the file (this might come up when distributing an ISO
+and separate signature of the file).
 
 Encoding
 
