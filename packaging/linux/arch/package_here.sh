f08d25229241570efadd44d3066157dc9b7826f1
Jack O'Connor <oconnor663@gmail.com>
2016-02-02 13:59:18 -0500

    always pull kbfs in the Arch package_here.sh script

--- packaging/linux/arch/package_here.sh
+++ packaging/linux/arch/package_here.sh
@@ -6,6 +6,7 @@ cd "$(dirname "$BASH_SOURCE")"
 
 client_root="$(git rev-parse --show-toplevel)"
 kbfs_root="$(realpath "$client_root/../kbfs")"
+../../check_status_and_pull.sh "$kbfs_root"
 
 mkdir -p src
 ln -snf "$client_root" src/client
