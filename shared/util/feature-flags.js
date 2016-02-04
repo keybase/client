4ad4b32c11549d9cdcf17d61c606985d5e9f5f83
Chris Ball <chris@printf.net>
2016-02-04 16:10:39 -0500

    typo

--- shared/util/feature-flags.js
+++ shared/util/feature-flags.js
@@ -4,6 +4,6 @@ type FeatureFlag = {
   tracker: 'v1' | 'v2'
 }
 const ff: FeatureFlag = {
-  tracker: process.env.KEYBASE_TRACKER_v2 ? 'v2' : 'v1'
+  tracker: process.env.KEYBASE_TRACKER_V2 ? 'v2' : 'v1'
 }
 export default ff
