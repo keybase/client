b3357721fbf51d08a603e11e45655b7c6208ccd8
Jeremy Stribling <strib@alum.mit.edu>
2016-02-09 16:36:36 -0800

    mdserver_local: don't send notifications on a rekey update
    Just like the real md server.
    
    Issue: KBFS-774

--- libkbfs/mdserver_local.go
+++ libkbfs/mdserver_local.go
@@ -498,7 +498,10 @@ func (md *MDServerLocal) Put(ctx context.Context, rmds *RootMetadataSigned) erro
 		return MDServerError{err}
 	}
 
-	if mStatus == Merged {
+	if mStatus == Merged &&
+		// Don't send notifies if it's just a rekey (the real mdserver
+		// sends a "folder needs rekey" notification in this case).
+		!(rmds.MD.IsRekeySet() && rmds.MD.IsWriterMetadataCopiedSet()) {
 		md.sessionHeads[id] = md
 
 		// now fire all the observers that aren't from this session
