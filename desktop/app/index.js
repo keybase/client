faeeaccec526c80ec2baf7b1f3594b9ba46ab378
Gabriel Handford <gabrielh@gmail.com>
2016-02-12 13:06:49 -0800

    Check OS version on startup

--- desktop/app/index.js
+++ desktop/app/index.js
@@ -1,7 +1,7 @@
 import {BrowserWindow} from 'electron'
 import splash from './splash'
 import installer from './installer'
-import {app} from 'electron'
+import {app, dialog} from 'electron'
 import ListenLogUi from '../shared/native/listen-log-ui'
 import menuHelper from './menu-helper'
 import consoleHelper, {ipcLogs} from './console-helper'
@@ -11,6 +11,19 @@ import storeHelper from './store-helper'
 import mainWindow from './main-window'
 import urlHelper from './url-helper'
 import hello from '../shared/util/hello'
+import semver from 'semver'
+import os from 'os'
+
+// Check supported OS version
+if (os.platform() === 'darwin') {
+  // Release numbers for OS versions can be looked up here: https://en.wikipedia.org/wiki/Darwin_%28operating_system%29#Release_history
+  // 14.0.0 == 10.10.0
+  // 15.0.0 == 10.11.0
+  if (!semver.satisfies(os.release(), '^14.0.0')) {
+    dialog.showErrorBox('Keybase Error', `This version of OS X isn\'t currently supported.`)
+    app.quit()
+  }
+}
 
 // MUST do this else we get limited by simultaneous hot reload event streams
 app.commandLine.appendSwitch('ignore-connections-limit', 'localhost')
@@ -65,4 +78,3 @@ app.on('before-quit', event => {
 
   // TODO: send some event to the service to tell it to shutdown all the things as well
 })
-
