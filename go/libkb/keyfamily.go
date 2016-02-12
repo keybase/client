6d7797d98e689af510d4e885caf03f8c492b4a50
Maxwell Krohn <themax@gmail.com>
2016-02-11 23:18:59 -0500

    libkb: a new bad sigchain link for jamgregory
    - His link #17 is a sibkey delegation to a PGP key with a fullhash, but due to
      a bug in Go Client (fixed in #1966), this was a broken key without a cross-sig
      on his signing subkey. This unfortunate bug breaks his sigchain play-foward.
    - @oconnor663 suggested this more complete fix for @jamgregory

--- go/libkb/keyfamily.go
+++ go/libkb/keyfamily.go
@@ -543,7 +543,11 @@ func (ckf *ComputedKeyFamily) Revoke(tcl TypedChainLink) (err error) {
 
 // SetPGPHash sets the authoritative version (by hash) of a PGP key
 func (ckf *ComputedKeyFamily) SetActivePGPHash(kid keybase1.KID, hash string) {
-	ckf.cki.Infos[kid].ActivePGPHash = hash
+	if _, ok := ckf.cki.Infos[kid]; ok {
+		ckf.cki.Infos[kid].ActivePGPHash = hash
+	} else {
+		ckf.G().Log.Debug("| Skipped setting active hash, since key was never delegated")
+	}
 }
 
 // revokeSigs operates on the per-signature revocations in the given
