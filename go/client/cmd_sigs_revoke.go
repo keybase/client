728788c14eea0bf50050540090f0acee27a3187e
Maxwell Krohn <themax@gmail.com>
2016-02-12 10:58:52 -0500

    nix usage message

--- go/client/cmd_sigs_revoke.go
+++ go/client/cmd_sigs_revoke.go
@@ -56,7 +56,6 @@ func NewCmdSigsRevoke(cl *libcmdline.CommandLine) cli.Command {
 	return cli.Command{
 		Name:         "revoke",
 		ArgumentHelp: "<id> ...",
-		Usage:        "Verify one or more signatures by sig ID",
 		Action: func(c *cli.Context) {
 			cl.ChooseCommand(&CmdSigsRevoke{}, "revoke", c)
 		},
