c206b218b1ab37f8f73259e99eab6722adbf1711
Jack O'Connor <oconnor663@gmail.com>
2016-02-19 11:33:56 -0500

    fix Go 1.6 vet errors
    Go now believes that any variable called C must represent native C code,
    and it gets scared when we do normal things with it. Calm it down.

--- go/auth/credential_authority_test.go
+++ go/auth/credential_authority_test.go
@@ -153,46 +153,46 @@ func newTestSetup() (*testState, *CredentialAuthority) {
 }
 
 func TestSimple(t *testing.T) {
-	S, C := newTestSetup()
-	u0 := S.newTestUser(4)
+	state, credentialAuthority := newTestSetup()
+	u0 := state.newTestUser(4)
 
 	key0 := u0.sibkeys[0]
 	key1 := u0.sibkeys[1]
 
-	if S.numGets != 0 {
+	if state.numGets != 0 {
 		t.Fatal("expected 0 gets")
 	}
 
-	err := C.CheckUserKey(context.TODO(), u0.uid, &u0.username, &key0)
+	err := credentialAuthority.CheckUserKey(context.TODO(), u0.uid, &u0.username, &key0)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if S.numGets != 1 {
+	if state.numGets != 1 {
 		t.Fatal("expected 1 get")
 	}
-	err = C.CheckUserKey(context.TODO(), u0.uid, &u0.username, &key0)
+	err = credentialAuthority.CheckUserKey(context.TODO(), u0.uid, &u0.username, &key0)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if S.numGets != 1 {
+	if state.numGets != 1 {
 		t.Fatal("expected 1 get")
 	}
 
-	S.mutateUser(u0.uid, func(u *testUser) {
+	state.mutateUser(u0.uid, func(u *testUser) {
 		u.sibkeys = u.sibkeys[1:]
 	})
 
 	// Advance just an iota, so that our polling of the server
 	// has a chance to complete.
-	S.tick(pollWait)
+	state.tick(pollWait)
 
 	// wait for the first eviction
-	uid := <-S.evictCh
+	uid := <-state.evictCh
 	if uid != u0.uid {
 		t.Fatalf("Wrong UID on eviction: %s != %s\n", uid, u0.uid)
 	}
 
-	err = C.CheckUserKey(context.TODO(), u0.uid, &u0.username, &key0)
+	err = credentialAuthority.CheckUserKey(context.TODO(), u0.uid, &u0.username, &key0)
 	if err == nil {
 		t.Fatal("Expected an error")
 	} else if bke, ok := err.(BadKeyError); !ok {
@@ -203,72 +203,72 @@ func TestSimple(t *testing.T) {
 		t.Fatalf("Expected a bad key error on key %s (not %s)", key0, bke.kid)
 	}
 
-	err = C.CheckUserKey(context.TODO(), u0.uid, &u0.username, &key1)
+	err = credentialAuthority.CheckUserKey(context.TODO(), u0.uid, &u0.username, &key1)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if S.numGets != 2 {
+	if state.numGets != 2 {
 		t.Fatal("expected 2 gets")
 	}
-	S.tick(userTimeout + time.Millisecond)
-	err = C.CheckUserKey(context.TODO(), u0.uid, &u0.username, &key1)
+	state.tick(userTimeout + time.Millisecond)
+	err = credentialAuthority.CheckUserKey(context.TODO(), u0.uid, &u0.username, &key1)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if S.numGets != 3 {
+	if state.numGets != 3 {
 		t.Fatal("expected 3 gets")
 	}
-	S.tick(cacheTimeout + time.Millisecond)
+	state.tick(cacheTimeout + time.Millisecond)
 
 	// u0 should now be gone since we haven't touched him in over cacheTimeout
 	// duration.
-	uid = <-S.evictCh
+	uid = <-state.evictCh
 	if uid != u0.uid {
 		t.Fatalf("Wrong UID on eviction: %s != %s\n", uid, u0.uid)
 	}
 
 	// Make a new user -- u1!
-	u1 := S.newTestUser(4)
+	u1 := state.newTestUser(4)
 
 	ng := 3
 	for i := 0; i < 10; i++ {
-		err = C.CheckUserKey(context.TODO(), u1.uid, &u1.username, &u1.sibkeys[0])
+		err = credentialAuthority.CheckUserKey(context.TODO(), u1.uid, &u1.username, &u1.sibkeys[0])
 		if err != nil {
 			t.Fatal(err)
 		}
 		ng++
-		if S.numGets != ng {
-			t.Fatalf("expected %d gets, got %d", ng, S.numGets)
+		if state.numGets != ng {
+			t.Fatalf("expected %d gets, got %d", ng, state.numGets)
 		}
-		S.tick(userTimeout + time.Millisecond)
+		state.tick(userTimeout + time.Millisecond)
 
 		select {
-		case uid = <-S.evictCh:
+		case uid = <-state.evictCh:
 			t.Fatalf("Got unwanted eviction for %s", uid)
 		default:
 		}
 	}
 
-	S.tick(cacheTimeout - userTimeout + 3*time.Millisecond)
-	uid = <-S.evictCh
+	state.tick(cacheTimeout - userTimeout + 3*time.Millisecond)
+	uid = <-state.evictCh
 	if uid != u1.uid {
 		t.Fatalf("Got wrong eviction: wanted %s but got %s\n", u1.uid, uid)
 	}
 
 	// Make a new user -- u2!
-	u2 := S.newTestUser(4)
-	err = C.CheckUserKey(context.TODO(), u2.uid, &u2.username, &u2.sibkeys[0])
+	u2 := state.newTestUser(4)
+	err = credentialAuthority.CheckUserKey(context.TODO(), u2.uid, &u2.username, &u2.sibkeys[0])
 	if err != nil {
 		t.Fatal(err)
 	}
 	ng++
-	if S.numGets != ng {
-		t.Fatalf("expected %d gets, got %d", ng, S.numGets)
+	if state.numGets != ng {
+		t.Fatalf("expected %d gets, got %d", ng, state.numGets)
 	}
 
 	// Check that u2 is evicted properly after we shutdown the CA.
-	C.Shutdown()
-	uid = <-S.evictCh
+	credentialAuthority.Shutdown()
+	uid = <-state.evictCh
 	if uid != u2.uid {
 		t.Fatalf("Got wrong eviction: wanted %s but got %s\n", u2.uid, uid)
 	}
@@ -276,65 +276,65 @@ func TestSimple(t *testing.T) {
 }
 
 func TestCheckUsers(t *testing.T) {
-	S, C := newTestSetup()
+	state, credentialAuthority := newTestSetup()
 
 	var users, usersWithDud []keybase1.UID
 	for i := 0; i < 10; i++ {
-		u := S.newTestUser(2)
+		u := state.newTestUser(2)
 		users = append(users, u.uid)
 		usersWithDud = append(usersWithDud, u.uid)
 	}
 	usersWithDud = append(usersWithDud, libkb.UsernameToUID(genUsername()))
 
-	if S.numGets != 0 {
+	if state.numGets != 0 {
 		t.Fatal("expected 0 gets")
 	}
 
-	err := C.CheckUsers(context.TODO(), users)
+	err := credentialAuthority.CheckUsers(context.TODO(), users)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if S.numGets != 10 {
+	if state.numGets != 10 {
 		t.Fatal("expected 10 gets")
 	}
-	err = C.CheckUsers(context.TODO(), users)
+	err = credentialAuthority.CheckUsers(context.TODO(), users)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if S.numGets != 10 {
+	if state.numGets != 10 {
 		t.Fatal("expected 10 gets")
 	}
 
-	err = C.CheckUsers(context.TODO(), usersWithDud)
+	err = credentialAuthority.CheckUsers(context.TODO(), usersWithDud)
 	if err == nil {
 		t.Fatal("Expected an error")
 	} else if _, ok := err.(userNotFoundError); !ok {
 		t.Fatal("Expected a user not found error")
 	}
-	C.Shutdown()
+	credentialAuthority.Shutdown()
 }
 
 func TestCompareKeys(t *testing.T) {
-	S, C := newTestSetup()
-	u := S.newTestUser(10)
+	state, credentialAuthority := newTestSetup()
+	u := state.newTestUser(10)
 
-	err := C.CompareUserKeys(context.TODO(), u.uid, u.sibkeys, u.subkeys)
+	err := credentialAuthority.CompareUserKeys(context.TODO(), u.uid, u.sibkeys, u.subkeys)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	err = C.CompareUserKeys(context.TODO(), u.uid, nil, u.subkeys)
+	err = credentialAuthority.CompareUserKeys(context.TODO(), u.uid, nil, u.subkeys)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	err = C.CompareUserKeys(context.TODO(), u.uid, u.sibkeys, nil)
+	err = credentialAuthority.CompareUserKeys(context.TODO(), u.uid, u.sibkeys, nil)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	missingSibkey := u.sibkeys[1:]
-	err = C.CompareUserKeys(context.TODO(), u.uid, missingSibkey, u.subkeys)
+	err = credentialAuthority.CompareUserKeys(context.TODO(), u.uid, missingSibkey, u.subkeys)
 	if err == nil {
 		t.Fatal("Expected an error")
 	} else if _, ok := err.(KeysNotEqualError); !ok {
@@ -342,11 +342,11 @@ func TestCompareKeys(t *testing.T) {
 	}
 
 	missingSubkey := u.subkeys[1:]
-	err = C.CompareUserKeys(context.TODO(), u.uid, u.sibkeys, missingSubkey)
+	err = credentialAuthority.CompareUserKeys(context.TODO(), u.uid, u.sibkeys, missingSubkey)
 	if err == nil {
 		t.Fatal("Expected an error")
 	} else if _, ok := err.(KeysNotEqualError); !ok {
 		t.Fatal("Expected keys not equal error")
 	}
-	C.Shutdown()
+	credentialAuthority.Shutdown()
 }
