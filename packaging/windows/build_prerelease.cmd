a2c0cba50fa222dc8819b9bbf527cec693eb030a
Steve Sanders <steve@keyba.se>
2016-02-20 14:06:58 -0800

    Use release tool for updating json, index and latest on Windows Revert duplicate update json generation

--- packaging/windows/build_prerelease.cmd
+++ packaging/windows/build_prerelease.cmd
@@ -1,18 +1,22 @@
+pushd
 :: Build keybase.exe with prerelease options
-cd %GOPATH%\src\github.com\keybase\client\go\keybase
+pushd %GOPATH%\src\github.com\keybase\client\go\keybase
 set GOARCH=386
 go generate
+for /f %%i in ('winresource.exe -cv') do set KEYBASE_VERSION=%%i
+echo %KEYBASE_VERSION%
 for /f %%i in ('winresource.exe -cb') do set KEYBASE_BUILD=%%i
 echo %KEYBASE_BUILD%
 go build -a -tags "prerelease production" -ldflags="-X github.com/keybase/client/go/libkb.CustomBuild=%KEYBASE_BUILD%"
+popd
 
 :: Then build kbfsdokan
-::cd %GOPATH%\src\github.com\keybase\kbfs\kbfsdokan
-::go build -tags "production prerelease"
+pushd %GOPATH%\src\github.com\keybase\kbfs\kbfsdokan
+go build -tags "production prerelease"
+popd
 
 :: Then the desktop:
-::cd %GOPATH%\src\github.com\keybase\client\react-native\react
-::npm i
-::cd %GOPATH%\src\github.com\keybase\client\desktop
-::npm i
+pushd  %GOPATH%\src\github.com\keybase\client\desktop
+cmd /k npm i
 npm run package -- --arch ia32 --platform win32 --appVersion %KEYBASE_VERSION%
+popd
