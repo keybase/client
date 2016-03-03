475478bd739e6b89e9413bd6ddfc25900a304b13
Marco Munizaga <marco@marcopolo.io>
2016-03-03 11:34:17 -0800

    Show noAvatar image until we load the avatar, then show the actual avatar

--- shared/common-adapters/avatar.desktop.js
+++ shared/common-adapters/avatar.desktop.js
@@ -2,6 +2,7 @@
 
 import React, {Component} from 'react'
 import resolveRoot from '../../desktop/resolve-root'
+import {globalColorsDZ2} from '../styles/style-guide'
 import type {Props} from './avatar'
 
 const noAvatar = `file:///${resolveRoot('shared/images/icons/placeholder-avatar@2x.png')}`
@@ -9,18 +10,43 @@ const noAvatar = `file:///${resolveRoot('shared/images/icons/placeholder-avatar@
 export default class Avatar extends Component {
   props: Props;
 
+  state: {
+    avatarLoaded: boolean
+  };
+
+  constructor (props: Props) {
+    super(props)
+    this.state = {avatarLoaded: false}
+  }
+
   render () {
     return (
-      <img
-        style={{width: this.props.size, height: this.props.size, borderRadius: this.props.size / 2, ...this.props.style}}
-        src={this.props.url}
-        onError={event => (event.target.src = noAvatar)}/>)
+      <div style={{position: 'relative'}}>
+        <div
+          style={{...avatarStyle(this.props.size),
+            backgroundImage: `url('${noAvatar}')`,
+            backgroundSize: 'cover',
+            ...this.props.style
+          }}/>
+        <img
+          src={this.props.url}
+          style={{...avatarStyle(this.props.size),
+            display: this.state.avatarLoaded ? 'block' : 'none',
+            backgroundColor: globalColorsDZ2.white,
+            ...this.props.style
+          }}
+          onLoad={() => this.setState({avatarLoaded: true})}/>
+      </div>
+    )
   }
 }
 
-Avatar.propTypes = {
-  size: React.PropTypes.number.isRequired,
-  url: React.PropTypes.string,
-  style: React.PropTypes.object
+function avatarStyle (size: number): Object {
+  return {
+    width: size,
+    height: size,
+    borderRadius: size / 2,
+    position: 'absolute'
+  }
 }
 
