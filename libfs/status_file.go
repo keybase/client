07ffc2feecd9cff12d6c471643aa4bcc62549acb
John Zila <john@keyba.se>
2016-03-03 10:06:42 -0800

    Implemented PR feedback to factor out status file generation to libfs
    Signed-off-by: John Zila <john@keyba.se>

--- libfs/status_file.go
+++ libfs/status_file.go
@@ -4,14 +4,69 @@
 
 package libfs
 
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/keybase/kbfs/libkbfs"
+	"golang.org/x/net/context"
+)
+
 // StatusFileName is the name of the KBFS status file -- it can be reached
 // anywhere within a top-level folder or inside the Keybase root
 const StatusFileName = ".kbfs_status"
 
 // KbfsStatus represents the content of the top-level status file
-type KbfsStatus struct {
+type kbfsStatus struct {
 	CurrentUser string
 	IsConnected bool
 	UsageBytes  int64
 	LimitBytes  int64
+}
+
+// GetEncodedStatus returns serialized JSON containing status information for a
+// folder
+func GetEncodedFolderStatus(ctx context.Context, config libkbfs.Config,
+	folderBranch *libkbfs.FolderBranch) (
+	data []byte, t time.Time, err error) {
+
+	var status libkbfs.FolderBranchStatus
+	status, _, err = config.KBFSOps().
+		Status(ctx, *folderBranch)
+	if err != nil {
+		return nil, time.Time{}, err
+	}
+
+	data, err = json.MarshalIndent(status, "", "  ")
+	if err != nil {
+		return nil, time.Time{}, err
+	}
+
+	data = append(data, '\n')
+	return data, time.Time{}, err
+}
+
+// GetEncodedStatus returns serialized JSON containing top-level KBFS status
+// information
+func GetEncodedStatus(ctx context.Context, config libkbfs.Config) (
+	data []byte, t time.Time, err error) {
+	username, _, _ := config.KBPKI().GetCurrentUserInfo(ctx)
+	var usageBytes int64
+	var limitBytes int64
+	quotaInfo, err := config.BlockServer().GetUserQuotaInfo(ctx)
+	if err == nil {
+		usageBytes = quotaInfo.Total.UsageBytes
+		limitBytes = quotaInfo.Limit
+	}
+	data, err = json.MarshalIndent(kbfsStatus{
+		CurrentUser: username.String(),
+		IsConnected: config.MDServer().IsConnected(),
+		UsageBytes:  usageBytes,
+		LimitBytes:  limitBytes,
+	}, "", "  ")
+	if err != nil {
+		return nil, t, err
+	}
+	data = append(data, '\n')
+	return data, t, err
 }
--- libfuse/status_file.go
+++ libfuse/status_file.go
@@ -1,58 +1,14 @@
 package libfuse
 
 import (
-	"encoding/json"
 	"time"
 
 	"bazil.org/fuse"
+	"golang.org/x/net/context"
 
 	"github.com/keybase/kbfs/libfs"
 	"github.com/keybase/kbfs/libkbfs"
-	"golang.org/x/net/context"
 )
-
-func getEncodedFolderStatus(ctx context.Context, fs *FS,
-	folderBranch *libkbfs.FolderBranch) (
-	data []byte, t time.Time, err error) {
-
-	var status libkbfs.FolderBranchStatus
-	status, _, err = fs.config.KBFSOps().
-		Status(ctx, *folderBranch)
-	if err != nil {
-		return nil, time.Time{}, err
-	}
-
-	data, err = json.MarshalIndent(status, "", "  ")
-	if err != nil {
-		return nil, time.Time{}, err
-	}
-
-	data = append(data, '\n')
-	return data, time.Time{}, err
-}
-
-func getEncodedStatus(ctx context.Context, fs *FS) (
-	data []byte, t time.Time, err error) {
-	username, _, _ := fs.config.KBPKI().GetCurrentUserInfo(ctx)
-	var usageBytes int64
-	var limitBytes int64
-	quotaInfo, err := fs.config.BlockServer().GetUserQuotaInfo(ctx)
-	if err == nil {
-		usageBytes = quotaInfo.Total.UsageBytes
-		limitBytes = quotaInfo.Limit
-	}
-	data, err = json.MarshalIndent(libfs.KbfsStatus{
-		CurrentUser: username.String(),
-		IsConnected: fs.config.MDServer().IsConnected(),
-		UsageBytes:  usageBytes,
-		LimitBytes:  limitBytes,
-	}, "", "  ")
-	if err != nil {
-		return nil, t, err
-	}
-	data = append(data, '\n')
-	return data, t, err
-}
 
 // NewStatusFile returns a special read file that contains a text
 // representation of the status of the current TLF.
@@ -61,9 +17,9 @@ func NewStatusFile(fs *FS, folderBranch *libkbfs.FolderBranch, resp *fuse.Lookup
 	return &SpecialReadFile{
 		read: func(ctx context.Context) ([]byte, time.Time, error) {
 			if folderBranch == nil {
-				return getEncodedStatus(ctx, fs)
+				return libfs.GetEncodedStatus(ctx, fs.config)
 			}
-			return getEncodedFolderStatus(ctx, fs, folderBranch)
+			return libfs.GetEncodedFolderStatus(ctx, fs.config, folderBranch)
 		},
 	}
 }
