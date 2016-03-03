3f01b66264dd48e901050b00f181be1a6de0add6
Chris Ball <chris@printf.net>
2016-03-03 12:57:20 -0500

    Only enable pinentry Continue button if there's a passphrase entered

--- shared/pinentry/index.render.desktop.js
+++ shared/pinentry/index.render.desktop.js
@@ -75,7 +75,7 @@ export default class PinentryRender extends Component {
     return (
       <div>
         <Header icon title='' onClose={() => this.props.onCancel()} />
-        <div style={{...styles2.container, alignItems: 'center', padding: 5, paddingLeft: 30, paddingRight: 30}}>
+        <div style={{...styles2.container, padding: 5, paddingLeft: 30, paddingRight: 30}}>
           <Text dz2 type='Body'>{this.props.prompt}</Text>
         </div>
         <div style={{...styles2.container, alignItems: 'center', padding: 10, paddingLeft: 30, paddingRight: 30}}>
@@ -87,7 +87,7 @@ export default class PinentryRender extends Component {
           />
         </div>
         <div style={{...styles2.container, alignItems: 'flex-end', paddingRight: 30, paddingBottom: 30}}>
-          <Button dz2 type='Primary' label='Continue' onClick={submitPassphrase} />
+          <Button dz2 type='Primary' label='Continue' onClick={submitPassphrase} disabled={!this.state.passphrase}/>
         </div>
       </div>
     )
