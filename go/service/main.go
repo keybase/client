a8dac73fd99d18faeb2a794b5a656484549b675b
Patrick Crosby <pcrosby@gmail.com>
2016-03-08 16:04:06 -0600

    PR feedback, refactor autologin to func

--- go/service/main.go
+++ go/service/main.go
@@ -178,27 +178,34 @@ func (d *Service) Run() (err error) {
 		}
 	}
 
-	// If daemon and they were logged in before, then try logging in
-	// automatically.
-	if d.isDaemon {
-		if d.G().Env.GetConfig().GetLoggedIn() {
-			un := d.G().Env.GetUsername()
-			d.G().Log.Debug("autologin: user %q was previously logged in, trying autologin", un)
-			if err := d.G().LoginState().LoginWithStoredSecret(un.String(), nil); err != nil {
-				d.G().Log.Debug("autologin: %q failed: %s", un, err)
-			} else {
-				d.G().Log.Debug("autologin: success")
-			}
-		} else {
-			d.G().Log.Debug("autologin: logged_in config flag not set, skipping autologin")
-		}
-	}
+	d.tryAutoLogin()
 
 	d.checkTrackingEveryHour()
 
 	d.G().ExitCode, err = d.ListenLoopWithStopper(l)
 
 	return err
+}
+
+func (d *Service) tryAutoLogin() {
+	// If daemon and they were logged in before, then try logging in
+	// automatically.
+	if !d.isDaemon {
+		return
+	}
+
+	if !d.G().Env.GetConfig().GetLoggedIn() {
+		d.G().Log.Debug("autologin: logged_in config flag not set, skipping autologin")
+		return
+	}
+
+	un := d.G().Env.GetUsername()
+	d.G().Log.Debug("autologin: user %q was previously logged in, trying autologin", un)
+	if err := d.G().LoginState().LoginWithStoredSecret(un.String(), nil); err != nil {
+		d.G().Log.Debug("autologin: %q failed: %s", un, err)
+	} else {
+		d.G().Log.Debug("autologin: success")
+	}
 }
 
 func (d *Service) StartLoopbackServer() error {
