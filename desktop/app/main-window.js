157db3722cc41586c41c115227be619ea62a0788
Marco Munizaga <marco@marcopolo.io>
2016-03-11 11:28:48 -0800

    Use content size when creating the main window

--- desktop/app/main-window.js
+++ desktop/app/main-window.js
@@ -8,6 +8,7 @@ import {globalResizing} from '../shared/styles/style-guide'
 export default function () {
   const mainWindow = new Window(
     resolveRoot(`renderer/index.html?src=${hotPath('index.bundle.js')}`), {
+      useContentSize: true,
       width: globalResizing.login.width,
       height: globalResizing.login.height,
       show: flags.login
