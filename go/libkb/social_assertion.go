9c19816502282d5ff2796a2bb1ad137575d94c90
Patrick Crosby <pcrosby@gmail.com>
2016-03-14 14:59:55 -0500

    Cleanup

--- go/libkb/social_assertion.go
+++ go/libkb/social_assertion.go
@@ -5,13 +5,25 @@ package libkb
 
 import "strings"
 
+// SocialAssertion is a type for individual user's on a social
+// network.  It should be created via NormalizeSocialAssertion.
 type SocialAssertion string
 
+// IsSocialAssertion returns true for strings that are valid
+// social assertions.  They do not need to be normalized, so
+// user@twitter and twitter:user will work, as will
+// USER@Twitter.
 func IsSocialAssertion(s string) bool {
 	_, ok := NormalizeSocialAssertion(s)
 	return ok
 }
 
+// NormalizeSocialAssertion creates a SocialAssertion from its
+// input and normalizes it.  The service name will be lowercased.
+// If the service is case-insensitive, then the username will also
+// be lowercased.  Colon assertions (twitter:user) will be
+// transformed to the user@twitter format.  Only registered
+// services are allowed.
 func NormalizeSocialAssertion(s string) (SocialAssertion, bool) {
 	if strings.Count(s, ":")+strings.Count(s, "@") != 1 {
 		return "", false
