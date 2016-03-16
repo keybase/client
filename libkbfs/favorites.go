e42eeb46137fbfe382b2b1389df877944b8c628b
Jeremy Stribling <strib@alum.mit.edu>
2016-03-16 16:58:22 -0700

    favorites: add a non-blocking Add interface
    Also batch adds together when a duplicate one is already in flight.
    
    Suggested by @akalin-keybase.
    
    Issue: #812

--- libkbfs/favorites.go
+++ libkbfs/favorites.go
@@ -1,6 +1,10 @@
 package libkbfs
 
-import "golang.org/x/net/context"
+import (
+	"sync"
+
+	"golang.org/x/net/context"
+)
 
 // favReq represents a request to access the logged-in user's
 // favorites list.  A single request can do one or more of the
@@ -15,8 +19,9 @@ type favReq struct {
 	toDel   []Favorite
 	favs    chan<- []Favorite
 
-	// Signaled when the request is done
-	done chan<- error
+	// Signaled when the request is done.  Protected by
+	// Favorites.inFlightLock.
+	done []chan<- error
 
 	// Context
 	ctx context.Context
@@ -34,19 +39,34 @@ type Favorites struct {
 	// favorites list, if other devices have modified the list since
 	// the last refresh.
 	cache map[Favorite]bool
+
+	inFlightLock sync.Mutex
+	inFlightAdds map[Favorite]favReq
 }
 
 // NewFavorites constructs a new Favorites instance.
 func NewFavorites(config Config) *Favorites {
 	f := &Favorites{
-		config:  config,
-		reqChan: make(chan favReq),
+		config:       config,
+		reqChan:      make(chan favReq, 100),
+		inFlightAdds: make(map[Favorite]favReq),
 	}
 	go f.loop()
 	return f
 }
 
-func (f *Favorites) handleReq(req favReq) {
+func (f *Favorites) handleReq(req favReq) (err error) {
+	defer func() {
+		f.inFlightLock.Lock()
+		defer f.inFlightLock.Unlock()
+		for _, ch := range req.done {
+			ch <- err
+		}
+		for _, fav := range req.toAdd {
+			delete(f.inFlightAdds, fav)
+		}
+	}()
+
 	kbpki := f.config.KBPKI()
 	// Fetch a new list if:
 	//  * The user asked us to refresh
@@ -57,8 +77,7 @@ func (f *Favorites) handleReq(req favReq) {
 		f.cache = make(map[Favorite]bool)
 		folders, err := kbpki.FavoriteList(req.ctx)
 		if err != nil {
-			req.done <- err
-			return
+			return err
 		}
 
 		for _, folder := range folders {
@@ -78,8 +97,7 @@ func (f *Favorites) handleReq(req favReq) {
 		// already favorited.
 		err := kbpki.FavoriteAdd(req.ctx, fav.toKBFolder())
 		if err != nil {
-			req.done <- err
-			return
+			return err
 		}
 	}
 
@@ -89,8 +107,7 @@ func (f *Favorites) handleReq(req favReq) {
 		folder := fav.toKBFolder()
 		err := kbpki.FavoriteDelete(req.ctx, folder)
 		if err != nil {
-			req.done <- err
-			return
+			return err
 		}
 		if !folder.Private {
 			// Public folders may be stored under a different name,
@@ -98,8 +115,7 @@ func (f *Favorites) handleReq(req favReq) {
 			folder.Name = folder.Name + ReaderSep + "public"
 			err := kbpki.FavoriteDelete(req.ctx, folder)
 			if err != nil {
-				req.done <- err
-				return
+				return err
 			}
 		}
 		delete(f.cache, fav)
@@ -113,7 +129,7 @@ func (f *Favorites) handleReq(req favReq) {
 		req.favs <- favorites
 	}
 
-	req.done <- nil
+	return nil
 }
 
 func (f *Favorites) loop() {
@@ -127,27 +143,84 @@ func (f *Favorites) Shutdown() {
 	close(f.reqChan)
 }
 
+func (f *Favorites) waitOnErrChan(ctx context.Context,
+	errChan <-chan error) (retry bool, err error) {
+	select {
+	case <-ctx.Done():
+		return false, ctx.Err()
+	case err := <-errChan:
+		// If the request was canceled due to a context timeout that
+		// wasn't our own, try it again.
+		if err == context.Canceled || err == context.DeadlineExceeded {
+			select {
+			case <-ctx.Done():
+				return false, err
+			default:
+				return true, nil
+			}
+		}
+		return false, err
+	}
+}
+
 func (f *Favorites) sendReq(ctx context.Context, req favReq) error {
 	errChan := make(chan error, 1)
-	req.done = errChan
+	req.done = append(req.done, errChan)
 	req.ctx = ctx
 	select {
 	case f.reqChan <- req:
 	case <-ctx.Done():
 		return ctx.Err()
 	}
-	select {
-	case <-ctx.Done():
-		return ctx.Err()
-	case err := <-errChan:
-		return err
-	}
+	// With a direct sendReq call, we'll never have a shared request,
+	// so no need to check the retry status.
+	_, err := f.waitOnErrChan(ctx, errChan)
+	return err
 }
 
-// Add adds a favorite to your favorites list, unless it
-// already exists in the cached list of favorites.
+func (f *Favorites) startOrJoinAddReq(
+	ctx context.Context, fav Favorite) (*favReq, <-chan error) {
+	f.inFlightLock.Lock()
+	defer f.inFlightLock.Unlock()
+	req, ok := f.inFlightAdds[fav]
+	var startReq *favReq
+	if !ok {
+		req = favReq{ctx: ctx, toAdd: []Favorite{fav}}
+		f.inFlightAdds[fav] = req
+		startReq = &req
+	}
+	errChan := make(chan error, 1)
+	req.done = append(req.done, errChan)
+	return startReq, errChan
+}
+
+// Add adds a favorite to your favorites list.
 func (f *Favorites) Add(ctx context.Context, fav Favorite) error {
-	return f.sendReq(ctx, favReq{toAdd: []Favorite{fav}})
+	doAdd := true
+	var err error
+	for doAdd {
+		startReq, errChan := f.startOrJoinAddReq(ctx, fav)
+		if startReq != nil {
+			return f.sendReq(ctx, *startReq)
+		}
+		doAdd, err = f.waitOnErrChan(ctx, errChan)
+	}
+	return err
+}
+
+// AddAsync initiates a request to add this favorite to your favorites
+// list, if one is not already in flight, but it doesn't wait for the
+// result.  (It could block while kicking off the request, if lots of
+// different favorite operations are in flight.)
+func (f *Favorites) AddAsync(ctx context.Context, fav Favorite) {
+	startReq, _ := f.startOrJoinAddReq(ctx, fav)
+	if startReq != nil {
+		select {
+		case f.reqChan <- *startReq:
+		case <-ctx.Done():
+			return
+		}
+	}
 }
 
 // Delete deletes a favorite from the favorites list.  It is
@@ -162,7 +235,7 @@ func (f *Favorites) RefreshCache(ctx context.Context) {
 	// and context.
 	req := favReq{
 		refresh: true,
-		done:    make(chan error, 1),
+		done:    []chan<- error{make(chan error, 1)},
 		ctx:     context.Background(),
 	}
 	select {
