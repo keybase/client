5250d4066b35d89a0d65a036dceff32cf61c1879
chrisnojima <cnojima@keyba.se>
2016-03-17 17:51:11 -0400

    auto focus

--- shared/login/signup/invite-code.render.desktop.js
+++ shared/login/signup/invite-code.render.desktop.js
@@ -30,7 +30,7 @@ export default class Render extends Component {
       <Container onBack={this.props.onBack} style={styles.container}>
         <Text style={styles.header} type='Header'>Type in your invite code:</Text>
         <Icon style={styles.icon} type='invite-code-m'/>
-        <Input style={styles.input} hintText='goddess brown result reject' value={this.state.inviteCode} errorText={this.props.inviteCodeErrorText} onEnterKeyDown={submitInviteCode} onChange={event => this.setState({inviteCode: event.target.value})}/>
+        <Input autoFocus style={styles.input} hintText='goddess brown result reject' value={this.state.inviteCode} errorText={this.props.inviteCodeErrorText} onEnterKeyDown={submitInviteCode} onChange={event => this.setState({inviteCode: event.target.value})}/>
         <Button style={styles.button} type='Primary' label='Continue' onClick={submitInviteCode} disabled={!this.state.inviteCode}/>
         <Text style={styles.text} type='Body'>Not invited?</Text>
         <Text type='BodyPrimaryLink' onClick={this.props.onRequestInvite}>Request an invite code</Text>
