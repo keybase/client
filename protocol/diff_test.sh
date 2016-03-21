e77bbf9cd0a5ca1ad5932f81bb59314b208df881
Jack O'Connor <oconnor663@gmail.com>
2016-03-21 14:10:36 -0400

    make the protocol cleanliness check produce a clear error message

---
+++ protocol/diff_test.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+
+set -e -u -o pipefail
+
+here="$(dirname "$BASH_SOURCE")"
+cd "$here"
+
+npm i
+gem install activesupport
+
+make clean
+make
+
+if ! git diff --quiet --exit-code ; then
+  echo 'ERROR: `git diff` detected changes. The generated protocol files are stale.'
+  exit 1
+fi
+
+echo 'SUCCESS: The generated protocol files are up to date.'
