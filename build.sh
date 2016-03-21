5782435d853c69233ccd86c43e3f3f6f6c787e8a
Chris Ball <chris@printf.net>
2016-03-21 10:13:15 -0400

    add basic build.sh

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e -u
+
+export GO15VENDOREXPERIMENT=1
+cd shared
+npm i --global flow-bin@`tail -n1 .flowconfig`
+flow
+cd ../protocol
+npm i
+gem install activesupport
+make clean
+make
+git diff --quiet --exit-code 
+cd ../go/keybase
+go build
