a770b13daf51c8e16106230c08fcca0bd6081e82
Andy Alness <andy.alness@gmail.com>
2016-03-25 12:53:21 -0700

    vendor: update github.com/keybase/go-redis-pubsub

--- vendor/github.com/keybase/go-redis-pubsub/publisher.go
+++ vendor/github.com/keybase/go-redis-pubsub/publisher.go
@@ -3,12 +3,17 @@ package pubsub
 import (
 	"errors"
 	"sync"
+	"time"
 
+	"github.com/cenkalti/backoff"
 	"github.com/garyburd/redigo/redis"
 )
 
 // ErrPublishWouldBlock is returned when the outgoing message buffer is full.
 var ErrPublishWouldBlock = errors.New("Publish would block")
+
+// ErrPublishPoolClosed is returned when the publish pool is closed.
+var ErrPublishPoolClosed = errors.New("Publish pool closed")
 
 // DefaultPublisherPoolSize is the default size of the worker pool.
 const DefaultPublisherPoolSize = 16
@@ -28,6 +33,8 @@ type Publisher interface {
 type PublicationHandler interface {
 	// OnPublishConnect is called upon each successful connection.
 	OnPublishConnect(conn redis.Conn, address string)
+	// OnPublishConnectError is called whenever there is an error connecting.
+	OnPublishConnectError(err error, nextTime time.Duration)
 	// OnPublishError is called upon any error when attempting to publish a message.
 	OnPublishError(err error, channel string, data []byte)
 }
@@ -38,10 +45,11 @@ type message struct {
 }
 
 type redisPublisher struct {
-	pool     *redis.Pool
-	handler  PublicationHandler
-	messages chan *message
-	wg       sync.WaitGroup
+	pool      *redis.Pool
+	handler   PublicationHandler
+	messages  chan *message
+	closeChan chan struct{}
+	wg        sync.WaitGroup
 }
 
 // NewRedisPublisher instantiates a Publisher implementation backed by Redis.
@@ -52,20 +60,41 @@ func NewRedisPublisher(address string, handler PublicationHandler, poolSize, buf
 	if bufferSize == 0 {
 		bufferSize = DefaultPublisherBufferSize
 	}
+	closeChan := make(chan struct{}, 1)
 	p := &redisPublisher{
 		pool: &redis.Pool{
 			MaxIdle: poolSize,
-			Dial: func() (redis.Conn, error) {
-				conn, err := redis.Dial("tcp", address)
-				if err != nil {
-					return nil, err
+			Dial: func() (conn redis.Conn, err error) {
+				expBackoff := backoff.NewExponentialBackOff()
+				// don't quit trying
+				expBackoff.MaxElapsedTime = 0
+				err = backoff.RetryNotify(func() error {
+					select {
+					case <-closeChan:
+						// break out of loop if closed
+						return nil
+					default:
+					}
+					var err error
+					conn, err = redis.Dial("tcp", address)
+					return err
+				}, expBackoff,
+					handler.OnPublishConnectError)
+				select {
+				case <-closeChan:
+					err = ErrPublishPoolClosed
+					return conn, err
+				default:
 				}
-				handler.OnPublishConnect(conn, address)
+				if err == nil {
+					handler.OnPublishConnect(conn, address)
+				}
 				return conn, err
 			},
 		},
-		messages: make(chan *message, bufferSize),
-		handler:  handler,
+		handler:   handler,
+		messages:  make(chan *message, bufferSize),
+		closeChan: closeChan,
 	}
 	// start the workers
 	for i := 0; i < poolSize; i++ {
@@ -99,6 +128,7 @@ func (p *redisPublisher) Publish(channel string, data []byte) {
 
 // Shutdown implements the Publisher interface.
 func (p *redisPublisher) Shutdown() {
+	close(p.closeChan)
 	close(p.messages)
 	p.pool.Close()
 	p.wg.Wait()
