06dbb9a62aaebc7dd8a8bfbf08de8abee2b7ad46
chrisnojima <cnojima@keyba.se>
2016-03-29 16:25:05 -0400

    buttons on android

--- shared/common-adapters/progress-indicator.android.js
+++ shared/common-adapters/progress-indicator.android.js
@@ -1,2 +1,18 @@
+import React, {Component} from 'react'
 import {ProgressBarAndroid} from 'react-native'
-export default ProgressBarAndroid
+import {globalColors} from '../styles/style-guide'
+
+export default class ProgressIndicator extends Component {
+  render () {
+    return <ProgressBarAndroid
+      color={this.props.white ? globalColors.white : globalColors.black}
+      styleAttr='Small'
+      style={{...style, ...this.props.style}}/>
+  }
+}
+
+const style = {
+  alignItems: 'center',
+  justifyContent: 'center'
+}
+
