e155a01e9d66771ad368daec384f066250d77e1b
Chris Ball <chris@printf.net>
2016-04-01 13:14:52 -0400

    switch to object lookup form

--- shared/util/kbfs.js
+++ shared/util/kbfs.js
@@ -41,75 +41,65 @@ export function cleanup (folderName: string): string {
 export function decodeKBFSError (user: string, notification: FSNotification): Array<string> {
   const basedir = notification.filename.split(path.sep)[0]
   const tlf = `/keybase${getTLF(notification.publicTopLevelFolder, basedir)}`
-  switch (notification.errorType) {
-    case enums.kbfs.FSErrorType.accessDenied:
+  const errors = {
+    [enums.kbfs.FSErrorType.accessDenied]: [
+      'Keybase: Access denied',
+      `${user} does not have ${notification.params.mode} access to ${tlf}`
+    ],
+    [enums.kbfs.FSErrorType.userNotFound]: [
+      'Keybase: User not found',
+      `${notification.params.username} is not a Keybase user`
+    ],
+    [enums.kbfs.FSErrorType.revokedDataDetected]: [
+      'Keybase: Possibly revoked data detected',
+      `${tlf} was modified by a revoked or bad device. Use 'keybase log send' to file an issue with the Keybase admins.`
+    ],
+    [enums.kbfs.FSErrorType.notLoggedIn]: [
+      `Keybase: Permission denied in ${tlf}`,
+      "You are not logged into Keybase. Try 'keybase login'."
+    ],
+    [enums.kbfs.FSErrorType.timeout]: [
+      `Keybase: ${_.capitalize(notification.params.mode)} timeout in ${tlf}`,
+      `The ${notification.params.mode} operation took too long and failed. Please run 'keybase log send' so our admins can review.`
+    ],
+    [enums.kbfs.FSErrorType.rekeyNeeded]: notification.rekeyself ? [
+      'Keybase: Files need to be rekeyed',
+      `Please open one of your other computers to unlock ${tlf}`
+    ] : [
+      'Keybase: Friends needed',
+      `Please ask another member of ${tlf} to open Keybase on one of their computers to unlock it for you.`
+    ],
+    [enums.kbfs.FSErrorType.badFolder]: [
+      'Keybase: Bad folder',
+      `${tlf} is not a Keybase folder. All folders begin with /keybase/private or /keybase/public.`
+    ]
+  }
+
+  if (notification.errorType in errors) {
+    return errors[notification.errorType]
+  }
+
+  if (notification.errorType === enums.kbfs.FSErrorType.notImplemented) {
+    if (notification.feature === '2gbFileLimit') {
       return ([
-        'Keybase: Access denied',
-        `${user} does not have ${notification.params.mode} access to ${tlf}`
+        'Keybase: Not yet implemented',
+        `You just tried to write a file larger than 2GB in ${tlf}. This limitation will be removed soon.`
       ])
-    case enums.kbfs.FSErrorType.userNotFound:
+    } else if (notification.feature === '512kbDirLimit') {
       return ([
-        'Keybase: User not found',
-        `${notification.username} is not a Keybase user`
+        'Keybase: Not yet implemented',
+        `You just tried to write too many files into ${tlf}. This limitation will be removed soon.`
       ])
-    case enums.kbfs.FSErrorType.revokedDataDetected:
+    } else {
       return ([
-        'Keybase: Possibly revoked data detected',
-        `${tlf} was modified by a revoked or bad device. Use 'keybase log send' to file an issue with the Keybase admins.`
+        'Keybase: Not yet implemented',
+        `You just hit a ${notification.feature} limitation in KBFS. It will be fixed soon.`
       ])
-    case enums.kbfs.FSErrorType.notLoggedIn:
-      return ([
-        `Keybase: Permission denied in ${tlf}`,
-        "You are not logged into Keybase. Try 'keybase login'."
-      ])
-    case enums.kbfs.FSErrorType.timeout:
-      return ([
-        `Keybase: ${_.capitalize(notification.params.mode)} timeout in ${tlf}`,
-        `The ${notification.params.mode} operation took too long and failed. Please run 'keybase log send' so our admins can review.`
-      ])
-    case enums.kbfs.FSErrorType.rekeyNeeded:
-      if (notification.rekeyself) {
-        return ([
-          'Keybase: Files need to be rekeyed',
-          `Please open one of your other computers to unlock ${tlf}`
-        ])
-      } else {
-        return ([
-          'Keybase: Friends needed',
-          `Please ask another member of ${tlf} to open Keybase on one of their computers to unlock it for you.`
-        ])
-      }
-    case enums.kbfs.FSErrorType.badFolder:
-      return ([
-        'Keybase: Bad folder',
-        `${tlf} is not a Keybase folder. All folders begin with /keybase/private or /keybase/public.`
-      ])
-    case enums.kbfs.FSErrorType.notImplemented:
-      if (notification.feature === '2gbFileLimit') {
-        return ([
-          'Keybase: Not yet implemented',
-          `You just tried to write a file larger than 2GB in ${tlf}. This limitation will be removed soon.`
-        ])
-      } else if (notification.feature === '512kbDirLimit') {
-        return ([
-          'Keybase: Not yet implemented',
-          `You just tried to write too many files into ${tlf}. This limitation will be removed soon.`
-        ])
-      } else {
-        return ([
-          'Keybase: Not yet implemented',
-          `You just hit a ${notification.feature} limitation in KBFS. It will be fixed soon.`
-        ])
-      }
-    case enums.kbfs.FSErrorType.oldVersion:
-      return ([
-        'Keybase: Old version found',
-        'Please upgrade your Keybase app!'
-      ])
-    default:
-      return ([
-        'Keybase: KBFS error',
-        `${notification.status}`
-      ])
+    }
+  } else {
+    return ([
+      'Keybase: KBFS error',
+      `${notification.status}`
+    ])
   }
 }
