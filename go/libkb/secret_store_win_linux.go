38185683701071ac8f6c7e67bade8ca9c72409d2
Patrick Crosby <pcrosby@gmail.com>
2016-04-11 12:18:26 -0500

    Add SecretStoreAll for windows, linux

---
+++ go/libkb/secret_store_win_linux.go
@@ -0,0 +1,10 @@
+// Copyright 2015 Keybase, Inc. All rights reserved. Use of
+// this source code is governed by the included BSD license.
+
+// +build !darwin,!android
+
+package libkb
+
+func NewSecretStoreAll(g *GlobalContext) SecretStoreAll {
+	return NewSecretStoreFile(g.Env.GetDataDir())
+}
