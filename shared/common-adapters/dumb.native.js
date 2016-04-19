cfe1dfd66b83dabdde59e86e3c94f35a7c744426
Marco Munizaga <marco@marcopolo.io>
2016-04-19 15:49:49 -0700

    Type the dumb checkbox components

--- shared/common-adapters/dumb.native.js
+++ shared/common-adapters/dumb.native.js
@@ -1,27 +1,38 @@
+// @flow
 import Checkbox from './checkbox'
 
-export default {
-  'Checkbox': {
-    component: Checkbox,
-    mocks: {
-      'Normal - checked': {
-        label: 'Normal - checked',
-        checked: true
-      },
-      'Normal - unchecked': {
-        label: 'Normal - unchecked',
-        checked: false
-      },
-      'Disabled - checked': {
-        label: 'Disabled - checked',
-        disabled: true,
-        checked: true
-      },
-      'Disabled - unchecked': {
-        label: 'Disabled - unchecked',
-        disabled: true,
-        checked: false
-      }
+import type {DumbComponentMap} from '../constants/types/more'
+
+const onCheck = () => console.log('checkbox:onCheck')
+
+const checkboxMap: DumbComponentMap<Checkbox> = {
+  component: Checkbox,
+  mocks: {
+    'Normal - checked': {
+      label: 'Normal - checked',
+      checked: true,
+      onCheck
+    },
+    'Normal - unchecked': {
+      label: 'Normal - unchecked',
+      checked: false,
+      onCheck
+    },
+    'Disabled - checked': {
+      label: 'Disabled - checked',
+      disabled: true,
+      checked: true,
+      onCheck
+    },
+    'Disabled - unchecked': {
+      label: 'Disabled - unchecked',
+      disabled: true,
+      checked: false,
+      onCheck
     }
   }
+}
+
+export default {
+  'Checkbox': checkboxMap
 }
