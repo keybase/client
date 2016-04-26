fcf16da719b9a5aebe20fce0f73ad76991708abd
Maxwell Krohn <themax@gmail.com>
2016-04-26 09:37:49 -0400

    blast these private warnings into space

--- go/engine/login_provision.go
+++ go/engine/login_provision.go
@@ -114,7 +114,7 @@ func (e *loginProvision) deviceWithType(ctx *Context, provisionerType keybase1.D
 	if err != nil {
 		return err
 	}
-	e.G().Log.Debug("secret phrase: %s", secret.Phrase())
+	e.G().Log.Debug("secret phrase received")
 
 	// make a new device:
 	deviceID, err := libkb.NewDeviceID()
