7d2114150a77002eadd2a17751115e147679a7f2
chrisnojima <chrisnojima@gmail.com>
2016-04-26 16:06:57 -0400

    device revoke screens (#2699)
    * device revoke screens
    
    * device revoke page on mobile (#2700)
    
    * device revoke page on mobile
    
    * remove logging
    
    * typing dumb and removing dumb filter
    
    * typing dumb and removing dumb filter

---
+++ shared/devices/device-revoke/dumb.desktop.js
@@ -0,0 +1,29 @@
+// @flow
+import Render from './index.render'
+import type {DumbComponentMap} from '../../constants/types/more'
+
+const common = {
+  type: 'desktop',
+  name: 'Home Computer',
+  isCurrent: false,
+  onSubmit: () => { console.log('device revoke on submit') },
+  onCancel: () => { console.log('device revoke on cancel') }
+}
+
+const map: DumbComponentMap<Render> = {
+  component: Render,
+  mocks: {
+    'Normal': {
+      ...common,
+      type: 'mobile'
+    },
+    'Current': {
+      ...common,
+      isCurrent: true
+    }
+  }
+}
+
+export default {
+  'Device Revoke': map
+}
