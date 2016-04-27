8938367aba9998b89bdbfaad087a2b9ec72b29a1
Chris Ball <chris@printf.net>
2016-04-27 11:41:51 -0400

    more plumbing

--- shared/devices/index.render.desktop.js
+++ shared/devices/index.render.desktop.js
@@ -1,34 +1,41 @@
 /* @flow */
-import React from 'react'
-import {Box, Text, Icon, Button} from '../common-adapters'
+import React, {Component} from 'react'
+import {Box, Text, Icon} from '../common-adapters'
 import {globalStyles, globalColors} from '../styles/style-guide'
 import type {Props as IconProps} from '../common-adapters/icon'
-
-import moment from 'moment'
-import commonStyles from '../styles/common'
 
 import type {Props} from './index'
 import type {Device as DeviceType} from '../constants/types/flow-types'
 
-const DeviceHeader = () => {
-  return (
-    <Box style={stylesCommonRow}>
-      <Box style={stylesCommonColumn}>
-        <Icon type='devices-add-s'/>
-      </Box>
-      <Box style={stylesCommonColumn}>
-        <Text type='BodyPrimaryLink'>Add new...</Text>
-      </Box>
-    </Box>
-  )
-}
+class RevokedHeader extends Component {
+  state: {
+    expanded: boolean
+  };
 
-const RevokedHeader = () => {
-  return (
-    <Box style={stylesRevokedRow}>
-      <Text type='BodySemibold'>Revoked devices</Text>
-    </Box>
-  )
+  constructor (props) {
+    super(props)
+    this.state = {
+      expanded: false
+    }
+    this.toggleHeader = this.toggleHeader.bind(this)
+  }
+
+  toggleHeader () {
+    this.setState({expanded: !this.state.expanded})
+  }
+
+  render () {
+    const iconType = this.state.expanded ? 'fa-caret-down' : 'fa-caret-up'
+    return (
+      <Box>
+        <Box style={stylesRevokedRow} onClick={this.toggleHeader}>
+          <Text type='BodySemibold'>Revoked devices</Text>
+          <Icon type={iconType} style={{padding: 5}}/>
+        </Box>
+        {this.state.expanded && this.props.children}
+      </Box>
+    )
+  }
 }
 
 const RevokedDescription = () => {
@@ -39,7 +46,29 @@ const RevokedDescription = () => {
   )
 }
 
-const DeviceRow = ({device, revoked}) => {
+const RevokedDevices = revokedDevices => {
+  return (
+    <RevokedHeader>
+      <RevokedDescription/>
+      {revokedDevices.map(device => DeviceRow({device, revoked: true, showRemoveDevicePage: () => {}, showExistingDevicePage: () => {}}))}
+    </RevokedHeader>
+  )
+}
+
+const DeviceHeader = addNewDevice => {
+  return (
+    <Box style={stylesCommonRow}>
+      <Box style={stylesCommonColumn}>
+        <Icon type='devices-add-s'/>
+      </Box>
+      <Box style={stylesCommonColumn}>
+        <Text type='BodyPrimaryLink' onClick={addNewDevice}>Add new...</Text>
+      </Box>
+    </Box>
+  )
+}
+
+const DeviceRow = ({device, revoked, showRemoveDevicePage, showExistingDevicePage}) => {
   const icon: IconProps.type = {
     'mobile': 'phone-bw-m',
     'desktop': 'computer-bw-s-2',
@@ -54,8 +83,9 @@ const DeviceRow = ({device, revoked}) => {
       textDecoration: 'line-through'
     }
   }
+
   return (
-    <Box id={device.name} style={{...stylesCommonRow, backgroundColor: revoked ? '#f0f0f0' : globalColors.white}}>
+    <Box key={device.name} style={{...stylesCommonRow, backgroundColor: revoked ? globalColors.lightGrey : globalColors.white}} onClick={showExistingDevicePage}>
       <Box style={revoked ? stylesRevokedIconColumn : stylesIconColumn}>
         <Icon type={icon}/>
       </Box>
@@ -67,21 +97,19 @@ const DeviceRow = ({device, revoked}) => {
           {device.isCurrent && <Text type='BodySmall'>Current device</Text>}
         </Box>
       </Box>
-      <Box style={{...stylesCommonColumn, alignSelf: 'center', flex: 1, textAlign: 'right', paddingRight: 20}}>
+      <Box style={{...stylesRevokedColumn}}>
         {!revoked && <Text style={{color: globalColors.red}} type='BodyPrimaryLink'>Revoke</Text>}
       </Box>
     </Box>
   )
 }
 
-const Render = ({devices, revokedDevices, waitingForServer, showRemoveDevicePage, showExistingDevicePage, showGenPaperKeyPage}: Props) => {
+const Render = ({devices, revokedDevices, waitingForServer, addNewDevice, showRemoveDevicePage, showExistingDevicePage}: Props) => {
   return (
     <Box style={stylesContainer}>
-      <DeviceHeader/>
-      {devices && devices.map(device => DeviceRow({device, revoked: false}))}
-      {revokedDevices && <RevokedHeader/>}
-      {revokedDevices && <RevokedDescription/>}
-      {revokedDevices && revokedDevices.map(device => DeviceRow({device, revoked: true}))}
+      {DeviceHeader(addNewDevice)}
+      {devices && devices.map(device => DeviceRow({device, revoked: false, showRemoveDevicePage, showExistingDevicePage}))}
+      {revokedDevices && RevokedDevices(revokedDevices)}
     </Box>
   )
 }
@@ -103,21 +131,29 @@ const stylesRevokedRow = {
   ...stylesCommonRow,
   height: 30,
   justifyContent: 'flex-start',
-  backgroundColor: '#f0f0f0'
+  backgroundColor: globalColors.lightGrey
 }
 
 const stylesRevokedDescription = {
   ...stylesCommonRow,
-  backgroundColor: '#f0f0f0'
+  backgroundColor: globalColors.lightGrey
 }
 
 const stylesCommonColumn = {
   padding: 20
 }
 
+const stylesRevokedColumn = {
+  ...stylesCommonColumn,
+  alignSelf: 'center',
+  flex: 1,
+  textAlign: 'right',
+  paddingRight: 20
+}
+
 const stylesIconColumn = {
   ...stylesCommonColumn,
-  width: 85
+  width: 65
 }
 
 const stylesRevokedIconColumn = {
