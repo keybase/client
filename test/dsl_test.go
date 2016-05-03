5a00af4393a3c36890c93d7e8715b5fc4f7f6b76
Frederick Akalin <akalin@keyba.se>
2016-05-03 15:48:37 -0700

    Add comment

--- test/dsl_test.go
+++ test/dsl_test.go
@@ -127,7 +127,9 @@ func users(ns ...username) optionOp {
 			o.usernames = append(o.usernames, username)
 			a = append(a, string(username))
 		}
+		// Default to the TLF shared by all the users.
 		o.tlfName = strings.Join(a, ",")
+		o.tlfIsPublic = false
 	}
 }
 
