6aa08fc24e8348024e6207858a5db510e29a5a4e
Mike Maxim <mike.maxim@gmail.com>
2016-05-06 14:17:01 -0400

    rpc log refactor (#2779)
    * vendor msgpack
    * use msgpack lib logger opts

--- go/libkb/rpc_log.go
+++ go/libkb/rpc_log.go
@@ -5,69 +5,9 @@ package libkb
 
 import (
 	"net"
-	"sync"
 
 	rpc "github.com/keybase/go-framed-msgpack-rpc"
 )
-
-// RPC log options, can turn on debugging, &c.
-
-type RPCLogOptions struct {
-	Contextified
-	clientTrace    bool
-	serverTrace    bool
-	profile        bool
-	verboseTrace   bool
-	connectionInfo bool
-	noAddress      bool
-}
-
-func (r *RPCLogOptions) Reload() {
-	s := r.G().Env.GetLocalRPCDebug()
-	r.clientTrace = false
-	r.serverTrace = false
-	r.profile = false
-	r.verboseTrace = false
-	r.connectionInfo = false
-	r.noAddress = false
-	for _, c := range s {
-		switch c {
-		case 'A':
-			r.noAddress = true
-		case 'c':
-			r.clientTrace = true
-		case 's':
-			r.serverTrace = true
-		case 'v':
-			r.verboseTrace = true
-		case 'i':
-			r.connectionInfo = true
-		case 'p':
-			r.profile = true
-		default:
-			r.G().Log.Warning("Unknown local RPC logging flag: %c", c)
-		}
-	}
-}
-
-func (r *RPCLogOptions) ShowAddress() bool    { return !r.noAddress }
-func (r *RPCLogOptions) ShowArg() bool        { return r.verboseTrace }
-func (r *RPCLogOptions) ShowResult() bool     { return r.verboseTrace }
-func (r *RPCLogOptions) Profile() bool        { return r.profile }
-func (r *RPCLogOptions) ClientTrace() bool    { return r.clientTrace }
-func (r *RPCLogOptions) ServerTrace() bool    { return r.serverTrace }
-func (r *RPCLogOptions) TransportStart() bool { return r.connectionInfo || r.G().Service }
-
-var rpcLogOptions *RPCLogOptions
-var rpcLogOptionsOnce sync.Once
-
-func getRPCLogOptions(g *GlobalContext) *RPCLogOptions {
-	rpcLogOptionsOnce.Do(func() {
-		rpcLogOptions = &RPCLogOptions{Contextified: NewContextified(g)}
-		rpcLogOptions.Reload()
-	})
-	return rpcLogOptions
-}
 
 type RPCLogFactory struct {
 	Contextified
@@ -78,7 +18,8 @@ func NewRPCLogFactory(g *GlobalContext) *RPCLogFactory {
 }
 
 func (r *RPCLogFactory) NewLog(a net.Addr) rpc.LogInterface {
-	ret := rpc.SimpleLog{Addr: a, Out: r.G().GetUnforwardedLogger(), Opts: getRPCLogOptions(r.G())}
+	opts := rpc.NewStandardLogOptions(r.G().Env.GetLocalRPCDebug(), r.G().Log)
+	ret := rpc.SimpleLog{Addr: a, Out: r.G().GetUnforwardedLogger(), Opts: opts}
 	ret.TransportStart()
 	return ret
 }
