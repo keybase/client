68958502e0ed6041f6befd9d0d2e979d231095b3
chrisnojima <cnojima@keyba.se>
2016-05-13 15:53:17 -0400

    updating colors to sheet

--- shared/styles/style-guide-colors.js
+++ shared/styles/style-guide-colors.js
@@ -1,51 +1,38 @@
 // @flow
-
 export default {
-  // Keybase Brand Colors
-  blue: '#33a0ff',
+  black: '#000000',
+  black_10: 'rgba(0, 0, 0, 0.10)',
+  black_20: 'rgba(0, 0, 0, 0.20)',
+  black_40: 'rgba(0, 0, 0, 0.40)',
+  black_60: 'rgba(0, 0, 0, 0.60)',
+  black_75: 'rgba(0, 0, 0, 0.75)',
   blue2: '#66b8ff',
   blue3: '#a8d7ff',
-  blue4: '#e6f3ff',
-
   blue3_40: 'rgba(168, 215, 255, 0.4)',
-
-  orange: '#ff6f21',
-
-  yellow: '#fff75a',
-
-  darkBlue: '#195080',
+  blue4: '#e6f3ff',
+  blue: '#33a0ff',
+  brown_60: 'rgba(71, 31, 17, 0.6)',
   darkBlue2: '#2470b3',
   darkBlue3: '#0a3052',
   darkBlue4: '#103c64',
-
-  midnightBlue: '#082640',
-
-  green: '#3dcc8e',
+  darkBlue: '#195080',
   green2: '#36b37c',
-
-  red: '#ff4d61',
-
-  yellowGreen: '#a8cf36',
-  yellowGreen2: '#89a82c',
-
-  lightGrey: '#f0f0f0',
-  lightGrey2: '#e6e6e6',
+  green3: '#e5f6ef',
+  green: '#3dcc8e',
   grey: '#cccccc',
-
+  lightGrey2: '#e6e6e6',
+  lightGrey: '#f0f0f0',
+  midnightBlue: '#082640',
+  orange: '#ff6f21',
+  red: '#ff4d61',
+  transparent: 'rgba(0, 0, 0, 0)',
   white: '#ffffff',
-  white_90: 'rgba(255, 255, 255, 0.90)',
-  white_75: 'rgba(255, 255, 255, 0.75)',
   white_40: 'rgba(255, 255, 255, 0.40)',
-
-  black: '#000000',
-  black_75: 'rgba(0, 0, 0, 0.75)',
-  black_60: 'rgba(0, 0, 0, 0.60)',
-  black_40: 'rgba(0, 0, 0, 0.40)',
-  black_20: 'rgba(0, 0, 0, 0.20)',
-  black_10: 'rgba(0, 0, 0, 0.10)',
-
-  brown_60: 'rgba(71, 31, 17, 0.6)',
-
-  transparent: 'rgba(0, 0, 0, 0)'
+  white_75: 'rgba(255, 255, 255, 0.75)',
+  white_90: 'rgba(255, 255, 255, 0.90)',
+  yellow: '#fff75a',
+  yellowGreen2: '#94b52f',
+  yellowGreen3: '#d2e697',
+  yellowGreen: '#a8cf36'
 }
 
