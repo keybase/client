1d49d4238de0a173cee48af0b31ba462033cd432
Jeremy Stribling <strib@alum.mit.edu>
2016-05-20 13:53:11 -0700

    vendor: update libkb for NotFoundError
    Issue: KBFS-1070

--- vendor/github.com/keybase/client/go/libkb/interfaces.go
+++ vendor/github.com/keybase/client/go/libkb/interfaces.go
@@ -285,6 +285,7 @@ type IdentifyUI interface {
 	ReportTrackToken(keybase1.TrackToken) error
 	Finish()
 	DisplayTLFCreateWithInvite(keybase1.DisplayTLFCreateWithInviteArg) error
+	Dismiss(string, keybase1.DismissReason)
 }
 
 type Checker struct {
