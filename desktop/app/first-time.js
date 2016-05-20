e9e5ac0291caa9722ef2b7ca0209ac01e5f2e6e2
Marco Munizaga <marco@marcopolo.io>
2016-05-20 13:27:34 -0700

    Popup login screen the first time only (#2853)
    * Popup login screen the first time only
    
    * Simply return promise

---
+++ desktop/app/first-time.js
@@ -0,0 +1,28 @@
+// @flow
+// Checks to see if we've launched before (we touch started.txt). If not, write the file and open a browser
+import {splashRoot} from '../shared/constants/platform.native.desktop'
+import path from 'path'
+import fs from 'fs'
+
+const filePath = path.join(splashRoot, 'started.txt')
+
+let isFirstTime: Promise<boolean> = new Promise((resolve, reject) => {
+  fs.stat(filePath, (err, stats) => {
+    if (err) {
+      try {
+        fs.writeFileSync(filePath, 'This file gets created on first run of the app')
+      } catch (e) {
+        console.error(`Couldn't touch startup.txt file: ${e}`)
+        // if we cant' write this file, let's not assume this is the first time, maybe something weird is going on
+        resolve(false)
+        return
+      }
+      resolve(true)
+      return
+    }
+
+    resolve(false)
+  })
+})
+
+export default isFirstTime
