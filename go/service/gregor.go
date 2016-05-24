c99b46ed8e7c19b131892d9a0124d6044e7993f8
Patrick Crosby <pcrosby@gmail.com>
2016-05-24 14:10:13 -0500

    Comment

--- go/service/gregor.go
+++ go/service/gregor.go
@@ -569,6 +569,8 @@ func (g *gregorHandler) handleRekeyNeeded(ctx context.Context, item gregor.Item)
 		return g.DismissItem(item.Metadata().MsgID())
 	}
 
+	// show a rekey status window
+
 	return errors.New("rekey not implemented")
 }
 
