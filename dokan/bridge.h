e7a22033b8c4ac4b77f03af82bda23e63733a94a
Jeremy Stribling <strib@alum.mit.edu>
2016-05-25 16:10:25 -0700

    Fix up the copyright year.
    Issue: KBFS-1074

--- dokan/bridge.h
+++ dokan/bridge.h
@@ -1,4 +1,4 @@
-// Copyright 2015 Keybase Inc. All rights reserved.
+// Copyright 2016 Keybase Inc. All rights reserved.
 // Use of this source code is governed by a BSD
 // license that can be found in the LICENSE file.
 
