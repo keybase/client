ae3450a215c6f8cf57b39cd6dd07726e29ac97cc
chrisnojima <cnojima@keyba.se>
2016-05-26 11:09:36 -0400

    remove store dumping in action logger

--- shared/store/action-logger.js
+++ shared/store/action-logger.js
@@ -1,27 +1,6 @@
-import Immutable from 'immutable'
-
-// Transform objects from Immutable on printing
-const objToJS = state => {
-  var newState = {}
-
-  Object.keys(state).forEach(i => {
-    if (Immutable.Iterable.isIterable(state[i])) {
-      newState[i] = state[i].toJS()
-    } else {
-      newState[i] = state[i]
-    }
-  })
-
-  return newState
-}
-
 export const actionLogger = store => next => action => {
   console.groupCollapsed && console.groupCollapsed(`Dispatching action: ${action.type}`)
-
   console.log(`Dispatching action: ${action.type}: ${JSON.stringify(action)} `)
-  let result = next(action)
-
-  console.log('Next state:', JSON.stringify(objToJS(store.getState())))
   console.groupEnd && console.groupEnd()
-  return result
+  return next(action)
 }
