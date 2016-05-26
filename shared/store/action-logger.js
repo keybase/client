399706563ed97168994d231f779c6ffc6a1fc4a9
chrisnojima <cnojima@keyba.se>
2016-05-26 15:36:36 -0400

    aggregating log output

--- shared/store/action-logger.js
+++ shared/store/action-logger.js
@@ -1,6 +1,41 @@
+import Immutable from 'immutable'
+import jsondiffpatch from 'jsondiffpatch'
+
+// Transform objects from Immutable on printing
+const objToJS = state => {
+  var newState = {}
+
+  Object.keys(state).forEach(i => {
+    if (Immutable.Iterable.isIterable(state[i])) {
+      newState[i] = state[i].toJS()
+    } else {
+      newState[i] = state[i]
+    }
+  })
+
+  return newState
+}
+
+let logs = []
+let startedLoop = false
+
 export const actionLogger = store => next => action => {
-  console.groupCollapsed && console.groupCollapsed(`Dispatching action: ${action.type}`)
-  console.log(`Dispatching action: ${action.type}: ${JSON.stringify(action)} `)
-  console.groupEnd && console.groupEnd()
-  return next(action)
+  if (!startedLoop) {
+    startedLoop = true
+    setInterval(() => {
+      if (!logs.length) return
+      console.groupCollapsed && console.groupCollapsed('Actions: ' + logs.length)
+      console.log(logs.join('\n'))
+      logs = []
+      console.groupEnd && console.groupEnd()
+    }, 2000)
+  }
+  const old = objToJS(store.getState())
+
+  let result = next(action)
+
+  logs.push('Dispatching action: ' + action.type + ': ', JSON.stringify(action) + '\n' +
+    'State diff: ' + JSON.stringify(jsondiffpatch.diff(old, objToJS(store.getState()))))
+
+  return result
 }
