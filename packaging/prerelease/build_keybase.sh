522ebfc142e8b28da8f8ca2a2892c5ff6c557f63
Gabriel Handford <gabrielh@gmail.com>
2016-05-31 15:05:26 -0700

    Fix packaging scripts to work on other platforms (#2929)
    These fixes allow these scripts to work under windows.

--- packaging/prerelease/build_keybase.sh
+++ packaging/prerelease/build_keybase.sh
@@ -5,9 +5,9 @@ set -e -u -o pipefail # Fail on error
 dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd "$dir"
 
-build_dir=${BUILD_DIR:-/tmp/build_keybase}
+build_dir=${BUILD_DIR:-/tmp/keybase}
 
-mkdir -p $build_dir
+mkdir -p "$build_dir"
 
 current_date=`date -u +%Y%m%d%H%M%S` # UTC
 commit_short=`git log -1 --pretty=format:%h`
@@ -22,17 +22,19 @@ if [ "$PLATFORM" = "darwin" ]; then
 fi
 
 echo "Building $build_dir/keybase ($keybase_build)"
-GO15VENDOREXPERIMENT=1 go build -a -tags "$tags" -ldflags "$ldflags" -o $build_dir/keybase github.com/keybase/client/go/keybase
+GO15VENDOREXPERIMENT=1 go build -a -tags "$tags" -ldflags "$ldflags" -o "$build_dir/keybase" "github.com/keybase/client/go/keybase"
 
 if [ "$PLATFORM" = "darwin" ]; then
   code_sign_identity="Developer ID Application: Keybase, Inc. (99229SGT5K)"
-  codesign --verbose --force --deep --sign "$code_sign_identity" $build_dir/keybase
+  codesign --verbose --force --deep --sign "$code_sign_identity" "$build_dir/keybase"
 elif [ "$PLATFORM" = "linux" ]; then
   echo "No codesigning for linux"
+elif [ "$PLATFORM" = "windows" ]; then
+  echo "No codesigning for windows"
 else
   echo "Invalid PLATFORM"
   exit 1
 fi
 
-version=`$build_dir/keybase version -S`
+version=`"$build_dir"/keybase version -S`
 echo "Keybase version: $version"
--- packaging/prerelease/build_updater.sh
+++ packaging/prerelease/build_updater.sh
@@ -5,15 +5,15 @@ set -e -u -o pipefail # Fail on error
 dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd "$dir"
 
-build_dir=${BUILD_DIR:-/tmp/build_updater}
+build_dir=${BUILD_DIR:-/tmp/keybase}
 gopath=${GOPATH:-}
 package="github.com/keybase/go-updater/service"
 dest="$build_dir/updater"
 
 src_dir="$gopath/src/$package"
-cd $src_dir
+cd "$src_dir"
 
-mkdir -p $build_dir
+mkdir -p "$build_dir"
 
 ldflags=""
 
@@ -30,10 +30,12 @@ if [ "$PLATFORM" = "darwin" ]; then
   codesign --verbose --force --deep --sign "$code_sign_identity" "$dest"
 elif [ "$PLATFORM" = "linux" ]; then
   echo "No codesigning for linux"
+elif [ "$PLATFORM" = "windows" ]; then
+  echo "No codesigning for windows"
 else
   echo "Invalid PLATFORM"
   exit 1
 fi
 
-updater_version=`$build_dir/updater -version`
+updater_version=`"$build_dir"/updater -version`
 echo "Updater version: $updater_version"
