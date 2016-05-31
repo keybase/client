dc072262cad59d2ba107b93630e1459ba29025d8
chrisnojima <chrisnojima@gmail.com>
2016-05-31 16:17:43 -0400

    add menu to logged out view (#2928)
    * add menu to logged out view
    
    * dont push down logo

--- shared/menubar/index.render.desktop.js
+++ shared/menubar/index.render.desktop.js
@@ -31,11 +31,23 @@ class Render extends Component<void, Props, State> {
   _renderLoggedOut () {
     const styles = stylesPublic
 
+    const menuColor = this.state.showingMenu ? globalColors.black : globalColors.black_40
+    const menuStyle = {...globalStyles.clickable, color: menuColor, hoverColor: menuColor, fontSize: 12}
+
     return (
-      <Box style={{...styles.container, justifyContent: 'center'}}>
-        <Icon type='logo-128' style={stylesLogo} />
-        <Text type='Body' small style={{alignSelf: 'center', marginTop: 6}}>You're logged out of Keybase!</Text>
-        <Button type='Primary' label='Log In' onClick={this.props.logIn} style={{alignSelf: 'center', minWidth: 160, marginTop: 12}} />
+      <Box style={{...styles.container}}>
+        <Box style={{...stylesTopRow, position: 'absolute'}}>
+          <Icon
+            style={menuStyle}
+            type='fa-custom-icon-hamburger'
+            onClick={() => this.setState({showingMenu: !this.state.showingMenu})} />
+        </Box>
+        <Box style={{...globalStyles.flexBoxColumn, flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <Icon type='logo-128' style={stylesLogo} />
+          <Text type='Body' small style={{alignSelf: 'center', marginTop: 6}}>You're logged out of Keybase!</Text>
+          <Button type='Primary' label='Log In' onClick={this.props.logIn} style={{alignSelf: 'center', minWidth: 160, marginTop: 12}} />
+        </Box>
+        <PopupMenu visible={this.state.showingMenu} items={this._menuItems()} onHidden={() => this.setState({showingMenu: false})} />
       </Box>
     )
   }
