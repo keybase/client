15c4a45dd3d4cd79491465493cdc7d0fc8456b4a
Alex Wendland <me@alexwendland.com>
2016-06-02 15:23:08 -0700

    Add dumb component for the Intro screen

--- shared/login/dumb.desktop.js
+++ shared/login/dumb.desktop.js
@@ -1,6 +1,8 @@
 // @flow
 import LoginError, {Mocks as loginErrorMocks} from './error.render'
+import IntroMap from './forms/dumb'
 
 export default {
-  'Login: Error': {component: LoginError, mocks: loginErrorMocks}
+  'Login: Error': {component: LoginError, mocks: loginErrorMocks},
+  ...IntroMap
 }
---
+++ shared/login/forms/dumb.desktop.js
@@ -0,0 +1,24 @@
+// @flow
+import Intro from './intro.render'
+
+import type IntroProps from './intro';
+
+const props: IntroProps = {
+  onSignup: () => {},
+  onLogin: () => {},
+  justRevokedSelf: null
+}
+
+export const dumbMap: DumbComponentMap<Intro> = {
+  component: Intro,
+  mocks: {
+    'First time user': props,
+    'User who just revoked device': Object.assign({}, props, {
+      justRevokedSelf: 'DEVICE_NAME'
+    })
+  }
+}
+
+export default {
+  'Intro': dumbMap
+}
