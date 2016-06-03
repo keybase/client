b86ba5a1060435d47c8347e6977034a0afb39900
chrisnojima <cnojima@keyba.se>
2016-06-03 13:28:08 -0400

    plumbing through most of badging for new tlfs

--- shared/folders/row.desktop.js
+++ shared/folders/row.desktop.js
@@ -33,7 +33,7 @@ const RowMeta = ({ignored, meta, styles}) => {
   }
 
   const metaBGColors = {
-    'new': globalColors.blue2,
+    'new': globalColors.orange,
     'rekey': globalColors.red
   }
 
