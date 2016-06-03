b1b57c91bd8fa258ca295858e7de5e445478a55b
Gabriel Handford <gabrielh@gmail.com>
2016-06-02 17:02:58 -0700

    Fix path

--- packaging/windows/doinstaller_gui.cmd
+++ packaging/windows/doinstaller_gui.cmd
@@ -104,7 +104,7 @@ IF %ERRORLEVEL% NEQ 0 (
 if NOT DEFINED JSON_UPDATE_FILENAME set JSON_UPDATE_FILENAME=update-windows-prod.json
 
 :: Run keybase sign to get signature
-set KeybaseBin=c:\Program Files (x86)\Keybase\keybase.exe
+set KeybaseBin="c:\Program Files (x86)\Keybase\keybase.exe"
 set SigFile=sig.txt
 %KeybaseBin% sign -d -i %KEYBASE_INSTALLER_NAME% -o %SigFile%
 IF %ERRORLEVEL% NEQ 0 (
