054730e7c1dafcf30abad492487eabf3e179dda1
John Zila <john@jzila.com>
2016-06-03 16:13:39 -0700

    Fix certificate environment (#23)
    Signed-off-by: John Zila <john@keyba.se>

--- kbfsfuse/kbfsfuse.sh
+++ kbfsfuse/kbfsfuse.sh
@@ -8,6 +8,10 @@ _term() {
 }
 trap _term SIGTERM
 
+if [ -z "$KEYBASE_TEST_ROOT_CERT_PEM" ]; then
+    export KEYBASE_TEST_ROOT_CERT_PEM="$(echo $KEYBASE_TEST_ROOT_CERT_PEM_B64 | base64 -d)";
+fi
+
 keybase service &
 SERVICE=$!
 kbfsfuse -debug -mdserver $MDSERVER_ADDR -bserver $BSERVER_ADDR /keybase &
