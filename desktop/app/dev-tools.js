751630261567c250a57a579e408904d73f98c066
chrisnojima <cnojima@keyba.se>
2016-06-09 10:37:45 -0400

    updating electron
    using mainline purepack
    
    fixing remote window problems. pointing to new branch of framed-msgpack-rpc which has less code and is off of master. adding dummy msgPack so we can reduce the size of our framed-msgpack-rpc branch and hopefully get onto master soon
    
    adding devtron
    
    adding shrinkwrap
    
    adding external devtools
    
    pointint to new pr for msgpack. fixing flex issues w/ new electron
    
    point to mainline node-framed-msgpack-rpc in github. next step is to publish it on npm

--- desktop/app/dev-tools.js
+++ desktop/app/dev-tools.js
@@ -1,17 +1,36 @@
 import {BrowserWindow, app, globalShortcut} from 'electron'
 import {showDevTools} from '../shared/local-debug.desktop'
 
+function setupDevToolsExtensions () {
+  if (__DEV__ && process.env.KEYBASE_LOCAL_DEBUG) {
+    if (process.env.KEYBASE_DEV_TOOL_EXTENSIONS) {
+      process.env.KEYBASE_DEV_TOOL_EXTENSIONS.split(',').forEach(p => {
+        BrowserWindow.addDevToolsExtension(p)
+      })
+    }
+  }
+}
+
+function setupOpenDevtools () {
+  globalShortcut.register('CommandOrControl+Alt+k+b', () => {
+    BrowserWindow.getAllWindows().map(bw => {
+      bw.openDevTools()
+    })
+  })
+}
+
+function cleanupOpenDevtools () {
+  globalShortcut.unregister('CommandOrControl+Alt+k+b')
+}
+
 export default function () {
   app.on('ready', () => {
-    globalShortcut.register('CommandOrControl+Alt+k+b', () => {
-      BrowserWindow.getAllWindows().map(bw => {
-        bw.openDevTools()
-      })
-    })
+    setupDevToolsExtensions()
+    setupOpenDevtools()
   })
 
   app.on('will-quit', () => {
-    globalShortcut.unregister('CommandOrControl+Alt+k+b')
+    cleanupOpenDevtools()
   })
 
   if (!showDevTools) {
