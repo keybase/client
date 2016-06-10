8fc97718fdefc26b0b4e00a2cad7bb63e1b62630
chrisnojima <chrisnojima@gmail.com>
2016-06-10 11:09:35 -0400

    move rate limiting to notify popup itself. add popup for new tlfs (#3032)
    * move rate limiting to notify popup itself. add popup for new tlfs
    
    * remove console log
    
    * don't overwrite body
    
    * remove null

--- shared/native/notifications.desktop.js
+++ shared/native/notifications.desktop.js
@@ -1,5 +1,18 @@
 /* @flow */
+import moment from 'moment'
 
-export function NotifyPopup (title: string, opts: ?Object): void {
+const rateLimit: {[key: string]: Object} = {}
+
+export function NotifyPopup (title: string, opts: ?Object, rateLimitSeconds: number = -1): void {
+  if (rateLimitSeconds > 0) {
+    const now = moment()
+    if (rateLimit[title] && now.isBefore(rateLimit[title])) {
+      console.log(`Skipping notify for ${title} due to rate limit`)
+      return
+    }
+
+    rateLimit[title] = now.add(rateLimitSeconds, 's')
+  }
+
   new Notification(title, {...opts, silent: true}) //eslint-disable-line
 }
