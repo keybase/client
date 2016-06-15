70725e5a860956ebe098c1549c61e9a5e43acd5f
Chris Ball <chris@printf.net>
2016-06-15 12:08:15 -0400

    Remove unneeded import

--- shared/constants/favorite.js
+++ shared/constants/favorite.js
@@ -2,7 +2,6 @@
 
 import type {TypedAction} from '../constants/types/flux'
 import type {Props as FolderProps} from '../folders/render'
-import type {Folder} from '../constants/types/flow-types'
 
 export const favoriteList = 'favorite:favoriteList'
 export type FavoriteList = TypedAction<'favorite:favoriteList', {folders: FolderProps}, void>
