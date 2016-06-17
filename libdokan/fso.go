e540b74ff7107d0a8216749cc03a924bf15bd671
Taru Karttunen <taruti@taruti.net>
2016-06-17 21:52:57 +0100

    libdokan: More debug on SetFileTime

--- libdokan/fso.go
+++ libdokan/fso.go
@@ -28,6 +28,7 @@ type FSO struct {
 func (f *FSO) SetFileTime(fi *dokan.FileInfo, creation time.Time, lastAccess time.Time, lastWrite time.Time) (err error) {
 	ctx, cancel := NewContextWithOpID(f.folder.fs, "FSO SetFileTime")
 	defer func() { f.folder.reportErr(ctx, libkbfs.WriteMode, err, cancel) }()
+	f.folder.fs.log.CDebugf(ctx, "FSO SetFileTime %v %v %v", creation, lastAccess, lastWrite)
 
 	if !lastWrite.IsZero() {
 		return f.folder.fs.config.KBFSOps().SetMtime(ctx, f.node, &lastWrite)
