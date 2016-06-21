13729624bb945813b5eec1b26062f3adb24ecc35
Patrick Crosby <pcrosby@gmail.com>
2016-06-21 10:56:07 -0500

    Test PGPProvision engine

--- go/engine/pgpprovision_test.go
+++ go/engine/pgpprovision_test.go
@@ -14,56 +14,29 @@ func TestPGPProvision(t *testing.T) {
 	tc := SetupEngineTest(t, "pgpprov")
 	defer tc.Cleanup()
 
-	u1 := createFakeUserWithPGPPubOnly(t, tc)
+	// create a user w/ a synced pgp key
+	u1 := createFakeUserWithPGPOnly(t, tc)
+	t.Log("Created fake user")
 	Logout(tc)
 
 	// redo SetupEngineTest to get a new home directory...should look like a new device.
 	tc2 := SetupEngineTest(t, "pgpprov")
 	defer tc2.Cleanup()
 
-	// we need the gpg keyring that's in the first homedir
-	if err := tc.MoveGpgKeyringTo(tc2); err != nil {
-		t.Fatal(err)
-	}
-
-	// now safe to cleanup first home
-	tc.Cleanup()
-
 	// run PGPProvision on new device
 	ctx := &Context{
 		LoginUI:  &libkb.TestLoginUI{Username: u1.Username},
-		SecretUI: u1.NewSecretUI(),
+		SecretUI: &libkb.TestSecretUI{},
+		LogUI:    tc2.G.UI.GetLogUI(),
 	}
-	eng := NewPGPProvision(tc2.G, u1.Username, "new device", "xxx")
+	eng := NewPGPProvision(tc2.G, u1.Username, "new device", u1.Passphrase)
 	if err := RunEngine(eng, ctx); err != nil {
 		t.Fatal(err)
 	}
-	/*
-		ctx := &Context{
-			ProvisionUI: newTestProvisionUIGPGImport(),
-			LogUI:       tc2.G.UI.GetLogUI(),
-			SecretUI:    u1.NewSecretUI(),
-			LoginUI:     &libkb.TestLoginUI{Username: u1.Username},
-			GPGUI:       &gpgtestui{},
-		}
-		eng := NewLogin(tc2.G, libkb.DeviceTypeDesktop, "", keybase1.ClientType_CLI)
-		if err := RunEngine(eng, ctx); err != nil {
-			t.Fatal(err)
-		}
-	*/
 
 	testUserHasDeviceKey(tc2)
 
-	// highly possible they didn't have a paper key, so make sure they have one now:
-	hasOnePaperDev(tc2, u1)
-
 	if err := AssertProvisioned(tc2); err != nil {
-		t.Fatal(err)
-	}
-
-	// since they imported their pgp key, they should be able to pgp sign something:
-	if err := signString(tc2, "sign me", u1.NewSecretUI()); err != nil {
-		t.Error("pgp sign failed after gpg provision w/ import")
 		t.Fatal(err)
 	}
 }
