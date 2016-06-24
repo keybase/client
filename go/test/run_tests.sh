af22071a6eca1ea4ba2179688389496ad1adf665
Gabriel Handford <gabrielh@gmail.com>
2016-06-24 12:13:36 -0700

    Skip loopback

--- go/test/run_tests.sh
+++ go/test/run_tests.sh
@@ -5,8 +5,11 @@ DIRS=$(go list ./... | grep -v /vendor/ | sed -e 's/^github.com\/keybase\/client
 
 export KEYBASE_LOG_SETUPTEST_FUNCS=1
 
-for i in $DIRS
-do
+for i in $DIRS; do
+	if [ "$i" = "loopback" ]; then
+		echo "Skipping loopback"
+	fi
+
 	echo -n "$i......."
 	(cd $i && go test -timeout 50m)
 done
