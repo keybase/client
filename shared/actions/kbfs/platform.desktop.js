526b18df6e05c808c3753a5f47065a48a5eac4a5
chrisnojima <cnojima@keyba.se>
2016-06-28 12:48:09 -0400

    fixing open logic

--- shared/actions/kbfs/platform.desktop.js
+++ shared/actions/kbfs/platform.desktop.js
@@ -32,30 +32,33 @@ export function openInKBFS (path: string = Constants.defaultKBFSPath): AsyncActi
 
     // On windows the path isn't /keybase
     // We can figure it out by looking at the extendedConfig though
-    if (process.platform === 'win32' && kbfsPath === Constants.defaultKBFSPath) {
-      const extendedConfig = Promise.resolve(state.config.extendedConfig)
+    if (process.platform === 'win32') {
+      if (kbfsPath === Constants.defaultKBFSPath) {
+        const extendedConfig = Promise.resolve(state.config.extendedConfig)
 
-      extendedConfig.then(extendedConfig => {
-        const kbfsClients = extendedConfig.Clients.filter(c => c.clientType === Common.ClientType.kbfs)
-        if (kbfsClients.length !== 1) {
-          return Promise.reject("There isn't exactly one kbfs client")
-        }
+        extendedConfig.then(extendedConfig => {
+          const kbfsClients = extendedConfig.Clients.filter(c => c.clientType === Common.ClientType.kbfs)
+          if (kbfsClients.length !== 1) {
+            return Promise.reject("There isn't exactly one kbfs client")
+          }
 
-        // Hacky Regex to find a mount point on windows matches anything like foobar:\ or K:\
-        const kbfsPath = kbfsClients[0].argv.filter(arg => arg.search(/.*:\\?$/) === 0)[0]
+          // Hacky Regex to find a mount point on windows matches anything like foobar:\ or K:\
+          const kbfsPath = kbfsClients[0].argv.filter(arg => arg.search(/.*:\\?$/) === 0)[0]
 
-        if (!kbfsPath) {
-          return Promise.reject("Couldn't figure out kbfs path from argv")
-        }
+          if (!kbfsPath) {
+            return Promise.reject("Couldn't figure out kbfs path from argv")
+          }
 
-        return Promise.resolve(kbfsPath + '\\')
-      }).then(kbfsPath => {
-        dispatch({type: Constants.changeKBFSPath, payload: {path: kbfsPath}})
+          return Promise.resolve(kbfsPath + '\\')
+        }).then(kbfsPath => {
+          dispatch({type: Constants.changeKBFSPath, payload: {path: kbfsPath}})
+          open(kbfsPath, path)
+        }).catch(e => {
+          console.warn('Error in parsing kbfsPath: ', e)
+        })
+      } else {
         open(kbfsPath, path)
-        shell.openItem(`${kbfsPath}${path}`)
-      }).catch(e => {
-        console.warn('Error in parsing kbfsPath: ', e)
-      })
+      }
     } else {
       open(Constants.defaultKBFSPath, path)
     }
