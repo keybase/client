c4a423b504ecbdcafa3d20a611fa29ff10d61bed
Gabriel Handford <gabrielh@gmail.com>
2016-06-30 13:24:51 -0700

    Clarify exec

--- desktop/app/exec.js
+++ desktop/app/exec.js
@@ -4,29 +4,34 @@ import os from 'os'
 
 import {runMode} from '../shared/constants/platform.native.desktop'
 
+// Execute at path with args.
+// If you specify platformOnly or runModeOnly, then callback will be called
+// without error if we don't match that environment.
+// Callback is optional and accepts (error, boolean), where boolean is if we
+// attempted to execute.
 export default function (path, args, platformOnly, runModeOnly, callback) {
   const platform = os.platform()
   if (platformOnly && platform !== platformOnly) {
     console.log('Exec (%s) not available for platform: %s != %s', path, platformOnly, platform)
-    callback(null)
+    if (callback) callback(null, false)
     return
   }
   if (!path) {
     console.log('Exec path not available:', path)
-    callback(null)
+    if (callback) callback(null, false)
     return
   }
   if (runModeOnly && runMode !== runModeOnly) {
     // Only run in prod
     console.log('Exec path not available for this run mode: %s != %s', runModeOnly, runMode)
-    callback(null)
+    if (callback) callback(null, false)
     return
   }
 
   fs.access(path, fs.X_OK, function (err) {
     if (err) {
       console.log('Exec path not found (or accessible as executable):', path)
-      callback(null)
+      if (callback) callback(null, false)
       return
     }
 
@@ -43,9 +48,7 @@ export default function (path, args, platformOnly, runModeOnly, callback) {
       if (execErr) {
         console.log('Exec (err):', execErr)
       }
-      if (callback) {
-        callback(execErr)
-      }
+      if (callback) callback(execErr, true)
     })
 
     // Kill the process if parent process exits
