b39fcdb906fbc94dce578b14f8c36dcdb3f3307b
John Zila <john@keyba.se>
2016-07-06 11:08:21 -0700

    Echo client and kbfs revisions if they exist
    Signed-off-by: John Zila <john@keyba.se>

--- kbfsfuse/kbfsfuse.sh
+++ kbfsfuse/kbfsfuse.sh
@@ -12,6 +12,13 @@ if [ -z "$KEYBASE_TEST_ROOT_CERT_PEM" ]; then
     export KEYBASE_TEST_ROOT_CERT_PEM="$(echo $KEYBASE_TEST_ROOT_CERT_PEM_B64 | base64 -d)";
 fi
 
+if [ -f kbfs_revision ]; then
+    echo "Running KBFS Docker with KBFS revision $(cat kbfs_revision)"
+fi
+if [ -f client_revision ]; then
+    echo "Client revision $(cat client_revision)"
+fi
+
 keybase service &
 SERVICE=$!
 kbfsfuse -debug -mdserver $MDSERVER_ADDR -bserver $BSERVER_ADDR /keybase &
