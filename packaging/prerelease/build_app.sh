8e1f5e6996f890ac5aabc27cdf7aa2e8a94223e9
Gabriel Handford <gabrielh@gmail.com>
2016-07-06 13:31:36 -0700

    Don't wait for travis on kbfs (it's turned off)

--- packaging/prerelease/build_app.sh
+++ packaging/prerelease/build_app.sh
@@ -95,7 +95,7 @@ if [ ! "$nowait" = "1" ]; then
   echo "Checking client CI"
   "$release_bin" wait-ci --repo="client" --commit=`git -C $client_dir log -1 --pretty=format:%h` --context="client-windows-master-only" --context="client-linux-master-only" --context="client-osx-master-only" --context="ci/circleci"
   echo "Checking kbfs CI"
-  "$release_bin" wait-ci --repo="kbfs" --commit=`git -C $kbfs_dir log -1 --pretty=format:%h` --context="continuous-integration/travis-ci/push" --context="continuous-integration/appveyor/branch"
+  "$release_bin" wait-ci --repo="kbfs" --commit=`git -C $kbfs_dir log -1 --pretty=format:%h` --context="continuous-integration/appveyor/branch"
   echo "Checking updater CI"
   "$release_bin" wait-ci --repo="go-updater" --commit=`git -C $updater_dir log -1 --pretty=format:%h` --context="continuous-integration/travis-ci/push"
 fi
