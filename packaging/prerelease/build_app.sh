bd949bf3e618254839a744e5524318f048153c2b
John Zila <john@keyba.se>
2016-07-07 12:08:32 -0700

    Add KBFS to release scripts as well
    Signed-off-by: John Zila <john@keyba.se>

--- packaging/prerelease/build_app.sh
+++ packaging/prerelease/build_app.sh
@@ -95,7 +95,7 @@ if [ ! "$nowait" = "1" ]; then
   echo "Checking client CI"
   "$release_bin" wait-ci --repo="client" --commit=`git -C $client_dir log -1 --pretty=format:%h` --context="client/master" --context="ci/circleci"
   echo "Checking kbfs CI"
-  "$release_bin" wait-ci --repo="kbfs" --commit=`git -C $kbfs_dir log -1 --pretty=format:%h` --context="continuous-integration/appveyor/branch"
+  "$release_bin" wait-ci --repo="kbfs" --commit=`git -C $kbfs_dir log -1 --pretty=format:%h` --context="kbfs/master" --context="continuous-integration/appveyor/branch"
   echo "Checking updater CI"
   "$release_bin" wait-ci --repo="go-updater" --commit=`git -C $updater_dir log -1 --pretty=format:%h` --context="continuous-integration/travis-ci/push"
 fi
