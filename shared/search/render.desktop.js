8564e46b40c852b42d1135f26faf78666d288c72
chrisnojima <cnojima@keyba.se>
2016-07-12 18:05:01 -0400

    fixing flow errors

--- shared/search/render.desktop.js
+++ shared/search/render.desktop.js
@@ -9,6 +9,9 @@ import SearchBar from './user-search/search-bar.desktop'
 import UserPane from './user-pane'
 
 import type {Props} from './render'
+import type {Props as UserSearchProps} from './user-search/render'
+import type {Props as UserGroupProps} from './user-search/user-group'
+import type {Props as SearchBarProps} from './user-search/search-bar'
 
 const ClearSearch = ({onReset, showUserGroup}) => (
   <Box style={{...globalStyles.flexBoxColumn, height: 48, justifyContent: 'center', alignItems: 'flex-end', paddingRight: 16}}>
@@ -26,13 +29,17 @@ class Render extends Component<void, Props, void> {
       return this._renderComingSoon()
     }
 
+    const userSearchProps: UserSearchProps = this.props
+    const userGroupProps: UserGroupProps = this.props
+    const searchBarProps: SearchBarProps = this.props
+
     return (
       <Box style={{...globalStyles.flexBoxRow, flex: 1}}>
         <Box style={{...globalStyles.flexBoxColumn, flex: 1}}>
           <ClearSearch {...this.props} />
-          <SearchBar {...this.props} />
+          <SearchBar {...searchBarProps} />
           <Box style={{overflowY: 'auto', height: 'calc(100% - 96px)'}}>
-            {this.props.showUserGroup ? <UserGroup {...this.props} /> : <UserSearch {...this.props} />}
+            {this.props.showUserGroup ? <UserGroup {...userGroupProps} /> : <UserSearch {...userSearchProps} />}
           </Box>
         </Box>
         <Box style={{boxShadow: `0 0 5px ${globalColors.black_20}`, overflow: 'hidden'}}>
