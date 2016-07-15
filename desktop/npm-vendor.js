bcd961152035eb7a543435d6aefc7930268492ab
Max Goodman <c@chromakode.com>
2016-07-15 15:38:22 -0700

    Add environment variable to customize vendored deps path

--- desktop/npm-vendor.js
+++ desktop/npm-vendor.js
@@ -5,6 +5,8 @@ var path = require('path')
 var fs = require('fs')
 var spawnSync = require('child_process').spawnSync
 
+var VENDOR_DIR = process.env.KEYBASE_JS_VENDOR_DIR || './js-vendor-desktop'
+
 function ensureSymlink (target, dest) {
   if (fs.existsSync(target)) {
     console.log('Removing existing', target)
@@ -32,18 +34,18 @@ function installVendored () {
   var url = parts[0]
   var commit = parts[1]
 
-  if (!fs.existsSync('./js-vendor-desktop')) {
-    spawn('git', ['clone', url, 'js-vendor-desktop'])
+  if (!fs.existsSync(VENDOR_DIR)) {
+    spawn('git', ['clone', url, VENDOR_DIR])
   }
-  spawn('git', ['fetch', 'origin'], {cwd: './js-vendor-desktop'})
-  spawn('git', ['checkout', '-f', commit], {cwd: './js-vendor-desktop'})
+  spawn('git', ['fetch', 'origin'], {cwd: VENDOR_DIR})
+  spawn('git', ['checkout', '-f', commit], {cwd: VENDOR_DIR})
   console.log(`js-vendor-desktop: ${url} @ ${commit}`)
 
-  ensureSymlink('./npm-shrinkwrap.json', './js-vendor-desktop/npm-shrinkwrap.json')
-  ensureSymlink('./node_shrinkwrap', './js-vendor-desktop/node_shrinkwrap')
+  ensureSymlink('./npm-shrinkwrap.json', path.join(VENDOR_DIR, 'npm-shrinkwrap.json'))
+  ensureSymlink('./node_shrinkwrap', path.join(VENDOR_DIR, 'node_shrinkwrap'))
   spawn(os.platform() === 'win32' ? 'npm.cmd' : 'npm', ['install', '--loglevel=http', '--no-optional'], {
     env: Object.assign({}, process.env, {
-      ELECTRON_CACHE: path.resolve('./js-vendor-desktop/electron'),
+      ELECTRON_CACHE: path.resolve(path.join(VENDOR_DIR, 'electron')),
       ELECTRON_ONLY_CACHE: 1,
     }),
   })
