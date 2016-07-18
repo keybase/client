896f44a8771857af55b565393028c794da9f7a84
chrisnojima <cnojima@keyba.se>
2016-07-18 17:45:54 -0400

    dumb desktop implemented

---
+++ shared/common-adapters/platform-icon.desktop.js
@@ -0,0 +1,56 @@
+/* @flow */
+
+import React from 'react'
+import {Box, Icon} from '../common-adapters'
+import {globalColors} from '../styles/style-guide'
+import type {Platforms} from '../constants/types/more'
+import type {IconType} from './icon'
+import type {Props} from './platform-icon'
+
+const supportedPlatformsAndSizes: {[key: Platforms | 'btc']: ?{[key: string]: ?IconType}} = {
+  'coinbase': {
+    '48': 'icon-coinbase-logo-48',
+  },
+  'twitter': {
+    '48': 'icon-twitter-logo-48',
+  },
+  'github': {
+    '48': 'icon-github-logo-48',
+  },
+  'reddit': {
+    '48': 'icon-reddit-logo-48',
+  },
+  'hackernews': {
+    '48': 'icon-hacker-news-logo-48',
+  },
+  'genericWebSite': {
+    '48': 'icon-website-48',
+  },
+  'dns': {
+    '48': 'icon-website-48',
+  },
+  'btc': {
+    '48': 'icon-bitcoin-logo-48',
+  },
+}
+
+const Render = ({platform, overlay, overlayColor, size}: Props) => {
+  const icon: ?IconType = supportedPlatformsAndSizes[platform] && supportedPlatformsAndSizes[platform][String(size)]
+  if (!icon) {
+    console.warn('unsupported platform + size: ', platform, size)
+    return null
+  }
+
+  // TODO switch with new assets from cecile. This is temp ( you can see through the checkmarks etc). remove white underlay
+  return (
+    <Box style={{position: 'relative'}}>
+      <Icon type={icon} />
+      <Box style={{position: 'absolute', bottom: 0, right: 0}}>
+        <Icon type={overlay} style={{position: 'absolute', bottom: -3, right: -3, color: globalColors.white, fontSize: 30}} />
+        <Icon type={overlay} style={{color: overlayColor}} />
+      </Box>
+    </Box>
+  )
+}
+
+export default Render
--- shared/profile/confirm-or-pending.desktop.js
+++ shared/profile/confirm-or-pending.desktop.js
@@ -1,13 +1,25 @@
 /* @flow */
 
 import React from 'react'
-import {Box} from '../common-adapters'
+import {Box, Text, Button, PlatformIcon} from '../common-adapters'
+import {globalStyles, globalColors, globalMargins} from '../styles/style-guide'
 import type {Props} from './confirm-or-pending'
 
-const Render = ({platform}: Props) => {
+const Render = ({platform, title, titleColor, platformIcon, platformIconOverlay, platformIconOverlayColor,
+  username, usernameSubtitle, message, messageSubtitle, onReloadProfile}: Props) => {
   return (
-    <Box>
-      <p>{platform}</p>
+    <Box style={{...globalStyles.flexBoxColumn, flex: 1}}>
+      <Box style={{...globalStyles.flexBoxColumn, justifyContent: 'center', alignItems: 'center', backgroundColor: titleColor, height: globalMargins.large}}>
+        <Text backgroundMode='Terminal' type='BodySmallSemibold'>{title}</Text>
+      </Box>
+      <Box style={{...globalStyles.flexBoxColumn, alignItems: 'center', padding: globalMargins.large}}>
+        <PlatformIcon platform={platform} overlay={platformIconOverlay} overlayColor={platformIconOverlayColor} size={48} />
+        <Text type='Header' style={{color: globalColors.blue}}>{username}</Text>
+        {!!usernameSubtitle && <Text type='Body' style={{color: globalColors.black_10}}>{usernameSubtitle}</Text>}
+        <Text type='Body' style={{marginTop: globalMargins.xlarge, textAlign: 'center'}}>{message}</Text>
+        {!!messageSubtitle && <Text type='BodySmall' style={{textAlign: 'center'}}>{messageSubtitle}</Text>}
+        <Button type='Primary' onClick={onReloadProfile} label='Reload profile' style={{marginTop: globalMargins.medium}} />
+      </Box>
     </Box>
   )
 }
