ed8dd37b734484f2a75bdd9c901228b8ce94d0bb
Gabriel Handford <gabrielh@gmail.com>
2016-07-19 10:50:10 -0700

    Fix os version parse for macOS Sierra (#3464)

--- go/test/run_tests.sh
+++ go/test/run_tests.sh
@@ -7,6 +7,10 @@ DIRS=$(go list ./... | grep -v /vendor/ | sed -e 's/^github.com\/keybase\/client
 
 export KEYBASE_LOG_SETUPTEST_FUNCS=1
 
+# Add libraries used in testing
+go get "github.com/stretchr/testify/require"
+go get "github.com/stretchr/testify/assert"
+
 for i in $DIRS; do
 	if [ "$i" = "bind" ]; then
 		echo "Skipping bind"
