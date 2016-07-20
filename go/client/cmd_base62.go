4c51ec5c17db571d2f0abb3a51445836c52ebcd3
Song Gao <song@gao.io>
2016-07-20 14:19:32 -0700

    add encoder to `keybase base62`

--- go/client/cmd_base62.go
+++ go/client/cmd_base62.go
@@ -5,13 +5,25 @@ package client
 
 import (
 	"errors"
+	"io"
+	"os"
+
 	"github.com/keybase/cli"
 	"github.com/keybase/client/go/libcmdline"
 	"github.com/keybase/client/go/libkb"
 	"github.com/keybase/saltpack/encoding/basex"
-	"io"
-	"os"
 )
+
+func NewCmdBase62(cl *libcmdline.CommandLine, g *libkb.GlobalContext) cli.Command {
+	return cli.Command{
+		Name:  "base62",
+		Usage: "base62 operations",
+		Subcommands: []cli.Command{
+			NewCmdBase62Decode(cl, g),
+			NewCmdBase62Encode(cl, g),
+		},
+	}
+}
 
 type CmdBase62Decode struct {
 	libkb.Contextified
@@ -20,16 +32,6 @@ type CmdBase62Decode struct {
 func NewBase62DecodeRunner(g *libkb.GlobalContext) *CmdBase62Decode {
 	return &CmdBase62Decode{
 		Contextified: libkb.NewContextified(g),
-	}
-}
-
-func NewCmdBase62(cl *libcmdline.CommandLine, g *libkb.GlobalContext) cli.Command {
-	return cli.Command{
-		Name:  "base62",
-		Usage: "base62 operations",
-		Subcommands: []cli.Command{
-			NewCmdBase62Decode(cl, g),
-		},
 	}
 }
 
@@ -59,5 +61,49 @@ func (v *CmdBase62Decode) Run() error {
 }
 
 func (v *CmdBase62Decode) GetUsage() libkb.Usage {
+	return libkb.Usage{}
+}
+
+type CmdBase62Encode struct {
+	libkb.Contextified
+}
+
+func NewBase62EncodeRunner(g *libkb.GlobalContext) *CmdBase62Encode {
+	return &CmdBase62Encode{
+		Contextified: libkb.NewContextified(g),
+	}
+}
+
+func NewCmdBase62Encode(cl *libcmdline.CommandLine, g *libkb.GlobalContext) cli.Command {
+	return cli.Command{
+		Name:  "encode",
+		Usage: "base62 encode",
+		Flags: []cli.Flag{},
+		Action: func(c *cli.Context) {
+			cl.ChooseCommand(NewBase62EncodeRunner(g), "encode", c)
+			cl.SetForkCmd(libcmdline.NoFork)
+			cl.SetLogForward(libcmdline.LogForwardNone)
+		},
+	}
+}
+
+func (v *CmdBase62Encode) ParseArgv(ctx *cli.Context) error {
+	if len(ctx.Args()) != 0 {
+		return errors.New("No arguments allowed")
+	}
+	return nil
+}
+
+func (v *CmdBase62Encode) Run() error {
+	enc := basex.NewEncoder(basex.Base62StdEncoding, os.Stdout)
+	_, err := io.Copy(enc, os.Stdin)
+	if err != nil {
+		return err
+	}
+	err = enc.Close()
+	return err
+}
+
+func (v *CmdBase62Encode) GetUsage() libkb.Usage {
 	return libkb.Usage{}
 }
