5dd356901ea6cc8a00671502eb1e333950f746b9
Max Goodman <c@chromakode.com>
2016-07-21 13:40:59 -0700

    Set up React Hot Loader and cleanup app init code
    This reorganizes the reloadable pieces into a new container component.
    It pulls init-related code out of the nav and into the desktop init. In
    addition, the startup `bootstrap` RPC is moved out of the Nav component,
    so that it fires once on init, not during hot reloads.

--- desktop/renderer/index.js
+++ desktop/renderer/index.js
@@ -3,131 +3,100 @@
  * The main renderer. Holds the global store. When it changes we send it to the main thread which then sends it out to subscribers
  */
 
-import React, {Component} from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
-import {Provider} from 'react-redux'
+import {AppContainer} from 'react-hot-loader'
 import configureStore from '../shared/store/configure-store'
-import Nav from '../shared/nav.desktop'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 import ListenLogUi from '../shared/native/listen-log-ui'
-import {reduxDevToolsEnable, devStoreChangingFunctions, setup as setupLocalDebug} from '../shared/local-debug.desktop'
+import {devStoreChangingFunctions} from '../shared/local-debug.desktop'
 import {listenForNotifications} from '../shared/actions/notifications'
+import {bootstrap} from '../shared/actions/config'
 import hello from '../shared/util/hello'
 
+import Root from './container'
 import {devEditAction} from '../shared/reducers/dev-edit'
 import {setupContextMenu} from '../app/menu-helper'
 
 // For Remote Components
 import electron, {ipcRenderer} from 'electron'
-import RemoteManager from './remote-manager'
 import {ipcLogsRenderer} from '../app/console-helper'
 import loadPerf from '../shared/util/load-perf'
 import merge from 'lodash/merge'
-import {MuiThemeProvider} from 'material-ui/styles'
-import materialTheme from '../shared/styles/material-theme.desktop'
 
-ipcLogsRenderer()
+function setupApp (store) {
+  ipcLogsRenderer()
 
-if (module.hot) {
-  module.hot.accept()
+  loadPerf()
+
+  if (devStoreChangingFunctions) {
+    window.devEdit = (path, value) => store.dispatch(devEditAction(path, value))
+  }
+
+  if (__DEV__ && process.env.KEYBASE_LOCAL_DEBUG) {
+    require('devtron').install()
+  }
+
+  setupContextMenu(electron.remote.getCurrentWindow())
+
+  // Used by material-ui widgets.
+  if (module.hot) {
+    // Don't reload this thing if we're hot reloading
+    if (module.hot.data === undefined) {
+      injectTapEventPlugin()
+    }
+  } else {
+    injectTapEventPlugin()
+  }
+
+  ipcRenderer.on('dispatchAction', (event, action) => {
+    // we MUST convert this else we'll run into issues with redux. See https://github.com/rackt/redux/issues/830
+    // This is because this is touched due to the remote proxying. We get a __proto__ which causes the _.isPlainObject check to fail. We use
+    // _.merge() to get a plain object back out which we can send
+    setImmediate(() => {
+      try {
+        store.dispatch(merge({}, action))
+      } catch (_) {
+      }
+    })
+  })
+
+  store.subscribe(() => {
+    ipcRenderer.send('stateChange', store.getState())
+  })
+
+  // Handle notifications from the service
+  store.dispatch(listenForNotifications())
+
+  // Handle logUi.log
+  ListenLogUi()
+
+  // Introduce ourselves to the service
+  hello(process.pid, 'Main Renderer', process.argv, __VERSION__) // eslint-disable-line no-undef
+
+  store.dispatch(bootstrap())
+  // Restartup when we connect online.
+  // If you startup while offline, you'll stay in an errored state
+  window.addEventListener('online', () => store.dispatch(bootstrap()))
 }
 
 const store = configureStore()
 
-setupLocalDebug(store)
+setupApp(store)
 
-if (devStoreChangingFunctions) {
-  window.devEdit = (path, value) => store.dispatch(devEditAction(path, value))
-}
+const appEl = document.getElementById('app')
 
-class Keybase extends Component {
-  state: {
-    panelShowing: boolean
-  };
+ReactDOM.render(
+  <AppContainer><Root store={store} /></AppContainer>,
+  appEl,
+)
 
-  constructor () {
-    super()
-
-    loadPerf()
-
-    this.state = {
-      panelShowing: false,
-    }
-
-    if (__DEV__) { // eslint-disable-line no-undef
-      if (typeof window !== 'undefined') {
-        window.addEventListener('keydown', event => {
-          if (event.ctrlKey && event.keyCode === 72) {
-            this.setState({panelShowing: !this.state.panelShowing})
-          }
-        })
-      }
-    }
-
-    setupContextMenu(electron.remote.getCurrentWindow())
-
-    // Used by material-ui widgets.
-    if (module.hot) {
-      // Don't reload this thing if we're hot reloading
-      if (module.hot.data === undefined) {
-        injectTapEventPlugin()
-      }
-    } else {
-      injectTapEventPlugin()
-    }
-
-    this.setupDispatchAction()
-    this.setupStoreSubscriptions()
-
-    // Handle notifications from the service
-    store.dispatch(listenForNotifications())
-
-    // Handle logUi.log
-    ListenLogUi()
-
-    // Introduce ourselves to the service
-    hello(process.pid, 'Main Renderer', process.argv, __VERSION__) // eslint-disable-line no-undef
-  }
-
-  setupDispatchAction () {
-    ipcRenderer.on('dispatchAction', (event, action) => {
-      // we MUST convert this else we'll run into issues with redux. See https://github.com/rackt/redux/issues/830
-      // This is because this is touched due to the remote proxying. We get a __proto__ which causes the _.isPlainObject check to fail. We use
-      // _.merge() to get a plain object back out which we can send
-      setImmediate(() => {
-        try {
-          store.dispatch(merge({}, action))
-        } catch (_) {
-        }
-      })
-    })
-  }
-
-  setupStoreSubscriptions () {
-    store.subscribe(() => {
-      ipcRenderer.send('stateChange', store.getState())
-    })
-  }
-
-  render () {
-    let dt = null
-    if (__DEV__ && reduxDevToolsEnable) { // eslint-disable-line no-undef
-      const DevTools = require('./redux-dev-tools').default
-      dt = <DevTools />
-    }
-
-    return (
-      <MuiThemeProvider muiTheme={materialTheme}>
-        <Provider store={store}>
-          <div style={{display: 'flex', flex: 1}}>
-            <RemoteManager />
-            <Nav />
-            {dt}
-          </div>
-        </Provider>
-      </MuiThemeProvider>
+if (module.hot) {
+  module.hot.accept('./container', () => {
+    const NewRoot = require('./container').default
+    ReactDOM.render(
+      <AppContainer><NewRoot store={store} /></AppContainer>,
+      appEl,
     )
-  }
+  })
 }
-
-ReactDOM.render(<Keybase />, document.getElementById('app'))
--- desktop/webpack.config.development.js
+++ desktop/webpack.config.development.js
@@ -34,6 +34,8 @@ config.plugins.push(
 )
 
 if (getenv.boolish('HOT', false)) {
+  config.entry.index = ['react-hot-loader/patch'].concat(config.entry.index)
+
   const HMR = 'webpack-hot-middleware/client?path=http://localhost:4000/__webpack_hmr'
 
   Object.keys(config.entry).forEach(k => {
