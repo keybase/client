91f17aab56187b2e18f40cf22ee62a6384b7e600
Marco Munizaga <marco@marcopolo.io>
2016-07-20 17:26:11 -0700

    Add edit avatar dumb component

---
+++ shared/profile/edit-avatar.desktop.js
@@ -0,0 +1,24 @@
+/* @flow */
+
+import React from 'react'
+import {Avatar, Box, Text, Button} from '../common-adapters'
+import {globalStyles, globalMargins} from '../styles/style-guide'
+
+import type {Props} from './edit-avatar'
+
+const Render = ({keybaseUsername, hasAvatar, onAck}: Props) => {
+  const text = !hasAvatar ? 'Keybase shows your profile photo from your Twitter or Github proofs. Please add a proof to your profile.'
+    : "For now, Keybase shows your profile photo from your Twitter or Github proofs. If you'd like to prioritize one service, you can change that setting on the Keybase website."
+
+  return (
+    <Box style={{...globalStyles.flexBoxColumn, flex: 1}}>
+      <Box style={{...globalStyles.flexBoxColumn, alignItems: 'center', padding: globalMargins.large}}>
+        <Avatar size={176} username={keybaseUsername} />
+        <Text type='Body' style={{marginTop: globalMargins.medium, textAlign: 'center'}}>{text}</Text>
+        <Button type='Primary' onClick={onAck} label='Got it!' style={{marginTop: globalMargins.medium}} />
+      </Box>
+    </Box>
+  )
+}
+
+export default Render
---
+++ shared/profile/edit-avatar.native.js
@@ -0,0 +1,12 @@
+/* @flow */
+
+import React from 'react'
+import {Text} from '../common-adapters'
+
+import type {Props} from './edit-avatar'
+
+const Render = (_: Props) => {
+  return <Text type='Body'>TODO: edit avatar</Text>
+}
+
+export default Render
