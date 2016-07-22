1380161d548399ce8f6def392b6af80a0a177299
Gabriel Handford <gabrielh@gmail.com>
2016-07-22 12:48:17 -0700

    Log helper tool output to file, so we can log send

---
+++ osx/Helper/KBLogger.h
@@ -0,0 +1,11 @@
+//
+//  KBLogger.h
+//  Keybase
+//
+//  Created by Gabriel on 7/22/16.
+//  Copyright © 2016 Keybase. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+void KBLog(NSString *msg, ...);
