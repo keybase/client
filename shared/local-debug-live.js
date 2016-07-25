91f926798d434029b3afb174415b237629e99ae7
chrisnojima <cnojima@keyba.se>
2016-07-25 17:11:04 -0400

    load dumb filter from env

--- shared/local-debug-live.js
+++ shared/local-debug-live.js
@@ -1,4 +1,15 @@
-export const dumbFilter = ''
+// @flow
+let envJson = {}
+if (process.env.KEYBASE_LOCAL_DEBUG_JSON) {
+  try {
+    envJson = JSON.parse(process.env.KEYBASE_LOCAL_DEBUG_JSON)
+  } catch (e) {
+    envJson = {}
+    console.warn('Invalid KEYBASE_LOCAL_DEBUG_JSON:', e)
+  }
+}
+
+export const dumbFilter = envJson.dumbFilter || ''
 
 // the following only apply to mobile:
 export const dumbIndex = 0
