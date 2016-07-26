32c0c6468c3b3eb56d0f5e4ad09305f911bafb44
Alex Wendland <me@alexwendland.com>
2016-07-26 15:01:27 -0700

    Add explicit type for support provable platforms

--- shared/constants/types/more.js
+++ shared/constants/types/more.js
@@ -5,6 +5,8 @@ import {proveCommon} from './keybase-v1'
 import type {Device as _Device, DeviceID, Time} from './flow-types'
 
 export type Platforms = $Keys<typeof proveCommon.ProofType>
+export type ProvablePlatforms = 'twitter' | 'reddit' | 'github' | 'coinbase' | 'hackernews' | 'dns' | 'genericWebSite'
+
 export type DeviceType = 'mobile' | 'desktop' | 'backup'
 export type $Exact<X> = $Shape<X> & X
 
