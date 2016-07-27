d43a4e485291624d39b0b2a94410dfcbb06ac1ff
Miles Steele <miles@milessteele.com>
2016-07-27 14:25:04 -0400

    Update test to use anon struct

--- go/libkb/identity_test.go
+++ go/libkb/identity_test.go
@@ -8,37 +8,42 @@ import (
 )
 
 func TestParseIdentity(t *testing.T) {
-	for i, identLine := range inputIdentLines {
-		expected := expectedIdentities[i]
-		identity, err := ParseIdentity(identLine)
-		if err != nil {
-			t.Errorf("error parsing identity: %s", err)
-		} else {
-			if *identity != expected {
-				t.Errorf("identity differs from expected\n%s\n%s", identity, expected)
-			}
+	for _, idents := range sampleIdentities {
+		checkIdentity(t, idents.input, idents.expected)
+	}
+}
+
+func checkIdentity(t *testing.T, input string, expected Identity) {
+	identity, err := ParseIdentity(input)
+	if err != nil {
+		t.Errorf("error parsing identity: %s", err)
+	} else {
+		if *identity != expected {
+			t.Errorf("identity differs from expected\n%s\n%s", identity, expected)
 		}
 	}
 }
 
-var inputIdentLines = [...]string{
-	`Barb Akew <barb@example.com>`,
-	`Barb Akew (bbq) <barb@example.com>`,
-	`"Barb Akew" (bbq) <barb@example.com>`,
-	`Barb Akew (b"b"q) <barb@example.com>`,
-	`"Barb Akew (bbq)" <barb@example.com>`,
-	`x/Barb <barb@example.com>`,
-	`Barb Akew (co<mme>nt)`,
-	`Barb`,
-}
-
-var expectedIdentities = [...]Identity{
-	{"Barb Akew", "", "barb@example.com"},
-	{"Barb Akew", "bbq", "barb@example.com"},
-	{"Barb Akew", "bbq", "barb@example.com"},
-	{"Barb Akew", `b"b"q`, "barb@example.com"},
-	{"Barb Akew", "bbq", "barb@example.com"},
-	{"x/Barb", "", "barb@example.com"},
-	{"Barb Akew", "co<mme>nt", ""},
-	{"Barb", "", ""},
+var sampleIdentities = []struct {
+	input    string
+	expected Identity
+}{
+	{`Barb Akew <barb@example.com>`,
+		Identity{"Barb Akew", "", "barb@example.com"}},
+	{`Barb Akew (bbq) <barb@example.com>`,
+		Identity{"Barb Akew", "bbq", "barb@example.com"}},
+	{`"Barb Akew" (bbq) <barb@example.com>`,
+		Identity{"Barb Akew", "bbq", "barb@example.com"}},
+	{`Barb Akew (b"b"q) <barb@example.com>`,
+		Identity{"Barb Akew", `b"b"q`, "barb@example.com"}},
+	{`"Barb Akew (bbq)" <barb@example.com>`,
+		Identity{"Barb Akew", "bbq", "barb@example.com"}},
+	{`x/Barb <barb@example.com>`,
+		Identity{"x/Barb", "", "barb@example.com"}},
+	{`Barb Akew (co<mme>nt)`,
+		Identity{"Barb Akew", "co<mme>nt", ""}},
+	{`Barb Akew (the "new" key)`,
+		Identity{"Barb Akew", `the "new" key`, ""}},
+	{`Barb`,
+		Identity{"Barb", "", ""}},
 }
