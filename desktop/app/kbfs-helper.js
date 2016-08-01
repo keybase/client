925d033dffa7ae601e35fc69e9836b81386c77fa
chrisnojima <chrisnojima@gmail.com>
2016-08-01 12:36:48 -0400

    Revert "Show item in folder instead of open" (#3690)

--- desktop/app/kbfs-helper.js
+++ desktop/app/kbfs-helper.js
@@ -4,6 +4,7 @@ import {ipcMain, shell} from 'electron'
 
 export default function () {
   ipcMain.on('openInKBFS', (e, path) => {
-    shell.showItemInFolder(path)
+    // We do the `shell.openItem` here to not block the ui thread
+    shell.openItem(path)
   })
 }
