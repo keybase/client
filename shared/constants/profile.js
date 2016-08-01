3a493a4c4efff1b35a6765f29a50b2d2ab27bf8e
chrisnojima <cnojima@keyba.se>
2016-08-01 15:20:28 -0400

    initial profile to help unblock revoke

--- shared/constants/profile.js
+++ shared/constants/profile.js
@@ -1,8 +1,29 @@
-/* @flow */
+// @flow
+import type {PlatformsExpanded} from '../constants/types/more'
+import type {TypedAction} from '../constants/types/flux'
 
-// Actions
-export const editingProfile = 'devices:editingProfile'
-export const editedProfile = 'devices:editedProfile'
+export const editingProfile = 'profile:editingProfile'
+export const editedProfile = 'profile:editedProfile'
 
-// Constants
+export const waiting = 'profile:waiting'
+export type Waiting = TypedAction<'profile:waiting', {waiting: boolean}, void>
+
+export const updatePlatform = 'profile:updatePlatform'
+export type UpdatePlatform = TypedAction<'profile:updatePlatform', {platform: PlatformsExpanded}, void>
+
+export const updateUsername = 'profile:updateUsername'
+export type UpdateUsername = TypedAction<'profile:updateUsername', {username: string}, void>
+
 export const maxProfileBioChars = 256
+
+export type Actions = Waiting
+  | UpdatePlatform
+  | UpdateUsername
+
+export type State = {
+  error: ?string,
+  waiting: boolean,
+  username: string,
+  platform: ?PlatformsExpanded,
+  usernameValid: boolean,
+}
---
+++ shared/reducers/profile.js
@@ -0,0 +1,45 @@
+// @flow
+import * as CommonConstants from '../constants/common'
+import * as Constants from '../constants/profile'
+import type {Actions, State} from '../constants/profile'
+
+const initialState: State = {
+  error: null,
+  waiting: false,
+  username: '',
+  platform: null,
+  usernameValid: true,
+}
+
+export default function (state: State = initialState, action: Actions) {
+  switch (action.type) {
+    case CommonConstants.resetStore:
+      return {...initialState}
+    case Constants.waiting:
+      if (action.error) {
+        break
+      }
+      return {
+        ...state,
+        waiting: action.payload.waiting,
+      }
+    case Constants.updatePlatform:
+      if (action.error) {
+        break
+      }
+      return {
+        ...state,
+        platform: action.payload.platform,
+      }
+    case Constants.updateUsername:
+      if (action.error) {
+        break
+      }
+      return {
+        ...state,
+        username: action.payload.username,
+      }
+  }
+
+  return state
+}
