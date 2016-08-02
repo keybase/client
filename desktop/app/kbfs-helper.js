e41ff3558a85750b165475ff083fed2d184c7ece
Gabriel Handford <gabrielh@gmail.com>
2016-08-02 15:58:47 -0700

    Open directory should focus Finder/Explorer
    This will open files into the Finder/Explorer selected or use
    openExternal to open a directory in Finder/Explorer.
    
    The openItem call in Electron doesn't grab focus.

--- desktop/app/kbfs-helper.js
+++ desktop/app/kbfs-helper.js
@@ -1,10 +1,32 @@
 // @flow
 
 import {ipcMain, shell} from 'electron'
+import fs from 'fs'
+import {pathToURL} from './paths'
 
 export default function () {
   ipcMain.on('openInKBFS', (e, path) => {
-    // We do the `shell.openItem` here to not block the ui thread
-    shell.openItem(path)
+    fs.stat(path, (err, stats) => {
+      if (err) {
+        console.warn('Error opening %s:', path, err)
+        return
+      }
+      if (stats.isFile()) {
+        shell.showItemInFolder(path)
+      } else if (stats.isDirectory()) {
+        // Paths if directories might be symlinks.
+        // For example /keybase/private/gabrielh,chris gets redirected to
+        // /keybase/private/chris,gabrielh.
+        fs.realpath(path, (err, resolvedPath) => {
+          if (err) {
+            console.warn('No realpath for %s:', path, err)
+            return
+          }
+          const url = pathToURL(resolvedPath)
+          console.log('Open URL (directory):', url)
+          shell.openExternal(url)
+        })
+      }
+    })
   })
 }
