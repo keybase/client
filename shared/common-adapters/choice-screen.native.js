d1a5b6853473f5e42fcc8f9470be0a3eab440bdc
Alex Wendland <me@alexwendland.com>
2016-08-04 11:40:23 -0700

    Implement dumb screen for choosing from multiple options

---
+++ shared/common-adapters/choice-screen.native.js
@@ -0,0 +1,11 @@
+// @flow
+import {Component} from 'react'
+import type {Props} from './choice-screen'
+
+class Render extends Component<void, Props, void> {
+  render () {
+    return null
+  }
+}
+
+export default Render
--- shared/common-adapters/dumb.desktop.js
+++ shared/common-adapters/dumb.desktop.js
@@ -4,7 +4,7 @@ import React, {Component} from 'react'
 import _ from 'lodash'
 
 import Checkbox from './checkbox'
-import {Button, Box, TabBar, Text, Avatar, ListItem, PopupMenu, Icon} from './index'
+import {Button, Box, TabBar, Text, Avatar, ListItem, PopupMenu, Icon, ChoiceScreen} from './index'
 import {iconMeta} from './icon.constants'
 import type {IconType} from './icon.constants'
 import {TabBarButton, TabBarItem} from './tab-bar'
@@ -196,6 +196,30 @@ const avatarMap: DumbComponentMap<Avatar> = {
   },
 }
 
+const choiceScreenMap: DumbComponentMap<ChoiceScreen> = {
+  component: ChoiceScreen,
+  mocks: {
+    'Two Choices - Website': {
+      title: 'Prove your website in two ways:',
+      onCancel: () => console.log('ChoiceScreen: onCancel'),
+      options: [
+        {
+          title: 'Host a TXT file',
+          description: 'Host a text file on your site, such as yoursite.com/keybase.txt.',
+          icon: 'icon-file-txt-48',
+          onClick: () => console.log('ChoiceScreen: onClick TXT file'),
+        },
+        {
+          title: 'Set a DNS',
+          description: 'Place a Keybase proof in your DNS records.',
+          icon: 'icon-dns-48',
+          onClick: () => console.log('ChoiceScreen: onClick DNS'),
+        },
+      ],
+    },
+  },
+}
+
 export default {
   Checkbox: checkboxMap,
   TabBar: tabBarMap,
@@ -203,4 +227,5 @@ export default {
   PopupMenu: popupMenuMap,
   Avatar: avatarMap,
   Icon: iconMap,
+  ChoiceScreen: choiceScreenMap,
 }
