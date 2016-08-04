252f677451f7ae2bb2e983cdc2efa6aabf5969d2
Alex Wendland <me@alexwendland.com>
2016-08-04 11:24:06 -0700

    Add standard screen component for general case
    - TODO: add success banner support
    - TODO: add stacked notifications support (zpl.io/1ALH37)
    - TODO: use as base throughout app

---
+++ shared/common-adapters/standard-screen.desktop.js
@@ -0,0 +1,67 @@
+// @flow
+import React from 'react'
+import {Box, Text, Icon} from '../common-adapters'
+import {globalStyles, globalColors, globalMargins} from '../styles/style-guide'
+import type {Props} from './standard-screen'
+
+const defaults = {
+  showClose: true,
+}
+
+const StandardScreen = (props: Props) => {
+  const {children, onClose, showClose, errorMessage} = {...defaults, ...props}
+  return (
+    <Box style={styleContainer}>
+      {!!onClose && showClose && <Icon style={styleClose} type='iconfont-close' onClick={() => onClose()} />}
+      {errorMessage && <Box style={styleErrorBanner}><Text style={styleErrorBannerText} type='BodySmallSemibold'>{errorMessage}</Text></Box>}
+      <Box style={styleContentContainer}>
+        {children}
+      </Box>
+    </Box>
+  )
+}
+
+const styleContainer = {
+  ...globalStyles.flexBoxColumn,
+  flex: 1,
+  alignItems: 'center',
+  position: 'relative',
+  paddingTop: globalMargins.large,
+  paddingBottom: globalMargins.large,
+  ...globalStyles.scrollable,
+}
+
+const styleClose = {
+  position: 'absolute',
+  top: globalMargins.small,
+  right: globalMargins.small,
+  ...globalStyles.clickable,
+  color: globalColors.black_10,
+}
+
+const styleErrorBanner = {
+  ...globalStyles.flexBoxColumn,
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '100%',
+  zIndex: 1,
+  height: globalMargins.large,
+  marginTop: -globalMargins.large,
+  backgroundColor: globalColors.red,
+}
+
+const styleErrorBannerText = {
+  color: globalColors.white,
+}
+
+const styleContentContainer = {
+  ...globalStyles.flexBoxColumn,
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center',
+  margin: globalMargins.large,
+  maxWidth: 512,
+  textAlign: 'center',
+}
+
+export default StandardScreen
---
+++ shared/common-adapters/standard-screen.native.js
@@ -0,0 +1,11 @@
+// @flow
+import {Component} from 'react'
+import type {Props} from './standard-screen'
+
+class StandardScreen extends Component<void, Props, void> {
+  render () {
+    return null
+  }
+}
+
+export default StandardScreen
