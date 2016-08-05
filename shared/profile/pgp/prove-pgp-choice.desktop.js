27198ab0a335d091cbe0fcbdea4d9ee541008288
Alex Wendland <me@alexwendland.com>
2016-08-05 16:57:33 -0700

    Use PgpIcon so that the screen matches design

--- shared/profile/pgp/prove-pgp-choice.desktop.js
+++ shared/profile/pgp/prove-pgp-choice.desktop.js
@@ -2,6 +2,7 @@
 import React, {Component} from 'react'
 import {StandardScreen, ChoiceList, Text, Button} from '../../common-adapters'
 import {globalMargins} from '../../styles/style-guide'
+import PgpIcon from './pgp-icon'
 import type {Props} from './prove-pgp-choice'
 
 class ProvePgpChoice extends Component<void, Props, void> {
@@ -14,13 +15,13 @@ class ProvePgpChoice extends Component<void, Props, void> {
             {
               title: 'Get a new PGP key',
               description: 'Keybase will generate a new PGP key and add it to your profile.',
-              icon: 'icon-pgp-key-48',
+              icon: <PgpIcon type='generate' />,
               onClick: () => this.props.onOptionClick('generate'),
             },
             {
               title: 'I have one already',
               description: 'Import an existing PGP key to your Keybase profile.',
-              icon: 'icon-pgp-key-48',
+              icon: <PgpIcon type='import' />,
               onClick: () => this.props.onOptionClick('import'),
             },
           ]}
