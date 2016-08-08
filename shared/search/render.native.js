a8fd3d8aa960b50a25e7602ddc93b8dff0bb6ac6
Gabriel Handford <gabrielh@gmail.com>
2016-08-08 16:19:32 -0700

    Commenting out test

--- shared/search/render.native.js
+++ shared/search/render.native.js
@@ -1,19 +1,21 @@
 /* @flow */
 import React, {Component} from 'react'
-import {connect} from 'react-redux'
-import {Button} from '../common-adapters'
+// import {connect} from 'react-redux'
+// import {Button} from '../common-adapters'}
+import {ComingSoon} from '../common-adapters'
 import UserSearch from './user-search/render'
 import UserGroup from './user-search/user-group'
 import type {Props} from './render'
 
-import {fsList} from '../actions/kbfs'
+// import {fsList} from '../actions/kbfs'
 
 class Render extends Component<void, Props, void> {
   _renderComingSoon () {
-    // For testing fs listing only, revert before PR merge
-    const Connected = connect(state => ({}), dispatch => ({fsList: () => dispatch(fsList('/keybase/private/gabrielh/'))}))
-      (props => <Button label='List /keybase' onClick={() => props.fsList()} type='Primary' />)
-    return <Connected />
+    // For testing fs listing
+    // const Connected = connect(state => ({}), dispatch => ({fsList: () => dispatch(fsList('/keybase/private/gabrielh/'))}))
+    //   (props => <Button label='List /keybase' onClick={() => props.fsList()} type='Primary' />)
+    // return <Connected />
+    return <ComingSoon />
   }
 
   render () {
