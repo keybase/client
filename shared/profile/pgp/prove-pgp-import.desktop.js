f78957f14d7c1c939c4960bc68fc0f3999debbcd
Chris Ball <chris@printf.net>
2016-08-08 13:59:32 -0400

    eslint

--- shared/profile/pgp/prove-pgp-import.desktop.js
+++ shared/profile/pgp/prove-pgp-import.desktop.js
@@ -1,7 +1,7 @@
 // @flow
 import React, {Component} from 'react'
 import {Button, Terminal, Text, StandardScreen} from '../../common-adapters'
-import {globalColors, globalMargins} from '../../styles/style-guide'
+import {globalMargins} from '../../styles/style-guide'
 import PgpIcon from './pgp-icon'
 import type {Props} from './prove-pgp-choice'
 
@@ -14,13 +14,13 @@ class ProvePgpChoice extends Component<void, Props, void> {
         <Text style={styleBody} type='Body'>To upload your existing PGP key to Keybase, please run the following command from your terminal:</Text>
         <Terminal style={styleTerminal}>
           <Text type='TerminalComment'>import a key from gpg's key chain</Text>
-          <Text type='Terminal'>{`keybase pgp select`}</Text>
+          <Text type='Terminal'>keybase pgp select</Text>
           <Text type='TerminalEmpty' />
           <Text type='TerminalComment'>import from stdin and send the public half to Keybase</Text>
-          <Text type='Terminal'>{`cat privkey.asc | keybase pgp import`}</Text>
+          <Text type='Terminal'>cat privkey.asc | keybase pgp import</Text>
           <Text type='TerminalEmpty' />
           <Text type='TerminalComment'>for more options</Text>
-          <Text type='Terminal'>{`keybase pgp help`}</Text>
+          <Text type='Terminal'>keybase pgp help</Text>
         </Terminal>
         <Button style={styleCancelButton} type='Secondary' onClick={() => this.props.onCancel()} label={'Close'} />
       </StandardScreen>
@@ -43,9 +43,9 @@ const styleCancelButton = {
 
 const styleTerminal = {
   borderRadius: 4,
-  padding: 32,
   width: '100%',
   maxWidth: 576,
+  padding: 32,
 }
 
 export default ProvePgpChoice
