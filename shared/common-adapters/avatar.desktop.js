e1ded8fb5d41ac5a8ffec45ac090357742a38697
chrisnojima <cnojima@keyba.se>
2016-08-09 14:12:29 -0400

    reset loaded state in avatar on new props

--- shared/common-adapters/avatar.desktop.js
+++ shared/common-adapters/avatar.desktop.js
@@ -20,6 +20,15 @@ class Avatar extends Component<void, Props, State> {
     this.state = {avatarLoaded: false, errored: false}
   }
 
+  componentWillReceiveProps (nextProps: Props) {
+    const url = shared.createAvatarUrl(this.props) || noAvatar
+    const nextUrl = shared.createAvatarUrl(nextProps) || noAvatar
+
+    if (url !== nextUrl) {
+      this.setState({avatarLoaded: false, errored: false})
+    }
+  }
+
   render () {
     const {size} = this.props
     const width = size
