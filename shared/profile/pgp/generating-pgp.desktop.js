af32ba70bbf034d78811e997b0c8e35ec631594e
Alex Wendland <me@alexwendland.com>
2016-08-09 14:25:57 -0700

    Add dumb component for PGP Generating screen

---
+++ shared/profile/pgp/generating-pgp.desktop.js
@@ -0,0 +1,38 @@
+// @flow
+import React, {Component} from 'react'
+import {Button, Text, StandardScreen, Icon} from '../../common-adapters'
+import {globalMargins} from '../../styles/style-guide'
+import type {Props} from './generating-pgp'
+
+class GeneratingPgp extends Component<void, Props, void> {
+  render () {
+    return (
+      <StandardScreen onClose={this.props.onCancel} style={styleContainer}>
+        <Icon type='icon-pgp-key-48' />
+        <Text style={styleHeader} type='Header'>Generating your unique key...</Text>
+        <Text style={styleBody} type='BodySmall'>Math time! You are about to discover a <Text type='BodySmallItalic'>4096-bit</Text> key pair.<br />This could take as long as a couple minutes.</Text>
+        <Icon type='icon-loader-infinity-64' />
+        <Button style={styleCancelButton} type='Secondary' onClick={() => this.props.onCancel()} label={'Cancel'} />
+      </StandardScreen>
+    )
+  }
+}
+
+const styleContainer = {
+  maxWidth: 512,
+}
+
+const styleHeader = {
+  marginTop: globalMargins.medium,
+}
+
+const styleBody = {
+  marginTop: globalMargins.small,
+  marginBottom: globalMargins.large,
+}
+
+const styleCancelButton = {
+  marginTop: globalMargins.large,
+}
+
+export default GeneratingPgp
