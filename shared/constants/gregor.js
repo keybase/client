824e27ff24d41b517b915fd7f03d38245d44d5cb
Marco Munizaga <marco@marcopolo.io>
2016-08-09 17:53:55 -0700

    Expand gregor actions

--- shared/constants/gregor.js
+++ shared/constants/gregor.js
@@ -1,10 +1,20 @@
 // @flow
 
-import type {State as GregorState} from '../constants/types/flow-types-gregor'
+import type {State as GregorState, Item, Metadata} from '../constants/types/flow-types-gregor'
 import type {PushReason} from '../constants/types/flow-types'
 import type {TypedAction} from '../constants/types/flux'
 
 export const pushState = 'gregor:pushState'
-export type PushState = TypedAction<'gregor:pushState', {state: GregorState, reason: PushReason}, {}>
+export type PushState = TypedAction<'gregor:pushState', {state: GregorState, reason: PushReason}, void>
 
-export type GregorActions = PushState
+export const updateSeenMsgs = 'gregor:updateSeenMsgs'
+export type UpdateSeenMsgs = TypedAction<'gregor:updateSeenMsgs', {seenMsgs: Array<NonNullGregorItem>}, void>
+
+export type GregorActions = PushState | UpdateSeenMsgs
+
+export type NonNullGregorItem = {
+  md: Metadata,
+  item: Item,
+}
+
+export type MsgMap = {[key: string]: NonNullGregorItem}
---
+++ shared/reducers/gregor.js
@@ -0,0 +1,36 @@
+/* @flow */
+
+import * as Constants from '../constants/gregor'
+import * as CommonConstants from '../constants/common'
+import {keyBy} from 'lodash'
+
+import type {GregorActions, MsgMap} from '../constants/gregor'
+
+export type State = {
+  seenMsgs: MsgMap,
+}
+
+const initialState: State = {
+  seenMsgs: {},
+}
+
+export default function (state: State = initialState, action: GregorActions): State {
+  switch (action.type) {
+    case CommonConstants.resetStore:
+      return initialState
+    case Constants.updateSeenMsgs:
+      if (!action.error) {
+        const newMsgs: MsgMap = keyBy(action.payload.seenMsgs, m => m.md.msgID.toString('base64'))
+        return {
+          ...state,
+          seenMsgs: {
+            ...state.seenMsgs,
+            ...newMsgs,
+          },
+        }
+      }
+      return state
+    default:
+      return state
+  }
+}
