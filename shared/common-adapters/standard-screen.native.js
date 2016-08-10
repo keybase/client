d3e9dc8dd523bfac303f154420d3cb202cde68bb
Alex Wendland <me@alexwendland.com>
2016-08-10 09:15:26 -0700

    Standard screen native DESKTOP-1666 (#3819)
    * Implement StandardScreen for mobile and expand styling hooks
    
    * Implement Confirm on top of StandardScreen
    
    Dog fooding attempt to validate that the mobile version works

--- shared/common-adapters/standard-screen.native.js
+++ shared/common-adapters/standard-screen.native.js
@@ -1,11 +1,64 @@
 // @flow
-import {Component} from 'react'
+import React from 'react'
 import type {Props} from './standard-screen'
+import {Box, Text} from './'
+import {globalColors, globalMargins, globalStyles} from '../styles/style-guide'
 
-class StandardScreen extends Component<void, Props, void> {
-  render () {
-    return null
-  }
+const StandardScreen = (props: Props) => {
+  return (
+    <Box style={{...styleContainer, ...props.styleOuter}}>
+      <Box style={styleCloseContainer}>
+        {!!props.onClose && <Text type='BodyPrimaryLink' style={{...styleClose, ...props.styleClose}} onClick={props.onClose}>{props.onCloseText || 'Cancel'}</Text>}
+      </Box>
+      {!!props.notification &&
+        <Box style={{...styleBanner(props.notification.type), ...props.styleBanner}}>
+          {typeof props.notification.message === 'string' ? <Text style={styleBannerText} type='BodySmallSemibold'>{props.notification.message}</Text> : props.notification.message}
+        </Box>}
+      <Box style={{...styleContentContainer(!!props.notification), ...props.style}}>
+        {props.children}
+      </Box>
+    </Box>
+  )
 }
+
+const styleContainer = {
+  ...globalStyles.flexBoxColumn,
+  padding: globalMargins.small,
+  flex: 1,
+}
+
+const styleCloseContainer = {
+  ...globalStyles.flexBoxRow,
+  height: globalMargins.large - globalMargins.tiny,
+  alignItems: 'center',
+}
+
+const styleClose = {
+  color: globalColors.blue,
+}
+
+const MIN_BANNER_HEIGHT = globalMargins.large
+
+const styleBanner = (type) => ({
+  ...globalStyles.flexBoxColumn,
+  minHeight: MIN_BANNER_HEIGHT,
+  padding: globalMargins.tiny,
+  alignItems: 'center',
+  justifyContent: 'center',
+  backgroundColor: type === 'error' ? globalColors.red : globalColors.green,
+})
+
+const styleBannerText = {
+  color: globalColors.white,
+  textAlign: 'center',
+}
+
+const styleContentContainer = (isBannerShowing) => ({
+  ...globalStyles.flexBoxColumn,
+  alignItems: 'center',
+  justifyContent: 'center',
+  flex: 1,
+  ...(isBannerShowing ? {marginTop: -MIN_BANNER_HEIGHT} : {}),
+})
 
 export default StandardScreen
