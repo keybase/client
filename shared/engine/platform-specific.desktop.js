2bf1968af81ab21117e598fd1b20f92bb8c17ff4
chrisnojima <chrisnojima@gmail.com>
2016-08-11 08:59:48 -0400

    refactor engine - cleanup platform stuff (#3833)
    * Cleanup all the platform specific pieces of code in engine into a
    simpler construct. reducing the back and forth. also fixes a reconnect
    but in native
    
    * fix ios android not naming native modules the same
    
    * add more comments

---
+++ shared/engine/platform-specific.desktop.js
@@ -0,0 +1,47 @@
+// @flow
+import net from 'net'
+import type {incomingRPCCallbackType, connectCallbackType} from './platform-specific'
+import {TransportShared, sharedCreateClient} from './transport-shared'
+import {socketPath} from '../constants/platform.native.desktop'
+
+class NativeTransport extends TransportShared {
+  constructor (incomingRPCCallback, connectCallback) {
+    super({path: socketPath}, connectCallback, incomingRPCCallback)
+    this.needsConnect = true
+  }
+
+  _connect_critical_section (cb: any) { // eslint-disable-line camelcase
+    super._connect_critical_section(cb)
+    windowsHack()
+  }
+}
+
+function windowsHack () {
+  // This net.connect() is a heinous hack.
+  //
+  // On Windows, but *only* in the renderer thread, our RPC connection
+  // hangs until other random net module operations, at which point it
+  // unblocks.  Could be Electron, could be a node-framed-msgpack-rpc
+  // bug, who knows.
+  // $FlowIssue
+  if (process.platform !== 'win32' || process.type !== 'renderer') {
+    return
+  }
+
+  var fake = net.connect({})
+  // net.connect({}) throws; we don't need to see the error, but we
+  // do need it not to raise up to the main thread.
+  fake.on('error', function () {})
+}
+
+function createClient (incomingRPCCallback: incomingRPCCallbackType, connectCallback: connectCallbackType) {
+  return sharedCreateClient(new NativeTransport(incomingRPCCallback, connectCallback))
+}
+
+// Resets are handled internally by framed-msg-pack
+function resetClient () { }
+
+export {
+  resetClient,
+  createClient,
+}
--- shared/engine/windows-hack.native.js
+++
@@ -1,4 +0,0 @@
-// @flow
-export default function () {
-  // Nothing like this on mobile
-}
