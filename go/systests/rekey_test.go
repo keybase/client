42e9bb4b84a840135fd453171600de4de969146e
Maxwell Krohn <themax@gmail.com>
2016-08-12 16:18:00 -0400

    get service tests working again
    - remove some comments we don't need anymore due to self-documenting code ;)

--- go/systests/rekey_test.go
+++ go/systests/rekey_test.go
@@ -1,28 +1,5 @@
 package systests
 
-// Strategy:
-//
-//   1. Sign up a fake user with a device and paper key
-//   2. Assert no rekey activity
-//   3. Call the test/fake_home_tlf endpoint to fake a TLF that's only
-//      keyed for the device key (and not the paper key).
-//   4. Assert that we get a rekey harassment window.
-//   5. Dismiss the window and assert it doesn't show up again for
-//      another 24 hours.
-//   6. Enter the paper key, and fast-forward a bunch of time. Assert
-//      we don't get harassed, since now all of our devices are online.
-//   7. Provision new device, but don't change the keying of the TLF.
-//   8. Assert that the window shows up right away.
-//   9. Snooze it.
-//  10. Assert that the snooze lasts for ~24 hours.
-//  11. Logout and Login. Assert we get the popup right away.
-//  12. Snooze it and assert snooze lasts for ~24 hours.
-//  13. Have the window open.
-//  14. Call the test/fake_home_tlf endpoint to fully rekey the TLF.
-//  15. Assert the window is dismissed within ~1 minute.
-//  16. Assert that our gregor queue is empty for this category.
-//
-
 import (
 	"encoding/hex"
 	"fmt"
@@ -810,7 +787,7 @@ func TestRekey(t *testing.T) {
 	primaryDevice := rkt.setup("rekey")
 	defer rkt.cleanup()
 
-	// Start up the primary device
+	// 0. Start up the primary device
 	primaryDevice.start(4)
 	rkt.startUIsAndClients(primaryDevice)
 
