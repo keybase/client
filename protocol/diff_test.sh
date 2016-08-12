0a6cfcc3c1f8633962da1f93a72cd6dce0e9e77f
Chris Ball <chris@printf.net>
2016-08-12 12:14:09 -0400

    Fix diff_test to catch additions too

--- protocol/diff_test.sh
+++ protocol/diff_test.sh
@@ -16,9 +16,18 @@ make
 # This build process is idempotent. We expect there to be no changes after
 # re-running the protocol generation, because any changes should have been
 # checked in.
+
 if ! git diff --exit-code ./ ../go/ ../shared/; then
   echo 'ERROR: `git diff` detected changes. The generated protocol files are stale.'
   exit 1
 fi
+
+( cd .. &&
+  if [ "`git ls-files --others --exclude-standard`" ]; then
+    echo 'ERROR: git detected that building the protocol resulted in newly created files. The generated protocol files are stale.'
+    exit 1
+  fi
+  cd protocol
+)
 
 echo 'SUCCESS: The generated protocol files are up to date.'
