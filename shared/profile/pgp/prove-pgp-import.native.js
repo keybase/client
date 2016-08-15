4a946c199b8fd49a478cb04bc9a7ced285a3c93c
Chris Ball <chris@printf.net>
2016-08-15 13:18:37 -0400

    foo

--- shared/profile/pgp/prove-pgp-import.native.js
+++ shared/profile/pgp/prove-pgp-import.native.js
@@ -1,11 +1,24 @@
 // @flow
-import {Component} from 'react'
+import React, {Component} from 'react'
+import {StandardScreen, Icon, Text} from '../../common-adapters'
+import {globalMargins} from '../../styles/style-guide'
 import type {Props} from './prove-pgp-import'
 
 class ProvePgpImport extends Component<void, Props, void> {
   render () {
-    return null
+    return (
+      <StandardScreen onClose={this.props.onCancel} style={styleContainer}>
+        <Icon type='icon-pgp-key-48' />
+        <Text type='Body'>Importing a PGP key is not supported on our mobile app. To continue, download the Keybase desktop app and follow the instructions there.</Text>
+      </StandardScreen>
+    )
   }
 }
 
+const styleContainer = {
+  maxWidth: 576,
+  padding: globalMargins.medium,
+  marginLeft: globalMargins.medium,
+  marginRight: globalMargins.medium,
+}
 export default ProvePgpImport
