227f198922559893355cee6a295f55b3f80d7fd9
Marco Munizaga <marco@marcopolo.io>
2016-08-15 13:18:41 -0700

    Hacky way to make sure we really quit (#3892)

--- desktop/app/ctl.js
+++ desktop/app/ctl.js
@@ -8,11 +8,19 @@ export function ctlStop (callback) {
   exec(binPath, args, 'darwin', 'prod', false, callback)
 }
 
+function exitApp () {
+  // For some reason the first app.exit kills only popups (remote components and pinentry)
+  // The main window survives. This makes sure to keep exiting until we are actually out.
+  // It seems to work even when we have a broken reference to a browser window.
+  // (Which happens because our first exit killed the browser window w/o updating our book keeping state)
+  setInterval(() => app.exit(0), 200)
+}
+
 export function quit () {
   // Only quit the app in dev mode
   if (__DEV__) {
     console.log('Only quiting gui in dev mode')
-    app.exit(0)
+    exitApp()
     return
   }
 
@@ -22,6 +30,6 @@ export function quit () {
     if (stopErr) {
       console.log('Error in ctl stop, when quiting:', stopErr)
     }
-    app.exit(0)
+    exitApp()
   })
 }
