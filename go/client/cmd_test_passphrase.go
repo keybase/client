1efad242fd557da5100e08980e1c4e521c293344
Patrick Crosby <pcrosby@gmail.com>
2016-08-15 14:37:25 -0500

    Remove arg

--- go/client/cmd_test_passphrase.go
+++ go/client/cmd_test_passphrase.go
@@ -48,7 +48,7 @@ func (s *CmdTestPassphrase) Run() (err error) {
 	}
 
 	arg := keybase1.PassphrasePromptArg{
-		GuiArg: libkb.DefaultPassphraseArg(s.G(), s.G().SecretStoreAll != nil),
+		GuiArg: libkb.DefaultPassphraseArg(s.G()),
 	}
 	res, err := cli.PassphrasePrompt(context.TODO(), arg)
 	if err != nil {
