5897785ef042f0984fd7873ce53c7f310da1b9b9
Marco Munizaga <marco@marcopolo.io>
2016-08-15 16:26:52 -0700

    Tweak margins a bit to add confirm or pending on mobile

--- shared/profile/confirm-or-pending.native.js
+++ shared/profile/confirm-or-pending.native.js
@@ -1,9 +1,35 @@
 /* @flow */
 
+import React from 'react'
+import {Box, Text, Button, PlatformIcon} from '../common-adapters'
+import {globalStyles, globalColors, globalMargins} from '../styles/style-guide'
+import {propsForPlatform} from './confirm-or-pending.shared'
+
 import type {Props} from './confirm-or-pending'
 
-const Render = ({platform}: Props) => {
-  return null
+const Render = (props: Props) => {
+  const {platform, onReloadProfile, titleColor, username, platformIconOverlayColor} = props
+  const {
+    title, platformIconOverlay, usernameSubtitle, message, messageSubtitle,
+  } = propsForPlatform(props)
+
+  return (
+    <Box style={{...globalStyles.flexBoxColumn, flex: 1}}>
+      <Box style={{...globalStyles.flexBoxColumn, justifyContent: 'center', alignItems: 'center', backgroundColor: titleColor, height: globalMargins.large}}>
+        <Text backgroundMode='Terminal' type='BodySmallSemibold'>{title}</Text>
+      </Box>
+      <Box style={{...globalStyles.flexBoxColumn, alignItems: 'center', paddingTop: globalMargins.small, paddingBottom: globalMargins.medium, paddingLeft: globalMargins.medium, paddingRight: globalMargins.medium}}>
+        <PlatformIcon platform={platform} overlay={platformIconOverlay} overlayColor={platformIconOverlayColor} size={48} />
+        <Text type='Header' style={{color: globalColors.blue}}>{username}</Text>
+        {!!usernameSubtitle && <Text type='Body' style={{color: globalColors.black_10}}>{usernameSubtitle}</Text>}
+        <Text type='Body' style={{marginTop: globalMargins.small, textAlign: 'center'}}>{message}</Text>
+        {!!messageSubtitle && <Text type='BodySmall' style={{textAlign: 'center'}}>{messageSubtitle}</Text>}
+      </Box>
+      <Box style={{...globalStyles.flexBoxRow, paddingLeft: globalMargins.small, paddingRight: globalMargins.small}}>
+        <Button type='Primary' onClick={onReloadProfile} label='Reload profile' style={{flex: 1}} />
+      </Box>
+    </Box>
+  )
 }
 
 export default Render
