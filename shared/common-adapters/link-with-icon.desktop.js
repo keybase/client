fccfbde58c9d27781f2a8c3a632296f9e07b7b4e
Alex Wendland <me@alexwendland.com>
2016-08-16 13:48:19 -0700

    Add common adapter for showing links w/ an icon

---
+++ shared/common-adapters/link-with-icon.desktop.js
@@ -0,0 +1,19 @@
+// @flow
+import React from 'react'
+import {Text, Icon} from '../common-adapters'
+import {globalMargins} from '../styles/style-guide'
+import type {Props} from './link-with-icon'
+
+const LinkWithIcon = ({label, icon, color, onClick, style}: Props) => (
+  <Text style={{...styleLabel, color, ...style}} type='BodyPrimaryLink' onClick={onClick}><Icon style={{...styleIcon, color}} type={icon} />{label}</Text>
+)
+
+const styleLabel = {
+  display: 'block',
+}
+
+const styleIcon = {
+  marginRight: globalMargins.xtiny,
+}
+
+export default LinkWithIcon
