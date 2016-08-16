02ec6abf552b10b60cc75f9ac59982dc38f7acc7
Alex Wendland <me@alexwendland.com>
2016-08-16 14:05:53 -0700

    Implement dumb post proofs on native

--- shared/profile/post-proof.native.js
+++ shared/profile/post-proof.native.js
@@ -1,11 +1,79 @@
 // @flow
-import {Component} from 'react'
+import React from 'react'
+import {Button, CopyableText, LinkWithIcon, PlatformIcon, StandardScreen, Text} from '../common-adapters'
+import {globalStyles, globalColors, globalMargins} from '../styles/style-guide'
+import * as shared from './post-proof.shared'
 import type {Props} from './post-proof'
 
-class Render extends Component<void, Props, void> {
-  render () {
-    return null
+const PostProof = (props: Props) => {
+  const {
+    platform, platformUserName, descriptionText, proofAction, onCancel,
+    onComplete, isOnCompleteWaiting, errorMessage,
+  } = props
+  const {
+    descriptionView, noteText, onCompleteText, proofText, platformSubtitle, proofActionIcon, proofActionText,
+  } = shared.propsForPlatform(props)
+
+  const notification = !errorMessage ? {} : {
+    notification: {
+      type: 'error',
+      message: errorMessage,
+    },
   }
+
+  return (
+    <StandardScreen {...notification} onClose={onCancel}>
+      <PlatformIcon style={stylePlatformIcon} platform={platform} overlay='icon-proof-unfinished' overlayColor={globalColors.grey} size={48} />
+      <Text style={{...stylePlatformUsername, ...(stylePlatformSubtitle ? {} : {marginBottom: globalMargins.medium})}} type='Header'>{platformUserName}</Text>
+      {platformSubtitle && <Text style={stylePlatformSubtitle} type='Body'>{platformSubtitle}</Text>}
+      {descriptionView || (descriptionText && <Text style={styleDescriptionText} type='Body'>{descriptionText}</Text>)}
+      {proofText && <CopyableText style={styleProofContainer} value={proofText} extras$native={{style: styleProofText}} />}
+      {noteText && <Text style={styleNoteText} type='BodySmall'>{noteText}</Text>}
+      {proofAction && proofActionText && proofActionIcon && <LinkWithIcon style={styleProofAction} label={proofActionText} icon={proofActionIcon} color={globalColors.blue} onClick={() => proofAction()} />}
+      <Button style={styleContinueButton} type='Primary' onClick={() => onComplete()} label={onCompleteText} waiting={isOnCompleteWaiting} />
+    </StandardScreen>
+  )
 }
 
-export default Render
+const stylePlatformIcon = {
+  alignSelf: 'center',
+}
+
+const stylePlatformUsername = {
+  color: globalColors.blue,
+  textAlign: 'center',
+}
+
+const stylePlatformSubtitle = {
+  color: globalColors.black_10,
+  textAlign: 'center',
+}
+
+const styleDescriptionText = {
+  textAlign: 'center',
+  marginTop: globalMargins.tiny,
+}
+
+const styleProofContainer = {
+  marginTop: globalMargins.tiny,
+}
+
+const styleProofText = {
+  maxHeight: 7 /* # lines */ * 20 /* line height */ + 2 * 10 /* padding */,
+}
+
+const styleNoteText = {
+  marginTop: globalMargins.tiny,
+  textAlign: 'center',
+}
+
+const styleProofAction = {
+  marginTop: globalMargins.medium,
+}
+
+const styleContinueButton = {
+  ...globalStyles.flexBoxRow,
+  marginTop: globalMargins.medium,
+}
+
+export default PostProof
