906564860c65d45bb52b3c54dd9083321cd6dd2a
Alex Wendland <me@alexwendland.com>
2016-08-17 14:28:44 -0700

    Implement SmallInput on native

--- shared/common-adapters/small-input.native.js
+++ shared/common-adapters/small-input.native.js
@@ -1,7 +1,42 @@
 // @flow
+import React from 'react'
+import {TextInput} from 'react-native'
+import Box from './box'
+import Text from './text.native'
+import {globalColors, globalMargins, globalStyles} from '../styles/style-guide'
 
 import type {SmallInputProps} from './small-input'
 
-export default function SmallInput ({hintText, label, onChange, value, errorState}: SmallInputProps) {
-  return null
+export default function SmallInput ({autoCapitalize, autoCorrect, errorState, hintText, label, onChange, style, value}: SmallInputProps) {
+  return (
+    <Box style={{...styleContainer(errorState), ...style}}>
+      <Text type='BodySmall' style={styleLabel(errorState)}>{label}</Text>
+      <TextInput
+        style={{...Text.textStyle({type: 'BodySemibold'}, {}), ...styleInput}}
+        placeholder={hintText}
+        placeholderTextColor={globalColors.black_10}
+        underlineColorAndroid={'transparent'}
+        autoCapitalize={autoCapitalize}
+        autoCorrect={autoCorrect}
+        value={value}
+        onChangeText={onChange} />
+    </Box>
+  )
+}
+
+const styleContainer = (hasError: boolean) => ({
+  ...globalStyles.flexBoxRow,
+  borderBottomWidth: 1,
+  borderColor: (hasError ? globalColors.red : globalColors.black_10),
+  paddingBottom: 2,
+})
+
+const styleLabel = (hasError: boolean) => ({
+  color: (hasError ? globalColors.red : globalColors.blue),
+  marginRight: globalMargins.xtiny,
+})
+
+const styleInput = {
+  textAlign: 'left',
+  flex: 1,
 }
