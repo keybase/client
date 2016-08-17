11774fa73af27a21bdfa2605b1d801a345e92649
Chris Ball <chris@printf.net>
2016-08-17 16:57:41 -0400

    Move where the add happens

--- protocol/diff_test.sh
+++ protocol/diff_test.sh
@@ -5,12 +5,12 @@ set -e -u -o pipefail
 here="$(dirname "$BASH_SOURCE")"
 cd "$here"
 
+# First let's get the index clean from other files that CI runs
+git add -A ./ ../go/ ../shared/
+
 npm i
 make clean
 make
-
-# First let's get the index clean from other files that CI runs create.
-git add -A ./ ../go/ ../shared/
 
 # Protocol changes could create diffs in the following directories:
 #   protocol/
@@ -19,11 +19,6 @@ git add -A ./ ../go/ ../shared/
 # This build process is idempotent. We expect there to be no changes after
 # re-running the protocol generation, because any changes should have been
 # checked in.
-
-git diff HEAD -- ./ ../go/ ../shared/ # for testing
-git diff --exit-code # for testing
-echo $? # for testing
-
 if ! git diff --quiet --exit-code HEAD -- ./ ../go/ ../shared/; then
   echo 'ERROR: `git diff` detected changes. The generated protocol files are stale.'
   exit 1
