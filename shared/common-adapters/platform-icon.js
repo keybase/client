80b887ab31327fac4513adfa0eb7a5a6ff11ad78
Alex Wendland <me@alexwendland.com>
2016-08-17 17:06:01 -0700

    Update PlatformIcon to include PGP

--- shared/common-adapters/platform-icon.js
+++ shared/common-adapters/platform-icon.js
@@ -8,51 +8,75 @@ type Props = {
   platform: PlatformsExpandedType,
   size: 48,
   overlay: IconType,
-  overlayColor: string,
+  overlayColor?: string,
   style?: Object,
 }
 
-const supportedPlatformsAndSizes: {[key: PlatformsExpandedType]: ?{[key: string]: ?IconType}} = {
-  'coinbase': {
-    '48': 'icon-coinbase-logo-48',
-  },
-  'twitter': {
-    '48': 'icon-twitter-logo-48',
-  },
-  'github': {
-    '48': 'icon-github-logo-48',
-  },
-  'reddit': {
-    '48': 'icon-reddit-logo-48',
-  },
-  'hackernews': {
-    '48': 'icon-hacker-news-logo-48',
-  },
-  'dns': {
-    '48': 'icon-website-48',
-  },
-  'http': {
-    '48': 'icon-website-48',
-  },
-  'https': {
-    '48': 'icon-website-48',
-  },
-  'btc': {
-    '48': 'icon-bitcoin-logo-48',
-  },
+type IconSpec = {
+  icon: IconType,
+  offsetBottom: number,
+  offsetRight: number,
+}
+
+const standardOffsets = {
+  offsetBottom: -2,
+  offsetRight: -5,
+}
+
+const getSpecForPlatform = (platform: PlatformsExpandedType): {[size: string]: IconSpec} => {
+  const specs = {
+    'coinbase': {
+      '48': {icon: 'icon-coinbase-logo-48', ...standardOffsets},
+    },
+    'twitter': {
+      '48': {icon: 'icon-twitter-logo-48', ...standardOffsets},
+    },
+    'github': {
+      '48': {icon: 'icon-github-logo-48', ...standardOffsets},
+    },
+    'reddit': {
+      '48': {icon: 'icon-reddit-logo-48', ...standardOffsets},
+    },
+    'hackernews': {
+      '48': {icon: 'icon-hacker-news-logo-48', ...standardOffsets},
+    },
+    'dns': {
+      '48': {icon: 'icon-website-48', ...standardOffsets},
+    },
+    'http': {
+      '48': {icon: 'icon-website-48', ...standardOffsets},
+    },
+    'https': {
+      '48': {icon: 'icon-website-48', ...standardOffsets},
+    },
+    'dnsOrGenericWebSite': {
+      '48': {icon: 'icon-website-48', ...standardOffsets},
+    },
+    'rooter': {
+      '48': {icon: 'icon-website-48', ...standardOffsets},
+    },
+    'btc': {
+      '48': {icon: 'icon-bitcoin-logo-48', ...standardOffsets},
+    },
+    'pgp': {
+      '48': {icon: 'icon-pgp-key-48', offsetBottom: -2, offsetRight: 4},
+    },
+  }
+  return specs[platform]
 }
 
 const Render = ({platform, overlay, overlayColor, size, style}: Props) => {
-  const icon: ?IconType = supportedPlatformsAndSizes[platform] && supportedPlatformsAndSizes[platform][String(size)]
-  if (!icon) {
-    console.warn('unsupported platform + size: ', platform, size)
+  const iconSpec = getSpecForPlatform(platform)[String(size)]
+
+  if (!iconSpec) {
+    console.warn(`Unsupported platform and size pair: ${platform} @ ${size}`)
     return null
   }
 
   return (
-    <Box style={{...style, position: 'relative'}}>
-      <Icon type={icon} />
-      <Icon type={overlay} style={{position: 'absolute', bottom: -2, right: -5}} />
+    <Box style={{position: 'relative', ...style}}>
+      <Icon type={iconSpec.icon} />
+      <Icon type={overlay} style={{color: overlayColor, position: 'absolute', bottom: iconSpec.offsetBottom, right: iconSpec.offsetRight}} />
     </Box>
   )
 }
