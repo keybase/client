6a0f454bf64c1fba5091ac7498d251f008361fa8
chrisnojima <cnojima@keyba.se>
2016-08-23 13:06:17 -0400

    WIP

--- shared/common-adapters/clickable-box.native.js
+++ shared/common-adapters/clickable-box.native.js
@@ -1,4 +1,19 @@
 // @flow
-// TODO
-import {View} from 'react-native'
-export default View
+import React from 'react'
+import type {Props} from './clickable-box'
+import {NativeTouchableHighlight, Box} from './index.native'
+import {globalColors} from '../styles/style-guide'
+
+const ClickableBox = ({onClick, style, children}: Props) => (
+  <NativeTouchableHighlight onPress={onClick} style={{...boxStyle, ...style}} underlayColor={globalColors.black_10}>
+    <Box>
+      {children}
+    </Box>
+  </NativeTouchableHighlight>
+)
+
+const boxStyle = {
+  borderRadius: 3,
+}
+
+export default ClickableBox
