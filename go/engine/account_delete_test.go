41af66d67e15f364d93abed69a54ca7b8ef3cc5e
Patrick Crosby <pcrosby@gmail.com>
2016-08-25 13:06:09 -0500

    Migrate jzila test, plumb error through resolve, merkle

--- go/engine/account_delete_test.go
+++ go/engine/account_delete_test.go
@@ -11,6 +11,7 @@ import (
 	"testing"
 
 	"github.com/keybase/client/go/libkb"
+	"github.com/keybase/client/go/protocol/keybase1"
 )
 
 func TestAccountDelete(t *testing.T) {
@@ -31,5 +32,38 @@ func TestAccountDelete(t *testing.T) {
 	}
 	if _, ok := err.(libkb.NotFoundError); !ok {
 		t.Errorf("loading deleted user error type: %T, expected libkb.NotFoundError", err)
+	}
+}
+
+func TestAccountDeleteIdentify(t *testing.T) {
+	tc := SetupEngineTest(t, "acct")
+	defer tc.Cleanup()
+
+	fu := CreateAndSignupFakeUser(tc, "acct")
+	u, err := libkb.LoadUser(libkb.NewLoadUserByNameArg(tc.G, fu.Username))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ctx := &Context{}
+	eng := NewAccountDelete(tc.G)
+	if err := RunEngine(eng, ctx); err != nil {
+		t.Fatal(err)
+	}
+
+	i := newIdentify2WithUIDTester(tc.G)
+	tc.G.Services = i
+	arg := &keybase1.Identify2Arg{
+		Uid: u.GetUID(),
+	}
+	ieng := NewIdentify2WithUID(tc.G, arg)
+	ictx := &Context{IdentifyUI: i}
+
+	err = RunEngine(ieng, ictx)
+	if err == nil {
+		t.Fatal("identify2 ran successfully on deleted user")
+	}
+	if _, ok := err.(libkb.DeletedError); !ok {
+		t.Errorf("identify2 error: %T, expected libkb.DeletedError", err)
 	}
 }
