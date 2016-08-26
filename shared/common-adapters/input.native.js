27e6f81e46e60974a9d1d179184b5614c66ad904
Alex Wendland <me@alexwendland.com>
2016-08-26 15:02:41 -0700

    Change input native to flex; Add auto-grow support on iOS

--- shared/common-adapters/input.native.js
+++ shared/common-adapters/input.native.js
@@ -5,9 +5,28 @@ import type {Props} from './input'
 import {Box, Text, NativeTextInput} from './index.native'
 import {globalColors, globalStyles} from '../styles'
 
+/* ==========
+ * =  BUGS  =
+ * ==========
+ *
+ * iOS:
+ *  - Placeholder text (hintText) is top left aligned. (AW 2016-08-26)
+ *  - Multiline inputs appear to get pushed ~4px right of center. (AW 2016-08-26)
+ *
+ * Android:
+ *  - Auto grow does not work. (AW 2016-8-26)
+ *    - The text will size to the initial dimensions, it just won't expand after a user types.
+ *    - onContentSizeChange never gets called again.
+ *  - Cursor is hidden when multiline text exceeds element bounds (AW 2016-08-26)
+ *    - Instead of the element focus following the cursor like in single line.
+ *  - Multiline flag changes the padding amount required to adjust for native element chrome. (AW 2016-08-26)
+ *
+ */
+
 type State = {
   inputFocused: boolean,
-  text: string
+  text: string,
+  textHeight: number,
 }
 
 class Input extends Component<void, Props, State> {
@@ -19,6 +38,7 @@ class Input extends Component<void, Props, State> {
     this.state = {
       inputFocused: false,
       text: this.props.value || '',
+      textHeight: 0,
     }
   }
 
@@ -35,17 +55,38 @@ class Input extends Component<void, Props, State> {
     }
   }
 
+  _onContentSizeChange (event: SyntheticEvent) {
+    // $ForceType
+    const nativeEvent: {contentSize: {height: number}} = event.nativeEvent
+    this.setState({
+      textHeight: nativeEvent.contentSize.height,
+    })
+  }
+
   render () {
-    const IOS = Platform.OS_IOS === OS
-    const inputStyle = Text.textStyle({type: 'BodySemibold'}, {})
+    const isIOS = Platform.OS_IOS === OS
+    const isShowingFloatingLabel = this.state.text.length > 0
     const password = this.props.type === 'password'
     const passwordVisible = this.props.type === 'passwordVisible'
+    const autoGrow = this.props.autoGrow
+      ? {onContentSizeChange: (event) => this._onContentSizeChange(event)}
+      : {}
+    const autoGrowActive = isIOS && this.props.autoGrow
 
     return (
       <Box style={{...containerStyle, ...this.props.style}}>
-        {this.state.text.length > 0 && <Text type='BodySmall' style={{...floatingLabelStyle}}>{this.props.floatingLabelText}</Text>}
+        {isShowingFloatingLabel &&
+          <Text
+            type='BodySmall'
+            style={{...floatingLabelStyle}}>
+            {this.props.floatingLabelText}
+          </Text>}
         <NativeTextInput
-          style={{...inputStyle, ...textInputStyle, ...(IOS && this.props.multiline && IOSMultilineTextInputStyle || {})}}
+          style={{
+            ...Text.textStyle({type: 'BodySemibold'}, {}),
+            ...textInputStyle({...this.state, ...this.props, isShowingFloatingLabel, autoGrowActive}),
+            ...this.props.inputStyle,
+          }}
           keyboardType={this.props.keyboardType}
           ref={component => { this._textInput = component }}
           autoCorrect={!(password || passwordVisible)}
@@ -61,59 +102,67 @@ class Input extends Component<void, Props, State> {
           onFocus={() => this.setState({inputFocused: true})}
           onBlur={() => this.setState({inputFocused: false})}
           onSubmitEditing={this.props.onEnterKeyDown}
+          {...autoGrow}
           onChange={this.props.onChange}
           onChangeText={text => { this.setState({text}); this.props.onChangeText && this.props.onChangeText(text) }} />
-        {IOS && !this.props.iosOmitUnderline && <HorizontalLine focused={this.state.inputFocused} />}
-        {!!(this.props.errorText) && <Text type='Error' style={{...errorText, ...this.props.errorStyle}}>{this.props.errorText}</Text>}
+        {isIOS && !this.props.iosOmitUnderline &&
+          <HorizontalLine focused={this.state.inputFocused} />}
+        {!!(this.props.errorText) &&
+          <Text
+            type='Error'
+            style={{...errorText, ...this.props.errorStyle}}>
+            {this.props.errorText}
+          </Text>}
       </Box>
     )
   }
 }
 
-const HorizontalLine = ({focused}) => <Box style={{
-  position: 'absolute',
-  left: 0,
-  right: 0,
-  bottom: 0,
-  height: 1,
-  backgroundColor: focused ? globalColors.blue : globalColors.black_10,
-}} />
+const FLOATING_LABEL_HEIGHT = 18
+const FLOATING_LABEL_OFFSET = -4
+const TEXT_INPUT_PLATFORM_PADDING = OS === Platform.OS_IOS
+  ? 6 // Minor padding adjustment needed on iOS
+  : 18 // Large padding adjustment needed on Android b/c native view has padding
 
 const containerStyle = {
   ...globalStyles.flexBoxColumn,
-  height: 36,
-  position: 'relative',
-  marginTop: 21,
-}
-
-const textInputStyle = {
-  textAlign: 'center',
-  textAlignVertical: 'bottom',
-  position: 'absolute',
-  top: 0,
-  bottom: -8,
-  left: -4,
-  right: -4,
-}
-
-const IOSMultilineTextInputStyle = {
-  bottom: 0,
+  alignItems: 'stretch',
 }
 
 const floatingLabelStyle = {
   color: globalColors.blue,
   textAlign: 'center',
-  position: 'relative',
-  height: 21,
-  top: -12,
+  height: FLOATING_LABEL_HEIGHT,
+  lineHeight: FLOATING_LABEL_HEIGHT,
+}
+
+const textInputStyle = ({isShowingFloatingLabel, textHeight, rows, multiline, autoGrowActive}: {
+  isShowingFloatingLabel: boolean,
+  textHeight: number,
+  rows?: number,
+  multiline?: boolean,
+  autoGrowActive?: boolean,
+}) => {
+  const MIN_TEXT_HEIGHT = Text.textStyle({type: 'BodySemibold'}, {}).lineHeight + TEXT_INPUT_PLATFORM_PADDING
+  return {
+    marginTop: (isShowingFloatingLabel ? 0 : FLOATING_LABEL_HEIGHT) + FLOATING_LABEL_OFFSET,
+    height: Math.max(textHeight, (autoGrowActive ? 1 : rows || 1) * MIN_TEXT_HEIGHT),
+    textAlign: 'center',
+    // BUG (AW): adjust for an odd bug in iOS that causes multiline inputs to
+    // not center their contents properly
+    marginLeft: (multiline && OS === Platform.OS_IOS) ? 4 : 0,
+  }
 }
 
 const errorText = {
   textAlign: 'center',
-  position: 'absolute',
+}
+
+const HorizontalLine = ({focused}) => <Box style={{
   left: 0,
   right: 0,
-  top: 36,
-}
+  height: 1,
+  backgroundColor: focused ? globalColors.blue : globalColors.black_10,
+}} />
 
 export default Input
