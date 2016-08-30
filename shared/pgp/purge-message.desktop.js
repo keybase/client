e8c1cee84e440809cd4cf6c588f20d72f0e8b598
Patrick Crosby <pcrosby@gmail.com>
2016-08-30 10:39:26 -0500

    Fix import

--- shared/pgp/purge-message.desktop.js
+++ shared/pgp/purge-message.desktop.js
@@ -1,7 +1,7 @@
 // @flow
 import React, {Component} from 'react'
 import {Box, Button, Header, Text} from '../common-adapters'
-import {globalStyles, globalMargins} from '../styles/style-guide'
+import {globalStyles, globalMargins} from '../styles'
 
 export type Props = {
   onClose: () => void,
