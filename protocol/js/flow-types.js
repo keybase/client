5097d46a43f458aff69c38f96684c2076242af1e
Song Gao <song@gao.io>
2016-08-30 17:08:17 -0700

    add support for multiple topics

--- protocol/js/flow-types.js
+++ protocol/js/flow-types.js
@@ -513,6 +513,10 @@ export function chatLocalResolveConversationLocalRpc (request: $Exact<requestCom
   engineRpcOutgoing({...request, method: 'chatLocal.resolveConversationLocal'})
 }
 
+export function chatLocalUpdateTopicNameLocalRpc (request: $Exact<requestCommon & requestErrorCallback & {param: chatLocalUpdateTopicNameLocalRpcParam}>) {
+  engineRpcOutgoing({...request, method: 'chatLocal.updateTopicNameLocal'})
+}
+
 export function configCheckAPIServerOutOfDateWarningRpc (request: $Exact<requestCommon & {callback?: ?(err: ?any, response: configCheckAPIServerOutOfDateWarningResult) => void}>) {
   engineRpcOutgoing({...request, method: 'config.checkAPIServerOutOfDateWarning'})
 }
@@ -1219,6 +1223,7 @@ export type ConfirmResult = {
 }
 
 export type ConversationInfoLocal = {
+  id: chat1.ConversationID,
   tlfName: string,
   topicName: string,
   topicType: chat1.TopicType,
@@ -2592,7 +2597,7 @@ export type chatLocalGetThreadLocalRpcParam = $Exact<{
 }>
 
 export type chatLocalNewConversationLocalRpcParam = $Exact<{
-  conversationTriple: chat1.ConversationIDTriple
+  conversation: ConversationInfoLocal
 }>
 
 export type chatLocalPostLocalRpcParam = $Exact<{
@@ -2602,6 +2607,11 @@ export type chatLocalPostLocalRpcParam = $Exact<{
 
 export type chatLocalResolveConversationLocalRpcParam = $Exact<{
   conversation: ConversationInfoLocal
+}>
+
+export type chatLocalUpdateTopicNameLocalRpcParam = $Exact<{
+  conversationID: chat1.ConversationID,
+  newTopicName: string
 }>
 
 export type configClearValueRpcParam = $Exact<{
@@ -3398,9 +3408,9 @@ type chatLocalGetMessagesLocalResult = ?Array<ConversationLocal>
 
 type chatLocalGetThreadLocalResult = ThreadView
 
-type chatLocalNewConversationLocalResult = chat1.ConversationID
+type chatLocalNewConversationLocalResult = ConversationInfoLocal
 
-type chatLocalResolveConversationLocalResult = ?Array<chat1.ConversationID>
+type chatLocalResolveConversationLocalResult = ?Array<ConversationInfoLocal>
 
 type configCheckAPIServerOutOfDateWarningResult = OutOfDateInfo
 
@@ -3624,6 +3634,7 @@ export type rpc =
   | chatLocalNewConversationLocalRpc
   | chatLocalPostLocalRpc
   | chatLocalResolveConversationLocalRpc
+  | chatLocalUpdateTopicNameLocalRpc
   | configCheckAPIServerOutOfDateWarningRpc
   | configClearValueRpc
   | configGetConfigRpc
