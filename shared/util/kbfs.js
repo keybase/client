09babc2dec56cbce798f0202741c4381c9a0a1ee
Marco Munizaga <marco@marcopolo.io>
2016-09-01 15:26:47 -0700

    Handle readers in folder name

--- shared/util/kbfs.js
+++ shared/util/kbfs.js
@@ -1,14 +1,34 @@
 /* @flow */
+import type {UserList} from '../common-adapters/usernames'
 
-// Parses the folder name and returns an array of usernames (TODO: handle read only-ers)
-export function parseFolderNameToUsers (folderName: string): Array<string> {
-  return folderName.split(',')
+// Parses the folder name and returns an array of usernames
+export function parseFolderNameToUsers (yourUsername: ?string, folderName: string): UserList {
+  const [rwers, readers = ''] = folderName.split('#')
+  return rwers.split(',')
+    .map(u => ({
+      username: u,
+      you: yourUsername === u,
+    }))
+    .concat(
+      readers
+      .split(',')
+      .map(u => ({
+        username: u,
+        you: yourUsername === u,
+        readOnly: true,
+      }))
+    ).filter(u => !!u.username)
 }
 
-// Make sure the given username is at the front of the array.
-// To fit our canonical representation of foldernames (yourself being in the front)
-export function canonicalizeUsernames (username: string, usernames: Array<string>): Array<string> {
-  return [].concat(usernames.filter(u => u === username), usernames.filter(u => u !== username))
+export function sortUserList (users: UserList): UserList {
+  const youAsRwer = users.filter(u => u.you && !u.readOnly)
+  const rwers = users.filter(u => !u.you && !u.readOnly)
+  const youAsReader = users.filter(u => u.you && !!u.readOnly)
+  const readers = users.filter(u => !u.you && !!u.readOnly)
+
+  // Turn boolean into int for flow to be okay with this type
+  const sortByUsername = (a, b) => +(a.username > b.username)
+  return youAsRwer.concat(rwers.sort(sortByUsername), youAsReader, readers.sort(sortByUsername))
 }
 
 export function stripPublicTag (folderName: string): string {
