yanzay/kbfs

libkbfs: infrastructure for sending sync status updates
Issue: KBFS-1411

libkbfs/keybase_service_base.go
@@ -37,6 +37,7 @@ type KeybaseServiceBase struct {
 
 	lastNotificationFilenameLock sync.Mutex
 	lastNotificationFilename     string
+	lastSyncNotificationPathname string
 }
 
 // NewKeybaseServiceBase makes a new KeybaseService.
@@ -444,6 +445,25 @@ func (k *KeybaseServiceBase) Notify(ctx context.Context, notification *keybase1.
 		}
 	}()
 	return k.kbfsClient.FSEvent(ctx, *notification)
+}
+
+// NotifySyncStatus implements the KeybaseService interface for
+// KeybaseServiceBase.
+func (k *KeybaseServiceBase) NotifySyncStatus(ctx context.Context,
+	status *keybase1.FSPathSyncStatus) error {
+	// Reduce log spam by not repeating log lines for
+	// notifications with the same pathname.
+	//
+	// TODO: Only do this in debug mode.
+	func() {
+		k.lastNotificationFilenameLock.Lock()
+		defer k.lastNotificationFilenameLock.Unlock()
+		if status.Pathname != k.lastSyncNotificationPathname {
+			k.lastSyncNotificationPathname = status.Pathname
+			k.log.CDebugf(ctx, "Sending notification for %s", status.Pathname)
+		}
+	}()
+	return k.kbfsClient.FSSyncEvent(ctx, *status)
 }
 
 // FlushUserFromLocalCache implements the KeybaseService interface for

keybase/client

libkbfs/keybase_service_base.go
@@ -37,6 +37,7 @@ type KeybaseServiceBase struct {
 
 	lastNotificationFilenameLock sync.Mutex
 	lastNotificationFilename     string
+	lastSyncNotificationPath     string
 }
 
 // NewKeybaseServiceBase makes a new KeybaseService.
@@ -444,6 +445,25 @@ func (k *KeybaseServiceBase) Notify(ctx context.Context, notification *keybase1.
 		}
 	}()
 	return k.kbfsClient.FSEvent(ctx, *notification)
+}
+
+// NotifySyncStatus implements the KeybaseService interface for
+// KeybaseServiceBase.
+func (k *KeybaseServiceBase) NotifySyncStatus(ctx context.Context,
+	status *keybase1.FSPathSyncStatus) error {
+	// Reduce log spam by not repeating log lines for
+	// notifications with the same pathname.
+	//
+	// TODO: Only do this in debug mode.
+	func() {
+		k.lastNotificationFilenameLock.Lock()
+		defer k.lastNotificationFilenameLock.Unlock()
+		if status.Path != k.lastSyncNotificationPath {
+			k.lastSyncNotificationPath = status.Path
+			k.log.CDebugf(ctx, "Sending notification for %s", status.Path)
+		}
+	}()
+	return k.kbfsClient.FSSyncEvent(ctx, *status)
 }
 
 // FlushUserFromLocalCache implements the KeybaseService interface for