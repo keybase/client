51c5a953eb1f8a774fc413c5547a61d8380046b5
Gabriel Handford <gabrielh@gmail.com>
2016-09-02 11:13:33 -0700

    Revert "Dial socket paths (for sandbox support)"

--- go/libkb/socket_windows.go
+++ go/libkb/socket_windows.go
@@ -18,7 +18,7 @@ import (
 
 func NewSocket(g *GlobalContext) (ret Socket, err error) {
 	var s string
-	s, err = g.Env.GetSocketBindFile()
+	s, err = g.Env.GetSocketFile()
 	if err != nil {
 		return
 	}
@@ -26,21 +26,20 @@ func NewSocket(g *GlobalContext) (ret Socket, err error) {
 		err = errors.New("Empty SocketFile, can't make pipe")
 		return
 	}
-	s = `\\.\pipe\kbservice` + strings.TrimPrefix(s, filepath.VolumeName(s))
+	s = strings.TrimPrefix(s, filepath.VolumeName(s))
 	return SocketInfo{
 		Contextified: NewContextified(g),
-		bindFile:     s,
-		dialFiles:    []string{s},
+		file:         `\\.\pipe\kbservice` + s,
 	}, nil
 }
 
 func (s SocketInfo) BindToSocket() (ret net.Listener, err error) {
-	s.G().Log.Info("Binding to pipe:%s", s.bindFile)
-	return npipe.Listen(s.bindFile)
+	s.G().Log.Info("Binding to pipe:%s", s.file)
+	return npipe.Listen(s.file)
 }
 
 func (s SocketInfo) DialSocket() (ret net.Conn, err error) {
-	pipe, err := npipe.DialTimeout(s.dialFiles[0], time.Duration(1)*time.Second)
+	pipe, err := npipe.DialTimeout(s.file, time.Duration(1)*time.Second)
 	if err != nil {
 		// Be sure to return a nil interface, and not a nil npipe.PipeConn
 		// See https://keybase.atlassian.net/browse/CORE-2675 for when this
