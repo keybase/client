480cd0f1c248dea53c86839c9bd94979f92e43b0
Taru Karttunen <taruti@taruti.net>
2016-09-05 22:29:55 +0300

    libfs: Document PrettyJSON

--- libfs/json.go
+++ libfs/json.go
@@ -8,6 +8,7 @@ import (
 	"encoding/json"
 )
 
+// PrettyJSON marshals a value to human-readable JSON.
 func PrettyJSON(value interface{}) ([]byte, error) {
 	data, err := json.MarshalIndent(value, "", "  ")
 	if err != nil {
