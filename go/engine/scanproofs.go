9f9dfb7e70fe578e01f1e4ee2cadd18719d7702a
Miles Steele <miles@milessteele.com>
2016-09-14 16:03:38 -0400

    fix missing-hint case, move tickers lower, check forgotten error

--- go/engine/scanproofs.go
+++ go/engine/scanproofs.go
@@ -60,6 +60,8 @@ func (c *ScanProofsCache) Save(filepath string) error {
 	return nil
 }
 
+type ScanProofsTickers map[keybase1.ProofType]*time.Ticker
+
 type ScanProofsEngine struct {
 	libkb.Contextified
 	infile     string
@@ -247,17 +249,11 @@ func (e *ScanProofsEngine) Run(ctx *Context) (err error) {
 	return nil
 }
 
-func (e *ScanProofsEngine) ProcessOne(i int, rec map[string]string, cache *ScanProofsCache, ignored []string, tickers map[keybase1.ProofType]*time.Ticker) error {
+func (e *ScanProofsEngine) ProcessOne(i int, rec map[string]string, cache *ScanProofsCache, ignored []string, tickers ScanProofsTickers) error {
 	serverstate, err := strconv.Atoi(rec["state"])
 	if err != nil {
 		return fmt.Errorf("Could not read serverstate: %v", err)
 	}
-	ptypestr, err := strconv.Atoi(rec["proof_type"])
-	if err != nil {
-		return fmt.Errorf("Could not read proof_type: %v", err)
-	}
-	// Note: There is no gaurantee this is a valid prooftype.
-	ptype := keybase1.ProofType(ptypestr)
 
 	shouldsucceed := true
 	skip := false
@@ -311,64 +307,86 @@ func (e *ScanProofsEngine) ProcessOne(i int, rec map[string]string, cache *ScanP
 		return nil
 	}
 
-	// Beyond this point, external requests will occur, and rate limiting is used
-	if tickers[ptype] != nil {
-		e.G().Log.Info("Waiting for ticker: %v (%v)", keybase1.ProofTypeRevMap[ptype], ptype)
-		<-tickers[ptype].C
+	perr1, foundhint1, err := e.CheckOne(rec, false, tickers)
+	if err != nil {
+		return err
+	}
+	perr2, foundhint2, err := e.CheckOne(rec, true, tickers)
+	if err != nil {
+		return err
 	}
 
-	perr1, err := e.CheckOne(rec, false)
-	perr2, err := e.CheckOne(rec, true)
+	if foundhint1 != foundhint2 {
+		return fmt.Errorf("Local verifiers disagree: foundhint1:%v foundhint:%v (likely timing)",
+			foundhint1, foundhint2)
+	}
 
 	if (perr1 == nil) != (perr2 == nil) {
 		return fmt.Errorf("Local verifiers disagree:\n  %v\n  %v", perr1, perr2)
 	}
 
-	if (perr1 == nil) != shouldsucceed {
+	succeeded := foundhint1 && (perr1 == nil)
+
+	if succeeded != shouldsucceed {
 		return fmt.Errorf("Local verifiers disagree with server: server:%v client:%v", serverstate, perr1)
 	}
 
 	return nil
 }
 
-func (e *ScanProofsEngine) CheckOne(rec map[string]string, forcepvl bool) (libkb.ProofError, error) {
+// CheckOne checks one proof using two checkers (default, pvl).
+// Returns nil or an error, whether a hint was found, and any more serious error
+func (e *ScanProofsEngine) CheckOne(rec map[string]string, forcepvl bool, tickers ScanProofsTickers) (libkb.ProofError, bool, error) {
 	uid := keybase1.UID(rec["uid"])
 	sigid := keybase1.SigID(rec["sig_id"])
 
+	foundhint := false
 	hint, err := e.GetSigHint(uid, sigid)
 	if err != nil {
-		return nil, err
+		return nil, foundhint, err
 	}
+	if hint == nil {
+		return nil, foundhint, nil
+	}
+	foundhint = true
 
 	link, err := e.GetRemoteProofChainLink(uid, sigid)
 	if err != nil {
-		return nil, err
+		return nil, foundhint, err
 	}
 
 	pc, err := libkb.MakeProofChecker(e.G().Services, link)
 	if err != nil {
-		return nil, err
+		return nil, foundhint, err
+	}
+
+	// Beyond this point, external requests will occur, and rate limiting is used
+	ptype := link.GetProofType()
+	if tickers[ptype] != nil {
+		e.G().Log.Info("Waiting for ticker: %v (%v)", keybase1.ProofTypeRevMap[ptype], ptype)
+		<-tickers[ptype].C
 	}
 
 	if forcepvl {
 		perr := pvl.CheckProof(e.G(), pvl.GetHardcodedPvl(), link.GetProofType(),
 			pvl.NewProofInfo(link, *hint))
-		return perr, nil
+		return perr, foundhint, nil
 	}
 
 	perr := pc.CheckHint(e.G(), *hint)
 	if perr != nil {
-		return perr, nil
+		return perr, foundhint, nil
 	}
 
 	perr = pc.CheckStatus(e.G(), *hint)
 	if perr != nil {
-		return perr, nil
+		return perr, foundhint, nil
 	}
 
-	return perr, nil
+	return perr, foundhint, nil
 }
 
+// GetSigHint gets the SigHint. This can return (nil, nil) if nothing goes wrong but there is no hint.
 func (e *ScanProofsEngine) GetSigHint(uid keybase1.UID, sigid keybase1.SigID) (*libkb.SigHint, error) {
 	sighints, err := libkb.LoadAndRefreshSigHints(uid, e.G())
 	if err != nil {
@@ -377,9 +395,8 @@ func (e *ScanProofsEngine) GetSigHint(uid keybase1.UID, sigid keybase1.SigID) (*
 
 	sighint := sighints.Lookup(sigid)
 	if sighint == nil {
-		return nil, fmt.Errorf("No sighint for sigid %v", sigid)
+		return nil, nil
 	}
-
 	return sighint, nil
 }
 
