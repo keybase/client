9ff0008567fc98789ac2e855179fa2b90315f451
chrisnojima <cnojima@keyba.se>
2016-09-19 17:17:39 -0400

    add 2 second delay to button

--- shared/settings/delete-confirm/index.desktop.js
+++ shared/settings/delete-confirm/index.desktop.js
@@ -1,33 +1,58 @@
 // @flow
-import React from 'react'
+import React, {Component} from 'react'
 import {globalStyles, globalMargins, globalColors} from '../../styles'
 import {Box, Button, Icon, Text, Avatar} from '../../common-adapters'
 
 import type {Props} from './index'
 
-function DeleteConfirm (props: Props) {
-  return (
-    <Box style={{...globalStyles.flexBoxColumn, alignItems: 'center', justifyContent: 'center', flex: 1}}>
-      <Avatar
-        size={48}
-        username={props.username}>
-        <Icon type='iconfont-remove' style={iconStyle} />
-      </Avatar>
-      <Text type='HeaderError' style={{...globalStyles.italic, textDecoration: 'line-through'}}>{props.username}</Text>
-      <Text type='Header' style={{marginTop: globalMargins.medium, width: 320, textAlign: 'center'}}>Are you sure you want to permanently delete your account?</Text>
-      <Box style={{...globalStyles.flexBoxRow, marginTop: globalMargins.medium}}>
-        <Button
-          type='Secondary'
-          label='Cancel'
-          onClick={props.onCancel} />
-        <Button
-          style={{margin: 0}}
-          type='Danger'
-          label='Yes, permanently delete it'
-          onClick={props.onDeleteForever} />
+type State = {
+  allowDeleteForever: boolean,
+}
+
+class DeleteConfirm extends Component<void, Props, State> {
+  state: State;
+
+  constructor (props: Props) {
+    super(props)
+
+    this.state = {allowDeleteForever: false}
+  }
+
+  componentWillMount () {
+    this.setState({allowDeleteForever: false})
+  }
+
+  componentDidMount () {
+    setTimeout(() => {
+      this.setState({allowDeleteForever: true})
+    }, 2000)
+  }
+
+  render () {
+    return (
+      <Box style={{...globalStyles.flexBoxColumn, alignItems: 'center', justifyContent: 'center', flex: 1}}>
+        <Avatar
+          size={48}
+          username={this.props.username}>
+          <Icon type='iconfont-remove' style={iconStyle} />
+        </Avatar>
+        <Text type='HeaderError' style={{...globalStyles.italic, textDecoration: 'line-through'}}>{this.props.username}</Text>
+        <Text type='Header' style={{marginTop: globalMargins.medium, width: 320, textAlign: 'center'}}>Are you sure you want to permanently delete your account?</Text>
+        <Box style={{...globalStyles.flexBoxRow, marginTop: globalMargins.medium}}>
+          <Button
+            type='Secondary'
+            label='Cancel'
+            onClick={this.props.onCancel} />
+          <Button
+            style={{margin: 0}}
+            disabled={!this.state.allowDeleteForever}
+            type='Danger'
+            label='Yes, permanently delete it'
+            onClick={this.props.onDeleteForever} />
+        </Box>
       </Box>
-    </Box>
-  )
+    )
+  }
 }
 
 const iconStyle = {
