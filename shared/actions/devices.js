bc406d6900fb7376bdedbc0c941b8e616a78ca92
Chris Ball <chris@printf.net>
2016-09-20 17:26:55 -0400

    WIP working

--- shared/actions/devices.js
+++ shared/actions/devices.js
@@ -1,138 +1,178 @@
 // @flow
 import * as Constants from '../constants/devices'
 import HiddenString from '../util/hidden-string'
-import type {AsyncAction} from '../constants/types/flux'
 import {Map} from 'immutable'
 import {devicesTab, loginTab} from '../constants/tabs'
-import {loginDeprovisionRpc, revokeRevokeDeviceRpc, deviceDeviceHistoryListRpc,
-  loginPaperKeyRpc} from '../constants/types/flow-types'
+import {loginDeprovisionRpcPromise, revokeRevokeDeviceRpcPromise, deviceDeviceHistoryListRpcPromise, loginPaperKeyRpc} from '../constants/types/flow-types'
 import {navigateTo, navigateUp, switchTab} from './router'
 import {setRevokedSelf} from './login'
+import {buffers, eventChannel, END, takeLatest, takeEvery} from 'redux-saga'
+import type {SagaGenerator} from '../constants/types/saga'
+import type {GeneratePaperKey, IncomingDisplayPaperKeyPhrase, LoadDevices, LoadingDevices, ShowDevices, RemoveDevice} from '../constants/devices'
+import {call, put, take, select} from 'redux-saga/effects'
 
-export function loadDevices () : AsyncAction {
-  return (dispatch) => {
-    dispatch({
-      type: Constants.loadingDevices,
-      payload: null,
-    })
+export function loadDevices (): LoadDevices {
+  console.log('in loadDevices')
+  return {type: Constants.loadDevices, payload: undefined}
+}
 
-    deviceDeviceHistoryListRpc({
-      callback: (error, devices) => {
-        // Flow is weird here, we have to give it true or false directly instead of just giving it !!error
-        if (error) {
-          dispatch({
-            type: Constants.showDevices,
-            payload: error,
-            error: true,
-          })
-        } else {
-          dispatch({
-            type: Constants.showDevices,
-            payload: devices,
-            error: false,
-          })
-        }
-      },
-    })
+export function loadingDevices (): LoadingDevices {
+  return {type: Constants.loadingDevices, payload: undefined}
+}
+
+export function removeDevice (deviceID: string, name: string, currentDevice: boolean): RemoveDevice {
+  return {
+    type: Constants.removeDevice,
+    payload: {
+      deviceID: deviceID, name: name, currentDevice: currentDevice,
+    },
   }
 }
 
-export function generatePaperKey () : AsyncAction {
-  return (dispatch) => {
-    dispatch({
-      type: Constants.paperKeyLoading,
-      payload: null,
-    })
+export function generatePaperKey (): GeneratePaperKey {
+  return {type: Constants.generatePaperKey, payload: undefined}
+}
 
-    const incomingCallMap = {
-      'keybase.1.loginUi.promptRevokePaperKeys': (param, response) => {
-        response.result(false)
-      },
-      'keybase.1.secretUi.getPassphrase': (param, response) => {
-        console.log(param)
-      },
-      'keybase.1.loginUi.displayPaperKeyPhrase': ({phrase: paperKey}, response) => {
-        dispatch({
-          type: Constants.paperKeyLoaded,
-          payload: new HiddenString(paperKey),
-        })
-        response.result()
-      },
+function * _deviceListSaga (): SagaGenerator<any, any> {
+  console.log('in _deviceListSaga')
+  yield put(loadingDevices())
+  try {
+    const devices = yield call(deviceDeviceHistoryListRpcPromise)
+    const showDevicesAction: ShowDevices = {
+      type: Constants.showDevices,
+      payload: devices,
+      error: false,
     }
-
-    loginPaperKeyRpc({
-      incomingCallMap,
-      callback: (error, paperKey) => {
-        if (error) {
-          dispatch({
-            type: Constants.paperKeyLoaded,
-            payload: error,
-            error: true,
-          })
-        }
-      },
-    })
+    yield put(showDevicesAction)
+  } catch (e) {
+    const showDevicesWithErrorAction: ShowDevices = {
+      type: Constants.showDevices,
+      payload: e,
+      error: true,
+    }
+    yield put(showDevicesWithErrorAction)
   }
 }
 
-export function removeDevice (deviceID: string, name: string, currentDevice: boolean): AsyncAction {
-  return (dispatch, getState) => {
-    if (currentDevice) {
+function * _deviceRemoveSaga (action: RemoveDevice): SagaGenerator<any, any> {
+  if (action.payload.currentDevice) {
+    try {
       // Revoking the current device uses the "deprovision" RPC instead.
-      const username = getState().config.username
+      const username = yield select(state => state.config && state.config.username)
       if (!username) {
         console.warn('No username in removeDevice')
         return
       }
-      loginDeprovisionRpc({
-        param: {username, doRevoke: true},
-        callback: error => {
-          if (error) {
-            dispatch({
-              type: Constants.deviceRemoved,
-              payload: error,
-              error: true,
-            })
-          } else {
-            dispatch({
-              type: Constants.deviceRemoved,
-              payload: null,
-              error: false,
-            })
-          }
-
-          if (!error) {
-            dispatch(loadDevices())
-            dispatch(setRevokedSelf(name))
-            dispatch(navigateTo([], loginTab))
-            dispatch(switchTab(loginTab))
-          }
+      yield call(loginDeprovisionRpcPromise, {param: {username, doRevoke: true}})
+      yield put({
+        type: Constants.deviceRemoved,
+        payload: null,
+        error: false,
+      })
+      yield put(loadDevices())
+      yield put(setRevokedSelf(action.payload.name))
+      yield put(navigateTo([], loginTab))
+      yield put(switchTab(loginTab))
+    } catch (e) {
+      console.warn('Error removing the current device:', e)
+      yield put({
+        type: Constants.deviceRemoved,
+        payload: e,
+        error: true,
+      })
+    }
+  } else {
+    // Not the current device.
+    try {
+      console.warn('calling revoke')
+      console.warn(action.payload.deviceID)
+      yield call(revokeRevokeDeviceRpcPromise, {
+        param: {
+          deviceID: action.payload.deviceID,
+          force: false,
         },
       })
-    } else {
-      revokeRevokeDeviceRpc({
-        param: {deviceID, force: false},
-        callback: error => {
-          if (error) {
-            dispatch({
-              type: Constants.deviceRemoved,
-              payload: error,
-              error: true,
-            })
-          } else {
-            dispatch({
-              type: Constants.deviceRemoved,
-              payload: null,
-              error: false,
-            })
-          }
-
-          if (!error) {
-            dispatch(loadDevices())
-            dispatch(navigateUp(devicesTab, Map({path: 'root'})))
-          }
-        },
+      yield put({
+        type: Constants.deviceRemoved,
+        payload: null,
+        error: false,
+      })
+    } catch (e) {
+      console.warn('Error removing a device:', e)
+      yield put({
+        type: Constants.deviceRemoved,
+        payload: e,
+        error: true,
       })
     }
   }
+  yield put(loadDevices())
+  yield put(navigateUp(devicesTab, Map({path: 'root'})))
 }
+
+function _generatePaperKey () {
+  return eventChannel(emit => {
+    loginPaperKeyRpc({
+      incomingCallMap: {
+        'keybase.1.loginUi.promptRevokePaperKeys': (param, response) => {
+          response.result(false)
+        },
+        'keybase.1.loginUi.displayPaperKeyPhrase': ({phrase: paperKey}, response) => {
+          emit(({
+            type: 'keybase.1.loginUi.displayPaperKeyPhrase',
+            payload: {params: {paperKey: new HiddenString(paperKey)}, response},
+          }: IncomingDisplayPaperKeyPhrase))
+        },
+      },
+      callback: (error) => {
+        emit({
+          type: 'finished',
+          payload: {error},
+        })
+        emit(END)
+      },
+    })
+
+    // TODO(MM) this is the unsubscribe function, not sure what we can do here,
+    // maybe cancel ongoing rpc requests?
+    return () => {}
+  }, buffers.fixed())
+}
+
+function * _devicePaperKeySaga (): SagaGenerator<any, any> {
+  yield put({
+    type: Constants.paperKeyLoading,
+    payload: null,
+  })
+  const generatePaperKeyChan = yield call(_generatePaperKey)
+  try {
+    const displayPaperKeyPhrase:
+    ?IncomingDisplayPaperKeyPhrase = yield take(generatePaperKeyChan, 'keybase.1.loginUi.displayPaperKeyPhrase')
+    if (!displayPaperKeyPhrase) {
+      console.warn('no displayPaperKeyPhrase response')
+      yield put({
+        type: Constants.paperKeyLoaded,
+        error: true,
+      })
+      return
+    }
+    yield put({
+      type: Constants.paperKeyLoaded,
+      payload: displayPaperKeyPhrase.payload.params.paperKey,
+    })
+    displayPaperKeyPhrase.payload.response.result()
+  } catch (e) {
+    generatePaperKeyChan && generatePaperKeyChan.close()
+    console.warn('error in generating paper key', e)
+  }
+}
+
+function * deviceSaga (): SagaGenerator<any, any> {
+  yield [
+    takeLatest(Constants.loadDevices, _deviceListSaga),
+    takeEvery(Constants.removeDevice, _deviceRemoveSaga),
+    takeEvery(Constants.generatePaperKey, _devicePaperKeySaga),
+  ]
+}
+
+export default deviceSaga
--- shared/constants/devices.js
+++ shared/constants/devices.js
@@ -1,6 +1,31 @@
 // @flow
-export const deviceRemoved = 'devices:deviceRemoved'
+import HiddenString from '../util/hidden-string'
+
+import type {TypedAction, NoErrorTypedAction} from './types/flux'
+
+export const loadDevices = 'devices:loadDevices'
+export type LoadDevices = NoErrorTypedAction<'devices:loadDevices', void>
+
 export const loadingDevices = 'devices:loadingDevices'
+export type LoadingDevices = NoErrorTypedAction<'devices:loadingDevices', void>
+
+export const removeDevice = 'devices:removeDevice'
+export type RemoveDevice = NoErrorTypedAction<'devices:removeDevice', {
+  deviceID: string,
+  name: string,
+  currentDevice: boolean,
+}>
+
+export const deviceRemoved = 'devices:deviceRemoved'
+export type DeviceRemoved = TypedAction<'devices:deviceRemoved', void, {errorText: string}>
+
 export const paperKeyLoaded = 'devices:paperKeyLoaded'
 export const paperKeyLoading = 'devices:paperKeyLoading'
+
 export const showDevices = 'devices:showDevices'
+export type ShowDevices = TypedAction<'devices:showDevices', void, {errorText: string}>
+
+export const generatePaperKey = 'devices:generatePaperKey'
+export type GeneratePaperKey = NoErrorTypedAction<'devices:generatePaperKey', void>
+
+export type IncomingDisplayPaperKeyPhrase = NoErrorTypedAction<'keybase.1.loginUi.displayPaperKeyPhrase', {params: {paperKey: HiddenString}, response: {result: () => void}}>
