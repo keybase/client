keybase/kbfs

libkbfs: changes to support local use of MDv3

libkbfs/bare_root_metadata_v3.go
@@ -995,6 +995,32 @@ func (md *BareRootMetadataV3) fillInDevices(crypto Crypto,
 	return newServerKeys, nil
 }
 
+// GetTLFWriterKeyBundleID implements the BareRootMetadata interface for BareRootMetadataV3.
+func (md *BareRootMetadataV3) GetTLFWriterKeyBundleID() TLFWriterKeyBundleID {
+	return md.WriterMetadata.WKeyBundleID
+}
+
+// GetTLFReaderKeyBundleID implements the BareRootMetadata interface for BareRootMetadataV3.
+func (md *BareRootMetadataV3) GetTLFReaderKeyBundleID() TLFReaderKeyBundleID {
+	return md.RKeyBundleID
+}
+
+// OnRekeyDone implements the MutableBareRootMetadata interface for BareRootMetadataV3.
+func (md *BareRootMetadataV3) OnRekeyDone(config Config, extra ExtraMetadata) error {
+	extraV3, ok := extra.(*ExtraMetadataV3)
+	if !ok {
+		return errors.New("Invalid extra metadata")
+	}
+	var err error
+	md.WriterMetadata.WKeyBundleID, err =
+		config.Crypto().MakeTLFWriterKeyBundleID(extraV3.wkb)
+	if err != nil {
+		return err
+	}
+	md.RKeyBundleID, err = config.Crypto().MakeTLFReaderKeyBundleID(extraV3.rkb)
+	return err
+}
+
 // BareRootMetadataSignedV3 is the MD that is signed by the reader or
 // writer including the signature info. Unlike RootMetadataSigned,
 // it contains exactly the serializable metadata and signature info.

libkbfs/root_metadata.go
@@ -123,6 +123,8 @@ var _ KeyMetadata = (*RootMetadata)(nil)
 // NewRootMetadata returns a new RootMetadata object at the latest known version.
 func NewRootMetadata() *RootMetadata {
 	return &RootMetadata{bareMd: &BareRootMetadataV2{}}
+	// MDv3 TODO: uncomment the below when we're ready for MDv3
+	//return &RootMetadata{bareMd: &BareRootMetadataV3{}}
 }
 
 // Data returns the private metadata of this RootMetadata.
@@ -648,6 +650,10 @@ func (md *RootMetadata) fillInDevices(crypto Crypto,
 	return serverKeyMap{}, errors.New("Unknown bare metadata version")
 }
 
+func (md *RootMetadata) onRekeyDone(config Config) error {
+	return md.bareMd.OnRekeyDone(config, md.extra)
+}
+
 // A ReadOnlyRootMetadata is a thin wrapper around a
 // *RootMetadata. Functions that take a ReadOnlyRootMetadata parameter
 // must not modify it, and therefore code that passes a
@@ -730,6 +736,8 @@ type RootMetadataSigned struct {
 // NewRootMetadataSigned returns a new RootMetadataSigned object at the latest known version.
 func NewRootMetadataSigned() *RootMetadataSigned {
 	return &RootMetadataSigned{MD: &BareRootMetadataV2{}}
+	// MDv3 TODO: uncomment the below when we're ready for MDv3
+	//return &RootMetadataSigned{MD: &BareRootMetadataV3{}}
 }
 
 // MerkleHash computes a hash of this RootMetadataSigned object for inclusion

keybase/client

libkbfs/bare_root_metadata_v3.go
@@ -1002,6 +1002,32 @@ func (md *BareRootMetadataV3) fillInDevices(crypto Crypto,
 	return newServerKeys, nil
 }
 
+// GetTLFWriterKeyBundleID implements the BareRootMetadata interface for BareRootMetadataV3.
+func (md *BareRootMetadataV3) GetTLFWriterKeyBundleID() TLFWriterKeyBundleID {
+	return md.WriterMetadata.WKeyBundleID
+}
+
+// GetTLFReaderKeyBundleID implements the BareRootMetadata interface for BareRootMetadataV3.
+func (md *BareRootMetadataV3) GetTLFReaderKeyBundleID() TLFReaderKeyBundleID {
+	return md.RKeyBundleID
+}
+
+// FinalizeRekey implements the MutableBareRootMetadata interface for BareRootMetadataV3.
+func (md *BareRootMetadataV3) FinalizeRekey(config Config, extra ExtraMetadata) error {
+	extraV3, ok := extra.(*ExtraMetadataV3)
+	if !ok {
+		return errors.New("Invalid extra metadata")
+	}
+	var err error
+	md.WriterMetadata.WKeyBundleID, err =
+		config.Crypto().MakeTLFWriterKeyBundleID(extraV3.wkb)
+	if err != nil {
+		return err
+	}
+	md.RKeyBundleID, err = config.Crypto().MakeTLFReaderKeyBundleID(extraV3.rkb)
+	return err
+}
+
 // BareRootMetadataSignedV3 is the MD that is signed by the reader or
 // writer including the signature info. Unlike RootMetadataSigned,
 // it contains exactly the serializable metadata and signature info.

libkbfs/root_metadata.go
@@ -124,6 +124,8 @@ var _ KeyMetadata = (*RootMetadata)(nil)
 // NewRootMetadata returns a new RootMetadata object at the latest known version.
 func NewRootMetadata() *RootMetadata {
 	return &RootMetadata{bareMd: &BareRootMetadataV2{}}
+	// MDv3 TODO: uncomment the below when we're ready for MDv3
+	//return &RootMetadata{bareMd: &BareRootMetadataV3{}}
 }
 
 // Data returns the private metadata of this RootMetadata.
@@ -654,6 +656,10 @@ func (md *RootMetadata) fillInDevices(crypto Crypto,
 	return serverKeyMap{}, errors.New("Unknown bare metadata version")
 }
 
+func (md *RootMetadata) finalizeRekey(config Config) error {
+	return md.bareMd.FinalizeRekey(config, md.extra)
+}
+
 // A ReadOnlyRootMetadata is a thin wrapper around a
 // *RootMetadata. Functions that take a ReadOnlyRootMetadata parameter
 // must not modify it, and therefore code that passes a
@@ -736,6 +742,8 @@ type RootMetadataSigned struct {
 // NewRootMetadataSigned returns a new RootMetadataSigned object at the latest known version.
 func NewRootMetadataSigned() *RootMetadataSigned {
 	return &RootMetadataSigned{MD: &BareRootMetadataV2{}}
+	// MDv3 TODO: uncomment the below when we're ready for MDv3
+	//return &RootMetadataSigned{MD: &BareRootMetadataV3{}}
 }
 
 // MerkleHash computes a hash of this RootMetadataSigned object for inclusion