cc1841c8139dd6d1defd4aad076c209fcd4bfc53
chrisnojima <cnojima@keyba.se>
2016-09-29 14:49:52 -0400

    people

--- shared/people/index.js
+++ shared/people/index.js
@@ -1,25 +1,13 @@
+// @flow
+// $FlowIssue
+import PeopleRender from './index.render'
 import React, {Component} from 'react'
+import flags from '../util/feature-flags'
 import {connect} from 'react-redux'
 
-import PeopleRender from './index.render'
-import flags from '../util/feature-flags'
-
 class People extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {count: 0}
-  }
-
-  handleCountIncrease () {
-    this.setState({count: this.state.count + 1})
-  }
-
   render () {
-    return <PeopleRender
-      showComingSoon={!flags.tabPeopleEnabled}
-      count={this.state.count}
-      onCount={() => this.handleCountIncrease()}
-    />
+    return <PeopleRender showComingSoon={!flags.tabPeopleEnabled} />
   }
 
   static parseRoute () {
