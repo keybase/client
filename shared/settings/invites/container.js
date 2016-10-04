726014f3d4f3de23ee79904016cd1f704eba3c90
Chris Ball <chris@printf.net>
2016-10-04 12:34:18 -0400

    Invitation sending working

--- shared/settings/invites/container.js
+++ shared/settings/invites/container.js
@@ -14,23 +14,45 @@ type State = {
 
 class InvitationsContainer extends Component<void, Props, State> {
   state: State;
+
+  constructor (props: Props) {
+    super(props)
+    this.state = {
+      email: this.props.inviteEmail,
+      message: this.props.inviteMessage,
+    }
+  }
+
   componentWillMount () {
     this.props.onRefresh()
   }
 
+  onGenerateInvitation () {
+    const {email, message} = this.state
+    console.log('in first onGenerateInvitation')
+    console.log(email)
+    console.log(message)
+    this.props.onGenerateInvitation(email, message)
+  }
+
   render () {
-    return <Invites {...this.props} />
+    return <Invites
+      {...this.props}
+      onChangeInviteEmail={email => this.setState({email})}
+      onChangeInviteMessage={message => this.setState({message})}
+      onGenerateInvitation={() => this.onGenerateInvitation()}
+    />
   }
 }
 
 export default connect(
   (state: TypedState, ownProps: {}) => state.settings.invites,
   (dispatch: any, ownProps: {}) => ({
+    onGenerateInvitation: (email: string, message: string) => dispatch(invitesSend(email, message)),
+    onRefresh: () => dispatch(invitesRefresh()),
+    onReclaimInvitation: (inviteId: string) => dispatch(invitesReclaim(inviteId)),
     onSave: () => dispatch(notificationsSave()),
     onToggle: (name: string) => dispatch(notificationsToggle(name)),
     onToggleUnsubscribeAll: () => dispatch(notificationsToggle()),
-    onRefresh: () => dispatch(invitesRefresh()),
-    onReclaimInvitation: (inviteId: string) => dispatch(invitesReclaim(inviteId)),
-    onGenerateInvitation: (email: string, message: ?string) => dispatch(invitesSend(email, message)),
   }),
 )(InvitationsContainer)
