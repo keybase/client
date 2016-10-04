37a1fbb35e46ec7830eb78fc1b24d83077a040dc
Marco Munizaga <marco@marcopolo.io>
2016-10-04 14:20:34 -0700

    Block start-hot on hot-server being up

--- desktop/client.js
+++ desktop/client.js
@@ -1,3 +1,4 @@
+const http = require('http')
 const fs = require('fs')
 const spawn = require('child_process').spawn
 const electron = require('electron-prebuilt')
@@ -10,9 +11,18 @@ try {
 const name = 'dist/main.bundle.js'
 const params = [name]
 
-if (process.env.USE_INSPECTOR) {
-  params.unshift('--debug-brk=5858')
+const handle = () => {
+  const e = spawn(electron, params, {stdio: 'inherit'})
+  e.on('close', function () {})
 }
 
-const e = spawn(electron, params, {stdio: 'inherit'})
-e.on('close', function () {})
+const hitServer = () => {
+  var req = http.get('http://localhost:4000/dist/index.bundle.js', handle)
+  req.on('error', e => {
+    console.log('Error: ', e)
+    console.log('Sleeping 5 seconds and retrying. Maybe start the hot-server?')
+    setTimeout(() => hitServer(), 5000)
+  })
+}
+
+hitServer()
