a95ad0faad221a55a19c6eacb165297892a45fe0
chrisnojima <chrisnojima@gmail.com>
2016-10-05 16:19:04 -0400

    fix flow causing webpack issues (#4530)

--- desktop/server.js
+++ desktop/server.js
@@ -33,6 +33,7 @@ if (NO_SERVER) {
   const app = express()
 
   app.use(require('webpack-dev-middleware')(compiler, {
+    // $FlowIssue
     publicPath: config.output.publicPath,
     hot: true,
     lazy: false,
