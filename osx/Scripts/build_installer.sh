8ba7f13b169cc2df447fd7f4091f3132a1c3ac7f
Gabriel Handford <gabrielh@gmail.com>
2016-10-05 14:47:36 -0700

    Fix script var

--- osx/Scripts/build_installer.sh
+++ osx/Scripts/build_installer.sh
@@ -7,9 +7,10 @@ cd "$dir"
 
 app_name="KeybaseInstaller"
 plist="$dir/../Installer/Info.plist"
+copy_into_bundle=${COPY_INTO_BUNDLE:-}
 APP_NAME=$app_name SCHEME="Installer" PLIST="$plist" ./build.sh
 
-if [ "$COPY_INTO_BUNDLE" = "1" ]; then
+if [ "$copy_into_bundle" = "1" ]; then
   dest="/Applications/Keybase.app/Contents/Resources/$app_name.app"
   echo "Copying into bundle $dest"
   rm -rf "$dest"
