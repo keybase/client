fdd8145e6ee5dfef56d5dd0adac79c5e81bac11f
Gabriel Handford <gabrielh@gmail.com>
2016-10-07 14:09:14 -0700

    Fix eslint

--- shared/tab-bar/index.render.desktop.js
+++ shared/tab-bar/index.render.desktop.js
@@ -1,7 +1,7 @@
 // @flow
 
 import React, {Component} from 'react'
-import {Box, TabBar, Avatar, Icon} from '../common-adapters'
+import {Box, TabBar, Avatar} from '../common-adapters'
 import {TabBarButton, TabBarItem} from '../common-adapters/tab-bar'
 import {globalStyles, globalColors} from '../styles'
 import flags from '../util/feature-flags'
