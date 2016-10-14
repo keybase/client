f77aea4c24dced92603078f2e9a8bce4a157155f
Steve Sanders <steve@keyba.se>
2016-10-13 17:12:40 -0700

    Do smoke release command in installer build

--- packaging/windows/doinstaller_wix.cmd
+++ packaging/windows/doinstaller_wix.cmd
@@ -154,3 +154,10 @@ IF %ERRORLEVEL% NEQ 0 (
 IF %UpdateChannel% EQU Smoke (
   %ReleaseBin% update-json --version=%SEMVER% --src=%KEYBASE_INSTALLER_NAME% --uri=https://prerelease.keybase.io/windows --signature=%SigFile% --description=%GOPATH%\src\github.com\keybase\client\desktop\CHANGELOG.txt --prop=DokanProductCodeX64:%DokanProductCodeX64% --prop=DokanProductCodeX86:%DokanProductCodeX86% > update-windows-prod-test-v2.json
 )
+
+IF %UpdateChannel% EQU Smoke2 (
+  :: SmokeABuildID is a build parameter provided by the first smoke build
+  for /F delims^=^"^ tokens^=4 %%x in ('findstr version %GOPATH%\src\github.com\keybase\client\packaging\windows\%SmokeABuildID%\update-windows-prod.json') do set SmokeAVersion=%%x
+
+  echo %ReleaseBin% announce-build --build-a="%SmokeAVersion%" --build-b="%SEMVER%" --platform="windows"
+)
