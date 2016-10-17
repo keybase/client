c10d23cb4e6ad57c943027decdd922f6ecbd1590
Patrick Crosby <pcrosby@gmail.com>
2016-10-17 16:21:58 -0500

    Chat download using CmdChatAPI

--- go/client/cmd_chat_download.go
+++ go/client/cmd_chat_download.go
@@ -1,17 +1,21 @@
 package client
 
 import (
+	"context"
 	"errors"
+	"strconv"
 
 	"github.com/keybase/cli"
 	"github.com/keybase/client/go/libcmdline"
 	"github.com/keybase/client/go/libkb"
+	"github.com/keybase/client/go/protocol/chat1"
 )
 
 type CmdChatDownload struct {
 	libkb.Contextified
 	tlf        string
-	messageID  string
+	public     bool
+	messageID  uint64
 	outputFile string
 	preview    bool
 }
@@ -30,6 +34,10 @@ func newCmdChatDownload(cl *libcmdline.CommandLine, g *libkb.GlobalContext) cli.
 				Name:  "p, preview",
 				Usage: "Download preview",
 			},
+			cli.BoolFlag{
+				Name:  "public",
+				Usage: "Download attachment from public conversation",
+			},
 			cli.StringFlag{
 				Name:  "o, outfile",
 				Usage: "Specify output file (stdout default)",
@@ -43,14 +51,35 @@ func (c *CmdChatDownload) ParseArgv(ctx *cli.Context) error {
 		return errors.New("usage: keybase chat download <conversation> <attachment id> [-o filename]")
 	}
 	c.tlf = ctx.Args()[0]
-	c.messageID = ctx.Args()[1]
+	id, err := strconv.ParseUint(ctx.Args()[1], 10, 64)
+	if err != nil {
+		return err
+	}
+	c.messageID = id
 	c.outputFile = ctx.String("outfile")
 	c.preview = ctx.Bool("preview")
+	c.public = ctx.Bool("public")
 
 	return nil
 }
 
 func (c *CmdChatDownload) Run() error {
+	opts := downloadOptionsV1{
+		Channel: ChatChannel{
+			Name:   c.tlf,
+			Public: c.public,
+		},
+		MessageID: chat1.MessageID(c.messageID),
+		Output:    c.outputFile,
+		Preview:   c.preview,
+	}
+	api := &CmdChatAPI{
+		Contextified: libkb.NewContextified(c.G()),
+	}
+	reply := api.DownloadV1(context.Background(), opts)
+	if reply.Error != nil {
+		return errors.New(reply.Error.Message)
+	}
 	return nil
 }
 
