2bc7af82d1aa9f2049be0e1d1b32699daebd472c
Gabriel Handford <gabrielh@gmail.com>
2016-10-19 10:00:33 -0700

    Move comment up

--- packaging/prerelease/pull_build.sh
+++ packaging/prerelease/pull_build.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
+# If you ever update this script you should manually git pull on the
+# build machines since this script may live inside the repo that is
+# being pulled. This script is only meant to be a wrapper around
+# pulling and building so it won't need to be changed often.
+
 set -e -u -o pipefail # Fail on error
 
 dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd "$dir"
 
-# If you ever update this script you should manually git pull on the
-# build machines since this script may live inside the repo that is
-# being pulled. This script is only meant to be a wrapper around
-# building so we don't run into this problem.
 client_dir="$GOPATH/src/github.com/keybase/client"
 nopull=${NOPULL:-} # Don't git pull
 if [ ! "$nopull" = "1" ]; then
