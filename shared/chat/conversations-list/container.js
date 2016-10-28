81748674746a6b7fea5f66f2af6c4ce84c69eb40
chrisnojima <cnojima@keyba.se>
2016-10-28 14:51:24 -0400

    The inbox

---
+++ shared/chat/conversations-list/container.js
@@ -0,0 +1,16 @@
+// @flow
+import ConversationList from './index'
+import {connect} from 'react-redux'
+import {selectConversation} from '../../actions/chat'
+
+import type {ConversationIDKey} from '../../constants/chat'
+import type {TypedState} from '../../constants/reducer'
+
+export default connect(
+  (state: TypedState) => ({
+    inbox: state.chat.get('inbox'),
+  }),
+  (dispatch: Dispatch) => ({
+    onSelectConversation: (key: ConversationIDKey) => dispatch(selectConversation(key)),
+  })
+)(ConversationList)
