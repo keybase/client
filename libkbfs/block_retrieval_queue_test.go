06ade0d6739d5b4692184877b246a79f88b9f20a
John Zila <john@keyba.se>
2016-10-31 14:23:30 -0700

    Add more complicated priority preemption test

--- libkbfs/block_retrieval_queue_test.go
+++ libkbfs/block_retrieval_queue_test.go
@@ -48,3 +48,34 @@ func TestBlockRetrievalQueuePreemptPriority(t *testing.T) {
 	require.Equal(t, 1, br.priority)
 	require.Equal(t, uint64(0), br.insertionOrder)
 }
+
+func TestBlockRetrievalQueueInterleavedPreemption(t *testing.T) {
+	t.Log("Handle a first request and then preempt another one")
+	q := NewBlockRetrievalQueue(1)
+	require.NotNil(t, q)
+
+	ctx := context.Background()
+	ptr1 := makeFakeBlockPointer(t)
+	ptr2 := makeFakeBlockPointer(t)
+	block := &FileBlock{}
+	_ = q.Request(ctx, 1, ptr1, block)
+	_ = q.Request(ctx, 1, ptr2, block)
+
+	br := <-q.WorkOnRequest()
+	require.Equal(t, ptr1, br.blockPtr)
+	require.Equal(t, 1, br.priority)
+	require.Equal(t, uint64(0), br.insertionOrder)
+
+	ptr3 := makeFakeBlockPointer(t)
+	_ = q.Request(ctx, 2, ptr3, block)
+
+	br = <-q.WorkOnRequest()
+	require.Equal(t, ptr3, br.blockPtr)
+	require.Equal(t, 2, br.priority)
+	require.Equal(t, uint64(2), br.insertionOrder)
+
+	br = <-q.WorkOnRequest()
+	require.Equal(t, ptr2, br.blockPtr)
+	require.Equal(t, 1, br.priority)
+	require.Equal(t, uint64(1), br.insertionOrder)
+}
