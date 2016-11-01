aae89f65cbbc85a5061e9942129d4dceda0b778f
John Zila <john@keyba.se>
2016-10-31 17:20:27 -0700

    Added copyright notices

--- libkbfs/block_retrieval_queue_test.go
+++ libkbfs/block_retrieval_queue_test.go
@@ -1,3 +1,7 @@
+// Copyright 2016 Keybase Inc. All rights reserved.
+// Use of this source code is governed by a BSD
+// license that can be found in the LICENSE file.
+
 package libkbfs
 
 import (
