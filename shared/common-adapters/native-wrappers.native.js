2da6df19c49ed0121740068e85b9921fe0099bfd
Marco Munizaga <marco@marcopolo.io>
2016-11-09 23:40:59 -0800

    add native wrappers

---
+++ shared/common-adapters/native-wrappers.native.js
@@ -0,0 +1,50 @@
+// @flow
+import {
+  ActivityIndicator as NativeActivityIndicator,
+  Animated as NativeAnimated,
+  BackAndroid as NativeBackAndroid,
+  Clipboard as NativeClipboard,
+  DrawerLayoutAndroid as NativeDrawerLayoutAndroid,
+  Easing as NativeEasing,
+  Image as NativeImage,
+  KeyboardAvoidingView as NativeKeyboardAvoidingView,
+  Linking as NativeLinking,
+  ListView as NativeListView,
+  Modal as NativeModal,
+  Navigator as NativeNavigator,
+  Picker as NativePicker,
+  ScrollView as NativeScrollView,
+  StyleSheet as NativeStyleSheet,
+  Switch as NativeSwitch,
+  Text as NativeText,
+  TextInput as NativeTextInput,
+  TouchableNativeFeedback as NativeTouchableNativeFeedback,
+  TouchableWithoutFeedback as NativeTouchableWithoutFeedback,
+} from 'react-native'
+
+if (module.hot) {
+  // module.hot.accept(() => { console.warn('Ive accepted my fate here in native wrappers index') })
+}
+
+export {
+  NativeActivityIndicator,
+  NativeAnimated,
+  NativeBackAndroid,
+  NativeClipboard,
+  NativeDrawerLayoutAndroid,
+  NativeEasing,
+  NativeImage,
+  NativeKeyboardAvoidingView,
+  NativeLinking,
+  NativeListView,
+  NativeModal,
+  NativeNavigator,
+  NativePicker,
+  NativeScrollView,
+  NativeStyleSheet,
+  NativeSwitch,
+  NativeText,
+  NativeTextInput,
+  NativeTouchableNativeFeedback,
+  NativeTouchableWithoutFeedback,
+}
