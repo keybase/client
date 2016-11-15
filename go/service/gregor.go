d8d9db64e751ea7fb0bfd986d679c81f72539b78
Mike Maxim <mike@keyba.se>
2016-11-15 14:19:59 -0500

    debug

--- go/service/gregor.go
+++ go/service/gregor.go
@@ -865,8 +865,14 @@ func (g *gregorHandler) newChatActivity(ctx context.Context, m gregor.OutOfBandM
 			g.G().Log.Error("push handler: chat activity: error decoding newMessage: %s", err.Error())
 			return err
 		}
+
 		g.G().Log.Debug("push handler: chat activity: newMessage: convID: %s sender: %s",
 			nm.ConvID, nm.Message.ClientHeader.Sender)
+		if nm.Message.ClientHeader.OutboxID != nil {
+			g.G().Log.Debug("push handler: chat activity: newMessage: outboxID: %s",
+				nm.Message.ClientHeader.OutboxID)
+		}
+
 		uid := m.UID().Bytes()
 		decmsg, err := g.G().ConvSource.Push(ctx, nm.ConvID, gregor1.UID(uid), nm.Message)
 		if err != nil {
