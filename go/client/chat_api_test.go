50ed018ffec191855257ad5ff0fbd30109c4c50c
Patrick Crosby <pcrosby@gmail.com>
2016-11-15 15:08:36 -0600

    Remove conversation_id from json api

--- go/client/chat_api_test.go
+++ go/client/chat_api_test.go
@@ -206,10 +206,10 @@ var optTests = []optTest{
 	},
 	{
 		input: `{"method": "read", "params":{"version": 1, "options": {"conversation_id": "123"}}}`,
+		err:   ErrInvalidOptions{},
 	},
 	{
 		input: `{"method": "read", "params":{"version": 1, "options": {"channel": {"name": "alice,bob"}, "conversation_id": "999111"}}}`,
-		err:   ErrInvalidOptions{},
 	},
 	{
 		input: `{"method": "send", "params":{"version": 1}}`,
@@ -227,20 +227,13 @@ var optTests = []optTest{
 		input: `{"method": "send", "params":{"version": 1, "options": {"channel": {"name": "alice,bob"}, "message": {"body": "hi"}}}}`,
 	},
 	{
-		input: `{"method": "send", "params":{"version": 1, "options": {"conversation_id": "123", "message": {"body": "hi"}}}}`,
-	},
-	{
-		input: `{"method": "send", "params":{"version": 1, "options": {"conversation_id": "222", "channel": {"name": "alice,bob"}, "message": {"body": "hi"}}}}`,
-		err:   ErrInvalidOptions{},
-	},
-	{
 		input: `{"method": "list", "params":{"version": 1}}{"method": "list", "params":{"version": 1}}`,
 	},
 	{
 		input: `{"method": "list", "params":{"version": 1, "options": {"topic_type": "dEv"}}}`,
 	},
 	{
-		input: `{"method": "list", "params":{"version": 1}}{"method": "read", "params":{"version": 1, "options": {"conversation_id": "7777"}}}`,
+		input: `{"method": "list", "params":{"version": 1}}{"method": "read", "params":{"version": 1, "options": {"channel": {"name": "alice,bob"}}}}`,
 	},
 	{
 		input: `{"method": "read", "params":{"version": 1, "options": {"channel": {"name": "alice,bob"}}}`, /* missing closing bracket at end */
@@ -294,6 +287,7 @@ var optTests = []optTest{
 	},
 	{
 		input: `{"id": 30, "method": "edit", "params":{"version": 1, "options": {"conversation_id": "333", "message_id": 123, "message": {"body": "edited"}}}}`,
+		err:   ErrInvalidOptions{},
 	},
 	{
 		input: `{"id": 30, "method": "delete", "params":{"version": 1, "options": {}}}`,
@@ -384,10 +378,6 @@ var echoTests = []echoTest{
 		output: `{"result":{"status":"ok"}}`,
 	},
 	{
-		input:  `{"method": "read", "params":{"version": 1, "options": {"conversation_id": "123"}}}`,
-		output: `{"result":{"status":"ok"}}`,
-	},
-	{
 		input:  `{"method": "send", "params":{"version": 1, "options": {"channel": {"name": "alice,bob"}, "message": {"body": "hi"}}}}`,
 		output: `{"result":{"status":"ok"}}`,
 	},
@@ -396,7 +386,7 @@ var echoTests = []echoTest{
 		output: `{"result":{"status":"ok"}}` + "\n" + `{"result":{"status":"ok"}}`,
 	},
 	{
-		input:  `{"method": "list", "params":{"version": 1}}{"method": "read", "params":{"version": 1, "options": {"conversation_id": "123"}}}`,
+		input:  `{"method": "list", "params":{"version": 1}}{"method": "read", "params":{"version": 1, "options": {"channel": {"name": "alice,bob"}}}}`,
 		output: `{"result":{"status":"ok"}}` + "\n" + `{"result":{"status":"ok"}}`,
 	},
 	{
