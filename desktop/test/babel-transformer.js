6d59fed85bf366cbe08b641841ba95a49b3aa2c6
Max Goodman <c@chromakode.com>
2016-11-18 16:14:04 -0800

    Remove some hacks by limiting jest testing to shared code
    Needed to add a package.json to shared for the babel istanbul code
    coverage plugin, which looks for configuration by walking up the tree
    for a package.json.

--- desktop/test/babel-transformer.js
+++ desktop/test/babel-transformer.js
@@ -1,7 +1,9 @@
 // @flow
+const path = require('path')
 const fs = require('fs')
 
-const babelConfig = JSON.parse(fs.readFileSync('.babelrc', 'utf8'))
+const babelrcPath = path.join(__dirname, '../.babelrc')
+const babelConfig = JSON.parse(fs.readFileSync(babelrcPath, 'utf8'))
 babelConfig.presets = babelConfig.presets.map(require.resolve)
 babelConfig.plugins = babelConfig.plugins.filter(n => n !== 'babel-plugin-transform-runtime' && n !== 'react-hot-loader/babel')
 babelConfig.plugins = babelConfig.plugins.map(name => {
@@ -11,11 +13,5 @@ babelConfig.plugins = babelConfig.plugins.map(name => {
     return [require.resolve(name[0]), name[1]]
   }
 })
-
-// FIXME: Horrible hack to override cwd passed to babel-plugin-istanbul because
-// it ignores files outside of its notion of cwd (such as our shared dir).
-babelConfig.plugins.concat = function (plugin) {
-  return Array.prototype.concat.call(this, [[plugin, {cwd: '..'}]])
-}
 
 module.exports = require('babel-jest').createTransformer(babelConfig)
