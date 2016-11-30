320858754f6bbdec2c0933c0fa69b5274265d8a0
Max Goodman <c@chromakode.com>
2016-11-29 16:45:16 -0800

    visdiff: Blur textareas before screenshotting

--- shared/test/render-dumb-sheet.js
+++ shared/test/render-dumb-sheet.js
@@ -52,7 +52,8 @@ function onDisplay (ev, msg) {
   try {
     ReactDOM.render(displayTree, appEl, () => {
       // Remove pesky blinking cursors
-      if (document.activeElement.tagName === 'INPUT') {
+      if (document.activeElement.tagName === 'INPUT' ||
+          document.activeElement.tagName === 'TEXTAREA') {
         document.activeElement.blur()
       }
 
