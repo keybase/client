33ba4345f766d282e4aab08908d16743a4aa029a
Chris Ball <chris@printf.net>
2016-12-15 11:14:59 -0500

    Copy all message contents from pending to sent at once

--- shared/actions/chat.js
+++ shared/actions/chat.js
@@ -296,10 +296,7 @@ function * _incomingMessage (action: IncomingMessage): SagaGenerator<any, any> {
             type: Constants.pendingMessageWasSent,
             payload: {
               conversationIDKey,
-              deviceName: message.deviceName,
-              deviceType: message.deviceType,
-              outboxID: message.outboxID,
-              messageID: message.messageID,
+              message,
               messageState: 'sent',
             },
           })
