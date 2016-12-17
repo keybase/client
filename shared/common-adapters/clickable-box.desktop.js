620b17d7d73b1a7e642a95e1430d68bc4bd86250
chrisnojima <cnojima@keyba.se>
2016-12-16 22:13:47 -0500

    less work in Clickable box. Stop avatar flicker on known errored out avatars

--- shared/common-adapters/clickable-box.desktop.js
+++ shared/common-adapters/clickable-box.desktop.js
@@ -5,11 +5,22 @@ import {FlatButton} from 'material-ui'
 
 import type {Props} from './clickable-box'
 
-export default function ClickableBox (props: Props & {children: any}) {
+const ClickableBox = (props: Props & {children: any}) => {
   const {style, children, ...otherProps} = props
   return (
-    <FlatButton {...otherProps} style={{textAlign: 'left', height: undefined, width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'stretch'}}>
+    <FlatButton {...otherProps} style={_buttonStyle}>
       <Box style={style}>{children}</Box>
     </FlatButton>
   )
 }
+
+const _buttonStyle = {
+  textAlign: 'left',
+  height: undefined,
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'stretch',
+}
+
+export default ClickableBox
