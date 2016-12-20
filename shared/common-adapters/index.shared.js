aae018fc70df7b5f1d0a286cc35205a7180198e5
chrisnojima <cnojima@keyba.se>
2016-12-20 11:27:22 -0500

    WIP

---
+++ shared/common-adapters/index.shared.js
@@ -0,0 +1,11 @@
+// @flow
+import {initLookup} from './avatar'
+import type {AvatarLookup} from './avatar'
+
+const initAvatarLookup = (lookup: AvatarLookup) => {
+  initLookup(lookup)
+}
+
+export {
+  initAvatarLookup,
+}
