5d1f7235710967136fe05e64cca3f358e3feb8d2
chrisnojima <cnojima@keyba.se>
2017-01-04 10:45:10 -0500

    fix flow issue

--- shared/desktop/yarn-helper.js
+++ shared/desktop/yarn-helper.js
@@ -505,7 +505,6 @@ function exec (command, env, options) {
     env = process.env
   }
 
-  // $FlowIssue
   console.log(execSync(command, {env: env, stdio: 'inherit', encoding: 'utf8', ...options}))
 }
 
