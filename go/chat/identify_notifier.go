483ace867d04812e932bb583003854f97c1c33e2
Mike Maxim <mike@keyba.se>
2017-01-05 13:59:46 -0500

    simplify lock code

--- go/chat/identify_notifier.go
+++ go/chat/identify_notifier.go
@@ -24,20 +24,20 @@ func NewIdentifyNotifier(g *libkb.GlobalContext) *IdentifyNotifier {
 func (i *IdentifyNotifier) Send(update keybase1.CanonicalTLFNameAndIDWithBreaks) {
 	i.RLock()
 	tlfName := update.CanonicalName.String()
-	if stored, ok := i.identCache[tlfName]; ok {
+	stored, ok := i.identCache[tlfName]
+	i.RUnlock()
+	if ok {
 		// We have the exact update stored, don't send it again
 		if stored.Eq(update) {
-			defer i.RUnlock()
 			i.G().Log.Debug("IdentifyNotifier: hit cache, not sending notify: %s", tlfName)
 			return
 		}
 	}
-	i.RUnlock()
 
 	i.Lock()
-	defer i.Unlock()
+	i.identCache[tlfName] = update
+	i.Unlock()
 
 	i.G().Log.Debug("IdentifyNotifier: cache miss, sending notify: %s dat: %v", tlfName, update)
 	i.G().NotifyRouter.HandleChatIdentifyUpdate(context.Background(), update)
-	i.identCache[tlfName] = update
 }
