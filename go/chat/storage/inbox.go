9d01efbf074e2c557470c4d1ce6fc8542bb40ec4
Mike Maxim <mike@keyba.se>
2017-01-12 14:26:25 -0500

    easier to read

--- go/chat/storage/inbox.go
+++ go/chat/storage/inbox.go
@@ -84,7 +84,7 @@ type Inbox struct {
 func NewInbox(g *libkb.GlobalContext, uid gregor1.UID, getSecretUI func() libkb.SecretUI) *Inbox {
 	return &Inbox{
 		Contextified: libkb.NewContextified(g),
-		DebugLabeler: utils.NewDebugLabeler(g, "Inbox(uid="+uid.String()+")"),
+		DebugLabeler: utils.NewDebugLabeler(g, "Inbox"),
 		baseBox:      newBaseBox(g, getSecretUI),
 		uid:          uid,
 	}
