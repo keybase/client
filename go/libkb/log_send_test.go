3de3ec1fe65343c7eb5a0e2158694ca33871103e
Patrick Crosby <pcrosby@gmail.com>
2017-01-13 11:25:33 -0600

    Cleanup

--- go/libkb/log_send_test.go
+++ go/libkb/log_send_test.go
@@ -26,17 +26,8 @@ func testTail(t *testing.T, testname, filename string, count, actual int, first,
 }
 
 func TestTail(t *testing.T) {
-	// the file has 70k lines in it
-	// filename := filepath.Join("testfixtures", "tailfail.keybase.service.log")
-
 	// file has 20k lines in it
 	filename := filepath.Join("testfixtures", "longline.log")
-
-	/*
-		firstLine := `2017-01-13T01:51:51.096982 ▶ [DEBU keybase standard.go:146] 17303 | doRequestShared(getCli) for sesscheck`
-		firstLine10k := `2017-01-13T10:03:22.655195 ▶ [DEBU keybase standard.go:146] 6ca2 + Resolver#getFromMemCache("uid:95e88f2087e480cae28f08d81554bc00") [tags:RSLV=64rxb11Oiw_g]`
-		lastLine := "2017-01-13T10:26:55.964272 ▶ [DEBU keybase bg_identifier.go:302] 93a5 BackgroundIdentifier sleeping for 30s"
-	*/
 
 	firstLine := "00000"
 	firstLine10k := "10000"
