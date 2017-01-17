687fb21525a2fbc24752a238eadeade080c7fbc4
Jeremy Stribling <strib@alum.mit.edu>
2017-01-17 14:10:36 -0800

    kbfs_ops_test: check that rms don't need to fetch direct blocks
    Issue: KBFS-1739

--- libkbfs/kbfs_ops_test.go
+++ libkbfs/kbfs_ops_test.go
@@ -1718,13 +1718,15 @@ func makeDirTree(id tlf.ID, uid keybase1.UID, components ...string) (
 	return rootEntry, path{FolderBranch{Tlf: id}, nodes}, blocks
 }
 
-func makeFile(dir path, parentDirBlock *DirBlock, name string, et EntryType) (
+func makeFile(dir path, parentDirBlock *DirBlock, name string, et EntryType,
+	isDirect bool) (
 	path, *FileBlock) {
 	if et != File && et != Exec {
 		panic(fmt.Sprintf("Unexpected type %s", et))
 	}
 	bid := kbfsblock.FakeIDAdd(dir.tailPointer().ID, 1)
 	bi := makeBIFromID(bid, dir.tailPointer().Creator)
+	bi.IsDirect = isDirect
 
 	parentDirBlock.Children[name] = DirEntry{
 		BlockInfo: bi,
@@ -1810,8 +1812,8 @@ func testKBFSOpsRemoveFileSuccess(t *testing.T, et EntryType) {
 	if et == Exec {
 		entryName += ".exe"
 	}
-	p, block := makeFile(dirPath, parentDirBlock, entryName, et)
-	testPutBlockInCache(t, config, p.tailPointer(), id, block)
+	p, _ := makeFile(dirPath, parentDirBlock, entryName, et, true)
+	//testPutBlockInCache(t, config, p.tailPointer(), id, block)
 
 	// sync block
 	var newRmd ImmutableRootMetadata
@@ -1832,7 +1834,8 @@ func testKBFSOpsRemoveFileSuccess(t *testing.T, et EntryType) {
 	_, ok := newParentDirBlock.Children[entryName]
 	require.False(t, ok)
 
-	for _, n := range p.path {
+	for i := 0; i < len(p.path)-1; i++ {
+		n := p.path[i]
 		blockIDs = append(blockIDs, n.ID)
 	}
 	checkBlockCache(t, config, id, blockIDs, nil)
@@ -1994,25 +1997,19 @@ func TestKBFSOpRemoveMultiBlockFileSuccess(t *testing.T) {
 		makeIFP(bid3, rmd, config, uid, 5, 10),
 		makeIFP(bid4, rmd, config, uid, 5, 15),
 	}
-	block1 := NewFileBlock().(*FileBlock)
-	block1.Contents = []byte{5, 4, 3, 2, 1}
-	block2 := NewFileBlock().(*FileBlock)
-	block2.Contents = []byte{10, 9, 8, 7, 6}
-	block3 := NewFileBlock().(*FileBlock)
-	block3.Contents = []byte{15, 14, 13, 12, 11}
-	block4 := NewFileBlock().(*FileBlock)
-	block4.Contents = []byte{20, 19, 18, 17, 16}
+	fileBlock.IPtrs[0].IsDirect = true
+	fileBlock.IPtrs[1].IsDirect = true
+	fileBlock.IPtrs[2].IsDirect = true
+	fileBlock.IPtrs[3].IsDirect = true
 	fileBP := makeBP(fileBID, rmd, config, uid)
 	p := dirPath.ChildPath(entryName, fileBP)
 	ops := getOps(config, id)
 	n := nodeFromPath(t, ops, dirPath)
 
-	// let the top block be uncached, so we have to fetch it from BlockOps.
+	// Let the top block be uncached, so we have to fetch it from
+	// BlockOps.  Don't cache any of the file direct blocks, to make
+	// sure we don't try to fetch them.
 	expectBlock(config, rmd, fileBP, fileBlock, nil)
-	testPutBlockInCache(t, config, fileBlock.IPtrs[0].BlockPointer, id, block1)
-	testPutBlockInCache(t, config, fileBlock.IPtrs[1].BlockPointer, id, block2)
-	testPutBlockInCache(t, config, fileBlock.IPtrs[2].BlockPointer, id, block3)
-	testPutBlockInCache(t, config, fileBlock.IPtrs[3].BlockPointer, id, block4)
 
 	// sync block
 	blockIDs := make([]kbfsblock.ID, len(dirPath.path))
@@ -2035,7 +2032,6 @@ func TestKBFSOpRemoveMultiBlockFileSuccess(t *testing.T) {
 	for _, n := range p.path {
 		blockIDs = append(blockIDs, n.ID)
 	}
-	blockIDs = append(blockIDs, bid1, bid2, bid3, bid4)
 	checkBlockCache(t, config, id, blockIDs, nil)
 
 	unrefBlocks := []BlockPointer{
@@ -2100,7 +2096,7 @@ func testKBFSOpsRemoveFileMissingBlockSuccess(t *testing.T, et EntryType) {
 	if et == Exec {
 		entryName += ".exe"
 	}
-	p, _ := makeFile(dirPath, parentDirBlock, entryName, et)
+	p, _ := makeFile(dirPath, parentDirBlock, entryName, et, false)
 	// The operation might be retried several times.
 	config.mockBops.EXPECT().Get(
 		gomock.Any(), gomock.Any(), p.tailPointer(),
