aac555d69dd5f2eccef68b8d80b9e69e513247ff
Max Goodman <c@chromakode.com>
2017-01-18 12:21:48 -0800

    Add jest snapshot test for markup

---
+++ shared/markdown/__test__/parser.test.js
@@ -0,0 +1,15 @@
+// @flow
+/* eslint-env jest */
+import parser from '../parser'
+
+describe('Markdown parser', () => {
+  it('parses multiple adjacent emoji correctly', () => {
+    const ast = parser.parse(':ok_hand::skin-tone-2::smile::wink:')
+    expect(ast).toMatchSnapshot()
+  })
+
+  it('parses invalid emoji fragments correctly', () => {
+    const ast = parser.parse('one::\n::two\n:three?::\n::four:\n::')
+    expect(ast).toMatchSnapshot()
+  })
+})
