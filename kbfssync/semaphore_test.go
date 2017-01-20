3cbe279d3b02a98c7a388fd766e35b7cc3ab15b1
Fred Akalin <akalin@keyba.se>
2017-01-20 00:53:39 -0800

    Fix race in TestCancel in semaphore_test.go (#672)
    The spawned goroutine could run before the release
    in the main goroutine happens, leading to a race on
    the count field of acquireCall. Fix this by doing the
    release first before spawning the goroutine.
    
    This only applies for TestCancel, since all other tests
    have a successful acquire, which will then have a
    count of 0.

--- kbfssync/semaphore_test.go
+++ kbfssync/semaphore_test.go
@@ -131,16 +131,16 @@ func TestCancel(t *testing.T) {
 	s := NewSemaphore()
 	require.Equal(t, int64(0), s.countForTest())
 
+	// Do this before spawning the goroutine, so that
+	// callAcquire() will always return a count of n-1.
+	count := s.Release(n - 1)
+	require.Equal(t, n-1, count)
+	require.Equal(t, n-1, s.countForTest())
+
 	callCh := make(chan acquireCall, 1)
 	go func() {
 		callCh <- callAcquire(ctx2, s, n)
 	}()
-
-	requireNoCall(t, callCh)
-
-	count := s.Release(n - 1)
-	require.Equal(t, n-1, count)
-	require.Equal(t, n-1, s.countForTest())
 
 	requireNoCall(t, callCh)
 
