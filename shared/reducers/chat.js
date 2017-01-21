429557a6aadd016431d7fd9c3900dd5b36852d69
Max Goodman <c@chromakode.com>
2017-01-20 18:55:12 -0800

    Retain pending messages when clearing threads
    This should hopefully fix a bug where thread messages get cleared while
    sending a message, causing a pending message to disappear.

--- shared/reducers/chat.js
+++ shared/reducers/chat.js
@@ -80,7 +80,10 @@ function reducer (state: State = initialState, action: Actions) {
         console.warn('Attempted to clear conversation state that doesn\'t exist')
         return
       }
-      const clearedConversationState = initialConversation.set('firstNewMessageID', origConversationState.get('firstNewMessageID'))
+      const clearedConversationState = initialConversation.merge({
+        firstNewMessageID: origConversationState.get('firstNewMessageID'),
+        messages: origConversationState.get('messages').filter(m => m.messageState === 'pending'),
+      })
       // $FlowIssue
       return state.update('conversationStates', conversationStates =>
         conversationStates.set(conversationIDKey, clearedConversationState)
