c468c42184695fac32ef95a180a23efcf91d29d6
Chris Ball <chris@printf.net>
2017-01-26 11:32:12 -0500

    restyle

--- shared/native/notification-listeners.desktop.js
+++ shared/native/notification-listeners.desktop.js
@@ -7,9 +7,7 @@ import {badgeApp} from '../actions/notifications'
 import {badgeAppForChat} from '../actions/chat'
 import {kbfsNotification} from '../util/kbfs-notifications'
 import {pgpKeyInSecretStoreFile} from '../constants/pgp'
-
 import {ConversationBadgeStateRecord} from '../constants/chat'
-
 import type {Dispatch} from '../constants/types/flux'
 import type {incomingCallMapType} from '../constants/types/flow-types'
 
