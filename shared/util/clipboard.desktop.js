bb3c12bf8e13e310b889b0112285a07604752cfa
Gabriel Handford <gabrielh@gmail.com>
2017-01-26 16:53:33 -0800

    Use clipboard.readImage

--- shared/util/clipboard.desktop.js
+++ shared/util/clipboard.desktop.js
@@ -9,46 +9,74 @@ export type ClipboardData = {
   format: string,
 }
 
-export function readClipboard (event: any, beforeSave: () => void): Promise<?ClipboardData> {
+function readBlob (name: string, format: string, blob: any): Promise<?ClipboardData> { // eslint-disable-line
   return new Promise((resolve, reject) => {
-    // If a user pastes image data, there are 2 items, the filename and
-    // the file.
-    let formats = clipboard.availableFormats()
-    let items = event.clipboardData.items
-    if (formats.length === 2 && items.length === 2 && formats[0] === 'text/plain' && formats[1].startsWith('image/')) {
-      event.preventDefault()
-
-      // We can get the filename (not full path) from the clipboard
-      // readText, since the HTML5 File object from getAsFile doesn't
-      // have this information.
-      let name = clipboard.readText()
-
-      // We get the data from the HTML5 File object, read it into a
-      // buffer and then save it to disk.
-      // If we wanted the Electron NativeImage, we could use
-      // clipboard.readImage() but we want the raw data since
-      // NativeImage only supports JPG and PNG.
-      // Unfortunately, there is no clipboard read method that gives
-      // us a buffer object.
-      beforeSave()
-      let blob = items[1].getAsFile()
-      tmpRandFile(name).then(path => {
-        console.log('Saving clipboard to:', path)
-        let reader = new FileReader() // eslint-disable-line
-        reader.onload = e => {
-          fs.writeFile(path, Buffer.from(e.target.result), err => {
-            if (err) {
-              reject(err)
-              return
-            }
-            resolve({path, title: name, format: formats[1]})
-          })
-        }
-        reader.readAsArrayBuffer(blob)
-      })
-    } else {
-      //
-      resolve(null)
-    }
+    // We get the data from the HTML5 File object, read it into a
+    // buffer and then save it to disk.
+    // If we wanted the Electron NativeImage, we could use
+    // clipboard.readImage() but we want the raw data since
+    // NativeImage only supports JPG and PNG.
+    // Unfortunately, there is no clipboard read method that gives
+    // us a buffer object.
+    tmpRandFile(name).then(path => {
+      console.log('Saving clipboard to:', path)
+      let reader = new FileReader() // eslint-disable-line
+      reader.onload = e => {
+        fs.writeFile(path, Buffer.from(e.target.result), err => {
+          if (err) {
+            reject(err)
+            return
+          }
+          resolve({format, path, title: name})
+        })
+      }
+      reader.readAsArrayBuffer(blob)
+    })
   })
+}
+
+function readImage (name: string, format: string): Promise<?ClipboardData> {
+  return new Promise((resolve, reject) => {
+    let data = clipboard.readImage().toPNG()
+    let ext = '.png'
+    tmpRandFile(ext).then(path => {
+      console.log('Saving image from clipboard to', path)
+      fs.writeFile(path, data, err => {
+        if (err) {
+          reject(err)
+          return
+        }
+        resolve({format, path, title: name})
+      })
+    })
+  })
+}
+
+export function readClipboard (event: any, willReadData: () => void): Promise<?ClipboardData> {
+  // If a user pastes image data, there are 2 items, the filename and
+  // the file.
+  let formats = clipboard.availableFormats()
+  let items = event.clipboardData.items
+  if (formats.length === 2 && items.length === 2 && formats[0] === 'text/plain' && formats[1].startsWith('image/')) {
+    event.preventDefault()
+
+    // We can get the filename (not full path) from the clipboard
+    // readText, since the HTML5 File object from getAsFile doesn't
+    // have this information.
+    const name = clipboard.readText()
+    const format = formats[1]
+
+    // Notify caller we're going to read some data
+    willReadData()
+
+    // If you want to read from the HTML5 file blob, use readBlob
+    // let blob = items[1].getAsFile()
+    // return readBlob(name, format, blob)
+
+    // Read image from Electron clipboard
+    return readImage(name, format)
+  } else {
+    // Nothing to read
+    return new Promise((resolve, reject) => { resolve(null) })
+  }
 }
