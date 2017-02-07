b0f3da6bea6495a266f3e1e59b6e21c1b5a95f09
chrisnojima <cnojima@keyba.se>
2017-02-07 14:14:06 -0500

    remove unused wrong asset

--- shared/common-adapters/icon.constants.js
+++ shared/common-adapters/icon.constants.js
@@ -750,11 +750,6 @@ const iconMeta_ = {
     extension: 'gif',
     require: require('../images/icons/icon-loader-uploading-16.gif'),
   },
-  'icon-loader-uploading-static-16': {
-    isFont: false,
-    extension: 'gif',
-    require: require('../images/icons/icon-loader-uploading-static-16.gif'),
-  },
   'icon-no-gpg': {
     isFont: false,
     extension: 'png',
