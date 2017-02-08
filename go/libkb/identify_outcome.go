804b64589ed62d7e5530153c4434a2fc0caccbac
Jack O'Connor <oconnor663@gmail.com>
2017-02-08 11:43:02 -0500

    replace 'tracked' with 'followed' in an error string

--- go/libkb/identify_outcome.go
+++ go/libkb/identify_outcome.go
@@ -225,7 +225,7 @@ func (i IdentifyOutcome) GetErrorAndWarnings(strict bool) (warnings Warnings, er
 
 	if ntf := i.NumTrackFailures(); ntf > 0 {
 		probs = append(probs,
-			fmt.Sprintf("%d tracked proof%s failed",
+			fmt.Sprintf("%d followed proof%s failed",
 				ntf, GiveMeAnS(ntf)))
 	}
 
