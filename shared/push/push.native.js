82aeeb6579d2f4d8fd6450f1adfcc1aa3ef7659f
Marco Munizaga <marco@marcopolo.io>
2017-02-17 10:46:55 -0800

    Remove image since it will hide the button on smaller screens

--- shared/push/push.native.js
+++ shared/push/push.native.js
@@ -36,19 +36,13 @@ class Push extends Component<void, Props, void> {
             {/* The image is wider and will stretch the parent flex box, so we'll
                position absolute, and then specify a "filler" height. */}
             <Box style={{marginTop: 41}} />
-            <Image
-              style={{position: 'absolute', left: 10}}
-              source={require('../images/illustrations/illustration-turn-on-notifications-527-x-294.png')}
-              />
-            <Box style={{height: 294, marginBottom: 23}} />
-
-            <Text type='BodySemibold' style={{textAlign: 'center'}}>It''s <Text type='BodySemiboldItalic'>very</Text> important you enable notifications.</Text>
+            <Text type='BodySemibold' style={{textAlign: 'center'}}>It's <Text type='BodySemiboldItalic'>very</Text> important you enable notifications.</Text>
             <Text type='Body' style={{textAlign: 'center', marginTop: 20}}>
                 This phone may need to perform crypto for you, which the Keybase servers cannot do. For example, if you provision a new device, this phone will be contacted.
             </Text>
-          </Box>
-          <Box style={{marginTop: 24, flex: 1}}>
-            <Button type='Primary' onClick={() => this.props.onRequestPermissions()} label='Got it' waiting={this.props.permissionsRequesting} />
+            <Box style={{marginTop: 24, flex: 1}}>
+              <Button type='Primary' onClick={() => this.props.onRequestPermissions()} label='Got it' waiting={this.props.permissionsRequesting} />
+            </Box>
           </Box>
         </Box>
       </Box>
