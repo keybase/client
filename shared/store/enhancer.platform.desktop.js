25c1c5c7664b5d934285fda9dd8247cff9c1275d
chrisnojima <cnojima@keyba.se>
2017-02-24 16:52:41 -0500

    rename

--- shared/store/enhancer.platform.desktop.js
+++ shared/store/enhancer.platform.desktop.js
@@ -5,12 +5,12 @@ import {compose, applyMiddleware} from 'redux'
 import {enableStoreLogging} from '../local-debug'
 import {throttle} from 'lodash'
 
-const debounceNotify = throttle(notify => notify(), 200)
+const throttleNotify = throttle(notify => notify(), 200)
 
 export default function storeEnhancer (middleware: Array<any>): Function {
   if (enableStoreLogging) {
-    return compose(applyMiddleware(...middleware), DevTools.instrument(), batchedSubscribe(debounceNotify))
+    return compose(applyMiddleware(...middleware), DevTools.instrument(), batchedSubscribe(throttleNotify))
   }
 
-  return compose(applyMiddleware(...middleware), batchedSubscribe(debounceNotify))
+  return compose(applyMiddleware(...middleware), batchedSubscribe(throttleNotify))
 }
