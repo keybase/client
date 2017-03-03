723f39b945fab78576f131912e6cc940bcf99345
Maxwell Krohn <themax@gmail.com>
2017-03-02 20:25:21 -0500

    bust user cache on key changed notification (#6105)
    * bust user cache on key changed notification
    
    - before it wasn't rigged up as we thought, it was only rigged up to user changed handler
    - consolidate code for what to do when a rekey happens, either on gregor or because we know it happened locally
    
    * add noisier debugging to figure out future issues...
    
    * better debugging for LoadUserPlusKeys
    
    * this hack was potentially masking bugs in prod

--- go/libkb/upak_loader.go
+++ go/libkb/upak_loader.go
@@ -349,8 +349,8 @@ func (u *CachedUPAKLoader) LoadUserPlusKeys(ctx context.Context, uid keybase1.UI
 	forcePollValues := []bool{false, true}
 
 	for _, fp := range forcePollValues {
-		// We need to force a reload to make KBFS tests pass
-		arg.ForcePoll = fp || (u.G().Env.GetRunMode() == DevelRunMode)
+
+		arg.ForcePoll = fp
 
 		upak, _, err := u.Load(arg)
 		if err != nil {
@@ -370,7 +370,7 @@ func (u *CachedUPAKLoader) LoadUserPlusKeys(ctx context.Context, uid keybase1.UI
 
 func (u *CachedUPAKLoader) Invalidate(ctx context.Context, uid keybase1.UID) {
 
-	u.G().Log.Debug("CachedUPAKLoader#Invalidate(%s)", uid)
+	u.G().Log.Debug("| CachedUPAKLoader#Invalidate(%s)", uid)
 
 	if u.noCache {
 		return
