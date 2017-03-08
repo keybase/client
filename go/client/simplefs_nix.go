a37f52fd2a1c1fd880954b3f645f9845ab445720
Steve Sanders <steve@keyba.se>
2017-03-07 17:06:11 -0800

    more linux

--- go/client/simplefs_nix.go
+++ go/client/simplefs_nix.go
@@ -6,11 +6,11 @@
 package client
 
 import (
-	"github.com/keybase/cli"
+	"context"
 	"github.com/keybase/client/go/libkb"
 	keybase1 "github.com/keybase/client/go/protocol/keybase1"
 )
 
-func doSimpleFSPlatformGlob(g *libkb.GlobalContext, ctx *cli.Context, cli SimpleFSInterface, paths []keybase1.path) ([]keybase1.Path, error) {
+func doSimpleFSPlatformGlob(g *libkb.GlobalContext, ctx context.Context, cli SimpleFSInterface, paths []keybase1.Path) ([]keybase1.Path, error) {
 	return paths, nil
 }
