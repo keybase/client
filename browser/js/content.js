092f6360a758c07d1a73f2a08c5a8422d2a5f241
Andrey Petrov <andrey.petrov@shazow.net>
2017-03-09 16:00:04 -0500

    browser: Progress with a basic inline chat widget

--- browser/js/content.js
+++ browser/js/content.js
@@ -1,30 +1,33 @@
+"use strict";
+
 function init() {
   // Only do work on reddit.
   if (!location.hostname.endsWith('.reddit.com')) return;
 
   // FIXME: This can be more declarative in the future.
-  if (checkCompose.test(location.pathname)) InjectCompose();
-  else if (checkProfile.test(location.pathname)) InjectProfile();
-  else if (checkThread.test(location.pathname)) InjectThread();
+  if (checkCompose.test(location.pathname)) injectCompose();
+  else if (checkProfile.test(location.pathname)) injectProfile();
+  else if (checkThread.test(location.pathname)) injectThread();
 }
+window.addEventListener('load', init);
 
 
 const checkCompose = /^\/message\/compose$/
-function InjectCompose() {
+function injectCompose() {
   // /message/compose
   // TODO: ...
   console.log("keybase: On compose.");
 }
 
 const checkProfile = /^\/user\//;
-function InjectProfile() {
+function injectProfile() {
   // /user/<user>
   // TODO: ...
   console.log("keybase: On profile.");
 }
 
 const checkThread = /^\/r\/\w+\/comments\/\w+\//;
-function InjectThread() {
+function injectThread() {
   // /r/<subreddit>/comments/<id>/<slug>
   console.log("keybase: On thread.");
 
@@ -36,8 +39,31 @@ function InjectThread() {
     li.className = "keybase-reply";
     li.innerHTML = "<a href=\"keybase://"+ author +"@reddit/\">keybase chat reply</a>";
     buttons.appendChild(li);
+
+    li.getElementsByTagName("a")[0].addEventListener('click', function(e) {
+      renderChat(e.currentTarget.parentNode, author);
+      e.preventDefault();
+    });
+
   }
 }
 
 
-window.addEventListener('load', init);
+function renderChat(parent, toUsername) {
+  // TODO: Cancel button
+  const f = document.createElement("form");
+  f.innerHTML = '\
+    <p>Encrypt to <span class="keybase-username">'+ toUsername +'</span>:</p>\
+    <p><textarea name="keybase-chat"></textarea></p>\
+    <p><input type="checkbox" name="keybase-nudge" /> <em>public</em> nudge (so they know about Keybase)</p>\
+    <p><textarea name="keybase-nudge">/u/'+ toUsername + ' - I left you an end-to-end encrypted replace in Keybase. https://keybase.io/reddit-crypto</textarea></p>\
+    <p><input type="submit" /></p> \
+  ';
+  f.addEventListener('submit', submitChat);
+  parent.insertBefore(f, parent.firstChild);
+}
+
+function submitChat(e) {
+  e.currentTarget.parentNode.removeChild(e.currentTarget);
+  console.log("Chat submitted: ", e);
+}
