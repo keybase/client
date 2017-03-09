c2e08f22a17a67dab74a9c54f9bf0e037e1c665a
chrisnojima <cnojima@keyba.se>
2017-03-09 16:29:16 -0500

    WIP

--- shared/devices/container.js
+++ shared/devices/container.js
@@ -3,9 +3,12 @@ import Render from '.'
 import {List} from 'immutable'
 import {addNewPhone, addNewComputer, addNewPaperKey} from '../actions/login'
 import {connect} from 'react-redux'
+import {createSelector} from 'reselect'
 import {lifecycle} from 'recompose'
 import {loadDevices} from '../actions/devices'
 import {navigateAppend} from '../actions/route-tree'
+
+import type {TypedState} from '../constants/reducer'
 
 const Devices = lifecycle({
   componentWillMount: function () {
@@ -13,29 +16,41 @@ const Devices = lifecycle({
   },
 })(Render)
 
-export default connect(
-  (state: any, {routeState}) => {
-    const {devices: allDevices, waitingForServer} = state.devices
-    const {showingRevoked} = routeState
+const getAllDevicesSelector = (state: TypedState) => state.devices.devices
 
+const getDevicesAndRevokedDevicesSelector = createSelector(
+  getAllDevicesSelector,
+  allDevices => {
     const split = allDevices.groupBy(d => d.revokedAt ? 'revokedDevices' : 'devices')
-    const devices = split.get('devices', List())
-    const revokedDevices = split.get('revokedDevices', List())
+    const devices = split.get('devices', List()).toJS()
+    const revokedDevices = split.get('revokedDevices', List()).toJS()
+    return {
+      devices,
+      revokedDevices,
+    }
+  }
+)
 
-    return {
-      devices: devices.toJS(), // toJS is temp
-      revokedDevices: revokedDevices.toJS(),
-      showingRevoked,
-      waitingForServer,
-    }
-  },
-  (dispatch: any, {routeState, setRouteState}) => {
-    return {
-      addNewComputer: () => dispatch(addNewComputer()),
-      addNewPaperKey: () => dispatch(addNewPaperKey()),
-      addNewPhone: () => dispatch(addNewPhone()),
-      loadDevices: () => dispatch(loadDevices()),
-      onToggleShowRevoked: () => { setRouteState({showingRevoked: !routeState.showingRevoked}) },
-      showExistingDevicePage: device => dispatch(navigateAppend([{props: {device}, selected: 'devicePage'}])),
-    }
-  })(Devices)
+const mapStateToProps = (state: any, {routeState}) => {
+  const {waitingForServer} = state.devices
+  const {showingRevoked} = routeState
+  const {devices, revokedDevices} = getDevicesAndRevokedDevicesSelector(state)
+
+  return {
+    devices,
+    revokedDevices,
+    showingRevoked,
+    waitingForServer,
+  }
+}
+
+const mapDispatchToProps = (dispatch: any, {routeState, setRouteState}) => ({
+  addNewComputer: () => dispatch(addNewComputer()),
+  addNewPaperKey: () => dispatch(addNewPaperKey()),
+  addNewPhone: () => dispatch(addNewPhone()),
+  loadDevices: () => dispatch(loadDevices()),
+  onToggleShowRevoked: () => { setRouteState({showingRevoked: !routeState.showingRevoked}) },
+  showExistingDevicePage: device => dispatch(navigateAppend([{props: {device}, selected: 'devicePage'}])),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Devices)
