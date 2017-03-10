c28134f159adcd1fc323d88cc96aa069b5ce0cb8
Steve Sanders <steve@keyba.se>
2017-03-10 12:42:54 -0800

    reviews

--- go/client/simplefs_nix.go
+++ go/client/simplefs_nix.go
@@ -7,12 +7,10 @@ package client
 
 import (
 	"context"
-
 	"github.com/keybase/client/go/libkb"
 	keybase1 "github.com/keybase/client/go/protocol/keybase1"
 )
 
-//
 func doSimpleFSPlatformGlob(g *libkb.GlobalContext, ctx context.Context, cli keybase1.SimpleFSInterface, paths []keybase1.Path) ([]keybase1.Path, error) {
 	return paths, nil
 }
--- go/client/simplefs_windows.go
+++ go/client/simplefs_windows.go
@@ -14,7 +14,7 @@ import (
 	keybase1 "github.com/keybase/client/go/protocol/keybase1"
 )
 
-func doSimpleFSRemoteGlob(g *libkb.GlobalContext, ctx context.Context, cli keybase1.SimpleFSInterface, path keybase1.Path) ([]keybase1.Path, error) {
+func doSimpleFSRemoteGlob(g *libkb.GlobalContext, ctx context.Context, cli SimpleFSInterface, path keybase1.Path) ([]keybase1.Path, error) {
 
 	var returnPaths []keybase1.Path
 	directory := filepath.ToSlash(filepath.Dir(path.Kbfs()))
@@ -24,7 +24,13 @@ func doSimpleFSRemoteGlob(g *libkb.GlobalContext, ctx context.Context, cli keyba
 	// We know the filename has wildcards at this point.
 	// kbfs list only works on directories, so build a glob from a list result.
 
-	cli, err := GetSimpleFSClient(g)
+	g.Log.Debug("doSimpleFSRemoteGlob %s", path.Kbfs())
+
+	if strings.ContainsAny(directory, "?*[]") == true {
+		return nil, errors.New("wildcards not supported in parent directories")
+	}
+
+	opid, err := cli.SimpleFSMakeOpid(ctx)
 	if err != nil {
 		return nil, err
 	}
@@ -34,6 +40,6 @@ func doSimpleFSRemoteGlob(g *libkb.GlobalContext, ctx context.Context, cli keyba
 	if strings.ContainsAny(directory, "?*[]") == true {
 		return nil, errors.New("wildcards not supported in parent directories")
 	}
-func doSimpleFSPlatformGlob(g *libkb.GlobalContext, ctx context.Context, paths []keybase1.Path) ([]keybase1.Path, error) {
+func doSimpleFSPlatformGlob(g *libkb.GlobalContext, ctx context.Context, cli keybase1.SimpleFSInterface, paths []keybase1.Path) ([]keybase1.Path, error) {
 		if strings.ContainsAny(filepath.Base(pathString), "?*") == false {
 			globbed, err := doSimpleFSRemoteGlob(g, ctx, path)
