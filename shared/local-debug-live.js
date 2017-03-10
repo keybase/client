68bc031a43e1adb2dc699eb06c54e80def4895e4
chrisnojima <cnojima@keyba.se>
2017-03-10 16:55:23 -0500

    WIP

--- shared/local-debug-live.js
+++ shared/local-debug-live.js
@@ -7,7 +7,7 @@ import {envVarDebugJson, isTesting} from './local-debug'
  */
 
 // Shared settings
-const dumbFilterJson = (envVarDebugJson() || {}).dumbFilter || 'devices list'
+const dumbFilterJson = (envVarDebugJson() || {}).dumbFilter || ''
 const dumbFilterOverride = '' // Changing this will apply during a hot reload session
 
 export const dumbFilter = dumbFilterOverride || dumbFilterJson
