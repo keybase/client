be7cfdf2cf2cf192a223fc2b026d9d9d10931b07
Gabriel Handford <gabrielh@gmail.com>
2017-03-13 11:33:53 -0700

    Disable sudden termination plist flag

--- packaging/desktop/package_darwin.sh
+++ packaging/desktop/package_darwin.sh
@@ -185,7 +185,6 @@ update_plist() {(
   cd "$out_dir"
   # App shouldn't display dock icon on startup
   /usr/libexec/plistBuddy -c "Add :LSUIElement bool true" "$app_name.app/Contents/Info.plist"
-  /usr/libexec/plistBuddy -c "Add :NSSupportsSuddenTermination bool true" "$app_name.app/Contents/Info.plist"
 )}
 
 sign() {(
