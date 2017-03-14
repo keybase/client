8ebd3391427e0f08928887d72dc55230b05f6e9c
Andrey Petrov <andrey.petrov@shazow.net>
2017-03-14 16:11:31 -0400

    browser: KBNM handler, does not work yet

--- browser/js/content.js
+++ browser/js/content.js
@@ -54,12 +54,14 @@ function renderChat(parent, toUsername) {
   // TODO: Prevent navigation?
   // The chat widget is enclosed in the form element.
   const f = document.createElement("form");
+  f.action = "#"; // Avoid submitting even if we failt to preventDefault
   f.innerHTML = '\
     <h3>Keybase Chat</h3>\
+    <input type="hidden" name="keybase-to" value="'+ toUsername +'" />\
     <p>Encrypt to <span class="keybase-username">'+ toUsername +'</span>:</p>\
     <p><textarea name="keybase-chat" rows="6"></textarea></p>\
     <p><label><input type="checkbox" name="keybase-nudge" checked /> <em>public</em> nudge (so they know about Keybase)</label></p>\
-    <p><textarea name="keybase-nudge">/u/'+ toUsername + ' - I left you an end-to-end encrypted replace in Keybase. https://keybase.io/reddit-crypto</textarea></p>\
+    <p><textarea name="keybase-text">/u/'+ toUsername + ' - I left you an end-to-end encrypted replace in Keybase. https://keybase.io/reddit-crypto</textarea></p>\
     <p><input type="submit" value="Send" /></p> \
   ';
   f.addEventListener('submit', submitChat);
@@ -67,7 +69,50 @@ function renderChat(parent, toUsername) {
 }
 
 function submitChat(e) {
+  e.preventDefault();
+
+  const to = e.currentTarget["keybase-to"];
+  const body = e.currentTarget["keybase-chat"];
+  const nudgeDo = e.currentTarget["keybase-nudge"].checked;
+  const nudgeText = e.currentTarget["keybase-text"];
+
+  KBNM.connect();
+  KBNM.send({
+    "method": "chat",
+    "to": to,
+    "body": body
+  });
+
+  // TODO: Send nudge
+
   // Detach the chat widget from the parent.
   e.currentTarget.parentNode.removeChild(e.currentTarget);
   console.log("Chat submitted: ", e);
 }
+
+
+var KBNM = function() {
+  this.host = "com.keybase.kbnm";
+  this.port = null;
+}
+
+KBNM.connect = function() {
+  if (this.port != null) return;
+
+  this.port = chrome.runtime.connectNative(this.port);
+  port.onMessage.addListener(this.receive);
+  port.onDisconnect.addListener(this.disconnect);
+}
+
+KBNM.send = function(msg) {
+  this.port.postMessage(msg);
+}
+
+KBNM.receive = function(msg) {
+  console.log("KBNM: received: ", msg);
+}
+
+KBNM.disconnect = function() {
+  this.port = null;
+}
+
