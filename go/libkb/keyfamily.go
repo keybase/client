69be0e9cce6a86c0111237ed3711100768f4e9e0
Maxwell Krohn <themax@gmail.com>
2017-03-15 18:12:40 -0400

    workaround NPE in the wild on keybase.pub (#6255)
    * workaround NPE in the wild on keybase.pub
    
    - i couldn't repro the crashing user, but change the crasher into a debug log warning for now
    
    * fix buggy fix
    
    * upgrade to warning
    
    * warn in the case jack was worried about
    
    (that was actually causing the NPE; this user has an Cv25519 key that couldn't be parsed in Go, so there was
    no key available for this hash in PGPKeySets)

--- go/libkb/keyfamily.go
+++ go/libkb/keyfamily.go
@@ -576,6 +576,13 @@ func (ckf *ComputedKeyFamily) Revoke(tcl TypedChainLink) (err error) {
 
 // SetPGPHash sets the authoritative version (by hash) of a PGP key
 func (ckf *ComputedKeyFamily) SetActivePGPHash(kid keybase1.KID, hash string) {
+	found := false
+	if ks, ok := ckf.kf.PGPKeySets[kid]; ok && ks != nil && ks.KeysByHash[hash] != nil {
+		found = true
+	}
+	if !found {
+		ckf.G().Log.Warning("Didn't have a PGP key for %s with hash %s", kid, hash)
+	}
 	if _, ok := ckf.cki.Infos[kid]; ok {
 		ckf.cki.Infos[kid].ActivePGPHash = hash
 	} else {
@@ -686,8 +693,15 @@ func (ckf ComputedKeyFamily) GetKeyRole(kid keybase1.KID) (ret KeyRole) {
 func (ckf ComputedKeyFamily) GetAllActiveKeysWithRoleAtTime(role KeyRole, t time.Time) (ret []GenericKey) {
 	for kid := range ckf.kf.AllKIDs {
 		if ckf.GetKeyRoleAtTime(kid, t) == role {
-			key, _ := ckf.FindKeyWithKIDUnsafe(kid)
-			ret = append(ret, key)
+			key, err := ckf.FindKeyWithKIDUnsafe(kid)
+			if err != nil {
+				ckf.G().Log.Warning("GetAllActiveKeysWithRoleAtTime: Error in getting KID %s: %s", kid, err)
+			}
+			if key == nil {
+				ckf.G().Log.Warning("GetAllActiveKeysWithRoleAtTime: Null key for KID %s", kid)
+			} else {
+				ret = append(ret, key)
+			}
 		}
 	}
 	return
