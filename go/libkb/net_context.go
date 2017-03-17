ddf7acca6701f6b9aab0811e958d2ca0c9333a49
Maxwell Krohn <themax@gmail.com>
2017-03-17 15:56:23 -0400

    fix bg context copying without reintroducing the original bug

--- go/libkb/net_context.go
+++ go/libkb/net_context.go
@@ -53,8 +53,8 @@ func LogTagsToString(ctx context.Context) string {
 func CopyTagsToBackground(ctx context.Context) context.Context {
 	ret := context.Background()
 	if tags, ok := logger.LogTagsFromContext(ctx); ok {
-		ret = logger.NewContextWithLogTags(ctx, tags)
-		for key, _ := range tags {
+		ret = logger.NewContextWithLogTags(ret, tags)
+		for key := range tags {
 			if ctxKey, ok := key.(withLogTagKey); ok {
 				if val, ok := ctx.Value(ctxKey).(string); ok && len(val) > 0 {
 					ret = context.WithValue(ret, ctxKey, val)
