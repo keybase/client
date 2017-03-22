18e9a6056f00ed163b8c07562ebada7b06c4684f
Marco Munizaga <marco@marcopolo.io>
2017-03-22 16:12:33 -0700

    Use cross platform openURLWithHelper

---
+++ shared/util/url-helper.js
@@ -0,0 +1,20 @@
+// @flow
+import keybaseUrl from '../constants/urls'
+
+const linkFuncs = {
+  home: () => keybaseUrl,
+  help: () => `${keybaseUrl}/getting-started`,
+  user: ({username}) => `${keybaseUrl}/${username || ''}`,
+}
+
+export function urlHelper (type: string, params: any): string {
+  const linkFunc = linkFuncs[type]
+  if (linkFunc) {
+    const link = linkFunc(params)
+    if (link) {
+      return link
+    }
+  } else {
+    console.warn(`No openURL handler for ${type}`, params)
+  }
+}
