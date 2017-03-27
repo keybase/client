e3400a14467439b84c6314d4044653e53de8311d
Mike Maxim <mike@keyba.se>
2017-03-27 16:12:12 -0400

    hook up gregorHandler with Reachability CORE-4712 (#6353)
    * hook up gregorHandler with Reachabilit
    
    * set reachability on gregor
    
    * despam
    
    * get rid of this error
    
    * fix tests and feedback

--- go/service/main.go
+++ go/service/main.go
@@ -39,7 +39,6 @@ type Service struct {
 	gregor               *gregorHandler
 	rekeyMaster          *rekeyMaster
 	attachmentstore      *chat.AttachmentStore
-	messageDeliverer     *chat.Deliverer
 	badger               *badges.Badger
 	reachability         *reachability
 	backgroundIdentifier *BackgroundIdentifier
@@ -59,7 +58,6 @@ func NewService(g *libkb.GlobalContext, isDaemon bool) *Service {
 		rekeyMaster:     newRekeyMaster(g),
 		attachmentstore: chat.NewAttachmentStore(g.Log, g.Env.GetRuntimeDir()),
 		badger:          badges.NewBadger(g),
-		reachability:    newReachability(g),
 	}
 }
 
@@ -317,11 +315,9 @@ func (d *Service) startupGregor() {
 		// Create gregorHandler instance first so any handlers can connect
 		// to it before we actually connect to gregor (either gregor is down
 		// or we aren't logged in)
-		var err error
-		if d.gregor, err = newGregorHandler(d.G()); err != nil {
-			d.G().Log.Warning("failed to create push service handler: %s", err)
-			return
-		}
+		d.gregor = newGregorHandler(d.G())
+		d.reachability = newReachability(d.G(), d.gregor)
+		d.gregor.setReachability(d.reachability)
 		d.gregor.badger = d.badger
 		d.G().GregorDismisser = d.gregor
 		d.G().GregorListener = d.gregor
@@ -472,9 +468,7 @@ func (d *Service) OnLogout() (err error) {
 	}
 
 	log("shutting down message deliverer")
-	if d.messageDeliverer != nil {
-		d.messageDeliverer.Stop(context.Background())
-	}
+	d.G().MessageDeliverer.Stop(context.Background())
 
 	log("shutting down rekeyMaster")
 	d.rekeyMaster.Logout()
--- go/service/reachability.go
+++ go/service/reachability.go
@@ -4,10 +4,7 @@
 package service
 
 import (
-	"net"
-	"net/url"
 	"sync"
-	"time"
 
 	"golang.org/x/net/context"
 
@@ -30,29 +27,35 @@ func newReachabilityHandler(xp rpc.Transporter, g *libkb.GlobalContext, reachabi
 	}
 }
 
-func (h *reachabilityHandler) ReachabilityChanged(_ context.Context, _ keybase1.Reachability) error {
+func (h *reachabilityHandler) ReachabilityChanged(_ context.Context, _ keybase1.Reachability) (err error) {
+	h.G().Trace("ReachabilityChanged", func() error { return err })()
 	return nil
 }
 
-func (h *reachabilityHandler) StartReachability(_ context.Context) (keybase1.Reachability, error) {
-	return h.reachability.start(), nil
+func (h *reachabilityHandler) StartReachability(_ context.Context) (res keybase1.Reachability, err error) {
+	h.G().Trace("StartReachability", func() error { return err })()
+	return keybase1.Reachability{
+		Reachable: keybase1.Reachable_UNKNOWN,
+	}, nil
 }
 
-func (h *reachabilityHandler) CheckReachability(_ context.Context) (keybase1.Reachability, error) {
+func (h *reachabilityHandler) CheckReachability(_ context.Context) (res keybase1.Reachability, err error) {
+	h.G().Trace("CheckReachability", func() error { return err })()
 	return h.reachability.check(), nil
 }
 
 type reachability struct {
 	libkb.Contextified
 	lastReachability keybase1.Reachability
-	started          bool
-	startMutex       sync.Mutex
 	setMutex         sync.Mutex
+
+	gh *gregorHandler
 }
 
-func newReachability(g *libkb.GlobalContext) *reachability {
+func newReachability(g *libkb.GlobalContext, gh *gregorHandler) *reachability {
 	return &reachability{
 		Contextified: libkb.NewContextified(g),
+		gh:           gh,
 	}
 }
 
@@ -67,46 +70,12 @@ func (h *reachability) setReachability(r keybase1.Reachability) {
 	h.lastReachability = r
 }
 
-func (h *reachability) start() keybase1.Reachability {
-	h.startMutex.Lock()
-	defer h.startMutex.Unlock()
-
-	if !h.started {
-		h.started = true
-		go func() {
-			// Do check right away
-			h.check()
-			for {
-				select {
-				case <-h.G().Clock().After(time.Second * 30):
-					h.check()
-				}
-			}
-		}()
-	}
-	return h.lastReachability
-}
-
 func (h *reachability) check() (k keybase1.Reachability) {
-	// When gregor connection issues are resolved, we might want to use the
-	// status of the gregorHandler connection to determine reachability, since
-	// it would be a more accurate indicator or the ability of the app to receive
-	// notifications.
-	u, err := url.Parse(h.G().Env.GetGregorURI())
-	if err != nil {
-		h.G().Log.Errorf("Invalid URI for reachability check: %s", err)
-		return
-	}
-
-	conn, err := net.DialTimeout("tcp", u.Host, 10*time.Second)
-	if conn != nil {
-		conn.Close()
-	}
-
-	if err != nil {
-		k.Reachable = keybase1.Reachable_NO
-	} else {
+	reachable := h.gh.isReachable()
+	if reachable {
 		k.Reachable = keybase1.Reachable_YES
+	} else {
+		k.Reachable = keybase1.Reachable_NO
 	}
 	h.setReachability(k)
 	return k
