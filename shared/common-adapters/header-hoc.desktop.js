f762a45783070098dfcb865d41f7621f1b27cf59
chrisnojima <chrisnojima@gmail.com>
2017-03-29 18:00:43 -0400

    add cancel to search (#6414)
    * add cancel to search. expand headerHoc to support onCancel and some style overriding. make reset clear out the input box also
    
    * ensure title is centerd always

--- shared/common-adapters/header-hoc.desktop.js
+++ shared/common-adapters/header-hoc.desktop.js
@@ -8,51 +8,51 @@ import {globalStyles, globalColors, globalMargins} from '../styles'
 import type {Props} from './header-hoc'
 
 function HeaderHoc<P> (WrappedComponent: ReactClass<P>) {
-  return ({onBack, title, ...restProps}: Props & P) => (
-    <Box style={containerStyle}>
-      <Box style={headerStyle}>
-        <Box style={backButtonStyle}>
-          {onBack && <BackButton iconStyle={backButtonIconStyle} onClick={onBack} />}
+  return ({onBack, onCancel, headerStyle, title, ...restProps}: Props & P) => (
+    <Box style={_containerStyle}>
+      <Box style={{..._headerStyle, ...headerStyle}}>
+        {onCancel && <Text type='BodyBigLink' onClick={onCancel}>Cancel</Text>}
+        {onBack && <BackButton iconStyle={_backButtonIconStyle} onClick={onBack} />}
+        <Box style={_titleStyle}>
+          <Text type='Header'>{title}</Text>
         </Box>
-        <Text type='Header' style={headerText}>{title}</Text>
       </Box>
       {<WrappedComponent {...restProps} />}
     </Box>
   )
 }
 
-const backButtonMarginLeft = globalMargins.small
-const backButtonWidth = 40
-
-const backButtonStyle = {
-  marginLeft: backButtonMarginLeft,
-  paddingBottom: 2,
-  width: backButtonWidth,
-}
-
-const backButtonIconStyle = {
+const _backButtonIconStyle = {
   color: globalColors.blue,
 }
 
-const containerStyle = {
+const _containerStyle = {
   ...globalStyles.flexBoxColumn,
   flex: 1,
 }
 
-const headerStyle = {
+const _headerStyle = {
   ...globalStyles.flexBoxRow,
-  justifyContent: 'center',
   alignItems: 'center',
-  borderBottomWidth: 1,
-  paddingTop: globalMargins.tiny,
-  paddingBottom: globalMargins.tiny,
   borderBottomColor: globalColors.black_05,
+  borderBottomWidth: 1,
+  justifyContent: 'flex-start',
+  minHeight: globalMargins.xlarge,
+  paddingLeft: globalMargins.small,
+  paddingRight: globalMargins.small,
+  position: 'relative',
 }
 
-const headerText = {
-  marginRight: backButtonMarginLeft + backButtonWidth,
-  textAlign: 'center',
+const _titleStyle = {
+  ...globalStyles.flexBoxRow,
+  alignItems: 'center',
+  bottom: 0,
   flex: 1,
+  justifyContent: 'center',
+  left: 0,
+  position: 'absolute', // This is always centered so we never worry about items to the left/right. If you have overlap or other issues you likely have to fix the content
+  right: 0,
+  top: 0,
 }
 
 export default HeaderHoc
