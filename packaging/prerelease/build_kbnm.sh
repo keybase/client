bd7c00a96a1d9faab3182b7c3f09ac02ced10383
Andrey Petrov <shazow@gmail.com>
2017-03-29 14:20:52 -0400

    kbnm installer for OSX (#6334)
    * WIP InstallKBNM
    * InstallKBNM, UninstallKBNM, and add KBNM component
    * Fixes for InstallKBNM and UninstallKBNM
    * Add --version flag
    * packaging/prerelease/build_kbnm.sh
    * Add build scripts for kbnm
    * chmod +x build_kbnm.sh
    * build_kbnm.sh: forgot to replace a kbfs copypasta
    * Fix comment

---
+++ packaging/prerelease/build_kbnm.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -e -u -o pipefail # Fail on error
+
+dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd "$dir"
+
+build_dir=${BUILD_DIR:-/tmp/keybase}
+
+mkdir -p "$build_dir"
+
+current_date=`date -u +%Y%m%d%H%M%S` # UTC
+commit_short=`git log -1 --pretty=format:%h`
+build="$current_date+$commit_short"
+kbnm_build=${KBNM_BUILD:-$build}
+tags=${TAGS:-"prerelease production"}
+ldflags="-X main.Version=$kbnm_build"
+pkg="github.com/keybase/client/go/kbnm"
+
+if [ "$PLATFORM" = "darwin" ]; then
+  # To get codesign to work you have to use -ldflags "-s ...", see https://github.com/golang/go/issues/11887
+  ldflags="-s $ldflags"
+fi
+
+echo "Building $build_dir/kbnm ($kbnm_build)"
+go build -a -tags "$tags" -ldflags "$ldflags" -o "$build_dir/kbnm" "$pkg"
+
+if [ "$PLATFORM" = "darwin" ]; then
+  echo "Signing binary..."
+  code_sign_identity="Developer ID Application: Keybase, Inc. (99229SGT5K)"
+  codesign --verbose --force --deep --sign "$code_sign_identity" $build_dir/kbnm
+elif [ "$PLATFORM" = "linux" ]; then
+  echo "No codesigning for linux"
+elif [ "$PLATFORM" = "windows" ]; then
+  echo "No codesigning for windows"
+else
+  echo "Invalid PLATFORM"
+  exit 1
+fi
+
+kbnm_version=`"$build_dir"/kbnm -version`
+echo "KBNM version: $kbnm_version"
