7f6aeecff1c99d693b1dffb8cb4056bb358ad1ed
Andrey Petrov <andrey.petrov@shazow.net>
2017-04-06 12:22:53 -0400

    kbnm: More comment for guessKeybasePath

--- go/kbnm/findbin_nix.go
+++ go/kbnm/findbin_nix.go
@@ -8,7 +8,9 @@ import (
 
 const keybaseBinary = "keybase"
 
-// guessKeybasePath makes a platform-specific guess to where the binary might be.
+// guessKeybasePath makes a platform-specific guess to where the binary might
+// be. This is only checked as a last-ditch effort when we can't find the
+// binary in other places.
 func guessKeybasePath() string {
 	return filepath.Join("/usr/local/bin", keybaseBinary)
 }
--- go/kbnm/findbin_windows.go
+++ go/kbnm/findbin_windows.go
@@ -9,7 +9,9 @@ import (
 
 const keybaseBinary = "keybase.exe"
 
-// guessKeybasePath makes a platform-specific guess to where the binary might be.
+// guessKeybasePath makes a platform-specific guess to where the binary might
+// be. This is only checked as a last-ditch effort when we can't find the
+// binary in other places.
 func guessKeybasePath() string {
 	return filepath.Join(os.Getenv("LOCALAPPDATA"), "Keybase", keybaseBinary)
 }
