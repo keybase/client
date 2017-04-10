292bcaf636a964c2b0abd60091d745d532a49918
Patrick Crosby <pcrosby@gmail.com>
2017-04-10 13:56:50 -0500

    WIP print stack on api calls

--- go/libkb/api.go
+++ go/libkb/api.go
@@ -13,6 +13,7 @@ import (
 	"net/http"
 	"net/url"
 	"runtime"
+	"runtime/debug"
 	"strings"
 	"sync"
 	"time"
@@ -231,6 +232,7 @@ func doRequestShared(api Requester, arg APIArg, req *http.Request, wantJSONRes b
 	}
 	ctx = WithLogTag(ctx, "API")
 	api.G().Log.CDebugf(ctx, "+ API %s %s", req.Method, req.URL)
+	debug.PrintStack()
 
 	var jsonBytes int
 	var status string
