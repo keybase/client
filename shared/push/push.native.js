289fe6ff9dc936bf80f8b1c421f04ec5e6e9c66c
Gabriel Handford <gabrielh@gmail.com>
2017-04-10 15:13:49 -0700

    Smaller graphic

--- shared/push/push.native.js
+++ shared/push/push.native.js
@@ -25,9 +25,9 @@ class Push extends Component<void, Props, void> {
   }
 
   render () {
-    if (!this.props.prompt) {
-      return null
-    }
+    // if (!this.props.prompt) {
+    //   return null
+    // }
     return (
       <Box style={{...globalStyles.flexBoxColumn, ...modal, flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: globalColors.white}}>
         <Box style={{margin: globalMargins.small}}>
@@ -37,9 +37,9 @@ class Push extends Component<void, Props, void> {
           {/* The image is wider and will stretch the parent flex box, so we'll
 -               position absolute, and then specify a "filler" height. */}
           <Image
-            style={{position: 'absolute', left: 10, top: 30}}
-            source={require('../images/illustrations/illustration-turn-on-notifications-527-x-294.png')} />
-          <Box style={{height: 304}} />
+            style={{position: 'absolute', left: 10, top: 45}}
+            source={require('../images/illustrations/illustration-turn-on-notifications-460-x-252.png')} />
+          <Box style={{height: 294}} />
           <Text type='BodySemibold' style={{textAlign: 'center'}}>It's <Text type='BodySemiboldItalic'>very</Text> important you enable notifications.</Text>
           <Text type='Body' style={{textAlign: 'center', marginTop: 20, marginBottom: 20}}>
               This phone may need to perform crypto for you, which the Keybase servers cannot do. For example, if you provision a new device, this phone will be contacted.
