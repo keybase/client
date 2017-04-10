82957bfd2748560786c45a1b67c385bf806d80dc
Mike Maxim <mike@keyba.se>
2017-04-10 14:12:30 -0400

    add a short timeout to API calls if chat is offline CORE-4890 (#6558)
    * basic hook into Syncer to shorten timeout on API reqs
    
    * use constant
    
    * fix tests
    
    * use null syncer in tests

--- go/libkb/api.go
+++ go/libkb/api.go
@@ -304,6 +304,15 @@ func doRequestShared(api Requester, arg APIArg, req *http.Request, wantJSONRes b
 // on this request; and an error. The canceler function is to clean up the timeout.
 func doRetry(ctx context.Context, g Contextifier, arg APIArg, cli *Client, req *http.Request) (*http.Response, func(), error) {
 
+	// This serves as a proxy for checking the status of the Gregor connection. If we are not
+	// connected to Gregor, then it is likely the case we are totally offline, or on a very bad
+	// connection. If that is the case, let's make these timeouts very aggressive, so we don't
+	// block up everything trying to succeed when we probably will not.
+	if !g.G().Syncer.IsConnected(ctx) {
+		arg.InitialTimeout = HTTPFastTimeout
+		arg.RetryCount = 0
+	}
+
 	if arg.InitialTimeout == 0 && arg.RetryCount == 0 {
 		resp, err := ctxhttp.Do(ctx, cli.cli, req)
 		return resp, nil, err
@@ -335,6 +344,12 @@ func doRetry(ctx context.Context, g Contextifier, arg APIArg, cli *Client, req *
 		}
 		lastErr = err
 		timeout = time.Duration(float64(timeout) * multiplier)
+
+		// If chat goes offline during this retry loop, then let's bail out early
+		if !g.G().Syncer.IsConnected(ctx) {
+			g.G().Log.CDebugf(ctx, "retry loop aborting since chat went offline")
+			break
+		}
 	}
 
 	return nil, nil, fmt.Errorf("doRetry failed, attempts: %d, timeout %s, last err: %s", retries, timeout, lastErr)
