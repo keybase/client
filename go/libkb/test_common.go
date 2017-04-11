d99b7d146fa02d3b6683fb68f34bf459f1e46344
Maxwell Krohn <themax@gmail.com>
2017-04-11 07:23:12 -0400

    fix prod mode build

--- go/libkb/test_common.go
+++ go/libkb/test_common.go
@@ -20,11 +20,8 @@ import (
 
 	"golang.org/x/net/context"
 
-	"github.com/keybase/client/go/chat/types"
 	"github.com/keybase/client/go/gregor"
 	"github.com/keybase/client/go/logger"
-	"github.com/keybase/client/go/protocol/chat1"
-	"github.com/keybase/client/go/protocol/gregor1"
 	keybase1 "github.com/keybase/client/go/protocol/keybase1"
 )
 
@@ -444,38 +441,3 @@ func (f *FakeGregorDismisser) DismissItem(id gregor.MsgID) error {
 	f.dismissedIDs = append(f.dismissedIDs, id)
 	return nil
 }
-
-type NullChatSyncer struct {
-}
-
-func (s NullChatSyncer) IsConnected(ctx context.Context) bool {
-	return true
-}
-
-func (s NullChatSyncer) Connected(ctx context.Context, cli chat1.RemoteInterface, uid gregor1.UID,
-	syncRes *chat1.SyncChatRes) error {
-	return nil
-}
-
-func (s NullChatSyncer) Disconnected(ctx context.Context) {
-
-}
-
-func (s NullChatSyncer) Sync(ctx context.Context, cli chat1.RemoteInterface, uid gregor1.UID,
-	syncRes *chat1.SyncChatRes) error {
-	return nil
-}
-
-func (s NullChatSyncer) RegisterOfflinable(offlinable types.Offlinable) {
-
-}
-
-func (s NullChatSyncer) SendChatStaleNotifications(ctx context.Context, uid gregor1.UID, convIDs []chat1.ConversationID, immediate bool) {
-
-}
-
-func (s NullChatSyncer) Shutdown() {
-
-}
-
-var _ types.Syncer = NullChatSyncer{}
