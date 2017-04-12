afccdf1702ef3c84964000a136b8ab425825accf
Gabriel Handford <gabrielh@gmail.com>
2017-04-12 11:11:15 -0700

    Fix push notifications screen (#6563)

--- shared/push/push.native.js
+++ shared/push/push.native.js
@@ -1,6 +1,6 @@
 // @flow
 import React, {Component} from 'react'
-import {NativeModules} from 'react-native'
+import {Image, NativeModules} from 'react-native'
 import {connect} from 'react-redux'
 import {Box, Button, Text} from '../common-adapters'
 import {globalStyles, globalColors, globalMargins} from '../styles'
@@ -30,21 +30,21 @@ class Push extends Component<void, Props, void> {
     }
     return (
       <Box style={{...globalStyles.flexBoxColumn, ...modal, flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: globalColors.white}}>
-        <Box style={{...globalStyles.flexBoxColumn, alignItems: 'stretch', margin: globalMargins.small}}>
-
-          <Box style={{marginLeft: 8, marginRight: 8, marginTop: 40}}>
-            <Text type='Header' style={{textAlign: 'center'}}>
-              Please turn on notifications!
-            </Text>
-            <Box style={{marginTop: 41}} />
-            <Text type='BodySemibold' style={{textAlign: 'center'}}>It's <Text type='BodySemiboldItalic'>very</Text> important you enable notifications.</Text>
-            <Text type='Body' style={{textAlign: 'center', marginTop: 20}}>
-                This phone may need to perform crypto for you, which the Keybase servers cannot do. For example, if you provision a new device, this phone will be contacted.
-            </Text>
-            <Box style={{marginTop: 24, flex: 1}}>
-              <Button type='Primary' onClick={() => this.props.onRequestPermissions()} label='Got it' waiting={this.props.permissionsRequesting} />
-            </Box>
-          </Box>
+        <Box style={{margin: globalMargins.small}}>
+          <Text type='Header' style={{textAlign: 'center'}}>
+            Please turn on notifications!
+          </Text>
+          {/* The image is wider and will stretch the parent flex box, so we'll
+-               position absolute, and then specify a "filler" height. */}
+          <Image
+            style={{position: 'absolute', left: 10, top: 45}}
+            source={require('../images/illustrations/illustration-turn-on-notifications-460-x-252.png')} />
+          <Box style={{height: 294}} />
+          <Text type='BodySemibold' style={{textAlign: 'center'}}>It's <Text type='BodySemiboldItalic'>very</Text> important you enable notifications.</Text>
+          <Text type='Body' style={{textAlign: 'center', marginTop: 20, marginBottom: 20}}>
+              This phone may need to perform crypto for you, which the Keybase servers cannot do. For example, if you provision a new device, this phone will be contacted.
+          </Text>
+          <Button type='Primary' style={{paddingBottom: 0}} onClick={() => this.props.onRequestPermissions()} label='Got it' waiting={this.props.permissionsRequesting} />
         </Box>
       </Box>
     )
