540b3711b6a8e291d5413c12b207c6deca9dd524
Jeremy Stribling <strib@alum.mit.edu>
2017-04-17 14:30:53 -0700

    test: regression test for KBFS-2091
    Issue: KBFS-2091

--- test/truncate_test.go
+++ test/truncate_test.go
@@ -8,7 +8,7 @@ package test
 
 import "testing"
 
-// bob writes a non-conflicting file while unstaged
+// Test out various truncate scenarios.
 func TestSimpleTruncate(t *testing.T) {
 	const mb = 1024 * 1024
 	mzero := make([]byte, mb)
@@ -55,4 +55,40 @@ func TestSimpleTruncate(t *testing.T) {
 			preadBS("file", mdat3, 0),
 		),
 	)
+}
+
+func testTruncateLargeThenWriteToSmallerOffset(t *testing.T, dataLen int) {
+	data := make([]byte, dataLen)
+	for i := 0; i < len(data); i++ {
+		if i < 12 || (i >= 64 && i < 68) || (i >= dataLen-12) {
+			data[i] = byte(i)
+		}
+	}
+	test(t,
+		blockSize(20), blockChangeSize(100*1024), users("alice", "bob"),
+		as(alice,
+			mkfile("file", ""),
+			truncate("file", uint64(dataLen)),
+			// Write first block and sync.
+			writeBS("file", data[:12]),
+			// Write last block, don't sync yet.
+			pwriteBSSync("file", data[dataLen-12:], int64(dataLen-12), false),
+			// Then write a block in the middle somewhere, and do the sync.
+			pwriteBSSync("file", data[64:68], 64, true),
+		),
+		as(bob,
+			read("file", string(data)),
+		),
+	)
+}
+
+func TestTruncateLargeThenWriteToSmallerOffset(t *testing.T) {
+	testTruncateLargeThenWriteToSmallerOffset(
+		t, 10*12 /* 10 min-sized blocks */)
+}
+
+// Regression for KBFS-2091.
+func TestTruncateLargeThenWriteToSmallerOffsetWithHoles(t *testing.T) {
+	testTruncateLargeThenWriteToSmallerOffset(
+		t, 1024*1024 /* above the holes threshold */)
 }
