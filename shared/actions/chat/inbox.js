6e9301e210f5655e3c8541efc773ca83b420349e
Chris Ball <chris@printf.net>
2017-04-18 14:09:06 -0400

    Use Array.includes()

--- shared/actions/chat/inbox.js
+++ shared/actions/chat/inbox.js
@@ -295,9 +295,11 @@ function _conversationLocalToInboxState (c: ?ChatTypes.ConversationLocal): ?Cons
   const toShow = List(c.maxMessages || [])
     .filter(m => m.valid && m.state === ChatTypes.LocalMessageUnboxedState.valid)
     .map((m: any) => ({body: m.valid.messageBody, time: m.valid.serverHeader.ctime}))
-    .filter(m => m.body.messageType === ChatTypes.CommonMessageType.text ||
-      m.body.messageType === ChatTypes.CommonMessageType.attachment ||
-      m.body.messageType === ChatTypes.CommonMessageType.edit)
+    .filter(m => [
+      ChatTypes.CommonMessageType.attachment,
+      ChatTypes.CommonMessageType.edit,
+      ChatTypes.CommonMessageType.text,
+    ].includes(m.body.messageType))
     .sort((a, b) => b.time - a.time)
     .map((message: {time: number, body: ?ChatTypes.MessageBody}) => ({
       snippet: Constants.makeSnippet(message.body),
