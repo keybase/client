fa246a2740a95acbe7a4f412a18247ae50baba47
Chris Ball <chris@printf.net>
2017-04-19 13:09:10 -0400

    Fit facebook description on iPhone SE

--- shared/profile/facebook-description.js
+++ shared/profile/facebook-description.js
@@ -2,19 +2,13 @@
 import React from 'react'
 import {Box, Icon, Text} from '../common-adapters'
 
-const styleCentered = {
-  style: {
-    textAlign: 'center',
-  },
-}
-
 export default function FacebookDescription () {
   return (
     <Box style={{flexDirection: 'column'}}>
       <Box>
-        <Text type='BodySemibold' {...styleCentered}>Click the link below and post. The text can be whatever you like, but make sure the post is <Text type='BodySemiboldItalic'>public</Text>, like this:</Text>
+        <Text style={{textAlign: 'center'}} type='BodySemibold'>Click the link below and post. The text can be whatever you like, but make sure the post is <Text type='BodySemiboldItalic'>public</Text>, like this:</Text>
       </Box>
-      <Box style={{alignItems: 'center', padding: 20}}>
+      <Box style={{alignItems: 'center'}}>
         <Icon type='icon-facebook-visibility' />
       </Box>
     </Box>
