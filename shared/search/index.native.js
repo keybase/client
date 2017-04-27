0896274cea176f3689353dda99ca02118bbbb4b3
chrisnojima <cnojima@keyba.se>
2017-04-27 15:07:26 -0400

    WIP

--- shared/search/index.native.js
+++ shared/search/index.native.js
@@ -13,21 +13,19 @@ import type {Props as UserSearchProps} from './user-search/render'
 import type {Props as UserGroupProps} from './user-search/user-group'
 import type {Props as SearchBarProps} from './user-search/search-bar'
 
-class SearchRender extends Component<void, Props, void> {
-  render () {
-    const userSearchProps: UserSearchProps = this.props
-    const userGroupProps: UserGroupProps = this.props
-    const searchBarProps: SearchBarProps = this.props
+const SearchRender = (props: Props) => {
+  const userSearchProps: UserSearchProps = this.props
+  const userGroupProps: UserGroupProps = this.props
+  const searchBarProps: SearchBarProps = this.props
 
-    return (
-      <Box style={{...globalStyles.flexBoxRow, flex: 1}}>
-        <Box style={{...globalStyles.flexBoxColumn, flex: 1}}>
-          <SearchBar {...searchBarProps} />
-          {this.props.showUserGroup ? <UserGroup {...userGroupProps} /> : <UserSearch {...userSearchProps} />}
-        </Box>
+  return (
+    <Box style={{...globalStyles.flexBoxRow, flex: 1}}>
+      <Box style={{...globalStyles.flexBoxColumn, flex: 1}}>
+        <SearchBar {...searchBarProps} />
+        {this.props.showUserGroup ? <UserGroup {...userGroupProps} /> : <UserSearch {...userSearchProps} />}
       </Box>
-    )
-  }
+    </Box>
+  )
 }
 
 export default compose(
