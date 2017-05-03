9496eab0cab09896c80d2cfac041568c5722128b
Mike Maxim <mike@keyba.se>
2017-05-03 16:14:15 -0400

    use chat context in all of OnConnect

--- go/service/gregor.go
+++ go/service/gregor.go
@@ -569,9 +569,9 @@ func (g *gregorHandler) OnConnect(ctx context.Context, conn *rpc.Connection,
 	// Run SyncAll to both authenticate, and grab all the data we will need to run the
 	// various resync procedures for chat and notifications
 	var identBreaks []keybase1.TLFIdentifyFailure
-	cctx := chat.Context(ctx, g.G().GetEnv(), keybase1.TLFIdentifyBehavior_CHAT_GUI, &identBreaks,
+	ctx = chat.Context(ctx, g.G().GetEnv(), keybase1.TLFIdentifyBehavior_CHAT_GUI, &identBreaks,
 		chat.NewIdentifyNotifier(g.G()))
-	syncAllRes, err := chatCli.SyncAll(cctx, chat1.SyncAllArg{
+	syncAllRes, err := chatCli.SyncAll(ctx, chat1.SyncAllArg{
 		Uid:       uid,
 		DeviceID:  gcli.Device.(gregor1.DeviceID),
 		Session:   token,
