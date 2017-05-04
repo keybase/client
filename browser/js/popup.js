14157ab0acd663e48fdf1697f02c225cceb69a84
Andrey Petrov <andrey.petrov@shazow.net>
2017-05-04 11:12:56 -0400

    browser: hacky workaround for chrome popup rendering glitch

--- browser/js/popup.js
+++ browser/js/popup.js
@@ -65,9 +65,22 @@ function renderPopup(el, username, service) {
     });
 
     // Sigh: This is a sad hack because the popup's DOM state seems to be
-    // unpredictable and our normal imperative focus call doesn't work so we
-    // do it again here.
+    // unpredictable and our normal imperative stuff doesn't always work
+    // in the initial rendering phase so this is a workaround:
     setTimeout(function() {
+        // Select the textarea. Seems the popup overrides the default selected
+        // element, maybe because you're clicking on an icon to achieve it.
         f["keybase-chat"].focus();
+
+        // Resize the window a tiny bit which forces a subtle rerender that
+        // fixes a bug where the popup is rendered in the wrong size initially
+        // sometimes.
+        document.body.style.height = window.innerHeight + 1 + "px";
+        setTimeout(function() {
+            // We can't remove the property too soon or otherwise it happens
+            // before the popup render glitch. We'd rather not keep it either
+            // because the size of our widget can change after some UI flows.
+            document.body.style.removeProperty("height");
+        }, 100);
     }, 200);
 }
