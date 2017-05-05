8e136a80f7cae5e9581e004fb65a914a6369e28b
Jeremy Stribling <strib@alum.mit.edu>
2017-05-05 15:24:43 -0700

    folder_block_ops: remove unused function
    Issue: KBFS-2076

--- libkbfs/folder_block_ops.go
+++ libkbfs/folder_block_ops.go
@@ -1102,32 +1102,6 @@ func (fbo *folderBlockOps) SetAttrInDirEntryInCache(lState *lockState,
 	})
 }
 
-// ClearCachedAddsAndRemoves clears out any cached directory entry
-// adds and removes for the given dir.
-func (fbo *folderBlockOps) ClearCachedAdd(
-	lState *lockState, dir path, name string) {
-	fbo.blockLock.Lock(lState)
-	defer fbo.blockLock.Unlock(lState)
-	cacheEntry, ok := fbo.deCache[dir.tailRef()]
-	if !ok {
-		return
-	}
-
-	delete(cacheEntry.adds, name)
-	delete(cacheEntry.addedSyms, name)
-
-	// If the entry is totally empty, we can just delete it.
-	if !cacheEntry.dirEntry.IsInitialized() &&
-		cacheEntry.dirEntry.Mtime == 0 && cacheEntry.dirEntry.Ctime == 0 &&
-		len(cacheEntry.adds) == 0 && len(cacheEntry.dels) == 0 &&
-		len(cacheEntry.addedSyms) == 0 {
-		delete(fbo.deCache, dir.tailRef())
-		return
-	}
-
-	fbo.deCache[dir.tailRef()] = cacheEntry
-}
-
 // ClearCachedRef clears any info from the cache for the given block
 // reference, if it's not still dirty.  Returns false if the reference
 // wasn't cleared because it is still dirty.
