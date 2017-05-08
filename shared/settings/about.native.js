5f8ba033092c71295f906bf674ed8e01b84d9dfe
chrisnojima <cnojima@keyba.se>
2017-05-08 09:39:49 -0400

    WIP

--- shared/settings/about.native.js
+++ shared/settings/about.native.js
@@ -9,7 +9,9 @@ import type {Props} from './about'
 const About = ({version}: Props) => (
   <Box style={{...globalStyles.flexBoxColumn, flex: 1, alignItems: 'center', justifyContent: 'center'}}>
     <Icon type='icon-keybase-logo-64' />
-    <Text style={{textAlign: 'center', paddingTop: globalMargins.large}} type='Body'>You are running version <Text type='BodySemibold'>{version}</Text></Text>
+    <Text style={{textAlign: 'center', paddingTop: globalMargins.large, marginBottom: globalMargins.large}} type='Body'>You are running version <Text type='BodySemibold'>{version}</Text></Text>
+    <Text style={{marginBottom: globalMargins.tiny}} type='BodyPrimaryLink' onClickURL='https://keybase.io/_/webview/terms'>Term and Conditions</Text>
+    <Text type='BodyPrimaryLink' onClickURL='https://keybase.io/_/webview/privacypolicy'>Privacy Policy</Text>
   </Box>
 )
 
