905caf8ba23e6df8ecf1fa2ae455dd97ac7b58d5
Marco Munizaga <marco@marcopolo.io>
2017-05-08 12:50:36 -0600

    Fix Set Public Name on native

--- shared/login/forms/container.native.js
+++ shared/login/forms/container.native.js
@@ -5,32 +5,27 @@ import {Box, HeaderHoc} from '../../common-adapters'
 import {globalMargins, globalStyles} from '../../styles'
 import {NativeScrollView} from '../../common-adapters/index.native'
 
-const Container = ({children, onBack, style, outerStyle}: Props) => {
+const Container = ({children, style, outerStyle}: Props) => {
   return (
-    <NativeScrollView style={{...styles.container, ...outerStyle}}>
-      <Box style={{...styles.innerContainer, ...style}}>
+    <NativeScrollView style={{...containerStyle, ...outerStyle}}>
+      <Box style={{...innerContainerStyle, ...style}}>
         {children}
       </Box>
     </NativeScrollView>
   )
 }
 
-const styles = {
-  container: {
-    ...globalStyles.flexBoxColumn,
-    flexGrow: 1,
-    paddingLeft: globalMargins.medium,
-    paddingRight: globalMargins.medium,
-  },
-  innerContainer: {
-    ...globalStyles.flexBoxColumn,
-    marginTop: globalMargins.medium,
-    flexGrow: 1,
-  },
-  button: {
-    paddingTop: globalMargins.medium,
-    paddingLeft: globalMargins.medium,
-  },
+const innerContainerStyle = {
+  ...globalStyles.flexBoxColumn,
+  marginTop: globalMargins.medium,
+  flexGrow: 1,
+}
+
+const containerStyle = {
+  ...globalStyles.flexBoxColumn,
+  flexGrow: 1,
+  paddingLeft: globalMargins.medium,
+  paddingRight: globalMargins.medium,
 }
 
 export default HeaderHoc(Container)
