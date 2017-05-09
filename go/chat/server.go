754537a54357c22646d23c38678f8478c7a84256
Mike Maxim <mike@keyba.se>
2017-05-08 22:13:07 -0400

    add conv IDs to retry list if we fail untrusted load (#6910)

--- go/chat/server.go
+++ go/chat/server.go
@@ -144,6 +144,13 @@ func (h *Server) GetInboxNonblockLocal(ctx context.Context, arg chat1.GetInboxNo
 	localizer := NewNonblockingLocalizer(h.G(), localizeCb, arg.MaxUnbox, h.tlfInfoSource)
 	_, rl, err := h.G().InboxSource.Read(ctx, uid.ToBytes(), localizer, true, arg.Query, arg.Pagination)
 	if err != nil {
+		// If this is a convID based query, let's go ahead and drop those onto the retrier
+		if arg.Query != nil {
+			h.Debug(ctx, "GetInboxNonblockLocal: failed to get unverified inbox, marking convIDs as failed")
+			for _, convID := range arg.Query.ConvIDs {
+				h.G().FetchRetrier.Failure(ctx, convID, uid.ToBytes(), types.InboxLoad)
+			}
+		}
 		return res, err
 	}
 	res.RateLimits = utils.AggRateLimitsP([]*chat1.RateLimit{rl})
