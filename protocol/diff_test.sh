1801f19ab43dcf6063b32a90d04188c5b0b98bb4
chrisnojima <cnojima@keyba.se>
2017-05-15 09:55:27 -0400

    ignore shared folder since we use prettier

--- protocol/diff_test.sh
+++ protocol/diff_test.sh
@@ -12,20 +12,15 @@ npm i
 make clean
 make
 
-cd ../shared
-yarn add --pure-lockfile prettier
-yarn run prettier -- --write ../protocol/js/*.js shared/constants/types/*.js
-cd "$here"
-
 # Protocol changes could create diffs in the following directories:
 #   protocol/
 #   go/
-#   shared/
+        #   shared/ Shared is no longer included as we transform this file automatically.
 # This build process is idempotent. We expect there to be no changes after
 # re-running the protocol generation, because any changes should have been
 # checked in.
-if ! git diff --quiet --exit-code HEAD -- ./ ../go/ ../shared/; then
-  git diff HEAD -- ./ ../go/ ../shared/;
+if ! git diff --quiet --exit-code HEAD -- ./ ../go/; then
+  git diff HEAD -- ./ ../go/;
   echo 'ERROR: `git diff` detected changes. The generated protocol files are stale.'
   exit 1
 fi
