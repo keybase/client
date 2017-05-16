fdeda9ee1f3ae1567808921b307ebfd7ca2f7b85
Marco Munizaga <marco@marcopolo.io>
2017-05-16 14:18:09 -0700

    Fix flow issue

--- shared/login/register/error/index.js
+++ shared/login/register/error/index.js
@@ -3,6 +3,7 @@ import RenderError from './index.render'
 import * as Creators from '../../../actions/login/creators'
 import {connect} from 'react-redux'
 
+// $FlowIssue
 export default connect(null, (dispatch, {routeProps: {error}}) => ({
   onBack: () => dispatch(Creators.onBack()),
   error: error,
