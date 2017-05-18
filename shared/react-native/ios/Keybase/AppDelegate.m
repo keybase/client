79df299e24ea7a35c721cda109e3d10dc74fc436
chrisnojima <cnojima@keyba.se>
2017-05-18 14:08:35 -0400

    didnt mean to check that in

--- shared/react-native/ios/Keybase/AppDelegate.m
+++ shared/react-native/ios/Keybase/AppDelegate.m
@@ -107,8 +107,7 @@ const BOOL isDebug = NO;
 
   NSURL *jsCodeLocation;
 
-//  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
-  jsCodeLocation = [NSURL URLWithString:@"http://192.168.1.50:8081/index.ios.bundle?platform=ios&dev=true"];
+  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"Keybase"
