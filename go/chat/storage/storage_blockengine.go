a3a6f224eef1e850c7e7d71ba35e140c453f688d
Mike Maxim <mike@keyba.se>
2017-05-18 22:33:23 -0400

    logging

--- go/chat/storage/storage_blockengine.go
+++ go/chat/storage/storage_blockengine.go
@@ -407,6 +407,8 @@ func (be *blockEngine) ReadMessages(ctx context.Context, res ResultCollector,
 				// If the result collector is happy to receive this blank entry, then don't complain
 				// and proceed as if this was a hit
 				lastAdded = be.getMsgID(b.BlockID, index)
+				be.Debug(ctx, "readMessages: adding placeholder: %d (blockid: %d pos: %d)",
+					lastAdded, b.BlockID, index)
 				continue
 			} else {
 				be.Debug(ctx, "readMessages: cache entry empty: index: %d block: %d msgID: %d", index,
