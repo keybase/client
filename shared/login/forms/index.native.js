c441b4256af9aedce1da38b33f276cf4151a27f3
Gabriel Handford <gabrielh@gmail.com>
2017-05-22 14:53:33 -0700

    Use HOCTimers

--- shared/login/forms/index.native.js
+++ shared/login/forms/index.native.js
@@ -1,45 +1,49 @@
 // @flow
 import React, {Component} from 'react'
-import {Box, Button, Icon, Text} from '../../common-adapters'
+import {Box, Button, HOCTimers, Icon, Text} from '../../common-adapters'
 import {globalColors, globalStyles, globalMargins} from '../../styles'
 
 import type {Props} from '.'
+import type {TimerProps} from '../../common-adapters/hoc-timers'
 
-class Splash extends Component<void, Props, {showFeedback: boolean}> {
-  state = {
-    showFeedback: false,
-  }
-  componentWillMount() {
-    if (!this.state.showFeedback) {
-      setTimeout(() => {
+const Splash = HOCTimers(
+  class _Splash extends Component<void, Props & TimerProps, {showFeedback: boolean}> {
+    timeoutId: number
+    state = {
+      showFeedback: false,
+    }
+
+    componentWillMount() {
+      this.props.clearTimeout(this.timeoutId)
+      this.timeoutId = this.props.setTimeout(() => {
         this.setState({
           showFeedback: true,
         })
       }, 4000)
     }
-  }
 
-  render() {
-    return (
-      <Box style={{...stylesLoginForm, justifyContent: 'center'}}>
-        <Icon type="icon-keybase-logo-80" />
-        <Text style={stylesHeader} type="HeaderBig">Keybase</Text>
-        {this.state.showFeedback &&
-          <Box style={globalStyles.flexBoxColumn}>
-            <Text style={{marginTop: globalMargins.large}} type="BodySmall">
-              Keybase not starting up?
-            </Text>
-            <Button
-              label="Let us know"
-              onClick={this.props.onFeedback}
-              style={{marginTop: globalMargins.small}}
-              type="Primary"
-            />
-          </Box>}
-      </Box>
-    )
+    render() {
+      return (
+        <Box style={{...stylesLoginForm, justifyContent: 'center'}}>
+          <Icon type="icon-keybase-logo-80" />
+          <Text style={stylesHeader} type="HeaderBig">Keybase</Text>
+          {this.state.showFeedback &&
+            <Box style={globalStyles.flexBoxColumn}>
+              <Text style={{marginTop: globalMargins.large}} type="BodySmall">
+                Keybase not starting up?
+              </Text>
+              <Button
+                label="Let us know"
+                onClick={this.props.onFeedback}
+                style={{marginTop: globalMargins.small}}
+                type="Primary"
+              />
+            </Box>}
+        </Box>
+      )
+    }
   }
-}
+)
 
 const Failure = (props: Props) => (
   <Box style={{...stylesLoginForm, marginTop: 0}}>
