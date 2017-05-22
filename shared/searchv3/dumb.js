1dbf374d8bdb533e1914cc734f2e399780c94f27
chrisnojima <cnojima@keyba.se>
2017-05-22 13:06:41 -0400

    WIP

--- shared/searchv3/dumb.js
+++ shared/searchv3/dumb.js
@@ -1,5 +1,6 @@
 // @flow
 import ServicesFilter from './services-filter'
+import ResultRow from './result-row'
 import type {DumbComponentMap} from '../constants/types/more'
 
 const commonServicesFilterMapProps = {
@@ -36,6 +37,102 @@ const servicesFilterMap: DumbComponentMap<ServicesFilter> = {
   },
 }
 
+const commonServicesReultMapProps = {
+  parentProps: {
+    style: {
+      width: 480,
+    },
+  },
+  showTrackerButton: false,
+  id: 0,
+  onShowTracker: () => console.log('onShowTracker clicked'),
+}
+
+const commonServicesReultMapPropsKB = {
+  ...commonServicesReultMapProps,
+  leftService: 'Keybase',
+  leftIcon: 'jzila',
+  leftUsername: 'jzila',
+  leftFollowing: true,
+  rightService: null,
+  rightUsername: null,
+  rightFullname: 'John Zila',
+  rightIcon: null,
+}
+
+const commonServicesReultMapPropsService = {
+  ...commonServicesReultMapProps,
+  leftUsername: 'jzila',
+  leftFollowing: true,
+  rightService: null,
+  rightUsername: null,
+  rightFullname: 'John Zila',
+  rightIcon: null,
+}
+
+const servicesResultMap: DumbComponentMap<ResultRow> = {
+  component: ResultRow,
+  mocks: {
+    KeybaseNoService: {
+      ...commonServicesReultMapPropsKB,
+    },
+    KeybaseNoServiceNoFollow: {
+      ...commonServicesReultMapPropsKB,
+      leftFollowing: false,
+    },
+    KeybaseNoServiceShowTracker: {
+      ...commonServicesReultMapPropsKB,
+      showTrackerButton: true,
+    },
+    KeybaseGitHub: {
+      ...commonServicesReultMapPropsKB,
+      rightFullname: 'John Zila on GitHub',
+      rightIcon: 'iconfont-identity-github',
+      rightService: 'GitHub',
+      rightUsername: 'jzilagithub',
+    },
+    KeybaseGitHubNoFullname: {
+      ...commonServicesReultMapPropsKB,
+      rightIcon: 'iconfont-identity-github',
+      rightService: 'GitHub',
+      rightUsername: 'jzilagithub',
+    },
+    Twitter: {
+      ...commonServicesReultMapPropsService,
+      leftIcon: 'icon-twitter-logo-32',
+      leftService: 'Twitter',
+    },
+    TwitterKeybase: {
+      ...commonServicesReultMapPropsService,
+      leftIcon: 'icon-twitter-logo-32',
+      leftService: 'Twitter',
+      rightService: 'Keybase',
+      rightUsername: 'jzila',
+    },
+    Facebook: {
+      ...commonServicesReultMapPropsService,
+      leftIcon: 'icon-facebook-logo-32',
+      leftService: 'Facebook',
+    },
+    GitHub: {
+      ...commonServicesReultMapPropsService,
+      leftIcon: 'icon-github-logo-32',
+      leftService: 'GitHub',
+    },
+    Reddit: {
+      ...commonServicesReultMapPropsService,
+      leftIcon: 'icon-reddit-logo-32',
+      leftService: 'Reddit',
+    },
+    'Hacker News': {
+      ...commonServicesReultMapPropsService,
+      leftIcon: 'icon-hacker-news-logo-32',
+      leftService: 'Hacker News',
+    },
+  },
+}
+
 export default {
   'SearchV3 filter': servicesFilterMap,
+  'SearchV3 result': servicesResultMap,
 }
