b66539e84f42f0d09579a9fa3f6706cef37888c5
Patrick Crosby <pcrosby@gmail.com>
2017-05-23 10:00:43 -0500

    Fix context type, put it in APIArg

--- go/teams/get_test.go
+++ go/teams/get_test.go
@@ -1,15 +1,48 @@
 package teams
 
 import (
-	"context"
 	"testing"
+
+	"golang.org/x/net/context"
 
 	"github.com/keybase/client/go/kbtest"
 	"github.com/keybase/client/go/libkb"
 )
 
 func TestTeamGet(t *testing.T) {
-	t.Skip("flake")
+	tc := libkb.SetupTest(t, "team", 1)
+	tc.Tp.UpgradePerUserKey = true
+	defer tc.Cleanup()
+
+	kbtest.CreateAndSignupFakeUser("team", tc.G)
+
+	name := createTeam(tc)
+
+	_, err := Get(context.TODO(), tc.G, name)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestTeamGetConcurrent(t *testing.T) {
+	t.Skip("this is slow but it passes")
+	work := make(chan bool)
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			for x := range work {
+				_ = x
+				teamGet(t)
+			}
+		}()
+	}
+
+	for j := 0; j < 100; j++ {
+		work <- true
+	}
+}
+
+func teamGet(t *testing.T) {
 	tc := libkb.SetupTest(t, "team", 1)
 	tc.Tp.UpgradePerUserKey = true
 	defer tc.Cleanup()
