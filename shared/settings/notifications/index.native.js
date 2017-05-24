5699b4c5330f4799d015ed341f9590f3e7395be6
Chris Ball <chris@printf.net>
2017-05-24 12:52:27 -0400

    Add mobile-only wrapper

---
+++ shared/settings/notifications/index.native.js
@@ -0,0 +1,14 @@
+// @flow
+import React from 'react'
+import {HeaderHoc, NativeScrollView} from '../../common-adapters'
+import {globalStyles, globalMargins} from '../../styles'
+import Notifications from './index.js'
+
+import type {Props} from './index'
+
+const MobileNotifications = (props: Props) =>
+  <NativeScrollView style={{...globalStyles.flexBoxColumn, flex: 1}}>
+    <Notifications {...props} />
+  </NativeScrollView>
+
+export default HeaderHoc(MobileNotifications)
