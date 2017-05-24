844f88639312a73a185ec3f0c637628fa0479ecc
Chris Ball <chris@printf.net>
2017-05-24 12:54:46 -0400

    prettier

--- shared/settings/notifications/index.native.js
+++ shared/settings/notifications/index.native.js
@@ -1,14 +1,15 @@
 // @flow
 import React from 'react'
-import {HeaderHoc, NativeScrollView} from '../../common-adapters'
-import {globalStyles, globalMargins} from '../../styles'
+import {HeaderHoc, NativeScrollView} from '../../common-adapters/index.native'
+import {globalStyles} from '../../styles'
 import Notifications from './index.js'
 
 import type {Props} from './index'
 
-const MobileNotifications = (props: Props) =>
+const MobileNotifications = (props: Props) => (
   <NativeScrollView style={{...globalStyles.flexBoxColumn, flex: 1}}>
     <Notifications {...props} />
   </NativeScrollView>
+)
 
 export default HeaderHoc(MobileNotifications)
