85d92973becf47c8581ad87283758d97aa976fe7
Chris Ball <chris@printf.net>
2017-05-31 12:05:32 -0400

    Switch from routeProps to recompose

--- shared/profile/edit-profile/index.js
+++ shared/profile/edit-profile/index.js
@@ -1,6 +1,7 @@
 // @flow
 import React, {Component} from 'react'
 import Render from './render'
+import {compose, lifecycle, withHandlers, withState} from 'recompose'
 import {connect} from 'react-redux'
 import {editProfile} from '../../actions/profile'
 import {maxProfileBioChars} from '../../constants/profile'
@@ -8,21 +9,51 @@ import {navigateUp} from '../../actions/route-tree'
 
 import type {Props} from './render'
 
-class EditProfile extends Component<void, Props, void> {
+type State = {
+  bio: ?string,
+  fullname: ?string,
+  location: ?string,
+}
+
+const _bioLengthLeft = (bio: ?string) => {
+  return bio ? maxProfileBioChars - bio.length : maxProfileBioChars
+}
+
+const RenderWrapped = compose(
+  withState('bio', 'onBioChange', props => props.bio),
+  withState('fullname', 'onFullnameChange', props => props.fullname),
+  withState('location', 'onLocationChange', props => props.location),
+  withHandlers({
+    onSubmit: ({bio, fullname, location, onSubmit}) => () => onSubmit({bio, fullname, location}),
+  }),
+  lifecycle({
+    componentWillReceiveProps: function(nextProps) {
+      const bioLengthLeft = _bioLengthLeft(nextProps.bio)
+      this.setState({bioLengthLeft})
+    },
+  })
+)(Render)
+
+class EditProfile extends Component<void, Props, State> {
+  state: State
+
+  constructor(props: Props) {
+    super(props)
+
+    const {bio, fullname, location} = this.props
+    this.state = {bio, fullname, location}
+  }
+
   render() {
-    const bioMaxChars = maxProfileBioChars
-    const bioLengthLeft = bioMaxChars - this.props.bio.length
+    const bioLengthLeft = _bioLengthLeft(this.state.bio)
     return (
-      <Render
-        bio={this.props.bio}
+      <RenderWrapped
+        bio={this.state.bio}
         bioLengthLeft={bioLengthLeft}
-        fullname={this.props.fullname}
-        location={this.props.location}
+        fullname={this.state.fullname}
+        location={this.state.location}
         onBack={this.props.onBack}
-        onBioChange={this.props.onBioChange}
         onCancel={this.props.onBack}
-        onFullnameChange={this.props.onFullnameChange}
-        onLocationChange={this.props.onLocationChange}
         onSubmit={this.props.onEditProfile}
       />
     )
@@ -31,35 +62,23 @@ class EditProfile extends Component<void, Props, void> {
 
 // $FlowIssue type this connector
 export default connect(
-  (state, {routeState}) => {
+  state => {
     const userInfo = state.tracker.trackers[state.config.username].userInfo
-    return {
-      bio: routeState.bio !== undefined ? routeState.bio : userInfo.bio,
-      fullname: routeState.fullname !== undefined ? routeState.fullname : userInfo.fullname,
-      location: routeState.location !== undefined ? routeState.location : userInfo.location,
-    }
+    const {bio, fullname, location} = userInfo
+    return {bio, fullname, location}
   },
-  (dispatch, {routeState, setRouteState}) => {
+  dispatch => {
     return {
       onBack: () => dispatch(navigateUp()),
-      onBioChange: bio => {
-        setRouteState({bio})
-      },
       onEditProfile: (bio, fullname, location) => dispatch(editProfile(bio, fullname, location)),
-      onFullnameChange: fullname => {
-        setRouteState({fullname})
-      },
-      onLocationChange: location => {
-        setRouteState({location})
-      },
     }
   },
   (stateProps, dispatchProps, ownProps) => ({
     ...stateProps,
     ...dispatchProps,
     ...ownProps,
-    onEditProfile: () => {
-      dispatchProps.onEditProfile(stateProps.bio, stateProps.fullname, stateProps.location)
+    onEditProfile: ({bio, fullname, location}) => {
+      dispatchProps.onEditProfile(bio, fullname, location)
     },
   })
 )(EditProfile)
