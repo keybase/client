84e8e1a9f75dce281ec793b24d6a6b7a8e243db9
Chris Ball <chris@printf.net>
2017-06-02 16:19:16 -0400

    Update FB proof flow to match new website flow

--- shared/profile/facebook-description.js
+++ shared/profile/facebook-description.js
@@ -1,19 +1,20 @@
 // @flow
 import React from 'react'
 import {Box, Icon, Text} from '../common-adapters'
+import {globalMargins} from '../styles'
 
 export default function FacebookDescription() {
   return (
     <Box style={{flexDirection: 'column'}}>
       <Box>
         <Text style={{textAlign: 'center'}} type="BodySemibold">
-          Click the link below and post. The text can be whatever you like, but make sure the post is
+          Finally, post your proof to Facebook. We'll ask for permission to read your posts, so that we can find this one afterwards. The text can be whatever you like, but — and this is really important — make sure your post is
           {' '}
           <Text type="BodySemiboldItalic">public</Text>
           , like this:
         </Text>
       </Box>
-      <Box style={{alignItems: 'center'}}>
+      <Box style={{alignItems: 'center', marginTop: globalMargins.small}}>
         <Icon type="icon-facebook-visibility" />
       </Box>
     </Box>
