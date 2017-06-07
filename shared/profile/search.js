c4f2652edd3c0a7c381388d8f5f3ca19307015b0
Chris Ball <chris@printf.net>
2017-06-07 17:49:37 -0400

    update against Marco's branch

--- shared/profile/search.js
+++ shared/profile/search.js
@@ -3,10 +3,11 @@ import React from 'react'
 import {onUserClick} from '../actions/profile'
 import * as Creators from '../actions/chat/creators'
 import * as SearchCreators from '../actions/searchv3/creators'
+import * as SearchConstants from '../constants/searchv3'
 import {debounce} from 'lodash'
-import SearchHeader from '../chat/search-header'
 import ServiceFilter from '../searchv3/services-filter'
 import ResultsList from '../searchv3/results-list'
+import UserInput from '../searchv3/user-input'
 import {compose, withState, withHandlers, defaultProps} from 'recompose'
 import {Box, Icon, Text} from '../common-adapters'
 import {connect} from 'react-redux'
@@ -14,22 +15,30 @@ import {globalStyles, globalColors, globalMargins} from '../styles'
 
 import type {TypedState} from '../constants/reducer'
 
-type OwnProps = {navigateUp: () => void}
+type OwnProps = {
+  navigateUp: () => void,
+  onChangeSearchText: (s: string) => void,
+  onSelectService: (s: string) => void,
+  _search: (term: string, service: SearchConstants.Service) => void,
+  searchText: string,
+  selectedService: string,
+  usernameText: string,
+}
 const mapStateToProps = ({profile: {searchResults}}: TypedState) => ({
   ids: searchResults.toArray(),
 })
 const mapDispatchToProps = (dispatch: Dispatch, {navigateUp, onBack, onToggleSidePanel}: OwnProps) => ({
-  _search: debounce(
-    (term: string, service) => dispatch(SearchCreators.search(term, 'profile:updateSearchResults', service)),
-    1e3
-  ),
+  _clearSearchResults: id => dispatch(Creators.clearSearchResults()),
   _onClick: username => {
     dispatch(navigateUp())
     dispatch(onUserClick(username))
   },
-  _clearSearchResults: id => dispatch(Creators.clearSearchResults()),
-  onShowTracker: id => console.log('show tracker of', id),
+  _search: debounce(
+    (term: string, service) => dispatch(SearchCreators.search(term, 'profile:updateSearchResults', service)),
+    1e3
+  ),
   onClose: () => dispatch(navigateUp()),
+  onShowTracker: id => console.log('show tracker of', id),
 })
 
 const Search = props => {
@@ -38,10 +47,13 @@ const Search = props => {
       <Box style={{...styleSearchRow}}>
         <Box style={{...globalStyles.flexBoxRow, flexGrow: 1}}>
           <Box style={{flexGrow: 1}}>
-            <SearchHeader
-              showAddButton={false}
+            <UserInput
+              userItems={props.userItems}
+              showAddButton={props.showAddButton}
+              onRemoveUser={props.onRemoveUser}
+              onClickAddButton={props.onClickAddButton}
               placeholder={props.placeholder}
-              usernameText={props.usernameText}
+              usernameText={props.searchText}
               onChangeText={props.onChangeText}
             />
           </Box>
@@ -77,40 +89,34 @@ const styleSearchContainer = {
 
 const styleSearchRow = {
   ...globalStyles.flexBoxRow,
-  padding: globalMargins.tiny,
   alignItems: 'center',
+  padding: globalMargins.tiny,
 }
 
 export default compose(
   connect(mapStateToProps, mapDispatchToProps),
   withState('usernameText', '_onChangeText', ''),
   withState('selectedService', '_onSelectService', 'Keybase'),
+  withState('searchText', 'onChangeSearchText', ''),
   withHandlers({
-    onChangeText: props => nextText => {
-      props._onChangeText(nextText)
-      if (nextText) {
-        props._search(nextText, props.selectedService)
-      } else {
-        props._clearSearchResults()
-      }
-    },
-    onSelectService: props => nextService => {
-      props._onSelectService(nextService)
-      if (props.usernameText) {
-        props._search(props.usernameText, nextService)
-      } else {
-        props._clearSearchResults()
-      }
+    onChangeText: (props: OwnProps & {_onSelectService: Function}) => nextText => {
+      console.warn('in onChangeText', nextText)
+      props.onChangeSearchText(nextText)
+      props._search(nextText, props.selectedService)
     },
     onClick: props => id => {
       props._onClick(id)
       props._onChangeText('')
       props._clearSearchResults()
     },
+    onSelectService: (props: OwnProps & {_onSelectService: Function}) => nextService => {
+      props._onSelectService(nextService)
+      props._search(props.usernameText, nextService)
+    },
   }),
   defaultProps({
     placeholder: 'Type someone',
-    userItems: [],
     showAddButton: false,
+    userItems: [],
   })
 )(Search)
