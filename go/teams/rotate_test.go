f45373e608b887337b6e2e56549428f80763514f
Patrick Crosby <pcrosby@gmail.com>
2017-06-09 14:49:31 -0500

    Add test for team.clkr with old generation

--- go/teams/rotate_test.go
+++ go/teams/rotate_test.go
@@ -43,6 +43,53 @@ func TestRotate(t *testing.T) {
 	assertRole(tc, name, other.Username, keybase1.TeamRole_WRITER)
 }
 
+func TestHandleRotateRequestOldGeneration(t *testing.T) {
+	tc, owner, other, name := memberSetupMultiple(t)
+	defer tc.Cleanup()
+
+	if err := SetRoleWriter(context.TODO(), tc.G, name, other.Username); err != nil {
+		t.Fatal(err)
+	}
+
+	team, err := Get(context.TODO(), tc.G, name)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// rotate to bump the generation
+	if err := team.Rotate(context.TODO()); err != nil {
+		t.Fatal(err)
+	}
+
+	team, err = Get(context.TODO(), tc.G, name)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if team.Box.Generation != 2 {
+		t.Fatalf("team generation: %d, expected 2", team.Box.Generation)
+	}
+	ctextInitial := team.Box.Ctext
+
+	// this shouldn't do anything
+	if err := HandleRotateRequest(context.TODO(), tc.G, team.ID, 1); err != nil {
+		t.Fatal(err)
+	}
+
+	after, err := Get(context.TODO(), tc.G, name)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if after.Box.Generation != 2 {
+		t.Fatalf("HandleRotateRequest with old generation changed team generation: %d, expected 2", after.Box.Generation)
+	}
+	if after.Box.Ctext != ctextInitial {
+		t.Fatal("TeamBox.Ctext changed after HandleRotateRequest with old generation")
+	}
+
+	assertRole(tc, name, owner.Username, keybase1.TeamRole_OWNER)
+	assertRole(tc, name, other.Username, keybase1.TeamRole_WRITER)
+}
+
 func TestHandleRotateRequest(t *testing.T) {
 	tc, owner, other, name := memberSetupMultiple(t)
 	defer tc.Cleanup()
