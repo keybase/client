f16e9e61452282a8fb692591f4cb4787ca07ec53
chrisnojima <cnojima@keyba.se>
2017-06-15 16:24:13 -0400

    WIP

--- shared/.storybook/webpack.config.js
+++ shared/.storybook/webpack.config.js
@@ -3,6 +3,13 @@ const webpack = require('webpack')
 // This is just the basic way to add additional webpack configurations.
 // For more information refer the docs: https://storybook.js.org/configurations/custom-webpack-config
 
+const fileLoaderRule = {
+  loader: 'file-loader',
+  options: {
+    name: '[name].[ext]',
+  },
+}
+
 module.exports = (storybookBaseConfig, configType) => {
   storybookBaseConfig.resolve = {
     extensions: ['.desktop.js', '.js', '.jsx', '.json', '.flow'],
@@ -11,6 +18,16 @@ module.exports = (storybookBaseConfig, configType) => {
     new webpack.DefinePlugin({
       __STORYBOOK__: true,
     })
+  )
+  storybookBaseConfig.module.rules.push(
+    {
+      test: [/emoji-datasource.*\.(gif|png)$/, /\.ttf$/],
+      use: [fileLoaderRule],
+    },
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader'],
+    }
   )
   return storybookBaseConfig
 }
--- shared/stories/index.js
+++ shared/stories/index.js
@@ -1,22 +1,13 @@
 /* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
 // @flow
-import React from 'react'
+import {configure} from '@storybook/react'
 
-import {storiesOf} from '@storybook/react'
-import {action} from '@storybook/addon-actions'
-import {linkTo} from '@storybook/addon-links'
+// Load css
+import '../desktop/renderer/style.css'
 
-import Text from '../common-adapters/text'
+// $FlowIssue
+const req = require.context('../common-adapters', true, /\.stories\.js$/)
 
-storiesOf('Text', module)
-  .add('Header', () => <Text type="Header">Header</Text>)
-  .add('Body', () => <Text type="Body">Body</Text>)
-
-// import Button from './Button'
-// import Welcome from './Welcome'
-
-// storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />)
-
-// storiesOf('Button', module)
-// .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-// .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>)
+configure(() => {
+  req.keys().forEach(filename => req(filename))
+}, module)
