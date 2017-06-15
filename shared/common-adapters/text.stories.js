7645c04ec6363b2c67b93a34a01319521c6a04a3
chrisnojima <cnojima@keyba.se>
2017-06-15 16:28:16 -0400

    WIP

--- shared/common-adapters/text.stories.js
+++ shared/common-adapters/text.stories.js
@@ -1,9 +1,10 @@
 // @flow
-import {storiesOf} from '@storybook/react'
+/* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
 import React from 'react'
-import {globalColors, globalStyles} from '../styles'
-
 import Text from './text'
+import {action} from '@storybook/addon-actions'
+import {globalColors, globalStyles} from '../styles'
+import {storiesOf} from '@storybook/react'
 
 const SmallGap = () => <div style={{minHeight: 24}} />
 const LargeGap = () => <div style={{minHeight: 36}} />
@@ -54,25 +55,25 @@ const groups = [
   [{label: 'Header big Header big', type: 'HeaderBig'}],
   [
     {label: 'Header Header', type: 'Header'},
-    {label: 'Header link Header link', normalOnly: true, type: 'HeaderLink'},
+    {action: true, label: 'Header link Header link', normalOnly: true, type: 'HeaderLink'},
   ],
   [
     {label: 'Body big Body big', type: 'BodyBig'},
-    {label: 'Body big link Body big link', normalOnly: true, type: 'BodyBigLink'},
+    {action: true, label: 'Body big link Body big link', normalOnly: true, type: 'BodyBigLink'},
   ],
   [
     {label: 'Body text Body text Body text', type: 'Body'},
     {label: 'Body semibold Body semibold', type: 'BodySemibold'},
-    {label: 'Body primary link', type: 'BodyPrimaryLink'},
-    {label: 'Body secondary link', normalOnly: true, type: 'BodySecondaryLink'},
+    {action: true, label: 'Body primary link', type: 'BodyPrimaryLink'},
+    {action: true, label: 'Body secondary link', normalOnly: true, type: 'BodySecondaryLink'},
     {label: 'Body error Body error', normalOnly: true, type: 'BodyError'},
     {label: 'Body success Body success', normalOnly: true, type: 'BodySuccess'},
   ],
   [
     {label: 'Body small Body small', type: 'BodySmall'},
-    {label: 'Body small semibold', type: 'BodySmallSemibold'},
-    {label: 'Body small primary link', type: 'BodySmallPrimaryLink'},
-    {label: 'Body small secondary link', normalOnly: true, type: 'BodySmallSecondaryLink'},
+    {action: true, label: 'Body small semibold', type: 'BodySmallSemibold'},
+    {action: true, label: 'Body small primary link', type: 'BodySmallPrimaryLink'},
+    {action: true, label: 'Body small secondary link', normalOnly: true, type: 'BodySmallSecondaryLink'},
     {label: 'Body small error Body small error', normalOnly: true, type: 'BodySmallError'},
     {label: 'Body small success Body small success', normalOnly: true, type: 'BodySmallSuccess'},
   ],
@@ -86,6 +87,7 @@ const mapText = (secondary: boolean) => {
       const item = key => (
         <Text
           type={types.type}
+          onClick={types.action ? action(`${types.type} clicked`) : undefined}
           key={key}
           {...{
             ...displayBlock,
