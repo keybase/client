e817db8134b497cce98b1a30adb3a562edab1110
John Zila <john@keyba.se>
2017-06-23 14:51:13 -0700

    kbfs-notifications: Disable file notifications and enable error state notifications.

--- shared/util/kbfs-notifications.js
+++ shared/util/kbfs-notifications.js
@@ -134,11 +134,17 @@ export function decodeKBFSError(user: string, notification: FSNotification): Dec
 
 export function kbfsNotification(notification: FSNotification, notify: any, getState: any) {
   const action = {
-    [KbfsCommonFSNotificationType.encrypting]: 'Encrypting and uploading',
-    [KbfsCommonFSNotificationType.decrypting]: 'Decrypting',
-    [KbfsCommonFSNotificationType.signing]: 'Signing and uploading',
-    [KbfsCommonFSNotificationType.verifying]: 'Verifying and downloading',
+    // For now, disable file notifications because they're really annoying and
+    // we now have the syncing indicator.
+    //[KbfsCommonFSNotificationType.encrypting]: 'Encrypting and uploading',
+    //[KbfsCommonFSNotificationType.decrypting]: 'Decrypting',
+    //[KbfsCommonFSNotificationType.signing]: 'Signing and uploading',
+    //[KbfsCommonFSNotificationType.verifying]: 'Verifying and downloading',
     [KbfsCommonFSNotificationType.rekeying]: 'Rekeying',
+    // The following notifications just need to be enabled, they get handled
+    // independently.
+    [KbfsCommonFSNotificationType.initialized]: '',
+    [KbfsCommonFSNotificationType.connection]: '',
   }[notification.notificationType]
 
   if (action === undefined) {
