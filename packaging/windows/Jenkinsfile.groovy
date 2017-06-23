743e2c09396b61b2d324061637d3fb3e484e24d9
Steve Sanders <steve@keyba.se>
2017-06-23 14:13:36 -0700

    Windows Jenkins build results via slackbot

--- packaging/windows/Jenkinsfile.groovy
+++ packaging/windows/Jenkinsfile.groovy
@@ -186,12 +186,41 @@ def doBuild() {
     }
 }
 
+
+def notifySlack(String buildStatus = 'STARTED') {
+    // Build status of null means success.
+    buildStatus = buildStatus ?: 'SUCCESS'
+
+    def color
+
+    if (buildStatus == 'STARTED') {
+        color = '#D4DADF'
+    } else if (buildStatus == 'SUCCESS') {
+        color = '#BDFFC3'
+    } else if (buildStatus == 'UNSTABLE') {
+        color = '#FFFE89'
+    } else {
+        color = '#FF9FA1'
+    }
+
+    def msg = "${buildStatus}: `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL}"
+
+    slackSend(color: color, message: msg)
+}
+
 // Invoke the build with a separate workspace for each executor,
 // and with GOPATH set to that workspace
 node ('windows-release') {
     ws("${WORKSPACE}_${EXECUTOR_NUMBER}") {
         withEnv(["GOPATH=${pwd()}"]) {
-            doBuild()
+            try {
+                doBuild()
+            } catch (e) {
+                currentBuild.result = 'FAILURE'
+                throw e
+            } finally {
+                notifySlack(currentBuild.result)
+            }
         }
     }
 }
