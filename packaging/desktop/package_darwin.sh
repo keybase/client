91f1fd4633bd2b6add1d07f299e7887c21a59af9
Gabriel Handford <gabrielh@gmail.com>
2017-06-26 14:02:25 -0700

    Use saltpack v1 for update signing

--- packaging/desktop/package_darwin.sh
+++ packaging/desktop/package_darwin.sh
@@ -264,7 +264,7 @@ create_zip() {(
 kbsign() {(
   cd "$out_dir"
   echo "Signing (via keybase)"
-  keybase sign -d -i "$zip_name" -o "$sig_name"
+  keybase sign -d --saltpack-version=1 -i "$zip_name" -o "$sig_name"
 )}
 
 update_json() {(
