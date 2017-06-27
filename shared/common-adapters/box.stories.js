c6d45b24b5d85f5fc6534bf6ca4969909043ecd0
chrisnojima <cnojima@keyba.se>
2017-06-27 16:59:23 -0400

    WIP

--- shared/common-adapters/box.stories.js
+++ shared/common-adapters/box.stories.js
@@ -2,35 +2,32 @@
 import React from 'react'
 import Box from './box'
 import Text from './text'
-import ScrollView from './scroll-view'
 import {storiesOf} from '../stories/storybook'
 import {globalMargins, globalStyles, globalColors} from '../styles'
 
 const load = () => {
-  storiesOf('Box', module).add('Box', () => (
-    <ScrollView style={{flex: 1}}>
-      {Object.keys(globalMargins).map(size => (
-        <Box key={size} style={{...globalStyles.flexBoxRow, margin: 30, width: '100%'}}>
-          <Box style={{...globalStyles.flexBoxColumn, alignItems: 'flex-end', width: '50%'}}>
-            <Box
-              style={{
-                borderColor: globalColors.grey,
-                borderStyle: 'dashed',
-                borderWidth: 2,
-                height: globalMargins[size],
-                marginRight: 24,
-                width: globalMargins[size],
-              }}
-            />
-          </Box>
-          <Box style={{width: '50%'}}>
-            <Text type="BodySmall">{size}: </Text>
-            <Text type="BodySmall">{globalMargins[size]}px</Text>
-          </Box>
+  storiesOf('Box', module).add('Box', () =>
+    Object.keys(globalMargins).map(size => (
+      <Box key={size} style={{...globalStyles.flexBoxRow, margin: 30, width: '100%'}}>
+        <Box style={{...globalStyles.flexBoxColumn, alignItems: 'flex-end', width: '50%'}}>
+          <Box
+            style={{
+              borderColor: globalColors.grey,
+              borderStyle: 'dashed',
+              borderWidth: 2,
+              height: globalMargins[size],
+              marginRight: 24,
+              width: globalMargins[size],
+            }}
+          />
         </Box>
-      ))}
-    </ScrollView>
-  ))
+        <Box style={{width: '50%'}}>
+          <Text type="BodySmall">{size}: </Text>
+          <Text type="BodySmall">{globalMargins[size]}px</Text>
+        </Box>
+      </Box>
+    ))
+  )
 }
 
 export default load
--- shared/stories/index.native.js
+++ shared/stories/index.native.js
@@ -1,11 +1,19 @@
 /* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
 // @flow
-import {configure} from '@storybook/react-native'
-import loadBox from '../common-adapters/box.stories'
+import React from 'react'
+import ScrollView from '../common-adapters/scroll-view'
 import loadAvatar from '../common-adapters/avatar.stories'
+import loadBox from '../common-adapters/box.stories'
+import {configure, addDecorator} from '@storybook/react-native'
 
 // Load common-adapter stories
 const load = () => {
+  addDecorator(story => (
+    <ScrollView style={{flex: 1}}>
+      {story()}
+    </ScrollView>
+  ))
+
   configure(() => {
     loadBox()
     loadAvatar()
