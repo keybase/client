d4c2e2c24c835800fde006348935884b281149e5
chrisnojima <cnojima@keyba.se>
2017-06-27 15:42:27 -0400

    WIP

--- shared/common-adapters/box.stories.js
+++ shared/common-adapters/box.stories.js
@@ -7,27 +7,31 @@ import ScrollView from './scroll-view'
 import {storiesOf} from '../stories/storybook'
 import {globalMargins, globalStyles, globalColors} from '../styles'
 
-storiesOf('Box', module).add('Box', () => (
-  <ScrollView style={{flex: 1}}>
-    {Object.keys(globalMargins).map(size => (
-      <Box key={size} style={{...globalStyles.flexBoxRow, margin: 30, width: '100%'}}>
-        <Box style={{...globalStyles.flexBoxColumn, alignItems: 'flex-end', width: '50%'}}>
-          <Box
-            style={{
-              borderColor: globalColors.grey,
-              borderStyle: 'dashed',
-              borderWidth: 2,
-              height: globalMargins[size],
-              marginRight: 24,
-              width: globalMargins[size],
-            }}
-          />
+const load = () => {
+  storiesOf('Box', module).add('Box', () => (
+    <ScrollView style={{flex: 1}}>
+      {Object.keys(globalMargins).map(size => (
+        <Box key={size} style={{...globalStyles.flexBoxRow, margin: 30, width: '100%'}}>
+          <Box style={{...globalStyles.flexBoxColumn, alignItems: 'flex-end', width: '50%'}}>
+            <Box
+              style={{
+                borderColor: globalColors.grey,
+                borderStyle: 'dashed',
+                borderWidth: 2,
+                height: globalMargins[size],
+                marginRight: 24,
+                width: globalMargins[size],
+              }}
+            />
+          </Box>
+          <Box style={{width: '50%'}}>
+            <Text type="BodySmall">{size}: </Text>
+            <Text type="BodySmall">{globalMargins[size]}px</Text>
+          </Box>
         </Box>
-        <Box style={{width: '50%'}}>
-          <Text type="BodySmall">{size}: </Text>
-          <Text type="BodySmall">{globalMargins[size]}px</Text>
-        </Box>
-      </Box>
-    ))}
-  </ScrollView>
-))
+      ))}
+    </ScrollView>
+  ))
+}
+
+export default load
