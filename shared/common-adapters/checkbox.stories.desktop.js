758975a5048c6923df1e63c7c1d39371b50937f6
chrisnojima <cnojima@keyba.se>
2017-06-28 10:06:44 -0400

    WIP

--- shared/common-adapters/checkbox.stories.desktop.js
+++ shared/common-adapters/checkbox.stories.desktop.js
@@ -2,8 +2,8 @@
 /* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
 import Checkbox from './checkbox'
 import React from 'react'
-import {action} from '@storybook/addon-actions'
-import {storiesOf} from '@storybook/react'
+// import {action} from '@storybook/addon-actions'
+// import {storiesOf} from '@storybook/react'
 
 const commonProps = {
   disabled: false,
@@ -11,11 +11,15 @@ const commonProps = {
   style: {margin: 10},
 }
 
-storiesOf('Checkbox', module).add('Checkbox', () => (
-  <div style={{flex: 1, overflow: 'auto'}}>
-    <Checkbox {...commonProps} label="Checkbox Unchecked Enabled" checked={false} />
-    <Checkbox {...commonProps} label="Checkbox Checked Enabled" checked={true} />
-    <Checkbox {...commonProps} label="Checkbox Unchecked Disabled" checked={false} disabled={true} />
-    <Checkbox {...commonProps} label="Checkbox Checked Disabled" checked={true} />
-  </div>
-))
+const load = () => {
+  // storiesOf('Checkbox', module).add('Checkbox', () => (
+  // <div style={{flex: 1, overflow: 'auto'}}>
+  // <Checkbox {...commonProps} label="Checkbox Unchecked Enabled" checked={false} />
+  // <Checkbox {...commonProps} label="Checkbox Checked Enabled" checked={true} />
+  // <Checkbox {...commonProps} label="Checkbox Unchecked Disabled" checked={false} disabled={true} />
+  // <Checkbox {...commonProps} label="Checkbox Checked Disabled" checked={true} />
+  // </div>
+  // ))
+}
+
+export default load
--- shared/stories/index.desktop.js
+++ shared/stories/index.desktop.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
 // @flow
 import React from 'react'
+import ScrollView from '../common-adapters/scroll-view'
 import {configure, addDecorator} from '@storybook/react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import materialTheme from '../styles/material-theme.desktop'
@@ -11,17 +12,21 @@ import '../desktop/renderer/style.css'
 const load = () => {
   // Load common-adapter stories
   // $FlowIssue
-  const req = require.context('../common-adapters', true, /\.stories(\.desktop)?\.js$/)
+  const req = require.context('../common-adapters', true, /\.stories\.js$/)
 
   // Add material-ui dependency
   addDecorator(story => (
     <MuiThemeProvider muiTheme={materialTheme}>
-      {story()}
+      <ScrollView key="scrollview" style={{flex: 1}}>
+        {story()}
+      </ScrollView>
     </MuiThemeProvider>
   ))
 
   configure(() => {
-    req.keys().forEach(filename => req(filename))
+    req.keys().forEach(filename => {
+      req(filename).default()
+    })
   }, module)
 }
 
