c16de197b464c7f00e5059e660530bdd319be789
Mike Maxim <mike@keyba.se>
2017-06-28 14:00:31 -0600

    set ident mode when creating a new conv in JSON API (#7567)

--- go/client/chat_svc_handler.go
+++ go/client/chat_svc_handler.go
@@ -721,10 +721,11 @@ func (c *chatServiceHandler) makePostHeader(ctx context.Context, arg sendArgV1, 
 		}
 
 		ncres, err := client.NewConversationLocal(ctx, chat1.NewConversationLocalArg{
-			TlfName:       arg.channel.Name,
-			TlfVisibility: visibility,
-			TopicName:     &arg.channel.TopicName,
-			TopicType:     tt,
+			TlfName:          arg.channel.Name,
+			TlfVisibility:    visibility,
+			TopicName:        &arg.channel.TopicName,
+			TopicType:        tt,
+			IdentifyBehavior: keybase1.TLFIdentifyBehavior_CHAT_CLI,
 		})
 		if err != nil {
 			return nil, err
