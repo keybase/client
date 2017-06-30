c99477511a0db8c3fc6d90a9b3819742f647a19d
chrisnojima <cnojima@keyba.se>
2017-06-30 10:48:42 -0400

    use regular flow for forward

--- shared/local-debug.native.js
+++ shared/local-debug.native.js
@@ -8,6 +8,7 @@ import {updateDebugConfig} from './actions/dev'
 
 const nativeBridge = NativeModules.KeybaseEngine
 
+// Set this to true if you want to turn off most console logging so you can profile easier
 const PERF = false
 
 let config: {[key: string]: any} = {
