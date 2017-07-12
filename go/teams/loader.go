39563d587edd9d3921414f2200b666e79a833f35
Miles Steele <miles@milessteele.com>
2017-07-12 18:02:00 -0400

    support 0 seqno in wantmembers

--- go/teams/loader.go
+++ go/teams/loader.go
@@ -93,16 +93,24 @@ func (l *TeamLoader) load1(ctx context.Context, me keybase1.UserVersion, lArg ke
 		}
 	}
 
+	mungedForceRepoll := lArg.ForceRepoll
+	mungedWantMembers, err := l.mungeWantMembers(ctx, lArg.Refreshers.WantMembers)
+	if err != nil {
+		l.G().Log.CDebugf(ctx, "TeamLoader munge failed: %v", err)
+		// drop the error and just force a repoll.
+		mungedForceRepoll = true
+	}
+
 	var ret *keybase1.TeamData
 	ret, err = l.load2(ctx, load2ArgT{
 		teamID: teamID,
 
 		needAdmin:         lArg.NeedAdmin,
 		needKeyGeneration: lArg.Refreshers.NeedKeyGeneration,
-		wantMembers:       lArg.Refreshers.WantMembers,
+		wantMembers:       mungedWantMembers,
 		wantMembersRole:   lArg.Refreshers.WantMembersRole,
 		forceFullReload:   lArg.ForceFullReload,
-		forceRepoll:       lArg.ForceRepoll,
+		forceRepoll:       mungedForceRepoll,
 		staleOK:           lArg.StaleOK,
 
 		needSeqnos:    nil,
@@ -180,11 +188,13 @@ type load2ArgT struct {
 
 	needAdmin         bool
 	needKeyGeneration keybase1.PerTeamKeyGeneration
-	wantMembers       []keybase1.UserVersion
-	wantMembersRole   keybase1.TeamRole
-	forceFullReload   bool
-	forceRepoll       bool
-	staleOK           bool
+	// wantMembers here is different from wantMembers on LoadTeamArg:
+	// The EldestSeqno's should not be 0.
+	wantMembers     []keybase1.UserVersion
+	wantMembersRole keybase1.TeamRole
+	forceFullReload bool
+	forceRepoll     bool
+	staleOK         bool
 
 	needSeqnos []keybase1.Seqno
 	// Non-nil if we are loading an ancestor for the greater purpose of
@@ -556,7 +566,7 @@ func (l *TeamLoader) satisfiesNeedKeyGeneration(ctx context.Context, needKeyGene
 	return nil
 }
 
-// Whether the snapshot has all of `wantMembers` as a member.
+// Whether the snapshot has each of `wantMembers` as a member.
 func (l *TeamLoader) satisfiesWantMembers(ctx context.Context,
 	wantMembers []keybase1.UserVersion, wantMembersRole keybase1.TeamRole, state *keybase1.TeamData) error {
 
@@ -594,6 +604,24 @@ func (l *TeamLoader) lookupMerkle(ctx context.Context, teamID keybase1.TeamID) (
 		return r1, r2, fmt.Errorf("merkle returned nil leaf")
 	}
 	return leaf.Private.Seqno, leaf.Private.LinkID.Export(), nil
+}
+
+func (l *TeamLoader) mungeWantMembers(ctx context.Context, wantMembers []keybase1.UserVersion) (res []keybase1.UserVersion, err error) {
+	for _, uv1 := range wantMembers {
+		uv2 := uv1
+		if uv2.EldestSeqno == 0 {
+			// Lookup the latest eldest seqno for that uid.
+			// This value may come from a cache.
+			upak, err := loadUPAK2(ctx, l.G(), uv2.Uid, false /*forcePoll */)
+			if err != nil {
+				return res, err
+			}
+			uv2.EldestSeqno = upak.Current.EldestSeqno
+			l.G().Log.CDebugf(ctx, "TeamLoader resolved wantMember %v -> %v", uv2.Uid, uv2.EldestSeqno)
+		}
+		res = append(res, uv2)
+	}
+	return res, err
 }
 
 // Whether y is in xs.
