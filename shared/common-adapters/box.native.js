58d25a2f050e8bbb5241f3b6d27f3928d5b85d8e
chrisnojima <cnojima@keyba.se>
2017-07-14 11:56:55 -0400

    color boxes to debug

--- shared/common-adapters/box.native.js
+++ shared/common-adapters/box.native.js
@@ -1,3 +1,23 @@
 // @flow
-import {View} from 'react-native'
+import React, {Component} from 'react'
+import {View as NativeView} from 'react-native'
+import {colorBoxes} from '../local-debug.native'
+
+// Has to be a class as we use refs to this sometimes apparently
+class ColorView extends Component<void, any, void> {
+  render() {
+    return (
+      <NativeView
+        {...this.props}
+        style={{
+          ...this.props.style,
+          backgroundColor: `rgba(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255},1)`,
+        }}
+      />
+    )
+  }
+}
+
+const View = colorBoxes ? ColorView : NativeView
+
 export default View
