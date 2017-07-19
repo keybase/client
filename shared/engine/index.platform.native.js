340ba62bc929349c15d1d6aa92bd9d432a032aba
chrisnojima <cnojima@keyba.se>
2017-07-19 11:09:13 -0400

    use new buffer for perf

--- shared/engine/index.platform.native.js
+++ shared/engine/index.platform.native.js
@@ -1,9 +1,9 @@
 // @flow
-import {Buffer} from 'buffer'
 import {NativeModules, NativeEventEmitter} from 'react-native'
 import {TransportShared, sharedCreateClient, rpcLog} from './transport-shared'
 import {pack} from 'purepack'
 import {toByteArray, fromByteArray} from 'base64-js'
+import toBuffer from 'typedarray-to-buffer'
 
 import type {createClientType, incomingRPCCallbackType, connectDisconnectCB} from './index.platform'
 
@@ -40,53 +40,9 @@ class NativeTransport extends TransportShared {
     return 1
   } // eslint-disable-line camelcase
 
-  // We get called 2 times per msg. once with the lenth and once with the payload
-  // // TODO DEL
-  _raw_write(bufStr: any, enc: any) {
-    // eslint-disable-line camelcase
-    if (this.rawWriteLength === null) {
-      this.rawWriteLength = Buffer.from(bufStr, enc)
-    } else {
-      const buffer = Buffer.concat([this.rawWriteLength, Buffer.from(bufStr, enc)])
-      this.rawWriteLength = null
-      // We have to write b64 encoded data over the RN bridge
-      // this.writeCallback(buffer.toString('base64'))
-    }
-  }
-
   // A custom send override to write b64 to the react native bridge
   send(msg) {
-    const oldStart = performance.now()
-    const TIMES = 1000
-    for (i = 0; i < TIMES; ++i) {
-      var b, b1, b2, bufs, enc, rc, _i, _len
-      this.rawWriteLength = null
-      b2 = pack(msg)
-      b1 = pack(b2.length)
-      bufs = [b1, b2]
-      rc = 0
-      enc = 'binary'
-      for ((_i = 0), (_len = bufs.length); _i < _len; _i++) {
-        b = bufs[_i]
-        this._raw_write(b.toString(enc), enc)
-      }
-    }
-    const old = performance.now() - oldStart
-    const newStart = performance.now()
-    for (i = 0; i < TIMES; ++i) {
-      const packed = pack(msg)
-      const len = pack(packed.length)
-      // We have to write b64 encoded data over the RN bridge
-
-      const buf = new Uint8Array(len.length + packed.length)
-      buf.set(len, 0)
-      buf.set(packed, len.length)
-      const b64 = fromByteArray(buf)
-    }
-    const n = performance.now() - newStart
-
     const packed = pack(msg)
-    console._log('aaa write', old, ' -> ', n, ' -- ', packed.length)
     const len = pack(packed.length)
     // We have to write b64 encoded data over the RN bridge
 
@@ -112,28 +68,7 @@ function createClient(
 
   // This is how the RN side writes back to us
   RNEmitter.addListener(nativeBridge.eventName, payload => {
-    const oldStart = performance.now()
-    const TIMES = 1000
-    for (i = 0; i < TIMES; ++i) {
-      const buffer = Buffer.from(payload, 'base64')
-    }
-    const old = performance.now() - oldStart
-    const newStart = performance.now()
-    for (i = 0; i < TIMES; ++i) {
-      // const buffer = Buffer.from(new Uint8Array(atob(payload).split('').map(c => c.charCodeAt(0))))
-      const buffer = toByteArray(payload)
-    }
-    const n = performance.now() - newStart
-
-    console._log('aaa read', old, ' -> ', n, ' -- ', payload.length)
-
-    // const oldBuffer = Buffer.from(payload, 'base64')
-    // const newBuffer = Buffer.from(new Uint8Array(atob(payload).split('').map(c => c.charCodeAt(0))))
-    //
-    const buffer = Buffer.from(payload, 'base64')
-    // const newBuffer = toByteArray(payload)
-    // console.log('aaa', oldBuffer, newBuffer)
-    return client.transport.packetize_data(buffer)
+    return client.transport.packetize_data(toBuffer(toByteArray(payload)))
   })
 
   return client
