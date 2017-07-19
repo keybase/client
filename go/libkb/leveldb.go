b3e960b724dcb133d6ba3fa126cd89deef1042aa
Maxwell Krohn <themax@gmail.com>
2017-07-19 12:54:52 -0400

    comments from review

--- go/libkb/leveldb.go
+++ go/libkb/leveldb.go
@@ -105,7 +105,7 @@ func (l *LevelDb) Open() error { return nil }
 
 func (l *LevelDb) Opts() *opt.Options {
 	return &opt.Options{
-		OpenFilesCacheCapacity: 16, // Only 16 files open by level DB per db
+		OpenFilesCacheCapacity: 64, // Only 64 files open by level DB per db
 	}
 }
 
