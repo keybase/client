084aff672dc63530f3571c6f7743c1097e43530e
Jeremy Stribling <strib@alum.mit.edu>
2017-07-19 17:27:34 -0700

    util: factor out eventually-consistent fetch decider
    For use in a future commit.
    
    Issue: KBFS-2271

--- libkbfs/quota_usage.go
+++ libkbfs/quota_usage.go
@@ -6,7 +6,6 @@ package libkbfs
 
 import (
 	"sync"
-	"sync/atomic"
 	"time"
 
 	"github.com/keybase/client/go/logger"
@@ -15,7 +14,7 @@ import (
 	"golang.org/x/net/context"
 )
 
-// ECQUCtxTagKey is the type for unique ECQU background opertaion IDs.
+// ECQUCtxTagKey is the type for unique ECQU background operation IDs.
 type ECQUCtxTagKey struct{}
 
 // ECQUID is used in EventuallyConsistentQuotaUsage for only background RPCs.
@@ -116,46 +115,24 @@ func (q *EventuallyConsistentQuotaUsage) Get(
 		defer q.mu.RUnlock()
 		return q.cached
 	}()
-	past := q.config.Clock().Now().Sub(c.timestamp)
-	switch {
-	case past > blockTolerance || c.timestamp.IsZero():
-		q.log.CDebugf(ctx, "Blocking on getAndCache. Cached data is %s old.", past)
-		// TODO: optimize this to make sure there's only one outstanding RPC. In
-		// other words, wait for it to finish if one is already in progress.
+	getAndCache := func(ctx context.Context) {
+		// The error is igonred here without logging since getAndCache already
+		// logs it.
+		_, _ = q.getAndCache(ctx)
+	}
+	decision, err := fetchAndCacheDecider(
+		ctx, bgTolerance, blockTolerance, c.timestamp, getAndCache,
+		&q.backgroundInProcess, q.log, ECQUCtxTagKey{}, ECQUID,
+		q.config.Clock())
+	if err != nil {
+		return time.Time{}, -1, -1, err
+	}
+
+	if decision == fetchAndCache {
 		c, err = q.getAndCache(ctx)
 		if err != nil {
 			return time.Time{}, -1, -1, err
 		}
-	case past > bgTolerance:
-		if atomic.CompareAndSwapInt32(&q.backgroundInProcess, 0, 1) {
-			id, err := MakeRandomRequestID()
-			if err != nil {
-				q.log.Warning("Couldn't generate a random request ID: %v", err)
-			}
-			q.log.CDebugf(ctx, "Cached data is %s old. Spawning getAndCache in "+
-				"background with tag:%s=%v.", past, ECQUID, id)
-			go func() {
-				// Make a new context so that it doesn't get canceled when returned.
-				logTags := make(logger.CtxLogTags)
-				logTags[ECQUCtxTagKey{}] = ECQUID
-				bgCtx := logger.NewContextWithLogTags(context.Background(), logTags)
-				bgCtx = context.WithValue(bgCtx, ECQUCtxTagKey{}, id)
-				// Make sure a timeout is on the context, in case the RPC blocks
-				// forever somehow, where we'd end up with never resetting
-				// backgroundInProcess flag again.
-				bgCtx, cancel := context.WithTimeout(bgCtx, 10*time.Second)
-				defer cancel()
-				// The error is igonred here without logging since getAndCache already
-				// logs it.
-				_, _ = q.getAndCache(bgCtx)
-				atomic.StoreInt32(&q.backgroundInProcess, 0)
-			}()
-		} else {
-			q.log.CDebugf(ctx,
-				"Cached data is %s old, but background getAndCache is already running.", past)
-		}
-	default:
-		q.log.CDebugf(ctx, "Returning cached data from %s ago.", past)
 	}
 	return c.timestamp, c.usageBytes, c.limitBytes, nil
 }
