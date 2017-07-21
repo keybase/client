9d7d41bf228f6f1f3fe660df84c9c4a2b1d93799
Jack O'Connor <oconnor663@gmail.com>
2017-07-21 16:12:55 -0400

    revendor go-framed-msgpack-rpc
    One more callsite for res_init.

--- vendor/github.com/keybase/go-framed-msgpack-rpc/rpc/resinit/resinit_nix.go
+++ vendor/github.com/keybase/go-framed-msgpack-rpc/rpc/resinit/resinit_nix.go
@@ -5,6 +5,10 @@
 
 package resinit
 
+// NOTE: The Go compiler will completely skip this file if cgo is disabled,
+// which is the default for any kind of cross-compiling (e.g. GOARCH=386). In
+// that case you must set CGO_ENABLED=1 in the environment.
+
 // #cgo LDFLAGS: -lresolv
 // #include<resolv.h>
 import "C"
