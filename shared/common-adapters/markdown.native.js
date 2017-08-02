8db1682d6928d808232e776feeba588ae4169c7f
chrisnojima <cnojima@keyba.se>
2017-08-02 15:25:50 -0400

    fix single line snippets

--- shared/common-adapters/markdown.native.js
+++ shared/common-adapters/markdown.native.js
@@ -61,7 +61,7 @@ function previewCreateComponent(style) {
       case 'native-emoji':
         return <Emoji emojiName={String(children)} size={12} key={key} />
       default:
-        return <Text type="Body" key={key} style={[neutralStyle, style]}>{children}</Text>
+        return <Text type="Body" key={key} lineClamp={1} style={[neutralStyle, style]}>{children}</Text>
     }
   }
 }
@@ -172,11 +172,15 @@ class Markdown extends PureComponent<void, Props, void> {
       : messageCreateComponent(this.props.style, this.props.allowFontScaling)
     const content = parseMarkdown(this.props.children, createComponent)
     if (typeof content === 'string') {
-      return (
-        <Text type="Body" style={this.props.style} allowFontScaling={this.props.allowFontScaling}>
-          {content}
-        </Text>
-      )
+      if (this.props.preview) {
+        return createComponent('', '', content, {})
+      } else {
+        return (
+          <Text type="Body" style={this.props.style} allowFontScaling={this.props.allowFontScaling}>
+            {content}
+          </Text>
+        )
+      }
     }
     return content
   }
