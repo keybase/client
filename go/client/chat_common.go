e7098bcf153e6a3981eb7e498ca9965935b285a1
Surya <surya@keyba.se>
2017-08-04 13:09:44 -0400

    support for sending to TLFs

--- go/client/chat_common.go
+++ go/client/chat_common.go
@@ -4,8 +4,6 @@
 package client
 
 import (
-	"fmt"
-
 	"golang.org/x/net/context"
 
 	"github.com/keybase/client/go/libkb"
@@ -13,19 +11,6 @@ import (
 )
 
 func CheckUserOrTeamName(ctx context.Context, g *libkb.GlobalContext, name string) (*keybase1.UserOrTeamResult, error) {
-	userCli, err := GetUserClient(g)
-	if err != nil {
-		return nil, err
-	}
-	_, err = userCli.LoadUserByName(ctx, keybase1.LoadUserByNameArg{Username: name})
-	if err == nil {
-		ret := keybase1.UserOrTeamResult_USER
-		return &ret, nil
-	}
-	if _, ok := err.(libkb.NotFoundError); !ok {
-		return nil, err
-	}
-
 	cli, err := GetTeamsClient(g)
 	if err != nil {
 		return nil, err
@@ -35,10 +20,8 @@ func CheckUserOrTeamName(ctx context.Context, g *libkb.GlobalContext, name strin
 		ret := keybase1.UserOrTeamResult_TEAM
 		return &ret, nil
 	}
-	ase, ok := err.(libkb.AppStatusError)
-	if !ok || keybase1.StatusCode(ase.Code) != keybase1.StatusCode_SCTeamNotFound {
-		return nil, err
-	}
 
-	return nil, libkb.NotFoundError{Msg: fmt.Sprintf("%s is neither a username or a team name.", name)}
+	// Assume name is a User or a TLF
+	ret := keybase1.UserOrTeamResult_USER
+	return &ret, nil
 }
