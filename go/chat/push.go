9be12a5e0358e72b67fb1b375313b4a03d3ce445
Mike Maxim <mike@keyba.se>
2017-08-08 16:53:20 -0400

    get inbox version inside of this lock, so we dont race to get on waiter list and get stuck (#8049)

--- go/chat/push.go
+++ go/chat/push.go
@@ -117,25 +117,16 @@ func (g *gregorMessageOrderer) cleanupAfterTimeoutLocked(uid gregor1.UID, vers c
 func (g *gregorMessageOrderer) WaitForTurn(ctx context.Context, uid gregor1.UID,
 	newVers chat1.InboxVers) (res chan struct{}) {
 	res = make(chan struct{})
-	// Grab latest inbox version if we can
-	vers, err := g.latestInboxVersion(ctx, uid)
-	if err != nil {
-		g.Debug(ctx, "WaitForTurn: failed to get current inbox version: %s", err.Error())
-		close(res)
-		return res
-	}
-
-	// Check for an in-order update
-	if newVers <= vers+1 {
-		close(res)
-		return
-	}
-
 	// Out of order update, we are going to wait a fixed amount of time for the correctly
 	// ordered update
 	deadline := g.clock.Now().Add(time.Second)
 	go func() {
 		g.Lock()
+		vers, err := g.latestInboxVersion(ctx, uid)
+		if err != nil {
+			g.Debug(ctx, "WaitForTurn: failed to get current inbox version: %s", err.Error())
+			vers = newVers - 1
+		}
 		waiters := g.addToWaitersLocked(ctx, uid, vers, newVers)
 		g.Unlock()
 		g.Debug(ctx, "WaitForTurn: out of order update received, waiting on %d updates: vers: %d newVers: %d", len(waiters), vers, newVers)
