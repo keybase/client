e93a58d67a9f4e2e30383852bf96694aad8c3cbe
chrisnojima <cnojima@keyba.se>
2017-08-09 16:37:06 -0400

    add old design

--- shared/chat/inbox/row/add-new-row.js
+++ shared/chat/inbox/row/add-new-row.js
@@ -22,43 +22,75 @@ const _AddNewRow = ({
   <Box style={{...globalStyles.flexBoxRow, alignItems: 'center', minHeight: 48, position: 'relative'}}>
     <ClickableBox
       style={{
-        ...globalStyles.flexBoxCenter,
+        ...globalStyles.flexBoxRow,
+        alignItems: 'center',
         flexGrow: 1,
         flexShrink: 0,
+        justifyContent: 'center',
         paddingLeft: globalMargins.medium,
         paddingRight: globalMargins.medium,
       }}
-      onClick={onJumpToChat}
-    >
-      <Box
-        style={{
-          ...globalStyles.flexBoxRow,
-          alignItems: 'center',
-          backgroundColor: globalColors.lightGrey,
-          borderRadius: 19,
-          flexShrink: 0,
-          height: globalMargins.medium,
-          justifyContent: 'center',
-          width: '100%',
-        }}
-      >
-        <Icon type="iconfont-search" style={{color: globalColors.black_20, fontSize: 12}} />
-        <Text type="Body" style={{color: globalColors.black_20, marginLeft: globalMargins.tiny}}>
-          Jump to chat
-        </Text>
-      </Box>
-    </ClickableBox>
-    <Icon
-      type="iconfont-add"
-      style={{color: globalColors.black_40, marginRight: 12, marginTop: 2}}
       onClick={onNewChat}
-    />
+    >
+      <Icon type="iconfont-new" style={{color: globalColors.blue, marginRight: 9}} />
+      <Text type="BodyBigLink">New chat</Text>
+    </ClickableBox>
     {isLoading &&
       <Box style={{bottom: 0, left: 0, position: 'absolute', right: 0}}>
         <LoadingLine />
       </Box>}
   </Box>
 )
+
+// const _AddNewRow= ({
+// onNewChat,
+// onJumpToChat,
+// isLoading,
+// }: {
+// onNewChat: () => void,
+// onJumpToChat: () => void,
+// isLoading: boolean,
+// }) => (
+// <Box style={{...globalStyles.flexBoxRow, alignItems: 'center', minHeight: 48, position: 'relative'}}>
+// <ClickableBox
+// style={{
+// ...globalStyles.flexBoxCenter,
+// flexGrow: 1,
+// flexShrink: 0,
+// paddingLeft: globalMargins.medium,
+// paddingRight: globalMargins.medium,
+// }}
+// onClick={onJumpToChat}
+// >
+// <Box
+// style={{
+// ...globalStyles.flexBoxRow,
+// alignItems: 'center',
+// backgroundColor: globalColors.lightGrey,
+// borderRadius: 19,
+// flexShrink: 0,
+// height: globalMargins.medium,
+// justifyContent: 'center',
+// width: '100%',
+// }}
+// >
+// <Icon type="iconfont-search" style={{color: globalColors.black_20, fontSize: 12}} />
+// <Text type="Body" style={{color: globalColors.black_20, marginLeft: globalMargins.tiny}}>
+// Jump to chat
+// </Text>
+// </Box>
+// </ClickableBox>
+// <Icon
+// type="iconfont-add"
+// style={{color: globalColors.black_40, marginRight: 12, marginTop: 2}}
+// onClick={onNewChat}
+// />
+// {isLoading &&
+// <Box style={{bottom: 0, left: 0, position: 'absolute', right: 0}}>
+// <LoadingLine />
+// </Box>}
+// </Box>
+// )
 
 const AddNewRow = branch(() => !isMobile, KeyHandler)(_AddNewRow)
 
