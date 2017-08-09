5b3588ff27977683888117e9b670ef0bfa070d9b
Surya <surya@keyba.se>
2017-08-09 14:48:01 -0400

    better errors on not found

--- go/client/chat_common.go
+++ go/client/chat_common.go
@@ -4,13 +4,32 @@
 package client
 
 import (
+	"fmt"
+	"strings"
+
 	"golang.org/x/net/context"
 
 	"github.com/keybase/client/go/libkb"
 	"github.com/keybase/client/go/protocol/keybase1"
+	"github.com/keybase/client/go/teams"
 )
 
 func CheckUserOrTeamName(ctx context.Context, g *libkb.GlobalContext, name string) (*keybase1.UserOrTeamResult, error) {
+	userCli, err := GetUserClient(g)
+	if err != nil {
+		return nil, err
+	}
+	_, err = userCli.LoadUserByName(ctx, keybase1.LoadUserByNameArg{Username: name})
+	if err == nil {
+		ret := keybase1.UserOrTeamResult_USER
+		return &ret, nil
+	}
+	_, okNotFound := err.(libkb.NotFoundError)
+	_, okInvalidUsername := err.(libkb.BadUsernameError)
+	if !(okNotFound || okInvalidUsername) {
+		return nil, err
+	}
+
 	cli, err := GetTeamsClient(g)
 	if err != nil {
 		return nil, err
@@ -21,7 +40,12 @@ func CheckUserOrTeamName(ctx context.Context, g *libkb.GlobalContext, name strin
 		return &ret, nil
 	}
 
-	// Assume name is a User or a TLF
-	ret := keybase1.UserOrTeamResult_USER
-	return &ret, nil
+	ase, okNet := err.(libkb.AppStatusError)
+	_, okNotFound = err.(teams.TeamDoesNotExistError)
+
+	if !(okNet && keybase1.StatusCode(ase.Code) == keybase1.StatusCode_SCTeamNotFound) && !okNotFound && !strings.HasSuffix(err.Error(), "does not exist") {
+		return nil, err
+	}
+
+	return nil, libkb.NotFoundError{Msg: fmt.Sprintf("%s is neither a username or a team name.", name)}
 }
