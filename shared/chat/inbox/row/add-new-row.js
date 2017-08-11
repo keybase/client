ac7e0e8b6f23baf1b54e313c365983b20b481133
Frederick Akalin <akalin@keyba.se>
2017-08-11 14:31:55 -0500

    Add editing mode

--- shared/chat/inbox/row/add-new-row.js
+++ shared/chat/inbox/row/add-new-row.js
@@ -1,6 +1,6 @@
 // @flow
 import React, {Component} from 'react'
-import {Icon, Box, LoadingLine, Input} from '../../../common-adapters'
+import {Icon, Box, LoadingLine, Input, Text} from '../../../common-adapters'
 import {globalStyles, globalColors} from '../../../styles'
 import {isMobile} from '../../../constants/platform'
 import {branch} from 'recompose'
@@ -18,6 +18,7 @@ type Props = {
 }
 
 type State = {
+  inEditingMode: boolean,
   filter: string,
 }
 
@@ -27,20 +28,62 @@ class _AddNewRow extends Component<void, Props, State> {
   constructor(props: Props) {
     super(props)
     this.state = {
+      inEditingMode: false,
       filter: '',
     }
   }
 
   _setFilter = filter => {
-    this.setState({filter})
+    this.setState({
+      inEditingMode: true,
+      filter,
+    })
     this.props.onSetFilter(filter)
   }
 
+  _enterEditingMode = () => {
+    this.setState({
+      inEditingMode: true,
+      filter: '',
+    })
+  }
+
+  _leaveEditingMode = () => {
+    this.setState({
+      inEditingMode: false,
+      filter: '',
+    })
+  }
+
   render() {
+    let children
+    if (this.state.inEditingMode) {
+      children = [
+        <Icon
+          key="0"
+          type="iconfont-search"
+          style={{color: globalColors.grey, marginLeft: 9, marginRight: 9}}
+        />,
+        <Input
+          key="1"
+          small={true}
+          value={this.state.filter}
+          hintText="Jump to..."
+          onChangeText={this._setFilter}
+          onBlur={() => this._leaveEditingMode()}
+        />,
+      ]
+    } else {
+      children = (
+        <Box style={styleFilterContainer} onClick={() => this._enterEditingMode()}>
+          <Icon type="iconfont-search" style={{marginLeft: 9, marginRight: 9}} />
+          <Text type="Body">Jump to chat</Text>
+        </Box>
+      )
+    }
     return (
       <Box style={{...globalStyles.flexBoxRow, alignItems: 'center', minHeight: 48, position: 'relative'}}>
-        <Icon type="iconfont-search" style={{color: globalColors.grey, marginLeft: 9, marginRight: 9}} />
-        <Input small={true} value={this.state.filter} hintText="Jump to..." onChangeText={this._setFilter} />
+        {children}
         <Icon
           type="iconfont-new"
           style={{color: globalColors.blue, marginLeft: 9, marginRight: 9}}
@@ -53,6 +96,16 @@ class _AddNewRow extends Component<void, Props, State> {
       </Box>
     )
   }
+}
+
+const styleFilterContainer = {
+  ...globalStyles.flexBoxRow,
+  ...globalStyles.clickable,
+  alignItems: 'center',
+  backgroundColor: globalColors.grey,
+  borderRadius: 100,
+  minHeight: 24,
+  minWidth: 160,
 }
 
 const AddNewRow = branch(() => !isMobile, KeyHandler)(_AddNewRow)
