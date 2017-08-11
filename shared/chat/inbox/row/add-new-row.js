6c79b98a5431363efbe37a648f724003065dbd6e
Frederick Akalin <akalin@keyba.se>
2017-08-11 14:02:57 -0500

    Rem commented out code

--- shared/chat/inbox/row/add-new-row.js
+++ shared/chat/inbox/row/add-new-row.js
@@ -55,56 +55,6 @@ class _AddNewRow extends Component<void, Props, State> {
   }
 }
 
-// const _AddNewRow= ({
-// onNewChat,
-// onJumpToChat,
-// isLoading,
-// }: {
-// onNewChat: () => void,
-// onJumpToChat: () => void,
-// isLoading: boolean,
-// }) => (
-// <Box style={{...globalStyles.flexBoxRow, alignItems: 'center', minHeight: 48, position: 'relative'}}>
-// <ClickableBox
-// style={{
-// ...globalStyles.flexBoxCenter,
-// flexGrow: 1,
-// flexShrink: 0,
-// paddingLeft: globalMargins.medium,
-// paddingRight: globalMargins.medium,
-// }}
-// onClick={onJumpToChat}
-// >
-// <Box
-// style={{
-// ...globalStyles.flexBoxRow,
-// alignItems: 'center',
-// backgroundColor: globalColors.lightGrey,
-// borderRadius: 19,
-// flexShrink: 0,
-// height: globalMargins.medium,
-// justifyContent: 'center',
-// width: '100%',
-// }}
-// >
-// <Icon type="iconfont-search" style={{color: globalColors.black_20, fontSize: 12}} />
-// <Text type="Body" style={{color: globalColors.black_20, marginLeft: globalMargins.tiny}}>
-// Jump to chat
-// </Text>
-// </Box>
-// </ClickableBox>
-// <Icon
-// type="iconfont-add"
-// style={{color: globalColors.black_40, marginRight: 12, marginTop: 2}}
-// onClick={onNewChat}
-// />
-// {isLoading &&
-// <Box style={{bottom: 0, left: 0, position: 'absolute', right: 0}}>
-// <LoadingLine />
-// </Box>}
-// </Box>
-// )
-
 const AddNewRow = branch(() => !isMobile, KeyHandler)(_AddNewRow)
 
 export default AddNewRow
