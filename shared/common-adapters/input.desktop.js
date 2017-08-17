ead778ae8f4f91b27d65bede9515e15962d6cdf7
Frederick Akalin <akalin@keyba.se>
2017-08-17 15:47:42 -0700

    Add onEscapeKeyDown

--- shared/common-adapters/input.desktop.js
+++ shared/common-adapters/input.desktop.js
@@ -119,6 +119,10 @@ class Input extends Component<void, Props, State> {
     if (this.props.onEnterKeyDown && e.key === 'Enter' && !e.shiftKey && !this._isComposingIME) {
       this.props.onEnterKeyDown(e)
     }
+
+    if (this.props.onEscapeKeyDown && e.key === 'Escape' && !this._isComposingIME) {
+      this.props.onEscapeKeyDown(e)
+    }
   }
 
   _onFocus = () => {
