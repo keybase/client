5bf91bb37fc7a8dd37364095d92767709ec7f82e
Mike Maxim <mike@keyba.se>
2017-08-18 15:41:36 -0400

    fix

--- go/service/teams.go
+++ go/service/teams.go
@@ -200,9 +200,11 @@ func (h *TeamsHandler) GetTeamRootID(ctx context.Context, id keybase1.TeamID) (k
 }
 
 func (h *TeamsHandler) LookupImplicitTeam(ctx context.Context, arg keybase1.LookupImplicitTeamArg) (keybase1.TeamID, error) {
-	return teams.LookupImplicitTeam(ctx, h.G().ExternalG(), arg.Name, arg.Public)
+	teamID, _, err := teams.LookupImplicitTeam(ctx, h.G().ExternalG(), arg.Name, arg.Public)
+	return teamID, err
 }
 
 func (h *TeamsHandler) LookupOrCreateImplicitTeam(ctx context.Context, arg keybase1.LookupOrCreateImplicitTeamArg) (keybase1.TeamID, error) {
-	return teams.LookupOrCreateImplicitTeam(ctx, h.G().ExternalG(), arg.Name, arg.Public)
+	teamID, _, err := teams.LookupOrCreateImplicitTeam(ctx, h.G().ExternalG(), arg.Name, arg.Public)
+	return teamID, err
 }
