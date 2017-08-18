71560df005901223bdd6487bde299999974a046c
Frederick Akalin <akalin@gmail.com>
2017-08-18 16:54:34 -0700

    Clean up BottomLine

--- shared/chat/inbox/row/bottom-line.js
+++ shared/chat/inbox/row/bottom-line.js
@@ -1,5 +1,5 @@
 // @flow
-import React, {PureComponent} from 'react'
+import React from 'react'
 import {Text, Markdown, Box} from '../../../common-adapters'
 import {
   globalStyles,
@@ -20,96 +20,94 @@ type BottomLineProps = {
   youNeedToRekey: boolean,
 }
 
-class BottomLine extends PureComponent<void, BottomLineProps, void> {
-  render() {
-    const {participantNeedToRekey, youNeedToRekey, showBold, subColor, snippet, backgroundColor} = this.props
-    let content
+function BottomLine(props: BottomLineProps) {
+  const {participantNeedToRekey, youNeedToRekey, showBold, subColor, snippet, backgroundColor} = props
+  let content
 
-    if (youNeedToRekey) {
-      content = (
-        <Box
+  if (youNeedToRekey) {
+    content = (
+      <Box
+        style={{
+          alignSelf: 'center',
+          backgroundColor: globalColors.red,
+          borderRadius: 2,
+          paddingLeft: globalMargins.xtiny,
+          paddingRight: globalMargins.xtiny,
+        }}
+      >
+        <Text
+          type="BodySmallSemibold"
+          backgroundMode="Terminal"
           style={{
-            alignSelf: 'center',
-            backgroundColor: globalColors.red,
-            borderRadius: 2,
-            paddingLeft: globalMargins.xtiny,
-            paddingRight: globalMargins.xtiny,
+            color: globalColors.white,
+            fontSize: 11,
+            lineHeight: 14,
           }}
         >
-          <Text
-            type="BodySmallSemibold"
-            backgroundMode="Terminal"
-            style={{
-              color: globalColors.white,
-              fontSize: 11,
-              lineHeight: 14,
-            }}
-          >
-            REKEY NEEDED
-          </Text>
-        </Box>
-      )
-    } else if (participantNeedToRekey) {
-      content = (
-        <Text type="BodySmall" backgroundMode="Terminal" style={{color: subColor}}>
-          Waiting for participants to rekey
+          REKEY NEEDED
         </Text>
-      )
-    } else if (snippet) {
-      const baseStyle = styles['bottomLine']
+      </Box>
+    )
+  } else if (participantNeedToRekey) {
+    content = (
+      <Text type="BodySmall" backgroundMode="Terminal" style={{color: subColor}}>
+        Waiting for participants to rekey
+      </Text>
+    )
+  } else if (snippet) {
+    const baseStyle = styles['bottomLine']
 
-      let style
+    let style
 
-      if (subColor !== globalColors.black_40 || showBold) {
-        style = collapseStyles([
-          baseStyle,
-          {
-            color: subColor,
-            ...(showBold ? globalStyles.fontBold : {}),
-          },
-        ])
-      } else {
-        style = baseStyle
-      }
-
-      content = (
-        <Markdown preview={true} style={style}>
-          {snippet}
-        </Markdown>
-      )
+    if (subColor !== globalColors.black_40 || showBold) {
+      style = collapseStyles([
+        baseStyle,
+        {
+          color: subColor,
+          ...(showBold ? globalStyles.fontBold : {}),
+        },
+      ])
     } else {
-      return null
+      style = baseStyle
     }
 
-    const height = isMobile ? 16 : 17
-    return (
+    content = (
+      <Markdown preview={true} style={style}>
+        {snippet}
+      </Markdown>
+    )
+  } else {
+    return null
+  }
+
+  const height = isMobile ? 16 : 17
+  return (
+    <Box
+      style={{
+        ...globalStyles.flexBoxRow,
+        backgroundColor: isMobile ? backgroundColor : undefined,
+        flexGrow: 1,
+        maxHeight: height,
+        minHeight: height,
+        position: 'relative',
+      }}
+    >
       <Box
         style={{
           ...globalStyles.flexBoxRow,
-          backgroundColor: isMobile ? backgroundColor : undefined,
-          flexGrow: 1,
-          maxHeight: height,
-          minHeight: height,
-          position: 'relative',
+          alignItems: 'flex-start',
+          bottom: 0,
+          justifyContent: 'flex-start',
+          left: 0,
+          position: 'absolute',
+          right: 0,
+          top: 0,
         }}
       >
-        <Box
-          style={{
-            ...globalStyles.flexBoxRow,
-            alignItems: 'flex-start',
-            bottom: 0,
-            justifyContent: 'flex-start',
-            left: 0,
-            position: 'absolute',
-            right: 0,
-            top: 0,
-          }}
-        >
-          {content}
-        </Box>
+        {content}
       </Box>
-    )
-  }
+    </Box>
+  )
 }
 
 const noWrapStyle = {
