3d86cc7a11e70460d64c9f5a6eb9c9b3127e6d95
Frederick Akalin <akalin@gmail.com>
2017-08-18 20:59:14 -0700

    Change TopLine back to non-functional

--- shared/chat/inbox/row/top-line.js
+++ shared/chat/inbox/row/top-line.js
@@ -1,5 +1,5 @@
 // @flow
-import React from 'react'
+import React, {PureComponent} from 'react'
 import {Text, PlaintextUsernames, Box} from '../../../common-adapters'
 import {globalStyles, globalColors, lineHeight} from '../../../styles'
 import {isMobile} from '../../../constants/platform'
@@ -16,15 +16,70 @@ type SimpleTopLineProps = {
 
 const height = isMobile ? 19 : 17
 
-function SimpleTopLine(props: SimpleTopLineProps) {
-  const {hasUnread, participants, showBold, subColor, timestamp, usernameColor} = props
-  const boldOverride = showBold ? globalStyles.fontBold : null
-  return (
-    <Box style={{...globalStyles.flexBoxRow, alignItems: 'center', maxHeight: height, minHeight: height}}>
+class SimpleTopLine extends PureComponent<void, SimpleTopLineProps, void> {
+  render() {
+    const {hasUnread, participants, showBold, subColor, timestamp, usernameColor} = this.props
+    const boldOverride = showBold ? globalStyles.fontBold : null
+    return (
+      <Box style={{...globalStyles.flexBoxRow, alignItems: 'center', maxHeight: height, minHeight: height}}>
+        <Box
+          style={{
+            ...globalStyles.flexBoxRow,
+            flex: 1,
+            maxHeight: height,
+            minHeight: height,
+            position: 'relative',
+          }}
+        >
+          <Box
+            style={{
+              ...globalStyles.flexBoxColumn,
+              bottom: 0,
+              justifyContent: 'flex-start',
+              left: 0,
+              position: 'absolute',
+              right: 0,
+              top: 0,
+            }}
+          >
+            <PlaintextUsernames
+              type="BodySemibold"
+              containerStyle={{...boldOverride, color: usernameColor, paddingRight: 7}}
+              users={participants.map(p => ({username: p})).toArray()}
+              title={participants.join(', ')}
+            />
+          </Box>
+        </Box>
+        <Text
+          key="0"
+          type="BodySmall"
+          style={{...boldOverride, color: subColor, lineHeight: lineHeight(height)}}
+        >
+          {timestamp}
+        </Text>
+        {hasUnread ? <Box key="1" style={unreadDotStyle} /> : null}
+      </Box>
+    )
+  }
+}
+
+type FilteredTopLineProps = {
+  participants: List<string>,
+  showBold: boolean,
+  usernameColor: ?string,
+}
+
+class FilteredTopLine extends PureComponent<void, FilteredTopLineProps, void> {
+  render() {
+    const {participants, showBold, usernameColor} = this.props
+    const boldOverride = showBold ? globalStyles.fontBold : null
+    return (
       <Box
         style={{
           ...globalStyles.flexBoxRow,
+          alignItems: 'center',
           flex: 1,
+          justifyContent: 'flex-start',
           maxHeight: height,
           minHeight: height,
           position: 'relative',
@@ -49,59 +104,8 @@ function SimpleTopLine(props: SimpleTopLineProps) {
           />
         </Box>
       </Box>
-      <Text
-        key="0"
-        type="BodySmall"
-        style={{...boldOverride, color: subColor, lineHeight: lineHeight(height)}}
-      >
-        {timestamp}
-      </Text>
-      {hasUnread ? <Box key="1" style={unreadDotStyle} /> : null}
-    </Box>
-  )
-}
-
-type FilteredTopLineProps = {
-  participants: List<string>,
-  showBold: boolean,
-  usernameColor: ?string,
-}
-
-function FilteredTopLine(props: FilteredTopLineProps) {
-  const {participants, showBold, usernameColor} = props
-  const boldOverride = showBold ? globalStyles.fontBold : null
-  return (
-    <Box
-      style={{
-        ...globalStyles.flexBoxRow,
-        alignItems: 'center',
-        flex: 1,
-        justifyContent: 'flex-start',
-        maxHeight: height,
-        minHeight: height,
-        position: 'relative',
-      }}
-    >
-      <Box
-        style={{
-          ...globalStyles.flexBoxColumn,
-          bottom: 0,
-          justifyContent: 'flex-start',
-          left: 0,
-          position: 'absolute',
-          right: 0,
-          top: 0,
-        }}
-      >
-        <PlaintextUsernames
-          type="BodySemibold"
-          containerStyle={{...boldOverride, color: usernameColor, paddingRight: 7}}
-          users={participants.map(p => ({username: p})).toArray()}
-          title={participants.join(', ')}
-        />
-      </Box>
-    </Box>
-  )
+    )
+  }
 }
 
 const unreadDotStyle = {
