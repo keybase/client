bdbd49d4156a2b930894ef9e0fab54ff312bbc72
chrisnojima <chrisnojima@gmail.com>
2017-08-22 11:49:19 -0400

    try and fix build (#8179)

--- packaging/ios/build_and_publish.sh
+++ packaging/ios/build_and_publish.sh
@@ -39,6 +39,7 @@ if [ ! "$cache_npm" = "1" ]; then
   echo "Cleaning up main node_modules from previous runs"
   rm -rf "$shared_dir/node_modules"
 
+  yarn cache clean
   yarn install --pure-lockfile
   yarn global add react-native-cli
 fi
