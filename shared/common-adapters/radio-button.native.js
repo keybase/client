1df70140122dda14c0730d998006d10c891150c0
Chris Ball <chris@printf.net>
2017-08-21 20:47:31 -0700

    no need for a class

--- shared/common-adapters/radio-button.native.js
+++ shared/common-adapters/radio-button.native.js
@@ -8,35 +8,33 @@ import {globalStyles, globalColors} from '../styles'
 export const RADIOBUTTON_SIZE = 22
 export const RADIOBUTTON_MARGIN = 8
 
-class RadioButton extends React.Component<void, Props, void> {
-  render() {
-    const boxStyle = {
-      backgroundColor: globalColors.white,
-      border: 1,
-      borderColor: this.props.selected ? globalColors.blue : globalColors.black_10,
-      borderRadius: 100,
-      borderWidth: 1,
-      height: RADIOBUTTON_SIZE,
-      marginRight: RADIOBUTTON_MARGIN,
-      opacity: this.props.disabled ? 0.4 : 1,
-      position: 'relative',
-      width: RADIOBUTTON_SIZE,
-    }
-
-    return (
-      <ClickableBox
-        style={{...styleContainer, ...this.props.style}}
-        onClick={this.props.disabled ? undefined : () => this.props.onSelect(!this.props.selected)}
-      >
-        <ClickableBox style={boxStyle}>
-          <ClickableBox
-            style={{...styleIcon, borderColor: this.props.selected ? globalColors.blue : globalColors.white}}
-          />
-        </ClickableBox>
-        <Text type="Body" small={true} style={{color: globalColors.black_75}}>{this.props.label}</Text>
-      </ClickableBox>
-    )
+const RadioButton = ({disabled, selected, onSelect}: Props) => {
+  const boxStyle = {
+    backgroundColor: globalColors.white,
+    border: 1,
+    borderColor: this.props.selected ? globalColors.blue : globalColors.black_10,
+    borderRadius: 100,
+    borderWidth: 1,
+    height: RADIOBUTTON_SIZE,
+    marginRight: RADIOBUTTON_MARGIN,
+    opacity: this.props.disabled ? 0.4 : 1,
+    position: 'relative',
+    width: RADIOBUTTON_SIZE,
   }
+
+  return (
+    <ClickableBox
+      style={{...styleContainer, ...this.props.style}}
+      onClick={this.props.disabled ? undefined : () => this.props.onSelect(!this.props.selected)}
+    >
+      <ClickableBox style={boxStyle}>
+        <ClickableBox
+          style={{...styleIcon, borderColor: this.props.selected ? globalColors.blue : globalColors.white}}
+        />
+      </ClickableBox>
+      <Text type="Body" small={true} style={{color: globalColors.black_75}}>{this.props.label}</Text>
+    </ClickableBox>
+  )
 }
 
 const styleContainer = {
