3d2b0d1d1d06267084bcb778919d2764364d11f2
Chris Ball <chris@printf.net>
2017-08-21 20:41:53 -0700

    more storybook/dumb sheet

--- shared/common-adapters/radio-button.stories.js
+++ shared/common-adapters/radio-button.stories.js
@@ -6,17 +6,17 @@ import {storiesOf, action} from '../stories/storybook'
 
 const commonProps = {
   disabled: false,
-  onSelect: newCheckedValue => action(`Got check: ${newCheckedValue ? 'true' : 'false'}`),
+  onSelect: newSelectedValue => action(`Got selected: ${newSelectedValue ? 'true' : 'false'}`),
   style: {margin: 10},
 }
 
 const load = () => {
   storiesOf('Common', module).add('RadioButton', () => (
     <Box style={{flex: 1}}>
-      <RadioButton {...commonProps} label="Checkbox Unchecked Enabled" selected={false} />
-      <RadioButton {...commonProps} label="Checkbox Checked Enabled" selected={true} />
-      <RadioButton {...commonProps} label="Checkbox Unchecked Disabled" selected={false} disabled={true} />
-      <RadioButton {...commonProps} label="Checkbox Checked Disabled" selected={true} disabled={true} />
+      <RadioButton {...commonProps} label="RadioButton Unselected Enabled" selected={false} />
+      <RadioButton {...commonProps} label="RadioButton Selected Enabled" selected={true} />
+      <RadioButton {...commonProps} label="RadioButton Unselected Disabled" selected={false} disabled={true} />
+      <RadioButton {...commonProps} label="RadioButton Selected Disabled" selected={true} disabled={true} />
     </Box>
   ))
 }
