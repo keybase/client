f54fe0dc36e0aa342ff8ac741e01bb85fcb13d10
Chris Ball <chris@printf.net>
2017-08-23 09:38:56 -0700

    prettier

--- shared/common-adapters/radio-button.native.js
+++ shared/common-adapters/radio-button.native.js
@@ -10,44 +10,48 @@ import type {Props} from './radio-button'
 export const RADIOBUTTON_SIZE = 22
 export const RADIOBUTTON_MARGIN = 8
 
-const RadioOuterBox = glamorous(ClickableBox)({
-  backgroundColor: globalColors.white,
-  borderRadius: 100,
-  borderWidth: 1,
-  height: RADIOBUTTON_SIZE,
-  marginRight: RADIOBUTTON_MARGIN,
-  position: 'relative',
-  width: RADIOBUTTON_SIZE,
-}, ({disabled, selected}) => ({
-  borderColor: selected ? globalColors.blue : globalColors.black_10,
-  opacity: disabled ? 0.4 : 1,
-}))
+const RadioOuterCircle = glamorous(ClickableBox)(
+  {
+    backgroundColor: globalColors.white,
+    borderRadius: 100,
+    borderWidth: 1,
+    height: RADIOBUTTON_SIZE,
+    marginRight: RADIOBUTTON_MARGIN,
+    position: 'relative',
+    width: RADIOBUTTON_SIZE,
+  },
+  ({disabled, selected}) => ({
+    borderColor: selected ? globalColors.blue : globalColors.black_10,
+    opacity: disabled ? 0.4 : 1,
+  })
+)
 
-const RadioInnerBox = glamorous(ClickableBox)({
-  borderColor: globalColors.white,
-  borderRadius: 10,
-  borderWidth: 5,
-  left: 5,
-  position: 'absolute',
-  top: 5,
-}, ({selected}) => ({
-  borderColor: selected ? globalColors.blue : globalColors.white
-}))
+const RadioInnerCircle = glamorous(ClickableBox)(
+  {
+    borderColor: globalColors.white,
+    borderRadius: 10,
+    borderWidth: 5,
+    left: 5,
+    position: 'absolute',
+    top: 5,
+  },
+  ({selected}) => ({
+    borderColor: selected ? globalColors.blue : globalColors.white,
+  })
+)
 
-const RadioButton = ({disabled, label, onSelect, selected, style}: Props) => {
-  return (
+const RadioButton = ({disabled, label, onSelect, selected, style}: Props) => (
   <ClickableBox
     style={{...styleContainer, ...style}}
     onClick={disabled ? undefined : () => onSelect(!selected)}
   >
-    <RadioOuterBox disabled={disabled} selected={selected}>
-      <RadioInnerBox selected={selected} />
-    </RadioOuterBox>
+    <RadioOuterCircle disabled={disabled} selected={selected}>
+      <RadioInnerCircle selected={selected} />
+    </RadioOuterCircle>
 
     <Text type="Body" style={{color: globalColors.black_75}}>{label}</Text>
   </ClickableBox>
 )
-}
 
 const styleContainer = {
   ...globalStyles.flexBoxRow,
