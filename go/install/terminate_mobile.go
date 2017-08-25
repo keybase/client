b3b1c728f863a58a51b44d805d9ca274778be5c1
Marco Munizaga <marco@marcopolo.io>
2017-08-25 14:28:00 -0700

    Update comment

--- go/install/terminate_mobile.go
+++ go/install/terminate_mobile.go
@@ -2,7 +2,7 @@
 
 package install
 
-// TerminateApp will stop the Keybase (UI) app
+// TerminateApp is Used on desktop to kill the app, doesn't apply on mobile
 func TerminateApp(context Context, log Log) error {
 	return nil
 }
