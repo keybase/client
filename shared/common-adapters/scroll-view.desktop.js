e1e05cc42a54a323b92486cfc6b388db8cece6db
Frederick Akalin <akalin@keyba.se>
2017-08-24 17:54:02 -0700

    Better type

--- shared/common-adapters/scroll-view.desktop.js
+++ shared/common-adapters/scroll-view.desktop.js
@@ -1,6 +1,7 @@
 // @flow
 import * as React from 'react'
+import type {Props} from './scroll-view'
 
-const ScrollView = (props: any) => <div {...props} style={{overflow: 'auto', ...props.style}} />
+const ScrollView = (props: Props) => <div {...props} style={{overflow: 'auto', ...props.style}} />
 
 export default ScrollView
