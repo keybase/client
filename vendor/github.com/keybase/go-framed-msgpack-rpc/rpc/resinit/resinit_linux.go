923358a896d510f3ee737540f27c9811e4566089
Jack O'Connor <oconnor663@gmail.com>
2017-08-29 17:07:02 -0400

    revendor go-framed-msgpack-rpc

--- vendor/github.com/keybase/go-framed-msgpack-rpc/rpc/resinit/resinit_linux.go
+++ vendor/github.com/keybase/go-framed-msgpack-rpc/rpc/resinit/resinit_linux.go
@@ -1,7 +1,7 @@
 // Copyright 2015 Keybase, Inc. All rights reserved. Use of
 // this source code is governed by the included BSD license.
 
-// +build linux,!android
+// +build linux,!android,!noresinit
 
 package resinit
 
