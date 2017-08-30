0e9ffcfd99ed3fcb1a6e69b027eb1d3b9eb34e8d
Mike Maxim <mike@keyba.se>
2017-08-30 15:13:52 -0400

    bigger unread dots, and make them show up on mobile (#8252)
    * bigger unread dots, and make them show up on mobile
    
    * just delete this
    
    * right size

--- shared/chat/inbox/row/big-team-rows.js
+++ shared/chat/inbox/row/big-team-rows.js
@@ -157,8 +157,7 @@ const channelBackgroundStyle = {
   ...globalStyles.flexBoxRow,
   alignItems: 'center',
   borderRadius: 2,
-  marginLeft: 32,
-  paddingLeft: globalMargins.tiny,
+  paddingLeft: 32,
   paddingRight: globalMargins.tiny,
   width: '100%',
 }
@@ -178,7 +177,7 @@ const unreadStyle = {
   backgroundColor: globalColors.orange,
   borderRadius: 6,
   flexShrink: 0,
-  height: 6,
-  width: 6,
+  height: 8,
+  width: 8,
 }
 export {BigTeamHeaderRow, BigTeamChannelRow, BigTeamChannelFilteredRow}
