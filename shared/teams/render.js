c2acee1917c3184ed448e39d6717a858667cdac5
Frederick Akalin <akalin@gmail.com>
2017-08-29 18:43:54 -0700

    Add text

--- shared/teams/render.js
+++ shared/teams/render.js
@@ -1,13 +1,20 @@
 // @flow
 import * as React from 'react'
-import {Box} from '../common-adapters'
+import {Box, Text} from '../common-adapters'
+import {globalStyles} from '../styles'
 
 import type {Props} from './render'
 
 function TeamsRender(props: Props) {
   return (
-    <Box>
-      Hello teams tab!
+    <Box style={{...globalStyles.flexBoxColumn, alignItems: 'center'}}>
+      <Text type="BodySmall">Teams are still very early-stage!</Text>
+      <Text type="BodySmall">
+        For now the GUI only allows you to create simple teams
+        no channels or subteams, but you can get to more complex teams
+        using the command line.
+      </Text>
+      <Text type="BodySmall">Read the doc</Text>
     </Box>
   )
 }
