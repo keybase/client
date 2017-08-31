c9852c26f23e33c256265d6e6eed05efaf02fdae
Patrick Crosby <pcrosby@gmail.com>
2017-08-31 11:26:48 -0500

    Don't use libkb.G in bind or keybase/main

--- go/bind/keybase.go
+++ go/bind/keybase.go
@@ -86,7 +86,7 @@ func Init(homeDir string, logFile string, runModeStr string, accessGroupOverride
 		fmt.Printf("Go: DNS Server: %s\n", srv)
 	}
 
-	kbCtx = libkb.G
+	kbCtx = libkb.NewGlobalContext()
 	kbCtx.Init()
 	kbCtx.SetServices(externals.GetServices())
 	usage := libkb.Usage{
