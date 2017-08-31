a65b683072a0b7f257320dad9d717e4eb0bdee53
Frederick Akalin <akalin@gmail.com>
2017-08-31 11:51:01 -0700

    Factor out into BetaNote

--- shared/teams/render.js
+++ shared/teams/render.js
@@ -34,6 +34,36 @@ const Header = (props: HeaderProps) => (
   </Box>
 )
 
+type BetaNoteProps = {
+  onReadDoc: () => void,
+}
+
+const BetaNote = (props: BetaNoteProps) => {
+  return (
+    <Box style={{...globalStyles.flexBoxColumn, alignItems: 'center'}}>
+      <Box style={{...globalStyles.flexBoxRow}}>
+        <Text type="BodySmall">&mdash;&mdash;</Text>
+        <Icon style={{paddingLeft: 5, paddingRight: 5}} type="iconfont-info" />
+        <Text type="BodySmall">&mdash;&mdash;</Text>
+      </Box>
+      <Text type="BodySmall">Teams are still very early-stage!</Text>
+      <Text style={{textAlign: 'center', width: 426}} type="BodySmall">
+        For now the GUI only allows you to create simple teams
+        no channels or subteams, but you can get to more complex teams
+        using the command line.
+      </Text>
+      <Text
+        type="BodySmall"
+        className="underline"
+        onClick={props.onReadDoc}
+        style={{...globalStyles.clickable}}
+      >
+        Read the doc
+      </Text>
+    </Box>
+  )
+}
+
 const TeamsRender = (props: Props) => {
   return (
     <Box style={{...globalStyles.flexBoxColumn, alignItems: 'center', height: '100%'}}>
@@ -42,25 +72,7 @@ const TeamsRender = (props: Props) => {
         style={{alignSelf: 'stretch'}}
         contentContainerStyle={{...globalStyles.flexBoxColumn, alignItems: 'center', margin: 10}}
       >
-        <Box style={{...globalStyles.flexBoxRow}}>
-          <Text type="BodySmall">&mdash;&mdash;</Text>
-          <Icon style={{paddingLeft: 5, paddingRight: 5}} type="iconfont-info" />
-          <Text type="BodySmall">&mdash;&mdash;</Text>
-        </Box>
-        <Text type="BodySmall">Teams are still very early-stage!</Text>
-        <Text style={{textAlign: 'center', width: 426}} type="BodySmall">
-          For now the GUI only allows you to create simple teams
-          no channels or subteams, but you can get to more complex teams
-          using the command line.
-        </Text>
-        <Text
-          type="BodySmall"
-          className="underline"
-          onClick={props.onReadDoc}
-          style={{...globalStyles.clickable}}
-        >
-          Read the doc
-        </Text>
+        <BetaNote {...props} />
       </ScrollView>
     </Box>
   )
