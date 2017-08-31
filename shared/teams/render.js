aa191d5534f4351fd3ad2afc13ab89affb7bf0f8
Frederick Akalin <akalin@gmail.com>
2017-08-31 12:04:32 -0700

    Adjust

--- shared/teams/render.js
+++ shared/teams/render.js
@@ -43,51 +43,48 @@ type BetaNoteProps = {
   onReadDoc: () => void,
 }
 
-const BetaNote = (props: BetaNoteProps) => {
-  return (
-    <Box style={{...globalStyles.flexBoxColumn, alignItems: 'center'}}>
-      <Box style={{...globalStyles.flexBoxRow}}>
-        <Text type="BodySmall">&mdash;&mdash;</Text>
-        <Icon
-          style={{paddingLeft: globalMargins.tiny, paddingRight: globalMargins.tiny}}
-          type="iconfont-info"
-        />
-        <Text type="BodySmall">&mdash;&mdash;</Text>
-      </Box>
-      <Text type="BodySmall">Teams are still very early-stage!</Text>
-      <Text style={{maxWidth: 426, textAlign: 'center'}} type="BodySmall">
-        For now the GUI only allows you to create simple teams
-        no channels or subteams, but you can get to more complex teams
-        using the command line.
-      </Text>
-      <Text
-        type="BodySmall"
-        className="underline"
-        onClick={props.onReadDoc}
-        style={{...globalStyles.clickable}}
-      >
-        Read the doc
-      </Text>
+const BetaNote = (props: BetaNoteProps) => (
+  <Box style={{...globalStyles.flexBoxColumn, alignItems: 'center'}}>
+    <Box style={{...globalStyles.flexBoxRow}}>
+      <Text type="BodySmall">&mdash;&mdash;</Text>
+      <Icon
+        style={{paddingLeft: globalMargins.tiny, paddingRight: globalMargins.tiny}}
+        type="iconfont-info"
+      />
+      <Text type="BodySmall">&mdash;&mdash;</Text>
     </Box>
-  )
-}
+    <Text type="BodySmall">Teams are still very early-stage!</Text>
+    <Text style={{maxWidth: 426, textAlign: 'center'}} type="BodySmall">
+      For now the GUI only allows you to create simple teams
+      no channels or subteams, but you can get to more complex teams
+      using the command line.
+    </Text>
+    <Text
+      type="BodySmall"
+      className="underline"
+      onClick={props.onReadDoc}
+      style={{...globalStyles.clickable}}
+    >
+      Read the doc
+    </Text>
+  </Box>
+)
 
-const Render = (props: Props) => {
-  return (
-    <Box style={{...globalStyles.flexBoxColumn, alignItems: 'center', height: '100%'}}>
-      <Header {...props} />
-      <ScrollView
-        style={{alignSelf: 'stretch'}}
-        contentContainerStyle={{
-          ...globalStyles.flexBoxColumn,
-          alignItems: 'center',
-          margin: globalMargins.tiny,
-        }}
-      >
-        <BetaNote {...props} />
-      </ScrollView>
-    </Box>
-  )
-}
+// TODO: Add banner and team rows.
+const Render = (props: Props) => (
+  <Box style={{...globalStyles.flexBoxColumn, alignItems: 'center', height: '100%'}}>
+    <Header {...props} />
+    <ScrollView
+      style={{alignSelf: 'stretch'}}
+      contentContainerStyle={{
+        ...globalStyles.flexBoxColumn,
+        alignItems: 'center',
+        margin: globalMargins.tiny,
+      }}
+    >
+      <BetaNote {...props} />
+    </ScrollView>
+  </Box>
+)
 
 export default Render
