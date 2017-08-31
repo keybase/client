8716b1d699e6e48fec614e234dd74fd32f4b7a8f
Frederick Akalin <akalin@gmail.com>
2017-08-31 11:57:51 -0700

    Pull out fns

--- shared/teams/index.js
+++ shared/teams/index.js
@@ -5,21 +5,22 @@ import openURL from '../util/open-url'
 
 import type {RouteProps} from '../route-tree/render-route'
 
+const mapStateToProps = (state, {routeSelected, routeLeafTags}: RouteProps<{}, {}>) => ({})
+
+const mapDispatchToProps = (dispatch, {routePath}: RouteProps<{}, {}>) => ({
+  onCreateTeam: () => {
+    // TODO: Hook this up. Need to change onShowNewTeamDialog to
+    // make its conversationIDKey parameter optional first.
+    console.log('onCreateTeam not implemented yet')
+  },
+  onJoinTeam: () => {
+    // TODO: Hook this up once we have a join team dialog.
+    console.log('onJoinTeam not implemented yet')
+  },
+  onReadDoc: () => {
+    openURL('https://keybase.io/docs/command_line/teams_alpha')
+  },
+})
+
 // $FlowIssue type this connector
-export default pausableConnect(
-  (state, {routeSelected, routeLeafTags}: RouteProps<{}, {}>) => ({}),
-  (dispatch, {routePath}: RouteProps<{}, {}>) => ({
-    onCreateTeam: () => {
-      // TODO: Hook this up. Need to change onShowNewTeamDialog to
-      // make its conversationIDKey parameter optional first.
-      console.log('onCreateTeam not implemented yet')
-    },
-    onJoinTeam: () => {
-      // TODO: Hook this up once we have a join team dialog.
-      console.log('onJoinTeam not implemented yet')
-    },
-    onReadDoc: () => {
-      openURL('https://keybase.io/docs/command_line/teams_alpha')
-    },
-  })
-)(TeamsRender)
+export default pausableConnect(mapStateToProps, mapDispatchToProps)(TeamsRender)
