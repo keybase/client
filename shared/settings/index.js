b58d276ddc906655724b099cd2463cdbd88666c7
Fred Akalin <akalin@keyba.se>
2017-09-05 14:13:43 -0700

    Type SettingsContainer's container (#8284)

--- shared/settings/index.js
+++ shared/settings/index.js
@@ -4,17 +4,34 @@ import pausableConnect from '../util/pausable-connect'
 import {switchTo} from '../actions/route-tree'
 import {logout} from '../actions/login/creators'
 
+import type {Tab} from '../constants/settings'
+import type {TypedState} from '../constants/reducer'
 import type {RouteProps} from '../route-tree/render-route'
 
-// $FlowIssue type this connector
-export default pausableConnect(
-  (state, {routeSelected, routeLeafTags}: RouteProps<{}, {}>) => ({
-    badgeNumbers: {}, // TODO add badging logic
-    selectedTab: routeSelected,
-    isModal: routeLeafTags.modal,
-  }),
-  (dispatch, {routePath}: RouteProps<{}, {}>) => ({
-    onTabChange: tab => dispatch(switchTo(routePath.push(tab))),
-    onLogout: () => dispatch(logout()),
-  })
-)(SettingsContainer)
+type StateProps = {
+  badgeNumbers: {[key: Tab]: number},
+  isModal: boolean,
+  selectedTab: Tab,
+}
+
+const mapStateToProps = (
+  state: TypedState,
+  {routeLeafTags, routeSelected}: RouteProps<{}, {}>
+): StateProps => ({
+  badgeNumbers: {}, // TODO add badging logic
+  isModal: routeLeafTags.modal,
+  // TODO: Is there a way to validate that routeSelected is a Tab?
+  selectedTab: (routeSelected: any),
+})
+
+type DispatchProps = {
+  onLogout: () => void,
+  onTabChange: (tab: Tab) => void,
+}
+
+const mapDispatchToProps = (dispatch: Dispatch, {routePath}: RouteProps<{}, {}>): DispatchProps => ({
+  onLogout: () => dispatch(logout()),
+  onTabChange: tab => dispatch(switchTo(routePath.push(tab))),
+})
+
+export default pausableConnect(mapStateToProps, mapDispatchToProps)(SettingsContainer)
