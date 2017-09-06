10516982b26a5a3565b0601cf4bc340eb4a3d096
Frederick Akalin <akalin@keyba.se>
2017-09-05 17:48:57 -0700

    Cleanup

--- shared/chat/new-team-dialog-container.js
+++ shared/chat/new-team-dialog-container.js
@@ -5,25 +5,18 @@ import {compose, withState, withHandlers} from 'recompose'
 import {createNewTeamFromConversation} from '../actions/teams/creators'
 import {selectConversation} from '../actions/chat/creators'
 
-import type {TypedState} from '../constants/reducer'
-
-const mapStateToProps = (state: TypedState, {routeProps}) => ({
-  _conversationIDKey: routeProps.conversationIDKey,
-})
-
-const mapDispatchToProps = (dispatch: Dispatch, {navigateUp}) => ({
-  _onCreateNewTeam: (conversationIDKey, name) => {
-    dispatch(createNewTeamFromConversation(conversationIDKey, name))
+const mapDispatchToProps = (dispatch: Dispatch, {navigateUp, routeProps}) => ({
+  _onCreateNewTeam: name => {
+    dispatch(createNewTeamFromConversation(routeProps.conversationIDKey, name))
     dispatch(selectConversation(null, true))
   },
   onBack: () => dispatch(navigateUp()),
 })
 
 export default compose(
-  connect(mapStateToProps, mapDispatchToProps),
+  connect(null, mapDispatchToProps),
   withState('name', 'onNameChange', ''),
   withHandlers({
-    onSubmit: ({_conversationIDKey, name, _onCreateNewTeam}) => () =>
-      _onCreateNewTeam(_conversationIDKey, name),
+    onSubmit: ({name, _onCreateNewTeam}) => () => _onCreateNewTeam(name),
   })
 )(NewTeamDialog)
