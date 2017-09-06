732423879414ee75a1c6630dcbc185db33a86277
John Zila <john@keyba.se>
2017-09-06 12:02:30 -0700

    folder_branch_ops: Fix panic due to nil disk cache.

--- libkbfs/folder_branch_ops.go
+++ libkbfs/folder_branch_ops.go
@@ -1964,8 +1964,11 @@ func (fbo *folderBranchOps) GetNodeMetadata(ctx context.Context, node Node) (
 	_, prefetchStatus, _, err :=
 		fbo.config.BlockCache().GetWithPrefetch(res.BlockInfo.BlockPointer)
 	if err != nil {
-		_, _, prefetchStatus, _ = fbo.config.DiskBlockCache().Get(ctx,
-			fbo.id(), res.BlockInfo.ID)
+		dbc := fbo.config.DiskBlockCache()
+		if dbc != nil {
+			_, _, prefetchStatus, _ = fbo.config.DiskBlockCache().Get(ctx,
+				fbo.id(), res.BlockInfo.ID)
+		}
 		// Swallow cache errors since we don't want to communicate
 		// errors for uncached blocks.
 	}
