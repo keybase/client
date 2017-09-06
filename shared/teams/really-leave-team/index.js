a355de2e5dfd2f9d6c6c05d121d2dce79fedea4e
chrisnojima <cnojima@keyba.se>
2017-09-06 19:02:53 -0400

    update font fix styling on dialog

--- shared/teams/really-leave-team/index.js
+++ shared/teams/really-leave-team/index.js
@@ -8,8 +8,8 @@ const ReallyLeaveTeam = (props: any) => (
   <PopupDialog onClose={props.onClose} styleCover={_styleCover} styleContainer={_styleContainer}>
     <Box style={{...globalStyles.flexBoxColumn, alignItems: 'center', flex: 1, padding: globalMargins.large}}>
       <Avatar teamname={props.name} size={64} />
-      <Icon type="iconfont-keybase" />
-      <Text type="BodyBig" style={{marginBottom: globalMargins.large, marginTop: globalMargins.large}}>
+      <Icon type="icon-team-leave-28" style={{marginTop: -20, marginRight: -60, zIndex: 1}} />
+      <Text type="Header" style={{marginBottom: globalMargins.large, marginTop: globalMargins.large}}>
         Are you sure you want to leave {props.name}?
       </Text>
       <Text type="Body" style={{maxWidth: 430, textAlign: 'center'}}>
@@ -24,7 +24,7 @@ const ReallyLeaveTeam = (props: any) => (
           type="Secondary"
           onClick={props.onClose}
           label="Cancel"
-          style={{marginRight: globalMargins.small}}
+          style={{marginRight: globalMargins.tiny}}
         />
         <Button type="Danger" onClick={props.onLeave} label={`Yes, leave ${props.name}`} />
       </Box>
