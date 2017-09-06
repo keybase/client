c3027fed308edd90d8515e8602f23d0872ecf080
Frederick Akalin <akalin@keyba.se>
2017-09-05 17:35:45 -0700

    Split

---
+++ shared/chat/new-team-dialog-container.js
@@ -0,0 +1,23 @@
+// @flow
+import NewTeamDialog from '../teams/new-team'
+import {connect} from 'react-redux'
+import {compose, withState, withHandlers} from 'recompose'
+
+import type {TypedState} from '../constants/reducer'
+
+const mapStateToProps = (state: TypedState, {routeProps}) => ({
+  _onCreateNewTeam: routeProps.onCreateNewTeam,
+})
+
+const mapDispatchToProps = (dispatch: Dispatch, {navigateUp}) => ({
+  _navigateUp: navigateUp,
+  onBack: () => dispatch(navigateUp()),
+})
+
+export default compose(
+  connect(mapStateToProps, mapDispatchToProps),
+  withState('name', 'onNameChange', ''),
+  withHandlers({
+    onSubmit: ({name, _onCreateNewTeam, _navigateUp}) => () => _onCreateNewTeam(name, _navigateUp),
+  })
+)(NewTeamDialog)
