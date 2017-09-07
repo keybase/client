8b3fbbd79467a1ce26f7ee4960aa576a63369ae8
John Zila <john@keyba.se>
2017-09-07 14:46:18 -0700

    disk_block_cache: Don't log the results of every DiskBlockCache.Get.

--- libkbfs/disk_block_cache.go
+++ libkbfs/disk_block_cache.go
@@ -490,9 +490,6 @@ func (cache *DiskBlockCacheStandard) Get(ctx context.Context, tlfID tlf.ID,
 	}
 
 	defer func() {
-		cache.log.CDebugf(ctx, "Cache Get id=%s tlf=%s bSize=%d, "+
-			"prefetchStatus=%s err=%+v", blockID, tlfID, len(buf),
-			prefetchStatus.String(), err)
 		if err == nil {
 			cache.hitMeter.Mark(1)
 		} else {
