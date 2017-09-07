b3db64e3558ba07832eba5f3d63ff1080b8eefc2
chrisnojima <cnojima@keyba.se>
2017-09-07 15:09:40 -0400

    WIP

--- shared/teams/main/header.js
+++ shared/teams/main/header.js
@@ -37,8 +37,8 @@ export type Props = {
 const Header = (props: Props) => (
   <Box style={{...globalStyles.flexBoxRow, alignItems: 'center', height: 48}}>
     <HeaderButton iconType="iconfont-new" label="Create a team" onClick={props.onCreateTeam} />
-    {false && <HeaderButton iconType="iconfont-team-join" label="Join a team" onClick={props.onJoinTeam // off until join works
-        } />}
+    {/* off until this works */}
+    {false && <HeaderButton iconType="iconfont-team-join" label="Join a team" onClick={props.onJoinTeam} />}
   </Box>
 )
 
