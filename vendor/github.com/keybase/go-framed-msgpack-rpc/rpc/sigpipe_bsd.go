365a90d1b233ebe47920047efbedf753ac6abd4d
Mike Maxim <mike@keyba.se>
2017-09-07 17:18:46 -0400

    didnt mean to push to master

--- vendor/github.com/keybase/go-framed-msgpack-rpc/rpc/sigpipe_bsd.go
+++ vendor/github.com/keybase/go-framed-msgpack-rpc/rpc/sigpipe_bsd.go
@@ -12,13 +12,6 @@ func DisableSigPipe(c net.Conn) error {
 	// Disable SIGPIPE on this connection since we currently need to do this manually for iOS
 	// to prevent the signal from crashing iOS apps.
 	// See: https://github.com/golang/go/issues/17393
-	netFD := reflect.ValueOf(c).Elem().FieldByName("fd").Elem()
-	sysfd := netFD.FieldByName("sysfd")
-	var fd int
-	if sysfd.IsValid() {
-		fd = int(sysfd.Int())
-	} else { // After go +3792db5
-		fd = int(netFD.FieldByName("pfd").FieldByName("Sysfd").Int())
-	}
+	fd := int(reflect.ValueOf(c).Elem().FieldByName("fd").Elem().FieldByName("sysfd").Int())
 	return syscall.SetsockoptInt(fd, syscall.SOL_SOCKET, syscall.SO_NOSIGPIPE, 1)
 }
