92854be3781a664a7aff6c0b661ee80ef67e4369
Frederick Akalin <akalin@keyba.se>
2017-09-12 15:59:30 -0700

    Always navigate up on createTeam from teams tab

--- shared/teams/new-team/container.js
+++ shared/teams/new-team/container.js
@@ -3,19 +3,14 @@ import NewTeamDialog from './'
 import {connect} from 'react-redux'
 import {compose, withState, withHandlers} from 'recompose'
 import {createNewTeam} from '../../actions/teams/creators'
-import {navigateTo} from '../../actions/route-tree'
-import {isMobile} from '../../constants/platform'
-import {chatTab} from '../../constants/tabs'
 
 const mapDispatchToProps = (dispatch: Dispatch, {navigateUp}) => ({
   _onCreateNewTeam: name => {
     dispatch(createNewTeam(name))
 
-    if (isMobile) {
-      dispatch(navigateTo([], [chatTab]))
-    } else {
-      dispatch(navigateUp())
-    }
+    // TODO: do dispatch(navigateTo([], [chatTab])) for mobile (for
+    // some reason, it doesn't work).
+    dispatch(navigateUp())
   },
   onBack: () => dispatch(navigateUp()),
 })
