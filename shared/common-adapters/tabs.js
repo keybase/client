9b8b91350b182425e9b565341a5eaf46f3a50934
cecileboucheron <cecile@keyba.se>
2017-09-18 18:01:43 -0700

    Misc visual tweaks

--- shared/common-adapters/tabs.js
+++ shared/common-adapters/tabs.js
@@ -3,29 +3,41 @@ import * as React from 'react'
 import Box from './box'
 import ClickableBox from './clickable-box'
 import Divider from './divider'
-import {globalColors, globalStyles} from '../styles'
+import {globalColors, globalMargins, globalStyles} from '../styles'
+import {isMobile} from '../constants/platform'
 
 type Props = {
   tabs: Array<React.Node>,
   selected: React.Node,
   onSelect: (s: React.Node) => void,
-  style: any,
+  style?: any,
 }
 
 const Tabs = ({tabs, selected, onSelect, style}: Props) => {
   return (
-    <Box style={style}>
+    <Box style={{...style, ...containerStyle}}>
       {tabs.map((t, idx) => {
         // $FlowIssue
         const key: string = (t && t.key) || idx
         return (
           <ClickableBox onClick={() => onSelect(t)} key={key}>
-            <Box style={globalStyles.flexBoxColumn}>
-              {t}
+            <Box style={{...globalStyles.flexBoxColumn, height: isMobile ? 48 : 40}}>
+              <Box
+                style={{
+                  flex: 1,
+                  paddingBottom: globalMargins.xtiny,
+                  paddingLeft: globalMargins.small,
+                  paddingTop: globalMargins.small,
+                  paddingRight: globalMargins.small,
+                }}
+              >
+                {t}
+              </Box>
               <Divider
                 style={{
+                  ...globalStyles.flexBoxRow,
                   backgroundColor: t === selected ? globalColors.blue : globalColors.transparent,
-                  height: 2,
+                  minHeight: 2,
                 }}
               />
             </Box>
@@ -34,6 +46,17 @@ const Tabs = ({tabs, selected, onSelect, style}: Props) => {
       })}
     </Box>
   )
+}
+
+const containerStyle = {
+  ...globalStyles.flexBoxRow,
+  alignItems: 'flex-start',
+  borderBottomColor: globalColors.black_05,
+  borderBottomWidth: 1,
+  borderStyle: 'solid',
+  flex: 1,
+  maxHeight: isMobile ? 48 : 40,
+  width: '100%',
 }
 
 export default Tabs
