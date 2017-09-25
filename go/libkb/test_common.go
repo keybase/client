1955aa20f28425d336f360e782264e448ee6cd74
Mike Maxim <mike@keyba.se>
2017-09-25 10:23:37 -0400

    setup tests with basic uid mapper

--- go/libkb/test_common.go
+++ go/libkb/test_common.go
@@ -266,6 +266,7 @@ func setupTestContext(tb testing.TB, name string, tcPrev *TestContext) (tc TestC
 	}
 
 	g.GregorDismisser = &FakeGregorDismisser{}
+	g.SetUIDMapper(NewTestUIDMapper(g.GetUPAKLoader()))
 
 	tc.PrevGlobal = G
 	G = g
@@ -453,4 +454,29 @@ func (f *FakeGregorDismisser) DismissItem(id gregor.MsgID) error {
 // Bypasses locks.
 func (g *GlobalContext) ResetLoginState() {
 	g.createLoginStateLocked()
+}
+
+type TestUIDMapper struct {
+	ul UPAKLoader
+}
+
+func NewTestUIDMapper(ul UPAKLoader) TestUIDMapper {
+	return TestUIDMapper{
+		ul: ul,
+	}
+}
+
+func (t TestUIDMapper) CheckUIDAgainstUsername(uid keybase1.UID, un NormalizedUsername) bool {
+	return true
+}
+
+func (t TestUIDMapper) MapUIDsToUsernames(ctx context.Context, g UIDMapperContext, uids []keybase1.UID) (res []NormalizedUsername, err error) {
+	for _, uid := range uids {
+		name, err := t.ul.LookupUsername(ctx, uid)
+		if err != nil {
+			return nil, err
+		}
+		res = append(res, name)
+	}
+	return res, nil
 }
