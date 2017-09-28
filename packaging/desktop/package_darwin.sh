52453ae05db421a611806a733d90f8a0ca69ee78
Chris Ball <chris@printf.net>
2017-09-27 21:50:51 -0700

    can't test build_app changes on a branch

--- packaging/desktop/package_darwin.sh
+++ packaging/desktop/package_darwin.sh
@@ -36,7 +36,9 @@ kbfs_version=${KBFS_VERSION:-}
 comment=""
 
 keybase_binpath=${KEYBASE_BINPATH:-}
-git_remote_keybase_binpath=${GIT_REMOTE_KEYBASE_BINPATH:-}
+# FIXME: Replace before merging.
+# git_remote_keybase_binpath=${GIT_REMOTE_KEYBASE_BINPATH:-}
+git_remote_keybase_binpath='/tmp/build_kbfs/git-remote-keybase'
 kbfs_binpath=${KBFS_BINPATH:-}
 kbnm_binpath=${KBNM_BINPATH:-}
 updater_binpath=${UPDATER_BINPATH:-}
