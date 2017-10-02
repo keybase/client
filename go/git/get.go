e68dd86485cd530c655486491e01178ae5779f13
Jack O'Connor <oconnor663@gmail.com>
2017-10-02 10:37:45 -0400

    use IsAdminOrAbove in another place

--- go/git/get.go
+++ go/git/get.go
@@ -221,17 +221,13 @@ func getMetadataInner(ctx context.Context, g *libkb.GlobalContext, folder *keyba
 		if err != nil {
 			return nil, err
 		}
-		canDelete := false
-		if role == keybase1.TeamRole_ADMIN || role == keybase1.TeamRole_OWNER {
-			canDelete = true
-		}
 
 		resultList = append(resultList, keybase1.GitRepoResult{
 			Folder:         repoFolder,
 			RepoID:         responseRepo.RepoID,
 			RepoUrl:        formatRepoURL(repoFolder, string(localMetadata.RepoName)),
 			GlobalUniqueID: formatUniqueRepoID(responseRepo.TeamID, responseRepo.RepoID),
-			CanDelete:      canDelete,
+			CanDelete:      role.IsAdminOrAbove(),
 			LocalMetadata:  localMetadata,
 			ServerMetadata: keybase1.GitServerMetadata{
 				Ctime: keybase1.ToTime(responseRepo.CTime),
