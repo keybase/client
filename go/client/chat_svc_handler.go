562f32f1ff3d550675980e519c6027e3363f2eba
Mike Maxim <mike@keyba.se>
2017-10-03 22:40:15 -0400

    hide internal name in json api

--- go/client/chat_svc_handler.go
+++ go/client/chat_svc_handler.go
@@ -39,6 +39,15 @@ func newChatServiceHandler(g *libkb.GlobalContext) *chatServiceHandler {
 	}
 }
 
+func (c *chatServiceHandler) channelName(conv chat1.ConversationLocal) string {
+	switch conv.Info.MembersType {
+	case chat1.ConversationMembersType_IMPTEAM:
+		return strings.Join(conv.Info.WriterNames, ",")
+	default:
+		return conv.Info.TlfName
+	}
+}
+
 // ListV1 implements ChatServiceHandler.ListV1.
 func (c *chatServiceHandler) ListV1(ctx context.Context, opts listOptionsV1) Reply {
 	var rlimits []chat1.RateLimit
@@ -101,7 +110,7 @@ func (c *chatServiceHandler) ListV1(ctx context.Context, opts listOptionsV1) Rep
 				super.ConversationID.String())
 		}
 		convSummary.Channel = ChatChannel{
-			Name:        conv.Info.TlfName,
+			Name:        c.channelName(conv),
 			Public:      conv.Info.Visibility == keybase1.TLFVisibility_PUBLIC,
 			TopicType:   strings.ToLower(conv.Info.Triple.TopicType.String()),
 			MembersType: strings.ToLower(conv.Info.MembersType.String()),
@@ -203,7 +212,7 @@ func (c *chatServiceHandler) ReadV1(ctx context.Context, opts readOptionsV1) Rep
 		msg := MsgSummary{
 			ID: mv.ServerHeader.MessageID,
 			Channel: ChatChannel{
-				Name:        mv.ClientHeader.TlfName,
+				Name:        c.channelName(conv),
 				Public:      mv.ClientHeader.TlfPublic,
 				TopicType:   strings.ToLower(mv.ClientHeader.Conv.TopicType.String()),
 				MembersType: strings.ToLower(conv.GetMembersType().String()),
