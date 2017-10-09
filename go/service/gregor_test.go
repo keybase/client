214a86978c07a49fd49ca0735efe8612d285ad2d
Mike Maxim <mike.maxim@gmail.com>
2017-10-09 18:23:14 -0400

    method

--- go/service/gregor_test.go
+++ go/service/gregor_test.go
@@ -111,11 +111,12 @@ func (n *nlistener) ChatTLFFinalize(uid keybase1.UID, convID chat1.ConversationI
 }
 func (n *nlistener) ChatTLFResolve(uid keybase1.UID, convID chat1.ConversationID, info chat1.ConversationResolveInfo) {
 }
-func (n *nlistener) ChatJoinedConversation(uid keybase1.UID, conv chat1.InboxUIItem)    {}
-func (n *nlistener) ChatLeftConversation(uid keybase1.UID, convID chat1.ConversationID) {}
-func (n *nlistener) ChatInboxStale(uid keybase1.UID)                                    {}
-func (n *nlistener) ChatInboxSynced(uid keybase1.UID, syncRes chat1.ChatSyncResult)     {}
-func (n *nlistener) ChatInboxSyncStarted(uid keybase1.UID)                              {}
+func (n *nlistener) ChatJoinedConversation(uid keybase1.UID, conv chat1.InboxUIItem)     {}
+func (n *nlistener) ChatLeftConversation(uid keybase1.UID, convID chat1.ConversationID)  {}
+func (n *nlistener) ChatResetConversation(uid keybase1.UID, convID chat1.ConversationID) {}
+func (n *nlistener) ChatInboxStale(uid keybase1.UID)                                     {}
+func (n *nlistener) ChatInboxSynced(uid keybase1.UID, syncRes chat1.ChatSyncResult)      {}
+func (n *nlistener) ChatInboxSyncStarted(uid keybase1.UID)                               {}
 func (n *nlistener) TeamChanged(teamID keybase1.TeamID, teamName string, latestSeqno keybase1.Seqno, changes keybase1.TeamChangeSet) {
 }
 func (n *nlistener) TeamDeleted(teamID keybase1.TeamID) {}
