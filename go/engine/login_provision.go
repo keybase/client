e54bfd336f9e4b94b7c5d82b3ebda21d26502427
Patrick Crosby <pcrosby@gmail.com>
2017-10-10 11:28:06 -0500

    Make not found err msg friendlier when KID not found for paper key

--- go/engine/login_provision.go
+++ go/engine/login_provision.go
@@ -354,6 +354,10 @@ func (e *loginProvision) getValidPaperKey(ctx *Context) (*keypair, error) {
 		uid, err := e.uidByKID(kp.sigKey.GetKID())
 		if err != nil {
 			e.G().Log.Debug("getValidPaperKey attempt %d (%s): %s", i, prefix, err)
+			if _, ok := err.(libkb.NotFoundError); ok {
+				// make Msg a little friendlier (instead of KID Not Found)
+				err.Msg = ("paper key not found, most likely due to a typo in one of the words in the phrase")
+			}
 			lastErr = err
 			continue
 		}
