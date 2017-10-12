23cd6ca04a3f2a587701a2786e1ef03a2c44940e
Frederick Akalin <akalin@keyba.se>
2017-10-12 01:19:18 -0700

    Rem advanced

--- shared/settings/nav/index.desktop.js
+++ shared/settings/nav/index.desktop.js
@@ -44,12 +44,6 @@ function SettingsNav({badgeNumbers, selectedTab, onTabChange, onLogout}: Props) 
         badgeNumber={0}
         onClick={() => onTabChange(Constants.deleteMeTab)}
       />
-      <SettingsItem
-        text="Advanced"
-        selected={selectedTab === Constants.advancedTab}
-        badgeNumber={0}
-        onClick={() => onTabChange(Constants.advancedTab)}
-      />
       <SettingsItem text="Sign out" selected={false} badgeNumber={0} onClick={onLogout} />
       {__DEV__ &&
         <SettingsItem
