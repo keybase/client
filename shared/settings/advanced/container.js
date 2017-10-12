537fe257da93bf887e9e1cfcadf07239fcc8a7ab
Frederick Akalin <akalin@keyba.se>
2017-10-12 00:43:20 -0700

    Hook up button

--- shared/settings/advanced/container.js
+++ shared/settings/advanced/container.js
@@ -1,11 +1,14 @@
 // @flow
 import {navigateAppend} from '../../actions/route-tree'
+import {clearCache} from '../../actions/settings'
 import ClearCache from './index'
 import {connect} from 'react-redux'
 
 const mapStateToProps = () => ({})
 const mapDispatchToProps = (dispatch: Dispatch) => ({
-  onDelete: () => dispatch(navigateAppend(['clearCacheConfirm'])),
+  onClearCache: () => {
+    dispatch(clearCache)
+  },
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(ClearCache)
