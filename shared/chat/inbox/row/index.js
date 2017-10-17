c9eed5f154874b010668732dcdb6e5ae159a90aa
chrisnojima <cnojima@keyba.se>
2017-10-17 10:05:01 -0400

    move bigTeamslabel into factory

--- shared/chat/inbox/row/index.js
+++ shared/chat/inbox/row/index.js
@@ -5,6 +5,10 @@ import BigTeamChannel from './big-team-channel/container'
 import FilterBigTeamChannel from './filter-big-team-channel/container'
 import FilterSmallTeamChannel from './filter-small-team/container'
 import SmallTeam from './small-team/container'
+import {BigTeamsLabel} from './big-teams-label'
+import {Box} from '../../../common-adapters'
+import {globalStyles, globalMargins} from '../../../styles'
+import {isMobile} from '../../../constants/platform'
 import * as Constants from '../../../constants/chat'
 
 type MakeRowOptions = {
@@ -13,9 +17,17 @@ type MakeRowOptions = {
   filtered: boolean,
   isActiveRoute: boolean,
   teamname: string,
-  type: 'small' | 'bigHeader' | 'bigTeamsLabel' | 'big' | 'divider',
+  type: 'small' | 'bigHeader' | 'bigTeamsLabel' | 'big',
 }
 const makeRow = (options: MakeRowOptions) => {
+  if (options.type === 'bigTeamsLabel') {
+    return (
+      <Box style={_bigTeamLabelStyle} key="bigTeamsLabel">
+        <BigTeamsLabel isFiltered={options.filtered} />
+      </Box>
+    )
+  }
+
   if (options.filtered) {
     switch (options.type) {
       case 'big':
@@ -71,6 +83,13 @@ const makeRow = (options: MakeRowOptions) => {
     }
   }
   return null
+}
+
+const _bigTeamLabelStyle = {
+  ...globalStyles.flexBoxRow,
+  alignItems: 'center',
+  height: isMobile ? 32 : 24,
+  marginLeft: globalMargins.tiny,
 }
 
 export {makeRow}
