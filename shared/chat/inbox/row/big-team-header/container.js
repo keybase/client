7fe236d89b591cd513d61a160ab83a7b2d90a056
chrisnojima <cnojima@keyba.se>
2017-10-18 10:48:02 -0400

    fix navigateappend on row connector

--- shared/chat/inbox/row/big-team-header/container.js
+++ shared/chat/inbox/row/big-team-header/container.js
@@ -1,19 +1,23 @@
 // @flow
 import {pausableConnect, type TypedState} from '../../../../util/container'
 import {BigTeamHeader} from '.'
+// Typically you'd use the navigateAppend from the routeable component but this is a child* of that
+// and I'd prefer not to plumb through anything that could cause render thrashing so using the
+// global one here
+import {navigateAppend} from '../../../../actions/route-tree'
 
 const mapStateToProps = (state: TypedState, {teamname, isActiveRoute}) => {
-  return {teamname, isActiveRoute}
+  return {isActiveRoute, teamname}
 }
 
-const mapDispatchToProps = (dispatch, {navigateAppend}) => ({
+const mapDispatchToProps = dispatch => ({
   _onShowMenu: (teamname: string) =>
     dispatch(navigateAppend([{props: {teamname}, selected: 'manageChannels'}])),
 })
 
 const mergeProps = (stateProps, dispatchProps, ownProps) => ({
-  onShowMenu: () => dispatchProps._onShowMenu(stateProps.teamname),
   isActiveRoute: stateProps.isActiveRoute,
+  onShowMenu: () => dispatchProps._onShowMenu(stateProps.teamname),
   teamname: stateProps.teamname,
 })
 
