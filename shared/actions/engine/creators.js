b912e2775ed937fa32d7e6c72094bb42e5545a62
Frederick Akalin <akalin@keyba.se>
2017-10-18 09:53:02 -0700

    Add log transformer

--- shared/actions/engine/creators.js
+++ shared/actions/engine/creators.js
@@ -1,8 +1,17 @@
 // @flow
 import * as Constants from '../../constants/engine'
 
+const waitingForRpcTransformer = (action: Constants.WaitingForRpc) => ({
+  payload: {
+    rpcName: action.payload.rpcName,
+    waiting: action.payload.waiting,
+  },
+  type: action.type,
+})
+
 function waitingForRpc(rpcName: string, waiting: boolean): Constants.WaitingForRpc {
   return {
+    logTransformer: waitingForRpcTransformer,
     type: 'engine:waitingForRpc',
     payload: {rpcName, waiting},
   }
