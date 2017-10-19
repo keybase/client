a19bd82f4572f7801593849e4933e87a566f6fd3
Steve Sanders <steve@keyba.se>
2017-10-19 18:15:08 -0400

    add lifecycle to clear out error

--- shared/chat/new-team-dialog-container.js
+++ shared/chat/new-team-dialog-container.js
@@ -3,8 +3,9 @@ import NewTeamDialog from '../teams/new-team'
 import {connect} from 'react-redux'
 import {compose, withState, withHandlers} from 'recompose'
 import {type TypedState} from '../constants/reducer'
-import {createNewTeamFromConversation} from '../actions/teams/creators'
+import {createNewTeamFromConversation, setTeamCreationError} from '../actions/teams/creators'
 import {upperFirst} from 'lodash'
+import {lifecycle} from '../util/container'
 
 const mapStateToProps = (state: TypedState) => ({
   errorText: upperFirst(state.chat.teamCreationError),
@@ -15,6 +16,9 @@ const mapDispatchToProps = (dispatch: Dispatch, {navigateUp, routeProps}) => ({
   _onCreateNewTeam: name => {
     dispatch(createNewTeamFromConversation(routeProps.get('conversationIDKey'), name))
   },
+  _onSetTeamCreationError: error => {
+    dispatch(setTeamCreationError(error))
+  },
   onBack: () => dispatch(navigateUp()),
 })
 
@@ -23,6 +27,11 @@ const NewTeamDialogFromChat = compose(
   withState('name', 'onNameChange', ''),
   withHandlers({
     onSubmit: ({name, _onCreateNewTeam}) => () => _onCreateNewTeam(name),
+  }),
+  lifecycle({
+    componentDidMount: function() {
+      this.props._onSetTeamCreationError('')
+    },
   })
 )(NewTeamDialog)
 
