81ca8b71de1472bdb8c1232e9e4a19b6228aef98
Song Gao <song@gao.io>
2017-10-19 14:29:31 -0700

    review feedback

--- go/protocol/keybase1/metadata.go
+++ go/protocol/keybase1/metadata.go
@@ -148,6 +148,32 @@ func (o LockContext) DeepCopy() LockContext {
 	}
 }
 
+type MDGetBehavior int
+
+const (
+	MDGetBehavior_GET_OR_CREATE_CLASSIC_TLF MDGetBehavior = 0
+	MDGetBehavior_GET_CLASSIC_TLF_NO_CREATE MDGetBehavior = 1
+)
+
+func (o MDGetBehavior) DeepCopy() MDGetBehavior { return o }
+
+var MDGetBehaviorMap = map[string]MDGetBehavior{
+	"GET_OR_CREATE_CLASSIC_TLF": 0,
+	"GET_CLASSIC_TLF_NO_CREATE": 1,
+}
+
+var MDGetBehaviorRevMap = map[MDGetBehavior]string{
+	0: "GET_OR_CREATE_CLASSIC_TLF",
+	1: "GET_CLASSIC_TLF_NO_CREATE",
+}
+
+func (e MDGetBehavior) String() string {
+	if v, ok := MDGetBehaviorRevMap[e]; ok {
+		return v
+	}
+	return ""
+}
+
 type GetChallengeArg struct {
 }
 
@@ -211,7 +237,7 @@ type GetMetadataArg struct {
 	StopRevision  int64             `codec:"stopRevision" json:"stopRevision"`
 	LogTags       map[string]string `codec:"logTags" json:"logTags"`
 	LockBeforeGet *LockID           `codec:"lockBeforeGet,omitempty" json:"lockBeforeGet,omitempty"`
-	NoCreate      bool              `codec:"noCreate" json:"noCreate"`
+	GetBehavior   MDGetBehavior     `codec:"getBehavior" json:"getBehavior"`
 }
 
 func (o GetMetadataArg) DeepCopy() GetMetadataArg {
@@ -246,7 +272,7 @@ func (o GetMetadataArg) DeepCopy() GetMetadataArg {
 			tmp := (*x).DeepCopy()
 			return &tmp
 		})(o.LockBeforeGet),
-		NoCreate: o.NoCreate,
+		GetBehavior: o.GetBehavior.DeepCopy(),
 	}
 }
 
