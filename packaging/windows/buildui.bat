690b2f0166bd6eea0f38bd23b953f248337a1234
Steve Sanders <steve@zanderz.com>
2017-10-23 13:45:19 -0700

    yarn install probs

--- packaging/windows/buildui.bat
+++ packaging/windows/buildui.bat
@@ -7,7 +7,8 @@ popd
 echo on
 pushd  %GOPATH%\src\github.com\keybase\client\shared
 echo Calling yarn run modules
-yarn run modules
+:: yarn sometimes exits this console
+cmd /C  yarn install --pure-lockfile
 
 yarn run package -- --arch ia32 --platform win32 --appVersion %KEYBASE_VERSION% --icon %GOPATH%\src\github.com\keybase\client\media\icons\Keybase.ico
 popd
