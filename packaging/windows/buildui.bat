034a5f8a7168f16f55197a3db20b5b5291756fea
Steve Sanders <steve@keyba.se>
2017-10-24 12:57:00 -0700

    Change from Jenkins to plain scripts for Windows release build (#9143)
    * make builds without jenkins

--- packaging/windows/buildui.bat
+++ packaging/windows/buildui.bat
@@ -1,14 +1,14 @@
 pushd %GOPATH%\src\github.com\keybase\client\go\keybase
 
 for /f %%i in ('winresource.exe -cv') do set KEYBASE_VERSION=%%i
-echo Calling vcvars
 
-echo on
-call "%ProgramFiles(x86)%\\Microsoft Visual Studio 14.0\\vc\\bin\\vcvars32.bat"
+popd
 
 echo on
 pushd  %GOPATH%\src\github.com\keybase\client\shared
 echo Calling yarn run modules
-call yarn run modules
+:: yarn sometimes exits this console
+cmd /C  yarn install --pure-lockfile
 
 yarn run package -- --arch ia32 --platform win32 --appVersion %KEYBASE_VERSION% --icon %GOPATH%\src\github.com\keybase\client\media\icons\Keybase.ico
+popd
