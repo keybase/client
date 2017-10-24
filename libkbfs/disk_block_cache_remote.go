21eea231174e1a87f95679d45d770a71ce76697c
John Zila <john@keyba.se>
2017-10-24 11:50:28 -0700

    disk_block_cache_remote: Implement Get

--- libkbfs/disk_block_cache_remote.go
+++ libkbfs/disk_block_cache_remote.go
@@ -4,25 +4,62 @@ import (
 	"context"
 	"errors"
 
+	keybase1 "github.com/keybase/client/go/protocol/keybase1"
 	"github.com/keybase/kbfs/kbfsblock"
 	"github.com/keybase/kbfs/kbfscrypto"
+	kbgitkbfs "github.com/keybase/kbfs/protocol/kbgitkbfs"
 	"github.com/keybase/kbfs/tlf"
 )
+
+type diskBlockCacheRemoteConfig interface {
+	logMaker
+}
 
 // DiskBlockCacheRemote implements a client to access a remote
 // DiskBlockCacheService. It implements the DiskBlockCache interface.
 type DiskBlockCacheRemote struct {
+	client kbgitkbfs.DiskBlockCacheClient
+	log    traceLogger
 }
 
 var _ DiskBlockCache = (*DiskBlockCacheRemote)(nil)
+
+// NewDiskBlockCacheRemote creates a new remote disk cache client.
+func NewDiskBlockCacheRemote(kbCtx Context, config diskBlockCacheRemoteConfig) (
+	*DiskBlockCacheRemote, error) {
+	client := kbgitkbfs.DiskBlockCacheClient{}
+	return &DiskBlockCacheRemote{
+		client: client,
+		log:    traceLogger{config.MakeLogger("DBR")},
+	}, nil
+}
 
 // Get implements the DiskBlockCache interface for DiskBlockCacheRemote.
 func (dbcr *DiskBlockCacheRemote) Get(ctx context.Context, tlfID tlf.ID,
 	blockID kbfsblock.ID) (buf []byte,
 	serverHalf kbfscrypto.BlockCryptKeyServerHalf,
 	prefetchStatus PrefetchStatus, err error) {
-	err = errors.New("not implemented")
-	return
+	dbcr.log.LazyTrace(ctx, "DiskBlockCacheRemote: Get %s", blockID)
+	defer func() {
+		dbcr.log.LazyTrace(ctx, "DiskBlockCacheRemote: Get %s done (err=%+v)", blockID, err)
+	}()
+
+	arg := kbgitkbfs.GetBlockArg{
+		keybase1.TLFID(tlfID.String()),
+		blockID.String(),
+	}
+
+	res, err := dbcr.client.GetBlock(ctx, arg)
+	if err != nil {
+		return buf, serverHalf, prefetchStatus, err
+	}
+
+	serverHalf, err = kbfscrypto.ParseBlockCryptKeyServerHalf(res.ServerHalf)
+	if err != nil {
+		return nil, kbfscrypto.BlockCryptKeyServerHalf{}, prefetchStatus, err
+	}
+
+	return res.Buf, serverHalf, PrefetchStatus(res.PrefetchStatus), nil
 }
 
 // Put implements the DiskBlockCache interface for DiskBlockCacheRemote.
