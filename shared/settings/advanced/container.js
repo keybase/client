ef5e1cb580f71c39434141639bf4c53a3ffe656d
cecileboucheron <cecile@keyba.se>
2017-10-27 10:16:26 -0700

    Added back button

--- shared/settings/advanced/container.js
+++ shared/settings/advanced/container.js
@@ -1,10 +1,13 @@
 // @flow
-import {navigateAppend} from '../../actions/route-tree'
+import {navigateAppend, navigateUp} from '../../actions/route-tree'
 import DBNuke from './index'
 import {connect} from 'react-redux'
 
 const mapStateToProps = () => ({})
 const mapDispatchToProps = (dispatch: Dispatch) => ({
+  onBack: () => {
+    dispatch(navigateUp())
+  },
   onDBNuke: () => {
     dispatch(navigateAppend(['dbNukeConfirm']))
   },
--- shared/settings/advanced/index.js
+++ shared/settings/advanced/index.js
@@ -1,35 +1,38 @@
 // @flow
 import * as React from 'react'
 import {globalStyles, globalMargins} from '../../styles'
-import {Box, Button, Text} from '../../common-adapters'
+import {StandardScreen, Box, Button, Text} from '../../common-adapters'
 
 type Props = {
   onDBNuke: () => void,
+  onBack: () => void,
 }
 
 function DBNuke(props: Props) {
   return (
-    <Box
-      style={{
-        ...globalStyles.flexBoxColumn,
-        alignItems: 'center',
-        flex: 1,
-        padding: globalMargins.medium,
-        paddingTop: globalMargins.xlarge,
-        paddingBottom: globalMargins.xlarge,
-      }}
-    >
-      <Text type="Body" style={{textAlign: 'center', padding: globalMargins.small}}>
-        Don't do anything here unless instructed to by a developer.
-      </Text>
-      <Button
-        style={{marginTop: globalMargins.small}}
-        type="Danger"
-        label="DB Nuke"
-        onClick={props.onDBNuke}
-      />
-      <Box style={{flex: 1}} />
-    </Box>
+    <StandardScreen onBack={props.onBack}>
+      <Box
+        style={{
+          ...globalStyles.flexBoxColumn,
+          alignItems: 'center',
+          flex: 1,
+          padding: globalMargins.medium,
+          paddingTop: globalMargins.xlarge,
+          paddingBottom: globalMargins.medium,
+        }}
+      >
+        <Text type="BodySemibold" style={{textAlign: 'center'}}>
+          Please don't do anything here unless instructed to by a developer.
+        </Text>
+        <Button
+          style={{marginTop: globalMargins.small}}
+          type="Danger"
+          label="DB Nuke"
+          onClick={props.onDBNuke}
+        />
+        <Box style={{flex: 1}} />
+      </Box>
+    </StandardScreen>
   )
 }
 
