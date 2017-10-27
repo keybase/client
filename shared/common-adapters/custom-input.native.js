f3123bb0bc44cc8466fa9738e45be619488fa93a
Marco Munizaga <marco@marcopolo.io>
2017-10-27 11:24:48 -0700

    WIP

---
+++ shared/common-adapters/custom-input.native.js
@@ -0,0 +1,7 @@
+// @flow
+// ImageView.js
+
+import PropTypes from 'prop-types'
+import {requireNativeComponent, View} from 'react-native'
+
+module.exports = requireNativeComponent('CustomTextInput', null)
