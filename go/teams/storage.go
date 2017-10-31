dfc50cb319d83b0bf60c1cec2a65ebf697b1edc6
Miles Steele <miles@milessteele.com>
2017-10-31 15:01:44 -0400

    enable seitan rkm coverage check

--- go/teams/storage.go
+++ go/teams/storage.go
@@ -89,7 +89,7 @@ type DiskStorage struct {
 }
 
 // Increment to invalidate the disk cache.
-const diskStorageVersion = 6
+const diskStorageVersion = 7
 
 type DiskStorageItem struct {
 	Version int                `codec:"V"`
