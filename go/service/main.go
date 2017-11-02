305b72586d2af546d1889e353fe9486673489290
Mike Maxim <mike@keyba.se>
2017-11-02 10:39:28 -0400

    check for loginstate timeout error as well as network errors (#9348)
    * check for loginstate timeout error as well as network errors
    
    * wrong language
    
    * just debug these out

--- go/service/main.go
+++ go/service/main.go
@@ -549,9 +549,11 @@ func (d *Service) tryGregordConnect() error {
 		// confirm with the API server. In that case we'll swallow the error
 		// and allow control to proceeed to the gregor loop. We'll still
 		// short-circuit for any unexpected errors though.
-		_, isNetworkError := err.(libkb.APINetError)
-		if !isNetworkError {
-			d.G().Log.Warning("Unexpected non-network error in tryGregordConnect: %s", err)
+		switch err.(type) {
+		case libkb.LoginStateTimeoutError, libkb.APINetError:
+			d.G().Log.Debug("Network/timeout error received from LoginState, continuing onward: %s", err)
+		default:
+			d.G().Log.Debug("Unexpected non-network error in tryGregordConnect: %s", err)
 			return err
 		}
 	} else if !loggedIn {
