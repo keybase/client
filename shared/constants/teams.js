2a68b68f5364c7f37a115a62949d471b7570b707
Daniel Ayoub <danny.a95@gmail.com>
2017-11-03 17:55:08 -0400

    Define getFollowingMap in team constants (#9379)

--- shared/constants/teams.js
+++ shared/constants/teams.js
@@ -219,7 +219,7 @@ const getConversationIDKeyFromChannelName = (state: TypedState, channelname: str
 const getParticipants = (state: TypedState, conversationIDKey: ChatConstants.ConversationIDKey) =>
   state.entities.getIn(['teams', 'convIDToChannelInfo', conversationIDKey, 'participants'], I.Set())
 
-export const getFollowingMap = ChatConstants.getFollowingMap
+export const getFollowingMap = (state: TypedState) => state.config.following
 export const getFollowerMap = (state: TypedState) => state.config.followers
 
 export {getConversationIDKeyFromChannelName, getParticipants, userIsInTeamHelper}
