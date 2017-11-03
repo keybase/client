04a4fc3d44133c76c76ba37e21bd09626fc90cea
Frederick Akalin <akalin@keyba.se>
2017-11-03 15:42:50 -0700

    Add tests for baseTeamname

--- shared/teams/__test__/teamname.test.js
+++ shared/teams/__test__/teamname.test.js
@@ -1,21 +1,29 @@
 // @noflow
 /* eslint-env jest */
-import {validTeamname} from '../teamname'
+import {validTeamname, baseTeamname} from '../teamname'
 
-describe('validTeamname', () => {
-  it('valid names', () => {
-    const validNames = ['someteam', 'team.sub', 'team.sub.sub']
-    for (let i = 0; i < validNames.length; ++i) {
-      const validName = validNames[i]
-      expect(validTeamname(validName)).toBe(true)
-    }
+describe('teamname', () => {
+  describe('validTeamname', () => {
+    it('valid names', () => {
+      const validNames = ['someteam', 'team.sub', 'team.sub.sub']
+      for (let i = 0; i < validNames.length; ++i) {
+        const validName = validNames[i]
+        expect(validTeamname(validName)).toBe(true)
+      }
+    })
+
+    it('invalid names', () => {
+      const validNames = ['some team', 'team.some sub', 'team.sub.some sub', '$ometeam', 'someteam-']
+      for (let i = 0; i < validNames.length; ++i) {
+        const validName = validNames[i]
+        expect(validTeamname(validName)).toBe(false)
+      }
+    })
   })
 
-  it('invalid names', () => {
-    const validNames = ['some team', 'team.some sub', 'team.sub.some sub', '$ometeam', 'someteam-']
-    for (let i = 0; i < validNames.length; ++i) {
-      const validName = validNames[i]
-      expect(validTeamname(validName)).toBe(false)
-    }
+  it('baseTeamname', () => {
+    expect(baseTeamname('team')).toBe(null)
+    expect(baseTeamname('team.sub')).toBe('team')
+    expect(baseTeamname('team.sub.sub')).toBe('team.sub')
   })
 })
