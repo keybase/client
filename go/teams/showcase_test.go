4ccb951339578eb9bc8c44f13a3efa035fa218ff
Michał Zochniak <michal@zapu.net>
2017-11-03 11:28:03 +0100

    More tests

--- go/teams/showcase_test.go
+++ go/teams/showcase_test.go
@@ -6,7 +6,9 @@ import (
 	"golang.org/x/net/context"
 
 	"github.com/keybase/client/go/kbtest"
+	"github.com/keybase/client/go/libkb"
 	"github.com/keybase/client/go/protocol/keybase1"
+
 	"github.com/stretchr/testify/require"
 )
 
@@ -102,4 +104,61 @@ func TestShowcaseMember(t *testing.T) {
 	require.Equal(t, user.User.GetUID(), *showcase.SetByUID)
 	require.NotNil(t, showcase.Description)
 	require.Equal(t, "Hello Team!", *showcase.Description)
+}
+
+func TestShowcasePermissions(t *testing.T) {
+	tc := SetupTest(t, "team", 1)
+	defer tc.Cleanup()
+
+	user, err := kbtest.CreateAndSignupFakeUser("team", tc.G)
+	require.NoError(t, err)
+
+	_, err = kbtest.CreateAndSignupFakeUser("team", tc.G)
+	require.NoError(t, err)
+
+	team := createTeam(tc)
+	t.Logf("Created team %q", team)
+
+	isShowcased := true
+	description := "This team is showcased"
+	err = SetTeamShowcase(context.TODO(), tc.G, team, &isShowcased, &description)
+	require.NoError(t, err)
+
+	err = SetTeamMemberShowcase(context.TODO(), tc.G, team, true)
+	require.NoError(t, err)
+
+	_, err = AddMember(context.TODO(), tc.G, team, user.Username, keybase1.TeamRole_WRITER)
+	require.NoError(t, err)
+
+	tc.G.Logout()
+	err = user.Login(tc.G)
+	require.NoError(t, err)
+
+	// Set- functions should check membership and not issue request
+	// when user is not an admin or higher.
+	isShowcased = false
+	err = SetTeamShowcase(context.TODO(), tc.G, team, &isShowcased, nil)
+	require.Error(t, err)
+
+	// AppStatusErrors means we bounced off server instead of being
+	// stopped by API helper code.
+	_, ok := err.(libkb.AppStatusError)
+	require.False(t, ok)
+
+	err = SetTeamMemberShowcase(context.TODO(), tc.G, team, true)
+	require.Error(t, err)
+	_, ok = err.(libkb.AppStatusError)
+	require.False(t, ok)
+
+	// Get- functions should still work.
+	ret, err := GetTeamAndMemberShowcase(context.TODO(), tc.G, team)
+	require.NoError(t, err)
+	require.False(t, ret.IsMemberShowcased) // always false if user is not an admin
+	require.NotNil(t, ret.TeamShowcase.Description)
+	require.Equal(t, description, *ret.TeamShowcase.Description)
+	require.True(t, ret.TeamShowcase.IsShowcased)
+
+	ret2, err := GetTeamShowcase(context.TODO(), tc.G, team)
+	require.NoError(t, err)
+	require.Equal(t, ret.TeamShowcase, ret2)
 }
