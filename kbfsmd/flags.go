e18fc2c2b0cf3b64107a56d3dc6251c82dd166d4
Fred Akalin <akalin@keyba.se>
2017-11-05 11:00:22 -0800

    Add string methods for flag types (#1311)
    This helps when doing kbfstool md dump.

--- kbfsmd/flags.go
+++ kbfsmd/flags.go
@@ -4,6 +4,11 @@
 
 package kbfsmd
 
+import (
+	"fmt"
+	"strings"
+)
+
 // MetadataFlags bitfield.
 type MetadataFlags byte
 
@@ -14,6 +19,30 @@ const (
 	MetadataFlagFinal
 )
 
+var metadataFlagStringMap = map[int]string{
+	int(MetadataFlagRekey):                "Rekey",
+	int(MetadataFlagWriterMetadataCopied): "WriterMetadataCopied",
+	int(MetadataFlagFinal):                "Final",
+}
+
+func flagsToString(flags int, flagStringMap map[int]string) string {
+	var flagStrings []string
+	for f, s := range flagStringMap {
+		if flags&f != 0 {
+			flagStrings = append(flagStrings, s)
+			flags &^= f
+		}
+	}
+	if flags != 0 {
+		flagStrings = append(flagStrings, fmt.Sprintf("%b", flags))
+	}
+	return strings.Join(flagStrings, " | ")
+}
+
+func (flags MetadataFlags) String() string {
+	return fmt.Sprintf("MetadataFlags(%s)", flagsToString(int(flags), metadataFlagStringMap))
+}
+
 // WriterFlags bitfield.
 type WriterFlags byte
 
@@ -21,3 +50,11 @@ type WriterFlags byte
 const (
 	MetadataFlagUnmerged WriterFlags = 1 << iota
 )
+
+var writerFlagStringMap = map[int]string{
+	int(MetadataFlagUnmerged): "Unmerged",
+}
+
+func (flags WriterFlags) String() string {
+	return fmt.Sprintf("WriterFlags(%s)", flagsToString(int(flags), writerFlagStringMap))
+}
