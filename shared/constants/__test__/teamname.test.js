deca2f41bd4f1f8d2144e2bf031ec52eeb738b74
Frederick Akalin <akalin@keyba.se>
2017-11-09 12:15:55 -0800

    Remove

--- shared/constants/__test__/teamname.test.js
+++ shared/constants/__test__/teamname.test.js
@@ -27,35 +27,4 @@ describe('teamname', () => {
     expect(baseTeamname('team.sub')).toBe('team')
     expect(baseTeamname('team.sub.sub')).toBe('team.sub')
   })
-
-  it('ancestorTeamnames', () => {
-    expect(ancestorTeamnames('team.sub.sub')).toEqual(['team.sub', 'team'])
-    expect(ancestorTeamnames('team')).toEqual([])
-  })
-
-  it('isExplicitAdmin', () => {
-    const memberInfo = I.Set([
-      {username: 'alice', type: 'owner'},
-      {username: 'bob', type: 'admin'},
-      {username: 'charlie', type: 'writer'},
-    ])
-
-    expect(isExplicitAdmin(memberInfo, 'alice')).toBe(true)
-    expect(isExplicitAdmin(memberInfo, 'bob')).toBe(true)
-    expect(isExplicitAdmin(memberInfo, 'charlie')).toBe(false)
-    expect(isExplicitAdmin(memberInfo, 'david')).toBe(false)
-  })
-
-  it('isImplicitAdmin', () => {
-    const rootMemberInfo = I.Set([{username: 'alice', type: 'owner'}])
-
-    const subMemberInfo = I.Set([{username: 'bob', type: 'admin'}])
-
-    const ancestorMemberInfo = I.Map([['root', rootMemberInfo], ['root.sub', subMemberInfo]])
-
-    expect(isImplicitAdmin(ancestorMemberInfo, 'alice')).toBe(true)
-    expect(isImplicitAdmin(ancestorMemberInfo, 'bob')).toBe(true)
-    expect(isImplicitAdmin(ancestorMemberInfo, 'charlie')).toBe(false)
-    expect(isImplicitAdmin(ancestorMemberInfo, 'david')).toBe(false)
-  })
 })
