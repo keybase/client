2d8826ffc30fa3bdd98035bca8f35ddc709ee77f
Daniel Ayoub <danny.a95@gmail.com>
2017-11-09 16:04:43 -0500

    Upgrade emojimart and add check for invalid emojis

--- shared/common-adapters/emoji.desktop.js
+++ shared/common-adapters/emoji.desktop.js
@@ -1,7 +1,7 @@
 // @flow
 
 import * as React from 'react'
-import {Emoji} from 'emoji-mart'
+import {Emoji, emojiIndex} from 'emoji-mart'
 
 // Just the single set we use
 import emojiSet from 'emoji-datasource-apple/img/apple/sheets/64.png'
@@ -10,9 +10,22 @@ import type {Props} from './emoji'
 
 const backgroundImageFn = (set: string, sheetSize: string) => emojiSet
 
+const skinToneRegex = /:([^:]*):(:skin-tone-\d:)?/i
+
+const isValidEmoji = (fullEmoji: string) => {
+  const parts = skinToneRegex.exec(fullEmoji)
+  if (parts) {
+    return !parts[1].startsWith('skin-tone') && emojiIndex.search(parts[1]).length > 0
+  }
+  return true
+}
+
 // Size 0 is cause we want the native emoji for copy/paste and not for rendering
 const EmojiWrapper = (props: Props) => {
   const {emojiName, size} = props
+  if (!isValidEmoji(emojiName)) {
+    return emojiName
+  }
   return (
     <Emoji emoji={emojiName} size={size} backgroundImageFn={backgroundImageFn}>
       <Emoji emoji={emojiName} size={0} native={true} />
