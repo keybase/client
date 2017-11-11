22cd74b8e90e9d0c591ea0fc34b2a0bdb7681491
Michał Zochniak <michal@zapu.net>
2017-11-11 01:33:09 +0100

    Add test for setting member showcase as writer

--- go/teams/showcase_test.go
+++ go/teams/showcase_test.go
@@ -156,7 +156,7 @@ func TestShowcasePermissions(t *testing.T) {
 	// Get- functions should still work.
 	ret, err := GetTeamAndMemberShowcase(context.TODO(), tc.G, team)
 	require.NoError(t, err)
-	require.False(t, ret.IsMemberShowcased) // always false if user is not an admin
+	require.False(t, ret.IsMemberShowcased) // false by default
 	require.NotNil(t, ret.TeamShowcase.Description)
 	require.Equal(t, description, *ret.TeamShowcase.Description)
 	require.True(t, ret.TeamShowcase.IsShowcased)
@@ -164,4 +164,38 @@ func TestShowcasePermissions(t *testing.T) {
 	ret2, err := GetTeamShowcase(context.TODO(), tc.G, team)
 	require.NoError(t, err)
 	require.Equal(t, ret.TeamShowcase, ret2)
+}
+
+func TestShowcaseAnyMember(t *testing.T) {
+	tc := SetupTest(t, "team", 1)
+	defer tc.Cleanup()
+
+	user, err := kbtest.CreateAndSignupFakeUser("team", tc.G)
+	require.NoError(t, err)
+
+	_, err = kbtest.CreateAndSignupFakeUser("team", tc.G)
+	require.NoError(t, err)
+
+	team := createTeam(tc)
+	t.Logf("Created team %q", team)
+
+	_, err = AddMember(context.TODO(), tc.G, team, user.Username, keybase1.TeamRole_READER)
+	require.NoError(t, err)
+
+	tc.G.Logout()
+	err = user.Login(tc.G)
+	require.NoError(t, err)
+
+	t.Logf("Logged in as %q (reader)", user.Username)
+
+	ret, err := GetTeamAndMemberShowcase(context.TODO(), tc.G, team)
+	require.NoError(t, err)
+	require.False(t, ret.IsMemberShowcased) // false by default
+
+	err = SetTeamMemberShowcase(context.TODO(), tc.G, team, true)
+	require.NoError(t, err)
+
+	ret, err = GetTeamAndMemberShowcase(context.TODO(), tc.G, team)
+	require.NoError(t, err)
+	require.True(t, ret.IsMemberShowcased) // membership is true and we are a reader
 }
