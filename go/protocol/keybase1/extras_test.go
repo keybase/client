4ba4aa18810e2ea7a1fdb142cc77f5fc2097a139
Song Gao <song@gao.io>
2017-11-15 10:43:41 -0800

    Revert "review feedback from @jzila"
    This reverts commit a93ebe9b37e3a907f73d9b68c86615eb628f62fa.

--- go/protocol/keybase1/extras_test.go
+++ go/protocol/keybase1/extras_test.go
@@ -7,8 +7,6 @@ import (
 	"errors"
 	"testing"
 	"time"
-
-	"github.com/keybase/kbfs/cache"
 )
 
 func TestTime(t *testing.T) {
@@ -135,8 +133,3 @@ func TestTeamNameFromString(t *testing.T) {
 		}
 	}
 }
-
-var _ cache.Measurable = (*KID)(nil)
-var _ cache.Measurable = UID("")
-var _ cache.Measurable = TeamID("")
-var _ cache.Measurable = UserOrTeamID("")
