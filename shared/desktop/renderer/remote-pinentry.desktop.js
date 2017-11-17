bead407a44b1fc12d576196d77d3e8bf1af92f94
chrisnojima <cnojima@keyba.se>
2017-11-17 11:33:32 -0500

    WIP

---
+++ shared/desktop/renderer/remote-pinentry.desktop.js
@@ -0,0 +1,60 @@
+// @flow
+import React, {Component} from 'react'
+import RemoteComponent from './remote-component'
+import {connect, type TypedState} from '../../util/container'
+import {registerPinentryListener, onCancel, onSubmit} from '../../actions/pinentry'
+import {type PinentryState} from '../../constants/types/pinentry'
+import {type GUIEntryFeatures} from '../../constants/types/flow-types'
+
+type Props = {
+  registerPinentryListener: () => void,
+  onCancel: (sessionID: number) => void,
+  onSubmit: (sessionID: number, passphrase: string, features: GUIEntryFeatures) => void,
+  pinentryStates: {[key: string]: PinentryState},
+}
+
+class RemotePinentry extends Component<Props> {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.pinentryStates !== this.props.pinentryStates
+  }
+
+  render() {
+    const {pinentryStates} = this.props
+
+    if (!pinentryStates) {
+      return null
+    }
+
+    return (
+      <div>
+        {Object.keys(pinentryStates).filter(sid => !pinentryStates[sid].closed).map(pSessionID => {
+          const sid = parseInt(pSessionID, 10)
+          return (
+            <RemoteComponent
+              title="Pinentry"
+              windowsOpts={{width: 440, height: 210}}
+              waitForState={true}
+              onRemoteClose={() => this.props.onCancel(sid)}
+              component="pinentry"
+              key={'pinentry:' + pSessionID}
+              onSubmit={(passphrase, features) => this.props.onSubmit(sid, passphrase, features)}
+              onCancel={() => this.props.onCancel(sid)}
+              sessionID={sid}
+            />
+          )
+        })}
+      </div>
+    )
+  }
+}
+
+export default connect(
+  (state: TypedState, ownProps: {}) => ({
+    pinentryStates: state.pinentry.pinentryStates || {},
+  }),
+  (dispatch: any, ownProps: {}) => ({
+    onCancel: (sid: number) => dispatch(onCancel(sid)),
+    onSubmit: (sid: number, passphrase: string, features: GUIEntryFeatures) =>
+      dispatch(onSubmit(sid, passphrase, features)),
+  })
+)(RemotePinentry)
---
+++ shared/desktop/renderer/remote-tracker.desktop.js
@@ -0,0 +1,78 @@
+// @flow
+import * as Creators from '../../actions/tracker'
+import * as TrackerGen from '../../actions/tracker-gen'
+import React, {Component} from 'react'
+import RemoteComponent from './remote-component'
+import {connect, type TypedState} from '../../util/container'
+import {type TrackerState, type NonUserState} from '../../constants/types/tracker'
+
+type Props = {
+  onClose: (username: string) => void,
+  started: boolean,
+  errorRetry: (username: string) => void,
+  startTimer: () => void,
+  stopTimer: () => void,
+  trackers: {[key: string]: TrackerState},
+  nonUserTrackers: {[key: string]: NonUserState},
+}
+
+const MAX_TRACKERS = 5
+
+class RemoteTracker extends Component<Props> {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.trackers !== this.props.trackers
+  }
+
+  render() {
+    const {trackers, nonUserTrackers} = this.props
+    const windowsOpts = {height: 470, width: 320}
+
+    const items = [
+      ...Object.keys(trackers).filter(t => !trackers[t].closed).map(t => ({
+        username: trackers[t].username,
+        hidden: trackers[t].hidden,
+      })),
+      ...Object.keys(nonUserTrackers).filter(n => !nonUserTrackers[n].closed).map(n => ({
+        username: nonUserTrackers[n].name,
+        hidden: nonUserTrackers[n].hidden,
+      })),
+    ].slice(0, MAX_TRACKERS)
+
+    return (
+      <div>
+        {items.map(item => (
+          <RemoteComponent
+            positionBottomRight={true}
+            windowsOpts={windowsOpts}
+            title={`tracker - ${item.username}`}
+            waitForState={true}
+            hidden={item.hidden}
+            onRemoteClose={() => this.props.onClose(item.username)}
+            component="tracker"
+            username={item.username}
+            startTimer={this.props.startTimer}
+            errorRetry={() => this.props.errorRetry(item.username)}
+            stopTimer={this.props.stopTimer}
+            selectorParams={item.username}
+            key={item.username}
+          />
+        ))}
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state: TypedState) => ({
+  started: state.tracker.serverStarted,
+  trackers: state.tracker.userTrackers,
+  nonUserTrackers: state.tracker.nonUserTrackers,
+})
+
+const mapDispatchToProps = (dispatch: any) => ({
+  startTimer: () => dispatch(Creators.startTimer()),
+  stopTimer: () => dispatch(TrackerGen.createStopTimer()),
+  errorRetry: (username: string) => dispatch(Creators.getProfile(username, true)),
+  onClose: (username: string) => dispatch(Creators.onClose(username)),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(RemoteTracker)
