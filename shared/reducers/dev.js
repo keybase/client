8c236a9b3a32cd562bc1d6f21a5d843788d64607
chrisnojima <cnojima@keyba.se>
2017-11-16 21:59:16 -0500

    maybe use functions instead

--- shared/reducers/dev.js
+++ shared/reducers/dev.js
@@ -3,27 +3,39 @@ import * as DevGen from '../actions/dev-gen'
 import * as Constants from '../constants/dev'
 import * as Types from '../constants/types/dev'
 
+const updateDebugConfig = (state, action) => {
+  const {config} = action.payload
+  return {
+    ...state,
+    debugConfig: {...state.debugConfig, ...config},
+  }
+}
+
+const updatehmrReloading = (state, action) => {
+  const {reloading} = action.payload
+  return {
+    ...state,
+    reloading,
+  }
+}
+
+const debugCount = (state, action) => {
+  return {
+    ...state,
+    debugCount: state.debugCount + 1,
+  }
+}
+
 export default function(state: Types.State = Constants.initialState, action: DevGen.Actions) {
   switch (action.type) {
     case DevGen.resetStore:
       return {...Constants.initialState}
     case DevGen.updateDebugConfig:
-      const {config} = action.payload
-      return {
-        ...state,
-        debugConfig: {...state.debugConfig, ...config},
-      }
+      return updateDebugConfig(state, action)
     case DevGen.updatehmrReloading:
-      const {reloading} = action.payload
-      return {
-        ...state,
-        reloading,
-      }
+      return updatehmrReloading(state, action)
     case DevGen.debugCount:
-      return {
-        ...state,
-        debugCount: state.debugCount + 1,
-      }
+      return debugCount(state, action)
     default:
       // eslint-disable-next-line no-unused-expressions
       (action: empty) // if you get a flow error here it means there's an action you claim to handle but didn't
