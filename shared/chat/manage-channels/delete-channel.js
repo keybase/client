b3596cadb06f446f58c8eb23b901acf80b7dfdf0
Frederick Akalin <akalin@keyba.se>
2017-11-16 22:22:46 -0800

    Add rough popup

--- shared/chat/manage-channels/delete-channel.js
+++ shared/chat/manage-channels/delete-channel.js
@@ -1,6 +1,7 @@
 // @flow
 import * as React from 'react'
-import {Text, Box, Icon} from '../../common-adapters'
+import ReactDOM from 'react-dom'
+import {Text, Box, Icon, PopupMenu} from '../../common-adapters'
 import {globalStyles, globalColors, globalMargins} from '../../styles'
 
 type Props = {
@@ -10,9 +11,39 @@ type Props = {
 
 type State = {}
 
+const stylePopup = {
+  overflow: 'visible',
+  width: 196,
+}
+
 class DeleteChannel extends React.Component<Props, State> {
+  _hidePopup = () => {
+    ReactDOM.unmountComponentAtNode(document.getElementById('popupContainer'))
+  }
+
+  _onClick(event: SyntheticEvent<>) {
+    const target = (event.target: any)
+    const clientRect = target.getBoundingClientRect()
+
+    // Position next to button (client rect)
+    // TODO: Measure instead of pixel math
+    const x = clientRect.left - 45
+    let y = clientRect.top - 116
+    if (y < 10) y = 10
+    const style = {left: x, position: 'absolute', top: y}
+
+    const items = []
+    items.push({title: 'Foo'})
+    const popupComponent = (
+      <PopupMenu items={items} onHidden={() => this._hidePopup()} style={{...stylePopup, ...style}} />
+    )
+    const container = document.getElementById('popupContainer')
+    // FIXME: this is the right way to render portals retaining context for now, though it will change in the future.
+    ReactDOM.unstable_renderSubtreeIntoContainer(this, popupComponent, container)
+  }
+
   render() {
-    const {disabled, onConfirmedDelete} = this.props
+    const {disabled} = this.props
     return (
       <Box
         style={{
@@ -29,7 +60,7 @@ class DeleteChannel extends React.Component<Props, State> {
         <Text
           type={disabled ? 'Body' : 'BodyPrimaryLink'}
           style={{color: globalColors.red}}
-          onClick={disabled ? undefined : onConfirmedDelete}
+          onClick={disabled ? undefined : e => this._onClick(e)}
         >
           Delete Channel
         </Text>
