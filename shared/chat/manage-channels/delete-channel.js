4e1191e15daef711347232d485de1f6ceee81dad
Frederick Akalin <akalin@keyba.se>
2017-11-16 18:08:33 -0800

    Turn into component

--- shared/chat/manage-channels/delete-channel.js
+++ shared/chat/manage-channels/delete-channel.js
@@ -4,28 +4,38 @@ import {Text, Box, Icon} from '../../common-adapters'
 import {globalStyles, globalColors, globalMargins} from '../../styles'
 
 type Props = {
-  onConfirmedDelete: () => void,
   disabled: boolean,
+  onConfirmedDelete: () => void,
 }
 
 type State = {}
 
-const DeleteChannel = ({onConfirmedDelete, disabled}: Props, _: State) => (
-  <Box
-    style={{...globalStyles.flexBoxRow, position: 'absolute', left: 0, opacity: disabled ? 0.5 : undefined}}
-  >
-    <Icon
-      type="iconfont-trash"
-      style={{height: 14, color: globalColors.red, marginRight: globalMargins.tiny}}
-    />
-    <Text
-      type={disabled ? 'Body' : 'BodyPrimaryLink'}
-      style={{color: globalColors.red}}
-      onClick={disabled ? undefined : onConfirmedDelete}
-    >
-      Delete Channel
-    </Text>
-  </Box>
-)
+class DeleteChannel extends React.Component<Props, State> {
+  render() {
+    const {disabled, onConfirmedDelete} = this.props
+    return (
+      <Box
+        style={{
+          ...globalStyles.flexBoxRow,
+          position: 'absolute',
+          left: 0,
+          opacity: disabled ? 0.5 : undefined,
+        }}
+      >
+        <Icon
+          type="iconfont-trash"
+          style={{height: 14, color: globalColors.red, marginRight: globalMargins.tiny}}
+        />
+        <Text
+          type={disabled ? 'Body' : 'BodyPrimaryLink'}
+          style={{color: globalColors.red}}
+          onClick={disabled ? undefined : onConfirmedDelete}
+        >
+          Delete Channel
+        </Text>
+      </Box>
+    )
+  }
+}
 
 export default DeleteChannel
