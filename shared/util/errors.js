4ec8622068861157e24cb828723cd2b9fa900f79
Daniel Ayoub <danny.a95@gmail.com>
2017-11-22 09:39:38 -0500

    More info in RPC errors (#9649)
    * Pipe through rpc status name to thrown error
    
    * Put code in error message
    
    * Get method in error obj
    
    * Extend global error timeout
    
    * Re-add comment
    
    * Re-type method
    
    * Review comments

--- shared/util/errors.js
+++ shared/util/errors.js
@@ -1,16 +1,36 @@
 // @flow
 
 export class RPCError extends Error {
-  code: number
+  code: number // Consult type StatusCode in flow-types.js for what this means
   fields: any
   desc: string // Don't use! This is for compatibility with RPC error object.
+  name: string
 
-  constructor(message: string, code: number, fields: ?any) {
-    super(message || 'Unknown RPC Error')
-    this.code = code
+  constructor(message: string, code: number, fields: any, name: ?string, method: ?string) {
+    super(paramsToErrorMsg(message, code, fields, name, method))
+    this.code = code // Consult type StatusCode in flow-types.js for what this means
     this.fields = fields
     this.desc = message // Don't use! This is for compatibility with RPC error object.
+    this.name = name || ''
   }
+}
+
+const paramsToErrorMsg = (
+  message: string,
+  code: number,
+  fields: any,
+  name: ?string,
+  method: ?string
+): string => {
+  let msg = ''
+  if (code) {
+    msg += `ERROR CODE ${code} - `
+  }
+  msg += message || (name && `RPC Error: ${name}`) || 'Unknown RPC Error'
+  if (method) {
+    msg += ` in method ${method}`
+  }
+  return msg
 }
 
 export class RPCTimeoutError extends Error {
@@ -29,18 +49,21 @@ export class ValidationError extends Error {}
 export class SearchError extends Error {}
 
 // convertToError converts an RPC error object (or any object) into an Error
-export function convertToError(err: Object): Error {
+export function convertToError(err: Object, method?: string): Error {
   if (err instanceof Error) {
     return err
   }
 
   if (err.hasOwnProperty('desc') && err.hasOwnProperty('code')) {
-    return convertToRPCError(err)
+    return convertToRPCError(err, method)
   }
 
   return new Error(`Unknown error: ${JSON.stringify(err)}`)
 }
 
-export function convertToRPCError(err: {code: number, desc: string, fields?: any}): RPCError {
-  return new RPCError(err.desc, err.code, err.fields)
+export function convertToRPCError(
+  err: {code: number, desc: string, fields?: any, name?: string},
+  method?: ?string
+): RPCError {
+  return new RPCError(err.desc, err.code, err.fields, err.name, method)
 }
