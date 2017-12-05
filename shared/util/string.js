feebba6a376c57cd1550f44739a513f089502b1d
Marco Munizaga <marco@marcopolo.io>
2017-12-05 10:26:43 -0800

    Print error stack

--- shared/util/string.js
+++ shared/util/string.js
@@ -4,7 +4,7 @@ function toStringForLog(a: any): string {
   if (typeof a === 'string') {
     return a
   } else if (a instanceof Error) {
-    return a.toString()
+    return a.stack
   } else if (typeof a === 'object') {
     return JSON.stringify(a)
   }
