e1fc284756d2cebdf2ecd4376124b076ec2a515e
Mike Maxim <mike.maxim@gmail.com>
2017-12-05 16:43:24 -0500

    parse out the invite token from a big message

---
+++ go/service/teams_test.go
@@ -0,0 +1,20 @@
+package service
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestParseSeitanToken(t *testing.T) {
+	token := "aazaaa0aaaaaaaaa"
+	s := fmt.Sprintf("HELLO AND WELCOME TO THIS TEAM. token: %s", token)
+	require.Equal(t, token, parseInviteToken(s))
+	s = token
+	require.Equal(t, token, parseInviteToken(s))
+	s = fmt.Sprintf("%s %s", token, token)
+	require.NotEqual(t, token, parseInviteToken(s))
+	s = "token: MIKE"
+	require.NotEqual(t, "MIKE", parseInviteToken(s))
+}
