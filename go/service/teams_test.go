3942f2a1365eef99f27b8c49407c97d2d8341326
Mike Maxim <mike.maxim@gmail.com>
2017-12-06 10:58:58 -0500

    new size

--- go/service/teams_test.go
+++ go/service/teams_test.go
@@ -8,10 +8,13 @@ import (
 )
 
 func TestParseSeitanToken(t *testing.T) {
-	token := "aazaaa0a+aaaaaaaa"
+	token := "aazaaa0a+aaaaaaaaa"
 	s := fmt.Sprintf("HELLO AND WELCOME TO THIS TEAM. token: %s", token)
 	require.Equal(t, token, parseInviteToken(s))
 	token = "aazaaa0aaaaaaaaa"
+	s = fmt.Sprintf("HELLO AND WELCOME TO THIS TEAM. token: %s", token)
+	require.Equal(t, token, parseInviteToken(s))
+	token = "aazaaa0aaaaaaaaaa"
 	s = fmt.Sprintf("HELLO AND WELCOME TO THIS TEAM. token: %s", token)
 	require.Equal(t, token, parseInviteToken(s))
 	s = token
@@ -23,5 +26,8 @@ func TestParseSeitanToken(t *testing.T) {
 	require.Equal(t, s, parseInviteToken(s))
 	token = "87zaaa0aaa1zyaaz"
 	s = fmt.Sprintf("invited to team 0123456789012345 with token: %s", token)
+	require.Equal(t, token, parseInviteToken(s))
+	token = "m947873cdbwdvtku"
+	s = fmt.Sprintf(`Please join the agot team on Keybase. Install and paste this in the "Teams" tab:  token: %s  quick install: keybase.io/_/go`, token)
 	require.Equal(t, token, parseInviteToken(s))
 }
