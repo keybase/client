d1fe9b59af90d14e83addec660a0ff34f97fe70a
chrisnojima <cnojima@keyba.se>
2017-12-06 23:55:09 -0500

    WIP

---
+++ shared/common-adapters/button-bar.js
@@ -0,0 +1,95 @@
+// @flow
+import * as React from 'react'
+import Box from './box'
+import {globalStyles, globalMargins, glamorous} from '../styles'
+import {isMobile} from '../constants/platform'
+
+// TODO test on mobile
+
+type Props =
+  | {
+      align?: 'flex-start' | 'flex-end' | 'center',
+      direction: 'row',
+      fullWidth?: boolean,
+      small?: boolean,
+    }
+  | {
+      direction: 'column',
+    }
+
+class ButtonBar extends React.PureComponent<Props> {
+  static defaultProps = {
+    align: 'center',
+    fullWidth: false,
+    small: false,
+  }
+
+  _spacing = () => {
+    if (this.props.direction === 'row' && this.props.small && !isMobile) {
+      return SmallSpacer
+    }
+
+    return BigSpacer
+  }
+
+  _surroundSpacing = () => {
+    return this.props.direction === 'column'
+  }
+
+  render() {
+    const Spacing = this._spacing()
+    const surroundSpacing = this._surroundSpacing()
+    const children = React.Children.toArray(this.props.children)
+    const childrenWithSpacing = children.reduce((arr, c, idx) => {
+      if (surroundSpacing || idx > 0) {
+        arr.push(<Spacing />)
+      }
+      arr.push(c)
+      if (surroundSpacing && idx === children.length - 1) {
+        arr.push(<Spacing />)
+      }
+      return arr
+    }, [])
+    return (
+      <Container
+        align={this.props.align}
+        direction={this.props.direction}
+        fullWidth={this.props.fullWidth}
+        small={this.props.small}
+        style={this.props.style}
+      >
+        {childrenWithSpacing}
+      </Container>
+    )
+  }
+}
+
+const BigSpacer = () => <Box style={bigSpacerStyle} />
+const bigSpacerStyle = {
+  height: 8,
+  width: 8,
+}
+const SmallSpacer = () => <Box style={smallSpacerStyle} />
+const smallSpacerStyle = {
+  height: isMobile ? 8 : 4,
+  width: isMobile ? 8 : 4,
+}
+
+const Container = glamorous(Box)(
+  {},
+  props =>
+    props.direction === 'column'
+      ? {
+          ...globalStyles.flexBoxColumn,
+          alignItems: props.fullWidth ? 'stretch' : 'center',
+        }
+      : {
+          ...globalStyles.flexBoxRow,
+          alignItems: 'center',
+          justifyContent: props.align,
+          minHeight: isMobile ? (props.small ? 64 : 72) : props.small ? 44 : 64,
+        },
+  props => props.style
+)
+
+export default ButtonBar
--- shared/common-adapters/button-bar.stories.js
+++ shared/common-adapters/button-bar.stories.js
@@ -5,10 +5,72 @@ import ButtonBar from './button-bar'
 import * as React from 'react'
 import {storiesOf, action} from '../stories/storybook'
 
-const commonProps = {}
+const commonButtonProps = {
+  backgroundMode: 'Normal',
+  disabled: false,
+  fullWidth: false,
+  label: 'Button',
+  onClick: action('onClick'),
+  onMouseEnter: action('onMouseEnter'),
+  onMouseLeave: action('onMouseLeave'),
+  onPress: action('onPress'),
+  small: false,
+  style: {alignSelf: undefined}, // button really shouldn't have this set
+  waiting: false,
+}
+
+const commonButtonBarProps = {
+  style: {
+    borderColor: 'pink',
+    borderStyle: 'solid',
+    borderWidth: 1,
+  },
+}
 
 const load = () => {
-  storiesOf('Common', module).add('ButtonBar', () => <Box style={{flex: 1}} />)
+  storiesOf('Common', module).add('ButtonBar', () => (
+    <Box
+      style={{
+        borderColor: 'grey',
+        borderStyle: 'solid',
+        borderWidth: 1,
+        flex: 1,
+        maxWidth: 600,
+        width: '100%',
+      }}
+    >
+      <ButtonBar {...commonButtonBarProps} direction="row">
+        <Button {...commonButtonProps} type="Primary" label="Button" />
+      </ButtonBar>
+      <ButtonBar {...commonButtonBarProps} direction="row">
+        <Button {...commonButtonProps} type="Secondary" />
+        <Button {...commonButtonProps} type="Primary" label="Another button" />
+      </ButtonBar>
+      <ButtonBar {...commonButtonBarProps} direction="column">
+        <Button {...commonButtonProps} type="Primary" />
+      </ButtonBar>
+      <ButtonBar {...commonButtonBarProps} direction="column">
+        <Button {...commonButtonProps} type="Secondary" />
+        <Button {...commonButtonProps} type="Primary" label="Another button" />
+      </ButtonBar>
+      <ButtonBar {...commonButtonBarProps} direction="row" align="flex-start">
+        <Button {...commonButtonProps} type="Primary" />
+      </ButtonBar>
+      <ButtonBar {...commonButtonBarProps} direction="row" align="flex-end">
+        <Button {...commonButtonProps} type="Primary" />
+      </ButtonBar>
+      <ButtonBar {...commonButtonBarProps} direction="row" small={true}>
+        <Button {...commonButtonProps} type="Primary" label="Small button" small={true} />
+      </ButtonBar>
+      <ButtonBar {...commonButtonBarProps} direction="row" small={true}>
+        <Button {...commonButtonProps} type="Secondary" label="Small button" small={true} />
+        <Button {...commonButtonProps} type="Primary" label="Small button" small={true} />
+      </ButtonBar>
+      <ButtonBar {...commonButtonBarProps} direction="column" fullWidth={true}>
+        <Button {...commonButtonProps} type="Primary" />
+      </ButtonBar>
+    </Box>
+  ))
 }
 
 export default load
