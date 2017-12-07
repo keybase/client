eb46c046feca6e6bdaea0a4c697c7e7ae55c3ee5
Jeremy Stribling <strib@alum.mit.edu>
2017-12-07 15:20:52 -0800

    kbfsfuse: update the md version for the startup script
    Used in docker tests.

--- kbfsfuse/kbfsfuse.sh
+++ kbfsfuse/kbfsfuse.sh
@@ -12,7 +12,7 @@ if [ -z "$KEYBASE_TEST_ROOT_CERT_PEM" ]; then
     export KEYBASE_TEST_ROOT_CERT_PEM="$(echo $KEYBASE_TEST_ROOT_CERT_PEM_B64 | base64 -d)";
 fi
 if [ -z "$KBFS_METADATA_VERSION" ]; then
-    export KBFS_METADATA_VERSION=3
+    export KBFS_METADATA_VERSION=4
 fi
 echo "Using KBFS metadata version $KBFS_METADATA_VERSION"
 
