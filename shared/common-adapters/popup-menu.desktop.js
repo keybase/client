8d193a0b186a087000569e329c7087257ec123eb
chrisnojima <chrisnojima@gmail.com>
2017-12-08 12:45:29 -0500

    bring back old workign poopuup for devices and git (#9833)

--- shared/common-adapters/popup-menu.desktop.js
+++ shared/common-adapters/popup-menu.desktop.js
@@ -80,6 +80,7 @@ class ModalLessPopupMenu extends Component<Props> {
 }
 
 // TODO don't use this anymore
+// TODO fix this so its not broken
 class PopupMenu extends Component<Props> {
   render() {
     return (
@@ -92,6 +93,89 @@ class PopupMenu extends Component<Props> {
           }}
         >
           <ModalLessPopupMenu {...(this.props: Props)} />
+        </Box>
+      </EscapeHandler>
+    )
+  }
+}
+
+// TEMP to just get the desktop release working. the previous change totally broke this in devices / git / etc
+class OLDPopupMenu extends Component<Props> {
+  render() {
+    const realCSS = `
+    .menu-hover:hover { background-color: ${(this.props.style && this.props.style.hoverColor) || globalColors.blue4}; }
+    .menu-hover-danger:hover { background-color: ${globalColors.red}; }
+
+    .menu-hover .title { color: ${globalColors.black_75}; }
+    .menu-hover-danger .title { color: ${globalColors.red}; }
+    .menu-hover-danger:hover .title { color: ${globalColors.white}; }
+    .menu-hover-danger .subtitle { color: ${globalColors.black_40}; }
+    .menu-hover-danger:hover .subtitle { color: ${globalColors.white}; }
+    `
+
+    return (
+      <EscapeHandler onESC={this.props.onHidden}>
+        <Box
+          style={stylesMenuCatcher}
+          onClick={e => {
+            this.props.onHidden()
+            e.stopPropagation()
+          }}
+        >
+          <style>{realCSS}</style>
+          <Box style={{...stylesMenu, ...this.props.style}}>
+            {this.props.header && this.props.header.view}
+            <Box
+              style={{
+                ...globalStyles.flexBoxColumn,
+                flexShrink: 0,
+                paddingTop: globalMargins.tiny,
+                paddingBottom: globalMargins.tiny,
+              }}
+            >
+              {this.props.items.filter(Boolean).map((i, idx) => {
+                if (i === 'Divider') {
+                  return <Divider key={idx} />
+                }
+
+                let hoverClassName
+                let styleDisabled = {}
+                if (!i.disabled) {
+                  hoverClassName = i.danger ? 'menu-hover-danger' : 'menu-hover'
+                } else {
+                  styleDisabled = {opacity: 0.4}
+                }
+
+                const styleClickable = i.disabled ? {} : globalStyles.clickable
+
+                return (
+                  <Box
+                    key={i.title}
+                    className={hoverClassName}
+                    style={{...stylesRow, ...styleClickable}}
+                    onClick={i.onClick}
+                  >
+                    <Text
+                      className="title"
+                      type="Body"
+                      style={{...stylesMenuText, ...i.style, ...styleDisabled}}
+                    >
+                      {i.title}
+                    </Text>
+                    {i.subTitle &&
+                      <Text
+                        className="subtitle"
+                        key={i.subTitle}
+                        type="BodySmall"
+                        style={{...stylesMenuText, ...i.style}}
+                      >
+                        {i.subTitle}
+                      </Text>}
+                  </Box>
+                )
+              })}
+            </Box>
+          </Box>
         </Box>
       </EscapeHandler>
     )
@@ -155,6 +239,6 @@ const stylesMenuText = {
   color: undefined,
 }
 
-export {PopupHeaderText, ModalLessPopupMenu}
+export {PopupHeaderText, ModalLessPopupMenu, OLDPopupMenu }
 
 export default PopupMenu
