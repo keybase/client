8a6f7e1eea8d012a314b0360f13549bb4457425c
chrisnojima <cnojima@keyba.se>
2017-12-11 15:36:52 -0500

    more pure in app/config/devices

--- shared/actions/app.js
+++ shared/actions/app.js
@@ -2,29 +2,29 @@
 import * as RPCTypes from '../constants/types/flow-types'
 import * as Saga from '../util/saga'
 import * as AppGen from './app-gen'
-import {call, put} from 'redux-saga/effects'
 import {showMainWindow} from './platform-specific'
 
-function* _onMobileAppStateChanged(action: AppGen.MobileAppStatePayload): Saga.SagaGenerator<any, any> {
+function _onMobileAppStateChanged(action: AppGen.MobileAppStatePayload) {
   const nextAppState = action.payload.nextAppState
 
   const appFocused = {
     active: true,
-    inactive: false,
     background: false,
+    inactive: false,
   }[nextAppState]
-
-  yield put(AppGen.createChangedFocus({appFocused}))
 
   const state =
     {
       active: RPCTypes.appStateAppState.foreground,
-      inactive: RPCTypes.appStateAppState.inactive,
       background: RPCTypes.appStateAppState.background,
+      inactive: RPCTypes.appStateAppState.inactive,
     }[nextAppState] || RPCTypes.appStateAppState.foreground
   console.log(`setting app state on service to: ${state}`)
 
-  yield call(RPCTypes.appStateUpdateAppStateRpcPromise, {state})
+  return Saga.all([
+    Saga.put(AppGen.createChangedFocus({appFocused})),
+    Saga.call(RPCTypes.appStateUpdateAppStateRpcPromise, {state}),
+  ])
 }
 
 function _onShowMain() {
@@ -32,8 +32,8 @@ function _onShowMain() {
 }
 
 function* appStateSaga(): Saga.SagaGenerator<any, any> {
-  yield Saga.safeTakeLatest(AppGen.showMain, _onShowMain)
-  yield Saga.safeTakeLatest(AppGen.mobileAppState, _onMobileAppStateChanged)
+  yield Saga.safeTakeLatestPure(AppGen.showMain, _onShowMain)
+  yield Saga.safeTakeLatestPure(AppGen.mobileAppState, _onMobileAppStateChanged)
 }
 
 export default appStateSaga
