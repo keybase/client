343135b62b79e6a9f977bec65a0c86d38c6fe9bf
chrisnojima <cnojima@keyba.se>
2017-12-14 15:39:38 -0500

    WIP

--- shared/actions/chat/inbox.js
+++ shared/actions/chat/inbox.js
@@ -989,7 +989,13 @@ function _resetChatWithoutThem(action: ChatGen.ResetChatWithoutThemPayload, stat
   }
 }
 
-function _resetLetThemIn(action: ChatGen.ResetLetThemInPayload) {}
+function _resetLetThemIn(action: ChatGen.ResetLetThemInPayload) {
+  // TODO waiting on merger from master
+  // Saga.call(RPCChatTypes.localAddTeamMemberAfterResetRpcPromise, {
+    // convID: Constants.keyToConversationID(action.payload.conversationIDKey),
+    // username: action.payload.username,
+  // })
+}
 
 function* registerSagas(): SagaGenerator<any, any> {
   yield Saga.safeTakeEveryPure(ChatGen.updateSnippet, _updateSnippet)
