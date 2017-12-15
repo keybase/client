bdde01c809299b37b98afc5468e3664300454c34
Marco Munizaga <marco@marcopolo.io>
2017-12-15 14:05:37 -0800

    Set bg in teams in tracker popup (#9940)

--- shared/tracker/index.desktop.js
+++ shared/tracker/index.desktop.js
@@ -83,6 +83,7 @@ export default class TrackerRender extends PureComponent<RenderProps> {
                 paddingLeft: globalMargins.medium,
                 paddingBottom: globalMargins.tiny,
                 paddingTop: globalMargins.tiny,
+                backgroundColor: globalColors.white,
               }}
             >
               {this.props.userInfo.showcasedTeams.map(team => (
