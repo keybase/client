3a4fad0fe24afe421735d449d5bdcc875ae79bee
Maxwell Krohn <themax@gmail.com>
2017-12-21 20:40:42 -0500

    one more modality

--- go/protocol/keybase1/extras.go
+++ go/protocol/keybase1/extras.go
@@ -1096,7 +1096,8 @@ func (b TLFIdentifyBehavior) ShouldSuppressTrackerPopups() bool {
 		TLFIdentifyBehavior_CHAT_GUI_STRICT,
 		TLFIdentifyBehavior_CHAT_CLI,
 		TLFIdentifyBehavior_KBFS_REKEY,
-		TLFIdentifyBehavior_KBFS_QR:
+		TLFIdentifyBehavior_KBFS_QR,
+		TLFIdentifyBehavior_SALTPACK:
 		// These are identifies that either happen without user interaction at
 		// all, or happen while you're staring at some Keybase UI that can
 		// report errors on its own. No popups needed.
