e25ca606a8ee46deaf3d7f5d3a95a95b5e3e5991
Daniel Ayoub <danny.a95@gmail.com>
2017-12-22 15:19:28 -0500

    add searchv3 to people pane

--- shared/people/container.js
+++ shared/people/container.js
@@ -4,6 +4,8 @@ import * as PeopleGen from '../actions/people-gen'
 import {connect} from 'react-redux'
 import {compose, lifecycle} from 'recompose'
 import {type TypedState} from '../util/container'
+import {createSearchSuggestions} from '../actions/search-gen'
+import {navigateAppend} from '../actions/route-tree'
 // import flags from '../util/feature-flags'
 
 const mapStateToProps = (state: TypedState) => ({
@@ -14,6 +16,10 @@ const mapStateToProps = (state: TypedState) => ({
 const mapDispatchToProps = (dispatch: Dispatch) => ({
   getData: () => dispatch(PeopleGen.createGetPeopleData({markViewed: true, numFollowSuggestionsWanted: 10})),
   todoDispatch: {},
+  onSearch: () => {
+    dispatch(createSearchSuggestions({searchKey: 'profileSearch'}))
+    dispatch(navigateAppend([{props: {}, selected: 'search'}]))
+  },
 })
 
 export default compose(
--- shared/people/index.shared.js
+++ shared/people/index.shared.js
@@ -2,11 +2,12 @@
 import * as React from 'react'
 import * as Types from '../constants/types/people'
 import * as I from 'immutable'
-import {Box, ScrollView} from '../common-adapters'
+import {Box, ClickableBox, Icon, ScrollView, Text} from '../common-adapters'
 import Todo from './task'
 import FollowNotification from './follow-notification'
 import {type Props} from '.'
 import {globalStyles, globalColors} from '../styles'
+import {isMobile} from '../constants/platform'
 
 const onConfirm = () => {}
 const onDismiss = () => {}
@@ -39,8 +40,53 @@ const intersperse = (list: I.List<any>, light: boolean) => {
 }
 
 export const PeoplePageContent = (props: Props) => (
-  <ScrollView style={{...globalStyles.flexBoxColumn, ...globalStyles.fullHeight, width: '100%'}}>
-    {intersperse(props.newItems.map(itemToComponent), true)}
-    {intersperse(props.oldItems.map(itemToComponent), false)}
-  </ScrollView>
+  <Box style={{...globalStyles.fullHeight}}>
+    <Box
+      style={{
+        ...globalStyles.flexBoxRow,
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 48,
+        width: '100%',
+      }}
+    >
+      <ClickableBox onClick={props.onSearch} style={{...styleSearchContainer}}>
+        <Icon style={styleSearch} type="iconfont-search" />
+        <Text style={styleSearchText} type="Body">Search people</Text>
+      </ClickableBox>
+    </Box>
+    <ScrollView style={{...globalStyles.flexBoxColumn, width: '100%'}}>
+      {intersperse(props.newItems.map(itemToComponent), true)}
+      {intersperse(props.oldItems.map(itemToComponent), false)}
+    </ScrollView>
+  </Box>
 )
+
+const styleSearchContainer = {
+  ...globalStyles.flexBoxRow,
+  ...globalStyles.clickable,
+  alignItems: 'center',
+  alignSelf: 'center',
+  backgroundColor: globalColors.white,
+  borderRadius: 100,
+  ...(isMobile
+    ? {borderColor: globalColors.black_05, borderWidth: 1}
+    : {border: `1px solid ${globalColors.black_05}`}),
+  justifyContent: 'center',
+  minHeight: isMobile ? 33 : 28,
+  width: 273,
+  zIndex: 20,
+}
+
+const styleSearch = {
+  color: globalColors.black_20,
+  fontSize: 12,
+  padding: 3,
+}
+
+const styleSearchText = {
+  ...styleSearch,
+  position: 'relative',
+  top: -1,
+  ...(isMobile ? {fontSize: 15} : {fontSize: 13}),
+}
