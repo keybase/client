f5f0e3f3e9b2c88419115a26868aaf6be2179c1c
Daniel Ayoub <danny.a95@gmail.com>
2017-12-27 17:29:48 -0500

    Todo dispatches WIP

--- shared/people/container.js
+++ shared/people/container.js
@@ -1,15 +1,20 @@
 // @flow
 import People from './'
 import * as PeopleGen from '../actions/people-gen'
+import * as Types from '../constants/types/people'
+import * as Tabs from '../constants/tabs'
 import {connect} from 'react-redux'
 import {compose, lifecycle} from 'recompose'
 import {type TypedState} from '../util/container'
 import {createSearchSuggestions} from '../actions/search-gen'
-import {navigateAppend} from '../actions/route-tree'
+import {navigateAppend, switchTo} from '../actions/route-tree'
 import {isMobile} from '../constants/platform'
 import {createShowUserProfile} from '../actions/profile-gen'
 import {createGetProfile} from '../actions/tracker-gen'
+import openURL from '../util/open-url'
 // import flags from '../util/feature-flags'
+
+const INSTALL_LINK_URL = 'https://keybase.io/download'
 
 const mapStateToProps = (state: TypedState) => ({
   newItems: state.people.newItems,
@@ -17,9 +22,33 @@ const mapStateToProps = (state: TypedState) => ({
   followSuggestions: state.people.followSuggestions,
 })
 
+const onSkipTodo = (type: Types.TodoType, dispatch: Dispatch) => () =>
+  dispatch(PeopleGen.createSkipTodo({type}))
+
 const mapDispatchToProps = (dispatch: Dispatch) => ({
   getData: () => dispatch(PeopleGen.createGetPeopleData({markViewed: true, numFollowSuggestionsWanted: 10})),
-  todoDispatch: {},
+  todoDispatch: {
+    bio: {
+      onConfirm: (username: string) => dispatch(createShowUserProfile({username})),
+      onDismiss: () => {},
+    },
+    proof: {
+      onConfirm: (username: string) => dispatch(createShowUserProfile({username})),
+      onDismiss: onSkipTodo('proof', dispatch),
+    },
+    device: {
+      onConfirm: () => openURL(INSTALL_LINK_URL),
+      onDismiss: onSkipTodo('device', dispatch),
+    },
+    follow: {
+      onConfirm: () => dispatch(navigateAppend(['search', Tabs.peopleTab])),
+      onDismiss: onSkipTodo('follow', dispatch),
+    },
+    chat: {
+      onConfirm: () => dispatch(switchTo([Tabs.chatTab])),
+      onDismiss: () => onSkipTodo('chat', dispatch),
+    },
+  },
   onSearch: () => {
     dispatch(createSearchSuggestions({searchKey: 'profileSearch'}))
     dispatch(navigateAppend([{props: {}, selected: 'search'}]))
