0f92c8fa5feb9932b76d14bd856456872945106c
Miles Steele <miles@milessteele.com>
2018-01-02 18:05:42 -0500

    important fix

--- go/protocol/chat1/local.go
+++ go/protocol/chat1/local.go
@@ -5,7 +5,6 @@ package chat1
 
 import (
 	"errors"
-
 	gregor1 "github.com/keybase/client/go/protocol/gregor1"
 	keybase1 "github.com/keybase/client/go/protocol/keybase1"
 	"github.com/keybase/go-framed-msgpack-rpc/rpc"
