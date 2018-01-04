21ce61eabb6b4e8be4d07fc3be0e7c9d34e5c6dc
Frederick Akalin <akalin@keyba.se>
2018-01-04 17:53:48 -0600

    Fix

--- shared/common-adapters/channel-container.js
+++ shared/common-adapters/channel-container.js
@@ -7,7 +7,7 @@ import * as ChatGen from '../actions/chat-gen'
 type OwnProps = {channel: string}
 
 const mapDispatchToProps = (dispatch, {channel}: OwnProps) => ({
-  onClick: dispatch(ChatGen.createSelectConversation({conversationIDKey: null, fromUser: true})),
+  onClick: () => dispatch(ChatGen.createSelectConversation({conversationIDKey: null, fromUser: true})),
 })
 
 // $FlowIssue
