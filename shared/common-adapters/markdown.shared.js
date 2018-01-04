9ebde08d4b9249672e3feb84ec923f0a4e7e4aa4
Frederick Akalin <akalin@keyba.se>
2018-01-04 15:24:34 -0600

    Hook up isValidChannel

--- shared/common-adapters/markdown.shared.js
+++ shared/common-adapters/markdown.shared.js
@@ -58,7 +58,7 @@ function isValidChannel(meta: ?MarkdownMeta, channel: string): boolean {
     return false
   }
 
-  return true
+  return !!meta.channelNames[channel]
 }
 
 export function parseMarkdown(
