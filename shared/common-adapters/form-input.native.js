339039009c20eb0e4d7525ac53cc5be1644b3848
Daniel Ayoub <danny.a95@gmail.com>
2018-01-04 18:00:36 -0500

    review comments

--- shared/common-adapters/form-input.native.js
+++ shared/common-adapters/form-input.native.js
@@ -1,7 +1,7 @@
 // @flow
 import * as React from 'react'
 import Box from './box'
-import {NativeTextInput} from './native-wrappers.native'
+import {NativeTextInput, NativeStyleSheet} from './native-wrappers.native'
 import {backgroundModeToColor, globalColors, globalMargins, globalStyles} from '../styles'
 import Text, {getStyle} from './text'
 import type {TextType, Background} from './text'
@@ -25,29 +25,21 @@ export type Props = {
 }
 
 type State = {
-  value: string,
   focused: boolean,
 }
 
 class FormInput extends React.Component<Props, State> {
-  state: State
+  static defaultProps: Props = {
+    value: '',
+  }
+  state: State = {
+    focused: false,
+  }
+
   _input: any
 
-  constructor(props: Props) {
-    super(props)
-
-    this.state = {
-      value: this.props.value || '',
-      focused: false,
-    }
-  }
-
-  getValue = (): string => {
-    return this.state.value || ''
-  }
-
   setValue = (value: string) => {
-    this.setState({value: value || ''})
+    this._onChangeText(value)
   }
 
   clearValue = () => {
@@ -67,9 +59,7 @@ class FormInput extends React.Component<Props, State> {
   }
 
   _onChangeText = (text: string) => {
-    this.setState({value: text || ''})
-
-    this.props.onChangeText && this.props.onChangeText(text || '')
+    this.props.onChangeText && this.props.onChangeText(text)
   }
 
   _onFocus = () => {
@@ -91,59 +81,32 @@ class FormInput extends React.Component<Props, State> {
   }
 
   render() {
-    let backgroundColor = this.props.backgroundMode
+    const backgroundColor = this.props.backgroundMode
       ? backgroundModeToColor[this.props.backgroundMode]
       : globalColors.white
+
     return (
       <Box
-        style={{
-          ...globalStyles.flexBoxColumn,
-          alignItems: 'center',
-          position: 'relative',
-          width: '100%',
-          borderTopWidth: this.props.hideTopBorder ? 0 : 1,
-          borderBottomWidth: this.props.hideBottomBorder ? 0 : 1,
-          borderColor: globalColors.black_10,
-          minHeight: this.props.multiline ? 128 : 64,
-          ...(this.props.maxHeight ? {maxHeight: this.props.maxHeight} : {}),
-          ...this.props.containerStyle,
-        }}
+        style={[
+          containerStyle.common,
+          this.props.hideTopBorder && containerStyle.hideTopBorder,
+          this.props.hideBottomBorder && containerStyle.hideBottomBorder,
+          this.props.multiline && containerStyle.multiline,
+          this.props.maxHeight && {maxHeight: this.props.maxHeight},
+          this.props.containerStyle,
+        ]}
       >
-        {(!!this.state.value || this.state.focused) && (
-          <Text
-            type="BodySmallSemibold"
-            style={{
-              position: 'absolute',
-              top: globalMargins.tiny,
-              left: globalMargins.small,
-              color: globalColors.blue,
-              fontSize: 11,
-              zIndex: 2,
-              backgroundColor: globalColors.transparent,
-            }}
-          >
+        {(!!this.props.value || this.state.focused) && (
+          <Text type="BodySmallSemibold" style={smallLabelStyle}>
             {this.props.label}
           </Text>
         )}
-        {this.props.label &&
-          this.props.multiline && (
-            <Box
-              style={{
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                right: 0,
-                height: 22,
-                backgroundColor: backgroundColor,
-                zIndex: 1,
-              }}
-            />
-          )}
+        {this.props.label && this.props.multiline && <Box style={{...headerBlockStyle, backgroundColor}} />}
         <NativeTextInput
           autoCorrect={this.props.autoCorrect}
           autoFocus={this.props.autoFocus}
           autoGrow={!!this.props.multiline && !!this.props.maxHeight}
-          value={this.state.value}
+          value={this.props.value}
           onChangeText={this._onChangeText}
           placeholder={this.state.focused ? '' : this.props.label}
           ref={input => (this._input = input)}
@@ -153,21 +116,72 @@ class FormInput extends React.Component<Props, State> {
           onSubmitEditing={this.props.onEnterKeyDown}
           secureTextEntry={this.props.secure}
           multiline={this.props.multiline}
-          style={{
-            flexBasis: 'auto',
-            width: '100%',
-            paddingLeft: globalMargins.small,
-            paddingRight: globalMargins.small,
-            paddingTop: this.state.value || this.state.focused ? 24 : 21,
-            marginBottom: 18,
-            ...((this.props.textType && getStyle(this.props.textType, this.props.backgroundMode)) ||
-              getStyle('BodySemibold')),
-            ...this.props.inputStyle,
-          }}
+          style={[
+            inputStyle.common,
+            (this.props.value || this.state.focused) && inputStyle.paddingTop,
+            this.props.textType && getStyle(this.props.textType, this.props.backgroundMode),
+            !this.props.textType && getStyle('BodySemibold', this.props.backgroundMode),
+            this.props.inputStyle,
+          ]}
         />
       </Box>
     )
   }
 }
+
+const containerStyle = NativeStyleSheet.create({
+  common: {
+    ...globalStyles.flexBoxColumn,
+    alignItems: 'center',
+    position: 'relative',
+    width: '100%',
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderColor: globalColors.black_10,
+    minHeight: 64,
+  },
+  hideTopBorder: {
+    borderTopWidth: 0,
+  },
+  hideBottomBorder: {
+    borderBottomWidth: 0,
+  },
+  multiline: {
+    minHeight: 128,
+  },
+})
+
+const smallLabelStyle = {
+  position: 'absolute',
+  top: globalMargins.tiny,
+  left: globalMargins.small,
+  color: globalColors.blue,
+  fontSize: 11,
+  zIndex: 2,
+  backgroundColor: globalColors.transparent,
+}
+
+const headerBlockStyle = {
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  right: 0,
+  height: 22,
+  zIndex: 1,
+}
+
+const inputStyle = NativeStyleSheet.create({
+  common: {
+    flexBasis: 'auto',
+    width: '100%',
+    paddingLeft: globalMargins.small,
+    paddingRight: globalMargins.small,
+    paddingTop: 21,
+    marginBottom: 18,
+  },
+  largePaddingTop: {
+    paddingTop: 24,
+  },
+})
 
 export {FormInput}
