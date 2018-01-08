4b826a6186108515c26b16c97de6933864177b66
Frederick Akalin <akalin@keyba.se>
2018-01-08 10:08:22 -0600

    Add double underscore test

--- shared/markdown/__test__/parser.test.js
+++ shared/markdown/__test__/parser.test.js
@@ -213,7 +213,7 @@ this is a code block with two newline above\`\`\`
   const allowAllMentions = (s: string) => true
   
   it('parses mentions with underscores correctly', () => {
-    check('hello there @ryan_singer @m_ @dan_t', {isValidMention: allowAllMentions})
+    check('hello there @ryan_singer @m_ @dan_t @a_b_c', {isValidMention: allowAllMentions})
     // This gets parsed as a mention for @invalid_ followed by the
     // text _name_.
     check('hello there @invalid__name_', {isValidMention: allowAllMentions})
