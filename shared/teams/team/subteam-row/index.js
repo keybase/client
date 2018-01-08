469a3c4b04730a7c82e10e9f17715a22ebe6b851
Chris Ball <chris@printf.net>
2018-01-07 16:44:24 -0800

    Start moving out subteam-row

---
+++ shared/teams/team/subteam-row/index.js
@@ -0,0 +1,51 @@
+// @flow
+import * as React from 'react'
+import {Avatar, Box, Button, ClickableBox, Text, Usernames} from '../../../common-adapters'
+import {globalStyles, globalMargins, isMobile} from '../../../styles'
+import {typeToLabel} from '../../../constants/teams'
+
+export type Props = {
+  label: string, // email, sbs, or seitan
+  onCancelInvite: () => void,
+  following: boolean,
+  role: string,
+  you: ?string,
+}
+
+export const SubteamRow = (props: Props) => {
+  const {following, onCancelInvite, role, you, label} = props
+  return (
+    <ClickableBox
+      style={{
+        ...globalStyles.flexBoxRow,
+        alignItems: 'center',
+        flexShrink: 0,
+        height: isMobile ? 56 : 48,
+        padding: globalMargins.tiny,
+        width: '100%',
+      }}
+    >
+      <Box style={{...globalStyles.flexBoxRow, flexGrow: 1, alignItems: 'center'}}>
+        <Avatar username={label} size={isMobile ? 48 : 32} />
+        <Box style={{...globalStyles.flexBoxColumn, marginLeft: globalMargins.small}}>
+          <Usernames
+            type="BodySemibold"
+            colorFollowing={true}
+            users={[{following, username: label, you: you === label}]}
+          />
+          <Box style={globalStyles.flexBoxRow}>
+            <Text type="BodySmall">{role && typeToLabel[role]}</Text>
+          </Box>
+        </Box>
+      </Box>
+      <Box style={{...globalStyles.flexBoxRow, flexShrink: 1}}>
+        <Button
+          small={true}
+          label={isMobile ? 'Cancel' : 'Cancel invite'}
+          onClick={onCancelInvite}
+          type="Secondary"
+        />
+      </Box>
+    </ClickableBox>
+  )
+}
