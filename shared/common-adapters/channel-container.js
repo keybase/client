4f45242f8d3a6cca7708f9a0fbfc82e549d6732a
Frederick Akalin <akalin@keyba.se>
2018-01-09 11:52:14 -0800

    Got channel previewing working

--- shared/common-adapters/channel-container.js
+++ shared/common-adapters/channel-container.js
@@ -1,13 +1,31 @@
 // @flow
 import Channel from './channel'
-import {connect} from '../util/container'
+import {connect, type TypedState} from '../util/container'
 import {type ConversationIDKey} from '../constants/types/chat'
 import * as ChatGen from '../actions/chat-gen'
+import {pathSelector} from '../actions/route-tree'
 
 type OwnProps = {channel: string, convID: ConversationIDKey}
 
+const mapStateToProps = (state: TypedState) => {
+  return {
+    currentPath: pathSelector(state),
+  }
+}
+
 const mapDispatchToProps = (dispatch, {convID}: OwnProps) => ({
-  onClick: () => dispatch(ChatGen.createSelectConversation({conversationIDKey: convID, fromUser: true})),
+  onClick: currentPath =>
+    dispatch(
+      ChatGen.createSelectOrPreviewConversation({conversationIDKey: convID, previousPath: currentPath})
+    ),
 })
 
-export default connect(null, mapDispatchToProps)(Channel)
+const mergeProps = ({currentPath}, {onClick}, ownProps: OwnProps) => {
+  return {
+    ...ownProps,
+    onClick: () => onClick(currentPath),
+  }
+}
+
+// $FlowIssue
+export default connect(mapStateToProps, mapDispatchToProps, mergeProps)(Channel)
