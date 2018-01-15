82865e26085a91d15c34cc8b049b7c0170a1004f
cecileboucheron <cecile@keyba.se>
2018-01-15 12:34:21 -0800

    Made team title selectionable

--- shared/teams/team/index.js
+++ shared/teams/team/index.js
@@ -626,7 +626,7 @@ class Team extends React.PureComponent<Props> {
         )}
         <Box style={stylesTeamHeader}>
           <Avatar isTeam={true} teamname={name} size={64} />
-          <Text type="HeaderBig" style={{marginTop: globalMargins.tiny}}>
+          <Text type="HeaderBig" style={{...globalStyles.selectable, marginTop: globalMargins.tiny}}>
             {name}
           </Text>
           <Text type="BodySmall">TEAM</Text>
