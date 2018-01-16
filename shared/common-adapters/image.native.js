f61598ae31db13b9f7a4e11ae795a141a93e5d03
Daniel Ayoub <danny.a95@gmail.com>
2018-01-16 16:23:14 -0500

    add resizeMode

--- shared/common-adapters/image.native.js
+++ shared/common-adapters/image.native.js
@@ -5,5 +5,5 @@ import type {Props} from './image'
 
 export default ({src, style}: Props) => {
   const source = {uri: `file://${src}`}
-  return <NativeImage source={source} style={style} />
+  return <NativeImage source={source} style={{resizeMode: 'contain', ...style}} />
 }
