14c17695b3d16baa88cb45173f341b0e32538c9f
Steve Sanders <steve@keyba.se>
2018-01-17 14:17:41 -0800

    Revert "make a new log level for stuf we really want to see"
    This reverts commit 1a444d70f1a6f0b9dad029f7955f82ff7dbfe757.

--- shared/actions/kbfs.desktop.js
+++ shared/actions/kbfs.desktop.js
@@ -194,7 +194,7 @@ function findKeybaseUninstallString(): Promise<string> {
         }
       })
     } catch (err) {
-      logger.error('findKeybaseUninstallString caught', err)
+      logger.info('findKeybaseUninstallString caught', err)
     }
   })
 }
@@ -207,7 +207,8 @@ function installCachedDokan(): Promise<*> {
     modifyCommand =>
       new Promise((resolve, reject) => {
         if (modifyCommand) {
-          logger.record('Invoking repair to add driver: ' + modifyCommand)
+          // use the action logger so it has a chance of making it into the upload
+          logger.action('Invoking repair to add driver: ' + modifyCommand)
           execFile(modifyCommand, [
             '/modify',
             'driver=1',
--- shared/logger/index.js
+++ shared/logger/index.js
@@ -42,13 +42,11 @@ class AggregateLoggerImpl implements AggregateLogger {
   _info: Logger
   _action: Logger
   _debug: Logger
-  _record: Logger
   error: LogFn
   warn: LogFn
   info: LogFn
   action: LogFn
   debug: LogFn
-  record: LogFn
   _allLoggers: {[key: LogLevel]: Logger}
 
   constructor({
@@ -57,21 +55,18 @@ class AggregateLoggerImpl implements AggregateLogger {
     info,
     action,
     debug,
-    record,
   }: {
     error: Logger,
     warn: Logger,
     info: Logger,
     action: Logger,
     debug: Logger,
-    record: Logger,
   }) {
     this._error = error
     this._warn = warn
     this._info = info
     this._action = action
     this._debug = debug
-    this._record = record
 
     this._allLoggers = {
       Error: error,
@@ -79,7 +74,6 @@ class AggregateLoggerImpl implements AggregateLogger {
       Info: info,
       Action: action,
       Debug: debug,
-      Record: record,
     }
 
     this.error = error.log
@@ -87,7 +81,6 @@ class AggregateLoggerImpl implements AggregateLogger {
     this.info = info.log
     this.action = action.log
     this.debug = debug.log
-    this.record = record.log
 
     const olderThanMs = 1e3 * 60 * 60 * 24 // 24 hours
     if (!__STORYBOOK__) {
@@ -124,7 +117,6 @@ const devLoggers = () => ({
   error: new ConsoleLogger('error'),
   info: new ConsoleLogger('log'),
   warn: new ConsoleLogger('warn'),
-  record: new ConsoleLogger('log'),
 })
 
 const prodLoggers = () => ({
@@ -137,9 +129,6 @@ const prodLoggers = () => ({
     : new DumpPeriodicallyLogger(new RingLogger(1000), 1 * 60e3, writeLogLinesToFile, 'Error'),
   info: new RingLogger(1000),
   warn: new RingLogger(1000),
-  record: isMobile
-    ? new NativeLogger()
-    : new DumpPeriodicallyLogger(new RingLogger(5000), 1 * 60e3, writeLogLinesToFile, 'Record'),
 })
 
 // Settings
