31f7fee76de261ea81d7eb048d64be3f9943bba3
Daniel Ayoub <danny.a95@gmail.com>
2018-01-18 15:30:28 -0500

    Add naive bordering to people items (#10230)
    * Add naive bordering to people items
    
    * don't alwaysBounceHorizontal
    
    * update storyshots

--- shared/people/item/index.js
+++ shared/people/item/index.js
@@ -46,6 +46,7 @@ export default (props: Props) => (
       position: 'relative',
       borderBottomWidth: 1,
       borderBottomColor: props.badged ? globalColors.white : globalColors.black_05,
+      borderBottomStyle: 'solid',
     }}
   >
     <Box style={{marginRight: 20, width: isMobile ? 48 : 32}}>{props.icon}</Box>
