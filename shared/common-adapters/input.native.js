6852dbf6f894211b9c545bbdb18db7f5bdde3623
Daniel Ayoub <danny.a95@gmail.com>
2018-01-24 16:22:22 -0500

    add render and onselectionchange to native input

--- shared/common-adapters/input.native.js
+++ shared/common-adapters/input.native.js
@@ -14,6 +14,7 @@ type State = {
   focused: boolean,
   height: ?number,
   value: string,
+  selections: {selectionStart: number, selectionEnd: number},
 }
 
 class Input extends Component<Props, State> {
@@ -27,6 +28,7 @@ class Input extends Component<Props, State> {
       focused: false,
       height: null,
       value: props.value || '',
+      selections: {selectionStart: 0, selectionEnd: 0},
     }
   }
 
@@ -166,6 +168,23 @@ class Input extends Component<Props, State> {
         }
   }
 
+  _onSelectionChange(event: any) {
+    this.setState(
+      {
+        selections: {
+          selectionStart: event.nativeEvent.selection.start,
+          selectionEnd: event.nativeEvent.selection.end,
+        },
+      },
+      () => this.props.onSelectionChange && this.props.onSelectionChange(this.state.selections)
+    )
+  }
+
+  // WARNING may not be up to date in time sensitive situations
+  selections() {
+    return this.state.selections
+  }
+
   render() {
     const underlineColor = this._underlineColor()
     const lineHeight = this._lineHeight()
@@ -224,6 +243,7 @@ class Input extends Component<Props, State> {
       onChangeText: this._onChangeText,
       onFocus: this._onFocus,
       onKeyDown: this._onKeyDown,
+      onSelectionChange: this._onSelectionChange,
       onSubmitEditing: this.props.onEnterKeyDown,
       onEndEditing: this.props.onEndEditing,
       placeholder: this.props.hintText,
