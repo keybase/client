0fd0aed6b92bffa8dc506942ab560f9b01287528
Daniel Ayoub <danny.a95@gmail.com>
2018-01-25 17:31:28 -0500

    get rid of storybook var check

--- shared/dev/storybook-connect.js
+++ shared/dev/storybook-connect.js
@@ -2,20 +2,16 @@
 // monkey-patch connect
 import {connectAdvanced} from 'react-redux'
 
-const noop = () => {}
-
-const _infect = __STORYBOOK__
-  ? () => {
-      const redux = require('react-redux')
-      const selectorDelegatorFactory = (dispatch, options) => {
-        const name = options.wrappedComponentName
-        return (state, ownProps) => {
-          return state[name](ownProps)
-        }
-      }
-      const connect = (_, __, ___) => connectAdvanced(selectorDelegatorFactory)
-      redux.connect = connect
+const _infect = () => {
+  const redux = require('react-redux')
+  const selectorDelegatorFactory = (dispatch, options) => {
+    const name = options.wrappedComponentName
+    return (state, ownProps) => {
+      return state[name](ownProps)
     }
-  : noop
+  }
+  const connect = (_, __, ___) => connectAdvanced(selectorDelegatorFactory)
+  redux.connect = connect
+}
 
 _infect()
