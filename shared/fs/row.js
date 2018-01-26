333be842bf26bead128ad7aa2a5db436409e70a8
chrisnojima <chrisnojima@gmail.com>
2018-01-26 14:00:39 -0500

    flow fix and some small stuff (#10340)
    * flow fix and some small stuff
    
    * remove leftover

--- shared/fs/row.js
+++ shared/fs/row.js
@@ -3,44 +3,45 @@ import * as Types from '../constants/types/fs'
 import {connect, type TypedState, type Dispatch} from '../util/container'
 import {isMobile} from '../constants/platform'
 import {navigateAppend} from '../actions/route-tree'
+import type {IconType} from '../common-adapters/icon'
 
 type OwnProps = {
   path: Types.Path,
 }
 
-type StateProps = {
-  type: Types.PathType,
-}
-
 type DispatchProps = {
-  _onViewFolder: Types.Path => void,
-  _onViewFile: Types.Path => void,
+  _onOpen: (type: Types.PathType, path: Types.Path) => void,
 }
 
-const mapStateToProps = ({fs}: TypedState, {path}: OwnProps) => ({
-  type: Types.stringToPathType(fs.pathItems.getIn([path, 'type'], '')),
+const mapStateToProps = (state: TypedState, {path}: OwnProps) => {
+  const pathItem = state.fs.pathItems.get(path)
+  return {path, type: pathItem ? pathItem.type : 'unknown'}
+}
+
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  _onOpen: (type: Types.PathType, path: Types.Path) => {
+    if (type === 'folder') {
+      dispatch(navigateAppend([{props: {path}, selected: 'folder'}]))
+    } else {
+      console.log('Cannot view files yet. Requested file: ' + Types.pathToString(path))
+    }
+  },
 })
 
-const mapDispatchToProps = (dispatch: Dispatch) => {
-  return {
-    _onViewFile: (path: Types.Path) =>
-      console.log('Cannot view files yet. Requested file: ' + Types.pathToString(path)),
-    _onViewFolder: (path: Types.Path) => dispatch(navigateAppend([{props: {path}, selected: 'folder'}])),
-  }
+const iconMap = {
+  exec: isMobile ? 'icon-file-24' : 'icon-file-24',
+  file: isMobile ? 'icon-file-24' : 'icon-file-24',
+  folder: isMobile ? 'icon-folder-private-24' : 'icon-folder-private-24',
+  symlink: isMobile ? 'icon-file-24' : 'icon-file-24',
 }
 
-const mergeProps = ({type}: StateProps, {_onViewFolder, _onViewFile}: DispatchProps, {path}: OwnProps) => {
-  const icon = {
-    exec: isMobile ? 'icon-file-24' : 'icon-file-24',
-    file: isMobile ? 'icon-file-24' : 'icon-file-24',
-    folder: isMobile ? 'icon-folder-private-24' : 'icon-folder-private-24',
-    symlink: isMobile ? 'icon-file-24' : 'icon-file-24',
-  }[Types.pathTypeToString(type)]
+const mergeProps = ({type, path}, {_onOpen}: DispatchProps) => {
+  const icon: IconType = iconMap[Types.pathTypeToString(type)]
 
   return {
     icon,
     name: Types.getPathName(path),
-    onOpen: () => (type === 'folder' ? _onViewFolder(path) : _onViewFile(path)),
+    onOpen: () => _onOpen(type, path),
     path,
   }
 }
