1628a596568af6679897a95bd4e5b5b1f9ec3d5a
Daniel Ayoub <danny.a95@gmail.com>
2018-01-26 09:53:44 -0500

    Create shared storybook & add error boundary

--- shared/stories/storybook.native.js
+++ shared/stories/storybook.native.js
@@ -2,13 +2,6 @@
 /* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
 import {storiesOf} from '@storybook/react-native'
 import {action} from '@storybook/addon-actions'
-import * as React from 'react'
-import {Provider} from 'react-redux'
-import {createStore} from 'redux'
-import {type SelectorMap} from './storybook'
-
-const createPropProvider = (map: SelectorMap) => (story: () => React.Node) => (
-  <Provider store={createStore(state => state, map)}>{story()}</Provider>
-)
+import {createPropProvider} from './storybook.shared'
 
 export {action, storiesOf, createPropProvider}
