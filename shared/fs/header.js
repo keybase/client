2450089c53d42fbf4afbd2accaeb06dfceef7a89
John Zila <john@keyba.se>
2018-02-05 18:07:02 -0800

    fs: Fix header so it displays correctly.

--- shared/fs/header.js
+++ shared/fs/header.js
@@ -2,7 +2,7 @@
 import * as React from 'react'
 import * as Types from '../constants/types/fs'
 import {globalStyles, globalColors, globalMargins, isMobile} from '../styles'
-import {Box, Icon, List, Text} from '../common-adapters'
+import {Box, Icon, Text} from '../common-adapters'
 
 type FolderHeaderProps = {
   path: Types.Path,
@@ -11,9 +11,9 @@ type FolderHeaderProps = {
 const stylesCommonRow = {
   ...globalStyles.flexBoxRow,
   alignItems: 'center',
-  borderBottomWidth: 0,
-  justifyContent: 'center',
-  minHeight: isMobile ? 64 : 40,
+  borderBottomWidth: 1,
+  borderBottomColor: globalColors.black_05,
+  minHeight: isMobile ? 64 : 48,
 }
 
 const folderHeaderStyleRoot = {
@@ -25,9 +25,15 @@ const folderHeaderStyleRoot = {
 const folderHeaderStyleTree = {
   ...stylesCommonRow,
   alignItems: 'center',
-  justifyContent: 'left',
   paddingLeft: 16,
   paddingRight: 16,
+}
+
+const folderBreadcrumbStyle = {
+  ...globalStyles.flexBoxRow,
+  paddingLeft: 0,
+  paddingRight: 0,
+  alignItems: 'end',
 }
 
 const rootHeaderContent = (
@@ -40,8 +46,43 @@ type FolderHeaderState = {
   pathElems: Array<string>,
 }
 
+type FolderBreadcrumbProps = {
+  index: number,
+  totalCrumbs: number,
+  visibility: Types.Visibility,
+  item: string,
+}
+
 const headerPathStyleParent = {color: globalColors.black_60}
-const iconStyle = {marginRight: globalMargins.small}
+const iconStyle = {
+  fontSize: 11,
+  marginLeft: globalMargins.xtiny,
+  marginRight: globalMargins.xtiny,
+}
+
+const FolderHeaderBreadcrumb = ({index, totalCrumbs, visibility, item}: FolderBreadcrumbProps) => {
+  let separator = <Icon type="iconfont-back" style={iconStyle} />
+  if (index === 0) {
+    separator = null
+  }
+  if (index === totalCrumbs - 1) {
+    return (
+      <Box style={folderBreadcrumbStyle}>
+        {separator}
+        <Text type="BodyBig">{item}</Text>
+      </Box>
+    )
+  } else {
+    return (
+      <Box style={folderBreadcrumbStyle}>
+        {separator}
+        <Text type="BodySmallSemibold" style={headerPathStyleParent}>
+          {item}
+        </Text>
+      </Box>
+    )
+  }
+}
 
 class FolderHeader extends React.Component<FolderHeaderProps, FolderHeaderState> {
   state: FolderHeaderState
@@ -57,49 +98,36 @@ class FolderHeader extends React.Component<FolderHeaderProps, FolderHeaderState>
     this.setState(prevState => ({pathElems: Types.getPathElements(nextProps.path)}))
   }
 
-  _renderItem = (index, item) => {
+  _createBreadcrumbs = () => {
     switch (this.state.pathElems.length) {
+      case 1:
+        return rootHeaderContent
       case 2:
       // {private, public, team}
       // fallthrough
       case 3:
         // TLF
-        let separator = <Icon type="iconfont-back" style={{...iconStyle}} />
-        if (index === 0) {
-          separator = null
-        }
-        if (index === this.state.pathElems.length - 1) {
-          return (
-            <Box>
-              {separator}
-              <Text type="BodyBig">{item}</Text>
-            </Box>
-          )
-        } else {
-          return (
-            <Box>
-              {separator}
-              <Text type="BodySmallSemibold" style={{...headerPathStyleParent}}>
-                {item}
-              </Text>
-            </Box>
-          )
-        }
+        const visibility = Types.getPathVisibility(this.props.path)
+        return (
+          <Box style={folderHeaderStyleTree}>
+            {this.state.pathElems.map((elem, index, pathElems) => (
+              <FolderHeaderBreadcrumb
+                key={index}
+                index={index}
+                visibility={visibility}
+                item={elem}
+                totalCrumbs={pathElems.length}
+              />
+            ))}
+          </Box>
+        )
       default:
-      // Subfolder within TLF
+      // TODO: fix with list
     }
   }
 
   render() {
-    let content = rootHeaderContent
-    if (this.state.pathElems.length > 1) {
-      content = (
-        <Box style={folderHeaderStyleTree}>
-          <List items={this.state.pathElems} renderItem={this._renderItem} />
-        </Box>
-      )
-    }
-    return <Box>{content}</Box>
+    return <Box>{this._createBreadcrumbs()}</Box>
   }
 }
 
