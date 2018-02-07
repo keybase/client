9f947f622f2e2daf48013aefb542ca82858c35e0
John Zila <john@keyba.se>
2018-02-06 16:58:43 -0800

    fs: Make navigation breadcrumbs work

--- shared/fs/header-container.js
+++ shared/fs/header-container.js
@@ -3,11 +3,14 @@ import * as Types from '../constants/types/fs'
 import {connect, type Dispatch} from '../util/container'
 import {navigateAppend} from '../actions/route-tree'
 
-const mapStateToProps = (state, ownProps) => ownProps
+type OwnProps = {
+  path: Types.Path,
+}
 
-const mapDispatchToProps = (dispatch: Dispatch, ownProps) => ({
-  onOpen: (path: Types.Path) => dispatch(navigateAppend([{props: {path}, selected: 'folder'}])),
-  ...ownProps,
+const mapStateToProps = (state, ownProps: OwnProps) => ownProps
+
+const mapDispatchToProps = (dispatch: Dispatch, {path}: OwnProps) => ({
+  onOpen: () => dispatch(navigateAppend([{props: {path}, selected: 'folder'}])),
 })
 
 const FolderHeaderBreadcrumbConnector = connect(mapStateToProps, mapDispatchToProps)
