9051994d3456e0addc8b47d794987675b01a10ca
John Zila <john@keyba.se>
2018-02-07 18:48:26 -0800

    fs: Try to fix storybook.

--- shared/fs/index.stories.js
+++ shared/fs/index.stories.js
@@ -2,17 +2,32 @@
 import React from 'react'
 import * as Types from '../constants/types/fs'
 import {Box} from '../common-adapters'
-import {storiesOf} from '../stories/storybook'
+import {action, storiesOf, createPropProvider} from '../stories/storybook'
 import Files from '.'
 
+const provider = createPropProvider({
+  FolderHeader: () => ({
+    isTeamPath: false,
+    items: [
+      {
+        name: 'keybase',
+        path: '/keybase',
+      },
+    ],
+    onOpenBreadcrumb: action('onOpenBreadcrumb'),
+  }),
+})
+
 const load = () => {
-  storiesOf('Files', module).add('Root', () => (
-    <Box style={{width: '100%'}}>
-      <Files path={Types.stringToPath('/keybase')} items={[]} onBack={() => {}} />
-      <Files path={Types.stringToPath('/keybase/private')} items={[]} onBack={() => {}} />
-      <Files path={Types.stringToPath('/keybase/public')} items={[]} onBack={() => {}} />
-    </Box>
-  ))
+  storiesOf('Files', module)
+    .addDecorator(provider)
+    .add('Root', () => (
+      <Box style={{width: '100%'}}>
+        <Files path={Types.stringToPath('/keybase')} items={[]} onBack={() => {}} />
+        <Files path={Types.stringToPath('/keybase/private')} items={[]} onBack={() => {}} />
+        <Files path={Types.stringToPath('/keybase/public')} items={[]} onBack={() => {}} />
+      </Box>
+    ))
 }
 
 export default load
