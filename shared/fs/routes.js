e3e56e5796d900096085c37fd440955699d727b9
John Zila <john@keyba.se>
2018-02-15 12:53:32 -0800

    fs: Make CircleCI pass

--- shared/fs/routes.js
+++ shared/fs/routes.js
@@ -1,15 +1,18 @@
 // @flow
+import * as React from 'react'
 import * as I from 'immutable'
 import Files from './container'
 import ConnectedDropdownPopupMenu from './header/popup-container'
 import RelativePopupHoc from '../common-adapters/relative-popup-hoc.desktop'
 import {makeRouteDefNode, makeLeafTags} from '../route-tree'
+import {isMobile} from '../constants/platform'
+import {Box} from '../common-adapters'
 
 const folderRoute = makeRouteDefNode({
   children: {
     folder: () => folderRoute,
     breadcrumbAction: {
-      component: RelativePopupHoc(ConnectedDropdownPopupMenu),
+      component: isMobile ? <Box /> : RelativePopupHoc(ConnectedDropdownPopupMenu),
       tags: makeLeafTags({layerOnTop: true}),
     },
   },
