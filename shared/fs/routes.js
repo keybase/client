3a96909462ca1b67ed402b226a82a115f9e98013
John Zila <john@keyba.se>
2018-02-15 14:14:17 -0800

    fs: Actually fix routes.

--- shared/fs/routes.js
+++ shared/fs/routes.js
@@ -1,18 +1,15 @@
 // @flow
-import * as React from 'react'
 import * as I from 'immutable'
 import Files from './container'
 import ConnectedDropdownPopupMenu from './header/popup-container'
-import RelativePopupHoc from '../common-adapters/relative-popup-hoc.desktop'
+import RelativePopupHoc from '../common-adapters/relative-popup-hoc'
 import {makeRouteDefNode, makeLeafTags} from '../route-tree'
-import {isMobile} from '../constants/platform'
-import {Box} from '../common-adapters'
 
 const folderRoute = makeRouteDefNode({
   children: {
     folder: () => folderRoute,
     breadcrumbAction: {
-      component: isMobile ? <Box /> : RelativePopupHoc(ConnectedDropdownPopupMenu),
+      component: RelativePopupHoc(ConnectedDropdownPopupMenu),
       tags: makeLeafTags({layerOnTop: true}),
     },
   },
