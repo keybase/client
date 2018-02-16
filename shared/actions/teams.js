fe33a9826f2adf67dfb8f6ba68ceb62688ea4a5b
Daniel Ayoub <danny.a95@gmail.com>
2018-02-16 17:29:56 -0500

    dismiss badges for teams we're no longer in

--- shared/actions/teams.js
+++ shared/actions/teams.js
@@ -5,6 +5,7 @@ import keyBy from 'lodash/keyBy'
 import last from 'lodash/last'
 import * as I from 'immutable'
 import * as TeamsGen from './teams-gen'
+import * as Types from '../constants/types/teams'
 import * as Constants from '../constants/teams'
 import * as ChatConstants from '../constants/chat'
 import * as ChatTypes from '../constants/types/chat'
@@ -500,11 +501,26 @@ const _getTeams = function*(action: TeamsGen.GetTeamsPayload): Saga.SagaGenerato
       teamNameToIsOpen[team.fqName] = team.isOpenTeam
     })
 
+    // Dismiss any stale badges for teams we're no longer in
+    const teamResetUsers = state.entities.getIn(['teams', 'teamNameToResetUsers'], I.Map())
+    const teamNameSet = I.Set(teamnames)
+    const dismissIDs = teamResetUsers.reduce((ids, value: I.Set<Types.ResetUser>, key: string) => {
+      if (!teamNameSet.has(key)) {
+        ids.push(...value.toArray().map(ru => ru.badgeIDKey))
+      }
+      return ids
+    }, [])
+    yield Saga.all(
+      dismissIDs.map(id =>
+        Saga.call(RPCTypes.gregorDismissItemRpcPromise, {id: Constants.keyToResetUserBadgeID(id)})
+      )
+    )
+
     yield Saga.put(
       replaceEntity(
         ['teams'],
         I.Map({
-          teamnames: I.Set(teamnames),
+          teamnames: teamNameSet,
           teammembercounts: I.Map(teammembercounts),
           teamNameToIsOpen: I.Map(teamNameToIsOpen),
           teamNameToRole: I.Map(teamNameToRole),
