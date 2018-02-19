52ebbaa50599ee7e90a3465a61efd5b912c2f948
Patrick Crosby <pcrosby@gmail.com>
2018-02-19 15:27:53 -0600

    Add SecretStoreMem

--- go/libkb/secret_store.go
+++ go/libkb/secret_store.go
@@ -28,8 +28,8 @@ type SecretStoreAll interface {
 	StoreSecret(username NormalizedUsername, secret LKSecFullSecret) error
 	ClearSecret(username NormalizedUsername) error
 	GetUsersWithStoredSecrets() ([]string, error)
-	GetApprovalPrompt() string
-	GetTerminalPrompt() string
+	// GetApprovalPrompt() string
+	// GetTerminalPrompt() string
 }
 
 type SecretStoreContext interface {
