83f0a6ea216cd56a58e6edd584f72e4ada4d8fe5
Daniel Ayoub <danny.a95@gmail.com>
2018-02-20 11:53:15 -0500

    Detect whether push notifications are enabled or not (#10577)
    * detect whether we've shown the native dialog and re-show the monster if we have. still need to badge settings and get rid of monster yes / no local storage
    
    * add logging and store whether we have permissions in the store
    
    * calculate settings badge action
    
    * ad-hoc badge tab action in notifications
    
    * check notification settings on app focus
    
    * don't badge the tab itself
    
    * typo
    
    * saga all
    
    * get rid of allowPush local storage
    
    * only check permissions on iOS
    
    * Change notifications settings on mobile if notifications are disabled (#10580)
    
    * monster on notifications screen
    
    * overflow
    
    * make sure we never hide settings on desktop
    
    * badge notifications item
    
    * src to const
    
    * better require and standardize saga name

--- shared/settings/notifications/index.native.js
+++ shared/settings/notifications/index.native.js
@@ -3,11 +3,13 @@ import * as React from 'react'
 import {HeaderHoc, NativeScrollView} from '../../common-adapters/index.native'
 import {globalStyles} from '../../styles'
 import Notifications from './index.js'
+import TurnOnNotifications from './turn-on-notifications.native'
 
 import type {Props} from './index'
 
 const MobileNotifications = (props: Props) => (
   <NativeScrollView style={{...globalStyles.flexBoxColumn, flex: 1}}>
+    {!props.mobileHasPermissions && <TurnOnNotifications />}
     <Notifications {...props} />
   </NativeScrollView>
 )
