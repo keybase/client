406972ce2a6df37cd8bf8d7054de7bca6850d379
Fred Akalin <akalin@keyba.se>
2018-03-01 13:14:31 -0800

    Bundle any existing trace files during a log send (#10651)
    Put it in a single .tar.gz to upload to the server.

---
+++ go/libkb/pprof.go
@@ -0,0 +1,39 @@
+// Copyright 2018 Keybase, Inc. All rights reserved. Use of
+// this source code is governed by the included BSD license.
+
+package libkb
+
+import (
+	"fmt"
+	"path/filepath"
+	"sort"
+	"time"
+)
+
+// The number of trace files to keep around and to bundle with sent
+// logs.
+const MaxTraceFileCount = 5
+
+// Return a filename to use for a trace file in the given directory
+// with the given start time and duration.
+func MakeTraceFilename(dir string, start time.Time, duration time.Duration) string {
+	// Copied from oldLogFileTimeRangeTimeLayout from
+	// logger/file.go. Chosen so that lexicographic sorting
+	// approximately sorts by increasing start time; time zones
+	// prevent it from being an exact sorting.
+	startStr := start.Format("20060102T150405Z0700")
+	filename := fmt.Sprintf("trace.%s.%s.out", startStr, duration)
+	return filepath.Join(dir, filename)
+}
+
+// Return all trace files in the given directory approximately sorted
+// by increasing start time.
+func GetSortedTraceFiles(dir string) ([]string, error) {
+	pattern := filepath.Join(dir, "trace.*.out")
+	matches, err := filepath.Glob(pattern)
+	if err != nil {
+		return nil, err
+	}
+	sort.Strings(matches)
+	return matches, nil
+}
--- shared/react-native/ios/Keybase/LogSend.m
+++ shared/react-native/ios/Keybase/LogSend.m
@@ -11,13 +11,14 @@ RCT_REMAP_METHOD(logSend,
                  feedback:(NSString*)feedback
                  sendLogs:(BOOL)sendLogs
                  logPath:(NSString*)logPath
+                 traceDir:(NSString*)traceDir
                  resolver:(RCTPromiseResolveBlock)resolve
                  rejecter:(RCTPromiseRejectBlock)reject)
 {
 
   NSString *logId = nil;
   NSError *err = nil;
-  logId = KeybaseLogSend(status, feedback, sendLogs, logPath, &err);
+  logId = KeybaseLogSend(status, feedback, sendLogs, logPath, traceDir, &err);
   if (err == nil) {
     resolve(logId);
   } else {
