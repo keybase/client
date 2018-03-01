ec06ecbc756294da9f70056d084964b35cbf05b9
Daniel Ayoub <danny.a95@gmail.com>
2018-03-01 17:10:43 -0500

    beef up desktop rings till we have for-sure logging on desktop

--- shared/logger/index.js
+++ shared/logger/index.js
@@ -125,13 +125,13 @@ const prodLoggers = () => ({
   debug: new RingLogger(500),
   error: isMobile
     ? new NativeLogger()
-    : new DumpPeriodicallyLogger(new RingLogger(500), 1 * 60e3, writeLogLinesToFile, 'Error'),
+    : new DumpPeriodicallyLogger(new RingLogger(10000), 1 * 60e3, writeLogLinesToFile, 'Error'),
   info: isMobile
     ? new NativeLogger()
-    : new DumpPeriodicallyLogger(new RingLogger(500), 1 * 60e3, writeLogLinesToFile, 'Info'),
+    : new DumpPeriodicallyLogger(new RingLogger(1000), 1 * 60e3, writeLogLinesToFile, 'Info'),
   warn: isMobile
     ? new NativeLogger()
-    : new DumpPeriodicallyLogger(new RingLogger(500), 1 * 60e3, writeLogLinesToFile, 'Warn'),
+    : new DumpPeriodicallyLogger(new RingLogger(10000), 1 * 60e3, writeLogLinesToFile, 'Warn'),
 })
 
 // Settings
