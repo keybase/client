6e77c1103fb658382cb5f35098b84994c252d0c3
Joshua Blum <jblum18@gmail.com>
2018-03-02 12:28:14 -0500

    Fix TestTeamBustCache

--- go/systests/teams_test.go
+++ go/systests/teams_test.go
@@ -47,9 +47,7 @@ func TestTeamBustCache(t *testing.T) {
 	tt.users[1].addTeamMember(team, tt.users[2].username, keybase1.TeamRole_WRITER)
 
 	// Poll for an update, we should get it as soon as gregor tells us to bust our cache.
-	backoff := 100 * time.Millisecond
-	found := false
-	for i := 0; i < 10; i++ {
+	pollForTrue(t, tt.users[0].tc.G, func(i int) bool {
 		after, err := teams.Load(context.TODO(), tt.users[0].tc.G, keybase1.LoadTeamArg{
 			Name:    team,
 			StaleOK: true,
@@ -57,14 +55,10 @@ func TestTeamBustCache(t *testing.T) {
 		require.NoError(t, err)
 		if after.CurrentSeqno() > beforeSeqno {
 			t.Logf("Found new seqno %d at poll loop iter %d", after.CurrentSeqno(), i)
-			found = true
-			break
+			return true
 		}
-		t.Logf("Still at old generation %d at poll loop iter %d", beforeSeqno, i)
-		time.Sleep(backoff)
-		backoff += backoff / 2
-	}
-	require.True(t, found)
+		return false
+	})
 }
 
 func TestTeamRotateOnRevoke(t *testing.T) {
