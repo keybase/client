72a64b3452e21a83230f87aabf149165e8b09494
Frederick Akalin <akalin@keyba.se>
2018-03-05 17:17:20 -0800

    Add cpu profile scaffolding

--- go/service/pprof.go
+++ go/service/pprof.go
@@ -35,6 +35,58 @@ func durationSecToDuration(s keybase1.DurationSec) time.Duration {
 	return time.Duration(float64(s) * float64(time.Second))
 }
 
+func (c *PprofHandler) cpuProfile(ctx engine.Context, traceFile string, traceDurationSeconds keybase1.DurationSec) (err error) {
+	panic("not implemented")
+}
+
+func (c *PprofHandler) ProcessorProfile(_ context.Context, arg keybase1.ProcessorProfileArg) (err error) {
+	ctx := engine.Context{
+		LogUI:     c.getLogUI(arg.SessionID),
+		SessionID: arg.SessionID,
+	}
+	return c.cpuProfile(ctx, arg.ProfileFile, arg.ProfileDurationSeconds)
+}
+
+func (c *PprofHandler) LogProcessorProfile(_ context.Context, arg keybase1.LogProcessorProfileArg) (err error) {
+	ctx := engine.Context{
+		LogUI:     c.getLogUI(arg.SessionID),
+		SessionID: arg.SessionID,
+	}
+
+	// Assume this directory already exists, i.e. we've already
+	// started logging.
+	var logDir string
+	if len(arg.LogDirForMobile) > 0 {
+		logDir = arg.LogDirForMobile
+	} else {
+		logDir = c.G().Env.GetLogDir()
+	}
+
+	_ = ctx
+	_ = logDir
+
+	/*
+		traceFiles, err := libkb.GetSortedCPUProfileFiles(logDir)
+		if err != nil {
+			ctx.LogUI.Warning("Error getting trace files in %q: %s", logDir, err)
+		} else if len(traceFiles)+1 > libkb.MaxCPUProfileFileCount {
+			// Remove old trace files.
+			toRemove := traceFiles[:len(traceFiles)+1-libkb.MaxCPUProfileFileCount]
+			for _, path := range toRemove {
+				c.G().Log.Info("Removing old trace file %q", path)
+				err := os.Remove(path)
+				if err != nil {
+					ctx.LogUI.Warning("Error on os.Remove(%q): %s", path, err)
+				}
+			}
+		}
+
+		traceFile := libkb.MakeCPUProfileFilename(logDir, time.Now(), durationSecToDuration(arg.CPUProfileDurationSeconds))
+		return c.trace(ctx, traceFile, arg.CPUProfileDurationSeconds)
+	*/
+	panic("not implemented")
+}
+
 func (c *PprofHandler) trace(ctx engine.Context, traceFile string, traceDurationSeconds keybase1.DurationSec) (err error) {
 	if !filepath.IsAbs(traceFile) {
 		return fmt.Errorf("%q is not an absolute path", traceFile)
