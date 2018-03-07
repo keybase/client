a64c86b90350cc7d47d9a0c64f8998e1e0946dbd
Jeremy Stribling <strib@alum.mit.edu>
2018-03-06 16:22:17 -0800

    redirector: fix unnecessary  double check
    Issue: KBFS-2814

--- redirector/main.go
+++ redirector/main.go
@@ -189,8 +189,7 @@ func (r *root) findKBFSMount(ctx context.Context) (
 		if i < 0 {
 			continue
 		}
-		if (len(mp) > i+len(str) && mp[i+len(str)] != '/') ||
-			(len(mp) > i+len(str) && mp[i+len(str)] != '/') {
+		if len(mp) > i+len(str) && mp[i+len(str)] != '/' {
 			continue
 		}
 
