f622ffd1db1b3720a60493e50f90e7ad82e6aeab
John Zila <john@keyba.se>
2018-03-08 18:54:06 -0800

    fs: Finish styling Finder settings.

--- shared/settings/files/container.js
+++ shared/settings/files/container.js
@@ -2,8 +2,14 @@
 import Files from './index'
 import {connect, type TypedState} from '../../util/container'
 
-const mapStateToProps = (state: TypedState) => ({})
+const mapStateToProps = (state: TypedState) => ({
+  kbfsEnabled: false,
+})
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({})
 
-export default connect(mapStateToProps, mapDispatchToProps)(Files)
+const mergeProps = ({kbfsEnabled}) => ({
+  kbfsEnabled,
+})
+
+export default connect(mapStateToProps, mapDispatchToProps, mergeProps)(Files)
--- shared/settings/files/index.desktop.js
+++ shared/settings/files/index.desktop.js
@@ -1,19 +1,105 @@
 // @flow
 import * as React from 'react'
-import {globalStyles, globalMargins} from '../../styles'
-import {Box, Text} from '../../common-adapters'
+import {globalStyles, globalMargins, globalColors} from '../../styles'
+import {Box, Text, Icon, Button, Checkbox} from '../../common-adapters'
 import {type Props} from '.'
+import {fileUIName} from '../../constants/platform'
 
-const Files = (props: Props) => (
-  <Box style={styleFiles}>
-    <Text type="BodyBig">Hello</Text>
+const checkBoxComponent = (kbfsEnabled: boolean) => (
+  <Box style={globalStyles.flexBoxColumn}>
+    <Text type="Body">Enable Keybase in {fileUIName}</Text>
+    <Text type="BodySmall">Access your Keybase files just like you normally do with your local files.</Text>
   </Box>
 )
 
-const styleFiles = {
+const Files = ({kbfsEnabled}: Props) => {
+  const iconType = kbfsEnabled ? 'icon-fancy-finder-enabled-132-96' : 'icon-fancy-finder-132-96'
+  const bannerStyle = {
+    ...globalStyles.flexBoxRow,
+    backgroundColor: kbfsEnabled ? globalColors.green : globalColors.blue,
+    height: 176,
+    alignItems: 'center',
+  }
+  let bannerContent
+  if (kbfsEnabled) {
+    bannerContent = (
+      <Text type="Header" style={textStyle}>
+        Keybase is enabled in your {fileUIName}.
+      </Text>
+    )
+  } else {
+    bannerContent = (
+      <Box style={globalStyles.flexBoxColumn}>
+        <Text type="Header" style={textStyle}>
+          Enable Keybase in {fileUIName}?
+        </Text>
+        <Text type="BodySemibold" style={textStyle}>
+          Get access to your files and folders just like you normally do with your local files. It's encrypted
+          and secure.
+        </Text>
+        <Box style={{justifyContent: 'flex-start'}}>
+          <Button type="PrimaryGreen" label="Yes, enable" onClick={() => undefined} />
+        </Box>
+      </Box>
+    )
+  }
+  return (
+    <Box style={{...globalStyles.flexBoxColumn, flex: 1}}>
+      <Box style={bannerStyle}>
+        <Box style={bannerIconStyle}>
+          <Icon type={iconType} />
+        </Box>
+        <Box style={bannerTextContentStyle}>{bannerContent}</Box>
+      </Box>
+      <Box style={mainContentStyle}>
+        <Box style={contentHeaderStyle}>
+          <Text type="BodySmallSemibold">{fileUIName} integration</Text>
+          <Icon type="iconfont-finder" style={contentHeaderIconStyle} />
+        </Box>
+        <Checkbox
+          onCheck={() => undefined}
+          labelComponent={checkBoxComponent(kbfsEnabled)}
+          checked={kbfsEnabled}
+        />
+      </Box>
+    </Box>
+  )
+}
+
+const sidePadding = globalMargins.large + globalMargins.tiny
+const bannerIconStyle = {
+  paddingLeft: sidePadding,
+  paddingRight: sidePadding,
+  paddingTop: globalMargins.large,
+  paddingBottom: globalMargins.medium,
+}
+
+const bannerTextContentStyle = {
+  alignItems: 'center',
+  paddingRight: globalMargins.xlarge + globalMargins.medium,
+}
+
+const mainContentStyle = {
   ...globalStyles.flexBoxColumn,
   flex: 1,
-  padding: globalMargins.medium,
+  paddingTop: globalMargins.medium,
+  paddingLeft: globalMargins.tiny,
+}
+
+const contentHeaderStyle = {
+  ...globalStyles.flexBoxRow,
+  paddingBottom: globalMargins.tiny,
+}
+
+const contentHeaderIconStyle = {
+  fontSize: 16,
+  color: globalColors.black_20,
+  paddingLeft: globalMargins.tiny,
+}
+
+const textStyle = {
+  color: globalColors.white,
+  paddingBottom: globalMargins.small,
 }
 
 export default Files
