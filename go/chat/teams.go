1519414ca11db22b4e52254258129269ff5b4f96
Mike Maxim <mike.maxim@gmail.com>
2018-03-15 17:15:40 -0400

    when loading team for upgrade, if we fail sanity check, load again from scratch

--- go/chat/teams.go
+++ go/chat/teams.go
@@ -360,15 +360,27 @@ func LoadTeam(ctx context.Context, g *libkb.GlobalContext, tlfID chat1.TLFID, tl
 		if err != nil {
 			return team, err
 		}
-		team, err = teams.Load(ctx, g, ltarg(teamID))
-		if err != nil {
-			return team, err
+		loadAttempt := func(repoll bool) error {
+			arg := ltarg(teamID)
+			arg.ForceRepoll = arg.ForceRepoll || repoll
+			team, err = teams.Load(ctx, g, arg)
+			if err != nil {
+				return err
+			}
+			if !tlfID.EqString(team.KBFSTLFID()) {
+				return ImpteamUpgradeBadteamError{
+					Msg: fmt.Sprintf("mismatch TLFID to team: %s != %s", team.KBFSTLFID(), tlfID),
+				}
+			}
+			return nil
 		}
-		if !tlfID.EqString(team.KBFSTLFID()) {
-			return team, ImpteamUpgradeBadteamError{
-				Msg: fmt.Sprintf("mismatch TLFID to team: %s != %s", team.KBFSTLFID(), tlfID),
+		if err = loadAttempt(false); err != nil {
+			g.Log.CDebugf(ctx, "++Chat: LoadTeam: failed to load without repoll, trying again: %s", err)
+			if err = loadAttempt(true); err != nil {
+				return team, err
 			}
 		}
+
 		impTeamName, err := team.ImplicitTeamDisplayNameString(ctx)
 		if err != nil {
 			return team, err
