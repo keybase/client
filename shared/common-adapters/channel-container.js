f08c8224d1b950bb80855350885d945fe6753a81
chrisnojima <chrisnojima@gmail.com>
2018-03-16 09:40:59 -0400

    fix channel mentions (#10916)
    * fix channel mentions
    
    * WIP

--- shared/common-adapters/channel-container.js
+++ shared/common-adapters/channel-container.js
@@ -2,18 +2,11 @@
 import * as Chat2Gen from '../actions/chat2-gen'
 import {Channel} from './channel'
 import {connect, type TypedState, type Dispatch} from '../util/container'
-import {pathSelector} from '../actions/route-tree'
 
-const mapStateToProps = (state: TypedState) => ({
-  _currentPath: pathSelector(state),
-})
+const mapStateToProps = (state: TypedState) => ({})
 
 const mapDispatchToProps = (dispatch: Dispatch, {convID}) => ({
-  onClick: currentPath => dispatch(Chat2Gen.createSelectConversation({conversationIDKey: convID})),
+  onClick: () => dispatch(Chat2Gen.createSelectConversation({conversationIDKey: convID})),
 })
 
-const mergeProps = ({_currentPath}, {onClick}) => ({
-  onClick: () => onClick(_currentPath),
-})
-
-export default connect(mapStateToProps, mapDispatchToProps, mergeProps)(Channel)
+export default connect(mapStateToProps, mapDispatchToProps)(Channel)
