40c6986721aa77666451155ee1a2d232d66ba481
Jeremy Stribling <strib@alum.mit.edu>
2018-03-19 13:09:38 -0700

    vendor: update keybase1 for simplefs debug-dumping rpc
    Issue: KBFS-2746

--- vendor/github.com/keybase/client/go/protocol/keybase1/simple_fs.go
+++ vendor/github.com/keybase/client/go/protocol/keybase1/simple_fs.go
@@ -724,6 +724,9 @@ type SimpleFSWaitArg struct {
 	OpID OpID `codec:"opID" json:"opID"`
 }
 
+type SimpleFSDumpDebuggingInfoArg struct {
+}
+
 type SimpleFSInterface interface {
 	// Begin list of items in directory at path
 	// Retrieve results with readList()
@@ -773,6 +776,8 @@ type SimpleFSInterface interface {
 	SimpleFSGetOps(context.Context) ([]OpDescription, error)
 	// Blocking wait for the pending operation to finish
 	SimpleFSWait(context.Context, OpID) error
+	// Instructs KBFS to dump debugging info into its logs.
+	SimpleFSDumpDebuggingInfo(context.Context) error
 }
 
 func SimpleFSProtocol(i SimpleFSInterface) rpc.Protocol {
@@ -1073,6 +1078,17 @@ func SimpleFSProtocol(i SimpleFSInterface) rpc.Protocol {
 				},
 				MethodType: rpc.MethodCall,
 			},
+			"simpleFSDumpDebuggingInfo": {
+				MakeArg: func() interface{} {
+					ret := make([]SimpleFSDumpDebuggingInfoArg, 1)
+					return &ret
+				},
+				Handler: func(ctx context.Context, args interface{}) (ret interface{}, err error) {
+					err = i.SimpleFSDumpDebuggingInfo(ctx)
+					return
+				},
+				MethodType: rpc.MethodCall,
+			},
 		},
 	}
 }
@@ -1208,5 +1224,11 @@ func (c SimpleFSClient) SimpleFSGetOps(ctx context.Context) (res []OpDescription
 func (c SimpleFSClient) SimpleFSWait(ctx context.Context, opID OpID) (err error) {
 	__arg := SimpleFSWaitArg{OpID: opID}
 	err = c.Cli.Call(ctx, "keybase.1.SimpleFS.simpleFSWait", []interface{}{__arg}, nil)
+	return
+}
+
+// Instructs KBFS to dump debugging info into its logs.
+func (c SimpleFSClient) SimpleFSDumpDebuggingInfo(ctx context.Context) (err error) {
+	err = c.Cli.Call(ctx, "keybase.1.SimpleFS.simpleFSDumpDebuggingInfo", []interface{}{SimpleFSDumpDebuggingInfoArg{}}, nil)
 	return
 }
