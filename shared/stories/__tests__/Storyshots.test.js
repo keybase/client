8d8379502c0e9ed46720c0f015ff655bc1e7e2bd
John Zila <john@keyba.se>
2018-03-19 17:23:48 -0700

    fs: Try to fix storyshots

--- shared/stories/__tests__/Storyshots.test.js
+++ shared/stories/__tests__/Storyshots.test.js
@@ -2,5 +2,12 @@
 /* eslint-env jest */
 // eslint-disable-next-line
 import initStoryshots from '@storybook/addon-storyshots'
+import 'jest'
+
+jest.mock('moment', () => {
+  const moment = jest.requireActual('moment-timezone')
+  moment.tz.setDefault('UTC')
+  return moment
+})
 
 initStoryshots()
