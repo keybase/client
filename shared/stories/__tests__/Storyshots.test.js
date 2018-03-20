43adb3b708957878c7346d38e337d4732fff2015
chrisnojima <chrisnojima@gmail.com>
2018-03-20 19:03:11 -0400

    mock out timestamp functions (#10983)

--- shared/stories/__tests__/Storyshots.test.js
+++ shared/stories/__tests__/Storyshots.test.js
@@ -2,12 +2,5 @@
 /* eslint-env jest */
 // eslint-disable-next-line
 import initStoryshots from '@storybook/addon-storyshots'
-import 'jest'
-
-jest.mock('moment', () => {
-  const moment = jest.requireActual('moment-timezone')
-  moment.tz.setDefault('UTC')
-  return moment
-})
-
+jest.mock('../../util/timestamp')
 initStoryshots()
