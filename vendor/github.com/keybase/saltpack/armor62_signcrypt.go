a73de36981bf19daf070427bc8a7efd79e35d672
Fred Akalin <akalin@keyba.se>
2018-03-20 13:39:27 -0700

    Vendor saltpack (#1522)
    Notable changes:
    
    - Hardcodes field order for encryption, so that fixes to the codec
      library won't change it.
    - Make receiver shuffling unbiased, even on go <1.10.
    - Bump CurrentVersion to Version2 (although keybase already always uses
      Version2).

--- vendor/github.com/keybase/saltpack/armor62_signcrypt.go
+++ vendor/github.com/keybase/saltpack/armor62_signcrypt.go
@@ -9,6 +9,19 @@ import (
 	"io/ioutil"
 )
 
+func newSigncryptArmor62SealStream(ciphertext io.Writer, sender SigningSecretKey, receiverBoxKeys []BoxPublicKey, receiverSymmetricKeys []ReceiverSymmetricKey, ephemeralKeyCreator EphemeralKeyCreator, rng signcryptRNG, brand string) (plaintext io.WriteCloser, err error) {
+	// Note: same "BEGIN SALTPACK ENCRYPTED" visible message type.
+	enc, err := NewArmor62EncoderStream(ciphertext, MessageTypeEncryption, brand)
+	if err != nil {
+		return nil, err
+	}
+	out, err := newSigncryptSealStream(enc, sender, receiverBoxKeys, receiverSymmetricKeys, ephemeralKeyCreator, rng)
+	if err != nil {
+		return nil, err
+	}
+	return closeForwarder([]io.WriteCloser{out, enc}), nil
+}
+
 // NewSigncryptArmor62SealStream creates a stream that consumes plaintext data.
 // It will write out signcrypted data to the io.Writer passed in as ciphertext.
 // The signcryption is from the specified sender, and is signcrypted for the
@@ -19,26 +32,19 @@ import (
 //
 // The ciphertext is additionally armored with the recommended armor62-style format.
 //
-// Returns an io.WriteCloser that accepts plaintext data to be signcrypted; and
-// also returns an error if initialization failed.
-func NewSigncryptArmor62SealStream(ciphertext io.Writer, keyring Keyring, sender SigningSecretKey, receiverBoxKeys []BoxPublicKey, receiverSymmetricKeys []ReceiverSymmetricKey, brand string) (plaintext io.WriteCloser, err error) {
-	// Note: same "BEGIN SALTPACK ENCRYPTED" visible message type.
-	enc, err := NewArmor62EncoderStream(ciphertext, MessageTypeEncryption, brand)
-	if err != nil {
-		return nil, err
-	}
-	out, err := NewSigncryptSealStream(enc, keyring, sender, receiverBoxKeys, receiverSymmetricKeys)
-	if err != nil {
-		return nil, err
-	}
-	return closeForwarder([]io.WriteCloser{out, enc}), nil
+// If initialization succeeds, returns an io.WriteCloser that accepts
+// plaintext data to be signcrypted and a nil error. Otherwise,
+// returns nil and the initialization error.
+//
+// ephemeralKeyCreator should be the last argument; it's the 2nd one
+// to preserve the public API.
+func NewSigncryptArmor62SealStream(ciphertext io.Writer, ephemeralKeyCreator EphemeralKeyCreator, sender SigningSecretKey, receiverBoxKeys []BoxPublicKey, receiverSymmetricKeys []ReceiverSymmetricKey, brand string) (plaintext io.WriteCloser, err error) {
+	return newSigncryptArmor62SealStream(ciphertext, sender, receiverBoxKeys, receiverSymmetricKeys, ephemeralKeyCreator, defaultSigncryptRNG{}, brand)
 }
 
-// SigncryptArmor62Seal is the non-streaming version of NewSigncryptArmor62SealStream, which
-// inputs a plaintext (in bytes) and output a ciphertext (as a string).
-func SigncryptArmor62Seal(plaintext []byte, keyring Keyring, sender SigningSecretKey, receiverBoxKeys []BoxPublicKey, receiverSymmetricKeys []ReceiverSymmetricKey, brand string) (string, error) {
+func signcryptArmor62Seal(plaintext []byte, sender SigningSecretKey, receiverBoxKeys []BoxPublicKey, receiverSymmetricKeys []ReceiverSymmetricKey, ephemeralKeyCreator EphemeralKeyCreator, rng signcryptRNG, brand string) (string, error) {
 	var buf bytes.Buffer
-	enc, err := NewSigncryptArmor62SealStream(&buf, keyring, sender, receiverBoxKeys, receiverSymmetricKeys, brand)
+	enc, err := newSigncryptArmor62SealStream(&buf, sender, receiverBoxKeys, receiverSymmetricKeys, ephemeralKeyCreator, rng, brand)
 	if err != nil {
 		return "", err
 	}
@@ -49,6 +55,15 @@ func SigncryptArmor62Seal(plaintext []byte, keyring Keyring, sender SigningSecre
 		return "", err
 	}
 	return buf.String(), nil
+}
+
+// SigncryptArmor62Seal is the non-streaming version of NewSigncryptArmor62SealStream, which
+// inputs a plaintext (in bytes) and output a ciphertext (as a string).
+//
+// ephemeralKeyCreator should be the last argument; it's the 2nd one
+// to preserve the public API.
+func SigncryptArmor62Seal(plaintext []byte, ephemeralKeyCreator EphemeralKeyCreator, sender SigningSecretKey, receiverBoxKeys []BoxPublicKey, receiverSymmetricKeys []ReceiverSymmetricKey, brand string) (string, error) {
+	return signcryptArmor62Seal(plaintext, sender, receiverBoxKeys, receiverSymmetricKeys, ephemeralKeyCreator, defaultSigncryptRNG{}, brand)
 }
 
 // NewDearmor62SigncryptOpenStream makes a new stream that dearmors and decrypts the given
