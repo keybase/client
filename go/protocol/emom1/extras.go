25f3b76250af017f394df70b4bc59c144e02a23c
Maxwell Krohn <themax@gmail.com>
2018-03-21 13:52:54 -0400

    wip

---
+++ go/protocol/emom1/extras.go
@@ -0,0 +1,14 @@
+package emom1
+
+import (
+	"time"
+)
+
+func ToTime(t time.Time) Time {
+	// the result of calling UnixNano on the zero Time is undefined.
+	// https://golang.org/pkg/time/#Time.UnixNano
+	if t.IsZero() {
+		return 0
+	}
+	return Time(t.UnixNano() / 1000000)
+}
