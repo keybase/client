d3b4990eee066f8e1d05e9e3e4f4275f0df3f20a
Miles Steele <miles@milessteele.com>
2018-03-21 19:56:43 -0400

    add stellar fetch func

--- go/stellar/service/service_test.go
+++ go/stellar/service/service_test.go
@@ -7,6 +7,8 @@ import (
 	"github.com/keybase/client/go/externalstest"
 	"github.com/keybase/client/go/kbtest"
 	"github.com/keybase/client/go/libkb"
+	"github.com/keybase/client/go/protocol/keybase1"
+	"github.com/keybase/client/go/stellar/remote"
 	insecureTriplesec "github.com/keybase/go-triplesec-insecure"
 	"github.com/stretchr/testify/require"
 )
@@ -38,4 +40,16 @@ func TestCreateWallet(t *testing.T) {
 	created, err = CreateWallet(context.Background(), tc.G)
 	require.NoError(t, err)
 	require.False(t, created)
+
+	bundle, err := remote.Fetch(context.Background(), tc.G)
+	require.NoError(t, err)
+	require.Equal(t, keybase1.StellarRevision(1), bundle.Revision)
+	require.Nil(t, bundle.Prev)
+	require.NotNil(t, bundle.OwnHash)
+	require.Len(t, bundle.Accounts, 1)
+	require.True(t, len(bundle.Accounts[0].AccountID) > 0)
+	require.Equal(t, keybase1.StellarAccountMode_USER, bundle.Accounts[0].Mode)
+	require.True(t, bundle.Accounts[0].IsPrimary)
+	require.Len(t, bundle.Accounts[0].Signers, 1)
+	require.Equal(t, "", bundle.Accounts[0].Name)
 }
