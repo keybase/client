4658ad114b1466061016b318836e53c64cebc779
Miles Steele <miles@milessteele.com>
2018-03-23 12:47:46 -0400

    remove vendor-stellar.sh

--- go/vendor-stellar.sh
+++
@@ -1,7 +0,0 @@
-govendor add github.com/stellar/go/strkey::github.com/keybase/stellar-org/strkey
-govendor add github.com/stellar/go/keypair::github.com/keybase/stellar-org/keypair
-govendor add github.com/stellar/go/crc16::github.com/keybase/stellar-org/crc16
-govendor add github.com/stellar/go/support/errors::github.com/keybase/stellar-org/support/errors
-govendor add github.com/stellar/go/xdr::github.com/keybase/stellar-org/xdr
-govendor add github.com/nullstyle/go-xdr/xdr3
-govendor add github.com/keybase/stellar-agl-ed25519
