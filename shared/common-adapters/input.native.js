7cdbd765025f4d4b4380e5a0b79651c76ccb7fa6
Frederick Akalin <akalin@keyba.se>
2018-03-26 11:04:01 -0700

    Remove more dead code

--- shared/common-adapters/input.native.js
+++ shared/common-adapters/input.native.js
@@ -256,12 +256,6 @@ class Input extends Component<Props, State> {
       ...commonProps,
       multiline: false,
       style: collapseStyles([singlelineStyle, this.props.inputStyle]),
-      type:
-        {
-          password: 'password',
-          text: 'text',
-          passwordVisible: 'text',
-        }[this.props.type || 'text'] || 'text',
     }
 
     const multilineProps = {
