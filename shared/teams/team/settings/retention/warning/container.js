4b93e889df7c03114d6378a5bc31c1e014dd98df
Daniel Ayoub <danny.a95@gmail.com>
2018-03-27 11:07:23 -0400

    add onCancel to retentionwarning

--- shared/teams/team/settings/retention/warning/container.js
+++ shared/teams/team/settings/retention/warning/container.js
@@ -8,17 +8,22 @@ const mapStateToProps = (state: TypedState, {routeProps}) => {
   }
 }
 
-const mapDispatchToProps = (dispatch: Dispatch, {routeProps, navigateUp}) => ({
-  onBack: () => dispatch(navigateUp()),
-  // TODO maybe take some more identifiers {teamname, conversationIDKey} and handle it here
-  // when you're not decreasing the policy this warning doesn't show so the parent component
-  // needs to know how to make the calls anyway
-  onConfirm: () => {
-    const cb: () => void = routeProps.get('onConfirm')
-    cb && cb()
-    dispatch(navigateUp())
-  },
-})
+const mapDispatchToProps = (dispatch: Dispatch, {routeProps, navigateUp}) => {
+  const onBack = () => dispatch(navigateUp())
+  return {
+    onBack,
+    onCancel: () => {
+      onBack()
+      const onCancel: () => void = routeProps.get('onCancel')
+      onCancel && onCancel()
+    },
+    onConfirm: () => {
+      onBack()
+      const cb: () => void = routeProps.get('onConfirm')
+      cb && cb()
+    },
+  }
+}
 
 export default compose(
   connect(mapStateToProps, mapDispatchToProps),
