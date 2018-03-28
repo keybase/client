837a381da9291645a6bc942b9272c3920a66d8d3
Frederick Akalin <akalin@keyba.se>
2018-03-28 12:54:05 -0700

    Fix

--- shared/react-native/android/app/src/main/java/io/keybase/ossifrage/KBReactPackage.java
+++ shared/react-native/android/app/src/main/java/io/keybase/ossifrage/KBReactPackage.java
@@ -6,6 +6,7 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
