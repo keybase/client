fe4e99a156eef55cacfc9ba2ed6299cc311c962a
Daniel Ayoub <danny.a95@gmail.com>
2018-03-28 11:47:46 -0400

    safer navigates to dropdown & warning

--- shared/teams/team/settings/retention/container.js
+++ shared/teams/team/settings/retention/container.js
@@ -1,11 +1,20 @@
 // @flow
 import * as TeamsGen from '../../../../actions/teams-gen'
 import {createSetConvRetentionPolicy} from '../../../../actions/chat2-gen'
-import {connect, compose, lifecycle, setDisplayName, type TypedState} from '../../../../util/container'
+import {
+  connect,
+  compose,
+  lifecycle,
+  setDisplayName,
+  withStateHandlers,
+  withHandlers,
+  type TypedState,
+} from '../../../../util/container'
 import {getTeamRetentionPolicy} from '../../../../constants/teams'
 import {getConversationRetentionPolicy} from '../../../../constants/chat2/meta'
 import {type RetentionPolicy} from '../../../../constants/types/teams'
-import {navigateAppend} from '../../../../actions/route-tree'
+import {navigateTo, pathSelector} from '../../../../actions/route-tree'
+import {type Path} from '../../../../route-tree'
 import type {ConversationIDKey} from '../../../../constants/types/chat2'
 import RetentionPicker from './'
 
@@ -28,7 +37,9 @@ const mapStateToProps = (state: TypedState, ownProps: OwnProps) => {
   if (!ownProps.isTeamWide && !policy) {
     throw new Error('Conv retpolicy not present in metaMap')
   }
+  const _path = pathSelector(state)
   return {
+    _path,
     policy: ownProps.isTeamWide ? teamPolicy : policy,
     loading,
     teamPolicy: ownProps.isTeamWide ? undefined : teamPolicy,
@@ -40,23 +51,29 @@ const mapDispatchToProps = (
   {conversationIDKey, teamname, onSelect, type, isTeamWide}: OwnProps
 ) => ({
   _loadTeamPolicy: () => dispatch(TeamsGen.createGetTeamRetentionPolicy({teamname})),
-  onShowDropdown: (items, target) =>
+  _onShowDropdown: (items, target, parentPath: Path) =>
     dispatch(
-      navigateAppend([
-        {
-          selected: 'retentionDropdown',
-          props: {items, position: 'top center', targetRect: target && target.getBoundingClientRect()},
-        },
-      ])
+      navigateTo(
+        [
+          {
+            selected: 'retentionDropdown',
+            props: {items, position: 'top center', targetRect: target && target.getBoundingClientRect()},
+          },
+        ],
+        parentPath
+      )
     ),
-  onShowWarning: (days: number, onConfirm: () => void, onCancel: () => void) => {
+  _onShowWarning: (days: number, onConfirm: () => void, onCancel: () => void, parentPath: Path) => {
     dispatch(
-      navigateAppend([
-        {
-          selected: 'retentionWarning',
-          props: {days, onCancel, onConfirm},
-        },
-      ])
+      navigateTo(
+        [
+          {
+            selected: 'retentionWarning',
+            props: {days, onCancel, onConfirm},
+          },
+        ],
+        parentPath
+      )
     )
   },
   setRetentionPolicy: (policy: RetentionPolicy) => {
@@ -74,9 +91,19 @@ const mapDispatchToProps = (
 export default compose(
   connect(mapStateToProps, mapDispatchToProps),
   setDisplayName('RetentionPicker'),
+  withStateHandlers({_parentPath: null}, {_setParentPath: () => _parentPath => ({_parentPath})}),
   lifecycle({
+    componentWillMount: function() {
+      this.props._setParentPath(this.props._path)
+    },
     componentDidMount: function() {
       this.props._loadTeamPolicy()
     },
+  }),
+  withHandlers({
+    onShowDropdown: ({_parentPath, _onShowDropdown}) => (items, target) =>
+      _onShowDropdown(items, target, _parentPath),
+    onShowWarning: ({_parentPath, _onShowWarning}) => (days, onConfirm, onCancel) =>
+      _onShowWarning(days, onConfirm, onCancel, _parentPath),
   })
 )(RetentionPicker)
