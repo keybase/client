f45f8883b92829d2cd80468e8016e3f2de4cf4bb
Frederick Akalin <akalin@keyba.se>
2018-03-29 21:14:44 -0700

    More input story stuff

--- shared/common-adapters/input.stories.js
+++ shared/common-adapters/input.stories.js
@@ -1,21 +1,25 @@
 // @flow
 import * as React from 'react'
-import Input from './input'
+import {type Props, default as Input} from './input'
 import {action, storiesOf} from '../stories/storybook'
 
+const commonProps: Props = {
+  onBlur: action('onBlur'),
+  onClick: action('onClick'),
+  onChangeText: action('onChangeText'),
+  onEnterKeyDown: action('onEnterKeyDown'),
+  onFocus: action('onFocus'),
+  onKeyDown: action('onKeyDown'),
+  onKeyUp: action('onKeyUp'),
+  onSelectionChange: action('onSelectionChange'),
+}
+
 const load = () => {
-  storiesOf('Common', module).add('Input (plain)', () => (
-    <Input
-      onBlur={action('onBlur')}
-      onClick={action('onClick')}
-      onChangeText={action('onChangeText')}
-      onEnterKeyDown={action('onEnterKeyDown')}
-      onFocus={action('onFocus')}
-      onKeyDown={action('onKeyDown')}
-      onKeyUp={action('onKeyUp')}
-      onSelectionChange={action('onSelectionChange')}
-    />
-  ))
+  storiesOf('Common', module)
+    .add('Input (single line)', () => <Input {...commonProps} />)
+    .add('Input (multiline)', () => <Input {...commonProps} multiline={true} />)
+    .add('Input (password)', () => <Input {...commonProps} type="password" />)
+    .add('Input (visible password)', () => <Input {...commonProps} type="passwordVisible" />)
 }
 
 export default load
