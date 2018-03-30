410b992f54c667171523e3ddbea0772dc36f5bda
Frederick Akalin <akalin@keyba.se>
2018-03-29 21:08:06 -0700

    Improve input

--- shared/common-adapters/input.stories.js
+++ shared/common-adapters/input.stories.js
@@ -1,10 +1,21 @@
 // @flow
 import * as React from 'react'
 import Input from './input'
-import {storiesOf} from '../stories/storybook'
+import {action, storiesOf} from '../stories/storybook'
 
 const load = () => {
-  storiesOf('Common', module).add('Input', () => <Input />)
+  storiesOf('Common', module).add('Input (plain)', () => (
+    <Input
+      onBlur={action('onBlur')}
+      onClick={action('onClick')}
+      onChangeText={action('onChangeText')}
+      onEnterKeyDown={action('onEnterKeyDown')}
+      onFocus={action('onFocus')}
+      onKeyDown={action('onKeyDown')}
+      onKeyUp={action('onKeyUp')}
+      onSelectionChange={action('onSelectionChange')}
+    />
+  ))
 }
 
 export default load
--- shared/stories/stories.js
+++ shared/stories/stories.js
@@ -17,6 +17,7 @@ import devices from '../devices/index.stories'
 import dropdown from '../common-adapters/dropdown.stories'
 import git from '../git/index.stories'
 import icon from '../common-adapters/icon.stories'
+import input from '../common-adapters/input.stories'
 import nameWithIcon from '../common-adapters/name-with-icon.stories'
 import radiobutton from '../common-adapters/radio-button.stories'
 import retention from '../teams/team/settings/retention/index.stories'
@@ -51,6 +52,7 @@ const stories = {
   fs,
   git,
   icon,
+  input,
   nameWithIcon,
   peopleTask,
   peopleFollowNotification,
