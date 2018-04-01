7da4aaaa11d573f34cb9445572880efaa5d8b57d
chrisnojima <cnojima@keyba.se>
2018-04-01 18:14:01 -0400

    WIP

--- shared/stories/mock-react-redux.js
+++ shared/stories/mock-react-redux.js
@@ -2,4 +2,5 @@
 import {connect} from '../__mocks__/react-redux'
 // Monkeypatch redux connect
 const redux = require('react-redux')
+// $FlowIssue doens't like writing over readonly stuff :)
 redux.connect = connect
