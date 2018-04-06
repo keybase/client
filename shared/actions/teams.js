4199a4d92355fcdad0f791050f671871e6171def
Daniel Ayoub <danny.a95@gmail.com>
2018-04-06 16:02:40 -0400

    sort out team store on update

--- shared/actions/teams.js
+++ shared/actions/teams.js
@@ -160,6 +160,38 @@ const _setTeamRetentionPolicy = function(action: TeamsGen.SetTeamRetentionPolicy
   ])
 }
 
+const _updateTeamRetentionPolicy = function(
+  action: Chat2Gen.UpdateTeamRetentionPolicyPayload,
+  state: TypedState
+) {
+  const {convs} = action.payload
+  if (convs.length === 0) {
+    logger.warn('Got updateTeamRetentionPolicy with no convs; aborting. Local copy may be out of date')
+    return
+  }
+  const {teamRetention, name} = convs[0]
+  try {
+    const newPolicy = Constants.serviceRetentionPolicyToRetentionPolicy(teamRetention)
+    // increment / decrement waiting here to signify we got an update in case we didn't do it
+    return Saga.sequentially([
+      Saga.put(
+        createIncrementWaiting({
+          key: [Constants.teamWaitingKey(name), Constants.retentionWaitingKey(name)],
+        })
+      ),
+      Saga.put(replaceEntity(['teams', 'teamNameToRetentionPolicy'], I.Map([[name, newPolicy]]))),
+      Saga.put(
+        createDecrementWaiting({
+          key: [Constants.teamWaitingKey(name), Constants.retentionWaitingKey(name)],
+        })
+      ),
+    ])
+  } catch (err) {
+    logger.error(err.message)
+    throw err
+  }
+}
+
 const _inviteByEmail = function*(action: TeamsGen.InviteToTeamByEmailPayload) {
   const {invitees, role, teamname} = action.payload
   yield Saga.put(createIncrementWaiting({key: Constants.teamWaitingKey(teamname)}))
@@ -853,18 +885,6 @@ function _setupTeamHandlers() {
       }
       actions.forEach(dispatch)
     })
-    engine().setIncomingHandler('chat.1.NotifyChat.ChatSetTeamRetention', args => {
-      logger.info('Got setTeamRetention from service')
-      const {convs, teamID} = args
-      if (convs.length === 0) {
-        logger.warn(`Teamhandler: Got setTeamRetention for team with no conversations: ${teamID}`)
-        return
-      }
-      const conv = convs[0]
-      const teamname = conv.name
-      const newPolicy = Constants.serviceRetentionPolicyToRetentionPolicy(conv.teamRetention)
-      dispatch(replaceEntity(['teams', 'teamNameToRetentionPolicy'], I.Map([[teamname, newPolicy]])))
-    })
   })
 }
 
@@ -1083,6 +1103,7 @@ const teamsSaga = function*(): Saga.SagaGenerator<any, any> {
   )
   yield Saga.safeTakeEvery(TeamsGen.getTeamRetentionPolicy, _getTeamRetentionPolicy)
   yield Saga.safeTakeEveryPure(TeamsGen.setTeamRetentionPolicy, _setTeamRetentionPolicy)
+  yield Saga.safeTakeEveryPure(Chat2Gen.updateTeamRetentionPolicy, _updateTeamRetentionPolicy)
 }
 
 export default teamsSaga
