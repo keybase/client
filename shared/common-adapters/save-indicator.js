bfd6ada3596ef5cda4540848b1904625f080ba28
Frederick Akalin <akalin@keyba.se>
2018-04-11 15:45:54 -0700

    Wrap in box

--- shared/common-adapters/save-indicator.js
+++ shared/common-adapters/save-indicator.js
@@ -1,14 +1,22 @@
 // @flow
 import * as React from 'react'
+import Box from './box'
 import Icon from './icon'
 import ProgressIndicator from './progress-indicator'
 import Text from './text'
-import {globalColors, globalMargins} from '../styles'
+import {globalColors, globalMargins, globalStyles} from '../styles'
 
 type SaveState = 'same' | 'saving' | 'justSaved'
 
 type Props = {
   saveState: SaveState,
+}
+
+const containerStyle = {
+  ...globalStyles.flexBoxRow,
+  alignItems: 'center',
+  height: globalMargins.medium,
+  justifyContent: 'center',
 }
 
 const SaveIndicator = ({saveState}: Props) => {
@@ -18,12 +26,14 @@ const SaveIndicator = ({saveState}: Props) => {
     case 'saving':
       return <ProgressIndicator style={{alignSelf: 'center', width: globalMargins.medium}} />
     case 'justSaved':
-      return [
-        <Icon key="0" type="iconfont-check" style={{color: globalColors.green}} />,
-        <Text key="1" type="BodySmall" style={{color: globalColors.green2}}>
-          &nbsp; Saved
-        </Text>,
-      ]
+      return (
+        <Box style={containerStyle}>
+          <Icon type="iconfont-check" style={{color: globalColors.green}} />
+          <Text type="BodySmall" style={{color: globalColors.green2}}>
+            &nbsp; Saved
+          </Text>
+        </Box>
+      )
   }
 }
 
