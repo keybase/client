80ac42a60d96508ad40638c486fe52d0e1dffcc5
Frederick Akalin <akalin@keyba.se>
2018-04-11 15:55:39 -0700

    Add story

--- shared/common-adapters/save-indicator.stories.js
+++ shared/common-adapters/save-indicator.stories.js
@@ -1,10 +1,41 @@
 // @flow
 import * as React from 'react'
-import SaveIndicator from './save-indicator'
+import {type SaveState, default as SaveIndicator} from './save-indicator'
 import {storiesOf} from '../stories/storybook'
+import {globalStyles} from '../styles'
+import Box from './box'
+import Button from './button'
+
+type State = {
+  saveState: SaveState,
+}
+
+const containerStyle = {
+  ...globalStyles.flexBoxColumn,
+}
+
+class SaveIndicatorContainer extends React.Component<{}, State> {
+  constructor() {
+    super({})
+    this.state = {saveState: 'same'}
+  }
+
+  _save = () => {
+    this.setState({saveState: 'justSaved'})
+  }
+
+  render() {
+    return (
+      <Box style={containerStyle}>
+        <Button label="Save" onClick={this._save} type="Primary" />
+        <SaveIndicator saveState={this.state.saveState} />
+      </Box>
+    )
+  }
+}
 
 const load = () => {
-  storiesOf('Common', module).add('SaveIndicator', () => <SaveIndicator saveState="justSaved" />)
+  storiesOf('Common', module).add('SaveIndicator', () => <SaveIndicatorContainer />)
 }
 
 export default load
