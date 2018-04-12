d05917f568a59b941e6285bbe8c82a2e09f53c66
Frederick Akalin <akalin@keyba.se>
2018-04-11 23:57:08 -0700

    Do state machine

--- shared/common-adapters/save-indicator.js
+++ shared/common-adapters/save-indicator.js
@@ -8,6 +8,8 @@ import {globalColors, globalMargins, globalStyles} from '../styles'
 
 type SaveState = 'same' | 'saving' | 'justSaved'
 
+type SaveState2 = 'steady' | 'saving' | 'savingHysteresis' | 'justSaved'
+
 type Props = {
   saving: boolean,
   minSavingTimeMs: number,
@@ -16,8 +18,74 @@ type Props = {
 }
 
 type State = {
-  saveState: SaveState,
-  lastStateChangeTime: number,
+  saving: boolean,
+  savingChanged: number,
+  saveState: SaveState2,
+  saveStateChanged: number,
+}
+
+const computeNextState = (props: Props, state: State, now: number): null | SaveState2 | number => {
+  const timeSinceSavingChanged = now - state.savingChanged
+  const timeSinceSaveStateChanged = now - state.saveStateChanged
+
+  const {saveState} = state
+  switch (saveState) {
+    case 'steady':
+      if (state.saving) {
+        return 'saving'
+      }
+
+      if (timeSinceSaveStateChanged <= props.savedTimeoutMs) {
+        return 'justSaved'
+      }
+
+      return null
+
+    case 'saving':
+      if (state.saving) {
+        return null
+      }
+
+      if (timeSinceSavingChanged <= props.minSavingTimeMs) {
+        return 'savingHysteresis'
+      } else {
+        return 'justSaved'
+      }
+
+    case 'savingHysteresis':
+      if (state.saving) {
+        return 'saving'
+      }
+
+      const timeToJustSaved = timeSinceSavingChanged - props.minSavingTimeMs
+      if (timeToJustSaved <= 0) {
+        return 'justSaved'
+      }
+
+      return timeToJustSaved
+
+    case 'justSaved':
+      if (state.saving) {
+        return 'saving'
+      }
+
+      if (timeSinceSavingChanged <= props.minSavingTimeMs) {
+        return 'savingHysteresis'
+      }
+
+      const timeToSteady = timeSinceSaveStateChanged - props.savedTimeoutMs
+      if (timeToSteady <= 0) {
+        return 'steady'
+      }
+
+      return timeToSteady
+
+    default:
+      // eslint-disable-next-line no-unused-expressions
+      ;(saveState: empty)
+  }
+
+  throw new Error('Unexpected state')
 }
 
 const containerStyle = {
@@ -32,90 +100,49 @@ class SaveIndicator extends React.Component<Props, State> {
 
   constructor(props: Props) {
     super(props)
-    this.state = {saveState: 'same', lastStateChangeTime: Date.now()}
+    this.state = {saving: false, savingChanged: 0, saveState: 'steady', saveStateChanged: 0}
   }
 
   static getDerivedStateFromProps = (nextProps: Props, prevState: State) => {
-    if (nextProps.saving) {
-      if (prevState.saveState === 'saving') {
-        // Already saving, so nothing to do.
-        return null
-      }
-
-      return {saveState: 'saving', lastStateChangeTime: Date.now()}
-    }
-
-    if (prevState.saveState === 'same' || prevState.saveState === 'justSaved') {
-      // Already done saving, so nothing to do.
+    if (nextProps.saving === prevState.saving) {
       return null
     }
 
-    const dt = Date.now() - prevState.lastStateChangeTime
-    if (dt < nextProps.minSavingTimeMs) {
-      // (1) Save took less than minSavingTimeMs, so stay in the
-      // saving state, set a timer in componentDidUpdate to transition
-      // to 'justSaved'. (See (1) below.)
-      return null
-    }
-
-    // (2) Save took at least minSavingTimeMs, so transition to
-    // justSaved, and set a timer in componentDidUpdate to transition
-    // to 'same'. (See (2) below.)
-    return {saveState: 'justSaved', lastStateChangeTime: Date.now()}
+    return {saving: nextProps.saving, savingChanged: Date.now()}
   }
 
-  _resetTimeout = (fn: () => void, delay: number) => {
+  _runStateMachine = () => {
     if (this._timeoutID) {
       clearTimeout(this._timeoutID)
-    }
-    this._timeoutID = setTimeout(fn, delay)
-  }
-
-  componentDidUpdate = (prevProps: Props, prevState: State) => {
-    const onStateChange = this.props.onStateChange
-    if (onStateChange && prevState.saveState !== this.state.saveState) {
-      const dtS = (Date.now() - prevState.lastStateChangeTime) / 1000
-      onStateChange(`was in '${prevState.saveState}' for ${dtS}s, now in '${this.state.saveState}'`)
+      this._timeoutID = null
     }
 
-    if (this.props.saving) {
-      // Already handled in getDerivedStateFromProps.
+    const now = Date.now()
+    const result = computeNextState(this.props, this.state, now)
+    if (!result) {
       return
     }
 
-    switch (this.state.saveState) {
-      case 'same':
-        // Nothing to do.
-        return
-
-      case 'saving': {
-        // (1) Transition to 'justSaved', either now or later.
-        const dt = Date.now() - this.state.lastStateChangeTime
-        if (dt < this.props.minSavingTimeMs) {
-          this._resetTimeout(() => {
-            this.setState({saveState: 'justSaved', lastStateChangeTime: Date.now()})
-          }, this.props.minSavingTimeMs - dt)
-        }
-        return
-      }
-
-      case 'justSaved': {
-        // (2) Transition to 'same', either now or later.
-        const dt = Date.now() - this.state.lastStateChangeTime
-        if (dt < this.props.savedTimeoutMs) {
-          this._resetTimeout(() => {
-            this.setState({saveState: 'same', lastStateChangeTime: Date.now()})
-          }, this.props.savedTimeoutMs - dt)
-        }
-      }
+    if (typeof result === 'number') {
+      this._timeoutID = setTimeout(this._runStateMachine, result)
+      return
     }
+
+    this.setState({saveState: result, saveStateChanged: now})
+  }
+
+  componentDidUpdate = () => {
+    this._runStateMachine()
   }
 
   render = () => {
-    switch (this.state.saveState) {
-      case 'same':
+    const {saveState} = this.state
+    switch (saveState) {
+      case 'steady':
         return null
+
       case 'saving':
+      case 'savingHysteresis':
         return (
           <Box style={containerStyle}>
             <ProgressIndicator style={{width: globalMargins.medium}} />
@@ -130,6 +157,10 @@ class SaveIndicator extends React.Component<Props, State> {
             </Text>
           </Box>
         )
+
+      default:
+        // eslint-disable-next-line no-unused-expressions
+        ;(saveState: empty)
     }
   }
 }
