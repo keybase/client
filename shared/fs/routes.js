c1915366206fe3c1e10f06cdbc5c4e4ea124d444
Song Gao <song@gao.io>
2018-04-14 10:37:43 +0800

    revert breadcrumb route and routes cleanup

--- shared/fs/routes.js
+++ shared/fs/routes.js
@@ -10,18 +10,11 @@ import {makeRouteDefNode, makeLeafTags} from '../route-tree'
 import RowPopupMenu from './popups/row-action-popup-container'
 import TransferPopup from './popups/transfer-container.js'
 
-const _headerChildren = {
-  breadcrumbAction: {
-    component: RelativePopupHoc(BreadcrumbPopupMenu),
-    tags: makeLeafTags({layerOnTop: true}),
-  },
+const _commonChildren = {
   finderAction: {
     component: RelativePopupHoc(FinderPopupMenu),
     tags: makeLeafTags({layerOnTop: true}),
   },
-}
-
-const _commonChildren = {
   pathItemAction: {
     component: RelativePopupHoc(RowPopupMenu),
     tags: makeLeafTags({layerOnTop: true}),
@@ -34,25 +27,21 @@ const _commonChildren = {
 
 const _folderRoute = {
   children: {
-    ..._headerChildren,
+    ..._commonChildren,
     folder: () => makeRouteDefNode(_folderRoute),
     preview: {
       component: FilePreview,
       children: _commonChildren,
       tags: makeLeafTags({title: 'Preview'}),
-      children: {
-        ..._headerChildren,
-      },
+    },
+    breadcrumbAction: {
+      component: RelativePopupHoc(BreadcrumbPopupMenu),
+      tags: makeLeafTags({layerOnTop: true}),
     },
     sortbarAction: {
       component: RelativePopupHoc(SortBarPopupMenu),
       tags: makeLeafTags({layerOnTop: true}),
     },
-    finderAction: {
-      component: RelativePopupHoc(FinderPopupMenu),
-      tags: makeLeafTags({layerOnTop: true}),
-    },
-    ..._commonChildren,
   },
   component: Files,
 }
