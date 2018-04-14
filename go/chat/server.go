8b64c20cd830ff0d28f5031bf0f886407e4362a3
Mike Maxim <mike@keyba.se>
2018-04-14 11:02:20 -0400

    disconn if not in foregorund on getthread

--- go/chat/server.go
+++ go/chat/server.go
@@ -494,6 +494,11 @@ func (h *Server) GetThreadNonblock(ctx context.Context, arg chat1.GetThreadNonbl
 	}
 	// If this is from a push, set us into the foreground
 	if arg.Reason == chat1.GetThreadNonblockReason_PUSH {
+		// Also if we get here and we claim to not be in the foreground yet, then hit disconnect
+		// to reset any delay checks or timers
+		if h.G().AppState.State() != keybase1.AppState_FOREGROUND {
+			h.G().Syncer.Disconnected(ctx)
+		}
 		h.G().AppState.Update(keybase1.AppState_FOREGROUND)
 	}
 
