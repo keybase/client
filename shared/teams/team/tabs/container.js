4678482375379c6eeb55cec4caa435fa25c6df56
chrisnojima <cnojima@keyba.se>
2018-04-16 14:23:41 -0400

    WIP

--- shared/teams/team/tabs/container.js
+++ shared/teams/team/tabs/container.js
@@ -1,15 +1,9 @@
 // @flow
 import * as Constants from '../../../constants/teams'
 import * as I from 'immutable'
-// import * as React from 'react'
-// import * as TeamsGen from '../../actions/teams-gen'
-// import * as KBFSGen from '../../actions/kbfs-gen'
-// import * as Chat2Gen from '../../actions/chat2-gen'
 import Tabs from '.'
 import {connect, type TypedState} from '../../../util/container'
-// import {navigateAppend} from '../../actions/route-tree'
 import {anyWaiting} from '../../../constants/waiting'
-// import {teamsTab} from '../../constants/tabs'
 
 const mapStateToProps = (state: TypedState, {teamname, selectedTab, setSelectedTab}) => {
   const yourOperations = Constants.getCanPerform(state, teamname)
@@ -29,36 +23,7 @@ const mapStateToProps = (state: TypedState, {teamname, selectedTab, setSelectedT
   }
 }
 
-const mapDispatchToProps = (
-  dispatch: Dispatch
-  // {navigateUp, newOpenTeamRole, setOpenTeamRole, setRouteState, routeProps}
-) => {
-  return {}
-  // const teamname = routeProps.get('teamname')
-  // return {
-  // setSelectedTab: selectedTab => setRouteState({selectedTab}),
-  // onChat: () => dispatch(Chat2Gen.createStartConversation({tlf: `/keybase/team/${teamname}`})),
-  // onBack: () => dispatch(navigateUp()),
-  // onShowMenu: target =>
-  // dispatch(
-  // navigateAppend(
-  // [
-  // {
-  // props: {
-  // teamname,
-  // position: 'bottom left',
-  // targetRect: target && target.getBoundingClientRect(),
-  // },
-  // selected: 'menu',
-  // },
-  // ],
-  // [teamsTab, 'team']
-  // )
-  // ),
-
-  // onOpenFolder: () => dispatch(KBFSGen.createOpen({path: `/keybase/team/${teamname}`})),
-  // }
-}
+const mapDispatchToProps = (dispatch: Dispatch) => ({})
 
 const mergeProps = (stateProps, dispatchProps): Props => {
   return {
