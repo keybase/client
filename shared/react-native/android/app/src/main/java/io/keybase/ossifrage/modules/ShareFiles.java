982fd4aedada58bb0bc7f15f1fdcb4a69350789b
John Zila <john@keyba.se>
2018-04-17 19:20:40 -0700

    fs: Make Android sharing work

--- shared/react-native/android/app/src/main/java/io/keybase/ossifrage/modules/ShareFiles.java
+++ shared/react-native/android/app/src/main/java/io/keybase/ossifrage/modules/ShareFiles.java
@@ -2,12 +2,14 @@ package io.keybase.ossifrage.modules;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Bundle;
+import android.support.v4.content.FileProvider;
 
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+
+import java.io.File;
 
 import io.keybase.ossifrage.R;
 
@@ -29,8 +31,11 @@ public class ShareFiles extends ReactContextBaseJavaModule {
     @ReactMethod
     public void share(String uriPath, String mimeType, Promise promise) {
         Uri uri = Uri.parse(uriPath);
+        File file = new File(reactContext.getCacheDir(), uri.getLastPathSegment());
+        Uri fileUri = FileProvider.getUriForFile(reactContext, "io.keybase.ossifrage.fileprovider", file);
+
         Intent intent = new Intent(Intent.ACTION_SEND)
-                .putExtra(Intent.EXTRA_STREAM, uri)
+                .putExtra(Intent.EXTRA_STREAM, fileUri)
                 .setType(mimeType);
         Intent chooser = Intent.createChooser(intent, reactContext.getResources().getText(R.string.send_to));
         if (intent.resolveActivity(reactContext.getPackageManager()) != null) {
