31a29733bb4826cdd6bd91e4e62c39d51f1e42bd
chrisnojima <cnojima@keyba.se>
2018-04-20 10:07:37 -0400

    WIP

--- shared/common-adapters/dumb.js
+++ shared/common-adapters/dumb.js
@@ -5,10 +5,8 @@ import type {DumbComponentMap} from '../constants/types/more'
 import {
   AutosizeInput,
   Avatar,
-  Button,
   Box,
   ChoiceList,
-  ListItem,
   Markdown,
   PopupDialog,
   PopupMenu,
@@ -16,213 +14,6 @@ import {
   Text,
 } from './index'
 import {globalStyles, globalColors, isMobile} from '../styles'
-
-// So we can share this between mobile and desktop
-const display = type => (isMobile ? {} : {display: type})
-
-const listItemMap: DumbComponentMap<ListItem> = {
-  component: ListItem,
-  mocks: {
-    'Small list item with icon (desktop only)': {
-      type: 'Small',
-      icon: <Box style={{height: 24, width: 24, backgroundColor: globalColors.black_20}} />,
-      body: <Box style={{backgroundColor: globalColors.black_20, flex: 1}} />,
-      action: <Button label={'Action'} type={'Primary'} onClick={() => {}} />,
-    },
-    'Small list item with button action': {
-      type: 'Small',
-      swipeToAction: true,
-      icon: <Box style={{height: 32, width: 32, backgroundColor: globalColors.black_20}} />,
-      body: <Box style={{backgroundColor: globalColors.black_20, flex: 1}} />,
-      action: <Button label={'Action'} type={'Primary'} onClick={() => {}} />,
-    },
-    'Small list item with avatar 40 (mobile only)': {
-      type: 'Small',
-      icon: <Box style={{height: 40, width: 40, backgroundColor: globalColors.black_20}} />,
-      body: <Box style={{backgroundColor: globalColors.black_20, flex: 1}} />,
-      swipeToAction: true,
-      action: <Button label={'Action'} type={'Primary'} onClick={() => {}} />,
-    },
-    'Small list item with text action': {
-      type: 'Small',
-      icon: <Box style={{height: 32, width: 32, backgroundColor: globalColors.black_20}} />,
-      body: <Box style={{backgroundColor: globalColors.black_20, flex: 1}} />,
-      action: (
-        <Text style={{color: globalColors.red}} type={'BodySmall'} onClick={() => {}}>
-          Action Jack
-        </Text>
-      ),
-      swipeToAction: true,
-      extraRightMarginAction: true,
-    },
-    'Large list item with Button': {
-      type: 'Large',
-      icon: <Box style={{height: 48, width: 48, backgroundColor: globalColors.black_20}} />,
-      body: <Box style={{backgroundColor: globalColors.black_20, flex: 1}} />,
-      swipeToAction: true,
-      action: <Button label={'Action'} type={'Primary'} onClick={() => {}} />,
-    },
-    'Large list item with text action': {
-      type: 'Large',
-      icon: <Box style={{height: 48, width: 48, backgroundColor: globalColors.black_20}} />,
-      body: <Box style={{backgroundColor: globalColors.black_20, flex: 1}} />,
-      action: (
-        <Text style={{color: globalColors.red}} type={'BodySmall'} onClick={() => {}}>
-          Action Jack
-        </Text>
-      ),
-      extraRightMarginAction: true,
-    },
-  },
-}
-
-const popupCommon = {
-  parentProps: isMobile
-    ? {style: {height: 300}}
-    : {style: {border: 'solid 1px black', position: 'relative', height: 300}},
-  onHidden: () => console.log('popup hidden'),
-  style: {marginLeft: 100, maxWidth: 320},
-}
-
-const popupItemCommon = {
-  onClick: () => console.log('item clicked'),
-}
-
-const popupMenuMap: DumbComponentMap<PopupMenu> = {
-  component: PopupMenu,
-  mocks: {
-    'Popup Simple': {
-      ...popupCommon,
-      items: [
-        {...popupItemCommon, title: 'One'},
-        {...popupItemCommon, title: 'Two'},
-        {...popupItemCommon, title: 'Three'},
-      ],
-    },
-    'Popup Complex': {
-      ...popupCommon,
-      items: [
-        {...popupItemCommon, title: 'Open in Finder'},
-        {...popupItemCommon, title: 'Ignore'},
-        'Divider',
-        {
-          ...popupItemCommon,
-          title: 'Clear history (3.24 MB)',
-          subTitle: 'Deletes old copies of files.',
-          danger: true,
-        },
-        {
-          ...popupItemCommon,
-          title: 'Delete files and clear history (5.17GB)',
-          subTitle: 'Deletes everything in this folder, including its backup versions',
-          danger: true,
-        },
-      ],
-    },
-  },
-}
-
-const avatarSizes = [176, 112, 80, 64, 48, 40, 32, 24, 16]
-const mockAvatarSizes = (title, modifiers) =>
-  keyBy(
-    avatarSizes.map(size => ({size, username: 'awendland', ...modifiers})),
-    props => `${title} x${props.size}`
-  )
-
-const avatarMap: DumbComponentMap<Avatar> = {
-  component: Avatar,
-  mocks: {
-    ...mockAvatarSizes('Normal', {}),
-    ...mockAvatarSizes('Fallback', {username: 'FALLBACK'}),
-    ...mockAvatarSizes('Following', {
-      following: true,
-    }),
-    ...mockAvatarSizes('Follows You', {
-      followsYou: true,
-    }),
-    ...mockAvatarSizes('Mutual Follow', {
-      following: true,
-      followsYou: true,
-    }),
-  },
-}
-
-const choiceListMap: DumbComponentMap<ChoiceList> = {
-  component: ChoiceList,
-  mocks: {
-    'Two Choices': {
-      options: [
-        {
-          title: 'Host a TXT file',
-          description: 'Host a text file on your site, such as yoursite.com/keybase.txt.',
-          icon: 'icon-file-txt-48',
-          onClick: () => console.log('ChoiceList: onClick TXT file'),
-        },
-        {
-          title: 'Set a DNS',
-          description: 'Place a Keybase proof in your DNS records.',
-          icon: 'icon-dns-48',
-          onClick: () => console.log('ChoiceList: onClick DNS'),
-        },
-      ],
-    },
-  },
-}
-
-const standardScreenProps = {
-  onClose: () => console.log('StandardScreen: onClose'),
-  children: (
-    <Text type="Header" style={{textAlign: 'center'}}>
-      Whoa, look at this centered thing
-    </Text>
-  ),
-  parentProps: {style: {...display('flex'), height: 578}},
-}
-
-const standardScreenMap: DumbComponentMap<StandardScreen> = {
-  component: StandardScreen,
-  mocks: {
-    Normal: {
-      ...standardScreenProps,
-    },
-    Error: {
-      ...standardScreenProps,
-      notification: {
-        message: 'Something went horribly wrong! :-(',
-        type: 'error',
-      },
-    },
-    'Success w/ Custom Notification Element': {
-      ...standardScreenProps,
-      notification: {
-        message: (
-          <Text type="BodySemibold" style={{color: globalColors.white}}>
-            You won a unicorn!{' '}
-            <Text type="BodySemibold" style={{color: globalColors.white}}>
-              Make sure to feed it
-            </Text>{' '}
-            :-)
-          </Text>
-        ),
-        type: 'success',
-      },
-    },
-    'Back Button': {
-      ...standardScreenProps,
-      onClose: null,
-      onBack: () => console.log('StandardScreen: onBack'),
-    },
-    'Error w/ Back Button': {
-      ...standardScreenProps,
-      onClose: null,
-      onBack: () => console.log('StandardScreen: onBack'),
-      notification: {
-        message: 'This is an error, but you can go back!',
-        type: 'error',
-      },
-    },
-  },
-}
 
 const markdownDumbMap: DumbComponentMap<Markdown> = {
   component: Markdown,
@@ -393,9 +184,7 @@ export default {
   AutosizeInput: autosizeInputMap,
   Avatar: avatarMap,
   ChoiceList: choiceListMap,
-  ListItem: listItemMap,
   Markdown: markdownDumbMap,
   PopupDialog: popupDialogMap,
   PopupMenu: popupMenuMap,
-  StandardScreen: standardScreenMap,
 }
--- shared/common-adapters/index.stories.js
+++ shared/common-adapters/index.stories.js
@@ -4,31 +4,41 @@ import box from './box.stories'
 import button from './button.stories'
 import buttonBar from './button-bar.stories'
 import checkbox from './checkbox.stories'
+import choiceList from './choice-list.stories'
 import dropdown from './dropdown.stories'
 import formWithCheckbox from './form-with-checkbox.stories'
 import icon from './icon.stories'
 import input from './input.stories'
+import listItem from './list-item.stories'
 import meta from './meta.stories'
 import nameWithIcon from './name-with-icon.stories'
+import popupMenu from './popup-menu.stories'
 import radiobutton from './radio-button.stories'
 import saveIndicator from './save-indicator.stories'
+import standardScreen from './standard-screen.stories'
 import text from './text.stories'
 
 const load = () => {
-  avatar()
-  box()
-  button()
-  buttonBar()
-  checkbox()
-  dropdown()
-  formWithCheckbox()
-  icon()
-  input()
-  meta()
-  nameWithIcon()
-  radiobutton()
-  saveIndicator()
-  text()
+  ;[
+    avatar,
+    box,
+    button,
+    buttonBar,
+    checkbox,
+    choiceList,
+    dropdown,
+    formWithCheckbox,
+    icon,
+    input,
+    listItem,
+    meta,
+    nameWithIcon,
+    popupMenu,
+    radiobutton,
+    saveIndicator,
+    standardScreen,
+    text,
+  ].forEach(load => load())
 }
 
 export default load
