5c0414e23b4af5f198816388a9e47e0c045c48a5
chrisnojima <cnojima@keyba.se>
2018-04-22 16:57:28 -0400

    WIP

--- shared/profile/dumb.js
+++ shared/profile/dumb.js
@@ -4,38 +4,6 @@ import Revoke from './revoke'
 import pgpDumb from './pgp/dumb'
 import type {DumbComponentMap} from '../constants/types/more'
 
-const revokeBase = {
-  onCancel: () => console.log('Revoke Proof: clicked Cancel'),
-  onRevoke: () => console.log('Revoke Proof: clicked Revoke'),
-}
-
-const revokeTwitter = {
-  ...revokeBase,
-  platformHandle: 'alexrwendland',
-  platform: 'twitter',
-}
-
-const dumbRevoke: DumbComponentMap<Revoke> = {
-  component: Revoke,
-  mocks: {
-    Twitter: {...revokeTwitter},
-    'Twitter - Error': {
-      ...revokeTwitter,
-      errorMessage: 'There was an error revoking your proof. You can click the button to try again.',
-    },
-    'Twitter - Waiting': {...revokeTwitter, isWaiting: true},
-    Reddit: {...revokeBase, platformHandle: 'malgorithms', platform: 'reddit'},
-    Facebook: {...revokeBase, platformHandle: 'malgorithms', platform: 'facebook'},
-    GitHub: {...revokeBase, platformHandle: 'malgorithms', platform: 'github'},
-    'Hacker News': {...revokeBase, platformHandle: 'malgorithms', platform: 'hackernews'},
-    Bitcoin: {...revokeBase, platformHandle: '1BjgMvwVkpmmJ5HFGZ3L3H1G6fcKLNGT5h', platform: 'btc'},
-    DNS: {...revokeBase, platformHandle: 'chriscoyne.com', platform: 'dns'},
-    Website: {...revokeBase, platformHandle: 'chriscoyne.com', platform: 'http'},
-    'https website': {...revokeBase, platformHandle: 'chriscoyne.com', platform: 'https'},
-    Zcash: {...revokeBase, platformHandle: '1234-fake', platform: 'zcash'},
-  },
-}
-
 const dumbProveWebsiteChoice: DumbComponentMap<ProveWebsiteChoice> = {
   component: ProveWebsiteChoice,
   mocks: {
