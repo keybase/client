c003c5f3663233d5fcd16ca5fc1cff4d47b404a2
chrisnojima <cnojima@keyba.se>
2018-04-23 10:14:20 -0400

    WIP

--- shared/profile/index.stories.js
+++ shared/profile/index.stories.js
@@ -2,11 +2,11 @@
 import confirmOrPending from './confirm-or-pending/index.stories'
 import editAvatar from './edit-avatar/index.stories'
 import editProfile from './edit-profile/index.stories'
-// import pgp from './pgp//index.stories'
+import pgp from './pgp/index.stories'
 import postProof from './post-proof/index.stories'
 import profile from './profile.stories'
 import proveEnter from './prove-enter-username/index.stories'
-// import proveWebsite from './prove-website-choice/index.stories'
+import proveWebsite from './prove-website-choice/index.stories'
 import revoke from './revoke/index.stories'
 
 const load = () => {
@@ -14,11 +14,11 @@ const load = () => {
     confirmOrPending,
     editAvatar,
     editProfile,
-    // pgp,
+    pgp,
     postProof,
     profile,
     proveEnter,
-    // proveWebsite,
+    proveWebsite,
     revoke,
   ].forEach(load => load())
 }
