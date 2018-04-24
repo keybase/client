4d14e5ef7415acc931e1bbda458e08dae8080761
chrisnojima <cnojima@keyba.se>
2018-04-24 10:08:48 -0400

    WIP

--- shared/settings/index.stories.js
+++ shared/settings/index.stories.js
@@ -1,22 +1,16 @@
 // @flow
 import email from './email/index.stories'
-// import passphrase from './passphrase/index.stories'
-// import delete from './delete/index.stories'
-// import deleteConfirm from './delete-confirm/index.stories'
-// import notifications from './notifications/index.stories'
-// import inviteGenerated from './invite-generated/index.stories'
-// import invites from './invites/index.stories'
+import passphrase from './passphrase/index.stories'
+import deleteMe from './delete/index.stories'
+import deleteConfirm from './delete-confirm/index.stories'
+import notifications from './notifications/index.stories'
+import inviteGenerated from './invite-generated/index.stories'
+import invites from './invites/index.stories'
 
 const load = () => {
-  ;[
-    email,
-    // passphrase,
-    // delete,
-    // deleteConfirm,
-    // notifications,
-    // inviteGenerated,
-    // invites,
-  ].forEach(load => load())
+  ;[email, passphrase, deleteMe, deleteConfirm, notifications, inviteGenerated, invites].forEach(load =>
+    load()
+  )
 }
 
 export default load
