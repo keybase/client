da9bfcf828cb90c293ea9233052680b227fcdef8
Maxwell Krohn <themax@gmail.com>
2018-04-26 14:37:47 -0400

    ping RPC for stellard (like gregor's)

--- go/protocol/stellar1/remote.go
+++ go/protocol/stellar1/remote.go
@@ -228,6 +228,9 @@ type IsMasterKeyActiveArg struct {
 	AccountID AccountID            `codec:"accountID" json:"accountID"`
 }
 
+type PingArg struct {
+}
+
 type RemoteInterface interface {
 	Balances(context.Context, BalancesArg) ([]Balance, error)
 	RecentPayments(context.Context, RecentPaymentsArg) ([]PaymentSummary, error)
@@ -235,6 +238,7 @@ type RemoteInterface interface {
 	AccountSeqno(context.Context, AccountSeqnoArg) (string, error)
 	SubmitPayment(context.Context, SubmitPaymentArg) (PaymentResult, error)
 	IsMasterKeyActive(context.Context, IsMasterKeyActiveArg) (bool, error)
+	Ping(context.Context) (string, error)
 }
 
 func RemoteProtocol(i RemoteInterface) rpc.Protocol {
@@ -337,6 +341,17 @@ func RemoteProtocol(i RemoteInterface) rpc.Protocol {
 				},
 				MethodType: rpc.MethodCall,
 			},
+			"ping": {
+				MakeArg: func() interface{} {
+					ret := make([]PingArg, 1)
+					return &ret
+				},
+				Handler: func(ctx context.Context, args interface{}) (ret interface{}, err error) {
+					ret, err = i.Ping(ctx)
+					return
+				},
+				MethodType: rpc.MethodCall,
+			},
 		},
 	}
 }
@@ -372,5 +387,10 @@ func (c RemoteClient) SubmitPayment(ctx context.Context, __arg SubmitPaymentArg)
 
 func (c RemoteClient) IsMasterKeyActive(ctx context.Context, __arg IsMasterKeyActiveArg) (res bool, err error) {
 	err = c.Cli.Call(ctx, "stellar.1.remote.isMasterKeyActive", []interface{}{__arg}, &res)
+	return
+}
+
+func (c RemoteClient) Ping(ctx context.Context) (res string, err error) {
+	err = c.Cli.Call(ctx, "stellar.1.remote.ping", []interface{}{PingArg{}}, &res)
 	return
 }
