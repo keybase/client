edf53f8c111a661e49debce14bafdc1185236cae
Daniel Ayoub <danny.a95@gmail.com>
2018-04-30 16:31:49 -0400

    Transition 'add device', showcased team info, and team menu to FloatingMenu (#11596)
    * add device dropdown
    
    * showcased team info extend floating menu
    
    * showcase team info on desktop
    
    * go over zIndex set on user bio
    
    * done with showcased team info
    
    * team menu

--- shared/teams/team/custom-title/index.js
+++ shared/teams/team/custom-title/index.js
@@ -2,40 +2,50 @@
 import * as React from 'react'
 import {Box, Icon} from '../../../common-adapters'
 import {globalStyles, globalMargins, isMobile} from '../../../styles'
+import {FloatingMenuParentHOC, type FloatingMenuParentProps} from '../../../common-adapters/floating-menu'
+import TeamMenu from '../menu-container'
 
 type Props = {
   onOpenFolder: () => void,
   onChat: () => void,
-  onShowMenu: any => void,
   canChat: boolean,
   canViewFolder: boolean,
+  teamname: string,
 }
 
-const CustomComponent = ({onOpenFolder, onChat, onShowMenu, canChat, canViewFolder}: Props) => (
+const _CustomComponent = (props: Props & FloatingMenuParentProps) => (
   <Box style={{...globalStyles.flexBoxRow, position: 'absolute', alignItems: 'center', right: 0}}>
-    {canChat && (
+    {props.canChat && (
       <Icon
-        onClick={onChat}
+        onClick={props.onChat}
         style={{fontSize: isMobile ? 20 : 16, marginRight: globalMargins.tiny}}
         type="iconfont-chat"
       />
     )}
     {!isMobile &&
-      canViewFolder && (
+      props.canViewFolder && (
         <Icon
-          onClick={onOpenFolder}
+          onClick={props.onOpenFolder}
           style={{fontSize: isMobile ? 20 : 16, marginRight: globalMargins.tiny}}
           type="iconfont-folder-private"
         />
       )}
     <Icon
-      onClick={evt => onShowMenu(isMobile ? undefined : evt.target)}
+      ref={props.setAttachmentRef}
+      onClick={props.toggleShowingMenu}
       type="iconfont-ellipsis"
       style={{
         fontSize: isMobile ? 20 : 16,
         marginRight: globalMargins.tiny,
       }}
     />
+    <TeamMenu
+      attachTo={props.attachmentRef}
+      onHidden={props.toggleShowingMenu}
+      teamname={props.teamname}
+      visible={props.showingMenu}
+    />
   </Box>
 )
+const CustomComponent = FloatingMenuParentHOC(_CustomComponent)
 export default CustomComponent
