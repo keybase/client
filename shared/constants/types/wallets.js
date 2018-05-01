68af89f17431c85636c3c7da3ef074382a937882
Daniel Ayoub <danny.a95@gmail.com>
2018-05-01 16:58:59 -0400

    constants 'n types

--- shared/constants/types/wallets.js
+++ shared/constants/types/wallets.js
@@ -1,6 +1,22 @@
 // @flow
 import * as I from 'immutable'
 
+// An asset that is held by a wallet
+export type _Asset = {
+  balance: string,
+  code: string, // Up to 12-character e.g. BTC, BOFAUSD
+  domain: string, // Verified federation name, empty if unverified
+  issuer: string, // Public key of issuing account
+}
+export type Asset = I.RecordOf<_Asset>
+
+// Reserves held against a wallet's XLM balance
+export type _Reserve = {
+  amount: string,
+  description: string, // e.g. 'account' or 'XLM/Stellar.org trust line'
+}
+export type Reserve = I.RecordOf<_Reserve>
+
 export type _State = {
   hello: string,
 }
--- shared/constants/wallets.js
+++ shared/constants/wallets.js
@@ -2,8 +2,20 @@
 import * as I from 'immutable'
 import * as Types from './types/wallets'
 
+const makeAsset: I.RecordFactory<Types._Asset> = I.Record({
+  balance: '',
+  code: '',
+  domain: '',
+  issuer: '',
+})
+
+const makeReserve: I.RecordFactory<Types._Reserve> = I.Record({
+  amount: '',
+  description: '',
+})
+
 const makeState: I.RecordFactory<Types._State> = I.Record({
   hello: 'world',
 })
 
-export {makeState}
+export {makeAsset, makeReserve, makeState}
