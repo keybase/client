b31ff0f56f736a9d89354ab341dd8c954542955d
Daniel Ayoub <danny.a95@gmail.com>
2018-05-02 16:10:07 -0400

    remove unused type

--- shared/constants/types/wallets.js
+++ shared/constants/types/wallets.js
@@ -1,15 +1,6 @@
 // @flow
 import * as I from 'immutable'
 
-// An asset that is held by an account
-export type _Asset = {
-  balance: string,
-  code: string, // Up to 12-character e.g. BTC, BOFAUSD
-  domain: string, // Verified federation name, empty if unverified
-  issuer: string, // Public key of issuing account
-}
-export type Asset = I.RecordOf<_Asset>
-
 // Reserves held against an account's XLM balance
 export type _Reserve = {
   amount: string,
--- shared/constants/wallets.js
+++ shared/constants/wallets.js
@@ -2,13 +2,6 @@
 import * as I from 'immutable'
 import * as Types from './types/wallets'
 
-const makeAsset: I.RecordFactory<Types._Asset> = I.Record({
-  balance: '',
-  code: '',
-  domain: '',
-  issuer: '',
-})
-
 const makeReserve: I.RecordFactory<Types._Reserve> = I.Record({
   amount: '',
   description: '',
@@ -18,4 +11,4 @@ const makeState: I.RecordFactory<Types._State> = I.Record({
   hello: 'world',
 })
 
-export {makeAsset, makeReserve, makeState}
+export {makeReserve, makeState}
