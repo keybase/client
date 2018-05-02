1b8c2ca6862bb747638a28fc80b193e6390b164c
Daniel Ayoub <danny.a95@gmail.com>
2018-05-02 14:27:16 -0400

    balance summary for native currency and issuer address view for issued assets

--- shared/wallets/asset/index.js
+++ shared/wallets/asset/index.js
@@ -1,12 +1,13 @@
 // @flow
 import * as React from 'react'
 import * as Types from '../../constants/types/wallets'
-import {Box2, ClickableBox, Icon, Text} from '../../common-adapters'
+import {Box2, ClickableBox, Divider, Icon, Text} from '../../common-adapters'
 import {globalMargins} from '../../styles'
 
 export type Props = {
   availableToSend: string, // non-empty only if native currency
   balance: string,
+  code: string, // The same as `name` except for XLM
   equivAvailableToSend: string, // non-empty only if native currency e.g. '$123.45 USD'
   equivBalance: string, // non-empty only if native currency
   expanded: boolean,
@@ -32,15 +33,19 @@ export const Asset = (props: Props) => {
           </Box2>
           <Box2 direction="vertical" style={balanceContainerStyle} fullHeight={true}>
             <Text type="BodySemibold" style={{color: '#814cf4', fontWeight: '800'}}>
-              {props.balance}
+              {props.balance} {props.code}
             </Text>
             <Text type="BodySmall">{props.equivBalance}</Text>
           </Box2>
         </Box2>
       </ClickableBox>
       {props.expanded && (
-        <Box2 direction="horizontal" fullWidth={true}>
-          {props.reserves.length && (
+        <Box2
+          direction="horizontal"
+          fullWidth={true}
+          style={{justifyContent: 'flex-end', paddingRight: globalMargins.small}}
+        >
+          {!!props.reserves.length && (
             <BalanceSummary
               availableToSend={props.availableToSend}
               equivAvailableToSend={props.equivAvailableToSend}
@@ -48,6 +53,7 @@ export const Asset = (props: Props) => {
               total={props.balance}
             />
           )}
+          {!!props.issuerAddress && <IssuerAddress issuerAddress={props.issuerAddress} />}
         </Box2>
       )}
     </Box2>
@@ -78,12 +84,67 @@ type BalanceSummaryProps = {
 
 const BalanceSummary = (props: BalanceSummaryProps) => (
   <Box2 direction="vertical" fullWidth={true} style={balanceSummaryContainerStyle}>
-    <Text type="BodySmall">{props.equivAvailableToSend}</Text>
+    <Divider style={{marginBottom: globalMargins.tiny}} />
+    <Box2 direction="horizontal" fullWidth={true}>
+      <Text type="BodySemibold" style={leftColTextStyle}>
+        Total
+      </Text>
+      <Text type="Body" selectable={true} style={markerBalanceTextStyle}>
+        {props.total}
+      </Text>
+    </Box2>
+    {props.reserves.map(reserve => (
+      <Box2 direction="horizontal" fullWidth={true} key={reserve.description}>
+        <Text type="Body" style={leftColTextStyle}>
+          Reserve ({reserve.description})
+        </Text>
+        <Text type="Body" selectable={true}>
+          -{reserve.amount}
+        </Text>
+      </Box2>
+    ))}
+    <Divider style={{marginBottom: globalMargins.tiny, marginTop: globalMargins.tiny}} />
+    <Box2 direction="horizontal" fullWidth={true} style={{alignItems: 'flex-start'}}>
+      <Text type="BodySemibold" style={leftColTextStyle}>
+        Available to send
+      </Text>
+      <Box2 direction="vertical" style={balanceContainerStyle}>
+        <Text type="Body" selectable={true} style={{fontWeight: '800'}}>
+          {props.availableToSend}
+        </Text>
+        <Text type="BodySmall">{props.equivAvailableToSend}</Text>
+      </Box2>
+    </Box2>
   </Box2>
 )
 
 const balanceSummaryContainerStyle = {
   flexBasis: 355,
 }
+
+const leftColTextStyle = {
+  flex: 1,
+}
+
+// TEMP until new text type
+const markerBalanceTextStyle = {
+  fontWeight: '800',
+}
+
+type IssuerAddressProps = {
+  issuerAddress: string,
+}
+
+const IssuerAddress = (props: IssuerAddressProps) => (
+  <Box2 direction="vertical" fullWidth={true} style={balanceSummaryContainerStyle}>
+    <Text type="Body">Issuer:</Text>
+    <Text type="Body" selectable={true}>
+      {/* TODO (DA) make the full address copyable */}
+      {props.issuerAddress.substr(0, 12) +
+        '..........' +
+        props.issuerAddress.substr(props.issuerAddress.length - 12)}
+    </Text>
+  </Box2>
+)
 
 export default Asset
--- shared/wallets/asset/index.stories.js
+++ shared/wallets/asset/index.stories.js
@@ -1,6 +1,7 @@
 // @flow
 import * as React from 'react'
 import * as Constants from '../../constants/wallets'
+import {withStateHandlers} from '../../util/container'
 import {Box} from '../../common-adapters'
 import {action, storiesOf} from '../../stories/storybook'
 import Asset from '.'
@@ -11,38 +12,77 @@ const common = {
 }
 
 const native = {
-  ...common,
-  availableToSend: '122.0000000 XLM',
-  balance: '123.5000000 XLM',
+  availableToSend: '122.0000000',
+  balance: '123.5000000',
+  code: 'XLM',
   equivAvailableToSend: '$53.41 USD',
   equivBalance: '$54.14 USD',
   issuer: 'Stellar network',
   issuerAddress: '',
   name: 'Lumens',
   reserves: [
-    Constants.makeReserve({amount: '1.0', description: 'account'}),
+    Constants.makeReserve({amount: '1', description: 'account'}),
     Constants.makeReserve({amount: '0.5', description: 'KEYZ/Unknown trust line'}),
   ],
 }
 
 const keyz = {
-  ...common,
-  availableToSend: '12.0000000 KEYZ',
-  balance: '12.0000000 KEYZ',
+  availableToSend: '',
+  balance: '12.0000000',
+  code: 'KEYZ',
   equivAvailableToSend: '',
   equivBalance: '',
   issuer: 'keybase.io',
-  issuerAddress: 'G......',
+  issuerAddress: 'GAXLYHWCWQK273FMHITINCMVTHHRBBNG7A5XWGDYRDDWCR3RSCGLIDWQ',
   name: 'KEYZ',
   reserves: [],
 }
 
+const btc = {
+  availableToSend: '',
+  balance: '0.1324354',
+  code: 'BTC',
+  equivAvailableToSend: '',
+  equivBalance: '',
+  issuer: 'FarcicalBTCAnchor.eg',
+  issuerAddress: 'GAT7ABIQKJ6BBBH7ASKMAV5FMND3YDQLKPFJUCHR7Y5PNRTA7VLA55IW',
+  name: 'BTC',
+  reserves: [],
+}
+
+const batexcellent = {
+  availableToSend: '',
+  balance: '0.0284664',
+  code: 'BATEXCELLENT',
+  equivAvailableToSend: '',
+  equivBalance: '',
+  issuer: 'Unknown',
+  issuerAddress: 'GCN5SJA4CFUC7AVZGEPVVSXDEIBZYA77MAAEIA5ZXZKL5CVTJH6TUL6A',
+  name: 'BATEXCELLENT',
+  reserves: [],
+}
+
+const expandedHOC = withStateHandlers(
+  {expanded: false},
+  {toggleExpanded: ({expanded}) => () => ({expanded: !expanded})}
+)
+const AssetWithExpanded = expandedHOC(Asset)
+
 const load = () => {
   storiesOf('Stellar/Assets', module)
-    .addDecorator(story => <Box style={{width: 520}}>{story()}</Box>)
-    .add('Native currency', () => <Asset {...native} />)
-    .add('Non-native currency', () => <Asset {...keyz} />)
-    .add('Native expanded', () => <Asset {...native} expanded={true} />)
+    .addDecorator(story => <Box style={{maxWidth: 520}}>{story()}</Box>)
+    .add('Native currency', () => <Asset {...common} {...native} />)
+    .add('Non-native currency', () => <Asset {...common} {...keyz} />)
+    .add('Native expanded', () => <Asset {...common} {...native} expanded={true} />)
+    .add('Non-native expanded', () => <Asset {...common} {...keyz} expanded={true} />)
+    .add('List', () => (
+      <React.Fragment>
+        <AssetWithExpanded {...native} />
+        <AssetWithExpanded {...keyz} />
+        <AssetWithExpanded {...btc} />
+        <AssetWithExpanded {...batexcellent} />
+      </React.Fragment>
+    ))
 }
 
 export default load
