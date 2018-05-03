3f0e6c9917aca097c446cf063416ac731a360cc9
Mike Maxim <mike.maxim@gmail.com>
2018-05-03 11:02:24 -0400

    nl

--- go/chat/push.go
+++ go/chat/push.go
@@ -657,8 +657,7 @@ func (g *PushHandler) Activity(ctx context.Context, m gregor.OutOfBandMessage) (
 }
 
 func (g *PushHandler) notifyNewChatActivity(ctx context.Context, uid gregor.UID,
-	convID chat1.ConversationID, conv *chat1.ConversationLocal,
-	activity *chat1.ChatActivity) error {
+	convID chat1.ConversationID, conv *chat1.ConversationLocal, activity *chat1.ChatActivity) error {
 	kbUID, err := keybase1.UIDFromString(hex.EncodeToString(uid.Bytes()))
 	if err != nil {
 		return err
