8c0073172ee186a257167e19d230c02ebbf57b65
Daniel Ayoub <danny.a95@gmail.com>
2018-05-03 11:18:06 -0400

    extend rn-start instead

--- shared/react-native/packageAndBuild.sh
+++ shared/react-native/packageAndBuild.sh
@@ -1,12 +1,34 @@
 #!/usr/bin/env bash
 # starts up the RN packager & requests both ios and android bundles
 
+set -e -u -o pipefail # Fail on error
+
+dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd $dir/..
+arg=${1:-}
+case $arg in 
+  "ios"|"android"|"both"|"")
+    ;;
+  *)
+    echo "Invalid build type. Valid types are 'ios', 'android', or 'both'." >&2
+    exit 1;;
+esac
+
 requestbundles ()
 {
+  if [ "$arg" == "" ]; then
+    return
+  fi
   printf "\nRequesting bundles...\n"
-  curl -s -o /dev/null localhost:8081/index.ios.bundle 2>&1 
-  curl -s -o /dev/null localhost:8081/index.android.bundle 2>&1
+  case $arg in 
+    "ios"|"both")
+      curl -s -o /dev/null localhost:8081/index.ios.bundle 2>&1;;
+  esac
+  case $arg in
+    "android"|"both")
+      curl -s -o /dev/null localhost:8081/index.android.bundle 2>&1
+  esac
 }
-# give the packager time to start
+
 sleep 5 && requestbundles &
-yarn rn-start 
+./node_modules/react-native/scripts/packager.sh --resetCache
