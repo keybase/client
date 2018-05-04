fbc649489d349a64c9bba266b5685cc6f38f7453
Michał Zochniak <michal@zapu.net>
2018-05-04 19:29:43 +0200

    Change storage version back to 9
    it's better to miss this optimizations for some teams rather than have
    everyone reload keybasefriends

--- go/teams/storage.go
+++ go/teams/storage.go
@@ -89,7 +89,7 @@ type DiskStorage struct {
 }
 
 // Increment to invalidate the disk cache.
-const diskStorageVersion = 10
+const diskStorageVersion = 9
 
 type DiskStorageItem struct {
 	Version int                `codec:"V"`
