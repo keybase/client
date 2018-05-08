9abdbfc96e36a5265b556b8e2d4dc48f4b28c19c
Frederick Akalin <akalin@keyba.se>
2018-05-08 15:45:11 -0700

    Add TODO

--- shared/wallets/transaction/index.js
+++ shared/wallets/transaction/index.js
@@ -84,6 +84,7 @@ const styles = styleSheetCreate({
   },
   detailContainer: {},
   note: platformStyles({
+    // TODO: Consider using markdown quoting.
     common: {
       marginTop: globalMargins.xtiny,
     },
