1b282f24e726030239a203b2a8f02631ece08710
Daniel Ayoub <danny.a95@gmail.com>
2018-05-08 10:54:12 -0400

    add prop provider to send form stories

--- shared/wallets/send-form/memo/container.js
+++ shared/wallets/send-form/memo/container.js
@@ -1,6 +1,6 @@
 // @flow
 import Memo from '.'
-import {connect, type TypedState, type Dispatch} from '../../../util/container'
+import {compose, connect, setDisplayName, type TypedState, type Dispatch} from '../../../util/container'
 
 const mapStateToProps = (state: TypedState) => ({})
 
@@ -8,4 +8,4 @@ const mapDispatchToProps = (dispatch: Dispatch) => ({})
 
 const mergeProps = (stateProps, dispatchProps) => ({})
 
-export default connect(mapStateToProps, mapDispatchToProps, mergeProps)(Memo)
+export default compose(connect(mapStateToProps, mapDispatchToProps, mergeProps), setDisplayName('Memo'))(Memo)
