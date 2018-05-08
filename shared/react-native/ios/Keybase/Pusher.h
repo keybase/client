85c4a3b41c2764bb8fde09f1cfe71f7f77ca0b60
Mike Maxim <mike@keyba.se>
2018-05-08 12:26:25 -0400

    dont use a background task and bypass JS for iOS plaintext notifications CORE-7494 (#11715)
    * wip
    
    * works
    
    * flesh
    
    * add stale check
    
    * cleanup
    
    * check type
    
    * native rework
    
    * works
    
    * logging
    
    * typo
    
    * fix
    
    * move this back

---
+++ shared/react-native/ios/Keybase/Pusher.h
@@ -0,0 +1,17 @@
+//
+//  Pusher.h
+//  Keybase
+//
+//  Created by Michael Maxim on 5/7/18.
+//  Copyright © 2018 Keybase. All rights reserved.
+//
+
+#ifndef Pusher_h
+#define Pusher_h
+#import <keybase/keybase.h>
+
+@interface PushNotifier : NSObject<KeybasePushNotifier> {
+}
+@end
+
+#endif /* Pusher_h */
