ec7af08aa7c6b533e12170b7cdc1355727287bc6
Mike Maxim <mike.maxim@gmail.com>
2018-05-09 16:36:49 -0400

    pull this out

--- go/chat/convsource.go
+++ go/chat/convsource.go
@@ -839,15 +839,13 @@ func (s *HybridConversationSource) PullLocalOnly(ctx context.Context, convID cha
 	// if the caller is ok with receiving placeholders
 	var iboxMaxMsgID chat1.MessageID
 	if maxPlaceholders > 0 {
-		_, iboxRes, _, err := storage.NewInbox(s.G(), uid).Read(ctx, &chat1.GetInboxQuery{
-			ConvID: &convID,
-		}, nil)
+		iboxRes, err := storage.NewInbox(s.G(), uid).GetConversation(ctx, convID)
 		if err != nil {
 			s.Debug(ctx, "PullLocalOnly: failed to read inbox for conv, not using: %s", err)
-		} else if len(iboxRes) != 1 || iboxRes[0].Conv.ReaderInfo == nil {
-			s.Debug(ctx, "PullLocalOnly: no conv returned for conv, not using")
+		} else if iboxRes.Conv.ReaderInfo == nil {
+			s.Debug(ctx, "PullLocalOnly: no reader infoconv returned for conv, not using")
 		} else {
-			iboxMaxMsgID = iboxRes[0].Conv.ReaderInfo.MaxMsgid
+			iboxMaxMsgID = iboxRes.Conv.ReaderInfo.MaxMsgid
 			s.Debug(ctx, "PullLocalOnly: found ibox max msgid: %d", iboxMaxMsgID)
 		}
 	}
