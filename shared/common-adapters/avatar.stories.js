7b38a04796ba799079925ede7cb68a359758ce7d
chrisnojima <cnojima@keyba.se>
2018-05-14 17:08:41 -0400

    WIP

--- shared/common-adapters/avatar.stories.js
+++ shared/common-adapters/avatar.stories.js
@@ -6,7 +6,7 @@ import Box from './box'
 import {globalStyles} from '../styles'
 import {storiesOf, action} from '../stories/storybook'
 
-const sizes = [176, 112, 80, 64, 48, 32, 16]
+const sizes = [128, 96, 64, 48, 32, 16]
 
 const load = () => {
   storiesOf('Common', module).add('Avatar', () =>
