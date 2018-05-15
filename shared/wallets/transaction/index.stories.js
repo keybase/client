5f19d10f9abfc1182f624146eaf913c321ea6146
Frederick Akalin <akalin@keyba.se>
2018-05-14 23:02:29 -0700

    More

--- shared/wallets/transaction/index.stories.js
+++ shared/wallets/transaction/index.stories.js
@@ -19,80 +19,71 @@ const beforeLastWeek = moment(now)
   .subtract(8, 'days')
   .toDate()
 
+const singleEmojiNote = '🎁'
 const shortNote = 'Short note.'
 const longNote =
   'Stellar deal!! You guys rock. This is to show a very long private note. Blah blah blah blah. Plus, emojis. 🍺'
 
-const makeConfigs = () => {
+const addConfigs = (stories, namePrefix, storyFn) => {
   const roles = [{yourRole: 'sender'}, {yourRole: 'receiver'}]
-  const times = [
-    {timestamp: yesterday},
-    {timestamp: lastWeek},
-    {timestamp: beforeLastWeek},
-    {timestamp: null},
-  ]
-  const notes = [{note: shortNote}, {note: longNote}]
   const sizes = [{large: true}, {large: false}]
+  const notesAndTimes = [
+    {note: shortNote, timestamp: yesterday},
+    {note: longNote, timestamp: lastWeek},
+    {note: singleEmojiNote, timestamp: beforeLastWeek},
+    // Pending.
+    {note: shortNote, timestamp: null},
+  ]
 
-  const configs = []
   roles.forEach(r => {
-    times.forEach(t => {
-      notes.forEach(n => {
-        sizes.forEach(s => {
-          configs.push({...r, ...t, ...n, ...s})
+    sizes.forEach(s => {
+      stories.add(namePrefix + ` (${r.yourRole} - ${s.large ? 'large' : 'small'})`, () => {
+        const components = []
+        notesAndTimes.forEach(t => {
+          components.push(storyFn({key: components.length, ...r, ...s, ...t}))
         })
+        return components
       })
     })
   })
-  return configs
 }
 
-const configs = makeConfigs()
-
 const load = () => {
-  storiesOf('Wallets/Transaction', module)
+  const stories = storiesOf('Wallets/Transaction', module)
     .addDecorator(provider)
     .addDecorator(story => (
       <Box2 direction="vertical" style={{maxWidth: 520}}>
         {story()}
       </Box2>
     ))
-    .add('Keybase User', () =>
-      configs.map((config, i) => (
-        <Transaction
-          key={i}
-          counterparty="paul"
-          counterpartyType="keybaseUser"
-          amountUser="$12.50"
-          amountXLM="53.1688643 XLM"
-          {...config}
-        />
-      ))
-    )
-    .add('Stellar Public Key', () =>
-      configs.map((config, i) => (
-        <Transaction
-          key={i}
-          counterparty="G43289XXXXX34OPL"
-          counterpartyType="stellarPublicKey"
-          amountUser="$12.50"
-          amountXLM="53.1688643 XLM"
-          {...config}
-        />
-      ))
-    )
-    .add('Wallet', () =>
-      configs.map((config, i) => (
-        <Transaction
-          key={i}
-          counterparty="Second wallet"
-          counterpartyType="wallet"
-          amountUser="$12.50"
-          amountXLM="53.1688643 XLM"
-          {...config}
-        />
-      ))
-    )
+
+  addConfigs(stories, 'Keybase User', config => (
+    <Transaction
+      counterparty="paul"
+      counterpartyType="keybaseUser"
+      amountUser="$12.50"
+      amountXLM="53.1688643 XLM"
+      {...config}
+    />
+  ))
+  addConfigs(stories, 'Stellar Public Key', config => (
+    <Transaction
+      counterparty="G43289XXXXX34OPL"
+      counterpartyType="stellarPublicKey"
+      amountUser="$12.50"
+      amountXLM="53.1688643 XLM"
+      {...config}
+    />
+  ))
+  addConfigs(stories, 'Wallet', config => (
+    <Transaction
+      counterparty="Second wallet"
+      counterpartyType="wallet"
+      amountUser="$12.50"
+      amountXLM="53.1688643 XLM"
+      {...config}
+    />
+  ))
 }
 
 export default load
