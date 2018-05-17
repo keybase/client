850e0855b20aa33a2209609d103e7e3352fc81e7
chrisnojima <cnojima@keyba.se>
2018-05-17 16:40:22 -0400

    WIP

---
+++ shared/settings/web-links.native.js
@@ -0,0 +1,22 @@
+// @flow
+import {HeaderHoc, NativeWebView} from '../common-adapters/native'
+import {connect, compose, defaultProps, type TypedState} from '../util/container'
+
+const mapStateToProps = (state: TypedState, {routeProps}) => ({
+  source: routeProps.get('source'),
+  title: routeProps.get('title'),
+})
+
+const mapDispatchToProps = (dispatch: Dispatch, {navigateUp}) => ({
+  onBack: () => dispatch(navigateUp()),
+})
+
+const WebLinks = compose(
+  connect(mapStateToProps, mapDispatchToProps),
+  defaultProps({
+    dataDetectorTypes: 'none',
+  }),
+  HeaderHoc
+)(NativeWebView)
+
+export default WebLinks
