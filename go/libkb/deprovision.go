6c5523a11da8dbb67ae836e9ad2642fd689631b3
Joshua Blum <jblum18@gmail.com>
2018-05-18 15:54:23 -0400

    Don't fail on errors

--- go/libkb/deprovision.go
+++ go/libkb/deprovision.go
@@ -1,9 +1,5 @@
 package libkb
 
-import (
-	"fmt"
-)
-
 // XXX: THIS DELETES SECRET KEYS. Deleting the wrong secret keys can make you
 // lose all your data forever. We only run this in the DeprovisionEngine and if
 // we detect that our device was revoked in LogoutAndDeprovisionIfRevoked.
@@ -12,6 +8,8 @@ func ClearSecretsOnDeprovision(m MetaContext, username NormalizedUsername) error
 	// 2. Delete the user's ephemeralKeys
 	// 3. Delete the user from the config file.
 	// 4. Db nuke.
+
+	epick := FirstErrorPicker{}
 
 	var logger func(string, ...interface{})
 	if m.UIs().LogUI == nil {
@@ -28,35 +26,27 @@ func ClearSecretsOnDeprovision(m MetaContext, username NormalizedUsername) error
 	// do this to the wrong user. Please do not copy this code :)
 	logger("Deleting %s's secret keys file...", username.String())
 	filename := m.G().SKBFilenameForUser(username)
-	if err := ShredFile(filename); err != nil {
-		return fmt.Errorf("Failed to delete secret key file: %s", err)
-	}
+	epick.Push(ShredFile(filename))
 
 	logger("Deleting %s's ephemeralKeys...", username.String())
 	// NOTE: We only store userEK/teamEK boxes locally and these are removed in
 	// the LocalDb.Nuke() below so we just delete any deviceEKs here.
 	deviceEKStorage := m.G().GetDeviceEKStorage()
 	if deviceEKStorage != nil {
-		if err := deviceEKStorage.ForceDeleteAll(m.Ctx(), username); err != nil {
-			return err
-		}
+		epick.Push(deviceEKStorage.ForceDeleteAll(m.Ctx(), username))
 	}
 
 	logger("Deleting %s from config.json...", username.String())
-	if err := m.SwitchUserDeprovisionNukeConfig(username); err != nil {
-		return err
-	}
+	epick.Push(m.SwitchUserDeprovisionNukeConfig(username))
 
 	logger("Clearing the local cache db...")
-	if _, err := m.G().LocalDb.Nuke(); err != nil {
-		return err
-	}
+	_, err := m.G().LocalDb.Nuke()
+	epick.Push(err)
 
 	logger("Clearing the local cache chat db...")
-	if _, err := m.G().LocalChatDb.Nuke(); err != nil {
-		return err
-	}
+	_, err = m.G().LocalChatDb.Nuke()
+	epick.Push(err)
 
 	logger("Deprovision finished.")
-	return nil
+	return epick.Error()
 }
