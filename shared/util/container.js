cdd524de59ad7747d2defee9dfc167b9eaa77a45
chrisnojima <cnojima@keyba.se>
2018-05-21 18:37:41 -0400

    new hoc for discussion

--- shared/util/container.js
+++ shared/util/container.js
@@ -2,6 +2,8 @@
 import * as I from 'immutable'
 import {isEqualWith} from 'lodash-es'
 import {createSelector, createSelectorCreator, defaultMemoize} from 'reselect'
+import {compose, setDisplayName} from 'recompose'
+import {connect} from 'react-redux'
 
 const createShallowEqualSelector = createSelectorCreator(defaultMemoize, (a, b) =>
   isEqualWith(a, b, (a, b, indexOrKey, object, other, stack) => (stack ? a === b : undefined))
@@ -11,7 +13,10 @@ const createImmutableEqualSelector = createSelectorCreator(defaultMemoize, I.is)
 
 const NullComponent = () => null
 
-export {connect} from 'react-redux'
+// flowlint deprecated-type:off
+const storybookableConnect = (name: string, ...rest: Array<*>) =>
+  compose(connect(...rest), setDisplayName(name))
+
 export {
   branch,
   compose,
@@ -30,12 +35,14 @@ export {
 export {default as createCachedSelector} from 're-reselect'
 export type {TypedState} from '../constants/reducer'
 export {
+  connect,
   createShallowEqualSelector,
   createImmutableEqualSelector,
   createSelector,
   createSelectorCreator,
   defaultMemoize,
   NullComponent,
+  storybookableConnect,
 }
 export {Dispatch} from '../constants/types/flux'
 export {isMobile} from '../constants/platform'
