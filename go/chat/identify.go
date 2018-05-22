8b427e76e4ff9046a9dbf647087fec3417ea7bfd
Mike Maxim <mike@keyba.se>
2018-05-22 17:51:03 -0400

    check for nil uid before processing (#11983)
    * check for nil uid before processing
    
    * change

--- go/chat/identify.go
+++ go/chat/identify.go
@@ -130,7 +130,10 @@ func (h *IdentifyChangedHandler) getUsername(ctx context.Context, uid keybase1.U
 
 func (h *IdentifyChangedHandler) getTLFtoCrypt(ctx context.Context, uid gregor1.UID) (string, chat1.TLFID, error) {
 
-	me := h.G().Env.GetUID()
+	me := h.G().ActiveDevice.UID()
+	if me.IsNil() {
+		return "", nil, libkb.LoggedInError{}
+	}
 	inbox := storage.NewInbox(h.G(), me.ToBytes())
 
 	_, allConvs, err := inbox.ReadAll(ctx)
