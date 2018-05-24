b28fdb0ab5f91387e86fee58a04bbea83ce2d85e
chrisnojima <cnojima@keyba.se>
2018-05-24 10:22:57 -0400

    WIP

--- shared/login/forms/index.stories.js
+++ shared/login/forms/index.stories.js
@@ -1,34 +1,42 @@
 // @flow
 import * as React from 'react'
 import {action, storiesOf} from '../../stories/storybook'
-import {Intro, Splash, Failure} from '.'
+// import {Intro, Failure} from '.'
+import Splash from './splash'
 
-const props = {
-  bootStatus: 'bootStatusLoading',
-  justDeletedSelf: null,
-  justLoginFromRevokedDevice: null,
-  justRevokedSelf: null,
-  onFeedback: action('onFeedback'),
-  onLogin: action('onLogin'),
-  onRetry: action('onRetry'),
-  onSignup: action('onSignup'),
-  retrying: false,
-}
+// const props = {
+// bootStatus: 'bootStatusLoading',
+// justDeletedSelf: null,
+// justLoginFromRevokedDevice: null,
+// justRevokedSelf: null,
+// onFeedback: action('onFeedback'),
+// onLogin: action('onLogin'),
+// onRetry: action('onRetry'),
+// onSignup: action('onSignup'),
+// retrying: false,
+// }
 
 const load = () => {
-  storiesOf('Login/Forms', module)
-    .add('Intro: First time user', () => <Intro {...props} bootStatus="bootStatusBootstrapped" />)
-    .add('Intro: User who just revoked device', () => (
-      <Intro {...props} bootStatus="bootStatusBootstrapped" justRevokedSelf="DEVICE_NAME" />
+  // storiesOf('Login/Intro', module)
+  // .add('First time user', () => <Intro {...props} bootStatus="bootStatusBootstrapped" />)
+  // .add('User who just revoked device', () => (
+  // <Intro {...props} bootStatus="bootStatusBootstrapped" justRevokedSelf="DEVICE_NAME" />
+  // ))
+  // .add('User who just deleted self', () => (
+  // <Intro {...props} bootStatus="bootStatusBootstrapped" justDeletedSelf="hal9000" />
+  // ))
+  // .add('User who tried to login from revoked device', () => (
+  // <Intro {...props} bootStatus="bootStatusBootstrapped" justLoginFromRevokedDevice="DEVICE_NAME" />
+  // ))
+  storiesOf('Login', module)
+    .add('Splash', () => <Splash status="Loading" onRetry={null} onFeedback={null} />)
+    .add('Failure', () => (
+      <Splash status="Something went wrong" onRetry={action('onRetry')} onFeedback={null} />
     ))
-    .add('Intro: User who just deleted self', () => (
-      <Intro {...props} bootStatus="bootStatusBootstrapped" justDeletedSelf="hal9000" />
+    .add('Failure feedback', () => (
+      <Splash status="Something went wrong" onRetry={action('onRetry')} onFeedback={action('onFeedback')} />
     ))
-    .add('Intro: User who tried to login from revoked device', () => (
-      <Intro {...props} bootStatus="bootStatusBootstrapped" justLoginFromRevokedDevice="DEVICE_NAME" />
-    ))
-    .add('Splash', () => <Splash {...props} />)
-    .add('Failure', () => <Failure {...props} bootStatus="bootStatusFailure" />)
+  // storiesOf('Login', module).add('Failure', () => <Failure {...props} bootStatus="bootStatusFailure" />)
 }
 
 export default load
