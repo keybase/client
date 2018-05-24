ba56e56f4c6a40a6301af9534b647343b0eeb945
Daniel Ayoub <danny.a95@gmail.com>
2018-05-24 16:21:27 -0400

    check pushState before updating teamswithchosenchannels

--- shared/actions/teams.js
+++ shared/actions/teams.js
@@ -3,7 +3,6 @@ import logger from '../logger'
 import {map, last, upperFirst} from 'lodash-es'
 import * as I from 'immutable'
 import * as SearchGen from './search-gen'
-import * as GregorGen from './gregor-gen'
 import * as TeamsGen from './teams-gen'
 import * as Types from '../constants/types/teams'
 import * as Constants from '../constants/teams'
@@ -1029,27 +1028,47 @@ function _updateTopic(action: TeamsGen.UpdateTopicPayload, state: TypedState) {
   ])
 }
 
-function _addTeamWithChosenChannels(action: TeamsGen.AddTeamWithChosenChannelsPayload, state: TypedState) {
+function* _addTeamWithChosenChannels(action: TeamsGen.AddTeamWithChosenChannelsPayload) {
+  const state = yield Saga.select()
   const {teamname} = action.payload
   if (state.teams.teamsWithChosenChannels.has(teamname)) {
+    // we've already dismissed for this team and we already know about it, bail
     return
   }
-  const newTeamsWithChosenChannels = state.teams.teamsWithChosenChannels.add(teamname)
-  // We'd actually like to do this in one message to avoid having the UI glitch
-  // momentarily inbetween the dismiss (and therefore thinking no teams have
-  // had channels selected) and the re-inject.  For now, we have a workaround
-  // of ignoring empty updates from the clearing in the reducer.  (CORE-7663.)
-  return Saga.sequentially([
-    Saga.call(RPCTypes.gregorDismissCategoryRpcPromise, {
-      category: 'chosenChannelsForTeam',
-    }),
-    Saga.put(
-      GregorGen.createInjectItem({
-        body: JSON.stringify(newTeamsWithChosenChannels.toJSON()),
-        category: 'chosenChannelsForTeam',
-      })
-    ),
-  ])
+  const pushState = yield Saga.call(RPCTypes.gregorGetStateRpcPromise)
+  const item = pushState.items.find(i => i.item.category === Constants.chosenChannelsGregorKey)
+  let teams = []
+  let msgID
+  if (item && item.item && item.item.body) {
+    const body = item.item.body
+    msgID = item.md.msgID
+    teams = JSON.parse(body.toString())
+  }
+  teams.push(teamname)
+  // make sure there're no dupes
+  teams = I.Set(teams).toArray()
+
+  const dtime = {
+    offset: 0,
+    time: 0,
+  }
+  // update if exists, else create
+  if (msgID) {
+    logger.info(`Updating teamsWithChosenChannels for team ${teamname}`)
+    yield Saga.call(RPCTypes.gregorUpdateItemRpcPromise, {
+      body: JSON.stringify(teams),
+      cat: Constants.chosenChannelsGregorKey,
+      dtime,
+      msgID,
+    })
+  } else {
+    logger.info(`Creating teamsWithChosenChannels with team ${teamname}`)
+    yield Saga.call(RPCTypes.gregorInjectItemRpcPromise, {
+      body: JSON.stringify(teams),
+      cat: Constants.chosenChannelsGregorKey,
+      dtime,
+    })
+  }
 }
 
 function _updateChannelname(action: TeamsGen.UpdateChannelNamePayload, state: TypedState) {
@@ -1201,7 +1220,7 @@ const teamsSaga = function*(): Saga.SagaGenerator<any, any> {
   yield Saga.safeTakeEvery(TeamsGen.getTeamRetentionPolicy, _getTeamRetentionPolicy)
   yield Saga.safeTakeEveryPure(TeamsGen.saveTeamRetentionPolicy, _saveTeamRetentionPolicy)
   yield Saga.safeTakeEveryPure(Chat2Gen.updateTeamRetentionPolicy, _updateTeamRetentionPolicy)
-  yield Saga.safeTakeEveryPure(TeamsGen.addTeamWithChosenChannels, _addTeamWithChosenChannels)
+  yield Saga.safeTakeEvery(TeamsGen.addTeamWithChosenChannels, _addTeamWithChosenChannels)
 }
 
 export default teamsSaga
