18cd5dce933b5ff1a1b6d86606c4e61518f39c40
Adam Spooner <adam@keyba.se>
2018-05-25 17:39:39 -0400

    Use the ellipsis icon for more user actions.

--- shared/profile/user-actions.js
+++ shared/profile/user-actions.js
@@ -3,7 +3,7 @@ import * as React from 'react'
 import {Box2, Button, ClickableBox, FloatingMenu, FollowButton, ButtonBar, Icon} from '../common-adapters'
 import {FloatingMenuParentHOC, type FloatingMenuParentProps} from '../common-adapters/floating-menu'
 import {normal as proofNormal} from '../constants/tracker'
-import {globalColors} from '../styles'
+import {globalColors, isMobile, platformStyles, styleSheetCreate} from '../styles'
 import type {SimpleProofState} from '../constants/types/tracker'
 
 type Props = {
@@ -53,7 +53,7 @@ function UserActions({
             onAddToTeam={onAddToTeam}
             onOpenPrivateFolder={onOpenPrivateFolder}
             onBrowsePublicFolder={onBrowsePublicFolder}
-          />{' '}
+          />
         </ButtonBar>
       )
     } else {
@@ -66,7 +66,7 @@ function UserActions({
             onOpenPrivateFolder={onOpenPrivateFolder}
             onBrowsePublicFolder={onBrowsePublicFolder}
             onUnfollow={onUnfollow}
-          />{' '}
+          />
         </ButtonBar>
       )
     }
@@ -108,6 +108,9 @@ class _DropdownButton extends React.PureComponent<DropdownProps & FloatingMenuPa
     },
     {
       onClick: () => this.props.onOpenPrivateFolder(),
+      style: {
+        borderTopWidth: 0,
+      },
       title: 'Open private folder',
     },
     {
@@ -132,20 +135,52 @@ class _DropdownButton extends React.PureComponent<DropdownProps & FloatingMenuPa
         ref={this.props.setAttachmentRef}
       >
         <Box2 direction="horizontal" fullWidth={true} gap="xsmall">
-          <Button onClick={null} type="Secondary" label="..." />
+          <Button onClick={null} type="Secondary" style={iconButton}>
+            <Icon
+              color={globalColors.black_75}
+              fontSize={isMobile ? 21 : 16}
+              style={ellipsisIcon}
+              type="iconfont-ellipsis"
+            />
+          </Button>
         </Box2>
         <FloatingMenu
           attachTo={this.props.attachmentRef}
           closeOnSelect={true}
+          containerStyle={styles.floatingMenu}
           items={this._menuItems}
           onHidden={this.props.toggleShowingMenu}
+          position="bottom right"
           visible={this.props.showingMenu}
-          position="bottom center"
         />
       </ClickableBox>
     )
   }
 }
+
+const ellipsisIcon = platformStyles({
+  common: {
+    position: 'relative',
+    top: 1,
+  },
+})
+
+const iconButton = platformStyles({
+  isElectron: {
+    paddingLeft: 16,
+    paddingRight: 16,
+  },
+  isMobile: {
+    paddingLeft: 12,
+    paddingRight: 12,
+  },
+})
+
+const styles = styleSheetCreate({
+  floatingMenu: {
+    marginTop: 4,
+  },
+})
 
 const DropdownButton = FloatingMenuParentHOC(_DropdownButton)
 
