dcdd02cd1a00391faf925a9e667902cc5a850986
Chris Ball <chris@printf.net>
2018-05-24 17:26:42 -0700

    When we receive a retention expunge, update the snippet using the supplied conv

--- shared/reducers/chat2.js
+++ shared/reducers/chat2.js
@@ -117,7 +117,9 @@ const metaMapReducer = (metaMap, action) => {
         action.payload.metas.forEach(meta => {
           map.update(meta.conversationIDKey, old => {
             if (old) {
-              return action.payload.fromEphemeralPurge ? meta : Constants.updateMeta(old, meta)
+              return action.payload.fromEphemeralPurge || action.payload.fromExpunge
+                ? meta
+                : Constants.updateMeta(old, meta)
             } else {
               return neverCreate ? old : meta
             }
