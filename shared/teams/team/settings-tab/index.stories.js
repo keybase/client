46173d49913c735bfefd755c520693970c1e3abe
Frederick Akalin <akalin@keyba.se>
2018-05-25 14:01:38 -0700

    Fix story

--- shared/teams/team/settings-tab/index.stories.js
+++ shared/teams/team/settings-tab/index.stories.js
@@ -1,49 +1,68 @@
 // @flow
 import * as React from 'react'
 import {Box} from '../../../common-adapters'
-import {action, storiesOf} from '../../../stories/storybook'
+import {action, storiesOf, createPropProvider} from '../../../stories/storybook'
+import {makeRetentionPolicy} from '../../../constants/teams'
 import {globalStyles} from '../../../styles'
 import {Settings} from './'
 
 const commonProps = {
-  isBigTeam: false,
-  ignoreAccessRequests: false,
-  publicityAnyMember: false,
-  publicityMember: false,
-  publicityTeam: false,
-  openTeam: false,
+  isBigTeam: true,
+  ignoreAccessRequests: true,
+  publicityAnyMember: true,
+  publicityMember: true,
+  publicityTeam: true,
+  openTeam: true,
   openTeamRole: 'admin',
   savePublicity: action('savePublicity'),
   setOpenTeamRole: action('setOpenTeamRole'),
   teamname: 'myteam',
   yourOperations: {
-    manageMembers: false,
-    manageSubteams: false,
-    createChannel: false,
-    chat: false,
-    deleteChannel: false,
-    renameChannel: false,
-    editChannelDescription: false,
-    setTeamShowcase: false,
-    setMemberShowcase: false,
-    setRetentionPolicy: false,
-    changeOpenTeam: false,
-    leaveTeam: false,
-    joinTeam: false,
-    setPublicityAny: false,
-    listFirst: false,
-    changeTarsDisabled: false,
-    deleteChatHistory: false,
+    manageMembers: true,
+    manageSubteams: true,
+    createChannel: true,
+    chat: true,
+    deleteChannel: true,
+    renameChannel: true,
+    editChannelDescription: true,
+    setTeamShowcase: true,
+    setMemberShowcase: true,
+    setRetentionPolicy: true,
+    changeOpenTeam: true,
+    leaveTeam: true,
+    joinTeam: true,
+    setPublicityAny: true,
+    listFirst: true,
+    changeTarsDisabled: true,
+    deleteChatHistory: true,
   },
-  waitingForSavePublicity: false,
+  waitingForSavePublicity: true,
 }
+
+const provider = createPropProvider({
+  RetentionPicker: () => ({
+    _loadTeamPolicy: action('_loadTeamPolicy'),
+    _loadTeamOperations: action('_loadTeamOperations'),
+    canSetPolicy: true,
+    policy: makeRetentionPolicy({type: 'retain'}),
+    loading: false,
+    showInheritOption: false,
+    showOverrideNotice: true,
+    showSaveIndicator: true,
+    type: 'auto',
+    saveRetentionPolicy: action('saveRetentionPolicy'),
+    onSelect: action('onSelect'),
+    onShowWarning: action('onShowWarning'),
+  }),
+})
 
 const load = () => {
   storiesOf('Teams/Settings', module)
+    .addDecorator(provider)
     .addDecorator(story => (
       <Box style={{...globalStyles.flexBoxCenter, ...globalStyles.fillAbsolute}}>{story()}</Box>
     ))
-    .add('Settings', () => <Settings {...commonProps} />)
+    .add('Everything', () => <Settings {...commonProps} />)
 }
 
 export default load
