9d0087670e9570ab26b623a03360ef691ff41955
Mike Maxim <mike@keyba.se>
2018-05-29 20:43:45 -0400

    dont let Merge wipe out cache, just merge on per conv versions (#12089)
    * dont let Merge wipe out cache, just merge on per conv versions
    
    * fixes
    
    * mistake
    
    * fix debug
    
    * fix test

--- go/chat/server_test.go
+++ go/chat/server_test.go
@@ -3643,9 +3643,10 @@ func TestChatSrvRetentionSweepTeam(t *testing.T) {
 		defer ctc.cleanup()
 		users := ctc.users()
 		ctx := ctc.as(t, users[0]).startCtx
-
+		_ = ctc.as(t, users[1]).startCtx
 		for i, u := range users {
 			t.Logf("user[%v] %v %v", i, u.Username, u.User.GetUID())
+			ctc.world.Tcs[u.Username].ChatG.Syncer.(*Syncer).isConnected = true
 		}
 
 		listener := newServerChatListener()
