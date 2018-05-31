3350c94d0c13f0a9636aaf8bd318c94d17d91784
Steve Sanders <steve@keyba.se>
2018-05-31 16:39:42 -0700

    suggest dokan version >= 1.1.0.1000 in GUI

--- go/install/fuse_status_windows.go
+++ go/install/fuse_status_windows.go
@@ -6,11 +6,41 @@
 package install
 
 import (
+	"errors"
 	"path/filepath"
 
+	"github.com/gonutz/w32"
 	"github.com/keybase/client/go/libkb"
 	"github.com/keybase/client/go/protocol/keybase1"
 )
+
+func isDokanCurrent(log Log, path string) (bool, error) {
+	size := w32.GetFileVersionInfoSize(path)
+	if size <= 0 {
+		return false, errors.New("GetFileVersionInfoSize failed")
+	}
+
+	info := make([]byte, size)
+	ok := w32.GetFileVersionInfo(path, info)
+	if !ok {
+		return false, errors.New("GetFileVersionInfo failed")
+	}
+
+	fixed, ok := w32.VerQueryValueRoot(info)
+	if !ok {
+		return false, errors.New("VerQueryValueRoot failed")
+	}
+	version := fixed.FileVersion()
+
+	major := version & 0xFFFF000000000000 >> 48
+	minor := version & 0x0000FFFF00000000 >> 32
+	patch := version & 0x00000000FFFF0000 >> 16
+	build := version & 0x000000000000FFFF >> 0
+
+	log.Info("dokan1.dll version: %d.%d.%d.%d\n", major, minor, patch, build)
+	// we're looking for 1.1.0.1000
+	return (major >= 1 && minor >= 1 && build >= 1000), nil
+}
 
 func detectDokanDll(log Log) bool {
 	dir, err := libkb.SystemDir()
@@ -18,9 +48,17 @@ func detectDokanDll(log Log) bool {
 		log.Info("detectDokanDll error getting system directory: %v", err)
 		return false
 	}
-
-	exists, _ := libkb.FileExists(filepath.Join(dir, "dokan1.dll"))
+	dokanPath := filepath.Join(dir, "dokan1.dll")
+	exists, _ := libkb.FileExists(dokanPath)
 	log.Info("detectDokanDll: returning %v", exists)
+	if exists {
+		current, err := isDokanCurrent(log, dokanPath)
+		if err != nil {
+			log.Errorf(err.Error())
+		} else {
+			exists = current
+		}
+	}
 	return exists
 }
 
