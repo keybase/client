4c1e533ca2720c973bebd9b42bf169d1fbc343c2
Maxwell Krohn <themax@gmail.com>
2018-06-01 17:16:37 -0400

    more robust strategy for finding next merkle root after revokes (#12155)
    * more robust strategy for finding next merkle root after revokes
    
    - handle bursts of merkle activity with two binary searches
    - a new utility, `keybase fnmr`, to test this on prod data
    - tested by hand, successfully, with:
    
    ```sh
    $ ~/src/go/bin/keybase --run-mode=prod --home=/tmp/x --standalone fnmr -k 88 e0e2b660297259e41145c049c8d60d19  # the user who found this bug, in the problem spot of his sigchain
    {"res":{"seqno":2105929,"hashMeta":"68bff3427d80ffed287c4a73bfdeda4c2f31581417320ee77a4691197a508ee7"}}
    ```
    
    and:
    
    ```sh
    $ ~/src/go/bin/keybase --run-mode=prod --home=/tmp/x --standalone fnmr -k 342 dbb165b7879fe7b1174df73bed0b9500 # normal revoke, should only take one step
    {"res":{"seqno":1548866,"hashMeta":"556629a0569625bd1e0b778ca621acea7e26a0250857088efa519cdea2afe9b9"}}
    ```
    
    - warning: it's not possible to use this function call on merkle roots older than:
    
    ```go
    var FirstProdMerkleTreeWithModernShape = keybase1.Seqno(531408)
    ```
    
    * comment from review
    
    * better error condition

---
+++ go/client/cmd_fnmr.go
@@ -0,0 +1,132 @@
+// Copyright 2015 Keybase, Inc. All rights reserved. Use of
+// this source code is governed by the included BSD license.
+
+package client
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+
+	"golang.org/x/net/context"
+
+	"github.com/keybase/cli"
+	"github.com/keybase/client/go/libcmdline"
+	"github.com/keybase/client/go/libkb"
+	keybase1 "github.com/keybase/client/go/protocol/keybase1"
+)
+
+type modeFNMR int
+
+const (
+	modeFNMRNone   modeFNMR = 0
+	modeFNMRRevoke modeFNMR = 1
+)
+
+type cmdFNMR struct {
+	libkb.Contextified
+	mode  modeFNMR
+	uid   keybase1.UID
+	seqno keybase1.Seqno
+	upak  keybase1.UPAKVersioned
+}
+
+func NewCmdFNMR(cl *libcmdline.CommandLine, g *libkb.GlobalContext) cli.Command {
+	return cli.Command{
+		Name:         "fnmr",
+		Description:  "Find next merkle Root",
+		ArgumentHelp: "uid",
+		Flags: []cli.Flag{
+			cli.IntFlag{
+				Name:  "revoke,k",
+				Usage: "sequence # of the revoke to query",
+			},
+		},
+		Action: func(c *cli.Context) {
+			cl.ChooseCommand(&cmdFNMR{Contextified: libkb.NewContextified(g)}, "fnmr", c)
+		},
+	}
+}
+
+func (c *cmdFNMR) runRevoke(ctx context.Context, cli keybase1.UserClient) error {
+	v, err := c.upak.V()
+	if err != nil {
+		return err
+	}
+	if v != keybase1.UPAKVersion_V2 {
+		return fmt.Errorf("wanted V2 upak, got something else")
+	}
+	var devKey *keybase1.PublicKeyV2NaCl
+	for _, i := range c.upak.V2().AllIncarnations() {
+		for _, k := range i.DeviceKeys {
+			if k.Base.Revocation != nil && k.Base.IsSibkey && k.Base.Revocation.SigChainLocation.Seqno == c.seqno {
+				tmp := k
+				devKey = &tmp
+			}
+		}
+	}
+	if devKey == nil {
+		return fmt.Errorf("can't find device key for revocation")
+	}
+	c.G().Log.CDebugf(ctx, "Found device key: %+v", *devKey)
+	revk := devKey.Base.Revocation
+	res, err := cli.FindNextMerkleRootAfterRevoke(ctx, keybase1.FindNextMerkleRootAfterRevokeArg{
+		Uid:  c.uid,
+		Kid:  devKey.Base.Kid,
+		Loc:  revk.SigChainLocation,
+		Prev: revk.PrevMerkleRootSigned,
+	})
+	if err != nil {
+		return err
+	}
+	jsonOut, err := json.Marshal(res)
+	if err != nil {
+		return err
+	}
+	c.G().UI.GetTerminalUI().Output(string(jsonOut) + "\n")
+	return nil
+}
+
+func (c *cmdFNMR) Run() error {
+	userClient, err := GetUserClient(c.G())
+	if err != nil {
+		return err
+	}
+	ctx := context.Background()
+
+	c.upak, err = userClient.GetUPAK(ctx, c.uid)
+	if err != nil {
+		return err
+	}
+
+	switch c.mode {
+	case modeFNMRRevoke:
+		return c.runRevoke(ctx, userClient)
+	default:
+		return fmt.Errorf("No operation mode found; try one of: {-r}")
+	}
+}
+
+func (c *cmdFNMR) ParseArgv(ctx *cli.Context) error {
+	if len(ctx.Args()) != 1 {
+		return errors.New("need a UID argument")
+	}
+	var err error
+	c.uid, err = keybase1.UIDFromString(ctx.Args()[0])
+	if err != nil {
+		return err
+	}
+	i := ctx.Int("revoke")
+	if i > 0 {
+		c.mode = modeFNMRRevoke
+		c.seqno = keybase1.Seqno(i)
+	}
+	return nil
+}
+
+func (c *cmdFNMR) GetUsage() libkb.Usage {
+	return libkb.Usage{
+		Config: true,
+		API:    true,
+	}
+}
