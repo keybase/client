ed97a85981069faf4208fb984c630aba3fdd8832
John Zila <john@jzila.com>
2018-06-05 09:33:35 -0700

    [KBFS-2892] TLF Resets in the Row (#11960)
    * build a new action for getting reset metadata
    
    * fs: implement basic reset retrieval action
    
    * fs: Make inbox RPC for resets work
    
    * fs: Rename reset actions
    
    * fs: Load resets properly
    
    * fs: Remove unnecessary TODO
    
    * fs: Reset rows working
    
    * fs: Fix Flow
    
    * fs: Fix lint
    
    * fs: First pass at PR feedback
    
    * fs: fix Flow
    
    * fs: Switch let to const
    
    * fs: Cleanup a bit in the reducer
    
    * fs: deconstruct some vars
    
    * fs: Show team resets as well
    
    * fs: PR feedback
    
    * Fix path item text for reset folders
    
    * fs: Handle your user having been reset
    
    * fs: Half fix flow
    
    * fs: "Fix" Flow
    
    * fs: Actually fix Flow.
    
    * fs: Fix text formatting on reset text
    
    * fs: Fix bad formatting

--- shared/actions/fs/index.js
+++ shared/actions/fs/index.js
@@ -4,7 +4,10 @@ import * as Constants from '../../constants/fs'
 import * as FsGen from '../fs-gen'
 import * as I from 'immutable'
 import * as RPCTypes from '../../constants/types/rpc-gen'
+import * as RPCChatTypes from '../../constants/types/rpc-chat-gen'
 import * as Saga from '../../util/saga'
+import * as EngineRpc from '../../constants/engine'
+import * as ChatConstants from '../../constants/chat2'
 import engine from '../../engine'
 import * as NotificationsGen from '../notifications-gen'
 import * as Types from '../../constants/types/fs'
@@ -409,6 +412,71 @@ function* openPathItem(action: FsGen.OpenPathItemPayload): Saga.SagaGenerator<an
   )
 }
 
+const inboxQuery = {
+  ...ChatConstants.makeInboxQuery([]),
+  computeActiveList: false,
+  tlfVisibility: RPCTypes.commonTLFVisibility.any,
+}
+
+const {team, impteamnative, impteamupgrade} = RPCChatTypes.commonConversationMembersType
+
+function* loadResets(action: FsGen.LoadResetsPayload): Saga.SagaGenerator<any, any> {
+  // TODO: maybe uncomment?
+  // const conversations = yield Saga.call(
+  //   RpcChatTypes.localFindConversationsLocalRpcPromise,
+  //   action.payload.tlfName
+  // )
+  const resetRpc = new EngineRpc.EngineRpcCall(
+    {
+      'chat.1.chatUi.chatInboxUnverified': function*({
+        inbox,
+      }: RPCChatTypes.ChatUiChatInboxUnverifiedRpcParam) {
+        const result: RPCChatTypes.UnverifiedInboxUIItems = JSON.parse(inbox)
+        // whatever
+        if (!result || !result.items) return EngineRpc.rpcResult()
+        const tlfs: Array<[Types.Path, Types.ResetMetadata]> = result.items.reduce((filtered, item: RPCChatTypes.UnverifiedInboxUIItem) => {
+          const visibility = item.visibility === RPCTypes.commonTLFVisibility.private
+                ? item.membersType === team
+                  ? 'team'
+                  : 'private'
+                : 'public'
+          const name = item.name
+          const path = Types.stringToPath(`/keybase/${visibility}/${name}`)
+          if (
+            item &&
+              item.localMetadata &&
+              item.localMetadata.resetParticipants &&
+              // Ignore KBFS-backed TLFs
+              [team, impteamnative, impteamupgrade].includes(item.membersType)
+          ) {
+            filtered.push([
+              path, {
+                name,
+                visibility,
+                resetParticipants: item.localMetadata.resetParticipants || [],
+              },
+            ])
+          }
+          return filtered
+        }, [])
+        yield Saga.put(FsGen.createLoadResetsResult({tlfs: I.Map(tlfs)}))
+        return EngineRpc.rpcResult()
+      },
+    },
+    RPCChatTypes.localGetInboxNonblockLocalRpcChannelMap,
+    'tlfCall',
+    {
+      identifyBehavior: RPCTypes.tlfKeysTLFIdentifyBehavior.chatGui,
+      maxUnbox: 0,
+      query: inboxQuery,
+      skipUnverified: false,
+    },
+    false,
+    loading => {}
+  )
+  yield Saga.call(resetRpc.run)
+}
+
 function* fsSaga(): Saga.SagaGenerator<any, any> {
   yield Saga.safeTakeEveryPure(
     FsGen.refreshLocalHTTPServerInfo,
@@ -422,6 +490,7 @@ function* fsSaga(): Saga.SagaGenerator<any, any> {
   yield Saga.safeTakeEvery(FsGen.favoritesLoad, listFavoritesSaga)
   yield Saga.safeTakeEvery(FsGen.favoriteIgnore, ignoreFavoriteSaga)
   yield Saga.safeTakeEveryPure(FsGen.mimeTypeLoad, loadMimeType)
+  yield Saga.safeTakeEvery(FsGen.loadResets, loadResets)
 
   if (!isMobile) {
     // TODO: enable these when we need it on mobile.
