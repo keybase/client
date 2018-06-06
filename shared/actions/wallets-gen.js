ebb45b1102463390a1fae686ee23300aa84ab931
Chris Ball <chris@printf.net>
2018-06-06 08:27:29 -0700

    Stellar integration: First pass, sagas and reducers (#12182)
    * Start on wallet integration
    
    * reducer is working
    
    * load wallets on wallet tab
    
    * work on asset loading
    
    * assets load working
    
    * transactions starting to load.. overwrites previous tho
    
    * prettier
    
    * use I.List, not []
    
    * flow
    
    * use a single debugging action for loading everything at once
    
    * remove debugging
    
    * Refactor, rename actions, fix any types
    
    * Don't need immutable in actions anymore
    
    * Rename wallet to account per Patrick
    
    * __DEV__ only for loadEverything
    
    * Type maps
    
    * tidy up
    
    * use setIn
    
    * more review

--- shared/actions/wallets-gen.js
+++ shared/actions/wallets-gen.js
@@ -8,22 +8,76 @@ import * as Types from '../constants/types/wallets'
 
 // Constants
 export const resetStore = 'common:resetStore' // not a part of wallets but is handled by every reducer
-export const walletsRefresh = 'wallets:walletsRefresh'
+export const accountsReceived = 'wallets:accountsReceived'
+export const assetsReceived = 'wallets:assetsReceived'
+export const loadAccounts = 'wallets:loadAccounts'
+export const loadAssets = 'wallets:loadAssets'
+export const loadEverything = 'wallets:loadEverything'
+export const loadPayments = 'wallets:loadPayments'
+export const paymentsReceived = 'wallets:paymentsReceived'
 
 // Payload Types
-type _WalletsRefreshPayload = void
+type _AccountsReceivedPayload = $ReadOnly<{|accounts: Array<Types.Account>|}>
+type _AssetsReceivedPayload = $ReadOnly<{|
+  accountID: string,
+  assets: Array<Types.Assets>,
+|}>
+type _LoadAccountsPayload = void
+type _LoadAssetsPayload = $ReadOnly<{|accountID: string|}>
+type _LoadEverythingPayload = void
+type _LoadPaymentsPayload = $ReadOnly<{|accountID: string|}>
+type _PaymentsReceivedPayload = $ReadOnly<{|
+  accountID: string,
+  payments: Array<Types.Payment>,
+|}>
 
 // Action Creators
 /**
- * Refresh out list of wallets
+ * Debugging only -- load accounts/assets/payments for every account at once
  */
-export const createWalletsRefresh = (payload: _WalletsRefreshPayload) => ({error: false, payload, type: walletsRefresh})
+export const createLoadEverything = (payload: _LoadEverythingPayload) => ({error: false, payload, type: loadEverything})
+/**
+ * Refresh our list of accounts
+ */
+export const createLoadAccounts = (payload: _LoadAccountsPayload) => ({error: false, payload, type: loadAccounts})
+/**
+ * Refresh our list of assets for a given account
+ */
+export const createLoadAssets = (payload: _LoadAssetsPayload) => ({error: false, payload, type: loadAssets})
+/**
+ * Refresh our list of payments for a given account
+ */
+export const createLoadPayments = (payload: _LoadPaymentsPayload) => ({error: false, payload, type: loadPayments})
+/**
+ * Update our store of account data
+ */
+export const createAccountsReceived = (payload: _AccountsReceivedPayload) => ({error: false, payload, type: accountsReceived})
+/**
+ * Update our store of assets data
+ */
+export const createAssetsReceived = (payload: _AssetsReceivedPayload) => ({error: false, payload, type: assetsReceived})
+/**
+ * Update our store of payments data
+ */
+export const createPaymentsReceived = (payload: _PaymentsReceivedPayload) => ({error: false, payload, type: paymentsReceived})
 
 // Action Payloads
-export type WalletsRefreshPayload = $Call<typeof createWalletsRefresh, _WalletsRefreshPayload>
+export type AccountsReceivedPayload = $Call<typeof createAccountsReceived, _AccountsReceivedPayload>
+export type AssetsReceivedPayload = $Call<typeof createAssetsReceived, _AssetsReceivedPayload>
+export type LoadAccountsPayload = $Call<typeof createLoadAccounts, _LoadAccountsPayload>
+export type LoadAssetsPayload = $Call<typeof createLoadAssets, _LoadAssetsPayload>
+export type LoadEverythingPayload = $Call<typeof createLoadEverything, _LoadEverythingPayload>
+export type LoadPaymentsPayload = $Call<typeof createLoadPayments, _LoadPaymentsPayload>
+export type PaymentsReceivedPayload = $Call<typeof createPaymentsReceived, _PaymentsReceivedPayload>
 
 // All Actions
 // prettier-ignore
 export type Actions =
-  | WalletsRefreshPayload
+  | AccountsReceivedPayload
+  | AssetsReceivedPayload
+  | LoadAccountsPayload
+  | LoadAssetsPayload
+  | LoadEverythingPayload
+  | LoadPaymentsPayload
+  | PaymentsReceivedPayload
   | {type: 'common:resetStore', payload: void}
