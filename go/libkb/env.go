4cfe14291b955753ad3a25badd962fcf8b130fc0
Mike Maxim <mike@keyba.se>
2018-06-07 09:35:41 -0400

    mistake

--- go/libkb/env.go
+++ go/libkb/env.go
@@ -684,7 +684,7 @@ func (e *Env) GetGregorPingInterval() time.Duration {
 }
 
 func (e *Env) GetGregorPingTimeout() time.Duration {
-	return e.GetDuration(30*time.Second,
+	return e.GetDuration(5*time.Second,
 		func() (time.Duration, bool) { return e.getEnvDuration("KEYBASE_PUSH_PING_TIMEOUT") },
 		func() (time.Duration, bool) { return e.GetConfig().GetGregorPingTimeout() },
 		func() (time.Duration, bool) { return e.cmd.GetGregorPingTimeout() },
