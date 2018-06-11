66b7e23e9c260ae6a6c7bcf175cbee3ead00fd73
Antonio Marcedone <a.marcedone@gmail.com>
2018-06-11 15:36:17 -0400

    Added escaping for stellar wallet

--- go/client/stellar_common.go
+++ go/client/stellar_common.go
@@ -2,19 +2,25 @@ package client
 
 import (
 	"fmt"
+	"os"
 	"strings"
+
+	"github.com/keybase/client/go/terminalescaper"
+	isatty "github.com/mattn/go-isatty"
 
 	"github.com/keybase/client/go/libkb"
 	"github.com/keybase/client/go/protocol/stellar1"
 )
 
 func printPayment(g *libkb.GlobalContext, p stellar1.PaymentCLILocal, verbose bool, dui libkb.DumbOutputUI) {
-	// TODO whitelist Colorstrings which are fine to print, escape the rest.
+	lineUnescaped := func(format string, args ...interface{}) {
+		dui.PrintfUnescaped(format+"\n", args...)
+	}
 	line := func(format string, args ...interface{}) {
 		dui.Printf(format+"\n", args...)
 	}
 	timeStr := p.Time.Time().Format("2006/01/02 15:04")
-	line("%v", ColorString(g, "bold", timeStr))
+	lineUnescaped("%v", ColorString(g, "bold", timeStr))
 	amount := fmt.Sprintf("%v XLM", libkb.StellarSimplifyAmount(p.Amount))
 	if !p.Asset.IsNativeXLM() {
 		amount = fmt.Sprintf("%v %v/%v", p.Amount, p.Asset.Code, p.Asset.Issuer)
@@ -22,7 +28,7 @@ func printPayment(g *libkb.GlobalContext, p stellar1.PaymentCLILocal, verbose bo
 	if p.DisplayAmount != nil && p.DisplayCurrency != nil && len(*p.DisplayAmount) > 0 && len(*p.DisplayAmount) > 0 {
 		amount = fmt.Sprintf("%v %v (%v)", *p.DisplayAmount, *p.DisplayCurrency, amount)
 	}
-	line("%v", ColorString(g, "green", amount))
+	lineUnescaped("%v", ColorString(g, "green", amount))
 	// Show sender and recipient. Prefer keybase form, fall back to stellar abbreviations.
 	var showedAbbreviation bool
 	var from string
@@ -42,7 +48,7 @@ func printPayment(g *libkb.GlobalContext, p stellar1.PaymentCLILocal, verbose bo
 		showedAbbreviation = true
 	default:
 		// This should never happen
-		line("%v", ColorString(g, "red", "missing recipient info"))
+		lineUnescaped("%v", ColorString(g, "red", "missing recipient info"))
 	}
 	line("%v -> %v", from, to)
 	// If an abbreviation was shown, show the full addresses
@@ -51,14 +57,23 @@ func printPayment(g *libkb.GlobalContext, p stellar1.PaymentCLILocal, verbose bo
 		if p.ToStellar != nil {
 			line("To:   %v", p.ToStellar.String())
 		} else {
-			line("To:   %v", ColorString(g, "yellow", "unclaimed"))
+			lineUnescaped("To:   %v", ColorString(g, "yellow", "unclaimed"))
 		}
 	}
-	if len(p.Note) > 0 {
-		line("Note: %v", ColorString(g, "yellow", printPaymentFilterNote(p.Note)))
-	}
-	if len(p.NoteErr) > 0 {
-		line("Note Error: %v", ColorString(g, "red", p.NoteErr))
+	if g.Env.GetDisplayRawUntrustedOutput() || !isatty.IsTerminal(os.Stdout.Fd()) {
+		if len(p.Note) > 0 {
+			lineUnescaped("Note: %v", ColorString(g, "yellow", printPaymentFilterNote(p.Note)))
+		}
+		if len(p.NoteErr) > 0 {
+			lineUnescaped("Note Error: %v", ColorString(g, "red", p.NoteErr))
+		}
+	} else {
+		if len(p.Note) > 0 {
+			lineUnescaped("Note: %v", ColorString(g, "yellow", printPaymentFilterNote(terminalescaper.Clean(p.Note))))
+		}
+		if len(p.NoteErr) > 0 {
+			lineUnescaped("Note Error: %v", ColorString(g, "red", terminalescaper.Clean(p.NoteErr)))
+		}
 	}
 	if verbose {
 		line("Transaction Hash: %v", p.TxID)
@@ -71,8 +86,8 @@ func printPayment(g *libkb.GlobalContext, p stellar1.PaymentCLILocal, verbose bo
 		if cicmp(p.Status, "claimable") {
 			color = "yellow"
 		}
-		line("Status: %v", ColorString(g, color, p.Status))
-		line("        %v", ColorString(g, color, p.StatusDetail))
+		lineUnescaped("Status: %v", ColorString(g, color, p.Status))
+		lineUnescaped("        %v", ColorString(g, color, p.StatusDetail))
 	}
 }
 
