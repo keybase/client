01acf4d290a7ae8d1109a5bd24a2a341d2d85149
chrisnojima <cnojima@keyba.se>
2018-06-12 15:55:36 -0400

    WIP

--- shared/common-adapters/floating-box.desktop.js
+++ shared/common-adapters/floating-box.desktop.js
@@ -8,32 +8,24 @@ import type {Props} from './floating-box'
 const StyleOnlyBox = (props: any) => <Box children={props.children} />
 const RelativeFloatingBox = ModalPositionRelative(StyleOnlyBox)
 
-type State = {
-  targetRect: ?ClientRect,
-}
-
-class FloatingBox extends React.Component<Props, State> {
-  state = {targetRect: null}
-
-  static getDerivedStateFromProps(nextProps: Props, prevState: State) {
-    if (!nextProps.attachTo) {
-      return {targetRect: null}
-    }
-    const node = findDOMNode(nextProps.attachTo)
-    return node instanceof window.HTMLElement
-      ? {targetRect: node.getBoundingClientRect()}
-      : {targetRect: null}
-  }
-
+class FloatingBox extends React.Component<Props> {
   _onHidden = () => {
     this.props.onHidden && this.props.onHidden()
   }
 
   render() {
+    let targetRect = null
+    if (this.props.attachTo) {
+      const node = findDOMNode(this.props.attachTo)
+      if (node instanceof window.HTMLElement) {
+        targetRect = node.getBoundingClientRect()
+      }
+    }
+
     return (
       <RelativeFloatingBox
         position={this.props.position || 'bottom center'}
-        targetRect={this.state.targetRect}
+        targetRect={targetRect}
         onClosePopup={this._onHidden}
         propagateOutsideClicks={this.props.propagateOutsideClicks}
         style={this.props.containerStyle}
