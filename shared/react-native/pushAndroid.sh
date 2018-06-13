fd1a8931b7c677b22f4e2b7117f1166ddef6a9c3
Frederick Akalin <akalin@keyba.se>
2018-06-13 16:28:30 -0700

    Add pushAndroid script

---
+++ shared/react-native/pushAndroid.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e -u -o pipefail # Fail on error
+
+dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+IFS=: read -a GOPATH_ARRAY <<< "$GOPATH"
+GOPATH0=${GOPATH_ARRAY[0]}
+
+adb push $GOPATH0/src/github.com/keybase/client/shared/react-native/android/app/build/outputs/apk/app-debug.apk /data/local/tmp/io.keybase.ossifrage.debug
+adb shell pm install -t -r "/data/local/tmp/io.keybase.ossifrage.debug"
+adb shell am start -n "io.keybase.ossifrage.debug/io.keybase.ossifrage.MainActivity" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER
