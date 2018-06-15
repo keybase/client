0988a4b1c6754a742234c8602b0ebbd977a16c50
Frederick Akalin <akalin@keyba.se>
2018-06-15 14:42:50 -0700

    Stub container

---
+++ shared/wallets/wallet-list/container.js
@@ -0,0 +1,49 @@
+// @flow
+import {WalletList} from '.'
+import {connect, type TypedState, type Dispatch} from '../../util/container'
+
+const common = {
+  isSelected: false,
+  name: '',
+  keybaseUser: '',
+  contents: '',
+}
+
+const mockWallets = [
+  {
+    ...common,
+    keybaseUser: 'cecileb',
+    isSelected: true,
+    name: "cecileb's wallet",
+    contents: '280.0871234 XLM + more',
+  },
+  {
+    ...common,
+    name: 'Second wallet',
+    contents: '56.9618203 XLM',
+  },
+]
+
+const mapStateToProps = (state: TypedState) => ({
+  wallets: mockWallets,
+})
+
+const mapDispatchToProps = (dispatch: Dispatch, {navigateUp}) => ({
+  onAddNew: () => {
+    console.log('TODO: onAddNew')
+  },
+  onLinkExisting: () => {
+    console.log('TODO: onAddNew')
+  },
+})
+
+const mergeProps = (stateProps, dispatchProps) => ({
+  wallets: stateProps.wallets,
+  onSelect: (name: string) => {
+    console.log(`TODO: stub out ${name}`)
+  },
+  onAddNew: dispatchProps.onAddNew,
+  onLinkExisting: dispatchProps.onLinkExisting,
+})
+
+export default connect(mapStateToProps, mapDispatchToProps, mergeProps)(WalletList)
