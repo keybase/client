e0c286b50956f93ecb157828ff4d7397759490e3
chrisnojima <cnojima@keyba.se>
2018-06-15 15:33:15 -0400

    WIP

--- shared/login/register/code-page2/index.stories.js
+++ shared/login/register/code-page2/index.stories.js
@@ -1,83 +1,19 @@
 // @flow
 import * as React from 'react'
-import CodePage from '.'
+import CodePage2 from '.'
 import {action, storiesOf} from '../../../stories/storybook'
-import {
-  qrGenerate,
-  codePageDeviceRoleExistingPhone,
-  codePageDeviceRoleExistingComputer,
-} from '../../../constants/login'
+
+const textCode = 'scrub disagree sheriff holiday cabin habit mushroom member four'
 
 const props = {
-  cameraBrokenMode: false,
-  enterCodeErrorText: '',
-  enterText: 'Foo Enter Text',
-  mode: 'codePageModeScanCode',
-  myDeviceRole: 'codePageDeviceRoleNewPhone',
-  onBack: action('onBack'),
-  onChangeText: action('onChangeText'),
-  onKeyDown: action('onKeyDown'),
-  qrCode: qrGenerate('derive jump shop federal'),
-  qrCodeScanned: false,
-  qrScanned: action('qrScanned'),
-  resetQRCodeScanned: action('resetQRCodeScanned'),
-  setCameraBrokenMode: action('setCameraBrokenMode'),
-  setCodePageMode: action('setCodePageMode'),
-  textCode: 'derive jump shop federal member moral clip prevent vast wear critic purple mistake',
-  textEntered: action('textEntered'),
+  textCode,
+  textCodeInstructions: ',w',
+  qrCode: qrGenerate(textCode),
+  qrCodeInstructions: '',
 }
 
 const load = () => {
-  storiesOf('Register/CodePage', module)
-    .add('Scan Code (Mobile)', () => (
-      <CodePage {...props} mode={'codePageModeScanCode'} otherDeviceRole={codePageDeviceRoleExistingPhone} />
-    ))
-    .add('Scanned Code (Mobile)', () => (
-      <CodePage
-        {...props}
-        mode={'codePageModeScanCode'}
-        otherDeviceRole={codePageDeviceRoleExistingPhone}
-        myDeviceRole={codePageDeviceRoleExistingPhone}
-        qrCodeScanned={true}
-      />
-    ))
-    .add('Show Text (Mobile)', () => (
-      <CodePage {...props} mode={'codePageModeShowText'} otherDeviceRole={codePageDeviceRoleExistingPhone} />
-    ))
-    .add('Enter Text (Mobile)', () => (
-      <CodePage {...props} mode={'codePageModeEnterText'} otherDeviceRole={codePageDeviceRoleExistingPhone} />
-    ))
-    .add('Show Code (Mobile)', () => (
-      <CodePage {...props} mode={'codePageModeShowCode'} otherDeviceRole={codePageDeviceRoleExistingPhone} />
-    ))
-    .add('Show Text', () => (
-      <CodePage
-        {...props}
-        mode={'codePageModeShowText'}
-        otherDeviceRole={codePageDeviceRoleExistingComputer}
-      />
-    ))
-    .add('Enter Text', () => (
-      <CodePage
-        {...props}
-        mode={'codePageModeEnterText'}
-        otherDeviceRole={codePageDeviceRoleExistingComputer}
-      />
-    ))
-    .add('Show Code', () => (
-      <CodePage
-        {...props}
-        mode={'codePageModeShowCode'}
-        otherDeviceRole={codePageDeviceRoleExistingComputer}
-      />
-    ))
-    .add('Scan Code', () => (
-      <CodePage
-        {...props}
-        mode={'codePageModeScanCode'}
-        otherDeviceRole={codePageDeviceRoleExistingComputer}
-      />
-    ))
+  storiesOf('Register/CodePage2', module).add('Scan Code (Mobile)', () => <CodePage2 {...props} />)
 }
 
 export default load
