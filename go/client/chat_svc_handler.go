4554ca6903ced0a330a492a17f3094153c46b352
Joshua Blum <jblum18@gmail.com>
2018-06-15 09:38:22 -0400

    Add current username to json api if not present (#12343)
    * Add current username to json api if not present
    
    * fix send

--- go/client/chat_svc_handler.go
+++ go/client/chat_svc_handler.go
@@ -837,8 +837,9 @@ func (c *chatServiceHandler) makePostHeader(ctx context.Context, arg sendArgV1, 
 		if arg.channel.TopicName != "" {
 			topicName = &arg.channel.TopicName
 		}
+		channelName := c.normalizeChannelName(arg.channel.Name)
 		ncres, err := client.NewConversationLocal(ctx, chat1.NewConversationLocalArg{
-			TlfName:          arg.channel.Name,
+			TlfName:          channelName,
 			TlfVisibility:    visibility,
 			TopicName:        topicName,
 			TopicType:        tt,
@@ -1026,17 +1027,19 @@ func (c *chatServiceHandler) findConversation(ctx context.Context, convIDStr str
 	var conv chat1.ConversationLocal
 	var rlimits []chat1.RateLimit
 
-	if (channel == ChatChannel{}) && len(convIDStr) == 0 {
+	if channel.IsNil() && len(convIDStr) == 0 {
 		return conv, rlimits, errors.New("missing conversation specificer")
 	}
 
 	var convID chat1.ConversationID
-	if channel == (ChatChannel{}) {
+	if channel.IsNil() {
 		var err error
 		convID, err = chat1.MakeConvID(convIDStr)
 		if err != nil {
 			return conv, rlimits, fmt.Errorf("invalid conversation ID: %s", convIDStr)
 		}
+	} else {
+		channel.Name = c.normalizeChannelName(channel.Name)
 	}
 
 	existing, existingRl, err := c.getExistingConvs(ctx, convID, channel)
@@ -1053,6 +1056,14 @@ func (c *chatServiceHandler) findConversation(ctx context.Context, convIDStr str
 	}
 
 	return existing[0], rlimits, nil
+}
+
+// If we have a channel name but the current username isn't present, add it.
+func (c *chatServiceHandler) normalizeChannelName(channelName string) string {
+	if channelName != "" && !strings.Contains(channelName, c.G().Env.GetUsername().String()) {
+		channelName = fmt.Sprintf("%s,%s", channelName, c.G().Env.GetUsername())
+	}
+	return channelName
 }
 
 func TopicTypeFromStrDefault(str string) (chat1.TopicType, error) {
