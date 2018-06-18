af0958ae172090553a489c3bb388d86e0ee59b21
Adam Spooner <adam@keyba.se>
2018-06-18 10:04:49 -0400

    Add some state.
    We need this for determining cropping coordinates.

--- shared/common-adapters/zoomable-box.ios.js
+++ shared/common-adapters/zoomable-box.ios.js
@@ -3,15 +3,46 @@ import * as React from 'react'
 import ScrollView from './scroll-view'
 import type {Props} from './zoomable-box'
 
-export const ZoomableBox = (props: Props) => (
-  <ScrollView
-    children={props.children}
-    contentContainerStyle={props.contentContainerStyle}
-    style={props.style}
-    minimumZoomScale={1}
-    maximumZoomScale={props.maxZoom || 3}
-    scrollsToTop={false}
-    indicatorStyle="white"
-    alwaysBounceVertical={false}
-  />
-)
+type State = {
+  height: number,
+  offsetX: number,
+  offsetY: number,
+  width: number,
+}
+
+class ZoomableBox extends React.Component<Props, State> {
+  state = {
+    height: 0,
+    offsetX: 0,
+    offsetY: 0,
+    width: 0,
+  }
+
+  onScroll = (e: Object) => {
+    this.setState({
+      height: e.nativeEvent.contentSize.height,
+      offsetX: e.nativeEvent.contentOffset.x,
+      offsetY: e.nativeEvent.contentOffset.y,
+      width: e.nativeEvent.contentSize.width,
+    })
+  }
+
+  render() {
+    return (
+      <ScrollView
+        alwaysBounceVertical={false}
+        children={this.props.children}
+        contentContainerStyle={this.props.contentContainerStyle}
+        indicatorStyle="white"
+        maximumZoomScale={this.props.maxZoom || 3}
+        minimumZoomScale={1}
+        onScroll={this.onScroll}
+        scrollEventThrottle={16}
+        scrollsToTop={false}
+        style={this.props.style}
+      />
+    )
+  }
+}
+
+export {ZoomableBox}
