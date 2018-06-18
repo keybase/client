ec77bc154b8698a6f8d39710a86ca0bd0b92170b
Adam Spooner <adam@keyba.se>
2018-06-18 10:05:12 -0400

    Update mobile.
    The cropping coordinates are wrong.

--- shared/profile/edit-avatar/index.native.js
+++ shared/profile/edit-avatar/index.native.js
@@ -2,23 +2,63 @@
 import * as React from 'react'
 import {Box, ButtonBar, StandardScreen, WaitingButton} from '../../common-adapters'
 import {NativeImage, ZoomableBox} from '../../common-adapters/mobile.native'
-import {globalColors, globalStyles, globalMargins, platformStyles} from '../../styles'
+import {globalColors, globalStyles, globalMargins, platformStyles, styleSheetCreate} from '../../styles'
+import {isIOS} from '../../constants/platform'
 import type {Props} from '.'
 
+const AVATAR_SIZE = 250
+
 class EditAvatar extends React.Component<Props> {
+  _zoom: ?ZoomableBox
+
   _onSave = () => {
-    // this.props.onSave(this.props.filename)
+    const filename = isIOS ? this.props.image.uri.replace('file://', '') : this.props.image.path
+    const crop = isIOS ? this._getIOSCrop() : this._getAndroidCrop()
+    this.props.onSave(filename, crop)
+  }
+
+  _getAndroidCrop = () => {
+    const x = -this._zoom.state.pan.x
+    const y = -this._zoom.state.pan.y
+    const scale = this._zoom.state.scale - 1
+    const rH = this.props.image.height * scale
+    const rW = this.props.image.width * scale
+    const x0 = rW * x
+    const y0 = rH * y
+    return {
+      x0: Math.round(x0),
+      y0: Math.round(y0),
+      x1: Math.round(x0 + AVATAR_SIZE * rW),
+      y1: Math.round(y0 + AVATAR_SIZE * rH),
+    }
+  }
+
+  _getIOSCrop = () => {
+    const x = this._zoom.state.offsetX
+    const y = this._zoom.state.offsetY
+    const rH = this.props.image.height / this._zoom.state.height
+    const rW = this.props.image.width / this._zoom.state.width
+    const x0 = rW * x
+    const y0 = rH * y
+    return {
+      x0: Math.round(x0),
+      y0: Math.round(y0),
+      x1: Math.round(x0 + AVATAR_SIZE * rW),
+      y1: Math.round(y0 + AVATAR_SIZE * rH),
+    }
+  }
+
+  _zoomRef = (ref: ?ZoomableBox) => {
+    this._zoom = ref
   }
 
   render() {
-    console.log('SPOONER', this.props.image)
-
     return (
       <StandardScreen
-        style={container}
         onCancel={this.props.onClose}
-        title="Zoom and pan"
         scrollEnabled={false}
+        style={container}
+        title="Zoom and pan"
       >
         <Box
           style={{
@@ -27,29 +67,17 @@ class EditAvatar extends React.Component<Props> {
             marginTop: globalMargins.small,
           }}
         >
-          <Box
-            style={{
-              backgroundColor: globalColors.lightGrey2,
-              borderRadius: 250,
-              height: 250,
-              marginBottom: globalMargins.tiny,
-              overflow: 'hidden',
-              position: 'relative',
-              width: 250,
-            }}
-          >
+          <Box style={styles.zoomContainer}>
             <ZoomableBox
               maxZoom={10}
+              ref={this._zoomRef}
               showsHorizontalScrollIndicator={false}
               showsVerticalScrollIndicator={false}
-              style={{
-                height: 250,
-                width: 250,
-              }}
+              style={styles.imageContainer}
             >
               <NativeImage
-                source={{uri: `data:image/jpeg;base64,${this.props.image.data}`}}
                 resizeMode="contain"
+                source={{uri: `data:image/jpeg;base64,${this.props.image.data}`}}
                 style={{
                   alignSelf: 'center',
                   flex: 1,
@@ -61,11 +89,11 @@ class EditAvatar extends React.Component<Props> {
           </Box>
           <ButtonBar direction="column">
             <WaitingButton
-              type="Primary"
               fullWidth={true}
-              onClick={this._onSave}
               label="Save"
-              style={{width: '100%', marginTop: globalMargins.tiny}}
+              onClick={this._onSave}
+              style={styles.button}
+              type="Primary"
               waitingKey={null}
             />
           </ButtonBar>
@@ -79,6 +107,26 @@ const container = platformStyles({
   common: {
     ...globalStyles.flexBoxColumn,
     flex: 1,
+  },
+})
+
+const styles = styleSheetCreate({
+  button: {
+    marginTop: globalMargins.tiny,
+    width: '100%',
+  },
+  imageContainer: {
+    height: AVATAR_SIZE,
+    width: AVATAR_SIZE,
+  },
+  zoomContainer: {
+    backgroundColor: globalColors.lightGrey2,
+    borderRadius: AVATAR_SIZE,
+    height: AVATAR_SIZE,
+    marginBottom: globalMargins.tiny,
+    overflow: 'hidden',
+    position: 'relative',
+    width: AVATAR_SIZE,
   },
 })
 
