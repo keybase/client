5d1bdb6c2d18c7698e54d7f8694e43332658e8f4
Mike Maxim <mike@keyba.se>
2018-06-19 15:03:16 -0400

    dont count all placeholder results as hit from PullLocalOnly

--- go/chat/convsource.go
+++ go/chat/convsource.go
@@ -783,9 +783,27 @@ func (p *pullLocalResultCollector) String() string {
 	return fmt.Sprintf("[ %s: t: %d ]", p.Name(), p.num)
 }
 
+func (p *pullLocalResultCollector) realHits() (total int) {
+	for _, m := range p.Result() {
+		st, err := m.State()
+		if err != nil {
+			// count these
+			total++
+			continue
+		}
+		switch st {
+		case chat1.MessageUnboxedState_PLACEHOLDER:
+			// don't count!
+		default:
+			total++
+		}
+	}
+	return total
+}
+
 func (p *pullLocalResultCollector) Error(err storage.Error) storage.Error {
 	// Swallow this error, we know we can miss if we get anything at all
-	if _, ok := err.(storage.MissError); ok && len(p.Result()) > 0 {
+	if _, ok := err.(storage.MissError); ok && p.realHits() > 0 {
 		return nil
 	}
 	return err
