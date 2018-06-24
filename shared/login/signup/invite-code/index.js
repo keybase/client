e05a54b39c3faa63b95587eebb671100071757dc
chrisnojima <cnojima@keyba.se>
2018-06-23 23:09:53 -0400

    WIP

---
+++ shared/login/signup/invite-code/index.js
@@ -0,0 +1,58 @@
+// @flow
+import * as React from 'react'
+import * as Constants from '../../../constants/signup'
+import {Icon, Text, Box2, Input, WaitingButton, HeaderHocHeader} from '../../../common-adapters'
+import {styleSheetCreate} from '../../../styles'
+
+type Props = {
+  onBack: () => void,
+  onSubmit: (code: string) => void,
+  onRequestInvite: () => void,
+  inviteCodeErrorText: ?string,
+}
+type State = {
+  inviteCode: string,
+}
+
+class UsernameAndEmail extends React.Component<Props, State> {
+  state = {inviteCode: ''}
+
+  _onSubmit = () => {
+    this.props.onSubmit(this.state.inviteCode)
+  }
+  render() {
+    return (
+      <Box2 direction="vertical" fullWidth={true} fullHeight={true}>
+        <HeaderHocHeader onBack={this.props.onBack} headerStyle={styles.header} />
+        <Box2 direction="vertical" fullWidth={true} fullHeight={true} centerChildren={true} gap="small">
+          <Text type="Header">Type in your invite code:</Text>
+          <Icon type="icon-invite-code-48" />
+          <Input
+            autoFocus={true}
+            value={this.state.inviteCode}
+            errorText={this.props.inviteCodeErrorText}
+            onEnterKeyDown={this._onSubmit}
+            onChangeText={inviteCode => this.setState({inviteCode})}
+          />
+          <WaitingButton
+            waitingKey={Constants.waitingKey}
+            type="Primary"
+            label="Continue"
+            disabled={!this.state.inviteCode}
+            onClick={this._onSubmit}
+          />
+          <Text type="BodySmall">Not invited?</Text>
+          <Text type="BodySmallSecondaryLink" onClick={this.props.onRequestInvite}>
+            Request an invite
+          </Text>
+        </Box2>
+      </Box2>
+    )
+  }
+}
+
+const styles = styleSheetCreate({
+  header: {position: 'absolute'},
+})
+
+export default UsernameAndEmail
