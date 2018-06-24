0805d14325bb25386b8dc71de9d7aa05934d9ba5
chrisnojima <cnojima@keyba.se>
2018-06-24 09:38:06 -0400

    WIP

---
+++ shared/login/signup/username-email/index.js
@@ -0,0 +1,66 @@
+// @flow
+import * as React from 'react'
+import * as Constants from '../../../constants/signup'
+import {Box2, Avatar, Input, WaitingButton, HeaderHocHeader} from '../../../common-adapters'
+import {styleSheetCreate} from '../../../styles'
+
+type Props = {|
+  email: string,
+  emailError: string,
+  onBack: () => void,
+  onSubmit: (username: string, email: string) => void,
+  username: string,
+  usernameError: string,
+|}
+type State = {|
+  username: string,
+  email: string,
+|}
+
+class UsernameAndEmail extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+    this.state = {email: this.props.email, username: this.props.username}
+  }
+
+  _onSubmit = () => {
+    this.props.onSubmit(this.state.username, this.state.email)
+  }
+  render() {
+    return (
+      <Box2 direction="vertical" fullWidth={true} fullHeight={true}>
+        <HeaderHocHeader onBack={this.props.onBack} headerStyle={styles.header} />
+        <Box2 direction="vertical" fullWidth={true} fullHeight={true} centerChildren={true} gap="small">
+          <Avatar username="" size={128} />
+          <Input
+            autoFocus={true}
+            hintText="Create a username"
+            value={this.state.username}
+            errorText={this.props.usernameError}
+            onChangeText={username => this.setState({username})}
+          />
+          <Input
+            hintText="Email address"
+            value={this.state.email}
+            errorText={this.props.emailError}
+            onEnterKeyDown={this._onSubmit}
+            onChangeText={email => this.setState({email})}
+          />
+          <WaitingButton
+            waitingKey={Constants.waitingKey}
+            type="Primary"
+            label="Continue"
+            disabled={!this.state.email || !this.state.username}
+            onClick={this._onSubmit}
+          />
+        </Box2>
+      </Box2>
+    )
+  }
+}
+
+const styles = styleSheetCreate({
+  header: {position: 'absolute'},
+})
+
+export default UsernameAndEmail
---
+++ shared/login/signup/username-email/index.stories.js
@@ -0,0 +1,30 @@
+// @flow
+import * as React from 'react'
+import UsernameEmail from '.'
+import {action, storiesOf} from '../../../stories/storybook'
+import {Box2} from '../../../common-adapters'
+import * as PropProviders from '../../../stories/prop-providers'
+
+const props = {
+  email: '',
+  emailError: '',
+  onBack: action('onBack'),
+  onSubmit: action('onSubmit'),
+  username: '',
+  usernameError: '',
+}
+
+const load = () => {
+  storiesOf('Signup/Username email', module)
+    .addDecorator(PropProviders.Common())
+    .addDecorator(story => (
+      <Box2 direction="vertical" style={{height: '100%', width: '100%'}}>
+        {story()}
+      </Box2>
+    ))
+    .add('Start', () => <UsernameEmail {...props} />)
+    .add('Name Error', () => <UsernameEmail {...props} usernameError={'Name bad, smash!'} />)
+    .add('Email Error', () => <UsernameEmail {...props} emailError={'Email bad, booo'} />)
+}
+
+export default load
