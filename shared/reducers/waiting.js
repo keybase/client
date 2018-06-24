d1b1e05ca76fade23bd1ca55f1b1b2afad4d71eb
chrisnojima <cnojima@keyba.se>
2018-06-24 15:20:48 -0400

    part 1: engine changes. added waiting change action

--- shared/reducers/waiting.js
+++ shared/reducers/waiting.js
@@ -3,28 +3,27 @@ import * as Constants from '../constants/waiting'
 import * as Types from '../constants/types/waiting'
 import * as Waiting from '../actions/waiting-gen'
 
+const changeHelper = (state: Types.State, keys: Array<string>, diff: 1 | -1) =>
+  state.withMutations(st => {
+    keys.forEach(k => st.update(k, diff === 1 ? 0 : 1, n => n + diff))
+  })
+
 function reducer(state: Types.State = Constants.initialState, action: Waiting.Actions): Types.State {
   switch (action.type) {
     case 'common:resetStore': {
       return Constants.initialState
     }
     case Waiting.decrementWaiting: {
-      const {
-        payload: {key},
-      } = action
-      const keys = typeof key === 'string' ? [key] : key
-      return state.withMutations(st => {
-        keys.forEach(k => st.update(k, 1, n => n - 1))
-      })
+      const {key} = action.payload
+      return changeHelper(state, typeof key === 'string' ? [key] : key, -1)
     }
     case Waiting.incrementWaiting: {
-      const {
-        payload: {key},
-      } = action
-      const keys = typeof key === 'string' ? [key] : key
-      return state.withMutations(st => {
-        keys.forEach(k => st.update(k, 0, n => n + 1))
-      })
+      const {key} = action.payload
+      return changeHelper(state, typeof key === 'string' ? [key] : key, 1)
+    }
+    case Waiting.changeWaiting: {
+      const {key, increment} = action.payload
+      return changeHelper(state, typeof key === 'string' ? [key] : key, increment ? 1 : -1)
     }
     default:
       /*::
