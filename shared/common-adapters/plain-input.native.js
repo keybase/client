9b8c31288657bfdf1b4fab3aacc7e092e55eb119
Daniel Ayoub <danny.a95@gmail.com>
2018-06-25 16:53:30 -0400

    use globalColors black_40

--- shared/common-adapters/plain-input.native.js
+++ shared/common-adapters/plain-input.native.js
@@ -128,7 +128,7 @@ class PlainInput extends Component<InternalProps, State> {
       onFocus: this._onFocus,
       onSubmitEditing: this.props.onEnterKeyDown,
       placeholder: this.props.placeholder,
-      placeholderTextColor: this.props.placeholderColor || 'rgba(0,0,0,.4)',
+      placeholderTextColor: this.props.placeholderColor || globalColors.black_40,
       ref: this._setInputRef,
       returnKeyType: this.props.returnKeyType,
       secureTextEntry: this.props.type === 'password',
