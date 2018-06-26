ac9066cb4c5bb7da390dafc2b92221d595d07315
chrisnojima <cnojima@keyba.se>
2018-06-26 15:36:26 -0400

    fix up inbox row measuring issues

--- shared/chat/inbox/row/divider/index.js
+++ shared/chat/inbox/row/divider/index.js
@@ -1,7 +1,15 @@
 // @flow
 import * as React from 'react'
 import {ClickableBox, Box, Text, Badge} from '../../../../common-adapters'
-import {globalStyles, globalColors, globalMargins, isMobile} from '../../../../styles'
+import {
+  styleSheetCreate,
+  collapseStyles,
+  globalStyles,
+  globalColors,
+  globalMargins,
+  isMobile,
+} from '../../../../styles'
+import * as RowSizes from '../sizes'
 
 type Props = {
   badgeCount: number,
@@ -13,58 +21,57 @@ type Props = {
 class Divider extends React.PureComponent<Props> {
   render() {
     return (
-      <Box style={this.props.style ? {..._toggleContainer, ...this.props.style} : _toggleContainer}>
-        <ClickableBox onClick={this.props.toggle} className="toggleButtonClass" style={_toggleButtonStyle}>
-          <Text type="BodySmallSemibold" style={_textStyle}>
+      <Box style={collapseStyles([styles.container, this.props.style])}>
+        <ClickableBox onClick={this.props.toggle} className="toggleButtonClass" style={styles.toggleButton}>
+          <Text type="BodySmallSemibold" style={styles.text}>
             {this.props.hiddenCount > 0 ? `+${this.props.hiddenCount} more` : 'Show less'}
           </Text>
           {this.props.hiddenCount > 0 &&
             this.props.badgeCount > 0 && (
-              <Badge badgeStyle={_badgeToggleStyle} badgeNumber={this.props.badgeCount} />
+              <Badge badgeStyle={styles.badgeToggle} badgeNumber={this.props.badgeCount} />
             )}
         </ClickableBox>
-        <Box style={_dividerStyle} />
+        <Box style={styles.divider} />
       </Box>
     )
   }
 }
 
-const _textStyle = {
-  color: globalColors.black_60,
-}
-const _toggleButtonStyle = {
-  ...globalStyles.flexBoxRow,
-  alignItems: 'center',
-  alignSelf: 'center',
-  backgroundColor: globalColors.black_05,
-  borderRadius: 19,
-  height: isMobile ? 28 : 20,
-  marginBottom: isMobile ? 16 : 8,
-  paddingLeft: isMobile ? globalMargins.small : globalMargins.tiny,
-  paddingRight: isMobile ? globalMargins.small : globalMargins.tiny,
-}
-
-const _badgeStyle = {
-  marginLeft: globalMargins.xtiny,
-  marginRight: 0,
-  position: 'relative',
-}
-
-const _dividerStyle = {
-  backgroundColor: globalColors.black_05,
-  height: 1,
-  width: '100%',
-}
-
-const _toggleContainer = {
-  ...globalStyles.flexBoxColumn,
-  height: isMobile ? 56 : 40,
-  justifyContent: 'center',
-}
-
-const _badgeToggleStyle = {
-  ..._badgeStyle,
-  marginLeft: globalMargins.xtiny,
-}
+const styles = styleSheetCreate({
+  badge: {
+    marginLeft: globalMargins.xtiny,
+    marginRight: 0,
+    position: 'relative',
+  },
+  badgeToggle: {
+    marginLeft: globalMargins.xtiny,
+    marginRight: 0,
+    position: 'relative',
+  },
+  container: {
+    ...globalStyles.flexBoxColumn,
+    height: RowSizes.dividerHeight,
+    justifyContent: 'center',
+  },
+  divider: {
+    backgroundColor: globalColors.black_05,
+    height: 1,
+    width: '100%',
+  },
+  text: {
+    color: globalColors.black_60,
+  },
+  toggleButton: {
+    ...globalStyles.flexBoxRow,
+    alignItems: 'center',
+    alignSelf: 'center',
+    backgroundColor: globalColors.black_05,
+    borderRadius: 19,
+    height: isMobile ? 28 : 20,
+    marginBottom: isMobile ? 16 : 8,
+    paddingLeft: isMobile ? globalMargins.small : globalMargins.tiny,
+    paddingRight: isMobile ? globalMargins.small : globalMargins.tiny,
+  },
+})
 
 export {Divider}
