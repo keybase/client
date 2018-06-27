21dbb7b078dea64bec1b2f0b995fb5227369af89
chrisnojima <cnojima@keyba.se>
2018-06-27 11:53:23 -0400

    pr feedback

--- shared/devices/gen-paper-key/index.native.js
+++ shared/devices/gen-paper-key/index.native.js
@@ -19,19 +19,19 @@ class SuccessRender extends Component<Props, State> {
       <Box2 direction="vertical" fullWidth={true} fullHeight={true}>
         <HeaderHocHeader onBack={this.props.onBack} headerStyle={styles.header} />
         <Box2 direction="vertical" fullWidth={true} fullHeight={true} centerChildren={true} gap="small">
-          <Text type="Header" style={textCenter}>
+          <Text type="Header" style={styles.textCenter}>
             {this.props.title || "Congratulations, you've just joined Keybase!"}
           </Text>
-          <Text type="Body" style={{...textCenter, marginTop: globalMargins.medium}}>
+          <Text type="Body" style={styles.textCenterMargin}>
             Here is your unique paper key, it will allow you to perform important Keybase tasks in the future.
             This is the only time you'll see this so be sure to write it down.
           </Text>
 
-          <Box style={paperKeyContainerStyle}>
-            <Text type="Header" selectable={true} style={paperkeyStyle}>
+          <Box style={styles.paperKeyContainer}>
+            <Text type="Header" selectable={true} style={styles.paperkey}>
               {this.props.paperkey.stringValue()}
             </Text>
-            <Box style={paperCornerStyle}>
+            <Box style={styles.paperCorner}>
               <Icon type="icon-paper-key-corner" />
             </Box>
           </Box>
@@ -55,40 +55,40 @@ class SuccessRender extends Component<Props, State> {
   }
 }
 
-const textCenter = {
-  textAlign: 'center',
-}
-
-const paperKeyContainerStyle = {
-  alignSelf: 'center',
-  backgroundColor: globalColors.white,
-  borderColor: globalColors.darkBlue,
-  borderRadius: 4,
-  borderStyle: 'solid',
-  borderWidth: 4,
-  marginBottom: globalMargins.medium,
-  marginTop: globalMargins.small,
-  paddingBottom: globalMargins.small,
-  paddingLeft: globalMargins.small,
-  paddingRight: globalMargins.large,
-  paddingTop: globalMargins.small,
-}
-
-const paperkeyStyle = {
-  ...getStyle('Header', 'Normal'),
-  ...globalStyles.fontTerminal,
-  color: globalColors.darkBlue,
-  textAlign: 'center',
-}
-
-const paperCornerStyle = {
-  position: 'absolute',
-  right: -4,
-  top: -4,
-}
-
 const styles = styleSheetCreate({
   header: {position: 'absolute'},
+  paperCorner: {
+    position: 'absolute',
+    right: -4,
+    top: -4,
+  },
+  paperKeyContainer: {
+    alignSelf: 'center',
+    backgroundColor: globalColors.white,
+    borderColor: globalColors.darkBlue,
+    borderRadius: 4,
+    borderStyle: 'solid',
+    borderWidth: 4,
+    marginBottom: globalMargins.medium,
+    marginTop: globalMargins.small,
+    paddingBottom: globalMargins.small,
+    paddingLeft: globalMargins.small,
+    paddingRight: globalMargins.large,
+    paddingTop: globalMargins.small,
+  },
+  paperkey: {
+    ...getStyle('Header', 'Normal'),
+    ...globalStyles.fontTerminal,
+    color: globalColors.darkBlue,
+    textAlign: 'center',
+  },
+  textCenter: {
+    textAlign: 'center',
+  },
+  textCenterMargin: {
+    marginTop: globalMargins.medium,
+    textAlign: 'center',
+  },
 })
 
 export default SuccessRender
