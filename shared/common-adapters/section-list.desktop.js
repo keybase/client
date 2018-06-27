d09e63ceb99b8fa8b3c011fe9bcdaf325bbd227d
Daniel Ayoub <danny.a95@gmail.com>
2018-06-27 18:41:34 -0400

    WIP desktop sectionlist

--- shared/common-adapters/section-list.desktop.js
+++ shared/common-adapters/section-list.desktop.js
@@ -3,4 +3,11 @@ import * as React from 'react'
 import ReactList from 'react-list'
 import type {Props} from './section-list'
 
-export default class extends React.Component<Props> {}
+export default class extends React.Component<Props> {
+  _makeItems = () => {}
+
+  render() {
+    const items = this._makeItems()
+    return <ReactList {...this.props} />
+  }
+}
