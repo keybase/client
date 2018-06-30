c4fb9d984ae19ea3a47ebf28b0a00c168ac974c6
Chris Ball <chris@printf.net>
2018-06-30 14:51:50 -0700

    Only your primary account has a federated address

--- shared/wallets/receive-modal/index.stories.js
+++ shared/wallets/receive-modal/index.stories.js
@@ -4,14 +4,22 @@ import {action, storiesOf} from '../../stories/storybook'
 import ReceiveModal from '.'
 
 const load = () => {
-  storiesOf('Wallets', module).add('Receive', () => (
-    <ReceiveModal
-      federatedAddress="cecile*keybase.io"
-      onClose={action('onClose')}
-      stellarAddress="G23T5671ASCZZX09235678ASQ511U12O91AQ"
-      username="cecileb"
-    />
-  ))
+  storiesOf('Wallets', module)
+    .add('Receive to primary', () => (
+      <ReceiveModal
+        federatedAddress="cecile*keybase.io"
+        onClose={action('onClose')}
+        stellarAddress="G23T5671ASCZZX09235678ASQ511U12O91AQ"
+        username="cecileb"
+      />
+    ))
+    .add('Receive to secondary', () => (
+      <ReceiveModal
+        onClose={action('onClose')}
+        stellarAddress="G33T5671ASCZZX09235678ASQ511U12O91AQ"
+        username="cecileb"
+      />
+    ))
 }
 
 export default load
