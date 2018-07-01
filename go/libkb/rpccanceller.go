5a83cb154db1f4e7766dd153219317ee212ae4b5
Mike Maxim <mike@keyba.se>
2018-07-01 16:15:10 -0400

    wip

---
+++ go/libkb/rpccanceller.go
@@ -0,0 +1,48 @@
+package libkb
+
+import (
+	"sync"
+
+	"golang.org/x/net/context"
+)
+
+type liveContext struct {
+	ctx      context.Context
+	cancelFn context.CancelFunc
+}
+
+type RPCCanceller struct {
+	sync.Mutex
+	liveCtxs map[string]liveContext
+}
+
+func NewRPCCanceller() *RPCCanceller {
+	return &RPCCanceller{
+		liveCtxs: make(map[string]liveContext),
+	}
+}
+
+func (r *RPCCanceller) RegisterContext(ctx context.Context) (context.Context, string) {
+	r.Lock()
+	defer r.Unlock()
+	var lc liveContext
+	lc.ctx, lc.cancelFn = context.WithCancel(ctx)
+	id, _ := RandHexString("", 8)
+	r.liveCtxs[id] = lc
+	return lc.ctx, id
+}
+
+func (r *RPCCanceller) UnregisterContext(id string) {
+	r.Lock()
+	defer r.Unlock()
+	delete(r.liveCtxs, id)
+}
+
+func (r *RPCCanceller) CancelLiveContexts() {
+	r.Lock()
+	defer r.Unlock()
+	for id, liveCtx := range r.liveCtxs {
+		liveCtx.cancelFn()
+		delete(r.liveCtxs, id)
+	}
+}
---
+++ go/service/cancelling.go
@@ -0,0 +1,24 @@
+package service
+
+import (
+	"github.com/keybase/client/go/libkb"
+	"github.com/keybase/go-framed-msgpack-rpc/rpc"
+	"golang.org/x/net/context"
+)
+
+func CancellingProtocol(g *libkb.GlobalContext, prot rpc.Protocol) (res rpc.Protocol) {
+	res.Name = prot.Name
+	res.WrapError = prot.WrapError
+	res.Methods = make(map[string]rpc.ServeHandlerDescription)
+	for name, desc := range prot.Methods {
+		var newDesc rpc.ServeHandlerDescription
+		newDesc.MakeArg = desc.MakeArg
+		newDesc.MethodType = desc.MethodType
+		newDesc.Handler = func(ctx context.Context, arg interface{}) (interface{}, error) {
+			// register ctx on G to get cancelled
+			return desc.Handler(ctx, arg)
+		}
+		res.Methods[name] = newDesc
+	}
+	return res
+}
