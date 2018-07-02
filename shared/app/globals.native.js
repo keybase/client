630cf4036a35439e0dff212777b4981451d7b308
chrisnojima <cnojima@keyba.se>
2018-07-02 10:38:44 -0400

    android new jsc

--- shared/app/globals.native.js
+++ shared/app/globals.native.js
@@ -3,15 +3,6 @@
 // >>>>>>>>>>>>>>>>>>>>>>>      DO NOT REORDER ANYTHING in this file      <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 // This is supposed to bootstrap / polyfill / fixup the app. Do NOT add things here or change things unless you really know
 // what's happening
-//
-// symbol polyfills
-global.Symbol = require('core-js/es6/symbol')
-require('core-js/fn/symbol/iterator')
-
-// collection fn polyfills
-require('core-js/fn/map')
-require('core-js/fn/set')
-require('core-js/fn/array/find')
 // Needed for purepack
 window.Buffer = require('buffer').Buffer
 const {NativeModules} = require('react-native')
@@ -24,13 +15,5 @@ if (typeof __STORYBOOK__ === 'undefined') {
 
 // We don't storyshot RN
 __STORYSHOT__ = false
-
-// Native String.startswith() sometimes incorrectly returns false on Android!
-// See https://github.com/facebook/react-native/issues/11370 for a report.
-// $ForceType redefining startsWith
-String.prototype.startsWith = function(searchString, _position) {
-  const position = _position || 0
-  return this.substr(position, searchString.length) === searchString
-}
 
 require('../dev/user-timings')
