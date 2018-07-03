a64ee787c25c4f1cc42a42c161357e8471715a27
Chris Ball <chris@printf.net>
2018-07-02 23:29:01 -0700

    use layerOnTop

--- shared/wallets/routes.js
+++ shared/wallets/routes.js
@@ -2,11 +2,14 @@
 import {makeRouteDefNode, makeLeafTags} from '../route-tree'
 import Container from './container'
 import ReceiveModal from './receive-modal/container'
+import {isMobile} from '../constants/platform'
 
 const routeTree = makeRouteDefNode({
   children: {
     receive: {
+      children: {},
       component: ReceiveModal,
+      tags: makeLeafTags({layerOnTop: !isMobile}),
     },
   },
   component: Container,
