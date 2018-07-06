f8716850fed9f94a8bdd966b0627e9232409047f
Frederick Akalin <akalin@keyba.se>
2018-07-05 18:41:39 -0700

    Add one

--- shared/profile/search/index.stories.js
+++ shared/profile/search/index.stories.js
@@ -2,7 +2,7 @@
 import * as React from 'react'
 import * as PropProviders from '../../stories/prop-providers'
 import {Box2} from '../../common-adapters'
-import {action, storiesOf} from '../../stories/storybook'
+import {action, storiesOf, createPropProvider} from '../../stories/storybook'
 import Search from '.'
 
 const Wrapper = ({children}) => (
@@ -11,7 +11,67 @@ const Wrapper = ({children}) => (
   </Box2>
 )
 
-const provider = PropProviders.CommonProvider()
+const commonServicesResultMapPropsKB = {
+  id: '0',
+  leftFollowingState: 'NoState',
+  leftFullname: 'John Zila',
+  leftIcon: 'jzila',
+  leftService: 'Keybase',
+  leftUsername: 'jzila',
+  onShowTracker: () => action('showtracker'),
+  rightFollowingState: 'NoState',
+  rightIcon: null,
+  rightService: null,
+  rightUsername: null,
+  showTrackerButton: false,
+}
+
+const servicesResultsListMapCommonRows = {
+  chris: {
+    ...commonServicesResultMapPropsKB,
+    leftFollowingState: 'Following',
+    leftFullname: 'chris on GitHub',
+    leftUsername: 'chris',
+    rightIcon: 'iconfont-identity-github',
+    rightService: 'GitHub',
+    rightUsername: 'chrisname',
+  },
+  cjb: {
+    ...commonServicesResultMapPropsKB,
+    leftFollowingState: 'NotFollowing',
+    leftFullname: 'cjb on facebook',
+    leftUsername: 'cjb',
+    rightIcon: 'iconfont-identity-facebook',
+    rightService: 'Facebook',
+    rightUsername: 'cjbname',
+  },
+  jzila: {
+    ...commonServicesResultMapPropsKB,
+    leftFollowingState: 'NoState',
+    leftFullname: 'jzila on twitter',
+    leftUsername: 'jzila',
+    rightIcon: 'iconfont-identity-twitter',
+    rightService: 'Twitter',
+    rightUsername: 'jzilatwit',
+  },
+}
+
+const props = {
+  disableIfInTeamName: '',
+  items: Object.keys(servicesResultsListMapCommonRows),
+  onClick: () => action('onClick'),
+  onShowTracker: () => action('onShowTracker'),
+  selectedId: null,
+  showSearchSuggestions: false,
+  keyPath: ['searchChat'],
+}
+
+const provider = createPropProvider(PropProviders.Common(), {
+  SearchResultRow: (props: {id: string}) => servicesResultsListMapCommonRows[props.id],
+  Chooser: () => {
+    return props
+  },
+})
 
 const load = () => {
   storiesOf('Profile/Search', module)
