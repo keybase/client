3124283f18f9b6d648670bc1363f72ff8531d0f9
Steve Sanders <steve@keyba.se>
2018-07-10 16:15:24 -0700

    popup

--- shared/chat/delete-history-warning/index.js
+++ shared/chat/delete-history-warning/index.js
@@ -1,6 +1,6 @@
 // @flow
 import React from 'react'
-import {Box, Button, HeaderOnMobile, Icon, Text} from '../../common-adapters'
+import {Box, Button, HeaderOnMobile, Icon, PopupDialog, ScrollView, Text} from '../../common-adapters'
 import {globalColors, globalMargins, globalStyles, isMobile} from '../../styles'
 
 type Props = {
@@ -10,34 +10,44 @@ type Props = {
   onDeleteHistory: () => void,
 }
 
+const Wrapper = ({children, onBack}) =>
+  isMobile ? (
+    <ScrollView style={{...globalStyles.fillAbsolute, ...globalStyles.flexBoxColumn}} children={children} />
+  ) : (
+    <PopupDialog onClose={onBack} children={children} />
+  )
+
 const DeleteHistoryWarning = ({errorText, name, onBack, onDeleteHistory}: Props) => (
-  <Box
-    style={{
-      ...globalStyles.flexBoxColumn,
-      ...stylePadding,
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: globalColors.white,
-      padding: globalMargins.small,
-    }}
-  >
-    <Icon type={isMobile ? 'icon-message-deletion-64' : 'icon-message-deletion-48'} />
-    <Text style={{padding: globalMargins.small}} type="Header">
-      Delete conversation history?
-    </Text>
-    <Text style={{padding: globalMargins.small}} type="Body">
-      You are about to delete all the messages in this conversation. For everyone.
-    </Text>
-    <Box style={{...globalStyles.flexBoxRow, marginTop: globalMargins.xlarge}}>
-      <Button type="Secondary" style={{marginLeft: globalMargins.tiny}} onClick={onBack} label="Cancel" />
-      <Button
-        type="Danger"
-        style={{marginLeft: globalMargins.tiny}}
-        onClick={onDeleteHistory}
-        label="Yes, clear for everyone"
-      />
+  <Wrapper onBack={onBack}>
+    <Box
+      style={{
+        ...globalStyles.flexBoxColumn,
+        ...stylePadding,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: globalColors.white,
+        padding: globalMargins.small,
+        maxWidth: 560,
+      }}
+    >
+      <Icon type={isMobile ? 'icon-message-deletion-64' : 'icon-message-deletion-48'} />
+      <Text style={{padding: globalMargins.small}} type="Header">
+        Delete conversation history?
+      </Text>
+      <Text style={{padding: globalMargins.small}} type="Body">
+        You are about to delete all the messages in this conversation. For everyone.
+      </Text>
+      <Box style={{...globalStyles.flexBoxRow, marginTop: globalMargins.xlarge}}>
+        <Button type="Secondary" style={{marginLeft: globalMargins.tiny}} onClick={onBack} label="Cancel" />
+        <Button
+          type="Danger"
+          style={{marginLeft: globalMargins.tiny}}
+          onClick={onDeleteHistory}
+          label="Yes, clear for everyone"
+        />
+      </Box>
     </Box>
-  </Box>
+  </Wrapper>
 )
 
 const stylePadding = isMobile
