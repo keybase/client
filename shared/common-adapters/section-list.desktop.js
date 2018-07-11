854cc67866c03aef759f5e3cad00907fe508f23b
Daniel Ayoub <danny.a95@gmail.com>
2018-07-11 15:52:16 -0400

    make section headers stick to top on desktop

--- shared/common-adapters/section-list.desktop.js
+++ shared/common-adapters/section-list.desktop.js
@@ -1,14 +1,16 @@
 // @flow
 import * as React from 'react'
 import ReactList from 'react-list'
+import Box from './box'
 import ScrollView from './scroll-view'
 import type {Props} from './section-list'
+import {platformStyles, styleSheetCreate} from '../styles'
 
 type State = {
-  items: any[],
+  items: any[][],
 }
-export default class extends React.Component<Props, State> {
-  state = {items: []}
+class SectionList extends React.Component<Props, State> {
+  state = {items: [[]]}
 
   componentDidMount() {
     this._storeItems()
@@ -21,29 +23,50 @@ export default class extends React.Component<Props, State> {
   }
 
   _makeItems = () => {
-    return this.props.sections.reduce((arr, section, sectionIndex) => {
-      arr.push({type: 'header', sectionIndex})
-      section.data.length && arr.push(...section.data.map(item => ({item, sectionIndex, type: 'body'})))
-      return arr
-    }, [])
+    return this.props.sections.reduce(
+      (arr, section, sectionIndex) => {
+        const next = []
+        next.push({type: 'header', sectionIndex})
+        section.data.length && next.push(...section.data.map(item => ({item, sectionIndex, type: 'body'})))
+        arr.push(next)
+        return arr
+      },
+      [[]]
+    )
   }
 
   _storeItems = () => this.setState({items: this._makeItems()})
 
-  _itemRenderer = (index, key) => {
-    const item = this.state.items[index]
+  _itemRenderer = (itemsIndex: number) => (index, key) => {
+    const item = this.state.items[itemsIndex][index]
     const section = this.props.sections[item.sectionIndex]
     const indexWithinSection = section.data.indexOf(item.item)
-    return item.type === 'header'
-      ? this.props.renderSectionHeader({section})
-      : this.props.renderItem({index: indexWithinSection, item: item.item, section})
+    return item.type === 'header' ? (
+      <Box style={styles.sectionHeader}>{this.props.renderSectionHeader({section})}</Box>
+    ) : (
+      this.props.renderItem({index: indexWithinSection, item: item.item, section})
+    )
   }
 
   render() {
     return (
       <ScrollView>
-        <ReactList itemRenderer={this._itemRenderer} length={this.state.items.length} />
+        {this.state.items.map((item, index) => (
+          <ReactList key={index} itemRenderer={this._itemRenderer(index)} length={item.length} />
+        ))}
       </ScrollView>
     )
   }
 }
+
+const styles = styleSheetCreate({
+  sectionHeader: platformStyles({
+    isElectron: {
+      position: 'sticky',
+      top: -1,
+      zIndex: '1', // needed to be on top of newly created stacking context
+    },
+  }),
+})
+
+export default SectionList
