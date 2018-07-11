35a0b428d8211d573d2f4015c3761921c51b8cff
Nathan Smith <nathan@nathansmith.io>
2018-07-11 11:17:03 -0400

    Add some badge stories

--- shared/settings/nav/index.stories.js
+++ shared/settings/nav/index.stories.js
@@ -3,15 +3,18 @@ import * as React from 'react'
 import SettingsNav from '.'
 import {action, storiesOf} from '../../stories/storybook'
 
+const defaultProps = {
+  badgeNumbers: {},
+  selectedTab: 'settingsTabs:landingTab',
+  onTabChange: action('ontabchange'),
+  onLogout: action('onlogout'),
+}
+
 const load = () => {
-  storiesOf('Settings/Nav', module).add('Normal', () => (
-    <SettingsNav
-      badgeNumbers={{}}
-      selectedTab={'settingsTabs:landingTab'}
-      onTabChange={action('ontabchange')}
-      onLogout={action('onlogout')}
-    />
-  ))
+  storiesOf('Settings/Nav', module)
+    .add('Normal', () => <SettingsNav {...defaultProps} />)
+    .add('With a badge', () => <SettingsNav {...defaultProps} badgeNumbers={{'tabs:gitTab': 1}} />)
+    .add('With a badge on Notifications', () => <SettingsNav {...defaultProps} badgeNotifications={true} />)
 }
 
 export default load
