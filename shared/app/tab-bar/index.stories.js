0824bb4a76051ff6a1bc77f787a262ba48febdcf
Nathan Smith <nathan@nathansmith.io>
2018-07-13 17:00:07 -0400

    Don’t need a container

--- shared/app/tab-bar/index.stories.js
+++ shared/app/tab-bar/index.stories.js
@@ -3,7 +3,6 @@ import * as React from 'react'
 import {action, storiesOf, createPropProvider} from '../../stories/storybook'
 import TabBarRender from '.'
 import * as PropProviders from '../../stories/prop-providers'
-import {Box} from '../../common-adapters'
 
 const provider = createPropProvider(PropProviders.Common())
 
@@ -27,12 +26,9 @@ const defaultProps = {
   },
 }
 
-const Container = storyFn => <Box style={{height: '100%', width: '100%'}}>{storyFn()}</Box>
-
 const load = () => {
   storiesOf('Tab Bar', module)
     .addDecorator(provider)
-    .addDecorator(Container)
     .add('Normal', () => <TabBarRender {...defaultProps} />)
     .add('With a badge', () => (
       <TabBarRender {...defaultProps} badgeNumbers={{...defaultProps.badgeNumbers, 'tabs:chatTab': 6}} />
