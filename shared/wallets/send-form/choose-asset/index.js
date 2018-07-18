18f5acd2a22b06ff5d2d704e4c0c3a076af2c7f3
Daniel Ayoub <danny.a95@gmail.com>
2018-07-18 11:44:54 -0400

    items rendered

--- shared/wallets/send-form/choose-asset/index.js
+++ shared/wallets/send-form/choose-asset/index.js
@@ -1,26 +1,134 @@
 // @flow
 import * as React from 'react'
-import {Box2} from '../../../common-adapters'
+import {Box2, SectionList, Text} from '../../../common-adapters'
 import {styleSheetCreate} from '../../../styles'
 import Header from '../header'
 
+type DisplayItem = {currencyCode: string, symbol: string, type: 'display choice'}
+type OtherItem = {code: string, disabledExplanation: string, issuer: string, type: 'other choice'}
+
 type Props = {
-  displayChoices: Array<{currencyCode: string, symbol: string}>,
-  otherChoices: Array<{code: string, disabledExplanation: string, issuer: string}>,
+  displayChoices: Array<DisplayItem>,
+  onChoose: (item: DisplayItem | OtherItem) => void,
+  otherChoices: Array<OtherItem>,
 }
 
-const ChooseAsset = (props: Props) => (
-  <Box2 direction="vertical" style={styles.container}>
-    <Header whiteBackground={true} />
-    <Box2 direction="horizontal" fullWidth={true} style={styles.header} />
+class ChooseAsset extends React.Component<Props> {
+  _renderItem = ({item}: {item: DisplayItem | OtherItem}) => {
+    const onClick = () => this.props.onChoose(item)
+    switch (item.type) {
+      case 'display choice':
+        return (
+          <DisplayChoice
+            currencyCode={item.currencyCode}
+            onClick={onClick}
+            selected={false}
+            symbol={item.symbol}
+          />
+        )
+      case 'other choice':
+        return (
+          <OtherChoice
+            code={item.code}
+            disabledExplanation={item.disabledExplanation}
+            issuer={item.issuer}
+            onClick={onClick}
+            selected={false}
+          />
+        )
+      default:
+        throw new Error(`ChooseAsset: impossible item type encountered: ${item.type}`)
+    }
+  }
+
+  render() {
+    const sections = [
+      {
+        data: this.props.displayChoices.map(dc => ({...dc, key: dc.currencyCode})),
+        key: 'display choices',
+      },
+      ...(this.props.otherChoices.length === 0
+        ? []
+        : [
+            {
+              data: this.props.otherChoices.map(oc => ({
+                ...oc,
+                key: `${oc.code}:${oc.issuer}`,
+              })),
+              key: 'other choices',
+            },
+          ]),
+    ]
+    return (
+      <Box2 direction="vertical" style={styles.container}>
+        <Header whiteBackground={true} />
+        <Box2 direction="vertical" fullWidth={true} style={styles.listContainer}>
+          <SectionList sections={sections} renderItem={this._renderItem} renderSectionHeader={() => null} />
+        </Box2>
+      </Box2>
+    )
+  }
+}
+
+type DisplayChoiceProps = {
+  currencyCode: string,
+  onClick: () => void,
+  selected: boolean,
+  symbol: string,
+}
+const DisplayChoice = (props: DisplayChoiceProps) => (
+  <Box2
+    direction="horizontal"
+    style={styles.choiceContainer}
+    fullWidth={true}
+    gap="small"
+    gapStart={true}
+    gapEnd={true}
+  >
+    <Text type="Body">
+      Lumens (XLM) displayed as{' '}
+      <Text type="BodyExtrabold">
+        {props.currencyCode} ({props.symbol})
+      </Text>
+    </Text>
+  </Box2>
+)
+
+type OtherChoiceProps = {
+  code: string,
+  disabledExplanation: string,
+  issuer: string,
+  onClick: () => void,
+}
+const OtherChoice = (props: OtherChoiceProps) => (
+  <Box2
+    direction="horizontal"
+    style={styles.choiceContainer}
+    fullWidth={true}
+    gap="small"
+    gapStart={true}
+    gapEnd={true}
+  >
+    <Box2 direction="vertical">
+      <Text type="Body">
+        <Text type="BodyExtrabold">{props.code}</Text>/{props.issuer}
+      </Text>
+      {!!props.disabledExplanation && <Text type="BodySmall">{props.disabledExplanation}</Text>}
+    </Box2>
   </Box2>
 )
 
 const styles = styleSheetCreate({
+  choiceContainer: {
+    alignItems: 'center',
+    height: 40,
+  },
   container: {
     width: 360,
   },
-  header: {},
+  listContainer: {
+    maxHeight: 525 - 48,
+  },
 })
 
 export default ChooseAsset
--- shared/wallets/send-form/choose-asset/index.stories.js
+++ shared/wallets/send-form/choose-asset/index.stories.js
@@ -1,10 +1,28 @@
 // @flow
 import * as React from 'react'
-import {storiesOf} from '../../../stories/storybook'
+import {action, storiesOf} from '../../../stories/storybook'
 import ChooseAsset from '.'
 
+const props = {
+  displayChoices: [
+    {currencyCode: 'USD', symbol: '$', type: 'display choice'},
+    {currencyCode: 'EUR', symbol: '€', type: 'display choice'},
+    {currencyCode: 'GBP', symbol: '£', type: 'display choice'},
+    {currencyCode: 'CAD', symbol: '$', type: 'display choice'},
+    {currencyCode: 'CRC', symbol: '₡', type: 'display choice'},
+    {currencyCode: 'JPY', symbol: '¥', type: 'display choice'},
+    {currencyCode: 'FJD', symbol: '$', type: 'display choice'},
+  ],
+  onChoose: action('onChoose'),
+  otherChoices: [
+    {code: 'BTC', disabledExplanation: '', issuer: 'Stronghold.co', type: 'other choice'},
+    {code: 'KEYZ', disabledExplanation: '', issuer: 'Unknown', type: 'other choice'},
+    {code: 'HUGZ', disabledExplanation: `max doesn't accept HUGZ.`, issuer: 'Jed', type: 'other choice'},
+  ],
+}
+
 const load = () => {
-  storiesOf('Wallets/SendForm/Choose asset', module).add('Basic', () => <ChooseAsset />)
+  storiesOf('Wallets/SendForm/Choose asset', module).add('Basic', () => <ChooseAsset {...props} />)
 }
 
 export default load
