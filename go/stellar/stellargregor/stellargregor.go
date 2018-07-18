6ec58e90c7004b9dc9d8c999243f9486f3e10d41
Michał Zochniak <michal@zapu.net>
2018-07-18 19:14:10 +0200

    Handle stellar request gregors

--- go/stellar/stellargregor/stellargregor.go
+++ go/stellar/stellargregor/stellargregor.go
@@ -34,6 +34,8 @@ func (h *Handler) Create(ctx context.Context, cli gregor1.IncomingInterface, cat
 		return true, h.autoClaim(mctx, cli, category, item)
 	case "stellar.payment_status":
 		return true, h.paymentStatus(mctx, cli, category, item)
+	case "stellar.request_status":
+		return true, h.requestStatus(mctx, cli, category, item)
 	default:
 		if strings.HasPrefix(category, "stellar.") {
 			return false, fmt.Errorf("unknown handler category: %q", category)
@@ -65,6 +67,19 @@ func (h *Handler) paymentStatus(mctx libkb.MetaContext, cli gregor1.IncomingInte
 	defer h.G().GregorDismisser.DismissItem(mctx.Ctx(), cli, item.Metadata().MsgID())
 	mctx.CDebugf("%v: %v received", h.Name(), category)
 	var msg stellar1.PaymentStatusMsg
+	if err := json.Unmarshal(item.Body().Bytes(), &msg); err != nil {
+		mctx.CDebugf("error unmarshaling %s item: %s", category, err)
+		return err
+	}
+	mctx.CDebugf("%s unmarshaled: %+v", category, msg)
+
+	return nil
+}
+
+func (h *Handler) requestStatus(mctx libkb.MetaContext, cli gregor1.IncomingInterface, category string, item gregor.Item) error {
+	defer h.G().GregorDismisser.DismissItem(mctx.Ctx(), cli, item.Metadata().MsgID())
+	mctx.CDebugf("%v: %v received", h.Name(), category)
+	var msg stellar1.RequestStatusMsg
 	if err := json.Unmarshal(item.Body().Bytes(), &msg); err != nil {
 		mctx.CDebugf("error unmarshaling %s item: %s", category, err)
 		return err
