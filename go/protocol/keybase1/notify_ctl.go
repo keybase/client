3c8a2061b9a3cec4f6057eba4df3a2e2cda14207
Mike Maxim <mike@keyba.se>
2018-07-19 16:05:04 -0400

    conflicts

--- go/protocol/keybase1/notify_ctl.go
+++ go/protocol/keybase1/notify_ctl.go
@@ -9,27 +9,6 @@ import (
 )
 
 type NotificationChannels struct {
-<<<<<<< HEAD
-	Session       bool `codec:"session" json:"session"`
-	Users         bool `codec:"users" json:"users"`
-	Kbfs          bool `codec:"kbfs" json:"kbfs"`
-	Tracking      bool `codec:"tracking" json:"tracking"`
-	Favorites     bool `codec:"favorites" json:"favorites"`
-	Paperkeys     bool `codec:"paperkeys" json:"paperkeys"`
-	Keyfamily     bool `codec:"keyfamily" json:"keyfamily"`
-	Service       bool `codec:"service" json:"service"`
-	App           bool `codec:"app" json:"app"`
-	Chat          bool `codec:"chat" json:"chat"`
-	PGP           bool `codec:"pgp" json:"pgp"`
-	Kbfsrequest   bool `codec:"kbfsrequest" json:"kbfsrequest"`
-	Badges        bool `codec:"badges" json:"badges"`
-	Reachability  bool `codec:"reachability" json:"reachability"`
-	Team          bool `codec:"team" json:"team"`
-	Ephemeral     bool `codec:"ephemeral" json:"ephemeral"`
-	Chatkbfsedits bool `codec:"chatkbfsedits" json:"chatkbfsedits"`
-	Chatdev       bool `codec:"chatdev" json:"chatdev"`
-	Deviceclone   bool `codec:"deviceclone" json:"deviceclone"`
-=======
 	Session         bool `codec:"session" json:"session"`
 	Users           bool `codec:"users" json:"users"`
 	Kbfs            bool `codec:"kbfs" json:"kbfs"`
@@ -48,33 +27,12 @@ type NotificationChannels struct {
 	Ephemeral       bool `codec:"ephemeral" json:"ephemeral"`
 	Chatkbfsedits   bool `codec:"chatkbfsedits" json:"chatkbfsedits"`
 	Chatdev         bool `codec:"chatdev" json:"chatdev"`
+	Deviceclone     bool `codec:"deviceclone" json:"deviceclone"`
 	Chatattachments bool `codec:"chatattachments" json:"chatattachments"`
->>>>>>> rework attachment send flow to run through Deliverer
 }
 
 func (o NotificationChannels) DeepCopy() NotificationChannels {
 	return NotificationChannels{
-<<<<<<< HEAD
-		Session:       o.Session,
-		Users:         o.Users,
-		Kbfs:          o.Kbfs,
-		Tracking:      o.Tracking,
-		Favorites:     o.Favorites,
-		Paperkeys:     o.Paperkeys,
-		Keyfamily:     o.Keyfamily,
-		Service:       o.Service,
-		App:           o.App,
-		Chat:          o.Chat,
-		PGP:           o.PGP,
-		Kbfsrequest:   o.Kbfsrequest,
-		Badges:        o.Badges,
-		Reachability:  o.Reachability,
-		Team:          o.Team,
-		Ephemeral:     o.Ephemeral,
-		Chatkbfsedits: o.Chatkbfsedits,
-		Chatdev:       o.Chatdev,
-		Deviceclone:   o.Deviceclone,
-=======
 		Session:         o.Session,
 		Users:           o.Users,
 		Kbfs:            o.Kbfs,
@@ -93,8 +51,8 @@ func (o NotificationChannels) DeepCopy() NotificationChannels {
 		Ephemeral:       o.Ephemeral,
 		Chatkbfsedits:   o.Chatkbfsedits,
 		Chatdev:         o.Chatdev,
+		Deviceclone:     o.Deviceclone,
 		Chatattachments: o.Chatattachments,
->>>>>>> rework attachment send flow to run through Deliverer
 	}
 }
 
