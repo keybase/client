2eb64e119fc6ef6199cfe738ee732f29baed4a33
chrisnojima <cnojima@keyba.se>
2018-07-19 19:39:45 -0400

    WIP

--- shared/actions/__tests__/provision.test.js
+++ shared/actions/__tests__/provision.test.js
@@ -1,7 +1,6 @@
 // @flow
 /* eslint-env jest */
 import * as Types from '../../constants/types/provision'
-import * as DeviceTypes from '../../constants/types/devices'
 import * as RPCTypes from '../../constants/types/rpc-gen'
 import * as Constants from '../../constants/provision'
 import * as Tabs from '../../constants/tabs'
@@ -18,17 +17,17 @@ jest.unmock('immutable')
 const makeTypedState = (provisionState: Types.State): TypedState => ({provision: provisionState}: any)
 
 describe('provisioningManagerProvisioning', () => {
-  const m = _testing.makeProvisioningManager(false)
-  const callMap = m.getIncomingCallMap()
+  const manager = _testing.makeProvisioningManager(false)
+  const callMap = manager.getIncomingCallMap()
 
   it('cancels are correct', () => {
     const keys = ['keybase.1.gpgUi.selectKey', 'keybase.1.loginUi.getEmailOrUsername']
     keys.forEach(k => {
-      const r = {error: jest.fn(), result: jest.fn()}
+      const response = {error: jest.fn(), result: jest.fn()}
       // $FlowIssue
-      callMap[k](undefined, r, undefined)
-      expect(r.result).not.toHaveBeenCalled()
-      expect(r.error).toHaveBeenCalledWith({
+      callMap[k](undefined, response, undefined)
+      expect(response.result).not.toHaveBeenCalled()
+      expect(response.error).toHaveBeenCalledWith({
         code: RPCTypes.constantsStatusCode.scgeneric,
         desc: 'Canceling RPC',
       })
@@ -43,35 +42,42 @@ describe('provisioningManagerProvisioning', () => {
       'keybase.1.provisionUi.DisplaySecretExchanged',
     ]
     keys.forEach(k => {
-      const r = {error: jest.fn(), result: jest.fn()}
+      const response = {error: jest.fn(), result: jest.fn()}
       // $FlowIssue
-      callMap[k](undefined, r, undefined)
-      expect(r.result).not.toHaveBeenCalled()
-      expect(r.error).not.toHaveBeenCalled()
+      callMap[k](undefined, response, undefined)
+      expect(response.result).not.toHaveBeenCalled()
+      expect(response.error).not.toHaveBeenCalled()
     })
   })
 })
 
-describe('text code', () => {
-  it('happy path', () => {
-    const m = _testing.makeProvisioningManager(false)
-    const callMap = m.getIncomingCallMap()
-    const state = Constants.makeState()
+describe('text code happy path', () => {
+  let manager
+  let callMap
+  let response
+  let action
+  let state
+  const phrase = 'incomingSecret'
+  beforeEach(() => {
+    manager = _testing.makeProvisioningManager(false)
+    callMap = manager.getIncomingCallMap()
+    state = Constants.makeState()
     const call = callMap['keybase.1.provisionUi.DisplayAndPromptSecret']
     if (!call) {
       throw new Error('No call')
     }
-    const r = {error: jest.fn(), result: jest.fn()}
-    const phrase = 'mocktest'
-    const put: any = call(({phrase}: any), r, makeTypedState(state))
+    response = {error: jest.fn(), result: jest.fn()}
+    const put: any = call(({phrase}: any), response, makeTypedState(state))
     if (!put || !put.PUT) {
       throw new Error('no put')
     }
-    const action = put.PUT.action
-    expect(m._stashedResponse).toEqual(r)
-    expect(m._stashedResponseKey).toEqual('keybase.1.provisionUi.DisplayAndPromptSecret')
-    expect(action.payload.code.stringValue()).toEqual(phrase)
+    action = put.PUT.action
+  })
 
+  it('init', () => {
+    expect(manager._stashedResponse).toEqual(response)
+    expect(manager._stashedResponseKey).toEqual('keybase.1.provisionUi.DisplayAndPromptSecret')
+    expect(action.payload.code.stringValue()).toEqual(phrase)
     const nextState = makeTypedState(reducer(state, action))
     expect(nextState.provision.codePageTextCode.stringValue()).toEqual(phrase)
     expect(nextState.provision.error.stringValue()).toEqual('')
@@ -79,37 +85,49 @@ describe('text code', () => {
     expect(_testing.showCodePage(nextState)).toEqual(
       Saga.put(RouteTree.navigateAppend(['codePage'], [Tabs.loginTab, 'login']))
     )
+  })
 
+  it('submit text code', () => {
     const reply = 'reply'
     const submitAction = ProvisionGen.createSubmitTextCode({phrase: new HiddenString(reply)})
     const submitState = makeTypedState(reducer(state, submitAction))
 
     _testing.submitTextCode(submitState)
-    expect(r.result).toHaveBeenCalledWith({code: null, phrase: reply})
-    expect(r.error).not.toHaveBeenCalled()
+    expect(response.result).toHaveBeenCalledWith({code: null, phrase: reply})
+    expect(response.error).not.toHaveBeenCalled()
 
     // only submit once
-    expect(() => _testing.submitTextCode(nextState)).toThrow()
+    expect(() => _testing.submitTextCode(submitState)).toThrow()
   })
+})
 
-  it('error path', () => {
-    const m = _testing.makeProvisioningManager(false)
-    const callMap = m.getIncomingCallMap()
-    const state = Constants.makeState()
+describe('text code error path', () => {
+  let manager
+  let callMap
+  let response
+  let action
+  let state
+  const phrase = 'incomingSecret'
+  const error = 'anerror'
+  beforeEach(() => {
+    manager = _testing.makeProvisioningManager(false)
+    callMap = manager.getIncomingCallMap()
+    state = Constants.makeState()
     const call = callMap['keybase.1.provisionUi.DisplayAndPromptSecret']
     if (!call) {
       throw new Error('No call')
     }
-    const r = {error: jest.fn(), result: jest.fn()}
-    const phrase = 'mocktest'
-    const error = 'anerror'
-    const put: any = call(({phrase, previousErr: error}: any), r, makeTypedState(state))
+    response = {error: jest.fn(), result: jest.fn()}
+    const put: any = call(({phrase, previousErr: error}: any), response, makeTypedState(state))
     if (!put || !put.PUT) {
       throw new Error('no put')
     }
-    const action = put.PUT.action
-    expect(m._stashedResponse).toEqual(r)
-    expect(m._stashedResponseKey).toEqual('keybase.1.provisionUi.DisplayAndPromptSecret')
+    action = put.PUT.action
+  })
+
+  it('init', () => {
+    expect(manager._stashedResponse).toEqual(response)
+    expect(manager._stashedResponseKey).toEqual('keybase.1.provisionUi.DisplayAndPromptSecret')
     expect(action.payload.code.stringValue()).toEqual(phrase)
     expect(action.payload.error.stringValue()).toEqual(error)
 
@@ -120,25 +138,25 @@ describe('text code', () => {
     expect(_testing.showCodePage(nextState)).toBeFalsy()
 
     expect(_testing.submitTextCode(nextState)).toBeFalsy()
-    expect(r.result).not.toHaveBeenCalled()
-    expect(r.error).not.toHaveBeenCalled()
+    expect(response.result).not.toHaveBeenCalled()
+    expect(response.error).not.toHaveBeenCalled()
   })
 })
 
 describe('device name', () => {
   it('happy path', () => {
-    const m = _testing.makeProvisioningManager(false)
-    const callMap = m.getIncomingCallMap()
+    const manager = _testing.makeProvisioningManager(false)
+    const callMap = manager.getIncomingCallMap()
     const state = Constants.makeState()
     const call = callMap['keybase.1.provisionUi.PromptNewDeviceName']
     if (!call) {
       throw new Error('No call')
     }
 
-    const r = {error: jest.fn(), result: jest.fn()}
+    const response = {error: jest.fn(), result: jest.fn()}
     const existingDevices = []
     const error = 'invalid name'
-    const put: any = call(({errorMessage: error, existingDevices}: any), r, makeTypedState(state))
+    const put: any = call(({errorMessage: error, existingDevices}: any), response, makeTypedState(state))
     if (!put || !put.PUT) {
       throw new Error('no put')
     }
@@ -155,51 +173,77 @@ describe('device name', () => {
     const submitState = makeTypedState(reducer(nextState.provision, submitAction))
 
     _testing.submitDeviceName(submitState)
-    expect(r.result).toHaveBeenCalledWith(name)
-    expect(r.error).not.toHaveBeenCalled()
+    expect(response.result).toHaveBeenCalledWith(name)
+    expect(response.error).not.toHaveBeenCalled()
 
     // only submit once
     expect(() => _testing.submitDeviceName(submitState)).toThrow()
   })
 })
 
-describe('other device', () => {
-  // let m
-  // beforeEach(() => {
-  // return initializeCityDatabase();
-  // });
-
-  it('happy path', () => {
-    const m = _testing.makeProvisioningManager(false)
-    const callMap = m.getIncomingCallMap()
+describe('other device happy path', () => {
+  let manager
+  let nextState
+  let action
+  let response
+  const rpcDevices = [
+    ({deviceID: '0', name: 'mobile', type: 'mobile'}: any),
+    ({deviceID: '1', name: 'desktop', type: 'desktop'}: any),
+    ({deviceID: '2', name: 'backup', type: 'backup'}: any),
+  ]
+  const devices = rpcDevices.map(Constants.rpcDeviceToDevice)
+  beforeEach(() => {
+    manager = _testing.makeProvisioningManager(false)
+    const callMap = manager.getIncomingCallMap()
     const state = Constants.makeState()
     const call = callMap['keybase.1.provisionUi.chooseDevice']
     if (!call) {
       throw new Error('No call')
     }
-    const r = {error: jest.fn(), result: jest.fn()}
-    const rpcDevices = [
-      ({deviceID: '0', name: 'mobile', type: 'mobile'}: any),
-      ({deviceID: '1', name: 'desktop', type: 'desktop'}: any),
-      ({deviceID: '2', name: 'backup', type: 'backup'}: any),
-    ]
-    const devices = rpcDevices.map(Constants.rpcDeviceToDevice)
 
-    const put: any = call(({devices: rpcDevices}: any), r, makeTypedState(state))
+    response = {error: jest.fn(), result: jest.fn()}
+
+    const put: any = call(({devices: rpcDevices}: any), response, makeTypedState(state))
     if (!put || !put.PUT) {
       throw new Error('no put')
     }
-    const action = put.PUT.action
+    action = put.PUT.action
+    nextState = makeTypedState(reducer(state, action))
+  })
+
+  it('init', () => {
     expect(action.payload.devices).toEqual(devices)
-    const nextState = makeTypedState(reducer(state, action))
     expect(nextState.provision.devices.toArray()).toEqual(devices)
     expect(nextState.provision.error.stringValue()).toEqual('')
+  })
 
+  it('mobile', () => {
     const submitAction = ProvisionGen.createSubmitDeviceSelect({name: 'mobile'})
     const submitState = makeTypedState(reducer(nextState.provision, submitAction))
     _testing.submitDeviceSelect(submitState)
-    expect(r.result).toHaveBeenCalledWith('0')
-    expect(r.error).not.toHaveBeenCalled()
+    expect(response.result).toHaveBeenCalledWith('0')
+    expect(response.error).not.toHaveBeenCalled()
+    // only submit once
+    expect(() => _testing.submitDeviceSelect(submitState)).toThrow()
+  })
+
+  it('desktop', () => {
+    const submitAction = ProvisionGen.createSubmitDeviceSelect({name: 'desktop'})
+    const submitState = makeTypedState(reducer(nextState.provision, submitAction))
+    _testing.submitDeviceSelect(submitState)
+    expect(response.result).toHaveBeenCalledWith('1')
+    expect(response.error).not.toHaveBeenCalled()
+    // only submit once
+    expect(() => _testing.submitDeviceSelect(submitState)).toThrow()
+  })
+
+  // TODO should fail
+  it('backup', () => {
+    const submitAction = ProvisionGen.createSubmitDeviceSelect({name: 'backup'})
+    const submitState = makeTypedState(reducer(nextState.provision, submitAction))
+    _testing.submitDeviceSelect(submitState)
+    expect(response.result).toHaveBeenCalledWith('2')
+    expect(response.error).not.toHaveBeenCalled()
     // only submit once
     expect(() => _testing.submitDeviceSelect(submitState)).toThrow()
   })
