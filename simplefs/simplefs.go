fc5017284ef1a4d2bdd63933348abedb62811b12
John Zila <john@keyba.se>
2018-07-20 10:01:45 -0700

    simplefs: PR feedback for grammar fix

--- simplefs/simplefs.go
+++ simplefs/simplefs.go
@@ -628,7 +628,7 @@ func (k *SimpleFS) listRecursiveToDepth(opID keybase1.OpID,
 			des = append(des, d)
 			// Leave paths empty so we can skip the loop below.
 		} else {
-			// Start with a depth of 0
+			// Start with a depth of 0.
 			// A TLF root will have a `finalElem` of "".
 			// A subdirectory will have a `finalElem` of just the name.
 			paths = append(paths, pathStackElem{finalElem, 0})
