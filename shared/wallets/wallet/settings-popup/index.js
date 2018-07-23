8d30857c39cb836f08fa5cc095e4c0cc9820ac83
Steve Sanders <steve@keyba.se>
2018-07-23 16:16:26 -0700

    don't delete primary wallet

--- shared/wallets/wallet/settings-popup/index.js
+++ shared/wallets/wallet/settings-popup/index.js
@@ -53,60 +53,76 @@ const makeDropdownItem = (item: string, isSelected: boolean) => {
   )
 }
 
-const SettingsPopup = (props: Props) => (
-  <MaybePopup onClose={() => {}}>
-    <Box2 direction="vertical" style={styles.padding}>
-      <Box2 centerChildren={true} direction="vertical">
-        <Text style={{padding: globalMargins.small}} type="Header">
-          Settings
-        </Text>
-      </Box2>
-      <Text type="BodySmallSemibold">Account name</Text>
-      <ClickableBox style={styles.nameBox}>
-        <Text type="BodySemibold">{props.name}</Text>
-        <Icon style={{marginLeft: globalMargins.xtiny}} type="iconfont-edit" onClick={props.onEditName} />
-      </ClickableBox>
-      <Text type="BodySmallSemibold">Identity</Text>
-      <Box2 direction="horizontal" fullWidth={true} style={{marginBottom: globalMargins.medium}}>
-        <Avatar size={32} style={styles.avatar} username={props.type === 'default' ? props.user : ''} />
-        <Box2 direction="vertical">
-          <Text type="Header">
-            {props.type === 'default'
-              ? 'This is your default Keybase account.'
-              : 'This is a secondary account.'}
+const SettingsPopup = (props: Props) => {
+  const deleteOpacity = props.type === 'default' ? 0.3 : 1
+  return (
+    <MaybePopup onClose={() => {}}>
+      <Box2 direction="vertical" style={styles.padding}>
+        <Box2 centerChildren={true} direction="vertical">
+          <Text style={{padding: globalMargins.small}} type="Header">
+            Settings
           </Text>
-          <Text type="BodySmall">
-            {props.type === 'default'
-              ? 'All transactions and overall activity are tied to your Keybase identity.'
-              : 'Transactions will be tied to your Stellar public address only.'}
-          </Text>
-          {props.type === 'secondary' && (
-            <Text type="BodySmallPrimaryLink" onClick={props.onSetDefault}>
-              Set as default Keybase account
-            </Text>
-          )}
         </Box2>
+        <Text type="BodySmallSemibold">Account name</Text>
+        <ClickableBox style={styles.nameBox}>
+          <Text type="BodySemibold">{props.name}</Text>
+          <Icon style={{marginLeft: globalMargins.xtiny}} type="iconfont-edit" onClick={props.onEditName} />
+        </ClickableBox>
+        <Text type="BodySmallSemibold">Identity</Text>
+        <Box2 direction="horizontal" fullWidth={true} style={{marginBottom: globalMargins.medium}}>
+          <Avatar size={32} style={styles.avatar} username={props.type === 'default' ? props.user : ''} />
+          <Box2 direction="vertical">
+            <Text type="Header">
+              {props.type === 'default'
+                ? 'This is your default Keybase account.'
+                : 'This is a secondary account.'}
+            </Text>
+            <Text type="BodySmall">
+              {props.type === 'default'
+                ? 'All transactions and overall activity are tied to your Keybase identity.'
+                : 'Transactions will be tied to your Stellar public address only.'}
+            </Text>
+            {props.type === 'secondary' && (
+              <Text type="BodySmallPrimaryLink" onClick={props.onSetDefault}>
+                Set as default Keybase account
+              </Text>
+            )}
+          </Box2>
+        </Box2>
+        <Text type="BodySmallSemibold">Display currency</Text>
+        <Dropdown
+          items={makeDropdownItems(props)}
+          selected={makeDropdownItem(props.currency, false)}
+          onChanged={(node: React.Node) => {
+            // $FlowIssue doesn't understand key will be string
+            const selectedCurrency: string = (node && node.key) || null
+            props.onCurrencyChange(selectedCurrency)
+          }}
+          style={styles.dropdown}
+        />
+        <ClickableBox style={styles.remove} onClick={props.onDelete}>
+          <Icon
+            type="iconfont-trash"
+            style={{marginRight: globalMargins.tiny, opacity: deleteOpacity}}
+            color={globalColors.red}
+          />
+          <Text
+            type="BodySemibold"
+            style={{color: globalColors.red, opacity: deleteOpacity}}
+            className="hover-underline"
+          >
+            Remove account
+          </Text>
+        </ClickableBox>
+        {props.type === 'default' && (
+          <Text style={{textAlign: 'center'}} type="BodySmall">
+            You can’t remove your default account.
+          </Text>
+        )}
       </Box2>
-      <Text type="BodySmallSemibold">Display currency</Text>
-      <Dropdown
-        items={makeDropdownItems(props)}
-        selected={makeDropdownItem(props.currency, false)}
-        onChanged={(node: React.Node) => {
-          // $FlowIssue doesn't understand key will be string
-          const selectedCurrency: string = (node && node.key) || null
-          props.onCurrencyChange(selectedCurrency)
-        }}
-        style={styles.dropdown}
-      />
-      <ClickableBox style={styles.remove} onClick={props.onDelete}>
-        <Icon type="iconfont-trash" style={{marginRight: globalMargins.tiny}} color={globalColors.red} />
-        <Text type="BodySemibold" style={{color: globalColors.red}} className="hover-underline">
-          Remove account
-        </Text>
-      </ClickableBox>
-    </Box2>
-  </MaybePopup>
-)
+    </MaybePopup>
+  )
+}
 
 const styles = styleSheetCreate({
   avatar: {marginRight: globalMargins.xtiny},
