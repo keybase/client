7112f63a6909d72575c58eaf4f9e6ab8fbcd8c46
chrisnojima <chrisnojima@gmail.com>
2018-07-25 12:11:01 -0400

    New Provision: switch qr back to using image, update qr code to generate blue (#12970)
    * switch qr back to using image, update qr code to generate blue
    
    * relink react-native-camera

--- shared/common-adapters/image.desktop.js
+++ shared/common-adapters/image.desktop.js
@@ -3,9 +3,9 @@ import * as React from 'react'
 import type {Props, ReqProps} from './image'
 
 const RequireImage = ({src, style}: ReqProps) => <img src={src} style={style} />
-const FileImage = ({src, style, onDragStart, onLoad}: Props) => (
+const Image = ({src, style, onDragStart, onLoad}: Props) => (
   <img src={src} style={style} onDragStart={onDragStart} onLoad={onLoad} />
 )
 
-export default FileImage
+export default Image
 export {RequireImage}
