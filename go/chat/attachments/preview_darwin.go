515fdf573d9b0bdeb629b5f238708dbba6d0aebb
Mike Maxim <mike@keyba.se>
2018-07-27 22:04:06 -0400

    wip

--- go/chat/attachments/preview_darwin.go
+++ go/chat/attachments/preview_darwin.go
@@ -8,7 +8,9 @@ package attachments
 
 #include <AVFoundation/AVFoundation.h>
 
-NSMutableData* makeVideoThumbnail(const char* inFilename) {
+NSData* imageData = NULL;
+
+void MakeVideoThumbnail(const char* inFilename) {
 	NSString* filename = [NSString stringWithUTF8String:inFilename];
 	NSURL *videoURL = [NSURL fileURLWithPath:filename];
 
@@ -18,23 +20,47 @@ NSMutableData* makeVideoThumbnail(const char* inFilename) {
 	CMTime time = CMTimeMake(1, 1);
 	CGImageRef refImg = [generateImg copyCGImageAtTime:time actualTime:NULL error:&error];
 
-	NSMutableData *imageData = nil;
+	imageData = NULL;
 	CGImageDestinationRef destination = NULL;
 	imageData = [NSMutableData data];
 	destination = CGImageDestinationCreateWithData((CFMutableDataRef)imageData, kUTTypeJPEG, 1, NULL);
 	CGImageDestinationFinalize(destination);
-	return imageData;
+}
+
+const void* ImageData() {
+	return [imageData bytes];
+}
+
+void ImageFree() {
+	[imageData release];
+}
+
+int ImageLength() {
+	return [imageData length];
 }
 */
 import "C"
 import (
 	"io"
+	"io/ioutil"
+	"unsafe"
 
 	"github.com/keybase/client/go/chat/utils"
 	"golang.org/x/net/context"
 )
 
-func previewVideo(ctx context.Context, log utils.DebugLabeler, src io.Reader, basename string) (*PreviewRes, error) {
-	C.makeVideoThumbnail(C.CString(basename))
+func CFDataToBytes(cfData C.CFDataRef) ([]byte, error) {
+	return C.GoBytes(unsafe.Pointer(C.CFDataGetBytePtr(cfData)), C.int(C.CFDataGetLength(cfData))), nil
+}
+
+func previewVideo(ctx context.Context, log utils.DebugLabeler, src io.Reader, basename string) (res *PreviewRes, err error) {
+	defer log.Trace(ctx, func() error { return err }, "previewVideo")()
+	C.MakeVideoThumbnail(C.CString(basename))
+	ioutil.WriteFile(
+		"/tmp/s.jpg",
+		(*[1 << 30]byte)(unsafe.Pointer(C.ImageData()))[0:C.ImageLength()],
+		0644,
+	)
+	C.ImageFree()
 	return nil, nil
 }
