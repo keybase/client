989c7762c3a4bd27e09a1e48e582a43b7521ba76
Frederick Akalin <akalin@keyba.se>
2018-08-01 15:01:51 -0700

    Rewrite string

--- shared/util/string.js
+++ shared/util/string.js
@@ -6,15 +6,30 @@ function pluralize(str: string): string {
 }
 
 function toStringForLog(a: any): string {
-  if (typeof a === 'undefined') {
-    return 'undefined'
-  } else if (typeof a === 'string') {
-    return a
-  } else if (a instanceof Error) {
-    return a.stack
-  } else if (typeof a === 'object') {
-    return JSON.stringify(a)
+  switch (typeof a) {
+    case 'undefined':
+      return 'undefined'
+
+    case 'boolean':
+      return a.toString()
+
+    case 'number':
+      return a.toString()
+
+    case 'string':
+      return a
+
+    case 'function':
+      break
+
+    case 'object':
+      // Includes null.
+      if (a instanceof Error) {
+        return a.stack
+      }
+      return JSON.stringify(a)
   }
+
   return (a && a.toString && a.toString()) || 'Failed to turn item to string in toStringForLog'
 }
 
