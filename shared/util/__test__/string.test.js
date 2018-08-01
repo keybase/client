ab28ad60403ffecc31a6eafc3dee887e0069a95c
Frederick Akalin <akalin@keyba.se>
2018-08-01 15:22:57 -0700

    More tests

--- shared/util/__test__/string.test.js
+++ shared/util/__test__/string.test.js
@@ -6,4 +6,16 @@ describe('toStringForLog', () => {
   it('undefined', () => {
     expect(toStringForLog(undefined)).toBe('undefined')
   })
+
+  it('boolean', () => {
+    expect(toStringForLog(true)).toBe('true')
+    expect(toStringForLog(false)).toBe('false')
+  })
+
+  it('number', () => {
+    expect(toStringForLog(3.14)).toBe('3.14')
+    expect(toStringForLog(1 / 0)).toBe('Infinity')
+    expect(toStringForLog(-1 / 0)).toBe('-Infinity')
+    expect(toStringForLog(0 / 0)).toBe('NaN')
+  })
 })
