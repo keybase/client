fc0fd96b9fe2ce63469de4e1a1b68362318015c0
Song Gao <song@gao.io>
2018-08-01 15:49:52 -0700

    rename row/index.js to row/containers.js

--- shared/fs/row/containers.js
+++ shared/fs/row/containers.js
@@ -6,6 +6,4 @@ import Still from './still-container'
 import Editing from './editing-container'
 import Uploading from './uploading-container'
 
-// TODO: rename this into containers.js
-
 export {TlfType, Tlf, Still, Editing, Placeholder, Uploading}
