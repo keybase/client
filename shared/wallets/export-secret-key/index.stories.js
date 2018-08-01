4f05b4e11ccec3fd7eb364e34128bfcf03d95ca5
Nathan Smith <nathan@nathansmith.io>
2018-08-01 16:45:23 -0400

    Add provider around ExportSecretKey

--- shared/wallets/export-secret-key/index.stories.js
+++ shared/wallets/export-secret-key/index.stories.js
@@ -1,17 +1,19 @@
 // @flow
 import React from 'react'
-import {action, storiesOf} from '../../stories/storybook'
+import {action, storiesOf, createPropProvider} from '../../stories/storybook'
 import ExportSecretKey from '.'
 
 const load = () => {
-  storiesOf('Wallets', module).add('Export secret key', () => (
-    <ExportSecretKey
-      onClose={action('onClose')}
-      onLoadSecretKey={action('onLoadSecretKey')}
-      secretKey="SETECASTRONOMY"
-      username="cecileb"
-    />
-  ))
+  storiesOf('Wallets', module)
+    .addDecorator(createPropProvider())
+    .add('Export secret key', () => (
+      <ExportSecretKey
+        onClose={action('onClose')}
+        onLoadSecretKey={action('onLoadSecretKey')}
+        secretKey="SETECASTRONOMY"
+        username="cecileb"
+      />
+    ))
 }
 
 export default load
