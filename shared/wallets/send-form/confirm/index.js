8cfa066af56e1be54e9515f2c8e5441664c429ab
Nathan Smith <nathan@nathansmith.io>
2018-08-03 16:15:21 -0400

    Add banner

--- shared/wallets/send-form/confirm/index.js
+++ shared/wallets/send-form/confirm/index.js
@@ -4,6 +4,7 @@ import * as Kb from '../../../common-adapters'
 import * as Styles from '../../../styles'
 // import Body from '../body/container'
 // import Header from '../header'
+import Banner from '../banner'
 import Memo from '../memo/container'
 // import Note from '../note/container'
 import Participants from '../participants/container'
@@ -19,7 +20,18 @@ export default function ConfirmSend(props: ConfirmSendProps) {
   return (
     <Kb.MaybePopup onClose={props.onClose}>
       <Kb.Box2 direction="vertical" style={styles.container}>
-        <Kb.Box2 direction="horizontal" fullWidth={true} style={styles.header} />
+        <Kb.Box2 direction="vertical" fullWidth={true} centerChildren={true} style={styles.header}>
+          <Kb.Text type="BodySmall" style={styles.headerText}>
+            Sending lumens worth
+          </Kb.Text>
+          <Kb.Text type="HeaderBigExtrabold" style={styles.headerText}>
+            $1
+          </Kb.Text>
+        </Kb.Box2>
+        <Banner
+          background="Announcements"
+          text="The conversion rate has changed since you got to this screen."
+        />
         <Participants />
         <Memo />
         <Kb.Box2
@@ -75,6 +87,12 @@ const styles = Styles.styleSheetCreate({
       height: 144,
       flex: 1,
       backgroundColor: Styles.globalColors.purple,
+    },
+  }),
+  headerText: Styles.platformStyles({
+    isElectron: {
+      color: Styles.globalColors.white,
+      textTransform: 'uppercase',
     },
   }),
   buttonContainer: Styles.platformStyles({
