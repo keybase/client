286f529470efb91c1448418459299755b28ddece
Mike Maxim <mike@keyba.se>
2018-08-03 15:24:08 -0400

    wip

--- shared/react-native/android/app/src/main/java/io/keybase/ossifrage/util/VideoHelper.java
+++ shared/react-native/android/app/src/main/java/io/keybase/ossifrage/util/VideoHelper.java
@@ -1,10 +1,24 @@
 package io.keybase.ossifrage.util;
 
+import android.media.MediaMetadataRetriever;
+import android.graphics.Bitmap;
+import java.io.ByteArrayOutputStream;
+
 public class VideoHelper implements keybase.NativeVideoHelper {
     public byte[] thumbnail(String filename) {
-        return null;
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        retriever.setDataSource(filename);
+        Bitmap bmp = retriever.getPrimaryImage();
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+        byte[] ret = stream.toByteArray();
+        retriever.release();
+        return ret;
     }
     public long duration(String filename) {
-        return 0;
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        retriever.setDataSource(filename);
+        String time = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
+        return Integer.parseInt(time);
     }
 }
