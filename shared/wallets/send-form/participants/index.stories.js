0f95e97ac773a541e83ad46173ac38547a57d4a8
Steve Sanders <steve@keyba.se>
2018-08-06 11:40:29 -0700

    wallet send participants story file

---
+++ shared/wallets/send-form/participants/index.stories.js
@@ -0,0 +1,26 @@
+// @flow
+import * as React from 'react'
+import * as Sb from '../../../stories/storybook'
+import {Box} from '../../../common-adapters'
+import Participants from '.'
+
+const provider = Sb.createPropProviderWithCommon({
+  // TODO mock out meaningful values once type `OwnProps` is defined
+  Participants: props => ({}),
+})
+
+const load = () => {
+  const story = Sb.storiesOf('Wallets/SendForm/Participants', module).addDecorator(story => (
+    <Box style={{maxWidth: 360}}>{story()}</Box>
+  ))
+  story.addDecorator(provider)
+  story.add('Normal', () => <Participants incorrect={false}/>)
+  story.add('Address Error', () => (
+    <Participants incorrect={true} />
+  ))
+  story.add('User match', () => (
+    <Participants username="yen" fullname="Addie Stokes" onShowProfile={Sb.action('onShowProfile')}/>
+  ))
+}
+
+export default load
