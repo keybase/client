6ece9ca31323d844ccf771bcc164b5a846381d33
Nathan Smith <nathan@nathansmith.io>
2018-08-07 11:02:33 -0400

    Convert header to SFC

--- shared/wallets/confirm-form/header.js
+++ shared/wallets/confirm-form/header.js
@@ -10,56 +10,28 @@ type HeaderProps = {|
   assetConversion?: string,
 |}
 
-type HeaderState = {|
-  coinOffset: ?Styles.StylesCrossPlatform,
-|}
-
-class Header extends React.Component<HeaderProps, HeaderState> {
-  _headerContentRef = React.createRef()
-
-  state = {
-    coinOffset: undefined,
-  }
-
-  componentDidMount() {
-    if (this._headerContentRef.current && this._headerContentRef.current.clientHeight <= 164) {
-      // eslint-disable-next-line react/no-did-mount-set-state
-      this.setState({
-        coinOffset: {
-          position: 'relative',
-          top: -20,
-        },
-      })
-    }
-  }
-
-  render() {
-    return (
-      <Kb.Box2 direction="vertical" fullWidth={true} style={styles.header}>
-        <Kb.BackButton
-          onClick={this.props.onBack}
-          style={styles.backButton}
-          iconColor={Styles.globalColors.white}
-          textStyle={styles.backButtonText}
-        />
-        <Kb.Box2 direction="vertical" fullHeight={true} centerChildren={true} style={styles.headerContent}>
-          <Kb.Icon
-            type={
-              Styles.isMobile ? 'icon-fancy-stellar-sending-desktop' : 'icon-fancy-stellar-sending-mobile'
-            }
-            style={Kb.iconCastPlatformStyles(styles.headerIcon)}
-          />
-          <Kb.Text type="BodySmall" style={styles.headerText}>
-            Sending{!!this.props.assetConversion && ` ${this.props.assetType} worth`}
-          </Kb.Text>
-          <Kb.Text type="HeaderBigExtrabold" style={styles.headerText}>
-            {this.props.assetConversion ? this.props.assetConversion : this.props.amount}
-          </Kb.Text>
-        </Kb.Box2>
-      </Kb.Box2>
-    )
-  }
-}
+const Header = (props: HeaderProps) => (
+  <Kb.Box2 direction="vertical" fullWidth={true} style={styles.header}>
+    <Kb.BackButton
+      onClick={props.onBack}
+      style={styles.backButton}
+      iconColor={Styles.globalColors.white}
+      textStyle={styles.backButtonText}
+    />
+    <Kb.Box2 direction="vertical" fullHeight={true} centerChildren={true} style={styles.headerContent}>
+      <Kb.Icon
+        type={Styles.isMobile ? 'icon-fancy-stellar-sending-desktop' : 'icon-fancy-stellar-sending-mobile'}
+        style={Kb.iconCastPlatformStyles(styles.headerIcon)}
+      />
+      <Kb.Text type="BodySmall" style={styles.headerText}>
+        Sending{!!props.assetConversion && ` ${props.assetType} worth`}
+      </Kb.Text>
+      <Kb.Text type="HeaderBigExtrabold" style={styles.headerText}>
+        {props.assetConversion ? props.assetConversion : props.amount}
+      </Kb.Text>
+    </Kb.Box2>
+  </Kb.Box2>
+)
 
 const styles = Styles.styleSheetCreate({
   header: Styles.platformStyles({
