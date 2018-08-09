7ab441c55a453ff6118d1dbdd052fa77bf3b9947
Maxwell Krohn <themax@gmail.com>
2018-08-08 21:14:51 -0400

    some stubs for how we'll call the real loader

--- go/teams/fast.go
+++ go/teams/fast.go
@@ -1,6 +1,7 @@
 package teams
 
 import (
+	"fmt"
 	lru "github.com/hashicorp/golang-lru"
 	"github.com/keybase/client/go/libkb"
 	"github.com/keybase/client/go/protocol/keybase1"
@@ -35,24 +36,69 @@ type FastTeamChainLoader struct {
 // sigchain sequence numbers are neeeded unstubbed.
 func (f *FastTeamChainLoader) Load(m libkb.MetaContext, arg keybase1.FastTeamLoadArg) (res keybase1.FastTeamLoadRes, err error) {
 
-	// will load via call to .load below
+	defer m.CTrace(fmt.Sprintf("FastTeamChainLoader#Load(%+v)", arg), func() error { return err })()
 
-	return res, err
+	flr, err := f.load(m, fastLoadArg{FastTeamLoadArg: arg})
+	if err != nil {
+		return res, err
+	}
+
+	res = flr.FastTeamLoadRes
+
+	res.Name, err = f.verifyTeamNameViaParentLoad(m, arg.Id, arg.Public, flr.unverifiedName, flr.upPointer)
+	if err != nil {
+		return res, err
+	}
+
+	return res, nil
 }
 
-// resolveIDToNameViaMerkelTree takes a team ID, and a pointer to a parent team's sigchain, and computes
+// verifyTeamNameViaParentLoad takes a team ID, and a pointer to a parent team's sigchain, and computes
 // the full resolved team name. If the pointer is null, we'll assume this is a root team and do the
 // verification via hash-comparison.
-func (f *FastTeamChainLoader) resolveIDToNameViaParentLoad(m libkb.MetaContext, id keybase1.TeamID, parent *keybase1.LinkTriple) (res keybase1.TeamName, err error) {
+func (f *FastTeamChainLoader) verifyTeamNameViaParentLoad(m libkb.MetaContext, id keybase1.TeamID, isPublic bool, unverifiedName keybase1.TeamName, parent *keybase1.LinkTriple) (res keybase1.TeamName, err error) {
 
-	// will load via call to .load below
+	defer m.CTrace(fmt.Sprintf("FastTeamChainLoader#verifyTeamNameViaParentLoad(%s,%s)", id, unverifiedName), func() error { return err })()
 
-	return res, err
+	if parent == nil {
+		if !unverifiedName.IsRootTeam() {
+			return res, NewBadNameError("expected a root team")
+		}
+		if !unverifiedName.ToTeamID(isPublic).Eq(id) {
+			return res, NewBadNameError("root team v. team ID mismatch")
+		}
+		return unverifiedName, nil
+	}
+
+	parentRes, err := f.load(m, fastLoadArg{
+		FastTeamLoadArg: keybase1.FastTeamLoadArg{
+			Id:     parent.Id,
+			Public: isPublic,
+		},
+		seqnosNeeded: []keybase1.Seqno{parent.Pair.Seqno},
+	})
+	if err != nil {
+		return res, err
+	}
+	downPointer, ok := parentRes.downPointers[parent.Pair.Seqno]
+	if !ok {
+		return res, NewBadNameError("down pointer not found in parent")
+	}
+	suffix := downPointer.NameComponent
+
+	parentName, err := f.verifyTeamNameViaParentLoad(m, parent.Id, isPublic, parentRes.unverifiedName, parentRes.upPointer)
+	if err != nil {
+		return res, err
+	}
+
+	return parentName.Append(suffix)
 }
 
 type fastLoadRes struct {
 	keybase1.FastTeamLoadRes
-	downPointers map[keybase1.Seqno]keybase1.DownPointer
+	unverifiedName keybase1.TeamName
+	downPointers   map[keybase1.Seqno]keybase1.DownPointer
+	upPointer      *keybase1.LinkTriple
 }
 
 type fastLoadArg struct {
