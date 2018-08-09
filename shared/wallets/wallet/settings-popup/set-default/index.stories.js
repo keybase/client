39648595f30f82e2abaf8a6de94697623dba2357
Daniel Ayoub <danny.a95@gmail.com>
2018-08-09 11:54:59 -0400

    Stellar storybook: confirm default account modal (#13226)
    * add wallet 48
    
    * the rest of the owl
    
    * storyshots

---
+++ shared/wallets/wallet/settings-popup/set-default/index.stories.js
@@ -0,0 +1,17 @@
+// @flow
+import * as React from 'react'
+import * as Sb from '../../../../stories/storybook'
+import SetDefaultAccount from '.'
+
+const load = () => {
+  Sb.storiesOf('Wallets/Wallet', module).add('Confirm set as default', () => (
+    <SetDefaultAccount
+      accountName="Second account"
+      onAccept={Sb.action('onAccept')}
+      onClose={Sb.action('onClose')}
+      username="cecileb"
+    />
+  ))
+}
+
+export default load
