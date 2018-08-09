78cb9f6cf59a0bf672671f243a1fe1d8fe9c1f85
Maxwell Krohn <themax@gmail.com>
2018-08-08 20:13:28 -0400

    rejuggle

--- go/teams/fast.go
+++ go/teams/fast.go
@@ -6,6 +6,14 @@ import (
 	"github.com/keybase/client/go/protocol/keybase1"
 )
 
+//
+// fast.go
+//
+// Routines for fast-team loading. In fast team loading, we ignore most signatures
+// and use the Merkle Tree as a source of truth. This is good enough for getting
+// directly into a chat, where it's not necessary to see a list of team members.
+//
+
 // FastTeamChainLoader loads teams using the "fast approach." It doesn't compute
 // membership or check any signatures. It just checks for consistency against the merkle
 // tree, and audits that the merkle tree is being faithfully constructed.
@@ -14,6 +22,8 @@ type FastTeamChainLoader struct {
 	// single-flight lock on TeamID
 	locktab libkb.LockTable
 
+	// Hold onto FastTeamLoad by-products as long as we have room
+	// We don't store them to disk (as we do slow Load objects).
 	// LRU of TeamID -> keybase1.FastTeamData
 	lru *lru.Cache
 }
@@ -23,7 +33,10 @@ type FastTeamChainLoader struct {
 // from the server, and then the keys can be output in the result. Or, if you're doing a fast-load for
 // the purposes of loading parent teams to compute a name, then you can specify "seqnosNeeded" to say that certain
 // sigchain sequence numbers are neeeded unstubbed.
-func (f *FastTeamChainLoader) Load(m libkb.MetaContext, arg keybase1.FastLoadTeamArg) (res keybase1.FastLoadTeamRes, err error) {
+func (f *FastTeamChainLoader) Load(m libkb.MetaContext, arg keybase1.FastTeamLoadArg) (res keybase1.FastTeamLoadRes, err error) {
+
+	// will load via call to .load below
+
 	return res, err
 }
 
@@ -31,5 +44,25 @@ func (f *FastTeamChainLoader) Load(m libkb.MetaContext, arg keybase1.FastLoadTea
 // the full resolved team name. If the pointer is null, we'll assume this is a root team and do the
 // verification via hash-comparison.
 func (f *FastTeamChainLoader) resolveIDToNameViaParentLoad(m libkb.MetaContext, id keybase1.TeamID, parent *keybase1.LinkTriple) (res keybase1.TeamName, err error) {
+
+	// will load via call to .load below
+
 	return res, err
+}
+
+type loadRes struct {
+	keybase1.FastTeamLoadRes
+	downPointers map[keybase1.Seqno]keybase1.DownPointer
+}
+
+type loadArg struct {
+	keybase1.FastTeamLoadArg
+	seqnosNeeded []keybase1.Seqno
+}
+
+func (f *FastTeamChainLoader) load(m libkb.MetaContext, arg loadArg) (res *loadRes, err error) {
+
+	// this should be meat.
+
+	return nil, nil
 }
