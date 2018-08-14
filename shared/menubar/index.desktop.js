82e00943060d22f7fc2b1eee33085c70737f5842
John Zila <john@jzila.com>
2018-08-14 09:41:57 -0500

    [KBFS-3230] widget chat container (#13227)
    * first pass at refactoring widget
    
    * next step of refactor
    
    * further import refactors for widget
    
    * more
    
    * add box for viewing all chats
    
    * spacing
    
    * storybook
    
    * more refactor and fixup
    
    * storybook
    
    * PR feedback
    
    * storybook

--- shared/menubar/index.desktop.js
+++ shared/menubar/index.desktop.js
@@ -1,44 +1,15 @@
 // @flow
-import Folders, {type FolderType, type Props as FolderProps} from '../folders/index.desktop'
-import React, {Component} from 'react'
-import UserAdd from './user-add.desktop'
-import flags from '../util/feature-flags'
-import {
-  Box,
-  Box2,
-  Icon,
-  Text,
-  Button,
-  FloatingMenu,
-  Badge,
-  ButtonBar,
-  type IconType,
-} from '../common-adapters'
-import {
-  fsTab,
-  peopleTab,
-  chatTab,
-  devicesTab,
-  teamsTab,
-  walletsTab,
-  gitTab,
-  settingsTab,
-  type Tab,
-} from '../constants/tabs'
-import {
-  globalStyles,
-  globalColors,
-  globalMargins,
-  desktopStyles,
-  collapseStyles,
-  platformStyles,
-} from '../styles'
+import * as React from 'react'
+import * as Kb from '../common-adapters'
+import Flags from '../util/feature-flags'
+import * as Tabs from '../constants/tabs'
+import * as Styles from '../styles'
+import ChatContainer from './chat.desktop'
 import {isDarwin} from '../constants/platform'
 import * as SafeElectron from '../util/safe-electron.desktop'
 import {throttle} from 'lodash-es'
 
 export type Props = {
-  folderProps: ?FolderProps,
   isAsyncWriteHappening: boolean,
   logIn: () => void,
   loggedIn: boolean,
@@ -55,13 +26,16 @@ export type Props = {
 }
 
 type State = {|
-  selected: FolderType,
   showingMenu: boolean,
 |}
 
-class MenubarRender extends Component<Props, State> {
+const ArrowTick = () => (
+  // Css triangle!
+  <Kb.Box style={styles.arrowTick} />
+)
+
+class MenubarRender extends React.Component<Props, State> {
   state: State = {
-    selected: 'private',
     showingMenu: false,
   }
 
@@ -80,25 +54,23 @@ class MenubarRender extends Component<Props, State> {
 
   render() {
     // TODO: refactor all this duplicated code!
-    return this.props.loggedIn ? this._renderFolders() : this._renderLoggedOut()
+    return this.props.loggedIn ? this._renderLoggedIn() : this._renderLoggedOut()
   }
 
   _renderLoggedOut() {
-    const styles = stylesPublic
-
-    const menuColor = this.state.showingMenu ? globalColors.black_60 : globalColors.black_40
-    const menuStyle = platformStyles({
+    const menuColor = this.state.showingMenu ? Styles.globalColors.black_60 : Styles.globalColors.black_40
+    const menuStyle = Styles.platformStyles({
       isElectron: {
-        ...desktopStyles.clickable,
+        ...Styles.desktopStyles.clickable,
       },
     })
 
     return (
-      <Box style={styles.container}>
+      <Kb.Box style={styles.widgetContainer}>
         {isDarwin && <style>{_realCSS}</style>}
         {isDarwin && <ArrowTick />}
-        <Box style={{...stylesTopRow, justifyContent: 'flex-end'}}>
-          <Icon
+        <Kb.Box style={Styles.collapseStyles([styles.topRow, {justifyContent: 'flex-end'}])}>
+          <Kb.Icon
             style={menuStyle}
             color={menuColor}
             hoverColor={menuColor}
@@ -106,69 +78,69 @@ class MenubarRender extends Component<Props, State> {
             onClick={() => this.setState(prevState => ({showingMenu: !prevState.showingMenu}))}
             ref={this.attachmentRef}
           />
-          <FloatingMenu
+          <Kb.FloatingMenu
             visible={this.state.showingMenu}
             attachTo={this.attachmentRef.current}
             items={this._menuItems(this.props.badgeInfo || {})}
             onHidden={() => this.setState({showingMenu: false})}
           />
-        </Box>
-        <Box style={{...globalStyles.flexBoxColumn, flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-          <Icon type="icon-keybase-logo-logged-out-64" style={stylesLogo} color={globalColors.yellow} />
-          <Text type="Body" small={true} style={{alignSelf: 'center', marginTop: 6}}>
+        </Kb.Box>
+        <Kb.Box style={{...Styles.globalStyles.flexBoxColumn, flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <Kb.Icon type="icon-keybase-logo-logged-out-64" style={Styles.collapseStyles([styles.logo])} color={Styles.globalColors.yellow} />
+          <Kb.Text type="Body" small={true} style={{alignSelf: 'center', marginTop: 6}}>
             You're logged out of Keybase!
-          </Text>
-          <ButtonBar direction="row">
-            <Button type="Primary" label="Log In" onClick={this.props.logIn} />
-          </ButtonBar>
-        </Box>
-      </Box>
+          </Kb.Text>
+          <Kb.ButtonBar direction="row">
+            <Kb.Button type="Primary" label="Log In" onClick={this.props.logIn} />
+          </Kb.ButtonBar>
+        </Kb.Box>
+      </Kb.Box>
     )
   }
 
-  _menuView(title: string, iconType: IconType, count: number) {
+  _menuView(title: string, iconType: Kb.IconType, count: number) {
     return (
-      <Box2 direction="horizontal" style={{width: '100%'}}>
-        <Box style={{marginRight: globalMargins.xsmall, position: 'relative'}}>
-          <Icon type={iconType} color={globalColors.black_20} fontSize={20} />
-          {!!count && <Badge badgeNumber={count} badgeStyle={{position: 'absolute', left: 14, top: -2}} />}
-        </Box>
-        <Text className="title" type="Body" style={collapseStyles([{color: undefined}])}>
+      <Kb.Box2 direction="horizontal" style={{width: '100%'}}>
+        <Kb.Box style={{marginRight: Styles.globalMargins.xsmall, position: 'relative'}}>
+          <Kb.Icon type={iconType} color={Styles.globalColors.black_20} fontSize={20} />
+          {!!count && <Kb.Badge badgeNumber={count} badgeStyle={{position: 'absolute', left: 14, top: -2}} />}
+        </Kb.Box>
+        <Kb.Text className="title" type="Body" style={Styles.collapseStyles([{color: undefined}])}>
           {title}
-        </Text>
-      </Box2>
+        </Kb.Text>
+      </Kb.Box2>
     )
   }
 
   _menuItems(countMap: Object) {
     return [
-      ...(flags.walletsEnabled
+      ...(Flags.walletsEnabled
         ? [
             {
               title: 'Wallet',
-              view: this._menuView('Wallet', 'iconfont-nav-wallets', countMap[walletsTab] || 0),
-              onClick: () => this.props.openApp(walletsTab),
+              view: this._menuView('Wallet', 'iconfont-nav-wallets', countMap[Tabs.walletsTab] || 0),
+              onClick: () => this.props.openApp(Tabs.walletsTab),
             },
           ]
         : []),
       {
         title: 'Git',
-        view: this._menuView('Git', 'iconfont-nav-git', countMap[gitTab] || 0),
-        onClick: () => this.props.openApp(gitTab),
+        view: this._menuView('Git', 'iconfont-nav-git', countMap[Tabs.gitTab] || 0),
+        onClick: () => this.props.openApp(Tabs.gitTab),
       },
       {
         title: 'Devices',
-        view: this._menuView('Devices', 'iconfont-nav-devices', countMap[devicesTab] || 0),
-        onClick: () => this.props.openApp(devicesTab),
+        view: this._menuView('Devices', 'iconfont-nav-devices', countMap[Tabs.devicesTab] || 0),
+        onClick: () => this.props.openApp(Tabs.devicesTab),
       },
       {
         title: 'Settings',
-        view: this._menuView('Settings', 'iconfont-nav-settings', countMap[settingsTab] || 0),
-        onClick: () => this.props.openApp(settingsTab),
+        view: this._menuView('Settings', 'iconfont-nav-settings', countMap[Tabs.settingsTab] || 0),
+        onClick: () => this.props.openApp(Tabs.settingsTab),
       },
       'Divider',
       ...(this.props.loggedIn ? [{title: 'Open main app', onClick: () => this.props.openApp()}] : []),
-      {title: 'Open folders', onClick: () => this.props.openApp(fsTab)},
+      {title: 'Open files', onClick: () => this.props.openApp(Tabs.fsTab)},
       'Divider',
       {title: 'Keybase.io', onClick: () => this.props.showUser()},
       {title: 'Report a bug', onClick: this.props.showBug},
@@ -182,91 +154,45 @@ class MenubarRender extends Component<Props, State> {
     this.props.refresh()
   }
 
-  _renderFolders() {
-    const newPrivate = {
-      ...(this.props.folderProps && this.props.folderProps.private),
-      ignored: [],
-      extraRows: [
-        <UserAdd
-          key="useraddPriv"
-          isPublic={false}
-          onAdded={path => this._onAdd(path)}
-          username={this.props.username}
-        />,
-      ],
-    }
-
-    const newPublic = {
-      ...(this.props.folderProps && this.props.folderProps.public),
-      ignored: [],
-      extraRows: [
-        <UserAdd
-          key="useraddPub"
-          isPublic={true}
-          onAdded={path => this._onAdd(path)}
-          username={this.props.username}
-        />,
-      ],
-    }
-
-    const styles = this.state.selected === 'private' ? stylesPrivate : stylesPublic
-
-    const mergedProps = {
-      ...this.props.folderProps,
-      onClick: this.props.onFolderClick,
-      private: newPrivate,
-      public: newPublic,
-      onSwitchTab: selected => this.setState({selected}),
-      selected: this.state.selected,
-      onRekey: this.props.onRekey,
-    }
-
-    const badgeTypesInHeader: Array<Tab> = [peopleTab, chatTab, fsTab, teamsTab]
-    const badgesInMenu = [...(flags.walletsEnabled ? [walletsTab] : []), gitTab, devicesTab, settingsTab]
+  _renderLoggedIn() {
+    const badgeTypesInHeader: Array<Tabs.Tab> = [Tabs.peopleTab, Tabs.chatTab, Tabs.fsTab, Tabs.teamsTab]
+    const badgesInMenu = [...(Flags.walletsEnabled ? [Tabs.walletsTab] : []), Tabs.gitTab, Tabs.devicesTab, Tabs.settingsTab]
     const badgeCountInMenu = badgesInMenu.reduce(
       (acc, val) => (this.props.badgeInfo[val] ? acc + this.props.badgeInfo[val] : acc),
       0
     )
 
     return (
-      <Box style={styles.container}>
+      <Kb.Box style={styles.widgetContainer}>
         {isDarwin && <style>{_realCSS}</style>}
         {isDarwin && <ArrowTick />}
-        <Box style={{...stylesTopRow, borderBottom: `1px solid ${globalColors.black_05}`}}>
-          <Box
-            style={{
-              ...globalStyles.flexBoxRow,
-              flex: 1,
-              justifyContent: 'center',
-              alignItems: 'center',
-              marginLeft: 24 + 8,
-            }}
-          >
+        <Kb.Box style={Styles.collapseStyles([styles.topRow, {borderBottom: `1px solid ${Styles.globalColors.black_05}`}])}>
+          <Kb.Box style={styles.headerBadgesContainer}>
             {badgeTypesInHeader.map(tab => (
               <BadgeIcon key={tab} tab={tab} countMap={this.props.badgeInfo} openApp={this.props.openApp} />
             ))}
-          </Box>
-          <Box
-            style={collapseStyles([
-              desktopStyles.clickable,
+          </Kb.Box>
+          <Kb.Box
+            style={Styles.collapseStyles([
+              Styles.desktopStyles.clickable,
               {
-                marginRight: globalMargins.tiny,
+                marginRight: Styles.globalMargins.tiny,
                 position: 'relative',
               },
             ])}
             onClick={() => this.setState(prevState => ({showingMenu: !prevState.showingMenu}))}
           >
-            <Icon
-              color={globalColors.darkBlue4}
-              hoverColor={globalColors.black_75}
+            <Kb.Icon
+              color={Styles.globalColors.darkBlue4}
+              hoverColor={Styles.globalColors.black_75}
               type="iconfont-nav-more"
               ref={this.attachmentRef}
             />
             {!!badgeCountInMenu && (
-              <Badge badgeNumber={badgeCountInMenu} badgeStyle={{position: 'absolute', left: 14, top: -2}} />
+              <Kb.Badge badgeNumber={badgeCountInMenu} badgeStyle={{position: 'absolute', left: 14, top: -2}} />
             )}
-          </Box>
-          <FloatingMenu
+          </Kb.Box>
+          <Kb.FloatingMenu
             items={this._menuItems(this.props.badgeInfo || {})}
             visible={this.state.showingMenu}
             onHidden={() =>
@@ -277,133 +203,122 @@ class MenubarRender extends Component<Props, State> {
             attachTo={this.attachmentRef.current}
             position="bottom right"
           />
-        </Box>
-        <Folders {...mergedProps} />
+        </Kb.Box>
+        <ChatContainer onViewAll={() => this.props.openApp(Tabs.chatTab)} />
         {this.props.isAsyncWriteHappening && (
-          <Box
-            style={{
-              ...globalStyles.flexBoxColumn,
-              justifyContent: 'center',
-              alignItems: 'center',
-              minHeight: 32,
-              backgroundColor: globalColors.white,
-              padding: 8,
-            }}
-          >
-            <Icon type="icon-loader-uploading-16" />
-            <Text type="BodySmall">UPLOADING CHANGES...</Text>
-          </Box>
+          <Kb.Box style={styles.uploadingContainer}>
+            <Kb.Icon type="icon-loader-uploading-16" />
+            <Kb.Text type="BodySmall">UPLOADING CHANGES...</Kb.Text>
+          </Kb.Box>
         )}
-      </Box>
+      </Kb.Box>
     )
   }
 }
 
 const _realCSS = `
 body {
-  background-color: ${globalColors.transparent};
+  background-color: ${Styles.globalColors.transparent};
 }
 `
-
-const ArrowTick = () => (
-  // Css triangle!
-  <Box
-    style={{
-      height: 0,
-      width: 0,
-      position: 'absolute',
-      left: 0,
-      right: 0,
-      marginLeft: 'auto',
-      marginRight: 'auto',
-      top: -6,
-      borderLeft: '6px solid transparent',
-      borderRight: '6px solid transparent',
-      borderBottom: `6px solid ${globalColors.white}`,
-    }}
-  />
-)
 
 const BadgeIcon = ({
   tab,
   countMap,
   openApp,
 }: {
-  tab: Tab,
+  tab: Tabs.Tab,
   countMap: Object,
   openApp: (tab: ?string) => void,
 }) => {
   const count = countMap[tab]
 
-  if (tab === devicesTab && !count) {
+  if (tab === Tabs.devicesTab && !count) {
     return null
   }
 
-  const iconMap: {[key: Tab]: IconType} = {
-    [peopleTab]: 'iconfont-nav-people',
-    [chatTab]: 'iconfont-nav-chat',
-    [devicesTab]: 'iconfont-nav-devices',
-    [fsTab]: 'iconfont-nav-files',
-    [teamsTab]: 'iconfont-nav-teams',
+  const iconMap: {[key: Tabs.Tab]: Kb.IconType} = {
+    [Tabs.peopleTab]: 'iconfont-nav-people',
+    [Tabs.chatTab]: 'iconfont-nav-chat',
+    [Tabs.devicesTab]: 'iconfont-nav-devices',
+    [Tabs.fsTab]: 'iconfont-nav-files',
+    [Tabs.teamsTab]: 'iconfont-nav-teams',
   }
-  const iconType: ?IconType = iconMap[tab]
+  const iconType: ?Kb.IconType = iconMap[tab]
 
   if (!iconType) {
     return null
   }
 
   return (
-    <Box
-      style={{...desktopStyles.clickable, marginLeft: 7, marginRight: 7, position: 'relative'}}
+    <Kb.Box
+      style={{...Styles.desktopStyles.clickable, marginLeft: 7, marginRight: 7, position: 'relative'}}
       onClick={() => openApp(tab)}
     >
-      <Icon color={globalColors.darkBlue4} hoverColor={globalColors.black_75} fontSize={22} type={iconType} />
-      {!!count && <Badge badgeNumber={count} badgeStyle={{position: 'absolute', top: -6, right: -8}} />}
-    </Box>
+      <Kb.Icon color={Styles.globalColors.darkBlue4} hoverColor={Styles.globalColors.black_75} fontSize={22} type={iconType} />
+      {!!count && <Kb.Badge badgeNumber={count} badgeStyle={{position: 'absolute', top: -6, right: -8}} />}
+    </Kb.Box>
   )
 }
 
-const borderRadius = 4
-
-const stylesContainer = {
-  ...globalStyles.flexBoxColumn,
-  flex: 1,
-  position: 'relative',
-  marginTop: 13,
-  borderTopLeftRadius: borderRadius,
-  borderTopRightRadius: borderRadius,
-}
-
-const stylesTopRow = {
-  ...globalStyles.flexBoxRow,
-  alignItems: 'center',
-  backgroundColor: globalColors.darkBlue2,
-  flex: 1,
-  minHeight: 40,
-  maxHeight: 40,
-  paddingLeft: 8,
-  paddingRight: 8,
-  borderTopLeftRadius: borderRadius,
-  borderTopRightRadius: borderRadius,
-}
-
-const stylesPrivate = {
-  container: {
-    ...stylesContainer,
-    backgroundColor: globalColors.darkBlue,
+const styles = Styles.styleSheetCreate({
+  widgetContainer: {
+    ...Styles.globalStyles.flexBoxColumn,
+    flex: 1,
+    position: 'relative',
+    marginTop: isDarwin ? 13 : 0,
+    borderTopLeftRadius: Styles.globalMargins.xtiny,
+    borderTopRightRadius: Styles.globalMargins.xtiny,
+    backgroundColor: Styles.globalColors.darkBlue,
   },
-}
-
-const stylesPublic = {
-  container: {
-    ...stylesContainer,
-    backgroundColor: globalColors.white,
+  topRow: {
+    ...Styles.globalStyles.flexBoxRow,
+    alignItems: 'center',
+    backgroundColor: Styles.globalColors.darkBlue2,
+    flex: 1,
+    minHeight: 40,
+    maxHeight: 40,
+    paddingLeft: 8,
+    paddingRight: 8,
+    borderTopLeftRadius: Styles.globalMargins.xtiny,
+    borderTopRightRadius: Styles.globalMargins.xtiny,
   },
-}
-
-const stylesLogo = {
-  alignSelf: 'center',
-  marginBottom: 12,
-}
+  logo: {
+    alignSelf: 'center',
+    marginBottom: 12,
+  },
+  headerBadgesContainer: {
+    ...Styles.globalStyles.flexBoxRow,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 24 + 8,
+  },
+  arrowTick: {
+    height: 0,
+    width: 0,
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    top: -6,
+    borderLeftWidth: 6,
+    borderRightWidth: 6,
+    borderBottomWidth: 6,
+    borderStyle: 'solid',
+    borderLeftColor: 'transparent',
+    borderRightColor: 'transparent',
+    borderBottomColor: Styles.globalColors.darkBlue2,
+  },
+  uploadingContainer: {
+    ...Styles.globalStyles.flexBoxColumn,
+    justifyContent: 'center',
+    alignItems: 'center',
+    minHeight: 32,
+    backgroundColor: Styles.globalColors.white,
+    padding: 8,
+  },
+})
 
 export default MenubarRender
