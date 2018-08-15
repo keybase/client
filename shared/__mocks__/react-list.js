57e04b85961dd1223b219b59ca600a76c7e9525e
Nathan Smith <nathan@nathansmith.io>
2018-08-15 11:26:08 -0400

    better mock

--- shared/__mocks__/react-list.js
+++ shared/__mocks__/react-list.js
@@ -3,9 +3,31 @@ import * as React from 'react'
 if (!__STORYBOOK__) {
   throw new Error('Invalid load of mock')
 }
-class ReactListMock extends React.Component<any> {
+
+/*
+react-list
+
+*/
+
+type Props = {
+  length: number,
+  itemRenderer: (index: number, key: number) => React.Node,
+}
+
+class ReactListMock extends React.Component<Props, {}> {
   render() {
-    return null
+    const length = this.props.length > 20 ? 20 : this.props.length
+
+    return (
+      <div className="ReactListMock">
+        {[...Array(length).keys()].map(index => (
+          <div className="ReactListItem" key={index}>
+            {this.props.itemRenderer(index, index)}
+          </div>
+        ))}
+      </div>
+    )
   }
 }
+
 export default ReactListMock
