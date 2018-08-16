a9c67e5dcc0f7d0776200f41bec4880d6d1114d5
Song Gao <song@gao.io>
2018-08-16 12:24:45 -0700

    review feedback from jzila

--- shared/fs/header/breadcrumb.desktop.js
+++ shared/fs/header/breadcrumb.desktop.js
@@ -2,14 +2,7 @@
 import * as React from 'react'
 import * as Types from '../../constants/types/fs'
 import * as Styles from '../../styles'
-import {
-  Avatar,
-  avatarCastPlatformStyles,
-  Box,
-  Icon,
-  Text,
-  iconCastPlatformStyles,
-} from '../../common-adapters'
+import * as Kb from '../../common-adapters'
 import BreadcrumbPopup from './breadcrumb-popup.desktop'
 
 type Props = {
@@ -18,87 +11,83 @@ type Props = {
 }
 
 const Breadcrumb = ({dropdownItems, shownItems}: Props) => (
-  <Box style={styles.container}>
+  <Kb.Box style={styles.container}>
     {!!dropdownItems && (
-      <Box style={styles.folderBreadcrumb}>
+      <Kb.Box style={styles.folderBreadcrumb}>
         <BreadcrumbPopup items={dropdownItems} />
-        <Icon
+        <Kb.Icon
           type="iconfont-arrow-right"
-          style={iconCastPlatformStyles(styles.icon)}
+          style={Kb.iconCastPlatformStyles(styles.icon)}
           fontSize={11}
           boxStyle={styles.iconBox}
         />
-      </Box>
+      </Kb.Box>
     )}
     {shownItems.map((item, idxItem) => (
       <React.Fragment key={Types.pathToString(item.path)}>
         {item.isTeamTlf && (
-          <Avatar
+          <Kb.Avatar
             size={16}
             teamname={item.name}
             isTeam={true}
-            style={avatarCastPlatformStyles(styles.teamAvatar)}
+            style={Kb.avatarCastPlatformStyles(styles.teamAvatar)}
           />
         )}
         {!item.isLastItem ? (
-          <Box style={styles.breadcrumbNonLastItemBox}>
-            <Text key={idxItem} onClick={item.onClick} type="BodySmallSemibold">
+          <Kb.Box style={styles.breadcrumbNonLastItemBox}>
+            <Kb.Text key={idxItem} onClick={item.onClick} type="BodySmallSemibold">
               {item.name}
-            </Text>
-          </Box>
+            </Kb.Text>
+          </Kb.Box>
         ) : (
-          <Box style={styles.breadcrumbLastItemBox}>
-            <Text type="BodyBig" selectable={true}>
+          <Kb.Box style={styles.breadcrumbLastItemBox}>
+            <Kb.Text type="BodyBig" selectable={true}>
               {// We are splitting on ',' here, so it won't work for
               // long names that don't have comma. If this becomes a
               // problem, we might have to do smarter splitting that
               // involve other characters, or just break the long name
               // apart into 3-character groups.
               item.name.split(',').map((sub, idx, {length}) => (
-                <Text key={idx} type={'BodyBig'} style={styles.lastNameText}>
+                <Kb.Text key={idx} type={'BodyBig'} style={styles.lastNameText}>
                   {sub}
                   {idx !== length - 1 ? ',' : ''}
-                </Text>
+                </Kb.Text>
               ))}
-            </Text>
-          </Box>
+            </Kb.Text>
+          </Kb.Box>
         )}
         {!item.isLastItem && (
-          <Icon
+          <Kb.Icon
             type="iconfont-arrow-right"
-            style={iconCastPlatformStyles(styles.icon)}
+            style={Kb.iconCastPlatformStyles(styles.icon)}
             fontSize={11}
             boxStyle={styles.iconBox}
           />
         )}
       </React.Fragment>
     ))}
-  </Box>
+  </Kb.Box>
 )
 
 const styles = Styles.styleSheetCreate({
-  container: Styles.platformStyles({
-    isElectron: {
-      ...Styles.globalStyles.flexBoxRow,
-      left: 0,
-      right: 0,
-      flexGrow: 1,
-      alignItems: 'center',
-      paddingLeft: 16,
-      paddingRight: 16,
-      height: 24,
-    },
-  }),
-  folderBreadcrumb: Styles.platformStyles({
-    isElectron: {
-      ...Styles.globalStyles.flexBoxRow,
-      alignItems: 'center',
-      paddingLeft: 0,
-      paddingRight: 0,
-      flexShrink: 0,
-      flexWrap: 'wrap',
-    },
-  }),
+  container: {
+    ...Styles.globalStyles.flexBoxRow,
+    left: 0,
+    right: 0,
+    flexGrow: 1,
+    alignItems: 'center',
+    paddingLeft: 16,
+    paddingRight: 16,
+    height: 24,
+  },
+  folderBreadcrumb: {
+    ...Styles.globalStyles.flexBoxRow,
+    alignItems: 'center',
+    paddingLeft: 0,
+    paddingRight: 0,
+    flexShrink: 0,
+    flexWrap: 'wrap',
+  },
   teamAvatar: {
     marginRight: Styles.globalMargins.xtiny,
   },
@@ -119,12 +108,10 @@ const styles = Styles.styleSheetCreate({
       whiteSpace: 'nowrap',
     },
   }),
-  breadcrumbLastItemBox: Styles.platformStyles({
-    isElectron: {
-      display: 'flex',
-      flexWrap: 'wrap',
-    },
-  }),
+  breadcrumbLastItemBox: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
   lastNameText: Styles.platformStyles({
     isElectron: {
       wordBreak: 'break-word',
