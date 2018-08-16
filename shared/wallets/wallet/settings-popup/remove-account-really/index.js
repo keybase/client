157f3b68ded36d1f3abd0641309653c2ab012624
Nathan Smith <nathan@nathansmith.io>
2018-08-16 16:36:12 -0400

    No need for a conditional render with toast

--- shared/wallets/wallet/settings-popup/remove-account-really/index.js
+++ shared/wallets/wallet/settings-popup/remove-account-really/index.js
@@ -28,42 +28,40 @@ class _RemoveAccountReally extends React.Component<Props, State> {
 
   render() {
     return (
-      <Kb.MaybePopup onClose={this.props.onClose}>
-        <Kb.Box style={styles.box}>
-          <Kb.Icon
-            type={isMobile ? 'icon-wallet-receive-64' : 'icon-wallet-receive-48'}
-            style={Kb.iconCastPlatformStyles(styles.icon)}
-          />
-          <Kb.Text style={styles.warning} type="Header">
-            One last thing! Make sure you keep a copy of your secret key before removing{' '}
-            <Kb.Text type="Header" style={styles.italic}>
-              {this.props.name}
-            </Kb.Text>.
-          </Kb.Text>
-          <Kb.Text type="BodySmall">Paste it in a 100% safe place.</Kb.Text>
-          <Kb.ButtonBar style={styles.buttonbar}>
-            <Kb.Button
-              label="Copy secret key"
-              onClick={this.copy}
-              type="Wallet"
-              ref={r => (this._attachmentRef = r)}
+      // <Kb.MaybePopup onClose={this.props.onClose}>
+      <Kb.Box style={styles.box}>
+        <Kb.Icon
+          type={isMobile ? 'icon-wallet-receive-64' : 'icon-wallet-receive-48'}
+          style={Kb.iconCastPlatformStyles(styles.icon)}
+        />
+        <Kb.Text style={styles.warning} type="Header">
+          One last thing! Make sure you keep a copy of your secret key before removing{' '}
+          <Kb.Text type="Header" style={styles.italic}>
+            {this.props.name}
+          </Kb.Text>.
+        </Kb.Text>
+        <Kb.Text type="BodySmall">Paste it in a 100% safe place.</Kb.Text>
+        <Kb.ButtonBar style={styles.buttonbar}>
+          <Kb.Button
+            label="Copy secret key"
+            onClick={this.copy}
+            type="Wallet"
+            ref={r => (this._attachmentRef = r)}
+          >
+            <Kb.Toast
+              visible={this.state.showingToast}
+              attachTo={this._attachmentRef}
+              position={'top center'}
             >
-              {this.state.showingToast && (
-                <Kb.Toast
-                  visible={this.state.showingToast}
-                  attachTo={this._attachmentRef}
-                  position={'top center'}
-                >
-                  <Kb.Text type="BodySmall" style={styles.text}>
-                    Copied to clipboard
-                  </Kb.Text>
-                </Kb.Toast>
-              )}
-            </Kb.Button>
-            <Kb.Button label="Cancel" onClick={this.props.onClose} type="Secondary" />
-          </Kb.ButtonBar>
-        </Kb.Box>
-      </Kb.MaybePopup>
+              <Kb.Text type="BodySmall" style={styles.text}>
+                Copied to clipboard
+              </Kb.Text>
+            </Kb.Toast>
+          </Kb.Button>
+          <Kb.Button label="Cancel" onClick={this.props.onClose} type="Secondary" />
+        </Kb.ButtonBar>
+      </Kb.Box>
+      // </Kb.MaybePopup>
     )
   }
 }
