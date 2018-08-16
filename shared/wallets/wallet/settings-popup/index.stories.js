5ab3c5b4f0399a8cddeeeedfbf00d01d17229fac
Nathan Smith <nathan@nathansmith.io>
2018-08-16 13:11:50 -0400

    Update the popup stories a bit

--- shared/wallets/wallet/settings-popup/index.stories.js
+++ shared/wallets/wallet/settings-popup/index.stories.js
@@ -3,6 +3,8 @@ import * as React from 'react'
 import * as Sb from '../../../stories/storybook'
 import SettingsPopup from '.'
 import confirmDefaultAccount from './set-default/index.stories'
+import RemoveAccountDialog from './remove-account'
+import ReallyRemoveDialog from './remove-account-really'
 
 const defaultSettingsProps = {
   name: 'awesome account',
@@ -28,10 +30,28 @@ const secondarySettingsProps = {
   onCurrencyChange: Sb.action('onCurrencyChange'),
 }
 
+const warningProps = {
+  name: 'awesome account',
+  currency: '0.00 XLM',
+  keys: '2 keys',
+  onDelete: Sb.action('onDelete'),
+  onClose: Sb.action('onClose'),
+}
+
+const reallyProps = {
+  name: 'awesome account',
+  onCopyKey: Sb.action('onCopyKey'),
+  onClose: Sb.action('onClose'),
+}
+
 const load = () => {
   Sb.storiesOf('Wallets/Wallet/Settings', module)
     .add('Default', () => <SettingsPopup {...defaultSettingsProps} />)
     .add('Secondary', () => <SettingsPopup {...secondarySettingsProps} />)
+    .add('Settings, default', () => <SettingsPopup {...defaultSettingsProps} />)
+    .add('Settings, secondary', () => <SettingsPopup {...secondarySettingsProps} />)
+    .add('Remove warning', () => <RemoveAccountDialog {...warningProps} />)
+    .add('Really remove', () => <ReallyRemoveDialog {...reallyProps} />)
   confirmDefaultAccount()
 }
 
