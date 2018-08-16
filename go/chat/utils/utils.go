9cb5cca5c93513801dabe7fa7fe8f9b7164e7e9a
Joshua Blum <jblum18@gmail.com>
2018-08-16 15:40:58 -0400

    Change logging tag to get picked up by log profile

--- go/chat/utils/utils.go
+++ go/chat/utils/utils.go
@@ -425,7 +425,7 @@ func (d DebugLabeler) Trace(ctx context.Context, f func() error, format string, 
 		start := time.Now()
 		d.log.CDebugf(ctx, "++Chat: + %s: %s", d.label, msg)
 		return func() {
-			d.log.CDebugf(ctx, "++Chat: - %s: %s -> %s (%v)", d.label, msg,
+			d.log.CDebugf(ctx, "++Chat: - %s: %s -> %s [time=%v]", d.label, msg,
 				libkb.ErrToOk(f()), time.Since(start))
 		}
 	}
