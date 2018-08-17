0ee65709194a60c0a3ade31f366c8e01a53809a4
Nathan Smith <nathan@nathansmith.io>
2018-08-17 17:16:35 -0400

    RemoveAccount looks good too

--- shared/wallets/wallet/settings-popup/remove-account.js
+++ shared/wallets/wallet/settings-popup/remove-account.js
@@ -12,31 +12,54 @@ export type Props = {
   onClose: () => void,
 }
 
-const RemoveAccountDialog = (props: Props) => (
-  <WalletModal
-    onClose={props.onClose}
-    bottomButtons={[
-      <Kb.Button key={0} label="Cancel" onClick={props.onClose} type="Secondary" style={styles.button} />,
-      <Kb.Button key={1} label="Yes, remove" onClick={props.onDelete} type="Danger" style={styles.button} />,
-    ]}
-  >
-    <Kb.Icon
-      type={Styles.isMobile ? 'icon-wallet-remove-64' : 'icon-wallet-remove-48'}
-      style={Kb.iconCastPlatformStyles(styles.icon)}
-    />
-    <Kb.Text style={styles.warningText} type="Header">
-      Are you sure you want to remove <Kb.Text type="HeaderItalic">{props.name}</Kb.Text> from Keybase?
-    </Kb.Text>
-    <Kb.Text type="BodySmall">Balance:</Kb.Text>
-    <Kb.Text type="BodySmallExtrabold">{props.currency}</Kb.Text>
-    <Kb.Text type="BodySmallExtrabold">{props.keys}</Kb.Text>
-  </WalletModal>
-)
+const RemoveAccountDialog = (props: Props) => {
+  const buttons = [
+    <Kb.Button
+      fullWidth={Styles.isMobile}
+      key={0}
+      label="Cancel"
+      onClick={props.onClose}
+      type="Secondary"
+      style={styles.button}
+    />,
+    <Kb.Button
+      fullWidth={Styles.isMobile}
+      key={1}
+      label="Yes, remove"
+      onClick={props.onDelete}
+      type="Danger"
+      style={styles.button}
+    />,
+  ]
+
+  return (
+    <WalletModal onClose={props.onClose} bottomButtons={Styles.isMobile ? buttons.reverse() : buttons}>
+      <Kb.Icon
+        type={Styles.isMobile ? 'icon-wallet-remove-64' : 'icon-wallet-remove-48'}
+        style={Kb.iconCastPlatformStyles(styles.icon)}
+      />
+      <Kb.Text style={styles.warningText} type="Header">
+        Are you sure you want to remove <Kb.Text type="HeaderItalic">{props.name}</Kb.Text> from Keybase?
+      </Kb.Text>
+      <Kb.Text type="BodySmall">Balance:</Kb.Text>
+      <Kb.Text type="BodySmallExtrabold">{props.currency}</Kb.Text>
+      <Kb.Text type="BodySmallExtrabold">{props.keys}</Kb.Text>
+    </WalletModal>
+  )
+}
 
 const styles = Styles.styleSheetCreate({
-  icon: {
-    marginBottom: Styles.globalMargins.large,
-  },
+  icon: Styles.platformStyles({
+    common: {
+      marginBottom: Styles.globalMargins.large,
+    },
+    isElectron: {
+      marginTop: Styles.globalMargins.medium,
+    },
+    isMobile: {
+      marginTop: Styles.globalMargins.xlarge,
+    },
+  }),
   warningText: {
     marginBottom: Styles.globalMargins.tiny,
     textAlign: 'center',
