9f8e48054e9305dc271d44afa4613bad169ae6d5
Adam Spooner <adam@keyba.se>
2018-08-17 11:16:42 -0400

    Add spellchecking.

--- shared/desktop/renderer/renderer-load.desktop.js
+++ shared/desktop/renderer/renderer-load.desktop.js
@@ -13,3 +13,26 @@ require('electron').ipcRenderer.once('load', (event, options) => {
     document.head.appendChild(script)
   })
 })
+
+// Require the electron spellchecker
+const electronSpellchecker = require('electron-spellchecker')
+
+// Retrieve required properties
+const SpellCheckHandler = electronSpellchecker.SpellCheckHandler
+const ContextMenuListener = electronSpellchecker.ContextMenuListener
+const ContextMenuBuilder = electronSpellchecker.ContextMenuBuilder
+
+// Configure the spellcheckhandler
+window.spellCheckHandler = new SpellCheckHandler()
+window.spellCheckHandler.attachToInput()
+
+// Start off as "US English, America"
+window.spellCheckHandler.switchLanguage('en-US')
+
+// Create the builder with the configured spellhandler
+let contextMenuBuilder = new ContextMenuBuilder(window.spellCheckHandler)
+
+// Add context menu listener
+let contextMenuListener = new ContextMenuListener(info => {
+  contextMenuBuilder.showPopupMenu(info)
+})
