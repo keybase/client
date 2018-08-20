9c98e2deba25da6ae460fc54186fa7e262e099ee
Nathan Smith <nathan@nathansmith.io>
2018-08-20 16:57:32 -0400

    Add text options

--- shared/wallets/participants/from-field.js
+++ shared/wallets/participants/from-field.js
@@ -11,27 +11,28 @@ type FromFieldProps = {|
   walletContents: string,
 |}
 
+type DropdownTextProps = {
+  text: string,
+}
+
+const DropdownText = ({text, ...props}: DropdownTextProps) => (
+  <Kb.Box2 {...props} direction="horizontal" centerChildren={true} fullWidth={true}>
+    <Kb.Text type="BodySemibold">{text}</Kb.Text>
+  </Kb.Box2>
+)
+
+const items = [
+  <DropdownText key="link-existing" text="Link an existing Stellar account" />,
+  <DropdownText key="create-new" text="Create a new account" />,
+]
+
 const FromField = (props: FromFieldProps) => (
   <Row heading="From:">
     {props.isConfirm && (
       <WalletEntry keybaseUser={props.username} name={props.walletName} contents={props.walletContents} />
     )}
     {/* TODO: Add wallet dropdown for wallet->wallet */}
-    {!props.isConfirm && (
-      <Kb.Dropdown
-        onChanged={() => {}}
-        items={[
-          <WalletEntry
-            key={0}
-            keybaseUser={props.username}
-            name={props.walletName}
-            contents={props.walletContents}
-            showWalletIcon={false}
-            center={true}
-          />,
-        ]}
-      />
-    )}
+    {!props.isConfirm && <Kb.Dropdown onChanged={() => {}} items={items} />}
   </Row>
 )
 
