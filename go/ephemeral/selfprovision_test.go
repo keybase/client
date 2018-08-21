926e97364bc71f6438121e19e424481432de81e5
Joshua Blum <jblum18@gmail.com>
2018-08-21 14:21:48 -0400

    ForceDeleteAll old deviceEKs on self provision

--- go/ephemeral/selfprovision_test.go
+++ go/ephemeral/selfprovision_test.go
@@ -14,12 +14,29 @@ func TestEphemeralSelfProvision(t *testing.T) {
 	tc, user := ephemeralKeyTestSetup(t)
 	defer tc.Cleanup()
 
+	ctx := context.TODO()
 	g := tc.G
+	m := libkb.NewMetaContextForTest(tc)
 	teamID := createTeam(tc)
 
 	ekLib := g.GetEKLib()
-	teamEK1, err := ekLib.GetOrCreateLatestTeamEK(context.Background(), teamID)
+	teamEK1, err := ekLib.GetOrCreateLatestTeamEK(ctx, teamID)
 	require.NoError(t, err)
+
+	// Publish a few deviceEKs on the cloned account and make sure the self
+	// provision goes through successfully and we can continue to generate
+	// deviceEKs after.
+	merkleRootPtr, err := g.GetMerkleClient().FetchRootFromServer(m, libkb.EphemeralKeyMerkleFreshness)
+	require.NoError(t, err)
+	merkleRoot := *merkleRootPtr
+	_, err = publishNewDeviceEK(ctx, g, merkleRoot)
+	require.NoError(t, err)
+	_, err = publishNewDeviceEK(ctx, g, merkleRoot)
+	require.NoError(t, err)
+	deviceEKStorage := g.GetDeviceEKStorage()
+	maxGen, err := deviceEKStorage.MaxGeneration(ctx)
+	require.NoError(t, err)
+	require.EqualValues(t, 3, maxGen)
 
 	// Now self provision the user and make sure she can still access the teamEK
 	secUI := user.NewSecretUI()
@@ -31,14 +48,26 @@ func TestEphemeralSelfProvision(t *testing.T) {
 		LoginUI:     provLoginUI,
 	}
 
-	m := libkb.NewMetaContextForTest(tc).WithUIs(uis)
+	m = m.WithUIs(uis)
 	libkb.CreateClonedDevice(tc, m)
 	newName := "uncloneme"
 	eng := engine.NewSelfProvisionEngine(g, newName)
 	err = engine.RunEngine2(m, eng)
 	require.NoError(t, err)
 
-	teamEK2, err := g.GetTeamEKBoxStorage().Get(context.Background(), teamID, teamEK1.Metadata.Generation)
+	teamEK2, err := g.GetTeamEKBoxStorage().Get(ctx, teamID, teamEK1.Metadata.Generation)
 	require.NoError(t, err)
 	require.Equal(t, teamEK1, teamEK2)
+
+	// After self provisioning we should only have a single deviceEK, and have
+	// no issues producing new ones.
+	maxGen, err = deviceEKStorage.MaxGeneration(ctx)
+	require.NoError(t, err)
+	require.EqualValues(t, 1, maxGen)
+
+	_, err = publishNewDeviceEK(ctx, g, merkleRoot)
+	require.NoError(t, err)
+	maxGen, err = deviceEKStorage.MaxGeneration(ctx)
+	require.NoError(t, err)
+	require.EqualValues(t, 2, maxGen)
 }
