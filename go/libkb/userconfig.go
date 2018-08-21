cf54c7981ed359c7834b290bf25a0cab3bb2d218
Frederick Akalin <akalin@keyba.se>
2018-08-21 15:30:48 -0700

    Use username

--- go/libkb/userconfig.go
+++ go/libkb/userconfig.go
@@ -5,49 +5,22 @@ package libkb
 
 import (
 	"encoding/hex"
-	"strings"
 
 	keybase1 "github.com/keybase/client/go/protocol/keybase1"
+	"github.com/keybase/client/go/username"
 	jsonw "github.com/keybase/go-jsonw"
 )
 
 //==================================================================
 
-// NormalizedUsername is a username that has been normalized (toLowered)
-// and therefore will compare correctly against other normalized usernames.
-type NormalizedUsername string
+type NormalizedUsername = username.NormalizedUsername
 
-// NewNormalizedUsername makes a normalized username out of a non-normalized
-// plain string username
 func NewNormalizedUsername(s string) NormalizedUsername {
-	return NormalizedUsername(strings.ToLower(s))
+	return username.NormalizedUsername(s)
 }
-
-// Eq returns true if the given normalized usernames are equal
-func (n NormalizedUsername) Eq(n2 NormalizedUsername) bool {
-	return string(n) == string(n2)
-}
-
-// String returns the normalized username as a string (in lower case)
-func (n NormalizedUsername) String() string { return string(n) }
-
-// IsNil returns true if the username is the empty string
-func (n NormalizedUsername) IsNil() bool { return len(string(n)) == 0 }
 
 func NormalizedUsernamesToStrings(names []NormalizedUsername) []string {
-	y := make([]string, len(names))
-	for i, n := range names {
-		y[i] = n.String()
-	}
-	return y
-}
-
-func (n NormalizedUsername) CheckValid() error {
-	s := n.String()
-	if !CheckUsername.F(s) {
-		return NewBadUsernameError(s)
-	}
-	return nil
+	return username.NormalizedUsernamesToStrings(names)
 }
 
 //==================================================================
