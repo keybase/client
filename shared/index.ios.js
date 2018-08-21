81c00af3ea44559b65a4387c7652530e68a3a176
chrisnojima <cnojima@keyba.se>
2018-08-21 09:33:51 -0400

    WIP

--- shared/index.ios.js
+++ shared/index.ios.js
@@ -5,10 +5,10 @@ import './app/globals.native'
 // Load storybook or the app
 if (__STORYBOOK__) {
   // MUST happen first
-  const {inject} = require('./stories/mock-react-redux')
-  inject()
-  const load = require('./stories/index.native.js').default
-  load()
+  // const {inject} = require('./stories/mock-react-redux')
+  // inject()
+  // const load = require('./stories/index.native.js').default
+  // load()
 } else {
   const {load} = require('./app/index.native')
   load()
