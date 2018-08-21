29501d287cdcfc39c32a347d61ea9189a374964f
chrisnojima <cnojima@keyba.se>
2018-08-21 15:59:26 -0400

    WIP

--- shared/engine/require.js
+++ shared/engine/require.js
@@ -6,7 +6,14 @@ let _engine: Engine
 export function initEngine(e: Engine) {
   _engine = e
 }
-
 export function getEngine(): Engine {
   return _engine
+}
+
+let _engineSaga: Function
+export function initEngineSaga(es: Function) {
+  _engineSaga = es
+}
+export function getEngineSaga() {
+  return _engineSaga
 }
