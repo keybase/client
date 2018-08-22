9646626c38d16ad33aae0a464233c8d00e4a710c
Frederick Akalin <akalin@keyba.se>
2018-08-21 17:55:13 -0700

    Fix

--- go/libkb/httpsrv.go
+++ go/libkb/httpsrv.go
@@ -9,6 +9,6 @@ import "github.com/keybase/client/go/kbhttp"
 
 type HTTPSrvListenerSource = kbhttp.ListenerSource
 
-func NewHTTPSrv(g *GlobalContext, listenerSource HTTPSrvListenerSource) *HTTPSrv {
+func NewHTTPSrv(g *GlobalContext, listenerSource HTTPSrvListenerSource) *kbhttp.HTTPSrv {
 	return kbhttp.NewHTTPSrv(g.GetLog(), listenerSource)
 }
