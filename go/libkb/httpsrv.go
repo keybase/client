cab717462a18c07e278b9fe7ec04a71b5d5c67cb
Frederick Akalin <akalin@keyba.se>
2018-08-21 17:52:25 -0700

    Add stub

---
+++ go/libkb/httpsrv.go
@@ -0,0 +1,14 @@
+// Copyright 2018 Keybase, Inc. All rights reserved. Use of
+// this source code is governed by the included BSD license.
+
+package libkb
+
+// TODO: Remove this file once libkbfs doesn't use it anymore.
+
+import "github.com/keybase/client/go/kbhttp"
+
+type HTTPSrvListenerSource = kbhttp.ListenerSource
+
+func NewHTTPSrv(g *GlobalContext, listenerSource HTTPSrvListenerSource) *HTTPSrv {
+	return kbhttp.NewHTTPSrv(g.GetLog(), listenerSource)
+}
