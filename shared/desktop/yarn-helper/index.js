4e88b987ff64125d57951f21e2b85370155966ba
chrisnojima <cnojima@keyba.se>
2018-08-23 14:55:30 -0400

    WIP

--- shared/desktop/yarn-helper/index.js
+++ shared/desktop/yarn-helper/index.js
@@ -27,13 +27,40 @@ const commands = {
     code: () => {
       // Inject dummy modules
       makeShims(['net', 'tls', 'msgpack'])
+      replaceShims(['babel-plugin-react-docgen']) // hopefully they fix this but short term to get storybook working just skip this plugin
     },
     help: 'all: install global eslint. dummy modules',
   },
 }
 
+function replaceShims(shims) {
+  shims.forEach(shim => {
+    console.log('Replacing module: ', shim)
+    const root = path.resolve(__dirname, '..', '..', 'node_modules', shim)
+
+    try {
+      fs.writeFileSync(
+        path.join(root, 'package.json'),
+        `{
+  "main": "index.js"
+}
+`
+      )
+    } catch (_) {}
+
+    try {
+      fs.writeFileSync(
+        path.join(root, 'index.js'),
+        `module.exports = function(){return {}} // Generated shim-module
+`
+      )
+    } catch (_) {}
+  })
+}
+
 function makeShims(shims) {
   shims.forEach(shim => {
+    console.log('Shimming module: ', shim)
     const root = path.resolve(__dirname, '..', '..', 'node_modules', shim)
 
     try {
