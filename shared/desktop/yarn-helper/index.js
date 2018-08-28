0d200ac6949c0c57ea14f675a9381268e2730f92
chrisnojima <cnojima@keyba.se>
2018-08-28 10:32:31 -0400

    WIP

--- shared/desktop/yarn-helper/index.js
+++ shared/desktop/yarn-helper/index.js
@@ -3,8 +3,6 @@
 import buildCommands from './build'
 import electronComands from './electron'
 import fontCommands from './font'
-import fs from 'fs'
-import path from 'path'
 import {execSync} from 'child_process'
 
 const [, , command, ...rest] = process.argv
@@ -25,66 +23,10 @@ const commands = {
   },
   postinstall: {
     code: () => {
-      // Inject dummy modules
-      // makeShims(['net', 'tls', 'msgpack'])
-      replaceShims(['babel-plugin-react-docgen']) // hopefully they fix this but short term to get storybook working just skip this plugin
+      // Nothing right now
     },
-    help: 'all: install global eslint. dummy modules',
+    help: '',
   },
-}
-
-function replaceShims(shims) {
-  shims.forEach(shim => {
-    console.log('Replacing module: ', shim)
-    const root = path.resolve(__dirname, '..', '..', 'node_modules', shim)
-
-    try {
-      fs.writeFileSync(
-        path.join(root, 'package.json'),
-        `{
-  "main": "index.js"
-}
-`
-      )
-    } catch (_) {}
-
-    try {
-      fs.writeFileSync(
-        path.join(root, 'index.js'),
-        `module.exports = function(){return {}} // Generated shim-module
-`
-      )
-    } catch (_) {}
-  })
-}
-
-function makeShims(shims) {
-  shims.forEach(shim => {
-    console.log('Shimming module: ', shim)
-    const root = path.resolve(__dirname, '..', '..', 'node_modules', shim)
-
-    try {
-      fs.mkdirSync(root)
-    } catch (_) {}
-
-    try {
-      fs.writeFileSync(
-        path.join(root, 'package.json'),
-        `{
-  "main": "index.js"
-}
-`
-      )
-    } catch (_) {}
-
-    try {
-      fs.writeFileSync(
-        path.join(root, 'index.js'),
-        `module.exports = null // Generated shim-module
-`
-      )
-    } catch (_) {}
-  })
 }
 
 function exec(command, env, options) {
