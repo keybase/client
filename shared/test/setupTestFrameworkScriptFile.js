9260f11711d22bfcfd03182ce4f00ba4f60958c0
chrisnojima <cnojima@keyba.se>
2018-08-29 09:26:41 -0400

    WIP

--- shared/test/setupTestFrameworkScriptFile.js
+++ shared/test/setupTestFrameworkScriptFile.js
@@ -1,7 +1,7 @@
 // @noflow
 // Used by jest. We need to force things to flush to glamor doesn't write things out of order
 /* eslint-disable */
-// DONE use es6 stuff here
+// DON'T use es6 stuff here
 const {flush} = require('glamor')
 
 afterEach(() => {
