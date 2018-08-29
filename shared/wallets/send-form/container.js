b17d047da8cdc910dc400488ce5f21acba9eae80
Daniel Ayoub <danny.a95@gmail.com>
2018-08-29 09:50:15 -0400

    clear building and built payments on backing out from dialog

--- shared/wallets/send-form/container.js
+++ shared/wallets/send-form/container.js
@@ -1,11 +1,16 @@
 // @flow
 import SendForm from '.'
+import * as WalletsGen from '../../actions/wallets-gen'
 import {connect, type TypedState} from '../../util/container'
 
 const mapStateToProps = (state: TypedState) => ({})
 
 const mapDispatchToProps = (dispatch, {navigateUp}) => ({
-  onClose: () => dispatch(navigateUp()),
+  onClose: () => {
+    dispatch(navigateUp())
+    dispatch(WalletsGen.createClearBuildingPayment())
+    dispatch(WalletsGen.createClearBuiltPayment())
+  },
 })
 
 const mergeProps = (stateProps, dispatchProps, ownProps) => ({
