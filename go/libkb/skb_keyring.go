ba355b0cadef9dec32cb4235c72271dca18bc3e8
Frederick Akalin <akalin@keyba.se>
2018-08-29 23:28:35 -0700

    Add comment

--- go/libkb/skb_keyring.go
+++ go/libkb/skb_keyring.go
@@ -83,6 +83,8 @@ func (s *skbPacket) CodecDecodeSelf(d *codec.Decoder) {
 	var skb SKB
 	err := DecodePacket(d, &skb)
 	if err != nil {
+		// Okay to panic here, since the decoder catches
+		// panics and turns them back into errors.
 		panic(err)
 	}
 	s.skb = &skb
