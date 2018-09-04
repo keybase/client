aafcb43043687b6995827c6dafef29ddff3b191a
Nathan Smith <nathan@nathansmith.io>
2018-09-04 15:44:37 -0400

    HOC is in common

--- shared/common-adapters/copy-text.js
+++ shared/common-adapters/copy-text.js
@@ -1,8 +1,7 @@
 // @flow
 import * as React from 'react'
 import * as ConfigGen from '../actions/config-gen'
-import {Box2, Icon, Button, ButtonBar, Text, Toast} from '.'
-import HOCTimers, {type PropsWithTimer} from './hoc-timers'
+import {Box2, Icon, Button, ButtonBar, Text, Toast, HOCTimers, type PropsWithTimer} from '.'
 import * as Styles from '../styles'
 import {compose, connect, setDisplayName} from '../util/container'
 
