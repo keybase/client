e0fab258ed313e458e19aef63667eb25c1e43d9b
Frederick Akalin <akalin@keyba.se>
2018-09-05 14:18:36 -0700

    Add test demonstrating bug

--- shared/engine/__tests__/transport-shared.test.js
+++ shared/engine/__tests__/transport-shared.test.js
@@ -4,25 +4,45 @@ import {TransportShared} from '../transport-shared'
 
 describe('TransportShared', () => {
   class FakeTransportShared extends TransportShared {
-    _messages: any[]
+    connected: boolean
+    messages: any[]
 
     constructor() {
       super({}, () => {}, () => {}, () => {})
-      this._messages = []
+      this.connected = false
+      this.messages = []
     }
 
     is_connected = () => {
-      return true
+      return this.connected
     }
 
     send = (msg: any) => {
-      this._messages.push(msg)
+      this.messages.push(msg)
     }
   }
+
   it('invoke', () => {
     const t = new FakeTransportShared()
     const msg = {program: 'foo', method: 'bar', args: {}, notify: null}
+
+    t.connected = true
     t.invoke(msg, () => {})
-    expect(t._messages).toEqual([[0, 1, 'foo.bar', [msg.args]]])
+    expect(t.messages).toEqual([[0, 1, 'foo.bar', [msg.args]]])
+  })
+
+  it('invoke queued', () => {
+    const t = new FakeTransportShared()
+    const msg = {program: 'foo', method: 'bar', args: {}, notify: null}
+
+    t.connected = false
+    t.invoke(msg, () => {})
+    expect(t.messages).toEqual([])
+
+    t.connected = true
+
+    // $FlowIssue Flow doesn't see inherited methods.
+    t._flush_queue()
+    expect(t.messages).toEqual([[0, 1, 'foo.bar', [msg.args]]])
   })
 })
