e07c97d5e0487d850caf865ce5dc584f40b07327
Maxwell Krohn <themax@gmail.com>
2018-09-07 21:51:30 -0400

    reenable ftl tests

--- go/teams/ftl_test.go
+++ go/teams/ftl_test.go
@@ -9,7 +9,6 @@ import (
 )
 
 func TestFastLoaderBasic(t *testing.T) {
-	t.Skip()
 	tc := SetupTest(t, "team", 1)
 	defer tc.Cleanup()
 
@@ -40,7 +39,6 @@ func TestFastLoaderBasic(t *testing.T) {
 
 // Test fast loading a team that does several key rotations.
 func TestFastLoaderKeyGen(t *testing.T) {
-	t.Skip()
 	fus, tcs, cleanup := setupNTests(t, 2)
 	defer cleanup()
 
@@ -103,7 +101,6 @@ func TestFastLoaderKeyGen(t *testing.T) {
 
 // Test loading a sub-sub-team: a.b.c.
 func TestFastLoaderMultilevel(t *testing.T) {
-	t.Skip()
 	fus, tcs, cleanup := setupNTests(t, 2)
 	defer cleanup()
 
@@ -146,7 +143,6 @@ func TestFastLoaderMultilevel(t *testing.T) {
 }
 
 func TestFastLoaderUpPointerUnstub(t *testing.T) {
-	t.Skip()
 	fus, tcs, cleanup := setupNTests(t, 2)
 	defer cleanup()
 
