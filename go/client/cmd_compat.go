c543ec237775242b0bd0694c3f7f38b3359df1ae
Maxwell Krohn <themax@gmail.com>
2018-09-13 08:41:45 -0400

    do not use the UI machinery until after initialization

--- go/client/cmd_compat.go
+++ go/client/cmd_compat.go
@@ -12,21 +12,36 @@ import (
 // compatibility with node client commands:
 
 func NewCmdCompatDir(cl *libcmdline.CommandLine, g *libkb.GlobalContext) cli.Command {
+	desc := "`keybase dir` has been deprecated."
 	return cli.Command{
 		Name: "dir",
 		Action: func(c *cli.Context) {
-			g.UI.GetTerminalUI().Printf("`keybase dir` has been deprecated.\n")
+			cl.ChooseCommand(&cmdCompat{Contextified: libkb.NewContextified(g), msg: desc}, "compat", c)
 		},
-		Description: "`keybase dir` has been deprecated.",
+		Description: desc,
 	}
 }
 
 func NewCmdCompatPush(cl *libcmdline.CommandLine, g *libkb.GlobalContext) cli.Command {
+	desc := "Use `keybase pgp select` instead."
 	return cli.Command{
 		Name: "push",
 		Action: func(c *cli.Context) {
-			g.UI.GetTerminalUI().Printf("Use `keybase pgp select` instead.\n")
+			cl.ChooseCommand(&cmdCompat{Contextified: libkb.NewContextified(g), msg: desc}, "compat", c)
 		},
-		Description: "Use `keybase pgp select` instead.",
 	}
+}
+
+type cmdCompat struct {
+	libkb.Contextified
+	msg string
+}
+
+func (c *cmdCompat) GetUsage() libkb.Usage { return libkb.Usage{} }
+func (c *cmdCompat) Run() error {
+	c.G().UI.GetTerminalUI().Printf("%s\n", c.msg)
+	return nil
+}
+func (c *cmdCompat) ParseArgv(ctx *cli.Context) error {
+	return nil
 }
