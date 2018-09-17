34f0fb6a1f3ac7ea4ce1d88a7e4c8a7faacb7353
chrisnojima <cnojima@keyba.se>
2018-09-17 13:59:41 -0400

    more logging. fix for errors on device add, go back a level and black bar. after scanning qr code remove scanner so it won't do any callbacks

--- shared/provision/code-page/qr-scan/index.native.js
+++ shared/provision/code-page/qr-scan/index.native.js
@@ -9,20 +9,22 @@ import type {Props} from '.'
 
 const QRScan = (props: Props) => (
   <Box2 direction="vertical" style={styles.container}>
-    <RNCamera
-      key={props.mountKey}
-      type={RNCamera.Constants.Type.back}
-      autoFocus={RNCamera.Constants.AutoFocus.on}
-      captureAudio={false}
-      flashMode={RNCamera.Constants.FlashMode.off}
-      permissionDialogTitle={'Permission to use camera'}
-      permissionDialogMessage={'We need access to your camera to scan in the secret code'}
-      notAuthorizedView={<QRScanNotAuthorized onOpenSettings={props.onOpenSettings} />}
-      onBarCodeRead={({data}) => props.onSubmitTextCode(data)}
-      barCodeTypes={[RNCamera.Constants.BarCodeType.qr]}
-      style={styles.camera}
-    />
-    <QRScanLines canScan={true} />
+    {!props.waiting && (
+      <RNCamera
+        key={props.mountKey}
+        type={RNCamera.Constants.Type.back}
+        autoFocus={RNCamera.Constants.AutoFocus.on}
+        captureAudio={false}
+        flashMode={RNCamera.Constants.FlashMode.off}
+        permissionDialogTitle={'Permission to use camera'}
+        permissionDialogMessage={'We need access to your camera to scan in the secret code'}
+        notAuthorizedView={<QRScanNotAuthorized onOpenSettings={props.onOpenSettings} />}
+        onBarCodeRead={({data}) => props.onSubmitTextCode(data)}
+        barCodeTypes={[RNCamera.Constants.BarCodeType.qr]}
+        style={styles.camera}
+      />
+    )}
+    {!props.waiting && <QRScanLines canScan={true} />}
     {props.waiting && <ProgressIndicator style={styles.waiting} type="Large" white={true} />}
   </Box2>
 )
