7ebf09d3ef628378f4e5f8856f6421b9c1b1dda6
chrisnojima <cnojima@keyba.se>
2018-09-20 16:49:05 -0400

    WIP

--- shared/chat/inbox/row/teams-divider/container.js
+++ shared/chat/inbox/row/teams-divider/container.js
@@ -4,6 +4,7 @@ import {connect} from '../../../../util/container'
 import * as Constants from '../../../../constants/chat2'
 import type {StylesCrossPlatform} from '../../../../styles'
 import type {RowItem} from '../../index.types'
+import memoize from 'memoize-one'
 
 type OwnProps = {
   rows: Array<RowItem>,
@@ -14,30 +15,52 @@ type OwnProps = {
 
 const mapStateToProps = state => ({_badges: state.chat2.badgeMap, _metaMap: state.chat2.metaMap})
 
-const mergeProps = (stateProps, dispatchProps, ownProps: OwnProps) => {
-  // we remove the badge count of the stuff we're showing
+const getMetaCounts = memoize((badges, metaMap) => {
   let badgeCount = 0
   let hiddenCount = 0
-  ownProps.rows.forEach(row => {
+  metaMap.forEach(meta => {
+    if (meta.teamType === 'big') {
+      return
+    }
+    const id = meta.conversationIDKey
+    if (!Constants.isValidConversationIDKey(id)) {
+      return
+    }
+
+    badgeCount += badges.get(id, 0)
+    hiddenCount++
+  })
+  return {
+    badgeCount,
+    hiddenCount,
+  }
+})
+
+const getRowCounts = memoize((badges, rows) => {
+  let badgeCount = 0
+  let hiddenCount = 0
+
+  rows.forEach(row => {
     if (row.type === 'small') {
-      badgeCount -= stateProps._badges.get(row.conversationIDKey, 0)
+      badgeCount -= badges.get(row.conversationIDKey, 0)
       hiddenCount -= 1
     }
   })
 
-  if (ownProps.showButton) {
-    stateProps._metaMap.forEach(meta => {
-      if (meta.teamType === 'big') {
-        return
-      }
-      const id = meta.conversationIDKey
-      if (!Constants.isValidConversationIDKey(id)) {
-        return
-      }
+  return {
+    badgeCount,
+    hiddenCount,
+  }
+})
 
-      badgeCount += stateProps._badges.get(id, 0)
-      hiddenCount++
-    })
+const mergeProps = (stateProps, dispatchProps, ownProps: OwnProps) => {
+  // we remove the badge count of the stuff we're showing
+  let {badgeCount, hiddenCount} = getRowCounts(stateProps._badges, ownProps.rows)
+
+  if (ownProps.showButton) {
+    const fromMeta = getMetaCounts(stateProps._badges, stateProps._metaMap)
+    badgeCount += fromMeta.badgeCount
+    hiddenCount += fromMeta.hiddenCount
   }
 
   return {
