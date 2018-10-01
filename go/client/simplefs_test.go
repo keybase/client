8f6ae72934929d1443ece456706c2474cff52e45
John Zila <john@keyba.se>
2018-10-01 15:15:23 -0700

    fix test

--- go/client/simplefs_test.go
+++ go/client/simplefs_test.go
@@ -106,14 +106,14 @@ func (s SimpleFSMock) SimpleFSRemove(ctx context.Context, arg keybase1.SimpleFSR
 }
 
 // SimpleFSStat - Get info about file
-func (s SimpleFSMock) SimpleFSStat(ctx context.Context, path keybase1.Path) (keybase1.Dirent, error) {
-	pathString := path.String()
+func (s SimpleFSMock) SimpleFSStat(ctx context.Context, arg keybase1.SimpleFSStatArg) (keybase1.Dirent, error) {
+	pathString := arg.Path.String()
 	entType := keybase1.DirentType_DIR
 	// For a quick test, assume it's a file if there is a dot and 3 chars at the end
 	if len(filepath.Ext(filepath.Base(pathString))) == 4 {
 		entType = keybase1.DirentType_FILE
 	}
-	pathType, _ := path.PathType()
+	pathType, _ := arg.Path.PathType()
 	if (pathType == keybase1.PathType_KBFS && s.remoteExists == true) ||
 		(pathType == keybase1.PathType_LOCAL && s.localExists == true) {
 		return keybase1.Dirent{
