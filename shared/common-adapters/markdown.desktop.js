eb74d478cc36a7453c285e0b1e1de4de0c25798f
chrisnojima <cnojima@keyba.se>
2018-10-01 14:28:31 -0400

    WIP

--- shared/common-adapters/markdown.desktop.js
+++ shared/common-adapters/markdown.desktop.js
@@ -189,6 +189,9 @@ function messageCreateComponent(type, key, children, options) {
   }
 }
 
+// https://gist.github.com/dperini/729294
+const linkRegex = /^(?:(?:(?:https?):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?$/i
+
 const rules = {
   escape: {
     // handle escaped chars
@@ -342,6 +345,26 @@ const rules = {
     react: (node, output, state) => {
       // TODO support big emoji
       return <EmojiIfExists emojiName={String(node.content)} size={16} key={state.key} />
+    },
+  },
+  links: {
+    order: SimpleMarkdown.defaultRules.text.order - 0.2,
+    match: SimpleMarkdown.inlineRegex(linkRegex),
+    parse: function(capture, parse, state) {
+      return {content: capture[0]}
+    },
+    react: (node, output, state) => {
+      return (
+        <Text
+          className="hover-underline"
+          type="BodyPrimaryLink"
+          key={state.key}
+          style={linkStyle}
+          onClickURL={node.content}
+        >
+          {node.content}
+        </Text>
+      )
     },
   },
 }
