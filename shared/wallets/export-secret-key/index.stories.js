5a73003d300d8a58c44fb18c61fd2c3a7711ecf1
Chris Ball <chris@printf.net>
2018-10-03 12:48:53 -0700

    Show the account name in the export secret key dialog

--- shared/wallets/export-secret-key/index.stories.js
+++ shared/wallets/export-secret-key/index.stories.js
@@ -2,14 +2,17 @@
 import React from 'react'
 import * as Sb from '../../stories/storybook'
 import ExportSecretKey from '.'
+import {stringToAccountID} from '../../constants/types/wallets'
 
 const load = () => {
   Sb.storiesOf('Wallets', module).add('Export secret key', () => (
     <ExportSecretKey
+      accountID={stringToAccountID('GBCCH4KHE5MUXXYSFCKJ3BRN4U3MTXOXD2GBJH5V7QF6OJ6S5R23DWYF')}
       onClose={Sb.action('onClose')}
       onLoadSecretKey={Sb.action('onLoadSecretKey')}
       secretKey="SETECASTRONOMY"
       username="cecileb"
+      walletName="Money for Sneakers"
     />
   ))
 }
