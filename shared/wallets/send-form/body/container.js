4ba6b67813d77f16f4057e7c3e0a3b31c6475eaa
Chris Ball <chris@printf.net>
2018-10-04 22:05:55 -0700

    Move work to mergeProps

--- shared/wallets/send-form/body/container.js
+++ shared/wallets/send-form/body/container.js
@@ -4,18 +4,17 @@ import {compose, connect, setDisplayName, type TypedState} from '../../../util/c
 import {bannerLevelToBackground} from '../../../constants/wallets'
 
 const mapStateToProps = (state: TypedState) => ({
-  banners: (state.wallets.builtPayment.banners || []).map(banner => ({
-    bannerBackground: bannerLevelToBackground(banner.level),
-    bannerText: banner.message,
-  })),
+  banners: state.wallets.builtPayment.banners,
 })
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({})
 
 const mergeProps = (stateProps, dispatchProps, ownProps) => ({
-  ...stateProps,
-  ...dispatchProps,
   ...ownProps,
+  banners: (stateProps.banners || []).map(banner => ({
+    bannerBackground: bannerLevelToBackground(banner.level),
+    bannerText: banner.message,
+  })),
 })
 
 export default compose(connect(mapStateToProps, mapDispatchToProps, mergeProps), setDisplayName('Body'))(Body)
