c965379ece52ed823d120871bd1d1c28684c94e2
Daniel Ayoub <danny.a95@gmail.com>
2018-10-08 10:31:45 -0400

    no redeclare

--- shared/util/react.js
+++ shared/util/react.js
@@ -5,8 +5,9 @@ import * as React from 'react'
 
 // Flow is missing a type for React.ForwardRef. See https://github.com/facebook/flow/issues/6103
 // Taken from https://github.com/facebook/flow/pull/6510
-declare export function forwardRef<Props, ElementType: React$ElementType>(
+export function forwardRef<Props, ElementType: React$ElementType>(
   render: (props: Props, ref: React$Ref<ElementType>) => React$Node
-): React$ComponentType<Props>
-// $FlowIssue
-export const forwardRef = React.forwardRef // eslint-disable-line no-redeclare
+): React$ComponentType<Props> {
+  // $FlowIssue
+  return React.forwardRef(render)
+}
