1e3e08024e55ba79214ff6119b5134be9818f2f2
Joshua Blum <jblum18@gmail.com>
2018-10-09 17:41:26 -0400

    param proof test update

--- go/protocol/keybase1/prove_common.go
+++ go/protocol/keybase1/prove_common.go
@@ -268,16 +268,6 @@ func (e ProofType) String() string {
 	return ""
 }
 
-type ProofServiceGroup struct {
-	Id string `codec:"id" json:"id"`
-}
-
-func (o ProofServiceGroup) DeepCopy() ProofServiceGroup {
-	return ProofServiceGroup{
-		Id: o.Id,
-	}
-}
-
 type SelectorEntry struct {
 	IsIndex    bool   `codec:"isIndex" json:"isIndex"`
 	Index      int    `codec:"index" json:"index"`
@@ -328,7 +318,9 @@ type ParamProofServiceConfig struct {
 	Version        int                      `codec:"version" json:"version"`
 	Domain         string                   `codec:"domain" json:"domain"`
 	DisplayName    string                   `codec:"displayName" json:"display_name"`
-	Group          *ProofServiceGroup       `codec:"group,omitempty" json:"group,omitempty"`
+	Group          *string                  `codec:"group,omitempty" json:"group,omitempty"`
+	LogoUrl        string                   `codec:"logoUrl" json:"logo_url"`
+	Description    string                   `codec:"description" json:"description"`
 	UsernameConfig ParamProofUsernameConfig `codec:"usernameConfig" json:"username"`
 	BrandColor     string                   `codec:"brandColor" json:"brand_color"`
 	PrefillUrl     string                   `codec:"prefillUrl" json:"prefill_url"`
@@ -342,13 +334,15 @@ func (o ParamProofServiceConfig) DeepCopy() ParamProofServiceConfig {
 		Version:     o.Version,
 		Domain:      o.Domain,
 		DisplayName: o.DisplayName,
-		Group: (func(x *ProofServiceGroup) *ProofServiceGroup {
+		Group: (func(x *string) *string {
 			if x == nil {
 				return nil
 			}
-			tmp := (*x).DeepCopy()
+			tmp := (*x)
 			return &tmp
 		})(o.Group),
+		LogoUrl:        o.LogoUrl,
+		Description:    o.Description,
 		UsernameConfig: o.UsernameConfig.DeepCopy(),
 		BrandColor:     o.BrandColor,
 		PrefillUrl:     o.PrefillUrl,
