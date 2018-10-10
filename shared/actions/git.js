2e32aefee341075d0f7790426e5fcd0d342c9019
Maxwell Krohn <themax@gmail.com>
2018-10-10 16:30:42 -0700

    fix flow

--- shared/actions/git.js
+++ shared/actions/git.js
@@ -34,6 +34,7 @@ const loadGitRepo = (state: TypedState, action: GitGen.LoadGitRepoPayload) =>
         name: action.payload.teamname || action.payload.username || '',
         notificationsOn: false,
         private: true,
+        team_id: ''
       },
     },
     Constants.loadingWaitingKey
@@ -102,6 +103,7 @@ const setTeamRepoSettings = (_, action: GitGen.SetTeamRepoSettingsPayload) =>
       private: true,
       created: false,
       notificationsOn: false,
+      team_id: ''
     },
     repoID: action.payload.repoID,
     channelName: action.payload.channelName,
