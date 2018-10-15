2669a153165ed2c8f63549af78ff9e0eb1c851cd
John Zila <john@keyba.se>
2018-10-15 15:54:42 -0700

    refactor styles in files header

--- shared/fs/header/header.desktop.js
+++ shared/fs/header/header.desktop.js
@@ -1,6 +1,6 @@
 // @flow
 import * as React from 'react'
-import {globalStyles, globalColors, globalMargins} from '../../styles'
+import * as Styles from '../../styles'
 import {Box, Icon, Text, WithTooltip} from '../../common-adapters'
 import AddNew from './add-new-container'
 import ConnectedFilesBanner from '../banner/fileui-banner/container'
@@ -9,24 +9,24 @@ import {type FolderHeaderProps} from './header'
 import {PathItemAction, OpenInSystemFileManager} from '../common'
 
 const FolderHeader = ({path, onChat}: FolderHeaderProps) => (
-  <Box style={styleHeaderContainer}>
-    <Box style={styleFolderHeader}>
+  <Box style={styles.headerContainer}>
+    <Box style={styles.folderHeader}>
       {path === '/keybase' ? (
-        <Box style={styleFolderHeaderContainer}>
-          <Box style={folderHeaderStyleRoot}>
+        <Box style={styles.folderHeaderContainer}>
+          <Box style={styles.folderHeaderRoot}>
             <Text type="BodyBig">Keybase Files</Text>
           </Box>
-          <Box style={styleFolderHeaderEnd}>
+          <Box style={styles.folderHeaderEnd}>
             <WithTooltip text="Show in Finder">
               <OpenInSystemFileManager path={path} />
             </WithTooltip>
           </Box>
         </Box>
       ) : (
-        <Box style={styleFolderHeaderContainer}>
+        <Box style={styles.folderHeaderContainer}>
           <Breadcrumb path={path} />
-          <Box style={styleFolderHeaderEnd}>
-            <AddNew path={path} style={styleAddNew} />
+          <Box style={styles.folderHeaderEnd}>
+            <AddNew path={path} style={styles.addNew} />
             <WithTooltip text="Show in Finder">
               <OpenInSystemFileManager path={path} />
             </WithTooltip>
@@ -34,9 +34,9 @@ const FolderHeader = ({path, onChat}: FolderHeaderProps) => (
               <Icon
                 type="iconfont-chat"
                 style={{
-                  marginLeft: globalMargins.small,
+                  marginLeft: Styles.globalMargins.small,
                 }}
-                color={globalColors.black_40}
+                color={Styles.globalColors.black_40}
                 fontSize={16}
                 onClick={onChat}
               />
@@ -49,50 +49,47 @@ const FolderHeader = ({path, onChat}: FolderHeaderProps) => (
   </Box>
 )
 
-const stylesCommonRow = {
-  ...globalStyles.flexBoxRow,
+const styleCommonRow = {
+  ...Styles.globalStyles.flexBoxRow,
   alignItems: 'center',
 }
 
-const styleHeaderContainer = {
-  ...globalStyles.flexBoxColumn,
-  width: '100%',
-}
-
-const styleFolderHeader = {
-  minHeight: 48,
-}
-
-const folderHeaderStyleRoot = {
-  ...stylesCommonRow,
-  justifyContent: 'center',
-  width: '100%',
-  height: 48,
-}
-
-const styleFolderHeaderEnd = {
-  ...stylesCommonRow,
-  alignItems: 'center',
-  paddingLeft: 16,
-  paddingRight: 16,
-  flexShrink: 0,
-}
-
-const styleFolderHeaderContainer = {
-  ...stylesCommonRow,
-  width: '100%',
-  height: 48,
-  alignItems: 'center',
-  position: 'relative',
-}
-
-const styleAddNew = {
-  ...globalStyles.flexBoxRow,
-  alignItems: 'center',
-  paddingTop: globalMargins.tiny,
-  paddingBottom: globalMargins.tiny,
-  paddingRight: globalMargins.small - 4,
-  paddingLeft: globalMargins.small,
-}
+const styles = Styles.styleSheetCreate({
+  headerContainer: {
+    ...Styles.globalStyles.flexBoxColumn,
+    width: '100%',
+  },
+  folderHeader: {
+    minHeight: 48,
+  },
+  folderHeaderRoot: {
+    ...styleCommonRow,
+    justifyContent: 'center',
+    width: '100%',
+    height: 48,
+  },
+  folderHeaderEnd: {
+    ...styleCommonRow,
+    alignItems: 'center',
+    paddingLeft: 16,
+    paddingRight: 16,
+    flexShrink: 0,
+  },
+  folderHeaderContainer: {
+    ...styleCommonRow,
+    width: '100%',
+    height: 48,
+    alignItems: 'center',
+    position: 'relative',
+  },
+  addNew: {
+    ...Styles.globalStyles.flexBoxRow,
+    alignItems: 'center',
+    paddingTop: Styles.globalMargins.tiny,
+    paddingBottom: Styles.globalMargins.tiny,
+    paddingRight: Styles.globalMargins.small - 4,
+    paddingLeft: Styles.globalMargins.small,
+  },
+})
 
 export default FolderHeader
