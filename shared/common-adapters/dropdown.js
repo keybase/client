15a73b57629eff9af8b8a228fb7f73a28ed5cd13
chrisnojima <cnojima@keyba.se>
2018-10-15 15:38:37 -0400

    fix dropdown for display currency on mobile

--- shared/common-adapters/dropdown.js
+++ shared/common-adapters/dropdown.js
@@ -1,7 +1,9 @@
 // @flow
 import * as React from 'react'
 import Box from './box'
+import ClickableBox from './clickable-box'
 import Overlay from './overlay'
+import ScrollView from './scroll-view'
 import OverlayParentHOC, {type OverlayParentProps} from './overlay/parent-hoc'
 import type {Position} from './relative-popup-hoc'
 import Icon from './icon'
@@ -41,21 +43,19 @@ class Dropdown extends React.Component<Props & OverlayParentProps, State> {
   render() {
     return (
       <Box style={Styles.collapseStyles([{width: Styles.isMobile ? '100%' : 270}, this.props.style])}>
-        <ButtonBox
-          onClick={!this.props.disabled ? this._toggleOpen : null}
-          disabled={this.props.disabled}
-          ref={this.props.setAttachmentRef}
-        >
-          <Box style={Styles.collapseStyles([styles.selectedBox, this.props.selectedBoxStyle])}>
-            {this.props.selected}
-          </Box>
-          <Icon
-            type="iconfont-caret-down"
-            inheritColor={true}
-            fontSize={Styles.isMobile ? 12 : 8}
-            style={{marginTop: Styles.isMobile ? 4 : -8}}
-          />
-        </ButtonBox>
+        <ClickableBox onClick={!this.props.disabled ? this._toggleOpen : null}>
+          <ButtonBox disabled={this.props.disabled} ref={this.props.setAttachmentRef}>
+            <Box style={Styles.collapseStyles([styles.selectedBox, this.props.selectedBoxStyle])}>
+              {this.props.selected}
+            </Box>
+            <Icon
+              type="iconfont-caret-down"
+              inheritColor={true}
+              fontSize={Styles.isMobile ? 12 : 8}
+              style={{marginTop: Styles.isMobile ? 4 : -8}}
+            />
+          </ButtonBox>
+        </ClickableBox>
         <Overlay
           style={styles.overlay}
           attachTo={this.props.getAttachmentRef}
@@ -63,11 +63,13 @@ class Dropdown extends React.Component<Props & OverlayParentProps, State> {
           onHidden={this._toggleOpen}
           position={this.props.position || 'center center'}
         >
-          {this.props.items.map((i, idx) => (
-            <ItemBox key={idx} onClick={() => this._onSelect(i)}>
-              {i}
-            </ItemBox>
-          ))}
+          <ScrollView style={styles.scrollView}>
+            {this.props.items.map((i, idx) => (
+              <ClickableBox key={idx} onClick={() => this._onSelect(i)} style={styles.itemClickBox}>
+                <ItemBox>{i}</ItemBox>
+              </ClickableBox>
+            ))}
+          </ScrollView>
         </Overlay>
       </Box>
     )
@@ -75,6 +77,25 @@ class Dropdown extends React.Component<Props & OverlayParentProps, State> {
 }
 
 const styles = Styles.styleSheetCreate({
+  itemClickBox: Styles.platformStyles({
+    common: {
+      width: '100%',
+      flexShrink: 0,
+    },
+    isMobile: {
+      minHeight: 40,
+    },
+  }),
+  scrollView: Styles.platformStyles({
+    common: {
+      width: '100%',
+      height: '100%',
+    },
+    isMobile: {
+      backgroundColor: Styles.globalColors.white,
+      maxHeight: '50%',
+    },
+  }),
   selectedBox: Styles.platformStyles({
     isMobile: {minHeight: 48},
     isElectron: {minHeight: 32},
@@ -85,7 +106,6 @@ const styles = Styles.styleSheetCreate({
   }),
   overlay: Styles.platformStyles({
     isElectron: {
-      ...Styles.desktopStyles.scrollable,
       border: `1px solid ${Styles.globalColors.blue}`,
       borderRadius: 4,
       maxHeight: 300,
@@ -96,7 +116,6 @@ const styles = Styles.styleSheetCreate({
       backgroundColor: Styles.globalColors.white,
       marginTop: Styles.globalMargins.xtiny,
     },
-    // TODO: make sure mobile looks alright
   }),
 })
 
@@ -112,7 +131,7 @@ const ItemBox = Styles.glamorous(Box)({
   borderBottomWidth: 1,
   borderColor: Styles.globalColors.black_10,
   borderStyle: 'solid',
-  minHeight: 32,
+  minHeight: Styles.isMobile ? 40 : 32,
   width: '100%',
 })
 
