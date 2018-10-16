599685eb7b34c3a2c32244ba9d35ad5a43335a8b
Patrick Crosby <pcrosby@gmail.com>
2018-10-16 12:24:18 -0500

    More bundle tests

--- go/stellar/bundle/acct_bundle_test.go
+++ go/stellar/bundle/acct_bundle_test.go
@@ -3,6 +3,7 @@ package bundle
 import (
 	"testing"
 
+	"github.com/keybase/client/go/protocol/stellar1"
 	"github.com/stretchr/testify/require"
 )
 
@@ -22,4 +23,11 @@ func TestBoxAccountBundle(t *testing.T) {
 	boxed, err := b.Box(gen, seed)
 	require.NoError(t, err)
 	require.NotNil(t, boxed, "b.Box() should return something")
+	require.Equal(t, stellar1.AccountBundleVersion_V1, boxed.FormatVersion, "should be V1")
+	require.NotEmpty(t, boxed.EncB64)
+	require.NotEmpty(t, boxed.VisB64)
+	require.Equal(t, 1, boxed.Enc.V)
+	require.NotEmpty(t, boxed.Enc.E)
+	require.NotZero(t, boxed.Enc.N)
+	require.Equal(t, gen, boxed.Enc.Gen)
 }
