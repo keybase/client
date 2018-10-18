0c319d29d094462a9e4e194aef8bab60646dfde2
Song Gao <song@gao.io>
2018-10-18 15:50:49 -0700

    typo caught by jzila

--- shared/fs/row/tlf.js
+++ shared/fs/row/tlf.js
@@ -10,7 +10,7 @@ type TlfProps = StillCommonProps & {
   isUserReset: boolean,
   needsRekey: boolean,
   resetParticipants: Array<string>,
-  // We don't use this at the moment. In the futuer this will be used for
+  // We don't use this at the moment. In the future this will be used for
   // showing ignored folders when we allow user to show ignored folders in GUI.
   isIgnored: boolean,
 }
