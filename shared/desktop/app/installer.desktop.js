69a4979412a0c27de80995be406daa6b6d989213
chrisnojima <cnojima@keyba.se>
2018-10-25 10:00:51 -0400

    remove jsonfile and general cleanup

--- shared/desktop/app/installer.desktop.js
+++ shared/desktop/app/installer.desktop.js
@@ -7,12 +7,11 @@ import {keybaseBinPath} from './paths.desktop'
 import {quit} from './ctl.desktop'
 import {isDarwin} from '../../constants/platform'
 import logger from '../../logger'
-import type {InstallResult} from '../../constants/types/rpc-gen'
 
 const file = path.join(SafeElectron.getApp().getPath('userData'), 'installer.json')
 let state = {promptedForCLI: false}
 
-const load = () => {
+const loadState = () => {
   try {
     const data = fs.readFileSync(file, 'utf8')
     state = JSON.parse(data)
@@ -25,7 +24,7 @@ const load = () => {
   }
 }
 
-const save = () => {
+const saveState = () => {
   try {
     fs.writeFileSync(file, JSON.stringify(state))
   } catch (err) {
@@ -33,14 +32,7 @@ const save = () => {
   }
 }
 
-type CheckErrorsResult = {
-  errors: Array<string>,
-  hasCLIError: boolean,
-  hasFUSEError: boolean,
-  hasKBNMError: boolean,
-}
-
-function checkErrors(result: InstallResult): CheckErrorsResult {
+const checkErrors = (result, errors, errorTypes) => {
   // Copied from old constants/favorite.js
   // See Installer.m: KBExitFuseKextError
   const ExitCodeFuseKextError = 4
@@ -49,46 +41,42 @@ function checkErrors(result: InstallResult): CheckErrorsResult {
   // See Installer.m: KBExitAuthCanceledError
   const ExitCodeAuthCanceledError = 6
 
-  let errors = []
-  let hasCLIError = false
-  let hasFUSEError = false
-  let hasKBNMError = false
-  const crs = (result && result.componentResults) || []
-  for (let cr of crs) {
-    if (cr.status.code !== 0) {
-      if (cr.name === 'fuse') {
-        hasFUSEError = true
-        if (cr.exitCode === ExitCodeFuseKextError) {
-          errors.push(
-            `We were unable to load KBFS (Fuse kext). This may be due to a limitation in MacOS where there aren't any device slots available. Device slots can be taken up by apps such as VMWare, VirtualBox, anti-virus programs, VPN programs and Intel HAXM.`
-          )
-        } else if (cr.exitCode === ExitCodeFuseKextPermissionError) {
-          // This will occur if they started install and didn't allow the extension in >= 10.13, and then restarted the app.
-          // The app will deal with this scenario in the folders tab, so we can ignore this specific error here.
-        }
-      } else if (cr.name === 'helper' && cr.exitCode === ExitCodeAuthCanceledError) {
-        // Consider this a FUSE error for the purpose of showing it to the user.
-        hasFUSEError = true
+  const results = (result && result.componentResults) || []
+  results.forEach(cr => {
+    if (cr.status.code === 0) {
+      return
+    }
+    if (cr.name === 'fuse') {
+      errorTypes.fuse = true
+      if (cr.exitCode === ExitCodeFuseKextError) {
         errors.push(
-          `Installation was canceled. The file system will not be available until authorization is granted.`
+          `We were unable to load KBFS (Fuse kext). This may be due to a limitation in MacOS where there aren't any device slots available. Device slots can be taken up by apps such as VMWare, VirtualBox, anti-virus programs, VPN programs and Intel HAXM.`
         )
-      } else if (cr.name === 'cli') {
-        hasCLIError = true
-      } else if (cr.name === 'redirector') {
-        hasFUSEError = true
-        errors.push(
-          `We were unable to load the part of Keybase that lets you access your files in your file system. You should be able to do so if you wait a few minutes and restart Keybase.`
-        )
-      } else {
-        errors.push(`There was an error trying to install the ${cr.name}.`)
-        errors.push(`\n${cr.status.desc}`)
-        if (cr.name === 'kbnm') {
-          hasKBNMError = true
-        }
+      } else if (cr.exitCode === ExitCodeFuseKextPermissionError) {
+        // This will occur if they started install and didn't allow the extension in >= 10.13, and then restarted the app.
+        // The app will deal with this scenario in the folders tab, so we can ignore this specific error here.
+      }
+    } else if (cr.name === 'helper' && cr.exitCode === ExitCodeAuthCanceledError) {
+      // Consider this a FUSE error for the purpose of showing it to the user.
+      errorTypes.fuse = true
+      errors.push(
+        `Installation was canceled. The file system will not be available until authorization is granted.`
+      )
+    } else if (cr.name === 'cli') {
+      errorTypes.cli = true
+    } else if (cr.name === 'redirector') {
+      errorTypes.fuse = true
+      errors.push(
+        `We were unable to load the part of Keybase that lets you access your files in your file system. You should be able to do so if you wait a few minutes and restart Keybase.`
+      )
+    } else {
+      errors.push(`There was an error trying to install the ${cr.name}.`)
+      errors.push(`\n${cr.status.desc}`)
+      if (cr.name === 'kbnm') {
+        errorTypes.kbnm = true
       }
     }
-  }
-  return {errors, hasCLIError, hasFUSEError, hasKBNMError}
+  })
 }
 
 // To test the installer from dev (on MacOS), you can point KEYBASE_GET_APP_PATH
@@ -96,95 +84,89 @@ function checkErrors(result: InstallResult): CheckErrorsResult {
 //   KEYBASE_GET_APP_PATH=/Applications/Keybase.app/Contents/Resources/app/ yarn run start
 //
 // Reminder: hot-server doesn't reload code in here (/desktop)
-const install = isDarwin
-  ? (callback: (err: any) => void): void => {
-      load()
-      logger.info('Installer check starting now')
-      const keybaseBin = keybaseBinPath()
-      if (!keybaseBin) {
-        callback(new Error('No keybase bin path'))
-        return
+type CB = (err: any) => void
+const darwinInstall = (callback: CB) => {
+  loadState()
+  logger.info('Installer check starting now')
+  const keybaseBin = keybaseBinPath()
+  if (!keybaseBin) {
+    callback(new Error('No keybase bin path'))
+    return
+  }
+  let timeout = 30
+  // If the app was opened at login, there might be contention for lots
+  // of resources, so let's bump the install timeout to something large.
+  if (SafeElectron.getApp().getLoginItemSettings().wasOpenedAtLogin) {
+    timeout = 90
+  }
+
+  const handleResults = (err, attempted, stdout, stderr) => {
+    const errors = []
+    const errorTypes = {
+      cli: false,
+      fuse: false,
+      kbnm: false,
+    }
+    if (err) {
+      errors.push(`There was an error trying to run the install (${err.code}).`)
+    } else if (stdout !== '') {
+      try {
+        const result = JSON.parse(stdout)
+        if (result) {
+          checkErrors(result, errors, errorTypes)
+        } else {
+          errors.push(`There was an error trying to run the install. No output.`)
+        }
+      } catch (err) {
+        errors.push(
+          `There was an error trying to run the install. We were unable to parse the output of keybase install-auto.`
+        )
       }
-      let timeout = 30
-      // If the app was opened at login, there might be contention for lots
-      // of resources, so let's bump the install timeout to something large.
-      if (SafeElectron.getApp().getLoginItemSettings().wasOpenedAtLogin) {
-        timeout = 90
-      }
+    }
+
+    if (errors.length > 0) {
+      logger.info(errors.join('\n'))
+      logger.info(`Install errors: stdout=${stdout || ''}, stderr=${stderr || ''}`)
+      const buttons = errorTypes.fuse || errorTypes.kbnm ? ['Okay'] : ['Ignore', 'Quit']
+      const detail = errors.join('\n') + `\n\nPlease run \`keybase log send\` to report the error.`
+      const message = 'Keybase Install Error'
+      SafeElectron.getDialog().showMessageBox(null, {buttons, detail, message}, resp => {
+        if (resp === 1) {
+          quit()
+        } else {
+          callback(null)
+        }
+      })
+      return
+    }
+
+    // If we had an error install CLI, let's prompt and try to do it via
+    // privileged install.
+    if (errorTypes.cli && !state.promptedForCLI) {
+      state.promptedForCLI = true
+      saveState()
       exec(
         keybaseBin,
-        ['--debug', 'install-auto', '--format=json', `--timeout=${timeout}s`],
+        ['--debug', 'install', '--components=clipaths', '--format=json', '--timeout=120s'],
         'darwin',
         'prod',
         true,
-        (err, attempted, stdout, stderr) => {
-          let errorsResult: CheckErrorsResult = {
-            errors: [],
-            hasCLIError: false,
-            hasFUSEError: false,
-            hasKBNMError: false,
-          }
-          if (err) {
-            errorsResult.errors = [`There was an error trying to run the install (${err.code}).`]
-          } else if (stdout !== '') {
-            try {
-              const result = JSON.parse(stdout)
-              if (result) {
-                errorsResult = checkErrors(result)
-              } else {
-                errorsResult.errors = [`There was an error trying to run the install. No output.`]
-              }
-            } catch (err) {
-              errorsResult.errors = [
-                `There was an error trying to run the install. We were unable to parse the output of keybase install-auto.`,
-              ]
-            }
-          }
-
-          if (errorsResult.errors.length > 0) {
-            logger.info(errorsResult.errors.join('\n'))
-            logger.info(`Install errors: stdout=${stdout || ''}, stderr=${stderr || ''}`)
-            const showOkay = errorsResult.hasFUSEError || errorsResult.hasKBNMError
-            const detail =
-              errorsResult.errors.join('\n') + `\n\nPlease run \`keybase log send\` to report the error.`
-            SafeElectron.getDialog().showMessageBox(
-              null,
-              {
-                buttons: showOkay ? ['Okay'] : ['Ignore', 'Quit'],
-                detail,
-                message: 'Keybase Install Error',
-              },
-              resp => {
-                if (resp === 1) {
-                  quit()
-                } else {
-                  callback(null)
-                }
-              }
-            )
-            return
-          }
-
-          // If we had an error install CLI, let's prompt and try to do it via
-          // privileged install.
-          if (errorsResult.hasCLIError && !state.promptedForCLI) {
-            state.promptedForCLI = true
-            save()
-            exec(
-              keybaseBin,
-              ['--debug', 'install', '--components=clipaths', '--format=json', '--timeout=120s'],
-              'darwin',
-              'prod',
-              true,
-              callback
-            )
-            return
-          }
-
-          callback(null)
-        }
+        callback
       )
+      return
     }
-  : callback => callback(null) // nothing on other platforms
 
+    callback(null)
+  }
+  exec(
+    keybaseBin,
+    ['--debug', 'install-auto', '--format=json', `--timeout=${timeout}s`],
+    'darwin',
+    'prod',
+    true,
+    handleResults
+  )
+}
+
+const install = isDarwin ? darwinInstall : (callback: CB) => callback(null) // nothing on other platforms
 export default install
