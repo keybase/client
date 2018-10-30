3935055d1cdf31ef15740002d0f31b2258e782aa
Mike Maxim <mike@keyba.se>
2018-10-29 22:42:58 -0400

    ws

--- go/chat/attachments/quarantine_darwin.go
+++ go/chat/attachments/quarantine_darwin.go
@@ -9,11 +9,11 @@ void quarantineFile(const char* inFilename) {
 	NSError* error = NULL;
 	NSString* filename = [NSString stringWithUTF8String:inFilename];
 	NSURL* url = [NSURL fileURLWithPath:filename];
-  	NSDictionary* opts = [[NSDictionary alloc] initWithObjectsAndKeys:
-                        (id)@"Keybase", (id)kLSQuarantineAgentNameKey,
-                        (id)kLSQuarantineTypeOtherDownload, (id)kLSQuarantineTypeKey,
-                        nil];
-  	[url setResourceValue:opts forKey:NSURLQuarantinePropertiesKey error:&error];
+	NSDictionary* opts = [[NSDictionary alloc] initWithObjectsAndKeys:
+				(id)@"Keybase", (id)kLSQuarantineAgentNameKey,
+				(id)kLSQuarantineTypeOtherDownload, (id)kLSQuarantineTypeKey,
+				nil];
+	[url setResourceValue:opts forKey:NSURLQuarantinePropertiesKey error:&error];
 }
 */
 import "C"
