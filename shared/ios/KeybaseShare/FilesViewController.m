f32e13697320028ac687659edac3e7fa10769cae
John Zila <john@keyba.se>
2018-10-31 16:16:08 -0700

    Add colors and allow navigation back up

--- shared/ios/KeybaseShare/FilesViewController.m
+++ shared/ios/KeybaseShare/FilesViewController.m
@@ -10,9 +10,11 @@
 #import "keybase/keybase.h"
 
 @interface FilesViewController ()
-@property NSArray* path; // the path we are currently showing
+@property NSString* path; // the path we are currently showing
 @property NSArray* directoryEntries; // the directory entries at the current path
 @end
+
+NSString* const UpOneLevel = @".. [Up one level]";
 
 @implementation FilesViewController
 
@@ -21,9 +23,11 @@
   
   self.preferredContentSize = CGSizeMake(self.view.frame.size.width, 2*self.view.frame.size.height); // expand
   self.definesPresentationContext = YES;
+  [self setPath:@"/"];
   
   // show this spinner on top of the table view until we have parsed the inbox
   UIActivityIndicatorView* av = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+  [av setTag:self.view.tag+1];
   [self.view addSubview:av];
   [av setTranslatesAutoresizingMaskIntoConstraints:NO];
   [av setHidesWhenStopped:YES];
@@ -45,11 +49,15 @@
      ]
    ];
   
+  [self dispatchFilesBrowser];
+}
+
+- (void)dispatchFilesBrowser {
+  UIActivityIndicatorView* av = [self.view viewWithTag:self.view.tag + 1];
   dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
     NSError* error = NULL;
-    [self setPath:[NSArray new]];
     [self setDirectoryEntries:[NSArray new]];
-    NSString* jsonFiles = KeybaseExtensionListPath(@"/", &error); // returns the path list in JSON format
+    NSString* jsonFiles = KeybaseExtensionListPath(self.path, &error); // returns the path list in JSON format
     if (jsonFiles == nil) {
       dispatch_async(dispatch_get_main_queue(), ^{
         NSLog(@"failed to get files: %@", error);
@@ -66,14 +74,39 @@
   });
 }
 
+NSInteger sortEntries(NSDictionary* one, NSDictionary* two, void* context) {
+  int t1 = [[one objectForKey:@"direntType"] intValue];
+  int t2 = [[two objectForKey:@"direntType"] intValue];
+  if (t1 == 1 && t2 != 1) {
+    return NSOrderedAscending;
+  } else if (t2 == 1 && t1 != 1) {
+    return NSOrderedDescending;
+  } else {
+    return [one[@"name"] compare:two[@"name"]];
+  }
+}
+
 - (void)parseFiles:(NSString*)jsonFiles {
-  NSError *error = nil;
-  NSData *data = [jsonFiles dataUsingEncoding:NSUTF8StringEncoding];
-  NSArray *items = [NSJSONSerialization JSONObjectWithData:data options: NSJSONReadingMutableContainers error: &error];
+  NSError* error = nil;
+  NSData* data = [jsonFiles dataUsingEncoding:NSUTF8StringEncoding];
+  NSArray* items = [NSJSONSerialization JSONObjectWithData:data options: NSJSONReadingMutableContainers error: &error];
+  NSArray* sortedItems;
   if (!items) {
     NSLog(@"parseFiles: error parsing JSON: %@", error);
+    sortedItems = [[NSArray alloc] init];
   } else {
-    [self setDirectoryEntries:items];
+    sortedItems = [items sortedArrayUsingFunction:sortEntries context:NULL];
+  }
+  if ([self.path isEqualToString:@"/"]) {
+    [self setDirectoryEntries:sortedItems];
+  } else {
+    NSMutableArray* itemsWithBack = [[NSMutableArray alloc] init];
+    [itemsWithBack addObject:[[NSDictionary alloc] initWithObjectsAndKeys:
+                              UpOneLevel, @"name",
+                              [NSNumber numberWithInt:1], @"direntType",
+                              nil]];
+    [itemsWithBack addObjectsFromArray:sortedItems];
+    [self setDirectoryEntries:itemsWithBack];
   }
 }
 
@@ -104,12 +137,29 @@
   }
   NSDictionary* item = [self getItemAtIndex:indexPath];
   [[cell textLabel] setText:item[@"name"]];
+  if ([[item objectForKey:@"direntType"] intValue] == 1) {
+    [[cell textLabel] setTextColor:[UIColor colorWithRed:76.0/255.0 green:142.0/255.0 blue:1 alpha:1]];
+  } else {
+    [[cell textLabel] setTextColor:[UIColor colorWithRed:0 green:0 blue:0 alpha:1]];
+  }
   return cell;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-  NSDictionary* folder = [self getItemAtIndex:indexPath];
-  [self.delegate folderSelected:folder]; // let main view controller know we have something
+  NSDictionary* target = [self getItemAtIndex:indexPath];
+  if ([indexPath isEqual:[NSIndexPath indexPathForRow:0 inSection:0]] && [[target objectForKey:@"name"] isEqualToString:UpOneLevel]) {
+    // '..' needs to navigate back up
+    NSArray* pathElems = [self.path componentsSeparatedByString:@"/"];
+    NSArray* upOneDirectory = [pathElems subarrayWithRange:NSMakeRange(0, [pathElems count] - 2)];
+    [self setPath:[NSString stringWithFormat:@"%@/", [upOneDirectory componentsJoinedByString:@"/"]]];
+    [self dispatchFilesBrowser];
+  } else if ([[target objectForKey:@"direntType"] intValue] == 1) {
+    [self setPath:[NSString stringWithFormat:@"%@%@/", self.path, target[@"name"]]];
+    [self dispatchFilesBrowser];
+  } else {
+    [tableView deselectRowAtIndexPath:indexPath animated:FALSE];
+  }
+  // [self.delegate folderSelected:self.path]; // let main view controller know we have something
 }
 
 @end
