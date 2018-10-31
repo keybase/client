2fa14c630147ab4d62be72a62e3564aac01cb6d6
Marco Munizaga <marco@marcopolo.io>
2018-10-31 13:48:32 -0700

    Remove unused cancelIdleCallback

--- shared/util/idle-callback.js
+++ shared/util/idle-callback.js
@@ -19,19 +19,14 @@ function timeoutFallback(cb: (info: TimeoutInfo) => void, deadlineOpts?: {timeou
   }, 20)
 }
 
-function cancelIdleCallbackFallback(id: TimeoutID) {
-  id && clearTimeout(id)
-}
-
 const useFallback = typeof window === 'undefined' || !window.requestIdleCallback
 const requestIdleCallback = forceImmediateLogging
   ? immediateCallback
   : useFallback
     ? timeoutFallback
     : window.requestIdleCallback.bind(window)
-const cancelIdleCallback = useFallback ? cancelIdleCallbackFallback : window.cancelIdleCallback.bind(window)
 
 const onIdlePromise = (timeout: number = 100): Promise<TimeoutInfo> =>
   new Promise(resolve => requestIdleCallback(resolve, {timeout}))
 
-export {requestIdleCallback, cancelIdleCallback, onIdlePromise}
+export {requestIdleCallback, onIdlePromise}
