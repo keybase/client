56bf047381cbe3e2be61cbd78a9c28c3782bb44e
Frederick Akalin <akalin@keyba.se>
2018-10-31 23:23:11 -0700

    Split up

--- shared/wallets/transaction-details/index.js
+++ shared/wallets/transaction-details/index.js
@@ -4,8 +4,9 @@ import * as Types from '../../constants/types/wallets'
 import * as Kb from '../../common-adapters'
 import * as Styles from '../../styles'
 import {capitalize} from 'lodash-es'
-import Transaction, {TimestampLine} from '../transaction'
+import Transaction from '../transaction'
 import {SmallAccountID} from '../common'
+import {formatTimeForMessages, formatTimeForStellarTooltip} from '../../util/timestamp'
 
 export type NotLoadingProps = {|
   amountUser: string,
@@ -226,6 +227,45 @@ const propsToParties = (props: NotLoadingProps) => {
       */
       throw new Error(`Unexpected role ${props.yourRole}`)
   }
+}
+
+type TimestampLineProps = {|
+  error: string,
+  status?: Types.StatusSimplified,
+  timestamp: ?Date,
+  selectableText: boolean,
+|}
+
+export const TimestampLine = (props: TimestampLineProps) => {
+  if (props.error) {
+    return (
+      <Kb.Text type="BodySmallError">
+        {capitalize(props.status)} • The Stellar network did not approve this transaction - {props.error}
+      </Kb.Text>
+    )
+  }
+  const timestamp = props.timestamp
+  if (!timestamp) {
+    return <Kb.Text type="BodySmall">The Stellar network hasn't confirmed your transaction.</Kb.Text>
+  }
+  const human = formatTimeForMessages(timestamp)
+  const tooltip = formatTimeForStellarTooltip(timestamp)
+  let status = capitalize(props.status)
+  // 'cancelable' -> show 'pending' and completed -> show nothing
+  switch (status) {
+    case 'Completed':
+      status = null
+      break
+    case 'Cancelable':
+      status = 'Pending'
+      break
+  }
+  return (
+    <Kb.Text selectable={props.selectableText} title={tooltip} type="BodySmall">
+      {human}
+      {status ? ` • ${status}` : null}
+    </Kb.Text>
+  )
 }
 
 const TransactionDetails = (props: NotLoadingProps) => {
