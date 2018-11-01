3c80983fc2d2cc2c336d279918a3aac975c240ac
Mike Maxim <mike.maxim@gmail.com>
2018-11-01 17:30:58 -0400

    wip

--- go/chat/unfurl/settings.go
+++ go/chat/unfurl/settings.go
@@ -1,16 +1,20 @@
 package unfurl
 
 import (
+	"github.com/keybase/client/go/chat/types"
 	"github.com/keybase/client/go/chat/utils"
 	"github.com/keybase/client/go/logger"
 )
 
 type Settings struct {
 	utils.DebugLabeler
+
+	storage types.ConversationBackedStorage
 }
 
-func NewSettings(log logger.Logger) *Settings {
+func NewSettings(log logger.Logger, storage types.ConversationBackedStorage) *Settings {
 	return &Settings{
 		DebugLabeler: utils.NewDebugLabeler(log, "Settings", false),
+		storage:      storage,
 	}
 }
