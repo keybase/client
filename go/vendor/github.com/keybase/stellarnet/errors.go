729b69704a1dfbcb5f08cd76446e8e5e8ef90ba7
Miles Steele <miles@milessteele.com>
2018-11-05 14:50:19 -0500

    govendor update github.com/keybase/stellarnet/...

--- go/vendor/github.com/keybase/stellarnet/errors.go
+++ go/vendor/github.com/keybase/stellarnet/errors.go
@@ -1,17 +1,6 @@
 package stellarnet
 
-import (
-	"errors"
-	"fmt"
-
-	perrors "github.com/pkg/errors"
-
-	"github.com/stellar/go/clients/horizon"
-)
-
-// ErrResourceNotFound is returned if there is no stellar resource found.
-// It is a generic not found error.
-var ErrResourceNotFound = errors.New("resource not found")
+import "errors"
 
 // ErrSourceAccountNotFound is returned if there is no stellar account for a source account address.
 var ErrSourceAccountNotFound = errors.New("source account not found")
@@ -30,74 +19,3 @@ var ErrUnknownKeypairType = errors.New("unknown keypair type")
 
 // ErrAssetNotFound is returned if no asset matches a code/issuer pair.
 var ErrAssetNotFound = errors.New("asset not found")
-
-// Error provides a hopefully user-friendly default in Error()
-// but with some details that might actually help debug in Verbose().
-type Error struct {
-	Display string
-	Details string
-}
-
-// Error implements the error interface.
-func (e Error) Error() string {
-	return e.Display
-}
-
-// Verbose returns additional details about the error.
-func (e Error) Verbose() string {
-	return fmt.Sprintf("%s [%s]", e.Display, e.Details)
-}
-
-// errMap maps some horizon errors to stellarnet errors.
-func errMap(err error) error {
-	if err == nil {
-		return nil
-	}
-
-	// the error might be wrapped, so get the unwrapped error
-	xerr := perrors.Cause(err)
-
-	switch xerr := xerr.(type) {
-	case Error:
-		// already wrapped it up
-		return xerr
-	case *horizon.Error:
-		if isOpNoDestination(xerr) {
-			return ErrDestinationAccountNotFound
-		}
-		if xerr.Problem.Status == 404 {
-			return ErrResourceNotFound
-		}
-
-		// catch-all
-		return Error{
-			Display: "stellar network error",
-			Details: fmt.Sprintf("horizon Problem: %+v", xerr.Problem),
-		}
-	default:
-		return err
-	}
-}
-
-func errMapAccount(err error) error {
-	xerr := errMap(err)
-	if xerr == ErrResourceNotFound {
-		return ErrSourceAccountNotFound
-	}
-	return xerr
-}
-
-func isOpNoDestination(herr *horizon.Error) bool {
-	resultCodes, err := herr.ResultCodes()
-	if err != nil {
-		return false
-	}
-	if resultCodes.TransactionCode != "tx_failed" {
-		return false
-	}
-	if len(resultCodes.OperationCodes) != 1 {
-		// only handle one operation now
-		return false
-	}
-	return resultCodes.OperationCodes[0] == "op_no_destination"
-}
