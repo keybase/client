b38ef60ac44e421d73a9f08760e77d487df8dcd2
chrisnojima <cnojima@keyba.se>
2018-11-07 14:57:24 -0500

    remove dev menu and move user timings

--- shared/engine/index.platform.native.js
+++ shared/engine/index.platform.native.js
@@ -7,7 +7,7 @@ import {pack} from 'purepack'
 import {toByteArray, fromByteArray} from 'base64-js'
 import toBuffer from 'typedarray-to-buffer'
 import {printRPCBytes} from '../local-debug'
-import {measureStart, measureStop} from '../dev/user-timings'
+import {measureStart, measureStop} from '../util/user-timings'
 
 import type {createClientType, incomingRPCCallbackType, connectDisconnectCB} from './index.platform'
 
