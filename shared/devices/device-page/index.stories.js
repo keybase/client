2e7c0b04604e818d42a7d1b0520c571b35c2ddf6
chrisnojima <cnojima@keyba.se>
2018-11-07 14:44:00 -0500

    owntypes for devices

--- shared/devices/device-page/index.stories.js
+++ shared/devices/device-page/index.stories.js
@@ -3,35 +3,49 @@ import * as React from 'react'
 import * as Types from '../../constants/types/devices'
 import * as Constants from '../../constants/devices'
 import * as Sb from '../../stories/storybook'
-import DevicePage from './container'
+import DevicePageReal from './container'
+
+type Props = {
+  _revoked?: boolean,
+  _type?: 'mobile' | 'desktop' | 'backup',
+  _current?: boolean,
+  _lastUsed?: boolean,
+  _revokedAt?: ?Date,
+}
+
+const DevicePage = (props: Props) => {
+  // $ForceType
+  const p: void = props
+  return <DevicePageReal {...p} />
+}
 
 const provider = Sb.createPropProviderWithCommon({
-  DevicePage: ({revoked, type, current, lastUsed = true, revokedAt = true}) => ({
+  DevicePage: ({_revoked, _type, _current, _lastUsed = true, _revokedAt = true}) => ({
     device: Constants.makeDevice({
       created: new Date('2002-10-09T01:23:45'),
-      currentDevice: !!current,
+      currentDevice: !!_current,
       deviceID: Types.stringToDeviceID('123'),
-      lastUsed: lastUsed ? new Date('2002-10-10T01:23:45') : 0,
-      name: `My ${type}`,
-      revokedAt: revokedAt && revoked ? new Date('2002-10-11T01:23:45') : null,
-      type,
+      lastUsed: _lastUsed ? new Date('2002-10-10T01:23:45') : 0,
+      name: `My ${_type}`,
+      revokedAt: _revokedAt && _revoked ? new Date('2002-10-11T01:23:45') : null,
+      type: _type,
     }),
     onBack: Sb.action('onback'),
-    showRevokeDevicePage: revoked ? null : Sb.action('showRevokeDevicePage'),
+    showRevokeDevicePage: _revoked ? null : Sb.action('showRevokeDevicePage'),
   }),
 })
 
 const load = () => {
   Sb.storiesOf('Devices/Device', module)
     .addDecorator(provider)
-    .add('Desktop', () => <DevicePage type="desktop" />)
-    .add('Desktop no last used', () => <DevicePage type="desktop" lastUsed={false} />)
-    .add('Desktop current', () => <DevicePage type="desktop" current={true} />)
-    .add('Desktop Revoked', () => <DevicePage type="desktop" revoked={true} />)
-    .add('Mobile', () => <DevicePage type="mobile" />)
-    .add('Mobile Revoked', () => <DevicePage type="mobile" revoked={true} />)
-    .add('Paper key', () => <DevicePage type="backup" />)
-    .add('Paper key Revoked', () => <DevicePage type="backup" revoked={true} />)
+    .add('Desktop', () => <DevicePage _type="desktop" />)
+    .add('Desktop no last used', () => <DevicePage _type="desktop" _lastUsed={false} />)
+    .add('Desktop current', () => <DevicePage _type="desktop" _current={true} />)
+    .add('Desktop Revoked', () => <DevicePage _type="desktop" _revoked={true} />)
+    .add('Mobile', () => <DevicePage _type="mobile" />)
+    .add('Mobile Revoked', () => <DevicePage _type="mobile" _revoked={true} />)
+    .add('Paper key', () => <DevicePage _type="backup" />)
+    .add('Paper key Revoked', () => <DevicePage _type="backup" _revoked={true} />)
 }
 
 export default load
