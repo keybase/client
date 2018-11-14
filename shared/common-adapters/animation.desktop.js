f092de005c1001717b388b9a2b682f6c0b8a0504
chrisnojima <cnojima@keyba.se>
2018-11-14 11:50:35 -0500

    WIP

--- shared/common-adapters/animation.desktop.js
+++ shared/common-adapters/animation.desktop.js
@@ -1,21 +1,39 @@
 // @flow
-import React from 'react'
-import Lottie from 'lottie-react-web'
+import * as React from 'react'
 import Box from './box'
-import animationData from './animation.json'
+import animationData from './animation-data.json'
 import type {Props} from './animation'
 
 const defaultDimension = 16
 
-const Animation = (props: Props) => (
-  <Box style={props.containerStyle}>
-    <Lottie
-      options={{animationData: animationData[props.animationType]}}
-      width={props.width || defaultDimension}
-      height={props.height || defaultDimension}
-      style={props.style}
-    />
-  </Box>
-)
+class Animation extends React.Component<Props> {
+  render() {
+    // jest doesnt' support canvas out of the box, so lets just not do anything
+    if (__STORYSHOT__) {
+      return (
+        <Box>
+          {JSON.stringify({
+            height: this.props.height || defaultDimension,
+            options: {animationData: animationData[this.props.animationType]},
+            style: this.props.style,
+            width: this.props.width || defaultDimension,
+          })}
+        </Box>
+      )
+    }
+    // jest pukes if the import is on top so just defer till render
+    const Lottie = require('lottie-react-web').default
+    return (
+      <Box style={this.props.containerStyle}>
+        <Lottie
+          options={{animationData: animationData[this.props.animationType]}}
+          width={this.props.width || defaultDimension}
+          height={this.props.height || defaultDimension}
+          style={this.props.style}
+        />
+      </Box>
+    )
+  }
+}
 
 export default Animation
--- shared/common-adapters/animation.native.js
+++ shared/common-adapters/animation.native.js
@@ -1,19 +1,23 @@
 // @flow
-import React from 'react'
+import * as React from 'react'
 import LottieView from 'lottie-react-native'
 import Box from './box'
-import animationData from './animation.json'
+import animationData from './animation-data.json'
 import type {Props} from './animation'
 
-const Animation = (props: Props) => (
-  <Box style={props.containerStyle}>
-    <LottieView
-      autoPlay={true}
-      loop={true}
-      source={animationData[props.animationType]}
-      style={props.style || {}}
-    />
-  </Box>
-)
+class Animation extends React.Component<Props> {
+  render() {
+    return (
+      <Box style={this.props.containerStyle}>
+        <LottieView
+          autoPlay={true}
+          loop={true}
+          source={animationData[this.props.animationType]}
+          style={this.props.style || {}}
+        />
+      </Box>
+    )
+  }
+}
 
 export default Animation
