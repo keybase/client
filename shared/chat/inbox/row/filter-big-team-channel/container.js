ef0460ea2ec01d555f416159177a12458495b8a3
chrisnojima <cnojima@keyba.se>
2018-11-15 10:16:30 -0500

    WIP

--- shared/chat/inbox/row/filter-big-team-channel/container.js
+++ shared/chat/inbox/row/filter-big-team-channel/container.js
@@ -1,5 +1,5 @@
 // @flow
-import {FilterBigTeamChannel, height} from '.'
+import {FilterBigTeamChannel} from '.'
 import * as Types from '../../../../constants/types/chat2'
 import * as Constants from '../../../../constants/chat2'
 import * as Route from '../../../../actions/route-tree'
@@ -39,5 +39,3 @@ export default connect<OwnProps, _, _, _, _>(
   mapDispatchToProps,
   mergeProps
 )(FilterBigTeamChannel)
-
-export {height}
