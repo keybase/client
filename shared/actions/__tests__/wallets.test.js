1245bce4494ee0ac00bdb41eab279b378095e217
Frederick Akalin <akalin@keyba.se>
2018-11-14 17:17:24 -0800

    Get test to pass

---
+++ shared/actions/__tests__/wallets.test.js
@@ -0,0 +1,75 @@
+// @flow
+/* eslint-env jest */
+import * as Constants from '../../constants/wallets'
+import * as Tabs from '../../constants/tabs'
+import * as WalletsGen from '../wallets-gen'
+import * as RPCStellarTypes from '../../constants/types/rpc-stellar-gen'
+import * as Types from '../../constants/types/wallets'
+import * as RouteTree from '../route-tree'
+import walletsSaga from '../wallets'
+import appRouteTree from '../../app/routes-app'
+import * as Testing from '../../util/testing'
+
+jest.mock('../../engine')
+
+// We want to be logged in usually
+const blankStore = Testing.getInitialStore()
+const initialStore = {
+  ...blankStore,
+  wallets: blankStore.wallets.merge({
+    accountMap: blankStore.wallets.accountMap.set(
+      Types.stringToAccountID('fake account ID'),
+      Constants.makeAccount()
+    ),
+  }),
+}
+
+const buildPaymentRpc = {
+  amountErrMsg: '',
+  banners: null,
+  displayAmountFiat: '$5.00 USD',
+  displayAmountXLM: '21.4168160 XLM',
+  from: 'fake account ID',
+  publicMemoErrMsg: '',
+  readyToSend: false,
+  secretNoteErrMsg: '',
+  sendingIntentionXLM: false,
+  toErrMsg: '',
+  worthAmount: '21.4168160',
+  worthCurrency: 'USD',
+  worthDescription: '21.4168160 XLM',
+  worthInfo: '$1.00 = 4.2833632 XLM\nSource: coinmarketcap.com',
+}
+
+const builtPayment = Constants.buildPaymentResultToBuiltPayment(buildPaymentRpc)
+
+const startOnWalletsTab = dispatch => {
+  dispatch(RouteTree.switchRouteDef(appRouteTree))
+  dispatch(RouteTree.navigateTo([Tabs.walletsTab]))
+}
+
+const startReduxSaga = Testing.makeStartReduxSaga(walletsSaga, initialStore, startOnWalletsTab)
+
+describe('load', () => {
+  let init
+  beforeEach(() => {
+    init = startReduxSaga()
+  })
+
+  let rpc
+  afterEach(() => {
+    rpc && rpc.mockRestore()
+  })
+
+  it('load leads to loaded', () => {
+    const {dispatch, getState} = init
+    rpc = jest.spyOn(RPCStellarTypes, 'localBuildPaymentLocalRpcPromise')
+    rpc.mockImplementation(() => new Promise(resolve => resolve(buildPaymentRpc)))
+
+    dispatch(WalletsGen.createBuildPayment())
+    return Testing.flushPromises().then(() => {
+      expect(getState().wallets.builtPayment).toEqual(builtPayment)
+      expect(rpc).toHaveBeenCalled()
+    })
+  })
+})
