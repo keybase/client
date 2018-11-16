6326f2e4eca6024549e713e79fc259bc8bb7d414
Mike Maxim <mike.maxim@gmail.com>
2018-11-16 10:29:52 -0500

    wip

--- go/chat/convsource.go
+++ go/chat/convsource.go
@@ -1141,14 +1141,13 @@ func (s *HybridConversationSource) notifyUnfurls(ctx context.Context, uid gregor
 		s.Debug(ctx, "notifyUnfurls: failed to transform supersedes: %s", err)
 		return
 	}
-	var notif chat1.UnfurlUpdateNotifs
-	for _, msg := range unfurledMsgs {
-		notif.Updates = append(notif.Updates, chat1.UnfurlUpdateNotif{
-			ConvID: convID,
-			Msg:    utils.PresentMessageUnboxed(ctx, s.G(), msg, uid, convID),
-		})
+	notif := chat1.MessagesUpdated{
+		ConvID: convID,
 	}
-	act := chat1.NewChatActivityWithMessageUnfurled(notif)
+	for _, msg := range unfurledMsgs {
+		notif.Updates = append(notif.Updates, utils.PresentMessageUnboxed(ctx, s.G(), msg, uid, convID))
+	}
+	act := chat1.NewChatActivityWithMessagesUpdated(notif)
 	s.G().ActivityNotifier.Activity(ctx, uid, chat1.TopicType_CHAT,
 		&act, chat1.ChatActivitySource_LOCAL)
 }
