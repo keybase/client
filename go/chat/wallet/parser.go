c6330f22fb76af04ce0e12d59d2d7a219012f929
Surya <surya@keyba.se>
2018-11-20 14:19:01 -0500

    use regex replacer

--- go/chat/wallet/parser.go
+++ go/chat/wallet/parser.go
@@ -48,65 +48,19 @@ func findChatTxCandidates(xs string) []chat1.ChatTxCandidate {
 }
 
 var startQuote = ">"
-var startCodeLine = []rune("`")
-var startCodeBlock = []rune("```")
-var endCodeLine = []rune("`")
-var endCodeBlock = []rune("```")
 var newline = []rune("\n")
 
-type newlineBehavior = int
-
-const (
-	sameLine newlineBehavior = iota
-	multiLine
-)
-
 func replaceQuotedSubstrings(xs string, replacer string) string {
-	t := []rune(nil)
-	runes := []rune(xs)
-	replacerRunes := []rune(replacer)
+	xs = regexp.MustCompile("(?s)```.*?```").ReplaceAllString(xs, replacer)
+	xs = regexp.MustCompile("(?s)`.*?`").ReplaceAllString(xs, replacer)
 
-	// First, split the string up into runes and remove all codelines and codeblocks.
-	i := 0
-	for i < len(runes) {
-		foundEndCodeblock := false
-		// Loop while the string starts with `, to handle adjacent codelines
-		for startsWithAt(runes, i, startCodeLine) {
-			// If it starts with ```, try to find the close block across newlines.
-			// If it exists, skip to it, otherwise fallback to codeline handling.
-			if startsWithAt(runes, i, startCodeBlock) {
-				if ret := findNextAt(runes, i+len(startCodeBlock), endCodeBlock, multiLine); ret != -1 {
-					i = ret + len(endCodeBlock)
-					t = append(t, replacerRunes...)
-					foundEndCodeblock = true
-				}
-			}
-			// If we're starting with a ` or a ``` that isn't closed, treat the first ` as the start of a
-			// codeline and try to find the close ` in the same line. If found, skip to it, otherwise
-			// break out of the loop and add the unmatched ` to the returned string.
-			if !foundEndCodeblock {
-				if ret := findNextAt(runes, i+len(startCodeLine), endCodeLine, sameLine); ret != -1 {
-					i = ret + len(endCodeLine)
-					t = append(t, replacerRunes...)
-				} else {
-					break
-				}
-			}
-		}
-		// If the first letter is a regular letter or an unmatched `, add it to the returned string
-		if i < len(runes) {
-			t = append(t, runes[i])
-			i++
-		}
-	}
-
-	// Finally, remove all quoted lines. Because we removed all codeblocks
+	// Remove all quoted lines. Because we removed all codeblocks
 	// before, we only need to consider single lines.
-	var r []string
-	for _, line := range strings.Split(string(t), string(newline)) {
+	var ret []string
+	for _, line := range strings.Split(xs, string(newline)) {
 		if !strings.HasPrefix(strings.TrimLeft(line, " "), startQuote) {
-			r = append(r, line)
+			ret = append(ret, line)
 		}
 	}
-	return strings.Join(r, string(newline))
+	return strings.Join(ret, string(newline))
 }
