530fee2ebb998b3aea88c324cfa8d5bddc9f64ab
John Zila <john@keyba.se>
2018-11-21 11:06:44 -0800

    ownprops for banner

--- shared/fs/banner/container.js
+++ shared/fs/banner/container.js
@@ -1,15 +1,14 @@
 // @flow
 import * as Constants from '../../constants/fs'
+import * as FsTypes from '../../constants/types/fs'
 import {namedConnect} from '../../util/container'
 import Banner from '.'
 
-const mapStateToProps = (state, {path}) => {
-  const _pathItem = state.fs.pathItems.get(path, Constants.unknownPathItem)
-  return {
-    path,
-    shouldShowReset:
-      _pathItem.type === 'folder' && !!_pathItem.tlfMeta && _pathItem.tlfMeta.resetParticipants.length > 0,
-  }
-}
+type OwnProps = {|path: FsTypes.Path|}
 
-export default namedConnect(mapStateToProps, () => ({}), (s, d, o) => ({...o, ...s, ...d}), 'Banner')(Banner)
+const mapStateToProps = (state, {path}: OwnProps) => ({
+  path,
+  shouldShowReset: Constants.getTlfFromPath(state.fs.tlfs, path).resetParticipants.size > 0,
+})
+
+export default namedConnect<OwnProps, _, _, _, _>(mapStateToProps, () => ({}), (s, d, o) => ({...o, ...s, ...d}), 'Banner')(Banner)
