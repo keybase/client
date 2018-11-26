c5f7b6a11dd91f1cdff1e810fd99b8fc33141364
Marco Munizaga <marco@marcopolo.io>
2018-11-25 17:10:03 -0800

    Use shallow Equal

--- shared/util/memoize.js
+++ shared/util/memoize.js
@@ -1,11 +1,12 @@
 // @flow
 import memoize from 'memoize-one'
+import shallowEqual from 'shallowequal'
 
 const memoize1Obj = (f: any) =>
   memoize(
     f,
     // $FlowIssue - the type of memoize1 is wonky
-    (a, b) => Object.keys(a).length === Object.keys(b).length && !Object.keys(a).some(k => a[k] !== b[k])
+    (a, b) => shallowEqual(a, b)
   )
 
 export {
