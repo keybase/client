00a9e2e8c2cc0860bd1356858448bc57dfdf35e6
Patrick Crosby <pcrosby@gmail.com>
2018-12-06 12:26:01 -0600

    Add func to return MiniChatPaymentSpecs for Wallet ICS confirm  (#14963)
    * WIP
    
    * First test passes
    
    * More tests

--- go/libkb/stellar_stub.go
+++ go/libkb/stellar_stub.go
@@ -45,4 +45,9 @@ func (n *nullStellar) GetMigrationLock() *sync.Mutex { return new(sync.Mutex) }
 func (n *nullStellar) SendMiniChatPayments(mctx MetaContext, payments []MiniChatPayment) ([]MiniChatPaymentResult, error) {
 	return nil, errors.New("nullStellar SendMiniChatPayments")
 }
+
+func (n *nullStellar) SpecMiniChatPayments(mctx MetaContext, payments []MiniChatPayment) (*MiniChatPaymentSummary, error) {
+	return nil, errors.New("nullStellar SpecMiniChatPayments")
+}
+
 func (n *nullStellar) RefreshWalletState(ctx context.Context) {}
