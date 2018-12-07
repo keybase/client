b90742562dab4945d492ea9319ddc5ac6cf14a90
chrisnojima <cnojima@keyba.se>
2018-12-07 14:49:27 -0500

    WIP

---
+++ shared/profile/pgp/routes.native.js
@@ -0,0 +1,39 @@
+// @flow
+import * as React from 'react'
+import {makeRouteDefNode} from '../../route-tree'
+import * as Kb from '../../common-adapters'
+import * as Styles from '../../styles'
+import {navigateUp} from '../../actions/route-tree'
+import {connect} from '../../util/container'
+
+type OwnProps = {||}
+
+const NoPGPView = props => (
+  <Kb.StandardScreen style={styleContainer} onCancel={props.onCancel}>
+    <Kb.Text style={styleTitle} type="Header">
+      Add a PGP key
+    </Kb.Text>
+    <Kb.Text type="Body">For now, please use our desktop app to create PGP keys.</Kb.Text>
+  </Kb.StandardScreen>
+)
+
+const NoPGP = connect<OwnProps, _, _, _, _>(
+  () => ({}),
+  dispatch => ({onCancel: () => dispatch(navigateUp())}),
+  (s, d, o) => ({...o, ...s, ...d})
+)(NoPGPView)
+
+const routeTree = makeRouteDefNode({
+  component: NoPGP,
+})
+
+const styleContainer = {
+  justifyContent: 'flex-start',
+}
+
+const styleTitle = {
+  marginBottom: Styles.globalMargins.xlarge,
+  textAlign: 'center',
+}
+
+export default routeTree
