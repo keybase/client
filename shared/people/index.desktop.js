5073539e1802593f1ec49c19f3e5bf9b1eec6f8f
Daniel Ayoub <danny.a95@gmail.com>
2018-12-09 11:03:47 -0500

    revert people changes

--- shared/people/index.desktop.js
+++ shared/people/index.desktop.js
@@ -1,12 +1,9 @@
 // @flow
 import * as React from 'react'
-import {Box2, PlainInput, ProgressIndicator, ScrollView} from '../common-adapters'
+import {ProgressIndicator, ScrollView} from '../common-adapters'
 import {PeoplePageSearchBar, PeoplePageList} from './index.shared'
 import {type Props} from '.'
 import {globalStyles} from '../styles'
-
-// TEMP
-import AddSuggestors, * as Suggestors from '../chat/conversation/input-area/suggestors'
 
 const People = (props: Props) => (
   <ScrollView style={{...globalStyles.fullHeight}}>
@@ -20,24 +17,7 @@ const People = (props: Props) => (
       styleSearchText={{fontSize: 13}}
     />
     <PeoplePageList {...props} />
-    <SuggestorTestArea
-      dataSources={{}}
-      renderers={{}}
-      suggestorToMarker={{}}
-      transformers={{}}
-      someOtherProps="hi"
-    />
   </ScrollView>
 )
-
-const _SuggestorTestArea = (props: Suggestors.PropsWithSuggestor<{someOtherProps: string}>) => (
-  <Box2
-    direction="vertical"
-    style={{borderColor: 'black', borderStyle: 'solid', borderWidth: 1, margin: 20, padding: 10}}
-  >
-    <PlainInput onChangeText={props.onChangeText} onKeyDown={props.onKeyDown} ref={props.inputRef} />
-  </Box2>
-)
-const SuggestorTestArea = AddSuggestors(_SuggestorTestArea)
 
 export default People
--- shared/people/index.native.js
+++ shared/people/index.native.js
@@ -1,7 +1,6 @@
 // @flow
 import * as React from 'react'
 import * as Kb from '../common-adapters/mobile.native'
-import AddSuggestors, * as Suggestors from '../chat/conversation/input-area/suggestors'
 import {PeoplePageSearchBar, PeoplePageList} from './index.shared'
 import {type Props} from '.'
 import {globalStyles, styleSheetCreate} from '../styles'
@@ -29,32 +28,8 @@ const People = (props: Props) => (
       style={Kb.avatarCastPlatformStyles(styles.avatar)}
     />
     <PeoplePageList {...props} />
-    <SuggestorTestArea
-      dataSources={{}}
-      renderers={{}}
-      suggestorToMarker={{}}
-      transformers={{}}
-      someOtherProps="hi"
-    />
   </Kb.ScrollView>
 )
-
-const _SuggestorTestArea = (props: Suggestors.PropsWithSuggestor<{someOtherProps: string}>) => (
-  <Kb.Box2
-    direction="vertical"
-    style={{
-      borderColor: 'black',
-      borderStyle: 'solid',
-      borderWidth: 1,
-      margin: 20,
-      padding: 10,
-      width: 300,
-    }}
-  >
-    <Kb.PlainInput onChangeText={props.onChangeText} onKeyDown={props.onKeyDown} ref={props.inputRef} />
-  </Kb.Box2>
-)
-const SuggestorTestArea = AddSuggestors(_SuggestorTestArea)
 
 const styles = styleSheetCreate({
   avatar: {
