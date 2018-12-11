9f2e806499bcb1660578d3c8cbd1ad9d61b75863
John Zila <john@keyba.se>
2018-12-11 15:47:41 -0800

    fix deps to handle all OSes

--- go/gen_deps.pl
+++ go/gen_deps.pl
@@ -6,28 +6,43 @@ use 5.010;
 use Data::Dumper qw(Dumper);
 use JSON::PP qw(encode_json);
 
-my @packages = split /\n/, `go list ./...`;
-my %dep_packages;
-my $num_packages = @packages;
+my $dep_packages = {};
+my @oses = ('linux', 'darwin', 'windows');
+my $os_packages = {};
+my $total_packages = 0;
 
-for (my $i=0; $i<$num_packages; $i++) {
-    my $package = $packages[$i];
-    my $percent_complete = (($i + 1) * 100) / $num_packages;
-    say STDERR "\rparsing package $package";
-    printf STDERR ("%d of %d complete (%.1f%%)", $i+1, $num_packages, $percent_complete);
+$ENV{'GOARCH'} = 'amd64';
+foreach my $os (@oses) {
+    $ENV{'GOOS'} = $os;
+    my @packages = split /\n/, `go list ./...`;
+    $os_packages->{$os} = \@packages;
+    $total_packages += scalar @packages;
+}
+say STDERR "total packages for which to calculate dependencies: $total_packages";
 
-    # This should include vendored dependencies.
-    $ENV{'GOOS'} = 'linux';
-    $ENV{'GOARCH'} = 'amd64';
-    my @deps = split /\n/, `go list -f '{{ print (join .TestImports "\\n") "\\n" (join .Imports "\\n") }}' "$package" 2>/dev/null | xargs go list -f '{{ join .Deps "\\n" }}' 2>/dev/null | sort | uniq | grep 'vendor\\|github.com\\/keybase\\/client'`;
+my $i = 0;
+foreach my $os (@oses) {
+    say STDERR "\rgenerating dependencies for OS: $os";
+    $ENV{'GOOS'} = $os;
+    foreach my $package (@{$os_packages->{$os}}) {
+        $i++;
+        my $percent_complete = (($i) * 100) / $total_packages;
+        say STDERR "\rparsing package $package";
+        printf STDERR ("%d of %d complete (%.0f%%)", $i, $total_packages, $percent_complete);
 
-    foreach my $dep (@deps) {
-        $dep_packages{$dep}{$package} = 1;
+        # This should include vendored dependencies.
+        my @deps = split /\n/, `go list -f '{{ print (join .TestImports "\\n") "\\n" (join .Imports "\\n") }}' "$package" 2>/dev/null | xargs go list -f '{{ join .Deps "\\n" }}' 2>/dev/null | sort | uniq | grep 'vendor\\|github.com\\/keybase\\/client'`;
+
+        foreach my $dep (@deps) {
+            $dep_packages->{$os}->{$dep}->{$package} = 1;
+        }
     }
 }
 say STDERR "";
 
-my $json_output = JSON::PP->new->utf8->pretty->canonical()->encode(\%dep_packages);
-open(my $fh, '>', '.go_package_deps');
-print $fh "$json_output";
-close($fh);
+foreach my $os (@oses) {
+    my $json_output = JSON::PP->new->utf8->pretty->canonical()->encode($dep_packages->{$os});
+    open(my $fh, '>', ".go_package_deps_$os");
+    print $fh "$json_output";
+    close($fh);
+}
