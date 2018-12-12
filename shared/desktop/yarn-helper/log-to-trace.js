5368254436ea4df0725c3f7b5236db2a33795bdb
chrisnojima <cnojima@keyba.se>
2018-12-12 17:52:13 -0500

    WIP

--- shared/desktop/yarn-helper/log-to-trace.js
+++ shared/desktop/yarn-helper/log-to-trace.js
@@ -1,26 +1,80 @@
 // @flow
+const [, , guiOrCore, logfile, outfile] = process.argv
+if (['gui', 'core'].indexOf(guiOrCore) === -1 || !logfile || !outfile) {
+  console.log('Usage: node log-to-trace (gui|core) logfile outfile')
+  process.exit(1)
+}
+const isGUI = guiOrCore === 'gui'
 const fs = require('fs')
-const data = fs.readFileSync(process.argv[2], 'utf8')
 const moment = require('moment')
-const lines = data.split('\n')
+// core regs
 const reg = /([^ ]+) ▶ \[DEBU (keybase|kbfs) ([^:]+):(\d+)] ([0-9a-f]+) ([^[]+)(\[tags:([^\]]+)])?/
 const tagsReg = /\[tags:([^\]]+)]/
 const methodPrefixReg = /^(\+\+Chat: )?/
 const methodResultReg = / -> .*$/
 const typeAndMethodReg = /^(\W*)/
+// gui regs
+const guiCountTypeTimeReg = /\["(Info|Warn|Action)","([^"]+)","(.*)"]/
+const actionReg = /type: ([^ ]+) (.*)/
+const actionPayloadReg = /\\"/g
 
-if (process.argv.length !== 4) {
-  console.log('Usage: node log-to-trace logfile outfile')
-  process.exit(1)
+const convertGuiLine = line => {
+  const e = guiCountTypeTimeReg.exec(line)
+  if (!e) {
+    console.log('Skipping unparsed line:', line)
+    return
+  }
+  const [, type, time, _data] = e
+  let name = ''
+  let args = {}
+  switch (type) {
+    case 'Warn':
+      name = `W: ${_data}`
+      break
+    case 'Info':
+      name = `I: ${_data}`
+      break
+    case 'Action':
+      {
+        const m = actionReg.exec(_data)
+        if (m) {
+          const [, actionType, payload] = m
+          name = actionType
+          try {
+            args = JSON.parse(payload.replace(actionPayloadReg, '"'))
+          } catch (e) {
+            console.log('throw e', e)
+          }
+        } else {
+          console.log('Unparsed action!', line, _data)
+          name = 'Unparsed action'
+        }
+      }
+      break
+    default:
+      console.log('Unknown inner type', type)
+      return
+  }
+  const app = 'gui'
+
+  return {
+    app,
+    args,
+    id: name,
+    line,
+    name,
+    time,
+    type: 'gui',
+  }
 }
 
-const convertLine = line => {
+const convertCoreLine = line => {
   const e = reg.exec(line)
   if (!e) {
     console.log('Skipping unparsed line:', line)
     return
   }
-  const [, time, coreOrKbfs, file, fileline, counter, _typeAndMethod, _tags] = e
+  const [, time, app, file, fileline, counter, _typeAndMethod, _tags] = e
   let tags = 'NO_TAG'
   if (_tags) {
     const match = tagsReg.exec(_tags)
@@ -44,20 +98,26 @@ const convertLine = line => {
   }
 
   const method = typeAndMethod.replace(typeAndMethodReg, '').trim()
+  const args = {
+    counter: counter,
+    file: file,
+    line: fileline,
+  }
+  const id = `${tags}:${method}`
+  const name = method
   return {
-    coreOrKbfs,
-    counter,
-    file,
-    fileline,
+    app,
+    args,
+    id,
     line,
-    method,
-    tags,
+    name,
     time,
     type,
   }
 }
 
-const tags = {}
+const convertLine = isGUI ? convertGuiLine : convertCoreLine
+
 const output = {
   // injecting a start and overwriting an unmatched one
   collision: [],
@@ -70,17 +130,13 @@ const output = {
 }
 
 const buildEvent = (info, ph) => ({
-  args: {
-    counter: info.counter,
-    file: info.file,
-    line: info.fileline,
-  },
-  id: `${info.tags}:${info.method}`,
-  name: info.method,
+  args: info.args,
+  id: info.id,
+  name: info.name,
   ph,
   pid: 0,
-  tid: info.coreOrKbfs,
-  ts: moment(info.time).valueOf() * 1000,
+  tid: info.app,
+  ts: moment(info.time).valueOf() * (isGUI ? 1 : 1000),
 })
 
 const buildGood = (old, info) => {
@@ -93,34 +149,36 @@ const buildGood = (old, info) => {
   return [s, e]
 }
 
+const lines = fs.readFileSync(logfile, 'utf8').split('\n')
+let lastGuiLine = null
+const knownIDs = {}
 lines.forEach(line => {
   const info = convertLine(line)
   if (!info) return
 
-  // ensure good
-  if (!tags[info.tags]) {
-    tags[info.tags] = {}
-  }
-
-  const data = tags[info.tags]
-  const dataKey = info.method
-
   switch (info.type) {
     case '+':
-      if (data[dataKey]) {
-        output.single = output.single.concat(buildEvent(data[dataKey], 'i'))
+      if (knownIDs[info.id]) {
+        output.single = output.single.concat(buildEvent(knownIDs[info.id], 'i'))
         output.collision.push(info)
       }
-      data[dataKey] = info
+      knownIDs[info.id] = info
       break
     case '-':
-      if (data[dataKey]) {
-        output.good = output.good.concat(buildGood(data[dataKey], info))
-        data[dataKey] = undefined
+      if (knownIDs[info.id]) {
+        output.good = output.good.concat(buildGood(knownIDs[info.id], info))
+        knownIDs[info.id] = undefined
       } else {
         output.single = output.single.concat(buildEvent(info, 'i'))
         output.unmatched.push(info)
       }
+      break
+    case 'gui':
+      // treat all these single fires as a span of time w/ the last item
+      if (lastGuiLine) {
+        output.good = output.good.concat(buildGood(lastGuiLine, {...info, id: lastGuiLine.id}))
+      }
+      lastGuiLine = info
       break
     default:
     // console.log('Unknown line type:', info.type, ':', line)
@@ -142,4 +200,4 @@ const format = {
   traceEvents: [...output.good, ...output.single],
 }
 const out = JSON.stringify(format, null, 2)
-fs.writeFileSync(process.argv[3], out, 'utf8')
+fs.writeFileSync(outfile, out, 'utf8')
