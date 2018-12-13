5d512c46b7764d278def0c0f1717bebde9d2a502
Song Gao <song@gao.io>
2018-12-13 13:59:42 -0800

    send-link-to-chat 1: components and storyshots (#15001)
    * send-link-to-chat 1: components and storyshots
    
    * use Flow.ifFlowComplainsAboutThisFunctionYouHaventHandledAllCasesInASwitch
    
    suggested by chrisnojima

---
+++ shared/fs/common/send-in-app-action.js
@@ -0,0 +1,38 @@
+// @flow
+import * as I from 'immutable'
+import * as React from 'react'
+import * as Types from '../../constants/types/fs'
+import * as Kb from '../../common-adapters'
+import * as Styles from '../../styles'
+import {namedConnect} from '../../util/container'
+
+type OwnProps = {
+  path: Types.Path,
+  routePath?: ?I.List<string>,
+  sendIconClassName: string,
+}
+
+const mapDispatchToProps = (dispatch, {path, routePath}: OwnProps) => ({
+  onClick: () => {},
+})
+
+const YouSeeAButtonYouPushIt = ({onClick, sendIconClassName}) => (
+  <Kb.Icon
+    type="iconfont-open-browser"
+    onClick={onClick}
+    className={sendIconClassName}
+    style={Kb.iconCastPlatformStyles(styles.icon)}
+  />
+)
+const styles = Styles.styleSheetCreate({
+  icon: {
+    padding: Styles.globalMargins.tiny,
+  },
+})
+
+export default namedConnect<OwnProps, _, _, _, _>(
+  () => ({}),
+  mapDispatchToProps,
+  (s, d, o) => ({...o, ...s, ...d}),
+  'SendInAppAction'
+)(YouSeeAButtonYouPushIt)
