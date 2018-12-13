158a265de11e9c458b9293154d929ac7009fc7b9
Daniel Ayoub <danny.a95@gmail.com>
2018-12-13 12:18:16 -0500

    missed hook

--- shared/chat/index.stories.js
+++ shared/chat/index.stories.js
@@ -1,5 +1,4 @@
 // @flow
-import channelMentionHud from './conversation/input-area/channel-mention-hud/index.stories'
 import createChannel from './create-channel/index.stories'
 import inbox from './inbox/index.stories.js'
 import infoPanel from './conversation/info-panel/index.stories'
@@ -9,7 +8,6 @@ import manageChannels from './manage-channels/index.stories'
 import messagePopup from './conversation/messages/message-popup/index.stories'
 import setExplodingMessage from './conversation/messages/set-explode-popup/index.stories'
 import suggestors from './conversation/input-area/suggestors/index.stories'
-import userMentionHud from './conversation/input-area/user-mention-hud/index.stories'
 import messages from './conversation/messages/index.stories'
 import startConversation from './conversation/list-area/start-conversation/index.stories'
 import thread from './conversation/list-area/normal/index.stories'
@@ -18,7 +16,6 @@ import confirmPayment from './confirm-payment/index.stories'
 
 const load = () => {
   ;[
-    channelMentionHud,
     createChannel,
     inbox,
     infoPanel,
@@ -29,7 +26,6 @@ const load = () => {
     messages,
     setExplodingMessage,
     suggestors,
-    userMentionHud,
     startConversation,
     thread,
     headerArea,
