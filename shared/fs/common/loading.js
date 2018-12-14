11b3a984b9f4457f23f804a0ff28d2c856a16f34
Song Gao <song@gao.io>
2018-12-14 15:03:54 -0800

    loading screen feedback from cecileb

--- shared/fs/common/loading.js
+++ shared/fs/common/loading.js
@@ -9,22 +9,25 @@ type Props = {
 }
 
 export default ({path}: Props) => (
-  <Kb.Box2
-    direction="vertical"
-    centerChildren={true}
-    style={styles.container}
-    fullHeight={true}
-    fullWidth={true}
-    gap="small"
-  >
-    <Kb.ProgressIndicator style={styles.indicator} />
-    <Kb.Text type="Body">Loading {Types.pathToString(path)} ...</Kb.Text>
+  <Kb.Box2 direction="vertical" fullHeight={true} fullWidth={true}>
+    <Kb.Box2 direction="horizontal" centerChildren={true} style={styles.header} fullWidth={true}>
+      <Kb.Text type="Header">Keybase Files</Kb.Text>
+    </Kb.Box2>
+    <Kb.Box2 direction="vertical" centerChildren={true} style={styles.container} fullWidth={true} gap="small">
+      <Kb.ProgressIndicator style={styles.indicator} />
+      <Kb.Text type="BodySmall">Loading ...</Kb.Text>
+    </Kb.Box2>
   </Kb.Box2>
 )
 
 const styles = Styles.styleSheetCreate({
   container: {
     backgroundColor: Styles.globalColors.blue5,
+    ...Styles.globalStyles.flexGrow,
+  },
+  header: {
+    height: 48,
+    justifyContent: 'center',
   },
   indicator: {
     height: 32,
