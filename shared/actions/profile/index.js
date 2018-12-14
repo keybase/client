b11355c4306181dd6e392809b56764f85eb87343
chrisnojima <cnojima@keyba.se>
2018-12-14 15:18:38 -0500

    WIP

--- shared/actions/profile/index.js
+++ shared/actions/profile/index.js
@@ -1,52 +1,39 @@
 // @flow
-import logger from '../../logger'
 import * as Constants from '../../constants/profile'
-import * as TrackerGen from '../tracker-gen'
 import * as ProfileGen from '../profile-gen'
+import * as RPCTypes from '../../constants/types/rpc-gen'
+import * as RouteTree from '../../actions/route-tree-gen'
 import * as Saga from '../../util/saga'
 import * as SearchConstants from '../../constants/search'
-import * as RPCTypes from '../../constants/types/rpc-gen'
+import * as TrackerGen from '../tracker-gen'
 import keybaseUrl from '../../constants/urls'
+import logger from '../../logger'
 import openURL from '../../util/open-url'
+import type {TypedState} from '../../constants/reducer'
 import {getPathProps} from '../../route-tree'
-import * as RouteTree from '../../actions/route-tree-gen'
 import {peopleTab} from '../../constants/tabs'
 import {pgpSaga} from './pgp'
 import {proofsSaga} from './proofs'
 
-import type {TypedState} from '../../constants/reducer'
+const editProfile = (_, action: ProfileGen.EditProfilePayload) =>
+  RPCTypes.userProfileEditRpcPromise({
+    bio: action.payload.bio,
+    fullName: action.payload.fullname,
+    location: action.payload.location,
+  }).then(() => RouteTree.createNavigateUp())
 
-function _editProfile(action: ProfileGen.EditProfilePayload) {
-  const {bio, fullname, location} = action.payload
-  return Saga.sequentially([
-    Saga.callUntyped(RPCTypes.userProfileEditRpcPromise, {
-      bio,
-      fullName: fullname,
-      location,
-    }),
-    // If the profile tab remained on the edit profile screen, navigate back to the top level.
-    Saga.put(RouteTree.createNavigateUp()),
-  ])
-}
+const uploadAvatar = (_, action: ProfileGen.UploadAvatarPayload) =>
+  RPCTypes.userUploadUserAvatarRpcPromise({
+    crop: action.payload.crop,
+    filename: action.payload.filename,
+  }).then(() => RouteTree.createNavigateUp())
 
-function _uploadAvatar(action: ProfileGen.UploadAvatarPayload) {
-  const {filename, crop} = action.payload
-  return Saga.sequentially([
-    Saga.callUntyped(RPCTypes.userUploadUserAvatarRpcPromise, {
-      crop,
-      filename,
-    }),
-    Saga.put(RouteTree.createNavigateUp()),
-  ])
-}
-
-function _finishRevoking() {
-  return Saga.sequentially([
+const finishRevoking = () =>
+  Saga.sequentially([
     Saga.put(TrackerGen.createGetMyProfile({ignoreCache: true})),
     Saga.put(ProfileGen.createRevokeFinish()),
     Saga.put(RouteTree.createNavigateUp()),
   ])
-}
 
 const showUserProfile = (state: TypedState, action: ProfileGen.ShowUserProfilePayload) => {
   const {username: userId} = action.payload
@@ -89,25 +76,17 @@ const openProfileOrWebsite = (
   }
 }
 
-function* _submitRevokeProof(action: ProfileGen.SubmitRevokeProofPayload): Saga.SagaGenerator<any, any> {
-  try {
-    yield* Saga.callPromise(
-      RPCTypes.revokeRevokeSigsRpcPromise,
-      {sigIDQueries: [action.payload.proofId]},
-      Constants.waitingKey
-    )
-    yield Saga.put(ProfileGen.createFinishRevoking())
-  } catch (error) {
-    logger.warn(`Error when revoking proof ${action.payload.proofId}`, error)
-    yield Saga.put(
-      ProfileGen.createRevokeFinishError({
+const submitRevokeProof = (_, action: ProfileGen.SubmitRevokeProofPayload) =>
+  RPCTypes.revokeRevokeSigsRpcPromise({sigIDQueries: [action.payload.proofId]}, Constants.waitingKey)
+    .then(() => ProfileGen.createFinishRevoking())
+    .catch(error => {
+      logger.warn(`Error when revoking proof ${action.payload.proofId}`, error)
+      return ProfileGen.createRevokeFinishError({
         error: 'There was an error revoking your proof. You can click the button to try again.',
       })
-    )
-  }
-}
+    })
 
-function _openURLIfNotNull(nullableThing, url, metaText): void {
+const openURLIfNotNull = (nullableThing, url, metaText) => {
   if (nullableThing == null) {
     logger.warn("Can't open URL because we have a null", metaText)
     return
@@ -123,7 +102,7 @@ const outputInstructionsActionLink = (
   switch (profile.platform) {
     case 'twitter':
       return Saga.callUntyped(
-        _openURLIfNotNull,
+        openURLIfNotNull,
         profile.proofText,
         `https://twitter.com/home?status=${profile.proofText || ''}`,
         'twitter url'
@@ -131,9 +110,9 @@ const outputInstructionsActionLink = (
     case 'github':
       return Saga.callUntyped(openURL, 'https://gist.github.com/')
     case 'reddit':
-      return Saga.callUntyped(_openURLIfNotNull, profile.proofText, profile.proofText, 'reddit url')
+      return Saga.callUntyped(openURLIfNotNull, profile.proofText, profile.proofText, 'reddit url')
     case 'facebook':
-      return Saga.callUntyped(_openURLIfNotNull, profile.proofText, profile.proofText, 'facebook url')
+      return Saga.callUntyped(openURLIfNotNull, profile.proofText, profile.proofText, 'facebook url')
     case 'hackernews':
       return Saga.callUntyped(openURL, `https://news.ycombinator.com/user?id=${profile.username}`)
     default:
@@ -141,19 +120,18 @@ const outputInstructionsActionLink = (
   }
 }
 
-function _backToProfile() {
-  return Saga.sequentially([
+const backToProfile = () =>
+  Saga.sequentially([
     Saga.put(TrackerGen.createGetMyProfile({})),
     Saga.put(RouteTree.createNavigateTo({parentPath: [peopleTab], path: ['profile']})),
   ])
-}
 
 function* _profileSaga() {
-  yield Saga.safeTakeEvery(ProfileGen.submitRevokeProof, _submitRevokeProof)
-  yield Saga.safeTakeEveryPure(ProfileGen.backToProfile, _backToProfile)
-  yield Saga.safeTakeEveryPure(ProfileGen.editProfile, _editProfile)
-  yield Saga.safeTakeEveryPure(ProfileGen.uploadAvatar, _uploadAvatar)
-  yield Saga.safeTakeEveryPure(ProfileGen.finishRevoking, _finishRevoking)
+  yield Saga.actionToPromise(ProfileGen.submitRevokeProof, submitRevokeProof)
+  yield Saga.actionToAction(ProfileGen.backToProfile, backToProfile)
+  yield Saga.actionToPromise(ProfileGen.editProfile, editProfile)
+  yield Saga.actionToPromise(ProfileGen.uploadAvatar, uploadAvatar)
+  yield Saga.actionToAction(ProfileGen.finishRevoking, finishRevoking)
   yield Saga.actionToAction(ProfileGen.onClickAvatar, onClickAvatar)
   yield Saga.actionToAction([ProfileGen.onClickFollowers, ProfileGen.onClickFollowing], openProfileOrWebsite)
   yield Saga.actionToAction(ProfileGen.outputInstructionsActionLink, outputInstructionsActionLink)
