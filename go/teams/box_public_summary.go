f519592a060fd18efa880a55cafb7129a1815bcb
Maxwell Krohn <themax@gmail.com>
2018-12-14 12:57:27 -0500

    bugfixes and comments from review
    - cover all the cases, when turning on checking from the server

--- go/teams/box_public_summary.go
+++ go/teams/box_public_summary.go
@@ -4,14 +4,15 @@ import (
 	"crypto/sha256"
 	"encoding/base64"
 	"encoding/hex"
-	"github.com/keybase/client/go/libkb"
 	"github.com/keybase/client/go/protocol/keybase1"
+	"github.com/keybase/go-codec/codec"
 )
 
 type boxPublicSummaryTable map[keybase1.UID]keybase1.Seqno
 
 type boxPublicSummary struct {
-	table boxPublicSummaryTable
+	table    boxPublicSummaryTable
+	encoding []byte
 }
 
 func newBoxPublicSummary(d map[keybase1.UserVersion]keybase1.PerUserKey) (*boxPublicSummary, error) {
@@ -29,7 +30,21 @@ func newBoxPublicSummary(d map[keybase1.UserVersion]keybase1.PerUserKey) (*boxPu
 }
 
 func (b *boxPublicSummary) encode() ([]byte, error) {
-	return libkb.MsgpackEncode(b.table)
+	// Just encode it once, since if ever canonical encoding
+	// stops working, it won't matter, we'll still get consitent results.
+	if b.encoding != nil {
+		return b.encoding, nil
+	}
+	var mh codec.MsgpackHandle
+	mh.WriteExt = true
+	mh.Canonical = true
+	var encoded []byte
+	err := codec.NewEncoderBytes(&encoded, &mh).Encode(b.table)
+	if err != nil {
+		return nil, err
+	}
+	b.encoding = encoded
+	return encoded, nil
 }
 
 func (b *boxPublicSummary) Hash() ([]byte, error) {
@@ -58,5 +73,5 @@ func (b *boxPublicSummary) EncodeToString() (string, error) {
 }
 
 func (b *boxPublicSummary) IsEmpty() bool {
-	return len(b.table) > 0
+	return len(b.table) == 0
 }
