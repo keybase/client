fe49c9e141410258c6543cf815cf4f36dd9e0996
Song Gao <song@gao.io>
2018-12-14 15:44:24 -0800

    feature flag chat integration

--- shared/common-adapters/markdown/kbfs-path.js
+++ shared/common-adapters/markdown/kbfs-path.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import Text from '../text'
 import * as Types from '../../constants/types/fs'
+import features from '../../util/feature-flags'
 
 export type Props = {
   path: Types.Path,
@@ -9,10 +10,14 @@ export type Props = {
   allowFontScaling?: ?boolean,
 }
 
-export default (props: Props) => {
-  return (
-    <Text type="BodyPrimaryLink" onClick={props.onClick} allowFontScaling={!!props.allowFontScaling}>
-      {Types.pathToString(props.path)}
-    </Text>
-  )
-}
+export default (features.kbfsChatIntegration
+  ? (props: Props) => (
+      <Text type="BodyPrimaryLink" onClick={props.onClick} allowFontScaling={!!props.allowFontScaling}>
+        {Types.pathToString(props.path)}
+      </Text>
+    )
+  : (props: Props) => (
+      <Text type="Body" allowFontScaling={!!props.allowFontScaling}>
+        {Types.pathToString(props.path)}
+      </Text>
+    ))
