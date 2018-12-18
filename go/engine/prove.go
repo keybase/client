f5dea173cddad9ceda64fee94ef3b02cb0be3647
Patrick Crosby <pcrosby@gmail.com>
2018-12-18 11:05:27 -0600

    Remove unrelated debugging

--- go/engine/prove.go
+++ go/engine/prove.go
@@ -110,7 +110,6 @@ func (p *Prove) promptRemoteName(m libkb.MetaContext) error {
 		})
 		if err != nil {
 			// Errors here are conditions like EOF. Return them rather than retrying.
-			m.CWarningf("prompt username err: %s", err)
 			return err
 		}
 		var remoteNameNormalized string
