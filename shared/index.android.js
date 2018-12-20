0d57e5476f5483eb9c5281e5629f7156bb1b54cd
Marco Munizaga <marco@marcopolo.io>
2018-12-20 11:07:05 -0500

    index.android setup for profiling

--- shared/index.android.js
+++ shared/index.android.js
@@ -2,6 +2,22 @@
 // React-native tooling assumes this file is here, so we just require our real entry point
 import './app/globals.native'
 import './util/android-perf-helper.js'
+require('./util/performance-polyfill.js')
+
+performance.mark('app-start')
+
+// require.Systrace.setEnabled(true)
+// let name = ''
+// require.Systrace.beginEvent = message => {
+//   name = message
+//   performance.mark(name)
+// }
+//
+// require.Systrace.endEvent = () => {
+//   performance.measure(name, name)
+// }
+//
+// console.log('is require systrace enabled?', require.Systrace.isEnabled())
 
 // Load storybook or the app
 if (__STORYBOOK__) {
@@ -12,5 +28,6 @@ if (__STORYBOOK__) {
   load()
 } else {
   const {load} = require('./app/index.native')
+  // const {load} = require('./app/presetup.native')
   load()
 }
