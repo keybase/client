6506bf5a968f866749ab4c1ded2ebc85f18d729d
Joshua Blum <jblum18@gmail.com>
2018-12-21 11:42:37 -0500

    Humanize err

--- go/libkb/errors.go
+++ go/libkb/errors.go
@@ -2068,7 +2068,7 @@ func (e ChatClientError) IsImmediateFail() (chat1.OutboxErrorType, bool) {
 type ChatStalePreviousStateError struct{}
 
 func (e ChatStalePreviousStateError) Error() string {
-	return "stale previous state error"
+	return "Unable to change chat channels. Please submit feedback"
 }
 
 //=============================================================================
