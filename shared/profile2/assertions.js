eead4af5454e917aedc15e3151454712635be172
chrisnojima <cnojima@keyba.se>
2018-12-25 20:33:05 -0500

    WIP

---
+++ shared/profile2/assertions.js
@@ -0,0 +1,13 @@
+// @flow
+import * as Types from '../constants/types/profile2'
+
+type Props = {|
+  assertion: string,
+  metas: [],
+  proofURL: string,
+  siteIcon: string,
+  siteURL: strong,
+  state: 'valid',
+|}
+
+const Assertion = (p: Props) => {}
