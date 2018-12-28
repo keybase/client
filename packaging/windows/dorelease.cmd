5759e04256f1f81d6d2fdce0c80e7d7cfc9ab2bf
Steve Sanders <steve@keyba.se>
2018-12-27 21:26:19 -0800

    some more monorepo build changes

--- packaging/windows/dorelease.cmd
+++ packaging/windows/dorelease.cmd
@@ -22,7 +22,7 @@ IF [%UpdateChannel%] == [Smoke2] goto:done_ci
 
 :: NOTE: We depend on the bot or caller to checkout client first
 :: call:checkout_keybase client, %ClientRevision% || goto:build_error || EXIT /B 1
-call:checkout_keybase kbfs, %KBFSRevision% || goto:build_error || EXIT /B 1
+:: call:checkout_keybase kbfs, %KBFSRevision% || goto:build_error || EXIT /B 1
 call:checkout_keybase go-updater, %UpdaterRevision% || goto:build_error || EXIT /B 1
 call:checkout_keybase release, %ReleaseRevision% || goto:build_error || EXIT /B 1
 
@@ -126,7 +126,7 @@ set BUILD_TAG_ENCODED=!BUILD_TAG:+=%%2B!
 ::Publish smoke updater jsons to S3
 if [%UpdateChannel%] NEQ [Smoke2] (
     echo "Non Smoke2 build"
-    %OUTPUT% "Successfully built Windows with client: %KEYBASE_VERSION%, kbfs: %KBFS_BUILD%"
+    %OUTPUT% "Successfully built Windows with client: %KEYBASE_VERSION%"
     %OUTPUT% "https://prerelease.keybase.io/windows/Keybase_%BUILD_TAG_ENCODED%.%GOARCH%.msi"
     goto :no_smokeb
 )
@@ -170,13 +170,13 @@ EXIT /B 1
 
 :check_ci 
 for /f %%i in ('git -C %GOPATH%\src\github.com\keybase\client rev-parse --short^=8 HEAD') do set clientCommit=%%i
-for /f %%i in ('git -C %GOPATH%\src\github.com\keybase\kbfs rev-parse --short^=8 HEAD') do set kbfsCommit=%%i
-echo [%clientCommit%] [%kbfsCommit%]
+:: for /f %%i in ('git -C %GOPATH%\src\github.com\keybase\kbfs rev-parse --short^=8 HEAD') do set kbfsCommit=%%i
+echo [%clientCommit%]
 :: need GITHUB_TOKEN
 pushd %GOPATH%\src\github.com\keybase\release
 go build || goto:build_error || EXIT /B 1
 release wait-ci --repo="client" --commit="%clientCommit%" --context="continuous-integration/jenkins/branch" --context="ci/circleci"  || goto:ci_error
-release wait-ci --repo="kbfs" --commit="%kbfsCommit%" --context="continuous-integration/jenkins/branch" --context="ci/circleci"  || goto:ci_error
+:: release wait-ci --repo="kbfs" --commit="%kbfsCommit%" --context="continuous-integration/jenkins/branch" --context="ci/circleci"  || goto:ci_error
 popd
 EXIT /B 0
 
