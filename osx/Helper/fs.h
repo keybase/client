b257c1e69052f163b62ac46331fe0c1f548b685a
Maxwell Krohn <themax@gmail.com>
2019-01-01 08:06:17 -0500

    formatting

--- osx/Helper/fs.h
+++ osx/Helper/fs.h
@@ -4,8 +4,8 @@
 
 @interface KBFSUtils : NSObject
 
-+(NSURL *) copyToTemporary:(NSString *)bin name:(NSString *)name fileType:(NSFileAttributeType)fileType error:(NSError **)error;
-+(BOOL) checkFile:(NSString *)linkPath isType:(NSFileAttributeType)fileType;
-+(void) checkKeybaseResource:(NSURL *)bin identifier:(NSString *)identifier error:(NSError **)error;
++(NSURL *)copyToTemporary:(NSString *)bin name:(NSString *)name fileType:(NSFileAttributeType)fileType error:(NSError **)error;
++(BOOL)checkFile:(NSString *)linkPath isType:(NSFileAttributeType)fileType;
++(void)checkKeybaseResource:(NSURL *)bin identifier:(NSString *)identifier error:(NSError **)error;
 
 @end
