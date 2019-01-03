b8aff2c5537e5d15fb05b425e2283fc19d38179c
Patrick Crosby <pcrosby@gmail.com>
2019-01-03 15:16:58 -0600

    Lookup method with null id ui

--- go/client/wallet_api_handler.go
+++ go/client/wallet_api_handler.go
@@ -1,11 +1,13 @@
 package client
 
 import (
+	"encoding/hex"
 	"errors"
 	"io"
 
 	"github.com/keybase/client/go/libkb"
 	"github.com/keybase/client/go/protocol/stellar1"
+	"github.com/keybase/go-framed-msgpack-rpc/rpc"
 	"github.com/stellar/go/strkey"
 	"golang.org/x/net/context"
 )
@@ -15,6 +17,9 @@ var ErrInvalidAccountID = errors.New("invalid stellar account ID")
 
 // ErrInvalidTxID is for invalid transaction IDs.
 var ErrInvalidTxID = errors.New("invalid stellar transaction ID")
+
+// ErrNameMissing is for missing name options.
+var ErrNameMissing = errors.New("'name' option is required")
 
 // walletAPIHandler is a type that can handle all the json api
 // methods for the wallet API.
@@ -44,6 +49,7 @@ const (
 	balancesMethod = "balances"
 	detailsMethod  = "details"
 	historyMethod  = "history"
+	lookupMethod   = "lookup"
 )
 
 // validWalletMethodsV1 is a map of the valid V1 methods for quick lookup.
@@ -51,6 +57,7 @@ var validWalletMethodsV1 = map[string]bool{
 	balancesMethod: true,
 	detailsMethod:  true,
 	historyMethod:  true,
+	lookupMethod:   true,
 }
 
 // handleV1 processes Call for version 1 of the wallet JSON API.
@@ -72,6 +79,8 @@ func (w *walletAPIHandler) handleV1(ctx context.Context, c Call, wr io.Writer) e
 		return w.details(ctx, c, wr)
 	case historyMethod:
 		return w.history(ctx, c, wr)
+	case lookupMethod:
+		return w.lookup(ctx, c, wr)
 	default:
 		return ErrInvalidMethod{name: c.Method, version: 1}
 	}
@@ -92,11 +101,11 @@ func (w *walletAPIHandler) details(ctx context.Context, c Call, wr io.Writer) er
 	if err := unmarshalOptions(c, &opts); err != nil {
 		return w.encodeErr(c, err, wr)
 	}
-	accounts, err := w.cli.WalletGetAccountsCLILocal(ctx)
+	detail, err := w.cli.PaymentDetailCLILocal(ctx, opts.TxID)
 	if err != nil {
-		return encodeErr(c, err, wr, w.indent)
+		return w.encodeErr(c, err, wr)
 	}
-	return w.encodeResult(c, accounts, wr)
+	return w.encodeResult(c, detail, wr)
 }
 
 // history outputs recent payment history for the specified account ID.
@@ -111,6 +120,27 @@ func (w *walletAPIHandler) history(ctx context.Context, c Call, wr io.Writer) er
 		return w.encodeErr(c, err, wr)
 	}
 	return w.encodeResult(c, payments, wr)
+}
+
+// lookup outputs an account ID for a keybase username or federation address.
+func (w *walletAPIHandler) lookup(ctx context.Context, c Call, wr io.Writer) error {
+	var opts nameOptions
+	if err := unmarshalOptions(c, &opts); err != nil {
+		return w.encodeErr(c, err, wr)
+	}
+
+	protocols := []rpc.Protocol{
+		NewNullIdentifyUIProtocol(),
+	}
+	if err := RegisterProtocolsWithContext(protocols, w.G()); err != nil {
+		return err
+	}
+
+	result, err := w.cli.LookupCLILocal(ctx, opts.Name)
+	if err != nil {
+		return w.encodeErr(c, err, wr)
+	}
+	return w.encodeResult(c, result, wr)
 }
 
 // encodeResult JSON encodes a successful result to the wr writer.
@@ -148,9 +178,24 @@ type txIDOptions struct {
 
 // Check makes sure that TxID exists and is valid.
 func (c *txIDOptions) Check() error {
-	_, err := strkey.Decode(strkey.VersionByteHashTx, c.TxID)
-	if err != nil {
+	if len(c.TxID) != 64 {
 		return ErrInvalidTxID
+	}
+	if _, err := hex.DecodeString(c.TxID); err != nil {
+		return ErrInvalidTxID
+	}
+	return nil
+}
+
+// nameOptions is for a command where a name is required.
+type nameOptions struct {
+	Name string `json:"name"`
+}
+
+// Check makes sure that name exists.
+func (c *nameOptions) Check() error {
+	if c.Name == "" {
+		return ErrNameMissing
 	}
 	return nil
 }
