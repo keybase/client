fd2638f2ee73542d0f855716b8db3a842af58256
Patrick Crosby <pcrosby@gmail.com>
2019-01-03 15:52:47 -0600

    Get/Set inflation working

--- go/client/wallet_api_handler.go
+++ go/client/wallet_api_handler.go
@@ -21,6 +21,9 @@ var ErrInvalidTxID = errors.New("invalid stellar transaction ID")
 // ErrNameMissing is for missing name options.
 var ErrNameMissing = errors.New("'name' option is required")
 
+// ErrDestinationMissing is for missing destination options.
+var ErrDestinationMissing = errors.New("'destination' option is required")
+
 // walletAPIHandler is a type that can handle all the json api
 // methods for the wallet API.
 type walletAPIHandler struct {
@@ -46,18 +49,26 @@ func (w *walletAPIHandler) handle(ctx context.Context, c Call, wr io.Writer) err
 
 // list of all supported methods:
 const (
-	balancesMethod = "balances"
-	detailsMethod  = "details"
-	historyMethod  = "history"
-	lookupMethod   = "lookup"
+	balancesMethod     = "balances"
+	cancelMethod       = "cancel"
+	detailsMethod      = "details"
+	getInflationMethod = "get-inflation"
+	historyMethod      = "history"
+	lookupMethod       = "lookup"
+	sendMethod         = "send"
+	setInflationMethod = "set-inflation"
 )
 
 // validWalletMethodsV1 is a map of the valid V1 methods for quick lookup.
 var validWalletMethodsV1 = map[string]bool{
-	balancesMethod: true,
-	detailsMethod:  true,
-	historyMethod:  true,
-	lookupMethod:   true,
+	balancesMethod:     true,
+	cancelMethod:       true,
+	detailsMethod:      true,
+	getInflationMethod: true,
+	historyMethod:      true,
+	lookupMethod:       true,
+	sendMethod:         true,
+	setInflationMethod: true,
 }
 
 // handleV1 processes Call for version 1 of the wallet JSON API.
@@ -75,12 +86,20 @@ func (w *walletAPIHandler) handleV1(ctx context.Context, c Call, wr io.Writer) e
 	switch c.Method {
 	case balancesMethod:
 		return w.balances(ctx, c, wr)
+	case cancelMethod:
+		return w.cancelPayment(ctx, c, wr)
 	case detailsMethod:
 		return w.details(ctx, c, wr)
+	case getInflationMethod:
+		return w.getInflation(ctx, c, wr)
 	case historyMethod:
 		return w.history(ctx, c, wr)
 	case lookupMethod:
 		return w.lookup(ctx, c, wr)
+	case sendMethod:
+		return w.send(ctx, c, wr)
+	case setInflationMethod:
+		return w.setInflation(ctx, c, wr)
 	default:
 		return ErrInvalidMethod{name: c.Method, version: 1}
 	}
@@ -90,7 +109,16 @@ func (w *walletAPIHandler) handleV1(ctx context.Context, c Call, wr io.Writer) e
 func (w *walletAPIHandler) balances(ctx context.Context, c Call, wr io.Writer) error {
 	accounts, err := w.cli.WalletGetAccountsCLILocal(ctx)
 	if err != nil {
-		return encodeErr(c, err, wr, w.indent)
+		return w.encodeErr(c, err, wr)
+	}
+	return w.encodeResult(c, accounts, wr)
+}
+
+// cancelPayment cancels a pending relay payment and yanks back the funds.
+func (w *walletAPIHandler) cancelPayment(ctx context.Context, c Call, wr io.Writer) error {
+	accounts, err := w.cli.WalletGetAccountsCLILocal(ctx)
+	if err != nil {
+		return w.encodeErr(c, err, wr)
 	}
 	return w.encodeResult(c, accounts, wr)
 }
@@ -106,6 +134,21 @@ func (w *walletAPIHandler) details(ctx context.Context, c Call, wr io.Writer) er
 		return w.encodeErr(c, err, wr)
 	}
 	return w.encodeResult(c, detail, wr)
+}
+
+// getInflation gets the inflation destination for an account.
+func (w *walletAPIHandler) getInflation(ctx context.Context, c Call, wr io.Writer) error {
+	var opts accountIDOptions
+	if err := unmarshalOptions(c, &opts); err != nil {
+		return w.encodeErr(c, err, wr)
+	}
+
+	inflation, err := w.getInflationLocal(ctx, opts.Convert())
+	if err != nil {
+		return w.encodeErr(c, err, wr)
+	}
+
+	return w.encodeResult(c, inflation, wr)
 }
 
 // history outputs recent payment history for the specified account ID.
@@ -143,6 +186,39 @@ func (w *walletAPIHandler) lookup(ctx context.Context, c Call, wr io.Writer) err
 	return w.encodeResult(c, result, wr)
 }
 
+// send sends XLM to an account.
+func (w *walletAPIHandler) send(ctx context.Context, c Call, wr io.Writer) error {
+	accounts, err := w.cli.WalletGetAccountsCLILocal(ctx)
+	if err != nil {
+		return w.encodeErr(c, err, wr)
+	}
+	return w.encodeResult(c, accounts, wr)
+}
+
+// setInflation sets the inflation destination for an account.
+func (w *walletAPIHandler) setInflation(ctx context.Context, c Call, wr io.Writer) error {
+	var opts inflationOptions
+	if err := unmarshalOptions(c, &opts); err != nil {
+		return w.encodeErr(c, err, wr)
+	}
+
+	arg := stellar1.SetInflationDestinationLocalArg{
+		AccountID:   opts.AccountIDConvert(),
+		Destination: opts.DestinationConvert(),
+	}
+	if err := w.cli.SetInflationDestinationLocal(ctx, arg); err != nil {
+		return w.encodeErr(c, err, wr)
+	}
+
+	// get it for display
+	inflation, err := w.getInflationLocal(ctx, opts.AccountIDConvert())
+	if err != nil {
+		return w.encodeErr(c, err, wr)
+	}
+
+	return w.encodeResult(c, inflation, wr)
+}
+
 // encodeResult JSON encodes a successful result to the wr writer.
 func (w *walletAPIHandler) encodeResult(call Call, result interface{}, wr io.Writer) error {
 	return encodeResult(call, result, wr, w.indent)
@@ -151,6 +227,22 @@ func (w *walletAPIHandler) encodeResult(call Call, result interface{}, wr io.Wri
 // encodeErr JSON encodes an error.
 func (w *walletAPIHandler) encodeErr(call Call, err error, wr io.Writer) error {
 	return encodeErr(call, err, wr, w.indent)
+}
+
+func (w *walletAPIHandler) getInflationLocal(ctx context.Context, accountID stellar1.AccountID) (stellar1.InflationDestinationResultLocal, error) {
+	arg := stellar1.GetInflationDestinationLocalArg{
+		AccountID: accountID,
+	}
+	inflation, err := w.cli.GetInflationDestinationLocal(ctx, arg)
+	if err != nil {
+		return stellar1.InflationDestinationResultLocal{}, err
+	}
+	if inflation.Destination == nil {
+		var empty stellar1.AccountID
+		inflation.Destination = &empty
+		inflation.Comment = "no inflation destination set"
+	}
+	return inflation, nil
 }
 
 // accountIDOptions is for a command where an account-id is required.
@@ -198,4 +290,38 @@ func (c *nameOptions) Check() error {
 		return ErrNameMissing
 	}
 	return nil
+}
+
+// inflationOptions are the options for the set-inflation command.
+type inflationOptions struct {
+	AccountID   string `json:"account-id"`
+	Destination string `json:"destination"`
+}
+
+// Check makes sure that AccountID is valid and that Destination isn't empty.
+func (c *inflationOptions) Check() error {
+	_, err := strkey.Decode(strkey.VersionByteAccountID, c.AccountID)
+	if err != nil {
+		return ErrInvalidAccountID
+	}
+	if c.Destination == "" {
+		return ErrDestinationMissing
+	}
+
+	return nil
+}
+
+func (c *inflationOptions) AccountIDConvert() stellar1.AccountID {
+	return stellar1.AccountID(c.AccountID)
+}
+
+func (c *inflationOptions) DestinationConvert() stellar1.InflationDestination {
+	switch c.Destination {
+	case "self":
+		return stellar1.NewInflationDestinationWithSelf()
+	case "lumenaut":
+		return stellar1.NewInflationDestinationWithLumenaut()
+	default:
+		return stellar1.NewInflationDestinationWithAccountid(stellar1.AccountID(c.Destination))
+	}
 }
