2a1dc232bf9c8b38d12bf1373845671fa9b27aa5
Michał Zochniak <michal@zapu.net>
2019-01-04 09:31:39 +0100

    Add sesscheck in TestSignupRandomPWUser

--- go/service/random_pw_test.go
+++ go/service/random_pw_test.go
@@ -11,25 +11,34 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func sessCheck(t *testing.T, g *libkb.GlobalContext) (err error) {
+	arg := libkb.NewRetryAPIArg("sesscheck")
+	arg.SessionType = libkb.APISessionTypeREQUIRED
+	_, err = g.API.Get(arg)
+	t.Logf("sesscheck returned: %q", err)
+	return err
+}
+
 func TestSignupRandomPWUser(t *testing.T) {
 	tc := libkb.SetupTest(t, "randompw", 3)
 	defer tc.Cleanup()
 
-	user, err := kbtest.CreateAndSignupFakeUserRandomPW("rpw", tc.G)
+	_, err := kbtest.CreateAndSignupFakeUserRandomPW("rpw", tc.G)
 	require.NoError(t, err)
-	_ = user
-
-	handler := NewLoginHandler(nil, tc.G)
-	err = handler.Logout(context.Background(), keybase1.LogoutArg{})
-	require.Error(t, err)
-	require.Contains(t, err.Error(), "Cannot logout")
-	require.Contains(t, err.Error(), "Set a password first")
 
 	userHandler := NewUserHandler(nil, tc.G, nil)
 	ret, err := userHandler.LoadHasRandomPw(context.Background(), 0)
 	require.NoError(t, err)
 	require.True(t, ret)
 
+	handler := NewLoginHandler(nil, tc.G)
+	err = handler.Logout(context.Background(), keybase1.LogoutArg{})
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "Cannot logout")
+	require.Contains(t, err.Error(), "Set a password first")
+	require.NoError(t, sessCheck(t, tc.G), "expecting to still have a valid session and no error in sesscheck")
+
 	err = handler.Logout(context.Background(), keybase1.LogoutArg{Force: true})
 	require.NoError(t, err)
+	require.Error(t, sessCheck(t, tc.G), "expecting to be logged out and error in sesscheck")
 }
