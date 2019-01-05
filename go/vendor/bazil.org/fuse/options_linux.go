7f5bd7fd409ea11c97fe95f4ca709d31d5fa6e05
John Zila <john@jzila.com>
2019-01-05 12:11:33 -0800

    vendor: whack-a-mole 2 for broken vendor builds. bazil.org/fuse/fs was the wrong origin.

--- go/vendor/bazil.org/fuse/options_linux.go
+++ go/vendor/bazil.org/fuse/options_linux.go
@@ -23,3 +23,11 @@ func noAppleDouble(conf *mountConfig) error {
 func exclCreate(conf *mountConfig) error {
 	return nil
 }
+
+func noBrowse(conf *mountConfig) error {
+	return nil
+}
+
+func noLocalCaches(conf *mountConfig) error {
+	return nil
+}
