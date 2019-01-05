57987e635e83438b1ab84728ace596f3632e063a
chrisnojima <cnojima@keyba.se>
2019-01-04 23:33:53 -0500

    WIP

---
+++ shared/profile/user/friend/container.js
@@ -0,0 +1,30 @@
+// @flow
+import * as Container from '../../../util/container'
+import Friend from '.'
+
+type OwnProps = {|
+  username: string,
+|}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    followThem: state.config.following.has(ownProps.username),
+    followsYou: state.config.followers.has(ownProps.username),
+    fullname: state.users.infoMap.getIn([ownProps.username, 'fullname'], ''),
+    username: ownProps.username,
+  }
+}
+const mapDispatchToProps = (dispatch, ownProps) => ({})
+const mergeProps = (stateProps, dispatchProps, ownProps) => ({
+  followThem: stateProps.followThem,
+  followsYou: stateProps.followsYou,
+  fullname: stateProps.fullname,
+  username: stateProps.username,
+})
+
+export default Container.namedConnect<OwnProps, _, _, _, _>(
+  mapStateToProps,
+  mapDispatchToProps,
+  mergeProps,
+  'Friend'
+)(Friend)
---
+++ shared/profile/user/friend/index.js
@@ -0,0 +1,34 @@
+// @flow
+import * as React from 'react'
+import * as Kb from '../../../common-adapters'
+import * as Styles from '../../../styles'
+
+type Props = {|
+  followThem: boolean,
+  followsYou: boolean,
+  fullname: string,
+  username: string,
+|}
+
+const Friend = (p: Props) => (
+  <Kb.Box2 direction="vertical" style={styles.container} centerChildren={true}>
+    <Kb.Avatar size={64} username={p.username} style={styles.avatar} />
+    <Kb.ConnectedUsernames usernames={[p.username]} />
+    <Kb.Text type="BodySmall" lineClamp={1}>
+      {p.fullname}
+    </Kb.Text>
+  </Kb.Box2>
+)
+
+const styles = Styles.styleSheetCreate({
+  avatar: {
+    marginBottom: Styles.globalMargins.xxtiny,
+  },
+  container: {
+    flexShrink: 0,
+    height: 106,
+    width: 112,
+  },
+})
+
+export default Friend
