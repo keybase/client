parallelcointeam/kbfs

folder_branch_ops: mark and sweep for partially-synced TLFs
Partially-synced TLFs need to clean up the sync cache in the
background, in the cases where a) some previously-synced path is now
unsynced manually by the user, or b) the user moves some
previously-synced directory out of a synced path.  In the latter case,
we don't have an explicit config action from the user, so it needs to
be done periodically in the background.

This adds a new background goroutine to FBO for partially-synced
folders, that triggers a mark-and-sweep operation either when a path
is removed from the sync config, or on a periodic timer (once an
hour).  It iterates through each synced path and marks each block that
should still be synced with a tag.  After that, it removes all blocks
from that TLF that aren't marked with that tag from the sync cache.

This process is stopped whenever the latest merged MD is updated.

Issue: KBFS-3644

libkbfs/folder_block_ops.go
@@ -3659,6 +3659,49 @@ func (fbo *folderBlockOps) GetInvalidationChangesForAll(
 	return fbo.getInvalidationChangesForNodes(ctx, lState, childNodes)
 }
 
+// MarkNode marks all the blocks in the node's block tree with the
+// given tag.
+func (fbo *folderBlockOps) MarkNode(
+	ctx context.Context, lState *lockState, node Node, kmd KeyMetadata,
+	tag string, cacheType DiskBlockCacheType) error {
+	dbc := fbo.config.DiskBlockCache()
+	if dbc == nil {
+		return nil
+	}
+
+	fbo.blockLock.Lock(lState)
+	defer fbo.blockLock.Unlock(lState)
+
+	chargedTo, err := fbo.getChargedToLocked(ctx, lState, kmd)
+	if err != nil {
+		return err
+	}
+	p := fbo.nodeCache.PathFromNode(node)
+	err = dbc.Mark(ctx, p.tailPointer().ID, tag, cacheType)
+	if err != nil {
+		return err
+	}
+	var infos []BlockInfo
+	if node.EntryType() == Dir {
+		dd := fbo.newDirDataLocked(lState, p, chargedTo, kmd)
+		infos, err = dd.getIndirectDirBlockInfos(ctx)
+	} else {
+		fd := fbo.newFileData(lState, p, chargedTo, kmd)
+		infos, err = fd.getIndirectFileBlockInfos(ctx)
+	}
+	if err != nil {
+		return err
+	}
+
+	for _, info := range infos {
+		err = dbc.Mark(ctx, info.BlockPointer.ID, tag, cacheType)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 type chainsPathPopulator interface {
 	populateChainPaths(context.Context, logger.Logger, *crChains, bool) error
 }

keybase/client

go/kbfs/libkbfs/folder_block_ops.go
@@ -3653,6 +3653,49 @@ func (fbo *folderBlockOps) GetInvalidationChangesForAll(
 	return fbo.getInvalidationChangesForNodes(ctx, lState, childNodes)
 }
 
+// MarkNode marks all the blocks in the node's block tree with the
+// given tag.
+func (fbo *folderBlockOps) MarkNode(
+	ctx context.Context, lState *lockState, node Node, kmd KeyMetadata,
+	tag string, cacheType DiskBlockCacheType) error {
+	dbc := fbo.config.DiskBlockCache()
+	if dbc == nil {
+		return nil
+	}
+
+	fbo.blockLock.RLock(lState)
+	defer fbo.blockLock.RUnlock(lState)
+
+	chargedTo, err := fbo.getChargedToLocked(ctx, lState, kmd)
+	if err != nil {
+		return err
+	}
+	p := fbo.nodeCache.PathFromNode(node)
+	err = dbc.Mark(ctx, p.tailPointer().ID, tag, cacheType)
+	if err != nil {
+		return err
+	}
+	var infos []BlockInfo
+	if node.EntryType() == Dir {
+		dd := fbo.newDirDataLocked(lState, p, chargedTo, kmd)
+		infos, err = dd.getIndirectDirBlockInfos(ctx)
+	} else {
+		fd := fbo.newFileData(lState, p, chargedTo, kmd)
+		infos, err = fd.getIndirectFileBlockInfos(ctx)
+	}
+	if err != nil {
+		return err
+	}
+
+	for _, info := range infos {
+		err = dbc.Mark(ctx, info.BlockPointer.ID, tag, cacheType)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 type chainsPathPopulator interface {
 	populateChainPaths(context.Context, logger.Logger, *crChains, bool) error
 }