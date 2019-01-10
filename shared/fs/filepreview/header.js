558c3ba79d37ada0bbdc5ce24bf00d53e0ad41d0
Song Gao <song@gao.io>
2019-01-10 14:52:49 -0800

    KBFS-3697: fix long filename cut-off in filepreview screen

--- shared/fs/filepreview/header.js
+++ shared/fs/filepreview/header.js
@@ -1,8 +1,8 @@
 // @flow
 import * as React from 'react'
 import * as Types from '../../constants/types/fs'
-import {globalStyles, globalMargins, platformStyles} from '../../styles'
-import {Box, Text, BackButton} from '../../common-adapters'
+import * as Styles from '../../styles'
+import * as Kb from '../../common-adapters'
 import * as Kbfs from '../common'
 import {isMobile} from '../../constants/platform'
 
@@ -14,39 +14,43 @@ type HeaderProps = {
 }
 
 const Header = (props: HeaderProps) => (
-  <Box style={globalStyles.flexBoxRow}>
-    <BackButton key="back" onClick={props.onBack} style={stylesClose} />
-    <Box style={filePreviewHeaderStyle}>
-      <Text type="BodyBig" selectable={true}>
+  <Kb.Box2 direction="horizontal" fullWidth={true} style={styles.container} gap="xtiny">
+    <Kb.BackButton key="back" onClick={props.onBack} style={styles.close} />
+    <Kb.Box2 direction="vertical" centerChildren={true} style={styles.filePreviewHeader}>
+      <Kb.Text type="BodyBig" selectable={true} style={styles.filename}>
         {props.name}
-      </Text>
+      </Kb.Text>
       {!isMobile && <Kbfs.PathItemInfo path={props.path} startWithLastModified={true} />}
-    </Box>
-    <Box style={stylesHeaderIcons}>
+    </Kb.Box2>
+    <Kb.Box style={styles.headerIcons}>
       <Kbfs.OpenInSystemFileManager path={props.path} />
       <Kbfs.SendInAppAction path={props.path} sendIconClassName="" />
       <Kbfs.PathItemAction path={props.path} fontSize={16} />
-    </Box>
-  </Box>
+    </Kb.Box>
+  </Kb.Box2>
 )
 
-const stylesClose = platformStyles({
-  isElectron: {marginLeft: globalMargins.tiny},
+const styles = Styles.styleSheetCreate({
+  close: Styles.platformStyles({
+    isElectron: {
+      marginLeft: Styles.globalMargins.tiny,
+    },
+  }),
+  container: {
+    minHeight: 48,
+  },
+  filePreviewHeader: {
+    flex: 1,
+    flexShrink: 1,
+  },
+  filename: {
+    textAlign: 'center',
+  },
+  headerIcons: {
+    ...Styles.globalStyles.flexBoxRow,
+    alignItems: 'center',
+    marginRight: Styles.globalMargins.small,
+  },
 })
-
-const filePreviewHeaderStyle = {
-  ...globalStyles.flexBoxColumn,
-  ...globalStyles.flexGrow,
-  alignItems: 'center',
-  borderBottomWidth: 0,
-  height: 48,
-  justifyContent: 'center',
-}
-
-const stylesHeaderIcons = {
-  ...globalStyles.flexBoxRow,
-  alignItems: 'center',
-  marginRight: globalMargins.small,
-}
 
 export default Header
