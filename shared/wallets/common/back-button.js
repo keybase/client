5706b65d2aa9d092f46dd2bca1bb83cd38d59479
chrisnojima <chrisnojima@gmail.com>
2019-01-10 17:51:07 -0500

    Send asset chooser (#15399)

--- shared/wallets/common/back-button.js
+++ shared/wallets/common/back-button.js
@@ -5,38 +5,40 @@ import * as Styles from '../../styles'
 
 type Props = {
   onBack: () => void,
+  isOnWhiteBackground?: boolean,
 }
 
 const WalletBackButton = (props: Props) =>
   Styles.isMobile ? (
-    <Kb.Text onClick={props.onBack} style={styles.backButton} textStyle={styles.backButtonText} type="Header">
+    <Kb.Text
+      onClick={props.onBack}
+      style={Styles.collapseStyles([styles.backButton, !props.isOnWhiteBackground && styles.whiteText])}
+      type="BodyPrimaryLink"
+    >
       Cancel
     </Kb.Text>
   ) : (
     <Kb.BackButton
       onClick={props.onBack}
       style={styles.backButton}
-      iconColor={Styles.globalColors.white}
-      textStyle={styles.backButtonText}
+      iconColor={props.isOnWhiteBackground ? undefined : Styles.globalColors.white}
+      textStyle={props.isOnWhiteBackground ? undefined : styles.whiteText}
     />
   )
 
 const styles = Styles.styleSheetCreate({
   backButton: Styles.platformStyles({
-    common: {
-      position: 'absolute',
-    },
+    common: {position: 'absolute'},
     isElectron: {
       left: Styles.globalMargins.small,
       top: Styles.globalMargins.small,
     },
     isMobile: {
-      color: Styles.globalColors.white,
       left: 12,
       top: 12,
     },
   }),
-  backButtonText: {
+  whiteText: {
     color: Styles.globalColors.white,
   },
 })
