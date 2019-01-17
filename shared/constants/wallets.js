cdc5377933ba38bbdef1a8c8b55d922014de3644
Frederick Akalin <akalin@keyba.se>
2019-01-16 23:53:47 -0800

    Revert dry

--- shared/constants/wallets.js
+++ shared/constants/wallets.js
@@ -449,32 +449,31 @@ export const updatePaymentsReceived = (
   )
 }
 
-const p = 'wallets:'
-export const acceptDisclaimerWaitingKey = `${p}acceptDisclaimer`
-export const changeAccountNameWaitingKey = `${p}changeAccountName`
-export const createNewAccountWaitingKey = `${p}createNewAccount`
-export const changeDisplayCurrencyWaitingKey = `${p}changeDisplayCurrency`
-export const getDisplayCurrencyWaitingKey = (id: Types.AccountID) => `${p}getDisplayCurrency:${id}`
-export const linkExistingWaitingKey = `${p}linkExisting`
-export const buildPaymentWaitingKey = `${p}buildPayment`
-export const sendPaymentWaitingKey = `${p}stellarSend`
-export const requestPaymentWaitingKey = `${p}requestPayment`
-export const setAccountAsDefaultWaitingKey = `${p}setAccountAsDefault`
-export const deleteAccountWaitingKey = `${p}deleteAccount`
+export const acceptDisclaimerWaitingKey = 'wallets:acceptDisclaimer'
+export const changeAccountNameWaitingKey = 'wallets:changeAccountName'
+export const createNewAccountWaitingKey = 'wallets:createNewAccount'
+export const changeDisplayCurrencyWaitingKey = 'wallets:changeDisplayCurrency'
+export const getDisplayCurrencyWaitingKey = (id: Types.AccountID) => `wallets:getDisplayCurrency:${id}`
+export const linkExistingWaitingKey = 'wallets:linkExisting'
+export const buildPaymentWaitingKey = 'wallets:buildPayment'
+export const sendPaymentWaitingKey = 'wallets:stellarSend'
+export const requestPaymentWaitingKey = 'wallets:requestPayment'
+export const setAccountAsDefaultWaitingKey = 'wallets:setAccountAsDefault'
+export const deleteAccountWaitingKey = 'wallets:deleteAccount'
 export const searchKey = 'walletSearch'
-export const loadAccountWaitingKey = (id: Types.AccountID) => `${p}loadAccount:${id}`
-export const loadAccountsWaitingKey = `${p}loadAccounts`
+export const loadAccountWaitingKey = (id: Types.AccountID) => `wallets:loadAccount:${id}`
+export const loadAccountsWaitingKey = 'wallets:loadAccounts'
 export const cancelPaymentWaitingKey = (id: Types.PaymentID) =>
-  `${p}cancelPayment:${Types.paymentIDToString(id)}`
-export const validateAccountNameWaitingKey = `${p}validateAccountName`
-export const validateSecretKeyWaitingKey = `${p}validateSecretKey`
+  `wallets:cancelPayment:${Types.paymentIDToString(id)}`
+export const validateAccountNameWaitingKey = 'wallets:validateAccountName'
+export const validateSecretKeyWaitingKey = 'wallets:validateSecretKey'
 export const getRequestDetailsWaitingKey = (id: Types.PaymentID) =>
-  `${p}requestDetailsWaitingKey:${Types.paymentIDToString(id)}`
-export const inflationDestinationWaitingKey = `${p}inflationDestination`
+  `wallets:requestDetailsWaitingKey:${Types.paymentIDToString(id)}`
+export const inflationDestinationWaitingKey = 'wallets:inflationDestination'
 export const setAccountMobileOnlyWaitingKey = (id: Types.AccountID) =>
-  `${p}setAccountMobileOnly:${Types.accountIDToString(id)}`
+  `wallets:setAccountMobileOnly:${Types.accountIDToString(id)}`
 
-export const isWaitingKey = (key: string) => key.startsWith(p)
+export const isWaitingKey = (key: string) => key.startsWith('wallets:')
 
 export const getAccountIDs = (state: TypedState) => state.wallets.accountMap.keySeq().toList()
 
