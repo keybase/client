d473453f35de9c5f979cf5af1f8660dde2bf30d4
Mike Maxim <mike@keyba.se>
2019-01-17 21:09:35 -0500

    more

--- go/chat/utils/urlparse_test.go
+++ go/chat/utils/urlparse_test.go
@@ -18,6 +18,18 @@ func TestDecorateURLs(t *testing.T) {
 			body:   "you should checkout google.com",
 			result: "you should checkout $>kb${\"typ\":1,\"url\":{\"text\":\"google.com\",\"url\":\"https://google.com\"}}$<kb$",
 		},
+		urlDecorateTest{
+			body:   "Jan 17 20:37:58 ws-0.localdomain app.iced",
+			result: "Jan 17 20:37:58 ws-0.localdomain app.iced",
+		},
+		urlDecorateTest{
+			body:   "hello jack-testing.keybase.io",
+			result: "hello $>kb${\"typ\":1,\"url\":{\"text\":\"jack-testing.keybase.io\",\"url\":\"https://jack-testing.keybase.io\"}}$<kb$",
+		},
+		urlDecorateTest{
+			body:   "parens wikipedia.org/wiki/John_McLean_(Illinois_politician).",
+			result: "parens $>kb${\"typ\":1,\"url\":{\"text\":\"wikipedia.org/wiki/John_McLean_(Illinois_politician)\",\"url\":\"https://wikipedia.org/wiki/John_McLean_(Illinois_politician)\"}}$<kb$.",
+		},
 	}
 	for _, c := range cases {
 		res := DecorateWithURLs(context.TODO(), c.body)
