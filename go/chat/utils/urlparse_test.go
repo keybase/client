e0ea0bea59d0ef785d21fd867599748c3cb0666f
Mike Maxim <mike@keyba.se>
2019-01-17 21:02:42 -0500

    test

--- go/chat/utils/urlparse_test.go
+++ go/chat/utils/urlparse_test.go
@@ -1,1 +1,26 @@
 package utils
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+type urlDecorateTest struct {
+	body   string
+	result string
+}
+
+func TestDecorateURLs(t *testing.T) {
+	cases := []urlDecorateTest{
+		urlDecorateTest{
+			body:   "you should checkout google.com",
+			result: "you should checkout $>kb${\"typ\":1,\"url\":{\"text\":\"google.com\",\"url\":\"https://google.com\"}}$<kb$",
+		},
+	}
+	for _, c := range cases {
+		res := DecorateWithURLs(context.TODO(), c.body)
+		require.Equal(t, c.result, res)
+	}
+}
