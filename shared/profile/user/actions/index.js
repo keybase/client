6658518996e163fdeeb779c2cfb5bc00ff712bd2
chrisnojima <chrisnojima@gmail.com>
2019-01-28 14:48:54 -0500

    ident 3 pixel pushing (#15719)
    * small style fixes
    
    * more pixel pushing

--- shared/profile/user/actions/index.js
+++ shared/profile/user/actions/index.js
@@ -129,7 +129,6 @@ const DropdownButton = Kb.OverlayParentHOC(p => {
       <Kb.FloatingMenu
         closeOnSelect={true}
         attachTo={p.getAttachmentRef}
-        containerStyle={styles.floatingMenu}
         items={items}
         onHidden={p.toggleShowingMenu}
         position="bottom right"
@@ -139,6 +138,8 @@ const DropdownButton = Kb.OverlayParentHOC(p => {
   )
 })
 
-const styles = Styles.styleSheetCreate({})
+const styles = Styles.styleSheetCreate({
+  chatIcon: {marginRight: Styles.globalMargins.tiny},
+})
 
 export default Actions
