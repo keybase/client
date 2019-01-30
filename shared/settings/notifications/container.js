06985839e6ccf342954b76b43cfc3191500ad435
Fred Akalin <akalin@keyba.se>
2019-01-30 13:33:22 -0800

    Add Reloadable to notifications tab on mobile (#15692)
    It's on for desktop, too, but hidden behind the Offline screen for Settings.

--- shared/settings/notifications/container.js
+++ shared/settings/notifications/container.js
@@ -1,7 +1,10 @@
 // @flow
+import * as React from 'react'
+import {Reloadable} from '../../common-adapters'
 import * as SettingsGen from '../../actions/settings-gen'
+import {refreshNotificationsWaitingKey} from '../../constants/settings'
 import * as Types from '../../constants/types/settings'
-import {connect, lifecycle, compose} from '../../util/container'
+import {connect, isMobile} from '../../util/container'
 import Notifications from './index'
 import * as RouteTreeGen from '../../actions/route-tree-gen'
 import * as ConfigGen from '../../actions/config-gen'
@@ -24,15 +27,21 @@ const mapDispatchToProps = (dispatch: any, ownProps: {}) => ({
     dispatch(SettingsGen.createNotificationsToggle({group})),
   title: 'Notifications',
 })
-export default compose(
-  connect<OwnProps, _, _, _, _>(
-    mapStateToProps,
-    mapDispatchToProps,
-    (s, d, o) => ({...o, ...s, ...d})
-  ),
-  lifecycle({
-    componentDidMount() {
-      this.props.onRefresh()
-    },
-  })
-)(Notifications)
+
+const ReloadableNotifications = props => (
+  <Reloadable
+    onBack={isMobile ? props.onBack : undefined}
+    waitingKeys={refreshNotificationsWaitingKey}
+    onReload={props.onRefresh}
+    reloadOnMount={true}
+    title={props.title}
+  >
+    <Notifications {...props} />
+  </Reloadable>
+)
+
+export default connect<OwnProps, _, _, _, _>(
+  mapStateToProps,
+  mapDispatchToProps,
+  (s, d, o) => ({...o, ...s, ...d})
+)(ReloadableNotifications)
