11d273a67570d15ddf0d61002a381c4c9448f6ec
Joshua Blum <jblum18@gmail.com>
2019-01-31 13:53:31 -0500

    Use chat context in inbox flush [CORE-10093] (#15809)
    * Use chat context in inbox flush
    
    * log

--- go/chat/inboxsource.go
+++ go/chat/inboxsource.go
@@ -494,7 +494,8 @@ func (s *HybridInboxSource) Stop(ctx context.Context) chan struct{} {
 }
 
 func (s *HybridInboxSource) inboxFlushLoop(uid gregor1.UID, stopCh chan struct{}) {
-	ctx := context.Background()
+	ctx := Context(context.Background(), s.G(),
+		keybase1.TLFIdentifyBehavior_CHAT_SKIP, nil, nil)
 	appState := s.G().AppState.State()
 	doFlush := func() {
 		s.createInbox().Flush(ctx, uid)
