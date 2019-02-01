8026361112447218cff8c50b904bcca69209ee6a
Adam Spooner <adamjspooner@gmail.com>
2019-02-01 12:12:39 -0500

    Finesse icon and text sizes. (#15827)

--- shared/team-building/user-bubble.js
+++ shared/team-building/user-bubble.js
@@ -33,7 +33,7 @@ const RemoveBubble = ({onRemove, prettyName}: {onRemove: () => void, prettyName:
       <Kb.Icon
         type={'iconfont-close'}
         color={Styles.globalColors.white}
-        fontSize={20}
+        fontSize={16}
         style={Kb.iconCastPlatformStyles(styles.removeIcon)}
       />
     </Kb.ClickableBox>
