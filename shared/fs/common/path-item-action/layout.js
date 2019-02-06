38501c379cc23cd75663661b2a0ec80851788967
Song Gao <song@gao.io>
2019-02-05 20:44:33 -0800

    enable sendLinkToChat on mobile; route fixes

--- shared/fs/common/path-item-action/layout.js
+++ shared/fs/common/path-item-action/layout.js
@@ -52,7 +52,7 @@ const getRawLayout = (path: Types.Path, pathItem: Types.PathItem): Layout => {
         ...empty,
         copyPath: true,
         ignoreTlf: true,
-        sendLinkToChat: false, // TODO enable mobile // desktop uses separate button
+        sendLinkToChat: isMobile, // desktop uses separate button
         showInSystemFileManager: !isMobile,
       }
     default:
@@ -68,7 +68,7 @@ const getRawLayout = (path: Types.Path, pathItem: Types.PathItem): Layout => {
         // share menu items
         // eslint-disable-next-line sort-keys
         sendAttachmentToChat: false, // TODO enable mobile pathItem.type === 'file' && isMobile, // desktop uses separate button
-        sendLinkToChat: false, // TODO enable mobile // desktop uses separate button
+        sendLinkToChat: isMobile, // desktop uses separate button
         sendToOtherApp: pathItem.type === 'file' && isMobile,
       }
   }
