16594500c1dec1ef5fc6b6c7fa08338941a970ed
Song Gao <song@gao.io>
2019-02-07 13:39:32 -0800

    shorter file names suggested by jzila

--- shared/fs/common/index.js
+++ shared/fs/common/index.js
@@ -1,5 +1,5 @@
 // @flow
-export {default as PathItemAction} from './path-item-action/path-item-action-container'
+export {default as PathItemAction} from './path-item-action/container'
 export {default as SendInAppAction} from './send-in-app-action'
 export {default as OpenInSystemFileManager} from './open-in-system-file-manager-container'
 export {default as PathItemInfo} from './path-item-info-container'
