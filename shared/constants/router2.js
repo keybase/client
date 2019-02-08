1e3a1507b1f3129321b8051284fb82625b15e828
chrisnojima <cnojima@keyba.se>
2019-02-08 18:01:54 -0500

    WIP

---
+++ shared/constants/router2.js
@@ -0,0 +1,28 @@
+// @flow
+let _navigator = null
+/*
+ * only used by config
+ */
+export const _setNavigator = (navigator: any) => {
+  _navigator = navigator
+}
+export const _getNavigator = () => {
+  return _navigator
+}
+
+const findVisibleRoute = (arr, s) => {
+  if (!s) return arr
+  if (!s.routes) return s
+  const route = s.routes[s.index]
+  if (!route) return arr
+  if (route.routes) return findVisibleRoute([...arr, route], route)
+  return [...arr, route]
+}
+
+// Public API
+export const getVisiblePath = () => {
+  if (!_navigator) return []
+  const TEMP = findVisibleRoute([], _navigator.getNavState())
+  console.log('aaaa rout epath', TEMP)
+  return TEMP
+}
