33eff866be13b05c84b86a42ced5d5e2f748bd21
Steve Sanders <steve@keyba.se>
2019-02-11 11:13:44 -0800

    review

--- shared/common-adapters/scroll-view.desktop.js
+++ shared/common-adapters/scroll-view.desktop.js
@@ -1,14 +1,21 @@
 // @flow
 import * as React from 'react'
-import {collapseStyles} from '../styles'
+import * as Styles from '../styles'
 import type {Props} from './scroll-view'
 
 const ScrollView = (props: Props) => {
   const {contentContainerStyle, style, hideVerticalScroll, ...rest} = props
-  const className = hideVerticalScroll ? 'hide-scrollbar scroll-container' : ''
-  const overflowStyle = hideVerticalScroll ? overflowHidden : overflowAuto
+  const className = Styles.classNames(
+    {'hide-scrollbar': hideVerticalScroll},
+    {'scroll-container': hideVerticalScroll}
+  )
+  const overflowStyle = hideVerticalScroll ? styles.overflowHidden : styles.overflowAuto
   return (
-    <div style={collapseStyles([overflowStyle, style])} onScroll={props.onScroll} className={className}>
+    <div
+      style={Styles.collapseStyles([overflowStyle, style])}
+      onScroll={props.onScroll}
+      className={className}
+    >
       <div style={contentContainerStyle} {...rest} />
     </div>
   )
@@ -16,12 +23,14 @@ const ScrollView = (props: Props) => {
 
 // not using a stylesheet because StylesCrossPlatform
 // doesn't include overflowX and overflowY
-const overflowAuto = {
-  overflow: 'auto',
-}
-const overflowHidden = {
-  overflowX: 'hidden',
-  overflowY: 'auto',
-}
+const styles = Styles.styleSheetCreate({
+  overflowAuto: {
+    overflow: 'auto',
+  },
+  overflowHidden: {
+    overflowX: 'hidden',
+    overflowY: 'auto',
+  },
+})
 
 export default ScrollView
