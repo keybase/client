ef444d949f5978a49abca817c52df65260ace7d9
Piotr Zduniak <piotr@zduniak.net>
2019-02-12 21:32:18 +0100

    field rename

--- go/protocol/keybase1/config.go
+++ go/protocol/keybase1/config.go
@@ -119,31 +119,31 @@ func (o DirSizeInfo) DeepCopy() DirSizeInfo {
 }
 
 type ExtendedStatus struct {
-	Standalone             bool                             `codec:"standalone" json:"standalone"`
-	PassphraseStreamCached bool                             `codec:"passphraseStreamCached" json:"passphraseStreamCached"`
-	TsecCached             bool                             `codec:"tsecCached" json:"tsecCached"`
-	DeviceSigKeyCached     bool                             `codec:"deviceSigKeyCached" json:"deviceSigKeyCached"`
-	DeviceEncKeyCached     bool                             `codec:"deviceEncKeyCached" json:"deviceEncKeyCached"`
-	PaperSigKeyCached      bool                             `codec:"paperSigKeyCached" json:"paperSigKeyCached"`
-	PaperEncKeyCached      bool                             `codec:"paperEncKeyCached" json:"paperEncKeyCached"`
-	StoredSecret           bool                             `codec:"storedSecret" json:"storedSecret"`
-	SecretPromptSkip       bool                             `codec:"secretPromptSkip" json:"secretPromptSkip"`
-	RememberPassphrase     bool                             `codec:"rememberPassphrase" json:"rememberPassphrase"`
-	Device                 *Device                          `codec:"device,omitempty" json:"device,omitempty"`
-	DeviceErr              *LoadDeviceErr                   `codec:"deviceErr,omitempty" json:"deviceErr,omitempty"`
-	LogDir                 string                           `codec:"logDir" json:"logDir"`
-	Session                *SessionStatus                   `codec:"session,omitempty" json:"session,omitempty"`
-	DefaultUsername        string                           `codec:"defaultUsername" json:"defaultUsername"`
-	ProvisionedUsernames   []string                         `codec:"provisionedUsernames" json:"provisionedUsernames"`
-	Clients                []ClientDetails                  `codec:"Clients" json:"Clients"`
-	DeviceEkNames          []string                         `codec:"deviceEkNames" json:"deviceEkNames"`
-	PlatformInfo           PlatformInfo                     `codec:"platformInfo" json:"platformInfo"`
-	DefaultDeviceID        DeviceID                         `codec:"defaultDeviceID" json:"defaultDeviceID"`
-	LocalDbStats           []string                         `codec:"localDbStats" json:"localDbStats"`
-	LocalChatDbStats       []string                         `codec:"localChatDbStats" json:"localChatDbStats"`
-	CacheDirSizeInfo       []DirSizeInfo                    `codec:"cacheDirSizeInfo" json:"cacheDirSizeInfo"`
-	NotifyMappings         []notifyCtl.NotificationChannels `codec:"notifyMappings" json:"notifyMappings"`
-	UiRouterConnections    map[string]int                   `codec:"uiRouterConnections" json:"uiRouterConnections"`
+	Standalone                bool                             `codec:"standalone" json:"standalone"`
+	PassphraseStreamCached    bool                             `codec:"passphraseStreamCached" json:"passphraseStreamCached"`
+	TsecCached                bool                             `codec:"tsecCached" json:"tsecCached"`
+	DeviceSigKeyCached        bool                             `codec:"deviceSigKeyCached" json:"deviceSigKeyCached"`
+	DeviceEncKeyCached        bool                             `codec:"deviceEncKeyCached" json:"deviceEncKeyCached"`
+	PaperSigKeyCached         bool                             `codec:"paperSigKeyCached" json:"paperSigKeyCached"`
+	PaperEncKeyCached         bool                             `codec:"paperEncKeyCached" json:"paperEncKeyCached"`
+	StoredSecret              bool                             `codec:"storedSecret" json:"storedSecret"`
+	SecretPromptSkip          bool                             `codec:"secretPromptSkip" json:"secretPromptSkip"`
+	RememberPassphrase        bool                             `codec:"rememberPassphrase" json:"rememberPassphrase"`
+	Device                    *Device                          `codec:"device,omitempty" json:"device,omitempty"`
+	DeviceErr                 *LoadDeviceErr                   `codec:"deviceErr,omitempty" json:"deviceErr,omitempty"`
+	LogDir                    string                           `codec:"logDir" json:"logDir"`
+	Session                   *SessionStatus                   `codec:"session,omitempty" json:"session,omitempty"`
+	DefaultUsername           string                           `codec:"defaultUsername" json:"defaultUsername"`
+	ProvisionedUsernames      []string                         `codec:"provisionedUsernames" json:"provisionedUsernames"`
+	Clients                   []ClientDetails                  `codec:"Clients" json:"Clients"`
+	DeviceEkNames             []string                         `codec:"deviceEkNames" json:"deviceEkNames"`
+	PlatformInfo              PlatformInfo                     `codec:"platformInfo" json:"platformInfo"`
+	DefaultDeviceID           DeviceID                         `codec:"defaultDeviceID" json:"defaultDeviceID"`
+	LocalDbStats              []string                         `codec:"localDbStats" json:"localDbStats"`
+	LocalChatDbStats          []string                         `codec:"localChatDbStats" json:"localChatDbStats"`
+	CacheDirSizeInfo          []DirSizeInfo                    `codec:"cacheDirSizeInfo" json:"cacheDirSizeInfo"`
+	NotifyRouterSubscriptions []notifyCtl.NotificationChannels `codec:"notifyRouterSubscriptions" json:"notifyRouterSubscriptions"`
+	UiRouterMapping           map[string]int                   `codec:"uiRouterMapping" json:"uiRouterMapping"`
 }
 
 func (o ExtendedStatus) DeepCopy() ExtendedStatus {
@@ -249,7 +249,7 @@ func (o ExtendedStatus) DeepCopy() ExtendedStatus {
 			}
 			return ret
 		})(o.CacheDirSizeInfo),
-		NotifyMappings: (func(x []notifyCtl.NotificationChannels) []notifyCtl.NotificationChannels {
+		NotifyRouterSubscriptions: (func(x []notifyCtl.NotificationChannels) []notifyCtl.NotificationChannels {
 			if x == nil {
 				return nil
 			}
@@ -259,8 +259,8 @@ func (o ExtendedStatus) DeepCopy() ExtendedStatus {
 				ret[i] = vCopy
 			}
 			return ret
-		})(o.NotifyMappings),
-		UiRouterConnections: (func(x map[string]int) map[string]int {
+		})(o.NotifyRouterSubscriptions),
+		UiRouterMapping: (func(x map[string]int) map[string]int {
 			if x == nil {
 				return nil
 			}
@@ -271,7 +271,7 @@ func (o ExtendedStatus) DeepCopy() ExtendedStatus {
 				ret[kCopy] = vCopy
 			}
 			return ret
-		})(o.UiRouterConnections),
+		})(o.UiRouterMapping),
 	}
 }
 
