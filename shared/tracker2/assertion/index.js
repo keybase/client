2a62d5a4f2c5c85f0e1ecdbafe9ead967cb923da
Daniel Ayoub <danny.a95@gmail.com>
2019-02-15 11:20:06 -0500

    id3: Add strikethrough to revoked proofs

--- shared/tracker2/assertion/index.js
+++ shared/tracker2/assertion/index.js
@@ -67,6 +67,22 @@ const stateToColor = state => {
   }
 }
 
+const stateToValueTextStyle = state => {
+  switch (state) {
+    case 'revoked':
+      return styles.strikeThrough
+    case 'checking':
+    case 'valid':
+    case 'error':
+    case 'warning':
+    case 'suggestion':
+      return null
+    default:
+      Flow.ifFlowComplainsAboutThisFunctionYouHaventHandledAllCasesInASwitch(state)
+      throw new Error('Impossible')
+  }
+}
+
 const assertionColorToColor = (c: Types.AssertionColor) => {
   switch (c) {
     case 'blue':
@@ -195,7 +211,11 @@ const Value = p => {
       <Kb.Text
         type="BodyPrimaryLink"
         onClick={p.onCreateProof || p.onShowSite}
-        style={Styles.collapseStyles([style, {color: assertionColorToColor(p.color)}])}
+        style={Styles.collapseStyles([
+          style,
+          stateToValueTextStyle(p.state),
+          {color: assertionColorToColor(p.color)},
+        ])}
       >
         {str}
       </Kb.Text>
@@ -372,6 +392,7 @@ const styles = Styles.styleSheetCreate({
   metaContainer: {flexShrink: 0, paddingLeft: 20 + Styles.globalMargins.tiny * 2 - 4}, // icon spacing plus meta has 2 padding for some reason
   site: {color: Styles.globalColors.black_20},
   stateIcon: {height: 17},
+  strikeThrough: {textDecorationLine: 'line-through'},
   textContainer: {flexGrow: 1, flexShrink: 1, marginTop: -1},
   tooltip: Styles.platformStyles({isElectron: {display: 'inline-flex'}}),
   username: Styles.platformStyles({
