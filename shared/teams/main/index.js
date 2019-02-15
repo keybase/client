267bd2deceb6ab538ae6e1bf4821093e2334c717
chrisnojima <cnojima@keyba.se>
2019-02-15 17:36:59 -0500

    WIP

--- shared/teams/main/index.js
+++ shared/teams/main/index.js
@@ -5,62 +5,144 @@ import * as Kb from '../../common-adapters'
 import Header from './header'
 import Banner from './banner'
 import BetaNote from './beta-note'
-import TeamList from './team-list'
+import {memoize} from '../../util/memoize'
 
 import type {Props as HeaderProps} from './header'
 import type {Props as BannerProps} from './banner'
 import type {Props as BetaNoteProps} from './beta-note'
-import type {Props as TeamListProps} from './team-list'
 
 // TODO: Don't make all these props just so we can pass it down. Make these their own connected components
 type Props = {|
   ...$Exact<HeaderProps>,
   ...$Exact<BetaNoteProps>,
-  ...$Exact<TeamListProps>,
   ...$Exact<BannerProps>,
   ...{|sawChatBanner: boolean, title: string, onBack: () => void|},
 |}
 
-const Teams = (props: Props) => (
-  <Kb.Box style={{...Styles.globalStyles.flexBoxColumn, alignItems: 'center', height: '100%'}}>
-    <Header loaded={props.loaded} onCreateTeam={props.onCreateTeam} onJoinTeam={props.onJoinTeam} />
-    <Kb.Box style={{flex: 1, position: 'relative', width: '100%'}}>
-      <Kb.ScrollView
-        style={{...Styles.globalStyles.fillAbsolute}}
-        contentContainerStyle={{
-          ...Styles.globalStyles.flexBoxColumn,
-          alignItems: 'center',
-        }}
-      >
-        {!props.sawChatBanner && (
-          <Banner onReadMore={props.onReadMore} onHideChatBanner={props.onHideChatBanner} />
-        )}
-        <TeamList
-          teamnames={props.teamnames}
-          teammembercounts={props.teammembercounts}
-          teamresetusers={props.teamresetusers}
-          teamNameToIsOpen={props.teamNameToIsOpen}
-          newTeams={props.newTeams}
-          newTeamRequests={props.newTeamRequests}
-          onOpenFolder={props.onOpenFolder}
-          onManageChat={props.onManageChat}
-          onViewTeam={props.onViewTeam}
-        />
-        <BetaNote onReadMore={props.onReadMore} />
-        {/* Put progress indicator in the footer on mobile because it won't fit in the header on small screens */}
-        {Styles.isMobile && (
-          <Kb.ProgressIndicator
-            style={{
-              alignSelf: 'center',
-              marginBottom: Styles.globalMargins.small,
-              opacity: props.loaded ? 0 : 1,
-              width: 20,
-            }}
+type RowProps = {
+  firstItem: boolean,
+  name: string,
+  membercount: number,
+  isNew: boolean,
+  isOpen: boolean,
+  newRequests: number,
+  onOpenFolder: ?() => void,
+  onManageChat: ?() => void,
+  resetUserCount?: number,
+  onViewTeam: () => void,
+}
+
+export const TeamRow = (props: RowProps) => {
+  const badgeCount = props.newRequests + props.resetUserCount
+
+  return (
+    <Kb.ListItem2
+      firstItem={props.firstItem}
+      onClick={props.onViewTeam}
+      icon={
+        <Kb.Box2 direction="vertical" style={styles.avatarContainer}>
+          <Kb.Avatar size={Styles.isMobile ? 48 : 32} teamname={props.name} isTeam={true} />
+          {!!badgeCount && <Kb.Badge badgeNumber={badgeCount} badgeStyle={styles.badge} />}
+        </Kb.Box2>
+      }
+      body={
+        <Kb.Box2 direction="vertical" fullWidth={true}>
+          <Kb.Box2 direction="horizontal" gap="tiny" alignSelf="flex-start">
+            <Kb.Text type="BodySemibold">{props.name}</Kb.Text>
+            {props.isOpen && <Kb.Meta title="open" backgroundColor={Styles.globalColors.green} />}
+          </Kb.Box2>
+          <Kb.Box2 direction="horizontal" gap="tiny" alignSelf="flex-start">
+            {props.isNew && <Kb.Meta title="new" backgroundColor={Styles.globalColors.orange} />}
+            <Kb.Text type="BodySmall">
+              {props.membercount + ' member' + (props.membercount !== 1 ? 's' : '')}
+            </Kb.Text>
+          </Kb.Box2>
+        </Kb.Box2>
+      }
+      action={
+        <Kb.Box2 direction="horizontal" gap="small">
+          {!Styles.isMobile && props.onOpenFolder && (
+            <Kb.Icon type="iconfont-folder-private" onClick={props.onOpenFolder} />
+          )}
+          {!Styles.isMobile && props.onManageChat && (
+            <Kb.Icon type="iconfont-chat" onClick={props.onManageChat} />
+          )}
+        </Kb.Box2>
+      }
+    />
+  )
+}
+
+class Teams extends React.PureComponent<Props> {
+  _teamsAndExtras = memoize(teamnames => {
+    return [
+      {key: '_banner', type: '_banner'},
+      ...teamnames.map(t => ({key: t, team: t, type: 'team'})),
+      {key: '_note', type: '_note'},
+    ]
+  })
+
+  _onOpenFolder = name => this.props.onOpenFolder(name)
+  _onManageChat = name => this.props.onManageChat(name)
+  _onViewTeam = name => this.props.onViewTeam(name)
+
+  _renderItem = (index, item) => {
+    switch (item.type) {
+      case '_banner':
+        return this.props.sawChatBanner ? null : (
+          <Banner onReadMore={this.props.onReadMore} onHideChatBanner={this.props.onHideChatBanner} />
+        )
+      case '_note':
+        return <BetaNote onReadMore={this.props.onReadMore} />
+      case 'team':
+        const name = item.team
+        return (
+          <TeamRow
+            firstItem={index === 1}
+            key={name}
+            name={name}
+            isNew={this.props.newTeams.includes(name)}
+            isOpen={this.props.teamNameToIsOpen[name]}
+            newRequests={this.props.newTeamRequests.some(team => team === name)}
+            membercount={this.props.teammembercounts[name]}
+            onOpenFolder={() => this._onOpenFolder(name)}
+            onManageChat={() => this._onManageChat(name)}
+            onViewTeam={() => this._onViewTeam(name)}
+            resetUserCount={this.props.teamresetusers[name] ? this.props.teamresetusers[name].size : 0}
           />
-        )}
-      </Kb.ScrollView>
-    </Kb.Box>
-  </Kb.Box>
-)
+        )
+      default:
+        return null
+    }
+  }
+
+  render() {
+    return (
+      <Kb.Box2 direction="vertical" fullWidth={true} fullHeight={true}>
+        <Header
+          loaded={this.props.loaded}
+          onCreateTeam={this.props.onCreateTeam}
+          onJoinTeam={this.props.onJoinTeam}
+        />
+        {!this.props.loaded && <Kb.ProgressIndicator style={styles.progress} />}
+        <Kb.List items={this._teamsAndExtras(this.props.teamnames)} renderItem={this._renderItem} />
+      </Kb.Box2>
+    )
+  }
+}
+
+const styles = Styles.styleSheetCreate({
+  avatarContainer: {position: 'relative'},
+  badge: {
+    position: 'absolute',
+    right: -5,
+    top: -5,
+  },
+  progress: {
+    alignSelf: 'center',
+    marginBottom: Styles.globalMargins.small,
+    width: 20,
+  },
+})
 
 export default Teams
--- shared/teams/main/index.stories.js
+++ shared/teams/main/index.stories.js
@@ -3,7 +3,7 @@ import * as React from 'react'
 import * as Sb from '../../stories/storybook'
 import BetaNote from './beta-note'
 import Header from './header'
-import TeamList from './team-list'
+import TeamList from '.'
 import {Box} from '../../common-adapters'
 
 const teamnames = ['stripe', 'stripe.usa', 'techtonica']
