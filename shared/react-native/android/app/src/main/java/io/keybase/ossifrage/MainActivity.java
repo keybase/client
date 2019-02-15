dcd97cc6f03dea7017bbc1f76d7b4a73f5a750de
John Zila <john@keyba.se>
2019-02-14 16:07:53 -0800

    android: Close the loop on a sharing intent

--- shared/react-native/android/app/src/main/java/io/keybase/ossifrage/MainActivity.java
+++ shared/react-native/android/app/src/main/java/io/keybase/ossifrage/MainActivity.java
@@ -12,9 +12,12 @@ import android.view.KeyEvent;
 import android.view.Window;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.ReactFragmentActivity;
+import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 import com.facebook.react.modules.core.PermissionListener;
 
@@ -30,6 +33,7 @@ import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
 import java.util.ArrayList;
 
+import io.keybase.ossifrage.modules.KeybaseEngine;
 import io.keybase.ossifrage.modules.NativeLogger;
 import io.keybase.ossifrage.util.ContactsPermissionsWrapper;
 import io.keybase.ossifrage.util.DNSNSFetcher;
@@ -61,20 +65,24 @@ public class MainActivity extends ReactFragmentActivity {
         }
     }
 
+    private ReactContext getReactContext() {
+        ReactInstanceManager instanceManager = getReactInstanceManager();
+        if (instanceManager == null) return null;
+
+        return instanceManager.getCurrentReactContext();
+    }
+
     private void handleNotificationIntent(Intent intent) {
         Bundle bundle = intent.getExtras();
         if (bundle == null || !bundle.containsKey("notification")) return;
 
-        ReactInstanceManager instanceManager = getReactInstanceManager();
-        if (instanceManager != null) {
-            ReactContext currentContext = instanceManager.getCurrentReactContext();
-            if (currentContext != null) {
-                DeviceEventManagerModule.RCTDeviceEventEmitter emitter = currentContext
-                        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class);
-                if (emitter != null) {
-                    emitter.emit("androidIntentNotification", "");
-                }
-            }
+        ReactContext reactContext = getReactContext();
+        if (reactContext == null) return;
+
+        DeviceEventManagerModule.RCTDeviceEventEmitter emitter = reactContext
+                .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class);
+        if (emitter != null) {
+            emitter.emit("androidIntentNotification", "");
         }
     }
 
@@ -96,7 +104,16 @@ public class MainActivity extends ReactFragmentActivity {
         String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
         if (sharedText == null) return;
 
-        // Do something with the intent data.
+        ReactContext reactContext = getReactContext();
+        if (reactContext == null) return;
+
+        WritableMap evt = Arguments.createMap();
+        evt.putString("text", sharedText);
+        DeviceEventManagerModule.RCTDeviceEventEmitter emitter = reactContext
+                .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class);
+        if (emitter != null) {
+            emitter.emit("onShareText", evt);
+        }
     }
 
     @Override
@@ -200,4 +217,5 @@ public class MainActivity extends ReactFragmentActivity {
     protected String getMainComponentName() {
         return "Keybase";
     }
+
 }
