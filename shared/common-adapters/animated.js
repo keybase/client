1a0634ae2aec6a174811c2a5ba625b8f9ee9a6b5
Adam Spooner <adam@keyba.se>
2019-02-15 17:39:07 -0500

    Use an older version of react-spring.

--- shared/common-adapters/animated.js
+++ shared/common-adapters/animated.js
@@ -1,6 +1,6 @@
 // @flow
 import * as React from 'react'
-import {Spring} from 'react-spring/renderprops'
+import {Spring} from 'react-spring'
 import type {Props} from './animated'
 
 const Animated = (props: Props) => {
