cf04d111e65242942e8054fb56da836cd8b0b36f
Song Gao <song@gao.io>
2019-02-19 15:10:09 -0800

    update storyshot url

--- shared/common-adapters/video.stories.js
+++ shared/common-adapters/video.stories.js
@@ -5,18 +5,19 @@ import Video from './video'
 import Box, {Box2} from './box'
 import * as Styles from '../styles'
 
+const starman =
+  'https://archive.org/download/youtube%2DA0FZIwabctw/Falcon%5FHeavy%5FStarman%2DA0FZIwabctw%2Emp4'
+
 const load = () => {
   Sb.storiesOf('Common/Video', module)
-    .add('Starman', () => (
-      <Video url="https://archive.org/download/youtube-A0FZIwabctw/Falcon_Heavy_Starman-A0FZIwabctw.mp4" />
-    ))
+    .add('Starman', () => <Video url={starman} />)
     .add('Starman Two', () => (
       <Box2 direction="vertical" gap="small" fullHeight={true} fullWidth={true} style={styles.bigBox}>
         <Box style={styles.smallBox}>
-          <Video url="https://archive.org/download/youtube-A0FZIwabctw/Falcon_Heavy_Starman-A0FZIwabctw.mp4" />
+          <Video url={starman} />
         </Box>
         <Box style={styles.smallBox}>
-          <Video url="https://archive.org/download/youtube-A0FZIwabctw/Falcon_Heavy_Starman-A0FZIwabctw.mp4" />
+          <Video url={starman} />
         </Box>
       </Box2>
     ))
