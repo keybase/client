7c91a36652ed037d9907723e2435975c3a5578f6
Miles Steele <miles@milessteele.com>
2019-02-22 10:57:23 -0500

    use inner capture group

--- go/chat/wallet/parser.go
+++ go/chat/wallet/parser.go
@@ -12,14 +12,14 @@ var txPattern = regexp.MustCompile(
 	// Start at the beginng of the line, space, or some hand picked artisanal characters
 	`(?:^|[\s([{:;.,])` +
 		// Have a + in front
-		`\+` +
+		`(\+` +
 		// Stellar decimal amount
 		`(\d+\.?\d*|\d*\.?\d+)` +
 		// Currency code
 		`([A-Za-z]{2,6})` +
 		// At sign and username, optional for direct messages
 		// If not used, must be followed by a non-tx-character or end of string
-		`(?:(?:@((?:[a-zA-Z0-9]+_?)+))|(?:[^A-Za-z@]|\z))`,
+		`(?:(?:@((?:[a-zA-Z0-9]+_?)+))|(?:[^A-Za-z@]|\z)))`,
 )
 
 var maxAmountLength = 100
@@ -42,17 +42,26 @@ func FindChatTxCandidates(xs string) []ChatTxCandidate {
 	allRawIndices := txPattern.FindAllStringSubmatchIndex(replaced, maxTxsPerMessage)
 	matches := make([]ChatTxCandidate, 0, len(allRawIndices))
 	for _, rawIndices := range allRawIndices {
-		amount := xs[rawIndices[2]:rawIndices[3]]
+		group := func(n int) (s string, startIndex, endIndex int) {
+			startIndex, endIndex = rawIndices[2*n], rawIndices[2*n+1]
+			if startIndex >= 0 {
+				return xs[startIndex:endIndex], startIndex, endIndex
+			}
+			return "", startIndex, endIndex
+		}
+		_, plusStart, _ := group(1)
+		amount, _, _ := group(2)
 		if amount == "0" {
 			continue
 		}
-		currencyCode := strings.ToUpper(xs[rawIndices[4]:rawIndices[5]])
-		var username, atSign string
-		endIndex := rawIndices[5]
-		if rawIndices[6] >= 0 {
-			username = xs[rawIndices[6]:rawIndices[7]]
+		currencyCode, _, ccEnd := group(3)
+		currencyCode = strings.ToUpper(currencyCode)
+		username, _, usernameEndIndex := group(4)
+		var atSign string
+		endIndex := ccEnd
+		if len(username) > 0 {
 			atSign = "@"
-			endIndex = rawIndices[7]
+			endIndex = usernameEndIndex
 		}
 		full := fmt.Sprintf("+%s%s%s%s", amount, currencyCode, atSign, username)
 		if len(amount) <= maxAmountLength && len(username) <= maxUsernameLength {
@@ -67,7 +76,7 @@ func FindChatTxCandidates(xs string) []ChatTxCandidate {
 				Amount:       amount,
 				CurrencyCode: currencyCode,
 				Username:     txUsername,
-				Position:     []int{rawIndices[0], endIndex},
+				Position:     []int{plusStart, endIndex},
 			})
 		}
 	}
