05ee4858cd7648cab1f78ec32b00c1f4e6b6c1d3
Chris Ball <chris@printf.net>
2019-02-25 16:59:49 -0800

    Review feedback from Noj

--- shared/provision/username-or-email/container.js
+++ shared/provision/username-or-email/container.js
@@ -2,14 +2,16 @@
 import * as ProvisionGen from '../../actions/provision-gen'
 import * as SignupGen from '../../actions/signup-gen'
 import UsernameOrEmail from '.'
-import {compose, connect, safeSubmit, withStateHandlers} from '../../util/container'
+import {compose, connect, safeSubmit} from '../../util/container'
 import {type RouteProps} from '../../route-tree/render-route'
+import {constantsStatusCode} from '../../constants/types/rpc-gen'
 
 type OwnProps = RouteProps<{}, {}>
 
 const mapStateToProps = state => ({
+  badUsernameError:
+    !!state.provision.inlineError && state.provision.inlineError.code === constantsStatusCode.scnotfound,
   error: state.provision.error.stringValue(),
-  inlineError: state.provision.inlineError,
   // So we can clear the error if the name is changed
   submittedUsernameOrEmail: state.provision.usernameOrEmail,
 })
@@ -25,13 +27,14 @@ export default compose(
   connect<OwnProps, _, _, _, _>(
     mapStateToProps,
     dispatchToProps,
-    (s, d, o) => ({...o, ...s, ...d})
+    (stateProps, dispatchProps, _) => ({
+      badUsernameError: stateProps.badUsernameError,
+      error: stateProps.error,
+      onBack: dispatchProps.onBack,
+      onGoToSignup: dispatchProps.onGoToSignup,
+      onSubmit: dispatchProps.onSubmit,
+      submittedUsernameOrEmail: stateProps.submittedUsernameOrEmail,
+    })
   ),
-  withStateHandlers(
-    {usernameOrEmail: ''},
-    {
-      setUsernameOrEmail: () => usernameOrEmail => ({usernameOrEmail}),
-    }
-  ),
-  safeSubmit(['onBack', 'onSubmit'], ['error', 'inlineError'])
+  safeSubmit(['onBack', 'onSubmit'], ['error', 'badUsernameError'])
 )(UsernameOrEmail)
--- shared/provision/username-or-email/index.js
+++ shared/provision/username-or-email/index.js
@@ -5,67 +5,72 @@ import * as Kb from '../../common-adapters'
 import * as Styles from '../../styles'
 import Container from '../../login/forms/container'
 import * as Constants from '../../constants/provision'
-import {RPCError} from '../../util/errors'
-import {constantsStatusCode} from '../../constants/types/rpc-gen'
 
-type Props = {
+type Props = {|
+  badUsernameError: boolean,
   error: string,
-  inlineError: ?RPCError,
   onBack: () => void,
   onGoToSignup: () => void,
   onSubmit: (usernameOrEmail: string) => void,
-  setUsernameOrEmail: (text: string) => void,
   submittedUsernameOrEmail: string,
-  usernameOrEmail: string,
-}
+|}
 
-const InlineError = (props: {inlineError: RPCError, onGoToSignup: () => void}) =>
-  props.inlineError.code === constantsStatusCode.scnotfound ? (
-    <Kb.Box2 direction="vertical" centerChildren={true}>
-      <Kb.Text type="BodySmallError" style={styles.error}>
-        This username or email doesn't exist.
-      </Kb.Text>
-      <Kb.Text onClick={props.onGoToSignup} style={styles.errorLink} type="BodySmallPrimaryLink">
-        Sign up for a new account?
-      </Kb.Text>
-    </Kb.Box2>
-  ) : (
-    <Kb.Text type="BodySmallError" style={{color: Styles.globalColors.red}}>
-      {props.inlineError.desc}
+const BadUsernameError = (props: {|onGoToSignup: () => void|}) => (
+  <Kb.Box2 direction="vertical" centerChildren={true}>
+    <Kb.Text type="BodySmallError" style={styles.error}>
+      This username or email doesn't exist.
     </Kb.Text>
-  )
-
-const UsernameOrEmail = (props: Props) => (
-  <Container style={styles.container} outerStyle={styles.outerStyle} onBack={() => props.onBack()}>
-    <Kb.UserCard style={styles.card} outerStyle={styles.outerCard}>
-      <Kb.Input
-        autoFocus={true}
-        style={styles.input}
-        hintText="Username or email"
-        errorText={props.submittedUsernameOrEmail === props.usernameOrEmail ? props.error : ''}
-        errorTextComponent={
-          props.submittedUsernameOrEmail === props.usernameOrEmail && props.inlineError ? (
-            <InlineError inlineError={props.inlineError} onGoToSignup={props.onGoToSignup} />
-          ) : (
-            undefined
-          )
-        }
-        onEnterKeyDown={() => props.onSubmit(props.usernameOrEmail)}
-        onChangeText={text => props.setUsernameOrEmail(text)}
-        value={props.usernameOrEmail}
-      />
-      <Kb.WaitingButton
-        label="Continue"
-        type="Primary"
-        fullWidth={true}
-        style={styles.button}
-        onClick={() => props.onSubmit(props.usernameOrEmail)}
-        disabled={!props.usernameOrEmail}
-        waitingKey={Constants.waitingKey}
-      />
-    </Kb.UserCard>
-  </Container>
+    <Kb.Text onClick={props.onGoToSignup} style={styles.errorLink} type="BodySmallPrimaryLink">
+      Sign up for a new account?
+    </Kb.Text>
+  </Kb.Box2>
 )
+
+class UsernameOrEmail extends React.Component<Props, State> {
+  state: State
+
+  constructor(props: Props) {
+    super(props)
+    this.state = {usernameOrEmail: ''}
+  }
+
+  render() {
+    return (
+      <Container style={styles.container} outerStyle={styles.outerStyle} onBack={() => this.props.onBack()}>
+        <Kb.UserCard style={styles.card} outerStyle={styles.outerCard}>
+          <Kb.Input
+            autoFocus={true}
+            style={styles.input}
+            hintText="Username or email"
+            errorText={
+              this.props.submittedUsernameOrEmail === this.state.usernameOrEmail ? this.props.error : ''
+            }
+            errorTextComponent={
+              this.props.submittedUsernameOrEmail === this.state.usernameOrEmail &&
+              this.props.badUsernameError ? (
+                <BadUsernameError onGoToSignup={this.props.onGoToSignup} />
+              ) : (
+                undefined
+              )
+            }
+            onEnterKeyDown={() => this.props.onSubmit(this.state.usernameOrEmail)}
+            onChangeText={text => this.setState({usernameOrEmail: text})}
+            value={this.state.usernameOrEmail}
+          />
+          <Kb.WaitingButton
+            label="Continue"
+            type="Primary"
+            fullWidth={true}
+            style={styles.button}
+            onClick={() => this.props.onSubmit(this.state.usernameOrEmail)}
+            disabled={!this.state.usernameOrEmail}
+            waitingKey={Constants.waitingKey}
+          />
+        </Kb.UserCard>
+      </Container>
+    )
+  }
+}
 
 const styles = Styles.styleSheetCreate({
   button: Styles.platformStyles({
@@ -90,17 +95,10 @@ const styles = Styles.styleSheetCreate({
     },
   }),
   error: {paddingTop: Styles.globalMargins.tiny, textAlign: 'center'},
-  errorLink: Styles.platformStyles({
-    common: {
-      color: Styles.globalColors.red,
-    },
-    isElectron: {
-      textDecoration: 'underline',
-    },
-    isMobile: {
-      textDecorationLine: 'underline',
-    },
-  }),
+  errorLink: {
+    color: Styles.globalColors.red,
+    textDecorationLine: 'underline',
+  },
   input: Styles.platformStyles({
     isMobile: {
       flexGrow: 1,
