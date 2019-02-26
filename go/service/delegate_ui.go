94e8ddbff5ee8410271f0dda34a1811c870a4643
Piotr Zduniak <piotr@zduniak.net>
2019-02-26 22:55:13 +0100

    Fix missing badges when Identify3UI is used (#16258)
    * initial fix
    
    * slight code reorg

--- go/service/delegate_ui.go
+++ go/service/delegate_ui.go
@@ -28,21 +28,26 @@ func NewDelegateUICtlHandler(xp rpc.Transporter, id libkb.ConnectionID, g *libkb
 	}
 }
 
-func (d *DelegateUICtlHandler) RegisterIdentifyUI(_ context.Context) error {
+func (d *DelegateUICtlHandler) RegisterIdentifyUI(ctx context.Context) error {
 	d.G().UIRouter.SetUI(d.id, libkb.IdentifyUIKind)
-
-	// Let Gregor related code know that a IdentifyUI client
-	// (probably Electron) has connected, and to sync out state to it
-	if d.G().GregorListener != nil {
-		d.G().GregorListener.PushHandler(NewIdentifyUIHandler(d.G(), d.id))
-	}
-
+	d.pushGregorIdentifyUIHandler(ctx)
 	return nil
 }
 
-func (d *DelegateUICtlHandler) RegisterIdentify3UI(_ context.Context) error {
+func (d *DelegateUICtlHandler) RegisterIdentify3UI(ctx context.Context) error {
 	d.G().UIRouter.SetUI(d.id, libkb.Identify3UIKind)
+	d.pushGregorIdentifyUIHandler(ctx)
 	return nil
+}
+
+func (d *DelegateUICtlHandler) pushGregorIdentifyUIHandler(_ context.Context) {
+	if d.G().GregorListener == nil {
+		return
+	}
+
+	// Let Gregor related code know that a IdentifyUI client
+	// (probably Electron) has connected, and to sync out state to it
+	d.G().GregorListener.PushHandler(NewIdentifyUIHandler(d.G(), d.id))
 }
 
 func (d *DelegateUICtlHandler) RegisterChatUI(_ context.Context) error {
