e9089284f6d7eeb8998109dab2280c54aa4272ed
Miles Steele <miles@milessteele.com>
2019-02-27 11:03:32 -0500

    android

--- go/libkb/secret_store_external.go
+++ go/libkb/secret_store_external.go
@@ -84,25 +84,25 @@ func getGlobalExternalKeyStore(m MetaContext) (ExternalKeyStore, error) {
 
 	if externalKeyStore == nil {
 		// perhaps SetGlobalExternalKeyStore has not been called by Android internals yet:
-		m.CDebugf("secret_store_external:getGlobalExternalKeyStore called, but externalKeyStore is nil")
+		m.Debug("secret_store_external:getGlobalExternalKeyStore called, but externalKeyStore is nil")
 		return nil, errNoExternalKeyStore
 	}
 
 	// always check this since perhaps SetGlobalExternalKeyStore called more than once
 	if !externalKeyStoreInitialized {
-		m.CDebugf("+ secret_store_external:setup (in getGlobalExternalKeyStore)")
-		defer m.CDebugf("- secret_store_external:setup (in getGlobalExternalKeyStore)")
+		m.Debug("+ secret_store_external:setup (in getGlobalExternalKeyStore)")
+		defer m.Debug("- secret_store_external:setup (in getGlobalExternalKeyStore)")
 
 		serviceName := m.G().GetStoredSecretServiceName()
 
 		// username not required
 		err := externalKeyStore.SetupKeyStore(serviceName, "")
 		if err != nil {
-			m.CDebugf("externalKeyStore.SetupKeyStore(%s) error: %s (%T)", serviceName, err, err)
+			m.Debug("externalKeyStore.SetupKeyStore(%s) error: %s (%T)", serviceName, err, err)
 			return nil, err
 		}
 
-		m.CDebugf("externalKeyStore.SetupKeyStore(%s) success", serviceName)
+		m.Debug("externalKeyStore.SetupKeyStore(%s) success", serviceName)
 		externalKeyStoreInitialized = true
 	}
 
@@ -122,7 +122,7 @@ func (s *secretStoreAndroid) serviceName(m MetaContext) string {
 }
 
 func (s *secretStoreAndroid) StoreSecret(m MetaContext, username NormalizedUsername, secret LKSecFullSecret) (err error) {
-	defer m.CTraceTimed("secret_store_external StoreSecret", func() error { return err })()
+	defer m.TraceTimed("secret_store_external StoreSecret", func() error { return err })()
 	ks, err := getGlobalExternalKeyStore(m)
 	if err != nil {
 		return err
@@ -132,7 +132,7 @@ func (s *secretStoreAndroid) StoreSecret(m MetaContext, username NormalizedUsern
 }
 
 func (s *secretStoreAndroid) RetrieveSecret(m MetaContext, username NormalizedUsername) (sec LKSecFullSecret, err error) {
-	defer m.CTraceTimed("secret_store_external RetrieveSecret", func() error { return err })()
+	defer m.TraceTimed("secret_store_external RetrieveSecret", func() error { return err })()
 
 	ks, err := getGlobalExternalKeyStore(m)
 	if err != nil {
@@ -143,9 +143,9 @@ func (s *secretStoreAndroid) RetrieveSecret(m MetaContext, username NormalizedUs
 }
 
 func (s *secretStoreAndroid) ClearSecret(m MetaContext, username NormalizedUsername) (err error) {
-	defer m.CTraceTimed("secret_store_external ClearSecret", func() error { return err })()
+	defer m.TraceTimed("secret_store_external ClearSecret", func() error { return err })()
 	if username.IsNil() {
-		m.CDebugf("NOOPing secretStoreAndroid#ClearSecret for empty username")
+		m.Debug("NOOPing secretStoreAndroid#ClearSecret for empty username")
 		return nil
 	}
 	ks, err := getGlobalExternalKeyStore(m)
@@ -157,7 +157,7 @@ func (s *secretStoreAndroid) ClearSecret(m MetaContext, username NormalizedUsern
 }
 
 func (s *secretStoreAndroid) GetUsersWithStoredSecrets(m MetaContext) (users []string, err error) {
-	defer m.CTraceTimed("secret_store_external GetUsersWithStoredSecrets", func() error { return err })()
+	defer m.TraceTimed("secret_store_external GetUsersWithStoredSecrets", func() error { return err })()
 
 	ks, err := getGlobalExternalKeyStore(m)
 	if err != nil {
