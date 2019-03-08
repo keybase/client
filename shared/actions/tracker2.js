7410259c73ecbc3a104fdd6711f4f25bf39e5044
chrisnojima <cnojima@keyba.se>
2019-03-08 17:07:23 -0500

    no more trackers

--- shared/actions/tracker2.js
+++ shared/actions/tracker2.js
@@ -1,11 +1,12 @@
 // @flow
 import * as Tracker2Gen from './tracker2-gen'
-// import {getProfile as getProfileOLD, type GetProfilePayload as GetProfilePayloadOLD} from './tracker-gen'
+import * as ProfileGen from './profile-gen'
 import * as EngineGen from './engine-gen-gen'
 import * as Saga from '../util/saga'
 import * as Constants from '../constants/tracker2'
 import * as RPCTypes from '../constants/types/rpc-gen'
 import logger from '../logger'
+import flags from '../util/feature-flags'
 
 const identify3Result = (_, action) => {
   const {guiID, result} = action.payload.params
@@ -185,16 +186,25 @@ const getProofSuggestions = () =>
     })
   )
 
-const showUser = (_, action) =>
-  Tracker2Gen.createLoad({
+const showUser = (_, action) => {
+  const load = Tracker2Gen.createLoad({
     assertion: action.payload.username,
-    forceDisplay: action.payload.asTracker,
+    // with new nav we never show trackers from inside the app
+    forceDisplay: flags.useNewRouter ? false : action.payload.asTracker,
     fromDaemon: false,
     guiID: Constants.generateGUIID(),
     ignoreCache: true,
     inTracker: action.payload.asTracker,
     reason: '',
   })
+
+  if (flags.useNewRouter) {
+    // go to profile page
+    return [load, ProfileGen.createShowUserProfile({username: action.payload.username})]
+  } else {
+    return load
+  }
+}
 
 function* tracker2Saga(): Saga.SagaGenerator<any, any> {
   yield* Saga.chainAction<EngineGen.Keybase1Identify3UiIdentify3UpdateUserCardPayload>(
