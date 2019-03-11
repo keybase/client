7147df974a88e7006ec0a4392f20b64abdba1ec1
chrisnojima <cnojima@keyba.se>
2019-03-11 11:11:46 -0400

    WIP

--- shared/profile/search/index.desktop.js
+++ shared/profile/search/index.desktop.js
@@ -3,12 +3,11 @@ import React from 'react'
 import SearchResultsList from '../../search/results-list/container'
 import UserInput from '../../search/user-input/container'
 import * as Kb from '../../common-adapters'
-import * as Styles from '../../styles'
 import type {Props} from '.'
 import {searchKey, placeholder} from './index.shared'
 
 const Search = (props: Props) => (
-  <Kb.Box2 direction="vertical" fullWidth={true} onClick={e => e.stopPropagation()}>
+  <Kb.Box2 direction="vertical" fullWidth={true}>
     <Kb.Box2 direction="vertical" fullWidth={true}>
       <UserInput
         disableListBuilding={true}
@@ -20,17 +19,13 @@ const Search = (props: Props) => (
         showServiceFilter={true}
       />
     </Kb.Box2>
-    <Kb.Box2 direction="vertical" fullWidth={true} style={styles.listContainer}>
-      <SearchResultsList searchKey={searchKey} onClick={props.onClick} disableListBuilding={true} />
-    </Kb.Box2>
+    <SearchResultsList
+      style={{minHeight: 240, overflow: 'auto'}}
+      searchKey={searchKey}
+      onClick={props.onClick}
+      disableListBuilding={true}
+    />
   </Kb.Box2>
 )
-
-const styles = Styles.styleSheetCreate({
-  listContainer: {
-    ...Styles.desktopStyles.scrollable,
-    justifyContent: 'center',
-  },
-})
 
 export default Search
