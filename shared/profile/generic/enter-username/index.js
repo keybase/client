aecbc772f90afa427cbb1ab1930548d8d519004e
Daniel Ayoub <danny.a95@gmail.com>
2019-03-11 15:34:37 -0400

    changes for mobile

--- shared/profile/generic/enter-username/index.js
+++ shared/profile/generic/enter-username/index.js
@@ -2,6 +2,7 @@
 import * as React from 'react'
 import * as Kb from '../../../common-adapters'
 import * as Styles from '../../../styles'
+import {isLargeScreen} from '../../../constants/platform'
 import {SiteIcon} from '../shared'
 import type {SiteIconSet} from '../../../constants/types/tracker2'
 
@@ -53,7 +54,7 @@ class EnterUsernameInput extends React.Component<InputProps, InputState> {
             full={false}
             style={this.state.username ? styles.opacity75 : styles.opacity40}
           />
-          <Kb.Box2 direction="horizontal" style={styles.inputPlaceholderContainer} fullWidth={true}>
+          <Kb.Box2 direction="horizontal" style={styles.positionRelative} fullWidth={true}>
             <Kb.PlainInput
               flexable={true}
               textType="BodySemibold"
@@ -61,19 +62,22 @@ class EnterUsernameInput extends React.Component<InputProps, InputState> {
               onChangeText={this._onChangeUsername}
               onFocus={this._onFocus}
               onBlur={this._onBlur}
+              style={styles.marginRightMedium}
             />
-            <Kb.Text type="BodySemibold" style={styles.invisible}>
-              {/* spacer to keep the input from going this far */}@{this.props.serviceName}
-            </Kb.Text>
-            <Kb.Box2 direction="horizontal" style={styles.inputPlaceholder}>
-              <Kb.Text
-                type="BodySemibold"
-                style={Styles.collapseStyles([styles.placeholder, !!this.state.username && styles.invisible])}
-              >
-                {this.state.username || 'Your username'}
-              </Kb.Text>
-              <Kb.Text type="BodySemibold" style={styles.placeholderService}>
-                @{this.props.serviceName}
+            <Kb.Box2 direction="horizontal" style={styles.inputPlaceholder} pointerEvents="none">
+              <Kb.Text type="BodySemibold" lineClamp={1} style={styles.paddingRightTiny}>
+                <Kb.Text
+                  type="BodySemibold"
+                  style={Styles.collapseStyles([
+                    styles.placeholder,
+                    !!this.state.username && styles.invisible,
+                  ])}
+                >
+                  {this.state.username || 'Your username'}
+                </Kb.Text>
+                <Kb.Text type="BodySemibold" style={styles.placeholderService}>
+                  @{this.props.serviceName}
+                </Kb.Text>
               </Kb.Text>
             </Kb.Box2>
           </Kb.Box2>
@@ -89,13 +93,14 @@ const Unreachable = props => (
     gap="xtiny"
     alignItems="flex-start"
     style={Styles.collapseStyles([styles.inputBox, styles.unreachableBox])}
+    fullWidth={Styles.isMobile}
   >
     <SiteIcon
       set={props.serviceIcon}
       full={false}
       style={Styles.collapseStyles([styles.opacity75, styles.inlineIcon])}
     />
-    <Kb.Box2 direction="vertical">
+    <Kb.Box2 direction="vertical" style={styles.flexOne}>
       <Kb.Text type="BodySemibold" style={styles.placeholder}>
         <Kb.Text type="BodySemibold" style={styles.colorRed}>
           {props.username}
@@ -132,7 +137,13 @@ const _EnterUsername = (props: Props) => (
       <Kb.BackButton onClick={props.onBack} style={styles.backButton} />
     )}
     <Kb.Box2 alignItems="center" direction="vertical" gap="xtiny" style={styles.serviceIconHeaderContainer}>
-      <SiteIcon set={props.serviceIconFull} full={true} style={styles.serviceIconFull} />
+      <Kb.Box2 direction="vertical" style={styles.positionRelative}>
+        <SiteIcon set={props.serviceIconFull} full={true} style={styles.serviceIconFull} />
+        <Kb.Icon
+          type={props.unreachable ? 'icon-proof-broken' : 'icon-proof-unfinished'}
+          style={styles.serviceProofIcon}
+        />
+      </Kb.Box2>
       <Kb.Box2 direction="vertical" alignItems="center">
         <Kb.Text type="BodySemibold">{props.serviceName}</Kb.Text>
         <Kb.Text type="BodySmall">{props.serviceSub}</Kb.Text>
@@ -180,7 +191,7 @@ const _EnterUsername = (props: Props) => (
         <Kb.Button
           type="PrimaryGreen"
           onClick={props.onSubmit}
-          label={`Authorize on ${props.serviceName}`}
+          label={`${isLargeScreen ? 'Authorize on' : 'Go to'} ${props.serviceName}`}
           style={styles.buttonBig}
         />
       </Kb.ButtonBar>
@@ -202,6 +213,7 @@ const styles = Styles.styleSheetCreate({
   colorBlue: {color: Styles.globalColors.blue},
   colorRed: {color: Styles.globalColors.red},
   container: Styles.platformStyles({isElectron: {height: 485, width: 560}}),
+  flexOne: {flex: 1},
   inlineIcon: {
     position: 'relative',
     top: 1,
@@ -222,35 +234,32 @@ const styles = Styles.styleSheetCreate({
     flex: 1,
     justifyContent: 'center',
   },
-  inputPlaceholder: Styles.platformStyles({
-    common: {
-      left: 1,
-      position: 'absolute',
-      right: 0,
-      top: 1,
-    },
-    isElectron: {
-      pointerEvents: 'none',
-    },
-  }),
-  inputPlaceholderContainer: {
-    position: 'relative',
+  inputPlaceholder: {
+    left: 1,
+    position: 'absolute',
+    right: 0,
+    top: 1,
   },
   invisible: {
     opacity: 0,
   },
   marginLeftAuto: {marginLeft: 'auto'},
+  marginRightMedium: {marginRight: Styles.globalMargins.medium},
   opacity40: {
     opacity: 0.4,
   },
   opacity75: {
     opacity: 0.75,
   },
+  paddingRightTiny: {paddingRight: Styles.globalMargins.tiny},
   placeholder: {
     color: Styles.globalColors.black_40,
   },
   placeholderService: {
     color: Styles.globalColors.black_20,
+  },
+  positionRelative: {
+    position: 'relative',
   },
   serviceIconFull: {
     height: 64,
@@ -259,7 +268,11 @@ const styles = Styles.styleSheetCreate({
   serviceIconHeaderContainer: {
     paddingTop: Styles.globalMargins.medium,
   },
-  unreachableBox: {...Styles.padding(Styles.globalMargins.tiny, Styles.globalMargins.xsmall), width: 360},
+  serviceProofIcon: {bottom: 0, position: 'absolute', right: 0},
+  unreachableBox: Styles.platformStyles({
+    common: {...Styles.padding(Styles.globalMargins.tiny, Styles.globalMargins.xsmall)},
+    isElectron: {width: 360},
+  }),
   warningText: {color: Styles.globalColors.brown_75, marginTop: Styles.globalMargins.small},
 })
 
