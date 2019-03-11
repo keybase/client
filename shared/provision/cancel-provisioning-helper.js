59560e0cde0076fd5176978d5a8325106ad1f390
Marco Munizaga <marco@marcopolo.io>
2019-03-12 00:04:15 +0100

    Simpler provision helper

--- shared/provision/cancel-provisioning-helper.js
+++ shared/provision/cancel-provisioning-helper.js
@@ -1,32 +1,19 @@
 // @flow
 import * as React from 'react'
 
-export class CancelProvisioningHelper extends React.Component<{
-  navigation: any,
-  children: React.Node,
-}> {
-  componentDidMount() {
-    const {navigation} = this.props
-    navigation.setParams({
-      customOnBack: () => navigation.popToTop(),
-    })
-  }
-  render() {
-    return this.props.children
-  }
-}
-
-export default function CancelProvisioningHelperHoc<P: {}>(
+export default function CancelProvisioningHelperHoc<P>(
   Component: React.AbstractComponent<P>
-): React.AbstractComponent<P & {navigation: any}> {
-  const _C = (p: P & {navigation: any}) => (
-    <CancelProvisioningHelper navigation={p.navigation}>
-      <Component {...(p: P)} />
-    </CancelProvisioningHelper>
-  )
+): React.AbstractComponent<P> {
+  // $FlowIssue
+  Component.navigationOptions = ({navigation}) => {
+    if (!navigation.getParam('customOnBack')) {
+      navigation.setParams({
+        customOnBack: () => navigation.popToTop(),
+      })
+    }
 
-  // Setting header to undefined unsets the null setting from before.
-  // This lets the header show up on native
-  _C.navigationOptions = {header: undefined}
-  return _C
+    return {header: undefined}
+  }
+
+  return Component
 }
