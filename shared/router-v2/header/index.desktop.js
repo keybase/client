4432c14d3d5d7a18174a2b0ce365be1f86fda777
Chris Ball <chris@printf.net>
2019-03-19 13:03:51 -0700

    Revert "Draggable area is doesn't extend down past the back button"
    This reverts commit fffaeb49cbd258e4c5dde82968811bb2077c510b.

--- shared/router-v2/header/index.desktop.js
+++ shared/router-v2/header/index.desktop.js
@@ -67,7 +67,6 @@ const styles = Styles.styleSheetCreate({
   }),
   headerBack: Styles.platformStyles({
     isElectron: {
-      ...Styles.desktopStyles.windowDragging,
       alignItems: 'center',
       height: 40,
       padding: 12,
@@ -80,6 +79,7 @@ const styles = Styles.styleSheetCreate({
   },
   headerContainer: Styles.platformStyles({
     isElectron: {
+      ...Styles.desktopStyles.windowDragging,
       alignItems: 'center',
     },
   }),
