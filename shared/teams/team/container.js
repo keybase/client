94a6195ea6b65c3ecafeb5f1582ebc376838c61d
Daniel Ayoub <danny.a95@gmail.com>
2019-03-19 15:59:51 -0400

    fancy team header

--- shared/teams/team/container.js
+++ shared/teams/team/container.js
@@ -3,13 +3,15 @@ import * as React from 'react'
 import * as TeamsGen from '../../actions/teams-gen'
 import Team from '.'
 import CustomTitle from './custom-title/container'
+import {HeaderRightActions, HeaderTitle} from './nav-header/container'
 import * as Kb from '../../common-adapters'
-import {connect, compose} from '../../util/container'
+import {connect, compose, isMobile} from '../../util/container'
 import * as Constants from '../../constants/teams'
 import {mapStateHelper as invitesMapStateHelper, getRows as getInviteRows} from './invites-tab/helper'
 import {mapStateHelper as memberMapStateHelper, getRows as getMemberRows} from './members-tab/helper'
 import {mapStateHelper as subteamsMapStateHelper, getRows as getSubteamsRows} from './subteams-tab/helper'
 import type {RouteProps} from '../../route-tree/render-route'
+import flags from '../../util/feature-flags'
 
 // $FlowIssue
 type OwnProps = RouteProps<{teamname: string}, {}> & {selectedTab: string, setSelectedTab: string => void}
@@ -59,7 +61,11 @@ const mergeProps = (stateProps, dispatchProps) => {
       tabSpecificRows = [{type: 'settings'}]
       break
   }
-  const rows = [{type: 'header'}, {type: 'tabs'}, ...tabSpecificRows]
+  const rows = [
+    ...(flags.useNewRouter && !isMobile ? [] : [{type: 'header'}]),
+    {type: 'tabs'},
+    ...tabSpecificRows,
+  ]
   const customComponent = <CustomTitle teamname={stateProps.teamname} />
   return {
     _load: () => dispatchProps._loadTeam(stateProps.teamname),
@@ -112,7 +118,10 @@ type State = {|selectedTab: string|}
 // We don't use route state anymore
 class TabsState extends React.PureComponent<React.ElementConfig<typeof Team>, State> {
   static navigationOptions = {
-    headerRightActions: ({params}: NavigationParams) => <CustomTitle teamname={params.teamname} />,
+    header: undefined,
+    headerHideBorder: true,
+    headerRightActions: ({params}: NavigationParams) => <HeaderRightActions teamname={params.teamname} />,
+    headerTitle: ({params}: NavigationParams) => <HeaderTitle teamname={params.teamname} />,
   }
   state = {selectedTab: 'members'}
   _setSelectedTab = selectedTab => {
