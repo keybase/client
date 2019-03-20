e979b6b24325ac633e218b9876855042f60bfc42
Chris Ball <chris@printf.net>
2019-03-20 15:51:17 -0700

    Only allow clicks on 'manage chat channels' icon on teams list if you're a member of the team

--- shared/teams/main/index.js
+++ shared/teams/main/index.js
@@ -21,6 +21,7 @@ type Props = {|
   onReadMore: () => void,
   onViewTeam: string => void,
   sawChatBanner: boolean,
+  teamNameToCanManageChat: {[key: string]: boolean},
   teamNameToIsOpen: {[key: string]: boolean},
   teammembercounts: {[key: string]: number},
   teamnames: $ReadOnlyArray<string>,
@@ -37,14 +38,13 @@ type RowProps = {
   isOpen: boolean,
   newRequests: number,
   onOpenFolder: () => void,
-  onManageChat: () => void,
+  onManageChat: ?() => void,
   resetUserCount: number,
   onViewTeam: () => void,
 }
 
 export const TeamRow = React.memo<RowProps>((props: RowProps) => {
   const badgeCount = props.newRequests + props.resetUserCount
-
   return (
     <Kb.ListItem2
       type="Small"
@@ -76,7 +76,7 @@ export const TeamRow = React.memo<RowProps>((props: RowProps) => {
         Styles.isMobile ? null : (
           <Kb.Box2 direction="horizontal" gap="small" gapEnd={true} gapStart={true}>
             {props.onOpenFolder && <Kb.Icon type="iconfont-folder-private" onClick={props.onOpenFolder} />}
-            {props.onManageChat && <Kb.Icon type="iconfont-chat" onClick={props.onManageChat} />}
+            <Kb.Icon type="iconfont-chat" onClick={props.onManageChat} />
           </Kb.Box2>
         )
       }
@@ -125,7 +125,7 @@ class Teams extends React.PureComponent<Props, State> {
             newRequests={this.props.teamToRequest[name] ?? 0}
             membercount={this.props.teammembercounts[name]}
             onOpenFolder={() => this._onOpenFolder(name)}
-            onManageChat={() => this._onManageChat(name)}
+            onManageChat={this.props.teamNameToCanManageChat[name] ? () => this._onManageChat(name) : null}
             onViewTeam={() => this._onViewTeam(name)}
             resetUserCount={resetUserCount}
           />
