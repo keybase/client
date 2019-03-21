c44b14d8141a7b670747cebe8f2c387c8d6baf95
Jeremy Stribling <strib@alum.mit.edu>
2019-03-21 14:23:18 -0700

    mocks: use reflect mode with mockgen, to avoid import errors
    We used to use "source" mode, which apparently now doesn't like
    packages named "context" and "errors" being imported two different
    ways.
    
    Unfortunately this means we have to explicitly list every symbol we
    want mocked.

--- go/kbfs/libkbfs/gen_mocks.sh
+++ go/kbfs/libkbfs/gen_mocks.sh
@@ -1,4 +1,44 @@
 #!/usr/bin/env bash
 
-mockgen -package="libkbfs" -source=interfaces.go > mocks_test.go
-go fmt mocks_test.go
+function gen {
+    srcPkg=$1
+    dst=$2
+    symbols=$3
+
+    tmp=`tempfile`
+    mockgen --package="libkbfs" \
+        --self_package github.com/keybase/client/go/kbfs/libkbfs \
+        $srcPkg $symbols > $tmp
+    dstFile=${dst}_mocks_test.go
+    mv $tmp $dstFile
+    go fmt $dstFile
+}
+
+gen github.com/keybase/client/go/kbfs/libkey libkey \
+KeyOps,\
+KeyServer
+
+gen github.com/keybase/client/go/kbfs/libkbfs libkbfs \
+BlockCache,\
+BlockOps,\
+BlockServer,\
+BlockSplitter,\
+BlockWithPtrs,\
+Chat,\
+Clock,\
+Crypto,\
+DirtyBlockCache,\
+KBFSOps,\
+KBPKI,\
+KeybaseService,\
+KeyCache,\
+KeyManager,\
+MDCache,\
+MDOps,\
+MDServer,\
+Node,\
+NodeCache,\
+NodeID,\
+Notifier,\
+RekeyQueue,\
+Reporter
