6b7c7036fa7e4b360fc48501525a847553ca920e
Marco Munizaga <marco@marcopolo.io>
2019-03-25 18:55:13 +0100

    Remove dispatchNav2Action

--- shared/actions/route-tree.js
+++ shared/actions/route-tree.js
@@ -2,7 +2,6 @@
 import * as I from 'immutable'
 import * as Saga from '../util/saga'
 import * as RouteTreeGen from './route-tree-gen'
-import {_getNavigator} from '../constants/router2'
 import {getPath} from '../route-tree'
 
 import type {Path} from '../route-tree'
@@ -19,16 +18,8 @@ const putActionIfOnPath = (state, {payload: {otherAction, expectedPath, parentPa
   }
 }
 
-const dispatchNav2Action = (state, {payload: {action}}) => {
-  const navigator = _getNavigator()
-  if (navigator) {
-    navigator.dispatch(action)
-  }
-}
-
 function* routeSaga(): any {
   yield* Saga.chainAction(RouteTreeGen.putActionIfOnPath, putActionIfOnPath)
-  yield* Saga.chainAction(RouteTreeGen.dispatchNav2Action, dispatchNav2Action)
 }
 
 export default routeSaga
