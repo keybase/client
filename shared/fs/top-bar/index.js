37a95581facba27dcdb704560528f023b05379e0
Song Gao <song@gao.io>
2019-03-29 14:37:06 -0700

    split sortbar into its own component and have a top-bar around

---
+++ shared/fs/top-bar/index.js
@@ -0,0 +1,47 @@
+// @flow
+import * as React from 'react'
+import * as Styles from '../../styles'
+import * as Kb from '../../common-adapters'
+import * as Types from '../../constants/types/fs'
+import * as RowTypes from '../row/types'
+import Loading from './loading'
+import Sort from './sort-container'
+
+type Props = {|
+  path: Types.Path,
+|}
+
+const TopBar = (props: Props) => (
+  <Kb.Box2
+    direction="horizontal"
+    style={styles.container}
+    gap="small"
+    gapStart={true}
+    gapEnd={true}
+    centerChildren={true}
+  >
+    <Loading path={props.path} />
+    <Kb.Box style={styles.flex} />
+    <Sort path={props.path} />
+  </Kb.Box2>
+)
+
+export const height = Styles.isMobile ? 40 : 32
+
+const styles = Styles.styleSheetCreate({
+  container: {
+    backgroundColor: Styles.globalColors.blue5,
+    height,
+  },
+  flex: {flex: 1},
+})
+
+export default TopBar
+export const asRows = (path: Types.Path): Array<RowTypes.RowItemWithKey> => [
+  {
+    height,
+    key: 'top-bar',
+    node: <TopBar path={path} />,
+    rowType: 'header',
+  },
+] // We always show this, but just fill with blank at /keybase
---
+++ shared/fs/top-bar/sort.js
@@ -0,0 +1,108 @@
+// @flow
+import * as React from 'react'
+import * as Styles from '../../styles'
+import * as Kb from '../../common-adapters'
+import * as Types from '../../constants/types/fs'
+import * as Flow from '../../util/flow'
+
+export type SortBarProps = {|
+  sortByNameAsc?: ?() => void,
+  sortByNameDesc?: ?() => void,
+  sortByTimeAsc?: ?() => void,
+  sortByTimeDesc?: ?() => void,
+  sortSetting?: ?Types.SortSetting,
+|}
+
+const getTextFromSortSetting = (sortSetting: Types.SortSetting) => {
+  switch (sortSetting) {
+    case 'name-asc':
+      return 'Name ascending'
+    case 'name-desc':
+      return 'Name descending'
+    case 'time-asc':
+      return 'Recent first'
+    case 'time-desc':
+      return 'Older first'
+    default:
+      Flow.ifFlowComplainsAboutThisFunctionYouHaventHandledAllCasesInASwitch(sortSetting)
+      return 'Name ascending'
+  }
+}
+
+const getIconFromSortSetting = (sortSetting: Types.SortSetting) => {
+  switch (sortSetting) {
+    case 'name-asc':
+      return 'iconfont-arrow-full-down'
+    case 'name-desc':
+      return 'iconfont-arrow-full-up'
+    case 'time-asc':
+      return 'iconfont-time'
+    case 'time-desc':
+      return 'iconfont-time-reversed'
+    default:
+      Flow.ifFlowComplainsAboutThisFunctionYouHaventHandledAllCasesInASwitch(sortSetting)
+      return 'iconfont-arrow-full-down'
+  }
+}
+
+const SortOption = ({mode, sortSetting}) => (
+  <Kb.Box2
+    direction="horizontal"
+    fullWidth={true}
+    gap={mode === 'menu' ? 'xtiny' : 'xxtiny'}
+    centerChildren={Styles.isMobile}
+  >
+    <Kb.Icon
+      type={getIconFromSortSetting(sortSetting)}
+      padding="xtiny"
+      color={mode === 'menu' && Styles.isMobile ? Styles.globalColors.blue : undefined}
+      sizeType={mode === 'bar' ? 'Small' : 'Default'}
+    />
+    <Kb.Box2 direction="vertical" fullHeight={true} centerChildren={true}>
+      <Kb.Text
+        type={mode === 'bar' ? 'BodySmallSemibold' : Styles.isMobile ? 'BodyBig' : 'Body'}
+        style={mode === 'menu' ? styles.blueMobile : undefined}
+      >
+        {getTextFromSortSetting(sortSetting)}
+      </Kb.Text>
+    </Kb.Box2>
+  </Kb.Box2>
+)
+
+const makeSortOptionItem = (sortSetting, onClick) => ({
+  onClick,
+  title: getTextFromSortSetting(sortSetting),
+  view: <SortOption mode="menu" sortSetting={sortSetting} />,
+})
+
+const Sort = (props: SortBarProps & Kb.OverlayParentProps) =>
+  !!props.sortSetting && (
+    <>
+      <Kb.ClickableBox onClick={props.toggleShowingMenu} ref={props.setAttachmentRef}>
+        <SortOption mode="bar" sortSetting={props.sortSetting} />
+      </Kb.ClickableBox>
+      <Kb.FloatingMenu
+        attachTo={props.getAttachmentRef}
+        visible={props.showingMenu}
+        onHidden={props.toggleShowingMenu}
+        position="bottom right"
+        closeOnSelect={true}
+        items={[
+          ...(props.sortByNameAsc ? [makeSortOptionItem('name-asc', props.sortByNameAsc)] : []),
+          ...(props.sortByNameDesc ? [makeSortOptionItem('name-desc', props.sortByNameDesc)] : []),
+          ...(props.sortByTimeAsc ? [makeSortOptionItem('time-asc', props.sortByTimeAsc)] : []),
+          ...(props.sortByTimeDesc ? [makeSortOptionItem('time-desc', props.sortByTimeDesc)] : []),
+        ]}
+      />
+    </>
+  )
+
+const styles = Styles.styleSheetCreate({
+  blueMobile: Styles.platformStyles({
+    isMobile: {
+      color: Styles.globalColors.blue,
+    },
+  }),
+})
+
+export default Kb.OverlayParentHOC(Sort)
