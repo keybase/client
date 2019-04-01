4e5abc9f9105fcc6cadc26f8355cbcb99a78e8c7
Daniel Ayoub <danny.a95@gmail.com>
2019-04-01 11:59:49 -0400

    enter devicename

--- shared/signup/device-name/index.js
+++ shared/signup/device-name/index.js
@@ -1,1 +1,63 @@
 // @flow
+import * as React from 'react'
+import * as Kb from '../../common-adapters'
+import * as Styles from '../../styles'
+import {SignupScreen} from '../common'
+
+type Props = {|
+  onBack: () => void,
+  onChangeDevicename: () => void,
+  onContinue: () => void,
+|}
+
+const EnterDevicename = (props: Props) => (
+  <SignupScreen
+    buttons={[{label: 'Continue', onClick: props.onContinue, type: 'PrimaryGreen'}]}
+    onBack={props.onBack}
+    title={Styles.isMobile ? 'Name this phone' : 'Name this computer'}
+  >
+    <Kb.Box2
+      alignItems="center"
+      direction="vertical"
+      gap={Styles.isMobile ? 'small' : 'medium'}
+      fullWidth={true}
+      style={styles.flexOne}
+    >
+      <Kb.Icon type={Styles.isMobile ? 'icon-phone-96' : 'icon-computer-96'} />
+      <Kb.Box2 direction="vertical" gap="tiny">
+        <Kb.NewInput
+          autoFocus={true}
+          containerStyle={styles.input}
+          placeholder={Styles.isMobile ? 'Phone 1' : 'Computer 1'}
+          onChangeText={props.onChangeDevicename}
+        />
+        <Kb.Text type="BodySmall" style={styles.inputSub}>
+          Your device name will be public.
+        </Kb.Text>
+      </Kb.Box2>
+    </Kb.Box2>
+  </SignupScreen>
+)
+
+const styles = Styles.styleSheetCreate({
+  flexOne: {
+    flex: 1,
+  },
+  input: Styles.platformStyles({
+    common: {},
+    isElectron: {
+      ...Styles.padding(0, Styles.globalMargins.xsmall),
+      height: 38,
+      width: 368,
+    },
+    isMobile: {
+      ...Styles.padding(0, Styles.globalMargins.small),
+      height: 48,
+    },
+  }),
+  inputSub: {
+    marginLeft: 2,
+  },
+})
+
+export default EnterDevicename
--- shared/signup/index.stories.js
+++ shared/signup/index.stories.js
@@ -1,10 +1,12 @@
 // @flow
 import joinOrLogin from './join-or-login/index.stories'
 import username from './username/index.stories'
+import devicename from './device-name/index.stories'
 
 const load = () => {
   joinOrLogin()
   username()
+  devicename()
 }
 
 export default load
