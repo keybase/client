2c13e9a6c74735b37a444a2463c399937b1d8820
Frederick Akalin <akalin@keyba.se>
2019-04-03 20:21:28 -0700

    Fix bug

--- shared/profile/search/container.js
+++ shared/profile/search/container.js
@@ -1,21 +1,45 @@
 // @flow
 import Search from '.'
 import {createShowUserProfile} from '../../actions/profile-gen'
-import {connect} from '../../util/container'
+import {connect, type RouteProps} from '../../util/container'
 
-type OwnProps = {|onClose: () => void|}
+// Either onClose is passed in from bar.js or we're instantiated via a
+// route.
+type OwnProps = {|onClose: () => void|} | RouteProps<{}, {}>
 
-const mapDispatchToProps = (dispatch, ownProps) => ({
-  onClick: username => {
-    ownProps.onClose()
-    dispatch(createShowUserProfile({username}))
-  },
-})
+const mapDispatchToProps = (dispatch, ownProps) => {
+  const onClick = username => dispatch(createShowUserProfile({username}))
+  if (ownProps.onClose) {
+    // onClosed passed in, so use it.
+    // $ForceType confused by non-disjoint union.
+    const onClose: () => void = ownProps.onClose
+    return {
+      onClick,
+      onClose,
+    }
+  } else {
+    // Instantiated via route, so just navigate up on close.
+    return {
+      onClick,
+      onClose: () => dispatch(ownProps.navigateUp()),
+    }
+  }
+}
+
+const mergeProps = (_, dispatchProps, ownProps) => {
+  return {
+    onClick: username => {
+      dispatchProps.onClose()
+      dispatchProps.onClick(username)
+    },
+    onClose: dispatchProps.onClose,
+  }
+}
 
 const connected = connect<OwnProps, _, _, _, _>(
   () => ({}),
   mapDispatchToProps,
-  (s, d, o) => ({...o, ...s, ...d})
+  mergeProps
 )(Search)
 
 export default connected
