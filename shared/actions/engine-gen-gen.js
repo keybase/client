e1318f4efd1407d3988e0043291968877f38049a
Piotr Zduniak <piotr@zduniak.net>
2019-04-07 01:05:31 +0200

    build-actions

--- shared/actions/engine-gen-gen.js
+++ shared/actions/engine-gen-gen.js
@@ -95,7 +95,9 @@ export const keybase1IdentifyUiStart = 'engine-gen:keybase1IdentifyUiStart'
 export const keybase1LogUiLog = 'engine-gen:keybase1LogUiLog'
 export const keybase1LoginUiDisplayPaperKeyPhrase = 'engine-gen:keybase1LoginUiDisplayPaperKeyPhrase'
 export const keybase1LoginUiDisplayPrimaryPaperKey = 'engine-gen:keybase1LoginUiDisplayPrimaryPaperKey'
+export const keybase1LoginUiDisplayResetProgress = 'engine-gen:keybase1LoginUiDisplayResetProgress'
 export const keybase1LoginUiGetEmailOrUsername = 'engine-gen:keybase1LoginUiGetEmailOrUsername'
+export const keybase1LoginUiPromptResetAccount = 'engine-gen:keybase1LoginUiPromptResetAccount'
 export const keybase1LoginUiPromptRevokePaperKeys = 'engine-gen:keybase1LoginUiPromptRevokePaperKeys'
 export const keybase1LogsendPrepareLogsend = 'engine-gen:keybase1LogsendPrepareLogsend'
 export const keybase1NotifyAppExit = 'engine-gen:keybase1NotifyAppExit'
@@ -264,7 +266,9 @@ type _Keybase1IdentifyUiStartPayload = $ReadOnly<{|params: $Exact<$PropertyType<
 type _Keybase1LogUiLogPayload = $ReadOnly<{|params: $Exact<$PropertyType<$PropertyType<keybase1Types.MessageTypes, 'keybase.1.logUi.log'>, 'inParam'>> & {|sessionID: number|}, response: {error: keybase1Types.IncomingErrorCallback, result: ($PropertyType<$PropertyType<keybase1Types.MessageTypes, 'keybase.1.logUi.log'>, 'outParam'>) => void}|}>
 type _Keybase1LoginUiDisplayPaperKeyPhrasePayload = $ReadOnly<{|params: $Exact<$PropertyType<$PropertyType<keybase1Types.MessageTypes, 'keybase.1.loginUi.displayPaperKeyPhrase'>, 'inParam'>> & {|sessionID: number|}, response: {error: keybase1Types.IncomingErrorCallback, result: ($PropertyType<$PropertyType<keybase1Types.MessageTypes, 'keybase.1.loginUi.displayPaperKeyPhrase'>, 'outParam'>) => void}|}>
 type _Keybase1LoginUiDisplayPrimaryPaperKeyPayload = $ReadOnly<{|params: $Exact<$PropertyType<$PropertyType<keybase1Types.MessageTypes, 'keybase.1.loginUi.displayPrimaryPaperKey'>, 'inParam'>> & {|sessionID: number|}, response: {error: keybase1Types.IncomingErrorCallback, result: ($PropertyType<$PropertyType<keybase1Types.MessageTypes, 'keybase.1.loginUi.displayPrimaryPaperKey'>, 'outParam'>) => void}|}>
+type _Keybase1LoginUiDisplayResetProgressPayload = $ReadOnly<{|params: $Exact<$PropertyType<$PropertyType<keybase1Types.MessageTypes, 'keybase.1.loginUi.displayResetProgress'>, 'inParam'>> & {|sessionID: number|}, response: {error: keybase1Types.IncomingErrorCallback, result: ($PropertyType<$PropertyType<keybase1Types.MessageTypes, 'keybase.1.loginUi.displayResetProgress'>, 'outParam'>) => void}|}>
 type _Keybase1LoginUiGetEmailOrUsernamePayload = $ReadOnly<{|params: $Exact<$PropertyType<$PropertyType<keybase1Types.MessageTypes, 'keybase.1.loginUi.getEmailOrUsername'>, 'inParam'>> & {|sessionID: number|}, response: {error: keybase1Types.IncomingErrorCallback, result: ($PropertyType<$PropertyType<keybase1Types.MessageTypes, 'keybase.1.loginUi.getEmailOrUsername'>, 'outParam'>) => void}|}>
+type _Keybase1LoginUiPromptResetAccountPayload = $ReadOnly<{|params: $Exact<$PropertyType<$PropertyType<keybase1Types.MessageTypes, 'keybase.1.loginUi.promptResetAccount'>, 'inParam'>> & {|sessionID: number|}, response: {error: keybase1Types.IncomingErrorCallback, result: ($PropertyType<$PropertyType<keybase1Types.MessageTypes, 'keybase.1.loginUi.promptResetAccount'>, 'outParam'>) => void}|}>
 type _Keybase1LoginUiPromptRevokePaperKeysPayload = $ReadOnly<{|params: $Exact<$PropertyType<$PropertyType<keybase1Types.MessageTypes, 'keybase.1.loginUi.promptRevokePaperKeys'>, 'inParam'>> & {|sessionID: number|}, response: {error: keybase1Types.IncomingErrorCallback, result: ($PropertyType<$PropertyType<keybase1Types.MessageTypes, 'keybase.1.loginUi.promptRevokePaperKeys'>, 'outParam'>) => void}|}>
 type _Keybase1LogsendPrepareLogsendPayload = $ReadOnly<{|params: $Exact<$PropertyType<$PropertyType<keybase1Types.MessageTypes, 'keybase.1.logsend.prepareLogsend'>, 'inParam'>> & {|sessionID: number|}, response: {error: keybase1Types.IncomingErrorCallback, result: ($PropertyType<$PropertyType<keybase1Types.MessageTypes, 'keybase.1.logsend.prepareLogsend'>, 'outParam'>) => void}|}>
 type _Keybase1NotifyAppExitPayload = $ReadOnly<{|params: $Exact<$PropertyType<$PropertyType<keybase1Types.MessageTypes, 'keybase.1.NotifyApp.exit'>, 'inParam'>> & {|sessionID: number|}, response: {error: keybase1Types.IncomingErrorCallback, result: ($PropertyType<$PropertyType<keybase1Types.MessageTypes, 'keybase.1.NotifyApp.exit'>, 'outParam'>) => void}|}>
@@ -433,7 +437,9 @@ export const createKeybase1IdentifyUiStart = (payload: _Keybase1IdentifyUiStartP
 export const createKeybase1LogUiLog = (payload: _Keybase1LogUiLogPayload) => ({payload, type: keybase1LogUiLog})
 export const createKeybase1LoginUiDisplayPaperKeyPhrase = (payload: _Keybase1LoginUiDisplayPaperKeyPhrasePayload) => ({payload, type: keybase1LoginUiDisplayPaperKeyPhrase})
 export const createKeybase1LoginUiDisplayPrimaryPaperKey = (payload: _Keybase1LoginUiDisplayPrimaryPaperKeyPayload) => ({payload, type: keybase1LoginUiDisplayPrimaryPaperKey})
+export const createKeybase1LoginUiDisplayResetProgress = (payload: _Keybase1LoginUiDisplayResetProgressPayload) => ({payload, type: keybase1LoginUiDisplayResetProgress})
 export const createKeybase1LoginUiGetEmailOrUsername = (payload: _Keybase1LoginUiGetEmailOrUsernamePayload) => ({payload, type: keybase1LoginUiGetEmailOrUsername})
+export const createKeybase1LoginUiPromptResetAccount = (payload: _Keybase1LoginUiPromptResetAccountPayload) => ({payload, type: keybase1LoginUiPromptResetAccount})
 export const createKeybase1LoginUiPromptRevokePaperKeys = (payload: _Keybase1LoginUiPromptRevokePaperKeysPayload) => ({payload, type: keybase1LoginUiPromptRevokePaperKeys})
 export const createKeybase1LogsendPrepareLogsend = (payload: _Keybase1LogsendPrepareLogsendPayload) => ({payload, type: keybase1LogsendPrepareLogsend})
 export const createKeybase1NotifyAppExit = (payload: _Keybase1NotifyAppExitPayload) => ({payload, type: keybase1NotifyAppExit})
@@ -602,7 +608,9 @@ export type Keybase1IdentifyUiStartPayload = {|+payload: _Keybase1IdentifyUiStar
 export type Keybase1LogUiLogPayload = {|+payload: _Keybase1LogUiLogPayload, +type: 'engine-gen:keybase1LogUiLog'|}
 export type Keybase1LoginUiDisplayPaperKeyPhrasePayload = {|+payload: _Keybase1LoginUiDisplayPaperKeyPhrasePayload, +type: 'engine-gen:keybase1LoginUiDisplayPaperKeyPhrase'|}
 export type Keybase1LoginUiDisplayPrimaryPaperKeyPayload = {|+payload: _Keybase1LoginUiDisplayPrimaryPaperKeyPayload, +type: 'engine-gen:keybase1LoginUiDisplayPrimaryPaperKey'|}
+export type Keybase1LoginUiDisplayResetProgressPayload = {|+payload: _Keybase1LoginUiDisplayResetProgressPayload, +type: 'engine-gen:keybase1LoginUiDisplayResetProgress'|}
 export type Keybase1LoginUiGetEmailOrUsernamePayload = {|+payload: _Keybase1LoginUiGetEmailOrUsernamePayload, +type: 'engine-gen:keybase1LoginUiGetEmailOrUsername'|}
+export type Keybase1LoginUiPromptResetAccountPayload = {|+payload: _Keybase1LoginUiPromptResetAccountPayload, +type: 'engine-gen:keybase1LoginUiPromptResetAccount'|}
 export type Keybase1LoginUiPromptRevokePaperKeysPayload = {|+payload: _Keybase1LoginUiPromptRevokePaperKeysPayload, +type: 'engine-gen:keybase1LoginUiPromptRevokePaperKeys'|}
 export type Keybase1LogsendPrepareLogsendPayload = {|+payload: _Keybase1LogsendPrepareLogsendPayload, +type: 'engine-gen:keybase1LogsendPrepareLogsend'|}
 export type Keybase1NotifyAppExitPayload = {|+payload: _Keybase1NotifyAppExitPayload, +type: 'engine-gen:keybase1NotifyAppExit'|}
@@ -773,7 +781,9 @@ export type Actions =
   | Keybase1LogUiLogPayload
   | Keybase1LoginUiDisplayPaperKeyPhrasePayload
   | Keybase1LoginUiDisplayPrimaryPaperKeyPayload
+  | Keybase1LoginUiDisplayResetProgressPayload
   | Keybase1LoginUiGetEmailOrUsernamePayload
+  | Keybase1LoginUiPromptResetAccountPayload
   | Keybase1LoginUiPromptRevokePaperKeysPayload
   | Keybase1LogsendPrepareLogsendPayload
   | Keybase1NotifyAppExitPayload
