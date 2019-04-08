f06970554a404bc5e1315dd39e49fca14276d81f
Miles Steele <miles@milessteele.com>
2019-04-08 17:13:23 -0400

    feedback

--- shared/profile/generic/proofs-list/container.js
+++ shared/profile/generic/proofs-list/container.js
@@ -21,20 +21,22 @@ const mergeProps = (stateProps, dispatchProps, ownProps) => ({
   onCancel: dispatchProps.onCancel,
   onClickLearn: () => openURL('https://keybase.io/docs/proof_integration_guide'),
   providerClicked: dispatchProps.providerClicked,
-  providers: promoteNew(stateProps._proofSuggestions
-    .map(s => ({
-      desc: s.pickerSubtext,
-      icon: s.pickerIcon,
-      key: s.assertionKey,
-      name: s.pickerText,
-      new: s.metas.some(({label}) => label === 'new'),
-    }))
-    .toArray()),
+  providers: promoteNew(
+    stateProps._proofSuggestions
+      .map(s => ({
+        desc: s.pickerSubtext,
+        icon: s.pickerIcon,
+        key: s.assertionKey,
+        name: s.pickerText,
+        new: s.metas.some(({label}) => label === 'new'),
+      }))
+      .toArray()
+  ),
   title: 'Prove your...',
 })
 
-const promoteNew = (rows) => {
-  return flatten(partition(rows, (row) => row.new).map(identity))
+const promoteNew = rows => {
+  return flatten(partition(rows, 'new').map(identity))
 }
 
 export default namedConnect<OwnProps, _, _, _, _>(
