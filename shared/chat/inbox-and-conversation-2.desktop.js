9c92873c5a7a2e8b95a6742e6dbda54e6395a9b6
Mike Maxim <mike.maxim@gmail.com>
2019-04-10 17:44:13 -0400

    wip

--- shared/chat/inbox-and-conversation-2.desktop.js
+++ shared/chat/inbox-and-conversation-2.desktop.js
@@ -3,11 +3,38 @@
 import * as React from 'react'
 import * as Kb from '../common-adapters'
 import Inbox from './inbox/container'
+import InboxSearch from './inbox-search/container'
 import Conversation from './conversation/container'
 import {type RouteProps} from '../route-tree/render-route'
 import Header from './header.desktop'
+import {namedConnect} from '../util/container'
 
 type Props = RouteProps<{}, {smallTeamsExpanded: boolean}> & {children: React.Node}
+
+type InboxSwitchProps = RouteProps<{}, {smallTeamsExpanded: boolean}> & {children: React.Node} & {|
+  searchEnabled: boolean,
+|}
+
+const InboxSwitch = (props: InboxSwitchProps) => {
+  return props.searchEnabled ? (
+    <InboxSearch />
+  ) : (
+    <Inbox routeState={props.routeState} navigateAppend={props.navigateAppend} />
+  )
+}
+
+const mapStateToProps = state => {
+  return {
+    searchEnabled: !!state.chat2.inboxSearch,
+  }
+}
+
+const InboxSwitchConnected = namedConnect<{}, _, _, _, _>(
+  mapStateToProps,
+  () => ({}),
+  s => ({...s}),
+  'InboxSwitchConnected'
+)(InboxSwitch)
 
 class InboxAndConversation extends React.PureComponent<Props> {
   static navigationOptions = {
@@ -17,7 +44,7 @@ class InboxAndConversation extends React.PureComponent<Props> {
   render() {
     return (
       <Kb.Box2 direction="horizontal" fullWidth={true} fullHeight={true}>
-        <Inbox routeState={this.props.routeState} navigateAppend={this.props.navigateAppend} />
+        <InboxSwitchConnected />
         <Conversation />
       </Kb.Box2>
     )
