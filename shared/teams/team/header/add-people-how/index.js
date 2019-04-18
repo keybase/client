ed30dc9afc36677549c652ba505a88ccfd0adff0
Chris Coyne <ccoyne77@gmail.com>
2019-04-18 17:18:02 -0400

    changed menu wording

--- shared/teams/team/header/add-people-how/index.js
+++ shared/teams/team/header/add-people-how/index.js
@@ -15,11 +15,16 @@ type Props = {
 const AddPeopleHow = (props: Props) => {
   const items = [
     {onClick: props.onAddPeople, subTitle: 'Keybase, Twitter, etc.', title: 'By username'},
-    {onClick: props.onInvite, style: {borderTopWidth: 0}, title: isMobile ? 'From address book' : 'By email'},
+    {
+      onClick: props.onInvite,
+      style: {borderTopWidth: 0},
+      subTitle: 'friends@friendships.com',
+      title: isMobile ? 'From address book' : 'By email',
+    },
     {
       onClick: props.onSlackImport,
       style: {borderTopWidth: 0},
-      subTitle: 'New! Secure your team',
+      subTitle: 'New! Migrate your team',
       title: 'From Slack',
     },
   ]
