0c3c8b57e493605dd5b4bf3b8d59a0e70103c7cf
Surya <surya@keyba.se>
2019-04-20 13:38:15 -0400

    fix ios mobile

--- go/libkb/log_send.go
+++ go/libkb/log_send.go
@@ -15,8 +15,6 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
-	"regexp"
-	"runtime"
 	"sort"
 	"strconv"
 	"strings"
@@ -24,10 +22,8 @@ import (
 
 	"github.com/keybase/client/go/logger"
 	"github.com/keybase/client/go/protocol/keybase1"
-	context "golang.org/x/net/context"
 
 	jsonw "github.com/keybase/go-jsonw"
-	ps "github.com/keybase/go-ps"
 )
 
 // Logs is the struct to specify the path of log files
@@ -571,63 +567,4 @@ func (l *LogSendContext) mergeExtendedStatus(status string) string {
 		return status
 	}
 	return string(fullStatus)
-}
-
-func keybaseProcessList() string {
-	ret := ""
-	osinfo, err := getOSInfo()
-	if err == nil {
-		ret += string(osinfo) + "\n\n"
-	} else {
-		ret += fmt.Sprintf("could not get OS info for platform %s: %s\n\n", runtime.GOOS, err)
-	}
-
-	processes, err := pgrep(keybaseProcessRegexp)
-	if err != nil {
-		return fmt.Sprintf("error getting processes: %s", err)
-	}
-	for _, process := range processes {
-		path, err := process.Path()
-		if err != nil {
-			path = "unable to get process path"
-		}
-		ret += fmt.Sprintf("%s (%+v)\n", path, process)
-	}
-	return ret
-}
-
-func getOSInfo() (string, error) {
-	switch runtime.GOOS {
-	case "linux":
-		osinfo, err := ioutil.ReadFile("/etc/os-release")
-		return string(osinfo), err
-	case "darwin":
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		defer cancel()
-		osinfo, err := exec.CommandContext(ctx, "/usr/bin/sw_vers").CombinedOutput()
-		return string(osinfo), err
-	case "windows":
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		defer cancel()
-		osinfo, err := exec.CommandContext(ctx, "cmd", "/c", "ver").CombinedOutput()
-		return string(osinfo), err
-	default:
-		return "", fmt.Errorf("no OS info for platform %s", runtime.GOOS)
-	}
-}
-
-var keybaseProcessRegexp = regexp.MustCompile(`(?i:kbfs|keybase|upd.exe)`)
-
-func pgrep(matcher *regexp.Regexp) ([]ps.Process, error) {
-	processes, err := ps.Processes()
-	if err != nil {
-		return nil, err
-	}
-	var filteredProcesses []ps.Process
-	for _, process := range processes {
-		if matcher.MatchString(process.Executable()) {
-			filteredProcesses = append(filteredProcesses, process)
-		}
-	}
-	return filteredProcesses, nil
 }
