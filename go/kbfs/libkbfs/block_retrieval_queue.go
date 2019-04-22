61dffd02dec21967110340e2b270e7c7e8d30d08
Jeremy Stribling <strib@alum.mit.edu>
2019-04-22 09:54:04 -0700

    prefetcher: propagate higher priority requests
    Partial prefetches work by first doing a normal (non-deep-sync)
    lookup, and then a full deep-sync request for the same block ID, at a
    high priority.  Any child blocks that get prefetched due to the first
    request get dropped to a throttled priority, and when the same blocks
    are then prefetched as part of the deep sync, the requests at the
    throttle priority are already in the queue and hence still get
    throttled.  Which means partial prefetches end up taking forever.
    
    So override the priority in the prefetch queue.
    
    Partial prefetches still seem to take a while, but not an unbounded
    amount of time, like before.
    
    And fix a debug message to stringify actions.
    
    Issue: KBFS-4070

--- go/kbfs/libkbfs/block_retrieval_queue.go
+++ go/kbfs/libkbfs/block_retrieval_queue.go
@@ -538,9 +538,9 @@ func (brq *blockRetrievalQueue) request(ctx context.Context,
 	}
 	// Update the action if needed.
 	brq.vlog.CLogf(
-		ctx, libkb.VLog2, "Combining actions %d and %d", action, br.action)
+		ctx, libkb.VLog2, "Combining actions %s and %s", action, br.action)
 	br.action = action.Combine(br.action)
-	brq.vlog.CLogf(ctx, libkb.VLog2, "Got action %d", br.action)
+	brq.vlog.CLogf(ctx, libkb.VLog2, "Got action %s", br.action)
 	return ch
 }
 
