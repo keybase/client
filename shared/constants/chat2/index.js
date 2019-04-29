f94dac93b017475905e8091528adc980882ef098
chrisnojima <cnojima@keyba.se>
2019-04-29 11:37:15 -0400

    WIP

--- shared/constants/chat2/index.js
+++ shared/constants/chat2/index.js
@@ -193,9 +193,6 @@ export const isUserActivelyLookingAtThisThread = (
   let chatThreadSelected = false
   if (flags.useNewRouter) {
     chatThreadSelected = true // conversationIDKey === selectedConversationIDKey is the only thing that matters in the new router
-    // const routePath = Router2.getVisiblePath()
-    // chatThreadSelected =
-    // routePath[routePath.length - 1]?.routeName === (isMobile ? 'chatConversation' : 'tabs.chatTab')
   } else {
     const routePath = getPath(state.routeTree.routeState)
     if (isMobile) {
