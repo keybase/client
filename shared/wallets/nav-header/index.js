9e27714fa3dde80d9024039ed26b1c04a8506953
Daniel Ayoub <danny.a95@gmail.com>
2019-04-29 10:30:19 -0400

    add some modals

--- shared/wallets/nav-header/index.js
+++ shared/wallets/nav-header/index.js
@@ -11,43 +11,52 @@ type HeaderTitleProps = {|
   accountName: string,
   isDefault: boolean,
   loading: boolean,
+  noDisclaimer: boolean,
   username: string,
 |}
 
-export const HeaderTitle = (props: HeaderTitleProps) => (
-  <Kb.Box2 direction="horizontal">
-    <Kb.Box2 alignItems="flex-end" direction="horizontal" style={styles.left}>
-      <AddAccount />
+export const HeaderTitle = (props: HeaderTitleProps) =>
+  props.noDisclaimer ? null : (
+    <Kb.Box2 direction="horizontal">
+      <Kb.Box2 alignItems="flex-end" direction="horizontal" style={styles.left}>
+        <AddAccount />
+      </Kb.Box2>
+      <Kb.Box2 direction="vertical" alignItems="flex-start" style={styles.accountInfo}>
+        {props.loading ? (
+          <Kb.ProgressIndicator small={true} style={styles.loading} />
+        ) : (
+          <>
+            <Kb.Box2
+              direction="horizontal"
+              alignItems="center"
+              gap="tiny"
+              style={styles.accountNameContainer}
+            >
+              {props.isDefault && <Kb.Avatar size={16} username={props.username} />}
+              <Kb.Text selectable={true} type="Header" lineClamp={1}>
+                {props.accountName}
+              </Kb.Text>
+            </Kb.Box2>
+            <SmallAccountID accountID={props.accountID} style={styles.accountID} />
+          </>
+        )}
+      </Kb.Box2>
     </Kb.Box2>
-    <Kb.Box2 direction="vertical" alignItems="flex-start" style={styles.accountInfo}>
-      {props.loading ? (
-        <Kb.ProgressIndicator small={true} style={styles.loading} />
-      ) : (
-        <>
-          <Kb.Box2 direction="horizontal" alignItems="center" gap="tiny" style={styles.accountNameContainer}>
-            {props.isDefault && <Kb.Avatar size={16} username={props.username} />}
-            <Kb.Text selectable={true} type="Header" lineClamp={1}>
-              {props.accountName}
-            </Kb.Text>
-          </Kb.Box2>
-          <SmallAccountID accountID={props.accountID} style={styles.accountID} />
-        </>
-      )}
-    </Kb.Box2>
-  </Kb.Box2>
-)
+  )
 
 type HeaderRightActionsProps = {|
+  noDisclaimer: boolean,
   onReceive: () => void,
 |}
 
-export const HeaderRightActions = (props: HeaderRightActionsProps) => (
-  <Kb.Box2 alignItems="flex-end" direction="horizontal" gap="tiny" style={styles.rightActions}>
-    <SendButton small={true} />
-    <Kb.Button type="Wallet" mode="Secondary" label="Receive" small={true} onClick={props.onReceive} />
-    <DropdownButton small={true} />
-  </Kb.Box2>
-)
+export const HeaderRightActions = (props: HeaderRightActionsProps) =>
+  props.noDisclaimer ? null : (
+    <Kb.Box2 alignItems="flex-end" direction="horizontal" gap="tiny" style={styles.rightActions}>
+      <SendButton small={true} />
+      <Kb.Button type="Wallet" mode="Secondary" label="Receive" small={true} onClick={props.onReceive} />
+      <DropdownButton small={true} />
+    </Kb.Box2>
+  )
 
 const styles = Styles.styleSheetCreate({
   accountID: Styles.platformStyles({
