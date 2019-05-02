c6753c77fbdf5d4ced3f40e8b51c59924c0fe765
Piotr Zduniak <piotr@zduniak.net>
2019-05-02 21:57:34 +0200

    fix TestProvisionAutoreset test

--- go/engine/login_provision.go
+++ go/engine/login_provision.go
@@ -786,6 +786,7 @@ func (e *loginProvision) chooseDevice(m libkb.MetaContext, pgp bool) (err error)
 		}
 
 		if !enterReset {
+			m.Debug("skipped login")
 			// User had to explicitly decline entering the pipeline so in order to prevent
 			// confusion prevent further prompts by completing a noop login flow.
 			e.skippedLogin = true
