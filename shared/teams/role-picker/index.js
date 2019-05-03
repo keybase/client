b45ae858b077f23a56b1f37f5b8b44ac8d1952ad
chrisnojima <cnojima@keyba.se>
2019-05-03 00:49:39 -0400

    role picker scroll on mobile (#17333)

--- shared/teams/role-picker/index.js
+++ shared/teams/role-picker/index.js
@@ -336,6 +336,9 @@ const styles = Styles.styleSheetCreate({
       paddingTop: Styles.globalMargins.small,
     },
   }),
+  scroll: {
+    backgroundColor: Styles.globalColors.white,
+  },
   text: {
     textAlign: 'left',
   },
@@ -360,6 +363,12 @@ export class FloatingRolePicker extends React.Component<FloatingProps, S> {
   _setRef = ref => this.setState({ref})
   render() {
     const {position, children, open, floatingContainerStyle, onCancel, ...props} = this.props
+    const picker = <RolePicker {...props} onCancel={Styles.isMobile ? undefined : onCancel} />
+    const wrappedPicker = Styles.isMobile ? (
+      <Kb.ScrollView style={styles.scroll}>{picker}</Kb.ScrollView>
+    ) : (
+      picker
+    )
     return (
       <>
         <Kb.Box ref={this._setRef}>{children}</Kb.Box>
@@ -373,7 +382,7 @@ export class FloatingRolePicker extends React.Component<FloatingProps, S> {
               {Styles.isMobile && (
                 <Kb.HeaderHocHeader onLeftAction={onCancel} leftAction={'cancel'} title="Pick a role" />
               )}
-              <RolePicker {...props} onCancel={Styles.isMobile ? undefined : onCancel} />
+              {wrappedPicker}
             </Kb.Box2>
           </Kb.FloatingBox>
         )}
