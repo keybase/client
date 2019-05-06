ca0d3a3ff90f1f63bd3819582c5fa9794ca63c89
chrisnojima <cnojima@keyba.se>
2019-05-06 15:16:20 -0400

    turn on inline requires

--- shared/metro.config.js
+++ shared/metro.config.js
@@ -11,7 +11,7 @@ module.exports = {
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
-        inlineRequires: false,
+        inlineRequires: true,
       },
     }),
   },
