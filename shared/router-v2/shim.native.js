9d58ac963d55ba9a8b10e373166b3892a3e1f366
chrisnojima <cnojima@keyba.se>
2019-05-08 14:02:14 -0400

    WIP

--- shared/router-v2/shim.native.js
+++ shared/router-v2/shim.native.js
@@ -3,19 +3,27 @@ import * as Kb from '../common-adapters/mobile.native'
 import * as React from 'react'
 import * as Styles from '../styles'
 import * as Shared from './shim.shared'
+import {withNavigationFocus} from '@react-navigation/core'
 
 export const shim = (routes: any) => Shared.shim(routes, shimNewRoute)
+
+// To reduce memory pressure on android we render null for non focused screens
 
 const shimNewRoute = (Original: any) => {
   // Wrap everything in a keyboard avoiding view (maybe this is opt in/out?)
   class ShimmedNew extends React.PureComponent<any, void> {
     static navigationOptions = Original.navigationOptions
     render() {
+      if (Styles.isAndroid && !this.props.isFocused) {
+        return null
+      }
+
       const navigationOptions =
         typeof Original.navigationOptions === 'function'
           ? Original.navigationOptions({navigation: this.props.navigation})
           : Original.navigationOptions
       const body = <Original {...this.props} />
+
       const keyboardBody = (
         <Kb.NativeKeyboardAvoidingView
           style={styles.keyboard}
@@ -38,7 +46,8 @@ const shimNewRoute = (Original: any) => {
       return safeKeyboardBody
     }
   }
-  return ShimmedNew
+
+  return Styles.isAndroid ? withNavigationFocus(ShimmedNew) : ShimmedNew
 }
 const styles = Styles.styleSheetCreate({
   keyboard: {
