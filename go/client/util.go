c43b181d4700a27d599cb891f9a725abb08fd1a5
heronhaye <surya@keyba.se>
2019-05-08 10:31:22 -0400

    work

---
+++ go/client/util.go
@@ -0,0 +1,26 @@
+package client
+
+import (
+	"fmt"
+
+	"github.com/keybase/client/go/libkb"
+)
+
+func ensureSetPassphraseFromRemote(mctx libkb.MetaContext) error {
+	if !mctx.G().ActiveDevice.Valid() {
+		return nil
+	}
+	cli, err := GetUserClient(mctx.G())
+	if err != nil {
+		return err
+	}
+	ret, err := cli.CanLogout(mctx.Ctx(), 0)
+	if err != nil {
+		return err
+	}
+	mctx.Debug("CanLogout call returned: %+v", ret)
+	if !ret.CanLogout {
+		return fmt.Errorf("Cannot logout: %s. Try `keybase passphrase set`.", ret.Reason)
+	}
+	return nil
+}
