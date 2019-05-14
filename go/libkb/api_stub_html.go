b92a317b94a0afe42defac278d243a7477cd6fde
Piotr Zduniak <piotr@zduniak.net>
2019-05-14 20:51:18 +0200

    comment header

--- go/libkb/api_stub_html.go
+++ go/libkb/api_stub_html.go
@@ -1,3 +1,8 @@
+// Copyright 2019 Keybase, Inc. All rights reserved. Use of
+// this source code is governed by the included BSD license.
+
+// +build !production
+
 package libkb
 
 var htmlAPIStubs = map[string]string{
