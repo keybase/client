ffe1052b0c041ebe57dfa07b3107ca7ddb5fb03d
chrisnojima <cnojima@keyba.se>
2019-05-17 10:34:01 -0400

    remove flow-typed types. fix stories lint. remove flow config / flow

--- shared/common-adapters/markdown/index.stories.js
+++ shared/common-adapters/markdown/index.stories.js
@@ -104,16 +104,15 @@ a = 1
       /foo
       /keybase`,
   nonemoji: `:party-parrot:`,
-  normal:
-    'I think we should try to use `if else` statements ```\
-if (var == "foo")\
-  echo "foo";\
-else echo "bar";```\
-     How about *bold* and _italic?_ nice. :smile:\
-a whole bunch of native emojis 😀 😁 😍 ☝️ ☎️\
-a whole bunch of string emojis :thumbsup: :cry: :fireworks:\
-Now youre thinking with ~portals~ crypto.\
-how about ~_*bold and italic and strike through?*_~ - now - _*some bold* and just italic_ bold.*with*.punctuation!',
+  normal: `I think we should try to use \`if else\` statements \`\`\`
+if (var == "foo")
+  echo "foo";
+else echo "bar";\`\`\`
+     How about *bold* and _italic?_ nice. :smile:
+a whole bunch of native emojis 😀 😁 😍 ☝️ ☎️
+a whole bunch of string emojis :thumbsup: :cry: :fireworks:
+Now youre thinking with ~portals~ crypto.
+how about ~_*bold and italic and strike through?*_~ - now - _*some bold* and just italic_ bold.*with*.punctuation!`,
   paragraphs: `this is a sentence.
 this is the next line
 and another with two below
