c88294dd58741b45361ce336c98f038c581c9c34
chrisnojima <cnojima@keyba.se>
2019-05-21 09:23:17 -0400

    WIP

---
+++ shared/login/forms/container.d.ts
@@ -0,0 +1,10 @@
+import * as React from 'react'
+
+export type Props = {
+  onBack?: () => void,
+  children?: React.ElementType,
+  style?: Object | null,
+  outerStyle?: Object | null
+};
+
+export declare class Container extends React.Component<Props> {}
