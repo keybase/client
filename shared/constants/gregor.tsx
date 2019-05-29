26af5167783b1aa82789c18df8e1fb6ce49b76ce
Daniel Ayoub <danny.a95@gmail.com>
2019-05-29 11:49:08 -0400

    convert all but fs

---
+++ shared/constants/gregor.tsx
@@ -0,0 +1,7 @@
+import * as I from 'immutable'
+import * as Types from './types/gregor'
+import * as RPCTypes from './types/rpc-gen'
+
+export const makeState: I.RecordFactory<Types._State> = I.Record({
+  reachable: RPCTypes.Reachable.unknown,
+})
---
+++ shared/constants/pinentry.tsx
@@ -0,0 +1,8 @@
+import * as I from 'immutable'
+import * as Types from './types/pinentry'
+
+const makeState: I.RecordFactory<Types._State> = I.Record({
+  sessionIDToPinentry: I.Map(),
+})
+
+export {makeState}
