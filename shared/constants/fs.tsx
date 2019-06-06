34e1b3cf3fc0db7854548aa323d8b2337ebb952c
Jakob Weisblat <jakob@keyba.se>
2019-06-06 11:02:09 -0400

    fix TLF rows on mobile, remove public tag from /keybase/public

--- shared/constants/fs.tsx
+++ shared/constants/fs.tsx
@@ -1046,6 +1046,12 @@ export const getTlfPath = (path: Types.Path): Types.Path | null => {
   return elems.length > 2 ? Types.pathConcat(Types.pathConcat(defaultPath, elems[1]), elems[2]) : null
 }
 
+export const hasPublicTag = (path: Types.Path): boolean => {
+  return (
+    Types.getPathVisibility(path) === Types.TlfType.Public && path !== Types.stringToPath('/keybase/public')
+  )
+}
+
 export const getPathUserSetting = (
   pathUserSettings: I.Map<Types.Path, Types.PathUserSetting>,
   path: Types.Path
