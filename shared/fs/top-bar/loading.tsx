d88e2ec21c4d1b5ded0d3f779d3a9a2afe960404
Song Gao <song@gao.io>
2019-06-10 17:06:03 -0700

    only show loading spinner at first time loading

--- shared/fs/top-bar/loading.tsx
+++ shared/fs/top-bar/loading.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import {namedConnect} from '../../util/container'
 import * as I from 'immutable'
 import * as Types from '../../constants/types/fs'
+import * as Constants from '../../constants/fs'
 import * as Kb from '../../common-adapters'
 
 type OwnProps = {
@@ -10,14 +11,28 @@ type OwnProps = {
 
 const mapStateToProps = (state, {path}: OwnProps) => ({
   _loadingPaths: state.fs.loadingPaths,
+  _pathItem: state.fs.pathItems.get(path, Constants.unknownPathItem),
 })
 
 const emptySet = I.Set()
 
-const mergeProps = (stateProps, dispatchProps, {path}: OwnProps) => ({
-  loading: stateProps._loadingPaths.get(path, emptySet).size > 0,
-})
+const mergeProps = (stateProps, dispatchProps, {path}: OwnProps) => {
+  // Only show the loading spinner when we are first-time loading a pathItem.
+  // If we already have content to show, just don't show spinner anymore even
+  // if we are loading.
+  if (stateProps._pathItem.type === Types.PathType.Unknown) {
+    return {show: true}
+  }
+  if (
+    stateProps._pathItem.type === Types.PathType.Folder &&
+    stateProps._pathItem.progress === Types.ProgressType.Pending &&
+    stateProps._loadingPaths.get(path, emptySet).size > 0
+  ) {
+    return {show: true}
+  }
+  return {show: false}
+}
 
-const Loading = props => props.loading && <Kb.ProgressIndicator type="Small" />
+const Loading = props => props.show && <Kb.ProgressIndicator type="Small" />
 
 export default namedConnect(mapStateToProps, () => ({}), mergeProps, 'TopBarLoading')(Loading)
