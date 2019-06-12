9f3ac592bc8772d630c85941c3d704384aaf0b35
Mike Maxim <mike@keyba.se>
2019-06-12 11:03:44 -0400

    dont need this

--- go/chat/storage/blockengine_memcache.go
+++ go/chat/storage/blockengine_memcache.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 
 	lru "github.com/hashicorp/golang-lru"
-	"github.com/keybase/client/go/chat/globals"
 	"github.com/keybase/client/go/libkb"
 	"github.com/keybase/client/go/protocol/chat1"
 	"github.com/keybase/client/go/protocol/gregor1"
@@ -12,7 +11,6 @@ import (
 )
 
 type blockEngineMemCacheImpl struct {
-	lockTab    *libkb.LockTable
 	blockCache *lru.Cache
 }
 
@@ -20,7 +18,6 @@ func newBlockEngineMemCache() *blockEngineMemCacheImpl {
 	c, _ := lru.New(100)
 	return &blockEngineMemCacheImpl{
 		blockCache: c,
-		lockTab:    &libkb.LockTable{},
 	}
 }
 
@@ -28,11 +25,8 @@ func (b *blockEngineMemCacheImpl) key(uid gregor1.UID, convID chat1.Conversation
 	return fmt.Sprintf("%s:%s:%d", uid, convID, id)
 }
 
-func (b *blockEngineMemCacheImpl) getBlock(ctx context.Context, g *globals.Context, uid gregor1.UID,
+func (b *blockEngineMemCacheImpl) getBlock(ctx context.Context, uid gregor1.UID,
 	convID chat1.ConversationID, id int) (block, bool) {
-	lockName := fmt.Sprintf("%s:%s:%d", uid, convID, id)
-	l := b.lockTab.AcquireOnName(ctx, g.ExternalG(), lockName)
-	defer l.Release(ctx)
 	key := b.key(uid, convID, id)
 	if v, ok := b.blockCache.Get(key); ok {
 		bl := v.(block)
