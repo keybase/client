b56f271d47f1fa3cd006c9210ce8b1a02e3fdaab
chrisnojima <cnojima@keyba.se>
2019-06-20 13:15:41 -0400

    WIP

---
+++ shared/styles/style-sheet-proxy.tsx
@@ -0,0 +1,35 @@
+import {isDarkMode} from './dark-mode'
+
+// Support a closure to enable simple dark mode.
+// transform is to allow native styleSheetCreate to convert the object
+
+type FuncOrObject = (() => Object) | Object
+type Transform = (o: Object) => Object
+
+const styleSheetCreate = (funcOrObj: FuncOrObject, transform: Transform) => {
+  if (typeof funcOrObj === 'function') {
+    let lightCached = null //isDarkMode() ? null : transform(funcOrObj())
+    let darkCached = null //isDarkMode() ? transform(funcOrObj()) : null
+
+    const wrapped = {
+      get: function(_: unknown, prop: string) {
+        if (isDarkMode()) {
+          darkCached = darkCached || transform(funcOrObj())
+          return darkCached[prop]
+        } else {
+          lightCached = lightCached || transform(funcOrObj())
+          return lightCached[prop]
+        }
+      },
+    }
+
+    return new Proxy({}, wrapped)
+  } else {
+    if (__DEV__) {
+      console.log('Darkmode incompatible style passed', funcOrObj)
+    }
+    return funcOrObj
+  }
+}
+
+export default styleSheetCreate
