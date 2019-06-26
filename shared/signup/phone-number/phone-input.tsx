30f6b656421cd757eaea4df5264aa4282a37af72
Piotr Zduniak <piotr@zduniak.net>
2019-06-26 20:35:14 +0200

    broken wip

--- shared/signup/phone-number/phone-input.tsx
+++ shared/signup/phone-number/phone-input.tsx
@@ -38,112 +38,6 @@ const MenuItem = props => (
   </Kb.Box2>
 )
 
-type Props = {
-  defaultCountry?: string
-  onChangeNumber: (phoneNumber: string) => void
-  onChangeValidity: (valid: boolean) => void
-  onEnterKeyDown?: () => void
-  style?: Styles.StylesCrossPlatform
-}
-
-type State = {
-  country: string
-  formatted: string
-}
-
-class _PhoneInput extends React.Component<Kb.PropsWithOverlay<Props>, State> {
-  state = {country: this.props.defaultCountry || defaultCountry, formatted: ''}
-  _formatter = new AsYouTypeFormatter(this.props.defaultCountry || defaultCountry)
-
-  _setFormatted = formatted =>
-    this.setState(s => {
-      if (s.formatted === formatted) {
-        return null
-      }
-      return {formatted}
-    }, this._updateParent)
-
-  // AsYouTypeFormatter doesn't support backspace
-  // To get around this, on every text change:
-  // 1. Clear the formatter
-  // 2. Remove any non-numerics from the text
-  // 3. Feed the new text into the formatter char by char
-  // 4. Set the value of the input to the new formatted
-  _reformat = _newText => {
-    this._formatter.clear()
-    const newText = filterNumeric(_newText)
-    if (newText.trim().length === 0) {
-      this._setFormatted('')
-      return
-    }
-    for (let i = 0; i < newText.length - 1; i++) {
-      this._formatter.inputDigit(newText[i])
-    }
-    const formatted = this._formatter.inputDigit(newText[newText.length - 1])
-    this._setFormatted(formatted)
-  }
-
-  _updateParent = () => {
-    const validation = validateNumber(this.state.formatted, this.state.country)
-    this.props.onChangeNumber(validation.e164)
-    this.props.onChangeValidity(validation.valid)
-  }
-
-  _setCountry = country => {
-    if (this.state.country !== country) {
-      this.setState({country})
-      this._formatter = new AsYouTypeFormatter(country)
-      this._reformat('')
-    }
-  }
-
-  render() {
-    return (
-      <>
-        <Kb.Box2
-          alignItems="center"
-          direction="horizontal"
-          style={Styles.collapseStyles([styles.container, this.props.style])}
-        >
-          <Kb.ClickableBox onClick={this.props.toggleShowingMenu} style={styles.fullHeight}>
-            <Kb.Box2
-              direction="horizontal"
-              style={styles.callingCodeContainer}
-              alignItems="center"
-              fullHeight={true}
-              gap="small"
-              ref={this.props.setAttachmentRef}
-            >
-              <Kb.Text type="BodySemibold">
-                {getCountryEmoji(this.state.country)} {getCallingCode(this.state.country)}
-              </Kb.Text>
-              <Kb.Icon type="iconfont-caret-down" sizeType="Tiny" />
-            </Kb.Box2>
-          </Kb.ClickableBox>
-          <Kb.PlainInput
-            autoFocus={true}
-            style={styles.input}
-            flexable={true}
-            keyboardType={isIOS ? 'number-pad' : 'numeric'}
-            placeholder={getPlaceholder(this.state.country)}
-            onChangeText={this._reformat}
-            onEnterKeyDown={this.props.onEnterKeyDown}
-            value={this.state.formatted}
-          />
-        </Kb.Box2>
-        <CountrySelector
-          attachTo={this.props.getAttachmentRef}
-          onSelect={this._setCountry}
-          onHidden={this.props.toggleShowingMenu}
-          selected={this.state.country}
-          visible={this.props.showingMenu}
-        />
-      </>
-    )
-  }
-}
-const PhoneInput = Kb.OverlayParentHOC(_PhoneInput)
-
 type CountrySelectorProps = {
   attachTo: () => React.Component<any> | null
   onSelect: (arg0: string) => void
@@ -187,6 +81,11 @@ class CountrySelector extends React.Component<CountrySelectorProps, CountrySelec
 
   _onChangeFilter = filter => this.setState(s => ({...s, filter}))
 
+  clearFilter() {
+    console.log('asd')
+    this._onChangeFilter('')
+  }
+
   render() {
     if (!Styles.isMobile) {
       return (
@@ -196,15 +95,14 @@ class CountrySelector extends React.Component<CountrySelectorProps, CountrySelec
           header={{
             title: 'Search',
             view: (
-              <Kb.NewInput
-                autoFocus={true}
-                containerStyle={styles.countrySearch}
-                placeholder="Search"
-                icon="iconfont-search"
-                onChangeText={this._onChangeFilter}
-                value={this.state.filter}
-                textType="BodySemibold"
-              />
+              <Kb.Box2 style={styles.searchWrapper} direction="horizontal" fullWidth={true}>
+                <Kb.SearchFilter
+                  icon="iconfont-search"
+                  fullWidth={true}
+                  onChange={this._onChangeFilter}
+                  placeholderText="Search"
+                />
+              </Kb.Box2>
             ),
           }}
           items={menuItems(countryData, this.state.filter, this._onSelectMenu)}
@@ -228,6 +126,123 @@ class CountrySelector extends React.Component<CountrySelectorProps, CountrySelec
     )
   }
 }
+
+type Props = {
+  defaultCountry?: string
+  onChangeNumber: (phoneNumber: string) => void
+  onChangeValidity: (valid: boolean) => void
+  onEnterKeyDown?: () => void
+  style?: Styles.StylesCrossPlatform
+}
+
+type State = {
+  country: string
+  formatted: string
+}
+
+class _PhoneInput extends React.Component<Kb.PropsWithOverlay<Props>, State> {
+  state = {country: this.props.defaultCountry || defaultCountry, formatted: ''}
+  _formatter = new AsYouTypeFormatter(this.props.defaultCountry || defaultCountry)
+  _countrySelectorRef = React.createRef<CountrySelector>()
+
+  _setFormatted = formatted =>
+    this.setState(s => {
+      if (s.formatted === formatted) {
+        return null
+      }
+      return {formatted}
+    }, this._updateParent)
+
+  // AsYouTypeFormatter doesn't support backspace
+  // To get around this, on every text change:
+  // 1. Clear the formatter
+  // 2. Remove any non-numerics from the text
+  // 3. Feed the new text into the formatter char by char
+  // 4. Set the value of the input to the new formatted
+  _reformat = _newText => {
+    this._formatter.clear()
+    const newText = filterNumeric(_newText)
+    if (newText.trim().length === 0) {
+      this._setFormatted('')
+      return
+    }
+    for (let i = 0; i < newText.length - 1; i++) {
+      this._formatter.inputDigit(newText[i])
+    }
+    const formatted = this._formatter.inputDigit(newText[newText.length - 1])
+    this._setFormatted(formatted)
+  }
+
+  _updateParent = () => {
+    const validation = validateNumber(this.state.formatted, this.state.country)
+    this.props.onChangeNumber(validation.e164)
+    this.props.onChangeValidity(validation.valid)
+  }
+
+  _setCountry = country => {
+    if (this.state.country !== country) {
+      this.setState({country})
+      this._formatter = new AsYouTypeFormatter(country)
+      this._reformat('')
+    }
+  }
+
+  _toggleShowingMenu = () => {
+    console.log(this._countrySelectorRef)
+    console.log(this._countrySelectorRef.current)
+    if (this._countrySelectorRef && this._countrySelectorRef.current) {
+      this._countrySelectorRef.current.clearFilter()
+    }
+    this.props.toggleShowingMenu()
+  }
+
+  render() {
+    return (
+      <>
+        <Kb.Box2
+          alignItems="center"
+          direction="horizontal"
+          style={Styles.collapseStyles([styles.container, this.props.style])}
+        >
+          <Kb.ClickableBox onClick={this._toggleShowingMenu} style={styles.fullHeight}>
+            <Kb.Box2
+              direction="horizontal"
+              style={styles.callingCodeContainer}
+              alignItems="center"
+              fullHeight={true}
+              gap="small"
+              ref={this.props.setAttachmentRef}
+            >
+              <Kb.Text type="BodySemibold">
+                {getCountryEmoji(this.state.country)} {getCallingCode(this.state.country)}
+              </Kb.Text>
+              <Kb.Icon type="iconfont-caret-down" sizeType="Tiny" />
+            </Kb.Box2>
+          </Kb.ClickableBox>
+          <Kb.PlainInput
+            autoFocus={true}
+            style={styles.input}
+            flexable={true}
+            keyboardType={isIOS ? 'number-pad' : 'numeric'}
+            placeholder={getPlaceholder(this.state.country)}
+            onChangeText={this._reformat}
+            onEnterKeyDown={this.props.onEnterKeyDown}
+            value={this.state.formatted}
+          />
+        </Kb.Box2>
+        <CountrySelector
+          attachTo={this.props.getAttachmentRef}
+          onSelect={this._setCountry}
+          onHidden={this._toggleShowingMenu}
+          selected={this.state.country}
+          visible={this.props.showingMenu}
+          ref={this._countrySelectorRef}
+        />
+      </>
+    )
+  }
+}
+const PhoneInput = Kb.OverlayParentHOC(_PhoneInput)
 
 const styles = Styles.styleSheetCreate({
   callingCodeContainer: {
@@ -277,6 +292,9 @@ const styles = Styles.styleSheetCreate({
   }),
   menuItem: {
     ...Styles.padding(Styles.globalMargins.tiny, Styles.globalMargins.xtiny),
+  },
+  searchWrapper: {
+    ...Styles.padding(Styles.globalMargins.tiny, Styles.globalMargins.tiny),
   },
 })
 
