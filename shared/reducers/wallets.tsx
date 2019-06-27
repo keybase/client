63c0d2ee492d55c212ce4a594211f56914178da6
Chris Ball <chris@printf.net>
2019-06-27 12:09:27 -0700

    Fix going immediately from a working SEP7 to an error one

--- shared/reducers/wallets.tsx
+++ shared/reducers/wallets.tsx
@@ -382,13 +382,6 @@ export default function(state: Types.State = initialState, action: WalletsGen.Ac
         airdropQualifications: I.List(action.payload.airdropQualifications),
         airdropState: action.payload.airdropState,
       })
-    case WalletsGen.validateSEP7Link:
-      // Clear out old state, isn't necessary but seems like a good idea.
-      return state.merge({
-        sep7ConfirmError: '',
-        sep7ConfirmInfo: null,
-        sep7ConfirmURI: '',
-      })
     case WalletsGen.validateSEP7LinkError:
       return state.merge({sep7ConfirmError: action.payload.error})
     case WalletsGen.setSEP7Tx:
@@ -489,6 +482,7 @@ export default function(state: Types.State = initialState, action: WalletsGen.Ac
     case WalletsGen.loadExternalPartners:
     case WalletsGen.acceptSEP7Pay:
     case WalletsGen.acceptSEP7Tx:
+    case WalletsGen.validateSEP7Link:
     case WalletsGen.refreshTrustlineAcceptedAssets:
     case WalletsGen.refreshTrustlinePopularAssets:
       return state
