bd7c9b919db14149e9d0e3130897c4d80266846c
Patrick Crosby <patrick@keyba.se>
2019-06-28 14:42:47 -0500

    Don't modify error results after tx finishes (#18168)
    * Don't modify error results after tx finishes
    
    * Turn off UseMulti for now

--- go/stellar/batch_multi.go
+++ go/stellar/batch_multi.go
@@ -110,6 +110,12 @@ func BatchMulti(mctx libkb.MetaContext, walletState *WalletState, arg stellar1.B
 		// make all ther results have success
 		now := stellar1.ToTimeMs(time.Now())
 		for i := 0; i < len(results); i++ {
+			if results[i].Status == stellar1.PaymentStatus_ERROR {
+				// some of the results have already been marked as an
+				// error, so skip those.
+				continue
+			}
+
 			results[i].TxID = submitRes.TxID
 			results[i].Status = stellar1.PaymentStatus_COMPLETED
 			results[i].EndTime = now
