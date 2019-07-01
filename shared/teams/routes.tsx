cee9f8055f41928be86b92bdb52bab08ea606d51
chrisnojima <cnojima@keyba.se>
2019-07-01 14:57:24 -0400

    WIP

--- shared/teams/routes.tsx
+++ shared/teams/routes.tsx
@@ -1,33 +1,73 @@
 import {MaybePopupHoc} from '../common-adapters'
+import Team from './team/container'
+import TeamMember from './team/member/container'
+import TeamsRoot from './container'
+import RetentionWarning from './team/settings-tab/retention/warning/container'
+import TeamDeleteTeam from './delete-team/container'
+import TeamEditTeamAvatar from '../profile/edit-avatar/container'
+import TeamEditTeamDescription from './edit-team-description/container'
+import TeamInviteByEmail from './invite-by-email/container'
+import TeamJoinTeamDialog from './join-team/container'
+import TeamNewTeamDialog from './new-team/container'
+import TeamReallyLeaveTeam from './really-leave-team/container'
+import TeamReallyRemoveMember from './team/really-remove-member/container'
+import TeamRename from './rename-team/container'
+import TeamsTeamBuilder from '../team-building/container'
 
 export const newRoutes = {
-  team: {getScreen: () => require('./team/container').default, upgraded: true},
-  teamMember: {getScreen: () => require('./team/member/container').default, upgraded: true},
-  teamsRoot: {getScreen: () => require('./container').default, upgraded: true},
+  team: {getScreen: (): typeof Team => require('./team/container').default, upgraded: true},
+  teamMember: {
+    getScreen: (): typeof TeamMember => require('./team/member/container').default,
+    upgraded: true,
+  },
+  teamsRoot: {getScreen: (): typeof TeamsRoot => require('./container').default, upgraded: true},
 }
 
 export const newModalRoutes = {
   retentionWarning: {
-    getScreen: () => require('./team/settings-tab/retention/warning/container').default,
+    getScreen: (): typeof RetentionWarning =>
+      require('./team/settings-tab/retention/warning/container').default,
     upgraded: true,
   },
-  teamDeleteTeam: {getScreen: () => require('./delete-team/container').default, upgraded: true},
-  teamEditTeamAvatar: {getScreen: () => require('../profile/edit-avatar/container').default, upgraded: true},
+  teamDeleteTeam: {
+    getScreen: (): typeof TeamDeleteTeam => require('./delete-team/container').default,
+    upgraded: true,
+  },
+  teamEditTeamAvatar: {
+    getScreen: (): typeof TeamEditTeamAvatar => require('../profile/edit-avatar/container').default,
+    upgraded: true,
+  },
   teamEditTeamDescription: {
-    getScreen: () => MaybePopupHoc(false)(require('./edit-team-description/container').default),
+    getScreen: (): typeof TeamEditTeamDescription =>
+      MaybePopupHoc(false)(require('./edit-team-description/container').default),
     upgraded: true,
   },
-  teamInviteByEmail: {getScreen: () => require('./invite-by-email/container').default, upgraded: true},
-  teamJoinTeamDialog: {getScreen: () => require('./join-team/container').default, upgraded: true},
-  teamNewTeamDialog: {getScreen: () => require('./new-team/container').default, upgraded: true},
-  teamReallyLeaveTeam: {getScreen: () => require('./really-leave-team/container').default, upgraded: true},
+  teamInviteByEmail: {
+    getScreen: (): typeof TeamInviteByEmail => require('./invite-by-email/container').default,
+    upgraded: true,
+  },
+  teamJoinTeamDialog: {
+    getScreen: (): typeof TeamJoinTeamDialog => require('./join-team/container').default,
+    upgraded: true,
+  },
+  teamNewTeamDialog: {
+    getScreen: (): typeof TeamNewTeamDialog => require('./new-team/container').default,
+    upgraded: true,
+  },
+  teamReallyLeaveTeam: {
+    getScreen: (): typeof TeamReallyLeaveTeam => require('./really-leave-team/container').default,
+    upgraded: true,
+  },
   teamReallyRemoveMember: {
-    getScreen: () => require('./team/really-remove-member/container').default,
+    getScreen: (): typeof TeamReallyRemoveMember => require('./team/really-remove-member/container').default,
     upgraded: true,
   },
   teamRename: {
-    getScreen: () => require('./rename-team/container').default,
+    getScreen: (): typeof TeamRename => require('./rename-team/container').default,
     upgraded: true,
   },
-  teamsTeamBuilder: {getScreen: () => require('../team-building/container').default, upgraded: true},
+  teamsTeamBuilder: {
+    getScreen: (): typeof TeamsTeamBuilder => require('../team-building/container').default,
+    upgraded: true,
+  },
 }
