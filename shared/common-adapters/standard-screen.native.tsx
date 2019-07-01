463e247da056b585274caa24f61d44613567b208
Adam Spooner <adam@keyba.se>
2019-07-01 11:27:59 -0400

    WIP

--- shared/common-adapters/standard-screen.native.tsx
+++ shared/common-adapters/standard-screen.native.tsx
@@ -3,10 +3,12 @@ import {Props} from './standard-screen'
 import {NativeScrollView} from './native-wrappers.native'
 import HeaderHoc from './header-hoc'
 import * as Styles from '../styles'
+import Banner from './banner'
 import Box from './box'
 import Text from './text'
 
 const Kb = {
+  Banner,
   Box,
   NativeScrollView,
   Text,
@@ -17,21 +19,10 @@ const StandardScreen = (props: Props) => {
     // @ts-ignore for now
     <Kb.NativeScrollView scrollEnabled={props.scrollEnabled}>
       {!!props.notification && (
-        <Kb.Box
-          style={Styles.collapseStyles([
-            styles.banner,
-            props.notification.type === 'error' && styles.bannerError,
-            props.styleBanner,
-          ])}
-        >
-          {typeof props.notification.message === 'string' ? (
-            <Kb.Text center={true} style={styles.bannerText} type="BodySmallSemibold">
-              {props.notification.message}
-            </Kb.Text>
-          ) : (
-            props.notification.message
-          )}
-        </Kb.Box>
+        <Kb.Banner
+          color={props.notification.type === 'error' ? 'red' : 'blue'}
+          text={props.notification.message}
+        />
       )}
       <Kb.Box
         style={Styles.collapseStyles([
@@ -48,28 +39,6 @@ const StandardScreen = (props: Props) => {
 
 const MIN_BANNER_HEIGHT = 40
 const styles = Styles.styleSheetCreate({
-  banner: Styles.platformStyles({
-    common: {
-      ...Styles.globalStyles.flexBoxColumn,
-      alignItems: 'center',
-      backgroundColor: Styles.globalColors.green,
-      justifyContent: 'center',
-      marginBottom: Styles.globalMargins.tiny,
-      minHeight: MIN_BANNER_HEIGHT,
-      paddingLeft: Styles.globalMargins.tiny,
-      paddingRight: Styles.globalMargins.tiny,
-    },
-    isElectron: {
-      paddingBottom: Styles.globalMargins.xtiny,
-      paddingTop: Styles.globalMargins.xtiny,
-    },
-    isMobile: {
-      paddingBottom: Styles.globalMargins.tiny,
-      paddingTop: Styles.globalMargins.tiny,
-    },
-  }),
-  bannerError: {backgroundColor: Styles.globalColors.red},
-  bannerText: {color: Styles.globalColors.white},
   container: {
     ...Styles.globalStyles.flexBoxColumn,
     backgroundColor: Styles.globalColors.white,
