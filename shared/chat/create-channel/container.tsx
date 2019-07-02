c899d870d8890bbb1f27a2bcfc8444fdabd93e0b
chrisnojima <cnojima@keyba.se>
2019-07-02 11:14:03 -0400

    WIP

--- shared/chat/create-channel/container.tsx
+++ shared/chat/create-channel/container.tsx
@@ -1,63 +1,56 @@
+import * as React from 'react'
 import * as TeamsGen from '../../actions/teams-gen'
 import CreateChannel from '.'
-import {
-  compose,
-  withHandlers,
-  lifecycle,
-  withStateHandlers,
-  connect,
-  getRouteProps,
-  RouteProps,
-} from '../../util/container'
+import * as Container from '../../util/container'
 import * as RouteTreeGen from '../../actions/route-tree-gen'
 import {upperFirst} from 'lodash-es'
 
-type OwnProps = RouteProps<
-  {
-    teamname: string
-  },
-  {}
->
+type OwnProps = Container.RouteProps<{teamname: string}, {}>
 
-const mapStateToProps = (state, ownProps) => {
-  return {
-    errorText: upperFirst(state.teams.channelCreationError),
-    teamname: getRouteProps(ownProps, 'teamname'),
-  }
+type Props = {
+  _onCreateChannel: (o: {channelname: string; description: string; teamname: string}) => void
+  _onSetChannelCreationError: (error: string) => void
+  errorText: string
+  onBack: () => void
+  onClose: () => void
+  teamname: string
 }
 
-const mapDispatchToProps = (dispatch, ownProps) => ({
-  _onCreateChannel: ({channelname, description, teamname}) =>
-    dispatch(TeamsGen.createCreateChannel({channelname, description, teamname})),
-  _onSetChannelCreationError: error => dispatch(TeamsGen.createSetChannelCreationError({error})),
-  onBack: () => dispatch(RouteTreeGen.createNavigateUp()),
-  onClose: () => dispatch(RouteTreeGen.createNavigateUp()),
-})
+const Wrapped = (p: Props) => {
+  React.useEffect(() => p._onSetChannelCreationError(''))
 
-export default compose(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps,
-    (s, d, o) => ({...o, ...s, ...d})
-  ),
-  withStateHandlers(
-    {
-      channelname: null,
-      description: null,
-    } as any,
-    {
-      onChannelnameChange: () => channelname => ({channelname}),
-      onDescriptionChange: () => description => ({description}),
-    } as any
-  ),
-  withHandlers({
-    onSubmit: ({channelname, description, _onCreateChannel, teamname}) => () => {
-      channelname && _onCreateChannel({channelname, description, teamname})
-    },
-  } as any),
-  lifecycle({
-    componentDidMount() {
-      this.props._onSetChannelCreationError('')
-    },
-  } as any)
-)(CreateChannel)
+  const [channelname, onChannelnameChange] = React.useState<string>(null)
+  const [description, onDescriptionChange] = React.useState<string>(null)
+
+  const {_onCreateChannel, _onSetChannelCreationError, teamname, ...rest} = p
+  const onSubmit = React.useCallback(() => {
+    channelname && _onCreateChannel({channelname, description, teamname})
+  }, [channelname, _onCreateChannel, description, teamname])
+
+  return (
+    <CreateChannel
+      {...rest}
+      teamname={teamname}
+      channelname={channelname}
+      onChannelnameChange={onChannelnameChange}
+      description={description}
+      onDescriptionChange={onDescriptionChange}
+      onSubmit={onSubmit}
+    />
+  )
+}
+
+export default  Container.connect(
+  (state, ownProps: OwnProps) => ({
+    errorText: upperFirst(state.teams.channelCreationError),
+    teamname: Container.getRouteProps(ownProps, 'teamname'),
+  }),
+  dispatch => ({
+    _onCreateChannel: ({channelname, description, teamname}: {channelname: string; description: string; teamname: string}) =>
+      dispatch(TeamsGen.createCreateChannel({channelname, description, teamname})),
+    _onSetChannelCreationError: (error: string) => dispatch(TeamsGen.createSetChannelCreationError({error})),
+    onBack: () => dispatch(RouteTreeGen.createNavigateUp()),
+    onClose: () => dispatch(RouteTreeGen.createNavigateUp()),
+  }),
+  (s, d, o: OwnProps) => ({...s, ...d})
+)(Wrapped)
