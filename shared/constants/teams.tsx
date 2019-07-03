fcb43712648c1363a4981212e3f829af899b71c6
chrisnojima <cnojima@keyba.se>
2019-07-03 14:02:21 -0400

    small random perf (#18131)

--- shared/constants/teams.tsx
+++ shared/constants/teams.tsx
@@ -229,6 +229,12 @@ const baseRetentionPolicies = [
   policyFiveMinutes,
   policyThirtySeconds,
 ]
+
+const baseRetentionPoliciesTitleMap = baseRetentionPolicies.reduce((map, p) => {
+  map[p.seconds] = p.title
+  return map
+}, {})
+
 const retentionPolicies = {
   policyFiveMinutes,
   policyInherit,
@@ -530,10 +536,9 @@ const serviceRetentionPolicyToRetentionPolicy = (
           throw new Error(`RPC returned retention policy of type 'expire' with no expire data`)
         }
         const {expire} = policy
-        const maybePolicy = baseRetentionPolicies.find(p => p.seconds === expire.age)
         retentionPolicy = makeRetentionPolicy({
           seconds: expire.age,
-          title: maybePolicy ? maybePolicy.title : `${expire.age} seconds`,
+          title: baseRetentionPoliciesTitleMap[expire.age] || `${expire.age} seconds`,
           type: 'expire',
         })
         break
@@ -543,10 +548,9 @@ const serviceRetentionPolicyToRetentionPolicy = (
           throw new Error(`RPC returned retention policy of type 'ephemeral' with no ephemeral data`)
         }
         const {ephemeral} = policy
-        const maybePolicy = baseRetentionPolicies.find(p => p.seconds === ephemeral.age)
         retentionPolicy = makeRetentionPolicy({
           seconds: ephemeral.age,
-          title: maybePolicy ? maybePolicy.title : `${ephemeral.age} seconds`,
+          title: baseRetentionPoliciesTitleMap[ephemeral.age] || `${ephemeral.age} seconds`,
           type: 'explode',
         })
         break
