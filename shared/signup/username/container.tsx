9433a9549b0d8372b3d5eaed5b44d11cd4e9ab7e
Jakob Weisblat <jakob@keyba.se>
2019-07-02 17:35:43 -0700

    address some user switching bugs

--- shared/signup/username/container.tsx
+++ shared/signup/username/container.tsx
@@ -14,25 +14,38 @@ import {InfoIcon} from '../common'
 type OwnProps = {}
 
 const mapStateToProps = (state: Container.TypedState) => ({
-  error: state.signup.usernameError,
-  initialUsername: state.signup.username,
-  usernameTaken: state.signup.usernameTaken,
-  waiting: anyWaiting(state, Constants.waitingKey),
+  _users: state.config.configuredAccounts,
+  main: {
+    error: state.signup.usernameError,
+    initialUsername: state.signup.username,
+    usernameTaken: state.signup.usernameTaken,
+    waiting: anyWaiting(state, Constants.waitingKey),
+  },
 })
 
 const mapDispatchToProps = (dispatch: Container.TypedDispatch) => ({
-  onBack: () => {
+  _onBack: () => {
     dispatch(SignupGen.createRestartSignup())
     dispatch(RouteTreeGen.createNavigateUp())
   },
-  onContinue: (username: string) => dispatch(SignupGen.createCheckUsername({username})),
-  onLogin: (initUsername: string) => dispatch(ProvisionGen.createStartProvision({initUsername})),
+  _onReturnToMain: () => {
+    dispatch(RouteTreeGen.createSwitchRouteDef({loggedIn: true, path: ''}))
+  },
+  main: {
+    onContinue: (username: string) => dispatch(SignupGen.createCheckUsername({username})),
+    onLogin: (initUsername: string) => dispatch(ProvisionGen.createStartProvision({initUsername})),
+  },
 })
 
 const ConnectedEnterUsername = Container.connect(
   mapStateToProps,
   mapDispatchToProps,
-  (s, d, o: OwnProps) => ({...s, ...d, ...o})
+  (s, d, o: OwnProps) => ({
+    ...s.main,
+    ...d.main,
+    ...o,
+    onBack: s._users.some(account => account.hasStoredSecret) ? d._onReturnToMain : d._onBack,
+  })
 )(EnterUsername)
 
 // @ts-ignore fix this
