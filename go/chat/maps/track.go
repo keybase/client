c41b83a7fc1b2b219928d013e79cb786b660773b
Mike Maxim <mike@keyba.se>
2019-07-04 22:19:15 -0400

    dont remove tracker on location stop too early (#18268)

--- go/chat/maps/track.go
+++ go/chat/maps/track.go
@@ -22,6 +22,7 @@ type locationTrack struct {
 	allCoords          []chat1.Coordinate
 	getCurrentPosition bool
 	maxCoords          int
+	stopped            bool
 }
 
 func (t *locationTrack) GetCoords() (res []chat1.Coordinate) {
@@ -66,6 +67,19 @@ func (t *locationTrack) SetCoords(coords []chat1.Coordinate) {
 	defer t.Unlock()
 	t.allCoords = coords
 	t.capLocked(t.maxCoords)
+}
+
+func (t *locationTrack) Stop() {
+	t.Lock()
+	defer t.Unlock()
+	t.stopped = true
+	close(t.stopCh)
+}
+
+func (t *locationTrack) IsStopped() bool {
+	t.Lock()
+	defer t.Unlock()
+	return t.stopped
 }
 
 func (t *locationTrack) Key() types.LiveLocationKey {
