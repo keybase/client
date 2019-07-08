f56474922acf74ccce3597a34d013e8311d2e57d
John Zila <john@keyba.se>
2019-07-08 16:11:16 -0700

    fix broken save to camera roll

--- shared/actions/platform-specific/index.native.tsx
+++ shared/actions/platform-specific/index.native.tsx
@@ -30,10 +30,25 @@ import ImagePicker from 'react-native-image-picker'
 import {TypedActions, TypedState} from 'util/container'
 
 type NextURI = string
+
+const requestPermissionsToWrite = (): Promise<void> => {
+  if (isAndroid) {
+    return PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE, {
+      message: 'Keybase needs access to your storage so we can download a file.',
+      title: 'Keybase Storage Permission',
+    }).then(permissionStatus =>
+      permissionStatus !== 'granted'
+        ? Promise.reject(new Error('Unable to acquire storage permissions'))
+        : Promise.resolve()
+    )
+  }
+  return Promise.resolve()
+}
+
 function saveAttachmentDialog(filePath: string): Promise<NextURI> {
   let goodPath = filePath
   logger.debug('saveAttachment: ', goodPath)
-  return CameraRoll.saveToCameraRoll(goodPath)
+  return requestPermissionsToWrite().then(() => CameraRoll.saveToCameraRoll(goodPath))
 }
 
 async function saveAttachmentToCameraRoll(filePath: string, mimeType: string): Promise<void> {
