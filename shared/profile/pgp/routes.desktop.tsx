7cd9f50940e8b231b7247b032270384033dbede4
chrisnojima <cnojima@keyba.se>
2019-07-08 10:35:29 -0400

    WIP

--- shared/profile/pgp/routes.desktop.tsx
+++ shared/profile/pgp/routes.desktop.tsx
@@ -1,7 +1,22 @@
+import ProfileFinished from './finished.desktop'
+import ProfileGenerate from './generate.desktop'
+import ProfileImport from './import.desktop'
+import ProfilePgp from './choice.desktop'
+import ProfileProvideInfo from './info.desktop'
+
 export const newRoutes = {
-  profileFinished: {getScreen: () => require('./finished.desktop').default, upgraded: true},
-  profileGenerate: {getScreen: () => require('./generate.desktop').default, upgraded: true},
-  profileImport: {getScreen: () => require('./import.desktop').default, upgraded: true},
-  profilePgp: {getScreen: () => require('./choice.desktop').default, upgraded: true},
-  profileProvideInfo: {getScreen: () => require('./info.desktop').default, upgraded: true},
+  profileFinished: {
+    getScreen: (): typeof ProfileFinished => require('./finished.desktop').default,
+    upgraded: true,
+  },
+  profileGenerate: {
+    getScreen: (): typeof ProfileGenerate => require('./generate.desktop').default,
+    upgraded: true,
+  },
+  profileImport: {getScreen: (): typeof ProfileImport => require('./import.desktop').default, upgraded: true},
+  profilePgp: {getScreen: (): typeof ProfilePgp => require('./choice.desktop').default, upgraded: true},
+  profileProvideInfo: {
+    getScreen: (): typeof ProfileProvideInfo => require('./info.desktop').default,
+    upgraded: true,
+  },
 }
