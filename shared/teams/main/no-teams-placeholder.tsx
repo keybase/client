73c561ee2fdad180d04845e486292682b7232b3b
Nathan Smith <nathan@keyba.se>
2019-07-15 16:36:49 -0400

    reimplement placeholder

---
+++ shared/teams/main/no-teams-placeholder.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react'
+import * as Container from '../../util/container'
+import * as Kb from '../../common-adapters'
+import * as RouteTreeGen from '../../actions/route-tree-gen'
+import {globalMargins, desktopStyles, platformStyles, styleSheetCreate} from '../../styles'
+
+const NoTeamsPlaceholder = () => {
+  const dispatch = Container.useDispatch()
+  const onCreateTeam = () =>
+    dispatch(
+      RouteTreeGen.createNavigateAppend({
+        path: [{props: {}, selected: 'teamNewTeamDialog'}],
+      })
+    )
+  const onJoinTeam = () => dispatch(RouteTreeGen.createNavigateAppend({path: ['teamJoinTeamDialog']}))
+
+  return (
+    <Kb.Box2 direction="vertical" fullWidth={true} centerChildren={true} style={styles.container}>
+      <Kb.Text type="BodySmall">You are not a part of any teams.</Kb.Text>
+      <Kb.Text type="BodySmall">
+        <Kb.Text type="BodySmallPrimaryLink" onClick={onCreateTeam}>
+          Create a team
+        </Kb.Text>{' '}
+        or{' '}
+        <Kb.Text type="BodySmallPrimaryLink" onClick={onJoinTeam}>
+          join a team you know.
+        </Kb.Text>
+      </Kb.Text>
+    </Kb.Box2>
+  )
+}
+
+const styles = styleSheetCreate({
+  container: {
+    marginBottom: globalMargins.large,
+    marginLeft: globalMargins.medium,
+    marginRight: globalMargins.medium,
+    marginTop: globalMargins.large,
+  },
+})
+
+export default NoTeamsPlaceholder
