acfe1a4802098a599d428c99ea707a46bd5a8097
Adam Spooner <adam@keyba.se>
2019-07-16 18:04:27 -0400

    WIP

--- shared/common-adapters/checkbox.desktop.tsx
+++ shared/common-adapters/checkbox.desktop.tsx
@@ -1,83 +1,107 @@
 import Icon from './icon'
 import React, {Component} from 'react'
+import Box from './box'
 import Text from './text'
 import {Props} from './checkbox'
-import {collapseStyles, globalStyles, globalColors, transition, desktopStyles} from '../styles'
+import * as Styles from '../styles'
+
+const Kb = {
+  Box,
+}
 
 export const CHECKBOX_SIZE = 13
 export const CHECKBOX_MARGIN = 8
 
 class Checkbox extends Component<Props> {
+  _onCheck = (e: React.SyntheticEvent) =>
+    // If something in labelComponent needs to catch a click without calling this, use
+    // event.preventDefault()
+    this.props.disabled || e.defaultPrevented
+      ? undefined
+      : this.props.onCheck && this.props.onCheck(!this.props.checked)
+
   render() {
-    let borderColor: string = this.props.checked ? globalColors.blue : globalColors.black_20
-
-    if (this.props.disabled && !this.props.checked) {
-      borderColor = globalColors.black_10
-    }
-
-    const boxStyle = {
-      ...transition('background'),
-      backgroundColor: this.props.checked
-        ? globalColors.blue
-        : this.props.boxBackgroundColor
-        ? this.props.boxBackgroundColor
-        : globalColors.white,
-      border: `solid 1px ${borderColor}`,
-      borderRadius: 2,
-      height: CHECKBOX_SIZE,
-      marginRight: CHECKBOX_MARGIN,
-      marginTop: 2,
-      opacity: this.props.disabled && this.props.checked ? 0.4 : 1,
-      position: 'relative' as 'relative',
-      width: CHECKBOX_SIZE,
-    }
-
-    const clickableStyle = this.props.disabled ? {} : desktopStyles.clickable
-
     return (
-      <div
-        style={collapseStyles([styleContainer, clickableStyle, this.props.style])}
-        onClick={e =>
-          // If something in labelComponent needs to catch a click without calling this, use
-          // event.preventDefault()
-          this.props.disabled || e.defaultPrevented
-            ? undefined
-            : this.props.onCheck && this.props.onCheck(!this.props.checked)
-        }
+      <Kb.Box
+        style={Styles.collapseStyles([
+          styles.container,
+          !this.props.disabled && Styles.desktopStyles.clickable,
+          this.props.style,
+        ])}
+        onClick={this._onCheck}
       >
-        <div style={boxStyle}>
-          <Icon
-            type="iconfont-check"
-            style={collapseStyles([styleIcon, this.props.checked ? {} : {opacity: 0}])}
-            hoverColor={globalColors.white}
-            color={globalColors.white}
-            fontSize={9}
-          />
-        </div>
-        <Text type="Body" style={collapseStyles([styleText, this.props.disabled && {opacity: 0.3}])}>
+        <Icon
+          boxStyle={Styles.collapseStyles([
+            styles.checkbox,
+            this.props.checked && styles.checkboxChecked,
+            this.props.disabled && styles.checkboxInactive,
+            this.props.disabled && this.props.checked && styles.semiTransparent,
+          ])}
+          type="iconfont-check"
+          style={Styles.collapseStyles([styles.icon, !this.props.checked && styles.transparent])}
+          hoverColor={Styles.globalColors.white}
+          color={Styles.globalColors.white}
+          fontSize={9}
+        />
+        <Text onClick={this._onCheck} type="Body" style={Styles.collapseStyles([
+            styles.text,
+            this.props.disabled && styles.semiLessTransparent,
+          ])}>
           {this.props.labelComponent || this.props.label}
         </Text>
-      </div>
+      </Kb.Box>
     )
   }
 }
 
-const styleContainer = {
-  ...globalStyles.flexBoxRow,
-  alignItems: 'flex-start',
-  paddingBottom: 2,
-  paddingTop: 2,
-}
-
-const styleIcon = {
-  ...transition('opacity'),
-  left: 1,
-  position: 'absolute',
-  top: 1,
-}
-
-const styleText = {
-  color: globalColors.black,
-}
+const styles = Styles.styleSheetCreate({
+  container: {
+    ...Styles.globalStyles.flexBoxRow,
+    alignItems: 'flex-start',
+    paddingBottom: 2,
+    paddingTop: 2,
+  },
+  icon: {
+    ...Styles.transition('opacity'),
+    alignSelf: 'center',
+  },
+  checkbox: {
+    ...Styles.globalStyles.flexBoxColumn,
+    ...Styles.transition('background'),
+    backgroundColor: Styles.globalColors.white,
+    borderColor: Styles.globalColors.black_20,
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderRadius: 2,
+    height: CHECKBOX_SIZE,
+    justifyContent: 'center',
+    marginRight: CHECKBOX_MARGIN,
+    marginTop: 2,
+    position: 'relative',
+    width: CHECKBOX_SIZE,
+  },
+  checkboxChecked: {
+    backgroundColor: Styles.globalColors.blue,
+    borderColor: Styles.globalColors.blue,
+  },
+  checkboxInactive: {
+    borderColor: Styles.globalColors.black_10,
+  },
+  text: {
+    color: Styles.globalColors.black,
+  },
+  opaque: {
+    opacity: 1,
+  },
+  semiLessTransparent: {
+    opacity: 0.3,
+  },
+  semiTransparent: {
+    opacity: 0.4,
+  },
+  transparent: {
+    opacity: 0,
+  },
+})
 
 export default Checkbox
