5a6eb692c96287bd97891a42b7b4477df99b4930
Patrick Crosby <pcrosby@gmail.com>
2019-07-30 14:52:47 -0500

    Add tests on the sep6 transfer server, urls

--- go/stellar/stellarsvc/anchor.go
+++ go/stellar/stellarsvc/anchor.go
@@ -2,16 +2,22 @@ package stellarsvc
 
 import (
 	"errors"
+	"net/url"
+	"path"
+	"strings"
 
 	"github.com/keybase/client/go/libkb"
 	"github.com/keybase/client/go/protocol/stellar1"
 )
 
+// anchorInteractor is used to interact with the sep6 transfer server for an asset.
 type anchorInteractor struct {
 	accountID stellar1.AccountID
 	asset     stellar1.Asset
 }
 
+// newAnchorInteractor creates an anchorInteractor for an account to interact
+// with an asset.
 func newAnchorInteractor(accountID stellar1.AccountID, asset stellar1.Asset) *anchorInteractor {
 	return &anchorInteractor{
 		accountID: accountID,
@@ -19,23 +25,77 @@ func newAnchorInteractor(accountID stellar1.AccountID, asset stellar1.Asset) *an
 	}
 }
 
+// Deposit runs the deposit action for accountID on the transfer server for asset.
 func (a *anchorInteractor) Deposit(mctx libkb.MetaContext) (stellar1.AssetActionResultLocal, error) {
-	return stellar1.AssetActionResultLocal{}, errors.New("nyi")
+	if err := a.checkAsset(mctx); err != nil {
+		return stellar1.AssetActionResultLocal{}, err
+	}
+	u, err := a.checkURL(mctx, "deposit")
+	if err != nil {
+		return stellar1.AssetActionResultLocal{}, err
+	}
+	return a.get(mctx, u)
 }
 
+// Withdraw runs the withdraw action for accountID on the transfer server for asset.
 func (a *anchorInteractor) Withdraw(mctx libkb.MetaContext) (stellar1.AssetActionResultLocal, error) {
-	return stellar1.AssetActionResultLocal{}, errors.New("nyi")
+	if err := a.checkAsset(mctx); err != nil {
+		return stellar1.AssetActionResultLocal{}, err
+	}
+	u, err := a.checkURL(mctx, "withdraw")
+	if err != nil {
+		return stellar1.AssetActionResultLocal{}, err
+	}
+	return a.get(mctx, u)
+}
+
+// checkAsset sanity-checks the asset to make sure it is verified and looks like
+// the transfer server actions are supported.
+func (a *anchorInteractor) checkAsset(mctx libkb.MetaContext) error {
+	if a.asset.VerifiedDomain == "" {
+		return errors.New("asset is unverified")
+	}
+	if a.asset.TransferServer == "" {
+		return errors.New("asset has no transfer server")
+	}
+
+	// we don't support transfer servers that require authentication via sep10 at this point
+	if a.asset.AuthEndpoint != "" {
+		return errors.New("asset anchor requires authentication, which Keybase does not support at this time")
+	}
+	return nil
+}
+
+// checkURL creates the URL with the transfer server value and a path
+// and checks it for validity and same domain as the asset.
+func (a *anchorInteractor) checkURL(mctx libkb.MetaContext, action string) (*url.URL, error) {
+	full := path.Join(a.asset.TransferServer, action)
+	u, err := url.ParseRequestURI(full)
+	if err != nil {
+		return nil, err
+	}
+
+	if strings.ToLower(u.Host) != strings.ToLower(a.asset.VerifiedDomain) {
+		return nil, errors.New("transfer server hostname does not match asset hostname")
+	}
+
+	if u.Scheme != "https" {
+		return nil, errors.New("transfer server URL is not https")
+	}
+
+	if u.RawQuery != "" {
+		return nil, errors.New("transfer server URL has a query")
+	}
+
+	return u, nil
+}
+
+// get performs the http GET requests and parses the result.
+func (a *anchorInteractor) get(mctx libkb.MetaContext, u *url.URL) (stellar1.AssetActionResultLocal, error) {
+	return stellar1.AssetActionResultLocal{}, nil
 }
 
 /*
-	// get the asset verified
-	// check that the asset has TRANSFER_SERVER
-	// make sure it doesn't have WEB_AUTH_ENDPOINT (not supported yet)
-	// form the URL with transfer_server + "deposit"
-	// parse the URL, make sure it is valid
-	// check that the domain name is the same as the asset host name
-	// make sure there are no parameters
-	// make sure it is https
 	// perform the GET request
 	// parse the output into a message or a url to open in a browser (or an error)
 	// return that info
---
+++ go/stellar/stellarsvc/anchor_test.go
@@ -0,0 +1,111 @@
+package stellarsvc
+
+import (
+	"testing"
+
+	"github.com/keybase/client/go/protocol/stellar1"
+)
+
+type anchorTest struct {
+	Name  string
+	Asset stellar1.Asset
+}
+
+var errAnchorTests = []anchorTest{
+	{
+		Name: "not verified",
+		Asset: stellar1.Asset{
+			Type:   "credit_alphanum4",
+			Code:   "EUR",
+			Issuer: "GAKBPBDMW6CTRDCXNAPSVJZ6QAN3OBNRG6CWI27FGDQT2ZJJEMDRXPKK",
+		},
+	},
+	{
+		Name: "no transfer server",
+		Asset: stellar1.Asset{
+			Type:           "credit_alphanum4",
+			Code:           "EUR",
+			Issuer:         "GAKBPBDMW6CTRDCXNAPSVJZ6QAN3OBNRG6CWI27FGDQT2ZJJEMDRXPKK",
+			VerifiedDomain: "keybase.io",
+		},
+	},
+	{
+		Name: "requires auth",
+		Asset: stellar1.Asset{
+			Type:           "credit_alphanum4",
+			Code:           "EUR",
+			Issuer:         "GAKBPBDMW6CTRDCXNAPSVJZ6QAN3OBNRG6CWI27FGDQT2ZJJEMDRXPKK",
+			VerifiedDomain: "keybase.io",
+			TransferServer: "https://transfer.keybase.io/transfer",
+			AuthEndpoint:   "https://transfer.keybase.io/auth",
+		},
+	},
+	{
+		Name: "invalid url",
+		Asset: stellar1.Asset{
+			Type:           "credit_alphanum4",
+			Code:           "EUR",
+			Issuer:         "GAKBPBDMW6CTRDCXNAPSVJZ6QAN3OBNRG6CWI27FGDQT2ZJJEMDRXPKK",
+			VerifiedDomain: "keybase.io",
+			TransferServer: ":transfer.keybase.io/transfer",
+		},
+	},
+	{
+		Name: "different host",
+		Asset: stellar1.Asset{
+			Type:           "credit_alphanum4",
+			Code:           "EUR",
+			Issuer:         "GAKBPBDMW6CTRDCXNAPSVJZ6QAN3OBNRG6CWI27FGDQT2ZJJEMDRXPKK",
+			VerifiedDomain: "keybase.io",
+			TransferServer: "https://transfer.keybays.io/transfer",
+		},
+	},
+	{
+		Name: "http not https",
+		Asset: stellar1.Asset{
+			Type:           "credit_alphanum4",
+			Code:           "EUR",
+			Issuer:         "GAKBPBDMW6CTRDCXNAPSVJZ6QAN3OBNRG6CWI27FGDQT2ZJJEMDRXPKK",
+			VerifiedDomain: "keybase.io",
+			TransferServer: "http://transfer.keybase.io/transfer",
+		},
+	},
+	{
+		Name: "ftp not https",
+		Asset: stellar1.Asset{
+			Type:           "credit_alphanum4",
+			Code:           "EUR",
+			Issuer:         "GAKBPBDMW6CTRDCXNAPSVJZ6QAN3OBNRG6CWI27FGDQT2ZJJEMDRXPKK",
+			VerifiedDomain: "keybase.io",
+			TransferServer: "ftp://transfer.keybase.io/transfer",
+		},
+	},
+	{
+		Name: "has a query",
+		Asset: stellar1.Asset{
+			Type:           "credit_alphanum4",
+			Code:           "EUR",
+			Issuer:         "GAKBPBDMW6CTRDCXNAPSVJZ6QAN3OBNRG6CWI27FGDQT2ZJJEMDRXPKK",
+			VerifiedDomain: "keybase.io",
+			TransferServer: "https://transfer.keybase.io/transfer?x=123&trans",
+		},
+	},
+}
+
+func TestAnchorInteractor(t *testing.T) {
+	tc := SetupTest(t, "AnchorInteractor", 1)
+	for i, test := range errAnchorTests {
+		accountID, _ := randomStellarKeypair()
+		ai := newAnchorInteractor(accountID, test.Asset)
+		_, err := ai.Deposit(tc.MetaContext())
+		if err == nil {
+			t.Errorf("err test %d [%s]: Deposit returned no error, but expected one", i, test.Name)
+			continue
+		}
+		_, err = ai.Withdraw(tc.MetaContext())
+		if err == nil {
+			t.Errorf("err test %d [%s]: Withdraw returned no error, but expected one", i, test.Name)
+			continue
+		}
+	}
+}
