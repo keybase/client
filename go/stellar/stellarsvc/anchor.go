8083496144d64bdfbf0318622c81a878dcbd4124
Patrick Crosby <patrick@keyba.se>
2019-07-31 13:49:42 -0500

    Implement AssetDepositLocal, AssetWithdrawLocal (#18674)
    * WIP
    
    * Refactor for anchorInteractor so this is testable
    
    * Add tests on the sep6 transfer server, urls
    
    * More tests
    
    * Add valid tests
    
    * Check the external url domain
    
    * Add query params to URLs
    
    * Remove newlines

---
+++ go/stellar/stellarsvc/anchor.go
@@ -0,0 +1,233 @@
+package stellarsvc
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"path"
+	"strings"
+	"time"
+
+	"github.com/keybase/client/go/libkb"
+	"github.com/keybase/client/go/protocol/stellar1"
+	"golang.org/x/net/publicsuffix"
+)
+
+// anchorInteractor is used to interact with the sep6 transfer server for an asset.
+type anchorInteractor struct {
+	accountID     stellar1.AccountID
+	asset         stellar1.Asset
+	httpGetClient func(mctx libkb.MetaContext, url string) (code int, body []byte, err error)
+}
+
+// newAnchorInteractor creates an anchorInteractor for an account to interact
+// with an asset.
+func newAnchorInteractor(accountID stellar1.AccountID, asset stellar1.Asset) *anchorInteractor {
+	return &anchorInteractor{
+		accountID:     accountID,
+		asset:         asset,
+		httpGetClient: httpGet,
+	}
+}
+
+// Deposit runs the deposit action for accountID on the transfer server for asset.
+func (a *anchorInteractor) Deposit(mctx libkb.MetaContext) (stellar1.AssetActionResultLocal, error) {
+	if err := a.checkAsset(mctx); err != nil {
+		return stellar1.AssetActionResultLocal{}, err
+	}
+	u, err := a.checkURL(mctx, "deposit")
+	if err != nil {
+		return stellar1.AssetActionResultLocal{}, err
+	}
+
+	v := url.Values{}
+	v.Set("account", a.accountID.String())
+	v.Set("asset_code", a.asset.Code)
+	u.RawQuery = v.Encode()
+
+	var okResponse okDepositResponse
+	return a.get(mctx, u, &okResponse)
+}
+
+// Withdraw runs the withdraw action for accountID on the transfer server for asset.
+func (a *anchorInteractor) Withdraw(mctx libkb.MetaContext) (stellar1.AssetActionResultLocal, error) {
+	if err := a.checkAsset(mctx); err != nil {
+		return stellar1.AssetActionResultLocal{}, err
+	}
+	u, err := a.checkURL(mctx, "withdraw")
+	if err != nil {
+		return stellar1.AssetActionResultLocal{}, err
+	}
+
+	// TODO: `type` is supposedly optional, but actually required...so figure it out.
+	v := url.Values{}
+	v.Set("asset_code", a.asset.Code)
+	u.RawQuery = v.Encode()
+
+	var okResponse okWithdrawResponse
+	return a.get(mctx, u, &okResponse)
+}
+
+// checkAsset sanity-checks the asset to make sure it is verified and looks like
+// the transfer server actions are supported.
+func (a *anchorInteractor) checkAsset(mctx libkb.MetaContext) error {
+	if a.asset.VerifiedDomain == "" {
+		return errors.New("asset is unverified")
+	}
+	if a.asset.TransferServer == "" {
+		return errors.New("asset has no transfer server")
+	}
+
+	// we don't support transfer servers that require authentication via sep10 at this point
+	if a.asset.AuthEndpoint != "" {
+		return errors.New("asset anchor requires authentication, which Keybase does not support at this time")
+	}
+	return nil
+}
+
+// checkURL creates the URL with the transfer server value and a path
+// and checks it for validity and same domain as the asset.
+func (a *anchorInteractor) checkURL(mctx libkb.MetaContext, action string) (*url.URL, error) {
+	u, err := url.ParseRequestURI(a.asset.TransferServer)
+	if err != nil {
+		return nil, err
+	}
+	u.Path = path.Join(u.Path, action)
+
+	if !a.domainMatches(u.Host) {
+		return nil, errors.New("transfer server hostname does not match asset hostname")
+	}
+
+	if u.Scheme != "https" {
+		return nil, errors.New("transfer server URL is not https")
+	}
+
+	if u.RawQuery != "" {
+		return nil, errors.New("transfer server URL has a query")
+	}
+
+	return u, nil
+}
+
+type okDepositResponse struct {
+	How        string  `json:"how"`
+	ETA        int     `json:"int"`
+	MinAmount  float64 `json:"min_amount"`
+	MaxAmount  float64 `json:"max_amount"`
+	FeeFixed   float64 `json:"fee_fixed"`
+	FeePercent float64 `json:"fee_percent"`
+	ExtraInfo  struct {
+		Message string `json:"message"`
+	} `json:"extra_info"`
+}
+
+// this will never happen, but:
+func (r *okDepositResponse) String() string {
+	return fmt.Sprintf("Deposit request approved by anchor.  %s: %s", r.How, r.ExtraInfo.Message)
+}
+
+type okWithdrawResponse struct {
+	AccountID  string  `json:"account_id"`
+	MemoType   string  `json:"memo_type"` // text, id or hash
+	Memo       string  `json:"memo"`
+	ETA        int     `json:"int"`
+	MinAmount  float64 `json:"min_amount"`
+	MaxAmount  float64 `json:"max_amount"`
+	FeeFixed   float64 `json:"fee_fixed"`
+	FeePercent float64 `json:"fee_percent"`
+}
+
+// this will never happen, but:
+func (r *okWithdrawResponse) String() string {
+	return fmt.Sprintf("Withdraw request approved by anchor.  Send token to %s.  Memo: %s (%s)", r.AccountID, r.Memo, r.MemoType)
+}
+
+type forbiddenResponse struct {
+	Type  string `json:"type"`
+	URL   string `json:"url"`
+	ID    string `json:"id"`
+	Error string `json:"error"`
+}
+
+// get performs the http GET requests and parses the result.
+func (a *anchorInteractor) get(mctx libkb.MetaContext, u *url.URL, okResponse fmt.Stringer) (stellar1.AssetActionResultLocal, error) {
+	mctx.Debug("performing http GET to %s", u)
+	code, body, err := a.httpGetClient(mctx, u.String())
+	if err != nil {
+		mctx.Debug("GET failed: %s", err)
+		return stellar1.AssetActionResultLocal{}, err
+	}
+	switch code {
+	case http.StatusOK:
+		if err := json.Unmarshal(body, okResponse); err != nil {
+			mctx.Debug("json unmarshal of 200 response failed: %s", err)
+			return stellar1.AssetActionResultLocal{}, err
+		}
+		msg := okResponse.String()
+		return stellar1.AssetActionResultLocal{MessageFromAnchor: &msg}, nil
+	case http.StatusForbidden:
+		var resp forbiddenResponse
+		if err := json.Unmarshal(body, &resp); err != nil {
+			mctx.Debug("json unmarshal of 403 response failed: %s", err)
+			return stellar1.AssetActionResultLocal{}, err
+		}
+		if resp.Error != "" {
+			return stellar1.AssetActionResultLocal{}, fmt.Errorf("Error from anchor: %s", resp.Error)
+		}
+		if resp.Type == "interactive_customer_info_needed" {
+			parsed, err := url.Parse(resp.URL)
+			if err != nil {
+				mctx.Debug("invalid URL received from anchor: %s", resp.URL)
+				return stellar1.AssetActionResultLocal{}, errors.New("invalid URL received from anchor")
+			}
+			if !a.domainMatches(parsed.Host) {
+				mctx.Debug("response URL on a different domain than asset domain: %s vs. %s", resp.URL, a.asset.VerifiedDomain)
+				return stellar1.AssetActionResultLocal{}, errors.New("anchor requesting opening a different domain")
+			}
+			return stellar1.AssetActionResultLocal{ExternalUrl: &resp.URL}, nil
+		}
+		mctx.Debug("unhandled anchor response for %s: %+v", u, resp)
+		return stellar1.AssetActionResultLocal{}, errors.New("unhandled asset anchor http response")
+	default:
+		mctx.Debug("unhandled anchor response code for %s: %d", u, code)
+		return stellar1.AssetActionResultLocal{}, errors.New("unhandled asset anchor http response code")
+	}
+}
+
+// httpGet is the live version of httpGetClient that is used
+// by default.
+func httpGet(mctx libkb.MetaContext, url string) (int, []byte, error) {
+	client := http.Client{Timeout: 10 * time.Second}
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return 0, nil, err
+	}
+
+	res, err := client.Do(req.WithContext(mctx.Ctx()))
+	if err != nil {
+		return 0, nil, err
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		return 0, nil, err
+	}
+
+	return res.StatusCode, body, nil
+}
+
+func (a *anchorInteractor) domainMatches(url string) bool {
+	urlTLD, err := publicsuffix.EffectiveTLDPlusOne(strings.ToLower(url))
+	if err != nil {
+		return false
+	}
+	assetTLD, err := publicsuffix.EffectiveTLDPlusOne(strings.ToLower(a.asset.VerifiedDomain))
+	if err != nil {
+		return false
+	}
+	return urlTLD == assetTLD
+}
---
+++ go/stellar/stellarsvc/anchor_test.go
@@ -0,0 +1,231 @@
+package stellarsvc
+
+import (
+	"errors"
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/keybase/client/go/libkb"
+	"github.com/keybase/client/go/protocol/stellar1"
+)
+
+type anchorTest struct {
+	Name                string
+	Asset               stellar1.Asset
+	DepositExternalURL  string
+	WithdrawExternalURL string
+}
+
+var errAnchorTests = []anchorTest{
+	{
+		Name: "not verified",
+		Asset: stellar1.Asset{
+			Type:   "credit_alphanum4",
+			Code:   "EUR",
+			Issuer: "GAKBPBDMW6CTRDCXNAPSVJZ6QAN3OBNRG6CWI27FGDQT2ZJJEMDRXPKK",
+		},
+	},
+	{
+		Name: "no transfer server",
+		Asset: stellar1.Asset{
+			Type:           "credit_alphanum4",
+			Code:           "EUR",
+			Issuer:         "GAKBPBDMW6CTRDCXNAPSVJZ6QAN3OBNRG6CWI27FGDQT2ZJJEMDRXPKK",
+			VerifiedDomain: "keybase.io",
+		},
+	},
+	{
+		Name: "requires auth",
+		Asset: stellar1.Asset{
+			Type:           "credit_alphanum4",
+			Code:           "EUR",
+			Issuer:         "GAKBPBDMW6CTRDCXNAPSVJZ6QAN3OBNRG6CWI27FGDQT2ZJJEMDRXPKK",
+			VerifiedDomain: "keybase.io",
+			TransferServer: "https://transfer.keybase.io/transfer",
+			AuthEndpoint:   "https://transfer.keybase.io/auth",
+		},
+	},
+	{
+		Name: "invalid url",
+		Asset: stellar1.Asset{
+			Type:           "credit_alphanum4",
+			Code:           "EUR",
+			Issuer:         "GAKBPBDMW6CTRDCXNAPSVJZ6QAN3OBNRG6CWI27FGDQT2ZJJEMDRXPKK",
+			VerifiedDomain: "keybase.io",
+			TransferServer: ":transfer.keybase.io/transfer",
+		},
+	},
+	{
+		Name: "different host",
+		Asset: stellar1.Asset{
+			Type:           "credit_alphanum4",
+			Code:           "EUR",
+			Issuer:         "GAKBPBDMW6CTRDCXNAPSVJZ6QAN3OBNRG6CWI27FGDQT2ZJJEMDRXPKK",
+			VerifiedDomain: "keybase.io",
+			TransferServer: "https://transfer.keybays.io/transfer",
+		},
+	},
+	{
+		Name: "http not https",
+		Asset: stellar1.Asset{
+			Type:           "credit_alphanum4",
+			Code:           "EUR",
+			Issuer:         "GAKBPBDMW6CTRDCXNAPSVJZ6QAN3OBNRG6CWI27FGDQT2ZJJEMDRXPKK",
+			VerifiedDomain: "keybase.io",
+			TransferServer: "http://transfer.keybase.io/transfer",
+		},
+	},
+	{
+		Name: "ftp not https",
+		Asset: stellar1.Asset{
+			Type:           "credit_alphanum4",
+			Code:           "EUR",
+			Issuer:         "GAKBPBDMW6CTRDCXNAPSVJZ6QAN3OBNRG6CWI27FGDQT2ZJJEMDRXPKK",
+			VerifiedDomain: "keybase.io",
+			TransferServer: "ftp://transfer.keybase.io/transfer",
+		},
+	},
+	{
+		Name: "has a query",
+		Asset: stellar1.Asset{
+			Type:           "credit_alphanum4",
+			Code:           "EUR",
+			Issuer:         "GAKBPBDMW6CTRDCXNAPSVJZ6QAN3OBNRG6CWI27FGDQT2ZJJEMDRXPKK",
+			VerifiedDomain: "keybase.io",
+			TransferServer: "https://transfer.keybase.io/transfer?x=123",
+		},
+	},
+	{
+		Name: "endpoint not found",
+		Asset: stellar1.Asset{
+			Type:           "credit_alphanum4",
+			Code:           "EUR",
+			Issuer:         "GAKBPBDMW6CTRDCXNAPSVJZ6QAN3OBNRG6CWI27FGDQT2ZJJEMDRXPKK",
+			VerifiedDomain: "keybase.io",
+			TransferServer: "https://transfer.keybase.io/nope",
+		},
+	},
+	{
+		Name: "external url changes domain name",
+		Asset: stellar1.Asset{
+			Type:           "credit_alphanum4",
+			Code:           "EUR",
+			Issuer:         "GAKBPBDMW6CTRDCXNAPSVJZ6QAN3OBNRG6CWI27FGDQT2ZJJEMDRXPKK",
+			VerifiedDomain: "keybase.io",
+			TransferServer: "https://transfer.keybase.io/transfer",
+		},
+		DepositExternalURL:  "https://portal.anchorusd.com/onboarding?account=GBZX4364PEPQTDICMIQDZ56K4T75QZCR4NBEYKO6PDRJAHZKGUOJPCXB&identifier=b700518e7430513abdbdab96e7ead566",
+		WithdrawExternalURL: "https://portal.anchorusd.com/onboarding?account=GACW7NONV43MZIFHCOKCQJAKSJSISSICFVUJ2C6EZIW5773OU3HD64VI",
+	},
+}
+
+var validAnchorTests = []anchorTest{
+	{
+		Name: "valid",
+		Asset: stellar1.Asset{
+			Type:           "credit_alphanum4",
+			Code:           "EUR",
+			Issuer:         "GAKBPBDMW6CTRDCXNAPSVJZ6QAN3OBNRG6CWI27FGDQT2ZJJEMDRXPKK",
+			VerifiedDomain: "www.anchorusd.com",
+			TransferServer: "https://api.anchorusd.com/transfer",
+		},
+		DepositExternalURL:  "https://portal.anchorusd.com/onboarding?account=GBZX4364PEPQTDICMIQDZ56K4T75QZCR4NBEYKO6PDRJAHZKGUOJPCXB&identifier=b700518e7430513abdbdab96e7ead566",
+		WithdrawExternalURL: "https://portal.anchorusd.com/onboarding?account=GACW7NONV43MZIFHCOKCQJAKSJSISSICFVUJ2C6EZIW5773OU3HD64VI",
+	},
+}
+
+func TestAnchorInteractor(t *testing.T) {
+	tc := SetupTest(t, "AnchorInteractor", 1)
+	for i, test := range errAnchorTests {
+		accountID, _ := randomStellarKeypair()
+		ai := newAnchorInteractor(accountID, test.Asset)
+		ai.httpGetClient = mockKeybaseTransferGet
+		_, err := ai.Deposit(tc.MetaContext())
+		if err == nil {
+			t.Errorf("err test %d [%s]: Deposit returned no error, but expected one", i, test.Name)
+			continue
+		}
+		_, err = ai.Withdraw(tc.MetaContext())
+		if err == nil {
+			t.Errorf("err test %d [%s]: Withdraw returned no error, but expected one", i, test.Name)
+			continue
+		}
+	}
+
+	for i, test := range validAnchorTests {
+		accountID, _ := randomStellarKeypair()
+		ai := newAnchorInteractor(accountID, test.Asset)
+		ai.httpGetClient = mockAnchorUSDTransferGet
+		res, err := ai.Deposit(tc.MetaContext())
+		if err != nil {
+			t.Errorf("valid test %d [%s]: Deposit returned an error: %s", i, test.Name, err)
+			continue
+		}
+		if res.ExternalUrl == nil && res.MessageFromAnchor == nil {
+			t.Errorf("valid test %d [%s] deposit: result fields are all nil", i, test.Name)
+			continue
+		}
+		if test.DepositExternalURL != "" && res.ExternalUrl == nil {
+			t.Errorf("valid test %d [%s] deposit: result external url field is nil, expected %s", i, test.Name, test.DepositExternalURL)
+			continue
+		}
+		if res.ExternalUrl != nil {
+			if test.DepositExternalURL != *res.ExternalUrl {
+				t.Errorf("valid test %d [%s] deposit: result external url field %s, expected %s", i, test.Name, *res.ExternalUrl, test.DepositExternalURL)
+			}
+		}
+
+		res, err = ai.Withdraw(tc.MetaContext())
+		if err != nil {
+			t.Errorf("valid test %d [%s]: Withdraw returned an error: %s", i, test.Name, err)
+			continue
+		}
+		if res.ExternalUrl == nil && res.MessageFromAnchor == nil {
+			t.Errorf("valid test %d [%s] withdraw: result fields are all nil", i, test.Name)
+			continue
+		}
+		if test.WithdrawExternalURL != "" && res.ExternalUrl == nil {
+			t.Errorf("valid test %d [%s] withdraw: result external url field is nil, expected %s", i, test.Name, test.WithdrawExternalURL)
+			continue
+		}
+		if res.ExternalUrl != nil {
+			if test.WithdrawExternalURL != *res.ExternalUrl {
+				t.Errorf("valid test %d [%s] withdraw: result external url field %s, expected %s", i, test.Name, *res.ExternalUrl, test.WithdrawExternalURL)
+			}
+		}
+	}
+}
+
+// mockKeybaseTransferGet is an httpGetClient func that returns a stored result
+// for TRANSFER_SERVER/deposit and TRANSFER_SERVER/withdraw
+func mockKeybaseTransferGet(mctx libkb.MetaContext, url string) (int, []byte, error) {
+	parts := strings.Split(url, "?")
+	switch parts[0] {
+	case "https://transfer.keybase.io/transfer/deposit":
+		return http.StatusForbidden, []byte(depositBody), nil
+	case "https://transfer.keybase.io/transfer/withdraw":
+		return http.StatusForbidden, []byte(withdrawBody), nil
+	default:
+		return 0, nil, errors.New("unknown mocked url")
+	}
+
+}
+
+// mockAnchorUSDTransferGet is an httpGetClient func that returns a stored result
+// for TRANSFER_SERVER/deposit and TRANSFER_SERVER/withdraw
+func mockAnchorUSDTransferGet(mctx libkb.MetaContext, url string) (int, []byte, error) {
+	parts := strings.Split(url, "?")
+	switch parts[0] {
+	case "https://api.anchorusd.com/transfer/deposit":
+		return http.StatusForbidden, []byte(depositBody), nil
+	case "https://api.anchorusd.com/transfer/withdraw":
+		return http.StatusForbidden, []byte(withdrawBody), nil
+	default:
+		return 0, nil, errors.New("unknown mocked url")
+	}
+
+}
+
+const depositBody = `{"type":"interactive_customer_info_needed","url":"https://portal.anchorusd.com/onboarding?account=GBZX4364PEPQTDICMIQDZ56K4T75QZCR4NBEYKO6PDRJAHZKGUOJPCXB&identifier=b700518e7430513abdbdab96e7ead566","identifier":"b700518e7430513abdbdab96e7ead566","dimensions":{"width":800,"height":600}}`
+const withdrawBody = `{ "type": "interactive_customer_info_needed", "url" : "https://portal.anchorusd.com/onboarding?account=GACW7NONV43MZIFHCOKCQJAKSJSISSICFVUJ2C6EZIW5773OU3HD64VI", "id": "82fhs729f63dh0v4" }`
