f95fe642c08cab7e235b2e4928df14ebbabc0ec1
cecileboucheron <cecile@keyba.se>
2019-08-02 15:32:08 -0700

    wallet button type

--- shared/wallets/asset/index.tsx
+++ shared/wallets/asset/index.tsx
@@ -112,6 +112,7 @@ export default class Asset extends React.Component<Props, State> {
                         label={this.props.depositButtonText}
                         onClick={this.props.onDeposit}
                         small={true}
+                        type="Wallet"
                       />
                     </>
                   )}
@@ -123,6 +124,7 @@ export default class Asset extends React.Component<Props, State> {
                         label={this.props.withdrawButtonText}
                         onClick={this.props.onWithdraw}
                         small={true}
+                        type="Wallet"
                       />
                     </>
                   )}
@@ -132,6 +134,7 @@ export default class Asset extends React.Component<Props, State> {
                       label={this.props.infoUrlText}
                       onClick={this.props.openInfoURL}
                       small={true}
+                      type="Wallet"
                     />
                   )}
                 </Kb.ButtonBar>
