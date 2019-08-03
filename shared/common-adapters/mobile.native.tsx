8158e09353e15684f2e7063d453d9632aa1c838d
chrisnojima <cnojima@keyba.se>
2019-08-03 11:40:31 -0400

    WIP

---
+++ shared/common-adapters/mobile.native.tsx
@@ -0,0 +1,12 @@
+import ReAnimated, {Easing as ReAnimatedEasing} from 'react-native-reanimated'
+import Swipeable from 'react-native-gesture-handler/Swipeable'
+import {RectButton} from 'react-native-gesture-handler'
+
+module.hot &&
+  module.hot.accept(() => {
+    console.log('accepted update in common-adapters/mobile.native')
+  })
+export * from '.'
+export * from './native-wrappers.native'
+export * from './zoomable-box'
+export {ReAnimated, ReAnimatedEasing, RectButton, Swipeable}
