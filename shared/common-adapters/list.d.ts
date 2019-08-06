592ee5c4e33f7a0d42f6de89643a0f798b2c0a94
Danny Ayoub <danny.a95@gmail.com>
2019-08-06 12:08:41 -0400

    Don't spam search while scrolling results (#18797)
    * reduce onEndReachedThreshold and throttle call
    
    * Improve "search more"
    
    * keyboardShouldPersistTaps="handled"
    
    * remove other throttle

--- shared/common-adapters/list.d.ts
+++ shared/common-adapters/list.d.ts
@@ -15,6 +15,7 @@ export type Props<Item> = {
   keyboardShouldPersistTaps?: 'never' | 'always' | 'handled' // mobile only,
   windowSize?: number // Mobile only, has a non-RN default,
   onEndReached?: () => void
+  onEndReachedThreshold?: number // mobile only
   ListHeaderComponent?: React.ReactNode // mobile only
 }
 
