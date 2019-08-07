1bf78f089424da0534b5108664fd8496231a2aa9
chrisnojima <cnojima@keyba.se>
2019-08-07 11:27:12 -0400

    WIP

--- shared/index.ios.js
+++ shared/index.ios.js
@@ -7,13 +7,6 @@ if (__STORYBOOK__) {
   const load = require('./storybook/index.native').default
   load()
 } else {
-  const {setIsDarkMode} = require('./styles/dark-mode')
-  // TODO plumb to some setting
-  // TODO later detect on ios13 (not in rn currently)
-  if (__DEV__) {
-    setIsDarkMode(true)
-  }
-
   const {load} = require('./app/index.native')
   load()
 }
