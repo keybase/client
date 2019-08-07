57257eaa7dd9a2d0d120560aad2f5b59874638e9
chrisnojima <cnojima@keyba.se>
2019-08-07 11:25:58 -0400

    WIP

---
+++ shared/rn-css-transformer.js
@@ -0,0 +1,12 @@
+const upstreamTransformer = require('metro-react-native-babel-transformer')
+
+module.exports.transform = function(p) {
+  if (p.filename.endsWith('css')) {
+    return upstreamTransformer.transform({
+      filename: p.filename,
+      options: p.options,
+      src: 'module.export = ""',
+    })
+  }
+  return upstreamTransformer.transform(p)
+}
