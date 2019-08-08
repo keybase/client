0eae53884f60a3dc76813a36b0580d30c696d9f1
Daniel Ayoub <danny.a95@gmail.com>
2019-08-08 16:32:13 -0400

    misc cleanup

--- shared/settings/account/container.tsx
+++ shared/settings/account/container.tsx
@@ -8,24 +8,14 @@ import AccountSettings from '.'
 import {isMobile} from '../../styles'
 
 type OwnProps = {}
-const mapStateToProps = (state: TypedState) => {
-  const supersededPhoneNumberRecord =
-    state.settings.phoneNumbers.phones &&
-    state.settings.phoneNumbers.phones.find(phoneNumber => phoneNumber.superseded)
-
-  return {
-    _emails: state.settings.email.emails,
-    _phones: state.settings.phoneNumbers.phones,
-    _supersededPhoneNumberKey: supersededPhoneNumberRecord && supersededPhoneNumberRecord.e164,
-    addedEmail: state.settings.email.addedEmail,
-    bootstrapDone: state.settings.email.emails !== null && state.settings.phoneNumbers.phones !== null,
-    hasPassword: !state.settings.password.randomPW,
-    supersededPhoneNumber: supersededPhoneNumberRecord
-      ? supersededPhoneNumberRecord.displayNumber
-      : undefined,
-    waiting: anyWaiting(state, Constants.loadSettingsWaitingKey),
-  }
-}
+const mapStateToProps = (state: TypedState) => ({
+  _emails: state.settings.email.emails,
+  _phones: state.settings.phoneNumbers.phones,
+  addedEmail: state.settings.email.addedEmail,
+  bootstrapDone: state.settings.email.emails !== null && state.settings.phoneNumbers.phones !== null,
+  hasPassword: !state.settings.password.randomPW,
+  waiting: anyWaiting(state, Constants.loadSettingsWaitingKey),
+})
 
 const mapDispatchToProps = (dispatch: TypedDispatch) => ({
   _onClearSupersededPhoneNumber: phone => dispatch(SettingsGen.createEditPhone({delete: true, phone})),
@@ -48,21 +38,24 @@ const mapDispatchToProps = (dispatch: TypedDispatch) => ({
 export default connect(
   mapStateToProps,
   mapDispatchToProps,
-  (stateProps, dispatchProps, _: OwnProps) => ({
-    ...dispatchProps,
-    addedEmail: stateProps.addedEmail,
-    contactKeys: I.List([
-      ...(stateProps._emails ? stateProps._emails.keys() : []),
-      ...(stateProps._phones ? stateProps._phones.keys() : []),
-    ]),
-    hasPassword: stateProps.hasPassword,
-    onClearSupersededPhoneNumber: () =>
-      stateProps._supersededPhoneNumberKey &&
-      dispatchProps._onClearSupersededPhoneNumber(stateProps._supersededPhoneNumberKey),
-    supersededPhoneNumber: stateProps.supersededPhoneNumber,
-    title: 'Your account',
-    tooManyEmails: !!stateProps._emails && stateProps._emails.size >= 10, // If you change this, also change in keybase/config/prod/email.iced
-    tooManyPhones: !!stateProps._phones && stateProps._phones.size >= 10, // If you change this, also change in keybase/config/prod/phone_numbers.iced
-    waiting: stateProps.waiting,
-  })
+  (stateProps, dispatchProps, _: OwnProps) => {
+    const supersededPhoneNumber = stateProps._phones && stateProps._phones.find(p => p.superseded)
+    const supersededKey = supersededPhoneNumber && supersededPhoneNumber.e164
+    return {
+      ...dispatchProps,
+      addedEmail: stateProps.addedEmail,
+      contactKeys: I.List([
+        ...(stateProps._emails ? stateProps._emails.keys() : []),
+        ...(stateProps._phones ? stateProps._phones.keys() : []),
+      ]),
+      hasPassword: stateProps.hasPassword,
+      onClearSupersededPhoneNumber: () =>
+        supersededKey && dispatchProps._onClearSupersededPhoneNumber(supersededKey),
+      supersededPhoneNumber: supersededPhoneNumber ? supersededPhoneNumber.displayNumber : undefined,
+      title: 'Your account',
+      tooManyEmails: !!stateProps._emails && stateProps._emails.size >= 10, // If you change this, also change in keybase/config/prod/email.iced
+      tooManyPhones: !!stateProps._phones && stateProps._phones.size >= 10, // If you change this, also change in keybase/config/prod/phone_numbers.iced
+      waiting: stateProps.waiting,
+    }
+  }
 )(AccountSettings)
