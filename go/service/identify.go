e43f6460c7d474591b17e83e51787d05e0631291
Song Gao <song@gao.io>
2019-08-09 09:58:44 -0700

    fix test maybe

--- go/service/identify.go
+++ go/service/identify.go
@@ -357,10 +357,6 @@ func (h *IdentifyHandler) resolveIdentifyImplicitTeamDoIdentifies(ctx context.Co
 			if idErr != nil {
 				h.G().Log.CDebugf(subctx, "Failed to convert result from Identify2: %s", idErr)
 			}
-			if err != nil {
-				h.G().Log.CDebugf(subctx, "identify failed (IDres %v, TrackBreaks %v): %v", idRes != nil, idRes != nil && idRes.TrackBreaks != nil, err)
-				return err
-			}
 			if idRes != nil && idRes.TrackBreaks != nil && idErr == nil {
 				trackBreaksLock.Lock()
 				defer trackBreaksLock.Unlock()
@@ -371,6 +367,10 @@ func (h *IdentifyHandler) resolveIdentifyImplicitTeamDoIdentifies(ctx context.Co
 				brokenUsernames = append(brokenUsernames, idRes.Upk.GetName())
 			} else {
 				okUsernames = append(okUsernames, idRes.Upk.GetName())
+			}
+			if err != nil {
+				h.G().Log.CDebugf(subctx, "identify failed (IDres %v, TrackBreaks %v): %v", idRes != nil, idRes != nil && idRes.TrackBreaks != nil, err)
+				return err
 			}
 			return nil
 		})
