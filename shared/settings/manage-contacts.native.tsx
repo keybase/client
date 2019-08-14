6f3fe48069a7e2a2cdc3c488b58594141a6d3956
Daniel Ayoub <danny.a95@gmail.com>
2019-08-14 14:03:25 -0400

    factor out ManageContactsBanner

--- shared/settings/manage-contacts.native.tsx
+++ shared/settings/manage-contacts.native.tsx
@@ -10,21 +10,15 @@ import * as Styles from '../styles'
 import {appendNewChatBuilder} from '../actions/typed-routes'
 import {SettingsSection} from './account/'
 
-type Props = {
-  contactsImported: boolean
-  onToggleImport: () => void
-}
-
 const enabledDescription = 'Your phone contacts are being synced on this device.'
 const disabledDescription = 'Import your phone contacts and start encrypted chats with your friends.'
 
-const ManageContacts = (_: Props) => {
+const ManageContacts = () => {
   const dispatch = Container.useDispatch()
   const nav = Container.useSafeNavigation()
 
   const status = Container.useSelector(s => s.settings.contacts.permissionStatus)
   const contactsImported = Container.useSelector(s => s.settings.contacts.importEnabled)
-  const importedCount = Container.useSelector(s => s.settings.contacts.importedCount)
   const waiting = Container.useAnyWaiting(Constants.importContactsWaitingKey)
 
   if (contactsImported === null) {
@@ -41,39 +35,12 @@ const ManageContacts = (_: Props) => {
       ),
     [dispatch, contactsImported, status]
   )
-  const onOpenAppSettings = React.useCallback(() => dispatch(ConfigGen.createOpenAppSettings()), [dispatch])
-  const onStartChat = React.useCallback(() => {
-    dispatch(RouteTreeGen.createSwitchTab({tab: Tabs.chatTab}))
-    dispatch(appendNewChatBuilder())
-  }, [dispatch])
 
   return (
     <Kb.Box2 direction="vertical" fullWidth={true} fullHeight={true} style={styles.positionRelative}>
       <Kb.HeaderHocHeader title="Contacts" onBack={onBack} />
       <Kb.BoxGrow>
-        {importedCount !== null && (
-          <Kb.Banner color="green">
-            <Kb.BannerParagraph bannerColor="green" content={[`You imported ${importedCount} contacts.`]} />
-            <Kb.BannerParagraph
-              bannerColor="green"
-              content={[{onClick: onStartChat, text: 'Start a chat'}]}
-            />
-          </Kb.Banner>
-        )}
-        {(status === 'never_ask_again' || (Styles.isAndroid && status !== 'granted' && contactsImported)) && (
-          <Kb.Banner color="red">
-            <Kb.BannerParagraph
-              bannerColor="red"
-              content={[
-                contactsImported
-                  ? "Contact importing is paused because Keybase doesn't have permission to access your contacts. "
-                  : "Keybase doesn't have permission to access your contacts. ",
-                {onClick: onOpenAppSettings, text: 'Enable in settings'},
-                '.',
-              ]}
-            />
-          </Kb.Banner>
-        )}
+        <ManageContactsBanner />
         <SettingsSection>
           <Kb.Box2 direction="vertical" gap="xtiny" fullWidth={true}>
             <Kb.Text type="Header">Phone contacts</Kb.Text>
@@ -95,6 +62,45 @@ const ManageContacts = (_: Props) => {
         </SettingsSection>
       </Kb.BoxGrow>
     </Kb.Box2>
+  )
+}
+
+const ManageContactsBanner = () => {
+  const dispatch = Container.useDispatch()
+
+  const status = Container.useSelector(s => s.settings.contacts.permissionStatus)
+  const contactsImported = Container.useSelector(s => s.settings.contacts.importEnabled)
+  const importedCount = Container.useSelector(s => s.settings.contacts.importedCount)
+
+  const onOpenAppSettings = React.useCallback(() => dispatch(ConfigGen.createOpenAppSettings()), [dispatch])
+  const onStartChat = React.useCallback(() => {
+    dispatch(RouteTreeGen.createSwitchTab({tab: Tabs.chatTab}))
+    dispatch(appendNewChatBuilder())
+  }, [dispatch])
+
+  return (
+    <>
+      {importedCount !== null && (
+        <Kb.Banner color="green">
+          <Kb.BannerParagraph bannerColor="green" content={[`You imported ${importedCount} contacts.`]} />
+          <Kb.BannerParagraph bannerColor="green" content={[{onClick: onStartChat, text: 'Start a chat'}]} />
+        </Kb.Banner>
+      )}
+      {(status === 'never_ask_again' || (Styles.isAndroid && status !== 'granted' && contactsImported)) && (
+        <Kb.Banner color="red">
+          <Kb.BannerParagraph
+            bannerColor="red"
+            content={[
+              contactsImported
+                ? "Contact importing is paused because Keybase doesn't have permission to access your contacts. "
+                : "Keybase doesn't have permission to access your contacts. ",
+              {onClick: onOpenAppSettings, text: 'Enable in settings'},
+              '.',
+            ]}
+          />
+        </Kb.Banner>
+      )}
+    </>
   )
 }
 
