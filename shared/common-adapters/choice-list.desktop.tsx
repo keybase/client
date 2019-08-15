8340a1ed13bfb990192a64ef6a20a15c1fca10f4
Cori Hudson <cori@keyba.se>
2019-08-15 11:05:56 -0400

    move choice-list css to file; use Styles.padding function

--- shared/common-adapters/choice-list.desktop.tsx
+++ shared/common-adapters/choice-list.desktop.tsx
@@ -2,18 +2,28 @@ import * as React from 'react'
 import {Box, Text, Icon, IconType} from '../common-adapters'
 import * as Styles from '../styles'
 import {Props} from './choice-list'
-import { darkColors } from '../styles/colors';
 
 const ChoiceList = ({options}: Props) => {
   return (
     <Box>
-      <style>{rawCSS}</style>
       {options.map((op, idx) => {
         // TODO is this okay?
         const iconType: IconType = op.icon as IconType
         return (
-          <Box style={Styles.collapseStyles([Styles.globalStyles.flexBoxRow, Styles.desktopStyles.clickable, styles.entry])} key={idx} className="cl-entry" onClick={() => op.onClick()}>
-            <Box style={Styles.collapseStyles([Styles.globalStyles.flexBoxColumn, styles.iconContainer])} className="cl-icon-container">
+          <Box
+            style={Styles.collapseStyles([
+              Styles.globalStyles.flexBoxRow,
+              Styles.desktopStyles.clickable,
+              styles.entry,
+            ])}
+            key={idx}
+            className="cl-entry"
+            onClick={() => op.onClick()}
+          >
+            <Box
+              style={Styles.collapseStyles([Styles.globalStyles.flexBoxColumn, styles.iconContainer])}
+              className="cl-icon-container"
+            >
               {typeof op.icon === 'string' ? (
                 <Icon style={styles.icon} type={iconType} className="cl-icon" />
               ) : (
@@ -33,41 +43,13 @@ const ChoiceList = ({options}: Props) => {
   )
 }
 
-const rawCSS = `
-  .cl-entry {
-    background-color: transparent;
-  }
-  .cl-entry:hover {
-    background-color: ${Styles.globalColors.blueLighter2};
-  }
-  .darkMode .cl-entry:hover {
-    background-color: ${darkColors.blueLighter2}
-  }
-
-  .cl-icon {
-    transform-origin: center center;
-    transition: 0.5s transform;
-  }
-  .cl-entry:hover .cl-icon {
-    transform: translateX(25%);
-  }
-
-  .cl-icon-container {
-    transition: 0.5s background;
-    border-radius: 50%;
-  }
-  .cl-entry:hover .cl-icon-container {
-    background: transparent;
-  }
-`
-
 const styles = Styles.styleSheetCreate(() => ({
   entry: {
-    padding: `${Styles.globalMargins.tiny}px ${Styles.globalMargins.small}px`,
-    width: '100%'
+    ...Styles.padding(Styles.globalMargins.tiny, Styles.globalMargins.small),
+    width: '100%',
   },
   iconContainer: {
-    alignItems: 'center', 
+    alignItems: 'center',
     justifyContent: 'center',
     height: 80,
     width: 80,
@@ -75,15 +57,15 @@ const styles = Styles.styleSheetCreate(() => ({
   },
   icon: {
     height: 48,
-    width: 48
+    width: 48,
   },
   infoContainer: {
     alignItems: 'flex-start',
     flex: 1,
     justifyContent: 'center',
     marginLeft: Styles.globalMargins.small,
-    textAlign: 'left', 
-  }
+    textAlign: 'left',
+  },
 }))
 
 export default ChoiceList
