37325bb69bd1f55c306c15047d82f8ae1bb35095
heronhaye <surya@keyba.se>
2019-08-15 23:21:20 +0200

    server driven gapu

--- go/libkb/all_provisioned_usernames.go
+++ go/libkb/all_provisioned_usernames.go
@@ -1,5 +1,11 @@
 package libkb
 
+import (
+	"time"
+
+	"github.com/keybase/client/go/protocol/keybase1"
+)
+
 func getUsernameIfProvisioned(m MetaContext, uc UserConfig) (ret NormalizedUsername, err error) {
 	m.Debug("getUsernameIfProvisioned(%+v)", uc)
 	did := uc.GetDeviceID()
@@ -32,35 +38,59 @@ func getUsernameIfProvisioned(m MetaContext, uc UserConfig) (ret NormalizedUsern
 	}
 }
 
-// GetAllProvisionedUsernames looks into the current config.json file, and finds all usernames
-// that are currently provisioned on this device. That is, it filters out those that are on revoked
-// devices or have reset their accounts. It uses UPAK loading for verifying the current user/device
-// statuses, so should be fast if everything is cached recently.
-func GetAllProvisionedUsernames(m MetaContext) (current NormalizedUsername, all []NormalizedUsername, err error) {
+type deviceForUsersRet struct {
+	AppStatusEmbed
+	UserConfigs []deviceForUser `json:"user_configs"`
+}
+type deviceForUser struct {
+	UID      keybase1.UID       `json:"uid"`
+	DeviceID keybase1.DeviceID  `json:"device_id"`
+	OK       bool               `json:"ok"`
+	Username NormalizedUsername `json:"username"`
+}
 
-	m = m.WithLogTag("GAPU")
-	defer m.Trace("GetAllProvisionedUsernames", func() error { return err })()
+// GetAllProvisionedUsernames looks into the current config.json file, and
+// finds all usernames that are currently provisioned on this device. Then, it
+// asks the server to filter out revoked devices or reset users.
+func GetAllProvisionedUsernames(mctx MetaContext) (current NormalizedUsername, all []NormalizedUsername, err error) {
+	mctx = mctx.WithLogTag("GAPU")
+	defer mctx.Trace("GetAllProvisionedUsernames", func() error { return err })()
 
-	currentUC, allUCs, err := m.G().Env.GetConfig().GetAllUserConfigs()
+	currentUC, allUCs, err := mctx.G().Env.GetConfig().GetAllUserConfigs()
 	if err != nil {
 		return current, nil, err
 	}
 
+	var userConfigs []deviceForUser
 	if currentUC != nil {
-		current, err = getUsernameIfProvisioned(m, *currentUC)
-		if err != nil {
-			m.Error("Error while checking user %q uid=%q, `current` will be nil", currentUC.GetUsername(), currentUC.GetUID())
-		}
+		userConfigs = append(userConfigs, deviceForUser{UID: currentUC.GetUID(), DeviceID: currentUC.GetDeviceID()})
+	}
+	for _, uc := range allUCs {
+		userConfigs = append(userConfigs, deviceForUser{UID: uc.GetUID(), DeviceID: uc.GetDeviceID()})
 	}
 
-	for _, uc := range allUCs {
-		tmp, err := getUsernameIfProvisioned(m, uc)
-		if err != nil {
-			m.Error("Error while checking user %q uid=%q, skipping", uc.GetUsername(), uc.GetUID())
-			continue
-		}
-		if !tmp.IsNil() {
-			all = append(all, tmp)
+	payload := make(JSONPayload)
+	payload["user_configs"] = userConfigs
+	arg := APIArg{
+		Endpoint:       "device/for_users",
+		JSONPayload:    payload,
+		SessionType:    APISessionTypeNONE,
+		InitialTimeout: 5 * time.Second,
+		RetryCount:     3,
+	}
+
+	resp := deviceForUsersRet{}
+	err = mctx.G().API.PostDecode(mctx, arg, &resp)
+	if err != nil {
+		return "", nil, err
+	}
+
+	for _, userConfig := range resp.UserConfigs {
+		if userConfig.OK {
+			all = append(all, userConfig.Username)
+			if userConfig.Username == currentUC.GetUsername() {
+				current = userConfig.Username
+			}
 		}
 	}
 
