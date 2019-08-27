951ff6bf8ad0ac257a7015308fd96f6b9aaa0fa4
Mike Maxim <mike.maxim@gmail.com>
2019-08-27 11:15:49 -0400

    wtf

--- shared/android/app/src/main/java/io/keybase/ossifrage/generated/BasePackageList.java
+++ shared/android/app/src/main/java/io/keybase/ossifrage/generated/BasePackageList.java
@@ -7,7 +7,6 @@ import org.unimodules.core.interfaces.Package;
 public class BasePackageList {
   public List<Package> getPackageList() {
     return Arrays.<Package>asList(
-        new expo.modules.av.AVPackage(),
         new expo.modules.barcodescanner.BarCodeScannerPackage(),
         new expo.modules.constants.ConstantsPackage(),
         new expo.modules.contacts.ContactsPackage(),
