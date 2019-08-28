cd4713d55f04cbb0ba2a5c347eba851683066ba6
John Zila <john@jzila.com>
2019-08-28 13:09:23 -0700

    refactor app main a bit more

--- shared/app/main.native.tsx
+++ shared/app/main.native.tsx
@@ -10,47 +10,34 @@ type OwnProps = {}
 
 type Props = {}
 
-class Main extends React.Component<Props> {
-  render() {
-    // TODO likely collapse index.native/main.native/nav.native etc
-    return (
-      <>
-        <RouterSwitcheroo />
-        <GatewayDest
-          name="popup-root"
-          component={ViewForGatewayDest}
-          pointerEvents="box-none"
-          style={Styles.globalStyles.fillAbsolute}
-        />
-        <Kb.KeyboardAvoidingView
-          style={Styles.globalStyles.fillAbsolute}
-          pointerEvents="box-none"
-          behavior={Styles.isIOS ? 'padding' : undefined}
-        >
-          <GatewayDest
-            name="keyboard-avoiding-root"
-            component={ViewForGatewayDest}
-            pointerEvents="box-none"
-            style={styles.gatewayDest}
-          />
-        </Kb.KeyboardAvoidingView>
-      </>
-    )
-  }
-}
-class ViewForGatewayDest extends React.Component {
-  render() {
-    return <View {...this.props} />
-  }
-}
+const Main = (props: Props) => (
+  <>
+    <RouterSwitcheroo />
+    <GatewayDest
+      name="popup-root"
+      component={ViewForGatewayDest}
+      pointerEvents="box-none"
+      style={Styles.globalStyles.fillAbsolute}
+    />
+    <Kb.KeyboardAvoidingView
+      style={Styles.globalStyles.fillAbsolute}
+      pointerEvents="box-none"
+      behavior={Styles.isIOS ? 'padding' : undefined}
+    >
+      <GatewayDest
+        name="keyboard-avoiding-root"
+        component={ViewForGatewayDest}
+        pointerEvents="box-none"
+        style={styles.gatewayDest}
+      />
+    </Kb.KeyboardAvoidingView>
+  </>
+)
+
+const ViewForGatewayDest = (props: Props) => <View {...props} />
 
 const styles = Styles.styleSheetCreate({
   gatewayDest: {flexGrow: 1, width: '100%'},
 })
 
-const Connected = connect(
-  () => ({}),
-  () => ({}),
-  (s, d, o: OwnProps) => ({...o, ...s, ...d})
-)(Main)
-export default Connected
+export default Main
