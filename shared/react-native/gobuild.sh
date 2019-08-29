dc5e8a31da280bbcdbf380ad84c0f57d8bb8da91
chrisnojima <cnojima@keyba.se>
2019-08-29 09:39:45 -0400

    WIP

--- shared/react-native/gobuild.sh
+++ shared/react-native/gobuild.sh
@@ -45,8 +45,6 @@ echo "Using GOPATH: $GOPATH"
 # already in $PATH (like the old GOPATH), so put $GOPATH/bin first.
 PATH="$GOPATH/bin:$PATH"
 
-# if we don't set this gomobile init get confused
-# GOMOBILE="$GOPATH/pkg/gomobile"
 # need to whitelist some flags we use
 export CGO_CFLAGS_ALLOW="-fmodules|-fblocks"
 
