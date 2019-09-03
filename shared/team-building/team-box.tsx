3e3041233dff6a50197af38b5a93713dc1237e50
Song Gao <song@gao.io>
2019-09-03 07:23:06 -0700

    fix horizontal scrolling in new chat conversation (#19161)
    * fix horizontal scrolling in new chat conversation
    
    * story
    
    * review feedback from buoyad
    
    * story

--- shared/team-building/team-box.tsx
+++ shared/team-building/team-box.tsx
@@ -62,7 +62,7 @@ const TeamBox = (props: Props) => {
   const last = !!props.teamSoFar.length && props.teamSoFar[props.teamSoFar.length - 1].userId
   const prevLast = Container.usePrevious(last)
   React.useEffect(() => {
-    if (Styles.isMobile && prevLast !== undefined && prevLast !== last && scrollViewRef.current) {
+    if (prevLast !== undefined && prevLast !== last && scrollViewRef.current) {
       scrollViewRef.current.scrollToEnd({animated: true})
     }
   }, [prevLast, last])
@@ -82,8 +82,13 @@ const TeamBox = (props: Props) => {
   ) : (
     <Kb.Box2 direction="horizontal" style={styles.container} fullWidth={true}>
       <Kb.Box2 direction="horizontal" style={styles.bubbles}>
-        <Kb.ScrollView horizontal={true}>
-          <Kb.Box2 direction="horizontal" fullHeight={true} style={styles.floatingBubbles}>
+        <Kb.ScrollView
+          horizontal={true}
+          ref={scrollViewRef}
+          showsHorizontalScrollIndicator={false}
+          showsVerticalScrollIndicator={false}
+        >
+          <Kb.Box2 direction="horizontal" fullHeight={true}>
             <UserBubbleCollection teamSoFar={props.teamSoFar} onRemove={props.onRemove} />
             {addMorePrompt}
           </Kb.Box2>
@@ -143,11 +148,6 @@ const styles = Styles.styleSheetCreate(() => ({
       minHeight: 90,
       paddingBottom: Styles.globalMargins.tiny,
       paddingTop: Styles.globalMargins.tiny,
-    },
-  }),
-  floatingBubbles: Styles.platformStyles({
-    isElectron: {
-      justifyContent: 'flex-end',
     },
   }),
   search: Styles.platformStyles({
