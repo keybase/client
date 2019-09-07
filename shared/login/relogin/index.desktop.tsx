ff1efa4f681b7f3088701b43411f8385a9c89ebb
John Zila <john@keyba.se>
2019-09-06 19:44:22 -0700

    styling almost done

--- shared/login/relogin/index.desktop.tsx
+++ shared/login/relogin/index.desktop.tsx
@@ -54,24 +54,17 @@ class Login extends React.Component<Props, State> {
       autoFocus: true,
       errorText: this.props.inputError,
       floatingHintTextOverride: '',
+      hideLabel: true,
       hintText: 'Password',
+      inputStyle: styles.passwordInput,
       key: this.props.inputKey,
       onChangeText: password => this.props.passwordChange(password),
       onEnterKeyDown: () => this.props.onSubmit(),
       ref: this._inputRef,
+      style: styles.passwordInputContainer,
       type: this.props.showTyping ? 'passwordVisible' : 'password',
       uncontrolled: true,
     } as const
-
-    const checkboxProps = [
-      {
-        checked: this.props.showTyping,
-        label: 'Show typing',
-        onCheck: check => {
-          this.props.showTypingChange(check)
-        },
-      } as const,
-    ]
 
     const userRows = this.props.users
       .concat({hasStoredSecret: false, username: other})
@@ -79,48 +72,54 @@ class Login extends React.Component<Props, State> {
 
     const selectedIdx = this.props.users.findIndex(u => u.username === this.props.selectedUser)
 
+    const temp = (
+      <Kb.Text style={{marginTop: 30}} type="BodyPrimaryLink" onClick={this.props.onSignup}>
+        Create an account
+      </Kb.Text>
+    )
     return (
-      <SignupScreen banners={errorBanner(this.props.bannerError)} title="Log in">
-        <Kb.Box2
-          direction="vertical"
-          fullHeight={true}
-          fullWidth={true}
-          style={styles.contentBox}
-          gap="medium"
-        >
-          <Kb.Box style={styles.userContainer}>
-            <Kb.UserCard username={this.props.selectedUser}>
-              <Kb.Dropdown
-                onChanged={this._onClickUser}
-                selected={userRows[selectedIdx]}
-                items={userRows}
-                position="bottom center"
-              />
-              <Kb.FormWithCheckbox
-                style={{alignSelf: 'stretch'}}
-                inputProps={inputProps}
-                checkboxesProps={checkboxProps}
-              />
+      <SignupScreen banners={errorBanner(this.props.bannerError)} headerStyle={styles.header} title="Log in">
+        <Kb.Box2 direction="vertical" fullHeight={true} fullWidth={true} style={styles.contentBox}>
+          <Kb.UserCard
+            username={this.props.selectedUser}
+            outerStyle={styles.container}
+            style={styles.userContainer}
+          >
+            <Kb.Dropdown
+              onChanged={this._onClickUser}
+              selected={userRows[selectedIdx]}
+              items={userRows}
+              position="bottom center"
+              style={styles.userDropdown}
+            />
+            <Kb.Box2 direction="horizontal" fullWidth={true} style={styles.inputRow}>
+              <Kb.Input {...inputProps} />
+            </Kb.Box2>
+            <Kb.Box2 direction="horizontal" fullWidth={true} style={styles.forgotPasswordContainer}>
+              <Kb.Text
+                type="BodySmallSecondaryLink"
+                onClick={this.props.onForgotPassword}
+                style={styles.forgotPassword}
+              >
+                Forgot password?
+              </Kb.Text>
+            </Kb.Box2>
+            <Kb.Box2
+              direction="vertical"
+              fullWidth={true}
+              fullHeight={true}
+              style={styles.loginSubmitContainer}
+            >
               <Kb.WaitingButton
                 disabled={!this.props.password}
                 fullWidth={true}
                 waitingKey={Constants.waitingKey}
-                style={{marginTop: 0, width: '100%'}}
+                style={styles.loginSubmitButton}
                 label="Log in"
-                onClick={() => this.props.onSubmit()}
+                onClick={this.props.onSubmit}
               />
-              <Kb.Text
-                type="BodySmallSecondaryLink"
-                onClick={this.props.onForgotPassword}
-                style={{marginTop: 24}}
-              >
-                Forgot password?
-              </Kb.Text>
-            </Kb.UserCard>
-            <Kb.Text style={{marginTop: 30}} type="BodyPrimaryLink" onClick={this.props.onSignup}>
-              Create an account
-            </Kb.Text>
-          </Kb.Box>
+            </Kb.Box2>
+          </Kb.UserCard>
         </Kb.Box2>
       </SignupScreen>
     )
@@ -128,21 +127,66 @@ class Login extends React.Component<Props, State> {
 }
 
 const styles = Styles.styleSheetCreate({
-  contentBox: Styles.platformStyles({
-    common: {alignSelf: 'center', flexGrow: 1},
-    isElectron: {
-      maxWidth: 460,
-      padding: Styles.globalMargins.small,
-    },
-  }),
-  other: {color: Styles.globalColors.black},
-  provisioned: {color: Styles.globalColors.orange},
-  userContainer: {
+  container: {
     ...Styles.globalStyles.flexBoxColumn,
     alignItems: 'center',
-    backgroundColor: Styles.globalColors.transparent,
     flex: 1,
     justifyContent: 'center',
+  },
+  contentBox: {
+    alignSelf: 'center',
+    flexGrow: 1,
+    maxWidth: 460,
+    padding: Styles.globalMargins.small,
+  },
+  forgotPassword: {
+    marginTop: Styles.globalMargins.tiny,
+  },
+  forgotPasswordContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  header: {
+    borderBottomWidth: 0,
+  },
+  inputRow: {
+    flex: 1,
+  },
+  loginSubmitButton: {
+    marginTop: 0,
+    maxHeight: 32,
+    width: '100%',
+  },
+  loginSubmitContainer: {
+    flexGrow: 1,
+    justifyContent: 'flex-end',
+  },
+  other: {color: Styles.globalColors.black},
+  passwordInput: {
+    backgroundColor: Styles.globalColors.white,
+    borderColor: Styles.globalColors.black_10,
+    borderRadius: 4,
+    borderStyle: 'solid',
+    borderWidth: 1,
+    paddingBottom: Styles.globalMargins.medium,
+    paddingLeft: Styles.globalMargins.xsmall,
+    paddingRight: Styles.globalMargins.xsmall,
+    paddingTop: Styles.globalMargins.medium,
+    textAlign: 'left',
+    width: '100%',
+  },
+  passwordInputContainer: {
+    marginBottom: 0,
+    marginTop: Styles.globalMargins.tiny,
+    width: '100%',
+  },
+  provisioned: {color: Styles.globalColors.orange},
+  userContainer: {
+    backgroundColor: Styles.globalColors.transparent,
+  },
+  userDropdown: {
+    backgroundColor: Styles.globalColors.white,
+    width: '100%',
   },
 })
 
