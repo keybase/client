c67197608329494fa917f3e4af204a811d8b6340
chrisnojima <cnojima@keyba.se>
2019-09-09 14:08:26 -0400

    WIP

--- shared/index.ios.js
+++ shared/index.ios.js
@@ -1,12 +1,19 @@
 // @flow
 // React-native tooling assumes this file is here, so we just require our real entry point
 import './app/globals.native'
+import {NativeModules} from 'react-native'
+import {_setSystemIsDarkMode} from './styles/dark-mode'
 
 // Load storybook or the app
 if (__STORYBOOK__) {
   const load = require('./storybook/index.native').default
   load()
 } else {
+  const NativeAppearance = NativeModules.Appearance
+
+  if (NativeAppearance) {
+    _setSystemIsDarkMode(NativeAppearance.initialColorScheme === 'dark')
+  }
   const {load} = require('./app/index.native')
   load()
 }
