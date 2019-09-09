d647c95633ea09c4a041f15453dffb52a7cf3243
chrisnojima <cnojima@keyba.se>
2019-09-09 16:37:11 -0400

    WIP

--- shared/desktop/renderer/preload-main.dev.desktop.tsx
+++ shared/desktop/renderer/preload-main.dev.desktop.tsx
@@ -1,6 +1,5 @@
 import url from 'url'
 import events from 'events'
-import * as Electron from 'electron'
 import './preload-main.shared.desktop'
 // dev only, needed by dev server
 
@@ -9,10 +8,5 @@ window.KB = {
   DEV: {
     events,
     url,
-    toggleDevTools: (enable: boolean) => {
-      Electron.BrowserWindow.getAllWindows().map(bw =>
-        enable ? bw.webContents.openDevTools({mode: 'detach'}) : bw.webContents.closeDevTools()
-      )
-    },
   },
 }
