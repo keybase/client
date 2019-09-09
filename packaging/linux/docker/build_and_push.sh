1dbcd191f148ca979d920c947fc6c6531b16168a
Piotr Zduniak <piotr@zduniak.net>
2019-09-09 22:37:56 +0200

    dotn print secret key

--- packaging/linux/docker/build_and_push.sh
+++ packaging/linux/docker/build_and_push.sh
@@ -5,15 +5,19 @@ build_dir="$1"
 here="$(dirname "$BASH_SOURCE")"
 client_dir="$(git -C "$here" rev-parse --show-toplevel)"
 source_commit="$(git -C "$here" log -1 --pretty=format:%h)"
-code_signing_fingerprint="$(cat "$here/../code_signing_fingerprint")"
-armored_private_key="$(gpg --export-secret-keys)"
 tag="$(cat "$build_dir/VERSION" | tr "+" "-")"
+
+# Clear the directory used for temporary, just in case a previous build failed
+rm -r $client_dir/.docker || true
+mkdir -p $client_dir/.docker
+code_signing_fingerprint="$(cat "$here/../code_signing_fingerprint")"
+gpg_tempfile="$client_dir/.docker/code_signing_key"
+gpg --export-secret-key --armor "$code_signing_fingerprint" > "$gpg_tempfile"
 
 # Build all three variants
 docker build \
   --build-arg SOURCE_COMMIT="$source_commit" \
   --build-arg SIGNING_FINGERPRINT="$code_signing_fingerprint" \
-  --build-arg ARMORED_PRIVATE_KEY="$armored_private_key" \
   -f $client_dir/packaging/linux/docker/service/Dockerfile \
   -t "keybaseio/service:$tag" \
   $client_dir
@@ -29,6 +33,9 @@ docker build \
   -f $client_dir/packaging/linux/docker/client-slim/Dockerfile \
   -t "keybaseio/client:$tag-slim" \
   $client_dir
+
+# Don't store any secrets in the repo dir
+rm -r $client_dir/.docker || true
 
 # Push it (TODO Y2K-607)
 #docker tag keybaseio/client:$tag keybaseio/client:nightly
