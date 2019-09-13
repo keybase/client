6e99a508ee690098fe374119019fe7cbc244f7fe
cecileboucheron <cecile@keyba.se>
2019-09-13 10:53:48 -0700

    48

--- shared/wallets/transaction/index.tsx
+++ shared/wallets/transaction/index.tsx
@@ -41,7 +41,7 @@ const CounterpartyIcon = (props: CounterpartyIconProps) => {
         />
       )
     case 'stellarPublicKey':
-      return <Icon type="icon-placeholder-secret-user-48" style={{height: size, width: size}} />
+      return <Icon type="icon-placeholder-secret-user-48" style={{height: 48, width: 48}} />
     case 'otherAccount':
       return (
         <Box2
