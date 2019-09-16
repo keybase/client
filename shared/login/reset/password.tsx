6965ea0fe89ba1bf1c45baab459fc7f50b823cf8
Daniel Ayoub <danny.a95@gmail.com>
2019-09-16 16:37:06 -0400

    do you know it

--- shared/login/reset/password.tsx
+++ shared/login/reset/password.tsx
@@ -1,11 +1,32 @@
 import * as React from 'react'
 import * as Kb from '../../common-adapters'
 import * as Styles from '../../styles'
+import {SignupScreen} from '../../signup/common'
 
-type KnowPasswordProps = {}
-const KnowPassword = (props: KnowPasswordProps) => <Kb.Text type="HeaderBig">Know your password?</Kb.Text>
+const todo = () => console.log('todo')
 
-type EnterPasswordProps = {}
-const EnterPassword = (props: EnterPasswordProps) => <Kb.Text type="HeaderBig">Enter your password?</Kb.Text>
+const KnowPassword = () => (
+  <SignupScreen
+    title="Account reset"
+    noBackground={true}
+    onBack={todo}
+    leftActionText="Cancel"
+    buttons={[{label: 'Yes', type: 'Success', onClick: todo}, {label: 'No', type: 'Dim', onClick: todo}]}
+  >
+    <Kb.Box2 direction="vertical" gap="medium" fullWidth={true} fullHeight={true} centerChildren={true}>
+      <Kb.Icon type="iconfont-wave-2" color={Styles.globalColors.black} fontSize={24} />
+      <Kb.Box2 direction="vertical" centerChildren={true}>
+        <Kb.Text type="Header" center={true}>
+          Do you know your
+        </Kb.Text>
+        <Kb.Text type="Header">password?</Kb.Text>
+      </Kb.Box2>
+    </Kb.Box2>
+  </SignupScreen>
+)
+
+const EnterPassword = () => <Kb.Text type="HeaderBig">Enter your password?</Kb.Text>
+
+const styles = Styles.styleSheetCreate(() => ({}))
 
 export {EnterPassword, KnowPassword}
