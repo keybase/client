7bbb67fcd7e4cc5c3f89d0fb2ca148676dbd1f9e
chrisnojima <cnojima@keyba.se>
2019-09-20 16:46:48 -0400

    WIP

---
+++ shared/ios/Pods/SDWebImage/SDWebImage/Core/SDAnimatedImageRep.h
@@ -0,0 +1,22 @@
+/*
+ * This file is part of the SDWebImage package.
+ * (c) Olivier Poitrey <rs@dailymotion.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+#import "SDWebImageCompat.h"
+
+#if SD_MAC
+
+/**
+ A subclass of `NSBitmapImageRep` to fix that GIF loop count issue because `NSBitmapImageRep` will reset `NSImageCurrentFrameDuration` by using `kCGImagePropertyGIFDelayTime` but not `kCGImagePropertyGIFUnclampedDelayTime`.
+ Built in GIF coder use this instead of `NSBitmapImageRep` for better GIF rendering. If you do not want this, only enable `SDImageIOCoder`, which just call `NSImage` API and actually use `NSBitmapImageRep` for GIF image.
+ This also support APNG format using `SDImageAPNGCoder`. Which provide full alpha-channel support and the correct duration match the `kCGImagePropertyAPNGUnclampedDelayTime`.
+ */
+@interface SDAnimatedImageRep : NSBitmapImageRep
+
+@end
+
+#endif
