6e169b4e3a381f273c2fa25b6256dbb0f17ea84c
Adam Spooner <adam@keyba.se>
2019-09-23 16:31:28 -0400

    WIP

--- shared/teams/team/settings-tab/index.tsx
+++ shared/teams/team/settings-tab/index.tsx
@@ -302,7 +302,7 @@ export class Settings extends React.Component<Props, State> {
           alignSelf: 'stretch',
           flexBasis: 0,
           flexGrow: 1,
-          padding: globalMargins.medium,
+          padding: globalMargins.small,
         }}
       >
         <SetMemberShowcase {...this.props} {...this.state} setBoolSettings={this.setBoolSettings} />
