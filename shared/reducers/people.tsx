f5fef3ba1ead95b0bdefda90bbf90f66d00df874
chrisnojima <cnojima@keyba.se>
2019-09-23 11:00:14 -0400

    WIP

--- shared/reducers/people.tsx
+++ shared/reducers/people.tsx
@@ -1,10 +1,15 @@
 import * as Types from '../constants/types/people'
+import * as TeamBuildingGen from '../actions/team-building-gen'
 import * as Constants from '../constants/people'
 import * as PeopleGen from '../actions/people-gen'
+import teamBuildingReducer from './team-building'
 
 const initialState: Types.State = Constants.makeState()
 
-export default function(state: Types.State = initialState, action: PeopleGen.Actions): Types.State {
+export default function(
+  state: Types.State = initialState,
+  action: PeopleGen.Actions | TeamBuildingGen.Actions
+): Types.State {
   switch (action.type) {
     case PeopleGen.resetStore:
       return initialState
@@ -25,6 +30,21 @@ export default function(state: Types.State = initialState, action: PeopleGen.Act
     case PeopleGen.skipTodo:
     case PeopleGen.dismissAnnouncement:
       return state
+    case TeamBuildingGen.resetStore:
+    case TeamBuildingGen.cancelTeamBuilding:
+    case TeamBuildingGen.addUsersToTeamSoFar:
+    case TeamBuildingGen.removeUsersFromTeamSoFar:
+    case TeamBuildingGen.searchResultsLoaded:
+    case TeamBuildingGen.finishedTeamBuilding:
+    case TeamBuildingGen.fetchedUserRecs:
+    case TeamBuildingGen.fetchUserRecs:
+    case TeamBuildingGen.search:
+    case TeamBuildingGen.selectRole:
+    case TeamBuildingGen.labelsSeen:
+    case TeamBuildingGen.changeSendNotification:
+      return state.merge({
+        teamBuilding: teamBuildingReducer('people', state.teamBuilding, action),
+      })
     default:
       return state
   }
