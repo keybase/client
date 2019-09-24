623d2a478be6afc3b6686eb6fefd7390c42179c5
Heronhaye <surya@keyba.se>
2019-09-24 16:33:14 -0400

    dont want systemd if started with custom home

--- go/libkb/env.go
+++ go/libkb/env.go
@@ -2017,7 +2017,7 @@ func (e *Env) RunningInCI() bool {
 }
 
 func (e *Env) WantsSystemd() bool {
-	return (e.GetRunMode() == ProductionRunMode && e.ModelessWantsSystemd())
+	return (e.GetRunMode() == ProductionRunMode && e.ModelessWantsSystemd() && e.Cmd.GetHome() == "")
 }
 
 func (e *Env) ModelessWantsSystemd() bool {
