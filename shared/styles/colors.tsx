657d1badd448b4732844ce038a9744e5d2631dcc
Daniel Ayoub <danny.a95@gmail.com>
2019-09-24 11:29:50 -0400

    fix popup underlay

--- shared/styles/colors.tsx
+++ shared/styles/colors.tsx
@@ -26,6 +26,9 @@ export const colors = {
   black_35: 'rgba(0, 0, 0, 0.35)',
   black_40: 'rgba(0, 0, 0, 0.40)',
   black_50: 'rgba(0, 0, 0, 0.50)',
+  get black_50OrBlack_60() {
+    return this.black_50
+  },
   get black_50OrWhite() {
     return this.black_50
   },
@@ -159,6 +162,9 @@ export const darkColors: {[P in keyof typeof colors]: string | undefined} = {
   black_35: 'rgba(255, 255, 255, 0.35)',
   black_40: 'rgba(255, 255, 255, 0.40)',
   black_50: 'rgba(255, 255, 255, 0.50)',
+  get black_50OrBlack_60() {
+    return colors.black_60
+  },
   get black_50OrWhite() {
     return colors.white
   },
