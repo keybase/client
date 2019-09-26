af61a73bede68c51316342da168e6c099e88a9fe
Song Gao <song@gao.io>
2019-09-26 15:07:55 -0700

    oops

--- shared/fs/browser/index.stories.tsx
+++ shared/fs/browser/index.stories.tsx
@@ -29,7 +29,7 @@ const _provider = {
 
 const storeCommon = Sb.createStoreWithCommon()
 const storeShowingSfmi = produce(storeCommon, draftStoreCommon => {
-  draftStoreCommon.fs.sfmi.showingBanner = true
+  draftStoreCommon.fs.sfmi = draftStoreCommon.fs.sfmi.set('showingBanner', true)
 })
 
 const provider = Sb.createPropProviderWithCommon(_provider)
