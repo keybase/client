0d4862989e66fc1f86b5862da02984790abd1e46
Jacob Young <jacob@jry.io>
2019-09-27 17:34:29 -0400

    Stories for new feature row

--- shared/whats-new/index.stories.tsx
+++ shared/whats-new/index.stories.tsx
@@ -1,13 +1,80 @@
 import React from 'react'
 import * as Sb from '../stories/storybook'
+import * as Kb from '../common-adapters'
+import * as Styles from '../styles'
 import HeaderIcon from './header-icon'
 import NewFeature from './new-feature'
 
+const commonNewFeatureProps = {
+  primaryButton: false,
+  secondaryButton: false,
+  seen: false,
+  text: '',
+}
+
+const newFeatures = [
+  {
+    ...commonNewFeatureProps,
+    text: 'Some short text',
+  },
+  {
+    ...commonNewFeatureProps,
+    text:
+      'Some very very long text that goes on forever and ever. Some very very long text that goes on forever and ever. Some very very long text that goes on forever and ever. Some very very long text that goes on forever and ever. Some very very long text that goes on forever and ever.',
+  },
+  {
+    ...commonNewFeatureProps,
+    primaryButton: true,
+    primaryButtonText: 'Try it out',
+    secondaryButton: true,
+    secondaryButtonText: 'No way',
+    text:
+      'Some very very long text that goes on forever and ever. Some very very long text that goes on forever and ever. Some very very long text that goes on forever and ever. Some very very long text that goes on forever and ever. Some very very long text that goes on forever and ever.',
+  },
+  {
+    ...commonNewFeatureProps,
+    primaryButton: true,
+    primaryButtonText: 'Lots to do on this button',
+    secondaryButton: true,
+    secondaryButtonText: 'Lots here as well',
+    text: 'Short text but long button text',
+  },
+]
+
+const NewFeatureWrapper = ({children}) => (
+  <Kb.Box2 direction="vertical" alignItems="flex-start" style={styles.newFeatureContainer}>
+    {children}
+  </Kb.Box2>
+)
+
 const load = () => {
   Sb.storiesOf('Whats New', module)
+    .addDecorator(Sb.scrollViewDecorator)
     .add('Radio Icon - Nothing New', () => <HeaderIcon newFeatures={false} />)
     .add('Radio Icon - New Features', () => <HeaderIcon newFeatures={true} />)
-    .add('New Feature - Seen - Text Only', () => <NewFeature seen={true} text="hey" />)
+    .add('New Feature Row', () =>
+      newFeatures.map((feature, index) => (
+        <NewFeatureWrapper key={index}>
+          <NewFeature {...feature} />
+        </NewFeatureWrapper>
+      ))
+    )
 }
+
+const modalWidth = 284
+const modalHeight = 424
+
+const styles = Styles.styleSheetCreate(() => ({
+  newFeatureContainer: {
+    backgroundColor: Styles.globalColors.blueGrey,
+    marginTop: 20,
+    maxHeight: modalHeight,
+    maxWidth: modalWidth,
+    paddingBottom: 8,
+    paddingLeft: 8,
+    paddingRight: 8,
+    paddingTop: 8,
+  },
+}))
 
 export default load
--- shared/whats-new/new-feature.tsx
+++ shared/whats-new/new-feature.tsx
@@ -5,15 +5,43 @@ import * as Styles from '../styles'
 type Props = {
   text: string
   seen: boolean
+  imageSrc?: string
+  primaryButton: boolean
+  primaryButtonText?: string
+  primaryButtonPath?: string
+  primaryButtonExternal?: boolean
+  secondaryButton: boolean
+  secondaryButtonText?: string
+  secondaryButtonPath?: string
+  secondaryButtonExternal?: boolean
 }
 
 const NewFeature = (props: Props) => {
   return (
-    <Kb.Box2 direction="horizontal">
-      {props.seen && (
+    <Kb.Box2 direction="horizontal" fullWidth={true} style={styles.container}>
+      {/* Badging */}
+      {!props.seen && (
         <Kb.Badge height={8} badgeStyle={styles.badgeStyle} containerStyle={styles.badgeContainerStyle} />
       )}
-      <Kb.Box2 direction="vertical" style={styles.contentContainer} />
+      <Kb.Box2 direction="vertical" fullWidth={true} style={styles.contentContainer}>
+        <Kb.Text type="Body">{props.text}</Kb.Text>
+        <Kb.Box2 direction="vertical" style={styles.imageContainer}>
+          {props.imageSrc && <Kb.Image src={props.imageSrc} style={styles.image} />}
+        </Kb.Box2>
+        <Kb.Box2 direction="horizontal" style={styles.buttonRowContainer} gap="tiny">
+          {props.primaryButton && (
+            <Kb.Button type="Default" mode="Primary" label={props.primaryButtonText} style={styles.buttons} />
+          )}
+          {props.secondaryButton && (
+            <Kb.Button
+              type="Default"
+              mode="Secondary"
+              label={props.secondaryButtonText}
+              style={styles.buttons}
+            />
+          )}
+        </Kb.Box2>
+      </Kb.Box2>
     </Kb.Box2>
   )
 }
@@ -24,14 +52,35 @@ const styles = Styles.styleSheetCreate(() => ({
   },
   badgeStyle: {
     backgroundColor: Styles.globalColors.blue,
+    marginRight: Styles.globalMargins.xsmall,
+    marginTop: 13,
   },
-  container: {},
+  buttonRowContainer: {
+    ...Styles.globalStyles.flexWrap,
+    alignSelf: 'flex-start',
+    justifyContent: 'space-between',
+  },
+  buttons: {
+    // Apply margins to buttons so that when they wrap there is vertical space between them
+    marginTop: Styles.globalMargins.xsmall,
+  },
+  container: {
+    ...Styles.globalStyles.fullWidth,
+    alignSelf: 'flex-start',
+  },
   contentContainer: {
+    backgroundColor: Styles.globalColors.white,
     paddingBottom: Styles.globalMargins.tiny,
     paddingLeft: Styles.globalMargins.small,
     paddingRight: Styles.globalMargins.small,
     paddingTop: Styles.globalMargins.tiny,
   },
+  image: {
+    alignSelf: 'center',
+    maxHeight: 96,
+    maxWidth: 216,
+  },
+  imageContainer: {},
 }))
 
 export default NewFeature
