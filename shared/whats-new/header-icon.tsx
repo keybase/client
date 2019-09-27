804d7e87024b38599e404b839237266ad42e885a
Jacob Young <jacob@jry.io>
2019-09-27 10:10:57 -0400

    Desktop only gradient

--- shared/whats-new/header-icon.tsx
+++ shared/whats-new/header-icon.tsx
@@ -8,8 +8,21 @@ type Props = {
 
 const HeaderIcon = (props: Props) =>
   props.newFeatures ? (
-    <Kb.Icon type="iconfont-radio" color={Styles.globalColors.black} />
+    Styles.isMobile ? null : (
+      <Kb.Icon type="iconfont-radio" color="transparent" style={styles.rainbowColor} />
+    )
   ) : (
     <Kb.Icon type="iconfont-radio" color={Styles.globalColors.black} />
   )
+
+const styles = Styles.styleSheetCreate(() => ({
+  rainbowColor: Styles.platformStyles({
+    isElectron: {
+      WebkitBackgroundClip: 'text',
+      WebkitTextFillColor: 'transparent',
+      background:
+        'linear-gradient(to top, #ff0000, rgba(255, 216, 0, 0.94) 19%, #27c400 40%, #0091ff 60%, #b000ff 80%, #ff0098)',
+    },
+  }),
+}))
 export default HeaderIcon
