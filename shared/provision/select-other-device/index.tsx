63134632c317c24383343be63f0d4e6d47a7f5c8
cecileboucheron <cecile@keyba.se>
2019-10-02 11:18:55 -0700

    padding tweaks on device list

--- shared/provision/select-other-device/index.tsx
+++ shared/provision/select-other-device/index.tsx
@@ -123,12 +123,14 @@ const styles = Styles.styleSheetCreate(() => ({
     common: {alignSelf: 'center', flexGrow: 1},
     isElectron: {
       maxWidth: 460,
-      padding: Styles.globalMargins.small,
+      paddingLeft: Styles.globalMargins.small,
+      paddingRight: Styles.globalMargins.small,
     },
   }),
   headerText: Styles.platformStyles({
     common: {
       paddingBottom: Styles.globalMargins.small,
+      paddingTop: Styles.globalMargins.small,
     },
     isMobile: {
       paddingLeft: Styles.globalMargins.small,
@@ -138,6 +140,7 @@ const styles = Styles.styleSheetCreate(() => ({
   }),
   list: {
     flexGrow: 1,
+    marginBottom: Styles.globalMargins.large,
   },
   or: {
     backgroundColor: Styles.globalColors.blueGrey,
