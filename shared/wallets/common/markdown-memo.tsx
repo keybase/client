3b287365e8ae7cbd95e7f87307ba59767168119f
John Zila <john@keyba.se>
2019-10-02 10:30:37 -0700

    fix

--- shared/wallets/common/markdown-memo.tsx
+++ shared/wallets/common/markdown-memo.tsx
@@ -2,7 +2,6 @@ import * as React from 'react'
 import * as Kb from '../../common-adapters'
 import * as Styles from '../../styles'
 import {StyleOverride} from '../../common-adapters/markdown'
-import {isMobile} from '../../constants/platform'
 
 const styleOverride: StyleOverride = Styles.styleSheetCreate(() => ({
   del: {
