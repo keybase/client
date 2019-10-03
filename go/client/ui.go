c6cf53bdef7d4f23ef0be8ebd28503bb2a9200a0
Michał Zochniak <michal@zapu.net>
2019-10-03 23:26:24 +0200

    Fix autoreset prompt in terminal

--- go/client/ui.go
+++ go/client/ui.go
@@ -830,24 +830,20 @@ func (l LoginUI) PromptResetAccount(ctx context.Context, arg keybase1.PromptRese
 	case keybase1.ResetPromptType_ENTER_NO_DEVICES:
 		msg = `The only way to provision this device is with access to one of your existing
 devices. You can try again later, or if you have lost access to all your
-existing devices you can reset your account and start fresh.
-
-Would you like to request a reset of your account?`
+existing devices you can reset your account and start fresh.`
 	case keybase1.ResetPromptType_ENTER_FORGOT_PW:
 		msg = `If you have forgotten your password and either lost all of your devices, or if you
 uninstalled Keybase from all of them, you can reset your account. You will keep your username,
-but lose all your data.
-
-Would you like to request a reset of your account?`
+but lose all your data.`
 	case keybase1.ResetPromptType_ENTER_RESET_PW:
 		msg = `If you have forgotten your password you can reset your password. You will keep your
-username, but lose all your data, including all of your uploaded encrypted PGP keys.
-
-Would you like to request a reset of your account?`
+username, but lose all your data, including all of your uploaded encrypted PGP keys.`
 	default:
 		return false, fmt.Errorf("Unknown prompt type - got %v", kind)
 	}
-	return l.parent.PromptYesNo(PromptDescriptorResetAccount, msg, libkb.PromptDefaultNo)
+	l.parent.PrintfUnescaped("%s\n\n", msg)
+	question := "Would you like to request a reset of your account?"
+	return l.parent.PromptYesNo(PromptDescriptorResetAccount, question, libkb.PromptDefaultNo)
 }
 
 func (l LoginUI) DisplayResetProgress(ctx context.Context, arg keybase1.DisplayResetProgressArg) error {
