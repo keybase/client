be81e6702a175266eafd17113fd092a3da02235d
Taru Karttunen <taruti@taruti.net>
2019-10-04 16:09:44 +0000

    Linux start on login GUI item

--- shared/actions/platform-specific/index.desktop.tsx
+++ shared/actions/platform-specific/index.desktop.tsx
@@ -394,7 +394,7 @@ const setOpenAtLogin = async (state: Container.TypedState) => {
 }
 
 const setNixOnLoginStartup = async (enabled: boolean) => {
-  RPCTypes.ctlSetNixOnLoginStartupRpcPromise(enabled).catch(err => {
+  RPCTypes.ctlSetNixOnLoginStartupRpcPromise({enabled}).catch(err => {
     console.info(`Error in sending ctlSetNixOnLoginStartup: ${err.message}`)
   })
 }
