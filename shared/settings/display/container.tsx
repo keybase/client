019099a4fb38eabbf2aa14cf5db797002e66fcd8
Adam Spooner <adam@keyba.se>
2019-10-04 12:45:36 -0400

    Move dark mode settings to display tab.

---
+++ shared/settings/display/container.tsx
@@ -0,0 +1,28 @@
+import * as ConfigGen from '../../actions/config-gen'
+import * as RouteTreeGen from '../../actions/route-tree-gen'
+import {HeaderHoc} from '../../common-adapters'
+import {compose} from 'recompose'
+import Display from '.'
+import {connect, TypedState} from '../../util/container'
+import {DarkModePreference} from '../../styles/dark-mode'
+
+type OwnProps = {}
+const mapStateToProps = (state: TypedState) => ({
+  darkModePreference: state.config.darkModePreference,
+})
+
+const mapDispatchToProps = dispatch => ({
+  onBack: () => dispatch(RouteTreeGen.createNavigateUp()),
+  onSetDarkModePreference: (preference: DarkModePreference) =>
+    dispatch(ConfigGen.createSetDarkModePreference({preference})),
+})
+
+export default compose(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps,
+    (s, d, o: OwnProps) => ({...o, ...s, ...d})
+  ),
+  HeaderHoc
+  // @ts-ignore
+)(Display)
