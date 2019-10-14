418c2a6ea39b3871b6b4f64b0a23ee059c04967d
Piotr Zduniak <piotr@zduniak.net>
2019-10-14 10:57:58 +0100

    fix passphrase recovery if no username is passed (#20336)

--- go/engine/passphrase_recover.go
+++ go/engine/passphrase_recover.go
@@ -59,6 +59,15 @@ func (e *PassphraseRecover) SubConsumers() []libkb.UIConsumer {
 func (e *PassphraseRecover) Run(mctx libkb.MetaContext) (err error) {
 	defer mctx.Trace("PassphraseRecover#Run", func() error { return err })()
 
+	// If no username was passed, ask for one
+	if e.arg.Username == "" {
+		res, err := mctx.UIs().LoginUI.GetEmailOrUsername(mctx.Ctx(), 0)
+		if err != nil {
+			return err
+		}
+		e.arg.Username = res
+	}
+
 	// Look up the passed username against the list of configured users
 	if err := e.processUsername(mctx); err != nil {
 		return err
