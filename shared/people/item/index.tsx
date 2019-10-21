5f75b67df07f9f9be507a4ef6ac7ab27b94ec8b4
Adam Spooner <adam@keyba.se>
2019-10-21 15:40:45 -0400

    Remove padding on badge on follow notifications.

--- shared/people/item/index.tsx
+++ shared/people/item/index.tsx
@@ -35,7 +35,7 @@ export default (props: Props) => (
       ])}
     >
       {!!props.when && <Kb.Text type="BodyTiny">{formatTimeForPeopleItem(props.when.getTime())}</Kb.Text>}
-      {props.badged && <Kb.Badge badgeStyle={styles.badge} height={Styles.globalMargins.tiny} />}
+      {props.badged && <Kb.Badge badgeStyle={styles.badge} height={Styles.globalMargins.tiny} leftRightPadding={0} />}
     </Kb.Box>
   </Kb.Box>
 )
