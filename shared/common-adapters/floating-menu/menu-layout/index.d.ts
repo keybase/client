f79cab34103f7b9af1428ed1562bd34b9ee52710
Song Gao <song@gao.io>
2019-10-22 09:26:55 -0700

    remove unneeded import

--- shared/common-adapters/floating-menu/menu-layout/index.d.ts
+++ shared/common-adapters/floating-menu/menu-layout/index.d.ts
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import {Color, StylesCrossPlatform} from '../../../styles'
+import {Color} from '../../../styles'
 
 export type MenuItem = {
   backgroundColor?: Color
