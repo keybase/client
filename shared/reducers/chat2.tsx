50e7c0d6aac837793445a5c7867bd38059d5205a
chrisnojima <cnojima@keyba.se>
2019-10-23 14:05:42 -0400

    WIP

--- shared/reducers/chat2.tsx
+++ shared/reducers/chat2.tsx
@@ -854,7 +854,7 @@ export default (_state: Types.State = initialState, action: Actions): Types.Stat
           // add new ones, remove deleted ones, sort
           const os = new Set(messageOrdinals.get(conversationIDKey) || [])
           removedOrdinals.forEach(o => os.delete(o))
-          messageOrdinals.set(conversationIDKey, new Set([...os, ...ordinals].sort()))
+          messageOrdinals.set(conversationIDKey, new Set([...os, ...ordinals].sort((a, b) => a - b)))
         })
 
         let messageMap = oldMessageMap.withMutations(
