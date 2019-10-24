70e8f4d02b38d7f78f6ae104d63fd1944e2c23ac
Adam Spooner <adam@keyba.se>
2019-10-24 09:16:09 -0400

    Use NewInput.

--- shared/settings/feedback/index.tsx
+++ shared/settings/feedback/index.tsx
@@ -99,19 +99,18 @@ class Feedback extends React.Component<Props, State> {
           )}
           <Kb.Box2 direction="vertical" style={styles.mainBox} gap="xsmall">
             <Kb.Box2 direction="horizontal" fullWidth={true}>
-              <Kb.RoundedBox style={styles.roundedBox}>
-                <Kb.PlainInput
-                  autoCapitalize="sentences"
-                  autoCorrect={true}
-                  autoFocus={true}
-                  multiline={true}
-                  onChangeText={this._onChangeFeedback}
-                  placeholder="Please tell us what you were doing, your experience, or anything else we should know. Thanks!"
-                  rowsMin={4}
-                  rowsMax={isMobile ? 4 : 10}
-                  value={this.state.feedback}
-                />
-              </Kb.RoundedBox>
+              <Kb.NewInput
+                autoCapitalize="sentences"
+                autoCorrect={true}
+                autoFocus={true}
+                containerStyle={styles.input}
+                multiline={true}
+                onChangeText={this._onChangeFeedback}
+                placeholder="Please tell us what you were doing, your experience, or anything else we should know. Thanks!"
+                rowsMin={4}
+                rowsMax={isMobile ? 4 : 10}
+                value={this.state.feedback}
+              />
             </Kb.Box2>
             {this._sendMaxBytes() && (
               <Kb.Banner color="green">
@@ -128,7 +127,7 @@ class Feedback extends React.Component<Props, State> {
             </Kb.Box2>
             {this.props.loggedOut && (
               <Kb.Box2 direction="horizontal" fullWidth={true}>
-                <Kb.NewInput placeholder="Email (pretty please!)" onChangeText={this._onChangeEmail} />
+                <Kb.NewInput containerStyle={styles.input} placeholder="Email (pretty please!)" onChangeText={this._onChangeEmail} />
               </Kb.Box2>
             )}
             <Kb.ButtonBar>
@@ -165,9 +164,12 @@ const styles = Styles.styleSheetCreate(
       container: Styles.platformStyles({
         common: {flex: 1},
       }),
+      input: Styles.platformStyles({
+        isElectron: {padding: Styles.globalMargins.tiny},
+        isMobile: {...Styles.padding(Styles.globalMargins.tiny, Styles.globalMargins.small)},
+      }),
       mainBox: {padding: Styles.globalMargins.small},
       outerStyle: {backgroundColor: Styles.globalColors.white},
-      roundedBox: {width: '100%'},
       smallLabel: {color: Styles.globalColors.black},
     } as const)
 )
