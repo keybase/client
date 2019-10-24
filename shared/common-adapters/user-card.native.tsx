712619a253f6ec740e50afe1285e791dac0d3256
cecileboucheron <cecile@keyba.se>
2019-10-24 14:42:14 -0700

    no background

--- shared/common-adapters/user-card.native.tsx
+++ shared/common-adapters/user-card.native.tsx
@@ -57,7 +57,6 @@ const styles = Styles.styleSheetCreate(() => ({
     marginTop: 0,
   },
   avatarBackground: {
-    backgroundColor: Styles.globalColors.blueGrey,
     left: 0,
     position: 'absolute',
     right: 0,
@@ -70,7 +69,6 @@ const styles = Styles.styleSheetCreate(() => ({
   inside: {
     ...Styles.globalStyles.flexBoxColumn,
     alignItems: 'stretch',
-    backgroundColor: Styles.globalColors.blueGrey,
     justifyContent: 'flex-start',
     padding: 16,
     width: '100%',
