2decfa9c1018ed3f7bc36eaa2f66c3e4f4aebebb
Heronhaye <surya@keyba.se>
2019-10-25 09:57:16 -0400

    --wip-- [skip ci]

--- packaging/linux/smoketest/vagrantcommon/generate-vagrantfiles.py
+++ packaging/linux/smoketest/vagrantcommon/generate-vagrantfiles.py
@@ -6,7 +6,6 @@ template = """Vagrant.configure("2") do |config|
   config.vm.box = "{}"
   config.vm.synced_folder "../vagrantcommon/", "/vagrant", type: "rsync"
   config.vm.provider "virtualbox" do |vb|
-    vb.gui = true
     vb.memory = "4096"
   end
   config.vm.provision "shell", path: "../vagrantcommon/provision_{}.bash"
