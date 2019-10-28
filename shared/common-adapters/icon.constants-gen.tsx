c26d22865c6e33a9a1b69d1cef16221c90ee218c
Jacob Young <jacob@jry.io>
2019-10-28 15:02:58 -0400

    PICNIC-528 - Hello Bot Banner (#20641)
    * Banner story
    
    * Hello bot banner
    
    * Change to hellobot-hi

--- shared/common-adapters/icon.constants-gen.tsx
+++ shared/common-adapters/icon.constants-gen.tsx
@@ -43,15 +43,23 @@ const iconMeta_ = {
     extension: 'png',
     isFont: false,
     nameDark: 'icon-dark-addon-file-uploading-error',
-    require: require('../images/icons/icon-addon-file-uploading-error.png'),
-    requireDark: require('../images/icons/icon-dark-addon-file-uploading-error.png'),
+    get require() {
+      return require('../images/icons/icon-addon-file-uploading-error.png')
+    },
+    get requireDark() {
+      return require('../images/icons/icon-dark-addon-file-uploading-error.png')
+    },
   },
   'icon-addon-file-uploading-offline': {
     extension: 'png',
     isFont: false,
     nameDark: 'icon-dark-addon-file-uploading-offline',
-    require: require('../images/icons/icon-addon-file-uploading-offline.png'),
-    requireDark: require('../images/icons/icon-dark-addon-file-uploading-offline.png'),
+    get require() {
+      return require('../images/icons/icon-addon-file-uploading-offline.png')
+    },
+    get requireDark() {
+      return require('../images/icons/icon-dark-addon-file-uploading-offline.png')
+    },
   },
   'icon-addon-lock-16': {
     extension: 'png',
@@ -1081,12 +1089,16 @@ const iconMeta_ = {
   'icon-dark-addon-file-uploading-error': {
     extension: 'png',
     isFont: false,
-    require: require('../images/icons/icon-dark-addon-file-uploading-error.png'),
+    get require() {
+      return require('../images/icons/icon-dark-addon-file-uploading-error.png')
+    },
   },
   'icon-dark-addon-file-uploading-offline': {
     extension: 'png',
     isFont: false,
-    require: require('../images/icons/icon-dark-addon-file-uploading-offline.png'),
+    get require() {
+      return require('../images/icons/icon-dark-addon-file-uploading-offline.png')
+    },
   },
   'icon-dark-addon-lock-16': {
     extension: 'png',
@@ -3296,6 +3308,13 @@ const iconMeta_ = {
     isFont: false,
     get require() {
       return require('../images/icons/icon-fancy-folder-file-inexistant-mobile-188-120.png')
+    },
+  },
+  'icon-fancy-hellobot-hi-96': {
+    extension: 'png',
+    isFont: false,
+    get require() {
+      return require('../images/icons/icon-fancy-hellobot-hi-96.png')
     },
   },
   'icon-fancy-no-access-desktop-96-94': {
