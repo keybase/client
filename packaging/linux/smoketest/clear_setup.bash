9f964d09c1cc8a85d7391437d9afce050d05dfc4
heronhaye <surya@keyba.se>
2019-10-29 10:03:48 -0400

    vagrant linux smoketests (#20566)

---
+++ packaging/linux/smoketest/clear_setup.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+set -euox pipefail
+echo "Starting clearing linux smoketests"
+
+for platform in */; do
+    case $platform in
+        vagrantcommon/) continue;;
+    esac
+    (
+        cd "$platform"
+        vagrant destroy -f
+    )
+done
+
+echo "Finished clearing linux smoketests successfully"
