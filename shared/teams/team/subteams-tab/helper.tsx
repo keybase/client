74c0c48b0fd4f6eb581fadaa52884ff82117277b
Daniel Ayoub <danny.a95@gmail.com>
2019-10-30 22:53:37 -0400

    remove old row helpers

--- shared/teams/team/subteams-tab/helper.tsx
+++ shared/teams/team/subteams-tab/helper.tsx
@@ -1,37 +1,8 @@
 import * as React from 'react'
-import * as I from 'immutable'
-import * as Constants from '../../../constants/teams'
-import * as Types from '../../../constants/types/teams'
-import {TypedState} from '../../../util/container'
 import Add from './add-row/container'
 import Intro from './intro-row/container'
 import None from './none-row'
 import Team from './team-row/container'
-
-export type OwnProps = {
-  teamname: string
-}
-
-type StateProps = {
-  _sawSubteamsBanner: boolean
-  _subteams: I.Set<Types.Teamname>
-  _yourOperations: Types.TeamOperations
-}
-
-/* Helpers to build the teams tabs. mapStateHelper is called by the master mapStateToProps, getRows makes the rows to be injected below the header, renderItem renders the individual row */
-export const mapStateHelper = (state: TypedState, {teamname}: OwnProps): StateProps => ({
-  _sawSubteamsBanner: state.teams.sawSubteamsBanner,
-  _subteams: Constants.getTeamSubteams(state, teamname),
-  _yourOperations: Constants.getCanPerform(state, teamname),
-})
-
-export const getRows = ({_subteams, _sawSubteamsBanner, _yourOperations}: StateProps) => {
-  const bannerRow = _sawSubteamsBanner ? [] : [{type: 'subteam-intro'}]
-  const addMembersRow = _yourOperations.manageSubteams ? [{type: 'subteam-add'}] : []
-  const subTeamsRows = _subteams.sort().map(subteam => ({teamname: subteam, type: 'subteam-subteam'}))
-  const noSubteamsRow = _subteams.isEmpty() ? [{type: 'subteam-none'}] : []
-  return [...bannerRow, ...addMembersRow, ...subTeamsRows, ...noSubteamsRow]
-}
 
 export const renderItem = (
   teamname: string,
