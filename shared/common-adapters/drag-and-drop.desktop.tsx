cca7972799da4df42132895c6e12788162ccba08
Song Gao <song@gao.io>
2019-11-01 10:07:21 -0700

    drag and drop improvements and fs browser layout cleanup (#20703)
    * drag and drop improvements and fs browser layout cleanup
    
    * storyshots

--- shared/common-adapters/drag-and-drop.desktop.tsx
+++ shared/common-adapters/drag-and-drop.desktop.tsx
@@ -7,28 +7,6 @@ import Text from './text'
 import {Props} from './drag-and-drop'
 import logger from '../logger'
 
-type OverlayProps = {
-  onDragLeave: (e: React.DragEvent) => void
-  onDrop: (e: React.DragEvent) => void
-}
-
-const DropOverlay = (props: OverlayProps) => (
-  <Box2
-    centerChildren={true}
-    direction="horizontal"
-    onDragLeave={props.onDragLeave}
-    onDrop={props.onDrop}
-    style={styles.dropOverlay}
-  >
-    <Box2 direction="vertical" centerChildren={true} gap="small">
-      <Box2 direction="horizontal" style={styles.iconContainer} centerChildren={true}>
-        <Icon type="iconfont-arrow-full-up" color={Styles.globalColors.white} style={styles.icon} />
-      </Box2>
-      <Text type="Header">Drop files to upload</Text>
-    </Box2>
-  </Box2>
-)
-
 type State = {
   showDropOverlay: boolean
 }
@@ -82,17 +60,40 @@ class DragAndDrop extends React.PureComponent<Props, State> {
     this.setState({showDropOverlay: false})
   }
 
+  _dropOverlay = () => (
+    <Box2
+      centerChildren={true}
+      direction="horizontal"
+      onDragLeave={this._onDragLeave}
+      onDrop={this._onDrop}
+      style={styles.dropOverlay}
+    >
+      <Box2 direction="vertical" centerChildren={true} gap="medium">
+        {this.props.rejectReason ? (
+          <Icon type="iconfont-remove" color={Styles.globalColors.red} sizeType="Huge" />
+        ) : (
+          <Icon type="iconfont-upload" color={Styles.globalColors.blue} sizeType="Huge" />
+        )}
+        {this.props.rejectReason ? (
+          <Text type="Header">{this.props.rejectReason}</Text>
+        ) : (
+          <Text type="Header">Drop files to upload</Text>
+        )}
+      </Box2>
+    </Box2>
+  )
+
   render() {
     return (
       <Box2
         direction="vertical"
-        fullHeight={true}
-        fullWidth={true}
+        fullHeight={this.props.fullHeight}
+        fullWidth={this.props.fullWidth}
         onDragOver={this._onDragOver}
-        style={styles.containerStyle}
+        style={Styles.collapseStyles([styles.containerStyle, this.props.containerStyle])}
       >
         {this.props.children}
-        {this.state.showDropOverlay && <DropOverlay onDragLeave={this._onDragLeave} onDrop={this._onDrop} />}
+        {this.state.showDropOverlay && this._dropOverlay()}
       </Box2>
     )
   }
@@ -108,6 +109,7 @@ const styles = Styles.styleSheetCreate(
         isElectron: {
           ...Styles.globalStyles.fillAbsolute,
           backgroundImage: `linear-gradient(${Styles.globalColors.white_75}, ${Styles.globalColors.white})`,
+          padding: Styles.globalMargins.large,
         },
       }),
       icon: {
--- shared/fs/browser/container.tsx
+++ shared/fs/browser/container.tsx
@@ -1,31 +1,24 @@
-import {namedConnect} from '../../util/container'
+import * as Container from '../../util/container'
 import Browser from '.'
 import * as Types from '../../constants/types/fs'
 import * as Constants from '../../constants/fs'
-import * as FsGen from '../../actions/fs-gen'
 
 type OwnProps = {
   path: Types.Path
 }
 
-const mapStateToProps = (state, {path}: OwnProps) => ({
-  _kbfsDaemonStatus: state.fs.kbfsDaemonStatus,
-  _pathItem: state.fs.pathItems.get(path, Constants.unknownPathItem),
-  _tlf: Constants.getTlfFromPath(state.fs.tlfs, path),
-  resetBannerType: Constants.resetBannerType(state, path),
-})
-
-const mapDispatchToProps = (dispatch, {path}: OwnProps) => ({
-  onAttach: (paths: Array<string>) => {
-    paths.forEach(localPath => dispatch(FsGen.createUpload({localPath, parentPath: path})))
-  },
-})
-
-const mergeProps = (stateProps, dispatchProps, {path}: OwnProps) => ({
-  offline: Constants.isOfflineUnsynced(stateProps._kbfsDaemonStatus, stateProps._pathItem, path),
-  onAttach: stateProps._pathItem.writable ? dispatchProps.onAttach : null,
-  path,
-  resetBannerType: stateProps.resetBannerType,
-})
-
-export default namedConnect(mapStateToProps, mapDispatchToProps, mergeProps, 'Browser')(Browser)
+export default Container.connect(
+  (state, {path}: OwnProps) => ({
+    _kbfsDaemonStatus: state.fs.kbfsDaemonStatus,
+    _pathItem: state.fs.pathItems.get(path, Constants.unknownPathItem),
+    _tlf: Constants.getTlfFromPath(state.fs.tlfs, path),
+    resetBannerType: Constants.resetBannerType(state, path),
+  }),
+  () => ({}),
+  (stateProps, _, {path}: OwnProps) => ({
+    offlineUnsynced: Constants.isOfflineUnsynced(stateProps._kbfsDaemonStatus, stateProps._pathItem, path),
+    path,
+    resetBannerType: stateProps.resetBannerType,
+    writable: stateProps._pathItem.writable,
+  })
+)(Browser)
