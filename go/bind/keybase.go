93f8b7e8277d5f5cf0e788f7b9b9acd8b6aa20bb
Mike Maxim <mike.maxim@gmail.com>
2019-11-06 10:47:49 -0500

    make sure we have been inited before reset

--- go/bind/keybase.go
+++ go/bind/keybase.go
@@ -367,6 +367,9 @@ func Reset() error {
 	if conn != nil {
 		conn.Close()
 	}
+	if !isInited() {
+		return errors.New("reset called before Init")
+	}
 
 	var err error
 	conn, err = kbCtx.LoopbackListener.Dial()
