53e9074d7c4628b9c3f1c45ba521729495b70cbf
Adam Spooner <adam@keyba.se>
2019-11-06 10:10:59 -0500

    HOC

--- shared/chat/inbox/row/build-team/container.tsx
+++ shared/chat/inbox/row/build-team/container.tsx
@@ -5,26 +5,28 @@ import BuildTeam from '.'
 
 type OwnProps = Container.PropsWithSafeNavigation<{}>
 
-export default Container.namedConnect(
-  state => ({
-    showBuildATeam: ((state.chat2.inboxLayout && state.chat2.inboxLayout.bigTeams) || []).length === 0,
-  }),
-  (dispatch, {safeNavigateAppendPayload}: OwnProps) => ({
-    // Route to the teams tab and open the NewTeamDialog component
-    _onCreateTeam: () => {
-      dispatch(RouteTreeGen.createSwitchTab({tab: teamsTab}))
-      dispatch(safeNavigateAppendPayload({path: ['teamNewTeamDialog']}))
-    },
-    // Route to the teams tab and open the JoinTeamDialog component
-    _onJoinTeam: () => {
-      dispatch(RouteTreeGen.createSwitchTab({tab: teamsTab}))
-      dispatch(safeNavigateAppendPayload({path: ['teamJoinTeamDialog']}))
-    },
-  }),
-  (stateProps, dispatchProps, _: OwnProps) => ({
-    onCreateTeam: dispatchProps._onCreateTeam,
-    onJoinTeam: dispatchProps._onJoinTeam,
-    showBuildATeam: stateProps.showBuildATeam,
-  }),
-  'BuildTeam'
-)(BuildTeam)
+export default Container.withSafeNavigation(
+  Container.namedConnect(
+    state => ({
+      showBuildATeam: ((state.chat2.inboxLayout && state.chat2.inboxLayout.bigTeams) || []).length === 0,
+    }),
+    (dispatch, {safeNavigateAppendPayload}: OwnProps) => ({
+      // Route to the teams tab and open the NewTeamDialog component
+      _onCreateTeam: () => {
+        dispatch(RouteTreeGen.createSwitchTab({tab: teamsTab}))
+        dispatch(safeNavigateAppendPayload({path: ['teamNewTeamDialog']}))
+      },
+      // Route to the teams tab and open the JoinTeamDialog component
+      _onJoinTeam: () => {
+        dispatch(RouteTreeGen.createSwitchTab({tab: teamsTab}))
+        dispatch(safeNavigateAppendPayload({path: ['teamJoinTeamDialog']}))
+      },
+    }),
+    (stateProps, dispatchProps, _: OwnProps) => ({
+      onCreateTeam: dispatchProps._onCreateTeam,
+      onJoinTeam: dispatchProps._onJoinTeam,
+      showBuildATeam: stateProps.showBuildATeam,
+    }),
+    'BuildTeam'
+  )(BuildTeam)
+)
