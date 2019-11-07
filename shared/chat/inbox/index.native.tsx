913a7b04b865abc74cc447137143a0bc3ca9bd67
Adam Spooner <adam@keyba.se>
2019-11-07 10:56:58 -0500

    And neverLoaded.

--- shared/chat/inbox/index.native.tsx
+++ shared/chat/inbox/index.native.tsx
@@ -265,9 +265,9 @@ class Inbox extends React.PureComponent<T.Props, State> {
           )}
           {noChats}
           {floatingDivider ||
-            ((this.props.rows.length === 0 || !this.props.hasBigTeams) && !this.props.isLoading && (
-              <BuildTeam />
-            ))}
+            ((this.props.rows.length === 0 || !this.props.hasBigTeams) &&
+              !this.props.isLoading &&
+              !this.props.neverLoaded && <BuildTeam />)}
           {this.state.showUnread && !this.props.isSearching && !this.state.showFloating && (
             <UnreadShortcut onClick={this._scrollToUnread} />
           )}
