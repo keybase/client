5fc19e07ee6e1114fe111ae39243199d3f4fba00
Miles Steele <miles@milessteele.com>
2019-11-07 17:02:34 -0500

    TEMP

--- shared/people/follow-suggestions/savage.tsx
+++ shared/people/follow-suggestions/savage.tsx
@@ -1,37 +1,55 @@
 import * as React from 'react'
 import * as Types from '../../constants/types/people'
-import {Box, ConnectedNameWithIcon, ScrollView, Text} from '../../common-adapters'
+import {Box2, ConnectedNameWithIcon, ScrollView, Text} from '../../common-adapters'
 import * as Styles from '../../styles'
 
-// import Svg, {
-//   Circle,
-//   Ellipse,
-//   G,
-//   TSpan,
-//   TextPath,
-//   Path,
-//   Polygon,
-//   Polyline,
-//   Line,
-//   Rect,
-//   Use,
-//   Image,
-//   Symbol,
-//   Defs,
-//   LinearGradient,
-//   RadialGradient,
-//   Stop,
-//   ClipPath,
-//   Pattern,
-//   Mask,
-// } from 'react-native-svg';
+import Svg, {
+  Circle,
+  Ellipse,
+  G,
+  TSpan,
+  TextPath,
+  Path,
+  Polygon,
+  Polyline,
+  Line,
+  Rect,
+  Use,
+  Image,
+  Symbol,
+  Defs,
+  LinearGradient,
+  RadialGradient,
+  Stop,
+  ClipPath,
+  Pattern,
+  Mask,
+} from 'react-native-svg';
 
 // import { View, StyleSheet } from 'react-native';
 
 export const Savage = (props) => (
-    <Text type="BodySmallSemibold">
-      Consider borrowing...
-    </Text>
+  <Box2 direction="vertical">
+    <Svg height="50%" width="50%" viewBox="0 0 100 100">
+      <Circle
+        cx="50"
+        cy="50"
+        r="45"
+        stroke="blue"
+        strokeWidth="2.5"
+        fill="green"
+      />
+      <Rect
+        x="15"
+        y="15"
+        width="70"
+        height="70"
+        stroke="red"
+        strokeWidth="2"
+        fill="yellow"
+      />
+    </Svg>
+  </Box2>
 )
 
 export default Savage
