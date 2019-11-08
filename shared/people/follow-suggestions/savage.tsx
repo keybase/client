4eb04ddfc9463a255c83cb81247cd3eb2de7d284
Miles Steele <miles@milessteele.com>
2019-11-08 11:19:16 -0500

    animation

--- shared/people/follow-suggestions/savage.tsx
+++ shared/people/follow-suggestions/savage.tsx
@@ -1,37 +1,49 @@
 import * as React from 'react'
 import * as Types from '../../constants/types/people'
 import {Box2, ConnectedNameWithIcon, ScrollView, Text} from '../../common-adapters'
+import * as Kb from '../../common-adapters'
 import * as Styles from '../../styles'
 
-import Svg, {
-  Circle,
-  Ellipse,
-  G,
-  TSpan,
-  TextPath,
-  Path,
-  Polygon,
-  Polyline,
-  Line,
-  Rect,
-  Use,
-  Image,
-  Symbol,
-  Defs,
-  LinearGradient,
-  RadialGradient,
-  Stop,
-  ClipPath,
-  Pattern,
-  Mask,
-} from 'react-native-svg';
+// import Svg, {
+//   Circle,
+//   Ellipse,
+//   G,
+//   TSpan,
+//   TextPath,
+//   Path,
+//   Polygon,
+//   Polyline,
+//   Line,
+//   Rect,
+//   Use,
+//   Image,
+//   Symbol,
+//   Defs,
+//   LinearGradient,
+//   RadialGradient,
+//   Stop,
+//   ClipPath,
+//   Pattern,
+//   Mask,
+// } from 'react-native-svg';
+
+import * as Svg from 'react-native-svg';
 
 // import { View, StyleSheet } from 'react-native';
 
 export const Savage = (props) => (
+  <Kb.Box2 direction="vertical">
+    <Kb.Text type="BodySemibold">Unmoving:</Kb.Text>
+    <Unmoving/>
+    <Kb.Text type="BodySemibold">Moving:</Kb.Text>
+    <Moving/>
+  </Kb.Box2>
+)
+
+const Unmoving = (props) => (
   <Box2 direction="vertical" style={styles.container}>
-    <Svg height="50%" width="50%" viewBox="0 0 100 100">
-      <Circle
+    <Svg.Svg height="50%" width="50%" viewBox="0 0 100 100">
+      <Svg.Circle
         cx="50"
         cy="50"
         r="45"
@@ -39,7 +51,7 @@ export const Savage = (props) => (
         strokeWidth="2.5"
         fill="green"
       />
-      <Rect
+      <Svg.Rect
         x="15"
         y="15"
         width="70"
@@ -48,9 +60,38 @@ export const Savage = (props) => (
         strokeWidth="2"
         fill="yellow"
       />
-    </Svg>
+    </Svg.Svg>
   </Box2>
 )
+
+const Moving = (props) => {
+  const [angleTarget, setAngleTarget] = React.useState(0)
+  Kb.useInterval(() => setAngleTarget(angleTarget === 0 ? 60 : 0), 500)
+  return (<Box2 direction="vertical" style={styles.container}>
+    <Kb.Animated to={{angle: angleTarget}}>
+      {({ angle }) =>
+        <Svg.Svg height="50%" width="50%" viewBox="0 0 100 100">
+          <Svg.Circle
+            cx="50"
+            cy="50"
+            r="45"
+            stroke="green"
+            strokeWidth="5"
+            fill="blue"
+          />
+          <Svg.Rect
+            x="15"
+            y="15"
+            width="70"
+            height="70"
+            fill="yellow"
+            rotation={angle}
+          />
+        </Svg.Svg>
+      }
+    </Kb.Animated>
+  </Box2>)
+}
 
 export default Savage
 
