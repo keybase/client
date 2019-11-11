4530858d2cbf2adf9f863698b3a8f6481fc8fb3f
Mike Maxim <mike@keyba.se>
2019-11-11 15:28:02 -0500

    make sure amp is a real number

--- shared/chat/audio/audio-player.tsx
+++ shared/chat/audio/audio-player.tsx
@@ -15,7 +15,8 @@ type VisProps = {
 const AudioVis = (props: VisProps) => {
   let maxHeight = 0
   const content = props.amps.map((amp, index) => {
-    const height = Math.min(1.0, Math.max(Math.sqrt(amp), 0.05)) * props.height
+    const prop = Math.min(1.0, Math.max(Math.sqrt(amp), 0.05))
+    const height = isNaN(prop) ? 0 : prop * props.height
     if (height >= maxHeight) {
       maxHeight = height
     }
