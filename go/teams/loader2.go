5a22a954460296b48e8a4fd18f12eb92245c28ab
Miles Steele <miles@milessteele.com>
2019-11-12 15:55:12 -0500

    add logging

--- go/teams/loader2.go
+++ go/teams/loader2.go
@@ -185,9 +185,11 @@ var whitelistedTeamLinkSigs = []keybase1.SigID{
 }
 
 func (l *TeamLoader) addProofsForKeyInUserSigchain(ctx context.Context, teamID keybase1.TeamID, link *ChainLinkUnpacked, uid keybase1.UID, key *keybase1.PublicKeyV2NaCl, userLinkMap linkMapT, proofSet *proofSetT) {
+	l.G().Log.CDebugf(ctx, "addProofsForKeyInUserSigchain: sigID:%v", link.SigID())
 	for _, okSigID := range whitelistedTeamLinkSigs {
 		if link.SigID().Equal(okSigID) {
 			// This proof is whitelisted, so don't check it.
+			l.G().Log.CDebugf(ctx, "addProofsForKeyInUserSigchain: skipping exceptional link: %v", link.SigID())
 			return
 		}
 	}
