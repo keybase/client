e6f0128294d152cf84543c99590d123084cd4c5c
cori hudson <54032873+hyperobject@users.noreply.github.com>
2019-11-12 09:51:17 -0500

    add you and hellobot to recommended users (#20900)
    * feat: add you and hellobot source to interestingpeople
    
    * feat: add first pass at help text for few results
    
    * add search hint text

--- shared/team-building/container.tsx
+++ shared/team-building/container.tsx
@@ -391,6 +391,7 @@ export const sortAndSplitRecommendations = memoize(
             },
           ]
         : []),
+
       {
         data: [],
         label: 'Recommendations',
@@ -434,6 +435,13 @@ export const sortAndSplitRecommendations = memoize(
         }
       }
     })
+    if (results.length < 5) {
+      sections.push({
+        data: [{isSearchHint: true as const}],
+        label: '',
+        shortcut: false,
+      })
+    }
     return sections.filter(s => s && s.data && s.data.length > 0)
   }
 )
@@ -447,7 +455,7 @@ export const sortAndSplitRecommendations = memoize(
 const flattenRecommendations = memoize((recommendations: Array<SearchRecSection>) => {
   const result: Array<SearchResult | null> = []
   for (const section of recommendations) {
-    result.push(...section.data.map(rec => ('isImportButton' in rec ? null : rec)))
+    result.push(...section.data.map(rec => ('isImportButton' in rec || 'isSearchHint' in rec ? null : rec)))
   }
   return result
 })
