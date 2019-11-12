c7ab1af51550312a97c084745a3d99f40dff002c
Miles Steele <miles@milessteele.com>
2019-11-12 17:02:20 -0500

    sigid compare ignore last byte

--- go/teams/loader2.go
+++ go/teams/loader2.go
@@ -187,7 +187,7 @@ var whitelistedTeamLinkSigs = []keybase1.SigID{
 func (l *TeamLoader) addProofsForKeyInUserSigchain(ctx context.Context, teamID keybase1.TeamID, link *ChainLinkUnpacked, uid keybase1.UID, key *keybase1.PublicKeyV2NaCl, userLinkMap linkMapT, proofSet *proofSetT) {
 	l.G().Log.CDebugf(ctx, "addProofsForKeyInUserSigchain: sigID:%v", link.SigID())
 	for _, okSigID := range whitelistedTeamLinkSigs {
-		if link.SigID().Equal(okSigID) {
+		if link.SigID().EqualIgnoreLastByte(okSigID) {
 			// This proof is whitelisted, so don't check it.
 			l.G().Log.CDebugf(ctx, "addProofsForKeyInUserSigchain: skipping exceptional link: %v", link.SigID())
 			return
