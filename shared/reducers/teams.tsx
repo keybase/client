9a21adaeeb200e54998d25518bbc8e7a32efd155
Danny Ayoub <danny.a95@gmail.com>
2019-11-20 12:52:57 -0500

    Teams cleanup new team dialog (#20975)
    * WIP new team dialog fixup
    
    * container + tweaks for mobile
    
    * chat container
    
    * cleanup
    
    * storyshots

--- shared/reducers/teams.tsx
+++ shared/reducers/teams.tsx
@@ -24,6 +24,7 @@ export default (
         draftState.channelCreationError = action.payload.error
         return
       case TeamsGen.createNewTeam:
+      case TeamsGen.createNewTeamFromConversation:
         draftState.teamCreationError = ''
         return
       case TeamsGen.setTeamCreationError:
@@ -275,7 +276,6 @@ export default (
       case TeamsGen.checkRequestedAccess:
       case TeamsGen.clearNavBadges:
       case TeamsGen.createChannel:
-      case TeamsGen.createNewTeamFromConversation:
       case TeamsGen.deleteChannelConfirmed:
       case TeamsGen.deleteTeam:
       case TeamsGen.editMembership:
