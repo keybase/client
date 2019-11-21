6fb01e0865036a90da9452f2f66b988f7cd1833b
Daniel Ayoub <danny.a95@gmail.com>
2019-11-21 10:02:43 -0500

    container take teamID

--- shared/teams/really-leave-team/container.tsx
+++ shared/teams/really-leave-team/container.tsx
@@ -2,12 +2,13 @@ import * as React from 'react'
 import * as TeamsGen from '../../actions/teams-gen'
 import * as RouteTreeGen from '../../actions/route-tree-gen'
 import * as Container from '../../util/container'
+import * as Constants from '../../constants/teams'
+import * as Types from '../../constants/types/teams'
 import ReallyLeaveTeam, {Props} from '.'
 import LastOwnerDialog from './last-owner'
-import * as Constants from '../../constants/teams'
 import {anyWaiting} from '../../constants/waiting'
 
-type OwnProps = Container.RouteProps<{teamname: string}>
+type OwnProps = Container.RouteProps<{teamID: Types.TeamID}>
 
 const RenderLastOwner = (p: Props & {_leaving: boolean; lastOwner: boolean}) => {
   const {lastOwner, _leaving, ...rest} = p
@@ -16,30 +17,31 @@ const RenderLastOwner = (p: Props & {_leaving: boolean; lastOwner: boolean}) => 
 
 export default Container.connect(
   (state, ownProps: OwnProps) => {
-    const name = Container.getRouteProps(ownProps, 'teamname', '')
-    const lastOwner = Constants.isLastOwner(state, name)
+    const teamID = Container.getRouteProps(ownProps, 'teamID', Types.noTeamID)
+    const {teamname} = Constants.getTeamDetails(state, teamID)
+    const lastOwner = Constants.isLastOwner(state, teamname)
     return {
       _leaving: anyWaiting(state, Constants.leaveTeamWaitingKey(name)),
       lastOwner,
-      name,
+      name: teamname,
     }
   },
-  (dispatch, ownProps: OwnProps) => ({
-    onBack: () => dispatch(RouteTreeGen.createNavigateUp()),
-    onLeave: () => {
+  dispatch => ({
+    _onLeave: (teamname: string) => {
       dispatch(
         TeamsGen.createLeaveTeam({
           context: 'teams',
-          teamname: Container.getRouteProps(ownProps, 'teamname', ''),
+          teamname,
         })
       )
     },
+    onBack: () => dispatch(RouteTreeGen.createNavigateUp()),
   }),
   (stateProps, dispatchProps, _: OwnProps) => ({
     _leaving: stateProps._leaving,
     lastOwner: stateProps.lastOwner,
     name: stateProps.name,
     onBack: stateProps._leaving ? () => {} : dispatchProps.onBack,
-    onLeave: dispatchProps.onLeave,
+    onLeave: () => dispatchProps._onLeave(stateProps.name),
   })
 )(Container.safeSubmit(['onLeave'], ['_leaving'])(RenderLastOwner))
