e149d213cc90e5e08584dbde3a9f76dfac57eaa1
Maxwell Krohn <themax@gmail.com>
2019-11-25 10:59:04 -0500

    nuke unneeded action

--- shared/actions/tracker2-gen.tsx
+++ shared/actions/tracker2-gen.tsx
@@ -18,7 +18,6 @@ export const updateAssertion = 'tracker2:updateAssertion'
 export const updateFollowers = 'tracker2:updateFollowers'
 export const updateResult = 'tracker2:updateResult'
 export const updatedDetails = 'tracker2:updatedDetails'
-export const userBlocked = 'tracker2:userBlocked'
 
 // Payload Types
 type _ChangeFollowPayload = {readonly guiID: string; readonly follow: boolean}
@@ -76,7 +75,6 @@ type _UpdatedDetailsPayload = {
   readonly teamShowcase: Array<Types.TeamShowcase>
   readonly blocked: boolean
 }
-type _UserBlockedPayload = {readonly blocker: string; readonly blocked: Array<string>}
 
 // Action Creators
 /**
@@ -122,10 +120,6 @@ export const createUpdatedDetails = (payload: _UpdatedDetailsPayload): UpdatedDe
   payload,
   type: updatedDetails,
 })
-export const createUserBlocked = (payload: _UserBlockedPayload): UserBlockedPayload => ({
-  payload,
-  type: userBlocked,
-})
 
 // Action Payloads
 export type ChangeFollowPayload = {readonly payload: _ChangeFollowPayload; readonly type: typeof changeFollow}
@@ -162,7 +156,6 @@ export type UpdatedDetailsPayload = {
   readonly payload: _UpdatedDetailsPayload
   readonly type: typeof updatedDetails
 }
-export type UserBlockedPayload = {readonly payload: _UserBlockedPayload; readonly type: typeof userBlocked}
 
 // All Actions
 // prettier-ignore
@@ -180,5 +173,4 @@ export type Actions =
   | UpdateFollowersPayload
   | UpdateResultPayload
   | UpdatedDetailsPayload
-  | UserBlockedPayload
   | {type: 'common:resetStore', payload: {}}
