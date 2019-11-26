ce80f1b7ea9c9159649f204dabb2d9110f8bfe8e
Jeremy Stribling <strib@alum.mit.edu>
2019-11-25 16:35:51 -0800

    search: leveldb reader and iterator for bleve
    Issue: HOTPOT-1262

--- go/kbfs/search/bleve_leveldb_store.go
+++ go/kbfs/search/bleve_leveldb_store.go
@@ -8,8 +8,105 @@ import (
 	"github.com/blevesearch/bleve/index/store"
 	"github.com/keybase/client/go/kbfs/libfs"
 	"github.com/syndtr/goleveldb/leveldb"
+	"github.com/syndtr/goleveldb/leveldb/iterator"
+	"github.com/syndtr/goleveldb/leveldb/util"
 	billy "gopkg.in/src-d/go-billy.v4"
 )
+
+type bleveLevelDBIterator struct {
+	iter iterator.Iterator
+}
+
+var _ store.KVIterator = (*bleveLevelDBIterator)(nil)
+
+// Seek implements the store.KVIterator interface for bleveLevelDBIterator.
+func (bldbi *bleveLevelDBIterator) Seek(key []byte) {
+	_ = bldbi.iter.Seek(key)
+}
+
+// Next implements the store.KVIterator interface for bleveLevelDBIterator.
+func (bldbi *bleveLevelDBIterator) Next() {
+	_ = bldbi.iter.Next()
+}
+
+// Key implements the store.KVIterator interface for bleveLevelDBIterator.
+func (bldbi *bleveLevelDBIterator) Key() []byte {
+	return bldbi.iter.Key()
+}
+
+// Value implements the store.KVIterator interface for bleveLevelDBIterator.
+func (bldbi *bleveLevelDBIterator) Value() []byte {
+	return bldbi.iter.Value()
+}
+
+// Valid implements the store.KVIterator interface for bleveLevelDBIterator.
+func (bldbi *bleveLevelDBIterator) Valid() bool {
+	return bldbi.iter.Valid()
+}
+
+// Current implements the store.KVIterator interface for bleveLevelDBIterator.
+func (bldbi *bleveLevelDBIterator) Current() ([]byte, []byte, bool) {
+	return bldbi.Key(), bldbi.Value(), bldbi.Valid()
+}
+
+// Close implements the store.KVIterator interface for bleveLevelDBIterator.
+func (bldbi *bleveLevelDBIterator) Close() error {
+	bldbi.iter.Release()
+	return nil
+}
+
+type bleveLevelDBReader struct {
+	snap *leveldb.Snapshot
+}
+
+var _ store.KVReader = (*bleveLevelDBReader)(nil)
+
+// Get implements the store.KVReader interface for bleveLevelDBReader.
+func (bldbr *bleveLevelDBReader) Get(key []byte) ([]byte, error) {
+	return bldbr.snap.Get(key, nil)
+}
+
+// MultiGet implements the store.KVReader interface for bleveLevelDBReader.
+func (bldbr *bleveLevelDBReader) MultiGet(keys [][]byte) (
+	values [][]byte, err error) {
+	// leveldb doesn't have a simple multi-get interface, so just do
+	// multiple fetches.  Snapshot is consistent so this is fine (if
+	// inefficient).
+	values = make([][]byte, len(keys))
+	for i, k := range keys {
+		v, err := bldbr.Get(k)
+		if err != nil {
+			return nil, err
+		}
+		values[i] = v
+	}
+	return values, nil
+}
+
+// PrefixIterator implements the store.KVReader interface for bleveLevelDBReader.
+func (bldbr *bleveLevelDBReader) PrefixIterator(prefix []byte) store.KVIterator {
+	r := util.BytesPrefix(prefix)
+	return &bleveLevelDBIterator{
+		iter: bldbr.snap.NewIterator(r, nil),
+	}
+}
+
+// RangeIterator implements the store.KVReader interface for bleveLevelDBReader.
+func (bldbr *bleveLevelDBReader) RangeIterator(
+	start, end []byte) store.KVIterator {
+	return &bleveLevelDBIterator{
+		iter: bldbr.snap.NewIterator(&util.Range{
+			Start: start,
+			Limit: end,
+		}, nil),
+	}
+}
+
+// Close implements the store.KVReader interface for bleveLevelDBReader.
+func (bldbr *bleveLevelDBReader) Close() error {
+	bldbr.snap.Release()
+	return nil
+}
 
 type bleveLevelDBStore struct {
 	db *leveldb.DB
@@ -37,7 +134,11 @@ func (bldbs *bleveLevelDBStore) Writer() (store.KVWriter, error) {
 
 // Writer implements the store.KVStore interface for bleveLevelDBStore.
 func (bldbs *bleveLevelDBStore) Reader() (store.KVReader, error) {
-	return nil, nil
+	snap, err := bldbs.db.GetSnapshot()
+	if err != nil {
+		return nil, err
+	}
+	return &bleveLevelDBReader{snap: snap}, nil
 }
 
 // close implements the store.KVStore interface for bleveLevelDBStore.
