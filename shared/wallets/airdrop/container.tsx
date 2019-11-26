f0b8da2618d850b8886182ed2779d5ef5ece75cd
chrisnojima <cnojima@keyba.se>
2019-11-26 08:49:39 -0500

    immer wallets 2 (#21105)
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * immer wallets 3 (#21107)
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * immer wallet 4 (#21136)
    
    * WIP
    
    * WIP
    
    * WIP
    
    * immer wallets 5 (#21142)
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * immer wallets 6 (#21169)
    
    * WIP
    
    * WIP
    
    * immer wallet 7 (#21175)
    
    * WIP
    
    * WIP
    
    * immer wallet 8 (#21191)
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * immer wallets 9 (#21193)
    
    * WIP
    
    * WIP
    
    * WIP
    
    * immer wallets 10 (final) (#21199)
    
    * WIP
    
    * remove dupe checking and old reducer (#21217)

--- shared/wallets/airdrop/container.tsx
+++ shared/wallets/airdrop/container.tsx
@@ -5,47 +5,42 @@ import * as RouteTreeGen from '../../actions/route-tree-gen'
 import * as Container from '../../util/container'
 import Onboarding from '../onboarding/container'
 
-const mapStateToProps = (state: Container.TypedState) => ({
-  _details: state.wallets.airdropDetails.details,
-  acceptedDisclaimer: state.wallets.acceptedDisclaimer,
-  loading: state.wallets.airdropState === 'loading',
-  signedUp: state.wallets.airdropState === 'accepted',
-})
-
-const mapDispatchToProps = (dispatch: Container.TypedDispatch) => ({
-  onBack: () => dispatch(RouteTreeGen.createNavigateUp()),
-  onCheckQualify: () => dispatch(RouteTreeGen.createNavigateAppend({path: ['airdropQualify']})),
-  onLoad: () => {
-    dispatch(WalletsGen.createUpdateAirdropState())
-    dispatch(WalletsGen.createUpdateAirdropDetails())
-  },
-  onReject: () => dispatch(WalletsGen.createChangeAirdrop({accept: false})),
-})
-
 export type Props = AirdropProps & {acceptedDisclaimer: boolean}
 const AirdropOrOnboarding = (props: Props) =>
   props.acceptedDisclaimer ? <Airdrop {...props} /> : <Onboarding nextScreen="airdrop" />
 
 const ConnectedAirdrop = Container.connect(
-  mapStateToProps,
-  mapDispatchToProps,
+  state => {
+    const {acceptedDisclaimer, airdropDetails, airdropState} = state.wallets
+    const {details} = airdropDetails
+    return {
+      acceptedDisclaimer,
+      headerBody: details.header.body,
+      headerTitle: details.header.title,
+      loading: airdropState === 'loading',
+      sections: details.sections,
+      signedUp: airdropState === 'accepted',
+    }
+  },
+  dispatch => ({
+    onBack: () => dispatch(RouteTreeGen.createNavigateUp()),
+    onCheckQualify: () => dispatch(RouteTreeGen.createNavigateAppend({path: ['airdropQualify']})),
+    onLoad: () => {
+      dispatch(WalletsGen.createUpdateAirdropState())
+      dispatch(WalletsGen.createUpdateAirdropDetails())
+    },
+    onReject: () => dispatch(WalletsGen.createChangeAirdrop({accept: false})),
+  }),
   (stateProps, dispatchProps) => ({
     acceptedDisclaimer: stateProps.acceptedDisclaimer,
-    headerBody: stateProps._details.header.body,
-    headerTitle: stateProps._details.header.title,
+    headerBody: stateProps.headerBody,
+    headerTitle: stateProps.headerTitle,
     loading: stateProps.loading,
     onBack: dispatchProps.onBack,
     onCheckQualify: dispatchProps.onCheckQualify,
     onLoad: dispatchProps.onLoad,
     onReject: dispatchProps.onReject,
-    sections: stateProps._details.sections.toArray().map(s => ({
-      icon: s.icon,
-      lines: s.lines.toArray().map(l => ({
-        bullet: l.bullet,
-        text: l.text,
-      })),
-      section: s.section,
-    })),
+    sections: stateProps.sections,
     signedUp: stateProps.signedUp,
     title: 'Airdrop',
   })
