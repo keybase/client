08a629955bd1d7bdc148262ef59b1822f1c5a47b
Daniel Ayoub <danny.a95@gmail.com>
2019-12-03 11:54:55 -0500

    lint

--- shared/tracker2/remote-serializer.desktop.tsx
+++ shared/tracker2/remote-serializer.desktop.tsx
@@ -1,4 +1,3 @@
-import * as I from 'immutable'
 import * as Constants from '../constants/tracker2'
 import * as Types from '../constants/types/tracker2'
 import * as Avatar from '../desktop/remote/sync-avatar-props.desktop'
