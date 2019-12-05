e083991ad3d5e4d5da9bfa294933742aab30ee74
Jakob Weisblat <jakob@keyba.se>
2019-12-05 10:36:13 -0500

    Block modal UI fixes (#21390)
    * ui fixes
    
    * move clickable box
    
    * ayoubd feedback
    
    * stories

--- shared/chat/blocking/block-modal/index.tsx
+++ shared/chat/blocking/block-modal/index.tsx
@@ -45,6 +45,7 @@ const CheckboxRow = (props: CheckboxRowProps) => {
     <>
       <Kb.Box2 direction="horizontal" alignItems="center" fullWidth={true} style={styles.checkBoxRow}>
         <Kb.Switch
+          allowLabelClick={!props.disabled}
           color="red"
           disabled={props.disabled}
           gapSize={Styles.globalMargins.tiny}
@@ -297,7 +298,7 @@ class BlockModal extends React.PureComponent<Props, State> {
       ) : (
         undefined
       ),
-      title: <Kb.Icon type="iconfont-block-user" sizeType="Big" color="red" />,
+      title: <Kb.Icon type="iconfont-block-user" sizeType="Big" color={Styles.globalColors.red} />,
     }
 
     if (this.props.loadingWaiting) {
