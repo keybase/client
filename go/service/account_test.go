b8fd1b65f031374fdc79107201f0ec5cc14986cd
M Mou <mmou@keyba.se>
2019-12-06 15:47:35 -0800

    fix tes

--- go/service/account_test.go
+++ go/service/account_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/keybase/client/go/kbtest"
 	"github.com/keybase/client/go/libkb"
 	keybase1 "github.com/keybase/client/go/protocol/keybase1"
+	"github.com/keybase/client/go/teams"
 	"github.com/stretchr/testify/require"
 	"golang.org/x/net/context"
 )
@@ -79,7 +80,7 @@ func TestContactSettingsAPI(t *testing.T) {
 	tc := libkb.SetupTest(t, "cset", 3)
 	defer tc.Cleanup()
 
-	_, err := kbtest.CreateAndSignupFakeUser("cset", tc.G)
+	user, err := kbtest.CreateAndSignupFakeUser("cset", tc.G)
 	require.NoError(t, err)
 
 	handler := NewAccountHandler(nil, tc.G)
@@ -87,9 +88,20 @@ func TestContactSettingsAPI(t *testing.T) {
 	res, err := handler.UserGetContactSettings(ctx)
 	require.NoError(t, err)
 
+	teamName := user.Username + "t"
+	teamID, err := teams.CreateRootTeam(ctx, tc.G, teamName, keybase1.TeamSettings{})
+	require.NoError(t, err)
+	require.NotNil(t, teamID)
+
 	err = handler.UserSetContactSettings(ctx, keybase1.ContactSettings{
 		Enabled:              true,
 		AllowFolloweeDegrees: 2,
+		Teams: []keybase1.TeamContactSettings{keybase1.TeamContactSettings{
+			TeamID:                      *teamID,
+			AllowFolloweesOfTeamMembers: false,
+			Enabled:                     true,
+		},
+		},
 	})
 	require.NoError(t, err)
 
@@ -97,5 +109,8 @@ func TestContactSettingsAPI(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, true, res.Enabled)
 	require.Equal(t, 2, res.AllowFolloweeDegrees)
-	require.Equal(t, 0, len(res.Teams))
+	require.Equal(t, 1, len(res.Teams))
+	require.Equal(t, *teamID, res.Teams[0].TeamID)
+	require.Equal(t, false, res.Teams[0].AllowFolloweesOfTeamMembers)
+	require.Equal(t, true, res.Teams[0].Enabled)
 }
