9851f93733b41b26cac321ced26203b70ecd32e8
chrisnojima <cnojima@keyba.se>
2019-12-10 09:38:37 -0500

    connect thrash helper

--- shared/util/typed-connect.tsx
+++ shared/util/typed-connect.tsx
@@ -1,8 +1,58 @@
 import * as RR from 'react-redux'
 import {compose, setDisplayName} from 'recompose'
 import {TypedState} from '../constants/reducer'
+import flags from './feature-flags'
+import shallowEqual from 'shallowequal'
+import {red} from './local-console'
 
-const connect = RR.connect
+const checkedMSP = new WeakMap()
+
+__DEV__ &&
+  flags.connectThrashCheck &&
+  console.log('Connect thrash enabled' + new Array(1000).fill('!').join(''))
+
+const compareAndComplain = (msp: any, try1: any, try2: any, loc: any) => {
+  if (!shallowEqual(try1, try2)) {
+    red(`Connect thrash check: bad connector! (${loc}) ${msp.toString()}`)
+    console.log('Connect thrash check => right click console | Show function definition', msp) // print the closure so you can jump to it
+  }
+}
+
+const connect: typeof RR.connect =
+  __DEV__ && flags.connectThrashCheck
+    ? (msp: any, mdp, mp) => {
+        let checkingMSP: any
+        const loc = new Error()?.stack?.split('\n')?.[4]?.match(/(\..*)/)?.[1] ?? 'unknown'
+        if (msp.length === 2) {
+          checkingMSP = (state: TypedState, ownProps: any) => {
+            // only check once
+            if (!checkedMSP.has(msp)) {
+              checkedMSP.set(msp, true)
+              // call twice and see if it mutates, which is bad
+              const try1 = msp(state, ownProps)
+              const try2 = msp(state, ownProps)
+              compareAndComplain(msp, try1, try2, loc)
+              return try2
+            }
+            return msp(state, ownProps)
+          }
+        } else {
+          checkingMSP = (state: TypedState) => {
+            // only check once
+            if (!checkedMSP.has(msp)) {
+              checkedMSP.set(msp, true)
+              // call twice and see if it mutates, which is bad
+              const try1 = msp(state)
+              const try2 = msp(state)
+              compareAndComplain(msp, try1, try2, loc)
+              return try2
+            }
+            return msp(state)
+          }
+        }
+        return RR.connect(checkingMSP, mdp, mp)
+      }
+    : RR.connect
 
 export const namedConnect = <TOwnProps, TStateProps, TDispatchProps, TMergedProps>(
   mapStateToProps: RR.MapStateToProps<TStateProps, TOwnProps>,
