d1a1ca7e6afb0b5e60bee9b9e6a7cbed0bf1c3a3
chrisnojima <cnojima@keyba.se>
2019-12-10 10:26:27 -0500

    add bad keys

--- shared/util/typed-connect.tsx
+++ shared/util/typed-connect.tsx
@@ -13,7 +13,13 @@ __DEV__ &&
 
 const compareAndComplain = (msp: any, try1: any, try2: any, loc: any) => {
   if (!shallowEqual(try1, try2)) {
-    red(`Connect thrash check: bad connector! (${loc}) ${msp.toString()}`)
+    const badKeys = Object.keys(try1).reduce<Array<string>>((arr, k) => {
+      if (try1[k] !== try2[k]) {
+        arr.push(k)
+      }
+      return arr
+    }, [])
+    red(`Connect thrash check: bad connector! (${loc}) keys: ${badKeys}\n ${msp.toString()}`)
     console.log('Connect thrash check => right click console | Show function definition', msp) // print the closure so you can jump to it
   }
 }
