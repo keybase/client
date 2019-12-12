659a02cddf265404ca30de98cb66c448d025ca4b
chrisnojima <cnojima@keyba.se>
2019-12-12 14:29:52 -0500

    fix dark mode switch empty inbox

--- shared/chat/inbox/container/defer-loading.tsx
+++ shared/chat/inbox/container/defer-loading.tsx
@@ -2,9 +2,15 @@ import * as React from 'react'
 import * as Kb from '../../../common-adapters'
 import Inbox from '.'
 
+// keep track of this even on unmount, else if you background / foreground you'll lose it
+let _everFocused = false
 const Deferred = () => {
-  const [everFocused, setEverFocused] = React.useState(false)
-  return everFocused ? <Inbox /> : <Kb.NavigationEvents onWillFocus={() => setEverFocused(true)} />
+  const [everFocused, _setEverFocused] = React.useState(_everFocused)
+  const setEverFocused = React.useCallback(() => {
+    _everFocused = true
+    _setEverFocused(_everFocused)
+  }, [_setEverFocused])
+  return everFocused ? <Inbox /> : <Kb.NavigationEvents onWillFocus={setEverFocused} />
 }
 
 // @ts-ignore TS doesn't understand hoisting
