4e53bf0644052830eba055539bdc16dca467b459
Joshua Blum <joshblum@keyba.se>
2019-12-17 15:29:51 -0500

    cache outbox and readoutbox in memory (#21681)

--- go/chat/storage/hooks.go
+++ go/chat/storage/hooks.go
@@ -6,6 +6,12 @@ func SetupGlobalHooks(g *globals.Context) {
 	g.ExternalG().AddLogoutHook(inboxMemCache, "chat/storage/inbox")
 	g.ExternalG().AddDbNukeHook(inboxMemCache, "chat/storage/inbox")
 
+	g.ExternalG().AddLogoutHook(outboxMemCache, "chat/storage/outbox")
+	g.ExternalG().AddDbNukeHook(outboxMemCache, "chat/storage/outbox")
+
+	g.ExternalG().AddLogoutHook(readOutboxMemCache, "chat/storage/readoutbox")
+	g.ExternalG().AddDbNukeHook(readOutboxMemCache, "chat/storage/readoutbox")
+
 	g.ExternalG().AddLogoutHook(reacjiMemCache, "chat/storage/reacjiMemCache")
 	g.ExternalG().AddDbNukeHook(reacjiMemCache, "chat/storage/reacjiMemCache")
 
