dad40d6650ee9cbad00cb2a06fbfabfedad12c10
chrisnojima <cnojima@keyba.se>
2019-12-18 16:48:18 -0500

    WIP

--- shared/chat/inbox/container/remote.tsx
+++ shared/chat/inbox/container/remote.tsx
@@ -1,79 +1,36 @@
 import * as Constants from '../../../constants/chat2'
 import * as Styles from '../../../styles'
-import * as ChatTypes from '../../../constants/types/chat2'
-import {TypedState} from '../../../constants/reducer'
+import * as Types from '../../../constants/types/chat2'
+import {memoize} from '../../../util/memoize'
 
 export type RemoteConvMeta = any
 
 // A hack to store the username to avoid plumbing.
 let _username: string
-let _lastSent:
-  | Array<{
-      hasBadge: boolean
-      hasUnread: boolean
-      conversation: ChatTypes.ConversationMeta
-    }>
-  | undefined
-let _lastSentParams = {
-  badgeCount: 0,
-  inboxVers: '',
-  unreadCount: 0,
-}
 
-const changed = (state: TypedState) => {
-  const {metaMap, inboxLayout, badgeMap, unreadMap} = state.chat2
-
-  const nextSentParams = {
-    badgeCount: 0,
-    inboxVers: '',
-    unreadCount: 0,
-  }
-
-  const wl = inboxLayout?.widgetList
-  wl?.forEach(w => {
-    const {convID} = w
-    nextSentParams.badgeCount += badgeMap.get(convID) ? 1 : 0
-    nextSentParams.unreadCount += unreadMap.get(convID) ? 1 : 0
-    nextSentParams.inboxVers += `:${metaMap.get(convID)?.inboxVersion ?? 'none'}`
-  })
-
-  const isChanged =
-    !_lastSentParams ||
-    !_lastSent ||
-    nextSentParams.badgeCount !== _lastSentParams.badgeCount ||
-    nextSentParams.unreadCount !== _lastSentParams.unreadCount ||
-    nextSentParams.inboxVers !== _lastSentParams.inboxVers
-
-  isChanged && console.log('aaa ischanged', isChanged, _lastSentParams, nextSentParams)
-  if (isChanged) {
-    _lastSentParams = nextSentParams
-  }
-
-  return isChanged
-}
-
-export const conversationsToSend = (state: TypedState) => {
-  _username = state.config.username
-  if (changed(state)) {
-    const TEMP = _lastSent
-    _lastSent = state.chat2.inboxLayout?.widgetList?.map(v => ({
-      conversation: state.chat2.metaMap.get(v.convID) || {
+export const conversationsToSend = memoize(
+  (
+    inboxLayout: Types.State['inboxLayout'],
+    metaMap: Types.State['metaMap'],
+    badgeMap: Types.State['badgeMap'],
+    unreadMap: Types.State['unreadMap'],
+    username: string
+  ) => {
+    // a terrible hack to capture this since the serialize flow doesn't understand things outside of itself
+    _username = username
+    // and a terrible hack
+    return inboxLayout?.widgetList?.map(v => ({
+      conversation: metaMap.get(v.convID) || {
         ...Constants.makeConversationMeta(),
         conversationIDKey: v.convID,
       },
-      hasBadge: (state.chat2.badgeMap.get(v.convID) || 0) > 0,
-      hasUnread: (state.chat2.unreadMap.get(v.convID) || 0) > 0,
+      hasBadge: !!badgeMap.get(v.convID),
+      hasUnread: !!unreadMap.get(v.convID),
     }))
-
-    console.log('aaa diff', TEMP, _lastSent)
   }
-  return _lastSent
-}
+)
 
-export const changeAffectsWidget = (
-  oldConv: ChatTypes.ConversationMeta,
-  newConv: ChatTypes.ConversationMeta
-) =>
+export const changeAffectsWidget = (oldConv: Types.ConversationMeta, newConv: Types.ConversationMeta) =>
   oldConv !== newConv &&
   !(
     oldConv.rekeyers === newConv.rekeyers &&
@@ -88,15 +45,12 @@ export const changeAffectsWidget = (
     oldConv.membershipType === newConv.membershipType
   )
 
-export const serialize = ({
-  hasBadge,
-  hasUnread,
-  conversation,
-}: {
+export const serialize = (s: {
   hasBadge: boolean
   hasUnread: boolean
-  conversation: ChatTypes.ConversationMeta
+  conversation: Types.ConversationMeta
 }): RemoteConvMeta => {
+  const {hasBadge, hasUnread, conversation} = s
   const styles = Constants.getRowStyles(false, hasUnread)
   const participantNeedToRekey = conversation.rekeyers.size > 0
   const youNeedToRekey = !!participantNeedToRekey && conversation.rekeyers.has(_username)
