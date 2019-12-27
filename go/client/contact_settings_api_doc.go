ec652a9281e4015d52fd3cac3ac61cd086c5d82e
M Mou <mmou@keyba.se>
2019-12-26 20:37:40 -0800

    clean up

--- go/client/contact_settings_api_doc.go
+++ go/client/contact_settings_api_doc.go
@@ -11,8 +11,8 @@ Set contact settings on, only allowing users to message me if I follow them:
 	{"method": "set", "params": {"options": {"settings": {"allow_followee_degrees": 1, "allow_good_teams": false, "enabled": true, "teams": []}}}}
 
 Set contact settings on, only allowing users to message me if I follow them or I follow someone who follows them:
-	{"method": "set", "params": {"options": {"settings": {"allow_followee_degrees": 1, "allow_good_teams": false, "enabled": true, "teams": []}}}}
+	{"method": "set", "params": {"options": {"settings": {"allow_followee_degrees": 2, "allow_good_teams": false, "enabled": true, "teams": []}}}}
 
 Set contact settings on, only allowing users to message me if I follow them, and if they are in the team 'acme' with me
-	{"method": "set", "params": {"options": {"settings": {"allow_followee_degrees": 1, "allow_good_teams": true, "enabled": true,"teams": [{"team_name": "acme", "enabled": true }, {"team_name": "nadir", "enabled": false}]}}}}
+	{"method": "set", "params": {"options": {"settings": {"allow_followee_degrees": 1, "allow_good_teams": true, "enabled": true, "teams": [{"team_name": "acme", "enabled": true}, {"team_name": "nadir", "enabled": false}]}}}}
 `
