613b7a68f9609efe1584232c79d867725c80534b
Alex Gessner <alex@keyba.se>
2020-01-03 14:59:12 -0500

    fix an issue where the batch script was exiting early because of the way 'yarn run package' was called

--- packaging/windows/buildui.cmd
+++ packaging/windows/buildui.cmd
@@ -13,12 +13,16 @@ IF %ERRORLEVEL% NEQ 0 (
   EXIT /B 1
 )
 
-yarn run package -- --arch x64 --platform win32 --appVersion %KEYBASE_VERSION% --icon %GOPATH%\src\github.com\keybase\client\media\icons\Keybase.ico
+cmd /C yarn run package --arch x64 --platform win32 --appVersion %KEYBASE_VERSION% --icon %GOPATH%\src\github.com\keybase\client\media\icons\Keybase.ico
+IF %ERRORLEVEL% NEQ 0 (
+  EXIT /B 1
+)
 popd
 
 :: wix (specifically heat) has a bad time with marking folders for deletion on uninstall if any
 :: of the folders in our distribution only contain folders (i.e. no non-folder files). so step into
 :: every folder in our distribution recursively, and create an empty file wherever necessary.
+echo off
 pushd %GOPATH%\src\github.com\keybase\client\shared\desktop\release\win32-x64\Keybase-win32-x64
 
 :: this is necessary to use variables inside a for loop
@@ -31,6 +35,7 @@ for /R %%G in (.) do (
   if !countOfNonDirectoryFiles!==0 (
     :: create an empty file in this directory
     copy NUL EmptyFile.txt
+    echo created an empty file at %%G
   )
   popd %%G
 )
