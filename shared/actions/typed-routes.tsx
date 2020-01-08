d83a2a4252a6337ac705b98f0c58ed29e7a168e3
Jacob Young <jacob@jry.io>
2020-01-08 13:19:57 -0500

    Merge thebearjew/crypo/tab
    (cherry picked from commit 8449a5289e5113045363cf4952045cf8a38075bf)

--- shared/actions/typed-routes.tsx
+++ shared/actions/typed-routes.tsx
@@ -48,4 +48,3 @@ export const appendEncryptRecipientsBuilder = () =>
   RouteTreeGen.createNavigateAppend({
     path: [{props: {namespace: 'crypto', title: 'Recipients'}, selected: 'cryptoTeamBuilder'}],
   })
-
