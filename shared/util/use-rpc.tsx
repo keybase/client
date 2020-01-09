7fbfbf066ac8dfdc6842ff8d4bf947777039d58f
chrisnojima <cnojima@keyba.se>
2020-01-09 13:12:53 -0500

    WIP

--- shared/util/use-rpc.tsx
+++ shared/util/use-rpc.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import {RPCError} from './errors'
 /** A hook to make an RPC call. This entirely skips our redux layer and shouldn't be used if you need any side effects
  */
 
@@ -7,17 +8,24 @@ function useRPC<ARGS extends Array<any>, RES extends Promise<any>, C extends (ar
   args: ARGS,
   skipCall: boolean,
   triggers: Array<any>
-): [RES | undefined, Boolean] {
+): [RPCError | undefined, RES | undefined, Boolean] {
   const [loading, setLoading] = React.useState<Boolean>(true)
   const [result, setResult] = React.useState<RES | undefined>(undefined)
+  const [error, setError] = React.useState<RPCError | undefined>(undefined)
   React.useEffect(() => {
     if (skipCall) {
       setLoading(false)
+      setError(undefined)
     } else {
       const fetchData = async () => {
-        const r = await rpcCall(args)
-        setResult(r)
-        setLoading(false)
+        try {
+          const r = await rpcCall(args)
+          setResult(r)
+        } catch (e) {
+          setError(e)
+        } finally {
+          setLoading(false)
+        }
       }
       setLoading(true)
       setResult(undefined)
@@ -25,7 +33,7 @@ function useRPC<ARGS extends Array<any>, RES extends Promise<any>, C extends (ar
     }
     // eslint-disable-next-line
   }, triggers)
-  return [result, loading]
+  return [error, result, loading]
 }
 
 export default useRPC
