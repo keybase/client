dee2e251d16cc8df25deca33a8d6e1e65d5faef6
chrisnojima <cnojima@keyba.se>
2020-01-15 16:28:39 -0500

    WIP

--- shared/__mocks__/resolve-root.tsx
+++ shared/__mocks__/resolve-root.tsx
@@ -4,5 +4,6 @@ if (!__STORYBOOK__) {
 export const resolveRoot = (...to: any) => to.join('/')
 export const resolveRootAsURL = (...to: any) => to.join('/')
 export const resolveImage = (...to: any) => to.join('/')
+export const resolveImageAsURL = (...to: any) => to.join('/')
 
 export default resolveRoot
