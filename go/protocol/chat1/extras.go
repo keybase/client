25d8a9af3074289c170fbed5fcae4b05005b3092
Miles Steele <miles@milessteele.com>
2020-01-16 14:53:02 -0500

    un-visible join and leave messages (#22072)

--- go/protocol/chat1/extras.go
+++ go/protocol/chat1/extras.go
@@ -190,8 +190,10 @@ func DeletableMessageTypesByDelete() []MessageType {
 var visibleMessageTypes = []MessageType{
 	MessageType_TEXT,
 	MessageType_ATTACHMENT,
-	MessageType_JOIN,
-	MessageType_LEAVE,
+	// TODO TRIAGE-1738 Join and leave are visible but changing this list kicks off some other bugs around notifications (HOTPOT-1688).
+	// Scour the effects before changing this list.
+	// MessageType_JOIN,
+	// MessageType_LEAVE,
 	MessageType_SYSTEM,
 	MessageType_SENDPAYMENT,
 	MessageType_REQUESTPAYMENT,
