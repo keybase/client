c26b918b20611b58ee768c97e22a12174162891d
Alex Gessner <alex@keyba.se>
2020-01-23 09:56:09 -0500

    remove unused hasEverConnected method

--- shared/engine/__mocks__/index.tsx
+++ shared/engine/__mocks__/index.tsx
@@ -13,7 +13,6 @@ class FakeEngine {
   rpc() {}
   listenOnConnect() {}
   listenOnDisconnect() {}
-  hasEverConnected() {}
   setIncomingActionCreator() {}
   createSession() {
     return new Session({
