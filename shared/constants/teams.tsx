a9363fd690b4ecaac43eb57f7679229a5d194b3e
Jakob Weisblat <jakob@keyba.se>
2020-01-27 11:29:32 -0500

    fix team add owner from chat maybe (#22154)

--- shared/constants/teams.tsx
+++ shared/constants/teams.tsx
@@ -392,7 +392,8 @@ export const getDisabledReasonsForRolePicker = (
 ): Types.DisabledReasonsForRolePicker => {
   const canManageMembers = getCanPerformByID(state, teamID).manageMembers
   const teamDetails = getTeamDetails(state, teamID)
-  const members: Map<string, Types.MemberInfo> = teamDetails.members || new Map()
+  const members: Map<string, Types.MemberInfo> =
+    teamDetails.members || state.teams.teamIDToMembers.get(teamID) || new Map()
   const teamname = teamDetails.teamname
   const member = memberToModify ? members.get(memberToModify) : undefined
   const theyAreOwner = member?.type === 'owner'
