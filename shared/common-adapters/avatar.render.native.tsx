ad9590df2821e42ec559e0edc1c645beef2bfc13
Mike Maxim <mike.maxim@gmail.com>
2020-01-30 14:18:32 -0500

    fix avatar

--- shared/common-adapters/avatar.render.native.tsx
+++ shared/common-adapters/avatar.render.native.tsx
@@ -29,7 +29,7 @@ const borderSize = 1
 
 const Avatar = (props: Props) => {
   const {size} = props
-  const borderRadius = (props.isTeam && sizeToTeamBorderRadius.get(size)) ?? size / 2
+  const borderRadius = (props.isTeam && sizeToTeamBorderRadius.get(size)) || size / 2
   const containerStyle = Styles.collapseStyles([boxStyles[size], props.style])
 
   return (
