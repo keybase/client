82ddd013df3c84718006db083cc0d146602db6c4
chrisnojima <chrisnojima@gmail.com>
2020-02-03 14:10:44 -0500

    Bring back preload + invoke for electron - again (#22294)
    * Revert "Revert "Bring back preload + invoke for electron (#22249)" (#22293)"
    
    This reverts commit 2161a3fa8f5fef1e80ab8a5cdd7c12ff454c7a67.
    
    * WIP

--- shared/util/clipboard.desktop.tsx
+++ shared/util/clipboard.desktop.tsx
@@ -1,4 +1,4 @@
-import * as SafeElectron from './safe-electron.desktop'
+import * as Electron from 'electron'
 
 export type ClipboardData = {
   path: string
@@ -7,7 +7,7 @@ export type ClipboardData = {
 
 function readImage(): Promise<Buffer | null> {
   return new Promise(resolve => {
-    const image = SafeElectron.getClipboard().readImage()
+    const image = Electron.clipboard.readImage()
     if (!image) {
       // Nothing to read
       resolve(null)
@@ -22,7 +22,7 @@ export function readImageFromClipboard(
   event: React.SyntheticEvent,
   willReadData: () => void
 ): Promise<Buffer | null> {
-  const formats = SafeElectron.getClipboard().availableFormats()
+  const formats = Electron.clipboard.availableFormats()
   console.log('Read clipboard, formats:', formats)
   const imageFormats = formats.filter(f => f.startsWith('image/'))
 
