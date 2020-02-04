4a7a7d80b685308744a59bab2fa60725945166fc
cori hudson <54032873+hyperobject@users.noreply.github.com>
2020-02-04 09:59:40 -0500

    SetPublicName new icons, autofill device name (#22333)
    * fix: use new backgrounded icon on provision screen
    
    * feat: use expo-constants to try and autofill device name
    
    * fix: find real next device icon, address feedback
    
    * chore: remove unused imports in set-public-name

--- shared/provision/set-public-name/container.tsx
+++ shared/provision/set-public-name/container.tsx
@@ -1,11 +1,14 @@
 import * as ProvisionGen from '../../actions/provision-gen'
 import * as RouteTreeGen from '../../actions/route-tree-gen'
 import * as Constants from '../../constants/provision'
+import * as Platform from '../../constants/platform'
+import * as Devices from '../../constants/devices'
 import SetPublicName from '.'
 import * as Container from '../../util/container'
 
 export default Container.connect(
   (state: Container.TypedState) => ({
+    devices: state.provision.devices,
     error: state.provision.error.stringValue(),
     waiting: Container.anyWaiting(state, Constants.waitingKey),
   }),
@@ -13,10 +16,17 @@ export default Container.connect(
     onBack: () => dispatch(RouteTreeGen.createNavigateUp()),
     onSubmit: (name: string) => dispatch(ProvisionGen.createSubmitDeviceName({name})),
   }),
-  (stateProps, dispatchProps) => ({
-    error: stateProps.error,
-    onBack: dispatchProps.onBack,
-    onSubmit: (name: string) => !stateProps.waiting && dispatchProps.onSubmit(name),
-    waiting: stateProps.waiting,
-  })
+  (stateProps, dispatchProps) => {
+    const deviceNumbers = stateProps.devices
+      .filter(d => d.type === (Platform.isMobile ? 'mobile' : 'desktop'))
+      .map(d => d.deviceNumberOfType)
+    const maxDeviceNumber = deviceNumbers.length > 0 ? Math.max(...deviceNumbers) : -1
+    return {
+      deviceIconNumber: ((maxDeviceNumber + 1) % Devices.numBackgrounds) + 1,
+      error: stateProps.error,
+      onBack: dispatchProps.onBack,
+      onSubmit: (name: string) => !stateProps.waiting && dispatchProps.onSubmit(name),
+      waiting: stateProps.waiting,
+    }
+  }
 )(Container.safeSubmit(['onSubmit', 'onBack'], ['error'])(SetPublicName))
