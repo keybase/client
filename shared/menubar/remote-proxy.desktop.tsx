bb98d371bb11c2e3a3277adbb06baedc04453d16
chrisnojima <chrisnojima@gmail.com>
2020-02-04 09:52:17 -0500

    dont use main window in renderer (#22352)

--- shared/menubar/remote-proxy.desktop.tsx
+++ shared/menubar/remote-proxy.desktop.tsx
@@ -9,8 +9,6 @@ import * as Electron from 'electron'
 import {intersect} from '../util/set'
 import useSerializeProps from '../desktop/remote/use-serialize-props.desktop'
 import {serialize} from './remote-serializer.desktop'
-import {getMainWindow} from '../desktop/remote/util.desktop'
-import {resolveImage} from '../desktop/app/resolve-root.desktop'
 import {isDarwin, isWindows} from '../constants/platform'
 import {isSystemDarkMode} from '../styles/dark-mode'
 import {uploadsToUploadCountdownHOCProps} from '../fs/footer/upload-container'
@@ -76,14 +74,6 @@ function useUpdateBadges(p: WidgetProps, darkCount: number) {
       payload: {desktopAppBadgeCount, icon, iconSelected},
       type: 'showTray',
     })
-    // Windows just lets us set (or unset, with null) a single 16x16 icon
-    // to be used as an overlay in the bottom right of the taskbar icon.
-    if (isWindows) {
-      const mw = getMainWindow()
-      const overlay = desktopAppBadgeCount > 0 ? resolveImage('icons', 'icon-windows-badge.png') : null
-      // @ts-ignore setOverlayIcon docs say null overlay's fine, TS disagrees
-      mw && mw.setOverlayIcon(overlay, 'new activity')
-    }
   }, [widgetBadge, desktopAppBadgeCount, darkCount])
 }
 
