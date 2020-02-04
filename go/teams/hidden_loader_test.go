c3dfaa9bc7712c1597f73964be4c62a92ddb4466
Maxwell Krohn <themax@gmail.com>
2020-02-03 20:53:20 -0500

    teams/hidden: still persist racthets from visible->hidden on parent FTL
    - we can't load the data or the seeds in all cases, since we might not have
      privileges to if we're loading a parent team in FTL.
    - similarly, we might be feature-flagged off
    - still, we should note and record if the main chain is ratcheting the hidden chain forward
    - since if we do become members of the parent, we don't want a rollback

--- go/teams/hidden_loader_test.go
+++ go/teams/hidden_loader_test.go
@@ -793,7 +793,7 @@ func TestSubteamReaderFTL(t *testing.T) {
 	defer cleanup()
 
 	t.Logf("create team")
-	teamName, _ := createTeam2(*tcs[0])
+	teamName, teamID := createTeam2(*tcs[0])
 	for i := 0; i < 4; i++ {
 		makeHiddenRotation(t, tcs[0].G, teamName)
 	}
@@ -801,11 +801,19 @@ func TestSubteamReaderFTL(t *testing.T) {
 	subteamName, subteamID := createSubteam(tcs[0], teamName, "sub")
 	_, err := AddMember(context.TODO(), tcs[0].G, subteamName.String(), fus[1].Username, keybase1.TeamRole_WRITER, nil)
 	require.NoError(t, err)
-	_, err = tcs[1].G.GetFastTeamLoader().Load(libkb.NewMetaContextForTest(*tcs[1]), keybase1.FastTeamLoadArg{
+	mctx := libkb.NewMetaContextForTest(*tcs[1])
+	_, err = mctx.G().GetFastTeamLoader().Load(mctx, keybase1.FastTeamLoadArg{
 		ID:                   subteamID,
 		ForceRefresh:         true,
 		Applications:         []keybase1.TeamApplication{keybase1.TeamApplication_CHAT},
 		KeyGenerationsNeeded: []keybase1.PerTeamKeyGeneration{keybase1.PerTeamKeyGeneration(1)},
 	})
 	require.NoError(t, err)
+	var htc *keybase1.HiddenTeamChain
+	htc, err = mctx.G().GetHiddenTeamChainManager().Load(mctx, teamID)
+	require.NoError(t, err)
+	require.NotNil(t, htc)
+	ratchet, ok := htc.RatchetSet.Ratchets[keybase1.RatchetType_MAIN]
+	require.True(t, ok)
+	require.Equal(t, ratchet.Triple.Seqno, keybase1.Seqno(4))
 }
