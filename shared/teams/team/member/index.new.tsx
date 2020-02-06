5c1cca28843b57f45c34d148aa61e9ccb2380d09
Daniel Ayoub <danny.a95@gmail.com>
2020-02-06 12:24:23 -0500

    routing and make it a real header

--- shared/teams/team/member/index.new.tsx
+++ shared/teams/team/member/index.new.tsx
@@ -5,16 +5,29 @@ import * as Constants from '../../../constants/teams'
 import * as Types from '../../../constants/types/teams'
 import * as Container from '../../../util/container'
 
-type Props = Container.RouteProps<{
+type Props = {
   teamID: Types.TeamID
   username: string
-}>
+}
 
-const TeamMember = (props: Props) => {
+class TeamMember extends React.Component<Container.RouteProps<Props>> {
+  static navigationOptions = (ownProps: Container.RouteProps<Props>) => ({
+    headerTitle: () => (
+      <TeamMemberHeader
+        teamID={Container.getRouteProps(ownProps, 'teamID', Types.noTeamID)}
+        username={Container.getRouteProps(ownProps, 'username', '')}
+      />
+    ),
+  })
+  render() {
+    return null
+  }
+}
+
+const TeamMemberHeader = (props: Props) => {
+  const {teamID, username} = props
   const dispatch = Container.useDispatch()
 
-  const teamID = Container.getRouteProps(props, 'teamID', Types.noTeamID)
-  const username = Container.getRouteProps(props, 'username', '')
   const teamMeta = Container.useSelector(s => Constants.getTeamMeta(s, teamID))
   const teamDetails = Container.useSelector(s => Constants.getTeamDetails(s, teamID))
 
@@ -23,6 +36,18 @@ const TeamMember = (props: Props) => {
     // loading? should never happen.
     return null
   }
+  return (
+    <Kb.Box2 direction="vertical" fullWidth={true} fullHeight={true}>
+      <Kb.Box2 direction="vertical" fullWidth={true} gap="tiny">
+        <Kb.Box2 direction="horizontal" alignItems="center" gap="xtiny" alignSelf="flex-start">
+          <Kb.Avatar size={16} teamname={teamMeta.teamname} />
+          <Kb.Text type="BodySmallSemiboldSecondaryLink" onClick={() => {} /* TODO */}>
+            {teamMeta.teamname}
+          </Kb.Text>
+        </Kb.Box2>
+      </Kb.Box2>
+    </Kb.Box2>
+  )
 }
 
 export default TeamMember
