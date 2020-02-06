566afe3570535095bde5bd13ca3b417541a05c16
Miles Steele <miles@milessteele.com>
2020-02-06 09:47:45 -0500

    add tablet flag, provision wording

--- shared/util/feature-flags.native.tsx
+++ shared/util/feature-flags.native.tsx
@@ -22,6 +22,7 @@ const ff: FeatureFlags = {
   outOfDateBanner: false,
   proofProviders: true,
   stellarExternalPartners: true,
+  tabletSupport: true,
   teamInvites: false,
   teamsRedesign: false,
   userBlocking: true,
