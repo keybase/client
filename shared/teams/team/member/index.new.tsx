806b39d470468cec1894ce30d43ce3af3d9b9bff
Daniel Ayoub <danny.a95@gmail.com>
2020-02-06 15:18:48 -0500

    block button, styling, safe nav

--- shared/teams/team/member/index.new.tsx
+++ shared/teams/team/member/index.new.tsx
@@ -4,7 +4,6 @@ import * as Styles from '../../../styles'
 import * as Constants from '../../../constants/teams'
 import * as Types from '../../../constants/types/teams'
 import * as Container from '../../../util/container'
-import * as RouteTreeGen from '../../../actions/route-tree-gen'
 import * as Chat2Gen from '../../../actions/chat2-gen'
 import * as ProfileGen from '../../../actions/profile-gen'
 
@@ -39,11 +38,13 @@ class TeamMember extends React.Component<Container.RouteProps<Props>> {
 const TeamMemberHeader = (props: Props) => {
   const {teamID, username} = props
   const dispatch = Container.useDispatch()
+  const nav = Container.useSafeNavigation()
 
   const teamMeta = Container.useSelector(s => Constants.getTeamMeta(s, teamID))
   const teamDetails = Container.useSelector(s => Constants.getTeamDetails(s, teamID))
+  const yourUsername = Container.useSelector(s => s.config.username)
 
-  const onBack = () => dispatch(RouteTreeGen.createNavigateUp())
+  const onBack = () => dispatch(nav.safeNavigateUpPayload())
   const onChat = () =>
     dispatch(Chat2Gen.createPreviewConversation({participants: [username], reason: 'memberView'}))
   const onViewProfile = () => dispatch(ProfileGen.createShowUserProfile({username}))
@@ -58,6 +59,7 @@ const TeamMemberHeader = (props: Props) => {
     <Kb.Box2 direction="horizontal" gap="tiny" alignSelf={Styles.isMobile ? 'flex-start' : 'flex-end'}>
       <Kb.Button small={true} label="Chat" onClick={onChat} />
       <Kb.Button small={true} label="View profile" onClick={onViewProfile} mode="Secondary" />
+      {username !== yourUsername && <BlockDropdown username={username} />}
     </Kb.Box2>
   )
 
@@ -85,12 +87,18 @@ const TeamMemberHeader = (props: Props) => {
             </Kb.Text>
           </Kb.Box2>
 
-          <Kb.Box2 direction="horizontal" gap="large" fullWidth={true} alignItems="flex-end">
+          <Kb.Box2
+            direction="horizontal"
+            gap="large"
+            fullWidth={true}
+            alignItems="flex-end"
+            style={styles.headerTextContainer}
+          >
             <Kb.Box2 direction="horizontal" gap="small">
               <Kb.Avatar size={64} username={username} />
-              <Kb.Box2 direction="vertical" alignItems="flex-start">
+              <Kb.Box2 direction="vertical" alignItems="flex-start" style={styles.headerText}>
                 <Kb.ConnectedUsernames type="Header" usernames={[username]} />
-                <Kb.Text type="BodySemibold">{member.fullName}</Kb.Text>
+                {!!member.fullName && <Kb.Text type="BodySemibold">{member.fullName}</Kb.Text>}
                 <Kb.Text type="BodySmall">Joined 1m ago</Kb.Text>
               </Kb.Box2>
             </Kb.Box2>
@@ -103,6 +111,37 @@ const TeamMemberHeader = (props: Props) => {
   )
 }
 
+const _BlockDropdown = (props: Kb.PropsWithOverlay<{username: string}>) => {
+  const dispatch = Container.useDispatch()
+  const nav = Container.useSafeNavigation()
+  const onBlock = () =>
+    dispatch(
+      nav.safeNavigateAppendPayload({
+        path: [{props: {username: props.username}, selected: 'chatBlockingModal'}],
+      })
+    )
+  return (
+    <>
+      <Kb.Button
+        small={true}
+        icon="iconfont-ellipsis"
+        tooltip=""
+        onClick={props.toggleShowingMenu}
+        mode="Secondary"
+        ref={props.setAttachmentRef}
+      />
+      <Kb.FloatingMenu
+        attachTo={props.getAttachmentRef}
+        visible={props.showingMenu}
+        onHidden={props.toggleShowingMenu}
+        closeOnSelect={true}
+        items={[{danger: true, icon: 'iconfont-remove', onClick: onBlock, title: 'Block'}]}
+      />
+    </>
+  )
+}
+const BlockDropdown = Kb.OverlayParentHOC(_BlockDropdown)
+
 const styles = Styles.styleSheetCreate(() => ({
   headerContainer: Styles.platformStyles({
     common: {
@@ -110,6 +149,7 @@ const styles = Styles.styleSheetCreate(() => ({
     },
     isElectron: {
       ...Styles.desktopStyles.windowDraggingClickable,
+      paddingBottom: Styles.globalMargins.small,
     },
     isMobile: {
       paddingTop: Styles.globalMargins.small,
@@ -118,6 +158,14 @@ const styles = Styles.styleSheetCreate(() => ({
   headerContent: {
     ...Styles.padding(0, Styles.globalMargins.small),
   },
+  headerText: Styles.platformStyles({
+    isElectron: {
+      width: 127,
+    },
+  }),
+  headerTextContainer: Styles.platformStyles({
+    isMobile: {paddingBottom: Styles.globalMargins.tiny},
+  }),
 }))
 
 export default TeamMember
