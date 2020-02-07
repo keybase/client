5128fe6e52fb84e89dab0c486f23576e67ee3a43
Maxwell Krohn <themax@gmail.com>
2020-02-07 14:34:03 -0500

    fix citogo doc

--- go/citogo/main.go
+++ go/citogo/main.go
@@ -68,7 +68,7 @@ func (r *runner) parseArgs() (err error) {
 	flag.BoolVar(&r.opts.NoCompile, "no-compile", false, "specify flag if you've pre-compiled the test")
 	flag.StringVar(&r.opts.TestBinary, "test-binary", "", "specify the test binary to run")
 	flag.StringVar(&r.opts.Timeout, "timeout", "60s", "timeout (in seconds) for any one individual test")
-	flag.DurationVar(&r.opts.Pause, "pause", 0, "pause that many seconds between each test")
+	flag.DurationVar(&r.opts.Pause, "pause", 0, "pause duration between each test (default 0)")
 	flag.Parse()
 	var d string
 	d, err = os.Getwd()
