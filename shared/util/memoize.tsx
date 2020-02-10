f8576124fb12bcaf13df1696ae6927bbe31008e9
chrisnojima <cnojima@keyba.se>
2020-02-10 15:38:17 -0500

    WIP

--- shared/util/memoize.tsx
+++ shared/util/memoize.tsx
@@ -1,8 +1,10 @@
 import memoize from 'memoize-one'
 import shallowEqual from 'shallowequal'
-import {useMemo, useCallback} from 'use-memo-one'
+import {useMemoOne, useCallbackOne} from 'use-memo-one'
 
 const memoizeShallow = (f: any) => memoize(f, ([a], [b]) => shallowEqual(a, b))
 
 export {memoizeShallow, memoize}
-export {useMemo, useCallback}
+
+export const useMemo = useMemoOne
+export const useCallback = useCallbackOne
