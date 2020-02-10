5242ffc63694af0651e7589d377e9555f106c341
Patrick Crosby <patrick@keyba.se>
2020-02-10 09:25:54 -0600

    Decrease width on wallet send forms on tablets (#22445)
    * Decrease width on wallet send forms on tablets
    
    * Fix order

--- shared/wallets/send-form/body/shared.tsx
+++ shared/wallets/send-form/body/shared.tsx
@@ -5,10 +5,16 @@ export const sharedStyles = Styles.styleSheetCreate(() => ({
     flexGrow: 1,
     flexShrink: 1,
   },
-  scrollView: {
-    flexGrow: 1,
-    flexShrink: 1,
-    width: '100%',
-  },
+  scrollView: Styles.platformStyles({
+    common: {
+      flexGrow: 1,
+      flexShrink: 1,
+      width: '100%',
+    },
+    isTablet: {
+      alignSelf: 'center',
+      width: 460,
+    },
+  }),
   spinnerContainer: {...Styles.globalStyles.fillAbsolute},
 }))
