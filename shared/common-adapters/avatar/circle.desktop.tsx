e3d2120ce799aaa282d6f6604b497cf01fdaf45c
chrisnojima <cnojima@keyba.se>
2020-02-10 15:09:15 -0500

    WIP

--- shared/common-adapters/avatar/circle.desktop.tsx
+++ shared/common-adapters/avatar/circle.desktop.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import * as Container from '../../util/container'
+import * as SVG from 'react-native-svg/lib/commonjs/ReactNativeSVG.web'
 
 type Props = {
   username: string
@@ -9,7 +10,11 @@ const Circle = (p: Props) => {
   const {username} = p
   if (!username) return null
 
-  return null
+  return (
+    <SVG.Svg height={100} width={100} viewBox="0 0 100 100">
+      <SVG.Circle cx="50" cy="50" r="45" stroke="blue" strokeWidth="2.5" fill="green" />
+    </SVG.Svg>
+  )
 }
 
 export default Circle
