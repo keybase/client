aa74ce939088629c961e034e90fdd5c565a1d58f
Chris Ball <chris@printf.net>
2020-02-11 11:52:58 -0800

    Revert "Attempt to solved flipped status bar height"
    This reverts commit dee55fe2cd7ec133072aabacda25dbde9b3d1bfb.

--- shared/common-adapters/keyboard-avoiding-view.native.tsx
+++ shared/common-adapters/keyboard-avoiding-view.native.tsx
@@ -10,26 +10,17 @@ import {isIPhoneX, isIOS, isTablet} from '../constants/platform'
 
 const {StatusBarManager} = NativeModules
 
-type FrameData = {
-  x: number
-  y: number
-  height: number
-  width: number
-}
-
 class KeyboardAvoidingView extends React.Component<KeyboardAvoidingViewProps, {verticalOffset: number}> {
   _listener?: EmitterSubscription
   _mounted = true
   state = {verticalOffset: 0}
 
   componentWillMount() {
-    if (isIOS) {
-      StatusBarManager.getHeight((frame: FrameData) => {
-        this.statusBarListener(frame)
-      })
-      this._listener = StatusBarIOS.addListener('statusBarFrameWillChange', (d: {frame: FrameData}) => {
+    if (isIOS && !isTablet) {
+      StatusBarManager.getHeight(this.statusBarListener)
+      this._listener = StatusBarIOS.addListener('statusBarFrameWillChange', (d: {frame: {height: number}}) =>
         this.statusBarListener(d.frame)
-      })
+      )
     }
   }
 
@@ -38,19 +29,12 @@ class KeyboardAvoidingView extends React.Component<KeyboardAvoidingViewProps, {v
     this._listener && this._listener.remove()
   }
 
-  private statusBarListener = (frameData: FrameData) => {
-    let statusBarHeight = frameData.height
-    // On tablets, the height and width values will be flipped depending on the orientation of the device.
-    // Portrait mode: statusBarHeight = FrameData.height
-    // Landscape mode: statusBarHeight = FrameData.width
-    if (isTablet && frameData.width && frameData.height) {
-      statusBarHeight = Math.min(frameData.width, frameData.height)
-    }
+  private statusBarListener = (frameData: {height: number}) => {
     // the iPhone X has default status bar height of 45px
     // and it doesn't increase in height like earlier devices.
     // (so this should always be 0 on an iPhone X, but this should still
     // be correct if it expands)
-    this._mounted && this.setState({verticalOffset: statusBarHeight - (isIPhoneX ? 45 : 20)})
+    this._mounted && this.setState({verticalOffset: frameData.height - (isIPhoneX ? 45 : 20)})
   }
 
   render() {
