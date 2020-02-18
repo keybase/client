b133204093549fb733dac90c8210ea5ea87a5a7f
Daniel Ayoub <danny.a95@gmail.com>
2020-02-18 17:36:40 -0500

    hook up

--- shared/teams/add-members-wizard/index.stories.tsx
+++ shared/teams/add-members-wizard/index.stories.tsx
@@ -3,7 +3,9 @@ import * as Sb from '../../stories/storybook'
 import EnableContacts from './enable-contacts'
 
 const load = () => {
-  Sb.storiesOf('Teams/Add member wizard', module).add('Enable contacts', () => <EnableContacts />)
+  Sb.storiesOf('Teams/Add member wizard', module).add('Enable contacts', () => (
+    <EnableContacts onClose={Sb.action('onClose')} />
+  ))
 }
 
 export default load
