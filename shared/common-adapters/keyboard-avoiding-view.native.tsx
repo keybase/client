60112664fc82c7bd41022b12b3ed4ec67c2abcde
chrisnojima <cnojima@keyba.se>
2020-02-18 13:31:32 -0500

    WIP

--- shared/common-adapters/keyboard-avoiding-view.native.tsx
+++ shared/common-adapters/keyboard-avoiding-view.native.tsx
@@ -1,15 +1,75 @@
+// @ts-nocheck
 import * as React from 'react'
 import {
   KeyboardAvoidingView as RnKBAV,
   StatusBarIOS,
   NativeModules,
+  LayoutAnimation,
+  StyleSheet,
   KeyboardAvoidingViewProps,
+  View,
 } from 'react-native'
 import {isIOS} from '../constants/platform'
-
 const {StatusBarManager} = NativeModules
 
-const KeyboardAvoidingView = (p: KeyboardAvoidingViewProps) => {
+// port of built in but is split aware
+class SplitAwareKeyboardAvoidingView extends RnKBAV {
+  _onKeyboardChange = (event: any) => {
+    if (event == null) {
+      this.setState({bottom: 0})
+      return
+    }
+
+    const {duration, easing, endCoordinates} = event
+    const height = this._relativeKeyboardHeight(endCoordinates)
+
+    if (this.state.bottom === height) {
+      return
+    }
+
+    if (duration && easing) {
+      LayoutAnimation.configureNext({
+        // We have to pass the duration equal to minimal accepted duration defined here: RCTLayoutAnimation.m
+        duration: duration > 10 ? duration : 10,
+        update: {
+          duration: duration > 10 ? duration : 10,
+          type: LayoutAnimation.Types[easing] || 'keyboard',
+        },
+      })
+    }
+    this.setState({bottom: height})
+  }
+
+  render() {
+    const {
+      behavior,
+      children,
+      contentContainerStyle,
+      enabled,
+      keyboardVerticalOffset,
+      style,
+      ...props
+    } = this.props
+    const bottomHeight = enabled && !this.state.disabledDueToSplit ? this.state.bottom : 0
+    switch (behavior) {
+      case 'padding':
+        return (
+          <View
+            ref={this.viewRef}
+            style={StyleSheet.compose(style, {paddingBottom: bottomHeight})}
+            onLayout={this._onLayout}
+            {...props}
+          >
+            {children}
+          </View>
+        )
+      default:
+        return super.render()
+    }
+  }
+}
+
+const StatusbarAwareKeyboardAvoidingView = (p: KeyboardAvoidingViewProps) => {
   const [statusBarHeight, setStatusBarHeight] = React.useState(0)
   const isTwoLineStatusBar = statusBarHeight === 40
   React.useEffect(() => {
@@ -27,11 +87,11 @@ const KeyboardAvoidingView = (p: KeyboardAvoidingViewProps) => {
   }, [])
 
   return (
-    <RnKBAV
+    <SplitAwareKeyboardAvoidingView
       {...p}
       keyboardVerticalOffset={(p?.keyboardVerticalOffset ?? 0) + (isTwoLineStatusBar ? 20 : 0)}
     />
   )
 }
 
-export default KeyboardAvoidingView
+export default StatusbarAwareKeyboardAvoidingView
