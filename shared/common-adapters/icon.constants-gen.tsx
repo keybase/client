e4baaaacbc13b6861afe4dde27e43c2bc25cb6e8
Jakob Weisblat <jakob@keyba.se>
2020-02-20 17:22:06 -0500

    wizard: add subteams page (#22677)
    * wizard: add subteams page
    
    * danny feedback

--- shared/common-adapters/icon.constants-gen.tsx
+++ shared/common-adapters/icon.constants-gen.tsx
@@ -530,6 +530,7 @@ export type IconType =
   | 'icon-illustration-teams-216'
   | 'icon-illustration-teams-80'
   | 'icon-illustration-teams-channels-460-96'
+  | 'icon-illustration-teams-subteams-460-96'
   | 'icon-illustration-welcome-96'
   | 'icon-illustration-zen-240-180'
   | 'icon-invite-link-48'
@@ -5721,6 +5722,14 @@ export const iconMeta: {[k in IconType]: IconMeta} = {
     isFont: false,
     get require() {
       return require('../images/icons/icon-illustration-teams-channels-460-96.png')
+    },
+  },
+  'icon-illustration-teams-subteams-460-96': {
+    extension: 'png',
+    imagesDir: 'icons',
+    isFont: false,
+    get require() {
+      return require('../images/icons/icon-illustration-teams-subteams-460-96.png')
     },
   },
   'icon-illustration-welcome-96': {
