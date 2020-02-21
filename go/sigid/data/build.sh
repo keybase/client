e1ae073b39d72f8d2b1d1cb8ab6ac1bf26dce0de
Maxwell Krohn <themax@gmail.com>
2020-02-21 08:54:26 -0500

    remove temp file

--- go/sigid/data/build.sh
+++ go/sigid/data/build.sh
@@ -2,10 +2,11 @@
 
 ALL="sigids-1.0.16"
 FIXES="sigid-fixes"
-OUT="nacl-1.0.16.txt"
+TMP=$(mktemp)
 
-grep -E '^h' $ALL > $OUT
-cat $FIXES | awk ' { print "g", $2 }' | sort >> $OUT
-iced3 mkdata.iced < $OUT > ../data.go
-iced3 mktest.iced < $OUT > ../data_test.go
+grep -E '^h' $ALL > $TMP
+cat $FIXES | awk ' { print "g", $2 }' | sort >> $TMP
+iced3 mkdata.iced < $TMP > ../data.go
+iced3 mktest.iced < $TMP > ../data_test.go
 (cd .. && go fmt .)
+rm $TMP
