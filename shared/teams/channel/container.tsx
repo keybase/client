c4ba3a4620a30728e830852d7bb382bbbf2d09a2
John Zila <john@keyba.se>
2020-02-24 15:22:27 -0800

    [Y2K-1279] Channels screen (#22636)
    * channels tab
    
    * wip
    
    * wip
    
    * thread through conversationIDKey
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * compiling
    
    * fix setSelectedTabs
    
    * not modal route
    
    * show header
    
    * more formatting
    
    * space between buttons and tabs
    
    * move activity
    
    * oops
    
    * fix setSelectedTabs again
    
    * PR feedback to launch channel editing modal
    
    * remove extraneous header styles
    
    * update to modern styling
    
    * clean up tabs a bit
    
    * remove extraneous header
    
    * PR feedback
    
    * nav to team when you click on the team name
    
    * remove some commented scaffolding
    
    * instead of using a Tab component which messes with the 'key' prop, just use a makeTab function
    
    * more PR feedback
    
    * remove as-yet unused scaffolding
    
    * fix storyshots
    
    * add tip
    
    * fix chatting weirdness with channel
    
    * fix
    
    * check onClick inside the function
    
    * remove silly bang
    
    * mobile header
    
    * storyshots
    
    * fix mobile header
    
    * revert flag

---
+++ shared/teams/channel/container.tsx
@@ -0,0 +1,97 @@
+import * as React from 'react'
+import * as RouteTreeGen from '../../actions/route-tree-gen'
+import * as Container from '../../util/container'
+import * as Constants from '../../constants/teams'
+import * as Types from '../../constants/types/teams'
+import * as ChatTypes from '../../constants/types/chat2'
+import {TabKey} from './tabs'
+import Channel, {Sections, TabProps} from '.'
+import makeRows from './rows'
+
+type RouteProps = Container.RouteProps<{teamID: Types.TeamID; conversationIDKey: ChatTypes.ConversationIDKey}>
+type OwnProps = TabProps & RouteProps
+
+// keep track during session
+const lastSelectedTabs: {[T: string]: TabKey} = {}
+const defaultTab: TabKey = 'members'
+
+const mapStateToProps = (state: Container.TypedState, ownProps: OwnProps) => {
+  const teamID = Container.getRouteProps(ownProps, 'teamID', '')
+  const conversationIDKey = Container.getRouteProps(ownProps, 'conversationIDKey', '')
+
+  return {
+    _channelInfo:
+      state.teams.teamIDToChannelInfos.get(teamID)?.get(conversationIDKey) ?? Constants.initialChannelInfo,
+    _yourOperations: Constants.getCanPerformByID(state, teamID),
+    _yourUsername: state.config.username,
+    conversationIDKey,
+    teamID,
+  }
+}
+
+const mapDispatchToProps = (dispatch: Container.TypedDispatch) => ({
+  onBack: () => dispatch(RouteTreeGen.createNavigateUp()),
+})
+
+const ConnectedChannel = Container.connect(
+  mapStateToProps,
+  mapDispatchToProps,
+  (stateProps, dispatchProps, ownProps) => {
+    const {conversationIDKey, teamID} = stateProps
+    const {selectedTab, setSelectedTab} = ownProps
+    const rows = makeRows(ownProps.selectedTab)
+    const sections: Sections = [
+      {data: [{key: 'header-inner', type: 'header' as const}], key: 'header'},
+      {data: rows, header: {key: 'tabs', type: 'tabs'}, key: 'body'},
+    ]
+    return {
+      conversationIDKey,
+      onBack: dispatchProps.onBack,
+      rows,
+      sections,
+      selectedTab,
+      setSelectedTab,
+      teamID,
+    }
+  }
+)(Channel)
+
+// Maintains the tab state.
+// Unfortunately this has to be in the container because it's interleaved with
+// connected state while providing its props to downstream components.
+const ConnectedChannelWithTabs = (props: RouteProps) => {
+  const teamID = Container.getRouteProps(props, 'teamID', Types.noTeamID)
+  const defaultSelectedTab = lastSelectedTabs[teamID] ?? defaultTab
+  const [selectedTab, _setSelectedTab] = React.useState<TabKey>(defaultSelectedTab)
+  const setSelectedTab = React.useCallback(
+    t => {
+      lastSelectedTabs[teamID] = t
+      _setSelectedTab(t)
+    },
+    [teamID, _setSelectedTab]
+  )
+  const prevTeamID = Container.usePrevious(teamID)
+  const prevSelectedTab = Container.usePrevious(selectedTab)
+  const dispatch = Container.useDispatch()
+
+  React.useEffect(() => {
+    if (teamID !== prevTeamID) {
+      setSelectedTab(defaultSelectedTab)
+    }
+  }, [teamID, prevTeamID, setSelectedTab, defaultSelectedTab])
+
+  React.useEffect(() => {
+    if (selectedTab !== prevSelectedTab && selectedTab === 'bots') {
+      // TODO: load bots here
+    }
+  }, [selectedTab, dispatch, teamID, prevSelectedTab])
+
+  return <ConnectedChannel {...props} selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
+}
+
+ConnectedChannelWithTabs.navigationOptions = () => ({
+  headerHideBorder: true,
+})
+
+export default ConnectedChannelWithTabs
+export type ChannelScreenType = React.ComponentType<OwnProps>
