fa936cefe5315edb6e6c423833ddb6313622982f
Joshua Blum <joshblum@keyba.se>
2020-02-25 15:33:24 -0500

    cmd conv-info (#22721)
    * sort cmds
    
    * conv info cmd

--- go/client/cmd_account.go
+++ go/client/cmd_account.go
@@ -4,6 +4,8 @@
 package client
 
 import (
+	"sort"
+
 	"github.com/keybase/cli"
 	"github.com/keybase/client/go/libcmdline"
 	"github.com/keybase/client/go/libkb"
@@ -21,6 +23,7 @@ func NewCmdAccount(cl *libcmdline.CommandLine, g *libkb.GlobalContext) cli.Comma
 		NewCmdAccountResetCancel(cl, g),
 	}
 	subcommands = append(subcommands, getBuildSpecificAccountCommands(cl, g)...)
+	sort.Sort(cli.ByName(subcommands))
 	return cli.Command{
 		Name:         "account",
 		Usage:        "Modify your account",
