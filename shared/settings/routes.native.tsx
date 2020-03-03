097e7df0bcf65762363a00010dce1968cd83be57
Chris Ball <chris@printf.net>
2020-03-02 22:54:42 -0800

    Start hooking up native settings subnav

--- shared/settings/routes.native.tsx
+++ shared/settings/routes.native.tsx
@@ -1,10 +1,16 @@
 import * as Constants from '../constants/settings'
 import * as Container from '../util/container'
+import * as Kb from '../common-adapters'
+import * as React from 'react'
+import {NavigationViewProps, createNavigator, StackRouter, SceneView} from '@react-navigation/core'
+import * as Shim from '../router-v2/shim'
 import AboutTab from './about-container'
 import AdvancedTab from './advanced'
 import ChatTab from './chat/container'
+import DevicesTab from '../devices/container'
 import DisplayTab from './display/container'
 import FsTab from './files/container'
+import GitTab from '../git/container'
 import WalletsTab from '../wallets/wallet/container'
 import FeedbackTab from './feedback/container'
 import InvitationsTab from './invites/container'
@@ -27,7 +33,9 @@ import PushPrompt from './notifications/push-prompt.native'
 import ContactsJoinedModal from './contacts-joined/index.native'
 import WhatsNewTab from '../whats-new/container'
 
-export const newRoutes = {
+const subRoutes = {
+  [Constants.devicesTab]: {getScreen: (): typeof DevicesTab => require('../devices/container').default},
+  [Constants.gitTab]: {getScreen: (): typeof GitTab => require('../git/container').default},
   [Constants.aboutTab]: {getScreen: (): typeof AboutTab => require('./about-container').default},
   [Constants.advancedTab]: {
     getScreen: (): typeof AdvancedTab => require('./advanced').default,
@@ -96,3 +104,42 @@ export const newModalRoutes = {
   },
   settingsVerifyPhone: {getScreen: (): typeof VerifyPhone => require('./account/add-modals').VerifyPhone},
 }
+
+const noScreenProps = {}
+class SettingsSubNav extends React.PureComponent<NavigationViewProps<any>> {
+  render() {
+    const navigation = this.props.navigation
+    const index = navigation.state.index
+    const activeKey = navigation.state.routes[index].key
+    const descriptor = this.props.descriptors[activeKey]
+    const childNav = descriptor.navigation
+
+    const Settings = require('./').default
+    return (
+      <Kb.Box2 direction="horizontal" fullHeight={true} fullWidth={true}>
+        <Settings routeSelected={descriptor.state.routeName}>
+          <SceneView
+            navigation={childNav}
+            component={descriptor.getComponent()}
+            screenProps={this.props.screenProps || noScreenProps}
+          />
+        </Settings>
+      </Kb.Box2>
+    )
+  }
+}
+const SettingsSubNavigator = createNavigator(
+  SettingsSubNav,
+  StackRouter(Shim.shim(subRoutes), {initialRouteName: Constants.accountTab}),
+  {}
+)
+
+SettingsSubNavigator.navigationOptions = {
+  title: 'More',
+}
+
+const tabletNewRoutes = {
+  settingsRoot: {screen: SettingsSubNavigator},
+}
+
+export const newRoutes = Container.isPhone ? subRoutes : tabletNewRoutes
