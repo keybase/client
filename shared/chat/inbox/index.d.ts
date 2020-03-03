4ce7f16578c014acda1e927644c42a68b969ab8c
Miles Steele <miles@milessteele.com>
2020-03-03 10:51:38 -0500

    iPad prettify selected BigTeamChannel row (#22808)
    * partial types
    
    * phone only
    
    * tighten selected BigTeamChannel
    
    * shuffle types
    
    * story, sort-keys
    
    * imports
    
    * move row to rowitem
    
    * go crazy

--- shared/chat/inbox/index.d.ts
+++ shared/chat/inbox/index.d.ts
@@ -1,65 +1,7 @@
 import * as React from 'react'
 import {ConversationIDKey} from '../../constants/types/chat2'
 import * as RPCChatTypes from '../../constants/types/rpc-chat-gen'
-
-export type RowItemSmall = {
-  type: 'small'
-  teamname: string
-  isTeam: boolean
-  conversationIDKey: ConversationIDKey
-  time: number
-  snippet?: string
-  snippetDecoration: RPCChatTypes.SnippetDecoration
-}
-export type RowItemBigTeamsLabel = {
-  type: 'bigTeamsLabel'
-  isFiltered: boolean
-  isTeam?: boolean
-  teamname?: never
-  conversationIDKey?: never
-  snippet?: string
-  snippetDecoration: RPCChatTypes.SnippetDecoration
-  time?: number
-}
-export type RowItemBigHeader = {
-  type: 'bigHeader'
-  isTeam?: boolean
-  teamname: string
-  teamID: string
-  conversationIDKey?: never
-  snippet?: string
-  snippetDecoration: RPCChatTypes.SnippetDecoration
-  time?: number
-}
-export type RowItemBig = {
-  type: 'big'
-  isTeam?: boolean
-  conversationIDKey: ConversationIDKey
-  teamname: string
-  channelname: string
-  snippet?: string
-  snippetDecoration: RPCChatTypes.SnippetDecoration
-  time?: number
-}
-export type RowItemDivider = {
-  conversationIDKey?: never
-  teamname?: never
-  showButton: boolean
-  type: 'divider'
-}
-export type RowItemTeamBuilder = {
-  conversationIDKey?: never
-  teamname?: never
-  type: 'teamBuilder'
-}
-
-export type RowItem =
-  | RowItemSmall
-  | RowItemBigTeamsLabel
-  | RowItemBigHeader
-  | RowItemBig
-  | RowItemDivider
-  | RowItemTeamBuilder
+import * as Types from '../../constants/types/chat2'
 
 export type Props = {
   allowShowFloatingButton: boolean
@@ -71,7 +13,7 @@ export type Props = {
   neverLoaded: boolean
   onNewChat: () => void
   onUntrustedInboxVisible: (conversationIDKeys: Array<ConversationIDKey>) => void
-  rows: Array<RowItem>
+  rows: Array<Types.ChatInboxRowItem>
   setInboxNumSmallRows: (rows: number) => void
   smallTeamsExpanded: boolean
   toggleSmallTeamsExpanded: () => void
