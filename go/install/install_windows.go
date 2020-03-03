df85fd8bd90ec387b751140ed4c94dd89c091090
Alex Gessner <alex@keyba.se>
2020-03-03 11:22:15 -0500

    delete the deprecated startup link file just in case it somehow exists

--- go/install/install_windows.go
+++ go/install/install_windows.go
@@ -261,8 +261,12 @@ func deprecatedStartupInfo(logFile *os.File) {
 	if appDataDir, err := libkb.AppDataDir(); err != nil {
 		logFile.WriteString("Error getting AppDataDir\n")
 	} else {
-		if exists, err := libkb.FileExists(filepath.Join(appDataDir, "Microsoft\\Windows\\Start Menu\\Programs\\Startup\\KeybaseStartup.lnk")); err == nil && exists == false {
+		autostartLinkPath := filepath.Join(appDataDir, "Microsoft\\Windows\\Start Menu\\Programs\\Startup\\KeybaseStartup.lnk")
+		if exists, err := libkb.FileExists(autostartLinkPath); err == nil && exists == false {
 			logFile.WriteString("  -- Service startup shortcut missing! --\n\n")
+		} else if exists == true {
+			// file exists. remove it because we have a new way of doing this.
+			_ = os.Remove(autostartLinkPath)
 		} else if err != nil {
 			k, err := registry.OpenKey(registry.CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\StartupApproved\\StartupFolder", registry.QUERY_VALUE|registry.READ)
 			if err != nil {
