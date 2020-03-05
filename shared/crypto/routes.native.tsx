b49461d298a6e61d81b6708a9f23df86743d5a7b
Jacob Young <jacob@jry.io>
2020-03-05 15:34:14 -0500

    WIP mobile sub nav

--- shared/crypto/routes.native.tsx
+++ shared/crypto/routes.native.tsx
@@ -1,2 +1,8 @@
-export const newRoutes = {}
+import NativeSubNav from 'render.native'
+
+export const newRoutes = {
+  cryptoRoot: {
+    getScreen: (): typeof NativeSubNav => require('./render.native').default,
+  },
+}
 export const newModalRoutes = {}
