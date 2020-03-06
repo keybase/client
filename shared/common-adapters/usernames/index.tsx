fe607e7998338328bea6fea87a39ebc4f5ea511b
Cori Hudson <cori@keyba.se>
2020-03-06 10:22:39 -0500

    fix: use provided text type in usernames component

--- shared/common-adapters/usernames/index.tsx
+++ shared/common-adapters/usernames/index.tsx
@@ -88,7 +88,7 @@ const UsernameText = (props: Props) => {
         const _onUsernameClicked = props.onUsernameClicked
         const isNegative = backgroundModeIsNegative(props.backgroundMode || null)
         const renderText = (onLongPress?: () => void) => (
-          <Text type="Body" key={u.username}>
+          <Text type={props.type} key={u.username}>
             {i !== 0 && i === props.users.length - 1 && props.showAnd && (
               <Text type={props.type} negative={isNegative} style={derivedJoinerStyle} underlineNever={true}>
                 {'and '}
