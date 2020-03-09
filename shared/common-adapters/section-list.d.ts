23d01b6ccd781822a426c851d4c42c36b732cb0b
Jakob Weisblat <jakob@keyba.se>
2020-03-09 15:51:06 -0400

    Channel page: rearchitect rows, tabs; add bots and attachments (#22942)
    * hookify team channels stuff, tear out rows/render and move things into index, add bots
    
    * ts
    
    * stories
    
    * make attaachments tab
    
    * feedback
    
    * jzila feedback
    
    * stories

--- shared/common-adapters/section-list.d.ts
+++ shared/common-adapters/section-list.d.ts
@@ -7,6 +7,15 @@ type DesktopProps = {
   selectedIndex: number | undefined
 }
 
+export interface SectionBase<ItemT> {
+  data: ReadonlyArray<ItemT>
+  key?: string
+  renderItem?: ({index: number, item: ItemT}) => React.ReactNode | null
+  // There exist mobile-only keyExtractor and ItemSeparatorComponent here, not included because I think they would create more confusion than usefulness and a mobile-only situation can import the native sectionlist anyway.
+}
+
+type PartialProps<ItemT> = {sections: Array<SectionBase<ItemT>>}
+
 // This resolves to 'any'
 // check https://facebook.github.io/react-native/docs/sectionlist#props for the time being
 // TODO import the type from react-native
