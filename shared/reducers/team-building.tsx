14bfb36e045a93b7e03a1e150819199b11a5508d
Daniel-Wang <danielw68@gmail.com>
2020-03-10 17:30:28 -0400

    Simplify further

--- shared/reducers/team-building.tsx
+++ shared/reducers/team-building.tsx
@@ -39,7 +39,7 @@ export const editTeambuildingDraft = (
     [TeamBuildingGen.searchResultsLoaded]: (draftState, action) => {
       const {query, service, users} = action.payload
       const results = mapGetEnsureValue(draftState.searchResults, query, new Map())
-      results.set(service, [...users])
+      results.set(service, users)
     },
     [TeamBuildingGen.finishTeamBuilding]: draftState => {
       draftState.error = ''
