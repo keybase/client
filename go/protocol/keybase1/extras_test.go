79203d327ddf0e2b06a5f16f1a51e212498f7d27
Antonio Marcedone <a.marcedone@gmail.com>
2020-03-11 10:42:40 -0400

    made test more strict

--- go/protocol/keybase1/extras_test.go
+++ go/protocol/keybase1/extras_test.go
@@ -33,27 +33,68 @@ func TestTime(t *testing.T) {
 	}
 }
 
+func Test0TimeConversion(t *testing.T) {
+	require.Equal(t, UnixTime(0), ToUnixTime(time.Time{}))
+	require.Equal(t, Time(0), ToTime(time.Time{}))
+	require.Equal(t, time.Time{}, FromUnixTime(UnixTime(0)))
+	require.Equal(t, time.Time{}, FromTime(Time(0)))
+}
+
 func TestTimeConversions(t *testing.T) {
+	const longForm = "Jan 2, 2006 at 3:04pm (MST)"
+	constTime, err := time.Parse(longForm, "Feb 3, 2013 at 7:54pm (PST)")
+	require.NoError(t, err)
+
+	// add an offset to test precision below a second
+	constTimeWithOffset := constTime.Add(5 * time.Nanosecond)
+
 	times := []time.Time{
 		time.Time{},
 		time.Now(),
 		time.Now().AddDate(1000, 0, 0),  // in a thousand years
 		time.Now().AddDate(10000, 0, 0), // in 10 thousand years
 		time.Now().AddDate(-3000, 0, 0), // 3 thousand years ago
+		constTime,
+		constTime.AddDate(1000, 0, 0),
+		constTime.AddDate(10000, 0, 0),
+		constTime.AddDate(-3000, 0, 0),
+		constTime.Add(12345678 * time.Second),
+		constTime.Add(3 * time.Millisecond),
+		constTime.Add(3 * time.Nanosecond),
+		constTimeWithOffset,
+		constTimeWithOffset.AddDate(1000, 0, 0),
+		constTimeWithOffset.AddDate(10000, 0, 0),
+		constTimeWithOffset.AddDate(-3000, 0, 0),
+		constTimeWithOffset.Add(12345678 * time.Second),
 	}
 
-	assertEqualTimes := func(t1, t2 time.Time) {
-		require.Equal(t, t1.Unix(), t2.Unix(), "expected %v and %v to be equal", t1, t2)
+	assertTimesEqualSec := func(t1, t2 time.Time) {
+		require.Equal(t, t1.Unix(), t2.Unix(), "expected %v and %v to be equal (with up to a second precision)", t1, t2)
+	}
+	assertTimesEqualMSec := func(t1, t2 time.Time) {
+		require.True(t, (t1.UnixNano()-t2.UnixNano()) < 1e6, "expected %v and %v to be equal (with up to a millisecond precision)", t1, t2)
+		require.True(t, (t2.UnixNano()-t1.UnixNano()) < 1e6, "expected %v and %v to be equal (with up to a millisecond precision)", t1, t2)
+	}
+	assertTimesEqualStrict := func(t1, t2 time.Time) {
+		require.True(t, t1.Equal(t2), "expected %v and %v to be equal", t1, t2)
 	}
 
 	for _, tm := range times {
 		kbTime := ToTime(tm)
 		tRev := FromTime(kbTime)
-		assertEqualTimes(tm, tRev)
+		// conversion to Time are only precise up to a millisecond
+		assertTimesEqualMSec(tm, tRev)
+		if tm.Nanosecond() == 0 {
+			assertTimesEqualStrict(tm, tRev)
+		}
 
 		kbUnixTime := ToUnixTime(tm)
 		tUnixRev := FromUnixTime(kbUnixTime)
-		assertEqualTimes(tm, tUnixRev)
+		// conversion to UnixTime are only precise up to a second
+		assertTimesEqualSec(tm, tUnixRev)
+		if tm.Nanosecond() == 0 {
+			assertTimesEqualStrict(tm, tUnixRev)
+		}
 	}
 }
 
