d175bb18eab2325ee72e629744e6dfa5fa3c32a0
Daniel-Wang <danielw68@gmail.com>
2020-03-16 16:47:28 -0400

    Change channel to store conversationIDKey instead of channel name

---
+++ shared/teams/delete-channel/index.tsx
@@ -0,0 +1,113 @@
+import * as React from 'react'
+import * as WaitingGen from '../../actions/waiting-gen'
+import * as Constants from '../../constants/teams'
+import * as Types from '../../constants/types/teams'
+import * as Kb from '../../common-adapters'
+import * as Container from '../../util/container'
+import * as Styles from '../../styles'
+
+export type Props = {
+  deleteWaiting: boolean
+  onBack: () => void
+  onDelete: () => void
+  teamID: Types.TeamID
+  teamname: string
+}
+
+const Header = (props: Props) => (
+  <>
+    <Kb.Avatar teamname={props.teamname} size={64} />
+    <Kb.Icon type="icon-team-delete-28" style={{marginRight: -60, marginTop: -20, zIndex: 1}} />
+    <Kb.Text style={styles.headerTeamname} type="BodySemibold">
+      {props.teamname}
+    </Kb.Text>
+  </>
+)
+
+export type CheckboxesProps = {
+  checkChats: boolean
+  checkFolder: boolean
+  checkNotify: boolean
+  onSetCheckChats: (checked: boolean) => void
+  onSetCheckFolder: (checked: boolean) => void
+  onSetCheckNotify: (checked: boolean) => void
+}
+
+const Checkboxes = (props: CheckboxesProps) => (
+  <Kb.Box2 direction="vertical">
+    <Kb.Checkbox
+      checked={props.checkChats}
+      label="Team chats will be lost"
+      onCheck={checked => props.onSetCheckChats(checked)}
+    />
+    <Kb.Checkbox
+      checked={props.checkFolder}
+      label="Data in the team folder will be lost"
+      onCheck={checked => props.onSetCheckFolder(checked)}
+    />
+    <Kb.Checkbox
+      checked={props.checkNotify}
+      label="Team members will be notified"
+      onCheck={checked => props.onSetCheckNotify(checked)}
+    />
+  </Kb.Box2>
+)
+
+const DeleteChannel = (props: Props) => {
+  const [checks, setChecks] = React.useState({
+    checkChats: false,
+    checkFolder: false,
+    checkNotify: false,
+  })
+  const {checkChats, checkFolder, checkNotify} = checks
+  const onCheck = (which: keyof typeof checks) => (enable: boolean) => setChecks({...checks, [which]: enable})
+  const disabled = !checkChats || !checkFolder || !checkNotify
+  const {deleteWaiting, onBack, teamID} = props
+  const error = Container.useAnyErrors(Constants.deleteTeamWaitingKey(props.teamID))
+  const prevDeleteWaiting = Container.usePrevious(deleteWaiting)
+  React.useEffect(() => {
+    if (prevDeleteWaiting !== undefined && !deleteWaiting && prevDeleteWaiting && !error) {
+      // Finished, nav up
+      onBack()
+    }
+  }, [deleteWaiting, prevDeleteWaiting, onBack, error])
+
+  const dispatch = Container.useDispatch()
+  React.useEffect(() => {
+    return () => {
+      dispatch(WaitingGen.createClearWaiting({key: Constants.deleteTeamWaitingKey(teamID)}))
+    }
+  }, [dispatch, teamID])
+
+  return (
+    <Kb.ConfirmModal
+      error={error ? error.message : ''}
+      confirmText="Delete team"
+      content={
+        <Checkboxes
+          checkChats={checkChats}
+          checkFolder={checkFolder}
+          checkNotify={checkNotify}
+          onSetCheckChats={onCheck('checkChats')}
+          onSetCheckFolder={onCheck('checkFolder')}
+          onSetCheckNotify={onCheck('checkNotify')}
+        />
+      }
+      description="This cannot be undone. By deleting the team, you agree that:"
+      header={<Header {...props} />}
+      onCancel={props.onBack}
+      onConfirm={disabled ? undefined : props.onDelete}
+      prompt={`Are you sure you want to delete ${props.teamname}?`}
+      waitingKey={Constants.deleteTeamWaitingKey(props.teamID)}
+    />
+  )
+}
+
+const styles = Styles.styleSheetCreate(
+  () =>
+    ({
+      headerTeamname: {color: Styles.globalColors.redDark, textDecorationLine: 'line-through'},
+    } as const)
+)
+
+export default DeleteChannel
--- shared/teams/team/rows/channel-row/channel.tsx
+++ shared/teams/team/rows/channel-row/channel.tsx
@@ -20,7 +20,7 @@ const ChannelRow = (props: ChannelRowProps) => {
   const isGeneral = channel.channelname === 'general'
 
   const selected = Container.useSelector(
-    state => !!state.teams.teamSelectedChannels.get(teamID)?.has(channel.channelname)
+    state => !!state.teams.teamSelectedChannels.get(teamID)?.has(channel.conversationIDKey)
   )
   const canPerform = Container.useSelector(state => Constants.getCanPerformByID(state, teamID))
   const canDelete = canPerform.deleteChannel
@@ -34,7 +34,7 @@ const ChannelRow = (props: ChannelRowProps) => {
   const dispatch = Container.useDispatch()
   const nav = Container.useSafeNavigation()
   const onSelect = (selected: boolean) => {
-    dispatch(TeamsGen.createSetChannelSelected({channel: channel.channelname, selected, teamID}))
+    dispatch(TeamsGen.createSetChannelSelected({channel: channel.conversationIDKey, selected, teamID}))
   }
   const onEditChannel = () =>
     dispatch(nav.safeNavigateAppendPayload({path: [{props, selected: 'chatEditChannel'}]}))
