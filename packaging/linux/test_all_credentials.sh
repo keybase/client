c8a7b12da13f0277e793128d208a430bb53a6abd
heronhaye <surya@keyba.se>
2020-03-16 21:09:02 -0400

    --wip-- [skip ci]

--- packaging/linux/test_all_credentials.sh
+++ packaging/linux/test_all_credentials.sh
@@ -17,8 +17,8 @@ echo "Checking credentials for s3://$bucket..."
 "$here/s3cmdwr" ls "s3://$bucket" > /dev/null
 
 echo 'Checking Arch AUR credentials...'
-git ls-remote aur:keybase-git > /dev/null
-git ls-remote aur:keybase-bin > /dev/null
+git ls-remote aur@aur.archlinux.org:keybase-git > /dev/null
+git ls-remote aur@aur.archlinux.org:keybase-bin > /dev/null
 
 echo 'Checking the GPG code signing key...'
 fingerprint="$(cat "$here/code_signing_fingerprint")"
