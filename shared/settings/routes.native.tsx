3c1236a3bfcbc4cf79327dfe10775db459255b03
chrisnojima <cnojima@keyba.se>
2020-03-19 21:32:20 -0400

    about

--- shared/settings/routes.native.tsx
+++ shared/settings/routes.native.tsx
@@ -1,6 +1,6 @@
 import * as Constants from '../constants/settings'
 import * as Container from '../util/container'
-import AboutTab from './about-container'
+import AboutTab from './about'
 import AdvancedTab from './advanced'
 import ChatTab from './chat/container'
 import DisplayTab from './display/container'
@@ -28,7 +28,7 @@ import ContactsJoinedModal from './contacts-joined/index.native'
 import WhatsNewTab from '../whats-new/container'
 
 export const newRoutes = {
-  [Constants.aboutTab]: {getScreen: (): typeof AboutTab => require('./about-container').default},
+  [Constants.aboutTab]: {getScreen: (): typeof AboutTab => require('./about').default},
   [Constants.advancedTab]: {
     getScreen: (): typeof AdvancedTab => require('./advanced').default,
   },
