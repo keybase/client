443a4787bc115012c908f92ed95712ee1d458433
Jakob Weisblat <jakob@keyba.se>
2020-03-20 13:57:30 -0400

    invite friends: make protocol

---
+++ go/protocol/keybase1/invite_friends.go
@@ -0,0 +1,84 @@
+// Auto-generated to Go types and interfaces using avdl-compiler v1.4.8 (https://github.com/keybase/node-avdl-compiler)
+//   Input file: avdl/keybase1/invite_friends.avdl
+
+package keybase1
+
+import (
+	"github.com/keybase/go-framed-msgpack-rpc/rpc"
+	context "golang.org/x/net/context"
+	"time"
+)
+
+type InviteCounts struct {
+	InviteCount      int     `codec:"inviteCount" json:"inviteCount"`
+	PercentageChange float32 `codec:"percentageChange" json:"percentageChange"`
+	ShowFire         bool    `codec:"showFire" json:"showFire"`
+}
+
+func (o InviteCounts) DeepCopy() InviteCounts {
+	return InviteCounts{
+		InviteCount:      o.InviteCount,
+		PercentageChange: o.PercentageChange,
+		ShowFire:         o.ShowFire,
+	}
+}
+
+type InvitePeopleArg struct {
+	Emails []EmailAddress   `codec:"emails" json:"emails"`
+	Phones []RawPhoneNumber `codec:"phones" json:"phones"`
+}
+
+type GetInviteCountsArg struct {
+}
+
+type InviteFriendsInterface interface {
+	InvitePeople(context.Context, InvitePeopleArg) error
+	GetInviteCounts(context.Context) (InviteCounts, error)
+}
+
+func InviteFriendsProtocol(i InviteFriendsInterface) rpc.Protocol {
+	return rpc.Protocol{
+		Name: "keybase.1.inviteFriends",
+		Methods: map[string]rpc.ServeHandlerDescription{
+			"invitePeople": {
+				MakeArg: func() interface{} {
+					var ret [1]InvitePeopleArg
+					return &ret
+				},
+				Handler: func(ctx context.Context, args interface{}) (ret interface{}, err error) {
+					typedArgs, ok := args.(*[1]InvitePeopleArg)
+					if !ok {
+						err = rpc.NewTypeError((*[1]InvitePeopleArg)(nil), args)
+						return
+					}
+					err = i.InvitePeople(ctx, typedArgs[0])
+					return
+				},
+			},
+			"getInviteCounts": {
+				MakeArg: func() interface{} {
+					var ret [1]GetInviteCountsArg
+					return &ret
+				},
+				Handler: func(ctx context.Context, args interface{}) (ret interface{}, err error) {
+					ret, err = i.GetInviteCounts(ctx)
+					return
+				},
+			},
+		},
+	}
+}
+
+type InviteFriendsClient struct {
+	Cli rpc.GenericClient
+}
+
+func (c InviteFriendsClient) InvitePeople(ctx context.Context, __arg InvitePeopleArg) (err error) {
+	err = c.Cli.Call(ctx, "keybase.1.inviteFriends.invitePeople", []interface{}{__arg}, nil, 0*time.Millisecond)
+	return
+}
+
+func (c InviteFriendsClient) GetInviteCounts(ctx context.Context) (res InviteCounts, err error) {
+	err = c.Cli.Call(ctx, "keybase.1.inviteFriends.getInviteCounts", []interface{}{GetInviteCountsArg{}}, &res, 0*time.Millisecond)
+	return
+}
