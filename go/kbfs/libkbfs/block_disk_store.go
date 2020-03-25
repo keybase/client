1b73112e19ad1e67f056aa6703dd732a36a2bba1
Jeremy Stribling <strib@alum.mit.edu>
2020-03-25 15:05:58 -0700

    block_disk_store: don't verify local block IDs in prod
    It's expensive, and doesn't really protect against anything but local
    disk corruption (which should be caught anyway during block
    decryption).
    
    Issue: HOTPOT-2024

--- go/kbfs/libkbfs/block_disk_store.go
+++ go/kbfs/libkbfs/block_disk_store.go
@@ -282,7 +282,7 @@ func (s *blockDiskStore) getDataExclusive(id kbfsblock.ID) (
 
 	// Check integrity.
 
-	err = kbfsblock.VerifyID(data, id)
+	err = verifyLocalBlockID(data, id)
 	if err != nil {
 		return nil, kbfscrypto.BlockCryptKeyServerHalf{}, err
 	}
