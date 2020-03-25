27c6bf3b82b680da0904734ccad2256cfd04d090
chrisnojima <cnojima@keyba.se>
2020-03-25 19:44:40 -0400

    correctly transform line heights

--- shared/common-adapters/markdown/index.desktop.tsx
+++ shared/common-adapters/markdown/index.desktop.tsx
@@ -1,1 +1,29 @@
-export {SimpleMarkdownComponent as default} from './shared'
+import * as React from 'react'
+import {SimpleMarkdownComponent} from './shared'
+import {Props} from '.'
+
+// We convert lint heights to px here
+const DesktopSimpleMarkdownComponent = (props: Props) => {
+  const {styleOverride, ...rest} = props
+
+  const fixedStyleOverride = React.useMemo(
+    () =>
+      styleOverride
+        ? Object.keys(styleOverride).reduce<Object>((o, k) => {
+            o[k] = {
+              ...styleOverride[k],
+            }
+            const old = styleOverride[k].lineHeight
+            if (old && typeof old === 'number') {
+              o[k].lineHeight = `${old}px`
+            }
+            return o
+          }, {})
+        : undefined,
+    [styleOverride]
+  )
+
+  return <SimpleMarkdownComponent {...rest} styleOverride={fixedStyleOverride} />
+}
+
+export default DesktopSimpleMarkdownComponent
