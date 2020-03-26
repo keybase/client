248e9d302753b34c60745533514b4965be1a195a
Song Gao <song@gao.io>
2020-03-25 17:42:48 -0700

    review feedback josh

--- go/service/incoming-share.go
+++ go/service/incoming-share.go
@@ -48,20 +48,20 @@ func (h *IncomingShareHandler) GetIncomingShareItems(ctx context.Context) (items
 	manifestPath := filepath.Join(h.G().Env.GetMobileSharedHome(), "Library", "Caches", "incoming-shares", "manifest.json")
 	f, err := os.Open(manifestPath)
 	if err != nil {
-		mctx.Error("incoming-share: open manifest.json error: %v", err)
+		mctx.Debug("incoming-share: open manifest.json error: %v", err)
 		return nil, err
 	}
 	defer f.Close()
 	var jsonItems []shareItemJSON
 	if err = json.NewDecoder(f).Decode(&jsonItems); err != nil {
-		mctx.Error("incoming-share: decode manifest.json error: %v", err)
+		mctx.Debug("incoming-share: decode manifest.json error: %v", err)
 		return nil, err
 	}
-	mctx.Info("incoming-share: %d items", len(jsonItems))
+	mctx.Debug("incoming-share: %d items", len(jsonItems))
 loop:
 	for _, jsonItem := range jsonItems {
 		if len(jsonItem.Error) > 0 {
-			mctx.Error("incoming-share: error: %s", jsonItem.Error)
+			mctx.Debug("incoming-share: error: %s", jsonItem.Error)
 			continue loop
 		}
 		var t keybase1.IncomingShareType
@@ -75,12 +75,12 @@ loop:
 		case "video":
 			t = keybase1.IncomingShareType_VIDEO
 		default:
-			mctx.Info("incoming-share: skippping unknown type: %v", jsonItem.Type)
+			mctx.Debug("incoming-share: skippping unknown type: %v", jsonItem.Type)
 			continue loop
 		}
 		fi, err := os.Stat(jsonItem.OriginalPath)
 		if err != nil {
-			mctx.Error("incoming-share: stat error on shared item: %v", err)
+			mctx.Debug("incoming-share: stat error on shared item: %v", err)
 			return nil, err
 		}
 		items = append(items, keybase1.IncomingShareItem{
