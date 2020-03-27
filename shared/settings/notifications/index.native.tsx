c9385e57f81d9048e84c5bedc4653e453ae77b54
chrisnojima <chrisnojima@gmail.com>
2020-03-27 10:25:47 -0400

    fix notification settings (#23311)

--- shared/settings/notifications/index.native.tsx
+++ shared/settings/notifications/index.native.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import * as Kb from '../../common-adapters/mobile.native'
+import * as Kb from '../../common-adapters'
 import * as Styles from '../../styles'
 import Notifications from './render'
 import TurnOnNotifications from './turn-on-notifications.native'
@@ -7,10 +7,10 @@ import {Props} from '.'
 
 const MobileNotifications = (props: Props) => (
   <Kb.HeaderHocWrapper title="Notifications" onBack={props.onBack}>
-    <Kb.NativeScrollView style={{...Styles.globalStyles.flexBoxColumn, flex: 1}}>
+    <Kb.ScrollView style={{...Styles.globalStyles.flexBoxColumn, flex: 1}}>
       {!props.mobileHasPermissions && <TurnOnNotifications />}
       <Notifications {...props} />
-    </Kb.NativeScrollView>
+    </Kb.ScrollView>
   </Kb.HeaderHocWrapper>
 )
 
