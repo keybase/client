aeddfd85310d85aede6da79107a29e8fe82a0876
chrisnojima <cnojima@keyba.se>
2020-03-28 15:52:03 -0400

    WIP

--- shared/ios/Keybase/AppDelegate.m
+++ shared/ios/Keybase/AppDelegate.m
@@ -81,7 +81,7 @@ static void InitializeFlipper(UIApplication *application) {
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
 #if DEBUG
- // InitializeFlipper(application);
+  InitializeFlipper(application);
 #endif
   // allow audio to be mixed
   [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryAmbient error:nil];
