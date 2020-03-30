8d7bc30f3cf64b293989972479c042f3edbf6da3
Danny Ayoub <danny.a95@gmail.com>
2020-03-30 13:12:20 -0400

    Integrate enable contacts modal in invite friends (#23316)
    * fix mobilepopup underlay in dark mode
    
    * organization; move useContacts and enable contacts modal to teams/common
    
    * integrate into invite friends by contacts
    
    * rename to useEnableContactsPopup
    
    * more renaming
    
    * storyshots
    
    * this can all go in EnableContactsPopup. add jsdoc
    
    * move Contact
    
    * init showingPopup to prop

--- shared/teams/add-members-wizard/index.stories.tsx
+++ shared/teams/add-members-wizard/index.stories.tsx
@@ -3,7 +3,6 @@ import * as Sb from '../../stories/storybook'
 import * as Container from '../../util/container'
 import * as Constants from '../../constants/teams'
 import AddFromWhere from './add-from-where'
-import EnableContacts from './enable-contacts'
 import AddEmail from './add-email'
 import AddPhone from './add-phone'
 import AddMembersConfirm from './confirm'
@@ -64,7 +63,6 @@ const load = () => {
     .addDecorator(story => <Sb.MockStore store={commonStore}>{story()}</Sb.MockStore>)
     .add('Add from where', () => <AddFromWhere {...Sb.createNavigator({})} />)
     .add('Add from where (new team)', () => <AddFromWhere {...fromWhereNewProps} />)
-    .add('Enable contacts', () => <EnableContacts onClose={Sb.action('onClose')} />)
     .add('Add by email', () => <AddEmail teamID={fakeTeamID} errorMessage="" />)
     .add('Add by phone', () => <AddPhone />)
 
