94dacc6fa1d606e8439cf6f3e354e3a560d39153
Mike Maxim <mike@keyba.se>
2020-03-30 21:32:49 -0400

    make GetMessages (and company) work if not a member of the conv (#23386)
    \

--- go/chat/search/indexer.go
+++ go/chat/search/indexer.go
@@ -541,7 +541,8 @@ func (idx *Indexer) reindexConv(ctx context.Context, rconv types.RemoteConversat
 
 	reason := chat1.GetThreadReason_INDEXED_SEARCH
 	if len(missingIDs) < idx.pageSize {
-		msgs, err := idx.G().ConvSource.GetMessages(ctx, rconv, idx.uid, missingIDs, &reason, nil)
+		msgs, err := idx.G().ConvSource.GetMessages(ctx, rconv.GetConvID(), idx.uid, missingIDs, &reason,
+			nil, false)
 		if err != nil {
 			if utils.IsPermanentErr(err) {
 				return 0, err
