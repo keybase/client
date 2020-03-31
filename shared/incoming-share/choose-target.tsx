2ad70ac59eb1637d5a18f78602f8d3dedd36230a
Song Gao <song@gao.io>
2020-03-31 13:14:09 -0700

    plumb the scaled size

--- shared/incoming-share/choose-target.tsx
+++ shared/incoming-share/choose-target.tsx
@@ -123,8 +123,13 @@ const getContentDescription = (props: Props) => {
 }
 
 const ChooseTarget = (props: Props) => {
-  const [useOriginal, setUseOriginal] = React.useState(false)
   const {onChat, onKBFS} = props
+  const originalTotalSize = props.items.reduce((bytes, item) => bytes + item.originalSize, 0)
+  const scaledTotalSize = props.items.reduce((bytes, item) => bytes + (item.scaledSize ?? 0), 0)
+  const offerScaled = scaledTotalSize > 0 && scaledTotalSize < originalTotalSize
+  const [useOriginalUserSelection, setUseOriginalUserSelection] = React.useState(false)
+  const useOriginal = !offerScaled || useOriginalUserSelection
+
   return !props.items.length ? (
     <Kb.Box2 direction="vertical" centerChildren={true} fullHeight={true}>
       <Kb.ProgressIndicator type="Large" />
@@ -144,22 +149,19 @@ const ChooseTarget = (props: Props) => {
           {getContentDescription(props)}
         </Kb.Box2>
 
-        {isAV(props.items[0].type) ? (
+        {isAV(props.items[0].type) && offerScaled ? (
           <Kb.Box2 direction="vertical" alignItems="flex-start">
             <Kb.RadioButton
               key="compress"
-              label="Compress image"
-              selected={!useOriginal}
-              onSelect={s => s && setUseOriginal(b => !b)}
+              label={`Compress image (${FsConstants.humanizeBytes(scaledTotalSize, 1)})`}
+              selected={!useOriginalUserSelection}
+              onSelect={s => s && setUseOriginalUserSelection(false)}
             />
             <Kb.RadioButton
               key="original"
-              label={`Keep full size (${FsConstants.humanizeBytes(
-                props.items.reduce((bytes, item) => bytes + item.originalSize, 0),
-                1
-              )})`}
-              selected={useOriginal}
-              onSelect={s => s && setUseOriginal(b => !b)}
+              label={`Keep full size (${FsConstants.humanizeBytes(originalTotalSize, 1)})`}
+              selected={useOriginalUserSelection}
+              onSelect={s => s && setUseOriginalUserSelection(true)}
             />
           </Kb.Box2>
         ) : null}
