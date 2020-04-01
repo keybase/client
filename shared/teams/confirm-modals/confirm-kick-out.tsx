933d6c9465ce4f30409df61fe6a94010998b2021
Danny Ayoub <danny.a95@gmail.com>
2020-04-01 13:53:05 -0400

    Team confirm modal style tweaks (#23426)
    * fix on really leave team
    
    * rework styles in confirm kick and tweak delete channel
    
    * tweak styles on confirm leave team
    
    * fix many many
    
    * team confirm modals storyshots

--- shared/teams/confirm-modals/confirm-kick-out.tsx
+++ shared/teams/confirm-modals/confirm-kick-out.tsx
@@ -69,16 +69,26 @@ const ConfirmKickOut = (props: Props) => {
     }
   }, [waiting, wasWaiting, onCancel])
 
-  const prompt = `Kick ${Constants.stringifyPeople(members)} out of ${teamname}?`
+  const prompt = (
+    <Kb.Text center={true} type="Header" style={styles.prompt}>
+      Kick {Constants.stringifyPeople(members)} out of {teamname}?
+    </Kb.Text>
+  )
   const header = (
     <Kb.Box style={styles.positionRelative}>
       <Kb.AvatarLine usernames={members} size={64} layout="horizontal" maxShown={5} />
-      <Kb.Icon
-        boxStyle={members.length <= 5 ? styles.iconContainerSingle : styles.iconContainer}
-        type="iconfont-block"
-        style={styles.headerIcon}
-        sizeType="Small"
-      />
+      <Kb.Box2
+        direction="horizontal"
+        centerChildren={true}
+        style={Styles.collapseStyles([styles.iconContainer, members.length > 5 && styles.iconContainerMany])}
+      >
+        <Kb.Icon
+          type="iconfont-block"
+          color={Styles.globalColors.white}
+          fontSize={14}
+          style={styles.headerIcon}
+        />
+      </Kb.Box2>
     </Kb.Box>
   )
   return (
@@ -86,21 +96,21 @@ const ConfirmKickOut = (props: Props) => {
       header={header}
       prompt={prompt}
       content={
-        <Kb.Box2 direction="vertical" gap="small">
-          <Kb.Text type="Body">
-            They will lose access to all the {teamname} chats and folders, and they won’t be able to get back
-            unless an admin invites them.
+        <Kb.Box2 direction="vertical" gap="small" fullWidth={true} style={styles.container}>
+          <Kb.Text type="Body" center={true}>
+            They will lose access to all the team chats and folders, and they won’t be able to get back unless
+            an admin invites them.
           </Kb.Text>
           {subteams.length != 0 && (
             <Kb.Checkbox
               checked={subteamsToo}
               onCheck={setSubteamsToo}
               labelComponent={
-                <Kb.Text type="Body">
-                  Also kick them out of all {teamname} subteams:{' '}
-                  <Kb.Text type="BodyBold">{subteams.join(', ')}</Kb.Text>
+                <Kb.Text type="Body" style={Styles.globalStyles.flexOne}>
+                  Also kick them out of all subteams: <Kb.Text type="BodyBold">{subteams.join(', ')}</Kb.Text>
                 </Kb.Text>
               }
+              style={Styles.globalStyles.fullWidth}
             />
           )}
         </Kb.Box2>
@@ -115,36 +125,30 @@ const ConfirmKickOut = (props: Props) => {
 export default ConfirmKickOut
 
 const styles = Styles.styleSheetCreate(() => ({
+  container: Styles.padding(0, Styles.globalMargins.small),
   headerIcon: Styles.platformStyles({
-    common: {
-      backgroundColor: Styles.globalColors.red,
-      borderColor: Styles.globalColors.white,
-      borderStyle: 'solid',
-      borderWidth: 3,
-      color: Styles.globalColors.white,
-      padding: 3,
-    },
-    isElectron: {
-      backgroundClip: 'padding-box',
-      borderRadius: 50,
-    },
-    isMobile: {
-      borderRadius: 18,
-      marginRight: -20,
-      marginTop: -30,
-    },
+    common: {position: 'relative'},
+    isElectron: {top: 1},
+    isMobile: {right: -0.5, top: 0.5},
   }),
   iconContainer: {
+    backgroundColor: Styles.globalColors.red,
+    borderColor: Styles.globalColors.white,
+    borderRadius: 12,
+    borderStyle: 'solid',
+    borderWidth: 3,
     bottom: -3,
+    height: 24,
+    overflow: 'hidden',
     position: 'absolute',
-    right: 20,
+    right: Styles.isMobile ? -24 : 0,
+    width: 24,
   },
-  iconContainerSingle: {
-    bottom: -3,
-    position: 'absolute',
-    right: 0,
+  iconContainerMany: {
+    right: Styles.isMobile ? 0 : 20,
   },
   positionRelative: {
     position: 'relative',
   },
+  prompt: Styles.padding(0, Styles.globalMargins.small, Styles.globalMargins.tiny),
 }))
