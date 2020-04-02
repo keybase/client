bb43e266516083bdecb0870d04240320ffaecd22
Daniel Ayoub <danny.a95@gmail.com>
2020-04-02 10:18:14 -0400

    hook up completion; use input for phone number reformatting

--- shared/teams/add-members-wizard/add-contacts.native.tsx
+++ shared/teams/add-members-wizard/add-contacts.native.tsx
@@ -2,9 +2,12 @@ import * as React from 'react'
 import * as Kb from '../../common-adapters'
 import * as Container from '../../util/container'
 import * as Styles from '../../styles'
+import * as TeamsGen from '../../actions/teams-gen'
+import * as RPCGen from '../../constants/types/rpc-gen'
 import {pluralize} from '../../util/string'
 import {ModalTitle} from '../common'
 import ContactsList, {useContacts, Contact, EnableContactsPopup} from '../common/contacts-list.native'
+import {formatPhoneNumber} from '../../util/phone-numbers'
 
 const AddContacts = () => {
   const dispatch = Container.useDispatch()
@@ -27,6 +30,31 @@ const AddContacts = () => {
     }
   }
 
+  const [waiting, setWaiting] = React.useState(false)
+  const toAssertionsRPC = Container.useRPC(RPCGen.userSearchBulkEmailOrPhoneSearchRpcPromise)
+  const onDone = () => {
+    setWaiting(true)
+    toAssertionsRPC(
+      [{emails: [...selectedEmails].join(','), phoneNumbers: [...selectedPhones]}],
+      r => {
+        if (r?.length) {
+          dispatch(
+            TeamsGen.createAddMembersWizardPushMembers({
+              members: r.map(m => ({
+                assertion: m.foundUser ? m.username : m.assertion,
+                note: m.foundUser ? formatPhoneNumber(m.input) : undefined,
+                role: 'writer',
+              })),
+            })
+          )
+        }
+      },
+      err => {
+        console.warn(err)
+      }
+    )
+  }
+
   const noneSelected = selectedPhones.size + selectedEmails.size === 0
 
   return (
@@ -36,8 +64,8 @@ const AddContacts = () => {
         hideBorder: true,
         leftButton: <Kb.Icon type="iconfont-arrow-left" onClick={onBack} />,
         rightButton: (
-          <Kb.Text type="BodyBigLink" style={noneSelected && Styles.globalStyles.opacity0}>
-            TODO
+          <Kb.Text type="BodyBigLink" onClick={onDone} style={noneSelected && Styles.globalStyles.opacity0}>
+            Done
           </Kb.Text>
         ),
         title: <ModalTitle teamID={teamID} title="Add members" />,
