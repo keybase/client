e113ed4ea895a3bfb79d20d105efe986d15f7498
Jakob Weisblat <jakob@keyba.se>
2020-04-02 17:51:18 -0400

    teams/main: update new/join buttons (#23450)
    * teams/main: update new/join buttons
    
    * add button
    
    * assets
    
    * danny feedback

--- shared/common-adapters/icon.constants-gen.tsx
+++ shared/common-adapters/icon.constants-gen.tsx
@@ -543,6 +543,7 @@ export type IconType =
   | 'icon-illustration-teams-180'
   | 'icon-illustration-teams-216'
   | 'icon-illustration-teams-80'
+  | 'icon-illustration-teams-96'
   | 'icon-illustration-teams-channels-460-96'
   | 'icon-illustration-teams-feature-profile-460-64'
   | 'icon-illustration-teams-subteams-460-96'
@@ -5889,6 +5890,14 @@ export const iconMeta: {[k in IconType]: IconMeta} = {
     isFont: false,
     get require() {
       return require('../images/icons/icon-illustration-teams-80.png')
+    },
+  },
+  'icon-illustration-teams-96': {
+    extension: 'png',
+    imagesDir: 'icons',
+    isFont: false,
+    get require() {
+      return require('../images/icons/icon-illustration-teams-96.png')
     },
   },
   'icon-illustration-teams-channels-460-96': {
