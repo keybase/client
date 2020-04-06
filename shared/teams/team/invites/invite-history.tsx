fcf915c1be4a8c3d3960fe5a327f49d9d14bd93d
Daniel Ayoub <danny.a95@gmail.com>
2020-04-06 17:15:49 -0400

    make sections

--- shared/teams/team/invites/invite-history.tsx
+++ shared/teams/team/invites/invite-history.tsx
@@ -4,6 +4,7 @@ import * as Styles from '../../../styles'
 import * as Container from '../../../util/container'
 import * as Constants from '../../../constants/teams'
 import * as Types from '../../../constants/types/teams'
+import {Section} from '../../../common-adapters/section-list'
 import {useTeamDetailsSubscribe} from '../../subscriber'
 import {ModalTitle} from '../../common'
 
@@ -12,14 +13,21 @@ type Props = Container.RouteProps<{teamID: Types.TeamID}>
 const InviteHistory = (props: Props) => {
   const teamID = Container.getRouteProps(props, 'teamID', Types.noTeamID)
   useTeamDetailsSubscribe(teamID)
-  const teamMeta = Container.useSelector(s => Constants.getTeamMeta(s, teamID))
   const teamDetails = Container.useSelector(s => Constants.getTeamDetails(s, teamID))
-  const loading = teamDetails.teamID === Types.noTeamID
+  const loading = teamDetails === Constants.emptyTeamDetails // TODO should be a better way to check this
 
   const dispatch = Container.useDispatch()
   const nav = Container.useSafeNavigation()
   const onClose = () => dispatch(nav.safeNavigateUpPayload())
   const onGenerate = () => {} // TODO
+
+  const {inviteLinks} = teamDetails
+  const sections: Array<Section<Types.InviteLink>> = [
+    {
+      data: [...inviteLinks],
+      key: 'invites',
+    },
+  ]
 
   return (
     <Kb.Modal
@@ -41,7 +49,7 @@ const InviteHistory = (props: Props) => {
       }}
       footer={{
         content: Styles.isMobile ? (
-          <Kb.Button fullWidth={true} label="Generate link" onClick={onGenerate} />
+          <Kb.Button fullWidth={true} mode="Secondary" label="Generate link" onClick={onGenerate} />
         ) : (
           <Kb.Button fullWidth={true} type="Dim" label="Close" onClick={onClose} />
         ),
@@ -50,8 +58,13 @@ const InviteHistory = (props: Props) => {
       onClose={onClose}
       allowOverflow={true}
       mode="DefaultFullHeight"
+      noScrollView={true}
     >
-      <Kb.Text type="HeaderBig">Hi</Kb.Text>
+      <Kb.SectionList
+        sections={sections}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => <Kb.Text type="Body">{item.url}</Kb.Text>}
+      />
     </Kb.Modal>
   )
 }
