3ef5e4057c83d457054c09382a5d4893285b464c
Mike Maxim <mike@keyba.se>
2020-04-07 23:30:59 -0400

    fix custom emojis on android

--- shared/common-adapters/custom-emoji.native.tsx
+++ shared/common-adapters/custom-emoji.native.tsx
@@ -1,30 +1,17 @@
 import * as React from 'react'
-import Image from './image'
-import {Box2} from './box'
-import Text from './text'
-import WithTooltip from './with-tooltip'
 import {Props} from './custom-emoji'
 import {FastImage} from './native-image.native'
-
-const Kb = {
-  Box2,
-  Image,
-  Text,
-  WithTooltip,
-}
 
 const CustomEmoji = (props: Props) => {
   const {size, src} = props
   return (
-    <Kb.Box2 direction="horizontal">
-      <FastImage
-        source={{uri: src}}
-        style={{
-          height: size,
-          width: size,
-        }}
-      />
-    </Kb.Box2>
+    <FastImage
+      source={{uri: src}}
+      style={{
+        height: size,
+        width: size,
+      }}
+    />
   )
 }
 
