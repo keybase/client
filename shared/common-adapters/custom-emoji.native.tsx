0275a10aff1c68e438b20d2688532c869e76c3c0
Adam Spooner <adam@keyba.se>
2020-04-08 15:40:38 -0400

    Wrap custom emoji to align them.

--- shared/common-adapters/custom-emoji.native.tsx
+++ shared/common-adapters/custom-emoji.native.tsx
@@ -1,18 +1,29 @@
 import * as React from 'react'
+import * as Styles from '../styles'
 import {Props} from './custom-emoji'
 import {FastImage} from './native-image.native'
+import {Box2} from './box'
 
 const CustomEmoji = (props: Props) => {
   const {size, src} = props
   return (
-    <FastImage
-      source={{uri: src}}
-      style={{
-        height: size,
-        width: size,
-      }}
-    />
+    <Box2 direction="vertical" style={styles.container}>
+      <FastImage
+        source={{uri: src}}
+        style={{
+          height: size,
+          width: size,
+        }}
+      />
+    </Box2>
   )
 }
+
+const styles = Styles.styleSheetCreate(() => ({
+  container: {
+    justifyContent: 'flex-end',
+    marginTop: Styles.globalMargins.tiny,
+  },
+}))
 
 export default CustomEmoji
