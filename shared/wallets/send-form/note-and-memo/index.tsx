5d69069f8ccb5b13b389fcbf296d74832517367d
Song Gao <song@gao.io>
2020-04-09 18:41:30 -0700

    disable custom emoji in wallet

--- shared/wallets/send-form/note-and-memo/index.tsx
+++ shared/wallets/send-form/note-and-memo/index.tsx
@@ -112,7 +112,7 @@ class SecretNote extends React.Component<SecretNoteProps, SecretNoteState> {
                 onHidden={() => this.setState({emojiPickerOpen: false})}
               >
                 <EmojiPickerDesktop
-                  disableCustomEmoji={false}
+                  disableCustomEmoji={true}
                   conversationIDKey={ChatConstants.noConversationIDKey}
                   onPickAction={this._insertEmoji}
                   onDidPick={this._emojiPickerToggle}
