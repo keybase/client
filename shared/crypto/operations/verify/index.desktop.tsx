93a2bd38ef20b7b806b3ac7fc0d986ad730991a6
Jacob Young <jacob@jry.io>
2020-04-13 13:08:06 -0400

    Constrain crypto input/output on desktop to not shrink (#23697)

--- shared/crypto/operations/verify/index.desktop.tsx
+++ shared/crypto/operations/verify/index.desktop.tsx
@@ -7,13 +7,13 @@ import {OperationOutput, SignedSender, OutputActionsBar} from '../../output'
 const operation = Constants.Operations.Verify
 
 export const VerifyInput = () => (
-  <>
+  <Kb.Box2 direction="vertical" fullHeight={true} style={Constants.inputDesktopMaxHeight}>
     <OperationBanner operation={operation} />
     <Input operation={operation} />
-  </>
+  </Kb.Box2>
 )
 export const VerifyOutput = () => (
-  <Kb.Box2 direction="vertical" fullHeight={true}>
+  <Kb.Box2 direction="vertical" fullHeight={true} style={Constants.outputDesktopMaxHeight}>
     <SignedSender operation={operation} />
     <OperationOutput operation={operation} />
     <OutputActionsBar operation={operation} />
