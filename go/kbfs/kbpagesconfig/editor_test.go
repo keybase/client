e8741c7dd5e66c870ef8799ad0a59883e860adaa
Song Gao <song@gao.io>
2020-04-13 22:05:21 -0700

    fix test

--- go/kbfs/kbpagesconfig/editor_test.go
+++ go/kbfs/kbpagesconfig/editor_test.go
@@ -42,15 +42,13 @@ func TestEditorACLs(t *testing.T) {
 
 	ctx := context.Background()
 
-	// add user
+	t.Logf("it's an empty config now so authentication should fail")
 	editor, err = newKBPConfigEditorWithPrompter(
 		configDir, prompter)
 	require.NoError(t, err)
-	// It's an empty config now so authentication should fail.
 	ok := editor.kbpConfig.Authenticate(ctx, "alice", "12345")
 	require.False(t, ok)
-	// Try adding a user "alice" with password "12345" and "bob" with password
-	// "54321".
+	t.Logf(`try adding a user "alice" with password "12345" and "bob" with password "54321"`)
 	nextResponse <- "n"
 	nextResponse <- "12345"
 	err = editor.setUser("alice", true)
@@ -62,7 +60,6 @@ func TestEditorACLs(t *testing.T) {
 	nextResponse <- "y"
 	err = editor.confirmAndWrite()
 	require.NoError(t, err)
-	// Re-read the config file and make sure the user is added properly.
 	editor, err = newKBPConfigEditorWithPrompter(
 		configDir, prompter)
 	require.NoError(t, err)
@@ -71,7 +68,7 @@ func TestEditorACLs(t *testing.T) {
 	ok = editor.kbpConfig.Authenticate(ctx, "bob", "54321")
 	require.True(t, ok)
 
-	// remove "bob"
+	t.Logf("remove bob")
 	editor, err = newKBPConfigEditorWithPrompter(
 		configDir, prompter)
 	require.NoError(t, err)
@@ -90,32 +87,17 @@ func TestEditorACLs(t *testing.T) {
 	ok = editor.kbpConfig.Authenticate(ctx, "alice", "12345")
 	require.True(t, ok)
 
-	// set anonymous permissions
+	t.Logf("default permission for root is read")
 	editor, err = newKBPConfigEditorWithPrompter(
 		configDir, prompter)
 	require.NoError(t, err)
-	// We don't have any permission set, so we should get the default read,list
-	// for root.
 	read, list, _, _, _, err := editor.kbpConfig.GetPermissions("/", nil)
-	require.NoError(t, err)
-	require.True(t, read)
-	require.True(t, list)
-	err = editor.setAnonymousPermission("read", "/")
-	require.NoError(t, err)
-	nextResponse <- "y"
-	err = editor.confirmAndWrite()
-	require.NoError(t, err)
-	// Re-read the config file and make sure the user is gone.
-	editor, err = newKBPConfigEditorWithPrompter(
-		configDir, prompter)
-	require.NoError(t, err)
-	read, list, _, _, _, err = editor.kbpConfig.GetPermissions("/", nil)
 	require.NoError(t, err)
 	require.True(t, read)
 	require.False(t, list)
 
 	alice := "alice"
-	// grant alice additional permissions
+	t.Logf("grant alice additional permissions")
 	editor, err = newKBPConfigEditorWithPrompter(
 		configDir, prompter)
 	require.NoError(t, err)
@@ -129,7 +111,6 @@ func TestEditorACLs(t *testing.T) {
 	nextResponse <- "y"
 	err = editor.confirmAndWrite()
 	require.NoError(t, err)
-	// Re-read the config file and make sure the user is gone.
 	editor, err = newKBPConfigEditorWithPrompter(
 		configDir, prompter)
 	require.NoError(t, err)
@@ -138,6 +119,21 @@ func TestEditorACLs(t *testing.T) {
 	require.NoError(t, err)
 	require.True(t, read)
 	require.True(t, list)
+
+	t.Logf("grant anonymous list permissions as well")
+	err = editor.setAnonymousPermission("read,list", "/")
+	require.NoError(t, err)
+	nextResponse <- "y"
+	err = editor.confirmAndWrite()
+	require.NoError(t, err)
+	editor, err = newKBPConfigEditorWithPrompter(
+		configDir, prompter)
+	require.NoError(t, err)
+	read, list, _, _, _, err = editor.kbpConfig.GetPermissions("/", nil)
+	require.NoError(t, err)
+	require.True(t, read)
+	require.True(t, list)
+
 }
 
 func TestEditorSimple(t *testing.T) {
