3605f5d27eb249d55ae54c9ea1275e42ca09e6f9
Daniel Ayoub <danny.a95@gmail.com>
2020-04-15 14:50:16 -0400

    WIP don't directly nav to team builder

--- shared/teams/team/menu-container.tsx
+++ shared/teams/team/menu-container.tsx
@@ -8,7 +8,6 @@ import * as RouteTreeGen from '../../actions/route-tree-gen'
 import * as TeamsGen from '../../actions/teams-gen'
 import * as Kb from '../../common-adapters'
 import flags from '../../util/feature-flags'
-import {appendNewTeamBuilder} from '../../actions/typed-routes'
 import capitalize from 'lodash/capitalize'
 import * as Styles from '../../styles'
 
@@ -39,7 +38,7 @@ const mapStateToProps = (state: Container.TypedState, {teamID}: OwnProps) => {
 }
 
 const mapDispatchToProps = (dispatch: Container.TypedDispatch, {teamID}: OwnProps) => ({
-  onAddOrInvitePeople: () => dispatch(appendNewTeamBuilder(teamID)),
+  onAddOrInvitePeople: () => dispatch(TeamsGen.createStartAddMembersWizard({teamID})),
   onCopyInviteLink: () => {}, // TODO
   onCreateSubteam: () => dispatch(TeamsGen.createLaunchNewTeamWizardOrModal({subteamOf: teamID})),
   onDeleteTeam: () =>
