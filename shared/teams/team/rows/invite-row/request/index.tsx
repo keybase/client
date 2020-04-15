5922da814a263dd726ee57f578971260e927870c
M Ember Mou <mmou@keyba.se>
2020-04-14 20:28:17 -0700

    fix

--- shared/teams/team/rows/invite-row/request/index.tsx
+++ shared/teams/team/rows/invite-row/request/index.tsx
@@ -227,10 +227,6 @@ const styles = Styles.styleSheetCreate(() => ({
     isMobile: {
       marginTop: Styles.globalMargins.tiny,
     },
-    isTablet: {
-      justifyContent: 'flex-start',
-      width: '100%',
-    },
   }),
   icon: {
     marginLeft: Styles.globalMargins.small,
