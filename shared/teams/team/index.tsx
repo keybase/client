5e766a6e5992634d00a8d349ba8901c5c8029132
Daniel Ayoub <danny.a95@gmail.com>
2020-04-16 15:28:09 -0400

    fix header animation

--- shared/teams/team/index.tsx
+++ shared/teams/team/index.tsx
@@ -82,6 +82,7 @@ const useLoadFeaturedBots = (teamDetails: Types.TeamDetails, shouldLoad: boolean
 const SectionList: typeof Kb.SectionList = Styles.isMobile
   ? Kb.ReAnimated.createAnimatedComponent(Kb.SectionList)
   : Kb.SectionList
+
 const Team = (props: Props) => {
   const teamID = Container.getRouteProps(props, 'teamID', Types.noTeamID)
   const initialTab = Container.getRouteProps(props, 'initialTab', undefined)
@@ -152,10 +153,12 @@ const Team = (props: Props) => {
   }
 
   // Animation
-  const offset = Styles.isMobile ? new Kb.ReAnimated.Value(0) : undefined
-  const onScroll = Styles.isMobile
-    ? Kb.ReAnimated.event([{nativeEvent: {contentOffset: {y: offset}}}], {useNativeDriver: true})
-    : undefined
+  const offset = React.useRef(Styles.isMobile ? new Kb.ReAnimated.Value(0) : undefined)
+  const onScroll = React.useRef(
+    Styles.isMobile
+      ? Kb.ReAnimated.event([{nativeEvent: {contentOffset: {y: offset.current}}}], {useNativeDriver: true})
+      : undefined
+  )
 
   const renderSectionHeader = React.useCallback(
     ({section}) =>
@@ -171,14 +174,14 @@ const Team = (props: Props) => {
 
   const body = (
     <Kb.Box style={styles.container}>
-      {Styles.isMobile && flags.teamsRedesign && <MobileHeader teamID={teamID} offset={offset} />}
+      {Styles.isMobile && flags.teamsRedesign && <MobileHeader teamID={teamID} offset={offset.current} />}
       <SectionList
         renderSectionHeader={renderSectionHeader}
         stickySectionHeadersEnabled={Styles.isMobile}
         sections={sections}
         contentContainerStyle={styles.listContentContainer}
         style={styles.list}
-        onScroll={onScroll}
+        onScroll={onScroll.current}
       />
       <SelectionPopup
         selectedTab={
@@ -219,12 +222,12 @@ Team.navigationOptions = flags.teamsRedesign
     })
 
 const startAnimationOffset = 40
+const AnimatedBox2 = Styles.isMobile ? Kb.ReAnimated.createAnimatedComponent(Kb.Box2) : undefined
 const MobileHeader = ({teamID, offset}: {teamID: Types.TeamID; offset: any}) => {
   const meta = Container.useSelector(s => Constants.getTeamMeta(s, teamID))
   const dispatch = Container.useDispatch()
   const nav = Container.useSafeNavigation()
   const onBack = () => dispatch(nav.safeNavigateUpPayload())
-  const AnimatedBox2 = Kb.ReAnimated.createAnimatedComponent(Kb.Box2)
   const top = Kb.ReAnimated.interpolate(offset, {
     inputRange: [-9999, startAnimationOffset, startAnimationOffset + 40, 99999999],
     outputRange: [40, 40, 0, 0],
