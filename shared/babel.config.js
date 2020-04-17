76c91751654a9de846b771b23e7537fc297a0cc2
chrisnojima <chrisnojima@gmail.com>
2020-04-17 14:50:33 -0400

    React native storyshots (#23781)

--- shared/babel.config.js
+++ shared/babel.config.js
@@ -1,34 +1,27 @@
 // Cache in the module. This can get called from multiple places and env vars can get lost
 let isElectron = null
 let isReactNative = null
+let isTest = null
 
 module.exports = function(api /*: any */) {
-  if (api.env() === 'test') {
-    return {
-      plugins: [
-        '@babel/plugin-proposal-optional-catch-binding',
-        '@babel/plugin-proposal-nullish-coalescing-operator',
-        '@babel/plugin-proposal-optional-chaining',
-        '@babel/plugin-proposal-object-rest-spread',
-        '@babel/transform-flow-strip-types',
-        '@babel/plugin-proposal-class-properties',
-      ],
-      presets: [
-        ['@babel/preset-env', {targets: {node: 'current'}}],
-        '@babel/preset-react',
-        '@babel/preset-typescript',
-      ],
-    }
-  }
+  const apiEnv = api.env()
 
-  api.caller(c => {
-    console.log('KB: Babel config detected caller: ', c && c.name)
-    if (!c || c.name === 'metro') {
-      isReactNative = true
-    } else {
-      isElectron = true
-    }
-  })
+  if (apiEnv === 'test') {
+    isTest = true
+    isElectron = true
+  } else if (apiEnv === 'test-rn') {
+    isTest = true
+    isReactNative = true
+  } else {
+    api.caller(c => {
+      console.error('KB: Babel config detected caller: ', c, c && c.name, api.env())
+      if (!c || c.name === 'metro') {
+        isReactNative = true
+      } else {
+        isElectron = true
+      }
+    })
+  }
 
   api.cache(true)
 
@@ -50,7 +43,11 @@ module.exports = function(api /*: any */) {
         '@babel/transform-flow-strip-types',
         '@babel/plugin-proposal-class-properties',
       ],
-      presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+      presets: [
+        isTest ? ['@babel/preset-env', {targets: {node: 'current'}}] : '@babel/preset-env',
+        '@babel/preset-react',
+        '@babel/preset-typescript',
+      ],
     }
   } else if (isReactNative) {
     console.error('KB babel.config.js for ReactNative')
--- shared/common-adapters/image.native.tsx
+++ shared/common-adapters/image.native.tsx
@@ -3,11 +3,16 @@ import {NativeImage} from './native-image.native'
 import {Props, ReqProps} from './image'
 
 const Image = ({onLoad, src, style}: Props) => (
-  <NativeImage onLoad={onLoad} source={{uri: src}} style={style} resizeMode="contain" />
+  <NativeImage
+    onLoad={onLoad}
+    source={{uri: __STORYSHOT__ ? 'shotsrc' : src}}
+    style={style}
+    resizeMode="contain"
+  />
 )
 
 const RequireImage = ({src, style}: ReqProps) => (
-  <NativeImage source={src} resizeMode="contain" style={style} />
+  <NativeImage source={__STORYSHOT__ ? 'shotsrc' : src} resizeMode="contain" style={style} />
 )
 
 export default Image
