38d52d87c1a4a56c1fe95766f44c570a7e0ebf00
Antonio Marcedone <AMarcedone@users.noreply.github.com>
2020-04-21 14:10:25 -0400

    do not accept invite links with inconsistent etime (#23871)
    * do not accept invite links with inconsistent etime
    
    * feedback

--- go/teams/teams.go
+++ go/teams/teams.go
@@ -354,6 +354,14 @@ func (t *Team) UserLastJoinTime(u keybase1.UserVersion) (time keybase1.Time, err
 	return t.chain().GetUserLastJoinTime(u)
 }
 
+// UserLastRoleChangeTime returns the time of the last role change for user
+// in team. If the user left the team as a last change, the time of such leave
+// event is returned. If the user was never in the team, then this function
+// returns time=0 and wasMember=false.
+func (t *Team) UserLastRoleChangeTime(u keybase1.UserVersion) (time keybase1.Time, wasMember bool) {
+	return t.chain().GetUserLastRoleChangeTime(u)
+}
+
 func (t *Team) Members() (keybase1.TeamMembers, error) {
 	var members keybase1.TeamMembers
 
