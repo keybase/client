23603cc86a0818b6f644ffe3841bb5ca2f83951a
Joshua Blum <jblum18@gmail.com>
2020-04-22 12:39:07 -0400

    buffered logging for service, revendor

--- go/keybase/main.go
+++ go/keybase/main.go
@@ -47,6 +47,9 @@ func keybaseExit(exitCode int) {
 }
 
 func main() {
+	logger.EnableBufferedLogging()
+	defer logger.Shutdown()
+
 	// Preserve non-critical errors that happen very early during
 	// startup, where logging is not set up yet, to be printed later
 	// when logging is functioning.
