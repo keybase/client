e72696ef8e26f9f887a50d4d61c8607bce1d6d93
Miles Steele <miles@milessteele.com>
2020-04-29 11:12:39 -0400

    weird cast

--- shared/actions/tracker2.tsx
+++ shared/actions/tracker2.tsx
@@ -7,6 +7,7 @@ import * as RouteTreeGen from './route-tree-gen'
 import * as Saga from '../util/saga'
 import * as Container from '../util/container'
 import * as Constants from '../constants/tracker2'
+import * as ProfileConstants from '../constants/profile'
 import {WebOfTrustVerificationType} from '../constants/types/more'
 import * as RPCTypes from '../constants/types/rpc-gen'
 import logger from '../logger'
@@ -150,7 +151,11 @@ const loadWebOfTrustEntries = async (
         proofID: entry.vouchProof,
         proofs: entry.proofs ?? undefined,
         status: entry.status,
-        verificationType: (entry.confidence.usernameVerifiedVia || 'none') as WebOfTrustVerificationType, // xxx this is an incorrect cast. Go does not gaurantee this match.
+        verificationType: (ProfileConstants.choosableWotVerificationTypes.find(
+          x => x === entry.confidence.usernameVerifiedVia
+        )
+          ? entry.confidence.usernameVerifiedVia
+          : 'none') as WebOfTrustVerificationType,
         vouchedAt: entry.vouchedAt,
       })) || []
     return Tracker2Gen.createUpdateWotEntries({
