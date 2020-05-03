f57ccbbc49a9c73cf757e713fd2812d99a2b9255
Michał Zochniak <michal@zapu.net>
2020-05-03 14:25:40 +0200

    Generalize add-member-wizard stories

--- shared/teams/add-members-wizard/index.stories.tsx
+++ shared/teams/add-members-wizard/index.stories.tsx
@@ -2,81 +2,62 @@ import * as React from 'react'
 import * as Sb from '../../stories/storybook'
 import * as Container from '../../util/container'
 import * as Constants from '../../constants/teams'
+import * as Types from '../../constants/types/teams'
 import AddFromWhere from './add-from-where'
 import AddEmail from './add-email'
 import AddPhone from './add-phone'
 import AddMembersConfirm from './confirm'
 
 const fakeTeamID = 'fakeTeamID'
-const commonStore = Container.produce(Sb.createStoreWithCommon(), draftState => {
-  draftState.teams = {
-    ...draftState.teams,
-    addMembersWizard: {
-      ...Constants.addMembersWizardEmptyState,
-      addingMembers: [
-        {assertion: 'ayoubd', role: 'writer'},
-        {assertion: 'max', role: 'writer'},
-        {assertion: '+12015550123@phone', role: 'writer'},
-        {assertion: '[chris@chris.chris]@email', role: 'writer'},
-        {assertion: 'mlsteeele', role: 'writer'},
-        {assertion: 'karenm', role: 'writer'},
-        {assertion: 'mikem', role: 'writer'},
-        {assertion: 'patrickxb', role: 'writer'},
-        {assertion: 'jakob223', role: 'writer'},
-      ],
-      teamID: fakeTeamID,
-    },
-    teamMeta: new Map([[fakeTeamID, {...Constants.emptyTeamMeta, teamname: 'greenpeace.board'}]]),
-  }
-  draftState.config = {
-    ...draftState.config,
-    username: 'andonuts',
-  }
-  draftState.settings.phoneNumbers.defaultCountry = 'FR'
-})
-const emailsOnlyStore = Container.produce(Sb.createStoreWithCommon(), draftState => {
-  draftState.teams = {
-    ...draftState.teams,
-    addMembersWizard: {
-      ...Constants.addMembersWizardEmptyState,
-      addingMembers: [
-        {assertion: '[danny@danny.danny]@email', role: 'writer'},
-        {assertion: '[max@max.max]@email', role: 'writer'},
-        {assertion: '[mike@mike.mike]@email', role: 'writer'},
-        {assertion: '[chris@chris.chris]@email', role: 'writer'},
-      ],
-      teamID: fakeTeamID,
-    },
-    teamMeta: new Map([[fakeTeamID, {...Constants.emptyTeamMeta, teamname: 'greenpeace.board'}]]),
-  }
-  draftState.config = {
-    ...draftState.config,
-    username: 'andonuts',
-  }
-  draftState.settings.phoneNumbers.defaultCountry = 'FR'
-})
-const emailsWithDuplicates = Container.produce(Sb.createStoreWithCommon(), draftState => {
-  draftState.teams = {
-    ...draftState.teams,
-    addMembersWizard: {
-      ...Constants.addMembersWizardEmptyState,
-      addingMembers: [
-        {assertion: '[danny@danny.danny]@email', role: 'writer'},
-        {assertion: '[max@max.max]@email', role: 'writer'},
-        {assertion: 'zapu', resolvedFrom: '[michal@zapu.zapu]@email', role: 'writer'},
-        {assertion: '+48784123123@phone', role: 'writer'},
-      ],
-      teamID: fakeTeamID,
-      membersAlreadyInTeam: ['[mike@mike.mike]@email', '[chris@chris.chris]@email', '+12015550123@phone'],
-    },
-    teamMeta: new Map([[fakeTeamID, {...Constants.emptyTeamMeta, teamname: 'greenpeace.board'}]]),
-  }
-  draftState.config = {
-    ...draftState.config,
-    username: 'andonuts',
-  }
-  draftState.settings.phoneNumbers.defaultCountry = 'FR'
-})
+function makeCommonStore(addingMembers: Types.AddingMember[], membersAlreadyInTeam?: string[]) {
+  return Container.produce(Sb.createStoreWithCommon(), draftState => {
+    draftState.teams = {
+      ...draftState.teams,
+      addMembersWizard: {
+        ...Constants.addMembersWizardEmptyState,
+        addingMembers,
+        teamID: fakeTeamID,
+        membersAlreadyInTeam: membersAlreadyInTeam ?? [],
+      },
+      teamMeta: new Map([[fakeTeamID, {...Constants.emptyTeamMeta, teamname: 'greenpeace.board'}]]),
+    }
+    draftState.config = {
+      ...draftState.config,
+      username: 'andonuts',
+    }
+    draftState.settings.phoneNumbers.defaultCountry = 'FR'
+  })
+}
+
+const commonStore = makeCommonStore([
+  {assertion: 'ayoubd', role: 'writer'},
+  {assertion: 'max', role: 'writer'},
+  {assertion: '+12015550123@phone', role: 'writer'},
+  {assertion: '[chris@chris.chris]@email', role: 'writer'},
+  {assertion: 'mlsteeele', role: 'writer'},
+  {assertion: 'karenm', role: 'writer'},
+  {assertion: 'mikem', role: 'writer'},
+  {assertion: 'patrickxb', role: 'writer'},
+  {assertion: 'jakob223', role: 'writer'},
+])
+
+const emailsOnlyStore = makeCommonStore([
+  {assertion: '[danny@danny.danny]@email', role: 'writer'},
+  {assertion: '[max@max.max]@email', role: 'writer'},
+  {assertion: '[mike@mike.mike]@email', role: 'writer'},
+  {assertion: '[chris@chris.chris]@email', role: 'writer'},
+])
+
+const emailsWithDuplicates = makeCommonStore(
+  [
+    {assertion: '[danny@danny.danny]@email', role: 'writer'},
+    {assertion: '[max@max.max]@email', role: 'writer'},
+    {assertion: 'zapu', resolvedFrom: '[michal@zapu.zapu]@email', role: 'writer'},
+    // {assertion: 'test', resolvedFrom: '+12125451231@phone', role: 'writer'},
+    {assertion: '+48784123123@phone', role: 'writer'},
+  ],
+  ['[mike@mike.mike]@email', '[chris@chris.chris]@email', '+12015550123@phone']
+)
 
 const load = () => {
   Sb.storiesOf('Teams/Add member wizard', module)
@@ -86,22 +67,18 @@ const load = () => {
     .add('Add by email', () => <AddEmail teamID={fakeTeamID} errorMessage="" />)
     .add('Add by phone', () => <AddPhone />)
 
-  Sb.storiesOf('Teams/Add member wizard/Confirm', module)
-    .add('Mixed types', () => (
-      <Sb.MockStore store={commonStore}>
+  const sb = Sb.storiesOf('Teams/Add member wizard/Confirm', module)
+  for (let [name, store] of [
+    ['Mixed types', commonStore],
+    ['All emails', emailsOnlyStore],
+    ['Members already in team', emailsWithDuplicates],
+  ] as const) {
+    sb.add(name, () => (
+      <Sb.MockStore store={store}>
         <AddMembersConfirm />
       </Sb.MockStore>
     ))
-    .add('All emails', () => (
-      <Sb.MockStore store={emailsOnlyStore}>
-        <AddMembersConfirm />
-      </Sb.MockStore>
-    ))
-    .add('Members already in team', () => (
-      <Sb.MockStore store={emailsWithDuplicates}>
-        <AddMembersConfirm />
-      </Sb.MockStore>
-    ))
+  }
 }
 
 export default load
