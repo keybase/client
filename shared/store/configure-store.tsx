e32860f366494ecf97bc41677811cca10962d819
chrisnojima <cnojima@keyba.se>
2020-05-05 20:27:26 -0400

    useWatchActions

--- shared/store/configure-store.tsx
+++ shared/store/configure-store.tsx
@@ -98,12 +98,33 @@ if (__DEV__) {
 
 const freezeMiddleware = _store => next => action => next(Object.freeze(action))
 
+// allow useWatchActions
+const hookMiddleware = () => next => action => {
+  _hookCBs.forEach(cb => cb(action))
+  return next(action)
+}
+
+type HookCB = (a: any) => void
+
+const _hookCBs: Array<HookCB> = []
+/** Register a useWatchActions hook */
+export const registerHookMiddleware = (cb: HookCB) => {
+  _hookCBs.push(cb)
+  return () => {
+    const idx = _hookCBs.indexOf(cb)
+    if (idx !== -1) {
+      _hookCBs.splice(idx, 1)
+    }
+  }
+}
+
 const middlewares = [
   errorCatching,
   sagaMiddleware,
   ...(__DEV__ ? [freezeMiddleware] : []),
   ...(enableStoreLogging && loggerMiddleware ? [loggerMiddleware] : []),
   ...(enableActionLogging ? [actionLogger] : []),
+  hookMiddleware,
 ]
 
 if (__DEV__ && typeof window !== 'undefined') {
