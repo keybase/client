b2fa9e0ad48b2311aaff1590292cb325286ccf88
chrisnojima <cnojima@keyba.se>
2020-05-06 10:38:54 -0400

    WIP

--- shared/util/use-watch-actions.tsx
+++ shared/util/use-watch-actions.tsx
@@ -3,7 +3,7 @@ import {TypedActions} from '../actions/typed-actions-gen'
 import {registerHookMiddleware} from '../store/configure-store'
 
 type CBType = (act: TypedActions) => void
-/** A hook to listen to redux actions.  setResult is only called if you're still mounted
+/** A hook to listen to redux actions.  watcher is only called if you're still mounted
   @param watcher?: A function which is called when an action is fired
  */
 function useWatchActions(watcher?: CBType) {
