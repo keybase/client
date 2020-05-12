904f2cfdda799e43a270f5f793c6686c7c8f7cc0
Jakob Weisblat <jakob@keyba.se>
2020-05-12 14:06:39 -0400

    Kb.Tabs QA (#24087)
    * tab bar: update, simplify
    
    * danny feedback
    
    * stories

--- shared/common-adapters/tabs.tsx
+++ shared/common-adapters/tabs.tsx
@@ -7,6 +7,7 @@ import Icon, {IconType} from './icon'
 import ProgressIndicator from './progress-indicator'
 import Text from './text'
 import {Box, Box2} from './box'
+import ScrollView from './scroll-view'
 import capitalize from 'lodash/capitalize'
 
 const Kb = {
@@ -17,6 +18,7 @@ const Kb = {
   Divider,
   Icon,
   ProgressIndicator,
+  ScrollView,
   Text,
 }
 
@@ -28,13 +30,13 @@ export type Tab<TitleT extends string> = {
 }
 
 type Props<TitleT extends string> = {
-  clickableBoxStyle?: Styles.StylesCrossPlatform
   tabs: Array<Tab<TitleT>>
   onSelect: (title: TitleT) => void
   selectedTab?: TitleT
   style?: Styles.StylesCrossPlatform
   tabStyle?: Styles.StylesCrossPlatform
   showProgressIndicator?: boolean
+  mobileTabModeOverride?: 'scroll' | 'distribute'
 }
 
 const TabText = ({selected, text}: {selected: boolean; text: string}) => (
@@ -45,38 +47,67 @@ const TabText = ({selected, text}: {selected: boolean; text: string}) => (
   </Kb.Box2>
 )
 
-const Tabs = <TitleT extends string>(props: Props<TitleT>) => (
-  <Kb.Box2
-    direction="horizontal"
-    style={Styles.collapseStyles([styles.container, props.style])}
-    alignItems="flex-start"
-    fullWidth={true}
-  >
-    {props.tabs.map((tab: Tab<TitleT>) => {
-      const selected = props.selectedTab === tab.title
-      return (
-        <Kb.ClickableBox
-          onClick={() => props.onSelect(tab.title)}
-          key={tab.title}
-          style={props.clickableBoxStyle}
-        >
-          <Kb.Box2 direction="vertical" style={styles.tabContainer} fullWidth={true}>
-            <Kb.Box style={Styles.collapseStyles([styles.tab, selected && styles.selected, props.tabStyle])}>
-              {tab.icon ? (
-                <Kb.Icon type={tab.icon} style={selected ? styles.iconSelected : styles.icon} />
-              ) : (
-                <TabText selected={selected} text={tab.text ?? capitalize(tab.title)} />
-              )}
-              {!!tab.badgeNumber && <Kb.Badge badgeNumber={tab.badgeNumber} badgeStyle={styles.badge} />}
-            </Kb.Box>
-            <Kb.Divider style={selected ? styles.dividerSelected : styles.divider} />
-          </Kb.Box2>
-        </Kb.ClickableBox>
-      )
-    })}
-    {props.showProgressIndicator && <Kb.ProgressIndicator style={styles.progressIndicator} />}
-  </Kb.Box2>
-)
+const Tabs = <TitleT extends string>(props: Props<TitleT>) => {
+  const mobileTabMode = Styles.isMobile
+    ? props.mobileTabModeOverride ?? (props.tabs.length > 3 ? 'scroll' : 'distribute')
+    : undefined
+  const tabContent = (
+    <Kb.Box2
+      direction="horizontal"
+      style={Styles.collapseStyles([
+        styles.container,
+        mobileTabMode !== 'scroll' && Styles.globalStyles.flexOne,
+        mobileTabMode !== 'scroll' && styles.borderBottom,
+        props.style,
+      ])}
+      alignItems="flex-start"
+      fullWidth={true}
+    >
+      {props.tabs.map((tab: Tab<TitleT>) => {
+        const selected = props.selectedTab === tab.title
+        return (
+          <Kb.ClickableBox
+            onClick={() => props.onSelect(tab.title)}
+            key={tab.title}
+            style={Styles.collapseStyles([
+              styles.clickableBoxStyle,
+              mobileTabMode === 'distribute' ? styles.mobileDistribute : styles.mobileScroll,
+            ])}
+          >
+            <Kb.Box2 direction="vertical" fullHeight={true} fullWidth={true}>
+              <Kb.Box2
+                style={Styles.collapseStyles([styles.tab, selected && styles.selected, props.tabStyle])}
+                direction="horizontal"
+                centerChildren={true}
+              >
+                {tab.icon ? (
+                  <Kb.Icon type={tab.icon} style={selected ? styles.iconSelected : styles.icon} />
+                ) : (
+                  <TabText selected={selected} text={tab.text ?? capitalize(tab.title)} />
+                )}
+                {!!tab.badgeNumber && <Kb.Badge badgeNumber={tab.badgeNumber} badgeStyle={styles.badge} />}
+              </Kb.Box2>
+              <Kb.Divider style={selected ? styles.dividerSelected : styles.divider} />
+            </Kb.Box2>
+          </Kb.ClickableBox>
+        )
+      })}
+      {props.showProgressIndicator && <Kb.ProgressIndicator style={styles.progressIndicator} />}
+    </Kb.Box2>
+  )
+  return mobileTabMode === 'scroll' ? (
+    <Kb.ScrollView
+      horizontal={true}
+      contentContainerStyle={Styles.collapseStyles([styles.minWidthFull, styles.borderBottom])}
+      alwaysBounceHorizontal={false}
+      showsHorizontalScrollIndicator={false}
+    >
+      {tabContent}
+    </Kb.ScrollView>
+  ) : (
+    tabContent
+  )
+}
 
 const styles = Styles.styleSheetCreate(() => ({
   badge: Styles.platformStyles({
@@ -88,23 +119,35 @@ const styles = Styles.styleSheetCreate(() => ({
       marginTop: 1,
     },
   }),
+  borderBottom: {borderBottomColor: Styles.globalColors.black_10, borderBottomWidth: 1},
+  clickableBoxStyle: Styles.platformStyles({
+    isElectron: {
+      flexShrink: 1,
+      height: 40,
+      width: 120,
+    },
+    isMobile: {
+      height: 48,
+    },
+  }),
   container: {
-    borderBottomColor: Styles.globalColors.black_10,
-    borderBottomWidth: 1,
+    backgroundColor: Styles.globalColors.white,
     borderStyle: 'solid',
-    flex: 1,
-    maxHeight: Styles.isMobile ? 48 : 40,
+    marginTop: Styles.globalMargins.tiny,
   },
   divider: {
     ...Styles.globalStyles.flexBoxRow,
     backgroundColor: Styles.globalColors.transparent,
     minHeight: 2,
   },
-  dividerSelected: {
-    ...Styles.globalStyles.flexBoxRow,
-    backgroundColor: Styles.globalColors.blue,
-    minHeight: 2,
-  },
+  dividerSelected: Styles.platformStyles({
+    common: {
+      ...Styles.globalStyles.flexBoxRow,
+      backgroundColor: Styles.globalColors.blue,
+      minHeight: 2,
+    },
+    isMobile: {minHeight: 3},
+  }),
   icon: {
     alignSelf: 'center',
   },
@@ -112,6 +155,9 @@ const styles = Styles.styleSheetCreate(() => ({
     alignSelf: 'center',
     color: Styles.globalColors.black,
   },
+  minWidthFull: {minWidth: '100%'},
+  mobileDistribute: Styles.platformStyles({isMobile: {flex: 1}}),
+  mobileScroll: Styles.platformStyles({isMobile: {width: 120}}),
   progressIndicator: {
     height: 17,
     width: 17,
@@ -121,19 +167,7 @@ const styles = Styles.styleSheetCreate(() => ({
   },
   tab: {
     flex: 1,
-    paddingBottom: Styles.globalMargins.xtiny,
-    paddingLeft: Styles.globalMargins.small,
-    paddingRight: Styles.globalMargins.small,
-    paddingTop: Styles.globalMargins.small,
   },
-  tabContainer: Styles.platformStyles({
-    isElectron: {
-      height: 40,
-    },
-    isMobile: {
-      height: 48,
-    },
-  }),
   tabTextContainer: {justifyContent: 'center'},
 }))
 
