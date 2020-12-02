51e0c1762435ad8619b8fadeab5812f42911c035
Song Gao <song@gao.io>
2020-12-01 16:10:17 -0800

    kbfuse 4.0.4

--- osx/Fuse/build.sh
+++ osx/Fuse/build.sh
@@ -5,45 +5,55 @@ set -e -u -o pipefail # Fail on error
 dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd "$dir"
 
-rm -rf osxfuse
+rm -rf osxfuse macfuse
 
 version=${VERSION:?"Need to set VERSION for Fuse"}
-if [ "$version" = "3.10.0" ]; then
-  tar -xjf /keybase/team/keybase.builds.macos/osxfuse/osxfuse-3.10.0.tbz
-  mv osxfuse-3.10.0 osxfuse
+if [ "$version" = "4.0.4" ]; then
+  tar -xjf /Volumes/Keybase/team/keybase.builds.macos/macfuse/macfuse-4.0.4-src.tbz
+  mv macfuse-4.0.4-src macfuse
 else
   branch="osxfuse-$version"
   # Checkout
   git clone --recursive -b "$branch" git://github.com/osxfuse/osxfuse.git osxfuse
 fi
 
-# Patch osxfuse to turn it into kbfuse
+# Patch macfuse to turn it into kbfuse
 ./patch.sh
 
 # Compile
 rm -rf /tmp/kbfuse*
-cd osxfuse
+cd macfuse
 # If you get an error compiling you might have to run `brew link gettext --force` (see https://github.com/osxfuse/osxfuse/issues/149).
 # build for 10.11, and have osxfuse builder symlink other versions.
-./build.sh -v 5 -t fsbundle -- -s 10.11 -d 10.11 --kext=10.11 --kext="10.12->10.11" --kext="10.13->10.11" --kext="10.14->10.11" --kext="10.15->10.11"
+./build.sh -v 5 --target=filesystembundle -- \
+  --sdk=11.0 --deployment-target=11.0 --kext=11.0 --kext="11.1->11.0"
+./build.sh -v 5 --target=filesystembundle -- \
+  --sdk=10.14 --deployment-target=10.14 --kext=10.14 --kext="10.15->10.14"
 
 cd $dir
 rm -rf kbfuse.bundle
-ditto /tmp/kbfuse/fsbundle/kbfuse.fs kbfuse.bundle
+ditto /tmp/kbfuse/filesystembundle/kbfuse.fs kbfuse.bundle
 
-# Backup the fsbundle directory in case we need debug symbols later
-cd /tmp/kbfuse/fsbundle
+# manually add 11.0 stuff in since the last built target was 10.14 and the
+# resulting bundle doesn't have 11.0
+ditto /tmp/kbfuse/kernelextension-11.0 kbfuse.bundle/Contents/Extensions/11.0
+pushd kbfuse.bundle/Contents/Extensions && ln -s 11.0 11.1 && popd
+
+# Backup the filesystembundle directory in case we need debug symbols later
+cd /tmp/kbfuse/filesystembundle
 tar zcvpf $dir/fsbundle.tgz  .
 
 # Sign the kext
 cd $dir
-codesign --verbose --timestamp --options runtime --sign "Developer ID Application: Keybase, Inc." kbfuse.bundle/Contents/Extensions/10.11/kbfuse.kext
+codesign --verbose --timestamp --options runtime --sign "Developer ID Application: Keybase, Inc." kbfuse.bundle/Contents/Extensions/11.0/kbfuse.kext
+codesign --verbose --timestamp --options runtime --sign "Developer ID Application: Keybase, Inc." kbfuse.bundle/Contents/Extensions/10.14/kbfuse.kext
 codesign --verbose --timestamp --options runtime --sign "Developer ID Application: Keybase, Inc." kbfuse.bundle/Contents/Resources/mount_kbfuse
 codesign --verbose --timestamp --options runtime --sign "Developer ID Application: Keybase, Inc." kbfuse.bundle/Contents/Resources/load_kbfuse
 codesign --verbose --force --deep --timestamp --options runtime --sign "Developer ID Application: Keybase, Inc." kbfuse.bundle
 
 # Verify
-codesign --verbose --verify kbfuse.bundle/Contents/Extensions/10.11/kbfuse.kext
+codesign --verbose --verify kbfuse.bundle/Contents/Extensions/10.14/kbfuse.kext
+codesign --verbose --verify kbfuse.bundle/Contents/Extensions/11.0/kbfuse.kext
 codesign --verbose --verify kbfuse.bundle/Contents/Resources/mount_kbfuse
 codesign --verbose --verify kbfuse.bundle/Contents/Resources/load_kbfuse
 codesign --verbose --verify kbfuse.bundle
--- osx/Fuse/rename.sh
+++ osx/Fuse/rename.sh
@@ -5,7 +5,7 @@ set -e # Fail on error
 dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $dir
 
-cd osxfuse
+cd macfuse
 
 files=("*.c" "*.h" "*.m" "*Info.plist" "*version.plist" "*.pbxproj" "*.sh" "*.am" "*.ac" "*.d" "*.in")
 for i in "${files[@]}"
@@ -14,10 +14,12 @@ echo "Files: $i"
 find . -name "$i" -type f
 find . -name "$i" -type f -exec sed -i '' s/osxfuse/kbfuse/g {} +
 find . -name "$i" -type f -exec sed -i '' s/OSXFUSE/KBFUSE/g {} +
+find . -name "$i" -type f -exec sed -i '' s/macfuse/kbfuse/g {} +
+find . -name "$i" -type f -exec sed -i '' s/MACFUSE/KBFUSE/g {} +
 done
 
-find . -type d -name '*osxfuse*' -exec sh -c 'mv {} $(echo {} | sed -e 's/osxfuse/kbfuse/g')' \;
-find . -name '*osxfuse*' -exec sh -c 'mv {} $(echo {} | sed -e 's/osxfuse/kbfuse/g')' \; || true
+find . -type d -name '*macfuse*' -exec sh -c 'mv {} $(echo {} | sed -e 's/macfuse/kbfuse/g')' \;
+find . -name '*macfuse*' -exec sh -c 'mv {} $(echo {} | sed -e 's/macfuse/kbfuse/g')' \; || true
 
 echo "OK"
 
