81a8784ed15b931ad62439ef76fa3d1cb291dd8a
Joshua Blum <jblum18@gmail.com>
2021-11-18 12:29:43 -0500

    src

--- update_gvm.sh
+++ update_gvm.sh
@@ -3,6 +3,7 @@
 echo "reinstalling gvm"
 rm -rf /home/jenkins/.gvm # nuke old install
 bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+source /home/jenkins/.gvm/scripts/gvm
 gvm update 
 gvm install go1.17.3 -B 
 gvm use go1.17.3 --default
