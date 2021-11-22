5f1a5228ecc8d591a77240e593d64d495f01ed91
Joshua Blum <jblum18@gmail.com>
2021-11-22 17:35:39 -0500

    x

--- packaging/slack/send.sh
+++ packaging/slack/send.sh
@@ -2,7 +2,14 @@
 
 set -e -u -o pipefail # Fail on error
 
+# Outputs to slack if you have slackbot installed and SLACK_TOKEN and
+# SLACK_CHANNEL set. This is primarily for build boxes.
+
+sender="$GOPATH/src/github.com/keybase/slackbot/send/main.go"
 echo "$@"
+if [ -f $sender ]; then
+  (cd $(dirname sender) && go run $sender -i=1 "$@")
+fi
 
 # send to keybase chat if we have it in the environment
 convid=${KEYBASE_CHAT_CONVID:-}
