3aa7dbd3c60de9a7f7d991218f40179aec1e768f
chrisnojima <cnojima@keyba.se>
2022-01-21 10:06:51 -0500

    WIP

---
+++ shared/common-adapters/skip-animations.js
@@ -0,0 +1,9 @@
+// used by babel.config so has to be a js file
+const skipAnimations = true
+module.exports = skipAnimations
+
+if (skipAnimations) {
+  console.log(
+    '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n SKIPPING ANIMATIONS\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n'
+  )
+}
