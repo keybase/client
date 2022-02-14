49c49bb7ba858310d0ebf164220d7df841a6d4e6
chrisnojima <cnojima@keyba.se>
2022-02-14 10:14:56 -0500

    fix path

--- shared/ios/Keybase/AppDelegate.m
+++ shared/ios/Keybase/AppDelegate.m
@@ -124,7 +124,7 @@
   
   // This is a mildly hacky solution to mock out some code when we're in storybook mode.
   // The code that handles this is in `shared/metro.config.js`.
-  NSString *bundlerURL = IS_STORYBOOK ? @"storybook-index" : @"normal-index";
+  NSString *bundlerURL = IS_STORYBOOK ? @"storybook-index" : @"index";
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:bundlerURL fallbackResource:nil];
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
