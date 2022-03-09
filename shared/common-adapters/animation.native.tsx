fd13a417848afff4e1cd2e6f5fa60e2be307d1eb
chrisnojima <cnojima@keyba.se>
2022-03-09 15:05:30 -0500

    WIP

--- shared/common-adapters/animation.native.tsx
+++ shared/common-adapters/animation.native.tsx
@@ -1,37 +1,53 @@
 import * as React from 'react'
 import LottieView from 'lottie-react-native'
-import Animated, {useSharedValue, useAnimatedProps, withTiming} from 'react-native-reanimated'
+import ReAnimated, {useSharedValue, useAnimatedProps, withTiming, withRepeat} from 'react-native-reanimated'
 import Box from './box'
 import type {Props} from './animation'
+import {useDepChangeEffect} from '../util/container'
 
-const AnimatedLottieView = Animated.createAnimatedComponent(LottieView)
+const AnimatedLottieView = ReAnimated.createAnimatedComponent(LottieView)
 
-const Animation = (props: Props) => {
-  const dataRef = React.useRef()
+const getDuration = (o: {ip: number; op: number; fr: number}) => {
+  const {ip, op, fr} = o
+  const duration = ((op - ip) / fr) * 1000
+  return duration
+}
+
+const AnimationNew = React.memo((props: Props) => {
+  const {animationType} = props
+  const dataRef = React.useRef<any>()
   if (!dataRef.current) {
     dataRef.current = require('./animation-data.json')
   }
+
+  const source = React.useRef(dataRef.current?.[animationType])
+  const duration = React.useRef(getDuration(source.current))
+  useDepChangeEffect(() => {
+    const data = dataRef.current?.[animationType]
+    source.current = data
+    duration.current = getDuration(source.current)
+  }, [animationType])
+
   const progress = useSharedValue(0)
   const animatedProps = useAnimatedProps(() => {
-    return {
-      progress: progress.value,
-    }
+    return {progress: progress.value}
   })
 
   React.useEffect(() => {
-    progress.value = withTiming(1)
+    progress.value = withRepeat(withTiming(1, {duration: duration.current}), -1)
   }, [progress])
+
   return (
     <Box style={props.containerStyle}>
       <AnimatedLottieView
         autoPlay={true}
         loop={true}
-        source={dataRef.current[props.animationType]}
+        source={source.current}
         animatedProps={animatedProps}
         style={props.style}
       />
     </Box>
   )
-}
+})
 
-export default Animation
+export default AnimationNew
