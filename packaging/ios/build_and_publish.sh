0be86a2b580954d9abda2f0a5fbbba83ed9ae4b2
chrisnojima <cnojima@keyba.se>
2022-03-22 09:52:52 -0400

    stop dirty files from stopping a checkout

--- packaging/ios/build_and_publish.sh
+++ packaging/ios/build_and_publish.sh
@@ -43,6 +43,7 @@ if [ -n "$kbfs_commit" ]; then
   cd "$kbfs_dir"
   echo "Checking out $kbfs_commit on kbfs (will reset to $kbfs_branch)"
   git fetch
+  git reset --hard
   git clean -f
   git checkout "$kbfs_commit"
   # tell gobuild.sh (called via "yarn run rn-gobuild-ios" below) to use our local commit
@@ -57,6 +58,7 @@ if [ -n "$client_commit" ]; then
   cd "$client_dir"
   echo "Checking out $client_commit on client (will reset to $client_branch)"
   git fetch
+  git reset --hard
   git clean -f
   git checkout "$client_commit"
 else
