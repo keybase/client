18053ba5a55db3267e7f5f77df652dacf465e606
chrisnojima <cnojima@keyba.se>
2022-04-05 17:06:54 -0400

    keep unselected tabs mounted but hidden

--- shared/router-v2/left-tab-navigator.desktop.tsx
+++ shared/router-v2/left-tab-navigator.desktop.tsx
@@ -4,34 +4,57 @@ import * as Kb from '../common-adapters'
 import TabBar from './tab-bar.desktop'
 import {useNavigationBuilder, TabRouter, createNavigatorFactory} from '@react-navigation/core'
 
-function LeftTabNavigator({initialRouteName, children, screenOptions, backBehavior}) {
+type Props = Parameters<typeof useNavigationBuilder>[1]
+
+const LeftTabNavigator = React.memo(({initialRouteName, children, screenOptions}: Props) => {
   const {state, navigation, descriptors, NavigationContent} = useNavigationBuilder(TabRouter, {
-    backBehavior,
     children,
     initialRouteName,
     screenOptions,
   })
+
+  const renderedRef = React.useRef({})
+  // render if its been rendered before
+  const shouldRender = React.useCallback(
+    (key: string, selected: boolean) => {
+      let should = renderedRef.current[key]
+      if (should) {
+        return true
+      }
+      if (selected) {
+        renderedRef.current[key] = true
+        return true
+      }
+      return false
+    },
+    [renderedRef]
+  )
 
   return (
     <NavigationContent>
       <Kb.Box2 direction="horizontal" fullHeight={true} fullWidth={true} style={styles.box}>
         <TabBar state={state} navigation={navigation} />
         <Kb.BoxGrow>
-          {state.routes.map((route, i) => {
-            return i === state.index ? (
-              <Kb.Box2 key={route.key} direction="vertical" fullHeight={true} fullWidth={true}>
-                {descriptors[route.key].render()}
-              </Kb.Box2>
-            ) : null
-          })}
+          {state.routes.map((route, i) => (
+            <Kb.Box2
+              key={route.key}
+              direction="vertical"
+              fullHeight={true}
+              fullWidth={true}
+              style={i === state.index ? undefined : styles.hidden}
+            >
+              {shouldRender(route.key, i === state.index) ? descriptors[route.key].render() : null}
+            </Kb.Box2>
+          ))}
         </Kb.BoxGrow>
       </Kb.Box2>
     </NavigationContent>
   )
-}
+})
 
 const styles = Styles.styleSheetCreate(() => ({
   box: {backgroundColor: Styles.globalColors.white},
+  hidden: {display: 'none'},
 }))
 
 export const createLeftTabNavigator = createNavigatorFactory(LeftTabNavigator)
