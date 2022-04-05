de26c1fb36819f98d38996e7dc5553397e4a7831
chrisnojima <cnojima@keyba.se>
2022-04-05 09:41:23 -0400

    fix image forward (#24899)
    * unpack msgpack buffer into a buffer and not an ArrayBuffer to match framed-msg-pack
    
    * more comments

--- shared/android/app/src/main/cpp/rpc.cpp
+++ shared/android/app/src/main/cpp/rpc.cpp
@@ -72,13 +72,20 @@ Value convertMPToJSI(Runtime &runtime, msgpack::object &o) {
   case msgpack::type::BIN: {
     auto ptr = o.via.bin.ptr;
     int size = o.via.bin.size;
+    // make ArrayBuffer and copy in data
     Function arrayBufferCtor =
         runtime.global().getPropertyAsFunction(runtime, "ArrayBuffer");
-    Value v = arrayBufferCtor.callAsConstructor(runtime, size);
-    Object o = v.getObject(runtime);
-    ArrayBuffer buf = o.getArrayBuffer(runtime);
-    std::copy(ptr, ptr + size, buf.data(runtime));
-    return v;
+    Value ab = arrayBufferCtor.callAsConstructor(runtime, size);
+    Object abo = ab.getObject(runtime);
+    ArrayBuffer abbuf = abo.getArrayBuffer(runtime);
+    std::copy(ptr, ptr + size, abbuf.data(runtime));
+
+    // Wrap in Buffer like framed-msg-pack
+    Object bufObj =
+        runtime.global().getPropertyAsObject(runtime, "Buffer");
+    Function bufFrom = bufObj.getPropertyAsFunction(runtime, "from");
+    Value buf = bufFrom.callWithThis(runtime, bufObj, std::move(ab)); // Buffer shares the memory and just wraps
+    return buf;
   }
   case msgpack::type::ARRAY: {
     auto size = o.via.array.size;
