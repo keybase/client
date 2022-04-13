34d79ffbf513fa148ca48b0a772f23fcc665320f
chrisnojima <cnojima@keyba.se>
2022-04-13 14:40:42 -0400

    WIP

--- shared/util/user-timings.tsx
+++ shared/util/user-timings.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument,@typescript-eslint/no-unsafe-return,@typescript-eslint/no-unsafe-call,@typescript-eslint/no-unnecessary-condition,@typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/restrict-template-expressions */
 /*
  * This file injects performance marks using the performance api (see the chrome timeline view). It:
  * --- We don't do this anymore -- 1. Monkeypatches redux connect to time mapStateToProps, mapDispatchToProps, mergeProps helpers
@@ -7,11 +8,11 @@
  */
 import {userTimings} from '../local-debug'
 
-const perf = typeof performance !== 'undefined' && performance // eslint-disable-line
-const mark = perf && perf.mark && perf.mark.bind(perf)
-const measure = perf && perf.measure && perf.measure.bind(perf)
-const clearMarks = perf && perf.clearMarks && perf.clearMarks.bind(perf)
-const clearMeasures = perf && perf.clearMeasures && perf.clearMeasures.bind(perf)
+const perf = typeof performance !== 'undefined' ? performance : undefined
+const mark = perf?.mark?.bind(perf)
+const measure = perf?.measure?.bind(perf)
+const clearMarks = perf?.clearMarks?.bind(perf)
+const clearMeasures = perf?.clearMeasures?.bind(perf)
 const allowTiming = __DEV__ && userTimings && mark && measure
 const markPrefix = '\uD83D\uDD11' // key unicode. inlining this actually screws up prettier so i had to escape it
 
@@ -19,7 +20,7 @@ const noop = () => {}
 
 const measureStart = allowTiming
   ? (name: string) => {
-      mark && mark(name)
+      mark?.(name)
     }
   : noop
 
@@ -28,10 +29,10 @@ const measureStop = allowTiming
       const measureName = `${markPrefix} ${name}`
       try {
         // measure can throw if you mention something it hasn't seen
-        measure && measure(measureName, name)
+        measure?.(measureName, name)
       } catch (_) {}
-      clearMarks && clearMarks(name)
-      clearMeasures && clearMeasures(measureName)
+      clearMarks?.(name)
+      clearMeasures?.(measureName)
     }
   : noop
 
@@ -67,37 +68,33 @@ const _infect = /* allowTiming
     }
   : */ noop
 
-const _endSaga = effectId => {
+const _endSaga = (effectId: string) => {
   const markName = `${markPrefix} saga:${effectId}`
   const name = `${markPrefix} saga:${_effectIdToLabel[effectId]}`
   try {
-    measure && measure(name, markName)
+    measure?.(name, markName)
   } catch (_) {}
-  clearMarks && clearMarks(markName)
-  clearMeasures && clearMeasures(name)
+  clearMarks?.(markName)
+  clearMeasures?.(name)
 }
 
-const _getLabel = obj => {
-  let label
+const _getLabel = (obj: any) => {
+  let label: string = ''
   try {
     // Try and extract a useful name from saga events
     if (!obj.effect) {
       label = obj.effectId
     } else if (obj.effect.saga) {
-      label = obj.effect && obj.effect.saga && obj.effect.saga.name
+      label = obj.effect.saga.name
     } else if (Array.isArray(obj.effect)) {
       label = obj.effect.map(effect => _getLabel({effect})).join(':')
     } else if (obj.effect.ALL) {
       label = `all:${obj.effect.ALL.map(effect => _getLabel({effect})).join(':')}`
     } else if (obj.effect.FORK) {
-      label = `fork:${obj.effect.FORK.fn && obj.effect.FORK.fn.name}`
+      label = `fork:${obj.effect.FORK?.fn?.name}`
     } else if (obj.effect.CALL) {
-      const contextName =
-        (obj.effect.CALL.context &&
-          obj.effect.CALL.context.constructor &&
-          obj.effect.CALL.context.constructor.name) ||
-        ''
-      const fnName = (obj.effect.CALL.fn && obj.effect.CALL.fn.name) || ''
+      const contextName = obj.effect.CALL.context?.constructor?.name || ''
+      const fnName = obj.effect.CALL.fn?.name || ''
       label = `call:${contextName}:${fnName}`
     } else if (obj.effect.SELECT) {
       label = obj.effect.SELECT.selector.name || `select:${obj.effectId}`
@@ -108,9 +105,7 @@ const _getLabel = obj => {
     } else if (obj.effect.TAKE) {
       label =
         (obj.effect.TAKE.pattern && `take:${obj.effect.TAKE.pattern}`) ||
-        (obj.effect.TAKE.channel &&
-          obj.effect.TAKE.channel.userTimingName &&
-          `take:${obj.effect.TAKE.channel.userTimingName}`)
+        (obj.effect.TAKE.channel?.userTimingName && `take:${obj.effect.TAKE.channel.userTimingName}`)
     } else if (obj.effect.CANCELLED) {
       label = `cancelled:${obj.effectId}`
     } else if (obj.effect.CANCEL) {
@@ -121,15 +116,15 @@ const _getLabel = obj => {
       label = `promise:${obj.effectId}`
     } else if (obj.effect.PUT) {
       label =
-        (obj.effect.PUT.action && obj.effect.PUT.action.type) ||
-        (obj.effect.PUT.action && obj.effect.PUT.action.name) ||
+        obj.effect.PUT.action?.type ||
+        obj.effect.PUT.action?.name ||
         (typeof obj.effect.PUT.action === 'function' && `put:${obj.effectId}`)
     }
   } catch (err) {}
   return label || obj.effectId
 }
 
-const _effectIdToLabel = {}
+const _effectIdToLabel: {[key: string]: string} = {}
 
 const sagaTimer = allowTiming
   ? {
@@ -139,10 +134,10 @@ const sagaTimer = allowTiming
       effectResolved: _endSaga,
       effectTriggered: desc => {
         _effectIdToLabel[desc.effectId] = _getLabel(desc)
-        if (desc.effect && desc.effect.TAKE) {
+        if (desc.effect?.TAKE) {
           return
         }
-        mark && mark(`${markPrefix} saga${desc.effectId}`)
+        mark?.(`${markPrefix} saga${desc.effectId}`)
       },
     }
   : null
