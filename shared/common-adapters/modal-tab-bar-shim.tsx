82ec75c10c0a1dd7e1ead9d08637f84d93e4bbc2
chrisnojima <cnojima@keyba.se>
2022-04-18 09:49:37 -0400

    unused (#24924)

--- shared/common-adapters/modal-tab-bar-shim.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react'
-import * as Kb from '.'
-import './modal-tab-bar-shim.css'
-const ModalTabBarShim = () => (
-  <Kb.Box2 direction="vertical" fullHeight={true} className="modal-tab-bar-shim" />
-)
-
-export default ModalTabBarShim
