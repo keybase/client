1451453c04a8271881768547d38dc9e8a97dc187
chrisnojima <cnojima@keyba.se>
2022-05-20 11:39:48 -0400

    derive TypedActionsMap

--- shared/desktop/yarn-helper/action-creator-creator.tsx
+++ shared/desktop/yarn-helper/action-creator-creator.tsx
@@ -134,14 +134,13 @@ function compileReduxTypeConstant(ns: ActionNS, actionName: ActionName, _: Actio
 
 function makeTypedActions(created: Array<string>) {
   return `// NOTE: This file is GENERATED from json files in actions/json. Run 'yarn build-actions' to regenerate
-/* eslint-disable no-unused-vars,no-use-before-define */
-  ${created.map(c => `import * as ${cleanName(c)} from './${c}-gen'`).join('\n')}
+  ${created.map(c => `import type * as ${cleanName(c)} from './${c}-gen'`).join('\n')}
 
   export type TypedActions = ${created.map(c => `${cleanName(c)}.Actions`).join(' | ')}
 
-  export type TypedActionsMap = {${typeMap.join(',\n')},
-    'common:resetStore': {readonly type: 'common:resetStore', readonly payload: undefined}
-  }
+  type DiscriminateUnion<T, K extends keyof T, V extends T[K]> = T extends Record<K, V> ? T : never
+  type MapDiscriminatedUnion<T extends Record<K, string>, K extends keyof T> = { [V in T[K]]: DiscriminateUnion<T, K, V> };
+  export type TypedActionsMap = MapDiscriminatedUnion<TypedActions, 'type'>
 `
 }
 
