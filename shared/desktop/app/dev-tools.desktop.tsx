8b44aa78a716f73c7cf3bc3e164be5627c193619
chrisnojima <cnojima@keyba.se>
2022-05-22 10:01:02 -0400

    plumb configOverload better

--- shared/desktop/app/dev-tools.desktop.tsx
+++ shared/desktop/app/dev-tools.desktop.tsx
@@ -1,5 +1,5 @@
 import * as Electron from 'electron'
-import {showDevTools} from './dynamic-config'
+import {showDevTools} from '../../local-debug'
 import flags from '../../util/feature-flags'
 
 export function setupDevToolsExtensions() {
