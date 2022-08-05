cbb24083a4f00e92198b9fd917cd6ae095ba07bc
chrisnojima <cnojima@keyba.se>
2022-08-05 16:15:35 -0400

    WIP

--- shared/ios/Keybase/AppDelegate.mm
+++ shared/ios/Keybase/AppDelegate.mm
@@ -31,6 +31,8 @@
 #import <ReactCommon/RCTTurboModuleManager.h>
 #import <react/config/ReactNativeConfig.h>
 
+static NSString *const kRNConcurrentRoot = @"concurrentRoot";
+
 @interface AppDelegate () <RCTCxxBridgeDelegate,
                            RCTTurboModuleManagerDelegate> {
   RCTTurboModuleManager *_turboModuleManager;
@@ -116,7 +118,8 @@ static const DDLogLevel ddLogLevel = DDLogLevelDebug;
       [UNUserNotificationCenter currentNotificationCenter];
   center.delegate = self;
 
-  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"Keybase", nil);
+  NSDictionary *initProps = [self prepareInitialProps];
+  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"Keybase", initProps);
   if (@available(iOS 13.0, *)) {
     rootView.backgroundColor = [UIColor systemBackgroundColor];
   } else {
@@ -158,6 +161,25 @@ static const DDLogLevel ddLogLevel = DDLogLevelDebug;
           UIApplicationBackgroundFetchIntervalMinimum];
   
   return YES;
+}
+
+/// This method controls whether the `concurrentRoot`feature of React18 is turned on or off.
+///
+/// @see: https://reactjs.org/blog/2022/03/29/react-v18.html
+/// @note: This requires to be rendering on Fabric (i.e. on the New Architecture).
+/// @return: `true` if the `concurrentRoot` feture is enabled. Otherwise, it returns `false`.
+- (BOOL)concurrentRootEnabled
+{
+  // Switch this bool to turn on and off the concurrent root
+  return true;
+}
+- (NSDictionary *)prepareInitialProps
+{
+  NSMutableDictionary *initProps = [NSMutableDictionary new];
+#ifdef RCT_NEW_ARCH_ENABLED
+  initProps[kRNConcurrentRoot] = @([self concurrentRootEnabled]);
+#endif
+  return initProps;
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge {
