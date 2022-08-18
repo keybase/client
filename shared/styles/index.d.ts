003d740480a031d7dc6f14db20a889541ba08cc9
chrisnojima <cnojima@keyba.se>
2022-08-18 10:40:54 -0400

    WIP

--- shared/styles/index.d.ts
+++ shared/styles/index.d.ts
@@ -151,5 +151,5 @@ export {default as classNames} from 'classnames'
 export {default as styled} from '@emotion/styled'
 export declare const StyleContext: React.Context<{canFixOverdraw: boolean}>
 export declare const dontFixOverdraw: {canFixOverdraw: false}
-export declare const yesFixOverdraw = {canFixOverdraw: true}
+export declare const yesFixOverdraw: {canFixOverdraw: true}
 export declare const undynamicColor: (col: any) => any
