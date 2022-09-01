ee4cfb18ecbe6c0645311fd050830f251c6bb44f
chrisnojima <cnojima@keyba.se>
2022-09-01 14:44:53 -0400

    fallback to native if exists

--- shared/util/arrays.tsx
+++ shared/util/arrays.tsx
@@ -17,6 +17,8 @@ export function intersperseFn<A, B>(
 }
 
 export function findLast<T>(arr: Array<T>, cb: (t: T) => boolean): T | undefined {
+  // @ts-ignore this exists
+  if (arr.findLast) return arr.findLast(cb)
   const last = arr.length - 1
   for (let i = last; i >= 0; --i) {
     if (cb(arr[i])) {
