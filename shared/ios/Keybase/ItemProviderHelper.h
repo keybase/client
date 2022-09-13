81b672e69886ef2186a3b03642bf2bb730382f56
chrisnojima <cnojima@keyba.se>
2022-09-13 16:51:22 -0400

    WIP

---
+++ shared/ios/Keybase/ItemProviderHelper.h
@@ -0,0 +1,18 @@
+//
+//  ItemProviderHelper.h
+//  Keybase
+//
+//  Created by Chris Nojima on 9/13/22.
+//  Copyright © 2022 Keybase. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ItemProviderHelper : NSObject
+-(id) initWithItems: (NSArray*) items completionHandler:(void (^)(void))handler;
+-(void) startProcessing;
+@end
+
+NS_ASSUME_NONNULL_END
