03caca3a27b35a05d775d0c548419086c23713aa
chrisnojima <cnojima@keyba.se>
2022-09-22 15:49:51 -0400

    WIP

--- shared/common-adapters/zoomable-image.d.ts
+++ shared/common-adapters/zoomable-image.d.ts
@@ -4,6 +4,7 @@ import * as Styles from '../styles'
 export type Props = {
   src: string
   style?: Styles.StylesCrossPlatform
+  onZoomed?: (z: boolean) => void // desktop only
 }
 
 export default class ZoomableImage extends React.Component<Props> {}
