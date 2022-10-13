1bbd0537e1920c39d3fe529db79be6804b38f1f6
chrisnojima <cnojima@keyba.se>
2022-10-13 09:48:37 -0400

    WIP

--- shared/android/app/src/main/jni/Android.mk
+++ shared/android/app/src/main/jni/Android.mk
@@ -28,8 +28,7 @@ LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
 LOCAL_SHARED_LIBRARIES := \
   libfabricjni \
   libfbjni \
-  libfolly_futures \
-  libfolly_json \
+  libfolly_runtime \
   libglog \
   libjsi \
   libreact_codegen_rncore \
