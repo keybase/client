a005832b8c48396188d01712495aa715da9bc6ae
chrisnojima <cnojima@keyba.se>
2022-11-11 15:55:34 -0500

    WIP

--- shared/common-adapters/dropdown.tsx
+++ shared/common-adapters/dropdown.tsx
@@ -5,40 +5,52 @@ import ClickableBox from './clickable-box'
 import Text from './text'
 import Overlay from './overlay'
 import ScrollView from './scroll-view'
-import OverlayParentHOC, {OverlayParentProps} from './overlay/parent-hoc'
-import {Position} from './relative-popup-hoc.types'
+import type {Position} from './relative-popup-hoc.types'
 import Icon from './icon'
 import {smallHeight, regularHeight} from './button'
+import {usePopup} from './use-popup'
 import * as Styles from '../styles'
+
+const Kb = {
+  Box,
+  Box2,
+  ClickableBox,
+  Icon,
+  Overlay,
+  ProgressIndicator,
+  ScrollView,
+  Text,
+  usePopup,
+}
 
 type DropdownButtonProps = {
   disabled?: boolean
   selected?: React.ReactNode
   selectedBoxStyle?: Styles.StylesCrossPlatform
   style?: Styles.StylesCrossPlatform
-  setAttachmentRef?: (arg0: React.Component<any> | null) => void
+  popupAnchor?: React.MutableRefObject<typeof ButtonBox | null>
   toggleOpen: (e: React.BaseSyntheticEvent) => void
   inline?: boolean
   loading?: boolean
 }
 const noTheme = {}
 export const DropdownButton = (props: DropdownButtonProps) => (
-  <ClickableBox
+  <Kb.ClickableBox
     onClick={!props.disabled ? props.toggleOpen : undefined}
     style={Styles.collapseStyles([styles.dropdownBoxContainer, props.style])}
   >
-    <ButtonBox inline={props.inline} disabled={props.disabled} ref={props.setAttachmentRef} theme={noTheme}>
-      <Box style={Styles.collapseStyles([styles.selectedBox, props.selectedBoxStyle])}>
-        {props.loading ? <ProgressIndicator type="Small" /> : props.selected}
-      </Box>
-      <Icon
+    <ButtonBox inline={props.inline} disabled={props.disabled} ref={props.popupAnchor} theme={noTheme}>
+      <Kb.Box style={Styles.collapseStyles([styles.selectedBox, props.selectedBoxStyle])}>
+        {props.loading ? <Kb.ProgressIndicator type="Small" /> : props.selected}
+      </Kb.Box>
+      <Kb.Icon
         type="iconfont-caret-down"
         inheritColor={true}
         sizeType="Tiny"
         style={{marginTop: Styles.isMobile ? 2 : -8}}
       />
     </ButtonBox>
-  </ClickableBox>
+  </Kb.ClickableBox>
 )
 
 type Props<N> = {
@@ -54,69 +66,67 @@ type Props<N> = {
   style?: Styles.StylesCrossPlatform
 }
 
-type State = {
-  expanded: boolean
-}
+function Dropdown<N>(p: Props<N>) {
+  const [expanded, setExpanded] = React.useState(false)
+  const disabled = p.disabled ?? false
+  const {style, onChanged, onChangedIdx, overlayStyle, selectedBoxStyle} = p
+  const {position, itemBoxStyle, items, selected} = p
 
-class Dropdown<N extends React.ReactNode> extends React.Component<Props<N> & OverlayParentProps, State> {
-  state = {expanded: false}
+  const toggleOpen = React.useCallback(
+    (evt?: React.BaseSyntheticEvent) => {
+      evt && evt.stopPropagation && evt.stopPropagation()
+      evt && evt.preventDefault && evt.preventDefault()
+      setExpanded(e => !e)
+    },
+    [setExpanded]
+  )
 
-  static defaultProps = {
-    disabled: false,
-  }
+  const onSelect = React.useCallback(() => {
+    setExpanded(false)
+  }, [setExpanded])
 
-  _toggleOpen = (evt?: React.BaseSyntheticEvent) => {
-    evt && evt.stopPropagation && evt.stopPropagation()
-    evt && evt.preventDefault && evt.preventDefault()
-    this.setState(prevState => ({
-      expanded: !prevState.expanded,
-    }))
-  }
+  const {popup, popupAnchor} = Kb.usePopup<typeof ButtonBox>(attachTo => (
+    <Kb.Overlay
+      style={Styles.collapseStyles([styles.overlay, overlayStyle])}
+      attachTo={attachTo}
+      visible={expanded}
+      onHidden={toggleOpen}
+      position={position || 'center center'}
+    >
+      <Kb.ScrollView style={styles.scrollView}>
+        {items.map((i: N, idx) => (
+          <Kb.ClickableBox
+            key={idx}
+            onClick={evt => {
+              evt.stopPropagation && evt.stopPropagation()
+              evt.preventDefault && evt.preventDefault()
+              // Bug in flow that doesn't let us just call this function
+              // onSelect(i)
+              onChanged && onChanged(i)
+              onChangedIdx && onChangedIdx(idx)
+              onSelect()
+            }}
+            style={styles.itemClickBox}
+          >
+            <ItemBox style={itemBoxStyle}>{i}</ItemBox>
+          </Kb.ClickableBox>
+        ))}
+      </Kb.ScrollView>
+    </Kb.Overlay>
+  ))
 
-  _onSelect = () => {
-    this.setState({expanded: false})
-  }
-
-  render() {
-    return (
-      <Box style={Styles.collapseStyles([styles.overlayContainer, this.props.style])}>
-        <DropdownButton
-          disabled={this.props.disabled}
-          selected={this.props.selected}
-          selectedBoxStyle={this.props.selectedBoxStyle}
-          setAttachmentRef={this.props.setAttachmentRef}
-          toggleOpen={this._toggleOpen}
-        />
-        <Overlay
-          style={Styles.collapseStyles([styles.overlay, this.props.overlayStyle])}
-          attachTo={this.props.getAttachmentRef}
-          visible={this.state.expanded}
-          onHidden={this._toggleOpen}
-          position={this.props.position || 'center center'}
-        >
-          <ScrollView style={styles.scrollView}>
-            {this.props.items.map((i: N, idx) => (
-              <ClickableBox
-                key={idx}
-                onClick={evt => {
-                  evt.stopPropagation && evt.stopPropagation()
-                  evt.preventDefault && evt.preventDefault()
-                  // Bug in flow that doesn't let us just call this function
-                  // this._onSelect(i)
-                  this.props.onChanged && this.props.onChanged(i)
-                  this.props.onChangedIdx && this.props.onChangedIdx(idx)
-                  this._onSelect()
-                }}
-                style={styles.itemClickBox}
-              >
-                <ItemBox style={this.props.itemBoxStyle}>{i}</ItemBox>
-              </ClickableBox>
-            ))}
-          </ScrollView>
-        </Overlay>
-      </Box>
-    )
-  }
+  return (
+    <Kb.Box style={Styles.collapseStyles([styles.overlayContainer, style])}>
+      <DropdownButton
+        disabled={disabled}
+        selected={selected as any}
+        selectedBoxStyle={selectedBoxStyle}
+        popupAnchor={popupAnchor}
+        toggleOpen={toggleOpen}
+      />
+      {popup}
+    </Kb.Box>
+  )
 }
 
 type InlineDropdownProps = {
@@ -138,9 +148,12 @@ type InlineDropdownProps = {
 
 export const InlineDropdown = (props: InlineDropdownProps) => {
   const selected = (
-    <Box2 direction="horizontal" style={Styles.collapseStyles([styles.inlineSelected, props.selectedStyle])}>
-      {props.textWrapperType ? <Text type={props.textWrapperType}>{props.label}</Text> : props.label}
-    </Box2>
+    <Kb.Box2
+      direction="horizontal"
+      style={Styles.collapseStyles([styles.inlineSelected, props.selectedStyle])}
+    >
+      {props.textWrapperType ? <Kb.Text type={props.textWrapperType}>{props.label}</Kb.Text> : props.label}
+    </Kb.Box2>
   )
   return (
     <DropdownButton
@@ -225,7 +238,7 @@ const styles = Styles.styleSheetCreate(() => ({
   },
 }))
 
-const ItemBox = Styles.styled(Box)(() => ({
+const ItemBox = Styles.styled(Kb.Box)(() => ({
   ...Styles.globalStyles.flexBoxRow,
   ...(Styles.isMobile
     ? {}
@@ -241,7 +254,7 @@ const ItemBox = Styles.styled(Box)(() => ({
   width: '100%',
 }))
 
-const ButtonBox = Styles.styled(Box, {shouldForwardProp: prop => prop !== 'inline'})(props => ({
+const ButtonBox = Styles.styled(Kb.Box, {shouldForwardProp: prop => prop !== 'inline'})(props => ({
   ...Styles.globalStyles.flexBoxRow,
   ...(!props.disabled && !Styles.isMobile
     ? {
@@ -269,23 +282,4 @@ const ButtonBox = Styles.styled(Box, {shouldForwardProp: prop => prop !== 'inlin
     : {}),
 }))
 
-// This whole wrapper exists so as to get proper typing on the export, so
-// that things can use Dropdown with the properly typed Props. This could
-// be cleaner, probably, but it's probably not worth spending too much time
-// on as hopefully we'll be moving to hooks for our HOCs
-class OverlayDropdown<N extends React.ReactNode> extends React.Component<Props<N>> {
-  private Container: React.Component<Props<N> & OverlayParentProps>
-
-  constructor(props: Props<N>) {
-    super(props)
-    // @ts-ignore
-    this.Container = OverlayParentHOC(Dropdown)
-  }
-
-  render() {
-    // @ts-ignore
-    return <this.Container {...this.props} />
-  }
-}
-
-export default OverlayDropdown
+export default Dropdown
