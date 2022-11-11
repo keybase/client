4fbe001d2deeef9ea9c65cb0b6a37423b245c865
chrisnojima <cnojima@keyba.se>
2022-11-11 14:21:25 -0500

    WIP

--- shared/fs/common/sfmi-popup.tsx
+++ shared/fs/common/sfmi-popup.tsx
@@ -11,7 +11,7 @@ type Props = {
   mode: 'Icon' | 'Button'
 }
 
-export default Kb.OverlayParentHOC((props: Props & Kb.OverlayParentProps) => {
+export default (props: Props) => {
   const driverStatus = Container.useSelector(state => state.fs.sfmi.driverStatus)
   const dispatch = Container.useDispatch()
   const enableDriver = () => dispatch(FsGen.createDriverEnable())
@@ -20,6 +20,46 @@ export default Kb.OverlayParentHOC((props: Props & Kb.OverlayParentProps) => {
     undefined,
     undefined
   )
+
+  const {toggleShowingPopup, showingPopup, popup, popupAnchor} = Kb.usePopup(attachTo => (
+    <Kb.Overlay
+      style={styles.popup}
+      attachTo={attachTo}
+      visible={showingPopup}
+      onHidden={toggleShowingPopup}
+      position="bottom right"
+    >
+      <Kb.Box
+        style={styles.container}
+        onClick={e => {
+          e.stopPropagation()
+        }}
+      >
+        <Kb.Box2 direction="horizontal" centerChildren={true} style={styles.fancyFinderIcon}>
+          <Kb.Icon type="icon-fancy-finder-132-96" />
+        </Kb.Box2>
+        <Kb.Text type="BodyBig" style={styles.text}>
+          Enable Keybase in {fileUIName}?
+        </Kb.Text>
+        <Kb.Text type="BodySmall" style={styles.text} center={true}>
+          Get access to your files and folders just like you normally do with your local files. It's encrypted
+          and secure.
+        </Kb.Text>
+        <Kb.Divider style={styles.divider} />
+        {fuseConsentComponent}
+        <Kb.Box2 direction="horizontal" fullWidth={true} centerChildren={true} style={styles.buttonBox}>
+          <Kb.Button
+            type="Success"
+            label="Yes, enable"
+            waiting={driverStatus.type === Types.DriverStatusType.Disabled && driverStatus.isEnabling}
+            disabled={!canContinue}
+            onClick={enableDriver}
+          />
+        </Kb.Box2>
+      </Kb.Box>
+    </Kb.Overlay>
+  ))
+
   if (driverStatus.type !== Types.DriverStatusType.Disabled) {
     return null
   }
@@ -33,8 +73,8 @@ export default Kb.OverlayParentHOC((props: Props & Kb.OverlayParentProps) => {
             fontSize={16}
             color={Styles.globalColors.black_50}
             hoverColor={Styles.globalColors.black}
-            onClick={props.toggleShowingMenu}
-            ref={props.setAttachmentRef}
+            onClick={toggleShowingPopup}
+            ref={popupAnchor as any}
           />
         </Kb.WithTooltip>
       ) : (
@@ -42,49 +82,14 @@ export default Kb.OverlayParentHOC((props: Props & Kb.OverlayParentProps) => {
           mode="Secondary"
           small={true}
           label={`Enable ${fileUIName} integration`}
-          onClick={props.toggleShowingMenu}
-          ref={props.setAttachmentRef}
+          onClick={toggleShowingPopup}
+          ref={popupAnchor}
         />
       )}
-      <Kb.Overlay
-        style={styles.popup}
-        attachTo={props.getAttachmentRef}
-        visible={props.showingMenu}
-        onHidden={props.toggleShowingMenu}
-        position="bottom right"
-      >
-        <Kb.Box
-          style={styles.container}
-          onClick={e => {
-            e.stopPropagation()
-          }}
-        >
-          <Kb.Box2 direction="horizontal" centerChildren={true} style={styles.fancyFinderIcon}>
-            <Kb.Icon type="icon-fancy-finder-132-96" />
-          </Kb.Box2>
-          <Kb.Text type="BodyBig" style={styles.text}>
-            Enable Keybase in {fileUIName}?
-          </Kb.Text>
-          <Kb.Text type="BodySmall" style={styles.text} center={true}>
-            Get access to your files and folders just like you normally do with your local files. It's
-            encrypted and secure.
-          </Kb.Text>
-          <Kb.Divider style={styles.divider} />
-          {fuseConsentComponent}
-          <Kb.Box2 direction="horizontal" fullWidth={true} centerChildren={true} style={styles.buttonBox}>
-            <Kb.Button
-              type="Success"
-              label="Yes, enable"
-              waiting={driverStatus.isEnabling}
-              disabled={!canContinue}
-              onClick={enableDriver}
-            />
-          </Kb.Box2>
-        </Kb.Box>
-      </Kb.Overlay>
+      {popup}
     </>
   )
-})
+}
 
 const styles = Styles.styleSheetCreate(() => ({
   buttonBox: {
