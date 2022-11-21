a8a879f6599b39f1a92c7523687d9972925c0e94
chrisnojima <cnojima@keyba.se>
2022-11-21 14:52:13 -0500

    WIP

--- shared/wallets/qr-scan/container.tsx
+++ shared/wallets/qr-scan/container.tsx
@@ -1,32 +1,40 @@
 import * as React from 'react'
 import QRScan from '.'
-import logger from '../../logger'
+// import logger from '../../logger'
 import * as Container from '../../util/container'
-import * as WalletsGen from '../../actions/wallets-gen'
-import * as RouteTreeGen from '../../actions/route-tree-gen'
+// import * as WalletsGen from '../../actions/wallets-gen'
+// import * as RouteTreeGen from '../../actions/route-tree-gen'
 
 export default () => {
-  const dispatch = Container.useDispatch()
-  const _onSubmitCode = React.useCallback(
-    (to: string | null) => {
-      if (to) {
-        if (to.startsWith('web+stellar:')) {
-          // Switch from the send form to the SEP7 handler.
-          dispatch(WalletsGen.createValidateSEP7Link({fromQR: true, link: to}))
-          dispatch(WalletsGen.createClearBuilding())
-        } else {
-          // Just a plain address.
-          dispatch(WalletsGen.createSetBuildingRecipientType({recipientType: 'stellarPublicKey'}))
-          dispatch(WalletsGen.createSetBuildingTo({to}))
-        }
-      } else {
-        logger.warn('QrScan.onSubmitCode: No `to` field for QRScan')
-      }
-      dispatch(RouteTreeGen.createNavigateUp())
-    },
-    [dispatch]
-  )
+  // const dispatch = Container.useDispatch()
+  // const _onSubmitCode = React.useCallback(
+  //   (to: string | null) => {
+  //     if (to) {
+  //       if (to.startsWith('web+stellar:')) {
+  //         // Switch from the send form to the SEP7 handler.
+  //         dispatch(WalletsGen.createValidateSEP7Link({fromQR: true, link: to}))
+  //         dispatch(WalletsGen.createClearBuilding())
+  //       } else {
+  //         // Just a plain address.
+  //         dispatch(WalletsGen.createSetBuildingRecipientType({recipientType: 'stellarPublicKey'}))
+  //         dispatch(WalletsGen.createSetBuildingTo({to}))
+  //       }
+  //     } else {
+  //       logger.warn('QrScan.onSubmitCode: No `to` field for QRScan')
+  //     }
+  //     dispatch(RouteTreeGen.createNavigateUp())
+  //   },
+  //   [dispatch]
+  // )
 
-  const safeOnSubmitCode = Container.useSafeSubmit(_onSubmitCode, false)
+  const _onSubmitCode = React.useCallback((_to: string | null) => {
+    setTimeout(() => setTemp(true), 1000)
+  }, [])
+
+  const [temp, setTemp] = React.useState(false)
+
+  const safeOnSubmitCode = Container.useSafeSubmit(_onSubmitCode, temp)
+
+  // const safeOnSubmitCode = Container.useSafeSubmit(_onSubmitCode, false)
   return <QRScan onSubmitCode={safeOnSubmitCode} />
 }
