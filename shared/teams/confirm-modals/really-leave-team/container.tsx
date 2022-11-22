ec6b7cc0ace16266d101f0702ced81c515806d3e
chrisnojima <cnojima@keyba.se>
2022-11-22 10:24:33 -0500

    WIP

--- shared/teams/confirm-modals/really-leave-team/container.tsx
+++ shared/teams/confirm-modals/really-leave-team/container.tsx
@@ -4,56 +4,36 @@ import * as RouteTreeGen from '../../../actions/route-tree-gen'
 import * as Container from '../../../util/container'
 import * as Constants from '../../../constants/teams'
 import * as Types from '../../../constants/types/teams'
-import ReallyLeaveTeam, {type Props} from '.'
+import ReallyLeaveTeam from '.'
 import LastOwnerDialog from './last-owner'
 import {anyWaiting} from '../../../constants/waiting'
 import {useTeamDetailsSubscribeMountOnly} from '../../subscriber'
 
 type OwnProps = Container.RouteProps<'teamReallyLeaveTeam'>
-type ExtraProps = {_leaving: boolean; lastOwner: boolean; stillLoadingTeam: boolean; teamID: Types.TeamID}
 
-const RenderLastOwner = (p: Props & ExtraProps) => {
-  const {lastOwner, _leaving, stillLoadingTeam, teamID, ...rest} = p
-  useTeamDetailsSubscribeMountOnly(teamID)
-  return lastOwner ? (
-    <LastOwnerDialog
-      onBack={rest.onBack}
-      onDeleteTeam={rest.onDeleteTeam}
-      name={rest.name}
-      stillLoadingTeam={stillLoadingTeam}
-    />
-  ) : (
-    <ReallyLeaveTeam {...rest} />
+export default (op: OwnProps) => {
+  const teamID = op.route.params?.teamID ?? Types.noTeamID
+  const {teamname} = Container.useSelector(state => Constants.getTeamMeta(state, teamID))
+  const {settings, members} = Container.useSelector(state => Constants.getTeamDetails(state, teamID))
+  const open = settings.open
+  const lastOwner = Container.useSelector(state => Constants.isLastOwner(state, teamID))
+  const stillLoadingTeam = !members
+  const leaving = Container.useSelector(state => anyWaiting(state, Constants.leaveTeamWaitingKey(teamname)))
+  const error = Container.useSelector(state =>
+    Container.anyErrors(state, Constants.leaveTeamWaitingKey(teamname))
   )
-}
 
-export default Container.connect(
-  (state, ownProps: OwnProps) => {
-    const teamID = ownProps.route.params?.teamID ?? Types.noTeamID
-    const {teamname} = Constants.getTeamMeta(state, teamID)
-    const {settings, members} = Constants.getTeamDetails(state, teamID)
-    const lastOwner = Constants.isLastOwner(state, teamID)
-    const stillLoadingTeam = !members
-    return {
-      _leaving: anyWaiting(state, Constants.leaveTeamWaitingKey(teamname)),
-      error: Container.anyErrors(state, Constants.leaveTeamWaitingKey(teamname)),
-      lastOwner,
-      name: teamname,
-      open: settings?.open,
-      stillLoadingTeam,
-      teamID,
-    }
-  },
-  dispatch => ({
-    _onDeleteTeam: (teamID: Types.TeamID) => {
-      dispatch(RouteTreeGen.createNavigateUp())
-      dispatch(
-        RouteTreeGen.createNavigateAppend({
-          path: [{props: {teamID}, selected: 'teamDeleteTeam'}],
-        })
-      )
-    },
-    _onLeave: (teamname: string, permanent: boolean) => {
+  const dispatch = Container.useDispatch()
+  const onDeleteTeam = React.useCallback(() => {
+    dispatch(RouteTreeGen.createNavigateUp())
+    dispatch(
+      RouteTreeGen.createNavigateAppend({
+        path: [{props: {teamID}, selected: 'teamDeleteTeam'}],
+      })
+    )
+  }, [dispatch, teamID])
+  const _onLeave = React.useCallback(
+    (permanent: boolean) => {
       dispatch(
         TeamsGen.createLeaveTeam({
           context: 'teams',
@@ -62,18 +42,28 @@ export default Container.connect(
         })
       )
     },
-    onBack: () => dispatch(RouteTreeGen.createNavigateUp()),
-  }),
-  (stateProps, dispatchProps, _: OwnProps) => ({
-    _leaving: stateProps._leaving,
-    error: stateProps.error?.message ?? '',
-    lastOwner: stateProps.lastOwner,
-    name: stateProps.name,
-    onBack: stateProps._leaving ? () => {} : dispatchProps.onBack,
-    onDeleteTeam: () => dispatchProps._onDeleteTeam(stateProps.teamID),
-    onLeave: (permanent: boolean) => dispatchProps._onLeave(stateProps.name, permanent),
-    open: stateProps.open,
-    stillLoadingTeam: stateProps.stillLoadingTeam,
-    teamID: stateProps.teamID,
-  })
-)(Container.safeSubmit(['onLeave'], ['_leaving'])(RenderLastOwner))
+    [dispatch, teamname]
+  )
+  const _onBack = React.useCallback(() => dispatch(RouteTreeGen.createNavigateUp()), [dispatch])
+  const onBack = leaving ? () => {} : _onBack
+  const onLeave = Container.useSafeSubmit(_onLeave, !leaving)
+
+  useTeamDetailsSubscribeMountOnly(teamID)
+  return lastOwner ? (
+    <LastOwnerDialog
+      onBack={onBack}
+      onDeleteTeam={onDeleteTeam}
+      name={teamname}
+      stillLoadingTeam={stillLoadingTeam}
+    />
+  ) : (
+    <ReallyLeaveTeam
+      error={error?.message ?? ''}
+      onBack={onBack}
+      onDeleteTeam={onDeleteTeam}
+      onLeave={onLeave}
+      open={open}
+      name={teamname}
+    />
+  )
+}
