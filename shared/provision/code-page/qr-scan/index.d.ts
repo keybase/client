8afc4a709ea8616e2e140929961b5d8991e4ea93
chrisnojima <cnojima@keyba.se>
2022-12-07 14:53:20 -0500

    update deps, fix qr scan

--- shared/provision/code-page/qr-scan/index.d.ts
+++ shared/provision/code-page/qr-scan/index.d.ts
@@ -1,7 +1,7 @@
 import * as React from 'react'
 export type Props = {
   waiting: boolean
-  onSubmitTextCode: (arg0: string) => void
+  onSubmitTextCode: (s: string) => void
 }
 declare class QRScan extends React.Component<Props> {}
 export default QRScan
