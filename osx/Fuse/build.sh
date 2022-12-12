2c66e4c1d79005fe726f3f59bd7e3fe0174ba6b0
Mike Maxim <mike@keyba.se>
2022-12-12 12:20:50 -0500

    upgrade kbfuse to 4.4.1

--- osx/Fuse/build.sh
+++ osx/Fuse/build.sh
@@ -8,9 +8,9 @@ cd "$dir"
 rm -rf osxfuse macfuse
 
 version=${VERSION:?"Need to set VERSION for Fuse"}
-if [ "$version" = "4.2.4" ]; then
-  tar -xjf /Volumes/Keybase/team/keybase.builds.macos/macfuse/macfuse-4.2.4-src.tbz
-  mv macfuse-4.2.4-src macfuse
+if [ "$version" = "4.4.1" ]; then
+  tar -xjf /Volumes/Keybase/team/keybase.builds.macos/macfuse/macfuse-4.4.1-src.tbz
+  mv macfuse-4.4.1-src macfuse
 else
   echo "Unsupported VERSION"
   exit 1
@@ -23,8 +23,8 @@ fi
 rm -rf /tmp/kbfuse*
 cd macfuse
 # If you get an error compiling you might have to run `brew link gettext --force` (see https://github.com/osxfuse/osxfuse/issues/149).
-# use 11.3 SDK (-s) and set deployment target 11.13 (-d). Build for macOS 11 kernel, and support up to Darwin 21 (macOS 12).
-./build.sh -v 5 -t filesystembundle -- -s 11.3 -d 11.3 --kext="11,11.3,21" --kext="12->11"
+# use 12.3 SDK (-s) and set deployment target 12.3 (-d). Build for macOS 12 kernel, and support up to Darwin 22 (macOS 13).
+./build.sh -v 5 -t filesystembundle -- -s 12.3 -d 12.3 --kext="12,12.3,22" --kext="13->12"
 
 cd $dir
 rm -rf kbfuse.bundle
@@ -36,13 +36,13 @@ tar zcvpf $dir/fsbundle.tgz  .
 
 # Sign the kext
 cd $dir
-codesign --verbose --timestamp --options runtime --sign "Developer ID Application: Keybase, Inc." kbfuse.bundle/Contents/Extensions/11/kbfuse.kext
+codesign --verbose --timestamp --options runtime --sign "Developer ID Application: Keybase, Inc." kbfuse.bundle/Contents/Extensions/12/kbfuse.kext
 codesign --verbose --timestamp --options runtime --sign "Developer ID Application: Keybase, Inc." kbfuse.bundle/Contents/Resources/mount_kbfuse
 codesign --verbose --timestamp --options runtime --sign "Developer ID Application: Keybase, Inc." kbfuse.bundle/Contents/Resources/load_kbfuse
 codesign --verbose --force --deep --timestamp --options runtime --sign "Developer ID Application: Keybase, Inc." kbfuse.bundle
 
 # Verify
-codesign --verbose --verify kbfuse.bundle/Contents/Extensions/11/kbfuse.kext
+codesign --verbose --verify kbfuse.bundle/Contents/Extensions/12/kbfuse.kext
 codesign --verbose --verify kbfuse.bundle/Contents/Resources/mount_kbfuse
 codesign --verbose --verify kbfuse.bundle/Contents/Resources/load_kbfuse
 codesign --verbose --verify kbfuse.bundle
