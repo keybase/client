f5b26e697fb73b3335759f5da6ddf7341f95c888
chrisnojima <cnojima@keyba.se>
2022-12-16 12:21:01 -0500

    disable animation loops on android short term (#25350)

--- shared/common-adapters/animation.native.tsx
+++ shared/common-adapters/animation.native.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react'
-import LottieView from 'lottie-react-native'
 import Box from './box'
-import {useDepChangeEffect} from '../util/container'
+import LottieView from 'lottie-react-native'
 import type {Props} from './animation'
+import {useDepChangeEffect, isAndroid} from '../util/container'
 type AnimationObject = {
   v: string
   fr: number
@@ -19,11 +19,7 @@ type AOM = {[key: string]: AnimationObject}
 
 const Animation = React.memo(function Animation(props: Props) {
   const {animationType} = props
-  const dataRef = React.useRef<AOM>()
-  if (!dataRef.current) {
-    dataRef.current = require('./animation-data.json') as AOM
-  }
-
+  const dataRef = React.useRef<AOM>(require('./animation-data.json'))
   const source = React.useRef<AnimationObject>(dataRef.current[animationType])
   useDepChangeEffect(() => {
     const data = dataRef.current?.[animationType]
@@ -34,9 +30,12 @@ const Animation = React.memo(function Animation(props: Props) {
     source.current = data
   }, [animationType])
 
+  // android perf is really hurt due to this. Short term let it run once and hang out while we decide an alternative
+  const allowLoop = !isAndroid
+
   return (
     <Box style={props.containerStyle}>
-      <LottieView autoPlay={true} loop={true} source={source.current} style={props.style} />
+      <LottieView autoPlay={true} loop={allowLoop} source={source.current} style={props.style} />
     </Box>
   )
 })
