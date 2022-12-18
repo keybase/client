bed8af6f499c289065ff8c62ae0998667ed3a6bf
chrisnojima <cnojima@keyba.se>
2022-12-18 14:18:29 -0500

    WIP

--- shared/why-did-you-render.tsx
+++ shared/why-did-you-render.tsx
@@ -1,34 +1,34 @@
 /// <reference types="@welldone-software/why-did-you-render" />
-import React from 'react'
-if (__DEV__) {
-  console.log('\n\n\nDEBUG: WHY DID YOU RENDER try to load')
-  const whyDidYouRender = require('@welldone-software/why-did-you-render')
-  console.log('\n\n\nDEBUG: WHY DID YOU RENDER enabled')
-  if (whyDidYouRender && typeof whyDidYouRender === 'function') {
-    const ush = require('./util/use-selector').default
-    whyDidYouRender(React, {
-      // TODO reduce these
-      exclude: [
-        /Pressable/,
-        /Portal/,
-        /RNSScreen/,
-        /^Screen$/,
-        /^Group$/,
-        /^View$/,
-        /^AnimatedComponentWrapper$/,
-        /CardContainer/,
-        /StaticContainer/,
-      ],
-      include: [
-        /^ConnectFunction/,
-        // uncomment to watch everything, realllllly slows things down
-        // /.*/,
-      ],
-      // logOnDifferentValues: true,
-      trackAllPureComponents: true,
-      trackExtraHooks: [[ush, 'useSelector']],
-    })
-  }
-}
+// import React from 'react'
+// if (__DEV__) {
+//   console.log('\n\n\nDEBUG: WHY DID YOU RENDER try to load')
+//   const whyDidYouRender = require('@welldone-software/why-did-you-render')
+//   console.log('\n\n\nDEBUG: WHY DID YOU RENDER enabled')
+//   if (whyDidYouRender && typeof whyDidYouRender === 'function') {
+//     const ush = require('./util/use-selector').default
+//     whyDidYouRender(React, {
+//       // TODO reduce these
+//       exclude: [
+//         /Pressable/,
+//         /Portal/,
+//         /RNSScreen/,
+//         /^Screen$/,
+//         /^Group$/,
+//         /^View$/,
+//         /^AnimatedComponentWrapper$/,
+//         /CardContainer/,
+//         /StaticContainer/,
+//       ],
+//       include: [
+//         /^ConnectFunction/,
+//         // uncomment to watch everything, realllllly slows things down
+//         // /.*/,
+//       ],
+//       // logOnDifferentValues: true,
+//       trackAllPureComponents: true,
+//       trackExtraHooks: [[ush, 'useSelector']],
+//     })
+//   }
+// }
 
 export {}
