76ea966111b6cd5dd4062efab3c6c8b416a9c02f
chrisnojima <cnojima@keyba.se>
2022-12-21 18:36:45 -0500

    usernames refactor (#25370)

--- shared/common-adapters/usernames.tsx
+++ shared/common-adapters/usernames.tsx
@@ -3,7 +3,6 @@ import * as Container from '../util/container'
 import * as Styles from '../styles'
 import * as ProfileGen from '../actions/profile-gen'
 import * as Tracker2Gen from '../actions/tracker2-gen'
-import * as UsersConstants from '../constants/users'
 import Text, {
   type TextType,
   type Background,
@@ -32,12 +31,11 @@ export type Props = {
   containerStyle?: Styles.StylesCrossPlatform
   inline?: boolean
   inlineGrammar?: boolean
-  joinerStyle?: Styles.StylesCrossPlatform
+  joinerStyle?: StylesTextCrossPlatform
   lineClamp?: LineClampType
   notFollowingColorOverride?: AllowedColors
   onUsernameClicked?: ((username: string) => void) | 'tracker' | 'profile'
   prefix?: string | null
-  redColor?: AllowedColors
   selectable?: boolean
   showAnd?: boolean
   skipSelf?: boolean
@@ -60,210 +58,266 @@ const space = Styles.isMobile ? ` ` : <>&nbsp;</>
 let WithProfileCardPopup: React.ComponentType<any> | null
 export const _setWithProfileCardPopup = (Comp: React.ComponentType<any>) => (WithProfileCardPopup = Comp)
 
-const UsernameText = (
-  props: Omit<Props, 'users' | 'onUsernameClicked'> & {
-    onUsernameClicked: undefined | ((s: string) => void)
-    users: Array<User>
-  }
-) => {
-  const derivedJoinerStyle = Styles.collapseStyles([
-    props.joinerStyle,
-    styles.joinerStyle,
-    {color: props.commaColor},
-  ])
+type UsernameProps = {
+  backgroundMode?: Background
+  colorBroken: boolean
+  colorFollowing: boolean
+  colorYou?: boolean | AllowedColors
+  inline?: boolean
+  joinerStyle?: StylesTextCrossPlatform
+  lineClamp?: LineClampType
+  notFollowingColorOverride?: AllowedColors
+  onUsernameClicked?: ((username: string) => void) | 'tracker' | 'profile'
+  selectable?: boolean
+  underline?: boolean
+  showAnd: boolean
+  showComma: boolean
+  showSpace: boolean
+  style?: StylesTextCrossPlatform
+  type: TextType
+  username: string
+  virtualText?: boolean // desktop only see text.desktop
+  you: string
+  withProfileCardPopup: boolean
+}
+const Username = React.memo(function Username(p: UsernameProps) {
+  const {colorFollowing, colorBroken, username, notFollowingColorOverride, colorYou} = p
+  const {inline, style, lineClamp, selectable, type, backgroundMode, showAnd, underline} = p
+  const {onUsernameClicked, joinerStyle, showComma, showSpace, virtualText, withProfileCardPopup} = p
+  const you = p.you === username
+  const following = Container.useSelector(state => colorFollowing && state.config.following.has(username))
+  const broken = Container.useSelector(
+    state => (colorBroken && state.users.infoMap.get(username)?.broken) ?? false
+  )
 
+  const dispatch = Container.useDispatch()
+
+  const onOpenProfile = React.useCallback(
+    (evt: any) => {
+      evt?.stopPropagation()
+      dispatch(ProfileGen.createShowUserProfile({username}))
+    },
+    [dispatch, username]
+  )
+  const onOpenTracker = React.useCallback(
+    (evt: any) => {
+      evt?.stopPropagation()
+      dispatch(Tracker2Gen.createShowUser({asTracker: true, username}))
+    },
+    [dispatch, username]
+  )
+  const onPassThrough = React.useCallback(() => {
+    if (typeof onUsernameClicked === 'function') {
+      onUsernameClicked(username)
+    }
+  }, [username, onUsernameClicked])
+  let onClicked: undefined | ((evt?: any) => void)
+  switch (onUsernameClicked) {
+    case 'tracker':
+      onClicked = onOpenTracker
+      break
+    case 'profile':
+      onClicked = onOpenProfile
+      break
+    default:
+      if (typeof onUsernameClicked === 'function') {
+        onClicked = onPassThrough
+      }
+  }
+
+  let userStyle = Styles.platformStyles({
+    common: {
+      ...(colorFollowing && !you
+        ? ({
+            color: following
+              ? Styles.globalColors.greenDark
+              : notFollowingColorOverride || Styles.globalColors.blueDark,
+          } as const)
+        : null),
+      ...(colorBroken && broken && !you ? ({color: Styles.globalColors.redDark} as const) : null),
+      ...(colorYou && you
+        ? ({
+            color: typeof colorYou === 'string' ? colorYou : Styles.globalColors.black,
+          } as const)
+        : null),
+    },
+    isElectron: inline ? {display: 'inline'} : {},
+  })
+  userStyle = Styles.collapseStyles([userStyle, style, type.startsWith('Body') && styles.kerning] as const)
+
+  // Make sure onClick is undefined when _onUsernameClicked is, so
+  // as to not override any existing onClick handler from containers
+  // on native. (See DESKTOP-3963.)
+  const isNegative = backgroundModeIsNegative(backgroundMode || null)
+  const renderText = (onLongPress?: () => void) => (
+    // type is set to Body here to prevent unwanted hover behaviors
+    // line height is unset to prevent some text clipping issues
+    // in children with larger text styles on Android (HOTPOT-2112)
+    // see also https://github.com/keybase/client/pull/22331#discussion_r374224355
+    <Text className="noLineHeight" type="Body" style={styles.noLineHeight} key={username}>
+      {showAnd && (
+        <Text type={type} negative={isNegative} style={joinerStyle} underlineNever={true}>
+          {'and '}
+        </Text>
+      )}
+      <Text
+        type={type}
+        negative={isNegative}
+        className={Styles.classNames({'hover-underline': underline})}
+        selectable={selectable}
+        onLongPress={onLongPress}
+        lineClamp={lineClamp}
+        virtualText={virtualText}
+        onClick={onClicked}
+        style={userStyle}
+      >
+        {assertionToDisplay(username)}
+      </Text>
+      {/* Injecting the commas here so we never wrap and have newlines starting with a , */}
+      {showComma && (
+        <Text type={type} negative={isNegative} style={joinerStyle}>
+          ,
+        </Text>
+      )}
+      {showSpace && (
+        <Text type={type} negative={isNegative} style={joinerStyle}>
+          {space}
+        </Text>
+      )}
+    </Text>
+  )
+
+  return withProfileCardPopup && WithProfileCardPopup ? (
+    <WithProfileCardPopup key={username} username={username} ellipsisStyle={styles.inlineStyle}>
+      {renderText}
+    </WithProfileCardPopup>
+  ) : (
+    renderText()
+  )
+})
+
+type UsernamesTextProps = {
+  users: Array<string>
+  backgroundMode?: Background
+  colorBroken: boolean
+  colorFollowing: boolean
+  colorYou?: boolean | AllowedColors
+  commaColor?: AllowedColors
+  inlineGrammar?: boolean
+  joinerStyle?: StylesTextCrossPlatform
+  notFollowingColorOverride?: AllowedColors
+  onUsernameClicked?: ((username: string) => void) | 'tracker' | 'profile' | undefined
+  selectable?: boolean
+  inline?: boolean
+  type: TextType
+  showAnd?: boolean
+  underline: boolean
+  virtualText?: boolean
+  withProfileCardPopup: boolean
+  you: string
+}
+const UsernamesText = (p: UsernamesTextProps) => {
+  const {showAnd, inlineGrammar, users, joinerStyle, commaColor, ...rest} = p
+  const derivedJoinerStyle = React.useMemo(() => {
+    return Styles.collapseStyles([joinerStyle, styles.joinerStyle, {color: commaColor}])
+  }, [commaColor, joinerStyle])
+
+  const lastIdx = users.length - 1
   return (
     <>
-      {props.users.map((u, i) => {
-        let userStyle = Styles.platformStyles({
-          common: {
-            ...(props.colorFollowing && !u.you
-              ? ({
-                  color: u.following
-                    ? Styles.globalColors.greenDark
-                    : props.notFollowingColorOverride || Styles.globalColors.blueDark,
-                } as const)
-              : null),
-            ...(props.colorBroken && u.broken && !u.you
-              ? ({color: props.redColor || Styles.globalColors.redDark} as const)
-              : null),
-            ...(props.colorYou && u.you
-              ? ({
-                  color: typeof props.colorYou === 'string' ? props.colorYou : Styles.globalColors.black,
-                } as const)
-              : null),
-          },
-          isElectron: props.inline ? {display: 'inline'} : {},
-        })
-        userStyle = Styles.collapseStyles([
-          userStyle,
-          props.style,
-          props.type.startsWith('Body') && styles.kerning,
-        ] as const)
-
-        // Make sure onClick is undefined when _onUsernameClicked is, so
-        // as to not override any existing onClick handler from containers
-        // on native. (See DESKTOP-3963.)
-        const onUsernameClicked = props.onUsernameClicked
-        const isNegative = backgroundModeIsNegative(props.backgroundMode || null)
-        const renderText = (onLongPress?: () => void) => (
-          // type is set to Body here to prevent unwanted hover behaviors
-          // line height is unset to prevent some text clipping issues
-          // in children with larger text styles on Android (HOTPOT-2112)
-          // see also https://github.com/keybase/client/pull/22331#discussion_r374224355
-          <Text className="noLineHeight" type="Body" style={{lineHeight: undefined}} key={u.username}>
-            {i !== 0 && i === props.users.length - 1 && props.showAnd && (
-              <Text type={props.type} negative={isNegative} style={derivedJoinerStyle} underlineNever={true}>
-                {'and '}
-              </Text>
-            )}
-            <Text
-              type={props.type}
-              negative={isNegative}
-              className={Styles.classNames({'hover-underline': props.underline})}
-              selectable={props.selectable}
-              onLongPress={onLongPress}
-              lineClamp={props.lineClamp}
-              virtualText={props.virtualText}
-              onClick={
-                onUsernameClicked
-                  ? evt => {
-                      evt && evt.stopPropagation()
-                      onUsernameClicked(u.username)
-                    }
-                  : undefined
-              }
-              style={userStyle}
-            >
-              {assertionToDisplay(u.username)}
-            </Text>
-            {/* Injecting the commas here so we never wrap and have newlines starting with a , */}
-            {i !== props.users.length - 1 && (!props.inlineGrammar || props.users.length > 2) && (
-              <Text type={props.type} negative={isNegative} style={derivedJoinerStyle}>
-                ,
-              </Text>
-            )}
-            {i !== props.users.length - 1 && (
-              <Text type={props.type} negative={isNegative} style={derivedJoinerStyle}>
-                {space}
-              </Text>
-            )}
-          </Text>
-        )
-
-        return props.withProfileCardPopup && WithProfileCardPopup ? (
-          <WithProfileCardPopup key={u.username} username={u.username} ellipsisStyle={styles.inlineStyle}>
-            {renderText}
-          </WithProfileCardPopup>
-        ) : (
-          renderText()
+      {users.map((u, i) => {
+        const sa = !!showAnd && i !== 0 && i === lastIdx
+        const showComma = i !== lastIdx && (!inlineGrammar || users.length > 2)
+        const showSpace = i !== lastIdx
+        return (
+          <Username
+            key={u}
+            {...rest}
+            username={u}
+            showAnd={sa}
+            showComma={showComma}
+            showSpace={showSpace}
+            joinerStyle={derivedJoinerStyle}
+          />
         )
       })}
     </>
   )
 }
-UsernameText.defaultProps = {
-  colorBroken: true,
-  inlineGrammar: false,
-  selectable: undefined,
-  showAnd: false,
-  underline: true,
-  withProfileCardPopup: true,
-}
 
 const inlineProps = Styles.isMobile ? {lineClamp: 1 as const} : {}
 
 const Usernames = React.memo(
-  function Usernames(props: Props) {
-    const styleContext = React.useContext(Styles.StyleContext)
-    const containerStyle: Styles.StylesCrossPlatform = props.inline
-      ? (styles.inlineStyle as any)
-      : (styles.nonInlineStyle as any)
-    const bgMode = props.backgroundMode || null
-    const isNegative = backgroundModeIsNegative(bgMode)
-
-    const dispatch = Container.useDispatch()
-
-    const onOpenProfile = (username: string) => dispatch(ProfileGen.createShowUserProfile({username}))
-    const onOpenTracker = (username: string) =>
-      dispatch(Tracker2Gen.createShowUser({asTracker: true, username}))
+  function Usernames(p: Props) {
+    const {backgroundMode, commaColor, inline, containerStyle} = p
+    const {joinerStyle, lineClamp, notFollowingColorOverride, onUsernameClicked, prefix, selectable} = p
+    const {showAnd, inlineGrammar, colorYou, skipSelf, style, suffix, suffixType, title} = p
+    const {usernames, fixOverdraw, virtualText, type} = p
+    const colorFollowing = p.colorFollowing ?? true
+    const colorBroken = p.colorBroken ?? true
+    const underline = p.underline ?? true
+    const withProfileCardPopup = p.withProfileCardPopup ?? true
     const you = Container.useSelector(state => state.config.username)
 
-    const usernamesArray = typeof props.usernames === 'string' ? [props.usernames] : props.usernames
-    const followingArray = Container.useSelector(state => {
-      const {following} = state.config
-      return usernamesArray.map(user => following.has(user))
-    }, shallowEqual)
+    const styleContext = React.useContext(Styles.StyleContext)
+    const containerStyle2: Styles.StylesCrossPlatform = inline
+      ? (styles.inlineStyle as any)
+      : (styles.nonInlineStyle as any)
+    const bgMode = backgroundMode || null
+    const isNegative = backgroundModeIsNegative(bgMode)
 
-    const brokenArray = Container.useSelector(state => {
-      const {infoMap} = state.users
-      return usernamesArray.map(user => UsersConstants.getIsBroken(infoMap, user) || false)
-    }, shallowEqual)
-
-    const users = usernamesArray.reduce<Array<User>>((arr, username, idx) => {
-      const isYou = you === username
-      if (!props.skipSelf || !isYou) {
-        arr.push({
-          broken: brokenArray[idx],
-          following: followingArray[idx],
-          username,
-          you: isYou,
-        })
-      }
-      return arr
-    }, [])
-
-    const rwers = users.filter(u => !u.readOnly)
-    const readers = users.filter(u => !!u.readOnly)
-
-    let onUsernameClicked: undefined | ((s: string) => void)
-    switch (props.onUsernameClicked) {
-      case 'tracker':
-        onUsernameClicked = onOpenTracker
-        break
-      case 'profile':
-        onUsernameClicked = onOpenProfile
-        break
-      default:
-        if (typeof props.onUsernameClicked === 'function') {
-          onUsernameClicked = props.onUsernameClicked
+    const names = React.useMemo(() => {
+      const n = typeof usernames === 'string' ? [usernames] : usernames
+      return n.reduce<Array<string>>((arr, n) => {
+        if (n !== you || !skipSelf) {
+          arr.push(n)
         }
-    }
+        return arr
+      }, [])
+    }, [usernames, skipSelf, you])
 
     return (
       <Text
-        type={props.type}
+        type={type}
         negative={isNegative}
-        fixOverdraw={props.fixOverdraw === 'auto' ? styleContext.canFixOverdraw : props.fixOverdraw ?? false}
-        style={Styles.collapseStyles([containerStyle, props.containerStyle])}
-        title={props.title}
+        fixOverdraw={fixOverdraw === 'auto' ? styleContext.canFixOverdraw : fixOverdraw ?? false}
+        style={Styles.collapseStyles([containerStyle2, containerStyle])}
+        title={title}
         ellipsizeMode="tail"
-        lineClamp={props.lineClamp}
-        {...(props.inline ? inlineProps : {})}
+        lineClamp={lineClamp}
+        {...(inline ? inlineProps : {})}
       >
-        {!!props.prefix && (
-          <Text type={props.type} negative={isNegative} style={props.style}>
-            {props.prefix}
+        {!!prefix && (
+          <Text type={type} negative={isNegative} style={style}>
+            {prefix}
           </Text>
         )}
-        <UsernameText {...props} onUsernameClicked={onUsernameClicked} users={rwers} />
-        {!!readers.length && (
+        <UsernamesText
+          backgroundMode={backgroundMode}
+          colorBroken={colorBroken}
+          colorFollowing={colorFollowing}
+          colorYou={colorYou}
+          commaColor={commaColor}
+          inlineGrammar={inlineGrammar}
+          joinerStyle={joinerStyle}
+          notFollowingColorOverride={notFollowingColorOverride}
+          onUsernameClicked={onUsernameClicked}
+          selectable={selectable}
+          showAnd={showAnd}
+          underline={underline}
+          type={type}
+          virtualText={virtualText}
+          withProfileCardPopup={withProfileCardPopup}
+          you={you}
+          users={names}
+        />
+        {!!suffix && (
           <Text
-            type={props.type}
+            type={suffixType || type}
             negative={isNegative}
-            style={Styles.collapseStyles([props.style, {marginRight: 1}])}
+            style={Styles.collapseStyles([style, {marginLeft: Styles.globalMargins.xtiny}])}
           >
-            #
-          </Text>
-        )}
-        <UsernameText {...props} onUsernameClicked={onUsernameClicked} users={readers} />
-        {!!props.suffix && (
-          <Text
-            type={props.suffixType || props.type}
-            negative={isNegative}
-            style={Styles.collapseStyles([props.style, {marginLeft: Styles.globalMargins.xtiny}])}
-          >
-            {props.suffix}
+            {suffix}
           </Text>
         )}
       </Text>
@@ -307,10 +361,9 @@ const styles = Styles.styleSheetCreate(() => ({
       whiteSpace: 'nowrap',
     },
   } as const),
-  joinerStyle: Styles.platformStyles({
-    isElectron: {textDecoration: 'none'},
-  } as const),
+  joinerStyle: Styles.platformStyles({isElectron: {textDecoration: 'none'}} as const),
   kerning: {letterSpacing: 0.2},
+  noLineHeight: {lineHeight: undefined},
   nonInlineStyle: Styles.platformStyles({
     common: {
       ...Styles.globalStyles.flexBoxRow,
@@ -320,5 +373,4 @@ const styles = Styles.styleSheetCreate(() => ({
   } as const),
 }))
 
-export {UsernameText}
 export default Usernames
