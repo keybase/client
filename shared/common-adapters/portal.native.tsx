2a9642b5fa166843e24f613183e5517d558f51ac
chrisnojima <cnojima@keyba.se>
2023-04-10 10:45:38 -0400

    fix missing safe area in fullscreen overlay in portals

--- shared/common-adapters/portal.native.tsx
+++ shared/common-adapters/portal.native.tsx
@@ -1,6 +1,8 @@
 import {Portal as GPortal} from '@gorhom/portal'
 import * as Styles from '../styles'
 import {FullWindowOverlay} from 'react-native-screens'
+import {SafeAreaView} from '../common-adapters'
+import {SafeAreaProvider, initialWindowMetrics} from 'react-native-safe-area-context'
 
 // useFullScreenOverlay=false is useful to stick to an onscreen element like the audio recorder
 // otherwise you want it to be true so you can go above modals, aka the ... menu in an image attachment modal
@@ -9,11 +11,22 @@ export const Portal = (p: {children: React.ReactNode; hostName?: string; useFull
   const fullWindow = (useFullScreenOverlay ?? true) && Styles.isIOS
   return fullWindow ? (
     <GPortal hostName={hostName}>
-      <FullWindowOverlay>{children}</FullWindowOverlay>
+      <FullWindowOverlay>
+        <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+          <SafeAreaView style={styles.safe}>{children}</SafeAreaView>
+        </SafeAreaProvider>
+      </FullWindowOverlay>
     </GPortal>
   ) : (
     <GPortal hostName={hostName}>{children}</GPortal>
   )
 }
+
+const styles = Styles.styleSheetCreate(
+  () =>
+    ({
+      safe: {flex: 1},
+    } as const)
+)
 
 export {PortalHost, PortalProvider} from '@gorhom/portal'
