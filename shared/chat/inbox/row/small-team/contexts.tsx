15cd845c1f21bd43e4bbaf1c088baa6cbba41960
chrisnojima <cnojima@keyba.se>
2023-04-19 12:28:41 -0400

    no kb titles, snippet decorations (#25638)
    * no keyboard when showing get titles
    
    * fix decorated plumbing

--- shared/chat/inbox/row/small-team/contexts.tsx
+++ shared/chat/inbox/row/small-team/contexts.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react'
 import {ConvoIDContext} from '../../../conversation/messages/ids-context'
+import * as RPCTypes from '../../../../constants/types/rpc-chat-gen'
 // so popups will work in both places
 export const ConversationIDKeyContext = ConvoIDContext
 export const SnippetContext = React.createContext('')
+export const SnippetDecorationContext = React.createContext(RPCTypes.SnippetDecoration.none)
 export const ParticipantsContext = React.createContext<Array<string> | string>('')
 export const IsTeamContext = React.createContext(false)
 export const TimeContext = React.createContext(0)
