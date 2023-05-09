d201fccdb26ec6bc82d95fc997513e55356d641e
chrisnojima <cnojima@keyba.se>
2023-05-09 11:46:31 -0400

    WIP

--- shared/common-adapters/image2.native.tsx
+++ shared/common-adapters/image2.native.tsx
@@ -4,12 +4,10 @@ import type {Props} from './image2'
 import {Image} from 'expo-image'
 
 const Image2 = (p: Props) => {
-  console.log('aaa image2', p)
   const {showLoadingStateUntilLoaded = true, src, onLoad, /*onError, */ style} = p
   const [loading, setLoading] = React.useState(true)
   const _onLoad = React.useCallback(
     (e: any) => {
-      console.log('aaa onloaded', e)
       setLoading(false)
       onLoad?.(e)
     },
@@ -17,7 +15,7 @@ const Image2 = (p: Props) => {
   )
 
   const onError = e => {
-    console.log('aaa onerror', e)
+    console.log('Image2 load error', e)
   }
 
   return (
@@ -27,10 +25,8 @@ const Image2 = (p: Props) => {
         style={
           // eslint-disable-next-line
           style as any
-          // {width: 200, height: 200, backgroundColor: 'red'}
         }
         onLoad={_onLoad}
-        cachePolicy="none"
         contentFit="contain"
         onError={onError}
       />
