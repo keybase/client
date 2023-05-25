5dbcd32f92e7eae58a962ed6f9c57d19e3b3e183
chrisnojima <cnojima@keyba.se>
2023-05-25 10:47:04 -0400

    missing bg on emoji

--- shared/chat/emoji-picker/container.tsx
+++ shared/chat/emoji-picker/container.tsx
@@ -336,6 +336,7 @@ const styles = Styles.styleSheetCreate(
           paddingRight: Styles.globalMargins.small,
         },
         isElectron: {
+          backgroundColor: Styles.globalColors.blueGrey,
           height: Styles.globalMargins.xlarge + Styles.globalMargins.xtiny,
         },
         isMobile: {
