045a94b5abb633b8c8cd36adfbf849fe2299f969
chrisnojima <cnojima@keyba.se>
2023-05-24 22:19:57 -0400

    WIP

--- shared/settings/root-phone.page.tsx
+++ shared/settings/root-phone.page.tsx
@@ -1,16 +1,11 @@
 import * as React from 'react'
-import type * as Container from '../util/container'
 
 const Root = React.lazy(async () => import('./root-phone'))
-type OwnProps = Container.ViewPropsToPageProps<typeof Root>
+const getOptions = () => ({title: 'More'})
 
-const getOptions = () => ({
-  title: 'More',
-})
-
-const Screen = (p: OwnProps) => (
+const Screen = () => (
   <React.Suspense>
-    <Root {...p.route.params} />
+    <Root />
   </React.Suspense>
 )
 
