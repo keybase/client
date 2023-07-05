e1f0efead6cce40485ca429797e8e5fa0cca4419
chrisnojima <cnojima@keyba.se>
2023-07-05 16:49:15 -0400

    WIP

--- shared/constants/recover-password.tsx
+++ shared/constants/recover-password.tsx
@@ -76,7 +76,7 @@ export const useState = Z.createZustand<State>((set, get) => {
       set(s => ({
         ...s,
         ...initialStore,
-        // ...dispatchOverrides,
+        ...dispatchOverrides,
       }))
     },
     startRecoverPassword: p => {
