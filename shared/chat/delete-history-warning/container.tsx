0af8c89ec8eefb98c65f31c9778fac04449e13b7
chrisnojima <cnojima@keyba.se>
2023-08-08 09:44:05 -0400

    WIP

--- shared/chat/delete-history-warning/container.tsx
+++ shared/chat/delete-history-warning/container.tsx
@@ -1,8 +1,12 @@
 import * as RouterConstants from '../../constants/router2'
 import * as Constants from '../../constants/chat2'
+import type * as Types from '../../constants/types/chat2'
 import DeleteHistoryWarning from '.'
 
-export default () => {
+type OwnProps = {conversationIDKey: Types.ConversationIDKey}
+
+// props needed by page for injection
+export default (_ownProps: OwnProps) => {
   const navigateUp = RouterConstants.useState(s => s.dispatch.navigateUp)
   const onCancel = () => {
     navigateUp()
--- shared/chat/delete-history-warning/page.tsx
+++ shared/chat/delete-history-warning/page.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react'
 import type * as Container from '../../util/container'
+import * as Constants from '../../constants/chat2'
 
 const Warning = React.lazy(async () => import('./container'))
 type OwnProps = Container.ViewPropsToPageProps<typeof Warning>
 
 const Screen = (p: OwnProps) => (
   <React.Suspense>
-    <Warning {...p.route.params} />
+    <Constants.Provider id={p.route.params.conversationIDKey}>
+      <Warning {...p.route.params} />
+    </Constants.Provider>
   </React.Suspense>
 )
 
