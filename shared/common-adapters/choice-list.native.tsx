d0171cb1826612f9d5dbd487f9f0fb57d3149571
chrisnojima <cnojima@keyba.se>
2023-10-04 14:40:41 -0400

    WIP

--- shared/common-adapters/choice-list.native.tsx
+++ shared/common-adapters/choice-list.native.tsx
@@ -3,11 +3,18 @@ import ClickableBox from './clickable-box'
 import Icon, {type IconType} from './icon'
 import Text from './text'
 import * as React from 'react'
-import {globalStyles, globalColors, globalMargins} from '../styles'
+import * as Styles from '../styles'
 import type {Props} from './choice-list'
 
 type State = {
   activeIndex?: number
+}
+
+const Kb = {
+  Box,
+  ClickableBox,
+  Icon,
+  Text,
 }
 
 class ChoiceList extends React.Component<Props, State> {
@@ -22,74 +29,74 @@ class ChoiceList extends React.Component<Props, State> {
   render() {
     const {options} = this.props
     return (
-      <Box>
+      <Kb.Box>
         {options.map((op, idx) => {
           // @ts-ignore
           const iconType: IconType = op.icon
           return (
-            <ClickableBox
+            <Kb.ClickableBox
               key={idx}
-              underlayColor={globalColors.blueLighter2}
+              underlayColor={Styles.globalColors.blueLighter2}
               onClick={op.onClick}
               onPressIn={() => this.setState({activeIndex: idx})}
               onPressOut={() => this.setState({activeIndex: undefined})}
             >
-              <Box style={styleEntry}>
-                <Box style={styleIconContainer(this.state.activeIndex === idx)}>
+              <Kb.Box style={styleEntry}>
+                <Kb.Box style={styleIconContainer(this.state.activeIndex === idx)}>
                   {typeof op.icon === 'string' ? (
                     <Icon style={styleIcon} type={iconType} />
                   ) : (
-                    <Box style={styleIcon}>{op.icon}</Box>
+                    <Kb.Box style={styleIcon}>{op.icon}</Kb.Box>
                   )}
-                </Box>
-                <Box style={styleInfoContainer}>
-                  <Text style={styleInfoTitle} type="Header">
+                </Kb.Box>
+                <Kb.Box style={styleInfoContainer}>
+                  <Kb.Text style={styleInfoTitle} type="Header">
                     {op.title}
-                  </Text>
-                  <Text type="Body">{op.description}</Text>
-                </Box>
-              </Box>
-            </ClickableBox>
+                  </Kb.Text>
+                  <Kb.Text type="Body">{op.description}</Kb.Text>
+                </Kb.Box>
+              </Kb.Box>
+            </Kb.ClickableBox>
           )
         })}
-      </Box>
+      </Kb.Box>
     )
   }
 }
 
 const styleEntry = {
-  ...globalStyles.flexBoxRow,
-  paddingBottom: globalMargins.tiny,
-  paddingLeft: globalMargins.small,
-  paddingRight: globalMargins.small,
-  paddingTop: globalMargins.tiny,
+  ...Styles.globalStyles.flexBoxRow,
+  paddingBottom: Styles.globalMargins.tiny,
+  paddingLeft: Styles.globalMargins.small,
+  paddingRight: Styles.globalMargins.small,
+  paddingTop: Styles.globalMargins.tiny,
 }
 
 const styleIconContainer = (active: boolean) => ({
-  ...globalStyles.flexBoxColumn,
+  ...Styles.globalStyles.flexBoxColumn,
   alignItems: 'center',
   alignSelf: 'center',
-  borderRadius: (globalMargins.large + globalMargins.medium) / 2,
-  height: globalMargins.large + globalMargins.medium,
+  borderRadius: (Styles.globalMargins.large + Styles.globalMargins.medium) / 2,
+  height: Styles.globalMargins.large + Styles.globalMargins.medium,
   justifyContent: 'center',
-  ...(active ? {} : {backgroundColor: globalColors.greyLight}),
-  width: globalMargins.large + globalMargins.medium,
+  ...(active ? {} : {backgroundColor: Styles.globalColors.greyLight}),
+  width: Styles.globalMargins.large + Styles.globalMargins.medium,
 })
 
 const styleIcon = {
-  height: globalMargins.large,
-  width: globalMargins.large,
+  height: Styles.globalMargins.large,
+  width: Styles.globalMargins.large,
 }
 
 const styleInfoContainer = {
-  ...globalStyles.flexBoxColumn,
+  ...Styles.globalStyles.flexBoxColumn,
   flex: 1,
   justifyContent: 'center',
-  marginLeft: globalMargins.small,
+  marginLeft: Styles.globalMargins.small,
 }
 
 const styleInfoTitle = {
-  color: globalColors.blueDark,
+  color: Styles.globalColors.blueDark,
 }
 
 export default ChoiceList
