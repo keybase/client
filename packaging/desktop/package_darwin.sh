da4417b25ec0b7f0417427572210ceba6f8a2a02
Mike Maxim <mike@keyba.se>
2023-10-17 16:17:55 -0400

    restore stapler

--- packaging/desktop/package_darwin.sh
+++ packaging/desktop/package_darwin.sh
@@ -298,6 +298,7 @@ notarize_dmg() {(
   fi
   echo "Uploading $dmg_name to notarization service in $out_dir"
   xcrun notarytool submit "$out_dir/$dmg_name" --keychain-profile "NOTARY_TOOL" --wait
+  xcrun stapler staple "$out_dir/$dmg_name" 
   echo "Successfully uploaded to notarization service"
 )}
 
