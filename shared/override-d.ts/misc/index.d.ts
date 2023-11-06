209e087582f3cb07f095be2236cbc8bc90c99622
chrisnojima <cnojima@keyba.se>
2023-11-06 15:42:00 -0500

    WIP

--- shared/override-d.ts/misc/index.d.ts
+++ shared/override-d.ts/misc/index.d.ts
@@ -51,3 +51,35 @@ declare module 'emoji-datasource-apple/img/apple/sheets/64.png' {
 declare module 'react-is' {
   export const isValidElementType: (a: unknown) => boolean
 }
+
+declare module 'fs-extra' {
+  import type {StatSyncFn, copyFileSync} from 'fs'
+  export const copy: (src: string, dst: string) => Promise<void>
+  export const copySync: (src: string, dst: string, options?: {dereference?: boolean}) => void
+  export const removeSync: (src: string) => void
+  export const statSync: StatSyncFn
+  export const writeJsonSync: (dst: string, o: {}) => void
+}
+
+declare module 'mousetrap' {
+  export const bind: (
+    keys: Array<string> | string,
+    cb: (e: {stopPropagation: () => void}, key: string) => void,
+    type: 'keydown'
+  ) => void
+  export const unbind: (keys: Array<string> | string) => void
+}
+
+declare module 'url-parse' {
+  export class URLParse {
+    constructor(url: string)
+    hostname: string
+    protocol: string
+    username: string
+    port: string
+    pathname: string
+    query: string
+    password: string
+  }
+  export default URLParse
+}
