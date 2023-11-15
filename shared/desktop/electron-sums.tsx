cb176549eab2fe5628a53b46b8bfabee938e0ac6
chrisnojima <cnojima@keyba.se>
2023-11-15 10:22:27 -0500

    WIP

--- shared/desktop/electron-sums.tsx
+++ shared/desktop/electron-sums.tsx
@@ -1,10 +1,10 @@
- // Generated with: ./extract-electron-shasums.sh {ver}
- // prettier-ignore
- export const electronChecksums = {
-  ['electron-v27.0.4-darwin-arm64.zip']: 'd43d26a36bbbe55fd9d2cbb66108bd6ebf341957c54443579073836473c6a1ba',
-  ['electron-v27.0.4-darwin-x64.zip']: '0995873e9dfba6dff4ad8a8ac8c585124409471ee999068f05ff16fdd30ccf6f',
-  ['electron-v27.0.4-linux-arm64.zip']: '9aa828f20921fe6dcebe2d08cb8c2d9e35d8805452d9df3b7833f5562e72bd68',
-  ['electron-v27.0.4-linux-x64.zip']: '62a794f5a90ac07b9a8a4e32ae349fa71e0fa58b2a3e3f12a992dc46a12e86b2',
-  ['electron-v27.0.4-win32-x64.zip']: '5a5c9841b6522604937e46a3788f1041fc1521532876d0290e13a8f8d968ca7d',
-  ['hunspell_dictionaries.zip']: 'ad7629c272348a9b0d62f3da4a25b0b4516d492e0e7ff282ad8555949eb5349a',
+// Generated with: ./extract-electron-shasums.sh {ver}
+// prettier-ignore
+export const electronChecksums = {
+  'electron-v27.0.4-darwin-arm64.zip': 'd43d26a36bbbe55fd9d2cbb66108bd6ebf341957c54443579073836473c6a1ba',
+  'electron-v27.0.4-darwin-x64.zip': '0995873e9dfba6dff4ad8a8ac8c585124409471ee999068f05ff16fdd30ccf6f',
+  'electron-v27.0.4-linux-arm64.zip': '9aa828f20921fe6dcebe2d08cb8c2d9e35d8805452d9df3b7833f5562e72bd68',
+  'electron-v27.0.4-linux-x64.zip': '62a794f5a90ac07b9a8a4e32ae349fa71e0fa58b2a3e3f12a992dc46a12e86b2',
+  'electron-v27.0.4-win32-x64.zip': '5a5c9841b6522604937e46a3788f1041fc1521532876d0290e13a8f8d968ca7d',
+  'hunspell_dictionaries.zip': 'ad7629c272348a9b0d62f3da4a25b0b4516d492e0e7ff282ad8555949eb5349a',
 }
--- shared/desktop/extract-electron-shasums.sh
+++ shared/desktop/extract-electron-shasums.sh
@@ -9,13 +9,13 @@ url="https://github.com/electron/electron/releases/download/v$version/SHASUMS256
 sums="$(curl -L -s "$url")"
 echo "$sums" | awk '
 BEGIN {
-  print " // Generated with: ./extract-electron-shasums.sh {ver}";
-  print " // prettier-ignore";
-  print " export const electronChecksums = {";
+  print "// Generated with: ./extract-electron-shasums.sh {ver}";
+  print "// prettier-ignore";
+  print "export const electronChecksums = {";
 }
 /electron-.*-(darwin-arm64|darwin-x64|linux-arm64|linux-x64|win32-x64)\.zip|hunspell_dictionaries\.zip/ {
   gsub(/\*/, "", $2);
-  print "  [\x27"$2"\x27]: \x27"$1"\x27,";
+  print "  \x27"$2"\x27: \x27"$1"\x27,";
 }
 END {
   print "}";
