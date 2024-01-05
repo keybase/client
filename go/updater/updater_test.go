f0ad7d6b7560419504815a5a1e9d4f25956ff169
Joshua Blum <jblum18@gmail.com>
2024-01-05 15:16:45 -0500

    Fix lint, skip updater_test on windows

--- go/updater/updater_test.go
+++ go/updater/updater_test.go
@@ -1,5 +1,5 @@
-// Copyright 2015 Keybase, Inc. All rights reserved. Use of
-// this source code is governed by the included BSD license.
+//go:build !windows
+// +build !windows
 
 package updater
 
