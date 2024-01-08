4ee9eaa793d0ad49c493f3e7e8fdf74e6bf1cb16
Joshua Blum <jblum18@gmail.com>
2024-01-08 12:58:36 -0500

    exclude more on windows

--- go/updater/command/command_test.go
+++ go/updater/command/command_test.go
@@ -1,6 +1,9 @@
 // Copyright 2016 Keybase, Inc. All rights reserved. Use of
 // this source code is governed by the included BSD license.
 
+//go:build !windows
+// +build !windows
+
 package command
 
 import (
