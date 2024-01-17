4f8f9141a4a514a2bd55f4c9fa7bbfe10cc90d6d
chrisnojima <cnojima@keyba.se>
2024-01-17 13:46:17 -0500

    dont assume about clipboard types (#26171)

--- shared/util/clipboard.desktop.tsx
+++ shared/util/clipboard.desktop.tsx
@@ -1,18 +1,9 @@
 import KB2 from './electron.desktop'
 const {clipboardAvailableFormats} = KB2.functions
 
-export async function readImageFromClipboard(event: React.SyntheticEvent): Promise<Uint8Array | undefined> {
-  const formats = await (clipboardAvailableFormats?.() ?? [])
+export async function readImageFromClipboard(_event: React.SyntheticEvent): Promise<Uint8Array | undefined> {
+  const formats = await (clipboardAvailableFormats?.() ?? new Array<string>())
   console.log('Read clipboard, formats:', formats)
-  const imageFormats = formats.filter(f => f.startsWith('image/'))
-
-  if (imageFormats.length > 0) {
-    event.preventDefault()
-
-    // Read image from Electron clipboard
-    return KB2.functions.readImageFromClipboard?.() ?? undefined
-  } else {
-    // Nothing to read
-    return Promise.resolve(undefined)
-  }
+  // Read image from Electron clipboard
+  return KB2.functions.readImageFromClipboard?.() ?? undefined
 }
