38310e4ae15eed7fb5168b143de0d6b4f8933c7c
chrisnojima <cnojima@keyba.se>
2024-04-08 16:21:22 -0400

    clicking a button that shows a popup should dismiss (#26497)

--- shared/common-adapters/use-popup.tsx
+++ shared/common-adapters/use-popup.tsx
@@ -6,6 +6,9 @@ export type Popup2Parms = {
   showPopup: () => void
   hidePopup: () => void
 }
+
+const tooQuick = 100
+
 export const usePopup2 = (makePopup: (p: Popup2Parms) => React.ReactElement | null) => {
   const [showingPopup, setShowingPopup] = React.useState(false)
   const wasShowingPopupRef = React.useRef(false)
@@ -13,13 +16,25 @@ export const usePopup2 = (makePopup: (p: Popup2Parms) => React.ReactElement | nu
   const [popup, setPopup] = React.useState<React.ReactNode>(null)
   const popupAnchor = React.useRef<MeasureRef>(null)
   const attachTo = popupAnchor
+  const lastToggle = React.useRef(0)
 
   const hidePopup = React.useCallback(() => {
+    const now = Date.now()
+    if (now - lastToggle.current < tooQuick) {
+      return
+    }
+    lastToggle.current = now
     setShowingPopup(false)
   }, [setShowingPopup])
   const showPopup = React.useCallback(() => {
+    const now = Date.now()
+    if (now - lastToggle.current < tooQuick) {
+      return
+    }
+    lastToggle.current = now
     setShowingPopup(true)
   }, [setShowingPopup])
+  const togglePopup = showingPopup ? hidePopup : showPopup
 
   if (showingPopup !== wasShowingPopupRef.current || makePopup !== wasMakePopupRef.current) {
     wasShowingPopupRef.current = showingPopup
@@ -27,5 +42,5 @@ export const usePopup2 = (makePopup: (p: Popup2Parms) => React.ReactElement | nu
     setPopup(showingPopup ? makePopup({attachTo, hidePopup, showPopup}) : null)
   }
 
-  return {hidePopup, popup, popupAnchor, showPopup, showingPopup}
+  return {hidePopup, popup, popupAnchor, showPopup, showingPopup, togglePopup}
 }
