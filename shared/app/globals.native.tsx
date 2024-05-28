8fcdedf6c2f376de74540ee8974db7cd2dd8827a
chrisnojima <cnojima@keyba.se>
2024-05-28 12:41:08 -0400

    631 hotfix (#26909)
    * 631
    
    * polyfill Array.at and Object.hasOwn for older RN engines

--- shared/app/globals.native.tsx
+++ shared/app/globals.native.tsx
@@ -4,9 +4,39 @@
 // what's happening
 import 'fastestsmallesttextencoderdecoder'
 import {encode as btoa, decode as atob} from 'base-64'
+/* eslint-disable */
+// Needs jsc 5.4+
+// @ts-ignore
+if (!Object.hasOwn) {
+  Object.defineProperty(Object, 'hasOwn', {
+    value: function (obj: object, prop: PropertyKey): boolean {
+      return Object.prototype.hasOwnProperty.call(obj, prop)
+    },
+    writable: true,
+    enumerable: false,
+    configurable: true,
+  })
+}
+
+// Needs jsc 5.4+
+// @ts-ignore
+if (!Array.prototype.at) {
+  Object.defineProperty(Array.prototype, 'at', {
+    value: function <T>(this: T[], index: number): T | undefined {
+      if (index < 0) {
+        index = this.length + index
+      }
+      return this[index]
+    },
+    writable: true,
+    enumerable: false,
+    configurable: true,
+  })
+}
 
 global.btoa = btoa
 global.atob = atob
 __FILE_SUFFIX__ = ''
 __PROFILE__ = false
 __HOT__ = false
+/* eslint-enable */
