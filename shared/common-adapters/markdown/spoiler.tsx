5b152ed66a26f80b4f327be4eaffc6e8090f7fef
chrisnojima <cnojima@keyba.se>
2024-06-10 11:42:26 -0400

    mobile

--- shared/common-adapters/markdown/spoiler.tsx
+++ shared/common-adapters/markdown/spoiler.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import * as Styles from '@/styles'
-import Text from '@/common-adapters/text'
+import Text, {type TextType} from '@/common-adapters/text'
+
+export const SpoilerContext = React.createContext<boolean>(false)
 
 type Props = {
   children: React.ReactNode
@@ -37,17 +39,29 @@ const Spoiler = (p: Props) => {
     return showMasked ? Array(len).fill('•').join('') : ''
   }, [showMasked, len])
 
-  return showMasked ? (
-    <Text type="BodySmall">{masked}</Text>
-  ) : (
-    <Text
-      className={shown ? undefined : 'spoiler'}
-      type="BodySmall"
-      onClick={onClick}
-      style={shown ? styles.shown : styles.hidden}
-      title={shown ? '' : 'Click to reveal'}
-    >
-      {children}
+  return (
+    <SpoilerContext.Provider value={!shown}>
+      {showMasked ? (
+        <Text type="BodySmall">{masked}</Text>
+      ) : (
+        <Text
+          className={shown ? undefined : 'spoiler'}
+          type="BodySmall"
+          onClick={onClick}
+          style={shown ? styles.shown : styles.hidden}
+          title={shown ? '' : 'Click to reveal'}
+        >
+          {children || content}
+        </Text>
+      )}
+    </SpoilerContext.Provider>
+  )
+}
+
+export const InnerSpoiled = (p: {children: React.ReactNode; type: TextType; style?: any}) => {
+  return (
+    <Text className={'spoiler'} type="BodySmall" style={Styles.collapseStyles([p.style, styles.hidden])}>
+      {p.children}
     </Text>
   )
 }
