82a5b4b87d312ac199879398d2b3e70d7264df79
chrisnojima <cnojima@keyba.se>
2024-07-26 15:10:35 -0400

    WIP

--- shared/router-v2/router.shared.tsx
+++ shared/router-v2/router.shared.tsx
@@ -140,14 +140,6 @@ export const useShared = () => {
   }
 }
 
-// export const useSharedAfter = (appState: React.MutableRefObject<AppState>) => {
-//   // stuff that happens after the first hook is done
-//   // if we handled NEEDS_INIT we're done
-//   if (appState.current === AppState.NEEDS_INIT) {
-//     appState.current = AppState.INITED
-//   }
-// }
-
 export const SimpleLoading = React.memo(function SimpleLoading() {
   return (
     <Kb.Box2
