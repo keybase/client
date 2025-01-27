32b03af6175e2dee4c61a3fa6715522f2bd6515f
chrisnojima <cnojima@keyba.se>
2025-01-27 15:26:00 -0500

    WIP

--- shared/desktop/electron-sums.tsx
+++ shared/desktop/electron-sums.tsx
@@ -1,10 +1,10 @@
 // Generated with: ./extract-electron-shasums.sh {ver}
 // prettier-ignore
 export const electronChecksums = {
-  'electron-v33.0.2-darwin-arm64.zip': '2d31f140b1a6cfa6efb509e644dee5830601e4d73d47539a8af20d57e8ae7b12',
-  'electron-v33.0.2-darwin-x64.zip': '4bf99f30f124b447ef3b178e5ba0cdfe1faefa6a725bcde76a35eb26a50d4333',
-  'electron-v33.0.2-linux-arm64.zip': '43157d520ff6094ef7c390e49d1eec1ba58a488e150114c08e7157fb321fca7b',
-  'electron-v33.0.2-linux-x64.zip': '933dc1eae3f2db67912b3ab056fd15a7ca3e1328fe8dd6a1f5266c5a063b1121',
-  'electron-v33.0.2-win32-x64.zip': 'a14b5bad4b6b04736c0e6bb3d4ad5ad58fd70483f758ca071d0fc162d4178a4c',
-  'hunspell_dictionaries.zip': '683ca43abade4c16825e027eee3544741367fd3fc93e0bea93fdf24bab1d670b',
+  'electron-v34.0.1-darwin-arm64.zip': 'eec4a6ed82ceb87fa242286971fd921c5d1e824a7b88f93ccfc54ae792f38ab8',
+  'electron-v34.0.1-darwin-x64.zip': '16caca276b8a4f6f36677827136fc7d7884f01889fe2a644da3856f10b623f26',
+  'electron-v34.0.1-linux-arm64.zip': 'db16bcca04ee7082f044d75e5410cb450725cb55c5498fac15c6f92079e856d0',
+  'electron-v34.0.1-linux-x64.zip': 'a80804d6469feeb71f8853cdc699a64dc07b07085c4a4c56eedfb24a3049f221',
+  'electron-v34.0.1-win32-x64.zip': '12a428d144546053725d810e96a15bb3ddeae4580cd1919d9b3a149afefdaa03',
+  'hunspell_dictionaries.zip': 'e5f994c250281939d1adddfb568099977b4acbf566a7bdb73980e345be210c62',
 }
