f3d8159c3c51d581873d9afc173b49d00ac767b3
chrisnojima <cnojima@keyba.se>
2025-02-04 14:57:36 -0500

    WIP

--- shared/profile/edit-profile/index.tsx
+++ shared/profile/edit-profile/index.tsx
@@ -13,89 +13,69 @@ type Props = {
   onSubmit: (bio: string, fullname: string, location: string) => void
 }
 
-type State = {
-  bio: string
-  fullname: string
-  location: string
-}
+const EditProfile = (props: Props) => {
+  const [bio, setBio] = React.useState(props.bio)
+  const [fullname, setFullname] = React.useState(props.fullname)
+  const [location, setLocation] = React.useState(props.location)
 
-class EditProfile extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props)
-    this.state = {
-      bio: props.bio,
-      fullname: props.fullname,
-      location: props.location,
-    }
-  }
-
-  _disabled = () => {
+  const disabled = () => {
     return (
-      (this.state.bio === this.props.bio &&
-        this.state.fullname === this.props.fullname &&
-        this.state.location === this.props.location) ||
-      this.state.bio.length >= maxBio
+      (bio === props.bio && fullname === props.fullname && location === props.location) ||
+      bio.length >= maxBio
     )
   }
 
-  _updateFullname = (fullname: string) => this.setState({fullname})
-  _updateBio = (bio: string) => this.setState({bio})
-  _updateLocation = (location: string) => this.setState({location})
-
-  _submit = () => {
-    this.props.onSubmit(this.state.bio, this.state.fullname, this.state.location)
+  const submit = () => {
+    props.onSubmit(bio, fullname, location)
   }
 
-  // TODO use NewInput when that supports better border radius changes
-  render() {
-    return (
-      <Kb.PopupWrapper onCancel={this.props.onCancel} title={this.props.title}>
-        <Kb.ScrollView>
-          <Kb.Box2 fullWidth={true} direction="vertical" style={styles.container}>
-            {Kb.Styles.isMobile ? null : (
-              <Kb.Text type="Header" style={styles.header}>
-                Edit Profile
-              </Kb.Text>
-            )}
-            <Kb.RoundedBox side="top">
-              <Kb.PlainInput
-                value={this.state.fullname}
-                placeholder="Full name"
-                autoFocus={true}
-                onChangeText={this._updateFullname}
-              />
-            </Kb.RoundedBox>
-            <Kb.RoundedBox side="middle">
-              <Kb.PlainInput
-                value={this.state.bio}
-                placeholder="Bio"
-                multiline={true}
-                rowsMin={7}
-                rowsMax={7}
-                onChangeText={this._updateBio}
-              />
-            </Kb.RoundedBox>
-            <Kb.RoundedBox side="bottom">
-              <Kb.PlainInput
-                value={this.state.location}
-                placeholder="Location"
-                onChangeText={this._updateLocation}
-                onEnterKeyDown={this._submit}
-              />
-            </Kb.RoundedBox>
-            <Kb.Box2 direction="vertical" style={styles.gap} />
-            <Kb.WaitingButton
-              waitingKey={Constants.waitingKey}
-              label="Save"
-              disabled={this._disabled()}
-              onClick={this._submit}
+  return (
+    <Kb.PopupWrapper onCancel={props.onCancel} title={props.title}>
+      <Kb.ScrollView>
+        <Kb.Box2 fullWidth={true} direction="vertical" style={styles.container}>
+          {Kb.Styles.isMobile ? null : (
+            <Kb.Text type="Header" style={styles.header}>
+              Edit Profile
+            </Kb.Text>
+          )}
+          <Kb.RoundedBox side="top">
+            <Kb.PlainInput
+              value={fullname}
+              placeholder="Full name"
+              autoFocus={true}
+              onChangeText={setFullname}
             />
-            {this.state.bio.length > maxBio && <Kb.Text type="BodySmallError">Bio too long, sorry</Kb.Text>}
-          </Kb.Box2>
-        </Kb.ScrollView>
-      </Kb.PopupWrapper>
-    )
-  }
+          </Kb.RoundedBox>
+          <Kb.RoundedBox side="middle">
+            <Kb.PlainInput
+              value={bio}
+              placeholder="Bio"
+              multiline={true}
+              rowsMin={7}
+              rowsMax={7}
+              onChangeText={setBio}
+            />
+          </Kb.RoundedBox>
+          <Kb.RoundedBox side="bottom">
+            <Kb.PlainInput
+              value={location}
+              placeholder="Location"
+              onChangeText={setLocation}
+              onEnterKeyDown={submit}
+            />
+          </Kb.RoundedBox>
+          <Kb.Box2 direction="vertical" style={styles.gap} />
+          <Kb.WaitingButton
+            waitingKey={Constants.waitingKey}
+            label="Save"
+            disabled={disabled()}
+            onClick={submit}
+          />
+          {bio.length > maxBio && <Kb.Text type="BodySmallError">Bio too long, sorry</Kb.Text>}
+        </Kb.Box2>
+      </Kb.ScrollView>
+    </Kb.PopupWrapper>
+  )
 }
 
 const styles = Kb.Styles.styleSheetCreate(() => ({
