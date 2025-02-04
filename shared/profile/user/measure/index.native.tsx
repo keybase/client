0508a134fffa3cfb4865fa2d6262d9965592f8a8
chrisnojima <cnojima@keyba.se>
2025-02-04 15:33:36 -0500

    WIP

--- shared/profile/user/measure/index.native.tsx
+++ shared/profile/user/measure/index.native.tsx
@@ -3,13 +3,16 @@ import * as Kb from '@/common-adapters'
 import type {Props} from '.'
 import {View, type LayoutChangeEvent} from 'react-native'
 
-class Measure extends React.Component<Props> {
-  _onLayout = (e: LayoutChangeEvent) => {
-    this.props.onMeasured(e.nativeEvent.layout.width)
-  }
-  render() {
-    return <View style={styles.container} onLayout={this._onLayout} />
-  }
+const Measure = (props: Props) => {
+  const {onMeasured} = props
+  const onLayout = React.useCallback(
+    (e: LayoutChangeEvent) => {
+      onMeasured(e.nativeEvent.layout.width)
+    },
+    [onMeasured]
+  )
+
+  return <View style={styles.container} onLayout={onLayout} />
 }
 
 const styles = Kb.Styles.styleSheetCreate(() => ({
