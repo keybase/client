a9f83dcd4357483fbfbee936707216c4dc4b1407
chrisnojima <cnojima@keyba.se>
2025-02-10 10:44:08 -0500

    WIP

--- shared/signup/phone-number/container.tsx
+++ shared/signup/phone-number/container.tsx
@@ -1,15 +1,91 @@
 import * as C from '@/constants'
 import * as React from 'react'
-import EnterPhoneNumber, {type Props} from '.'
+import * as Kb from '@/common-adapters'
+import {SignupScreen, errorBanner} from '../common'
 
-type WatcherProps = Props & {
-  onClear: () => void
-  onGoToVerify: () => void
-  pendingVerification: string
+type BodyProps = {
+  autoFocus?: boolean
+  defaultCountry?: string
+  onChangeNumber: (phoneNumber: string, valid: boolean) => void
+  onContinue: () => void
+  searchable: boolean
+  onChangeSearchable?: (allow: boolean) => void
+  iconType: Kb.IconType
 }
 
-const WatchForGoToVerify = (props: WatcherProps) => {
-  const {onClear, onGoToVerify, pendingVerification, error} = props
+export const EnterPhoneNumberBody = (props: BodyProps) => {
+  const showCheckbox = !!props.onChangeSearchable
+  return (
+    <Kb.Box2
+      alignItems="center"
+      direction="vertical"
+      gap={Kb.Styles.isMobile ? 'small' : 'medium'}
+      fullWidth={true}
+      style={styles.container}
+    >
+      <Kb.Icon type={props.iconType} />
+      <Kb.Box2 direction="vertical" gap="tiny" style={styles.inputBox}>
+        <Kb.PhoneInput
+          autoFocus={props.autoFocus ?? true}
+          defaultCountry={props.defaultCountry}
+          style={styles.input}
+          onChangeNumber={props.onChangeNumber}
+          onEnterKeyDown={props.onContinue}
+        />
+        {showCheckbox ? (
+          <Kb.Checkbox
+            label="Allow friends to find you by this phone number"
+            checked={props.searchable}
+            onCheck={props.onChangeSearchable}
+            style={styles.checkbox}
+          />
+        ) : (
+          <Kb.Text type="BodySmall">Allow your friends to find you.</Kb.Text>
+        )}
+      </Kb.Box2>
+    </Kb.Box2>
+  )
+}
+
+const styles = Kb.Styles.styleSheetCreate(() => ({
+  checkbox: {width: '100%'},
+  container: Kb.Styles.platformStyles({
+    common: Kb.Styles.globalStyles.flexOne,
+    isTablet: {maxWidth: 386},
+  }),
+  input: Kb.Styles.platformStyles({
+    isElectron: {
+      height: 38,
+      width: 368,
+    },
+    isMobile: {
+      height: 48,
+      width: '100%',
+    },
+  }),
+  inputBox: Kb.Styles.platformStyles({
+    // need to set width so subtext will wrap
+    isElectron: {width: 368},
+  }),
+}))
+
+const ConnectedEnterPhoneNumber = () => {
+  const defaultCountry = C.useSettingsPhoneState(s => s.defaultCountry)
+  const error = C.useSettingsPhoneState(s => s.error)
+  const pendingVerification = C.useSettingsPhoneState(s => s.pendingVerification)
+  const waiting = C.Waiting.useAnyWaiting(C.SettingsPhone.addPhoneNumberWaitingKey)
+  const clearPhoneNumberErrors = C.useSettingsPhoneState(s => s.dispatch.clearPhoneNumberErrors)
+  const clearPhoneNumberAdd = C.useSettingsPhoneState(s => s.dispatch.clearPhoneNumberAdd)
+  const onClear = clearPhoneNumberErrors
+  const addPhoneNumber = C.useSettingsPhoneState(s => s.dispatch.addPhoneNumber)
+  const navigateAppend = C.useRouterState(s => s.dispatch.navigateAppend)
+  const onGoToVerify = React.useCallback(() => {
+    navigateAppend('signupVerifyPhoneNumber')
+  }, [navigateAppend])
+  const onSkip = React.useCallback(() => {
+    clearPhoneNumberAdd()
+    navigateAppend('signupEnterEmail', true)
+  }, [clearPhoneNumberAdd, navigateAppend])
 
   React.useEffect(() => {
     return () => {
@@ -25,48 +101,47 @@ const WatchForGoToVerify = (props: WatcherProps) => {
     lastPendingVerificationRef.current = pendingVerification
   }, [pendingVerification, error, onGoToVerify])
 
-  return (
-    <EnterPhoneNumber
-      defaultCountry={props.defaultCountry}
-      error={props.error}
-      onContinue={props.onContinue}
-      onSkip={props.onSkip}
-      waiting={props.waiting}
-    />
-  )
-}
+  // trigger a default phone number country rpc if it's not already loaded
+  const loadDefaultPhoneCountry = C.useSettingsPhoneState(s => s.dispatch.loadDefaultPhoneCountry)
+  React.useEffect(() => {
+    !defaultCountry && loadDefaultPhoneCountry()
+  }, [defaultCountry, loadDefaultPhoneCountry])
 
-const ConnectedEnterPhoneNumber = () => {
-  const defaultCountry = C.useSettingsPhoneState(s => s.defaultCountry)
-  const error = C.useSettingsPhoneState(s => s.error)
-  const pendingVerification = C.useSettingsPhoneState(s => s.pendingVerification)
-  const waiting = C.Waiting.useAnyWaiting(C.SettingsPhone.addPhoneNumberWaitingKey)
-  const clearPhoneNumberErrors = C.useSettingsPhoneState(s => s.dispatch.clearPhoneNumberErrors)
-  const clearPhoneNumberAdd = C.useSettingsPhoneState(s => s.dispatch.clearPhoneNumberAdd)
-  const onClear = clearPhoneNumberErrors
-  const addPhoneNumber = C.useSettingsPhoneState(s => s.dispatch.addPhoneNumber)
-  const onContinue = addPhoneNumber
-  const navigateAppend = C.useRouterState(s => s.dispatch.navigateAppend)
-  const onGoToVerify = React.useCallback(() => {
-    navigateAppend('signupVerifyPhoneNumber')
-  }, [navigateAppend])
-  const onSkip = React.useCallback(() => {
-    clearPhoneNumberAdd()
-    navigateAppend('signupEnterEmail', true)
-  }, [clearPhoneNumberAdd, navigateAppend])
-
-  const props = {
-    defaultCountry,
-    error,
-    onClear,
-    onContinue,
-    onGoToVerify,
-    onSkip,
-    pendingVerification,
-    waiting,
+  const [phoneNumber, onChangePhoneNumber] = React.useState('')
+  const [valid, onChangeValidity] = React.useState(false)
+  const disabled = !valid
+  const onContinue = () => (disabled || waiting ? {} : addPhoneNumber(phoneNumber, true /* searchable */))
+  const onChangeNumberCb = (phoneNumber: string, validity: boolean) => {
+    onChangePhoneNumber(phoneNumber)
+    onChangeValidity(validity)
   }
-
-  return <WatchForGoToVerify {...props} />
+  return (
+    <SignupScreen
+      buttons={[
+        {
+          disabled,
+          label: 'Continue',
+          onClick: onContinue,
+          type: 'Success' as const,
+          waiting: waiting,
+        },
+      ]}
+      banners={errorBanner(error)}
+      rightActionLabel="Skip"
+      onRightAction={onSkip}
+      title="Your phone number"
+      showHeaderInfoicon={true}
+    >
+      <EnterPhoneNumberBody
+        autoFocus={!Kb.Styles.isMobile}
+        defaultCountry={defaultCountry}
+        onChangeNumber={onChangeNumberCb}
+        onContinue={onContinue}
+        searchable={true}
+        iconType={C.isLargeScreen ? 'icon-phone-number-add-96' : 'icon-phone-number-add-64'}
+      />
+    </SignupScreen>
+  )
 }
 
 export default ConnectedEnterPhoneNumber
--- shared/teams/join-team/container.tsx
+++ shared/teams/join-team/container.tsx
@@ -1,6 +1,7 @@
 import * as C from '@/constants'
-import JoinTeam from '.'
 import upperFirst from 'lodash/upperFirst'
+import * as React from 'react'
+import * as Kb from '@/common-adapters'
 
 type OwnProps = {initialTeamname?: string}
 
@@ -16,16 +17,114 @@ const Container = (ownProps: OwnProps) => {
   }
   const joinTeam = C.useTeamsState(s => s.dispatch.joinTeam)
   const onJoinTeam = joinTeam
-  const props = {
-    errorText,
-    initialTeamname,
-    onBack,
-    onJoinTeam,
-    open,
-    success,
-    successTeamName,
+
+  const [name, _setName] = React.useState(initialTeamname ?? '')
+  const setName = (n: string) => _setName(n.toLowerCase())
+  const resetTeamJoin = C.useTeamsState(s => s.dispatch.resetTeamJoin)
+  React.useEffect(() => {
+    resetTeamJoin()
+  }, [resetTeamJoin])
+
+  const onSubmit = () => {
+    onJoinTeam(name)
   }
-  return <JoinTeam {...props} />
+
+  return (
+    <Kb.Modal
+      banners={
+        errorText ? (
+          <Kb.Banner key="red" color="red">
+            <Kb.BannerParagraph bannerColor="red" content={errorText} />
+          </Kb.Banner>
+        ) : null
+      }
+      footer={{
+        content: (
+          <Kb.ButtonBar align="center" direction="row" fullWidth={true} style={styles.buttonBar}>
+            <Kb.WaitingButton
+              fullWidth={true}
+              label={success ? 'Close' : 'Continue'}
+              onClick={success ? onBack : onSubmit}
+              type={success ? 'Dim' : 'Default'}
+              waitingKey={C.Teams.joinTeamWaitingKey}
+            />
+          </Kb.ButtonBar>
+        ),
+      }}
+      header={{
+        hideBorder: success,
+        leftButton:
+          Kb.Styles.isMobile && !success ? (
+            <Kb.Text type="BodyBigLink" onClick={onBack}>
+              Cancel
+            </Kb.Text>
+          ) : null,
+        title: success ? 'Request sent' : 'Join a team',
+      }}
+      onClose={onBack}
+    >
+      {success ? (
+        <Kb.Box2 alignItems="center" direction="horizontal" fullHeight={true} fullWidth={true}>
+          {open ? (
+            <Success teamname={successTeamName} />
+          ) : (
+            <Kb.Box2 alignItems="center" direction="vertical" fullWidth={true}>
+              <Kb.Box2 direction="horizontal" fullWidth={true} style={styles.banner} centerChildren={true}>
+                <Kb.Icon type="icon-illustration-teams-zen-460-96" />
+              </Kb.Box2>
+              <Kb.Box style={styles.container}>
+                <Kb.Text center={true} type="Body">
+                  Your request was sent to the admins of{' '}
+                  {successTeamName ? <Kb.Text type="BodySemibold">{successTeamName}</Kb.Text> : 'the team'}.
+                  Hang tight, you'll get notified as soon as you're let in.
+                </Kb.Text>
+              </Kb.Box>
+            </Kb.Box2>
+          )}
+        </Kb.Box2>
+      ) : (
+        <Kb.Box2 direction="vertical" style={styles.container}>
+          <Kb.RoundedBox style={styles.roundedBox}>
+            <Kb.PlainInput
+              autoFocus={true}
+              onChangeText={setName}
+              onEnterKeyDown={onSubmit}
+              placeholder="Token or team name"
+              value={name}
+            />
+          </Kb.RoundedBox>
+          <Kb.Text type="BodySmall">Examples: keybasefriends, stellar.public, etc.</Kb.Text>
+        </Kb.Box2>
+      )}
+    </Kb.Modal>
+  )
 }
+
+export const Success = (props: {teamname: string}) => (
+  <Kb.Box2 alignItems="center" direction="vertical" gap="tiny" style={styles.container}>
+    <Kb.Icon type="icon-illustration-welcome-96" />
+    {!!props.teamname && (
+      <Kb.Text center={true} type="Header">
+        You’ve joined {props.teamname}!
+      </Kb.Text>
+    )}
+    <Kb.Text center={true} type="Body">
+      The team may take a tiny while to appear as an admin needs to come online. But you’re in.
+    </Kb.Text>
+  </Kb.Box2>
+)
+
+const styles = Kb.Styles.styleSheetCreate(
+  () =>
+    ({
+      banner: Kb.Styles.platformStyles({isElectron: {overflowX: 'hidden'}}),
+      buttonBar: {minHeight: undefined},
+      container: {
+        padding: Kb.Styles.globalMargins.small,
+        width: '100%',
+      },
+      roundedBox: {marginBottom: Kb.Styles.globalMargins.tiny},
+    }) as const
+)
 
 export default Container
