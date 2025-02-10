03581d7ed1e87263ea035a67be281730145eca99
chrisnojima <cnojima@keyba.se>
2025-02-10 10:21:16 -0500

    WIP

--- shared/settings/logout/container.tsx
+++ shared/settings/logout/container.tsx
@@ -2,7 +2,8 @@ import * as React from 'react'
 import * as Container from '@/util/container'
 import * as Constants from '@/constants/settings'
 import * as C from '@/constants'
-import LogOut from '.'
+import * as Kb from '@/common-adapters'
+import UpdatePassword from '../password'
 
 const LogoutContainer = () => {
   const checkPasswordIsCorrect = C.useSettingsState(s => s.checkPasswordIsCorrect)
@@ -29,35 +30,191 @@ const LogoutContainer = () => {
   }, [resetCheckPassword, requestLogout])
 
   const submitNewPassword = C.useSettingsPasswordState(s => s.dispatch.submitNewPassword)
-  const setPassword = C.useSettingsPasswordState(s => s.dispatch.setPassword)
+  const _setPassword = C.useSettingsPasswordState(s => s.dispatch.setPassword)
   const setPasswordConfirm = C.useSettingsPasswordState(s => s.dispatch.setPasswordConfirm)
 
   const onSavePassword = React.useCallback(
     (password: string) => {
-      setPassword(password)
+      _setPassword(password)
       setPasswordConfirm(password)
       submitNewPassword(true)
     },
-    [submitNewPassword, setPassword, setPasswordConfirm]
+    [submitNewPassword, _setPassword, setPasswordConfirm]
   )
 
   const onLogout = Container.useSafeSubmit(_onLogout, false)
 
   const onUpdatePGPSettings = C.useSettingsPasswordState(s => s.dispatch.loadPgpSettings)
   const hasPGPKeyOnServer = C.useSettingsPasswordState(s => !!s.hasPGPKeyOnServer)
-  return (
-    <LogOut
-      hasPGPKeyOnServer={hasPGPKeyOnServer}
+
+  const [loggingOut, setLoggingOut] = React.useState(false)
+  const [password, setPassword] = React.useState('')
+  const [showTyping, setShowTyping] = React.useState(false)
+
+  React.useEffect(() => {
+    onBootstrap()
+  }, [onBootstrap])
+
+  const logOut = () => {
+    if (loggingOut) return
+    onLogout()
+    setLoggingOut(true)
+  }
+
+  const inputType = showTyping ? 'text' : 'password'
+  const keyboardType = showTyping && Kb.Styles.isAndroid ? 'visible-password' : 'default'
+
+  return hasRandomPW === undefined ? (
+    <Kb.Modal onClose={onCancel}>
+      <Kb.ProgressIndicator style={styles.progress} type="Huge" />
+    </Kb.Modal>
+  ) : hasRandomPW ? (
+    <UpdatePassword
+      error=""
       onUpdatePGPSettings={onUpdatePGPSettings}
-      checkPasswordIsCorrect={checkPasswordIsCorrect}
+      hasPGPKeyOnServer={hasPGPKeyOnServer}
       hasRandomPW={hasRandomPW}
-      onBootstrap={onBootstrap}
       onCancel={onCancel}
-      onCheckPassword={onCheckPassword}
-      onLogout={onLogout}
-      onSavePassword={onSavePassword}
+      onSave={onSavePassword}
+      saveLabel="Sign out"
       waitingForResponse={waitingForResponse}
     />
+  ) : (
+    <Kb.Modal
+      backgroundStyle={styles.logoutBackground}
+      banners={
+        <>
+          {checkPasswordIsCorrect === false ? (
+            <Kb.Banner color="red">Wrong password. Please try again.</Kb.Banner>
+          ) : null}
+          {checkPasswordIsCorrect === true ? (
+            <Kb.Banner color="green">Your password is correct.</Kb.Banner>
+          ) : null}
+        </>
+      }
+      footer={{
+        content: !checkPasswordIsCorrect ? (
+          <Kb.ButtonBar align="center" direction="column" fullWidth={true} style={styles.buttonBar}>
+            <Kb.WaitingButton
+              fullWidth={true}
+              waitingKey={Constants.checkPasswordWaitingKey}
+              disabled={!password || loggingOut}
+              label="Test password"
+              onClick={() => onCheckPassword(password)}
+            />
+            <Kb.Box2 direction="horizontal">
+              {loggingOut ? (
+                <Kb.ProgressIndicator style={styles.smallProgress} type="Small" />
+              ) : (
+                <Kb.ClickableBox
+                  onClick={logOut}
+                  style={styles.logoutContainer}
+                  className="hover-underline-container"
+                >
+                  <Kb.Icon type="iconfont-leave" />
+                  <Kb.Text className="underline" style={styles.logout} type="BodySmallSecondaryLink">
+                    Just sign out
+                  </Kb.Text>
+                </Kb.ClickableBox>
+              )}
+            </Kb.Box2>
+          </Kb.ButtonBar>
+        ) : (
+          <Kb.ButtonBar align="center" direction="row" fullWidth={true} style={styles.buttonBar}>
+            {loggingOut ? (
+              <Kb.ProgressIndicator style={styles.smallProgress} type="Small" />
+            ) : (
+              <Kb.Button label="Safely sign out" fullWidth={true} onClick={logOut} type="Success" />
+            )}
+          </Kb.ButtonBar>
+        ),
+      }}
+      header={{
+        leftButton: Kb.Styles.isMobile ? (
+          <Kb.Text type="BodyBigLink" onClick={onCancel}>
+            Cancel
+          </Kb.Text>
+        ) : null,
+        title: !Kb.Styles.isMobile && 'Do you know your password?',
+      }}
+      onClose={onCancel}
+    >
+      <Kb.Box2 direction="vertical" fullHeight={true} style={styles.container}>
+        {Kb.Styles.isMobile && (
+          <Kb.Text style={styles.headerText} type="Header">
+            Do you know your password?
+          </Kb.Text>
+        )}
+        <Kb.Text style={styles.bodyText} type="Body">
+          You will need it to sign back in.
+        </Kb.Text>
+        <Kb.RoundedBox>
+          <Kb.PlainInput
+            keyboardType={keyboardType}
+            onEnterKeyDown={() => {
+              checkPasswordIsCorrect ? logOut() : onCheckPassword(password)
+            }}
+            onChangeText={setPassword}
+            placeholder="Your password"
+            type={inputType}
+            value={password}
+          />
+        </Kb.RoundedBox>
+        <Kb.Checkbox
+          checked={showTyping}
+          label="Show typing"
+          onCheck={() => setShowTyping(!showTyping)}
+          style={styles.checkbox}
+        />
+      </Kb.Box2>
+    </Kb.Modal>
   )
 }
+
+const styles = Kb.Styles.styleSheetCreate(
+  () =>
+    ({
+      bodyText: {
+        paddingBottom: Kb.Styles.globalMargins.tiny,
+        textAlign: 'center',
+      },
+      buttonBar: {minHeight: undefined},
+      checkbox: {paddingTop: Kb.Styles.globalMargins.tiny},
+      container: {
+        ...Kb.Styles.padding(
+          Kb.Styles.globalMargins.medium,
+          Kb.Styles.globalMargins.small,
+          Kb.Styles.globalMargins.medium,
+          Kb.Styles.globalMargins.small
+        ),
+        backgroundColor: Kb.Styles.globalColors.blueGrey,
+        flexGrow: 1,
+      },
+      headerText: {
+        marginBottom: Kb.Styles.globalMargins.small,
+        textAlign: 'center',
+      },
+      logout: {paddingLeft: Kb.Styles.globalMargins.xtiny},
+      logoutBackground: Kb.Styles.platformStyles({
+        isTablet: {backgroundColor: Kb.Styles.globalColors.blueGrey},
+      }),
+      logoutContainer: Kb.Styles.platformStyles({
+        common: {
+          ...Kb.Styles.globalStyles.flexBoxRow,
+          justifyContent: 'center',
+          paddingTop: Kb.Styles.globalMargins.tiny,
+        },
+        isElectron: {
+          ...Kb.Styles.desktopStyles.clickable,
+        },
+      }),
+      progress: {
+        alignSelf: 'center',
+        marginBottom: Kb.Styles.globalMargins.xlarge,
+        marginTop: Kb.Styles.globalMargins.xlarge,
+      },
+      smallProgress: {alignSelf: 'center'},
+    }) as const
+)
+
 export default LogoutContainer
