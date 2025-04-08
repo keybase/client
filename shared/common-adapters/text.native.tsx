01a2eb655f7fd8ad0a86d0873d7e6fc60ddb33e3
chrisnojima <cnojima@keyba.se>
2025-04-08 14:43:09 -0400

    WIP

--- shared/common-adapters/text.native.tsx
+++ shared/common-adapters/text.native.tsx
@@ -31,66 +31,69 @@ const styles = Styles.styleSheetCreate(() =>
 
 // Init common styles for perf
 
-const Text = React.memo((p: Props) => {
-  const _urlClick = () => {
-    p.onClickURL && openURL(p.onClickURL)
-  }
+const Text = React.memo(
+  React.forwardRef<any, Props>(function Text(p, ref) {
+    const _urlClick = () => {
+      p.onClickURL && openURL(p.onClickURL)
+    }
 
-  const _urlCopy = (url?: string) => {
-    if (!url) return
-    Clipboard.setStringAsync(url)
-      .then(() => {})
-      .catch(() => {})
-  }
+    const _urlCopy = (url?: string) => {
+      if (!url) return
+      Clipboard.setStringAsync(url)
+        .then(() => {})
+        .catch(() => {})
+    }
 
-  const _urlChooseOption = () => {
-    const url = p.onLongPressURL
-    if (!url) return
-    Alert.alert('', url, [
-      {style: 'cancel', text: 'Cancel'},
-      {onPress: () => openURL(url), text: 'Open Link'},
-      {onPress: () => _urlCopy(url), text: 'Copy Link'},
-    ])
-  }
+    const _urlChooseOption = () => {
+      const url = p.onLongPressURL
+      if (!url) return
+      Alert.alert('', url, [
+        {style: 'cancel', text: 'Cancel'},
+        {onPress: () => openURL(url), text: 'Open Link'},
+        {onPress: () => _urlCopy(url), text: 'Copy Link'},
+      ])
+    }
 
-  const baseStyle: Styles.StylesCrossPlatform = styles[`${p.type}:${p.negative ? 'negative' : 'positive'}`]
-  const dynamicStyle: Styles._StylesCrossPlatform = p.negative
-    ? _getStyle(p.type, p.negative, !!p.underline)
-    : {}
+    const baseStyle: Styles.StylesCrossPlatform = styles[`${p.type}:${p.negative ? 'negative' : 'positive'}`]
+    const dynamicStyle: Styles._StylesCrossPlatform = p.negative
+      ? _getStyle(p.type, p.negative, !!p.underline)
+      : {}
 
-  let style: Array<Styles.StylesCrossPlatform> | Styles.StylesCrossPlatform
-  if (!Object.keys(dynamicStyle).length) {
-    style =
-      p.style || p.center || p.fixOverdraw
-        ? [baseStyle, p.center && styles2.center, p.fixOverdraw && styles2.fixOverdraw, p.style]
-        : baseStyle
-  } else {
-    style = [baseStyle, dynamicStyle, p.center && styles2.center, p.style]
-  }
+    let style: Array<Styles.StylesCrossPlatform> | Styles.StylesCrossPlatform
+    if (!Object.keys(dynamicStyle).length) {
+      style =
+        p.style || p.center || p.fixOverdraw
+          ? [baseStyle, p.center && styles2.center, p.fixOverdraw && styles2.fixOverdraw, p.style]
+          : baseStyle
+    } else {
+      style = [baseStyle, dynamicStyle, p.center && styles2.center, p.style]
+    }
 
-  const onPress =
-    p.onClick ||
-    (p.onClickURL ? _urlClick : undefined) ||
-    // If selectable and there isn't already an onClick handler,
-    // make a dummy one so that it shows the selection (on iOS).
-    (p.selectable ? () => {} : undefined)
+    const onPress =
+      p.onClick ||
+      (p.onClickURL ? _urlClick : undefined) ||
+      // If selectable and there isn't already an onClick handler,
+      // make a dummy one so that it shows the selection (on iOS).
+      (p.selectable ? () => {} : undefined)
 
-  const onLongPress = p.onLongPress || (p.onLongPressURL ? _urlChooseOption : undefined)
+    const onLongPress = p.onLongPress || (p.onLongPressURL ? _urlChooseOption : undefined)
 
-  return (
-    <NativeText
-      selectable={p.selectable}
-      textBreakStrategy={p.textBreakStrategy ?? 'simple'}
-      style={style}
-      {...lineClamp(p.lineClamp || undefined, p.ellipsizeMode || undefined)}
-      onPress={onPress}
-      onLongPress={onLongPress}
-      allowFontScaling={p.allowFontScaling ?? false}
-    >
-      {p.children}
-    </NativeText>
-  )
-})
+    return (
+      <NativeText
+        ref={ref}
+        selectable={p.selectable}
+        textBreakStrategy={p.textBreakStrategy ?? 'simple'}
+        style={style}
+        {...lineClamp(p.lineClamp || undefined, p.ellipsizeMode || undefined)}
+        onPress={onPress}
+        onLongPress={onLongPress}
+        allowFontScaling={p.allowFontScaling ?? false}
+      >
+        {p.children}
+      </NativeText>
+    )
+  })
+)
 
 // external things call this so leave the original alone
 function _getStyle(type: TextType, negative?: boolean, forceUnderline?: boolean) {
