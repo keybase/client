a0ea9b6719c8c969cdd141a5e61a81354da08167
chrisnojima <cnojima@keyba.se>
2025-09-25 11:53:43 -0400

    WIP

--- shared/app/globals.native.tsx
+++ shared/app/globals.native.tsx
@@ -1,7 +1,6 @@
 // >>>>>>>>>>>>>>>>>>>>>>>      DO NOT REORDER ANYTHING in this file      <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 // This is supposed to bootstrap / polyfill / fixup the app. Do NOT add things here or change things unless you really know
 // what's happening
-import 'fastestsmallesttextencoderdecoder'
 global.__FILE_SUFFIX__ = ''
 global.__PROFILE__ = false
 global.__HOT__ = false
