b9e14616f8c398b07f9ff4dd99c3305febde451e
chrisnojima <cnojima@keyba.se>
2025-09-30 16:02:30 -0400

    WIP

--- shared/android/app/src/main/java/io/keybase/ossifrage/MainApplication.kt
+++ shared/android/app/src/main/java/io/keybase/ossifrage/MainApplication.kt
@@ -14,20 +14,22 @@ import com.bumptech.glide.Glide
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
+import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.ReactContext
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
+import com.facebook.react.common.ReleaseLevel
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.soloader.SoLoader
-import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import expo.modules.ApplicationLifecycleDispatcher.onApplicationCreate
 import expo.modules.ApplicationLifecycleDispatcher.onConfigurationChanged
 import io.keybase.ossifrage.modules.BackgroundSyncWorker
 import io.keybase.ossifrage.modules.NativeLogger
 import keybase.Keybase
 import java.util.concurrent.TimeUnit
+import expo.modules.ApplicationLifecycleDispatcher
+import expo.modules.ReactNativeHostWrapper
 
 internal class AppLifecycleListener(private val context: Context?) :
     DefaultLifecycleObserver {
@@ -42,18 +44,18 @@ internal class AppLifecycleListener(private val context: Context?) :
 }
 
 class MainApplication : Application(), ReactApplication {
-    override val reactNativeHost: ReactNativeHost = object : DefaultReactNativeHost(this) {
-        override fun getPackages(): List<ReactPackage> {
-            val packages = PackageList(this).packages.toMutableList()
-            packages.add(KBReactPackage())
-            return packages
-        }
+    override val reactNativeHost: ReactNativeHost = ReactNativeHostWrapper(
+      this,
+      object : DefaultReactNativeHost(this) {
+        override fun getPackages(): List<ReactPackage> =
+            PackageList(this).packages.apply {
+              add(KBReactPackage())
+            }
 
         override fun getJSMainModuleName(): String = "index"
         override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
         override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
-    }
+    })
 
     override val reactHost: ReactHost
         get() = getDefaultReactHost(applicationContext, reactNativeHost)
@@ -62,11 +64,12 @@ class MainApplication : Application(), ReactApplication {
     override fun onCreate() {
         NativeLogger.info("MainApplication created")
         super.onCreate()
-        SoLoader.init(this, OpenSourceMergedSoMapping)
-        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-            // If you opted-in for the New Architecture, we load the native entry point for this app.
-            load(bridgelessEnabled = true)
+        try {
+            DefaultNewArchitectureEntryPoint.releaseLevel = ReleaseLevel.valueOf(BuildConfig.REACT_NATIVE_RELEASE_LEVEL.uppercase())
+        } catch (e: IllegalArgumentException) {
+            DefaultNewArchitectureEntryPoint.releaseLevel = ReleaseLevel.STABLE
         }
+        loadReactNative(this)
 
         // KB
         onApplicationCreate(this)
