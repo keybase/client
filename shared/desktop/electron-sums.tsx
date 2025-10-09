3332c0fcd6bae29b357fb50a0d48c0adfc77fcc7
chrisnojima <cnojima@keyba.se>
2025-10-09 15:59:49 -0400

    small deps, fix swipes

--- shared/desktop/electron-sums.tsx
+++ shared/desktop/electron-sums.tsx
@@ -1,10 +1,10 @@
 // Generated with: ./extract-electron-shasums.sh {ver}
 // prettier-ignore
 export const electronChecksums = {
-  'electron-v38.2.1-darwin-arm64.zip': '8f8eb31c69bbf0fb45bc596e271571ade426cf076a3e07a858390c45c399e213',
-  'electron-v38.2.1-darwin-x64.zip': '3b636e2826a90f6e8b9ebf1c6dc5a6fb7c7fad876aa91520eed520b40bcab3c8',
-  'electron-v38.2.1-linux-arm64.zip': '99ed0b3e4edbdf4f8c51e8dabb92dccc7b0b5d728aa5c014d5e5adaf17a6993c',
-  'electron-v38.2.1-linux-x64.zip': '471b91628357ffd40f4f9d61f2421b6e9fb7aa1769d6c9728d24f8376acf775a',
-  'electron-v38.2.1-win32-x64.zip': 'e3831387518acc8430fa674e585d24833aabb994dae43bcca0257302c0816967',
-  'hunspell_dictionaries.zip': 'e159462fd2c8c6d245878df9b6a60e5d737fe737c19c39bd0ed15bcdffcb8849',
+  'electron-v38.2.2-darwin-arm64.zip': '323d805132db09f92b9766ff5f6845ce106c22a543b0f7a4a663d5270a76db22',
+  'electron-v38.2.2-darwin-x64.zip': '55fef7f835d471973627c511965ef090e097e07eb7a5292b3ff52a81595ea8c8',
+  'electron-v38.2.2-linux-arm64.zip': 'ab46f8f2b137ec1774d9a9b949697a84212cf8b4d13c67b32d8d41b0c03e5ca0',
+  'electron-v38.2.2-linux-x64.zip': '84855f9262b6b5a2cd9944795bf5c7e11b301cb53bdd586d0912e310feec0674',
+  'electron-v38.2.2-win32-x64.zip': 'af5230d3f698cb50fce18805a3fd584e06836c61e72b6e0fd90d410b719f24dc',
+  'hunspell_dictionaries.zip': '5ce043943caa3507e544ffe0256dfcd18c6478b919562782d87cecfee8761047',
 }
