e2acec2c5a750b55fec4489b631b106284668f62
Joshua Blum <joshua.blum@zoom.us>
2025-10-21 10:24:00 -0400

    x

--- shared/ios/KeybaseShare/KeybaseShare-Bridging-Header.h
+++ shared/ios/KeybaseShare/KeybaseShare-Bridging-Header.h
@@ -2,3 +2,6 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+// Workaround for iOS SDK 18/Xcode 16 malloc header bug with pointer authentication
+#define _MALLOC_TYPED(x,y)
+
