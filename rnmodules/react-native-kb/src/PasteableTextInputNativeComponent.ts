e5d3a1af7eb2148beddf60806819151dd003e315
Chris Nojima <christopher.nojima@zoom.us>
2025-10-22 12:18:33 -0400

    WIP

--- rnmodules/react-native-kb/src/PasteableTextInputNativeComponent.ts
+++ rnmodules/react-native-kb/src/PasteableTextInputNativeComponent.ts
@@ -1,19 +1,14 @@
 import type {HostComponent, ViewProps} from 'react-native'
 import type {
-  BubblingEventHandler,
   DirectEventHandler,
-  Double,
-  Int32,
-  WithDefault,
 } from 'react-native/Libraries/Types/CodegenTypes'
 import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent'
-import type {TextInputProps} from 'react-native'
 
 export interface OnPasteImageEvent {
   imagePath: string
 }
 
-export interface NativePasteableTextInputProps extends TextInputProps {
+export interface NativePasteableTextInputProps extends ViewProps {
   onPasteImage?: DirectEventHandler<OnPasteImageEvent>
 }
 
