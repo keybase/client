8b53b8d27ba2089041a85dba8edd00b5fc8a4fc6
chrisnojima <cnojima@keyba.se>
2025-10-31 12:38:13 -0400

    WIP

--- shared/android/app/src/main/java/io/keybase/ossifrage/MainApplication.kt
+++ shared/android/app/src/main/java/io/keybase/ossifrage/MainApplication.kt
@@ -54,7 +54,7 @@ class MainApplication : Application(), ReactApplication {
 
         override fun getJSMainModuleName(): String = "index"
         override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-        override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+        override val isNewArchEnabled: Boolean = true
     })
 
     override val reactHost: ReactHost
