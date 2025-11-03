08f7657d96402f624dc56e36ac5d7a4eca152405
Chris Nojima <christopher.nojima@zoom.us>
2025-11-03 13:24:01 -0500

    WIP

--- rnmodules/react-native-kb/android/src/main/java/com/reactnativekb/KbSpec.kt
+++ rnmodules/react-native-kb/android/src/main/java/com/reactnativekb/KbSpec.kt
@@ -2,7 +2,7 @@ package com.reactnativekb
 
 import com.facebook.react.bridge.ReactApplicationContext
 
-internal abstract class KbSpec(context: ReactApplicationContext?) : NativeKbSpec(context)
+abstract class KbSpec(context: ReactApplicationContext?) : NativeKbSpec(context)
 
 
 
