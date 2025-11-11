1c3c2cf15292831dc79373622ecec10c48025b0d
chrisnojima <cnojima@keyba.se>
2025-11-11 08:59:22 -0500

    WIP

--- shared/devices/routes.tsx
+++ shared/devices/routes.tsx
@@ -1,11 +1,30 @@
+import * as React from 'react'
 import type * as C from '@/constants'
+import {HeaderLeftCancel, type HeaderBackButtonProps} from '@/common-adapters/header-hoc'
 import {newRoutes as provisionNewRoutes} from '../provision/routes-sub'
 
-import devicePage from './device-page.page'
-import deviceRevoke from './device-revoke.page'
 import devicesRoot from './page'
 import deviceAdd from './add-device.page'
 import devicePaperKey from './paper-key.page'
+
+const Device = React.lazy(async () => import('./device-page'))
+const devicePage = {
+  getOptions: {title: ''},
+  screen: function DevicePage(p: C.ViewPropsToPageProps<typeof Device>) {
+    return <Device {...p.route.params} />
+  },
+}
+
+const Revoke = React.lazy(async () => import('./device-revoke'))
+const deviceRevoke = {
+  getOptions: {
+    headerLeft: (p: HeaderBackButtonProps) => <HeaderLeftCancel {...p} />,
+    title: '',
+  },
+  screen: function DeviceRevoke(p: C.ViewPropsToPageProps<typeof Revoke>) {
+    return <Revoke {...p.route.params} />
+  },
+}
 
 export const newRoutes = {
   devicePage,
