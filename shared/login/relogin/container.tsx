f5913361e70d2aeeeb197ceaaf549e957610ef8d
chrisnojima <cnojima@keyba.se>
2025-11-14 16:21:53 -0500

    WIP

--- shared/login/relogin/container.tsx
+++ shared/login/relogin/container.tsx
@@ -2,24 +2,31 @@ import * as C from '@/constants'
 import * as React from 'react'
 import Login from '.'
 import sortBy from 'lodash/sortBy'
-import type * as T from '@/constants/types'
 
 const needPasswordError = 'passphrase cannot be empty'
 
-type Props = {
-  error: string
-  loggedInMap: Map<string, boolean>
-  onFeedback: () => void
-  onForgotPassword: (username: string) => void
-  onLogin: (user: string, password: string) => void
-  onSignup: () => void
-  onSomeoneElse: () => void
-  selectedUser: string
-  users: Array<T.Config.ConfiguredAccount>
-}
-
-const LoginWrapper = (props: Props) => {
-  const {onLogin, loggedInMap, error, selectedUser: pselectedUser} = props
+const ReloginContainer = () => {
+  const _users = C.useConfigState(s => s.configuredAccounts)
+  const perror = C.useConfigState(s => s.loginError)
+  const pselectedUser = C.useConfigState(s => s.defaultUsername)
+  const startRecoverPassword = C.useRecoverState(s => s.dispatch.startRecoverPassword)
+  const onForgotPassword = (username: string) => {
+    startRecoverPassword({username})
+  }
+  const navigateAppend = C.useRouterState(s => s.dispatch.navigateAppend)
+  const onFeedback = () => {
+    navigateAppend('signupSendFeedbackLoggedOut')
+  }
+  const onLogin = C.useConfigState(s => s.dispatch.login)
+  const requestAutoInvite = C.useSignupState(s => s.dispatch.requestAutoInvite)
+  const onSignup = () => requestAutoInvite()
+  const onSomeoneElse = C.useProvisionState(s => s.dispatch.startProvision)
+  const error = perror?.desc || ''
+  const loggedInMap = React.useMemo(
+    () => new Map<string, boolean>(_users.map(account => [account.username, account.hasStoredSecret])),
+    [_users]
+  )
+  const users = sortBy(_users, 'username')
 
   const [password, setPassword] = React.useState('')
   const [selectedUser, setSelectedUser] = React.useState(pselectedUser)
@@ -74,11 +81,11 @@ const LoginWrapper = (props: Props) => {
     <Login
       error={error}
       needPassword={!loggedInMap.get(selectedUser) || gotNeedPasswordError}
-      onFeedback={props.onFeedback}
-      onForgotPassword={() => props.onForgotPassword(selectedUser)}
+      onFeedback={onFeedback}
+      onForgotPassword={() => onForgotPassword(selectedUser)}
       onLogin={onLogin}
-      onSignup={props.onSignup}
-      onSomeoneElse={props.onSomeoneElse}
+      onSignup={onSignup}
+      onSomeoneElse={onSomeoneElse}
       onSubmit={onSubmit}
       password={password}
       passwordChange={setPassword}
@@ -86,39 +93,9 @@ const LoginWrapper = (props: Props) => {
       selectedUserChange={selectedUserChange}
       showTypingChange={setShowTyping}
       showTyping={showTyping}
-      users={props.users}
+      users={users}
     />
   )
-}
-
-const ReloginContainer = () => {
-  const _users = C.useConfigState(s => s.configuredAccounts)
-  const error = C.useConfigState(s => s.loginError)
-  const selectedUser = C.useConfigState(s => s.defaultUsername)
-  const startRecoverPassword = C.useRecoverState(s => s.dispatch.startRecoverPassword)
-  const onForgotPassword = (username: string) => {
-    startRecoverPassword({username})
-  }
-  const navigateAppend = C.useRouterState(s => s.dispatch.navigateAppend)
-  const onFeedback = () => {
-    navigateAppend('signupSendFeedbackLoggedOut')
-  }
-  const onLogin = C.useConfigState(s => s.dispatch.login)
-  const requestAutoInvite = C.useSignupState(s => s.dispatch.requestAutoInvite)
-  const onSignup = () => requestAutoInvite()
-  const onSomeoneElse = C.useProvisionState(s => s.dispatch.startProvision)
-  const props = {
-    error: error?.desc || '',
-    loggedInMap: new Map<string, boolean>(_users.map(account => [account.username, account.hasStoredSecret])),
-    onFeedback,
-    onForgotPassword,
-    onLogin,
-    onSignup,
-    onSomeoneElse,
-    selectedUser,
-    users: sortBy(_users, 'username'),
-  }
-  return <LoginWrapper {...props} />
 }
 
 export default ReloginContainer
