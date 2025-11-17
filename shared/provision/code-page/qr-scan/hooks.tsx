982041cd19389a164184a18856efa50df7c2bbf5
chrisnojima <cnojima@keyba.se>
2025-11-17 11:17:25 -0500

    WIP

--- shared/provision/code-page/qr-scan/hooks.tsx
+++ shared/provision/code-page/qr-scan/hooks.tsx
@@ -1,10 +1,12 @@
 import * as C from '@/constants'
-import QRScan from '.'
 
-const QRScanContainer = () => {
+const useQR = () => {
   const submitTextCode = C.useProvisionState(s => s.dispatch.dynamic.submitTextCode)
   const waiting = C.Waiting.useAnyWaiting(C.Provision.waitingKey)
   const onSubmitTextCode = (c: string) => submitTextCode?.(c)
-  return <QRScan onSubmitTextCode={onSubmitTextCode} waiting={waiting} />
+  return {
+    onSubmitTextCode,
+    waiting,
+  }
 }
-export default QRScanContainer
+export default useQR
