c7de85b5434721c01d6369b538a99cdcce0d3057
chrisnojima <cnojima@keyba.se>
2025-11-24 10:14:32 -0500

    WIP

--- shared/common-adapters/emoji/slow-data.tsx
+++ shared/common-adapters/emoji/slow-data.tsx
@@ -4,10 +4,10 @@ import groupBy from 'lodash/groupBy'
 import type * as Kb from '@/common-adapters'
 
 export const skinTones = ['1F3FA', '1F3FB', '1F3FC', '1F3FD', '1F3FE', '1F3FF'] as const
-export const emojiNameMap = Object.values(emojidata).reduce((res, emoji) => {
+export const emojiNameMap = Object.values(emojidata).reduce<Record<string, EmojiData>>((res, emoji) => {
   res[emoji.short_name] = emoji
   return res
-}, {} as Record<string, EmojiData>) satisfies Record<string, EmojiData>
+}, {})
 
 export type {EmojiData} from 'emoji-datasource-apple'
 
