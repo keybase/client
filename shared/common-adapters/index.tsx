d80d96b142067b7eb07635554794a06fd9ad31d8
Chris Nojima <christopher.nojima@zoom.us>
2025-12-02 11:32:24 -0500

    WIP

--- shared/common-adapters/index.tsx
+++ shared/common-adapters/index.tsx
@@ -348,7 +348,9 @@ import type * as SimpleToastT from './simple-toast'
 export declare const SimpleToast: typeof SimpleToastT.default
 Object.defineProperty(exports, 'SimpleToast', {get: () => require('./simple-toast').default})
 
+import type * as StylesT from '@/styles'
 export type * as Styles from '@/styles'
+export declare const Styles: typeof StylesT
 Object.defineProperty(exports, 'Styles', {get: () => require('@/styles')})
 
 import type * as SwitchT from './switch'
--- shared/constants/index.tsx
+++ shared/constants/index.tsx
@@ -12,18 +12,21 @@ export declare const useArchiveState: typeof Archive.useState_
 Object.defineProperty(exports, 'useArchiveState', {get: () => require('./archive').useState_})
 
 import type * as AutoResetT from './autoreset'
+export type * as AutoReset from './autoreset'
 export declare const useAutoResetState: typeof AutoResetT.useState_
 export declare const AutoReset: typeof AutoResetT
 Object.defineProperty(exports, 'useAutoResetState', {get: () => require('./autoreset').useState_})
 Object.defineProperty(exports, 'AutoReset', {get: () => require('./autoreset')})
 
 import type * as BotsT from './bots'
+export type * as Bots from './bots'
 export declare const useBotsState: typeof BotsT.useState_
 export declare const Bots: typeof BotsT
 Object.defineProperty(exports, 'useBotsState', {get: () => require('./bots').useState_})
 Object.defineProperty(exports, 'Bots', {get: () => require('./bots')})
 
 import type * as Chat2T from './chat2'
+export type * as Chat from './chat2'
 export declare const useChatState: typeof Chat2T.useState_
 export declare const Chat: typeof Chat2T
 export declare const useConvoState: typeof Chat2T.useConvoState_
@@ -40,12 +43,14 @@ Object.defineProperty(exports, 'getConvoState', {get: () => require('./chat2').g
 Object.defineProperty(exports, 'useChatContext', {get: () => require('./chat2').useContext_})
 
 import type * as ConfigT from './config'
+export type * as Config from './config'
 export declare const useConfigState: typeof ConfigT.useConfigState_
 export declare const Config: typeof ConfigT
 Object.defineProperty(exports, 'useConfigState', {get: () => require('./config').useConfigState_})
 Object.defineProperty(exports, 'Config', {get: () => require('./config')})
 
 import type * as CryptoT from './crypto'
+export type * as Crypto from './crypto'
 export declare const useCryptoState: typeof CryptoT.useState_
 export declare const Crypto: typeof CryptoT
 Object.defineProperty(exports, 'useCryptoState', {get: () => require('./crypto').useState_})
@@ -66,12 +71,14 @@ export declare const useDarkModeState: typeof DarkMode.useState_
 Object.defineProperty(exports, 'useDarkModeState', {get: () => require('./darkmode').useState_})
 
 import type * as DeepLinksT from './deeplinks'
+export type * as DeepLinks from './deeplinks'
 export declare const useDeepLinksState: typeof DeepLinksT.useState_
 export declare const DeepLinks: typeof DeepLinksT
 Object.defineProperty(exports, 'useDeepLinksState', {get: () => require('./deeplinks').useState_})
 Object.defineProperty(exports, 'DeepLinks', {get: () => require('./deeplinks')})
 
 import type * as DevicesT from './devices'
+export type * as Devices from './devices'
 export declare const useDevicesState: typeof DevicesT.useState_
 export declare const Devices: typeof DevicesT
 Object.defineProperty(exports, 'useDevicesState', {get: () => require('./devices').useState_})
@@ -86,18 +93,21 @@ export declare const useFollowerState: typeof Followers.useState_
 Object.defineProperty(exports, 'useFollowerState', {get: () => require('./followers').useState_})
 
 import type * as FST from './fs'
+export type * as FS from './fs'
 export declare const useFSState: typeof FST.useState_
 export declare const FS: typeof FST
 Object.defineProperty(exports, 'useFSState', {get: () => require('./fs').useState_})
 Object.defineProperty(exports, 'FS', {get: () => require('./fs')})
 
 import type * as GitT from './git'
+export type * as Git from './git'
 export declare const useGitState: typeof GitT.useState_
 export declare const Git: typeof GitT
 Object.defineProperty(exports, 'useGitState', {get: () => require('./git').useState_})
 Object.defineProperty(exports, 'Git', {get: () => require('./git')})
 
 import type * as GregorT from './gregor'
+export type * as Gregor from './gregor'
 export declare const Gregor: typeof GregorT
 Object.defineProperty(exports, 'Gregor', {get: () => require('./gregor')})
 
@@ -110,6 +120,7 @@ export declare const useNotifState: typeof Notifications.useState_
 Object.defineProperty(exports, 'useNotifState', {get: () => require('./notifications').useState_})
 
 import type * as PeopleT from './people'
+export type * as People from './people'
 export declare const usePeopleState: typeof PeopleT.useState_
 export declare const People: typeof PeopleT
 Object.defineProperty(exports, 'usePeopleState', {get: () => require('./people').useState_})
@@ -120,30 +131,35 @@ export declare const usePinentryState: typeof Pinentry.useState_
 Object.defineProperty(exports, 'usePinentryState', {get: () => require('./pinentry').useState_})
 
 import type * as ProfileT from './profile'
+export type * as Profile from './profile'
 export declare const useProfileState: typeof ProfileT.useState_
 export declare const Profile: typeof ProfileT
 Object.defineProperty(exports, 'useProfileState', {get: () => require('./profile').useState_})
 Object.defineProperty(exports, 'Profile', {get: () => require('./profile')})
 
 import type * as ProvisionT from './provision'
+export type * as Provision from './provision'
 export declare const useProvisionState: typeof ProvisionT.useState_
 export declare const Provision: typeof ProvisionT
 Object.defineProperty(exports, 'useProvisionState', {get: () => require('./provision').useState_})
 Object.defineProperty(exports, 'Provision', {get: () => require('./provision')})
 
 import type * as PushT from './push'
+export type * as Push from './push'
 export declare const usePushState: typeof PushT.useState_
 export declare const Push: typeof PushT
 Object.defineProperty(exports, 'usePushState', {get: () => require('./push').useState_})
 Object.defineProperty(exports, 'Push', {get: () => require('./push')})
 
 import type * as RecoverPwdT from './recover-password'
+export type * as RecoverPwd from './recover-password'
 export declare const useRecoverState: typeof RecoverPwdT.useState_
 export declare const RecoverPwd: typeof RecoverPwdT
 Object.defineProperty(exports, 'useRecoverState', {get: () => require('./recover-password').useState_})
 Object.defineProperty(exports, 'RecoverPwd', {get: () => require('./recover-password')})
 
 import type * as Router2T from './router2'
+export type * as Router2 from './router2'
 export declare const useRouterState: typeof Router2T.useState_
 export declare const makeScreen: typeof Router2T.makeScreen
 export declare const Router2: typeof Router2T
@@ -152,12 +168,14 @@ Object.defineProperty(exports, 'makeScreen', {get: () => require('./router2').ma
 Object.defineProperty(exports, 'Router2', {get: () => require('./router2')})
 
 import type * as SettingsT from './settings'
+export type * as Settings from './settings'
 export declare const Settings: typeof SettingsT
 export declare const useSettingsState: typeof SettingsT.useState_
 Object.defineProperty(exports, 'Settings', {get: () => require('./settings')})
 Object.defineProperty(exports, 'useSettingsState', {get: () => require('./settings').useState_})
 
 import type * as SettingsChatT from './settings-chat'
+export type * as SettingsChat from './settings-chat'
 export declare const useSettingsChatState: typeof SettingsChatT.useState_
 export declare const SettingsChat: typeof SettingsChatT
 Object.defineProperty(exports, 'useSettingsChatState', {get: () => require('./settings-chat').useState_})
@@ -204,22 +222,26 @@ Object.defineProperty(exports, 'useSettingsPasswordState', {
 })
 
 import type * as SettingsPhoneT from './settings-phone'
+export type * as SettingsPhone from './settings-phone'
 export declare const SettingsPhone: typeof SettingsPhoneT
 export declare const useSettingsPhoneState: typeof SettingsPhoneT.useState_
 Object.defineProperty(exports, 'SettingsPhone', {get: () => require('./settings-phone')})
 Object.defineProperty(exports, 'useSettingsPhoneState', {get: () => require('./settings-phone').useState_})
 
 import type * as SignupT from './signup'
+export type * as Signup from './signup'
 export declare const useSignupState: typeof SignupT.useState_
 export declare const Signup: typeof SignupT
 Object.defineProperty(exports, 'useSignupState', {get: () => require('./signup').useState_})
 Object.defineProperty(exports, 'Signup', {get: () => require('./signup')})
 
 import type * as TabsT from './tabs'
+export type * as Tabs from './tabs'
 export declare const Tabs: typeof TabsT
 Object.defineProperty(exports, 'Tabs', {get: () => require('./tabs')})
 
 import type * as TeamBuildingT from './team-building'
+export type * as TeamBuilding from './team-building'
 export declare const TBProvider: typeof TeamBuildingT.TBProvider_
 export declare const TBstores: typeof TeamBuildingT.stores_
 export declare const useTBContext: typeof TeamBuildingT.useContext_
@@ -230,12 +252,14 @@ Object.defineProperty(exports, 'useTBContext', {get: () => require('./team-build
 Object.defineProperty(exports, 'TeamBuilding', {get: () => require('./team-building')})
 
 import type * as TeamsT from './teams'
+export type * as Teams from './teams'
 export declare const useTeamsState: typeof TeamsT.useState_
 export declare const Teams: typeof TeamsT
 Object.defineProperty(exports, 'useTeamsState', {get: () => require('./teams').useState_})
 Object.defineProperty(exports, 'Teams', {get: () => require('./teams')})
 
 import type * as TrackerT from './tracker2'
+export type * as Tracker from './tracker2'
 export declare const useTrackerState: typeof TrackerT.useState_
 export declare const Tracker: typeof TrackerT
 Object.defineProperty(exports, 'useTrackerState', {get: () => require('./tracker2').useState_})
@@ -246,18 +270,21 @@ export declare const useUFState: typeof UnlockFolders.useState_
 Object.defineProperty(exports, 'useUFState', {get: () => require('./unlock-folders').useState_})
 
 import type * as UsersT from './users'
+export type * as Users from './users'
 export declare const useUsersState: typeof UsersT.useState_
 export declare const Users: typeof UsersT
 Object.defineProperty(exports, 'useUsersState', {get: () => require('./users').useState_})
 Object.defineProperty(exports, 'Users', {get: () => require('./users')})
 
 import type * as WaitingT from './waiting'
+export type * as Waiting from './waiting'
 export declare const useWaitingState: typeof WaitingT.useState_
 export declare const Waiting: typeof WaitingT
 Object.defineProperty(exports, 'useWaitingState', {get: () => require('./waiting').useState_})
 Object.defineProperty(exports, 'Waiting', {get: () => require('./waiting')})
 
 import type * as WalletsT from './wallets'
+export type * as Wallets from './wallets'
 export declare const Wallets: typeof WalletsT
 export declare const useWalletsState: typeof WalletsT.useState_
 Object.defineProperty(exports, 'Wallets', {get: () => require('./wallets')})
