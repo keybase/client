e6efd72abeae2b062a88f9de2cc2d5b811778d9c
chrisnojima <cnojima@keyba.se>
2025-12-16 09:26:47 -0500

    small deps expo .29 (#28698)
    * small deps expo .29

--- shared/test/setupTestFrameworkScriptFile-rn.js
+++
@@ -1,7 +0,0 @@
-// Used by jest.
-// DON'T use es6 stuff here
-
-beforeEach(() => {
-  // require('../app/preload.native')
-})
-afterEach(() => {})
--- shared/test/setupTestFrameworkScriptFile.js
+++
@@ -1,7 +0,0 @@
-// Used by jest.
-// DON'T use es6 stuff here
-
-beforeEach(() => {
-  // require('../desktop/app/preload.desktop')
-})
-afterEach(() => {})
