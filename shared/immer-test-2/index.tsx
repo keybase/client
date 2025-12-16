2d7e6a42021b299a2add8028188d7bedc3c1ff89
Chris Nojima <christopher.nojima@zoom.us>
2025-12-16 12:57:45 -0500

    WIP

--- shared/immer-test-2/index.tsx
+++ shared/immer-test-2/index.tsx
@@ -1,6 +1,30 @@
 import * as React from 'react'
 import {useStore} from './store'
 
+const serializeForDisplay = (value: unknown): unknown => {
+  if (value instanceof Set) {
+    return Array.from(value).map(serializeForDisplay)
+  }
+  if (value instanceof Map) {
+    const obj: Record<string, unknown> = {}
+    for (const [key, val] of value.entries()) {
+      obj[String(key)] = serializeForDisplay(val)
+    }
+    return obj
+  }
+  if (value && typeof value === 'object' && !Array.isArray(value)) {
+    const obj: Record<string, unknown> = {}
+    for (const [key, val] of Object.entries(value)) {
+      obj[key] = serializeForDisplay(val)
+    }
+    return obj
+  }
+  if (Array.isArray(value)) {
+    return value.map(serializeForDisplay)
+  }
+  return value
+}
+
 const TestApp = () => {
   const toggleLocalReaction = useStore(s => s.toggleLocalReaction)
   const message = useStore(s => s.messageMap.get(1))
@@ -18,7 +42,7 @@ const TestApp = () => {
   return (
     <div>
       <button onClick={handleClick}>Toggle Reaction</button>
-      <p>{JSON.stringify(message, null, 2)}</p>
+      <pre>{JSON.stringify(serializeForDisplay(message), null, 2)}</pre>
     </div>
   )
 }
