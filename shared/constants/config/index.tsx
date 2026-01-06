cbf9bc4ad6742d6d76c01cad098dc198a83e8123
chrisnojima-zoom <83838430+chrisnojima-zoom@users.noreply.github.com>
2026-01-06 17:20:53 -0500

    store shift 6 (#28765)
    * WIP
    
    * WIP
    
    * WIP (#28767)
    
    ---------
    
    Co-authored-by: chrisnojima <cnojima@keyba.se>

--- shared/constants/config/index.tsx
+++ shared/constants/config/index.tsx
@@ -15,7 +15,6 @@ import {invalidPasswordErrorString} from './util'
 import {navigateAppend} from '../router2/util'
 import {storeRegistry} from '../store-registry'
 import {useWhatsNewState} from '../whats-new'
-import {getSelectedConversation} from '@/constants/chat2/common'
 
 type Store = T.Immutable<{
   forceSmallNav: boolean
@@ -244,15 +243,6 @@ export const useConfigState = Z.createZustand<State>((set, get) => {
     set(s => {
       s.gregorReachable = r
     })
-    // Re-get info about our account if you log in/we're done handshaking/became reachable
-    if (r === T.RPCGen.Reachable.yes) {
-      // not in waiting state
-      if (storeRegistry.getState('daemon').handshakeWaiters.size === 0) {
-        ignorePromise(storeRegistry.getState('daemon').dispatch.loadDaemonBootstrapStatus())
-      }
-    }
-
-    storeRegistry.getState('teams').dispatch.eagerLoadTeams()
   }
 
   const setGregorPushState = (state: T.RPCGen.Gregor1.State) => {
@@ -299,13 +289,6 @@ export const useConfigState = Z.createZustand<State>((set, get) => {
       set(s => {
         s.appFocused = f
       })
-
-      if (!isMobile || !f) {
-        return
-      }
-      const {dispatch} = storeRegistry.getConvoState(getSelectedConversation())
-      dispatch.loadMoreMessages({reason: 'foregrounding'})
-      dispatch.markThreadAsRead()
     },
     checkForUpdate: () => {
       const f = async () => {
@@ -402,8 +385,6 @@ export const useConfigState = Z.createZustand<State>((set, get) => {
       set(s => {
         s.installerRanCount++
       })
-
-      storeRegistry.getState('fs').dispatch.checkKbfsDaemonRpcStatus()
     },
     loadIsOnline: () => {
       const f = async () => {
@@ -764,11 +745,6 @@ export const useConfigState = Z.createZustand<State>((set, get) => {
 
       if (!changed) return
 
-      if (loggedIn) {
-        ignorePromise(storeRegistry.getState('daemon').dispatch.loadDaemonBootstrapStatus())
-      }
-      storeRegistry.getState('daemon').dispatch.loadDaemonAccounts()
-
       const {loadOnStart} = get().dispatch
       if (loggedIn) {
         if (!causedByStartup) {
@@ -784,14 +760,6 @@ export const useConfigState = Z.createZustand<State>((set, get) => {
       } else {
         Z.resetAllStores()
       }
-
-      if (loggedIn) {
-        storeRegistry.getState('fs').dispatch.checkKbfsDaemonRpcStatus()
-      }
-
-      if (!causedByStartup) {
-        ignorePromise(storeRegistry.getState('daemon').dispatch.refreshAccounts())
-      }
     },
     setLoginError: error => {
       set(s => {
@@ -806,9 +774,6 @@ export const useConfigState = Z.createZustand<State>((set, get) => {
       set(s => {
         s.mobileAppState = nextAppState
       })
-      if (nextAppState === 'background' && storeRegistry.getState('chat').inboxSearch) {
-        storeRegistry.getState('chat').dispatch.toggleInboxSearch(false)
-      }
     },
     setNotifySound: n => {
       set(s => {
