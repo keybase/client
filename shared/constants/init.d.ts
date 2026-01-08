a47308496b4ce6086be67ce826e8a1f72a41cef3
Chris Nojima <christopher.nojima@zoom.us>
2026-01-08 15:35:39 -0500

    WIP

--- shared/constants/init.d.ts
+++ shared/constants/init.d.ts
@@ -1,2 +1,5 @@
+import type * as EngineGen from '@/actions/engine-gen-gen'
+
 export declare function initPlatformListener(): void
+export declare function onEngineIncoming(action: EngineGen.Actions): void
 
