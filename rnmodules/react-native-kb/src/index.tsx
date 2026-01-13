0bd00dbdce2bf1fa2b398dd6277d957604e92095
chrisnojima <cnojima@keyba.se>
2026-01-13 15:19:44 -0500

    WIP

--- rnmodules/react-native-kb/src/index.tsx
+++ rnmodules/react-native-kb/src/index.tsx
@@ -177,7 +177,7 @@ export const shareListenersRegistered = (): void => {
 }
 
 export const processVideo = (path: string): Promise<string> => {
-  return Kb.processVideo(path)
+  return Kb.processVideo(Platform.OS === 'android' ? path.replace('file://', '') : path)
 }
 export const getNativeEmitter = () => {
   return new NativeEventEmitter(Kb as any)
