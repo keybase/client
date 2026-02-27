c6bd61b5d2a3762b54562d5db94ae63e33356ea6
chrisnojima <cnojima@keyba.se>
2026-02-26 21:33:29 -0500

    WIP

--- shared/ios/Keybase/AppDelegate.swift
+++ shared/ios/Keybase/AppDelegate.swift
@@ -469,7 +469,7 @@ class ReactNativeDelegate: ExpoReactNativeFactoryDelegate {
 
   override func bundleURL() -> URL? {
 #if DEBUG
-    return RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: ".expo/.virtual-metro-entry")
+    return RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
 #else
     return Bundle.main.url(forResource: "main", withExtension: "jsbundle")
 #endif
