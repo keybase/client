102f4feb0af631a155bbe4a427bbc608c5a8a3fa
chrisnojima <cnojima@keyba.se>
2026-03-18 16:33:29 -0400

    update deps, add skill (#29033)

--- shared/desktop/electron-sums.tsx
+++ shared/desktop/electron-sums.tsx
@@ -1,10 +1,10 @@
 // Generated with: ./extract-electron-shasums.sh {ver}
 // prettier-ignore
 export const electronChecksums = {
-  'electron-v41.0.2-darwin-arm64.zip': '8e18ef53da62bca6132508721c1f94e06b5773b48d366b95e593479892f0a2fe',
-  'electron-v41.0.2-darwin-x64.zip': '446bd464236cec71fdfb5562bae5d092cbf27ed5213feac39b7667081578b9c6',
-  'electron-v41.0.2-linux-arm64.zip': '5eff34c3c28f7a35d8f90ba23b58e5d5125b7f8fb39be12416dc61fe28b24c21',
-  'electron-v41.0.2-linux-x64.zip': '9f1f347dc81e94b1207fc6ae4c1f1d189d26d3356da6f99e7e46087c84d28f10',
-  'electron-v41.0.2-win32-x64.zip': 'dcd36396a606a5ae2f5651b4ee6bb463a624dbf15f786eda57cee2cc361c138c',
-  'hunspell_dictionaries.zip': 'f123e7c08ee03680df480d054cf0a914ea1eda18c83ce97b43a7b4cb1cd2354e',
+  'electron-v41.0.3-darwin-arm64.zip': 'd8aeeb263b234a243411e02b3828e9349fe976417798334cba9f34f6eae28d7e',
+  'electron-v41.0.3-darwin-x64.zip': '08edd23e1b1ee8c06a2fb4ef6d5e41c9ca19d3f6c70a2e1432a33c44e06b3170',
+  'electron-v41.0.3-linux-arm64.zip': '378fcbbc5258a33ae987348d0bbec85c12f0e11dfcb69564adc112c06d20593b',
+  'electron-v41.0.3-linux-x64.zip': 'ca1a4963ef855f67afd34033ccfe4ca4cc986e5b58a77c2d0cf88ff3fc9933ee',
+  'electron-v41.0.3-win32-x64.zip': '93346b31dfcd779c4c4cdfdfb2e3f9642e7a9888e388a8971ff3e95dbf948887',
+  'hunspell_dictionaries.zip': 'b672905fafcbbe8315a4552be439a0000d9cda80c5380f76d0f22bdc23bfbe57',
 }
