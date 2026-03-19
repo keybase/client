fb269879a0854a437db04a2e63b1e68716b9c92f
Chris Nojima <christopher.nojima@zoom.us>
2026-03-19 18:01:37 -0400

    WIP

--- shared/jest.config.js
+++ shared/jest.config.js
@@ -14,6 +14,7 @@ module.exports = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
   },
+  setupFiles: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'node',
   testMatch: ['<rootDir>/stores/**/*.test.ts'],
   transform: {
