f62beda418fe875b7bbb597f2b6b352b283dfe65
Chris Nojima <christopher.nojima@zoom.us>
2026-03-24 14:22:37 -0400

    WIP

--- shared/desktop/app/html-root.desktop.tsx
+++ shared/desktop/app/html-root.desktop.tsx
@@ -3,7 +3,7 @@ import path from 'path'
 
 const isWindows = process.platform === 'win32'
 const htmlPrefix = isWindows ? `file:///` : `file://`
-const hotRoot = 'http://localhost:4000'
+const hotRoot = 'http://localhost:4000/dist'
 const distRoot = path.resolve(__DEV__ || __PROFILE__ ? './desktop/dist' : path.join(app.getAppPath(), './desktop/dist'))
 const fileRoot = `${htmlPrefix}${(`${distRoot}/`).replaceAll(path.sep, '/')}`
 
