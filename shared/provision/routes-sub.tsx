c8d77bdb73a1da6d4299e87544cc3d1049480cb5
Chris Nojima <christopher.nojima@zoom.us>
2026-03-30 12:39:24 -0400

    WIP

--- shared/provision/routes-sub.tsx
+++ shared/provision/routes-sub.tsx
@@ -15,37 +15,42 @@ const CodePageHeaderLeft = () => {
   )
 }
 
+const provisionRouteOptions = <T extends Record<string, unknown>>(options: T) => ({
+  ...options,
+  ...(Kb.Styles.isElectron ? {headerShown: false} : {}),
+})
+
 export const newRoutes = {
   codePage: C.makeScreen(React.lazy(async () => import('./code-page/container')), {
-    getOptions: {
+    getOptions: provisionRouteOptions({
       headerLeft: () => <CodePageHeaderLeft />,
       title: '',
-    },
+    }),
   }),
-  error: {getOptions: {title: 'Error'}, screen: React.lazy(async () => import('./error'))},
+  error: {getOptions: provisionRouteOptions({title: 'Error'}), screen: React.lazy(async () => import('./error'))},
   forgotUsername: {
-    getOptions: {title: 'Recover username'},
+    getOptions: provisionRouteOptions({title: 'Recover username'}),
     screen: React.lazy(async () => import('./forgot-username')),
   },
   // gpgSign,
   paperkey: {
-    getOptions: {title: 'Enter paper key'},
+    getOptions: provisionRouteOptions({title: 'Enter paper key'}),
     screen: React.lazy(async () => import('./paper-key')),
   },
   password: {
-    getOptions: {title: 'Enter password'},
+    getOptions: provisionRouteOptions({title: 'Enter password'}),
     screen: React.lazy(async () => import('./password')),
   },
   selectOtherDevice: {
-    getOptions: {title: 'Authorize this device'},
+    getOptions: provisionRouteOptions({title: 'Authorize this device'}),
     screen: React.lazy(async () => import('./select-other-device-connected')),
   },
   setPublicName: {
-    getOptions: {title: 'Name this device'},
+    getOptions: provisionRouteOptions({title: 'Name this device'}),
     screen: React.lazy(async () => import('./set-public-name')),
   },
   username: C.makeScreen(React.lazy(async () => import('./username-or-email')), {
-    getOptions: {title: 'Log in'},
+    getOptions: provisionRouteOptions({title: 'Log in'}),
   }),
 }
 
