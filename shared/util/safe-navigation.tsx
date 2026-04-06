ca0f5d0317dac19d0f4295461ca108bfcf2b0832
chrisnojima-zoom <83838430+chrisnojima-zoom@users.noreply.github.com>
2026-04-06 19:13:25 -0400

    fix safe append (#29119)

--- shared/util/safe-navigation.tsx
+++ shared/util/safe-navigation.tsx
@@ -1,24 +1,14 @@
 import * as C from '@/constants'
 import {useIsFocused} from '@react-navigation/core'
-import type {AllOptionalParamRouteKeys, NoParamRouteKeys, ParamRouteKeys, RootParamList} from '@/router-v2/route-params'
+import type {NavigateAppendType} from '@/router-v2/route-params'
 
 export const useSafeNavigation = () => {
   const isFocused = useIsFocused()
   const navigateUp = C.Router2.navigateUp
   const navigateAppend = C.Router2.navigateAppend
-  function safeNavigateAppend<RouteName extends NoParamRouteKeys | AllOptionalParamRouteKeys>(path: RouteName, replace?: boolean): void
-  function safeNavigateAppend<RouteName extends ParamRouteKeys>(
-    path: {name: RouteName; params: RootParamList[RouteName]},
-    replace?: boolean
-  ): void
-  function safeNavigateAppend(
-    path: NoParamRouteKeys | AllOptionalParamRouteKeys | {name: ParamRouteKeys; params: object | undefined},
-    replace?: boolean
-  ) {
-    return isFocused && navigateAppend(path as never, replace)
-  }
   return {
-    safeNavigateAppend,
+    safeNavigateAppend: (path: NavigateAppendType, replace?: boolean) =>
+      isFocused && navigateAppend(path as never, replace),
     safeNavigateUp: () => isFocused && navigateUp(),
   }
 }
