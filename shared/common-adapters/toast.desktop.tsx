71add7f3f406d3edd443d5874881a06b16f75f03
Chris Nojima <christopher.nojima@zoom.us>
2026-04-07 09:46:17 -0400

    fix stuck toast

--- shared/common-adapters/toast.desktop.tsx
+++ shared/common-adapters/toast.desktop.tsx
@@ -1,3 +1,5 @@
+import * as C from '@/constants'
+import * as React from 'react'
 import type {Props} from './toast'
 import Popup from './popup'
 import * as Styles from '@/styles'
@@ -5,23 +7,47 @@ import './toast.css'
 
 const positionFallbacks = [] as const
 
-const Toast = (props: Props) => (
-  <Popup
-    attachTo={props.attachTo}
-    propagateOutsideClicks={true}
-    position={props.position}
-    containerStyle={styles.float}
-    offset={4}
-    positionFallbacks={positionFallbacks}
-  >
-    <div
-      className={Styles.classNames({visible: props.visible}, props.className, 'fadeBox')}
-      style={Styles.collapseStyles([styles.container, props.containerStyle]) as React.CSSProperties}
+const Toast = (props: Props) => {
+  const [dismissedOnBlur, setDismissedOnBlur] = React.useState(false)
+  const lastVisibleRef = React.useRef(props.visible)
+
+  React.useEffect(() => {
+    if (props.visible && !lastVisibleRef.current) {
+      setDismissedOnBlur(false)
+    } else if (!props.visible) {
+      setDismissedOnBlur(false)
+    }
+    lastVisibleRef.current = props.visible
+  }, [props.visible])
+
+  C.Router2.useSafeFocusEffect(() => {
+    return () => {
+      setDismissedOnBlur(true)
+    }
+  })
+
+  return (
+    <Popup
+      attachTo={props.attachTo}
+      propagateOutsideClicks={true}
+      position={props.position}
+      containerStyle={styles.float}
+      offset={4}
+      positionFallbacks={positionFallbacks}
     >
-      {props.children}
-    </div>
-  </Popup>
-)
+      <div
+        className={Styles.classNames(
+          {visible: props.visible && !dismissedOnBlur},
+          props.className,
+          'fadeBox'
+        )}
+        style={Styles.collapseStyles([styles.container, props.containerStyle]) as React.CSSProperties}
+      >
+        {props.children}
+      </div>
+    </Popup>
+  )
+}
 export default Toast
 
 const styles = Styles.styleSheetCreate(() => ({
