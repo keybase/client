5637c4972fef747a7b53556d0107289077cbcf46
Chris Nojima <christopher.nojima@zoom.us>
2026-04-10 11:28:08 -0400

    WIP

--- shared/chat/inbox-and-conversation.native.tsx
+++ shared/chat/inbox-and-conversation.native.tsx
@@ -1,11 +1,14 @@
 import Inbox from './inbox'
 import {InboxAndConversationShell, type InboxAndConversationProps} from './inbox-and-conversation-shared'
+import {useInboxSearch} from './inbox/use-inbox-search'
 
 export default function InboxAndConversationNative(props: InboxAndConversationProps) {
+  const search = useInboxSearch()
+
   return (
     <InboxAndConversationShell
       {...props}
-      leftPane={<Inbox conversationIDKey={props.conversationIDKey} />}
+      leftPane={<Inbox conversationIDKey={props.conversationIDKey} search={search} />}
     />
   )
 }
