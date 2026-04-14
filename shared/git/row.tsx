e8130c6aff3b130d929269de8a05c2606f67add0
Chris Nojima <christopher.nojima@zoom.us>
2026-04-14 10:46:13 -0400

    WIP

--- shared/git/row.tsx
+++ shared/git/row.tsx
@@ -7,6 +7,7 @@ import {openURL} from '@/util/misc'
 import {useTrackerState} from '@/stores/tracker'
 import * as FS from '@/stores/fs'
 import {useCurrentUserState} from '@/stores/current-user'
+import {navToProfile} from '@/constants/router'
 
 export const NewContext = React.createContext<ReadonlySet<string>>(new Set())
 
@@ -79,7 +80,11 @@ function ConnectedRow(ownProps: OwnProps) {
 
   const showTracker = useTrackerState(s => s.dispatch.showTracker)
   const openUserTracker = (username: string) => {
-    showTracker(username)
+    if (C.isMobile) {
+      navToProfile(username)
+    } else {
+      showTracker(username)
+    }
   }
 
   const onBrowseGitRepo = () =>
