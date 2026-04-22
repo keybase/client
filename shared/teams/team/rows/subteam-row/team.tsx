2adb56c5f6ab7b3dac2a38a4dd51473822703cbd
Chris Nojima <christopher.nojima@zoom.us>
2026-04-22 11:23:54 -0400

    WIP

--- shared/teams/team/rows/subteam-row/team.tsx
+++ shared/teams/team/rows/subteam-row/team.tsx
@@ -1,2 +1,27 @@
+import * as C from '@/constants'
+import type * as T from '@/constants/types'
 import TeamRow from '@/teams/main/team-row'
-export default TeamRow
+import * as Teams from '@/stores/teams'
+
+type Props = {
+  teamID: T.Teams.TeamID
+}
+
+const SubteamTeamRow = ({teamID}: Props) => {
+  const item = Teams.useTeamsState(
+    C.useShallow(s => {
+      const teamMeta = Teams.getTeamMeta(s, teamID)
+      return {
+        activityLevel: s.activityLevels.teams.get(teamID) || 'none',
+        badgeCount: Teams.getTeamRowBadgeCount(s.newTeamRequests, s.teamIDToResetUsers, teamID),
+        id: teamID,
+        isNew: s.newTeams.has(teamID),
+        teamMeta,
+      }
+    })
+  )
+
+  return <TeamRow {...item} />
+}
+
+export default SubteamTeamRow
