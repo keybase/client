eba2772d1d25ad484f6c6689433d2284f17b8f25
chrisnojima <cnojima@keyba.se>
2026-05-27 12:24:10 -0400

    fix unused browser variable in e2e test files

--- shared/tests/e2e/electron/smoke/git.test.ts
+++ shared/tests/e2e/electron/smoke/git.test.ts
@@ -1,13 +1,12 @@
-import {test, expect, type Browser, type Page} from '@playwright/test'
+import {test, expect, type Page} from '@playwright/test'
 import {connectToElectron, disconnect} from '../helpers/connect'
 import {navigateToGit} from '../helpers/navigate'
 import {GIT_REPO_LIST} from '../../shared/test-ids'
 
-let browser: Browser
 let page: Page
 
 test.beforeAll(async () => {
-  ;({browser, page} = await connectToElectron())
+  ;({page} = await connectToElectron())
 })
 
 test.afterAll(async () => {
--- shared/tests/e2e/electron/smoke/teams.test.ts
+++ shared/tests/e2e/electron/smoke/teams.test.ts
@@ -1,13 +1,12 @@
-import {test, expect, type Browser, type Page} from '@playwright/test'
+import {test, expect, type Page} from '@playwright/test'
 import {connectToElectron, disconnect} from '../helpers/connect'
 import {navigateToTeams} from '../helpers/navigate'
 import {TEAMS_LIST} from '../../shared/test-ids'
 
-let browser: Browser
 let page: Page
 
 test.beforeAll(async () => {
-  ;({browser, page} = await connectToElectron())
+  ;({page} = await connectToElectron())
 })
 
 test.afterAll(async () => {
