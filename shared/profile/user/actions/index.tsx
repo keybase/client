69ea954f68b7dd865b655673c2cb4d57bcdd129b
chrisnojima <cnojima@keyba.se>
2026-05-29 17:01:04 -0400

    fix profile dropdown button full-width regression on mobile (#29265)

--- shared/profile/user/actions/index.tsx
+++ shared/profile/user/actions/index.tsx
@@ -208,7 +208,7 @@ const DropdownButton = (p: DropdownProps) => {
   const {showPopup, popup, popupAnchor} = Kb.usePopup2(makePopup)
 
   return (
-    <Kb.ClickableBox3 direction="horizontal" fullWidth={true} gap="xsmall" onClick={showPopup} ref={popupAnchor}>
+    <Kb.ClickableBox3 direction="horizontal" gap="xsmall" onClick={showPopup} ref={popupAnchor}>
       <Kb.Button onClick={undefined} mode="Secondary" style={styles.dropdownButton}>
         <Kb.Icon color={Kb.Styles.globalColors.blue} type="iconfont-ellipsis" />
       </Kb.Button>
