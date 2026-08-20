5ae3e5210d960fdcef17cf95a0f56c2e6e95142b
Joshua Blum <joshua.blum@zoom.us>
2026-08-20 18:46:23 +0300

    x

--- go/updater/util/unzip_nix_test.go
+++ go/updater/util/unzip_nix_test.go
@@ -56,12 +56,12 @@ func TestUnzipFileModTime(t *testing.T) {
 	dirMod := fileInfo.ModTime()
 	diffDir := dirMod.Sub(now)
 	t.Logf("Diff (dir): %s", diffDir)
-	assert.GreaterOrEqual(t, diffDir, 0, "now=%s, dirtime=%s", now, dirMod)
+	assert.GreaterOrEqual(t, diffDir, time.Duration(0), "now=%s, dirtime=%s", now, dirMod)
 
 	fileInfo, err = os.Stat(filepath.Join(destinationPath, "test", "testfile"))
 	require.NoError(t, err)
 	fileMod := fileInfo.ModTime()
 	diffFile := fileMod.Sub(now)
 	t.Logf("Diff (file): %s", diffFile)
-	assert.GreaterOrEqual(t, diffFile, 0, "now=%s, filetime=%s", now, fileMod)
+	assert.GreaterOrEqual(t, diffFile, time.Duration(0), "now=%s, filetime=%s", now, fileMod)
 }
