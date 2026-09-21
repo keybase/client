97b3d788291ef4a623ca63a98ec3d584ccc0a0d6
chrisnojima <cnojima@keyba.se>
2026-09-21 11:03:36 -0400

    feat(protocol): add NotifyApp.mobileAppStateChanged
    The service derives the app's lifecycle state; this is how a client learns
    it. Deriving it a second time from the OS would mean two answers -- on iOS
    from two different notification streams -- with nothing ordering them against
    each other.
    
    Additive: nothing sends or handles it yet.

--- go/protocol/keybase1/notify_app.go
+++ go/protocol/keybase1/notify_app.go
@@ -13,8 +13,13 @@ import (
 type ExitArg struct {
 }
 
+type MobileAppStateChangedArg struct {
+	State MobileAppState `codec:"state" json:"state"`
+}
+
 type NotifyAppInterface interface {
 	Exit(context.Context) error
+	MobileAppStateChanged(context.Context, MobileAppState) error
 }
 
 func NotifyAppProtocol(i NotifyAppInterface) rpc.Protocol {
@@ -31,6 +36,21 @@ func NotifyAppProtocol(i NotifyAppInterface) rpc.Protocol {
 					return
 				},
 			},
+			"mobileAppStateChanged": {
+				MakeArg: func() any {
+					var ret [1]MobileAppStateChangedArg
+					return &ret
+				},
+				Handler: func(ctx context.Context, args any) (ret any, err error) {
+					typedArgs, ok := args.(*[1]MobileAppStateChangedArg)
+					if !ok {
+						err = rpc.NewTypeError((*[1]MobileAppStateChangedArg)(nil), args)
+						return
+					}
+					err = i.MobileAppStateChanged(ctx, typedArgs[0].State)
+					return
+				},
+			},
 		},
 	}
 }
@@ -41,5 +61,11 @@ type NotifyAppClient struct {
 
 func (c NotifyAppClient) Exit(ctx context.Context) (err error) {
 	err = c.Cli.Notify(ctx, "keybase.1.NotifyApp.exit", []any{ExitArg{}}, 0*time.Millisecond)
+	return
+}
+
+func (c NotifyAppClient) MobileAppStateChanged(ctx context.Context, state MobileAppState) (err error) {
+	__arg := MobileAppStateChangedArg{State: state}
+	err = c.Cli.Notify(ctx, "keybase.1.NotifyApp.mobileAppStateChanged", []any{__arg}, 0*time.Millisecond)
 	return
 }
